3a313863771ef26237b6b55a31f448b5bfa34366
Oleh Kravets <oleg.kravets92@gmail.com>
2020-12-04 19:35:53 +0200

    Tests passed, style issues fixed

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -5,7 +5,6 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.ArrayList;
 import java.util.List;
 
 public class WorkWithFile {
@@ -19,27 +18,24 @@ public class WorkWithFile {
         try {
             readData = Files.readAllLines(fileToRead.toPath());
         } catch (IOException e) {
-            throw new RuntimeException("I wasn't able to read the file", e);
+            throw new RuntimeException("Can't correctly read data from file "
+                    + fileToRead.getName(), e);
         }
         return readData;
     }
 
-    private static void writeFile(String filePath, List<String> data) {
+    private static void writeFile(String filePath, String data) {
         File fileToWrite = new File(filePath);
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(fileToWrite))) {
-            for (int i = 0; i < data.size(); i++) {
-                bw.write(data.get(i));
-                if (i <= data.size() - 1) {
-                    bw.write("\n");
-                }
-                bw.flush();
-            }
+            bw.write(data);
+            bw.flush();
         } catch (IOException e) {
-            throw new RuntimeException("Wow... something went wrong, couldn't write the file");
+            throw new RuntimeException("Wow... something went wrong,"
+                    + "couldn't write the " + fileToWrite.getName(), e);
         }
     }
 
-    private static List<String> reportStatistics(List<String> list) {
+    private static String reportStatistics(List<String> list) {
         int supply = 0;
         int buy = 0;
 
@@ -55,15 +51,15 @@ public class WorkWithFile {
         return reportConstructor(supply, buy);
     }
 
-    private static List<String> reportConstructor(int supply, int buy) {
-        List<String> list = new ArrayList<>();
-        list.add(new StringBuilder().append(FILE_FIELDS[0])
-                .append(",").append(supply).append("\n").toString());
-        list.add(new StringBuilder().append(FILE_FIELDS[1])
-                .append(",").append(buy).append("\n").toString());
-        list.add(new StringBuilder().append(FILE_FIELDS[2])
-                .append(",").append(supply - buy).append("\n").toString());
-        return list;
+    private static String reportConstructor(int supply, int buy) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(FILE_FIELDS[0]).append(",").append(supply)
+                .append(System.lineSeparator())
+                .append(FILE_FIELDS[1]).append(",").append(buy)
+                .append(System.lineSeparator())
+                .append(FILE_FIELDS[2]).append(",").append(supply - buy)
+                .append(System.lineSeparator());
+        return sb.toString();
     }
 
     public void getStatistic(String fromFileName, String toFileName) {
