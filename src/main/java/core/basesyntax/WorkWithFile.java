eb62cbb88498605fb3159828dd82ffb155403f78
Fedenko Maksym <fedenko.maxim.a@gmail.com>
2020-12-04 18:35:47 +0200

    added solution

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,8 +1,50 @@
 package core.basesyntax;
 
+import java.io.*;
+
 public class WorkWithFile {
 
     public void getStatistic(String fromFileName, String toFileName) {
+        writeFile(toFileName, readFileAndCalculate(fromFileName));
+    }
 
+    private String readFileAndCalculate(String fileName) {
+        StringBuilder returnValue = new StringBuilder();
+        String[] workList;
+        int supply = 0;
+        int buy = 0;
+        File file = new File(fileName);
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            StringBuilder stringBuilder = new StringBuilder();
+            String value = bufferedReader.readLine();
+            while (value != null) {
+                stringBuilder.append(value).append(",");
+                value = bufferedReader.readLine();
+            }
+            workList = stringBuilder.toString().split(",");
+            for (int i = 0; i < workList.length; i++) {
+                if (workList[i].equals("supply")) {
+                    supply += Integer.parseInt(workList[i + 1]);
+                } else if (workList[i].equals("buy")) {
+                    buy += Integer.parseInt(workList[i + 1]);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read data from file.", e);
+        }
+        return returnValue.append("supply").append(",").append(supply)
+                .append(System.lineSeparator()).append("buy").append(",").append(buy)
+                .append(System.lineSeparator()).append("result").append(",")
+                .append(supply - buy).toString();
+    }
+
+    private void writeFile(String fileName, String data) {
+        File file = new File(fileName);
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
+            bufferedWriter.write(data);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write data file", e);
+        }
     }
 }
