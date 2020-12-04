867a743ee64e8f5f4be54aaa1a0851338697eb49
Oleh Kravets <oleg.kravets92@gmail.com>
2020-12-04 18:39:12 +0200

    Added empty readFile, writeFile, createReport methods

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,8 +1,24 @@
 package core.basesyntax;
 
+import java.util.List;
+
 public class WorkWithFile {
 
     public void getStatistic(String fromFileName, String toFileName) {
 
+    }
+
+    public static List<Object> readFile(String filePath) {
+        return null;
+    }
+
+    public static boolean writeFile(String filePath, List<Object> data) {
+        return true;
+    }
+
+    public static List<Object> createReport(List<Object> list) {
+
+
+        return null;
     }
 }
