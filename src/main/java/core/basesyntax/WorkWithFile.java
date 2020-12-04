2dd404c2e5294cbbeeea120c51a5470b1c63de5a
Oleh Kravets <oleg.kravets92@gmail.com>
2020-12-04 19:21:51 +0200

    getStatistic was implemented

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -5,9 +5,13 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.List;
 
 public class WorkWithFile {
+
+    private static final String[] FILE_FIELDS
+            = new String[] {"supply", "buy", "result"};
 
     private static List<String> readFile(String filePath) {
         File fileToRead = new File(filePath);
@@ -35,11 +39,35 @@ public class WorkWithFile {
         }
     }
 
-    private static List<String> createReport(List<String> list) {
-        return null;
+    private static List<String> reportStatistics(List<String> list) {
+        int supply = 0;
+        int buy = 0;
+
+        for (String line : list) {
+            if (line.contains(FILE_FIELDS[0])) {
+                supply += Integer.parseInt(line.split(",")[1]);
+            }
+            if (line.contains(FILE_FIELDS[1])) {
+                buy += Integer.parseInt(line.split(",")[1]);
+            }
+        }
+
+        return reportConstructor(supply, buy);
+    }
+
+    private static List<String> reportConstructor(int supply, int buy) {
+        List<String> list = new ArrayList<>();
+        list.add(new StringBuilder().append(FILE_FIELDS[0])
+                .append(",").append(supply).append("\n").toString());
+        list.add(new StringBuilder().append(FILE_FIELDS[1])
+                .append(",").append(buy).append("\n").toString());
+        list.add(new StringBuilder().append(FILE_FIELDS[2])
+                .append(",").append(supply - buy).append("\n").toString());
+        return list;
     }
 
     public void getStatistic(String fromFileName, String toFileName) {
-
+        List<String> list = readFile(fromFileName);
+        writeFile(toFileName, reportStatistics(list));
     }
 }
