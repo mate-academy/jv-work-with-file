37224205c193c50bbfc0875f0fbf9c00faf1e46a
Likh-Alex <likhachov991@protonmail.com>
2020-12-05 12:32:14 +0200

    Solution attempt

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,8 +1,47 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WorkWithFile {
 
     public void getStatistic(String fromFileName, String toFileName) {
+        File fileFrom = new File(fromFileName);
+        StringBuilder stringBuilder = new StringBuilder();
 
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileFrom))) {
+            String line = reader.readLine();
+            int supply = 0;
+            int buy = 0;
+            while (line != null) {
+                String[] lineData = line.split(",");
+                if (lineData[0].equals("supply")) {
+                    supply += Integer.parseInt(lineData[1]);
+                } else if (lineData[0].equals("buy")) {
+                    buy += Integer.parseInt(lineData[1]);
+                }
+                line = reader.readLine();
+            }
+            stringBuilder.append("supply,").append(supply)
+                .append("\n" + "buy,").append(buy)
+                .append("\n" + "result,").append(supply - buy);
+
+        } catch (IOException e) {
+            throw new RuntimeException("Can not read data from file", e);
+        }
+
+        File fileTo = new File(toFileName);
+        try {
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileTo));
+            String output = stringBuilder.toString();
+            bufferedWriter.write(output);
+            bufferedWriter.close();
+        } catch (IOException e) {
+            throw new RuntimeException("Can not write data to file", e);
+        }
     }
 }
