55661e781c59f2b8d426c264c669feecfd4f65a7
Vitalik Kopach <apple_air@ukr.net>
2020-12-05 15:11:19 +0200

    implement method getStatistic() and added constant value

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,8 +1,55 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WorkWithFile {
 
-    public void getStatistic(String fromFileName, String toFileName) {
+    private static final String DELIMITER = ",";
 
+    public void getStatistic(String fromFileName, String toFileName) {
+        writeToFile(toFileName, calculateReport(fromFileName));
+    }
+
+    private String calculateReport(String fileName) {
+        StringBuilder builder = new StringBuilder();
+        int buy = 0;
+        int supply = 0;
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            String line = "";
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] token = line.split(DELIMITER);
+                for (String data : token) {
+                    if (data.equals("buy")) {
+                        buy += Integer.parseInt(token[1]);
+                    } else if (data.equals("supply")) {
+                        supply += Integer.parseInt(token[1]);
+                    }
+                }
+            }
+            builder.append("supply,").append(supply).append(System.lineSeparator());
+            builder.append("buy,").append(buy).append(System.lineSeparator());
+            builder.append("result,").append(supply - buy);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("File not found!");
+        } catch (IOException e) {
+            throw new RuntimeException("Something go wrong!");
+        }
+        return builder.toString();
+    }
+
+    private static void writeToFile(String fileName, String report) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName))) {
+            bufferedWriter.write(report);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("File doesn't exist");
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write to File");
+        }
     }
 }
