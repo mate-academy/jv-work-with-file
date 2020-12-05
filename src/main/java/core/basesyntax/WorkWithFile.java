a39b57e45afdbc4286ba1f293e6740ecca6e2ad6
YuraBondarenko <yuriybondarenko2001@gmail.com>
2020-12-05 13:55:23 +0200

    split method "readFileAndCalculate" into two methods: "readFile" and "CalculateData" read the file, calculate values, write the file

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -9,11 +9,10 @@ import java.io.IOException;
 public class WorkWithFile {
 
     public void getStatistic(String fromFileName, String toFileName) {
-        writeFile(toFileName, readFileAndCalculate(fromFileName));
-
+        writeFile(toFileName, calculateData(readFile(fromFileName)));
     }
 
-    private String[] readFileAndCalculate(String fileName) {
+    private StringBuilder readFile(String fileName) {
         try (BufferedReader reader = new BufferedReader(new FileReader(fileName));) {
             StringBuilder fromFileNameReader = new StringBuilder();
             int value = reader.read();
@@ -27,30 +26,34 @@ public class WorkWithFile {
                 }
                 value = reader.read();
             }
-            String[] strFromFileName = fromFileNameReader.toString().split(" ");
-            Integer[] values = new Integer[]{0, 0, 0};
-            for (int i = 0; i < strFromFileName.length; i++) {
-                if (strFromFileName[i].equals("supply")) {
-                    values[0] += Integer.valueOf(strFromFileName[i + 1]);
-                } else if (strFromFileName[i].equals("buy")) {
-                    values[1] += Integer.valueOf(strFromFileName[i + 1]);
-                }
-            }
-            values[2] = values[0] - values[1];
-            String[] generalInformation = new String[]{"", "", ""};
-            String[] titles = new String[]{"supply", "buy", "result"};
-            for (int i = 0; i < generalInformation.length; i++) {
-                StringBuilder stringBuilder = new StringBuilder();
-                stringBuilder.append(titles[i]).append(",").append(values[i]);
-                generalInformation[i] = stringBuilder.toString();
-            }
-            return generalInformation;
+            return fromFileNameReader;
         } catch (IOException e) {
             throw new RuntimeException("Can't read file");
         }
     }
 
-    private void writeFile(String toFileName, String[]generalInformation) {
+    private String[] calculateData(StringBuilder fromFileNameReader) {
+        String[] strFromFileName = fromFileNameReader.toString().split(" ");
+        Integer[] values = new Integer[]{0, 0, 0};
+        for (int i = 0; i < strFromFileName.length; i++) {
+            if (strFromFileName[i].equals("supply")) {
+                values[0] += Integer.valueOf(strFromFileName[i + 1]);
+            } else if (strFromFileName[i].equals("buy")) {
+                values[1] += Integer.valueOf(strFromFileName[i + 1]);
+            }
+        }
+        values[2] = values[0] - values[1];
+        String[] generalInformation = new String[]{"", "", ""};
+        String[] titles = new String[]{"supply", "buy", "result"};
+        for (int i = 0; i < generalInformation.length; i++) {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append(titles[i]).append(",").append(values[i]);
+            generalInformation[i] = stringBuilder.toString();
+        }
+        return generalInformation;
+    }
+
+    private void writeFile(String toFileName, String[] generalInformation) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
             for (String generalInfo : generalInformation) {
                 bufferedWriter.write(generalInfo + System.lineSeparator());
