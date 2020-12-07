f41cbcf6d4b1f64478a6821626fa8cc84ef54f92
Okulov Vadim <Vedroidua@gmail.com>
2020-12-07 14:47:04 +0200

    moved result to constants, renamed constants

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,9 +8,10 @@ import java.io.IOException;
 
 public class WorkWithFile {
     
-    public static final int FIRST_INFO_INDEX = 0;
-    public static final int SECOND_INFO_INDEX = 1;
+    public static final int ZERO_INDEX = 0;
+    public static final int FIRST_INDEX = 1;
     public static final String DELIMITER = ",";
+    public static final String RESULT = "result";
     public static final String SUPPLY = "supply";
     public static final String BUY = "buy";
     
@@ -21,11 +22,11 @@ public class WorkWithFile {
             String line = reader.readLine();
             while (line != null) {
                 String[] info = line.split(DELIMITER);
-                if (info[FIRST_INFO_INDEX].equals(SUPPLY)) {
-                    supply += Integer.parseInt(info[SECOND_INFO_INDEX]);
+                if (info[ZERO_INDEX].equals(SUPPLY)) {
+                    supply += Integer.parseInt(info[FIRST_INDEX]);
                 }
-                if (info[FIRST_INFO_INDEX].equals(BUY)) {
-                    buy += Integer.parseInt(info[SECOND_INFO_INDEX]);
+                if (info[ZERO_INDEX].equals(BUY)) {
+                    buy += Integer.parseInt(info[FIRST_INDEX]);
                 }
                 line = reader.readLine();
             }
@@ -37,8 +38,8 @@ public class WorkWithFile {
     
     private void createResultFile(String toFileName, int supply, int buy) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
-            bufferedWriter.write("supply," + supply + System.lineSeparator()
-                    + "buy," + buy + System.lineSeparator() + "result," + (supply - buy));
+            bufferedWriter.write(SUPPLY + "," + supply + System.lineSeparator()
+                    + BUY + "," + buy + System.lineSeparator() + RESULT + "," + (supply - buy));
         } catch (IOException e) {
             throw new RuntimeException("Can`t write data to file", e);
         }
