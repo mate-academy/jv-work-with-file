16cc8472a7337d2dca66050a1e5ed216bb503886
nikelok01@gmail.com <163amefeZ>
2020-12-07 14:56:58 +0200

    - small bugs fixed; - method readFromFile() return int array; - created variable for report in method writeToFIle; - changed name of variable "value" to "lineText";

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,37 +7,36 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    private int supply;
-    private int buy;
-
     public void getStatistic(String fromFileName, String toFileName) {
-        readFromFIle(fromFileName);
-        writeToFIle(toFileName);
+        writeToFIle(toFileName, readFromFIle(fromFileName));
     }
 
-    private void readFromFIle(String fromFile) {
-        try (BufferedReader readFile = new BufferedReader(new FileReader(fromFile + ""))) {
-            String value = readFile.readLine();
-            while (value != null) {
-                String[] line = value.split(",");
+    private int[] readFromFIle(String fromFile) {
+        int[] sales = new int[2];
+        try (BufferedReader readFile = new BufferedReader(new FileReader(fromFile))) {
+            String lineText = readFile.readLine();
+            while (lineText != null) {
+                String[] line = lineText.split(",");
                 if (line[0].equals("supply")) {
-                    supply += Integer.parseInt(line[1]);
+                    sales[0] += Integer.parseInt(line[1]);
                 } else {
-                    buy += Integer.parseInt(line[1]);
+                    sales[1] += Integer.parseInt(line[1]);
                 }
-                value = readFile.readLine();
+                lineText = readFile.readLine();
             }
         } catch (IOException e) {
-            throw new RuntimeException("File not found" + fromFile, e);
+            throw new RuntimeException("File not found - " + fromFile, e);
         }
+        return sales;
     }
 
-    private void writeToFIle(String toFile) {
+    private void writeToFIle(String toFile, int[] sales) {
+        String reportToFile = new StringBuilder().append("supply,").append(sales[0])
+                .append(System.lineSeparator()).append("buy,").append(sales[1])
+                .append(System.lineSeparator()).append("result,")
+                .append(sales[0] - sales[1]).toString();
         try (BufferedWriter writeFile = new BufferedWriter(new FileWriter(toFile + ""))) {
-            writeFile.write(new StringBuilder().append("supply,").append(supply)
-                    .append(System.lineSeparator()).append("buy,").append(buy)
-                    .append(System.lineSeparator()).append("result,")
-                    .append(supply - buy).toString());
+            writeFile.write(reportToFile);
         } catch (IOException e) {
             throw new RuntimeException("Can't create a file!" + toFile, e);
         }
