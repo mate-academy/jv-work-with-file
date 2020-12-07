ee5f25b6bbabcb63f5495cbca85395a811a11a9c
Andrii Bardin <andrii.bardin@gmail.com>
2020-12-07 16:43:45 +0200

    added create report method, fixed previous issues

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,13 +8,14 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    private static final String DELIMITER = ",";
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
 
     public void getStatistic(String fromFileName, String toFileName) {
         File read = new File(fromFileName);
-        File report = new File(toFileName);
 
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(read));
+        try (BufferedReader reader = new BufferedReader(new FileReader(read))) {
             StringBuilder temporary = new StringBuilder();
             String value = reader.readLine();
             while (value != null) {
@@ -25,36 +26,30 @@ public class WorkWithFile {
             int supply = 0;
             int buy = 0;
             for (String line : lines) {
-                String[] operation = line.split(",");
-
-                if (operation[0].equals("supply")) {
+                String[] operation = line.split(DELIMITER);
+                if (operation[0].equals(SUPPLY)) {
                     supply += Integer.parseInt(operation[1]);
                 } else {
                     buy += Integer.parseInt(operation[1]);
                 }
             }
-            StringBuilder results = new StringBuilder();
-            results.append("supply,").append(supply).append("\n")
-                    .append("buy,").append(buy).append("\n")
-                    .append("result,").append(supply - buy);
-
-            BufferedWriter bufferedWriter = null;
-            try {
-                bufferedWriter = new BufferedWriter(new FileWriter(report));
-                bufferedWriter.write(results.toString());
-            } catch (IOException e) {
-                throw new RuntimeException("Can't write to file",e);
-            } finally {
-                if (bufferedWriter != null) {
-                    try {
-                        bufferedWriter.close();
-                    } catch (IOException e) {
-                        throw new RuntimeException("Can't close BufferedWriter");
-                    }
-                }
-            }
+            createReport(supply, buy, toFileName);
         } catch (IOException e) {
-            throw new RuntimeException("Can't read file " + fromFileName, e);
+            throw new RuntimeException("Can't read file", e);
+        }
+    }
+
+    private void createReport(int supply, int buy, String toFileName) {
+        File report = new File(toFileName);
+        StringBuilder results = new StringBuilder();
+        results.append(SUPPLY).append(",").append(supply).append("\n")
+                .append(BUY).append(",").append(buy).append("\n")
+                .append("result,").append(supply - buy);
+
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(report))) {
+            bufferedWriter.write(results.toString());
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write to file", e);
         }
     }
 }
