fa697e3fcc87a2b5f0884aa7e4cb08c555a578dc
Likh-Alex <likhachov991@protonmail.com>
2020-12-07 19:26:47 +0200

    added 0 and 1 to constants for line parsing changed helper method access modifier to private

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -11,6 +11,8 @@ public class WorkWithFile {
     private static final String SUPPLY_KEY_WORD = "supply";
     private static final String BUY_KEY_WORD = "buy";
     private static final String DATA_SEPARATOR = ",";
+    private static final int DATA_NAME = 0;
+    private static final int DATA_VALUE = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
         File fileFrom = new File(fromFileName);
@@ -21,20 +23,20 @@ public class WorkWithFile {
             int buy = 0;
             while (line != null) {
                 String[] lineData = line.split(DATA_SEPARATOR);
-                if (lineData[0].equals(SUPPLY_KEY_WORD)) {
-                    supply += Integer.parseInt(lineData[1]);
-                } else if (lineData[0].equals(BUY_KEY_WORD)) {
-                    buy += Integer.parseInt(lineData[1]);
+                if (lineData[DATA_NAME].equals(SUPPLY_KEY_WORD)) {
+                    supply += Integer.parseInt(lineData[DATA_VALUE]);
+                } else if (lineData[DATA_NAME].equals(BUY_KEY_WORD)) {
+                    buy += Integer.parseInt(lineData[DATA_VALUE]);
                 }
                 line = reader.readLine();
             }
-            writeReportToFile(supply,buy,toFileName);
+            writeReportToFile(supply, buy, toFileName);
         } catch (IOException e) {
             throw new RuntimeException("Can not read data from file: " + fileFrom, e);
         }
     }
 
-    public void writeReportToFile(int supply, int buy, String toFileName) {
+    private void writeReportToFile(int supply, int buy, String toFileName) {
         StringBuilder stringBuilder = new StringBuilder();
         File fileTo = new File(toFileName);
         stringBuilder.append("supply,")
