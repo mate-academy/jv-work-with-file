59b5edbe19a0b8862fb36ea171e6499379f803db
Baldinini <baldinat063@gmail.com>
2020-12-07 13:28:31 +0200

    bugs fixed

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,18 +8,22 @@ import java.io.IOException;
 public class WorkWithFile {
     private static final String BUY = "buy";
     private static final String SUPPLY = "supply";
+    private static final int ONE = 1;
+    private static final int ZERO = 0;
+    private static final String COMMA = ",";
+    private static final String WHITESPACE = " ";
 
     public void getStatistic(String fromFileName, String toFileName) {
 
-        String[] buysSupplies = readerFromFile(fromFileName);
-        int buy = supplyBuyResult(buysSupplies, BUY);
-        int supply = supplyBuyResult(buysSupplies, SUPPLY);
+        String[] buysSupplies = readFromFile(fromFileName);
+        int buy = getSupplyBuyResult(buysSupplies, BUY);
+        int supply = getSupplyBuyResult(buysSupplies, SUPPLY);
 
         try (FileWriter writerToFile = new FileWriter(toFileName)) {
-            writerToFile.append(SUPPLY).append(",")
+            writerToFile.append(SUPPLY).append(COMMA)
                     .append(String.valueOf(supply))
                     .append(System.lineSeparator())
-                    .append(BUY).append(",")
+                    .append(BUY).append(COMMA)
                     .append(String.valueOf(buy))
                     .append(System.lineSeparator())
                     .append("result,").append(String.valueOf(supply - buy));
@@ -28,28 +32,26 @@ public class WorkWithFile {
         }
     }
 
-    private String[] readerFromFile(String file) {
+    private String[] readFromFile(String file) {
         StringBuilder csvFile = new StringBuilder();
 
         try (BufferedReader readerFromFile = new BufferedReader(new FileReader(file))) {
             String value;
             while ((value = readerFromFile.readLine()) != null) {
-                String[] data = value.split("\n");
-                csvFile.append(data[0]).append(" ");
+                csvFile.append(value).append(WHITESPACE);
             }
         } catch (IOException e) {
             throw new RuntimeException("Can't read file " + file, e);
         }
-        return csvFile.toString().split(" ");
+        return csvFile.toString().split(WHITESPACE);
     }
 
-    private int supplyBuyResult(String[] suppliesBuys, String compare) {
-        int result = 0;
+    private int getSupplyBuyResult(String[] suppliesBuys, String targetString) {
+        int result = ZERO;
 
         for (String buySupply : suppliesBuys) {
-            String[] data = buySupply.split(",");
-            result += data[0].equals(compare) ? Integer.parseInt(data[1]) : 0;
-
+            String[] data = buySupply.split(COMMA);
+            result += data[ZERO].equals(targetString) ? Integer.parseInt(data[ONE]) : ZERO;
         }
         return result;
     }
