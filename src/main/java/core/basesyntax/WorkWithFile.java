f3e36851d91b0acda1d413b69c645089581e6dc2
sanek98cv <73754789+sanek98cv@users.noreply.github.com>
2020-12-07 14:29:32 +0200

    Fixed: added new method

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,39 +7,44 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    private static final String SYMBOL_SPLIT = ",";
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+    StringBuilder fileRead = new StringBuilder();
 
     public void getStatistic(String fromFileName, String toFileName) {
         writeData(toFileName, readFile(fromFileName));
     }
 
     private String readFile(String fileName) {
-        StringBuilder stringBuilder = new StringBuilder();
-
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
             String value = bufferedReader.readLine();
             while (value != null) {
-                stringBuilder.append(value).append(",");
+                fileRead.append(value).append(SYMBOL_SPLIT);
                 value = bufferedReader.readLine();
             }
         } catch (IOException e) {
             throw new RuntimeException("Can't read file", e);
         }
+        return countAndCreateReport();
+    }
 
-        String[] readFromFile = stringBuilder.toString().split(",");
+    private String countAndCreateReport() {
+        String[] readFromFile = fileRead.toString().split(SYMBOL_SPLIT);
         int supply = 0;
         int buy = 0;
         for (int i = 0; i < readFromFile.length; i++) {
-            if (readFromFile[i].equals("supply")) {
+            if (readFromFile[i].equals(SUPPLY)) {
                 supply += Integer.parseInt(readFromFile[i + 1]);
-            } else if (readFromFile[i].equals("buy")) {
+            } else if (readFromFile[i].equals(BUY)) {
                 buy += Integer.parseInt(readFromFile[i + 1]);
             }
         }
-
         StringBuilder report = new StringBuilder();
-        return report.append("supply").append(",").append(supply).append(System.lineSeparator())
-                .append("buy").append(",").append(buy).append(System.lineSeparator())
-                .append("result").append(",").append(supply - buy).toString();
+        return report.append(SUPPLY).append(SYMBOL_SPLIT).append(supply)
+                .append(System.lineSeparator()).append(BUY).append(SYMBOL_SPLIT)
+                .append(buy).append(System.lineSeparator()).append("result")
+                .append(SYMBOL_SPLIT).append(supply - buy).toString();
     }
 
     private void writeData(String filename, String data) {
