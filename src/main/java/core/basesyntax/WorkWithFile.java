ed00e39a2a29ac313657d9008c8fd6280369d8db
ilya-brinchuk <ilya.brinchuk@gmail.com>
2020-12-07 16:19:57 +0200

    added new method getReport added constants

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,39 +7,45 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    private int supply;
-    private int buy;
+    private static final String COMMA_SEPARATOR = ",";
+    private static final String SUPPLY = "supply";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        getInformation(fromFileName);
-        writeInFile(toFileName);
+        writeToFile(toFileName,getReport(getInformation(fromFileName)));
     }
 
-    private void getInformation(String fileName) {
+    private int[] getInformation(String fileName) {
+        int supply = 0;
+        int buy = 0;
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
             while (bufferedReader.ready()) {
-                String[] line = bufferedReader.readLine().split(",");
-                if (line[0].equals("supply")) {
+                String[] line = bufferedReader.readLine().split(COMMA_SEPARATOR);
+                if (line[0].equals(SUPPLY)) {
                     supply += Integer.parseInt(line[1]);
                 } else {
                     buy += Integer.parseInt(line[1]);
                 }
             }
         } catch (IOException e) {
-            throw new RuntimeException("Can not read this file");
+            throw new RuntimeException("Can not read this file",e);
         }
+        return new int[]{supply, buy};
     }
 
-    private void writeInFile(String fileName) {
+    private String getReport(int[] data) {
+        StringBuilder result = new StringBuilder()
+                .append("supply,").append(data[0]).append(System.lineSeparator())
+                .append("buy,").append(data[1]).append(System.lineSeparator())
+                .append("result,").append(data[0] - data[1]);
+        return result.toString();
+    }
+
+    private void writeToFile(String fileName, String report) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName))) {
-            StringBuilder result = new StringBuilder();
-            result.append("supply,").append(supply).append(System.lineSeparator())
-                    .append("buy,").append(buy).append(System.lineSeparator())
-                    .append("result,").append(supply - buy);
-            bufferedWriter.write(result.toString());
+            bufferedWriter.write(report);
 
         } catch (IOException e) {
-            throw new RuntimeException("Can not write information in this file");
+            throw new RuntimeException("Can not write information in this file",e);
         }
     }
 }
