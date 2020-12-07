62b28351d7285491032e540f0ccea8edfd979cc3
Resci <vIsUAl123>
2020-12-07 16:18:17 +0200

    Move fields to constant

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,12 +8,17 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    private static final String REGEX = ",";
+    private static final String SUPPLY_OPERATION = "supply";
+    private static final String BUY_OPERATION = "buy";
+    private static final int OPERATION_TYPE = 0;
+    private static final int AMOUNT = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
         File fromFile = new File(fromFileName);
         File toFile = new File(toFileName);
-        writeReport(sumOfOperation("supply", fromFile),
-                    sumOfOperation("buy", fromFile),
+        writeReport(sumOfOperation(SUPPLY_OPERATION, fromFile),
+                    sumOfOperation(BUY_OPERATION, fromFile),
                     toFile);
     }
 
@@ -22,8 +27,8 @@ public class WorkWithFile {
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             String readValue = bufferedReader.readLine();
             while (readValue != null) {
-                if (readValue.split(",")[0].equals(operationName)) {
-                    sum += Integer.parseInt(readValue.split(",")[1]);
+                if (readValue.split(REGEX)[OPERATION_TYPE].equals(operationName)) {
+                    sum += Integer.parseInt(readValue.split(REGEX)[AMOUNT]);
                 }
                 readValue = bufferedReader.readLine();
             }
@@ -35,15 +40,18 @@ public class WorkWithFile {
 
     private void writeReport(int supply, int buy, File toFile) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFile))) {
-            bufferedWriter.write("supply,");
+            bufferedWriter.write(SUPPLY_OPERATION);
+            bufferedWriter.write(REGEX);
             bufferedWriter.write(String.valueOf(supply));
             bufferedWriter.newLine();
 
-            bufferedWriter.write("buy,");
+            bufferedWriter.write(BUY_OPERATION);
+            bufferedWriter.write(REGEX);
             bufferedWriter.write(String.valueOf(buy));
             bufferedWriter.newLine();
 
-            bufferedWriter.write("result,");
+            bufferedWriter.write("result");
+            bufferedWriter.write(REGEX);
             bufferedWriter.write(String.valueOf(supply - buy));
 
             bufferedWriter.flush();
