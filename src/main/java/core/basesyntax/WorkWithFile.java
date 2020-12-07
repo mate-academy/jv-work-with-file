1e8f1685e3f36e53bcd90c88e991a2cccdda6a55
Volodymyr Lykhvar <vlykhvar@gmail.com>
2020-12-07 20:38:48 +0200

    added trycath, private. clean code change

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -12,51 +12,60 @@ public class WorkWithFile {
     private static final String BUY = "buy";
     private static final String SUPPLY = "supply";
     private static final String RESULT = "result,";
+    private static final String COMA = ",";
+    private static final int ZERO = 0;
+    private static final int ONE = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        try {
-            writeFile(toFileName, prepareFile(getFile(fromFileName)));
-        } catch (IOException e) {
-            System.out.println("File was not found");
-        }
+
+        writeFile(toFileName, prepareFile(getFile(fromFileName)));
+
     }
 
-    private String getFile(String fromFileName) throws IOException {
+    private String getFile(String fromFileName) {
         StringBuilder file = new StringBuilder();
-        BufferedReader bufferedReader = new BufferedReader(new
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new
                     InputStreamReader(new FileInputStream(fromFileName)));
-        String newLine = bufferedReader.readLine();
-        while (newLine != null) {
-            file.append(newLine.toLowerCase()).append(" ");
-            newLine = bufferedReader.readLine();
+            String newLine = bufferedReader.readLine();
+            while (newLine != null) {
+                file.append(newLine.toLowerCase()).append(" ");
+                newLine = bufferedReader.readLine();
+            }
+            bufferedReader.close();
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file");
         }
-        bufferedReader.close();
         return file.toString();
     }
 
     private String prepareFile(String file) {
-        int sumOfSupply = 0;
-        int sumOfbuy = 0;
+        int sumOfSupply = ZERO;
+        int sumOfbuy = ZERO;
         String[] fileToArr = file.split(" ");
-        for (String fileInArr: fileToArr) {
-            String[] temp;
-            temp = fileInArr.split(",");
-            if (temp[0].equals(BUY)) {
-                sumOfbuy += Integer.parseInt(temp[1]);
-            } else if (temp[0].equals(SUPPLY)) {
-                sumOfSupply += Integer.parseInt(temp[1]);
+        for (String fileInArr : fileToArr) {
+            String[] temp = fileInArr.split(COMA);
+            if (temp[ZERO].equals(BUY)) {
+                sumOfbuy += Integer.parseInt(temp[ONE]);
+            } else if (temp[ZERO].equals(SUPPLY)) {
+                sumOfSupply += Integer.parseInt(temp[ONE]);
             }
         }
-        return new StringBuilder(SUPPLY + "," + sumOfSupply + System.lineSeparator()
-                + BUY + "," + sumOfbuy + System.lineSeparator() + RESULT
-                + (sumOfSupply - sumOfbuy)).toString();
+        return new StringBuilder(SUPPLY).append(COMA).append(sumOfSupply)
+                .append(System.lineSeparator()).append(BUY).append(COMA)
+                .append(sumOfbuy).append(System.lineSeparator()).append(RESULT)
+                .append(((sumOfSupply - sumOfbuy))).toString();
     }
 
-    private boolean writeFile(String fileName, String fileInPut) throws IOException {
+    private boolean writeFile(String fileName, String fileInPut) {
         File file = new File(fileName);
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));
-        bufferedWriter.write(fileInPut);
-        bufferedWriter.close();
+        try {
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));
+            bufferedWriter.write(fileInPut);
+            bufferedWriter.close();
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write to the file", e);
+        }
         return true;
     }
 }
