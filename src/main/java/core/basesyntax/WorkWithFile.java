950516ef58ba1a9175c1f97ab3235b1b6d9b566a
antoniukivan <ivan.antonuk1992@gmail.com>
2020-12-07 13:12:43 +0200

    divide my code into several methods

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -9,8 +9,15 @@ import java.io.IOException;
 public class WorkWithFile {
     private static final String SUPPLY = "supply";
     private static final String BUY = "buy";
+    private static final int OPERATION = 0;
+    private static final int AMOUNT = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
+        String[] data = readFromFile(fromFileName);
+        writeToFile(data, toFileName);
+    }
+
+    private String[] readFromFile(String fromFileName) {
         StringBuilder builder = new StringBuilder();
         try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
             int value = reader.read();
@@ -21,16 +28,19 @@ public class WorkWithFile {
         } catch (IOException e) {
             throw new RuntimeException("Can't read the file", e);
         }
-        String[] rows = builder.toString().split(System.lineSeparator());
+        return builder.toString().split(System.lineSeparator());
+    }
+
+    private void writeToFile(String[] allData, String toFileName) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName, true))) {
             int supplyValue = 0;
             int buyValue = 0;
-            for (String row : rows) {
+            for (String row : allData) {
                 String[] data = row.split(",");
-                if (data[0].equals(SUPPLY)) {
+                if (data[OPERATION].equals(SUPPLY)) {
                     supplyValue += Integer.parseInt(data[1]);
-                } else if (data[0].equals(BUY)) {
-                    buyValue += Integer.parseInt(data[1]);
+                } else if (data[OPERATION].equals(BUY)) {
+                    buyValue += Integer.parseInt(data[AMOUNT]);
                 }
             }
             writer.write(SUPPLY + "," + supplyValue);
