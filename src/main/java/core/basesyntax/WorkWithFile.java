0912f3d7e9301c006483b488cc4afdcadd3f0daf
Okulov Vadim <Vedroidua@gmail.com>
2020-12-07 14:29:18 +0200

    moved delimiter, info index, supply and buy to constants

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,18 +7,25 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    
+    public static final int FIRST_INFO_INDEX = 0;
+    public static final int SECOND_INFO_INDEX = 1;
+    public static final String DELIMITER = ",";
+    public static final String SUPPLY = "supply";
+    public static final String BUY = "buy";
+    
     public void getStatistic(String fromFileName, String toFileName) {
         try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
             int supply = 0;
             int buy = 0;
             String line = reader.readLine();
             while (line != null) {
-                String[] info = line.split(",");
-                if (info[0].equals("supply")) {
-                    supply += Integer.parseInt(info[1]);
+                String[] info = line.split(DELIMITER);
+                if (info[FIRST_INFO_INDEX].equals(SUPPLY)) {
+                    supply += Integer.parseInt(info[SECOND_INFO_INDEX]);
                 }
-                if (info[0].equals("buy")) {
-                    buy += Integer.parseInt(info[1]);
+                if (info[FIRST_INFO_INDEX].equals(BUY)) {
+                    buy += Integer.parseInt(info[SECOND_INFO_INDEX]);
                 }
                 line = reader.readLine();
             }
