cbd55d3092ad92ea238812b7f92925dd3c737194
antonpanchishko <PanchishkoMayor@gmail.com>
2020-12-07 13:07:35 +0200

    2) add new method, amend mistakes

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -9,36 +9,36 @@ import java.io.IOException;
 
 public class WorkWithFile {
 
-    public void getStatistic(String fromFileName, String toFileName) {
+    public String getStatistic(String fromFileName, String toFileName) {
         int buy = 0;
         int supply = 0;
-        try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName));
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             String value = bufferedReader.readLine();
             while (value != null) {
-                String [] strings = value.split(",");
+                String[] strings = value.split(",");
                 if (strings[0].equals("buy")) {
                     buy = buy + Integer.parseInt(strings[1]);
                 } else {
                     supply = supply + Integer.parseInt(strings[1]);
                 }
-                System.out.println(buy + " " + supply);
                 value = bufferedReader.readLine();
             }
         } catch (IOException e) {
-            throw new RuntimeException("Cant read file");
+            throw new RuntimeException("Cant read file - " + fromFileName, e);
         }
+        StringBuilder result = new StringBuilder().append("supply, ").append(supply)
+                .append(System.lineSeparator()).append("buy, ").append(buy)
+                .append(System.lineSeparator()).append("result, ").append(supply - buy);
+        return result.toString();
+    }
 
-        try {
-            File fileToWrite = new File(toFileName);
+    private void printInFile(String toFileName, String result) {
+        File fileToWrite = new File(toFileName);
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileToWrite))) {
             fileToWrite.createNewFile();
-            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileToWrite));
-            bufferedWriter.write("supply," + Integer.toString(supply) + System.lineSeparator()
-                    + "buy," + Integer.toString(buy) + System.lineSeparator()
-                    + "result," + Integer.toString(supply - buy));
-            bufferedWriter.close();
+            bufferedWriter.write(result);
         } catch (IOException e) {
-            throw new RuntimeException("Cant write file");
+            throw new RuntimeException("Cant write to file - " + toFileName, e);
         }
     }
 }
