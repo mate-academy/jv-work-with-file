ebe7554f6b7752d2effae8489ba69a6c43cb9f1b
m.kislukhina <marharita.kislukhina@gmail.com>
2020-12-07 18:29:48 +0100

    fields are local now

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -10,10 +10,10 @@ public class WorkWithFile {
     public static final String COMA = ",";
     public static final int ZERO = 0;
     public static final int ONE = 1;
-    private int supply = 0;
-    private int buy = 0;
 
     public void getStatistic(String fromFileName, String toFileName) {
+        int supply = 0;
+        int buy = 0;
         try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
             String value = reader.readLine();
             while (value != null) {
