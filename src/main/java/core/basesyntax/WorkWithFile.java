0c1eee0cc2bb4302da767b453c3e5cd32311c7c6
Alekearl <alexandrhurko@gmail.com>
2020-12-07 17:14:28 +0200

    refactored class WorkWithFile

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -12,11 +12,17 @@ public class WorkWithFile {
     private static final String COMA = ",";
     private static final int INDEX_ZERO = 0;
     private static final int INDEX_ONE = 1;
+    private List<String> writeToFile;
 
     public void getStatistic(String fromFileName, String toFileName) {
+        readFile(fromFileName);
+        writeFile(toFileName);
+    }
+
+    public void readFile(String readFile) {
         List<String> list;
         try {
-            list = Files.readAllLines(new File(fromFileName).toPath());
+            list = Files.readAllLines(new File(readFile).toPath());
         } catch (IOException e) {
             throw new RuntimeException("File not found!", e);
         }
@@ -32,15 +38,17 @@ public class WorkWithFile {
             }
         }
 
-        List<String> writeToFile = new ArrayList<>();
+        writeToFile = new ArrayList<>();
         String buy = "buy";
         String result = "result";
         writeToFile.add(supply + COMA + supplySum);
         writeToFile.add(buy + COMA + buySum);
         writeToFile.add(result + COMA + (supplySum - buySum));
+    }
 
+    public void writeFile(String writeFile) {
         try (BufferedWriter bufferedWriter
-                     = new BufferedWriter(new FileWriter(new File(toFileName)))) {
+                     = new BufferedWriter(new FileWriter(new File(writeFile)))) {
             for (String information : writeToFile) {
                 bufferedWriter.write(information + System.lineSeparator());
             }
