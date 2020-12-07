e5bee05cbc3ac585090d8ff0bd60e20ee665c8ba
bogdan <b.kostevich@gmail.com>
2020-12-07 19:27:38 +0200

    Removed unimportant imports

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,8 +1,6 @@
 package core.basesyntax;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
