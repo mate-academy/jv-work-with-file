a57a1e0d78e8bb6755e2c62296a9057097de21d3
m.kislukhina <marharita.kislukhina@gmail.com>
2020-12-07 18:17:21 +0100

    StringBuilder added. fields are private now

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -10,8 +10,8 @@ public class WorkWithFile {
     public static final String COMA = ",";
     public static final int ZERO = 0;
     public static final int ONE = 1;
-    int supply = 0;
-    int buy = 0;
+    private int supply = 0;
+    private int buy = 0;
 
     public void getStatistic(String fromFileName, String toFileName) {
         try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
@@ -34,8 +34,11 @@ public class WorkWithFile {
 
     private void writeToFile(String toFileName, int supply, int buy) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
-            writer.write(("supply," + supply + System.lineSeparator()
-                    + "buy," + buy + System.lineSeparator() + "result," + (supply - buy)));
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append("supply,").append(supply).append(System.lineSeparator())
+                    .append("buy,").append(buy).append(System.lineSeparator()).append("result,")
+                    .append((supply - buy));
+            writer.write(stringBuilder.toString());
         } catch (IOException e) {
             throw new RuntimeException("Can't write write data to file", e);
         }
