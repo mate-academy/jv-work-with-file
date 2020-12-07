de54da66ed315c96383cd38ac431adfcbb6c92f5
KostyantynProskura <kostyantyn.proskura@gmail.com>
2020-12-07 19:50:26 +0200

    3) - Implemented try with resources for BufferedReader and BufferedWriter;  - Changed "if lines" from substring to split (Thanks by the way, going to be a good variant to make code better!!!);  - removed empty lines;

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -3,12 +3,12 @@ package core.basesyntax;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class WorkWithFile {
-
     private static final String SUPPLY_FIELD = "supply";
     private static final String BUY_FIELD = "buy";
     private static final String RESULT_FIELD = "result";
@@ -16,9 +16,7 @@ public class WorkWithFile {
 
     public void getStatistic(String fromFileName, String toFileName) {
         File file = new File(fromFileName);
-
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(file));
+        try (BufferedReader reader = Files.newBufferedReader(Paths.get(fromFileName))) {
             StringBuilder dataFromFile = new StringBuilder();
             String value = reader.readLine();
             while (value != null) {
@@ -35,12 +33,11 @@ public class WorkWithFile {
     private String reportCreation(String[] dataFromFile, String toFileName) {
         int supply = 0;
         int buy = 0;
-
         for (String line : dataFromFile) {
-            if (line.substring(0,line.indexOf(COMMA)).equals(SUPPLY_FIELD)) {
-                supply += Integer.parseInt(line.substring(line.indexOf(COMMA) + 1));
+            if (line.split(COMMA)[0].equals(SUPPLY_FIELD)) {
+                supply += Integer.parseInt(line.split(COMMA)[1]);
             } else {
-                buy += Integer.parseInt(line.substring(line.indexOf(COMMA) + 1));
+                buy += Integer.parseInt(line.split(COMMA)[1]);
             }
         }
 
@@ -54,15 +51,12 @@ public class WorkWithFile {
 
     private String writeToFile(String report, String toFileName) {
         File file = new File(toFileName);
-        try {
-            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));
-
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
             bufferedWriter.write(report);
             bufferedWriter.flush();
         } catch (IOException e) {
             throw new RuntimeException("Can't write data to file " + toFileName, e);
         }
-
         return report;
     }
 }
