a1cd0638c4c70e590f6e1dd34fd250c3b87b19eb
YuraBondarenko <yuriybondarenko2001@gmail.com>
2020-12-07 13:44:17 +0200

    replaced "read" to "readLine", added some constants and changed data writing method in StringBuilder split method "readFileAndCalculate" into two methods: "readFile" and "CalculateData" read the file, calculate values, write the file

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,28 +7,28 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    private static final int SUPPLY = 0;
+    private static final int BUY = 1;
+    private static final int RESULT = 2;
 
     public void getStatistic(String fromFileName, String toFileName) {
         writeFile(toFileName, calculateData(readFile(fromFileName)));
     }
 
     private StringBuilder readFile(String fileName) {
-        try (BufferedReader reader = new BufferedReader(new FileReader(fileName));) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             StringBuilder fromFileNameReader = new StringBuilder();
-            int value = reader.read();
-            while (value != -1) {
-                if ((char) value == ',' || value == 10) {
-                    fromFileNameReader.append(" ");
-                } else if (value == 13) {
-                    fromFileNameReader.append("");
-                } else {
-                    fromFileNameReader.append((char) value);
+            String readLine = reader.readLine();
+            while (readLine != null) {
+                String[] arrReadLine = readLine.split(",");
+                for (int i = 0; i < arrReadLine.length; i++) {
+                    fromFileNameReader.append(arrReadLine[i]).append(" ");
                 }
-                value = reader.read();
+                readLine = reader.readLine();
             }
             return fromFileNameReader;
         } catch (IOException e) {
-            throw new RuntimeException("Can't read file");
+            throw new RuntimeException("Can't read file", e);
         }
     }
 
@@ -37,12 +37,12 @@ public class WorkWithFile {
         Integer[] values = new Integer[]{0, 0, 0};
         for (int i = 0; i < strFromFileName.length; i++) {
             if (strFromFileName[i].equals("supply")) {
-                values[0] += Integer.valueOf(strFromFileName[i + 1]);
+                values[SUPPLY] += Integer.valueOf(strFromFileName[i + 1]);
             } else if (strFromFileName[i].equals("buy")) {
-                values[1] += Integer.valueOf(strFromFileName[i + 1]);
+                values[BUY] += Integer.valueOf(strFromFileName[i + 1]);
             }
         }
-        values[2] = values[0] - values[1];
+        values[RESULT] = values[SUPPLY] - values[BUY];
         String[] generalInformation = new String[]{"", "", ""};
         String[] titles = new String[]{"supply", "buy", "result"};
         for (int i = 0; i < generalInformation.length; i++) {
@@ -57,10 +57,9 @@ public class WorkWithFile {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
             for (String generalInfo : generalInformation) {
                 bufferedWriter.write(generalInfo + System.lineSeparator());
-                bufferedWriter.flush();
             }
         } catch (IOException e) {
-            throw new RuntimeException("Can't write file");
+            throw new RuntimeException("Can't write file", e);
         }
     }
 }
