1048a758bbdb727f2827a704984246068788b904
Tetiana Chupika <mtv1301@gmail.com>
2021-02-21 17:54:22 +0200

    added some refactoring

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -16,38 +16,32 @@ public class WorkWithFile {
     }
 
     private String[] readFromFile(String fromFileName) {
-        try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName));
-            StringBuilder builder = new StringBuilder();
+        StringBuilder builder = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             int value = bufferedReader.read();
             while (value != -1) {
                 builder.append((char) value);
                 value = bufferedReader.read();
             }
             String wordWithoutEnter = builder.toString().replaceAll("\n", ",");
-            String[] splitArray = wordWithoutEnter.split(",");
-            return splitArray;
+            return wordWithoutEnter.split(",");
         } catch (IOException e) {
             throw new RuntimeException("Can't read from file", e);
         }
-
     }
 
     private String createReport(String[] splitArray) {
         int supply = 0;
         int buy = 0;
-        for (int i = 0; i < splitArray.length; i++) {
+        for (int i = 0; i < splitArray.length; i = i + 2) {
             if (splitArray[i].equals("supply")) {
                 supply += Integer.parseInt(splitArray[i + 1]);
             } else if (splitArray[i].equals("buy")) {
                 buy += Integer.parseInt(splitArray[i + 1]);
             }
         }
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("supply,").append(supply).append("\n").append("buy,").append(buy)
-                .append("\n").append("result,").append(supply - buy);
-        String resultReport = stringBuilder.toString();
-        return resultReport;
+        return "supply," + supply + "\n" + "buy," + buy
+                + "\n" + "result," + (supply - buy);
     }
 
     public void writeToFile(String resultReport, String toFileName) {
