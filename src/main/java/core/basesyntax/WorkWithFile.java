5d2818a5a2581c5068be0229b2b75eef4fd87b81
vsak <vsak@itsmartflex.com>
2021-03-23 18:59:17 +0200

    Fixed comments from approval

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,42 +8,59 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    private static final int NAME_PART_ARRAY = 0;
+    private static final int COUNT_PART_ARRAY = 1;
+    private static final String SUPPLY_CONSTANT = "supply";
+
     public void getStatistic(String fromFileName, String toFileName) {
-        File newFile = new File(toFileName);
+
         File initialFile = new File(fromFileName);
         int supply = 0;
         int buy = 0;
-        int result;
 
-        try (BufferedWriter bufferWriter = new BufferedWriter(new FileWriter(newFile));
-             BufferedReader bufferedReader = new BufferedReader(new FileReader(initialFile))) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(initialFile))) {
             String bufferValue = bufferedReader.readLine();
-            StringBuilder writerStringBuilder = new StringBuilder();
             String name = "";
             String count = "";
+            String resultText = "";
 
             while (bufferValue != null) {
-                name = bufferValue.split(",")[0];
-                count = bufferValue.split(",")[1];
+                name = bufferValue.split(",")[NAME_PART_ARRAY];
+                count = bufferValue.split(",")[COUNT_PART_ARRAY];
 
-                if (name.equals("supply")) {
+                if (name.equals(SUPPLY_CONSTANT)) {
                     supply += Integer.parseInt(count);
-                } else if (name.equals("buy")) {
+                } else {
                     buy += Integer.parseInt(count);
                 }
 
                 bufferValue = bufferedReader.readLine();
             }
 
-            result = supply - buy;
-
-            writerStringBuilder.append("supply,").append(supply).append("\n")
-                    .append("buy,").append(buy).append("\n")
-                    .append("result,").append(result);
-
-            bufferWriter.write(writerStringBuilder.toString());
+            resultText = createResultString(supply, buy);
+            writeToFile(toFileName, resultText);
         } catch (IOException e) {
             throw new RuntimeException("Something wrong with file", e);
+        }
+    }
+
+    private String createResultString(int supply, int buy) {
+        StringBuilder writerStringBuilder = new StringBuilder();
+        final int result = supply - buy;
+        writerStringBuilder.append("supply,").append(supply).append("\n")
+                .append("buy,").append(buy).append("\n")
+                .append("result,").append(result).append("\n");
+
+        return writerStringBuilder.toString();
+    }
+
+    private void writeToFile(String fileName, String text) {
+        File newFile = new File(fileName);
+
+        try (BufferedWriter bufferWriter = new BufferedWriter(new FileWriter(newFile))) {
+            bufferWriter.write(text);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write to " + fileName, e);
         }
     }
 }
