4b26270fcdca0bcbbfae34bbab24503dc81d4eed
dant0n <dmibuc@gmail.com>
2021-04-02 19:41:10 +0300

    Refactor code.

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -6,6 +6,11 @@ import java.nio.file.Path;
 import java.util.List;
 
 public class WorkWithFile {
+    private static final String FIRST_OPERATION_TYPE_STRING = "supply";
+    private static final String SECOND_OPERATION_TYPE_STRING = "buy";
+    private static final String RESULT_STRING = "result";
+    private static final String SEPARATOR = ",";
+
     public void getStatistic(String fromFileName, String toFileName) {
         int[] data = parseData(getData(fromFileName));
         String report = createReport(data);
@@ -33,17 +38,17 @@ public class WorkWithFile {
 
     private String createReport(int[] data) {
         int result = data[0] - data[1];
-        return "supply," + data[0] + System.lineSeparator()
-                + "buy," + data[1] + System.lineSeparator()
-                + "result," + result;
+        return FIRST_OPERATION_TYPE_STRING + SEPARATOR + data[0] + System.lineSeparator()
+                + SECOND_OPERATION_TYPE_STRING + SEPARATOR + data[1] + System.lineSeparator()
+                + RESULT_STRING + SEPARATOR + result;
     }
 
     private int[] parseData(List<String> data) {
         int supplyAmount = 0;
         int buyAmount = 0;
         for (String line : data) {
-            String[] typeAndAmount = line.split(",");
-            if (typeAndAmount[0].equals("supply")) {
+            String[] typeAndAmount = line.split(SEPARATOR);
+            if (typeAndAmount[0].equals(FIRST_OPERATION_TYPE_STRING)) {
                 supplyAmount += Integer.parseInt(typeAndAmount[1]);
             } else {
                 buyAmount += Integer.parseInt(typeAndAmount[1]);
