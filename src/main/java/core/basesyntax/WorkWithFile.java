1362208bad90d00cc206f72f42145b4e2c959d66
Den4eg007 <68178197+Den4eg007@users.noreply.github.com>
2021-04-03 01:47:28 +0300

    Implemented writeToCsvFile method

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,6 +1,8 @@
 package core.basesyntax;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.List;
@@ -10,17 +12,17 @@ public class WorkWithFile {
     private static final String[] CSV_CATEGORIES = new String[]{"buy", "supply"};
 
     public void getStatistic(String fromFileName, String toFileName) {
+        writeToCsvFile(toFileName, readFromCsvFile(fromFileName));
     }
 
     private String[] readFromCsvFile(String fromFileName) {
         File fileFrom = new File(fromFileName);
-        List<String> csv_values;
+        List<String> csvValues;
         int buyTotal = 0;
         int supplyTotal = 0;
-
         try {
-            csv_values = Files.readAllLines(fileFrom.toPath());
-            for (String currentLine : csv_values) {
+            csvValues = Files.readAllLines(fileFrom.toPath());
+            for (String currentLine : csvValues) {
                 if (currentLine.contains(CSV_CATEGORIES[0])) {
                     buyTotal += Integer.parseInt(currentLine.split(",")[1]);
                     continue;
@@ -30,10 +32,21 @@ public class WorkWithFile {
         } catch (IOException e) {
             throw new RuntimeException("Can't read file", e);
         }
-        String[] CalculatedTotal = new String[RESULT_ARRAY_LENGTH];
-        CalculatedTotal[0] = "supply," + supplyTotal + System.lineSeparator();
-        CalculatedTotal[1] = "buy," + buyTotal + System.lineSeparator();
-        CalculatedTotal[2] = "result," + (supplyTotal - buyTotal) + System.lineSeparator();
-        return CalculatedTotal;
+        String[] calculatedTotal = new String[RESULT_ARRAY_LENGTH];
+        calculatedTotal[0] = "supply," + supplyTotal + System.lineSeparator();
+        calculatedTotal[1] = "buy," + buyTotal + System.lineSeparator();
+        calculatedTotal[2] = "result," + (supplyTotal - buyTotal) + System.lineSeparator();
+        return calculatedTotal;
+    }
+
+    private void writeToCsvFile(String toFileName, String[] csvData) {
+        File fileTo = new File(toFileName);
+        for (String csvDatum : csvData) {
+            try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileTo, true))) {
+                bufferedWriter.write(csvDatum);
+            } catch (IOException e) {
+                throw new RuntimeException("Can't write data to file", e);
+            }
+        }
     }
 }
