6db61b234e1ac18bfcbe8a9d12e49fb41bba770b
fotomassimo <maksym.pavliuchenko@gmail.com>
2021-04-02 20:23:32 +0300

    Task jv-work-with-file, second commit

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,58 +2,61 @@ package core.basesyntax;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.util.Scanner;
 
 public class WorkWithFile {
     private static final String FIRST_OPERATION_NAME = "supply";
     private static final String SECOND_OPERATION_NAME = "buy";
+    private static final String DELIMITER = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
         String[] textLines = getInputText(fromFileName).split(System.lineSeparator());
-        int[] amountCounter = new int[2]; //supply - index 0, buy - index 1
+        int supplyAmount = 0;
+        int buyAmount = 0;
         for (String line : textLines) {
-            String[] lineInfo = line.split(",");
+            String[] lineInfo = line.split(DELIMITER);
             if (lineInfo[0].equals(FIRST_OPERATION_NAME)) {
-                amountCounter[0] += Integer.parseInt(lineInfo[1]);
+                supplyAmount += Integer.parseInt(lineInfo[1]);
             } else {
-                amountCounter[1] += Integer.parseInt(lineInfo[1]);
+                buyAmount += Integer.parseInt(lineInfo[1]);
             }
         }
-        String inputText = getReportText(amountCounter);
+        String inputText = getReportText(supplyAmount, buyAmount);
         writeToFile(toFileName, inputText);
     }
 
-    private static void writeToFile(String fileName, String inputText) {
+    private void writeToFile(String fileName, String inputText) {
         File file = new File(fileName);
         try {
             file.createNewFile();
-        } catch (java.io.IOException e) {
+        } catch (IOException e) {
             throw new RuntimeException("Can't create a file", e);
         }
         try {
             Files.write(file.toPath(), inputText.getBytes());
-        } catch (java.io.IOException e) {
+        } catch (IOException e) {
             throw new RuntimeException("Can't write a file", e);
         }
     }
 
-    private String getReportText(int[] amountCounter) {
+    private String getReportText(int supplyAmount, int buyAmount) {
         StringBuilder report = new StringBuilder();
         report.append(FIRST_OPERATION_NAME)
-                .append(",")
-                .append(amountCounter[0])
+                .append(DELIMITER)
+                .append(supplyAmount)
                 .append(System.lineSeparator())
                 .append(SECOND_OPERATION_NAME)
-                .append(",")
-                .append(amountCounter[1])
+                .append(DELIMITER)
+                .append(buyAmount)
                 .append(System.lineSeparator())
                 .append("result,")
-                .append(amountCounter[0] - amountCounter[1]);
+                .append(supplyAmount - buyAmount);
         return report.toString();
     }
 
-    private static String getInputText(String fileName) {
+    private String getInputText(String fileName) {
         try (Scanner scanner = new Scanner(new File(fileName))) {
             StringBuilder sourceTextBuilder = new StringBuilder();
             while (scanner.hasNextLine()) {
