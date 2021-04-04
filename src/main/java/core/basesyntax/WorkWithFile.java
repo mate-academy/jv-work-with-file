f7d0b63a838a756d428f2e0e9f9758f1d0e05f91
Дмитро <dima4994@gmail.com>
2021-04-04 20:30:44 +0300

    The method "getStatistic" was implemented whose the business logic was realized in follow methods: - readFile; - countData; - writeFile.

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,8 +1,65 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WorkWithFile {
 
     public void getStatistic(String fromFileName, String toFileName) {
+        String[] resultOfReadFile = readFile(new File(fromFileName));
+        writeFile(countData(resultOfReadFile), toFileName);
+    }
 
+    private String[] readFile(File oldFile) {
+        StringBuilder builder = new StringBuilder();
+
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(oldFile));
+            String line = bufferedReader.readLine();
+
+            while (line != null) {
+                builder.append(line).append(" ");
+                line = bufferedReader.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("Can't found the file", e);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read the file", e);
+        }
+
+        return builder.toString().trim().split(" ");
+    }
+
+    private String countData(String[] arrayOfData) {
+        int supply = 0;
+        int buy = 0;
+
+        for (String arrayOfDatum : arrayOfData) {
+            String[] data = arrayOfDatum.split(",");
+            if (data[0].equals("buy")) {
+                buy += Integer.parseInt(data[1]);
+            } else {
+                supply += Integer.parseInt(data[1]);
+            }
+        }
+
+        int result = supply - buy;
+        return new StringBuilder("supply,").append(supply).append(System.lineSeparator())
+                .append("buy,").append(buy).append(System.lineSeparator())
+                .append("result,").append(result).toString();
+    }
+
+    private void writeFile(String newData, String nameFile) {
+        File newFile = new File(nameFile);
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(newFile, true))) {
+            bufferedWriter.write(newData);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't found the file", e);
+        }
     }
 }
