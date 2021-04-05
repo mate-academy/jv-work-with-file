503b7b87c3684533a72fea8ad1c634899500b4a7
Dmitriy <dmitriy4ever97@gmail.com>
2021-04-05 15:52:05 +0300

    Done Homework.

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,8 +1,88 @@
 package core.basesyntax;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+
 public class WorkWithFile {
+    private String PURPOSE_AND_VALUE_SEPARATOR = ",";
+    private String DATA_SEPARATOR = "\n";
+    private String CHARACTERS_AND_SIGNS_FILTER = "^a-z ^,";
+    private String fromFileName;
+    private String toFileName;
+
+    public String getFromFileName() {
+        return fromFileName;
+    }
+
+    public void setFromFileName(String fromFileName) {
+        this.fromFileName = fromFileName;
+    }
+
+    public String getToFileName() {
+        return toFileName;
+    }
+
+    public void setToFileName(String toFileName) {
+        this.toFileName = toFileName;
+    }
+
+    private String[] readFileAndCreateDataArray() {
+        File file = new File(getFromFileName());
+        if (file.length() > 0) {
+            String[] fileDataArray;
+            String buffer;
+            try {
+                buffer = Files.readString(file.toPath())
+                        .replaceAll(CHARACTERS_AND_SIGNS_FILTER, "");
+                fileDataArray = buffer.split(DATA_SEPARATOR);
+                return fileDataArray;
+            } catch (IOException e) {
+                throw new RuntimeException("Can`t read file and list data.", e);
+            }
+        }
+        return new String[]{};
+    }
+
+    private int[] sumOfSupplyAndBuyInFile() {
+        int supplyNumber = 0;
+        int buyNumber = 0;
+        for (String data : readFileAndCreateDataArray()) {
+            if (data.substring(0, data.indexOf(PURPOSE_AND_VALUE_SEPARATOR)).equals("supply")) {
+                supplyNumber += Integer.parseInt(data.substring(data
+                        .indexOf(PURPOSE_AND_VALUE_SEPARATOR) + 1).trim());
+            } else if (data.substring(0, data.indexOf(PURPOSE_AND_VALUE_SEPARATOR))
+                    .equals("buy")) {
+                buyNumber += Integer.parseInt(data.substring(data
+                        .indexOf(PURPOSE_AND_VALUE_SEPARATOR) + 1).trim());
+            }
+        }
+        int result = supplyNumber - buyNumber;
+        return new int[]{supplyNumber, buyNumber, result};
+    }
+
+    private String[] createReport() {
+        int[] buyAndSupplyArray = sumOfSupplyAndBuyInFile();
+        String reportBuilder = "supply," + buyAndSupplyArray[0] +
+                " " + "buy," + buyAndSupplyArray[1] +
+                " " + "result," + buyAndSupplyArray[2];
+        return reportBuilder.split(" ");
+    }
 
     public void getStatistic(String fromFileName, String toFileName) {
-
+        setFromFileName(fromFileName);
+        setToFileName(toFileName);
+        StringBuilder b = new StringBuilder();
+        b.append(createReport()[0]).append(System.lineSeparator())
+                .append(createReport()[1]).append(System.lineSeparator())
+                .append(createReport()[2]).append(System.lineSeparator());
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+            bufferedWriter.write(String.format("%10s", b));
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t write data to" + toFileName, e);
+        }
     }
 }
+
