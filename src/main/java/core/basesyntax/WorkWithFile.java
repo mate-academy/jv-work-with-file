42bc634f2cf1789f3290062089172e8c65b746a0
anufriiev <dimaanufriev1012@gmail.com>
2021-04-05 19:41:23 +0300

    HW-5.Added setResult method, setStatistic method,try catch with resources blocks and constants

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -9,37 +9,44 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    private static final String REGEX_CONST = ",";
+    private static final String CSV_SEPARATOR = ",";
+    private static final String SUPPLY_WORD = "supply";
+    private static final String BUY_WORD = "buy";
+    private static final int FIRST_ELEMENT = 1;
+    private static final int ZERO_ELEMENT = 0;
+    private StringBuilder builder = new StringBuilder();
 
     public void getStatistic(String fromFileName, String toFileName) {
-        int buy = 0;
-        int supply = 0;
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
             String line;
             while ((line = reader.readLine()) != null) {
-                String[] stringArray = line.split(REGEX_CONST);
-                if (stringArray[0].equals("supply")) {
-                    supply += Integer.parseInt(stringArray[1]);
-                } else if (stringArray[0].equals("buy")) {
-                    buy += Integer.parseInt(stringArray[1]);
-                }
+                String[] stringArray = line.split(CSV_SEPARATOR);
             }
-            reader.close();
         } catch (FileNotFoundException e) {
             throw new RuntimeException("File not found " + fromFileName, e);
         } catch (IOException e) {
             throw new RuntimeException("File not found " + fromFileName, e);
         }
-        StringBuilder builder = new StringBuilder();
-        builder.append("supply").append(",").append(supply).append(System.lineSeparator())
-                .append("buy").append(",").append(buy).append(System.lineSeparator())
+    }
+
+    private void getResult(String[] stringArray) {
+        int buy = 0;
+        int supply = 0;
+        if (stringArray[ZERO_ELEMENT].equals(SUPPLY_WORD)) {
+            supply += Integer.parseInt(stringArray[FIRST_ELEMENT]);
+        } else if (stringArray[ZERO_ELEMENT].equals(BUY_WORD)) {
+            buy += Integer.parseInt(stringArray[FIRST_ELEMENT]);
+        }
+        builder.append(SUPPLY_WORD).append(",").append(SUPPLY_WORD).append(System.lineSeparator())
+                .append(BUY_WORD).append(",").append(BUY_WORD).append(System.lineSeparator())
                 .append("result").append(",").append(supply - buy);
+    }
+
+    private void setStatistic(String fromFileName, String toFileName) {
         File file = new File(toFileName);
-        try {
-            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName));
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
             bufferedWriter.write(builder.toString());
-            bufferedWriter.close();
         } catch (IOException e) {
             throw new RuntimeException("File not found " + fromFileName, e);
         }
