ee398089b40590234c259989381fbaf9d912e5c2
roman <nashpatsa@gmai.com>
2021-04-05 16:03:37 +0300

    I changed my mistakes

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,27 +2,31 @@ package core.basesyntax;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    public static void getStatistic(String fromFileName, String toFileName) {
-        File fileFrom = new File(fromFileName);
-        File fileTo = new File(toFileName);
-        StringBuilder returnedStringToFile = new StringBuilder();
+    private static final int INDEX_OF_SUPPLY = 0;
+    private static final int INDEX_OF_VALUE = 1;
+    private static final String VALUE_SPLITTER = ",";
+    private static final String SUPPLY_VALUE = "supply";
+    private static final String RESULT = "result";
+    private static final String BUY = "buy";
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        String stringWithResult;
         int supply = 0;
         int buy = 0;
 
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileFrom))) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             String value = bufferedReader.readLine();
             while (value != null) {
-                String[] stringsFromLine = value.split(",");
-                if (stringsFromLine[0].equals("supply")) {
-                    supply += Integer.parseInt(stringsFromLine[1]);
+                String[] stringsFromLine = value.split(VALUE_SPLITTER);
+                if (stringsFromLine[INDEX_OF_SUPPLY].equals(SUPPLY_VALUE)) {
+                    supply += Integer.parseInt(stringsFromLine[INDEX_OF_VALUE]);
                 } else {
-                    buy += Integer.parseInt(stringsFromLine[1]);
+                    buy += Integer.parseInt(stringsFromLine[INDEX_OF_VALUE]);
                 }
                 value = bufferedReader.readLine();
             }
@@ -30,12 +34,22 @@ public class WorkWithFile {
             throw new RuntimeException("Can't write from file");
         }
 
-        returnedStringToFile.append("supply,").append(supply).append("\n")
-                .append("buy,").append(buy).append("\n")
-                .append("result,").append(supply - buy);
+        stringWithResult = createString(supply,buy);
+        writeDataFromFile(toFileName,stringWithResult);
 
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileTo))) {
-            bufferedWriter.write(returnedStringToFile.toString());
+    }
+
+    private String createString(int supply, int buy) {
+        StringBuilder returnedStringToFile = new StringBuilder();
+        returnedStringToFile.append(SUPPLY_VALUE).append(VALUE_SPLITTER).append(supply).append("\n")
+                .append(BUY).append(VALUE_SPLITTER).append(buy).append("\n")
+                .append(RESULT).append(VALUE_SPLITTER).append(supply - buy);
+        return returnedStringToFile.toString();
+    }
+
+    private void writeDataFromFile(String toFileName, String dataName) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+            bufferedWriter.write(dataName);
         } catch (IOException e) {
             throw new RuntimeException("Can't write file");
         }
