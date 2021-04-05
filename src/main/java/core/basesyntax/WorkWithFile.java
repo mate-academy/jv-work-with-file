9c6638b3f09609dc4dd92f5f7cc407d639c4cd69
IhorRome <ihor.rome@gmail.com>
2021-04-05 16:42:10 +0300

    done it with suggested logic

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,7 +2,6 @@ package core.basesyntax;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -16,24 +15,26 @@ public class WorkWithFile {
     private static final char COMMA = ',';
 
     public void getStatistic(String fromFileName, String toFileName) {
-        File fileToRead = new File(fromFileName);
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName, true))) {
-            StringBuilder builder = new StringBuilder();
-            builder.append(SUPPLY)
-                    .append(COMMA)
-                    .append(amountOfOneType(fileToRead, SUPPLY))
-                    .append(System.lineSeparator())
-                    .append(BUY)
-                    .append(COMMA)
-                    .append(amountOfOneType(fileToRead, BUY))
-                    .append(System.lineSeparator())
-                    .append(RESULT)
-                    .append(COMMA)
-                    .append((amountOfOneType(fileToRead, SUPPLY)
-                            - amountOfOneType(fileToRead, BUY)));
-            bufferedWriter.write(builder.toString());
+        int totalSupply = 0;
+        int totalBuy = 0;
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
+            String value = bufferedReader.readLine();
+            while (value != null) {
+                if (getName(value).equals(SUPPLY)) {
+                    totalSupply += getAmount(value);
+                }
+                if (getName(value).equals(BUY)) {
+                    totalBuy += getAmount(value);
+                }
+                value = bufferedReader.readLine();
+            }
         } catch (IOException e) {
             throw new RuntimeException("can't write to file");
+        }
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+            bufferedWriter.write(buildInput(totalSupply, totalBuy));
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write to file", e);
         }
     }
 
@@ -45,19 +46,19 @@ public class WorkWithFile {
         return Integer.parseInt(lineFromFile.split(",")[VALUE_INDEX]);
     }
 
-    private int amountOfOneType(File file, String name) {
-        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-            String value = reader.readLine();
-            int totalAmount = 0;
-            while (value != null) {
-                if (getName(value).equals(name)) {
-                    totalAmount += getAmount(value);
-                }
-                value = reader.readLine();
-            }
-            return totalAmount;
-        } catch (IOException e) {
-            throw new RuntimeException("can't read from file", e);
-        }
+    private String buildInput(int totalSupply, int totalBuy) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(SUPPLY)
+                .append(COMMA)
+                .append(totalSupply)
+                .append(System.lineSeparator())
+                .append(BUY)
+                .append(COMMA)
+                .append(totalBuy)
+                .append(System.lineSeparator())
+                .append(RESULT)
+                .append(COMMA)
+                .append(totalSupply - totalBuy);
+        return builder.toString();
     }
 }
