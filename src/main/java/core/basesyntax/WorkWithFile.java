93003a24c3cc37dc1a30f0ce2420c16d655354a4
Nazar-yo <bilaknazar@gmail.com>
2021-04-05 14:31:58 +0300

    All fixed

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,12 +2,14 @@ package core.basesyntax;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    private static final String SPLIT_SYMBOL = ",";
+    private static final int INDEX_OF_NAME = 0;
+    private static final int INDEX_OF_VALUE = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
@@ -18,25 +20,24 @@ public class WorkWithFile {
     }
 
     private StringBuilder readFromFile(String fromFileName) {
-        File file = new File(fromFileName);
-        int supplyCounter = 0;
-        int buyCounter = 0;
+        int supplySum = 0;
+        int buySum = 0;
         try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName));
             String line = bufferedReader.readLine();
             while (line != null) {
-                String[] information = line.split(",");
-                if ("supply".equals(information[0])) {
-                    supplyCounter += Integer.parseInt(information[1]);
+                String[] information = line.split(SPLIT_SYMBOL);
+                if ("supply".equals(information[INDEX_OF_NAME])) {
+                    supplySum += Integer.parseInt(information[INDEX_OF_VALUE]);
                 } else {
-                    buyCounter += Integer.parseInt(information[1]);
+                    buySum += Integer.parseInt(information[INDEX_OF_VALUE]);
                 }
                 line = bufferedReader.readLine();
             }
         } catch (IOException e) {
             throw new RuntimeException("Can`t find or read file" + fromFileName, e);
         }
-        return crateTabular(supplyCounter, buyCounter);
+        return crateTabular(supplySum, buySum);
     }
 
     private StringBuilder crateTabular(int supplyCounter, int buyCounter) {
