e6b18d2396f5baf312f013a34c7ce4ac482f1fd8
Lugos <lugos94@gmail.com>
2021-04-06 00:02:56 +0300

    Created HW-7

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,8 +1,48 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WorkWithFile {
+    public static final String SUPPLY = "supply";
 
     public void getStatistic(String fromFileName, String toFileName) {
+        int supplyAmount = 0;
+        int buyAmount = 0;
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
+            String textLines = bufferedReader.readLine();
 
+            while (textLines != null) {
+                String[] lineInfo = textLines.split(",");
+                if (lineInfo[0].equals(SUPPLY)) {
+                    supplyAmount += Integer.parseInt(lineInfo[1]);
+                } else {
+                    buyAmount += Integer.parseInt(lineInfo[1]);
+                }
+                textLines = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read data from the file " + fromFileName, e);
+        }
+        writeToFile(toFileName, getReport(supplyAmount, buyAmount));
+    }
+
+    private String getReport(int supply, int buy) {
+        return "supply," + supply + System.lineSeparator()
+                + "buy," + buy + System.lineSeparator()
+                + "result," + (supply - buy);
+    }
+
+    private void writeToFile(String toFileName, String data) {
+        File file = new File(toFileName);
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
+            bufferedWriter.write(data);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write data to the file " + toFileName, e);
+        }
     }
 }
