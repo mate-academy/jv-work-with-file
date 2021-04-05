fc169bc852e9d561a814cfa610ab59d4c7f3512c
SerKisJD <sergej.kislyakov@gmail.com>
2021-04-05 14:23:00 +0300

    2. added final fields, renamed few variables, added stringBuilder

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -9,10 +9,14 @@ import java.io.IOException;
 public class WorkWithFile {
     private static final String BUY = "buy";
     private static final String COMMA = ",";
+    private static final String SUPPLY = "supply";
+    private static final String RESULT = "result";
+    private static final int AMOUNT_INDEX = 1;
+    private static final int OPERATION_INDEX = 0;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        String readDataFromFile = readDataFromFile(fromFileName);
-        writeDataFromFile(readDataFromFile, toFileName);
+        String dataFromFile = analyseDataFromFile(fromFileName);
+        writeDataFromFile(dataFromFile, toFileName);
     }
 
     private void writeDataFromFile(String dataToFile, String fileName) {
@@ -23,25 +27,30 @@ public class WorkWithFile {
         }
     }
 
-    private String readDataFromFile(String fileName) {
+    private String analyseDataFromFile(String fileName) {
         int buy = 0;
         int supply = 0;
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
-            String readLine = bufferedReader.readLine();
-            while (readLine != null) {
-                String[] value = readLine.split(COMMA);
-                if (value[0].equals(BUY)) {
-                    buy += Integer.parseInt(value[1]);
-                } else {
-                    supply += Integer.parseInt(value[1]);
+            String line = bufferedReader.readLine();
+            while (line != null) {
+                String[] value = line.split(COMMA);
+                if (value[OPERATION_INDEX].equals(BUY)) {
+                    buy += Integer.parseInt(value[AMOUNT_INDEX]);
+                } else if (value[OPERATION_INDEX].equals(SUPPLY)) {
+                    supply += Integer.parseInt(value[AMOUNT_INDEX]);
                 }
-                readLine = bufferedReader.readLine();
+                line = bufferedReader.readLine();
             }
         } catch (IOException e) {
             throw new RuntimeException("Can't read file" + fileName, e);
         }
-        return "supply," + supply + System.lineSeparator()
-                + "buy," + buy + System.lineSeparator()
-                + "result," + (supply - buy);
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(SUPPLY).append(COMMA).append(supply)
+                .append(System.lineSeparator())
+                .append(BUY).append(COMMA).append(buy)
+                .append(System.lineSeparator())
+                .append(RESULT).append(COMMA).append(supply - buy);
+        return stringBuilder.toString();
     }
 }
