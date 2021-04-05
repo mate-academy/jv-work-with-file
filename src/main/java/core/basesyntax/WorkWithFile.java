aa715beefa56be231803ca2d6924a80b113eff01
unknown <vovazalli@gmail.com>
2021-04-05 16:55:52 +0300

    createReport method edited, data is written outside reader

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,20 +2,18 @@ package core.basesyntax;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
+        int supply = 0;
+        int buy = 0;
+
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             String value = bufferedReader.readLine();
-
-            int supply = 0;
-            int buy = 0;
             String[] readLine;
-
             while (value != null) {
                 readLine = value.split(",");
                 if (readLine[0].equals("supply")) {
@@ -25,24 +23,23 @@ public class WorkWithFile {
                 }
                 value = bufferedReader.readLine();
             }
-            writeToFile(createReport(toFileName), supply, buy);
         } catch (IOException e) {
             throw new RuntimeException("File does not exist", e);
         }
+        writeToFile(toFileName, createReport(supply, buy));
     }
 
-    private File createReport(String newFile) {
-        return new File(newFile);
-    }
-
-    private void writeToFile(File file, int supply, int buy) {
+    private String createReport(int supply, int buy) {
         int result = supply - buy;
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("supply,").append(supply).append(System.lineSeparator())
+                .append("buy,").append(buy).append(System.lineSeparator())
+                .append("result,").append(result);
+        return stringBuilder.toString();
+    }
+
+    private void writeToFile(String file, String finalReport) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
-            StringBuilder stringBuilder = new StringBuilder();
-            stringBuilder.append("supply,").append(supply).append(System.lineSeparator())
-                    .append("buy,").append(buy).append(System.lineSeparator())
-                    .append("result,").append(result);
-            String finalReport = String.format("%10s", stringBuilder);
             bufferedWriter.write(finalReport);
         } catch (IOException e) {
             throw new RuntimeException("File does not exist", e);
