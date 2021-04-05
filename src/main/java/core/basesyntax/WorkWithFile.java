40ec173f890d3fb7b75ae5dae111d2372262981c
redxp <redxpertus1@gmail.com>
2021-04-05 14:55:51 +0300

    work with csv files v.02 in q&a Sofia told to leave for I and make pull without change this cycle because in my case I lose logic of my code

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,6 +8,9 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+    private static final String RESULT = "result";
 
     public void getStatistic(String fromFileName, String toFileName) {
         File informationFromFile = new File(fromFileName);
@@ -19,17 +22,19 @@ public class WorkWithFile {
         try {
             FileReader fileReader = new FileReader(informationFromFile);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
-            while (bufferedReader.ready()) {
-                stringBuilder.append(bufferedReader.readLine()).append(",");
+            String line = bufferedReader.readLine();
+            while (line != null) {
+                stringBuilder.append(line).append(",");
+                line = bufferedReader.readLine();
             }
         } catch (IOException e) {
-            throw new RuntimeException("Some went wrong", e);
+            throw new RuntimeException("Can't find the file", e);
         }
         String[] datesFromFile = stringBuilder.toString().split(",");
         for (int i = 0; i < datesFromFile.length; i++) {
-            if (datesFromFile[i].equals("supply")) {
+            if (datesFromFile[i].equals(SUPPLY)) {
                 supplySummary += Integer.parseInt(datesFromFile[i + 1]);
-            } else if (datesFromFile[i].equals("buy")) {
+            } else if (datesFromFile[i].equals(BUY)) {
                 buySummary += Integer.parseInt(datesFromFile[i + 1]);
             }
         }
@@ -37,15 +42,15 @@ public class WorkWithFile {
         try {
             FileWriter fileWriter = new FileWriter(resultFile);
             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-            bufferedWriter.write("supply," + supplySummary);
+            bufferedWriter.write(SUPPLY + "," + supplySummary);
             bufferedWriter.newLine();
-            bufferedWriter.write("buy," + buySummary);
+            bufferedWriter.write(BUY + "," + buySummary);
             bufferedWriter.newLine();
-            bufferedWriter.write("result," + operationResult);
+            bufferedWriter.write(RESULT + "," + operationResult);
             bufferedWriter.flush();
             bufferedWriter.close();
         } catch (IOException e) {
-            throw new RuntimeException("Some went wrong", e);
+            throw new RuntimeException("Can't write data to file", e);
         }
     }
 }
