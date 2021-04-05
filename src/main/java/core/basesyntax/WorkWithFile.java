936c228f9c2de531a4dbbf1b89c2eb5355ab748c
Stanislav Aleksandrovych <astanislav2361@gmail.com>
2021-04-05 15:23:20 +0300

    reworked two methods

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,61 +2,46 @@ package core.basesyntax;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    private static final String SENTENCE_SPLITTER = "next sentence";
+    private static final String SENTENCE_SPLITTER = " ";
     private static final String SUBSTRING_REGEX = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        String[] information = getDataFromFile(fromFileName);
-        String[] reportAboutInformation = createReport(information);
-        writeToFile(reportAboutInformation, toFileName);
+        String[] reportFromFile = getReportFromFile(fromFileName);
+        writeToFile(reportFromFile, toFileName);
+
     }
 
-    private String[] getDataFromFile(String fromFileName) {
-        StringBuilder stringBuilder = new StringBuilder();
+    private String[] getReportFromFile(String fromFileName) {
+        int supplyPrice = 0;
+        int buyPrice = 0;
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             String line = bufferedReader.readLine();
             while (line != null) {
-                stringBuilder.append(line).append(SENTENCE_SPLITTER);
+                if (line.contains("supply")) {
+                    supplyPrice += Integer.parseInt(line.substring(line.indexOf(',') + 1));
+                } else {
+                    buyPrice += Integer.parseInt(line.substring(line.indexOf(',') + 1));
+                }
                 line = bufferedReader.readLine();
             }
         } catch (IOException e) {
             throw new RuntimeException("Can't read file", e);
         }
-        return stringBuilder.toString().split(SENTENCE_SPLITTER);
+
+        String report = "supply," + supplyPrice + SENTENCE_SPLITTER
+                + "buy," + buyPrice + SENTENCE_SPLITTER
+                + "result," + (supplyPrice - buyPrice);
+        return report.split(SENTENCE_SPLITTER);
     }
 
-    private String[] createReport(String[] records) {
-        int supplyPrice = 0;
-        int buyPrice = 0;
-        String[] report = new String[3];
-
-        for (String someDatum : records) {
-            if (someDatum.contains("supply")) {
-                supplyPrice += Integer.parseInt(someDatum.substring(someDatum
-                        .indexOf(SUBSTRING_REGEX) + 1));
-
-            } else {
-                buyPrice += Integer.parseInt(someDatum.substring(someDatum
-                        .indexOf(SUBSTRING_REGEX) + 1));
-            }
-
-            report[0] = "supply" + "," + supplyPrice;
-            report[1] = "buy" + "," + buyPrice;
-            report[2] = "result," + (supplyPrice - buyPrice);
-        }
-        return report;
-    }
-
-    private void writeToFile(String[] reports, String toFileName) {
-        File file = new File(toFileName);
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file, true))) {
-            for (String s : reports) {
+    private void writeToFile(String[] report, String toFileName) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName, true))) {
+            for (String s : report) {
                 bufferedWriter.write(s + System.lineSeparator());
             }
         } catch (IOException e) {
