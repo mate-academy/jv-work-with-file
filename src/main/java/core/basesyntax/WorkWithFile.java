08c074a715091795c50d92a111bf08da16824322
Elia2909 <290900rR>
2021-04-05 17:00:40 +0300

    fix edits

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,46 +7,49 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+    private static final String RESULT = "result";
 
     public void getStatistic(String fromFileName, String toFileName) {
         String dataFromFileName = dataFromFileName(fromFileName);
         writerToFile(dataFromFileName, toFileName);
     }
 
-    public String dataFromFileName(String fromFileName) {
+    private String dataFromFileName(String fromFileName) {
         int supply = 0;
         int buy = 0;
-        int result = 0;
+        StringBuilder stringBuilder = new StringBuilder();
 
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             String value = bufferedReader.readLine();
             while (value != null) {
                 String[] resultNumbers = value.split(",");
-                if (resultNumbers[0].equals("supply")) {
+                if (resultNumbers[0].equals(SUPPLY)) {
                     supply += Integer.parseInt(resultNumbers[1]);
                 }
-                if (resultNumbers[0].equals("buy")) {
+                if (resultNumbers[0].equals(BUY)) {
                     buy += Integer.parseInt(resultNumbers[1]);
                 }
                 value = bufferedReader.readLine();
             }
-            result = supply - buy;
 
         } catch (IOException e) {
-            throw new RuntimeException("Can't read file");
+            throw new RuntimeException("Can't read file", e);
         }
-        return "supply," + supply
-                + System.lineSeparator()
-                + "buy," + buy
-                + System.lineSeparator()
-                + "result," + result;
+        stringBuilder.append(SUPPLY).append(",").append(supply)
+                .append(System.lineSeparator())
+                .append(BUY).append(",").append(buy)
+                .append(System.lineSeparator())
+                .append(RESULT).append(",").append(supply-buy);
+        return stringBuilder.toString();
     }
 
-    public void writerToFile(String resultData, String toFileName) {
+    private void writerToFile(String resultData, String toFileName) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
             bufferedWriter.write(resultData);
         } catch (IOException e) {
-            throw new RuntimeException("Can't open file");
+            throw new RuntimeException("Can't open file", e);
         }
     }
 
