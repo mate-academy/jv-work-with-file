cb9d767ad8fc4f734f349288c4f5839a984b2d80
Cyouffy <cyouffy@gmail.com>
2021-04-05 17:58:52 +0300

    fixed requested changes

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,6 +7,10 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    private final static String BUY = "buy";
+    private final static String SUPPLY = "supply";
+    private final static String COMA = ",";
+    private final static String RESULT = "result";
 
     public void getStatistic(String fromFileName, String toFileName) {
         File file = new File(fromFileName);
@@ -15,8 +19,8 @@ public class WorkWithFile {
         int supply = 0;
 
         for (String line : array) {
-            String[] temp = line.split(",");
-            if (temp[0].equals("buy")) {
+            String[] temp = line.split(COMA);
+            if (temp[0].equals(BUY)) {
                 buy += Integer.parseInt(temp[1]);
             } else {
                 supply += Integer.parseInt(temp[1]);
@@ -28,24 +32,23 @@ public class WorkWithFile {
 
     private String[] readFile(File file) {
         StringBuilder wordsArray = new StringBuilder();
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(file));
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             String value = reader.readLine();
             while (value != null) {
                 wordsArray.append(value).append(" ");
                 value = reader.readLine();
             }
         } catch (IOException e) {
-            throw new RuntimeException("Can't read the file");
+            throw new RuntimeException("Can't read the file", e);
         }
         return wordsArray.toString().split(" ");
     }
 
     private void writeToFile(String toFileName, int buy, int supply) {
         try (FileWriter fileWriter = new FileWriter(toFileName)) {
-            fileWriter.write("supply," + supply + System.lineSeparator());
-            fileWriter.write("buy," + buy + System.lineSeparator());
-            fileWriter.write("result," + (supply - buy));
+            fileWriter.write(SUPPLY + COMA + supply + System.lineSeparator());
+            fileWriter.write(BUY + COMA + buy + System.lineSeparator());
+            fileWriter.write(RESULT + COMA + (supply - buy));
         } catch (Exception e) {
             throw new RuntimeException("Can't read data from the file ", e);
         }
