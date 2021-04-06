14badc6b1d903df4fabe12ff0e43a29659d49076
Gord-Nik <75978452+Gord-Nik@users.noreply.github.com>
2021-04-06 08:20:09 +0300

    add changes2

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -10,21 +10,21 @@ public class WorkWithFile {
     private static final String SUPPLY = "supply";
     private static final String BUY = "buy";
     private static final String RESULT = "result";
+    private static final int INDEX_FOR_OPERATION_TYPE = 0;
+    private static final int INDEX_FOR_AMOUNT = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
         int supplyNumber = 0;
         int buyNumber = 0;
-        int resultNumber = 0;
-        try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName));
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             String value = bufferedReader.readLine();
             while (value != null) {
                 String[] stringLine = value.split(",");
-                if (stringLine[0].equals(SUPPLY) == true) {
-                    supplyNumber += Integer.parseInt(stringLine[1]);
+                if (stringLine[INDEX_FOR_OPERATION_TYPE].equals(SUPPLY)) {
+                    supplyNumber += Integer.parseInt(stringLine[INDEX_FOR_AMOUNT]);
                 }
-                if (stringLine[0].equals(BUY) == true) {
-                    buyNumber += Integer.parseInt(stringLine[1]);
+                if (stringLine[INDEX_FOR_OPERATION_TYPE].equals(BUY)) {
+                    buyNumber += Integer.parseInt(stringLine[INDEX_FOR_AMOUNT]);
                 }
                 value = bufferedReader.readLine();
             }
@@ -33,6 +33,9 @@ public class WorkWithFile {
         }
         String output = SUPPLY + "," + supplyNumber + System.lineSeparator() + BUY + ","
                 + buyNumber + System.lineSeparator() + RESULT + "," + (supplyNumber - buyNumber);
+    }
+
+    private void writeToWile(String output, String toFileName) {
         try {
             BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName));
             bufferedWriter.write(output);
