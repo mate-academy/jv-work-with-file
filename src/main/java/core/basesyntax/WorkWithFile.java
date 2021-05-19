5fcb96557d7453f0782ead34756a89036759b4df
forsfakel <81535727+forsfakel@users.noreply.github.com>
2021-05-19 22:39:34 +0300

    The body method getStatistic is divided into two method: readFile and writeFile;
    Blocks try-catch changed to try with resources;
    
    added constants :
        private String supply = "supply";
        private String buy = "buy";
        private char csvSeparator = ',';
        private int startIndex = 0;
        private int stepIndex = 1;
        private int startValue = 0;
    
    public resultMap --> private resultMap

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,7 +2,6 @@ package core.basesyntax;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -10,27 +9,38 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class WorkWithFile {
+    private final String supply = "supply";
+    private final String buy = "buy";
+    private final char csvSeparator = ',';
+    private final int startIndex = 0;
+    private final int stepIndex = 1;
+    private final int startValue = 0;
+    private Map<String, Integer> resultMap = new HashMap<>();
 
     public void getStatistic(String fromFileName, String toFileName) {
-        File fromFile = new File(fromFileName);
-        final String supply = "supply";
-        final String buy = "buy";
-        Map<String, Integer> resultMap = new HashMap<>();
-        resultMap.put(supply, 0);
-        resultMap.put(buy, 0);
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(fromFile));
+        resultMap.put(supply, startValue);
+        resultMap.put(buy, startValue);
+        readFile(fromFileName);
+        writeFile(toFileName);
+    }
+
+    private void readFile(String fromFileName) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
             while (reader.ready()) {
                 String lineInFile = reader.readLine();
-                String operation = lineInFile.substring(0, lineInFile.indexOf(','));
-                String textValue = lineInFile.substring(lineInFile.indexOf(',') + 1);
+                String operation = lineInFile
+                        .substring(startIndex, lineInFile.indexOf(csvSeparator));
+                String textValue = lineInFile
+                        .substring(lineInFile.indexOf(csvSeparator) + stepIndex);
                 Integer valueOperation = Integer.parseInt(textValue);
                 resultMap.put(operation, (resultMap.get(operation) + valueOperation));
             }
-            reader.close();
         } catch (IOException e) {
-            throw new RuntimeException("Error!!! Can not read file.");
+            throw new RuntimeException("Error!!! Can not read file.", e);
         }
+    }
+
+    private void writeFile(String toFileName) {
         try {
             BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName));
             writer.write("supply," + resultMap.get(supply) + System.lineSeparator()
@@ -38,8 +48,7 @@ public class WorkWithFile {
                     + "result," + (resultMap.get(supply) - resultMap.get(buy)));
             writer.close();
         } catch (IOException e) {
-            throw new RuntimeException("Error!!! Can not write file.");
+            throw new RuntimeException("Error!!! Can not write file.", e);
         }
-
     }
 }
