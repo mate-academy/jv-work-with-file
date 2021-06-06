49151f9abf01718f9f4919b23018c012be8bfa09
EugeneSinica <eugenesinica@gmail.com>
2021-06-06 13:34:07 +0300

    Some checkstyle fixes and little changes in code

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -10,24 +10,29 @@ import java.nio.file.Path;
 
 public class WorkWithFile {
 
-    int supplySum = 0;
-    int buySum = 0;
-    int result = buySum - supplySum;
+    private int supplySum = 0;
+    private int buySum = 0;
 
     public void getStatistic(String fromFileName, String toFileName) {
         readFromFile(fromFileName);
+        try {
+            Files.createFile(Path.of(toFileName));
+        } catch (IOException e) {
+            throw new RuntimeException("Some troubles with creating a file " + toFileName + e);
+        }
         writeToFile(toFileName);
     }
 
     private void readFromFile(String fromFileName) {
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
-            int AMOUNT_INDEX = 1;
-            final String SUPPLY = "supply";
-            String line = bufferedReader.readLine();
-            if (line.contains(SUPPLY)) {
-                supplySum += Integer.parseInt(line.split(",")[AMOUNT_INDEX]);
-            } else {
-                buySum += Integer.parseInt(line.split(",")[AMOUNT_INDEX]);
+            int amountIndex = 1;
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                if (line.contains("supply")) {
+                    supplySum += Integer.parseInt(line.split(",")[amountIndex]);
+                } else {
+                    buySum += Integer.parseInt(line.split(",")[amountIndex]);
+                }
             }
         } catch (IOException e) {
             throw new RuntimeException("Can't read from file " + fromFileName + e);
@@ -35,14 +40,13 @@ public class WorkWithFile {
     }
 
     private String makeReport() {
-        return "supply" + "," + supplySum + System.lineSeparator() + "n" +
-                "buy" + "," + buySum + System.lineSeparator() + "n" +
-                "result" + "," + result + System.lineSeparator() + "n";
+        return "supply" + "," + supplySum + System.lineSeparator()
+                + "buy" + "," + buySum + System.lineSeparator()
+                + "result" + "," + (supplySum - buySum) + System.lineSeparator();
     }
 
-    private void writeToFile (String toFileName) {
+    private void writeToFile(String toFileName) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
-            Files.createFile(Path.of(toFileName));
             bufferedWriter.write(makeReport());
         } catch (IOException e) {
             throw new RuntimeException("Can't write to file " + toFileName + e);
