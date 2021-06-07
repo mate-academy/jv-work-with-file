873a1dcc43f69af420a9b8688d80c81e81301891
vKutik <84785564+vKutik@users.noreply.github.com>
2021-06-07 17:17:23 +0300

    fix Checkstyle

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,32 +7,33 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    public static final int INDEX_OPERATION = 0;
+    public static final int INDEX_SCORE = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
         int scoreSupply = 0;
         int scoreBuy = 0;
         StringBuilder stringBuilder = new StringBuilder();
-        try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName));
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             String infoLine;
             while ((infoLine = bufferedReader.readLine()) != null) {
                 String[] valuesLine = infoLine.split(",");
-                if (valuesLine[0].equals("supply")) {
-                    scoreSupply += Integer.valueOf(valuesLine[1]);
+                if (valuesLine[INDEX_OPERATION].equals("supply")) {
+                    scoreSupply += Integer.valueOf(valuesLine[INDEX_SCORE]);
                 } else {
-                    scoreBuy += Integer.parseInt(valuesLine[1]);
+                    scoreBuy += Integer.parseInt(valuesLine[INDEX_SCORE]);
                 }
             }
-            stringBuilder.append("supply,").append(scoreSupply).append(System.lineSeparator())
-                    .append("buy,").append(scoreBuy).append(System.lineSeparator())
-                    .append("result,").append(scoreSupply - scoreBuy);
-            writeFile(toFileName,stringBuilder.toString());
         } catch (IOException e) {
             throw new RuntimeException("Can`t read file", e);
         }
+        stringBuilder.append("supply,").append(scoreSupply).append(System.lineSeparator())
+                .append("buy,").append(scoreBuy).append(System.lineSeparator())
+                .append("result,").append(scoreSupply - scoreBuy);
+        writeFile(toFileName,stringBuilder.toString());
     }
 
-    public void writeFile(String file, String info) {
+    private void writeFile(String file, String info) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
             bufferedWriter.write(info);
         } catch (IOException e) {
