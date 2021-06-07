56df946223114827938997f07e84aba2e6dc58ef
Denis <deniskichan@gmail.com>
2021-06-07 12:54:17 +0300

    added few constants and implemented additional method for writing to file

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -9,18 +9,15 @@ import java.io.IOException;
 public class WorkWithFile {
     private static final int OPERATION_TYPE_INDEX = 0;
     private static final int AMOUNT_INDEX = 1;
-    private static final String SUPPLY = "supply,";
-    private static final String BUY = "buy,";
-    private static final String RESULT = "result,";
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+    private static final String RESULT = "result";
+    private static final String DELIMITER = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
         String infoFromFile = readFromFile(fromFileName);
         String report = createReport(infoFromFile);
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
-            bufferedWriter.write(report);
-        } catch (IOException e) {
-            throw new RuntimeException("Can't write to file", e);
-        }
+        writeToFile(toFileName, report);
     }
 
     private String readFromFile(String infoFromFile) {
@@ -42,17 +39,26 @@ public class WorkWithFile {
         int buyAmount = 0;
         String[] splittedFile = infoFromFile.replace("\r", "").split("\n");
         for (String splittedData : splittedFile) {
-            String[] splittedLine = splittedData.split(",");
-            if (splittedLine[OPERATION_TYPE_INDEX].equals("supply")) {
+            String[] splittedLine = splittedData.split(DELIMITER);
+            if (splittedLine[OPERATION_TYPE_INDEX].equals(SUPPLY)) {
                 supplyAmount += Integer.parseInt(splittedLine[AMOUNT_INDEX]);
             } else {
                 buyAmount += Integer.parseInt(splittedLine[AMOUNT_INDEX]);
             }
         }
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(SUPPLY).append(supplyAmount)
-                .append(System.lineSeparator()).append(BUY).append(buyAmount)
-                .append(System.lineSeparator()).append(RESULT).append(supplyAmount - buyAmount);
+        stringBuilder.append(SUPPLY).append(DELIMITER).append(supplyAmount)
+                .append(System.lineSeparator()).append(BUY).append(DELIMITER).append(buyAmount)
+                .append(System.lineSeparator()).append(RESULT).append(DELIMITER)
+                .append(supplyAmount - buyAmount);
         return stringBuilder.toString();
+    }
+
+    private void writeToFile(String fileName, String content) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName))) {
+            bufferedWriter.write(content);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write to file", e);
+        }
     }
 }
