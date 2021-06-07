676166fabf56c5610d671cc664498093e4ef43a4
“Valerii <torsher09@gmail.com>
2021-06-07 14:28:00 +0300

    made methods to processing data, made global values

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,43 +1,31 @@
 package core.basesyntax;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class WorkWithFile {
+    private int[] supply = new int[10];
+    private int sumSupply = 0;
+    private int counterSupply = 0;
+    private int[] buy = new int[10];
+    private int sumBuy = 0;
+    private int counterBuy = 0;
+    private StringBuilder stringBuilder = new StringBuilder();
 
     public void getStatistic(String fromFileName, String toFileName) {
-        int[] supply = new int[10];
-        int sumSupply = 0;
-        int counterSupply = 0;
-        int[] buy = new int[10];
-        int sumBuy = 0;
-        int counterBuy = 0;
-        StringBuilder stringBuilder = new StringBuilder();
-        try {
-            File file = new File(fromFileName);
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName));
-            String workingString = bufferedReader.readLine();
-            while (workingString != null) {
-                if (workingString.substring(0, workingString.indexOf(',')).equals("supply")) {
-                    int i = 0;
-                    supply[counterSupply] = Integer.parseInt(workingString.substring(workingString.indexOf(',') + 1));
-                    counterSupply++;
-                } else {
-                    int i = 0;
-                    buy[counterBuy] = Integer.parseInt(workingString.substring(workingString.indexOf(',') + 1));
-                    counterBuy++;
-                }
-                workingString = bufferedReader.readLine();
-            }
-            bufferedReader.close();
-        } catch (RuntimeException | IOException e) {
-            throw new RuntimeException("Can`t read from file", e);
-        }
-
+        readFromFile(fromFileName);
         for (int i = 0; i < supply.length; i++) {
             sumSupply += supply[i];
             sumBuy += buy[i];
         }
+        writeToFile(toFileName);
+    }
 
+    public void writeToFile(String toFileName) {
         File file = new File(toFileName);
         try {
             file.createNewFile();
@@ -47,11 +35,14 @@ public class WorkWithFile {
         try {
             stringBuilder.delete(0, stringBuilder.length());
             BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName));
-            bufferedWriter.write(String.valueOf(stringBuilder.append("supply,").append(sumSupply)));
+            bufferedWriter.write(String.valueOf(stringBuilder
+                    .append("supply,").append(sumSupply)));
             stringBuilder.delete(0, stringBuilder.length());
-            bufferedWriter.write(String.valueOf(stringBuilder.append("\r\nbuy,").append(sumBuy)));
+            bufferedWriter.write(String.valueOf(stringBuilder
+                    .append("\r\nbuy,").append(sumBuy)));
             stringBuilder.delete(0, stringBuilder.length());
-            bufferedWriter.write(String.valueOf(stringBuilder.append("\r\nresult,").append(sumSupply - sumBuy)));
+            bufferedWriter.write(String.valueOf(stringBuilder
+                    .append("\r\nresult,").append(sumSupply - sumBuy)));
             stringBuilder.delete(0, stringBuilder.length());
             bufferedWriter.close();
         } catch (RuntimeException | IOException e) {
@@ -59,11 +50,26 @@ public class WorkWithFile {
         }
     }
 
-    public void writeToFile() {
-
-    }
-
-    public void readFromFile() {
-
+    public void readFromFile(String fromFileName) {
+        try {
+            File file = new File(fromFileName);
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName));
+            String workingString = bufferedReader.readLine();
+            while (workingString != null) {
+                if (workingString.substring(0, workingString.indexOf(',')).equals("supply")) {
+                    supply[counterSupply] = Integer.parseInt(
+                            workingString.substring(workingString.indexOf(',') + 1));
+                    counterSupply++;
+                } else {
+                    buy[counterBuy] = Integer.parseInt(
+                            workingString.substring(workingString.indexOf(',') + 1));
+                    counterBuy++;
+                }
+                workingString = bufferedReader.readLine();
+            }
+            bufferedReader.close();
+        } catch (RuntimeException | IOException e) {
+            throw new RuntimeException("Can`t read from file", e);
+        }
     }
 }
