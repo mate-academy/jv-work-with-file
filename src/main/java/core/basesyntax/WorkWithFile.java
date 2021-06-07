2f75185f66d02b3b7e24397344fe32e42f733837
PashaTerletskyi <p.terletsky@gmail.com>
2021-06-07 13:51:11 +0300

    Changed dataSepararor to DATA_SEPARATOR StringBuilder argumentOne,argumentTwo changed to String constant ARGUMENT_ONE,ARGUMENT_TWO. Added Suggested change Changed fileIn,fileWriteIn to incomingFile. Changed valueToWrite to incomingValue. Changed fileOut to exitFile. Removed isValueContainsArgument() and changed to contains(). Changed if() - if() to if() - else. Use constant in getArgumentAmount(). Removed append flag to true.

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,62 +7,57 @@ import java.io.FileReader;
 import java.io.FileWriter;
 
 public class WorkWithFile {
-    public static final String dataSeparator = ",";
+    public static final String DATA_SEPARATOR = ",";
+    public static final String ARGUMENT_ONE = "supply";
+    public static final String ARGUMENT_TWO = "buy";
 
     public void getStatistic(String fromFileName, String toFileName) {
         StringBuilder stringBuilder = readFromFile(fromFileName);
-        writeInFile(toFileName,stringBuilder);
+        writeInFile(toFileName, stringBuilder.toString());
     }
 
     private StringBuilder readFromFile(String readFile) {
-        File fileIn = new File(readFile);
-        StringBuilder argumentOne = new StringBuilder("supply");
+        File incomingFile = new File(readFile);
         int argumentOneAmount = 0;
-        StringBuilder argumentTwo = new StringBuilder("buy");
         int argumentTwoAmount = 0;
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileIn))) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(incomingFile))) {
             String value = bufferedReader.readLine();
             while (value != null) {
-                if (isValueContainsArgument(value,argumentOne)) {
+                if (value.contains(ARGUMENT_ONE)) {
                     argumentOneAmount += getArgumentAmount(value);
-                }
-                if (isValueContainsArgument(value,argumentTwo)) {
+                } else {
                     argumentTwoAmount += getArgumentAmount(value);
                 }
                 value = bufferedReader.readLine();
             }
         } catch (Exception e) {
-            throw new RuntimeException("Can't read data from the file " + fileIn, e);
+            throw new RuntimeException("Can't read data from the file " + incomingFile, e);
         }
         return new StringBuilder()
-                .append(argumentOne)
-                .append(dataSeparator)
+                .append(ARGUMENT_ONE)
+                .append(DATA_SEPARATOR)
                 .append(argumentOneAmount)
                 .append(System.lineSeparator())
-                .append(argumentTwo)
-                .append(dataSeparator)
+                .append(ARGUMENT_TWO)
+                .append(DATA_SEPARATOR)
                 .append(argumentTwoAmount)
                 .append(System.lineSeparator())
                 .append("result")
-                .append(dataSeparator)
+                .append(DATA_SEPARATOR)
                 .append(argumentOneAmount - argumentTwoAmount);
     }
 
     private int getArgumentAmount(String value) {
-        return Integer.parseInt(value
-                .substring(value.indexOf(",") + 1,value.length()));
+        return Integer.parseInt(value.substring(value
+                .indexOf(DATA_SEPARATOR) + 1,value.length()));
     }
 
-    private boolean isValueContainsArgument(String value,StringBuilder argument) {
-        return value.substring(0,value.indexOf(dataSeparator)).equals(argument.toString());
-    }
-
-    private void writeInFile(String fileWriteIn, StringBuilder valueToWrite) {
-        File fileOut = new File(fileWriteIn);
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileOut,true))) {
-            bufferedWriter.write(valueToWrite.toString());
+    private void writeInFile(String incomingFile, String incomingValue) {
+        File exitFile = new File(incomingFile);
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(exitFile))) {
+            bufferedWriter.write(incomingValue.toString());
         } catch (Exception e) {
-            throw new RuntimeException("Can't write data to the file " + fileOut, e);
+            throw new RuntimeException("Can't write data to the file " + exitFile, e);
         }
     }
 }
