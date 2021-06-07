85c508c876a3387c5975d05185b434f1cd33f6af
Nikitozin <nikitozin91@gmail.com>
2021-06-07 14:14:05 +0300

    fixed_problems

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,38 +7,37 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    public static final String SEPARATOR = ",";
     public static final String SUPPLY = "supply";
     public static final String BUY = "buy";
     public static final String RESULT = "result";
-    public static final int INDEX_OF_VALUE = 1;
+    private static final String SEPARATOR = ",";
+    private static final int INDEX_OF_VALUE = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
         int supply = 0;
         int buy = 0;
         StringBuilder stringBuilder = new StringBuilder();
-        String line;
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
+            String line;
             while ((line = bufferedReader.readLine()) != null) {
                 if (line.contains(SUPPLY)) {
                     supply += Integer.parseInt(line.split(SEPARATOR)[INDEX_OF_VALUE]);
-                }
-                if (line.contains(BUY)) {
+                } else {
                     buy += Integer.parseInt(line.split(SEPARATOR)[INDEX_OF_VALUE]);
                 }
             }
-            stringBuilder.append(SUPPLY).append(SEPARATOR).append(supply)
-                    .append(System.lineSeparator())
-                    .append(BUY).append(SEPARATOR).append(buy)
-                    .append(System.lineSeparator())
-                    .append(RESULT).append(SEPARATOR).append(supply - buy);
         } catch (IOException e) {
             throw new RuntimeException("Can't read data from file", e);
         }
+        stringBuilder.append(SUPPLY).append(SEPARATOR).append(supply)
+                .append(System.lineSeparator())
+                .append(BUY).append(SEPARATOR).append(buy)
+                .append(System.lineSeparator())
+                .append(RESULT).append(SEPARATOR).append(supply - buy);
         writeToFile(toFileName, stringBuilder.toString());
     }
 
-    public void writeToFile(String toFileName, String data) {
+    private void writeToFile(String toFileName, String data) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
             bufferedWriter.write(data);
         } catch (IOException e) {
