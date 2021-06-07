0f1aef58b9803ba82c7a35b7a58d59d9292531ce
nikdagaev <nikdagaev@gmail.com>
2021-06-07 13:41:35 +0300

    readFromFile and writeToFile methods were changed. Tests passed

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -11,54 +11,56 @@ public class WorkWithFile {
 
     private static final String BUY = "buy";
     private static final String SUPPLY = "supply";
+    private static final String DELIMITER_COMMA = ",";
+    private static final String DELIMITER_SPACE = " ";
+    private static final String DELIMITER_EMPTY = "";
     private static final int OPERATION_INDEX = 0;
     private static final int AMOUNT_INDEX = 1;
 
-    private String readFromFile(String fromFileName) {
+    public void getStatistic(String fromFileName, String toFileName) {
+        String report = readFromFile(fromFileName);
+        writeToFile(toFileName, report);
+    }
 
+    private String readFromFile(String fromFileName) {
         StringBuilder stringBuilder = new StringBuilder();
         File fileToRead = new File(fromFileName);
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileToRead))) {
             String line = bufferedReader.readLine();
-            stringBuilder.append(line);
             while (line != null) {
-                line = bufferedReader.readLine();
                 stringBuilder.append(" ").append(line);
+                line = bufferedReader.readLine();
             }
         } catch (IOException e) {
-            throw new RuntimeException("Error while reading the file");
+            throw new RuntimeException("Error while reading the file", e);
         }
-        String[] resultingString = stringBuilder.toString().split(" ");
+        String[] resultingString = stringBuilder.toString().trim().split(DELIMITER_SPACE);
         int buySum = 0;
         int supplySum = 0;
         String[] stringSplit;
         for (String line : resultingString) {
-            stringSplit = line.split(",");
+            stringSplit = line.split(DELIMITER_COMMA);
             if (stringSplit[OPERATION_INDEX].equals(BUY)) {
                 buySum += Integer.parseInt(stringSplit[AMOUNT_INDEX]);
-            } else if (stringSplit[OPERATION_INDEX].equals(SUPPLY)) {
+            } else {
                 supplySum += Integer.parseInt(stringSplit[AMOUNT_INDEX]);
             }
         }
         int resultSum = supplySum - buySum;
-        return "supply," + supplySum + System.lineSeparator()
-                + "buy," + buySum + System.lineSeparator()
-                + "result, " + resultSum;
+        return new StringBuilder().append("supply,").append(supplySum)
+                .append(System.lineSeparator()).append("buy,")
+                .append(buySum).append(System.lineSeparator())
+                .append("result,").append(resultSum).toString();
     }
 
     private void writeToFile(String toFileName, String report) {
         File fileToWrite = new File(toFileName);
 
         try (BufferedWriter bufferedWriter =
-                     new BufferedWriter(new FileWriter(fileToWrite, true))) {
-            bufferedWriter.write(report.replaceAll(" ",""));
+                     new BufferedWriter(new FileWriter(fileToWrite))) {
+            bufferedWriter.write(report);
         } catch (IOException e) {
             throw new RuntimeException("Error while writing into the file", e);
         }
-    }
-
-    public void getStatistic(String fromFileName, String toFileName) {
-        String report = readFromFile(fromFileName);
-        writeToFile(toFileName, report);
     }
 }
