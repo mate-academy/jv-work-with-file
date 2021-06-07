449ceedbf0a92bd43c7bd1184a8c21c6c2b728f4
Yura Lug <LUV97@ukr.net>
2021-06-07 12:40:00 +0300

    Create program, which read data and write result. Logic was built in two methods

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,8 +1,43 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WorkWithFile {
+    private static final int INDEX_OF_NUMERIC = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+            bufferedWriter.write(readFromFile(fromFileName));
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write data to the file ", e);
+        }
+    }
 
+    private String readFromFile(String fileName) {
+        int supply = 0;
+        int buy = 0;
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            String reader;
+            while ((reader = bufferedReader.readLine()) != null) {
+                if (reader.contains("supply")) {
+                    String [] inheritArray = reader.split(",");
+                    supply += Integer.parseInt(inheritArray[INDEX_OF_NUMERIC]);
+                }
+                if (reader.contains("buy")) {
+                    String [] inheritArray = reader.split(",");
+                    buy += Integer.parseInt(inheritArray[INDEX_OF_NUMERIC]);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't correctly read data from file ", e);
+        }
+        return new StringBuilder().append("supply,").append(supply).append(System.lineSeparator())
+                .append("buy,").append(buy).append(System.lineSeparator())
+                .append("result,").append(supply - buy).append(System.lineSeparator()).toString();
     }
 }
