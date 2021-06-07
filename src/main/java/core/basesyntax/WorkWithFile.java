ee5371f3d7706d569d91753eb4e0358c4da0f408
EugeneSinica <eugenesinica@gmail.com>
2021-06-07 13:42:17 +0300

    some changes following Kseniia's recomendations

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,53 +1,45 @@
 package core.basesyntax;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class WorkWithFile {
-
-    private int supplySum = 0;
-    private int buySum = 0;
+    private static final int AMOUNT_INDEX = 1;
+    private static final String COMMA = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        readFromFile(fromFileName);
-        try {
-            Files.createFile(Path.of(toFileName));
-        } catch (IOException e) {
-            throw new RuntimeException("Some troubles with creating a file " + toFileName + e);
-        }
-        writeToFile(toFileName);
+        writeToFile(toFileName, readFromFile(fromFileName));
     }
 
-    private void readFromFile(String fromFileName) {
+    private String readFromFile(String fromFileName) {
+        int supplySum = 0;
+        int buySum = 0;
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
-            int amountIndex = 1;
-            String line;
+            String line = null;
+            String[] splittedLine = line.split(COMMA);
             while ((line = bufferedReader.readLine()) != null) {
                 if (line.contains("supply")) {
-                    supplySum += Integer.parseInt(line.split(",")[amountIndex]);
+                    supplySum += Integer.parseInt(splittedLine[AMOUNT_INDEX]);
                 } else {
-                    buySum += Integer.parseInt(line.split(",")[amountIndex]);
+                    buySum += Integer.parseInt(splittedLine[AMOUNT_INDEX]);
                 }
             }
         } catch (IOException e) {
             throw new RuntimeException("Can't read from file " + fromFileName + e);
         }
+        return makeReport(buySum, supplySum);
     }
 
-    private String makeReport() {
+    private String makeReport(int buySum, int supplySum ) {
         return "supply" + "," + supplySum + System.lineSeparator()
                 + "buy" + "," + buySum + System.lineSeparator()
                 + "result" + "," + (supplySum - buySum) + System.lineSeparator();
     }
 
-    private void writeToFile(String toFileName) {
+    private void writeToFile(String toFileName, String content) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
-            bufferedWriter.write(makeReport());
+            bufferedWriter.write(content);
         } catch (IOException e) {
             throw new RuntimeException("Can't write to file " + toFileName + e);
         }
