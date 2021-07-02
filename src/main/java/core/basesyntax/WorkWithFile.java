df4d8189c15b9e9b7e59f61d566e88113aebfdb3
Alex Pravda <kabinkos@gmail.com>
2021-07-02 10:34:49 +0300

    - code refactor, 1 method instead of 2.

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -9,11 +9,41 @@ import java.io.IOException;
 
 public class WorkWithFile {
 
-    public void getStatistic(String fromFileName, String toFileName) {
+    private final int[] countOccurrences = new int[2];
 
-        String fullReport = "supply," + getSupply(fromFileName) + System.lineSeparator()
-                + "buy," + getBuy(fromFileName) + System.lineSeparator()
-                + "result," + (getSupply(fromFileName) - getBuy(fromFileName));
+    private void getOccurrences(String fromFile) {
+        File inputFrom = new File(fromFile);
+        final String supplyLine = "supply";
+        final String buyLine = "buy";
+        int countSupply = 0;
+        int countBuy = 0;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFrom))) {
+            String lineFromFile = reader.readLine();
+            while (lineFromFile != null) {
+                if (lineFromFile.startsWith(supplyLine)) {
+                    countSupply += Integer.parseInt(
+                            lineFromFile.substring(lineFromFile.indexOf(',') + 1));
+                }
+                if (lineFromFile.startsWith(buyLine)) {
+                    countBuy += Integer.parseInt(
+                            lineFromFile.substring(lineFromFile.indexOf(',') + 1));
+                }
+                lineFromFile = reader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read the file", e);
+        }
+        countOccurrences[0] = countSupply;
+        countOccurrences[1] = countBuy;
+    }
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        getOccurrences(fromFileName);
+
+        String fullReport = "supply," + countOccurrences[0] + System.lineSeparator()
+                + "buy," + countOccurrences[1] + System.lineSeparator()
+                + "result," + (countOccurrences[0] - countOccurrences[1]);
 
         File writeResult = new File(toFileName);
 
@@ -28,44 +58,5 @@ public class WorkWithFile {
         } catch (IOException e) {
             throw new RuntimeException("Can't write data to file", e);
         }
-    }
-
-    private int getSupply(String fromFile) {
-        File inputFrom = new File(fromFile);
-        final String supplyLine = "supply";
-        int supply = 0;
-
-        try (BufferedReader reader = new BufferedReader(new FileReader(inputFrom))) {
-            String lineFromFile = reader.readLine();
-            while (lineFromFile != null) {
-                if (lineFromFile.startsWith(supplyLine)) {
-                    supply += Integer.parseInt(
-                            lineFromFile.substring(lineFromFile.indexOf(',') + 1));
-                }
-                lineFromFile = reader.readLine();
-            }
-        } catch (IOException e) {
-            throw new RuntimeException("Can't read the file", e);
-        }
-        return supply;
-    }
-
-    private int getBuy(String fromFile) {
-        File inputFrom = new File(fromFile);
-        final String buyLine = "buy";
-        int buy = 0;
-
-        try (BufferedReader reader = new BufferedReader(new FileReader(inputFrom))) {
-            String lineFromFile = reader.readLine();
-            while (lineFromFile != null) {
-                if (lineFromFile.startsWith(buyLine)) {
-                    buy += Integer.parseInt(lineFromFile.substring(lineFromFile.indexOf(',') + 1));
-                }
-                lineFromFile = reader.readLine();
-            }
-        } catch (IOException e) {
-            throw new RuntimeException("Can't read the file", e);
-        }
-        return buy;
     }
 }
