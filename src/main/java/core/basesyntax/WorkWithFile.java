8bd8dcfb1e5c23bfd9694104ac73d03e7f664e53
Lena Kravchenko <cupp.lenashapoval@gmail.com>
2021-07-20 22:26:23 +0300

    Fixes after review

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,23 +7,23 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-
     public void getStatistic(String fromFileName, String toFileName) {
-        StringBuilder stringBuilder = new StringBuilder();
-        String supply = "supply";
-        String buy = "buy";
-        String result = "result";
+        int[] amounts = getDataFromFile(fromFileName);
+        String[] resultData = buildData(amounts[0], amounts[1], amounts[2]);
+        writeData(resultData, toFileName);
+    }
+
+    private int[] getDataFromFile(String fileName) {
         int supplyAmount = 0;
         int buyAmount = 0;
 
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             String line = reader.readLine();
 
             while (line != null) {
-                if (line.split(",")[0].equals(supply)) {
+                if (line.split(",")[0].equals("supply")) {
                     supplyAmount += Integer.parseInt(line.split(",")[1]);
-                } else if (line.split(",")[0].equals(buy)) {
+                } else if (line.split(",")[0].equals("buy")) {
                     buyAmount += Integer.parseInt(line.split(",")[1]);
                 }
 
@@ -33,31 +33,33 @@ public class WorkWithFile {
             throw new RuntimeException("Can't read data.", e);
         }
 
-        int resultAmount = supplyAmount - buyAmount;
+        int[] arrayWithAmounts = new int[3];
+        arrayWithAmounts[0] = supplyAmount;
+        arrayWithAmounts[1] = buyAmount;
+        arrayWithAmounts[2] = supplyAmount - buyAmount;
 
-        stringBuilder.append(supply).append(",").append(supplyAmount).append(System.lineSeparator())
-                .append(buy).append(",").append(buyAmount).append(System.lineSeparator())
-                .append(result).append(",").append(resultAmount);
+        return arrayWithAmounts;
+    }
 
-        String[] resultData = stringBuilder.toString().split(System.lineSeparator());
+    private String[] buildData(int amount1, int amount2, int amount3) {
+        StringBuilder stringBuilder = new StringBuilder();
 
-        BufferedWriter bufferedWriter = null;
+        stringBuilder.append("supply").append(",").append(amount1).append(System.lineSeparator())
+                .append("buy").append(",").append(amount2).append(System.lineSeparator())
+                .append("result").append(",").append(amount3);
 
-        for (String dataRow : resultData) {
-            try {
-                bufferedWriter = new BufferedWriter(new FileWriter(toFileName, true));
+        return stringBuilder.toString().split(System.lineSeparator());
+    }
+
+    private void writeData(String[] data, String fileName) {
+        for (String dataRow : data) {
+            try (BufferedWriter bufferedWriter =
+                         new BufferedWriter(new FileWriter(fileName, true))) {
                 bufferedWriter.write(dataRow);
                 bufferedWriter.write(System.lineSeparator());
             } catch (IOException e) {
                 throw new RuntimeException("Can't write data.", e);
-            } finally {
-                if (bufferedWriter != null) {
-                    try {
-                        bufferedWriter.close();
-                    } catch (IOException e) {
-                        throw new RuntimeException("Can't close BufferedWriter.", e);
-                    }
-                }
+
             }
         }
     }
