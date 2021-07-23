37a66fc6b7fbb7c594f19d2f4749a9461204fac0
Lena Kravchenko <cupp.lenashapoval@gmail.com>
2021-07-23 10:00:15 +0300

    Fixes after review-2

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,12 +8,11 @@ import java.io.IOException;
 
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
-        int[] amounts = getDataFromFile(fromFileName);
-        String[] resultData = buildData(amounts[0], amounts[1], amounts[2]);
+        String resultData = getDataFromFile(fromFileName);
         writeData(resultData, toFileName);
     }
 
-    private int[] getDataFromFile(String fileName) {
+    private String getDataFromFile(String fileName) {
         int supplyAmount = 0;
         int buyAmount = 0;
 
@@ -33,26 +32,19 @@ public class WorkWithFile {
             throw new RuntimeException("Can't read data.", e);
         }
 
-        int[] arrayWithAmounts = new int[3];
-        arrayWithAmounts[0] = supplyAmount;
-        arrayWithAmounts[1] = buyAmount;
-        arrayWithAmounts[2] = supplyAmount - buyAmount;
-
-        return arrayWithAmounts;
-    }
-
-    private String[] buildData(int amount1, int amount2, int amount3) {
         StringBuilder stringBuilder = new StringBuilder();
 
-        stringBuilder.append("supply").append(",").append(amount1).append(System.lineSeparator())
-                .append("buy").append(",").append(amount2).append(System.lineSeparator())
-                .append("result").append(",").append(amount3);
+        stringBuilder.append("supply").append(",").append(supplyAmount)
+                .append(System.lineSeparator())
+                .append("buy").append(",").append(buyAmount)
+                .append(System.lineSeparator())
+                .append("result").append(",").append(supplyAmount - buyAmount);
 
-        return stringBuilder.toString().split(System.lineSeparator());
+        return stringBuilder.toString();
     }
 
-    private void writeData(String[] data, String fileName) {
-        for (String dataRow : data) {
+    private void writeData(String data, String fileName) {
+        for (String dataRow : data.split(System.lineSeparator())) {
             try (BufferedWriter bufferedWriter =
                          new BufferedWriter(new FileWriter(fileName, true))) {
                 bufferedWriter.write(dataRow);
