663a1f522a7e8baad3937a0dddb10a2cd76d95d3
vadim9482 <vadim9482@gmail.com>
2021-08-14 20:01:49 +0300

    Separate methods for read and write files were implemented, try with resourses was used

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -10,21 +10,11 @@ import java.io.IOException;
 public class WorkWithFile {
 
     public void getStatistic(String fromFileName, String toFileName) {
-        StringBuilder stringBuilder = new StringBuilder();
         File from = new File(fromFileName);
         File to = new File(toFileName);
-        try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(from));
-            String line = bufferedReader.readLine();
-            System.out.println(line);
-            while (line != null) {
-                stringBuilder.append(line).append(System.lineSeparator());
-                line = bufferedReader.readLine();
-            }
-        } catch (IOException e) {
-            throw new RuntimeException("Cannot read file " + fromFileName);
-        }
-        String[] rows = stringBuilder.toString().split(System.lineSeparator());
+        String fileData = fileReader(from);
+
+        String[] rows = fileData.split(System.lineSeparator());
         int supplyValue = 0;
         int buyValue = 0;
         for (String row : rows) {
@@ -40,15 +30,31 @@ public class WorkWithFile {
         String report = "supply," + supplyValue + System.lineSeparator()
                 + "buy," + buyValue + System.lineSeparator()
                 + "result," + (supplyValue - buyValue);
-        try {
-            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(to));
-            for (String line : report.split(System.lineSeparator())) {
+        fileWriter(to, report);
+    }
+
+    private String fileReader(File file) {
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+            String line = bufferedReader.readLine();
+            while (line != null) {
+                stringBuilder.append(line).append(System.lineSeparator());
+                line = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Cannot read file " + file.getName());
+        }
+        return stringBuilder.toString();
+    }
+
+    private void fileWriter(File file, String input) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
+            for (String line : input.split(System.lineSeparator())) {
                 bufferedWriter.write(line + System.lineSeparator());
                 bufferedWriter.flush();
             }
-            bufferedWriter.close();
         } catch (IOException e) {
-            throw new RuntimeException("Cannot write to file " + to);
+            throw new RuntimeException("Cannot write to file " + file);
         }
     }
 }
