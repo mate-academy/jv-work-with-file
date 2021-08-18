6c588151a3d95f38f8cc9452137f9ab838768071
Roman Novikov <novikovr121281@gmail.com>
2021-08-18 12:01:54 +0300

    Replaced public and private methods, changed arguments in method write>getResult>String and replaced regex with ","

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,6 +8,15 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    public void getStatistic(String fromFileName, String toFileName) {
+        String dataFromFile = getData(fromFileName);
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+            bufferedWriter.write(getResult(dataFromFile));
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write file " + toFileName + e);
+        }
+    }
+
     private String getData(String file) {
         StringBuilder stringBuilder = new StringBuilder();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
@@ -27,14 +36,16 @@ public class WorkWithFile {
         int buyProduct = 0;
         int supplyProduct = 0;
         int result = 0;
-
-        String[] strings = data.split(" ");
+        // после data.split(","), цифры от buy находятся с элементом supply
+        // в одной строке(в следующем элементе), что бы извлечь число
+        // использовал regex: "[^0-9]"
+        String[] strings = data.split(",");
         for (int i = 0; i < strings.length; i++) {
-            if (strings[i].substring(0, strings[i].indexOf(",")).equals("buy")) {
-                buyProduct += Integer.parseInt(strings[i].substring((strings[i].indexOf(",")) + 1));
-            } else if (strings[i].substring(0, strings[i].indexOf(",")).equals("supply")) {
+            if (strings[i].contains("buy")) {
+                buyProduct += Integer.parseInt(strings[i + 1].replaceAll("[^0-9]", ""));
+            } else if (strings[i].contains("supply")) {
                 supplyProduct += Integer.parseInt(
-                        strings[i].substring((strings[i].indexOf(",")) + 1));
+                        strings[i + 1].replaceAll("[^0-9]", ""));
             }
         }
         result = supplyProduct - buyProduct;
@@ -42,13 +53,5 @@ public class WorkWithFile {
         return "supply," + supplyProduct + System.lineSeparator()
                 + "buy," + buyProduct + System.lineSeparator()
                 + "result," + result;
-    }
-
-    public void getStatistic(String fromFileName, String toFileName) {
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
-            bufferedWriter.write(getResult(getData(fromFileName)));
-        } catch (IOException e) {
-            throw new RuntimeException("Can't write file " + toFileName + e);
-        }
     }
 }
