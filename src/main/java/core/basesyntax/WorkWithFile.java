1c45d75a86eb73a8f78aee680cc5863b064a8cd6
Oleksandr Rozvazhaiev <alexrozv@gmail.com>
2021-08-18 15:48:43 +0300

    changed public methods to private, created separate method to calculate data for report, made "," a constant, exception messages are more informative now,

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -10,6 +10,9 @@ import java.io.IOException;
 public class WorkWithFile {
     private static final int INDEX_OF_WORD = 0;
     private static final int INDEX_OF_AMOUNT = 1;
+    private static final int INDEX_OF_SUPPLY_AMOUNT = 0;
+    private static final int INDEX_OF_BUY_AMOUNT = 1;
+    private static final String SYMBOL_TO_SPLIT_DATA = ",";
     private static final String STRING_TO_COMPARE = "supply";
 
     public void getStatistic(String fromFileName, String toFileName) {
@@ -18,7 +21,7 @@ public class WorkWithFile {
         writeToFile(toFileName, report);
     }
 
-    public String[] readFromFile(String fileName) {
+    private String[] readFromFile(String fileName) {
         StringBuilder stringBuilder = new StringBuilder();
         try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
@@ -28,17 +31,16 @@ public class WorkWithFile {
                 value = bufferedReader.readLine();
             }
         } catch (IOException e) {
-            throw new RuntimeException("can't read file", e);
+            throw new RuntimeException("can't read file " + fileName, e);
         }
         return stringBuilder.toString().split(" ");
     }
 
-    public String createReport(String[] data) {
-        StringBuilder stringBuilder = new StringBuilder();
+    private int[] calculateForReport(String[] data) {
         int supplyAmount = 0;
         int buyAmount = 0;
         for (String datum : data) {
-            String[] datumArr = datum.split(",");
+            String[] datumArr = datum.split(SYMBOL_TO_SPLIT_DATA);
             int amount = Integer.parseInt(datumArr[INDEX_OF_AMOUNT]);
             if (datumArr[INDEX_OF_WORD].equals(STRING_TO_COMPARE)) {
                 supplyAmount += amount;
@@ -46,17 +48,25 @@ public class WorkWithFile {
                 buyAmount += amount;
             }
         }
-        return stringBuilder.append("supply,").append(supplyAmount).append(System.lineSeparator())
+        return new int[] {supplyAmount, buyAmount};
+    }
+
+    private String createReport(String[] data) {
+        int[] amounts = calculateForReport(data);
+        int supplyAmount = amounts[INDEX_OF_SUPPLY_AMOUNT];
+        int buyAmount = amounts[INDEX_OF_BUY_AMOUNT];
+        return new StringBuilder().append("supply,").append(supplyAmount)
+                .append(System.lineSeparator())
                 .append("buy,").append(buyAmount).append(System.lineSeparator())
                 .append("result,").append(supplyAmount - buyAmount).toString();
     }
 
-    public void writeToFile(String fileName, String report) {
+    private void writeToFile(String fileName, String report) {
         File file = new File(fileName);
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file, true))) {
             bufferedWriter.write(report);
         } catch (IOException e) {
-            throw new RuntimeException("cant write to file", e);
+            throw new RuntimeException("cant write to file " + fileName, e);
         }
     }
 }
