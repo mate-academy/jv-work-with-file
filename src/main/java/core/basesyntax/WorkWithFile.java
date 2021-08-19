72bcb045ae1e951f57c98c9c623b40a866f6dfee
Sinelife <ya.serzhan@gmail.com>
2021-08-19 20:21:48 +0300

    add getStatistics method for reading from file and writing to other file

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,8 +1,47 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WorkWithFile {
 
     public void getStatistic(String fromFileName, String toFileName) {
+        String statisticsResult = getStatisticsFromFile(fromFileName);
+        writeTextToFile(toFileName, statisticsResult);
+    }
 
+    private String getStatisticsFromFile(String fromFileName) {
+        int buy = 0;
+        int supply = 0;
+        try (BufferedReader br = new BufferedReader(new FileReader(fromFileName))) {
+            for (String line; (line = br.readLine()) != null; ) {
+                String[] parts = line.split(",");
+                if (parts[0].equals("buy")) {
+                    buy += Integer.parseInt(parts[1]);
+                } else {
+                    supply += Integer.parseInt(parts[1]);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return getResult(buy, supply);
+    }
+
+    private void writeTextToFile(String toFileName, String text) {
+        try (FileWriter writer = new FileWriter(toFileName, false)) {
+            writer.write(text);
+            writer.flush();
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    private String getResult(int buy, int supply) {
+        return "supply," + supply + System.lineSeparator()
+                + "buy," + buy + System.lineSeparator()
+                + "result," + (supply - buy);
     }
 }
