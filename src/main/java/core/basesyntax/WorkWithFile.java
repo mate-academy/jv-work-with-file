a1273ce39945a82395fb3d1803e2464b56e5558b
Yehor-Zapara <87674325+Yehor-Zapara@users.noreply.github.com>
2021-08-20 17:24:33 +0300

    Update WorkWithFile.java

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,6 +2,7 @@ package core.basesyntax;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
