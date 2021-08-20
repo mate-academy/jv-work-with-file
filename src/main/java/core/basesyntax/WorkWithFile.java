435e57840dad89871a2d2c0b372272958a9f2223
Yehor-Zapara <87674325+Yehor-Zapara@users.noreply.github.com>
2021-08-20 17:27:13 +0300

    Update WorkWithFile.java

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,10 +2,10 @@ package core.basesyntax;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 
 public class WorkWithFile {
     public static final String KEY_WORD_FOR_SUPPLY = "supply";
