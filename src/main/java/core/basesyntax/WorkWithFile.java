bb0862bd641e7b2e1d7b7d6c6a294c583cbd3c53
Yehor Zapara <egor.zapara1994@gmail.com>
2021-08-20 17:01:42 +0300

    class WorkWithFile was fixed

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,8 +1,53 @@
 package core.basesyntax;
 
+import java.io.*;
+
 public class WorkWithFile {
+    public static final String KEY_WORD_FOR_SUPPLY = "supply";
+    public static final String KEY_WORD_FOR_BUY = "buy";
+    public static final char CSV_SEPARATOR = ',';
 
     public void getStatistic(String fromFileName, String toFileName) {
+        StringBuilder fromFileStringBuilder = readFromFile(fromFileName);
+        String[] fromFileStrings = fromFileStringBuilder.toString().split(System.lineSeparator());
+        int supplySum = 0;
+        int buySum = 0;
+        for (String string : fromFileStrings) {
+            int amount = Integer.parseInt(string.substring(string.indexOf(CSV_SEPARATOR) + 1));
+            supplySum += string.startsWith(KEY_WORD_FOR_SUPPLY) ? amount : 0;
+            buySum += string.startsWith(KEY_WORD_FOR_BUY) ? amount : 0;
+        }
+        int result = supplySum - buySum;
+        StringBuilder toFileStringBuilder = new StringBuilder();
+        toFileStringBuilder.append(KEY_WORD_FOR_SUPPLY).append(CSV_SEPARATOR).append(supplySum)
+                .append(System.lineSeparator()).append(KEY_WORD_FOR_BUY).append(CSV_SEPARATOR).append(buySum)
+                .append(System.lineSeparator()).append("result").append(CSV_SEPARATOR).append(result);
+        writeToFile(toFileName, toFileStringBuilder.toString());
+    }
 
+    public StringBuilder readFromFile(String fromFileName) {
+        File fromFile = new File(fromFileName);
+        StringBuilder stringBuilderFrom = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFile))) {
+            String valueFrom = bufferedReader.readLine();
+            while (valueFrom != null) {
+                stringBuilderFrom.append(valueFrom).append(System.lineSeparator());
+                valueFrom = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read from file " + fromFileName, e);
+        }
+        return stringBuilderFrom;
+    }
+
+    public void writeToFile(String toFileName, String dataToWrite) {
+        File toFile = new File(toFileName);
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFile))) {
+            toFile.createNewFile();
+            bufferedWriter.write(dataToWrite);
+            System.out.println(dataToWrite);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write to file" + toFileName, e);
+        }
     }
 }
