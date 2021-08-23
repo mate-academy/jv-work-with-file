f96aaa51072912613f018c6e65732fe2fcc68c34
Serhii Orlov <orlov30@gmail.com>
2021-08-23 15:51:04 +0300

    working program, handsome, fix checkstyle problems.

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,54 +1,58 @@
 package core.basesyntax;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class WorkWithFile {
 
     public void getStatistic(String fromFileName, String toFileName) {
-        String[] record;
-        String recordString = "";
         int supply = 0;
         int buy = 0;
         File inputData = new File(fromFileName);
-        FileReader fileReader = null;
+        FileReader fileReader;
         try {
             fileReader = new FileReader(inputData);
         } catch (FileNotFoundException e) {
             throw new RuntimeException("Problem", e);
         }
-        BufferedReader bufferedReader = new BufferedReader(fileReader);
-        try {
-            recordString = bufferedReader.readLine();
-        } catch (IOException e) {
-            throw new RuntimeException("Problem", e);
-        }
-        while (recordString != null) {
-            record = recordString.split(",");
-            if (record[0].equals("supply")) {
-                supply += Integer.parseInt(record[1]);
-            } else {
-                buy += Integer.parseInt(record[1]);
-            }
-            try {
-                recordString = bufferedReader.readLine();
-            } catch (IOException e) {
-                throw new RuntimeException("Problem", e);
-            }
-        }
-        try {
-            bufferedReader.close();
-        } catch (IOException e) {
-            throw new RuntimeException("Problem", e);
-        }
+        int[] out = createReport(fileReader, supply, buy);
         File outputData = new File(toFileName);
-        FileWriter fileWriter = null;
+        FileWriter fileWriter;
         try {
             fileWriter = new FileWriter(outputData);
         } catch (IOException e) {
             throw new RuntimeException("Problem", e);
         }
-        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-        try {
+        writeToFile(fileWriter, out[0], out[1]);
+    }
+
+    private int[] createReport(FileReader fileReader, int supply, int buy) {
+        String[] record;
+        String recordString = "";
+        try (BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+            recordString = bufferedReader.readLine();
+            while (recordString != null) {
+                record = recordString.split(",");
+                if (record[0].equals("supply")) {
+                    supply += Integer.parseInt(record[1]);
+                } else {
+                    buy += Integer.parseInt(record[1]);
+                }
+                recordString = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Problem", e);
+        }
+        return new int[] {supply, buy};
+    }
+
+    private void writeToFile(FileWriter fileWriter, int supply, int buy) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(fileWriter)) {
             bufferedWriter.write(new StringBuilder("supply,")
                     .append(String.valueOf(supply))
                     .append(System.lineSeparator())
@@ -60,12 +64,6 @@ public class WorkWithFile {
                     .toString());
         } catch (IOException e) {
             throw new RuntimeException("Problem", e);
-        } finally {
-            try {
-                bufferedWriter.close();
-            } catch (IOException e) {
-                throw new RuntimeException("Problem", e);
-            }
         }
     }
 }
