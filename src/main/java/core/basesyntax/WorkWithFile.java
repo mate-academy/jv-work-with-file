bb64f9f76488ce99ef4455e78fd7e9d576267b93
Maksym Mazlov <mazlovmaxim@gmail.com>
2021-08-23 16:20:32 +0300

    created method which read csv file and write a report to new File

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,8 +1,43 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WorkWithFile {
-
     public void getStatistic(String fromFileName, String toFileName) {
+        int allSupplyForDay = 0;
+        int allBuyForDay = 0;
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
+            String readLine = reader.readLine();
+            while (readLine != null) {
+                String[] splitLine = readLine.split(",");
+                String type = splitLine[0];
+                int count = Integer.parseInt(splitLine[1]);
+                if (type.equals("supply")) {
+                    allSupplyForDay += count;
+                }
+                if (type.equals("buy")) {
+                    allBuyForDay += count;
+                }
 
+                readLine = reader.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("Can`t found file - " + fromFileName, e);
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t read file - " + fromFileName, e);
+        }
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
+            writer.write("supply," + allSupplyForDay + System.lineSeparator());
+            writer.write("buy," + allBuyForDay + System.lineSeparator());
+            writer.write("result," + (allSupplyForDay - allBuyForDay));
+        } catch (IOException e) {
+            throw new RuntimeException("");
+        }
     }
 }
