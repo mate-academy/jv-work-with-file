e38d2e7a11a2eb73df189a18e91bc28d19633959
Maksym Mazlov <mazlovmaxim@gmail.com>
2021-08-23 19:39:43 +0300

    refactor after review: added two methods readFromFile() and writeToFile()

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,42 +2,54 @@ package core.basesyntax;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+
     public void getStatistic(String fromFileName, String toFileName) {
+        String content = readFromFile(fromFileName);
+        writeToFile(content, toFileName);
+    }
+
+    private String readFromFile(String path) {
+        StringBuilder builder = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(path))) {
+            String s = bufferedReader.readLine();
+            while (s != null) {
+                builder.append(s).append(";");
+                s = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t read file " + path, e);
+        }
+        return builder.toString();
+    }
+
+    private void writeToFile(String content, String toFileName) {
         int allSupplyForDay = 0;
         int allBuyForDay = 0;
-        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
-            String readLine = reader.readLine();
-            while (readLine != null) {
-                String[] splitLine = readLine.split(",");
-                String type = splitLine[0];
-                int count = Integer.parseInt(splitLine[1]);
-                if (type.equals("supply")) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
+            String[] split = content.split(";");
+            for (String line : split) {
+                String[] split1 = line.split(",");
+                int count = Integer.parseInt(split1[1]);
+
+                if (split1[0].equals("supply")) {
                     allSupplyForDay += count;
                 }
-                if (type.equals("buy")) {
+
+                if (split1[0].equals("buy")) {
                     allBuyForDay += count;
                 }
-
-                readLine = reader.readLine();
             }
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException("Can`t found file - " + fromFileName, e);
-        } catch (IOException e) {
-            throw new RuntimeException("Can`t read file - " + fromFileName, e);
-        }
-
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
             writer.write("supply," + allSupplyForDay + System.lineSeparator());
             writer.write("buy," + allBuyForDay + System.lineSeparator());
             writer.write("result," + (allSupplyForDay - allBuyForDay));
         } catch (IOException e) {
-            throw new RuntimeException("");
+            throw new RuntimeException("Can`t write data to file " + toFileName, e);
         }
     }
 }
+
