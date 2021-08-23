ff0a6f602965f072ac296186608ee1a18c2d3d83
SonyaPakhomova <88778908+SonyaPakhomova@users.noreply.github.com>
2021-08-23 20:55:23 +0300

    realise getStatistic, readFile and writeFile methods

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,8 +1,54 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WorkWithFile {
+    private static final int OPERATION_TYPE = 0;
+    private static final int AMOUNT = 1;
+    private static final String SUPPLY = "supply";
+    private static final String COMMA = ",";
+    private static final String BUY = "buy";
 
     public void getStatistic(String fromFileName, String toFileName) {
+        String result = readFile(fromFileName);
+        writeFile(result, toFileName);
+    }
 
+    public String readFile(String fromFileName) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
+            String line = reader.readLine();
+            String[] lineData;
+            StringBuilder builder = new StringBuilder();
+            int buy = 0;
+            int supply = 0;
+            while (line != null) {
+                lineData = line.split(COMMA);
+                if (lineData[OPERATION_TYPE].equals(SUPPLY)) {
+                    supply += Integer.parseInt(lineData[AMOUNT]);
+                }
+                if (lineData[OPERATION_TYPE].equals(BUY)) {
+                    buy += Integer.parseInt(lineData[AMOUNT]);
+                }
+                line = reader.readLine();
+            }
+            builder.append(SUPPLY).append(COMMA).append(supply).append(System.lineSeparator())
+                    .append(BUY).append(COMMA).append(buy).append(System.lineSeparator())
+                    .append("result").append(COMMA).append(supply - buy);
+            return builder.toString();
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
+    }
+
+    private void writeFile(String toWrite, String toFileName) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName, true))) {
+            writer.write(toWrite);
+        } catch (Exception e) {
+            throw new RuntimeException("Can't write to file", e);
+        }
     }
 }
