88a8faca868dee74daf42467fe2199d3dbb15308
Evgen Krasnenkov <krasnenkov.evgen@gmail.com>
2021-08-25 01:42:53 +0300

    constants, methods namings, informative names, local vars, try-with-resourse for Autocloseable, no nested connections, exceptions, modifies CHECKED

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,40 +8,46 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    public static final int FIRST_PART = 0;
-    public static final int SECOND_PART = 1;
+    private static final int FIRST_WORD_OF_LINE = 0;
+    private static final int SECOND_WORD_OF_LINE = 1;
+    private static final String FIRST_UNIQUE_CATEGORY = "supply";
+    private static final String SECOND_UNIQUE_CATEGORY = "buy";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        String[] arrayNames = new String[] {"supply", "buy"};
-        int[] arrayOfSums = new int[arrayNames.length];
-        File file = new File(fromFileName);
-        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-            String readValue = reader.readLine();
-            String[] readArray = readValue.split(",");
-            while (readValue != null) {
-                readArray = readValue.split(",");
-                for (int i = 0; i < arrayNames.length; i++) {
-                    if (arrayNames[i].equals(readArray[FIRST_PART])) {
-                        arrayOfSums[i] += Integer.parseInt(readArray[SECOND_PART]);
+
+        String[] arrayOfCategories = new String[] {FIRST_UNIQUE_CATEGORY, SECOND_UNIQUE_CATEGORY};
+        int[] arrayOfSumsOfCategories = new int[arrayOfCategories.length];
+        File fileToRead = new File(fromFileName);
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileToRead))) {
+            String readLineOfFile = reader.readLine();
+            String[] readLineSplitTwoParts;
+            while (readLineOfFile != null) {
+                readLineSplitTwoParts = readLineOfFile.split(",");
+                for (int i = 0; i < arrayOfCategories.length; i++) {
+                    if (arrayOfCategories[i].equals(readLineSplitTwoParts[FIRST_WORD_OF_LINE])) {
+                        arrayOfSumsOfCategories[i]
+                                += Integer.parseInt(readLineSplitTwoParts[SECOND_WORD_OF_LINE]);
                     }
                 }
-                readValue = reader.readLine();
+                readLineOfFile = reader.readLine();
             }
         } catch (IOException exc) {
-            throw new RuntimeException("Problems with file reading", exc);
+            throw new RuntimeException("Problems with file reading" + fileToRead, exc);
         }
-        String toWriteStr = "";
-        File fileWrite = new File(toFileName);
-        toWriteStr = new String(arrayNames[FIRST_PART] + ","
-                + arrayOfSums[FIRST_PART] + System.lineSeparator()
-                + arrayNames[SECOND_PART] + "," + arrayOfSums[SECOND_PART] + System.lineSeparator()
-                + "result," + (arrayOfSums[FIRST_PART] - arrayOfSums[SECOND_PART]));
+        String toWriteString = new String(arrayOfCategories[FIRST_WORD_OF_LINE] + ","
+                + arrayOfSumsOfCategories[FIRST_WORD_OF_LINE] + System.lineSeparator()
+                + arrayOfCategories[SECOND_WORD_OF_LINE] + ","
+                + arrayOfSumsOfCategories[SECOND_WORD_OF_LINE] + System.lineSeparator()
+                + "result," + (arrayOfSumsOfCategories[FIRST_WORD_OF_LINE]
+                - arrayOfSumsOfCategories[SECOND_WORD_OF_LINE]));
+
+        File fileToWrite = new File(toFileName);
         try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter(fileWrite, true));
-            writer.write(toWriteStr);
+            BufferedWriter writer = new BufferedWriter(new FileWriter(fileToWrite, true));
+            writer.write(toWriteString);
             writer.close();
         } catch (IOException exc) {
-            throw new RuntimeException("Can't write file", exc);
+            throw new RuntimeException("Can't write to file" + fileToWrite, exc);
         }
     }
 }
