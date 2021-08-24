4691827f8f73ee436ddc68337d7089ded2fcb6d8
pustoj0 <pustoj0@gmail.com>
2021-08-24 10:08:39 +0300

    created readStatisticFromFile method that read and calculate statistic and writeStatisticToFile, that write statistic to file.

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,8 +1,59 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WorkWithFile {
+    private static final int OPERATION_TYPE_INDEX = 0;
+    private static final int PRODUCT_AMOUNT_INDEX = 1;
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+    private static final String RESULT = "result";
+    private static final String REGEX = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
+        String statistic = readStatisticFromFile(fromFileName);
+        writeStatisticToFile(statistic, toFileName);
+    }
 
+    private String readStatisticFromFile(String fileName) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            int suppliedAmount = 0;
+            int boughtAmount = 0;
+            String [] lineData;
+            String line = bufferedReader.readLine();
+            while (line != null) {
+                lineData = line.split(REGEX);
+                if (lineData[OPERATION_TYPE_INDEX].equals(SUPPLY)) {
+                    suppliedAmount += Integer.parseInt(lineData[PRODUCT_AMOUNT_INDEX]);
+                }
+                if (lineData[OPERATION_TYPE_INDEX].equals(BUY)) {
+                    boughtAmount += Integer.parseInt(lineData[PRODUCT_AMOUNT_INDEX]);
+                }
+                line = bufferedReader.readLine();
+            }
+            StringBuilder statistics = new StringBuilder();
+            statistics
+                    .append(SUPPLY).append(REGEX)
+                    .append(suppliedAmount).append(System.lineSeparator())
+                    .append(BUY).append(REGEX)
+                    .append(boughtAmount).append(System.lineSeparator())
+                    .append(RESULT).append(REGEX)
+                    .append(suppliedAmount - boughtAmount).append(System.lineSeparator());
+            return statistics.toString();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void writeStatisticToFile(String information, String fileName) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName))) {
+            bufferedWriter.write(information);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
