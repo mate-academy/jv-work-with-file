971b3b5d02cded9944414cb4bac8407705b37da7
SolnalarmJulia <solnalarm@meta.ua>
2021-08-25 14:34:38 +0300

    dividing method

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -3,7 +3,6 @@ package core.basesyntax;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -11,38 +10,65 @@ import java.io.IOException;
 public class WorkWithFile {
     private static final int OPERATION_TYPE_COLUMN = 0;
     private static final int AMOUNT_COLUMN = 1;
+    private static final int SUM_APPLAY_COLUMN = 0;
+    private static final int SUM_BUY_COLUMN = 1;
+    private static final int SUM_RES_COLUMN = 2;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        File file = new File(toFileName);
-        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
-                BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file,
-                           true))) {
-            String value = reader.readLine();
-            int amountSupply = 0;
-            int amountBuy = 0;
-            while (value != null) {
-                String[] record = value.split(",");
-                if (record[OPERATION_TYPE_COLUMN].equals("supply")) {
-                    amountSupply += Integer.parseInt(record[AMOUNT_COLUMN]);
+        StringBuilder content = createReport(countResults(readFile(fromFileName)));
+        writeFile(toFileName, content);
+    }
+
+    public int[] countResults(StringBuilder stringBuilder) {
+        String[] records = stringBuilder.toString().split("\r\n");
+        int amountSupply = 0;
+        int amountBuy = 0;
+        for (String record : records) {
+            String[] columns = record.split(",");
+            for (int i = 0; i < columns.length; i++) {
+                if (columns[OPERATION_TYPE_COLUMN].equals("supply")) {
+                    amountSupply += Integer.parseInt(columns[AMOUNT_COLUMN]);
+                    break;
                 } else {
-                    amountBuy += Integer.parseInt(record[AMOUNT_COLUMN]);
+                    amountBuy += Integer.parseInt(columns[AMOUNT_COLUMN]);
+                    break;
                 }
-                value = reader.readLine();
             }
-            StringBuilder stringBuilder = new StringBuilder("");
-            stringBuilder.append("supply,")
-                    .append(amountSupply)
-                    .append(System.lineSeparator())
-                    .append("buy,")
-                    .append(amountBuy)
-                    .append(System.lineSeparator())
-                    .append("result,")
-                    .append(amountSupply - amountBuy);
-            bufferedWriter.write(stringBuilder.toString());
-        } catch (FileNotFoundException e) {
-            System.out.println("File does not exist");
+        }
+        return new int[]{amountSupply, amountBuy, amountSupply - amountBuy};
+    }
+
+    public StringBuilder createReport(int[] data) {
+        StringBuilder stringBuilder = new StringBuilder("");
+        return stringBuilder.append("supply,")
+                .append(data[SUM_APPLAY_COLUMN])
+                .append(System.lineSeparator())
+                .append("buy,")
+                .append(data[SUM_BUY_COLUMN])
+                .append(System.lineSeparator())
+                .append("result,")
+                .append(data[SUM_RES_COLUMN]);
+    }
+
+    public StringBuilder readFile(String fromFileName) {
+        StringBuilder stringBuilder = new StringBuilder("");
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
+            String value;
+            while ((value = reader.readLine()) != null) {
+                stringBuilder.append(value).append(System.lineSeparator());
+            }
         } catch (IOException e) {
             throw new RuntimeException("can't read file", e);
+        }
+        return stringBuilder;
+    }
+
+    public void writeFile(String toFileName, StringBuilder content) {
+        File file = new File(toFileName);
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file, true))) {
+            bufferedWriter.append(content);
+        } catch (IOException e) {
+            throw new RuntimeException("can't write file", e);
         }
     }
 }
