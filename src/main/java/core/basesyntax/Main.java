7c5a19136931f4d5862ea2b2018da3cab9681540
Sinelife <ya.serzhan@gmail.com>
2021-08-25 16:30:38 +0300

    delete Main class

--- src/main/java/core/basesyntax/Main.java
+++
@@ -1,8 +0,0 @@
-package core.basesyntax;
-
-public class Main {
-    public static void main(String[] args) {
-        WorkWithFile workWithFile = new WorkWithFile();
-        workWithFile.getStatistic("banana.csv", "result.txt");
-    }
-}
