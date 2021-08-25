cf38d191cc70c230a457f93041dd760ca7704c28
Kristina Voytikh <61868920+kvoytikh@users.noreply.github.com>
2021-08-25 11:44:47 +0300

    references done
    references done

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,11 +7,12 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    private static final int OPERATION_TYPE_INDEX = 0;
-    private static final int AMOUNT_INDEX = 1;
+    private static final int OPERATION_TYPE = 0;
+    private static final int AMOUNT = 1;
     private static final String SUPPLY = "supply";
     private static final String BUY = "buy";
     private static final String RESULT = "result";
+    private static final String CSV_SEPARATOR = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
         String resultString = readSourceFile(fromFileName);
@@ -25,12 +26,12 @@ public class WorkWithFile {
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             String line = bufferedReader.readLine();
             while (line != null) {
-                String[] words = line.split(",");
-                if (words[OPERATION_TYPE_INDEX].equals(BUY)) {
-                    buyAmount += Integer.parseInt(words[AMOUNT_INDEX]);
+                String[] words = line.split(CSV_SEPARATOR);
+                if (words[OPERATION_TYPE].equals(BUY)) {
+                    buyAmount += Integer.parseInt(words[AMOUNT]);
                 }
-                if (words[OPERATION_TYPE_INDEX].equals(SUPPLY)) {
-                    supplyAmount += Integer.parseInt(words[AMOUNT_INDEX]);
+                if (words[OPERATION_TYPE].equals(SUPPLY)) {
+                    supplyAmount += Integer.parseInt(words[AMOUNT]);
                 }
                 line = bufferedReader.readLine();
             }
@@ -38,11 +39,11 @@ public class WorkWithFile {
             throw new RuntimeException("Can't read file " + fromFileName, exception);
         }
 
-        resultBuilder.append(SUPPLY + ",").append(supplyAmount)
+        resultBuilder.append(SUPPLY).append(CSV_SEPARATOR).append(supplyAmount)
                 .append(System.lineSeparator())
-                .append(BUY + ",").append(buyAmount)
+                .append(BUY).append(CSV_SEPARATOR).append(buyAmount)
                 .append(System.lineSeparator())
-                .append(RESULT + ",").append(supplyAmount - buyAmount)
+                .append(RESULT).append(CSV_SEPARATOR).append(supplyAmount - buyAmount)
                 .append(System.lineSeparator());
 
         return resultBuilder.toString();
