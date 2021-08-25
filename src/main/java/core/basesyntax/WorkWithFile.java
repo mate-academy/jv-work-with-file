e05f4cf4bc192ec016a600bc8d0f09faf70563b3
Lekhter <lehter319@gmail.com>
2021-08-25 13:06:07 +0300

    Changed contains on equals in if statement. Added new constant for equals. Fixed styles mistakes.

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -12,34 +12,35 @@ public class WorkWithFile {
     private static final String BUY = "buy";
     private static final String RESULT = "result";
     private static final int PRICE = 1;
+    private static final int OPERATION = 0;
 
     public void getStatistic(String fromFileName, String toFileName) {
         writeToFile(getFileReport(fromFileName), toFileName);
     }
 
     private String getFileReport(String fromFileName) {
-        int countSupply = 0;
-        int countBuy = 0;
-        StringBuilder fileReport = new StringBuilder();
+        int supplyCount = 0;
+        int buyCount = 0;
+        StringBuilder report = new StringBuilder();
         File fromFile = new File(fromFileName);
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFile))) {
             String value = bufferedReader.readLine();
             while (value != null) {
                 String[] readedFromLine = value.split(",");
-                if (value.contains(SUPPLY)) {
-                    countSupply += Integer.parseInt(readedFromLine[PRICE]);
+                if (readedFromLine[OPERATION].equals(SUPPLY)) {
+                    supplyCount += Integer.parseInt(readedFromLine[PRICE]);
                 } else {
-                    countBuy += Integer.parseInt(readedFromLine[PRICE]);
+                    buyCount += Integer.parseInt(readedFromLine[PRICE]);
                 }
                 value = bufferedReader.readLine();
             }
         } catch (IOException e) {
             throw new RuntimeException("Can't read file" + e);
         }
-        fileReport.append(SUPPLY).append(",").append(countSupply).append(System.lineSeparator())
-                .append(BUY).append(",").append(countBuy).append(System.lineSeparator())
-                .append(RESULT).append(",").append(countSupply - countBuy);
-        return fileReport.toString();
+        report.append(SUPPLY).append(",").append(supplyCount).append(System.lineSeparator())
+                .append(BUY).append(",").append(buyCount).append(System.lineSeparator())
+                .append(RESULT).append(",").append(supplyCount - buyCount);
+        return report.toString();
     }
 
     private void writeToFile(String fileReport, String toFileName) {
