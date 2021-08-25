0aa3fcf1e1bf61d757046f1d3fa96912a6f9f134
Andrii Demchenko <avd998ne@gmail.com>
2021-08-25 11:47:37 +0300

    added changes according to comments

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,15 +7,18 @@ import java.io.FileReader;
 import java.io.FileWriter;
 
 public class WorkWithFile {
-    private static final int WORD_INDEX = 0;
+    private static final int OPERATION_TYPE_INDEX = 0;
     private static final int AMOUNT_INDEX = 1;
     private static final int BUY_INDEX = 0;
     private static final int SUPPLY_INDEX = 1;
-    private static final String COMPARISON_WORD = "buy";
+    private static final String BUY = "buy";
+    private static final String SUPPLY = "supply";
+    private static final String RESULT = "result";
+    private static final String CSV_SEPARATOR = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
         String readData = readFromFile(fromFileName);
-        String report = formatData(readData);
+        String report = createReport(readData);
         writeToFile(toFileName, report);
     }
 
@@ -29,33 +32,37 @@ public class WorkWithFile {
         } catch (Exception e) {
             throw new RuntimeException("Can't read from file" + fileName, e);
         }
-        return readString.toString().replaceAll(System.lineSeparator(), " ");
+        return readString.toString();
     }
 
     private int[] calculate(String data) {
         int calculateBuy = 0;
         int calculateSupply = 0;
-        String[] calculations = data.split(" ");
+        String[] calculations = data.split(System.lineSeparator());
         for (int i = 0; i < calculations.length; i++) {
-            String[] splitCalculations = calculations[i].split(",");
+            String[] splitCalculations = calculations[i].split(CSV_SEPARATOR);
             int amount = Integer.parseInt(splitCalculations[AMOUNT_INDEX]);
-            if (splitCalculations[WORD_INDEX].equals(COMPARISON_WORD)) {
+            if (splitCalculations[OPERATION_TYPE_INDEX].equals(BUY)) {
                 calculateBuy += amount;
-            } else {
+            }
+            if (splitCalculations[OPERATION_TYPE_INDEX].equals(SUPPLY)) {
                 calculateSupply += amount;
             }
         }
         return new int[] { calculateBuy, calculateSupply };
     }
 
-    private String formatData(String extractedData) {
+    private String createReport(String extractedData) {
         int[] calculatedData = calculate(extractedData);
         int calculatedBuy = calculatedData[BUY_INDEX];
         int calculatedSupply = calculatedData[SUPPLY_INDEX];
         return new StringBuilder()
-                .append("supply,").append(calculatedSupply).append(System.lineSeparator())
-                .append("buy,").append(calculatedBuy).append(System.lineSeparator())
-                .append("result,").append(calculatedSupply - calculatedBuy).toString();
+                .append(SUPPLY).append(CSV_SEPARATOR)
+                .append(calculatedSupply).append(System.lineSeparator())
+                .append(BUY).append(CSV_SEPARATOR)
+                .append(calculatedBuy).append(System.lineSeparator())
+                .append(RESULT).append(CSV_SEPARATOR)
+                .append(calculatedSupply - calculatedBuy).toString();
     }
 
     private void writeToFile(String fileName, String data) {
