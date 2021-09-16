07d3297860c7771c99f661f6b448e1ef9ad9611d
inbox-reva <inbox.reva@gmail.com>
2021-09-16 21:21:36 +0300

    add separate methods for read from file, create report, write to file

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,46 +7,61 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    private static final String COMMA = ","; // file data separator
-    private int buyReport = 0; // result buy per day
-    private int supplyReport = 0; // result supply per day
+    private static final String DATA_SEPARATOR = ",";
+    private int buyReport = 0;
+    private int supplyReport = 0;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        // read fromFileName and build string
-        String fileLineContent; // read line file content
-        int rowsCount = 0; // count rows in file
-        StringBuilder str = new StringBuilder(); // string from file content
+        String[][] dataMatrix = readFromFile(fromFileName);
+        String report = createReport(dataMatrix);
+        writeReportToFile(toFileName, report);
+    }
+
+    // read from file method
+    private String[][] readFromFile(String fromFileName) {
+        String line;
+        int rowsCount = 0;
+        StringBuilder fileContent = new StringBuilder();
         try (BufferedReader br = new BufferedReader(new FileReader(fromFileName))) {
-            while ((fileLineContent = br.readLine()) != null) {
-                str.append(fileLineContent).append(System.lineSeparator());
+            while ((line = br.readLine()) != null) {
+                fileContent.append(line).append(System.lineSeparator());
                 rowsCount++;
             }
         } catch (IOException e) {
-            throw new RuntimeException("Error");
+            throw new RuntimeException("Error reading from file", e);
         }
-        // build matrix array from file data
-        String [] dataArray = str.toString().split(System.lineSeparator());
-        String [][] dataMatrix = new String[rowsCount][2];
+        String[] dataArray = fileContent.toString().split(System.lineSeparator());
+        String[][] dataMatrix = new String[rowsCount][2];
         rowsCount = 0;
         for (String dataArrayItem : dataArray) {
-            dataMatrix[rowsCount] = dataArrayItem.split(COMMA);
+            dataMatrix[rowsCount] = dataArrayItem.split(DATA_SEPARATOR);
             rowsCount++;
         }
-        // calculate buy, supply and result per day value
-        for (String [] dataMatrixItem : dataMatrix) {
+        return dataMatrix;
+    }
+
+    // create report method
+    private String createReport(String[][] dataMatrix) {
+        StringBuilder report = new StringBuilder();
+        for (String[] dataMatrixItem : dataMatrix) {
             if (dataMatrixItem[0].equals("buy")) {
                 buyReport = buyReport + Integer.parseInt(dataMatrixItem[1]);
             } else if (dataMatrixItem[0].equals("supply")) {
                 supplyReport = supplyReport + Integer.parseInt(dataMatrixItem[1]);
             }
         }
-        // save result to file report
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(toFileName))) {
-            bw.write("supply," + supplyReport + System.lineSeparator()
-                    + "buy," + buyReport + System.lineSeparator()
-                    + "result," + (supplyReport - buyReport));
+        return report.append("supply,").append(supplyReport).append(System.lineSeparator())
+                .append("buy,").append(buyReport).append(System.lineSeparator())
+                .append("result,").append(supplyReport - buyReport)
+                .toString();
+    }
+
+    // write to file method
+    private void writeReportToFile(String fileName, String report) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(fileName))) {
+            bw.write(report);
         } catch (IOException e) {
-            throw new RuntimeException("Error");
+            throw new RuntimeException("Error reading from file", e);
         }
     }
 }
