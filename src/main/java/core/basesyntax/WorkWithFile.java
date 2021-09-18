567dcffa2121209ea513072209241d57b49ffd6f
Yaroslav Yarmoshyk <yarmoshyk96@gmail.com>
2021-09-18 12:43:38 +0300

    added fixes after review

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,67 +1,59 @@
 package core.basesyntax;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
-import java.nio.file.Files;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class WorkWithFile {
 
     public void getStatistic(String fromFileName, String toFileName) {
-        String data = getData(fromFileName);
-        writeData(data,toFileName);
-    }
-
-    public String getData(String fromFileName) {
         File file = new File(fromFileName);
         StringBuilder report = new StringBuilder();
-        int supValue;
-        int buyValue;
         int fullBuyValue = 0;
         int fullSupValue = 0;
-        try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+        StringBuilder dataFromFile = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
             int value = bufferedReader.read();
-            StringBuilder dataFromFile = new StringBuilder();
             while (value != -1) {
                 dataFromFile.append((char) value);
                 value = bufferedReader.read();
             }
-            String[] array = dataFromFile.toString().split("\\W+");
-            for (int j = 0; j < array.length - 1; j += 2) {
-                if ((array[j]).equals("supply")) {
-                    supValue = Integer.parseInt(array[j + 1]);
-                    fullSupValue += supValue;
-                } else if ((array[j]).equals("buy")) {
-                    buyValue = Integer.parseInt(array[j + 1]);
-                    fullBuyValue += buyValue;
-                }
-            }
-            report.append("supply,")
-                    .append(fullSupValue)
-                    .append(System.lineSeparator())
-                    .append("buy,")
-                    .append(fullBuyValue)
-                    .append(System.lineSeparator())
-                    .append("result,")
-                    .append(fullSupValue - fullBuyValue);
         } catch (Exception e) {
             throw new RuntimeException("Something wen wrong", e);
         }
-        return report.toString();
-    }
-
-    public void writeData(String data, String toFile) {
-        File file = new File(toFile);
+        String[] array = dataFromFile.toString().split("\\W+");
+        int supValue;
+        int buyValue;
+        for (int j = 0; j < array.length - 1; j += 2) {
+            if ((array[j]).equals("supply")) {
+                supValue = Integer.parseInt(array[j + 1]);
+                fullSupValue += supValue;
+            } else if ((array[j]).equals("buy")) {
+                buyValue = Integer.parseInt(array[j + 1]);
+                fullBuyValue += buyValue;
+            }
+        }
+        report.append("supply,")
+                .append(fullSupValue)
+                .append(System.lineSeparator())
+                .append("buy,")
+                .append(fullBuyValue)
+                .append(System.lineSeparator())
+                .append("result,")
+                .append(fullSupValue - fullBuyValue);
+        File toFile = new File(toFileName);
         try {
-            file.createNewFile();
+            toFile.createNewFile();
         } catch (Exception e) {
             throw new RuntimeException("can't create file");
         }
-        try {
-            Files.write(file.toPath(),data.getBytes());
-        } catch (Exception e) {
-            throw new RuntimeException("Something went wrong", e);
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFile))) {
+            bufferedWriter.write(report.toString());
+        } catch (IOException e) {
+            throw new RuntimeException("can't write data");
         }
     }
 }
