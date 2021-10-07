c10d007b9817ee5863c4041b530cfe79492e271c
Maryna Blank <marinablank96@gmail.com>
2021-10-07 19:41:07 +0300

    Fixed after review.

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,56 +8,47 @@ import java.io.IOException;
 
 public class WorkWithFile {
     private static final String SEPARATOR = ",";
-    private int buyRep = 0;
-    private int supplyRep = 0;
-    private StringBuilder stringBuilder = new StringBuilder();
-    private int rows = 0;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        WorkWithFile statistics = new WorkWithFile();
-        statistics.readFromFile(fromFileName);
-        statistics.createReport();
-        statistics.writeToFile(toFileName);
+        String[] content = readFromFile(fromFileName);
+        String report = createReport(content);
+        writeToFile(toFileName, report);
     }
 
-    private void readFromFile(String fromFileName) {
+    private String[] readFromFile(String fromFileName) {
+        StringBuilder innerContent = new StringBuilder();
         String fileLineContent;
-
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             while ((fileLineContent = bufferedReader.readLine()) != null) {
-                stringBuilder.append(fileLineContent).append(System.lineSeparator());
-                rows++;
+                innerContent.append(fileLineContent).append(System.lineSeparator());
             }
         } catch (IOException e) {
             throw new RuntimeException("Can't read from file " + fromFileName, e);
         }
+        return innerContent.toString().split(System.lineSeparator());
     }
 
-    private void createReport() {
-        String[] dataArray = stringBuilder.toString().split(System.lineSeparator());
-        String[][] dataMatrix = new String[rows][2];
-        rows = 0;
-        for (String dataArrayElement : dataArray) {
-            dataMatrix[rows] = dataArrayElement.split(SEPARATOR);
-            rows++;
-        }
-
-        for (String[] dataMatrixElement : dataMatrix) {
-            for (rows = 0; rows < 1; rows++) {
-                if (dataMatrixElement[0].equals("buy")) {
-                    buyRep += Integer.parseInt(dataMatrixElement[1]);
-                } else if (dataMatrixElement[0].equals("supply")) {
-                    supplyRep += Integer.parseInt(dataMatrixElement[1]);
-                }
+    private String createReport(String[] lines) {
+        int buyAmount = 0;
+        int supplyAmount = 0;
+        StringBuilder report = new StringBuilder();
+        for (String dataArrayElement : lines) {
+            String[] record = dataArrayElement.split(SEPARATOR);
+            if (record[0].equals("buy")) {
+                buyAmount += Integer.parseInt(record[1]);
+            } else if (record[0].equals("supply")) {
+                supplyAmount += Integer.parseInt(record[1]);
             }
         }
+        report.append("supply," + supplyAmount + System.lineSeparator()
+                + "buy," + buyAmount + System.lineSeparator()
+                + "result," + (supplyAmount - buyAmount));
+        return report.toString();
     }
 
-    private void writeToFile(String toFileName) {
+    private void writeToFile(String toFileName, String content) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
-            bufferedWriter.write("supply," + supplyRep + System.lineSeparator()
-                    + "buy," + buyRep + System.lineSeparator()
-                    + "result," + (supplyRep - buyRep));
+            bufferedWriter.write(content);
         } catch (IOException e) {
             throw new RuntimeException("Can't write to file " + toFileName, e);
         }
