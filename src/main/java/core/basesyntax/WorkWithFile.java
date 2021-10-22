008c3b2fd0108d72304c55c02c35f78972bcad6c
Ruslan Malyavsky <ruslan-gorod@ukr.net>
2021-10-22 18:55:17 +0300

    Changed method getStatistic in WorkWithFile class after conversation

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -6,12 +6,11 @@ import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 
 public class WorkWithFile {
-
     public void getStatistic(String fromFileName, String toFileName) {
-        String supplyOperationType = "supply";
-        String buyOperationType = "buy";
-        String result = "result";
-        String comma = ",";
+        final String supplyOperationType = "supply";
+        final String buyOperationType = "buy";
+        final String result = "result";
+        final String comma = ",";
         int supplySum = 0;
         int buySum = 0;
         try {
@@ -19,26 +18,27 @@ public class WorkWithFile {
                     Path.of(fromFileName)).split(System.lineSeparator());
             for (String line : stringsFromFile) {
                 String operationType = line.split(comma)[0];
-                int operationSumma = Integer.parseInt(line.split(comma)[1]);
+                int operationSum = Integer.parseInt(line.split(comma)[1]);
                 if (supplyOperationType.equals(operationType)) {
-                    supplySum += operationSumma;
+                    supplySum += operationSum;
                 }
                 if (buyOperationType.equals(operationType)) {
-                    buySum += operationSumma;
+                    buySum += operationSum;
                 }
             }
         } catch (IOException e) {
             throw new RuntimeException("Cannot read from file: " + fromFileName, e);
         }
-        String resultToWrite = supplyOperationType + comma + supplySum + System.lineSeparator()
-                + buyOperationType + comma + buySum + System.lineSeparator()
-                + result + comma + (supplySum - buySum);
+        StringBuilder resultToWrite = new StringBuilder();
+        resultToWrite.append(supplyOperationType).append(comma).append(supplySum)
+                .append(System.lineSeparator()).append(buyOperationType).append(comma)
+                .append(buySum).append(System.lineSeparator())
+                .append(result).append(comma).append((supplySum - buySum));
         try {
             Files.write(
-                    Path.of(toFileName), resultToWrite.getBytes(), StandardOpenOption.CREATE_NEW);
+                    Path.of(toFileName), resultToWrite.toString().getBytes(), StandardOpenOption.CREATE_NEW);
         } catch (IOException e) {
             throw new RuntimeException("Cannot write to file: " + toFileName, e);
         }
-
     }
 }
