83fedac109973f512fd4a98f24b5cf4bc81840bd
Pavlo_Martsiniv <imitator2010@gmail.com>
2021-10-22 17:37:29 +0300

    My solution v 1.0

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,8 +1,32 @@
 package core.basesyntax;
 
+import core.basesyntax.misc.Utils;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class WorkWithFile {
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+    private static final String RESULT = "result";
 
     public void getStatistic(String fromFileName, String toFileName) {
-
+        final HashMap<String, Integer> map = new HashMap<>();
+        final StringBuilder stringBuilder = new StringBuilder();
+        final List<String> inputList = Utils.getFile(fromFileName).collect(Collectors.toList());
+        map.put(SUPPLY, 0);
+        map.put(BUY, 0);
+        map.put(RESULT, 0);
+        for (String s : inputList) {
+            String[] arr = s.split(",");
+            map.put(arr[0], map.get(arr[0]) + Integer.parseInt(arr[1]));
+        }
+        map.put(RESULT, map.get(SUPPLY) - map.get(BUY));
+        stringBuilder.append(SUPPLY).append(",").append(map.get(SUPPLY))
+                .append(System.lineSeparator())
+                .append(BUY).append(",").append(map.get(BUY))
+                .append(System.lineSeparator())
+                .append(RESULT).append(",").append(map.get(RESULT));
+        Utils.writeToFile(toFileName, stringBuilder.toString().trim());
     }
 }
---
+++ src/main/java/core/basesyntax/misc/Utils.java
@@ -0,0 +1,26 @@
+package core.basesyntax.misc;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class Utils {
+    public static Stream<String> getFile(String name) {
+        try {
+            return Files.lines(Paths.get(name));
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Unable to read " + name, e);
+        }
+    }
+
+    public static void writeToFile(String toFileName, String data) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+            bufferedWriter.write(data);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write data to file" + toFileName, e);
+        }
+    }
+}
