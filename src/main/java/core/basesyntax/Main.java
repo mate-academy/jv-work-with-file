31d951c0b3e556363a59e01d940c59d66cb166cf
Dzmitry Kozel <dmitrokoz88@gmail.com>
2021-10-22 17:32:34 +0300

    mvn clean test passed (now for sure)

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -3,8 +3,8 @@ package core.basesyntax;
 public class Main {
     public static void main(String[] args) {
         WorkWithFile workWithFile = new WorkWithFile();
-        workWithFile.getStatistic("C:\\Users\\User\\IdeaProjects\\jv-work-with-file\\src"
-                + "\\main\\java\\core\\basesyntax\\teststarthere.txt", "C:\\Users\\User\\IdeaProjects\\"
+        workWithFile.getStatistic("C:\\Users\\User\\IdeaProjects\\jv-work-with-file\\src\\main"
+                + "\\java\\core\\basesyntax\\teststarthere.txt", "C:\\Users\\User\\IdeaProjects\\"
                 + "jv-work-with-file\\src\\main\\java\\core\\basesyntax\\testfinishhere.txt");
     }
 }
