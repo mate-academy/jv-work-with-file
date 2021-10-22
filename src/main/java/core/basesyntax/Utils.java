983c8f4d2f8f4f5066a3f200b4d76e344ddd6f99
Pavlo_Martsiniv <imitator2010@gmail.com>
2021-10-22 17:42:48 +0300

    My solution v 1.0

--- src/main/java/core/basesyntax/Utils.java
+++ src/main/java/core/basesyntax/Utils.java
@@ -1,4 +1,4 @@
-package core.basesyntax.misc;
+package core.basesyntax;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,6 +1,5 @@
 package core.basesyntax;
 
-import core.basesyntax.misc.Utils;
 import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
