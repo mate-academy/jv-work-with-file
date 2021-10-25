bc2fd96edc0977b40f191079cdbd0ce126d0a721
Serhii Pedchenko <djmolot@gmail.com>
2021-10-25 14:05:16 +0300

    fixed mvn clean packege errors

--- src/main/java/core/basesyntax/FileWriterService.java
+++ src/main/java/core/basesyntax/FileWriterService.java
@@ -7,7 +7,8 @@ import java.io.IOException;
 public class FileWriterService {
     public static void writeDataToFile(String[] data, String fileName) {
         File file = new File(fileName);
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new java.io.FileWriter(file, true))) {
+        try (BufferedWriter bufferedWriter =
+                     new BufferedWriter(new java.io.FileWriter(file, true))) {
             for (String lineData: data) {
                 bufferedWriter.write(lineData + System.lineSeparator());
                 bufferedWriter.flush();
