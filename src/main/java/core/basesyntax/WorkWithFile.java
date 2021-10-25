a7d07f928109d0a6e623346fa5a0c5660a5fdd66
Pavlo_Martsiniv <imitator2010@gmail.com>
2021-10-25 17:35:28 +0300

    My solution v 1.4 fix names, remove final

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -10,15 +10,15 @@ public class WorkWithFile {
     private static final String COMMA = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        FileService fileService = new FileService();
-        final List<String> inputList = fileService.readFromFile(fromFileName);
         final StringBuilder stringBuilder = new StringBuilder();
+        FileService fileService = new FileService();
+        List<String> inputList = fileService.readFromFile(fromFileName);
         HashMap<String, Integer> map = new HashMap<>();
         map.put(SUPPLY, 0);
         map.put(BUY, 0);
-        for (String s : inputList) {
-            String[] arr = s.split(COMMA);
-            map.put(arr[0], map.get(arr[0]) + Integer.parseInt(arr[1]));
+        for (String string : inputList) {
+            String[] splittedData = string.split(COMMA);
+            map.put(splittedData[0], map.get(splittedData[0]) + Integer.parseInt(splittedData[1]));
         }
         map.put(RESULT, map.get(SUPPLY) - map.get(BUY));
         stringBuilder.append(SUPPLY).append(COMMA).append(map.get(SUPPLY))
