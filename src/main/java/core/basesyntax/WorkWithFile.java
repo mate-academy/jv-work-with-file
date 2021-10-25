e1b4da17da52a4e2767d8bbd15c3886e3bad8ba7
Andrey Sidorkin <andrei.sidorkin@gmail.com>
2021-10-25 14:14:41 +0300

    changes after review were made - please read comments

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -9,10 +9,13 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class WorkWithFile {
+    private static final String SUPPLY = "supply,";
+    private static final String BUY = "buy,";
+    private static final String RESULT = "result,";
+
     public void getStatistic(String fromFileName, String toFileName) {
         ArrayList<String> spreadSheet = new ArrayList<>();
-        try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName));
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             String line = "";
             while (line != null) {
                 line = bufferedReader.readLine();
@@ -36,8 +39,11 @@ public class WorkWithFile {
                 supply += Integer.valueOf(target.replaceAll("[^0-9]", ""));
             }
         }
-        return "supply," + supply + System.lineSeparator() + "buy," + buy
-                + System.lineSeparator() + "result," + (supply - buy);
+        StringBuilder sb = new StringBuilder();
+        sb.append(SUPPLY).append(supply).append(System.lineSeparator())
+                .append(BUY).append(buy).append(System.lineSeparator())
+                .append(RESULT).append(supply - buy);
+        return sb.toString();
     }
 
     private void writeStatistic(String toFileName, String report) {
@@ -47,11 +53,9 @@ public class WorkWithFile {
         } catch (IOException e) {
             throw new RuntimeException("Can't create new file", e);
         }
-        try {
-            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName));
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
             bufferedWriter.write(report);
             bufferedWriter.flush();
-            bufferedWriter.close();
         } catch (IOException e) {
             throw new RuntimeException("Can't write to file", e);
         }
