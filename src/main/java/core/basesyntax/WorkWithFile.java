11acc446c4b135aedf784dae4be838c9e9477236
Natalia Vidzivashets <nata.vidzivashets@gmail.com>
2021-10-25 13:27:33 +0300

    fixed mistakes jv-work-with-file

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -25,28 +25,27 @@ public class WorkWithFile {
         return stringBuilder.toString().trim().split(" ");
     }
 
-    private byte[] createReport(String[] data) {
+    private String createReport(String[] data) {
         StringBuilder stringBuilder = new StringBuilder();
         int supply = 0;
         int purchases = 0;
-        for (String datum : data) {
-            String[] information = datum.split(",");
+        for (String string : data) {
+            String[] information = string.split(",");
             if (information[INDEX_FOR_ACTION].equals(ACTION_NAME)) {
                 supply += Integer.parseInt(information[INDEX_FOR_AMOUNT]);
             } else {
                 purchases += Integer.parseInt(information[INDEX_FOR_AMOUNT]);
             }
         }
-        String report = stringBuilder.append("supply,").append(supply)
-                        .append(System.lineSeparator())
-                        .append("buy,").append(purchases).append(System.lineSeparator())
-                        .append("result,").append(supply - purchases).toString();
-        return report.getBytes();
+        return stringBuilder.append("supply,").append(supply)
+                .append(System.lineSeparator())
+                .append("buy,").append(purchases).append(System.lineSeparator())
+                .append("result,").append(supply - purchases).toString();
     }
 
     public void getStatistic(String fromFileName, String toFileName) {
         try {
-            Files.write(Path.of(toFileName), createReport(readFile(fromFileName)));
+            Files.write(Path.of(toFileName), createReport(readFile(fromFileName)).getBytes());
         } catch (IOException e) {
             throw new RuntimeException("Cannot write data to file" + toFileName, e);
         }
