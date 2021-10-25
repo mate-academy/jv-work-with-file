a21f89164fa4d17c075952511527464e54c91456
Pavlo_Martsiniv <imitator2010@gmail.com>
2021-10-25 15:44:33 +0300

    My solution v 1.3

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,7 +7,7 @@ public class WorkWithFile {
     private static final String SUPPLY = "supply";
     private static final String BUY = "buy";
     private static final String RESULT = "result";
-    private static final String CHAR = ",";
+    private static final String COMMA = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
         FileService fileService = new FileService();
@@ -17,15 +17,15 @@ public class WorkWithFile {
         map.put(SUPPLY, 0);
         map.put(BUY, 0);
         for (String s : inputList) {
-            String[] arr = s.split(CHAR);
+            String[] arr = s.split(COMMA);
             map.put(arr[0], map.get(arr[0]) + Integer.parseInt(arr[1]));
         }
         map.put(RESULT, map.get(SUPPLY) - map.get(BUY));
-        stringBuilder.append(SUPPLY).append(CHAR).append(map.get(SUPPLY))
+        stringBuilder.append(SUPPLY).append(COMMA).append(map.get(SUPPLY))
                 .append(System.lineSeparator())
-                .append(BUY).append(CHAR).append(map.get(BUY))
+                .append(BUY).append(COMMA).append(map.get(BUY))
                 .append(System.lineSeparator())
-                .append(RESULT).append(CHAR).append(map.get(RESULT));
+                .append(RESULT).append(COMMA).append(map.get(RESULT));
         fileService.writeToFile(toFileName, stringBuilder.toString().trim());
     }
 }
