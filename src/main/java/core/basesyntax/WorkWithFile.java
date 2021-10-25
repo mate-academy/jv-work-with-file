88b9c0bd63c83f13abee0524d8bf7e2b45deff69
Pavlo_Martsiniv <imitator2010@gmail.com>
2021-10-25 15:01:02 +0300

    My solution v 1.1 fix mistakes

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,7 +2,6 @@ package core.basesyntax;
 
 import java.util.HashMap;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class WorkWithFile {
     private static final String SUPPLY = "supply";
@@ -10,12 +9,12 @@ public class WorkWithFile {
     private static final String RESULT = "result";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        final HashMap<String, Integer> map = new HashMap<>();
+        FileService fileService = new FileService();
+        final List<String> inputList = fileService.readFromFile(fromFileName);
         final StringBuilder stringBuilder = new StringBuilder();
-        final List<String> inputList = Utils.getFile(fromFileName).collect(Collectors.toList());
+        HashMap<String, Integer> map = new HashMap<>();
         map.put(SUPPLY, 0);
         map.put(BUY, 0);
-        map.put(RESULT, 0);
         for (String s : inputList) {
             String[] arr = s.split(",");
             map.put(arr[0], map.get(arr[0]) + Integer.parseInt(arr[1]));
@@ -26,6 +25,6 @@ public class WorkWithFile {
                 .append(BUY).append(",").append(map.get(BUY))
                 .append(System.lineSeparator())
                 .append(RESULT).append(",").append(map.get(RESULT));
-        Utils.writeToFile(toFileName, stringBuilder.toString().trim());
+        fileService.writeToFile(toFileName, stringBuilder.toString().trim());
     }
 }
