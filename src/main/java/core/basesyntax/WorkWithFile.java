e197bcfad543de0e8eb4c9f586ed04b628cb9f82
Vladyslav Chyver <lione2009lione@gmail.com>
2021-10-26 13:15:51 +0300

    code was optimised

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,28 +2,24 @@ package core.basesyntax;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    private int amountOfSupply = 0;
-    private int amountOfBuy = 0;
-    private int resultValue;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        readFileWithData(fromFileName);
-        getResultValue();
-        writeProcessedData(toFileName);
+        String statistic = processMarketData(getMarketDataFromFile(fromFileName));
+        writeMarketStatisticToFile(toFileName,statistic);
     }
 
-    private void readFileWithData(String fileWithData) {
+    private int[] getMarketDataFromFile(String fileWithData) {
+        int[] amountsSupplyAndBuy = {0,0};
         try (BufferedReader readerFromData = new BufferedReader(new FileReader(fileWithData))) {
             String lineWithOperation = readerFromData.readLine();
             while (lineWithOperation != null) {
-                getAmount(lineWithOperation);
+                getAmount(lineWithOperation, amountsSupplyAndBuy);
                 lineWithOperation = readerFromData.readLine();
             }
         } catch (FileNotFoundException e) {
@@ -31,41 +27,41 @@ public class WorkWithFile {
         } catch (IOException e) {
             throw new RuntimeException("Error of reading", e);
         }
+        return amountsSupplyAndBuy;
     }
 
-    private void getAmount(String operationLine) {
+    private void getAmount(String operationLine,int[] currentAmountsSupplyAndBuy) {
         int amountOnOperationLine = Integer.parseInt(operationLine
                 .substring(operationLine.indexOf(",") + 1));
         switch (operationLine.substring(0, operationLine.indexOf(","))) {
             case "supply": {
-                amountOfSupply += amountOnOperationLine;
+                currentAmountsSupplyAndBuy[0] += amountOnOperationLine;
                 break;
             }
             case "buy": {
-                amountOfBuy += amountOnOperationLine;
+                currentAmountsSupplyAndBuy[1] += amountOnOperationLine;
                 break;
             }
             default: {
-
             }
         }
     }
 
-    private void getResultValue() {
-        resultValue = amountOfSupply - amountOfBuy;
-    }
-
-    private void writeProcessedData(String toFileName) {
+    private void writeMarketStatisticToFile(String toFileName, String processedData) {
         try (BufferedWriter writerProcessedData = new BufferedWriter(
-                new FileWriter(new File(toFileName)))) {
-            writerProcessedData.append("supply,").append(Integer.toString(amountOfSupply))
-                    .append(System.lineSeparator())
-                    .append("buy,").append(Integer.toString(amountOfBuy))
-                    .append(System.lineSeparator())
-                    .append("result,").append(Integer.toString(resultValue))
-                    .append(System.lineSeparator());
+                new FileWriter(toFileName))) {
+            writerProcessedData.append(processedData);
         } catch (IOException e) {
             throw new RuntimeException("Writing error", e);
         }
+    }
+
+    private String processMarketData(int[] supplyAndBuy) {
+
+        return "supply," + supplyAndBuy[0]
+                + System.lineSeparator()
+                + "buy," + supplyAndBuy[1]
+                + System.lineSeparator()
+                + "result," + (supplyAndBuy[0] - supplyAndBuy[1]);
     }
 }
