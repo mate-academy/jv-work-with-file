5b0bd48de5fbe128c1ec5c75a0d2ce4b5be122fe
Ruslan Malyavsky <ruslan-gorod@ukr.net>
2021-10-30 11:02:44 +0300

    Changed variables

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -6,23 +6,24 @@ import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 
 public class WorkWithFile {
+    private static final String SUPPLY_OPERATION_TYPE = "supply";
+    private static final String BUY_OPERATION_TYPE = "buy";
+    private static final String RESULT = "result";
+    private static final String COMMA = ",";
+
     public void getStatistic(String fromFileName, String toFileName) {
-        final String supplyOperationType = "supply";
-        final String buyOperationType = "buy";
-        final String result = "result";
-        final String comma = ",";
         int supplySum = 0;
         int buySum = 0;
         try {
             String[] stringsFromFile = Files.readString(
                     Path.of(fromFileName)).split(System.lineSeparator());
             for (String line : stringsFromFile) {
-                String operationType = line.split(comma)[0];
-                int operationSum = Integer.parseInt(line.split(comma)[1]);
-                if (supplyOperationType.equals(operationType)) {
+                String operationType = line.split(COMMA)[0];
+                int operationSum = Integer.parseInt(line.split(COMMA)[1]);
+                if (SUPPLY_OPERATION_TYPE.equals(operationType)) {
                     supplySum += operationSum;
                 }
-                if (buyOperationType.equals(operationType)) {
+                if (BUY_OPERATION_TYPE.equals(operationType)) {
                     buySum += operationSum;
                 }
             }
@@ -30,10 +31,10 @@ public class WorkWithFile {
             throw new RuntimeException("Cannot read from file: " + fromFileName, e);
         }
         StringBuilder resultToWrite = new StringBuilder();
-        resultToWrite.append(supplyOperationType).append(comma).append(supplySum)
-                .append(System.lineSeparator()).append(buyOperationType).append(comma)
+        resultToWrite.append(SUPPLY_OPERATION_TYPE).append(COMMA).append(supplySum)
+                .append(System.lineSeparator()).append(BUY_OPERATION_TYPE).append(COMMA)
                 .append(buySum).append(System.lineSeparator())
-                .append(result).append(comma).append((supplySum - buySum));
+                .append(RESULT).append(COMMA).append((supplySum - buySum));
         try {
             Files.write(
                     Path.of(toFileName),
