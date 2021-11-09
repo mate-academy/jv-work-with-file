5baf309f431cdc617ee4871f1cddc0588883c734
morozovyrv <72643876+morozovyrv@users.noreply.github.com>
2021-11-09 16:59:58 +0200

    methods createReport and writeReportToFile changed

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -6,19 +6,12 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class WorkWithFile {
-    private final String FILE_SEPARATOR = ",";
-    private final String OPERATION_SUPPLY_NAME = "supply";
-    private final String OPERATION_BUY_NAME = "buy";
-    private final String OPERATION_RESULT_NAME = "result";
-    private List<String> allOperations;
-    private int supplyAmount;
-    private int buyAmount;
-    private int operationsAmountResult;
-    private String report;
-    private String fromFileName;
-    private String toFileName;
+    private static final String FILE_SEPARATOR = ",";
+    private static final String OPERATION_SUPPLY_NAME = "supply";
+    private static final String OPERATION_BUY_NAME = "buy";
+    private static final String OPERATION_RESULT_NAME = "result";
 
-    private List<String> getAllOperationsFromFile() {
+    private List<String> getAllOperationsFromFile(String fromFileName) {
         try {
             return Files.readAllLines(Paths.get(fromFileName));
         } catch (IOException e) {
@@ -26,7 +19,7 @@ public class WorkWithFile {
         }
     }
 
-    private void writeReportToFile() {
+    private void writeReportToFile(String report, String toFileName) {
         try {
             Files.writeString(Paths.get(toFileName),report);
         } catch (IOException e) {
@@ -34,42 +27,46 @@ public class WorkWithFile {
         }
     }
 
-    private void calculateOperationByName(String  operationName, int operationAmount) {
-        switch (operationName) {
-            case OPERATION_SUPPLY_NAME:
-                supplyAmount += operationAmount;
-                break;
-            case OPERATION_BUY_NAME:
-                buyAmount += operationAmount;
-        }
-    }
-
-    private void calculateOperationsResult() {
-        operationsAmountResult = supplyAmount - buyAmount;
-    }
-
-    private void calculateOperationsAmount() {
-        for (String operation : allOperations) {
+    private int calculateSupplyOperationsCount(List<String> operations) {
+        int supplyOperationsCount = 0;
+        for (String operation : operations) {
             String[] operationInfo = operation.split(FILE_SEPARATOR);
             String operationName = operationInfo[0];
-            int operationAmount = Integer.parseInt(operationInfo[1]);
-            calculateOperationByName(operationName,operationAmount);
+            if (operationName.equals(OPERATION_SUPPLY_NAME)) {
+                supplyOperationsCount += Integer.parseInt(operationInfo[1]);
+            }
         }
-        calculateOperationsResult();
+        return supplyOperationsCount;
     }
 
-    private void createReport() {
-        calculateOperationsAmount();
-        report = OPERATION_SUPPLY_NAME + FILE_SEPARATOR + supplyAmount + System.lineSeparator() +
-                OPERATION_BUY_NAME + FILE_SEPARATOR + buyAmount + System.lineSeparator() +
-                OPERATION_RESULT_NAME + FILE_SEPARATOR + operationsAmountResult;
+    private int calculateBuyOperationsCount(List<String> operations) {
+        int buyOperationsCount = 0;
+        for (String operation : operations) {
+            String[] operationInfo = operation.split(FILE_SEPARATOR);
+            String operationName = operationInfo[0];
+            if (operationName.equals(OPERATION_BUY_NAME)) {
+                buyOperationsCount += Integer.parseInt(operationInfo[1]);
+            }
+        }
+        return buyOperationsCount;
+    }
+
+    private int calculateOperationsResult(int supplyAmount, int buyAmount) {
+        return supplyAmount - buyAmount;
+    }
+
+    private String createReport(String fromFileName) {
+        List<String> allOperations = getAllOperationsFromFile(fromFileName);
+        int supplyAmount = calculateSupplyOperationsCount(allOperations);
+        int buyAmount = calculateBuyOperationsCount(allOperations);
+        int operationsAmountResult = calculateOperationsResult(supplyAmount,buyAmount);
+        return OPERATION_SUPPLY_NAME + FILE_SEPARATOR + supplyAmount + System.lineSeparator()
+                + OPERATION_BUY_NAME + FILE_SEPARATOR + buyAmount + System.lineSeparator()
+                + OPERATION_RESULT_NAME + FILE_SEPARATOR + operationsAmountResult;
     }
 
     public void getStatistic(String fromFileName, String toFileName) {
-        this.fromFileName = fromFileName;
-        this.toFileName = toFileName;
-        this.allOperations = getAllOperationsFromFile();
-        createReport();
-        writeReportToFile();
+        String report = createReport(fromFileName);
+        writeReportToFile(report, toFileName);
     }
 }
