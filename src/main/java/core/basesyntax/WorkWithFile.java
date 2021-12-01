9471591099a9c04932489818a9354e815a612c67
Stelmakh Yevhen <stelmakh.m.d@gmail.com>
2021-12-02 00:47:43 +0200

    Created new method, that can read and write from csv-files.

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,8 +1,50 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WorkWithFile {
 
     public void getStatistic(String fromFileName, String toFileName) {
+        String[] allInputDataOnArray;
+        StringBuilder stringBuilder = new StringBuilder();
+        String resultInfo;
+        int supply = 0;
+        int buy = 0;
 
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
+            String value = bufferedReader.readLine();
+            while (value != null) {
+                stringBuilder.append(value).append(System.lineSeparator());
+                value = bufferedReader.readLine();
+            }
+            allInputDataOnArray = stringBuilder.toString().split(System.lineSeparator());
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("Can't read file" + fromFileName, e);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
+
+        for (String i: allInputDataOnArray) {
+            if (i.startsWith("supply")) {
+                supply += Integer.parseInt(i.replace("supply,",""));
+            } else {
+                buy += Integer.parseInt(i.replace("buy,",""));
+            }
+        }
+
+        resultInfo = "supply," + supply + System.lineSeparator()
+                + "buy," + buy + System.lineSeparator()
+                + "result," + (supply - buy);
+
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName,true))) {
+            bufferedWriter.write(resultInfo);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write to file" + toFileName, e);
+        }
     }
 }
