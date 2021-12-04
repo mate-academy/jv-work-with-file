85b06ae98f29edbe749f544069b0a75979f659a6
Cergei Borovski <borovskicerg0001@gmail.com>
2021-12-04 21:22:31 +0300

    Imported libraries are sorted alphabetically.

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,11 +1,11 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.FileReader;
 
 public class WorkWithFile {
     private static final String SEPARATOR = ",";
