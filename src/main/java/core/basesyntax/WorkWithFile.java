a1ea02007011e9069a4575ff38e9075bae03e7cb
Oleksandr Semytskyi <ssemitskiy@gmail.com>
2021-12-06 16:19:38 +0200

    Improved the code

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -6,47 +6,51 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Locale;
 
 public class WorkWithFile {
+
     public void getStatistic(String fromFileName, String toFileName) {
         int supply = 0;
         int buy = 0;
-        int result = 0;
-        File file = new File(toFileName);
         StringBuilder stringBuilder = new StringBuilder();
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
             String value = reader.readLine();
             while (value != null) {
-                stringBuilder.append(value).append(" ");
+                String[] line = stringBuilder.append(value).toString().trim().split("\\W+");
+                switch (line[0]) {
+                    case "supply":
+                        supply += Integer.parseInt(line[1]);
+                        break;
+                    case "buy":
+                        buy += Integer.parseInt(line[1]);
+                        break;
+                    default:
+                }
+                stringBuilder.setLength(0);
                 value = reader.readLine();
             }
         } catch (IOException e) {
             throw new RuntimeException("Can`t read file" + e);
         }
-        String[] words = stringBuilder.toString().trim().toLowerCase(Locale.ROOT).split("\\W+");
-        for (int i = 0;i < words.length;i++) {
-            switch (words[i]) {
-                case "supply":
-                    supply += Integer.parseInt(words[i + 1]);
-                    break;
-                case "buy":
-                    buy += Integer.parseInt(words[i + 1]);
-                    break;
-                default:
-            }
-        }
-        result = supply - buy;
+        writeToFile(toFileName,createResultReport(buy,supply));
+    }
+
+    private String createResultReport(int buy, int supply) {
+        StringBuilder stringBuilder = new StringBuilder();
+        return stringBuilder.append("supply," + supply + System.lineSeparator()
+                + "buy," + buy + System.lineSeparator()
+                + "result," + (supply - buy)).toString();
+    }
+
+    private void writeToFile(String fileName,String text) {
+        File file = new File(fileName);
         try {
             file.createNewFile();
         } catch (IOException e) {
             throw new RuntimeException("Wrong file " + e);
         }
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file,true))) {
-            bufferedWriter.write("supply," + supply + System.lineSeparator());
-            bufferedWriter.write("buy," + buy + System.lineSeparator());
-            bufferedWriter.write("result," + result);
+            bufferedWriter.write(text);
             bufferedWriter.close();
         } catch (IOException e) {
             throw new RuntimeException("Wrong file" + e);
