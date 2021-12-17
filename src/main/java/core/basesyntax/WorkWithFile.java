84e163bd311819ca546c3586fcb34bfea60aca4b
Ivan Oberemok <59802427+YaTut1901@users.noreply.github.com>
2021-12-17 20:58:39 +0200

    checkstyle fixed

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.List;
 
 public class WorkWithFile {
@@ -14,7 +15,7 @@ public class WorkWithFile {
         Path infoFile = Paths.get(fromFileName);
         try {
             List<String> list = Files.readAllLines(infoFile);
-            for (String info : list){
+            for (String info : list) {
                 String[] infoArray = info.split(",");
                 switch (infoArray[0]) {
                     case ("buy") :
@@ -23,6 +24,8 @@ public class WorkWithFile {
                     case("supply") :
                         supply += Integer.valueOf(infoArray[1]);
                         break;
+                    default:
+                        break;
                 }
             }
         } catch (IOException e) {
@@ -30,7 +33,9 @@ public class WorkWithFile {
         }
         Path reportFile = Paths.get(toFileName);
         try {
-            Files.writeString(reportFile, "supply," + supply + System.lineSeparator() + "buy," + buy + System.lineSeparator() + "result," + (supply - buy));
+            Files.writeString(reportFile, "supply," + supply + System.lineSeparator());
+            Files.writeString(reportFile,"buy," + buy + System.lineSeparator(), StandardOpenOption.APPEND);
+            Files.writeString(reportFile, "result," + (supply - buy), StandardOpenOption.APPEND);
         } catch (IOException e) {
             throw new RuntimeException("Can`t write to the file", e);
         }
