1b1dd0ea27d68efcc932137342ebfd50af7f425b
Tsymbal Sergey <sfisher@ukr.net>
2021-12-20 19:24:46 +0200

    fixes after review

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,9 +7,15 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    private StringBuilder stringBuilder = new StringBuilder();
 
     public void getStatistic(String fromFileName, String toFileName) {
-        StringBuilder stringBuilder = new StringBuilder();
+        rearFromFile(fromFileName);
+        String result = createResult(stringBuilder.toString());
+        writeToFIle(result, toFileName);
+    }
+
+    private void rearFromFile(String fromFileName) {
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             String line = bufferedReader.readLine();
             while (line != null) {
@@ -19,11 +25,9 @@ public class WorkWithFile {
         } catch (IOException e) {
             throw new RuntimeException("Can't read data from the file " + fromFileName, e);
         }
-        String result = createResult(stringBuilder.toString());
-        writeToFIle(result, toFileName);
     }
 
-    public String createResult(String input) {
+    private String createResult(String input) {
         String[] array = input.split(System.lineSeparator());
         int supplyAmount = 0;
         int buyAmount = 0;
@@ -44,7 +48,7 @@ public class WorkWithFile {
         return stringBuilder.toString();
     }
 
-    public void writeToFIle(String report, String toFileName) {
+    private void writeToFIle(String report, String toFileName) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
             bufferedWriter.write(report);
         } catch (IOException e) {
