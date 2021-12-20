01743360e5f2e9c4cde2ebda8301608157943074
Ivan Oberemok <59802427+YaTut1901@users.noreply.github.com>
2021-12-20 19:04:17 +0200

    program logic was divided on 3 parts, file paths were added to exceptions texts

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -4,41 +4,43 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 import java.util.List;
 
 public class WorkWithFile {
 
     public void getStatistic(String fromFileName, String toFileName) {
-        int buy = 0;
-        int supply = 0;
         Path infoFile = Paths.get(fromFileName);
         try {
             List<String> list = Files.readAllLines(infoFile);
-            for (String info : list) {
-                String[] infoArray = info.split(",");
-                switch (infoArray[0]) {
-                    case ("buy") :
-                        buy += Integer.valueOf(infoArray[1]);
-                        break;
-                    case("supply") :
-                        supply += Integer.valueOf(infoArray[1]);
-                        break;
-                    default:
-                        break;
-                }
-            }
+            String report = createReport(list);
+            writeToFile(report, toFileName);
         } catch (IOException e) {
-            throw new RuntimeException("Can`t read the file", e);
+            throw new RuntimeException("Can`t read the file" + infoFile, e);
         }
+    }
+
+    public String createReport(List<String> list) {
+        int buy = 0;
+        int supply = 0;
+        for (String info : list) {
+            String[] infoArray = info.split(",");
+            if (infoArray[0].equals("buy")) {
+                buy += Integer.parseInt(infoArray[1]);
+            } else if (infoArray[0].equals("supply")) {
+                supply += Integer.parseInt(infoArray[1]);
+            }
+        }
+        return "supply," + supply + System.lineSeparator()
+                + "buy," + buy + System.lineSeparator()
+                + "result," + (supply - buy);
+    }
+
+    public void writeToFile(String report,String toFileName) {
         Path reportFile = Paths.get(toFileName);
         try {
-            Files.writeString(reportFile, "supply," + supply + System.lineSeparator());
-            Files.writeString(reportFile,"buy," + buy + System.lineSeparator(),
-                    StandardOpenOption.APPEND);
-            Files.writeString(reportFile, "result," + (supply - buy), StandardOpenOption.APPEND);
+            Files.writeString(reportFile, report);
         } catch (IOException e) {
-            throw new RuntimeException("Can`t write to the file", e);
+            throw new RuntimeException("Can`t write to the file" + reportFile, e);
         }
     }
 }
