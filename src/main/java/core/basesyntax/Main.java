23426cae914826ca8ab1dcabbab83bc999933932
Oleh B <oleg.busygin@gmail.com>
2021-12-21 13:09:40 +0200

    Method getStatistic implemented three new methods created: getStatistic generateReport writeToFile

---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,9 @@
+package core.basesyntax;
+
+public class Main {
+    public static void main(String[] args) {
+        WorkWithFile wf = new WorkWithFile();
+        wf.getStatistic("apple.csv", "reportApple.csv");
+
+    }
+}
--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,6 +7,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
@@ -16,8 +17,7 @@ public class WorkWithFile {
     private String readContent(String fromFileName) {
         final StringBuilder stringBuilder = new StringBuilder();
         String value;
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
             value = reader.readLine();
             while (value != null) {
                 stringBuilder.append(value).append(System.lineSeparator());
@@ -29,25 +29,19 @@ public class WorkWithFile {
         return stringBuilder.toString();
     }
 
-    private ArrayList<String> generateReport(String rawContent) {
+    private List<String> generateReport(String rawContent) {
+
         String[] receivedData = rawContent.split(System.lineSeparator());
-        ArrayList<String> report = new ArrayList<>();
+        List<String> report = new ArrayList<>();
         int supplySum = 0;
         int buySum = 0;
-        for (int i = 0; i < receivedData.length; i++) {
-            String operation = receivedData[i].substring(0, receivedData[i].indexOf(","));
-            int sumByOperration = 0;
-            for (int j = 0; j < receivedData.length; j++) {
-                if (operation.equals(receivedData[j]
-                        .substring(0, receivedData[j].indexOf(",")))) {
-                    sumByOperration += Integer.parseInt(receivedData[j]
-                            .substring(receivedData[j].indexOf(",") + 1));
-                }
-            }
-            if (operation.equals("supply")) {
-                supplySum = sumByOperration;
-            } else if (operation.equals("buy")) {
-                buySum = sumByOperration;
+
+        for (int j = 0; j < receivedData.length; j++) {
+            String[] operation = receivedData[j].split(",");
+            if (operation[0].equals("buy")) {
+                buySum += Integer.parseInt(operation[1]);
+            } else if (operation[0].equals("supply")) {
+                supplySum += Integer.parseInt(operation[1]);
             }
         }
         report.add("supply," + supplySum);
@@ -56,29 +50,20 @@ public class WorkWithFile {
         return report;
     }
 
-    private void writeToFile(String toFileName, ArrayList<String> report) {
+    private void writeToFile(String toFileName, List<String> report) {
         File file = new File(toFileName);
         try {
             file.createNewFile();
         } catch (IOException e) {
             throw new RuntimeException("Can`t create new file", e);
         }
-        BufferedWriter bufferedWriter = null;
-        try {
-            bufferedWriter = new BufferedWriter((new FileWriter(toFileName, true)));
+
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName, true))) {
             for (String record : report) {
                 bufferedWriter.write(record + System.lineSeparator());
             }
         } catch (IOException e) {
-            throw new RuntimeException("Can`t write to file" + toFileName, e);
-        } finally {
-            if (bufferedWriter != null) {
-                try {
-                    bufferedWriter.close();
-                } catch (IOException e) {
-                    throw new RuntimeException("Can`t close bufferedWriter");
-                }
-            }
+            throw new RuntimeException("Cant write to file " + toFileName, e);
         }
     }
 }
