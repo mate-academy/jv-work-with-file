a524a8bf268dc234dfcfa5266d07566c0a24b493
Yurii Tomashevskyi <niteris92@gmail.com>
2022-01-22 16:42:03 +0200

    implemented getStatistic method and 2 supported methods

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,8 +1,55 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WorkWithFile {
+    private static final String SUPPLY_CODENAME = "supply";
 
     public void getStatistic(String fromFileName, String toFileName) {
+        String[] readData = readFromFile(fromFileName);
+        int supply = 0;
+        int buy = 0;
+        for (int i = 0; i < readData.length; i += 2) {
+            if (readData[i].equals(SUPPLY_CODENAME)) {
+                supply += Integer.parseInt(readData[i + 1]);
+            } else {
+                buy += Integer.parseInt(readData[i + 1]);
+            }
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("supply,").append(supply).append(System.lineSeparator())
+                .append("buy,").append(buy).append(System.lineSeparator())
+                .append("result,").append(supply - buy).append(System.lineSeparator());
+        writeToFile(stringBuilder.toString(), toFileName);
+    }
 
+    private void writeToFile(String report, String toFileName) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+            bufferedWriter.write(report);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't writer file " + toFileName, e);
+        }
+    }
+
+    private String[] readFromFile(String fileName) {
+        File file = new File(fileName);
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            int value = reader.read();
+            while (value != -1) {
+                stringBuilder.append((char) value);
+                value = reader.read();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t read file", e);
+        }
+        String sentence = stringBuilder.toString().toLowerCase();
+        return sentence.split("\\W+");
     }
 }
