9f446d1572063cab23479588b84b8780f836eb33
Anton Hut <tonnyhut@icloud.com>
2022-01-22 13:50:38 +0200

    1)created method readFile which reads file and return info in the array. 2)created method makeReport which makes report and returns him like a String. 3)method fileWrite takes the String and writs into the new file.

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,8 +1,56 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class WorkWithFile {
+    static final String SEPARATOR = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
+        writeFile(makeReport(readFile(fromFileName)), toFileName);
 
+    }
+
+    private String[] readFile(String fileName) {
+        ArrayList<String> list = new ArrayList<>();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                list.add(line);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read", e);
+        }
+        return list.toArray(new String[0]);
+    }
+
+    private String makeReport(String[] arrOfInfo) {
+        int supply = 0;
+        int buy = 0;
+
+        for (String s : arrOfInfo) {
+            String[] split = s.split(SEPARATOR);
+            if (split[0].startsWith("s")) {
+                supply = supply + Integer.parseInt(split[1]);
+            } else {
+                buy = buy + Integer.parseInt(split[1]);
+            }
+        }
+
+        return "supply," + supply + System.lineSeparator()
+                + "buy," + buy + System.lineSeparator()
+                + "result," + (supply - buy);
+    }
+
+    private void writeFile(String dataToWrite, String fileWhereWrite) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(fileWhereWrite))) {
+            bw.write(dataToWrite);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write to file", e);
+        }
     }
 }
