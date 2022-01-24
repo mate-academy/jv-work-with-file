41b56bcbc7cab2c4d0d92e9a05c1942c01076508
Liubov Chernohor <liubovchernohor@gmail.com>
2022-01-24 19:10:31 +0200

    "getStatistic" method was updated

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -9,75 +9,34 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    static final int STATISTIC_POSITIONS_COUNT = 6;
-    static final int SUPPLY_POSITION_NUMBER = 0;
-    static final int BUY_POSITION_NUMBER = 2;
-    static final int RESULT_POSITION_NUMBER = 4;
+    private static final String supply = "supply";
+    private static final String buy = "buy";
+    private static final String separator = ",";
+    private int supplySum = 0;
+    private int buySum = 0;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        String[] data = getDataFromFile(fromFileName);
-        String[]statistic = getStatisticPattern();
-        StringBuilder stringBuilder = new StringBuilder();
-        int sum;
+        String[] data = readFromFile(fromFileName);
+        String report;
         for (String datum : data) {
-            String[] note = datum.split(",");
-            sum = Integer.parseInt(note[1]);
-            switch (note[0]) {
-                case "supply":
-                    statistic[SUPPLY_POSITION_NUMBER + 1]
-                            = String.valueOf(Integer.parseInt(statistic[SUPPLY_POSITION_NUMBER + 1])
-                            + sum);
-                    break;
-                case "buy":
-                    statistic[BUY_POSITION_NUMBER + 1]
-                            = String.valueOf(Integer.parseInt(statistic[BUY_POSITION_NUMBER + 1])
-                            + sum);
-                    break;
-                default: break;
-            }
+            String[] note = datum.split(separator);
+            supplySum += note[0].equals(supply) ? Integer.parseInt(note[1]) : 0;
+            buySum += note[0].equals(buy) ? Integer.parseInt(note[1]) : 0;
         }
-        statistic[RESULT_POSITION_NUMBER + 1]
-                = String.valueOf(Integer.parseInt(statistic[SUPPLY_POSITION_NUMBER + 1])
-                - Integer.parseInt(statistic[BUY_POSITION_NUMBER + 1]));
-        for (int i = 0; i < statistic.length; i++) {
-            stringBuilder.append(statistic[i]);
-            if (i % 2 != 0) {
-                stringBuilder.append(System.lineSeparator());
-            }
-        }
-        writeDataToFile(stringBuilder.toString(), toFileName);
+        report = (new StringBuilder(supply).append(separator).append(supplySum)
+                .append(System.lineSeparator()).append(buy).append(separator)
+                .append(buySum).append(System.lineSeparator()).append("result,")
+                .append(supplySum - buySum)).toString();
+        writeToFile(report, toFileName);
     }
 
-    public String[] getStatisticPattern() {
-        String[] statistic = new String[STATISTIC_POSITIONS_COUNT];
-        for (int i = 0; i < statistic.length; i++) {
-            switch (i) {
-                case SUPPLY_POSITION_NUMBER: statistic[i] = "supply,";
-                    break;
-                case (SUPPLY_POSITION_NUMBER + 1): statistic[i] = "0";
-                    break;
-                case BUY_POSITION_NUMBER: statistic[i] = "buy,";
-                    break;
-                case BUY_POSITION_NUMBER + 1: statistic[i] = "0";
-                    break;
-                case RESULT_POSITION_NUMBER: statistic[i] = "result,";
-                    break;
-                case RESULT_POSITION_NUMBER + 1: statistic[i] = "0";
-                    break;
-                default: break;
-            }
-        }
-        return statistic;
-    }
-
-    public String[] getDataFromFile(String fromFileName) {
+    public String[] readFromFile(String fromFileName) {
         File fileOut = new File(fromFileName);
         StringBuilder stringBuilder = new StringBuilder();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileOut))) {
-            String textLine = bufferedReader.readLine();
-            while (textLine != null) {
+            String textLine;
+            while ((textLine = bufferedReader.readLine()) != null) {
                 stringBuilder.append(textLine).append(System.lineSeparator());
-                textLine = bufferedReader.readLine();
             }
         } catch (FileNotFoundException e) {
             throw new RuntimeException("File doesn't exist", e);
@@ -87,7 +46,7 @@ public class WorkWithFile {
         return stringBuilder.toString().split(System.lineSeparator());
     }
 
-    public void writeDataToFile(String data, String toFileName) {
+    public void writeToFile(String data, String toFileName) {
         File fileTo = new File(toFileName);
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileTo, true))) {
             bufferedWriter.write(data);
