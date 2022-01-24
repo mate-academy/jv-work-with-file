be8a98530a03ebb56218685fdbafb4ebee401eb9
LinkClink <shekaqwe@gmail.com>
2022-01-24 14:01:56 +0200

    add constants

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,6 +8,11 @@ import java.io.IOException;
 
 class WorkWithFile {
     private static final String[] OPERATION_TYPE_NAMES = new String[]{"supply", "buy", "result"};
+    private static final int OPERATION_TYPES_ARRAY_SIZE = 2;
+    private static final int INDEX_OF_TYPE_SUPPLY = 0;
+    private static final int INDEX_OF_TYPE_BUY = 1;
+    private static final int INDEX_OF_TYPE_RESULT = 2;
+    private static final int INDEX_OF_OPERATION_TYPE_IN_LINE = 0;
 
     public void getStatistic(String fromFileName, String toFileName) {
         writeToFile(toFileName, readFromFile(fromFileName));
@@ -25,15 +30,18 @@ class WorkWithFile {
     private String readFromFile(String fromFileName) {
         String line = "";
         StringBuilder stringBuilder = new StringBuilder();
-        int[] sumOfOperations = new int[2];
+        int[] sumOfOperations = new int[OPERATION_TYPES_ARRAY_SIZE];
         try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName));
             while ((line = bufferedReader.readLine()) != null) {
                 String[] currentLine = line.split(",");
-                if (currentLine[0].equals(OPERATION_TYPE_NAMES[0])) {
-                    sumOfOperations[0] += Integer.parseInt(currentLine[1]);
-                } else if (currentLine[0].equals(OPERATION_TYPE_NAMES[1])) {
-                    sumOfOperations[1] += Integer.parseInt(currentLine[1]);
+                if (currentLine[INDEX_OF_OPERATION_TYPE_IN_LINE]
+                        .equals(OPERATION_TYPE_NAMES[INDEX_OF_TYPE_SUPPLY])) {
+                    sumOfOperations[INDEX_OF_TYPE_SUPPLY]
+                            += Integer.parseInt(currentLine[1]);
+                } else if (currentLine[INDEX_OF_OPERATION_TYPE_IN_LINE]
+                        .equals(OPERATION_TYPE_NAMES[INDEX_OF_TYPE_BUY])) {
+                    sumOfOperations[INDEX_OF_TYPE_BUY] += Integer.parseInt(currentLine[1]);
                 }
             }
             bufferedReader.close();
@@ -44,8 +52,8 @@ class WorkWithFile {
             stringBuilder.append(OPERATION_TYPE_NAMES[a]).append(',')
                     .append(sumOfOperations[a]).append(System.lineSeparator());
         }
-        stringBuilder.append(OPERATION_TYPE_NAMES[2]).append(",")
-                .append(sumOfOperations[0] - sumOfOperations[1])
+        stringBuilder.append(OPERATION_TYPE_NAMES[INDEX_OF_TYPE_RESULT]).append(",")
+                .append(sumOfOperations[INDEX_OF_TYPE_SUPPLY] - sumOfOperations[INDEX_OF_TYPE_BUY])
                 .append(System.lineSeparator());
         return stringBuilder.toString();
     }
