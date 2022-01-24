3cf8902b0fd4cc025697fa24fac1a3cedeae7dde
LinkClink <shekaqwe@gmail.com>
2022-01-24 15:31:31 +0200

    create method (createReport) add constant SEPARATOR

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -6,16 +6,19 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
-class WorkWithFile {
+public class WorkWithFile {
     private static final String[] OPERATION_TYPE_NAMES = new String[]{"supply", "buy", "result"};
+    private static final String SEPARATOR = ",";
     private static final int OPERATION_TYPES_ARRAY_SIZE = 2;
     private static final int INDEX_OF_TYPE_SUPPLY = 0;
     private static final int INDEX_OF_TYPE_BUY = 1;
     private static final int INDEX_OF_TYPE_RESULT = 2;
     private static final int INDEX_OF_OPERATION_TYPE_IN_LINE = 0;
+    private int[] sumOfOperations = new int[OPERATION_TYPES_ARRAY_SIZE];
 
     public void getStatistic(String fromFileName, String toFileName) {
-        writeToFile(toFileName, readFromFile(fromFileName));
+        readFromFile(fromFileName);
+        writeToFile(toFileName, createReport());
     }
 
     private void writeToFile(String fileName, String dateToWrite) {
@@ -27,14 +30,11 @@ class WorkWithFile {
         }
     }
 
-    private String readFromFile(String fromFileName) {
+    private void readFromFile(String fromFileName) {
         String line = "";
-        StringBuilder stringBuilder = new StringBuilder();
-        int[] sumOfOperations = new int[OPERATION_TYPES_ARRAY_SIZE];
-        try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName));
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             while ((line = bufferedReader.readLine()) != null) {
-                String[] currentLine = line.split(",");
+                String[] currentLine = line.split(SEPARATOR);
                 if (currentLine[INDEX_OF_OPERATION_TYPE_IN_LINE]
                         .equals(OPERATION_TYPE_NAMES[INDEX_OF_TYPE_SUPPLY])) {
                     sumOfOperations[INDEX_OF_TYPE_SUPPLY]
@@ -44,15 +44,18 @@ class WorkWithFile {
                     sumOfOperations[INDEX_OF_TYPE_BUY] += Integer.parseInt(currentLine[1]);
                 }
             }
-            bufferedReader.close();
         } catch (IOException e) {
             throw new RuntimeException("Can't read data from the file " + e);
         }
+    }
+
+    private String createReport() {
+        StringBuilder stringBuilder = new StringBuilder();
         for (int a = 0; a < sumOfOperations.length; a++) {
-            stringBuilder.append(OPERATION_TYPE_NAMES[a]).append(',')
+            stringBuilder.append(OPERATION_TYPE_NAMES[a]).append(SEPARATOR)
                     .append(sumOfOperations[a]).append(System.lineSeparator());
         }
-        stringBuilder.append(OPERATION_TYPE_NAMES[INDEX_OF_TYPE_RESULT]).append(",")
+        stringBuilder.append(OPERATION_TYPE_NAMES[INDEX_OF_TYPE_RESULT]).append(SEPARATOR)
                 .append(sumOfOperations[INDEX_OF_TYPE_SUPPLY] - sumOfOperations[INDEX_OF_TYPE_BUY])
                 .append(System.lineSeparator());
         return stringBuilder.toString();
