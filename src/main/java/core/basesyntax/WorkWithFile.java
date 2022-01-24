e0e61d337116a218060da55b5fcea625d24300a4
LinkClink <shekaqwe@gmail.com>
2022-01-24 12:31:55 +0200

    renaming + simpl

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,43 +7,45 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 class WorkWithFile {
-    void getStatistic(String fromFileName, String toFileName) {
+    private static String[] OPERATION_TYPE_NAMES = new String[]{"supply", "buy", "result"};
+
+    public void getStatistic(String fromFileName, String toFileName) {
         writeToFile(toFileName, readFromFile(fromFileName));
     }
 
-    private void writeToFile(String toFileName, String arrayToWrite) {
+    private void writeToFile(String fileName, String dateToWrite) {
         try (BufferedWriter bufferedWriter
-                     = new BufferedWriter(new FileWriter(toFileName, true))) {
-            bufferedWriter.write(arrayToWrite);
+                     = new BufferedWriter(new FileWriter(fileName, true))) {
+            bufferedWriter.write(dateToWrite);
         } catch (IOException e) {
-            throw new RuntimeException("Can't open the file " + e);
+            throw new RuntimeException("Can't write to file" + e);
         }
     }
 
     private String readFromFile(String fromFileName) {
         String line = "";
         StringBuilder stringBuilder = new StringBuilder();
-        int[] counts = new int[2];
+        int[] sumOfOperations = new int[2];
         try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName));
             while ((line = bufferedReader.readLine()) != null) {
                 String[] currentLine = line.split(",");
-                if (currentLine[0].equals("supply")) {
-                    counts[0] += Integer.parseInt(currentLine[1]);
-                } else if (currentLine[0].equals("buy")) {
-                    counts[1] += Integer.parseInt(currentLine[1]);
+                if (currentLine[0].equals(OPERATION_TYPE_NAMES[0])) {
+                    sumOfOperations[0] += Integer.parseInt(currentLine[1]);
+                } else if (currentLine[0].equals(OPERATION_TYPE_NAMES[1])) {
+                    sumOfOperations[1] += Integer.parseInt(currentLine[1]);
                 }
             }
             bufferedReader.close();
         } catch (IOException e) {
             throw new RuntimeException("Can't read data from the file " + e);
         }
-        stringBuilder.append("supply,").append(counts[0])
-                .append(System.lineSeparator());
-        stringBuilder.append("buy,").append(counts[1])
-                .append(System.lineSeparator());
-        stringBuilder.append("result").append(",")
-                .append(counts[0] - counts[1])
+        for (int a = 0; a < sumOfOperations.length; a++) {
+            stringBuilder.append(OPERATION_TYPE_NAMES[a]).append(',')
+                    .append(sumOfOperations[a]).append(System.lineSeparator());
+        }
+        stringBuilder.append(OPERATION_TYPE_NAMES[2]).append(",")
+                .append(sumOfOperations[0] - sumOfOperations[1])
                 .append(System.lineSeparator());
         return stringBuilder.toString();
     }
