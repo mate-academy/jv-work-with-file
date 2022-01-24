b2822f0b9a73b1351945825285221cd7aeb69289
Oleh Mytiai <mytiai.ov@icloud.com>
2022-01-24 20:43:26 +0200

    created constants

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,9 @@
 package core.basesyntax;
 
 public class WorkWithFile {
-
+    private static final String BUY = "buy";
+    private static final String SUPPLY = "supply";
+    private static final String REGEX = ",";
     public void getStatistic(String fromFileName, String toFileName) {
 
     }
