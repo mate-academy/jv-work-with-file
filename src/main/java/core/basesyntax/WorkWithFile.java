a9fa6fb6662984f6a38f30bd890ec2ad98e35cd8
Valera Lemberov <valera.lemberov1993@gmail.com>
2022-01-24 18:01:09 +0200

    request corrected

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -9,48 +9,53 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+    private static final String RESULT = "result";
+    private static final String COMMA = ",";
+
     public void getStatistic(String fromFileName, String toFileName) {
         String[] boofData;
         boofData = getDataFromFile(fromFileName).split("\\W+");
-        StringBuilder builder = new StringBuilder();
         int buy = 0;
         int supply = 0;
         int result;
-        for (int i = 0; i < boofData.length - 1;i = i + 2) {
-            supply += (boofData[i].equals("supply") ? Integer.parseInt(boofData[i + 1]) : 0);
-            buy += (boofData[i].equals("buy") ? Integer.parseInt(boofData[i + 1]) : 0);
+        for (int i = 0; i < boofData.length - 1; i = i + 2) {
+            supply += (boofData[i].equals(SUPPLY) ? Integer.parseInt(boofData[i + 1]) : 0);
+            buy += (boofData[i].equals(BUY) ? Integer.parseInt(boofData[i + 1]) : 0);
         }
-
         result = supply - buy;
-
-        builder.append("supply,").append(supply).append(System.lineSeparator())
-                .append("buy,").append(buy).append(System.lineSeparator())
-                .append("result,").append(result).toString();
-        writeDataToFile(builder.toString(),toFileName);
+        writeDataToFile(getStringStatistic(supply,buy,result),toFileName);
     }
 
-    public void writeDataToFile(String dataForWrite,String toFileName) {
+    private String getStringStatistic(int supply,int buy,int result) {
+        return new StringBuilder().append(SUPPLY).append(COMMA)
+                .append(supply).append(System.lineSeparator())
+                .append(BUY).append(COMMA).append(buy).append(System.lineSeparator())
+                .append(RESULT).append(COMMA).append(result).toString();
+    }
+
+    private void writeDataToFile(String dataForWrite,String toFileName) {
         try (BufferedWriter dataWriter = new BufferedWriter(new FileWriter(toFileName))) {
             dataWriter.write(dataForWrite);
         } catch (IOException e) {
-            throw new WorkWithFileException("Can't write data to file");
+            throw new RuntimeException("Can't write data to file");
         }
     }
 
-    public String getDataFromFile(String fromFileName) {
+    private String getDataFromFile(String fromFileName) {
         File fileFrom = new File(fromFileName);
-        StringBuilder builder = new StringBuilder("");
+        StringBuilder builder = new StringBuilder();
         try (BufferedReader readerFrom = new BufferedReader(new FileReader(fileFrom))) {
-            String fileString = readerFrom.readLine();
-            while (fileString != null) {
+            String fileString;
+            while ((fileString = readerFrom.readLine()) != null) {
                 builder.append(fileString).append(System.lineSeparator());
-                fileString = readerFrom.readLine();
             }
         } catch (FileNotFoundException e) {
-            throw new WorkWithFileException("File not found");
+            throw new RuntimeException("File not found");
         } catch (IOException e) {
-            throw new WorkWithFileException("Can't work with file");
+            throw new RuntimeException("Can't work with file");
         }
-        return builder.toString();
+        return builder.toString().trim();
     }
 }
