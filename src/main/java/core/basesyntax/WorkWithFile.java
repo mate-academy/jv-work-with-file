083d3a0b1bcacbf7c877012b9fee4113d097a053
Anton Ignatenko <87337009+antonignatenko@users.noreply.github.com>
2022-01-25 18:12:03 +0200

    newer version

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,36 +1,42 @@
 package core.basesyntax;
 
-import java.io.*;
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class WorkWithFile {
 
-    private final String SUPPLY = "supply";
-    private final String BUY = "buy";
-    private final String RESULT = "result";
-    private final String COMMA = ",";
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+    private static final String RESULT = "result";
+    private static final String COMMA = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
         int buy = 0;
         int supply = 0;
-        int result = 0;
 
-        StringBuilder build = new StringBuilder(readFromFile(fromFileName));
-        String[] resultingData = build.toString().split("\\W+");
+        String[] resultingData = readFromFile(fromFileName).split("\\W+");
         for (int i = 0; i < resultingData.length - 1; i += 2) {
-            supply += resultingData[i].equals("supply") ? Integer.parseInt(resultingData[i + 1]) : 0;
-            buy += resultingData[i].equals("buy") ? Integer.parseInt(resultingData[i + 1]) : 0;
+            supply += resultingData[i].equals("supply") ? Integer.parseInt(resultingData[i + 1])
+                    : 0;
+            buy += resultingData[i].equals("buy") ? Integer.parseInt(resultingData[i + 1])
+                    : 0;
         }
-        result = buy - supply;
-        writeDataToFile(createsStatistic(supply, buy, result), toFileName);
 
+        int delta = buy - supply;
+        writeDataToFile(createsStatistic(supply, buy, delta), toFileName);
 
     }
 
     private String readFromFile(String filePathName) {
+        File file = new File(filePathName);
         StringBuilder builder = new StringBuilder();
         try {
-            BufferedReader reader = new BufferedReader(new FileReader(filePathName));
+            BufferedReader reader = new BufferedReader(new FileReader(file));
             String readFromFile;
             while ((readFromFile = reader.readLine()) != null) {
                 builder.append(readFromFile).append(System.lineSeparator());
