6d87da4c5a25a9e6b60ba04c691cf0780b1aafd3
Dima Semeniuk <semeniuk.dima@ukr.net>
2022-01-25 12:36:22 +0200

    Fixed after second review

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -10,15 +10,18 @@ public class WorkWithFile {
     private static final String SUPPLY = "supply";
     private static final String BUY = "buy";
     private static final String RESULT = "result";
+    private static final String COMMA = ",";
+    private static final int FIRST_ELEM = 0;
+    private static final int SECOND_ELEM = 1;
+    private static final int THIRD_ELEM = 2;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        writeInfoToReport(createReport(getData(fromFileName)), toFileName);
+        writeInfoToReport(createReport(countingStock(getData(fromFileName))), toFileName);
     }
 
     private String getData(String fromFile) {
         StringBuilder stringBuilder = new StringBuilder();
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(fromFile));
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFile))) {
             String value = reader.readLine();
             while (value != null) {
                 stringBuilder.append(value).append(System.lineSeparator());
@@ -30,25 +33,32 @@ public class WorkWithFile {
         return stringBuilder.toString();
     }
 
-    private String createReport(String data) {
+    private String countingStock(String data) {
         int buy = 0;
         int supply = 0;
         int result = 0;
+        StringBuilder summaryResult = new StringBuilder();
         String[] info = data.split(System.lineSeparator());
         for (String line : info) {
-            String[] lineElements = line.split(",");
-            if (lineElements[0].equals(SUPPLY)) {
-                supply += Integer.parseInt(lineElements[1]);
-            } else if (lineElements[0].equals(BUY)) {
-                buy += Integer.parseInt(lineElements[1]);
+            String[] lineElements = line.split(COMMA);
+            if (lineElements[FIRST_ELEM].equals(SUPPLY)) {
+                supply += Integer.parseInt(lineElements[SECOND_ELEM]);
+            } else if (lineElements[FIRST_ELEM].equals(BUY)) {
+                buy += Integer.parseInt(lineElements[SECOND_ELEM]);
             }
         }
         result = supply - buy;
+        return summaryResult.append(supply).append(COMMA)
+                .append(buy).append(COMMA).append(result).toString();
+    }
+
+    private String createReport(String data) {
         StringBuilder stringBuilderForReport = new StringBuilder();
-        stringBuilderForReport.append(SUPPLY).append(",").append(supply)
-                .append(System.lineSeparator()).append(BUY).append(",").append(buy)
-                .append(System.lineSeparator())
-                .append(RESULT).append(",").append(result);
+        String[] dataElements = data.split(COMMA);
+        stringBuilderForReport.append(SUPPLY).append(COMMA).append(dataElements[FIRST_ELEM])
+                .append(System.lineSeparator()).append(BUY).append(COMMA)
+                .append(dataElements[SECOND_ELEM]).append(System.lineSeparator())
+                .append(RESULT).append(COMMA).append(dataElements[THIRD_ELEM]);
         return stringBuilderForReport.toString();
     }
 
