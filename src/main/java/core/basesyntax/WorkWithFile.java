8b432719772a2e736fb6b8f0752d5ba1ae1bf5e8
DenTima <tymoshchuk.den@gmail.com>
2022-01-25 11:11:40 +0200

    Created new method for writing report

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -11,10 +11,14 @@ public class WorkWithFile {
     private static final String SUPPLY = "supply";
     private static final String BUY = "buy";
     private static final String RESULT = "result";
+    private static final String COMA = ",";
+    private static final int SUPPLY_INDEX = 0;
+    private static final int BUY_INDEX = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
         String data = readFile(fromFileName);
-        String report = countData(data);
+        int[] arrayValues = countData(data);
+        String report = writeReport(arrayValues);
         writeDataToFile(toFileName, report);
     }
 
@@ -25,7 +29,7 @@ public class WorkWithFile {
             StringBuilder stringBuilder = new StringBuilder();
             String value = bufferedReader.readLine();
             while (value != null) {
-                stringBuilder.append(value).append(",");
+                stringBuilder.append(value).append(COMA);
                 value = bufferedReader.readLine();
             }
             return stringBuilder.toString();
@@ -34,8 +38,8 @@ public class WorkWithFile {
         }
     }
 
-    public String countData(String data) {
-        String[] splitArr = data.split(",");
+    public int[] countData(String data) {
+        String[] splitArr = data.split(COMA);
         int supply = 0;
         int buy = 0;
         for (int i = 0; i < splitArr.length; i += 2) {
@@ -45,11 +49,16 @@ public class WorkWithFile {
                 buy += Integer.parseInt(splitArr[i + 1]);
             }
         }
+        int[] array = {supply, buy};
+        return array;
+    }
+
+    public String writeReport(int[] arrayOfData) {
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(SUPPLY).append(",").append(supply)
-                .append(System.lineSeparator()).append(BUY).append(",")
-                .append(buy).append(System.lineSeparator()).append(RESULT)
-                .append(",").append(supply - buy);
+        stringBuilder.append(SUPPLY).append(COMA).append(arrayOfData[SUPPLY_INDEX])
+                .append(System.lineSeparator()).append(BUY).append(COMA)
+                .append(arrayOfData[BUY_INDEX]).append(System.lineSeparator()).append(RESULT)
+                .append(COMA).append(arrayOfData[SUPPLY_INDEX] - arrayOfData[BUY_INDEX]);
         return stringBuilder.toString();
     }
 
