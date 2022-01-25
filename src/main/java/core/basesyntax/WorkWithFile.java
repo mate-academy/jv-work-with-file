d7b548c224814d4212599374fb9fbc1a93f385f2
Roman <dudanecr@gmail.com>
2022-01-25 18:03:03 +0200

    homevork work with cvs file

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,61 +1,79 @@
 package core.basesyntax;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.util.List;
+import java.io.*;
 
 public class WorkWithFile {
-
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+    private static final String RESULT = "result";
     private static final String COMMA_SEPARATOR = ",";
-    private int supply = 0;
-    private int buy = 0;
     private int result = 0;
-    private StringBuilder stringBuilder = new StringBuilder();
-    private String[] res;
-    private StringBuilder builder = new StringBuilder();
 
     public void getStatistic(String fromFileName, String toFileName) {
         readFromFile(fromFileName);
-        writeToFile(toFileName, createReport());
+        writeToFile(toFileName, writeToFile());
     }
 
-    private void readFromFile(String fromFileName) {
+    private static String readFromFile(String fromFileName) {
         File file = new File(fromFileName);
+        StringBuilder builder = new StringBuilder();
+
         try {
-            List<String> strings = Files.readAllLines(file.toPath());
-            res = strings.toArray(new String[strings.size()]);
-            for (String a: res) {
-                String[] temporary = a.split(",");
-                if (temporary[0].equals("supply")) {
-                    supply += Integer.valueOf(temporary[1]);;
-                } else if (temporary[0].equals("buy")) {
-                    buy += Integer.valueOf(temporary[1]);
-                }
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        String value = null;
+
+            value = reader.readLine();
+            while (value != null) {
+                builder.append(value).append("|");
+                value = reader.readLine();
             }
-        } catch (IOException e) {
-            throw new RuntimeException("cant read file", e);
+
+        }catch (IOException e) {
+            throw new RuntimeException("can`t read file...", e);
         }
+            return builder.toString();
+        }
+
+    private int[] countData(int[] builder) {
+        int supply = 0;
+        int buy = 0;
+        String[] res = builder.toString().split("|");
+        for (String a: res) {
+            String[] temporary = a.split(",");
+            if (temporary[0].equals("supply")) {
+                supply += Integer.valueOf(temporary[1]);;
+            } else if (temporary[0].equals("buy")) {
+                buy += Integer.valueOf(temporary[1]);
+            }
+        }
+        int[] data = new int[]{supply, buy};
+        return data;
     }
 
-    private String createReport() {
+    private String createReport(int[] data) {
+        countData(data);
+        int supply = data[0];
+        int buy = data[1];
+        StringBuilder builder = new StringBuilder();
         result += supply - buy;
-        builder.append("supply").append(COMMA_SEPARATOR).append(supply
-        ).append(System.lineSeparator()).append("buy").append(COMMA_SEPARATOR
-        ).append(buy).append(System.lineSeparator()
-        ).append("result").append(COMMA_SEPARATOR
-        ).append(result).append(System.lineSeparator());
-        return builder.toString();
+        builder.append(SUPPLY).append(COMMA_SEPARATOR).append(supply)
+                .append(System.lineSeparator()).append(BUY).append(COMMA_SEPARATOR)
+                .append(buy).append(System.lineSeparator())
+                .append(RESULT).append(COMMA_SEPARATOR)
+                .append(result).append(System.lineSeparator());
+        String report = builder.toString();
+        return report;
     }
 
     private void writeToFile(String fileName, String report) {
+
         File fileOne = new File(fileName);
+        createReport(report);
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileOne, true));) {
-            writer.write(builder.toString());
+            writer.write(report);
         } catch (IOException e) {
             throw new RuntimeException("Can`t write file...", e);
         }
     }
 }
+
