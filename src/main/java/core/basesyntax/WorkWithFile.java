0bf1e36d5816dbfe9816614cfb42b7f181949438
Sasha Bachynskyi <allfred12@gmail.com>
2022-01-25 13:27:06 +0200

    implemented readFromFile, countData, createReport and writeToFile methods instead of one getStatistics

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -11,11 +11,20 @@ import java.nio.file.Path;
 public class WorkWithFile {
     private static final int INDEX_FOR_OPERATION_TYPE = 0;
     private static final int INDEX_FOR_AMOUNT = 1;
+    private static final int INDEX_FOR_SUPPLY_AMOUNT = 0;
+    private static final int INDEX_FOR_BUY_AMOUNT = 1;
     private static final String SEPARATOR_FOR_DATA = ",";
     private static final String WORD_BUY = "buy";
     private static final String WORD_SUPPLY = "supply";
 
     public void getStatistic(String fromFileName, String toFileName) {
+        String data = readFromFile(fromFileName);
+        int[] arraySupplyBuyAmount = countData(data);
+        byte[] result = createReport(arraySupplyBuyAmount);
+        writeToFile(result, toFileName);
+    }
+
+    private String readFromFile(String fromFileName) {
         File file = new File(fromFileName);
         StringBuilder builder = new StringBuilder();
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
@@ -29,7 +38,10 @@ public class WorkWithFile {
         } catch (IOException e) {
             throw new RuntimeException("Can't read the file " + fromFileName, e);
         }
-        String data = builder.toString();
+        return builder.toString();
+    }
+
+    private int[] countData(String data) {
         String[] arrayData = data.split(System.lineSeparator());
         int supplyAmout = 0;
         int buyAmout = 0;
@@ -42,19 +54,27 @@ public class WorkWithFile {
                 buyAmout += Integer.parseInt(temp[INDEX_FOR_AMOUNT]);
             }
         }
+        return new int[]{supplyAmout, buyAmout};
+    }
+
+    private byte[] createReport(int[] arraySupplyBuyAmount) {
         StringBuilder builderResult = new StringBuilder();
         builderResult.append(WORD_SUPPLY)
                 .append(SEPARATOR_FOR_DATA)
-                .append(supplyAmout)
+                .append(arraySupplyBuyAmount[INDEX_FOR_SUPPLY_AMOUNT])
                 .append(System.lineSeparator())
                 .append(WORD_BUY)
                 .append(SEPARATOR_FOR_DATA)
-                .append(buyAmout)
+                .append(arraySupplyBuyAmount[INDEX_FOR_BUY_AMOUNT])
                 .append(System.lineSeparator())
                 .append("result")
                 .append(SEPARATOR_FOR_DATA)
-                .append(supplyAmout - buyAmout);
-        byte[] result = builderResult.toString().getBytes();
+                .append(arraySupplyBuyAmount[INDEX_FOR_SUPPLY_AMOUNT]
+                        - arraySupplyBuyAmount[INDEX_FOR_BUY_AMOUNT]);
+        return builderResult.toString().getBytes();
+    }
+
+    private void writeToFile(byte[] result, String toFileName) {
         try {
             Files.write(Path.of(toFileName), result);
         } catch (IOException e) {
