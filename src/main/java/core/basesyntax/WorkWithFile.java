15b5d814234a646ba53098b56d7033fa5f46c052
Roman <dudanecr@gmail.com>
2022-01-26 11:38:37 +0200

    homevork work with cvs file

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,6 +1,11 @@
 package core.basesyntax;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class WorkWithFile {
     private static final String SUPPLY = "supply";
@@ -10,39 +15,37 @@ public class WorkWithFile {
     private int result = 0;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        readFromFile(fromFileName);
-        writeToFile(toFileName, writeToFile());
+        String fileData = readFromFile(fromFileName);
+        writeToFile(toFileName, createReport(countData(fileData)));
     }
 
-    private static String readFromFile(String fromFileName) {
+    private String readFromFile(String fromFileName) {
         File file = new File(fromFileName);
         StringBuilder builder = new StringBuilder();
-
         try {
-        BufferedReader reader = new BufferedReader(new FileReader(file));
-        String value = null;
-
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String value = null;
             value = reader.readLine();
             while (value != null) {
-                builder.append(value).append("|");
+                builder.append(value).append("_");
                 value = reader.readLine();
             }
 
-        }catch (IOException e) {
+        } catch (IOException e) {
             throw new RuntimeException("can`t read file...", e);
         }
-            return builder.toString();
-        }
+        return builder.toString();
+    }
 
-    private int[] countData(int[] builder) {
+    private int[] countData(String fileData) {
         int supply = 0;
         int buy = 0;
-        String[] res = builder.toString().split("|");
+        String[] res = fileData.split("_");
         for (String a: res) {
             String[] temporary = a.split(",");
-            if (temporary[0].equals("supply")) {
-                supply += Integer.valueOf(temporary[1]);;
-            } else if (temporary[0].equals("buy")) {
+            if (temporary[0].equals(SUPPLY)) {
+                supply += Integer.valueOf(temporary[1]);
+            } else if (temporary[0].equals(BUY)) {
                 buy += Integer.valueOf(temporary[1]);
             }
         }
@@ -51,7 +54,6 @@ public class WorkWithFile {
     }
 
     private String createReport(int[] data) {
-        countData(data);
         int supply = data[0];
         int buy = data[1];
         StringBuilder builder = new StringBuilder();
@@ -68,7 +70,6 @@ public class WorkWithFile {
     private void writeToFile(String fileName, String report) {
 
         File fileOne = new File(fileName);
-        createReport(report);
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileOne, true));) {
             writer.write(report);
         } catch (IOException e) {
