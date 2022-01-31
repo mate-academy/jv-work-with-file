4aec89d7c4825276dbd4d90c9c8646e64ba41970
Dmytro Lemeshchenko <dimmonishe@gmail.com>
2022-01-31 22:29:41 +0200

    Some changes

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -6,22 +6,22 @@ public class WorkWithFile {
 
     public void getStatistic(String fromFileName, String toFileName) throws IOException {
         File fromFile = new File(fromFileName);
-
+//        int supplyCounter = 0;
+//        int buyCounter = 0;
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             int value = bufferedReader.read();
             while (value != -1) {
                 String[] dataFromFile = (bufferedReader.readLine()).split(",");
-                getResult(dataFromFile);
+                String result = getResult(dataFromFile);
                 value = bufferedReader.read();
             }
-            } catch(FileNotFoundException e){
-                throw new RuntimeException("Error! File " + fromFileName + " not found!", e);
             } catch(IOException e){
                 throw new RuntimeException("Input/Output error for file " + fromFileName + "!", e);
             }
         File toFile = new File(toFileName);
+        WorkWithFile workWithFile = new WorkWithFile();
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFile, true))) {
-            bufferedWriter.write("supply, " + supplyCounter);
+            bufferedWriter.write(getResult());
         } catch (FileNotFoundException e){
             throw new RuntimeException("Error! File " + toFileName + " not found!", e);
         } catch(IOException e){
@@ -29,7 +29,7 @@ public class WorkWithFile {
         }
         }
 
-    public void getResult(String[] dataFromFile) {
+    private String getResult(String[] dataFromFile) {
         int supplyCounter = 0;
         int buyCounter = 0;
           if (dataFromFile[0].equals("supply")) {
@@ -37,6 +37,8 @@ public class WorkWithFile {
           } else {
             buyCounter += Integer.getInteger(dataFromFile[1]);
           }
+        return ("supply, " + supplyCounter + System.lineSeparator() + "bye, " + buyCounter
+                + System.lineSeparator() + "result, " + (supplyCounter - buyCounter));
     }
 
 
