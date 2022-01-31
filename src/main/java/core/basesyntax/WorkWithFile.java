f794fa9ea82d4f6de6f96a5b19bded702225fb99
Dmytro Lemeshchenko <dimmonishe@gmail.com>
2022-02-01 01:54:59 +0200

    Create tree methods

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -6,41 +6,38 @@ public class WorkWithFile {
 
     public void getStatistic(String fromFileName, String toFileName) throws IOException {
         File fromFile = new File(fromFileName);
-//        int supplyCounter = 0;
-//        int buyCounter = 0;
+        int supplyCounter = 0;
+        int buyCounter = 0;
+        BufferedReader newBufferedReader = null;
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             int value = bufferedReader.read();
             while (value != -1) {
                 String[] dataFromFile = (bufferedReader.readLine()).split(",");
-                String result = getResult(dataFromFile);
+                if (dataFromFile[0].equals("supply")) {
+                    supplyCounter += Integer.getInteger(dataFromFile[1]);
+                } else {
+                    buyCounter += Integer.getInteger(dataFromFile[1]);
+                }
                 value = bufferedReader.read();
             }
-            } catch(IOException e){
-                throw new RuntimeException("Input/Output error for file " + fromFileName + "!", e);
-            }
-        File toFile = new File(toFileName);
-        WorkWithFile workWithFile = new WorkWithFile();
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFile, true))) {
-            bufferedWriter.write(getResult());
-        } catch (FileNotFoundException e){
-            throw new RuntimeException("Error! File " + toFileName + " not found!", e);
-        } catch(IOException e){
-            throw new RuntimeException("Input/Output error for file " + toFileName + "!", e);
+        } catch (IOException e) {
+            throw new RuntimeException("Read error for file " + fromFileName + "!", e);
         }
-        }
-
-    private String getResult(String[] dataFromFile) {
-        int supplyCounter = 0;
-        int buyCounter = 0;
-          if (dataFromFile[0].equals("supply")) {
-            supplyCounter += Integer.getInteger(dataFromFile[1]);
-          } else {
-            buyCounter += Integer.getInteger(dataFromFile[1]);
-          }
-        return ("supply, " + supplyCounter + System.lineSeparator() + "bye, " + buyCounter
-                + System.lineSeparator() + "result, " + (supplyCounter - buyCounter));
+        String result = getResult(supplyCounter,buyCounter);
+        fileWriter(toFileName, result);
     }
-
-
+    private void fileWriter(String fileToWrite, String infoToWrite) {
+        BufferedWriter startBufferedWriter = null;
+        File toFile = new File(fileToWrite);
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFile, true))) {
+            bufferedWriter.write(infoToWrite);
+        } catch(IOException e){
+            throw new RuntimeException("Write error for file " + fileToWrite + "!", e);
+        }
+    }
+    private String getResult(int supply, int buy) {
+        return ("supply, " + supply + System.lineSeparator() + "buy, " + buy
+                + System.lineSeparator() + "result, " + (supply - buy));
+    }
 }
 
