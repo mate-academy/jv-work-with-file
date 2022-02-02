7c1dad31b634f1d3acc771d8e3e37aa1f3610dfd
Vitalii Bozadzhy <VitaliiBozadzhy@icloud.com>
2022-02-02 15:20:26 +0200

    fixed checklist decided to use try with resourses corrected message of RunTimeException

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,36 +8,57 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-
     public void getStatistic(String fromFileName, String toFileName) {
-        File file = new File(fromFileName);
-        String line = "";
-        int sumSupply = 0;
-        int sumBuy = 0;
-        int diffSumSupplySumBuy = 0;
-        try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
-            while ((line = bufferedReader.readLine()) != null) {
-                String[] dataArray = line.split(",");
-                int numbers = Integer.parseInt(dataArray[1]);
-                if (dataArray[0].equals("supply")) {
-                    sumSupply += numbers;
-                } else if (dataArray[0].equals("buy")) {
-                    sumBuy = sumBuy + numbers;
+        write(toFileName, createReport(generateReport(read(fromFileName))));
+    }
+
+    private String[] read(String fileName) {
+        File file = new File(fileName);
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String value = reader.readLine();
+            while (value != null) {
+                if (value.startsWith("buy") || value.startsWith("supply")) {
+                    stringBuilder.append(value).append(" ");
                 }
-                diffSumSupplySumBuy = sumSupply - sumBuy;
+                value = reader.readLine();
             }
         } catch (IOException e) {
-            throw new RuntimeException("Can not read this file");
+            throw new RuntimeException("Can not read file", e);
         }
-        try {
-            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName));
-            bufferedWriter.write("supply," + sumSupply
-                                    + "\nbuy," + sumBuy
-                                    + "\nresult," + diffSumSupplySumBuy);
-            bufferedWriter.close();
+        return stringBuilder.toString().split(" ");
+    }
+
+    private String[] generateReport(String[] operations) {
+        int buy = 0;
+        int supply = 0;
+        for (String array : operations) {
+            String[] arr = array.split(",");
+            if (arr[0].equals("buy")) {
+                buy += Integer.parseInt(arr[1]);
+            } else if (arr[0].equals("supply")) {
+                supply += Integer.parseInt(arr[1]);
+            }
+        }
+        return new String[] {String.valueOf(buy), String.valueOf(supply)};
+    }
+
+    private String[] createReport(String[] data) {
+        String[] report = new String[3];
+        report[0] = "supply," + data[1] + System.lineSeparator();
+        report[1] = "buy," + data[0] + System.lineSeparator();
+        report[2] = "result," + (Integer.parseInt(data[1]) - Integer.parseInt(data[0]));
+        return report;
+    }
+
+    private void write(String fileName, String[] report) {
+        File file = new File(fileName);
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file, false))) {
+            for (String reports : report) {
+                bufferedWriter.write(reports);
+            }
         } catch (IOException e) {
-            throw new RuntimeException("Can not write file");
+            throw new RuntimeException("Can not write file ", e);
         }
     }
 }
