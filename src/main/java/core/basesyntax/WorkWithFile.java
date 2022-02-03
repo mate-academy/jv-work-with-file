5fb553a11bc0200983a0b936a69cd590eae46b82
Sergii Turchyn <turchyn13@gmail.com>
2022-02-03 21:25:51 +0200

    implemented general logic

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,8 +1,40 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class WorkWithFile {
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
 
     public void getStatistic(String fromFileName, String toFileName) {
+        int supplyCounter = 0;
+        int buyCounter = 0;
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
+            String value = bufferedReader.readLine();
+            while (value != null) {
+                String[] splited = value.split(",");
+                int parsed = Integer.parseInt(splited[1]);
+                if (splited[0].equals(SUPPLY)){
+                    supplyCounter += parsed;
+                } else if (splited[0].equals(BUY)) {
+                    buyCounter += parsed;
+                }
+                value = bufferedReader.readLine();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Cann't read file...", e);
+        }
+        int result = supplyCounter - buyCounter;
+        System.out.println(result + " " + supplyCounter + " " + buyCounter );
+    }
 
+    public static void main(String[] args) {
+        WorkWithFile workWithFile = new WorkWithFile();
+        workWithFile.getStatistic("apple.csv", "AppleStat.csv");
     }
 }
+
+
