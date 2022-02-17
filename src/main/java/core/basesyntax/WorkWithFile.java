67ddc383944795b0850c584246e2bfe94494af0a
Nikita Melnikov <bam19921303@gmail.com>
2022-02-17 18:23:32 +0200

    -Create reader for file and Algorithm for counting "supply" and "buy" -Added writer to write to file "toFileName"

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,8 +1,51 @@
 package core.basesyntax;
+import java.io.*;
+import java.util.Arrays;
+
 
 public class WorkWithFile {
 
     public void getStatistic(String fromFileName, String toFileName) {
+        int buy = 0;
+        int supply = 0;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
+            String record = reader.readLine();
+            while (record != null) {
+                String[] split = record.split(",");
+                record = reader.readLine();
+                if (split[0].equals("buy")) {
+                    buy = buy + Integer.parseInt(split[1]);
+                } else {
+                    supply = supply + Integer.parseInt(split[1]);
+                }
+            }
+            System.out.println(buy);
+            System.out.println(supply);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
+        try {
+            FileWriter writer = new FileWriter(toFileName);
+            writer.append("supply");
+            writer.append(",");
+            writer.append(Integer.toString(supply));
+            writer.append(System.lineSeparator());
 
+            writer.append("buy");
+            writer.append(",");
+            writer.append(Integer.toString(buy));
+            writer.append(System.lineSeparator());
+
+            writer.append("result");
+            writer.append(",");
+            writer.append(Integer.toString(supply - buy));
+            writer.append(System.lineSeparator());
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write file", e);
+        }
     }
 }
+
