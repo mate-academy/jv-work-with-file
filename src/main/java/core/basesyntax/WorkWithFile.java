f8e827cf8499299e42b8951ae4ac328e6bfa0d45
Oleksandr Domnich <dmnichz@gmail.com>
2022-02-19 19:11:34 +0200

    added solution for jv-work-with-file task

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,8 +1,41 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+
 public class WorkWithFile {
 
     public void getStatistic(String fromFileName, String toFileName) {
-
+        int buy = 0;
+        int supply = 0;
+        StringBuilder stringBuilder = new StringBuilder();
+        File file = new File(fromFileName);
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            String value = bufferedReader.readLine();
+            while (value != null) {
+                String[] dataLine = value.toLowerCase().split("\\W+");
+                if (dataLine[0].equals("buy")) {
+                    buy += Integer.parseInt(dataLine[1]);
+                } else if (dataLine[0].equals("supply")) {
+                    supply += Integer.parseInt(dataLine[1]);
+                }
+                value = bufferedReader.readLine();
+            }
+            stringBuilder.append("supply,").append(supply).append(System.lineSeparator());
+            stringBuilder.append("buy,").append(buy).append(System.lineSeparator());
+            stringBuilder.append("result,").append(supply - buy);
+        } catch (Exception e) {
+            throw new RuntimeException("Can't read file", e);
+        }
+        file = new File(toFileName);
+        try {
+            Files.write(file.toPath(), stringBuilder.toString().getBytes());
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write data to file", e);
+        }
     }
 }
