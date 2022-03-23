e14608a3cfc9f3c643ea7404be2aa5d3530e17fe
DanyloTiazhev <finckreg@gmail.com>
2022-03-23 22:50:54 +0200

    WorkWithFile method implemented

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,18 +1,18 @@
 package core.basesyntax;
 
-import java.io.*;
-
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class WorkWithFile {
-    public static void main(String[] args) {  //delete main method
-        WorkWithFile example = new WorkWithFile();
-        example.getStatistic("apple.csv", "example");
-    }
-
     public void getStatistic(String fromFileName, String toFileName) {
         StringBuilder stringBuilder = new StringBuilder();
         File file = new File(fromFileName);
-        try {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName, true))) {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
             String value = bufferedReader.readLine();
             while (value != null) {
@@ -24,24 +24,20 @@ public class WorkWithFile {
             int buy = 0;
             for (String position:array) {
                 int amount = Integer.parseInt(position.substring(position.indexOf(',') + 1));
-                if(position.charAt(0) == 's'){
+                if (position.charAt(0) == 's') {
                     supply += amount;
                 } else {
                     buy += amount;
                 }
             }
             int res = supply - buy;
-            String supplyString = "supply," + supply;
-            String buyString = "buy," + buy;
+            String supplyString = "supply," + supply + System.lineSeparator();
+            String buyString = "buy," + buy + System.lineSeparator();
             String result = "result," + res;
-
             String[] reportArray = {supplyString, buyString, result};
             for (String reportLine:reportArray) {
-                BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName, true));
-                bufferedWriter.write(reportLine + "\n");
-                bufferedWriter.close();
+                bufferedWriter.write(reportLine);
             }
-
 
         } catch (FileNotFoundException e) {
             throw new RuntimeException("File can`t be read", e);
