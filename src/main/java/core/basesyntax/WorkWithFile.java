39f98af53dcefca676ba07f06e4a7d1e95b9a76b
DanyloTiazhev <finckreg@gmail.com>
2022-03-23 23:48:50 +0200

    WorkWithFile method implemented + divided in different methods

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -10,39 +10,52 @@ import java.io.IOException;
 
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
-        StringBuilder stringBuilder = new StringBuilder();
-        File file = new File(fromFileName);
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName, true))) {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
-            String value = bufferedReader.readLine();
+            String[] array = reader(fromFileName);
+            String[] reportArray = reportGenerator(array);
+            for (String reportLine : reportArray) {
+                bufferedWriter.write(reportLine);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("Can`t find a file", e);
+        } catch (IOException e) {
+            throw new RuntimeException("Line can`t be written", e);
+        }
+    }
+
+    private String[] reader(String fileName) {
+        File file = new File(fileName);
+        StringBuilder stringBuilder = new StringBuilder();
+        BufferedReader bufferedReader;
+        try {
+            bufferedReader = new BufferedReader(new FileReader(file));
+            String value;
+            value = bufferedReader.readLine();
             while (value != null) {
                 stringBuilder.append(value).append(" ");
                 value = bufferedReader.readLine();
             }
-            String [] array = stringBuilder.toString().split(" ");
-            int supply = 0;
-            int buy = 0;
-            for (String position:array) {
-                int amount = Integer.parseInt(position.substring(position.indexOf(',') + 1));
-                if (position.charAt(0) == 's') {
-                    supply += amount;
-                } else {
-                    buy += amount;
-                }
-            }
-            int res = supply - buy;
-            String supplyString = "supply," + supply + System.lineSeparator();
-            String buyString = "buy," + buy + System.lineSeparator();
-            String result = "result," + res;
-            String[] reportArray = {supplyString, buyString, result};
-            for (String reportLine:reportArray) {
-                bufferedWriter.write(reportLine);
-            }
-
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException("File can`t be read", e);
+            return stringBuilder.toString().split(" ");
         } catch (IOException e) {
             throw new RuntimeException("Line can`t be read", e);
         }
+    }
+
+    private String[] reportGenerator(String[] readFile) {
+        int supply = 0;
+        int buy = 0;
+        for (String position: readFile) {
+            int amount = Integer.parseInt(position.substring(position.indexOf(',') + 1));
+            if (position.charAt(0) == 's') {
+                supply += amount;
+            } else {
+                buy += amount;
+            }
+        }
+        int res = supply - buy;
+        String supplyString = "supply," + supply + System.lineSeparator();
+        String buyString = "buy," + buy + System.lineSeparator();
+        String result = "result," + res;
+        return new String[]{supplyString, buyString, result};
     }
 }
