8436dd914bdbe039db98ac0bd5760be5276d4cf4
Volodymyr Klimyuk <klimyuk054@gmail.com>
2022-03-24 17:08:39 +0200

    Fixed all the remarks of the mentor.

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,38 +1,54 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Collections;
 
 public class WorkWithFile {
+    private final StringBuilder lineRead = new StringBuilder();
+    private final StringBuilder report = new StringBuilder();
+
+    private int supply = 0;
+    private int buy = 0;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        StringBuilder lineRead = new StringBuilder();
-        File file = new File(fromFileName);
-        int supply = 0;
-        int buy = 0;
+        String[] data = readFromFile(fromFileName).split(",");
+        String report = createReport(getResult(data));
+        writeToFile(toFileName, report);
+    }
 
-        try {
-            lineRead.append(Files.readAllLines(file.toPath()));
+    public String readFromFile(String fromFileName) {
+        File file = new File(fromFileName);
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                lineRead.append(line).append(",");
+            }
         } catch (IOException e) {
             throw new RuntimeException("Can't read all line", e);
         }
+        return lineRead.toString();
+    }
 
-        String[] arrayInput = lineRead.substring(1,lineRead.length() - 1).split(",");
-        for (int i = 0, j = 1; i < arrayInput.length; i += 2, j += 2) {
-            if (arrayInput[i].charAt(1) == 's' || arrayInput[i].equals("supply")) {
-                supply += Integer.parseInt(arrayInput[j]);
+    public int getResult(String[] data) {
+        for (int i = 0, j = 1; i < data.length; i += 2, j += 2) {
+            if (data[i].equals("supply")) {
+                supply += Integer.parseInt(data[j]);
             }
-            if (arrayInput[i].charAt(1) == 'b' || arrayInput[i].equals("buy")) {
-                buy += Integer.parseInt(arrayInput[j]);
+            if (data[i].equals("buy")) {
+                buy += Integer.parseInt(data[j]);
             }
         }
-        int result = supply - buy;
+        return supply - buy;
+    }
+
+    public String createReport(int result) {
         String nextLine = System.lineSeparator();
-        StringBuilder writeText = new StringBuilder();
-        writeText.append("supply,")
+        report.append("supply,")
                 .append(supply)
                 .append(nextLine)
                 .append("buy,")
@@ -40,9 +56,12 @@ public class WorkWithFile {
                 .append(nextLine)
                 .append("result,")
                 .append(result);
+        return report.toString();
+    }
 
+    public void writeToFile(String toFileName, String report) {
         try {
-            Files.write(Path.of(toFileName), Collections.singleton(writeText));
+            Files.write(Path.of(toFileName), Collections.singleton(report));
         } catch (IOException e) {
             throw new RuntimeException("Can't write text in file", e);
         }
