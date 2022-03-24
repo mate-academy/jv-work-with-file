45e0a8ae60e74bbcff460303988b3e95917a1edb
Volodymyr Klimyuk <klimyuk054@gmail.com>
2022-03-24 20:00:30 +0200

    Fixed all the remarks of the mentor.

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -6,14 +6,13 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Collections;
 
 public class WorkWithFile {
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+
     private final StringBuilder lineRead = new StringBuilder();
     private final StringBuilder report = new StringBuilder();
-
-    private int supply = 0;
-    private int buy = 0;
 
     public void getStatistic(String fromFileName, String toFileName) {
         String[] data = readFromFile(fromFileName).split(",");
@@ -21,7 +20,7 @@ public class WorkWithFile {
         writeToFile(toFileName, report);
     }
 
-    public String readFromFile(String fromFileName) {
+    private String readFromFile(String fromFileName) {
         File file = new File(fromFileName);
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
             String line;
@@ -34,34 +33,36 @@ public class WorkWithFile {
         return lineRead.toString();
     }
 
-    public int getResult(String[] data) {
+    private int[] getResult(String[] data) {
+        int supply = 0;
+        int buy = 0;
         for (int i = 0, j = 1; i < data.length; i += 2, j += 2) {
-            if (data[i].equals("supply")) {
+            if (data[i].equals(SUPPLY)) {
                 supply += Integer.parseInt(data[j]);
             }
-            if (data[i].equals("buy")) {
+            if (data[i].equals(BUY)) {
                 buy += Integer.parseInt(data[j]);
             }
         }
-        return supply - buy;
+        return new int[] {supply, buy, supply - buy};
     }
 
-    public String createReport(int result) {
+    private String createReport(int[] result) {
         String nextLine = System.lineSeparator();
         report.append("supply,")
-                .append(supply)
+                .append(result[0])
                 .append(nextLine)
                 .append("buy,")
-                .append(buy)
+                .append(result[1])
                 .append(nextLine)
                 .append("result,")
-                .append(result);
+                .append(result[2]);
         return report.toString();
     }
 
-    public void writeToFile(String toFileName, String report) {
+    private void writeToFile(String toFileName, String report) {
         try {
-            Files.write(Path.of(toFileName), Collections.singleton(report));
+            Files.writeString(Path.of(toFileName), report);
         } catch (IOException e) {
             throw new RuntimeException("Can't write text in file", e);
         }
