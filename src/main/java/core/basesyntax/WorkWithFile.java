de4e9a55bfb4e9c6442e6859c999a8a668cb10cf
Oleksandr Nesteruk <54321777tv@gmail.com>
2022-03-25 18:18:32 +0200

    mvn test success

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,10 @@
 package core.basesyntax;
 
-import java.io.*;
-import java.nio.file.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class WorkWithFile {
     private static final String SUPPLY = "supply";
