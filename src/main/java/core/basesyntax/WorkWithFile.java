9fbd416dfd09903b1c9b53f7ee860a329aa8c366
Nikita Melnikov <bam19921303@gmail.com>
2022-03-26 15:05:45 +0300

    apply try with resources Change method writer on BufferedWriter

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,6 +1,7 @@
 package core.basesyntax;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -10,8 +11,7 @@ public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
         int buy = 0;
         int supply = 0;
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
             String record = reader.readLine();
             while (record != null) {
                 String[] split = record.split(",");
@@ -27,24 +27,23 @@ public class WorkWithFile {
         } catch (IOException e) {
             throw new RuntimeException("Can't read file", e);
         }
-        try {
-            FileWriter writer = new FileWriter(toFileName);
-            writer.append("supply");
-            writer.append(",");
-            writer.append(Integer.toString(supply));
-            writer.append(System.lineSeparator());
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
+            StringBuilder str = new StringBuilder();
+            str.append("supply");
+            str.append(",");
+            str.append(supply);
+            str.append(System.lineSeparator());
 
-            writer.append("buy");
-            writer.append(",");
-            writer.append(Integer.toString(buy));
-            writer.append(System.lineSeparator());
+            str.append("buy");
+            str.append(",");
+            str.append(buy);
+            str.append(System.lineSeparator());
 
-            writer.append("result");
-            writer.append(",");
-            writer.append(Integer.toString(supply - buy));
-            writer.append(System.lineSeparator());
-            writer.flush();
-            writer.close();
+            str.append("result");
+            str.append(",");
+            str.append(supply - buy);
+            str.append(System.lineSeparator());
+            writer.write(str.toString());
         } catch (IOException e) {
             throw new RuntimeException("Can't write file", e);
         }
