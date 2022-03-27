a4facb0a375b69f29836f462927710e4c3acf57a
Dima Davydenko <dmytro.davydenko89@gmail.com>
2022-03-27 20:57:15 +0300

    solution is implemented. Common mistakes fixed.

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -9,46 +9,48 @@ import java.io.IOException;
 
 public class WorkWithFile {
 
-    public void getStatistic(String fromFileName, String toFileName) {
-        int totalSupply = 0;
-        int totalBuy = 0;
-
+    private static String getDataFromFile(String fromFileName) {
         try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
-
             StringBuilder fileInfoLineBuilder = new StringBuilder();
             String lineFromFile = reader.readLine();
-
             while (lineFromFile != null) {
-                fileInfoLineBuilder.append(lineFromFile).append("/");
+                fileInfoLineBuilder.append(lineFromFile).append(System.lineSeparator());
                 lineFromFile = reader.readLine();
             }
-            String[] fileData = fileInfoLineBuilder.toString().split("/");
-
-            for (String data : fileData) {
-                if (data.indexOf('b') == 0) {
-                    totalBuy = totalBuy + Integer.parseInt(data.replaceAll("[^0-9]", ""));
-                } else {
-                    totalSupply = totalSupply + Integer.parseInt(data.replaceAll("[^0-9]", ""));
-                }
-            }
-            int result = totalSupply - totalBuy;
-            StringBuilder resultDataLineBuilder = new StringBuilder();
-            String resultData = resultDataLineBuilder.append("supply,")
-                    .append(totalSupply).append(System.lineSeparator())
-                    .append("buy,")
-                    .append(totalBuy).append(System.lineSeparator())
-                    .append("result,")
-                    .append(result).toString();
-
-            File file = new File(toFileName);
-
-            try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
-                bufferedWriter.write(resultData);
-            } catch (IOException e) {
-                throw new RuntimeException("Can't write data to File", e);
-            }
+            return fileInfoLineBuilder.toString();
         } catch (IOException e) {
             throw new RuntimeException("Can't find File", e);
+        }
+    }
+
+    private static String getReportFromFile(String fromFileName) {
+        int totalSupply = 0;
+        int totalBuy = 0;
+        String[] fileData = getDataFromFile(fromFileName).split(System.lineSeparator());
+        for (String data : fileData) {
+            if (data.indexOf('b') == 0) {
+                totalBuy = totalBuy + Integer.parseInt(data.replaceAll("[^0-9]", ""));
+            } else {
+                totalSupply = totalSupply + Integer.parseInt(data.replaceAll("[^0-9]", ""));
+            }
+        }
+        int result = totalSupply - totalBuy;
+        StringBuilder resultDataLineBuilder = new StringBuilder();
+        String resultData = resultDataLineBuilder.append("supply,")
+                .append(totalSupply).append(System.lineSeparator())
+                .append("buy,")
+                .append(totalBuy).append(System.lineSeparator())
+                .append("result,")
+                .append(result).toString();
+        return resultData;
+    }
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        File file = new File(toFileName);
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
+            bufferedWriter.write(getReportFromFile(fromFileName));
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write data to File", e);
         }
     }
 }
