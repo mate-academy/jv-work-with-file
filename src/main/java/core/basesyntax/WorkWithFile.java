c17a80e239174758e9fca80a079285f56555a467
Dmytro Koliada <dmitrikstar@gmail.com>
2022-03-28 18:16:30 +0300

    added helper method

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,6 +8,25 @@ import java.io.IOException;
 
 public class WorkWithFile {
 
+    public String [] calculating(StringBuilder builder) {
+        String [] separateLine = builder.toString().split(System.lineSeparator());
+        int [] result = new int[] {0, 0};
+        for (String str : separateLine) {
+            String[] amounts = str.split(",");
+            if (amounts[0].equals("supply")) {
+                result[0] += Integer.parseInt(amounts[1]);
+            } else {
+                result[1] += Integer.parseInt(amounts[1]);
+            }
+        }
+        String[] operationType = new String[] {"supply,", "buy,", "result,"};
+        String[] spreadsheet = new String[3];
+        spreadsheet [0] = operationType[0] + result[0];
+        spreadsheet [1] = System.lineSeparator() + operationType[1] + result[1];
+        spreadsheet [2] = System.lineSeparator() + operationType[2] + (result[0] - result[1]);
+        return spreadsheet;
+    }
+
     public void getStatistic(String fromFileName, String toFileName) {
         try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName));
@@ -17,22 +36,7 @@ public class WorkWithFile {
                 builder.append(value).append(System.lineSeparator());
                 value = bufferedReader.readLine();
             }
-            String[] separateLine = builder.toString().split(System.lineSeparator());
-            int [] result = new int[] {0, 0};
-            for (String str : separateLine) {
-                String[] ammount = str.split(",");
-                if (ammount[0].equals("supply")) {
-                    result[0] += Integer.parseInt(ammount[1]);
-                } else {
-                    result[1] += Integer.parseInt(ammount[1]);
-                }
-            }
-            String[] operationType = new String[] {"supply,", "buy,", "result,"};
-            String[] spreadsheet = new String[3];
-            spreadsheet [0] = operationType[0] + result[0];
-            spreadsheet [1] = System.lineSeparator() + operationType[1] + result[1];
-            spreadsheet [2] = System.lineSeparator() + operationType[2] + (result[0] - result[1]);
-            for (String finishSpreadsheet : spreadsheet) {
+            for (String finishSpreadsheet : calculating(builder)) {
                 try (BufferedWriter bufferedWriter = new BufferedWriter(
                         new FileWriter(toFileName, true))) {
                     bufferedWriter.write(finishSpreadsheet);
