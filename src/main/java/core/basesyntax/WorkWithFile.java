c4d7ca8e064362ec58ffba2cdd8da0042882c35c
GerasimovTA-92 <olexandr.gerasimov.02@gmail.com>
2022-03-28 15:57:17 +0300

    added methods

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,28 +8,42 @@ import java.nio.file.Files;
 import java.util.List;
 
 public class WorkWithFile {
+    private static final int INDEX_OF_CATEGORY = 0;
+    private static final int INDEX_OF_VALUE = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
         File baseFile = new File(fromFileName);
         File reportFile = new File(toFileName);
-        int sumOfSupply = 0;
-        int sumOfBuy = 0;
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(reportFile, true))) {
             List<String> data = Files.readAllLines(baseFile.toPath());
-            for (String value : data) {
-                String[] values = value.split(",");
-                if (values[0].equals("supply")) {
-                    sumOfSupply += Integer.parseInt(values[1]);
-                }
-                if (values[0].equals("buy")) {
-                    sumOfBuy += Integer.parseInt(values[1]);
-                }
-            }
-            writer.write("supply," + sumOfSupply + System.lineSeparator());
-            writer.write("buy," + sumOfBuy + System.lineSeparator());
-            writer.write("result," + (sumOfSupply - sumOfBuy));
+            writer.write(makeReport("supply","buy",data));
         } catch (IOException e) {
             throw new RuntimeException("Can't read/write data");
         }
+    }
+
+    private int getCalculate(List<String> data, String category) {
+        int sum = 0;
+        for (String value : data) {
+            String[] values = value.split(",");
+            if (values[INDEX_OF_CATEGORY].equals(category)) {
+                sum += Integer.parseInt(values[INDEX_OF_VALUE]);
+            }
+        }
+        return sum;
+    }
+
+    private String makeReport(String supply, String buy, List<String> data) {
+        StringBuilder report = new StringBuilder(supply)
+                .append(",")
+                .append(getCalculate(data, supply))
+                .append(System.lineSeparator())
+                .append(buy)
+                .append(",")
+                .append(getCalculate(data,buy))
+                .append(System.lineSeparator())
+                .append("result,")
+                .append(getCalculate(data, supply) - getCalculate(data,buy));
+        return report.toString();
     }
 }
