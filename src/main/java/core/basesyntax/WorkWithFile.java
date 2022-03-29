5dbd5fe51f2c31ac55672f98d1b8d1bf242bc5dd
Lyudmila D <dunda94lyudmila@gmail.com>
2022-03-29 10:11:26 +0300

    renamed variables

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,13 +7,9 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    private StringBuilder stringBuilder;
-
-    public WorkWithFile() {
-        stringBuilder = new StringBuilder();
-    }
 
     private String readFromFile(String fromFileName) {
+        StringBuilder stringBuilder = new StringBuilder();
         try (BufferedReader newReader = new BufferedReader(new FileReader(fromFileName))) {
             String value = newReader.readLine();
             while (value != null) {
@@ -30,28 +26,26 @@ public class WorkWithFile {
         int supply = 0;
         int buy = 0;
         String[] splittedLine = readFromFile(fromFileName).split("\\W+");
-
         for (int i = 0; i < splittedLine.length - 1; i += 2) {
-            int data = Integer.parseInt(splittedLine[i + 1]);
+            int quantity = Integer.parseInt(splittedLine[i + 1]);
             if (splittedLine[i].equals("supply")) {
-                supply += data;
+                supply += quantity;
             } else {
-                buy += data;
+                buy += quantity;
             }
         }
         int result = supply - buy;
-        stringBuilder = new StringBuilder();
-        stringBuilder.append("supply,").append(String.valueOf(supply))
-                .append(System.lineSeparator()).append("buy,").append(String.valueOf(buy))
-                .append(System.lineSeparator()).append("result,").append(String.valueOf(result));
-        return stringBuilder.toString();
+        StringBuilder reportBuilder = new StringBuilder();
+        reportBuilder.append("supply,").append(supply)
+                .append(System.lineSeparator()).append("buy,").append(buy)
+                .append(System.lineSeparator()).append("result,").append(result);
+        return reportBuilder.toString();
     }
 
     public void getStatistic(String fromFileName, String toFileName) {
-        String resultData = makeReport(fromFileName);
-
+        String report = makeReport(fromFileName);
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName, true))) {
-            writer.write(resultData);
+            writer.write(report);
             writer.flush();
         } catch (IOException e) {
             throw new RuntimeException("Can't write the data to the file" + toFileName, e);
