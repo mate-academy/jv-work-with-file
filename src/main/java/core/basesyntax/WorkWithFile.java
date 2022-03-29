dee97ddfaf3142f089bc2f95f7dcaaa4bade477c
Dmytro Skrypnyk <skripnik139@gmail.com>
2022-03-29 11:22:11 +0200

    Refactor the code and add new methods

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -4,23 +4,27 @@ import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvValidationException;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    private static final int CHECK_POINT = 0;
-    private static final int COUNT_POINT = 1;
-    private static final String SUPPLY_LYNE = "supply,";
-    private static final String BUY_LYNE = "buy,";
-    private static final String RESULT_LYNE = "result,";
+    private static final int FIRST_POINT = 0;
+    private static final int SECOND_POINT = 1;
+    private static final String SUPPLY_LINE = "supply";
+    private static final String BUY_LINE = "buy";
+    private static final String RESULT_LINE = "result";
+    private static final String COMA = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        int supplyCount = 0;
-        int buyCount = 0;
-        try {
-            CSVReader csvReader = new CSVReader(new FileReader(fromFileName));
+        String value = getDataFromFile(fromFileName);
+        String sentence = dataCalculation(value);
+        writeToFile(sentence, toFileName);
+    }
+
+    private String getDataFromFile(String fromFileName) {
+        StringBuilder stringBuilder = new StringBuilder();
+        try (CSVReader csvReader = new CSVReader(new FileReader(fromFileName))) {
             String[] nextLine;
             while (true) {
                 try {
@@ -30,29 +34,47 @@ public class WorkWithFile {
                 } catch (IOException | CsvValidationException e) {
                     throw new RuntimeException(e);
                 }
-                if (nextLine[CHECK_POINT].equals("supply")) {
-                    supplyCount += Integer.parseInt(nextLine[COUNT_POINT]);
-                } else {
-                    buyCount += Integer.parseInt(nextLine[COUNT_POINT]);
-                }
+                stringBuilder.append(nextLine[FIRST_POINT])
+                        .append(",")
+                        .append(nextLine[SECOND_POINT])
+                        .append(" ");
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read data from file", e);
         }
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(SUPPLY_LYNE)
+        return stringBuilder.toString().trim();
+    }
+
+    private String dataCalculation(String value) {
+        int supplyCount = 0;
+        int buyCount = 0;
+        StringBuilder calculatorResult = new StringBuilder();
+        String[] array = value.split(" ");
+        for (String s : array) {
+            String[] split = s.split(",");
+            if (split[FIRST_POINT].equals(SUPPLY_LINE)) {
+                supplyCount += Integer.parseInt(split[SECOND_POINT]);
+            } else {
+                buyCount += Integer.parseInt(split[SECOND_POINT]);
+            }
+        }
+        calculatorResult.append(SUPPLY_LINE + COMA)
                 .append(supplyCount)
                 .append(System.lineSeparator())
-                .append(BUY_LYNE)
+                .append(BUY_LINE + COMA)
                 .append(buyCount)
                 .append(System.lineSeparator())
-                .append(RESULT_LYNE)
+                .append(RESULT_LINE + COMA)
                 .append(supplyCount - buyCount);
+        return calculatorResult.toString();
+    }
+
+    private void writeToFile(String statistic, String toFileName) {
         File file = new File(toFileName);
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
-            bufferedWriter.write(stringBuilder.toString());
+            bufferedWriter.write(statistic);
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException("Can't write data to file", e);
         }
     }
 }
