8d3a2aeea2b7136ba919e7cc7fb231cafdf2b0f6
Vlad Nadtochiy <v20102002n@gmail.com>
2022-03-30 14:48:05 +0300

    split method into 3 parts

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -12,13 +12,10 @@ public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
         File file1 = new File(fromFileName);
         File file2 = new File(toFileName);
-        try {
-            file2.createNewFile();
-        } catch (IOException e) {
-            throw new RuntimeException("Cant create file",e);
-        }
         int supply = 0;
         int buy = 0;
+
+        //1st part: reading from file
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file1));) {
             String string = bufferedReader.readLine();
             while (string != null) {
@@ -32,8 +29,13 @@ public class WorkWithFile {
         } catch (IOException e) {
             throw new RuntimeException("Some problems", e);
         }
+
+        //2nd part: making report
         int result = supply - buy;
+
+        //3d part: create file and write to it
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file2, true))) {
+            file2.createNewFile();
             bufferedWriter.write("supply," + supply + System.lineSeparator()
                     + "buy," + buy + System.lineSeparator() + "result," + result);
         } catch (IOException e) {
