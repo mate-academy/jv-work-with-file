c6c32fee7396a9b314087c4f2e62e530f88bdd40
Fantom5511 <mironovanton5511@gmail.com>
2022-03-30 17:09:46 +0300

    finaly sucessfull done

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,8 +1,58 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WorkWithFile {
 
     public void getStatistic(String fromFileName, String toFileName) {
+        String resultOfRead = readFromFile(fromFileName);
+        int supply = 0;
+        int buy = 0;
+        String[] split = resultOfRead.split(System.lineSeparator());
+        for (String each : split) {
+            String[] commaArray = new String[2];
+            commaArray = each.split(",");
+            if (commaArray[0].equals("supply")) {
+                supply += Integer.parseInt(commaArray[1]);
+            } else {
+                buy += Integer.parseInt(commaArray[1]);
+            }
+        }
+        StringBuilder reportBuilder = new StringBuilder();
+        reportBuilder.append("supply,").append(supply).append(System.lineSeparator())
+                .append("buy,").append(buy).append(System.lineSeparator())
+                .append("result,").append(supply - buy);
+        String resultStat = reportBuilder.toString();
+        write(toFileName, resultStat);
+    }
 
+    private String readFromFile(String fromFileName) {
+        File file = new File(fromFileName);
+        StringBuilder builder = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String value = reader.readLine();
+            while (value != null) {
+                builder.append(value).append(System.lineSeparator());
+                value = reader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read the file" + fromFileName, e);
+        }
+        return builder.toString();
+    }
+
+    private void write(String toFileName, String resultStat) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
+            writer.write(resultStat);
+        } catch (IOException e) {
+            throw new RuntimeException("Something is going wrong", e);
+        }
     }
 }
+
+
