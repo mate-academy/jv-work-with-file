b05dcb8e457e4a5702bcef551b1755ca1734fd0f
dombrovskiy2850@gmail.com <dombrovskiy2850@gmail.com>
2022-03-30 11:49:58 +0300

    fixed bugs - create private methods for different parts of logic (reading, calculating, writing), rename variables

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,36 +8,51 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    public static void getStatistic(String fromFileName, String toFileName) {
-        int supply = 0;
-        int buy = 0;
-        File file1 = new File(fromFileName);
-        StringBuilder stringBuilder1 = new StringBuilder();
-        try (BufferedReader reader = new BufferedReader(new FileReader(file1))) {
+    private int supply;
+    private int buy;
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        WorkWithFile workFile = new WorkWithFile();
+        StringBuilder resultOfread = workFile.read(fromFileName);
+        workFile.calculate(resultOfread);
+        workFile.write(toFileName);
+    }
+
+    private StringBuilder read(String fromFileName) {
+        File readFile = new File(fromFileName);
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(readFile))) {
             String value = reader.readLine();
             while (value != null) {
-                stringBuilder1.append(value).append(System.lineSeparator());
+                stringBuilder.append(value).append(System.lineSeparator());
                 value = reader.readLine();
             }
         } catch (IOException e) {
             throw new RuntimeException("Can't read file", e);
         }
-        String[] strings = stringBuilder1.toString().split(System.lineSeparator());
-        String[] temp = new String[2];
+        return stringBuilder;
+    }
+
+    private void calculate(StringBuilder stringBuilder) {
+        String[] strings = stringBuilder.toString().split(System.lineSeparator());
+        String[] arraySpliter = new String[2];
         for (String string : strings) {
-            temp = string.split(",");
-            if (temp[0].equals("supply")) {
-                supply += Integer.parseInt(temp[1]);
+            arraySpliter = string.split(",");
+            if (arraySpliter[0].equals("supply")) {
+                supply += Integer.parseInt(arraySpliter[1]);
             } else {
-                buy += Integer.parseInt(temp[1]);
+                buy += Integer.parseInt(arraySpliter[1]);
             }
         }
-        StringBuilder stringBuilder2 = new StringBuilder();
-        stringBuilder2.append("supply,").append(supply).append(System.lineSeparator())
+    }
+
+    private void write(String toFileName) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("supply,").append(supply).append(System.lineSeparator())
                 .append("buy,").append(buy).append(System.lineSeparator())
                 .append("result,").append(supply - buy);
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
-            writer.write(stringBuilder2.toString());
+            writer.write(stringBuilder.toString());
         } catch (IOException e) {
             throw new RuntimeException("Can't read file", e);
         }
