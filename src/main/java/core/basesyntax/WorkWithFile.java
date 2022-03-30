437cc90eca8b0393b54a57221313c8f363e2d792
Vladyslav Fomenko <with.god.victorius@gmail.com>
2022-03-30 16:29:50 +0300

    implemented different methods aproach, renamed variables appropriately

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,32 +8,39 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-
     public void getStatistic(String fromFileName, String toFileName) {
+        writeToFile(getCalculated(getFromFile(fromFileName)), toFileName);
+    }
+
+    private String getFromFile(String fromFileName) {
         File myFile = new File(fromFileName);
-        StringBuilder builder = new StringBuilder();
-        StringBuilder calculation = new StringBuilder();
+        StringBuilder dataFromFile = new StringBuilder();
         try {
             FileReader fileReader = new FileReader(myFile);
             BufferedReader reader = new BufferedReader(fileReader);
             String value = reader.readLine();
             while (value != null) {
-                builder.append(value).append(" ");
+                dataFromFile.append(value).append(" ");
                 value = reader.readLine();
             }
         } catch (IOException e) {
             throw new RuntimeException("", e);
         }
-        String[] split1 = builder.toString().split(" ");
-        String[] firstE = new String[] {"supply", "buy"};
+        return dataFromFile.toString();
+    }
+
+    private String getCalculated(String builder) {
+        StringBuilder calculation = new StringBuilder();
+        String[] splitData = builder.split(" ");
+        String[] fieldsToCalc = new String[]{"supply", "buy"};
         int buy = 0;
         int supply = 0;
-        for (String name : firstE) {
+        for (String name : fieldsToCalc) {
             int sumAmount = 0;
-            for (int i = 0; i < split1.length; i++) {
-                String[] tmp = split1[i].split(",");
-                if (name.equals(tmp[0])) {
-                    sumAmount += Integer.parseInt(tmp[1]);
+            for (int i = 0; i < splitData.length; i++) {
+                String[] temp = splitData[i].split(",");
+                if (name.equals(temp[0])) {
+                    sumAmount += Integer.parseInt(temp[1]);
                 }
             }
             if (name.equals("buy")) {
@@ -44,8 +51,12 @@ public class WorkWithFile {
             calculation.append(name).append(",").append(sumAmount).append(System.lineSeparator());
         }
         calculation.append("result").append(",").append(supply - buy);
+        return calculation.toString();
+    }
+
+    private void writeToFile(String calculation, String toFileName) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
-            bufferedWriter.write(calculation.toString());
+            bufferedWriter.write(calculation);
         } catch (IOException e) {
             throw new RuntimeException("Can`t write to file", e);
         }
