3e5a255f4f3b904af1e1bcdcc0f5f6369f652cc1
GrabenkoVitaliy <grabenkovitaliy7@gmail.com>
2022-03-31 09:40:34 +0300

    mistakes have been fixed

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -6,10 +6,8 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Arrays;
 
 public class WorkWithFile {
-
     public void getStatistic(String fromFileName, String toFileName) {
         StringBuilder stringBuilder = new StringBuilder();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
@@ -22,16 +20,15 @@ public class WorkWithFile {
             throw new RuntimeException("Can`t read data from file", e);
         }
         String[] array = stringBuilder.toString().split(" ");
-        Arrays.sort(array);
         int supplyCount = 0;
         int buyCount = 0;
-        for (String str : array) {
-            String[] temp = str.split(",");
-            if (temp[0].equals("supply")) {
-                supplyCount += Integer.parseInt(temp[1]);
+        for (String element : array) {
+            String[] split = element.split(",");
+            if (split[0].equals("supply")) {
+                supplyCount += Integer.parseInt(split[1]);
             }
-            if (temp[0].equals("buy")) {
-                buyCount += Integer.parseInt(temp[1]);
+            if (split[0].equals("buy")) {
+                buyCount += Integer.parseInt(split[1]);
             }
         }
         File file = new File(toFileName);
@@ -41,7 +38,7 @@ public class WorkWithFile {
             throw new RuntimeException("Can`t create file", e);
         }
         stringBuilder = new StringBuilder();
-        String[] writer = {stringBuilder.append("supply")
+        String[] report = {stringBuilder.append("supply")
                 .append(",")
                 .append(supplyCount).toString(),
                 stringBuilder.append(System.lineSeparator())
@@ -52,9 +49,9 @@ public class WorkWithFile {
                         .append("result")
                         .append(",")
                         .append(supplyCount - buyCount).toString()};
-        for (String write : writer) {
+        for (String line : report) {
             try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
-                bufferedWriter.write(write);
+                bufferedWriter.write(line);
                 bufferedWriter.flush();
             } catch (IOException e) {
                 throw new RuntimeException("Can`t write data to file", e);
