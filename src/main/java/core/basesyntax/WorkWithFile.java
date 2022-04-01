ec274337630ac0af839caa0363117ab9f4585bd5
Vlad Nadtochiy <v20102002n@gmail.com>
2022-04-01 12:31:15 +0300

    commit

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -10,13 +10,13 @@ import java.io.IOException;
 public class WorkWithFile {
 
     public void getStatistic(String fromFileName, String toFileName) {
-        writer(toFileName, calculator(reader(fromFileName).split(" ")));
+        writer(toFileName, calculator(read(fromFileName)));
     }
 
-    public String reader(String fromFileName) {
-        File file1 = new File(fromFileName);
+    public String read(String fromFileName) {
+        File file = new File(fromFileName);
         StringBuilder data = new StringBuilder();
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file1));) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file));) {
             String string = bufferedReader.readLine();
             while (string != null) {
                 data.append(string).append(" ");
@@ -28,31 +28,28 @@ public class WorkWithFile {
         return data.toString();
     }
 
-    public int[] calculator(String[] str) {
-        int[] answer = new int[3];
-        // answer[0] - supply
-        // answer[1] - buy
-        // answer[2] - result
-        for (String data : str) {
-            if (data.contains("supply")) {
-                answer[0] += Integer.parseInt(data.trim().substring(7));
-            } else if (data.contains("buy")) {
-                answer[1] += Integer.parseInt(data.trim().substring(4));
+    public int[] calculator(String str) {
+        int[] result = new int[3];
+        String[] lines = str.split(" ");
+        for (String line : lines) {
+            if (line.contains("supply")) {
+                result[0] += Integer.parseInt(line.trim().substring(7));
+            } else if (line.contains("buy")) {
+                result[1] += Integer.parseInt(line.trim().substring(4));
             }
         }
-        answer[2] = answer[0] - answer[1];
-        return answer;
+        result[2] = result[0] - result[1];
+        return result;
     }
 
-    public void writer(String toFileName, int[] answer) {
-        // answer[0] - supply
-        // answer[1] - buy
-        // answer[2] - result
-        File file2 = new File(toFileName);
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file2, true))) {
-            file2.createNewFile();
-            bufferedWriter.write("supply," + answer[0] + System.lineSeparator()
-                    + "buy," + answer[1] + System.lineSeparator() + "result," + answer[2]);
+    public void writer(String toFileName, int[] result) {
+        File file = new File(toFileName);
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file, true))) {
+            file.createNewFile();
+            StringBuilder stringBuilder = new StringBuilder();
+            bufferedWriter.write(String.valueOf(stringBuilder.append("supply,").append(result[0])
+                    .append(System.lineSeparator()).append("buy,").append(result[1])
+                    .append(System.lineSeparator()).append("result,").append(result[2])));
         } catch (IOException e) {
             throw new RuntimeException("Some problems", e);
         }
