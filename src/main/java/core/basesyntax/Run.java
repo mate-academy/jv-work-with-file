8da0bf9dba44173ed3933345caed163f4ec0b1da
agash <alexgash8@gmail.com>
2022-04-05 18:51:55 +0300

    done from 1st try

---
+++ src/main/java/core/basesyntax/Run.java
@@ -0,0 +1,8 @@
+package core.basesyntax;
+
+public class Run {
+    public static void main(String[] args) {
+//        new WorkWithFile().getStatistic("apple.csv", "result.csv");
+        WorkWithFile.getStatistic("banana.csv", "result2.csv");
+    }
+}
--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,8 +1,49 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+
 public class WorkWithFile {
 
-    public void getStatistic(String fromFileName, String toFileName) {
+    public static void getStatistic(String fromFileName, String toFileName) {
+        File file = new File(fromFileName);
+        int buySum = 0;
+        int supplySum = 0;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
 
+            String value = reader.readLine();
+
+            while (value != null) {
+                String[] tempSplit = value.split(",");
+                int tempValue = Integer.parseInt(tempSplit[1]);
+                if (tempSplit[0].equals("buy")) {
+                    buySum += tempValue;
+                } else {
+                    supplySum += tempValue;
+                }
+                value = reader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("cant", e);
+        }
+        int result = supplySum - buySum;
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("buy,").append(buySum).append(System.lineSeparator());
+        stringBuilder.append("supply,").append(supplySum).append(System.lineSeparator());
+        stringBuilder.append("result,").append(result);
+
+        File resultFile = new File(toFileName);
+        try {
+            Files.write(resultFile.toPath(),
+                    stringBuilder.toString().getBytes(StandardCharsets.UTF_8));
+        } catch (Exception e) {
+            throw new RuntimeException("cant write to file", e);
+        }
     }
 }
