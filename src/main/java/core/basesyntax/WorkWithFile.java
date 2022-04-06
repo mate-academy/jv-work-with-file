88cd0e294e76fd493ee7c6db73d9a484b9a98ed3
SerhiiPankov <pankoff.serg@gmail.com>
2022-04-06 15:40:00 +0300

    fixed informative message and divide program logic for jv-work-with-file

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -9,28 +9,38 @@ public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
         File fromFile = new File(fromFileName);
         File toFile = new File(toFileName);
+        String[] strings;
         try {
-            String[] strings = Files.readAllLines(fromFile.toPath()).toArray(new String[0]);
-            int buy = 0;
-            int supply = 0;
-            for (String string: strings) {
-                if (string.split(",")[0].equals("buy")) {
-                    buy = buy + Integer.parseInt(string.split(",")[1]);
-                }
-                if (string.split(",")[0].equals("supply")) {
-                    supply = supply + Integer.parseInt(string.split(",")[1]);
-                }
-            }
-            int result = supply - buy;
-            String[] outputString = new String[] {"supply," + supply + System.lineSeparator(),
-                    "buy," + buy + System.lineSeparator(),
-                    "result," + result};
+            strings = Files.readAllLines(fromFile.toPath()).toArray(new String[0]);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read data from the file " + fromFileName, e);
+        }
+        try {
             toFile.createNewFile();
+            WorkWithFile workWithFile = new WorkWithFile();
+            String[] outputString = workWithFile.outputString(strings);
             for (String string: outputString) {
                 Files.writeString(toFile.toPath(),string, StandardOpenOption.APPEND);
             }
         } catch (IOException e) {
-            throw new RuntimeException("Can'n work with file");
+            throw new RuntimeException("Can't write data to the file " + toFileName, e);
         }
+    }
+
+    private String[] outputString(String[] strings) {
+        int buy = 0;
+        int supply = 0;
+        for (String string: strings) {
+            if (string.split(",")[0].equals("buy")) {
+                buy = buy + Integer.parseInt(string.split(",")[1]);
+            }
+            if (string.split(",")[0].equals("supply")) {
+                supply = supply + Integer.parseInt(string.split(",")[1]);
+            }
+        }
+        int result = supply - buy;
+        return new String[] {"supply," + supply + System.lineSeparator(),
+                "buy," + buy + System.lineSeparator(),
+                "result," + result};
     }
 }
