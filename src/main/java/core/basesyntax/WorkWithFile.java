5498fd8ba175b518fce9954bd3fdc48f42a1db85
Roma Yarishkin <romarijkov@gmail.com>
2022-04-06 12:46:41 +0300

    Task complete finally

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -9,26 +9,7 @@ import java.io.IOException;
 
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
-        int supply = 0;
-        int buy = 0;
-        String resultRead = readFrom(fromFileName);
-
-        String[] lines = resultRead.split(System.lineSeparator());
-        for (String line : lines) {
-            String[] arraySplit = line.split(",");
-            if (arraySplit[0].equals("supply")) {
-                supply += Integer.parseInt(arraySplit[1]);
-            } else {
-                buy += Integer.parseInt(arraySplit[1]);
-            }
-            StringBuilder resultBuilder = new StringBuilder();
-            resultBuilder.append("supply").append(",").append(supply).append(System.lineSeparator())
-                    .append("buy").append(",").append(buy).append(System.lineSeparator())
-                    .append("result").append(",").append(supply - buy)
-                    .append(System.lineSeparator());
-            String reportStatistic = resultBuilder.toString();
-            writeToFile(toFileName, reportStatistic);
-        }
+        writeToFile(calculateStatistic(readFrom(fromFileName)), toFileName);
     }
 
     private String readFrom(String fromFileName) {
@@ -41,16 +22,39 @@ public class WorkWithFile {
                 read = readReader.readLine();
             }
         } catch (IOException e) {
-            throw new RuntimeException("Can't read the file", e);
+            throw new RuntimeException("Can't read the file " + fromFileName, e);
         }
         return readBuilder.toString();
     }
 
-    private void writeToFile(String toFileName, String reportStatistic) {
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
-            bufferedWriter.write(reportStatistic);
-        } catch (IOException e) {
-            throw new RuntimeException("Can't write file", e);
+    private String[] calculateStatistic(String read) {
+        int supply = 0;
+        int buy = 0;
+        String[] lines = read.split(System.lineSeparator());
+        for (String line : lines) {
+            String[] arraySplit = line.split(",");
+            if (arraySplit[0].equals("supply")) {
+                supply += Integer.parseInt(arraySplit[1]);
+            } else {
+                buy += Integer.parseInt(arraySplit[1]);
+            }
+        }
+        StringBuilder resultBuilder = new StringBuilder();
+        return new String[]{resultBuilder.append("supply").append(",").append(supply)
+                .toString(),
+                resultBuilder.append(System.lineSeparator()).append("buy").append(",").append(buy)
+                        .toString(),
+                resultBuilder.append(System.lineSeparator()).append("result").append(",")
+                        .append(supply - buy).toString()};
+    }
+
+    private void writeToFile(String[] reportStatistic, String toFileName) {
+        for (String reportStatistics : reportStatistic) {
+            try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+                bufferedWriter.write(reportStatistics);
+            } catch (IOException e) {
+                throw new RuntimeException("Can't write data to file " + toFileName, e);
+            }
         }
     }
 }
