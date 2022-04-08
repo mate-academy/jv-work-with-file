ea0887fe8dcc72fc4018e4a28e716d8bfe5cdc5a
agash <alexgash8@gmail.com>
2022-04-08 16:01:54 +0300

    and now without *

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 
