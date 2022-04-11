4ae56ea04c177aeb73899afde941bdf0e82c534d
Alex <alexoriobel@gmail.com>
2022-04-11 17:09:59 +0300

    after Re-review

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,41 +1,57 @@
 package core.basesyntax;
 
-import core.basesyntax.calculation.CalculationStatistics;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class WorkWithFile {
-    private ArrayList<String> str = new ArrayList<>();
-    private CalculationStatistics calculation = new CalculationStatistics();
 
     public void getStatistic(String fromFileName, String toFileName) {
-        readFile(fromFileName);
-        writeFile(toFileName);
+        writeFile(toFileName, readFile(fromFileName));
     }
 
-    private void readFile(String fileNameRead) {
+    private List readFile(String fileNameRead) {
+        List<String> str = new ArrayList<>();
         String text;
         try (FileReader fileFrom = new FileReader(fileNameRead)) {
             BufferedReader bufferedReader = new BufferedReader(fileFrom);
             while ((text = bufferedReader.readLine()) != null) {
                 str.add(text);
             }
-            str.trimToSize();
         } catch (IOException e) {
             throw new RuntimeException("Can't read data from the file " + fileNameRead, e);
         }
+        return str;
     }
 
-    private void writeFile(String fileNameWrite) {
+    private void writeFile(String fileNameWrite, List<String> str) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileNameWrite))) {
-            bufferedWriter.write(calculation.getNumber(str));
+            bufferedWriter.write(getNumber(str));
             bufferedWriter.flush();
         } catch (IOException e) {
             throw new RuntimeException("Can't write data to the file " + fileNameWrite, e);
         }
+    }
+
+    public String getNumber(List<String> str) {
+        int supplyOperation = 0;
+        int buyOperation = 0;
+        for (String line : str) {
+            String[] splitFile = line.split(",");
+            if (splitFile[0].equals("buy")) {
+                buyOperation += Integer.parseInt(splitFile[1]);
+            }
+            if (splitFile[0].equals("supply")) {
+                supplyOperation += Integer.parseInt(splitFile[1]);
+            }
+        }
+        StringBuilder b = new StringBuilder();
+        return b.append("supply,").append(supplyOperation).append(System.lineSeparator())
+                .append("buy,").append(buyOperation).append(System.lineSeparator())
+                .append("result,").append(supplyOperation - buyOperation).toString();
     }
 }
