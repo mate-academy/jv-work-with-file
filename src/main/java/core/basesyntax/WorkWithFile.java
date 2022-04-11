37d1ce1d5c3d8368a90387b1c6f4fd77cf1a15b3
dippp <Dipppp84@gmail.com>
2022-04-11 15:21:18 +0300

    fix 1

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -3,16 +3,24 @@ package core.basesyntax;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class WorkWithFile {
 
     public void getStatistic(String fromFileName, String toFileName) {
-        Map<String, Integer> mapCSV = readingCSV(Path.of(fromFileName));
+        Map<String, Integer> mapCsv;
+        try {
+            mapCsv = readingCsv(Path.of(fromFileName));
+        } catch (IOException e) {
+            throw new RuntimeException("Reading error", e);
+        }
         List<String> result = new ArrayList<>();
-        result.add("supply," + mapCSV.get("supply"));
-        result.add("buy," + mapCSV.get("buy"));
-        result.add("result," + (mapCSV.get("supply") - mapCSV.get("buy")));
+        result.add("supply," + mapCsv.get("supply"));
+        result.add("buy," + mapCsv.get("buy"));
+        result.add("result," + (mapCsv.get("supply") - mapCsv.get("buy")));
         try {
             Files.write(Path.of(toFileName), result);
         } catch (IOException e) {
@@ -20,19 +28,15 @@ public class WorkWithFile {
         }
     }
 
-    private Map<String, Integer> readingCSV(Path fromPath) {
-        List<String> list = null;
-        try {
-            list = Files.readAllLines(fromPath);
-        } catch (IOException e) {
-            throw new RuntimeException("Reading error", e);
-        }
-        Map<String, Integer> mapCSV = new HashMap<>();
-        for (String s : list) {
+    private Map<String, Integer> readingCsv(Path fromPath) throws IOException {
+        List<String> lines = Files.readAllLines(fromPath);
+        Map<String, Integer> mapCsv = new HashMap<>();
+        for (String s : lines) {
             String[] split = s.split(",");
             Integer value = Integer.parseInt(split[1]);
-            mapCSV.put(split[0], mapCSV.containsKey(split[0]) ? mapCSV.get(split[0]) + value : value);
+            mapCsv.put(split[0], mapCsv.containsKey(split[0]) ?
+                    mapCsv.get(split[0]) + value : value);
         }
-        return mapCSV;
+        return mapCsv;
     }
 }
