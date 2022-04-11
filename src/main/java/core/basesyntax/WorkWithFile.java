2f7f5f88fdf0f2366c0c49402bd50f2b00b2dd71
dippp <Dipppp84@gmail.com>
2022-04-11 23:31:35 +0300

    my decision 2: по советам ментораÐ

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,43 +1,64 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class WorkWithFile {
+    private static final int ZERO_INDEX = 0;
+    private static final int FIRST_INDEX = 1;
+    private static final String[] SUPPLY_AND_BUY = new String[]{"supply", "buy"};
+
     public void getStatistic(String fromFileName, String toFileName) {
-        List<String> lines;
-        try {
-            //1. read from file, return List
-            lines = Files.readAllLines(Path.of(fromFileName));
+        List<String> lines = readFile(fromFileName);
+        String report = getReport(lines);
+        writeToFile(toFileName, report);
+    }
+
+    private List<String> readFile(String fromFileName) {
+        List<String> lines = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
+            while (reader.ready()) {
+                lines.add(reader.readLine());
+            }
         } catch (IOException e) {
             throw new RuntimeException("Can't read data from the file " + fromFileName, e);
         }
-        //2. process List and form report, return report
-        List<String> report = getReport(lines);
-        try {
-            //3. write to file
-            Files.write(Path.of(toFileName), report);
+        return lines;
+    }
+
+    private void writeToFile(String toFileName, String report) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
+            writer.write(report);
         } catch (IOException e) {
             throw new RuntimeException("Can't write data to the file " + toFileName, e);
         }
     }
 
-    private List<String> getReport(List<String> lines) {
-        Map<String, Integer> map = new HashMap<>();
-        for (String s : lines) {
-            String[] split = s.split(",");
-            Integer value = Integer.parseInt(split[1]);
-            map.put(split[0], map.containsKey(split[0]) ? map.get(split[0]) + value : value);
+    private String getReport(List<String> lines) {
+        StringBuilder report = new StringBuilder();
+        int quantityOfSupplies = 0;
+        int quantityOfBuys = 0;
+        for (String supplyAndBuy : SUPPLY_AND_BUY) {
+            Integer quantity = 0;
+            for (String line : lines) {
+                String[] split = line.split(",");
+                if (supplyAndBuy.equals(split[ZERO_INDEX])) {
+                    quantity += Integer.parseInt(split[FIRST_INDEX]);
+                }
+            }
+            if (supplyAndBuy.equals(SUPPLY_AND_BUY[ZERO_INDEX])) {
+                quantityOfSupplies = quantity;
+            } else {
+                quantityOfBuys = quantity;
+            }
+            report.append(supplyAndBuy).append(",").append(quantity).append(System.lineSeparator());
         }
-        List<String> report = new ArrayList<>();
-        report.add("supply," + map.get("supply"));
-        report.add("buy," + map.get("buy"));
-        report.add("result," + (map.get("supply") - map.get("buy")));
-        return report;
+        report.append("result,").append(quantityOfSupplies - quantityOfBuys);
+        return report.toString();
     }
 }
