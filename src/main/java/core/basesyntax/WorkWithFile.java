3ae0c2f52900b45df575a55317548d3bd30f449d
Ihor Dmytrush <ihor.dm.a@gmail.com>
2022-04-14 16:08:38 +0300

    4th try: Was divided  into functions

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,7 +8,26 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
+    private int supply = 0;
+    private int buy = 0;
+
+    public void setSupply(int supply) {
+        this.supply = supply;
+    }
+
+    public void setBuy(int buy) {
+        this.buy = buy;
+    }
+
+    public int getSupply() {
+        return supply;
+    }
+
+    public int getBuy() {
+        return buy;
+    }
+
+    public String[] operations(String fromFileName) {
         try {
             BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
             StringBuilder stringBuilder = new StringBuilder();
@@ -17,25 +36,40 @@ public class WorkWithFile {
                 stringBuilder.append(row).append(";");
                 row = reader.readLine();
             }
-            String[] operationsData = stringBuilder.toString().split(";");
-            int supllyAmount = 0;
-            int buyAmount = 0;
-            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName));
-            for (String operation : operationsData) {
-                if (operation.substring(0,operation.indexOf(",")).equals("supply")) {
-                    supllyAmount += Integer.parseInt(operation.substring(7));
+            return stringBuilder.toString().split(";");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("Can't find the file " + fromFileName + "!", e);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read data from the file " + fromFileName + "!", e);
+        }
+    }
+
+    public void calculateStatistic(String[] operations) {
+        if (operations != null) {
+            setSupply(0);
+            setBuy(0);
+            for (String operation : operations) {
+                if (operation.substring(0, operation.indexOf(",")).equals("supply")) {
+                    setSupply(getSupply() + Integer.parseInt(operation.substring(7)));
                 } else {
-                    buyAmount += Integer.parseInt(operation.substring(4));
+                    setBuy(getBuy() + Integer.parseInt(operation.substring(4)));
                 }
             }
-            bufferedWriter.write("supply," + supllyAmount + System.lineSeparator()
-                    + "buy," + buyAmount + System.lineSeparator()
-                    + "result," + (supllyAmount - buyAmount));
+        } else {
+            throw new RuntimeException("No data to calculate is found!");
+        }
+    }
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        calculateStatistic(operations(fromFileName));
+        try {
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName));
+            bufferedWriter.write("supply," + getSupply() + System.lineSeparator()
+                                    + "buy," + getBuy() + System.lineSeparator()
+                                    + "result," + (getSupply() - getBuy()));
             bufferedWriter.close();
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException("File " + fromFileName + "is not founded!", e);
         } catch (IOException e) {
-            throw new RuntimeException("Can't read file " + fromFileName + "!", e);
+            throw new RuntimeException("Can't write to the file " + toFileName + "!", e);
         }
     }
 }
