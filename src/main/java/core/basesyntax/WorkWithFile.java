550d90883a4c8a4cd34d21cb22d4cb1d39ebbb66
Ihor Dmytrush <ihor.dm.a@gmail.com>
2022-04-15 10:16:31 +0300

    5th try: variables to local, set/get methods deleted, other mistakes fixed too

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,26 +8,17 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    private int supply = 0;
-    private int buy = 0;
-
-    public void setSupply(int supply) {
-        this.supply = supply;
+    public void getStatistic(String fromFileName, String toFileName) {
+        try {
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName));
+            bufferedWriter.write(calculateStatistic(readFromFile(fromFileName)));
+            bufferedWriter.close();
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write to the file " + toFileName + "!", e);
+        }
     }
 
-    public void setBuy(int buy) {
-        this.buy = buy;
-    }
-
-    public int getSupply() {
-        return supply;
-    }
-
-    public int getBuy() {
-        return buy;
-    }
-
-    public String[] operations(String fromFileName) {
+    public String[] readFromFile(String fromFileName) {
         try {
             BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
             StringBuilder stringBuilder = new StringBuilder();
@@ -44,32 +35,18 @@ public class WorkWithFile {
         }
     }
 
-    public void calculateStatistic(String[] operations) {
-        if (operations != null) {
-            setSupply(0);
-            setBuy(0);
-            for (String operation : operations) {
-                if (operation.substring(0, operation.indexOf(",")).equals("supply")) {
-                    setSupply(getSupply() + Integer.parseInt(operation.substring(7)));
-                } else {
-                    setBuy(getBuy() + Integer.parseInt(operation.substring(4)));
-                }
+    public String calculateStatistic(String[] operations) {
+        int supply = 0;
+        int buy = 0;
+        for (String operation : operations) {
+            if (operation.substring(0, operation.indexOf(",")).equals("supply")) {
+                supply += Integer.parseInt(operation.substring(7));
+            } else {
+                buy += Integer.parseInt(operation.substring(4));
             }
-        } else {
-            throw new RuntimeException("No data to calculate is found!");
         }
-    }
-
-    public void getStatistic(String fromFileName, String toFileName) {
-        calculateStatistic(operations(fromFileName));
-        try {
-            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName));
-            bufferedWriter.write("supply," + getSupply() + System.lineSeparator()
-                                    + "buy," + getBuy() + System.lineSeparator()
-                                    + "result," + (getSupply() - getBuy()));
-            bufferedWriter.close();
-        } catch (IOException e) {
-            throw new RuntimeException("Can't write to the file " + toFileName + "!", e);
-        }
+        return "supply," + supply + System.lineSeparator()
+                + "buy," + buy + System.lineSeparator()
+                + "result," + (supply - buy);
     }
 }
