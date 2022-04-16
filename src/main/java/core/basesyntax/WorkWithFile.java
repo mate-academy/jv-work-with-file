a74d8d340df05d22b8806e4f8e2c8a813848251c
l_osyash <los.nazar24@gmail.com>
2022-04-16 15:23:10 +0300

    Implemented method readFile.

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,8 +1,26 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class WorkWithFile {
 
     public void getStatistic(String fromFileName, String toFileName) {
+    }
 
+    public String readFile(String fromFileName) {
+        StringBuilder builder = new StringBuilder();
+        String string;
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))){
+            string = reader.readLine();
+            while (string!= null) {
+                builder.append(string).append(System.lineSeparator());
+                string = reader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read data from file", e);
+        }
+        return builder.toString();
     }
 }
