8798533ce1bc6b2e89fae0f708396badde200b16
Denys Skrinnik <skrinnikden2000@gmail.com>
2022-04-20 20:53:01 +0300

    + try with resourses + separated logic

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,18 +7,27 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class WorkWithFile {
+    private int supplyInt = 0;
+    private int buyInt = 0;
 
     public void getStatistic(String fromFileName, String toFileName) {
+        String[] textArray = fileTextToArray(fromFileName);
+        countSupplyAndBuy(textArray);
+        writeTextToFile(toFileName);
+    }
+
+    public String[] fileTextToArray(String fromFileName) {
         String text = "";
         try {
             text = Files.readString(Paths.get(fromFileName));
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Something wrong with the file to read" + e);
         }
-        String[] textMassive = text.split(System.lineSeparator());
-        int supplyInt = 0;
-        int buyInt = 0;
-        for (String element : textMassive) {
+        return text.split(System.lineSeparator());
+    }
+
+    public void countSupplyAndBuy(String[] textArray) {
+        for (String element : textArray) {
             String[] elementSplit = element.split(",");
             if (elementSplit[0].equals("supply")) {
                 supplyInt += Integer.parseInt(elementSplit[1]);
@@ -26,15 +35,15 @@ public class WorkWithFile {
                 buyInt += Integer.parseInt(elementSplit[1]);
             }
         }
-        try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName));
+    }
+
+    public void writeTextToFile(String toFileName) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
             writer.write(System.lineSeparator() + "supply," + supplyInt
                     + System.lineSeparator() + "buy," + buyInt
                     + System.lineSeparator() + "result," + (supplyInt - buyInt));
-            writer.close();
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Oops, something wrong with the file to write" + e);
         }
-
     }
 }
