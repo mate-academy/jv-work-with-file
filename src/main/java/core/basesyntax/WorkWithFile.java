bd9b5ea6093594af34d59830f495ccc8af58a384
Olesya Khalak <khalak.olesya.official@gmail.com>
2022-04-22 13:49:36 +0300

    Fixed

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,36 +7,39 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    private int supply = 0;
-    private int buy = 0;
-
     public void getStatistic(String fromFileName, String toFileName) {
-        readFromFile(fromFileName);
-        StringBuilder reportBuilder;
-        reportBuilder = new StringBuilder();
-        reportBuilder.append("supply,").append(supply).append(System.lineSeparator())
-                .append("buy,").append(buy).append(System.lineSeparator())
-                .append("result,").append(supply - buy)
-                .append(System.lineSeparator());
-        String report = reportBuilder.toString().trim();
+        int[] data = readFromFile(fromFileName);
+        String report = calcResult(data);
         writeToFile(report, toFileName);
     }
 
-    private void readFromFile(String fromFileName) {
+    private String calcResult(int[] data) {
+        StringBuilder reportBuilder;
+        reportBuilder = new StringBuilder();
+        reportBuilder.append("supply,").append(data[0]).append(System.lineSeparator())
+                .append("buy,").append(data[1]).append(System.lineSeparator())
+                .append("result,").append(data[0] - data[1])
+                .append(System.lineSeparator());
+        return reportBuilder.toString().trim();
+    }
+
+    private int[] readFromFile(String fromFileName) {
+        int[] data = {0, 0};
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             String read = bufferedReader.readLine();
             while (read != null) {
                 String[] readSplit = read.split(",");
                 read = bufferedReader.readLine();
                 if (readSplit[0].equals("supply")) {
-                    supply += Integer.parseInt(readSplit[1]);
+                    data[0] += Integer.parseInt(readSplit[1]);
                 } else {
-                    buy += Integer.parseInt(readSplit[1]);
+                    data[1] += Integer.parseInt(readSplit[1]);
                 }
             }
         } catch (IOException e) {
             throw new RuntimeException("Can't read file" + fromFileName, e);
         }
+        return data;
     }
 
     private void writeToFile(String report, String toFileName) {
