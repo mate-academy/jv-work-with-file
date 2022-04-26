15fb680013751303cc7b91cdff804d024875e055
Fokin <Fokin1@ukr.net>
2022-04-26 21:24:21 +0200

    Correction names variable

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -12,34 +12,34 @@ public class WorkWithFile {
     private static final int OPERATION_INDEX = 0;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        List<String> dataFile = getDataFile(fromFileName);
-        StringBuilder statisticBuilder = getStatisticBuilder(dataFile);
-        setStatisticFile(toFileName, statisticBuilder);
+        List<String> lines = readFromFile(fromFileName);
+        String statisticBuilder = getStatisticParsed(lines);
+        writeToFile(toFileName, statisticBuilder);
     }
 
-    private List<String> getDataFile(String fromFileName) {
-        List<String> data;
+    private List<String> readFromFile(String fromFileName) {
+        List<String> lines;
         try {
             File file = new File(fromFileName);
-            data = Files.readAllLines(file.toPath());
+            lines = Files.readAllLines(file.toPath());
         } catch (IOException e) {
             throw new RuntimeException("File can't read", e);
         }
-        return data;
+        return lines;
     }
 
-    private StringBuilder getStatisticBuilder(List<String> data) {
+    private String getStatisticParsed(List<String> lines) {
         StringBuilder statisticBuilder = new StringBuilder();
         int supplySum = 0;
         int buySum = 0;
         int result = 0;
-        for (String str : data) {
-            String[] substring = str.split(",");
-            if (substring[OPERATION_INDEX].equals("supply")) {
-                supplySum += Integer.parseInt(substring[QUANTITY_INDEX]);
+        for (String line : lines) {
+            String[] splittedLine = line.split(",");
+            if (splittedLine[OPERATION_INDEX].equals("supply")) {
+                supplySum += Integer.parseInt(splittedLine[QUANTITY_INDEX]);
             }
-            if (substring[OPERATION_INDEX].equals("buy")) {
-                buySum += Integer.parseInt(substring[QUANTITY_INDEX]);
+            if (splittedLine[OPERATION_INDEX].equals("buy")) {
+                buySum += Integer.parseInt(splittedLine[QUANTITY_INDEX]);
             }
         }
         result = supplySum - buySum;
@@ -47,12 +47,12 @@ public class WorkWithFile {
                 .append(System.lineSeparator()).append("buy").append(",")
                 .append(buySum).append(System.lineSeparator())
                 .append("result").append(",").append(result);
-        return statisticBuilder;
+        return statisticBuilder.toString();
     }
 
-    private void setStatisticFile(String toFileName, StringBuilder stringBuilder) {
+    private void writeToFile(String toFileName, String statistic) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
-            bufferedWriter.write(stringBuilder.toString());
+            bufferedWriter.write(statistic);
         } catch (IOException e) {
             throw new RuntimeException("File can't write", e);
         }
