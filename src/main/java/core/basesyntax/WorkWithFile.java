634e8169138f8421615194d97bc83cbcdc25ddc3
Dmytro Dmytrenko <dmitrynetfx@gmail.com>
2022-04-26 15:05:20 +0300

    Init

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,5 +1,8 @@
 package core.basesyntax;
 
+import java.io.FileReader;
+import java.util.ArrayList;
+
 public class WorkWithFile {
 
     public void getStatistic(String fromFileName, String toFileName) {
