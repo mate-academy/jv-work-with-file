b9a8c47dffba2966d38838485932100e87d39811
Oleksii Mospan <alexey.mospan@gmail.com>
2022-05-11 02:49:50 +0300

    Lighter code (removed not necessary exception and validation, added try with resources)

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,8 +2,6 @@ package core.basesyntax;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -15,11 +13,9 @@ public class WorkWithFile {
     public static final int RESULT_INDEX = 2;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        File fromFile = new File(fromFileName);
         String stringLine;
         int[] values = {0, 0, 0};
-
-        try (BufferedReader br = new BufferedReader(new FileReader(fromFile))) {
+        try (BufferedReader br = new BufferedReader(new FileReader(fromFileName))) {
             while ((stringLine = br.readLine()) != null) {
                 if (stringLine.split(",")[0].equals("supply")) {
                     values[SUPPLY_INDEX] += Integer.valueOf(stringLine.split(",")[1]);
@@ -28,40 +24,23 @@ public class WorkWithFile {
                     values[BUY_INDEX] += Integer.valueOf(stringLine.split(",")[1]);
                 }
             }
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException("File not found: " + fromFileName, e);
         } catch (IOException e) {
             throw new RuntimeException("Can't read file: " + fromFileName, e);
-        } finally {
-            values[RESULT_INDEX] = values[SUPPLY_INDEX] - values[BUY_INDEX];
         }
-        writeToFile(toFileName, new String[]{"supply", "buy", "result"}, values);
+        values[RESULT_INDEX] = values[SUPPLY_INDEX] - values[BUY_INDEX];
+        writeToFile(toFileName, values);
     }
 
-    public void writeToFile(String toFileName, String[] params, int[] values) {
-        if (params.length != values.length) {
-            System.out.println("Params and values has different length: params["
-                    + params.length
-                    + "] and values[" + values.length + "].");
-            return;
-        }
-
+    public void writeToFile(String toFileName, int[] values) {
+        String[] params = {"supply", "buy", "result"};
         StringBuilder stringBuilder = new StringBuilder();
         for (int i = 0; i < params.length; i++) {
             stringBuilder.append(params[i]).append(",").append(values[i])
                     .append(System.lineSeparator());
         }
 
-        File file = new File(toFileName);
-        FileWriter fileWriter;
-        BufferedWriter bufferedWriter;
-
-        try {
-            fileWriter = new FileWriter(file);
-            bufferedWriter = new BufferedWriter(fileWriter);
-            bufferedWriter.write(stringBuilder.toString());
-            bufferedWriter.close();
-            fileWriter.close();
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(toFileName))) {
+            bw.write(stringBuilder.toString());
         } catch (IOException e) {
             throw new RuntimeException("Can't write or close the file " + toFileName, e);
         }
