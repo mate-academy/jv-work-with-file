28b5f082bd3df019c769e7f61ecd6bb08c52eb9c
Anton Karpov <antonkarpov547@gmail.com>
2022-05-20 00:47:50 +0200

    Checkstyle improved

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,42 +2,62 @@ package core.basesyntax;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class WorkWithFile {
+    private int supply = 0;
+    private int buy = 0;
+
+    public int getBuy() {
+        return buy;
+    }
+
+    public void setBuy(int buy) {
+        this.buy = buy;
+    }
+
+    public int getSupply() {
+        return supply;
+    }
+
+    public void setSupply(int supply) {
+        this.supply = supply;
+    }
 
     public void getStatistic(String fromFileName, String toFileName) {
-        int supply = 0;
-        int buy = 0;
-        File fromFile = new File(fromFileName);
-        File toFile = new File(toFileName);
-        try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFile));
+        readFile(fromFileName);
+        writeFile(toFileName);
+    }
+
+    public void readFile(String fileName) {
+        try (BufferedReader bufferedReader =
+                     Files.newBufferedReader(Paths.get(fileName))) {
             String value = bufferedReader.readLine();
             while (value != null) {
                 if (value.toCharArray()[0] == 'b') {
-                    buy += Integer.parseInt(value.substring(value.indexOf(',') + 1));
+                    setBuy(getBuy() + Integer.parseInt(value.split(",")[1]));
                 } else {
-                    supply += Integer.parseInt(value.substring(value.indexOf(',') + 1));
+                    setSupply(getSupply() + Integer.parseInt(value.split(",")[1]));
                 }
                 value = bufferedReader.readLine();
             }
         } catch (IOException e) {
-            throw new RuntimeException("Can't correctly read data from file " + fromFileName, e);
+            throw new RuntimeException("Can't correctly read data from file " + fileName, e);
         }
-        try {
+    }
+
+    public void writeFile(String fileName) {
+        try (BufferedWriter bufferedWriter =
+                     Files.newBufferedWriter(Paths.get(fileName))) {
             StringBuilder stringBuilder = new StringBuilder("supply,");
-            stringBuilder.append(supply).append(System.lineSeparator())
-                    .append("buy,").append(buy).append(System.lineSeparator())
-                    .append("result,").append(supply - buy);
-            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFile));
+            stringBuilder.append(getSupply()).append(System.lineSeparator())
+                    .append("buy,").append(getBuy()).append(System.lineSeparator())
+                    .append("result,").append(getSupply() - getBuy());
             bufferedWriter.write(stringBuilder.toString());
-            bufferedWriter.close();
         } catch (IOException e) {
-            throw new RuntimeException("Can't correctly write data to file " + toFileName, e);
+            throw new RuntimeException("Can't correctly write data to file " + fileName, e);
         }
     }
 }
