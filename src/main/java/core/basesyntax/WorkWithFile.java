6606b85093ed5cd1f82335f35058ad8ad9139150
Lev Ber <levberezov01@gmail.com>
2022-05-20 17:43:56 +0300

    added fix: try-with-resources line 46, separate methods to write and reade files,  throw new RuntimeException line 57 instead of   e.printStackTrace();

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,26 +8,12 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    private int supplySum = 0;
+    private int buySum = 0;
 
     public void getStatistic(String fromFileName, String toFileName) {
 
-        File fromFile = new File(fromFileName);
-        File toFile = new File(toFileName);
-        StringBuilder stringBuilder = new StringBuilder();
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(fromFile));
-            String value = reader.readLine();
-            while (value != null) {
-                stringBuilder.append(value);
-                stringBuilder.append(",");
-                value = reader.readLine();
-            }
-        } catch (IOException e) {
-            throw new RuntimeException("Can't not reade data from file", e);
-        }
-        int supplySum = 0;
-        int buySum = 0;
-        String[] arrayString = stringBuilder.toString().split(",");
+        String[] arrayString = readeFromFile(fromFileName).toString().split(",");
         for (int i = 1; i < arrayString.length; i++) {
             if (arrayString[i - 1].equals("supply")) {
                 supplySum = Integer.parseInt(arrayString[i].trim()) + supplySum;
@@ -35,8 +21,28 @@ public class WorkWithFile {
                 buySum = Integer.parseInt(arrayString[i].trim()) + buySum;
             }
         }
+        writerToFile(toFileName);
+    }
+
+    private StringBuilder readeFromFile(String fileName) {
+        File fromFile = new File(fileName);
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFile))) {
+            String value = reader.readLine();
+            while (value != null) {
+                stringBuilder.append(value);
+                stringBuilder.append(",");
+                value = reader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't not reade data from file " + fileName, e);
+        }
+        return stringBuilder;
+    }
+
+    private void writerToFile(String fileName) {
         int result = supplySum - buySum;
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFile, true))) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName, true))) {
             bufferedWriter.write("supply,");
             bufferedWriter.write(String.valueOf(supplySum));
             bufferedWriter.write(System.lineSeparator());
@@ -46,7 +52,8 @@ public class WorkWithFile {
             bufferedWriter.write("result,");
             bufferedWriter.write(String.valueOf(result));
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException("Can't not write data to file" + fileName, e);
         }
+
     }
 }
