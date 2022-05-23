d736910a7c4191803d62899e5c57079756abe8f4
Oleksii Kordysh <aleksey.kordysh@gmail.com>
2022-05-23 22:08:37 +0300

    merged createReport and readData to one method

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,40 +7,32 @@ import java.io.IOException;
 import java.nio.file.Files;
 
 public class WorkWithFile {
-    private int totalSupply = 0;
-    private int totalBuy = 0;
-
     public void getStatistic(String fromFileName, String toFileName) {
-        analyzeData(fromFileName);
-        String report = createReport(totalSupply, totalBuy);
+        String report = createReport(fromFileName);
         writeReport(toFileName, report);
     }
 
-    private void analyzeData(String fromFileName) {
+    private String createReport(String fromFileName) {
         try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
+            int totalSupply = 0;
+            int totalBuy = 0;
             String line = reader.readLine();
+
             while (line != null) {
-                analyzeRecord(line);
+                String[] info = line.split(",");
+                if (info[0].equals("supply")) {
+                    totalSupply += Integer.parseInt(info[1]);
+                } else if (info[0].equals("buy")) {
+                    totalBuy += Integer.parseInt(info[1]);
+                }
                 line = reader.readLine();
             }
+            return "supply," + totalSupply + System.lineSeparator()
+                    + "buy," + totalBuy + System.lineSeparator()
+                    + "result," + (totalSupply - totalBuy);
         } catch (IOException e) {
             throw new RuntimeException("Can't read file", e);
         }
-    }
-
-    private void analyzeRecord(String record) {
-        String[] info = record.split(",");
-        if (info[0].equals("supply")) {
-            totalSupply += Integer.parseInt(info[1]);
-        } else if (info[0].equals("buy")) {
-            totalBuy += Integer.parseInt(info[1]);
-        }
-    }
-
-    private String createReport(int totalSupply, int totalBuy) {
-        return "supply," + totalSupply + System.lineSeparator()
-                + "buy," + totalBuy + System.lineSeparator()
-                + "result," + (totalSupply - totalBuy);
     }
 
     private void writeReport(String toFileName, String report) {
