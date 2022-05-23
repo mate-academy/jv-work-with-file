f094ae0eeed59d08e41647d89c661fe378600906
Anton Karpov <antonkarpov547@gmail.com>
2022-05-23 13:18:31 +0200

    Constants implemented, public removed, if else fixed

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -13,11 +13,11 @@ public class WorkWithFile {
         writeFile(toFileName, evaluatedData);
     }
 
-    public String readFile(String fileName) {
+    private String readFile(String fileName) {
         try (BufferedReader bufferedReader =
                      Files.newBufferedReader(Paths.get(fileName))) {
             String value = bufferedReader.readLine();
-            StringBuilder stringBuilder = new StringBuilder();
+            final StringBuilder stringBuilder = new StringBuilder();
             while (value != null) {
                 stringBuilder.append(value).append(System.lineSeparator());
                 value = bufferedReader.readLine();
@@ -28,26 +28,31 @@ public class WorkWithFile {
         }
     }
 
-    public String evaluateData(String data) {
+    private String evaluateData(String data) {
         int supply = 0;
         int buy = 0;
-        StringBuilder stringBuilder = new StringBuilder();
-        String[] array = data.split(System.lineSeparator());
-        System.out.println(data);
+        final String wordBuy = "buy";
+        final String wordSupply = "supply";
+        final String wordResult = "result";
+        final String comma = ",";
+        final String separator = System.lineSeparator();
+        final StringBuilder stringBuilder = new StringBuilder();
+        final String[] array = data.split(separator);
         for (String line : array) {
-            if (line.toCharArray()[0] == 'b') {
-                buy += Integer.parseInt(line.split(",")[1]);
-            } else {
-                supply += Integer.parseInt(line.split(",")[1]);
+            final String[] words = line.split(comma);
+            if (words[0].equals(wordBuy)) {
+                buy += Integer.parseInt(words[1]);
+            } else if (words[0].equals(wordSupply)) {
+                supply += Integer.parseInt(words[1]);
             }
         }
-        stringBuilder.append("supply,").append(supply).append(System.lineSeparator())
-                .append("buy,").append(buy).append(System.lineSeparator())
-                .append("result,").append(supply - buy);
+        stringBuilder.append(wordSupply).append(comma).append(supply).append(separator)
+                .append(wordBuy).append(comma).append(buy).append(separator)
+                .append(wordResult).append(comma).append(supply - buy);
         return stringBuilder.toString();
     }
 
-    public void writeFile(String fileName, String data) {
+    private void writeFile(String fileName, String data) {
         try (BufferedWriter bufferedWriter =
                      Files.newBufferedWriter(Paths.get(fileName))) {
             bufferedWriter.write(data);
