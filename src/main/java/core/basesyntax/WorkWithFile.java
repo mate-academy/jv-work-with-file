4474454986fa6bc592c9d7b7212ee9b45c319454
Nikita Semisoshenko <jaderussell197@gmail.com>
2022-05-24 12:34:03 +0300

    TaskFinalizer class was removed as redundant

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -6,10 +6,9 @@ public class WorkWithFile {
 
     public void getStatistic(String fromFileName, String toFileName) {
         File file = new File(fromFileName);
-        TaskFinalizer taskFinalizer = new TaskFinalizer();
         Writer writer = new Writer();
         String[] data = new DataExtractor().extract(file);
-        String finalString = taskFinalizer.finalString(data);
-        writer.write(finalString, toFileName);
+        String count = new AmountCounter().count(data);
+        writer.write(count, toFileName);
     }
 }
