0cb00ba105df32c7829327d0bb84fc0e27289af8
Illya Svyshcho <iill44iikk@gmail.com>
2022-06-03 18:53:09 +0300

    my main changes #4

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -9,12 +9,9 @@ import java.nio.file.Files;
 public class WorkWithFile {
     private static final String SPLITTER = "&";
     private static final String COMMA = ",";
-    private static final String SUPPLY = "supply";
-    private static final String BUY = "buy";
-    private static final String RESULT = "result";
     private static final String LINE_SEPARATOR = System.lineSeparator();
-    private static final int OPERATION_CELL = 0;
-    private static final int AMOUNT_CELL = 1;
+    private static final int OPERATION_INDEX = 0;
+    private static final int AMOUNT_INDEX = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
         int totalSupply = 0;
@@ -23,21 +20,22 @@ public class WorkWithFile {
         String[] data = readFile(new File(fromFileName));
         for (int i = 0; i < data.length; i++) {
             tableRow = data[i].split(COMMA);
-            if (tableRow[OPERATION_CELL].equals(SUPPLY)) {
-                totalSupply += Integer.parseInt(tableRow[AMOUNT_CELL]);
+            if (tableRow[OPERATION_INDEX].equals("supply")) {
+                totalSupply += Integer.parseInt(tableRow[AMOUNT_INDEX]);
             } else {
-                totalBuy += Integer.parseInt(tableRow[AMOUNT_CELL]);
+                totalBuy += Integer.parseInt(tableRow[AMOUNT_INDEX]);
             }
         }
         buildReport(totalSupply,totalBuy,toFileName);
     }
 
-    private void buildReport(int totalSupply, int totalBuy, String toFileName) {
+    private String buildReport(int totalSupply, int totalBuy, String toFileName) {
         StringBuilder builder = new StringBuilder();
-        builder.append(SUPPLY).append(COMMA).append(totalSupply).append(LINE_SEPARATOR)
-                .append(BUY).append(COMMA).append(totalBuy).append(LINE_SEPARATOR)
-                .append(RESULT).append(COMMA).append(totalSupply - totalBuy);
+        builder.append("supply").append(COMMA).append(totalSupply).append(LINE_SEPARATOR)
+                .append("buy").append(COMMA).append(totalBuy).append(LINE_SEPARATOR)
+                .append("result").append(COMMA).append(totalSupply - totalBuy);
         writeFile(new File(toFileName), builder.toString());
+        return builder.toString();
     }
 
     private String[] readFile(File file) {
@@ -50,16 +48,16 @@ public class WorkWithFile {
                 value = bufferedReader.readLine();
             }
         } catch (IOException e) {
-            throw new RuntimeException("Cannot read file: " + e);
+            throw new RuntimeException("Cannot read file: " + file, e);
         }
         return builder.toString().split(SPLITTER);
     }
 
-    private void writeFile(File file, String message) {
+    private void writeFile(File file,String buildReport) {
         try {
-            Files.write(file.toPath(), message.getBytes());
+            Files.write(file.toPath(), buildReport.getBytes());
         } catch (IOException e) {
-            throw new RuntimeException("Can't write data to file " + e);
+            throw new RuntimeException("Can't write data to file " + file, e);
         }
     }
 }
