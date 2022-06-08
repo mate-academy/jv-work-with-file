590b073267055435edec9c3ef9f1f049ffcbd618
Oleksandr Zaporozhets <zpc23041988@gmail.com>
2022-06-08 23:59:55 +0300

    Fixed code

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,50 +8,44 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    private static final String SUPPLY = "supply";
-    private static final String BUY = "buy";
-    private static final String RESULT = "result";
-    private static final String SIGN = ",";
-    private static final int INDEX_ZERO = 0;
-    private static final int INDEX_ONE = 1;
-    private static final String SEPARATOR = System.lineSeparator();
+    private int supplyCount = 0;
+    private int buyCount = 0;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        File fileReader = new File(fromFileName);
-        int supplyCount = 0;
-        int buyCount = 0;
-        int result;
-        try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(fileReader));
-            String value;
-            StringBuilder builder = new StringBuilder();
-            while ((value = bufferedReader.readLine()) != null) {
-                builder.append(value).append(SEPARATOR);
+        StringBuilder builder = new StringBuilder();
+        String[] file = readFile(new File(fromFileName));
+        for (String files : file) {
+            String[] reportFile = files.split(",");
+            if (reportFile[0].equals("supply")) {
+                supplyCount += Integer.parseInt(reportFile[1]);
+            } else {
+                buyCount += Integer.parseInt(reportFile[1]);
             }
-            String[] file = builder.toString().split(SEPARATOR);
-            for (String files : file) {
-                String[] reportFile = files.split(SIGN);
-                if (reportFile[INDEX_ZERO].equals(BUY)) {
-                    buyCount += Integer.parseInt(reportFile[INDEX_ONE]);
-                } else {
-                    supplyCount += Integer.parseInt(reportFile[INDEX_ONE]);
-                }
-            }
-            result = supplyCount - buyCount;
-        } catch (IOException e) {
-            throw new RuntimeException("Read to file " + fromFileName, e);
         }
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(SUPPLY).append(SIGN).append(supplyCount)
-                .append(SEPARATOR).append(BUY).append(SIGN)
-                .append(buyCount).append(SEPARATOR).append(RESULT)
-                .append(SIGN).append(result);
+        builder.append("supply").append(",").append(supplyCount).append(System.lineSeparator())
+                .append("buy").append(",").append(buyCount).append(System.lineSeparator())
+                .append("result").append(",").append(supplyCount - buyCount);
+        writeReportFile(new File(toFileName), builder.toString());
+    }
 
-        File resultFile = new File(toFileName);
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(resultFile, true))) {
-            bufferedWriter.write(stringBuilder.toString());
+    private String[] readFile(File file) {
+        StringBuilder builder = new StringBuilder();
+        String value;
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+            while ((value = bufferedReader.readLine()) != null) {
+                builder.append(value).append(System.lineSeparator());
+            }
         } catch (IOException e) {
-            throw new RuntimeException("write to file " + toFileName, e);
+            throw new RuntimeException("Can't read file: " + file, e);
+        }
+        return builder.toString().split(System.lineSeparator());
+    }
+
+    private void writeReportFile(File file,String writeToFile) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file,true))) {
+            bufferedWriter.write(writeToFile);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write file: " + writeToFile, e);
         }
     }
 }
