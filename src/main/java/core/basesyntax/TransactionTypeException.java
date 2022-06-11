cce91ec84d3644f7d2aab6b12961fb9935538edf
Nata <natserbo@gmail.com>
2022-06-11 21:55:07 +0300

    first solution

---
+++ src/main/java/core/basesyntax/TransactionTypeException.java
@@ -0,0 +1,8 @@
+package core.basesyntax;
+
+public class TransactionTypeException extends Exception {
+
+    public TransactionTypeException(String message) {
+        super(message);
+    }
+}
--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,8 +1,43 @@
 package core.basesyntax;
 
-public class WorkWithFile {
+import java.io.*;
 
-    public void getStatistic(String fromFileName, String toFileName) {
+public class WorkWithFile {
+    private static final int TRANSACTION_POSITION = 0;
+    private static final int MONEY_AMOUNT_POSITION = 1;
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+
+
+    public void getStatistic(String fromFileName, String toFileName) throws TransactionTypeException {
+        int supplyAmount = 0;
+        int buyAmount = 0;
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
+            String line = reader.readLine();
+            while ((line = reader.readLine()) != null) {
+                String[] transactionType = line.split(",");
+                switch (transactionType[TRANSACTION_POSITION]) {
+                    case SUPPLY:
+                        supplyAmount += Integer.parseInt(transactionType[MONEY_AMOUNT_POSITION]);
+                        break;
+                    case BUY:
+                        buyAmount += Integer.parseInt(transactionType[MONEY_AMOUNT_POSITION]);
+                        break;
+                    default:
+                        throw new TransactionTypeException("Invalid transaction type");
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read from " + fromFileName + e);
+        }
+
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName,true))) {
+            writer.write(SUPPLY + "," + supplyAmount + System.lineSeparator() +
+                    BUY + "," + buyAmount + System.lineSeparator() +
+                    "result" + "," + (supplyAmount - buyAmount));
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write to file " + toFileName + e);
+        }
 
     }
 }
