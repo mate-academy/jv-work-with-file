0434a03b31a84b52b92bdb1ba9762909ea6141ed
Valerii-Sh <val-sh@ukr.net>
2022-07-06 23:21:18 +0300

    changed after mvn review

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -9,30 +9,29 @@ public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
         int buyTotal = 0;
         int supplyTotal = 0;
-        final int INDEX_OF_OPERATION_TYPE = 0;
-        final int INDEX_OF_AMOUNT = 1;
-        final String COMMA = ",";
-        final String SUPPLY = "supply";
-        final String BUY = "buy";
-        final String RESULT = "result";
+        final int indexOfOperationType = 0;
+        final int indexOfAmount = 1;
+        final String comma = ",";
+        final String supply = "supply";
+        final String buy = "buy";
 
         String[] strings = readDataFromFile(fromFileName);
 
         for (String stringInputData : strings) {
-            if (stringInputData.split(COMMA)[INDEX_OF_OPERATION_TYPE].equals(BUY)) {
-                buyTotal += Integer.parseInt(stringInputData.split(COMMA)[INDEX_OF_AMOUNT]);
+            if (stringInputData.split(comma)[indexOfOperationType].equals(buy)) {
+                buyTotal += Integer.parseInt(stringInputData.split(comma)[indexOfAmount]);
             }
-            if (stringInputData.split(COMMA)[INDEX_OF_OPERATION_TYPE].equals(SUPPLY)) {
-                supplyTotal += Integer.parseInt(stringInputData.split(COMMA)[INDEX_OF_AMOUNT]);
+            if (stringInputData.split(comma)[indexOfOperationType].equals(supply)) {
+                supplyTotal += Integer.parseInt(stringInputData.split(comma)[indexOfAmount]);
             }
         }
-        int result = supplyTotal - buyTotal;
+        int resultAmount = supplyTotal - buyTotal;
 
         StringBuilder stringReport = new StringBuilder();
-        stringReport.append(SUPPLY).append(COMMA).append(supplyTotal)
+        stringReport.append(supply).append(comma).append(supplyTotal)
                 .append(System.lineSeparator());
-        stringReport.append(BUY).append(COMMA).append(buyTotal).append(System.lineSeparator());
-        stringReport.append(RESULT).append(COMMA).append(result);
+        stringReport.append(buy).append(comma).append(buyTotal).append(System.lineSeparator());
+        stringReport.append("result").append(comma).append(resultAmount);
 
         try {
             File toFile = new File(toFileName);
@@ -40,7 +39,6 @@ public class WorkWithFile {
         } catch (IOException e) {
             throw new RuntimeException("Can't write data to the file: " + toFileName, e);
         }
-
     }
 
     private String[] readDataFromFile(String fromFileName) {
