d79a27bdad9f756d001450dcbfe137145ca5ec38
Yurii Lisovskyi <yuri.lisovski@gmail.com>
2022-07-06 14:20:32 +0300

    do fifth attempt

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -3,7 +3,6 @@ package core.basesyntax;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -11,27 +10,39 @@ import java.io.IOException;
 public class WorkWithFile {
     private static final byte OPERATION_TYPE_INDEX = 0;
     private static final byte AMOUNT_INDEX = 1;
+    private static final String COLUMN_SEPARATOR = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
         int supplyTotal = 0;
         int buyTotal = 0;
-        File fileInput = new File(fromFileName);
-        try (BufferedReader reader = new BufferedReader(new FileReader(fileInput))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String[] splittedLine = line.split(",");
-                if (splittedLine[OPERATION_TYPE_INDEX].equals("supply")) {
-                    supplyTotal += Integer.parseInt(splittedLine[AMOUNT_INDEX]);
-                } else {
-                    buyTotal += Integer.parseInt(splittedLine[AMOUNT_INDEX]);
-                }
+        int currentAmount;
+        String[] splittedLine;
+
+        String[] linesFromFile = readFromFile(fromFileName);
+        for (String line: linesFromFile) {
+            splittedLine = line.split(COLUMN_SEPARATOR);
+            currentAmount = Integer.parseInt(splittedLine[AMOUNT_INDEX]);
+            if (splittedLine[OPERATION_TYPE_INDEX].equals("supply")) {
+                supplyTotal += currentAmount;
+            } else {
+                buyTotal += currentAmount;
             }
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException("Can't open input file " + fromFileName, e);
-        } catch (IOException e) {
-            throw new RuntimeException("Can't read from input file " + fromFileName, e);
         }
         writeToFile(toFileName, supplyTotal, buyTotal);
+    }
+
+    private String[] readFromFile(String fromFileName) {
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
+            String line = bufferedReader.readLine();
+            while (line != null) {
+                stringBuilder.append(line).append(System.lineSeparator());
+                line = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Error reading file" + fromFileName, e);
+        }
+        return stringBuilder.toString().split(System.lineSeparator());
     }
 
     private void writeToFile(String toFileName, int supplyTotal, int buyTotal) {
