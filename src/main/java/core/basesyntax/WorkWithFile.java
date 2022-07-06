ca9e59107fe9e1ad1f71d9951d972daa5531f803
Yurii Lisovskyi <yuri.lisovski@gmail.com>
2022-07-06 09:21:43 +0300

    do third attempt

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -11,31 +11,19 @@ import java.io.IOException;
 public class WorkWithFile {
     private static final byte OPERATION_TYPE_INDEX = 0;
     private static final byte AMOUNT_INDEX = 1;
-    private int supplyTotal = 0;
-    private int buyTotal = 0;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        calculateReportData(fromFileName);
-        outputReportToFile(toFileName);
-    }
-
-    private void calculateReportData(String fromFileName) {
+        int supplyTotal = 0;
+        int buyTotal = 0;
         File fileInput = new File(fromFileName);
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(fileInput));
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileInput))) {
             String line;
             while ((line = reader.readLine()) != null) {
-                String[] splitLine = line.split(",");
-                switch (splitLine[OPERATION_TYPE_INDEX]) {
-                    case "supply": {
-                        supplyTotal += Integer.parseInt(splitLine[AMOUNT_INDEX]);
-                        break;
-                    }
-                    case "buy": {
-                        buyTotal += Integer.parseInt(splitLine[AMOUNT_INDEX]);
-                        break;
-                    }
-                    default:
+                String[] splittedLine = line.split(",");
+                if (splittedLine[OPERATION_TYPE_INDEX].equals("supply")) {
+                    supplyTotal += Integer.parseInt(splittedLine[AMOUNT_INDEX]);
+                } else {
+                    buyTotal += Integer.parseInt(splittedLine[AMOUNT_INDEX]);
                 }
             }
         } catch (FileNotFoundException e) {
@@ -43,9 +31,10 @@ public class WorkWithFile {
         } catch (IOException e) {
             throw new RuntimeException("Can't read from Input file", e);
         }
+        writeToFile(toFileName, supplyTotal, buyTotal);
     }
 
-    private void outputReportToFile(String toFileName) {
+    private void writeToFile(String toFileName, int supplyTotal, int buyTotal) {
         File outputFile = new File(toFileName);
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(outputFile))) {
             bufferedWriter.write("supply," + supplyTotal + System.lineSeparator());
