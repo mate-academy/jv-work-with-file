9b3739060173789f3229acf7d4b461f8601cdc36
Valerii-Sh <val-sh@ukr.net>
2022-07-07 12:56:41 +0300

    changed after mentor review

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -5,40 +5,31 @@ import java.io.IOException;
 import java.nio.file.Files;
 
 public class WorkWithFile {
+    private static final int INDEX_OF_OPERATION_TYPE = 0;
+    private static final int INDEX_OF_AMOUNT = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
         int buyTotal = 0;
         int supplyTotal = 0;
-        final int indexOfOperationType = 0;
-        final int indexOfAmount = 1;
-        final String comma = ",";
-        final String supply = "supply";
-        final String buy = "buy";
+        String supply = "supply";
+        String buy = "buy";
+        String[] lines = readDataFromFile(fromFileName);
+        for (String stringInputData : lines) {
 
-        String[] strings = readDataFromFile(fromFileName);
-
-        for (String stringInputData : strings) {
-            if (stringInputData.split(comma)[indexOfOperationType].equals(buy)) {
-                buyTotal += Integer.parseInt(stringInputData.split(comma)[indexOfAmount]);
+            if (stringInputData.split(",")[INDEX_OF_OPERATION_TYPE].equals(buy)) {
+                buyTotal += Integer.parseInt(stringInputData.split(",")[INDEX_OF_AMOUNT]);
             }
-            if (stringInputData.split(comma)[indexOfOperationType].equals(supply)) {
-                supplyTotal += Integer.parseInt(stringInputData.split(comma)[indexOfAmount]);
+
+            if (stringInputData.split(",")[INDEX_OF_OPERATION_TYPE].equals(supply)) {
+                supplyTotal += Integer.parseInt(stringInputData.split(",")[INDEX_OF_AMOUNT]);
             }
         }
         int resultAmount = supplyTotal - buyTotal;
-
         StringBuilder stringReport = new StringBuilder();
-        stringReport.append(supply).append(comma).append(supplyTotal)
-                .append(System.lineSeparator());
-        stringReport.append(buy).append(comma).append(buyTotal).append(System.lineSeparator());
-        stringReport.append("result").append(comma).append(resultAmount);
-
-        try {
-            File toFile = new File(toFileName);
-            Files.writeString(toFile.toPath(), stringReport);
-        } catch (IOException e) {
-            throw new RuntimeException("Can't write data to the file: " + toFileName, e);
-        }
+        stringReport.append(supply).append(",").append(supplyTotal).append(System.lineSeparator());
+        stringReport.append(buy).append(",").append(buyTotal).append(System.lineSeparator());
+        stringReport.append("result").append(",").append(resultAmount);
+        writeReportToFile(toFileName, stringReport.toString());
     }
 
     private String[] readDataFromFile(String fromFileName) {
@@ -47,6 +38,15 @@ public class WorkWithFile {
             return Files.readAllLines(fromFile.toPath()).toArray(new String[0]);
         } catch (IOException e) {
             throw new RuntimeException("Can't read data from the file: " + fromFileName, e);
+        }
+    }
+
+    private void writeReportToFile(String toFileName, String stringReport) {
+        try {
+            File toFile = new File(toFileName);
+            Files.writeString(toFile.toPath(), stringReport);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write data to the file: " + toFileName, e);
         }
     }
 }
