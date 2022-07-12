c918ebd0204540fbacb3418eca7b5b2511fcd67f
Andrii Zakhaarchenko <C:\Progs\BatBase>
2022-07-12 21:44:51 +0300

    jv-work-with-file-hw8-fixed-devided-program-logiic

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,52 +8,69 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    private static final String BUY_EVENT_NAME = "buy";
+    private static final String SUPPLY_EVENT_NAME = "supply";
+    private static final int OPERATION_TYPE = 0;
+    private static final int AMMOUNT = 1;
+    private static int supplyTotal = 0;
+    private static int buyTotal = 0;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        final String buyEventName = "buy";
-        final String supplyEventName = "supply";
+        readFile(fromFileName);
+        writeFile(toFileName);
+    }
+
+    public void readFile(String fromFileName) {
+        supplyTotal = 0;
+        buyTotal = 0;
         File fileSource = new File(fromFileName);
-        File fileTarget = new File(toFileName);
-        StringBuilder stringBuilder = new StringBuilder();
-        int supplyTotal = 0;
-        int buyTotal = 0;
         try (BufferedReader reader = new BufferedReader(new FileReader(fileSource))) {
             String lineFromFile = reader.readLine();
             String[] splittedLine;
             while (lineFromFile != null) {
-                splittedLine = null;
                 splittedLine = lineFromFile.split(",");
-                switch (splittedLine[0]) {
-                    case (supplyEventName):
-                        supplyTotal += Integer.valueOf(splittedLine[1]);
-                        break;
-                    case (buyEventName):
-                        buyTotal += Integer.valueOf(splittedLine[1]);
-                        break;
-                    default:
-                        break;
-                }
+                calculateTotalValues(splittedLine);
                 lineFromFile = reader.readLine();
             }
-            reader.close();
         } catch (IOException ex) {
             throw new RuntimeException("File " + fromFileName + " not found.", ex);
         }
-        stringBuilder.append(supplyEventName)
+    }
+
+    private void calculateTotalValues(String[] splittedLine) {
+        switch (splittedLine[OPERATION_TYPE]) {
+            case (SUPPLY_EVENT_NAME):
+                supplyTotal += Integer.valueOf(splittedLine[AMMOUNT]);
+                break;
+            case (BUY_EVENT_NAME):
+                buyTotal += Integer.valueOf(splittedLine[AMMOUNT]);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void writeFile(String toFileName) {
+        File fileTarget = new File(toFileName);
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileTarget, true))) {
+            writer.write(createReport(supplyTotal, buyTotal));
+        } catch (Exception e) {
+            throw new RuntimeException("Can't write file " + toFileName, e);
+        }
+    }
+
+    private String createReport(int supplyTotal, int buyTotal) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(SUPPLY_EVENT_NAME)
                 .append(",")
                 .append(supplyTotal)
                 .append(System.lineSeparator());
-        stringBuilder.append(buyEventName)
+        stringBuilder.append(BUY_EVENT_NAME)
                 .append(",")
                 .append(buyTotal)
                 .append(System.lineSeparator());
         stringBuilder.append("result,")
                 .append(supplyTotal - buyTotal);
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileTarget, true))) {
-            writer.write(stringBuilder.toString());
-            writer.close();
-        } catch (Exception e) {
-            throw new RuntimeException("Can't write file " + toFileName, e);
-        }
+        return stringBuilder.toString();
     }
 }
