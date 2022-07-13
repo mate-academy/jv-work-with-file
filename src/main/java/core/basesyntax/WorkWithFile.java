e254db9da639c6edf132ed8a9e948951536365da
Andrii Zakhaarchenko <C:\Progs\BatBase>
2022-07-13 19:43:18 +0300

    jv-work-with-file-hw8-fixed-Total-variables-as-Array-IndexConstant_names_throw_RTErr

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -10,50 +10,56 @@ import java.io.IOException;
 public class WorkWithFile {
     private static final String BUY_EVENT_NAME = "buy";
     private static final String SUPPLY_EVENT_NAME = "supply";
-    private static final int OPERATION_TYPE = 0;
-    private static final int AMMOUNT = 1;
-    private static int supplyTotal = 0;
-    private static int buyTotal = 0;
+    private static final int OPERATION_TYPE_INDEX = 0;
+    private static final int AMMOUNT_INDEX = 1;
+    private static final int SUPPLY_INDEX = 0;
+    private static final int BUY_INDEX = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        readFile(fromFileName);
-        writeFile(toFileName);
+        int[] supplyBuyTotal = new int[2];
+        supplyBuyTotal = readFile(fromFileName);
+        writeFile(toFileName,supplyBuyTotal);
     }
 
-    public void readFile(String fromFileName) {
-        supplyTotal = 0;
-        buyTotal = 0;
+    public int[] readFile(String fromFileName) {
         File fileSource = new File(fromFileName);
+        int[] supplyBuyTotal = new int[2];
         try (BufferedReader reader = new BufferedReader(new FileReader(fileSource))) {
             String lineFromFile = reader.readLine();
             String[] splittedLine;
             while (lineFromFile != null) {
                 splittedLine = lineFromFile.split(",");
-                calculateTotalValues(splittedLine);
+                int[] temporaryArray = new int[2];
+                temporaryArray = calculateTotalValues(splittedLine);
+                supplyBuyTotal[SUPPLY_INDEX] += temporaryArray[SUPPLY_INDEX];
+                supplyBuyTotal[BUY_INDEX] += temporaryArray[BUY_INDEX];
                 lineFromFile = reader.readLine();
             }
         } catch (IOException ex) {
             throw new RuntimeException("File " + fromFileName + " not found.", ex);
         }
+        return supplyBuyTotal;
     }
 
-    private void calculateTotalValues(String[] splittedLine) {
-        switch (splittedLine[OPERATION_TYPE]) {
+    private int[] calculateTotalValues(String[] splittedLine) {
+        int[] supplyBuyTotal = new int [] {0,0};
+        switch (splittedLine[OPERATION_TYPE_INDEX]) {
             case (SUPPLY_EVENT_NAME):
-                supplyTotal += Integer.valueOf(splittedLine[AMMOUNT]);
+                supplyBuyTotal[SUPPLY_INDEX] += Integer.valueOf(splittedLine[AMMOUNT_INDEX]);
                 break;
             case (BUY_EVENT_NAME):
-                buyTotal += Integer.valueOf(splittedLine[AMMOUNT]);
+                supplyBuyTotal[BUY_INDEX] += Integer.valueOf(splittedLine[AMMOUNT_INDEX]);
                 break;
             default:
-                break;
+                throw new RuntimeException("Unknown event, please check data source file.");
         }
+        return supplyBuyTotal;
     }
 
-    private void writeFile(String toFileName) {
+    private void writeFile(String toFileName, int[] supplyBuyTotal) {
         File fileTarget = new File(toFileName);
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileTarget, true))) {
-            writer.write(createReport(supplyTotal, buyTotal));
+            writer.write(createReport(supplyBuyTotal[SUPPLY_INDEX], supplyBuyTotal[BUY_INDEX]));
         } catch (Exception e) {
             throw new RuntimeException("Can't write file " + toFileName, e);
         }
