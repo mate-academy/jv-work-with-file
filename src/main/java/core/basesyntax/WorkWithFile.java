2513c2baeb753383e9d904eaabb25099dbe9ea95
Maksym Bakunov <mpbakunov.it@gmail.com>
2022-07-13 15:55:30 +0200

    Fixed style bugs #3

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -3,7 +3,6 @@ package core.basesyntax;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -11,34 +10,43 @@ import java.io.IOException;
 public class WorkWithFile {
 
     public void getStatistic(String fromFileName, String toFileName) {
-        File fileIn = new File(fromFileName);
-        File fileOut = new File(toFileName);
-        int sumBuy = 0;
-        int sumSupply = 0;
+        writeResult(toFileName, getTotalData(fromFileName));
+    }
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(fileIn));
-                BufferedWriter writer = new BufferedWriter(new FileWriter(fileOut, true))) {
+    private int[] getTotalData(String fileName) {
+        File fileIn = new File(fileName);
+        int [] totalData = {0, 0};
 
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileIn))) {
             String value = reader.readLine();
 
             while (value != null) {
                 String [] parsedString = value.split(",");
                 if (parsedString[0].equals("buy")) {
-                    sumBuy += Integer.parseInt(parsedString[1]);
+                    totalData[0] += Integer.parseInt(parsedString[1]);
                 } else {
-                    sumSupply += Integer.parseInt(parsedString[1]);
+                    totalData[1] += Integer.parseInt(parsedString[1]);
                 }
                 value = reader.readLine();
             }
-
-            writer.write("supply," + sumSupply + System.lineSeparator());
-            writer.write("buy," + sumBuy + System.lineSeparator());
-            writer.write("result," + (sumSupply - sumBuy));
-
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException("Can't wright file", e);
         } catch (IOException e) {
             throw new RuntimeException("Can't read file", e);
         }
+        return totalData;
     }
+
+    private void writeResult(String toFileName, int [] totalData) {
+        File fileOut = new File(toFileName);
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileOut, true))) {
+
+            writer.write("supply," + totalData[1] + System.lineSeparator());
+            writer.write("buy," + totalData[0] + System.lineSeparator());
+            writer.write("result," + (totalData[1] - totalData[0]));
+
+        } catch (IOException e) {
+            throw new RuntimeException("Can't wright file", e);
+        }
+    }
+
 }
