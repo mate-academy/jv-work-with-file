2b05f968b3a76514a8500e492d9bd7baba9f75c6
Vasyl Piznak <piznak3145@gmail.com>
2022-07-17 18:43:30 +0300

    Divide program on methods.

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -9,39 +9,60 @@ import java.io.IOException;
 
 public class WorkWithFile {
     private static final int OPERATION_TYPE = 0;
-    private static final int AMMOUNT = 1;
+    private static final int AMOUNT = 1;
+    private static final int SUPPLY_AMOUNT = 0;
+    private static final int BUY_AMOUNT = 1;
+    private static final int RESULT_AMOUNT = 2;
 
     public void getStatistic(String fromFileName, String toFileName) {
+        int [] report = createReport(fromFileName);
+        writeToFile(report,toFileName);
+    }
+
+    private int[] createReport(String fromFileName) {
         File file = new File(fromFileName);
-        File fileresult = new File(toFileName);
         int buy = 0;
         int supply = 0;
+        int result = 0;
         try {
             BufferedReader reader = new BufferedReader(new FileReader(file));
             String data = reader.readLine();
             while (data != null) {
                 String[] splittedLine = data.split(",");
                 if (splittedLine[OPERATION_TYPE].equals("buy")) {
-                    buy = buy + Integer.parseInt(splittedLine[AMMOUNT]);
+                    buy = buy + Integer.parseInt(splittedLine[AMOUNT]);
                 }
                 if (splittedLine[OPERATION_TYPE].equals("supply")) {
-                    supply = supply + Integer.parseInt(splittedLine[AMMOUNT]);
+                    supply = supply + Integer.parseInt(splittedLine[AMOUNT]);
                 }
                 data = reader.readLine();
+                result = supply - buy;
             }
-            int result = supply - buy;
-            String[] report = new String[]
-                    {"supply," + supply, "buy," + buy, "result," + result};
-            for (String lines : report) {
-                BufferedWriter bufferedWriter = new BufferedWriter(
-                        new FileWriter(fileresult, true));
-                bufferedWriter.write(lines);
-                bufferedWriter.newLine();
-                bufferedWriter.close();
-            }
+            return new int[]{supply, buy, result};
         } catch (IOException e) {
-            throw new RuntimeException("Cant read and write");
+            throw new RuntimeException("Cant read file ");
         }
     }
+
+    private void writeToFile(int [] report,String toFileName) {
+        File fileResult = new File(toFileName);
+        String[] writtenReport = new String[]
+                {"supply," + report[SUPPLY_AMOUNT], "buy,"
+                        + report[BUY_AMOUNT], "result,"
+                        + report[RESULT_AMOUNT]};
+        for (String lines : writtenReport) {
+            try (BufferedWriter bufferedWriter = new BufferedWriter(
+                    new FileWriter(fileResult, true))) {
+                bufferedWriter.write(lines);
+                bufferedWriter.newLine();
+            } catch (IOException e) {
+                throw new RuntimeException("Cant read file ");
+            }
+
+        }
+
+    }
 }
+
+
 
