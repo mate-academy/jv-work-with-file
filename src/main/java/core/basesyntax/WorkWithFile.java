a3eb0ade60e297d172759eee1dad145e929e4eb3
Serhii Rovenskyi <katapulta13@gmail.com>
2022-07-19 10:41:09 +0300

    Version 2.Errors were fixed

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -9,17 +9,17 @@ import java.io.IOException;
 public class WorkWithFile {
 
     public void getStatistic(String fromFileName, String toFileName) {
-        writeToCsv(createReport(readFromCsv(fromFileName)), toFileName);
+        writeToFile(createReport(readFromFile(fromFileName)), toFileName);
     }
 
-    private String readFromCsv(String fromFileName) {
+    private String readFromFile(String fromFileName) {
         StringBuilder stringBuilder = new StringBuilder();
         try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName));
-            String content = bufferedReader.readLine();
-            while (content != null) {
-                stringBuilder.append(content).append(",");
-                content = bufferedReader.readLine();
+            String line = bufferedReader.readLine();
+            while (line != null) {
+                stringBuilder.append(line).append(",");
+                line = bufferedReader.readLine();
             }
         } catch (IOException e) {
             throw new RuntimeException("Can't read data from the file " + fromFileName, e);
@@ -28,23 +28,25 @@ public class WorkWithFile {
         return stringBuilder.toString();
     }
 
-    private String createReport(String content) {
+    private String createReport(String data) {
         int buy = 0;
         int supply = 0;
-        String[] report = content.split(",");
-        for (int i = 0; i < report.length; i = i + 2) {
-            if (report[i].equals("buy")) {
-                buy = buy + Integer.parseInt(report[i + 1]);
-            } else if (report[i].equals("supply")) {
-                supply = supply + Integer.parseInt(report[i + 1]);
+        StringBuilder builder = new StringBuilder("supply,");
+        String[] lines = data.split(",");
+        for (int i = 0; i < lines.length; i = i + 2) {
+            if (lines[i].equals("buy")) {
+                buy = buy + Integer.parseInt(lines[i + 1]);
+            } else if (lines[i].equals("supply")) {
+                supply = supply + Integer.parseInt(lines[i + 1]);
             }
         }
         int result = supply - buy;
-        return "supply," + supply + System.lineSeparator() + "buy," + buy
-                + System.lineSeparator() + "result," + result;
+        return builder.append(supply).append(System.lineSeparator())
+                .append("buy,").append(buy).append(System.lineSeparator()).append("result,")
+                .append(result).toString();
     }
 
-    private void writeToCsv(String report, String toFileName) {
+    private void writeToFile(String report, String toFileName) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
             writer.write(report);
         } catch (IOException e) {
