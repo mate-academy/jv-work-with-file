8c072469a798a640e606f816bd06da1397bbc426
Dmytro Vyshnivskyi <d.vyshnivskyi@gmail.com>
2022-07-22 10:13:28 +0300

    corrected the points described in the checklist.

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,38 +7,54 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    private static final int INDEX_OF_ACTION = 0;
-    private static final int INDEX_OF_AMOUNT = 1;
     private static final String SEPARATOR = ",";
+    private static final String BUY = "buy";
+    private static final String SUPPLY = "supply";
 
     public void getStatistic(String fromFileName, String toFileName) {
+        writeToReport(toFileName, createReport(readFromFile(fromFileName)));
+    }
+
+    private String readFromFile(String fromFileName) {
         StringBuilder stringBuilder = new StringBuilder();
-        int sumBuy = 0;
-        int sumSupply = 0;
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             String line = bufferedReader.readLine();
             while (line != null) {
-                String[] splittedLine = line.split(SEPARATOR);
-                if (splittedLine[INDEX_OF_ACTION].equals("buy")) {
-                    sumBuy += Integer.parseInt(splittedLine[INDEX_OF_AMOUNT]);
-                }
-                if (splittedLine[INDEX_OF_ACTION].equals("supply")) {
-                    sumSupply += Integer.parseInt(splittedLine[INDEX_OF_AMOUNT]);
-                }
+                stringBuilder.append(line).append(SEPARATOR);
                 line = bufferedReader.readLine();
             }
         } catch (IOException e) {
-            throw new RuntimeException("File" + fromFileName + "cannot be read", e);
+            throw new RuntimeException("File " + fromFileName + " cannot be read", e);
+        }
+        return stringBuilder.toString();
+    }
+
+    private String createReport(String line) {
+        int sumBuy = 0;
+        int sumSupply = 0;
+        StringBuilder stringBuilder = new StringBuilder();
+        String[] splittedLine = line.split(SEPARATOR);
+        for (int i = 0; i < splittedLine.length; i += 2) {
+            if (splittedLine[i].equals(BUY)) {
+                sumBuy += Integer.parseInt(splittedLine[i + 1]);
+            }
+            if (splittedLine[i].equals(SUPPLY)) {
+                sumSupply += Integer.parseInt(splittedLine[i + 1]);
+            }
         }
         int result = sumSupply - sumBuy;
+        stringBuilder.append(SUPPLY).append(SEPARATOR).append(sumSupply)
+                .append(System.lineSeparator()).append(BUY).append(SEPARATOR)
+                .append(sumBuy).append(System.lineSeparator())
+                .append("result,").append(result);
+        return stringBuilder.toString();
+    }
+
+    public void writeToReport(String toFileName, String report) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
-            bufferedWriter.write(String.valueOf(stringBuilder.append("supply")
-                    .append(SEPARATOR).append(sumSupply).append(System.lineSeparator())
-                    .append("buy").append(SEPARATOR).append(sumBuy)
-                    .append(System.lineSeparator()).append("result")
-                    .append(SEPARATOR).append(result)));
+            bufferedWriter.write(report);
         } catch (IOException e) {
-            throw new RuntimeException("File" + toFileName + "cannot be write", e);
+            throw new RuntimeException("File " + toFileName + " cannot be write", e);
         }
     }
 }
