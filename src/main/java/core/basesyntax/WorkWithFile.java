7ebf40eaa48f79e28ca5cf21085d3c6818508cac
Serhii Khomchenko <khomchenko18@gmail.com>
2022-07-23 11:32:04 +0200

    Fixed variables, methods by comments.

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,16 +8,16 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    private static final String COMMA_SEPARATOR = ",";
+    private static final String LINE_SEPARATOR = ",";
     private static final int OPERATION_TYPE_INDEX = 0;
     private static final int AMOUNT_INDEX = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        String data = readFromFileToString(fromFileName);
+        String data = readFromFile(fromFileName);
         writeToFile(toFileName, createReport(data));
     }
 
-    private String readFromFileToString(String filename) {
+    private String readFromFile(String filename) {
         File file = new File(filename);
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
             String data = bufferedReader.readLine();
@@ -35,26 +35,26 @@ public class WorkWithFile {
     private String createReport(String data) {
         int supplySum = 0;
         int buySum = 0;
-        String[] splittedLines = data.split(" ");
-        for (String value : splittedLines) {
-            String[] splittedData = value.split(COMMA_SEPARATOR);
-            switch (splittedData[OPERATION_TYPE_INDEX]) {
+        String[] lines = data.split(" ");
+        for (String line : lines) {
+            String[] splittedLine = line.split(LINE_SEPARATOR);
+            switch (splittedLine[OPERATION_TYPE_INDEX]) {
                 case "supply":
-                    supplySum += Integer.parseInt(splittedData[AMOUNT_INDEX]);
+                    supplySum += Integer.parseInt(splittedLine[AMOUNT_INDEX]);
                     break;
                 case "buy":
-                    buySum += Integer.parseInt(splittedData[AMOUNT_INDEX]);
+                    buySum += Integer.parseInt(splittedLine[AMOUNT_INDEX]);
                     break;
                 default:
             }
         }
-        StringBuilder resultReport = new StringBuilder();
-        resultReport.append("supply").append(COMMA_SEPARATOR).append(supplySum)
+        StringBuilder reportBuilder = new StringBuilder();
+        reportBuilder.append("supply").append(LINE_SEPARATOR).append(supplySum)
                 .append(System.lineSeparator())
-                .append("buy").append(COMMA_SEPARATOR).append(buySum)
+                .append("buy").append(LINE_SEPARATOR).append(buySum)
                 .append(System.lineSeparator())
-                .append("result").append(COMMA_SEPARATOR).append(supplySum - buySum);
-        return resultReport.toString();
+                .append("result").append(LINE_SEPARATOR).append(supplySum - buySum);
+        return reportBuilder.toString();
     }
 
     private void writeToFile(String filename, String data) {
