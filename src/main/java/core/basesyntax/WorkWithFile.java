dc101872ca44cdec590cc4a29364494022af8ee2
Misha Getsko <getsko.gil@gmail.com>
2022-07-24 12:28:58 +0300

    I fixed the mistakes

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,24 +7,23 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    private static final String SPECIAL_CHAR = "s";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        String readFile = readingFile(fromFileName);
-        String supplier = supply(readFile);
-        writeToFile(toFileName, supplier);
+        String data = readFromFile(fromFileName);
+        String report = supply(data);
+        writeToFile(toFileName, report);
     }
 
-    public String readingFile(String fromFile) {
+    public String readFromFile(String fromFileName) {
         StringBuilder builder = new StringBuilder();
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFile))) {
-            String value = bufferedReader.readLine();
-            while (value != null) {
-                builder.append(value).append(System.lineSeparator());
-                value = bufferedReader.readLine();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
+            String line = bufferedReader.readLine();
+            while (line != null) {
+                builder.append(line).append(System.lineSeparator());
+                line = bufferedReader.readLine();
             }
         } catch (IOException e) {
-            throw new RuntimeException("Can`t read file", e);
+            throw new RuntimeException("Can`t read file: " + fromFileName, e);
         }
         return builder.toString();
     }
@@ -32,10 +31,10 @@ public class WorkWithFile {
     public String supply(String text) {
         int summSupplies = 0;
         int summPurchases = 0;
-        String[] splited = text.split(System.lineSeparator());
-        for (String line : splited) {
-            String[] resultSplited = line.split(",");
-            if (resultSplited[0].startsWith(SPECIAL_CHAR)) {
+        String[] lines = text.split(System.lineSeparator());
+        for (String words : lines) {
+            String[] resultSplited = words.split(",");
+            if (resultSplited[0].startsWith("s")) {
                 summSupplies += Integer.parseInt(resultSplited[1]);
             } else {
                 summPurchases += Integer.parseInt(resultSplited[1]);
