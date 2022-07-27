06250ee080e2f6a70066204b2640cbe3ce903fba
Benatti <ssunica0@gmail.com>
2022-07-27 18:16:32 +0300

    fixed common mist

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -6,28 +6,49 @@ import java.io.FileReader;
 import java.io.FileWriter;
 
 public class WorkWithFile {
+    private static final int OPERATION_INDEX = 0;
+    private static final int AMMOUNT_INDEX = 1;
+    private static final int SUPPLY_INDEX = 0;
+    private static final int BUY_INDEX = 1;
+    private static final String SPLIT_CHAR = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        int[] arr = new int[2];
-        try (BufferedReader buffer = new BufferedReader(new FileReader(fromFileName))) {
-            String line = buffer.readLine();
+        int[] supplyAndBuyResult = new int[2];
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
+            String line = bufferedReader.readLine();
             while (line != null) {
-                if (line.split(",")[0].equals("supply")) {
-                    arr[0] += Integer.parseInt(line.split(",")[1]);
+                String[] splitedLine = line.split(SPLIT_CHAR);
+                if (splitedLine[OPERATION_INDEX].equals("supply")) {
+                    supplyAndBuyResult [0] += Integer.parseInt(splitedLine[AMMOUNT_INDEX]);
                 } else {
-                    arr[1] += Integer.parseInt(line.split(",")[1]);
+                    supplyAndBuyResult [1] += Integer.parseInt(splitedLine[AMMOUNT_INDEX]);
                 }
-                line = buffer.readLine();
+                line = bufferedReader.readLine();
             }
         } catch (Exception e) {
-            throw new RuntimeException("Can't write to file", e);
+            throw new RuntimeException("Can't read from file" + fromFileName, e);
         }
+        writeToFile(createReport(supplyAndBuyResult), toFileName);
+    }
+
+    private void writeToFile(String report, String toFileName) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
-            bufferedWriter.write("supply," + arr[0] + System.lineSeparator());
-            bufferedWriter.write("buy," + arr[1] + System.lineSeparator());
-            bufferedWriter.write("result," + (arr[0] - arr[1]));
+            bufferedWriter.write(report);
         } catch (Exception e) {
             throw new RuntimeException("Can't write to file", e);
         }
+    }
+
+    private String createReport(int[] supplyAndBuyResult) {
+        return new StringBuilder()
+                .append("supply,")
+                .append(supplyAndBuyResult[SUPPLY_INDEX])
+                .append(System.lineSeparator())
+                .append("buy,")
+                .append(supplyAndBuyResult[BUY_INDEX])
+                .append(System.lineSeparator())
+                .append("result,")
+                .append(supplyAndBuyResult [SUPPLY_INDEX] - supplyAndBuyResult [BUY_INDEX])
+                .append(System.lineSeparator()).toString();
     }
 }
