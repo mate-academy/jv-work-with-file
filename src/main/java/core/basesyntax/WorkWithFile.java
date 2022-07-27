1aa62c45920bb3ab57985dceb11b42bb190ce366
Vyacheslav Verbanov <v.verbanov@gmail.com>
2022-07-27 14:08:17 +0200

    delete comments, add constants

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,14 +8,14 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    //services in daily report
     private static final String SERVICE_SUPPLY = "supply";
     private static final String SERVICE_BUY = "buy";
     private static final String SERVICE_RESULT = "result";
-    //daily report line structure
     private static final int NUMBER_OF_COLUMNS = 2;
     private static final int COLUMN_WITH_NAME = 0;
     private static final int COLUMN_WITH_SUM = 1;
+    private static final String SEPARATOR_IN_CSV_FILES = ",";
+    private static final String TEMPORARY_SEPARATOR = " ";
 
     public void getStatistic(String fromFileName, String toFileName) {
         String[] dataFromFile = getStringFromFile(fromFileName);
@@ -23,24 +23,20 @@ public class WorkWithFile {
         writeReportToFile(report, toFileName);
     }
 
-    //read file, return array of Strings from the file
-    //each of the elements of array is a line in a fromFileName
     private String[] getStringFromFile(String fromFileName) {
         StringBuilder stringBuilder = new StringBuilder();
         File fileFrom = new File(fromFileName);
-
         try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(fileFrom));
             String lineFromFile = bufferedReader.readLine();
             while (lineFromFile != null) {
-                stringBuilder.append(lineFromFile).append(" ");
+                stringBuilder.append(lineFromFile).append(TEMPORARY_SEPARATOR);
                 lineFromFile = bufferedReader.readLine();
             }
         } catch (IOException e) {
             throw new RuntimeException("Can't read file" + fileFrom, e);
         }
-        //return array of Strings, each of the elements is a line in a file
-        return stringBuilder.toString().split(" ");
+        return stringBuilder.toString().split(TEMPORARY_SEPARATOR);
     }
 
     private String makeReport(String[] dataFromFile) {
@@ -50,7 +46,7 @@ public class WorkWithFile {
         String[] lineFromArray = new String[NUMBER_OF_COLUMNS];
 
         for (String data : dataFromFile) {
-            lineFromArray = data.split(",");
+            lineFromArray = data.split(SEPARATOR_IN_CSV_FILES);
             if (lineFromArray[COLUMN_WITH_NAME].equals(SERVICE_SUPPLY)) {
                 dailySupply += Integer.parseInt(lineFromArray[COLUMN_WITH_SUM]);
             } else if (lineFromArray[COLUMN_WITH_NAME].equals(SERVICE_BUY)) {
@@ -58,20 +54,15 @@ public class WorkWithFile {
             }
         }
         dailyResult = dailySupply - dailyBuy;
-
-        //collect all report information to stringBuilder
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(SERVICE_SUPPLY).append(",").append(dailySupply)
+        stringBuilder.append(SERVICE_SUPPLY).append(SEPARATOR_IN_CSV_FILES).append(dailySupply)
                 .append(System.lineSeparator())
-                .append(SERVICE_BUY).append(",").append(dailyBuy)
+                .append(SERVICE_BUY).append(SEPARATOR_IN_CSV_FILES).append(dailyBuy)
                 .append(System.lineSeparator())
-                .append(SERVICE_RESULT).append(",").append(dailyResult);
-
-        //return String with the report
+                .append(SERVICE_RESULT).append(SEPARATOR_IN_CSV_FILES).append(dailyResult);
         return stringBuilder.toString();
     }
 
-    //write to toFileName info from the String report
     private void writeReportToFile(String report, String toFileName) {
         File fileTo = new File(toFileName);
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileTo, true))) {
