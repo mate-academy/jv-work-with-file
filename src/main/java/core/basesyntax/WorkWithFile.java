47fa522a87d023d82f5e8db8c2339566154997fc
Blokitin Oleh <87024861+oleh-satoshi@users.noreply.github.com>
2022-07-28 12:54:46 +0200

    implement calculateResultForDay()

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -4,16 +4,24 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class WorkWithFile {
+    private static final String SUPPLY_CONDITION = "supply";
+    private static final String BUY_CONDITION = "buy";
+    private static final String REGEX_CONDITION = "\\D";
     private StringBuilder stringBuilder = new StringBuilder();
+    private int supplySum = 0;
+    private int buySum = 0;
+
     public void getStatistic(String fromFileName, String toFileName) {
 
-        readDataFromFile(fromFileName);
+        calculateResultForDay(fromFileName);
 
     }
+        //отдал стринг билдер
     private String readDataFromFile(String fromFileName) {
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))){
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             String value = null;
             try {
                 value = bufferedReader.readLine();
@@ -35,5 +43,28 @@ public class WorkWithFile {
             throw new RuntimeException("Can't read file " + e);
         }
         return stringBuilder.toString();
+    }
+        //отдал массив с двумя индексами
+    public int[] calculateResultForDay(String fromFileNam) {
+        String[] sortedArray = readDataFromFile(fromFileNam).split(System.lineSeparator());
+        for (String line : sortedArray) {
+            //получил массив который нужно просчитать
+            sortedArray = line.split(" ");
+            //получил число из линии
+            int numberFromLine = Integer.parseInt(line.replaceAll(REGEX_CONDITION, ""));
+            //посчитали числа
+            if (sortedArray[0].contains(SUPPLY_CONDITION)) {
+                supplySum += numberFromLine;
+            }
+            if (sortedArray[0].contains(BUY_CONDITION)){
+                buySum += numberFromLine;
+            }
+        }
+        return new int[] {buySum, supplySum};
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new WorkWithFile().calculateResultForDay("apple.csv"));
+
     }
 }
