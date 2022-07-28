6dcd07360b25042c689fbecf24aa41ec2529ff00
Blokitin Oleh <87024861+oleh-satoshi@users.noreply.github.com>
2022-07-28 08:53:31 +0200

    implement readDataFromFile()

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,8 +1,39 @@
 package core.basesyntax;
 
-public class WorkWithFile {
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
+public class WorkWithFile {
+    private StringBuilder stringBuilder = new StringBuilder();
     public void getStatistic(String fromFileName, String toFileName) {
 
+        readDataFromFile(fromFileName);
+
+    }
+    private String readDataFromFile(String fromFileName) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))){
+            String value = null;
+            try {
+                value = bufferedReader.readLine();
+            } catch (IOException e) {
+                throw new RuntimeException("Can't read file " + e);
+            }
+            while (value != null) {
+                stringBuilder.append(value).append(System.lineSeparator());
+                try {
+                    value = bufferedReader.readLine();
+                } catch (IOException e) {
+                    throw new RuntimeException("Can't write file in StringBuilder " + e);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("Can't find a file " + e);
+
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file " + e);
+        }
+        return stringBuilder.toString();
     }
 }
