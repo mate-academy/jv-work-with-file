ea8fba0f6d5959363996eab39b172d08fa302f5e
X3NON-135 <immort4ld3momo@gmail.com>
2022-07-28 16:23:24 +0100

    Implemented methods to write and read from some files

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,8 +1,77 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+
 public class WorkWithFile {
+    public static void getStatistic(String fromFileName, String toFileName) {
+        File myFile = new File(fromFileName);
+        StringBuilder stringBuilder = new StringBuilder();
+        List<Integer> listOfSupplyCosts = new ArrayList<Integer>();
+        List<Integer> listOfBuyCosts = new ArrayList<Integer>();
+        String result = calculateResult(fromFileName, listOfSupplyCosts,
+                listOfBuyCosts, stringBuilder);
+        createAndWriteToFile(toFileName, result);
+    }
 
-    public void getStatistic(String fromFileName, String toFileName) {
+    public static String calculateResult(String fromFile, List<Integer> supplies,
+                                         List<Integer> buys, StringBuilder stringBuilder) {
+        try {
+            BufferedReader bufReader = new BufferedReader(new FileReader(fromFile));
+            String string = bufReader.readLine();
+            while (string != null) {
+                String[] splitedString = string.split(" ");
 
+                for (String value : splitedString) {
+                    if (value.contains("supply")) {
+                        supplies.add(Integer.valueOf(value
+                                .substring(value.indexOf(",") + 1, value.length())));
+                    } else {
+                        buys.add(Integer.valueOf(value
+                                .substring(value.indexOf(",") + 1, value.length())));
+                    }
+                }
+                string = bufReader.readLine();
+            }
+
+            int sumOfSupplies = 0;
+            for (int number : supplies) {
+                sumOfSupplies += number;
+            }
+
+            int sumOfBuys = 0;
+            for (int number : buys) {
+                sumOfBuys += number;
+            }
+            return stringBuilder.append("supply,").append(sumOfSupplies)
+                    .append(System.lineSeparator()).append("buy,")
+                    .append(sumOfBuys).append(System.lineSeparator())
+                    .append("result,").append(sumOfSupplies - sumOfBuys).toString();
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t read data from file", e);
+        }
+    }
+
+    public static void createAndWriteToFile(String toFileName, String data) {
+        File resultFile = new File(toFileName);
+        try {
+            resultFile.createNewFile();
+        } catch (IOException e) {
+            throw new RuntimeException("Can't create file", e);
+        }
+        try {
+            Files.write(resultFile.toPath(), data.getBytes());
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write data to file", e);
+        }
+    }
+
+    public static void main(String[] args) {
+        getStatistic("apple.csv", "appleResult.csv");
     }
 }
