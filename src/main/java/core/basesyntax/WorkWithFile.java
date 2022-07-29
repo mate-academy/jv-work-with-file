1a1b1e0d4b8f9299cc101c7d6efc032ac1e4c9b5
Alexandr Semashko <semashko123456789@gmail.com>
2022-07-29 23:32:59 +0300

    improved solution, fixed exception and added string report

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,6 +2,7 @@ package core.basesyntax;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -9,24 +10,22 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class WorkWithFile {
-    private static int FIRST_INDEX = 0;
-    private static int SECOND_INDEX = 1;
+    private static final int FIRST_INDEX = 0;
+    private static final int SECOND_INDEX = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        Map<String, Integer> map;
-        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
-            map = reader.lines().collect(Collectors.groupingBy(str -> str.split(",")[FIRST_INDEX],
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
+                BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
+            Map<String, Integer> map = reader.lines().collect(Collectors
+                    .groupingBy(str -> str.split(",")[FIRST_INDEX],
                     Collectors.reducing(0, str -> Integer.valueOf(str.split(",")[SECOND_INDEX]),
                             Integer::sum)));
-        } catch (IOException e) {
+            String report = "supply," + map.get("supply") + System.lineSeparator()
+                    + "buy," + map.get("buy") + System.lineSeparator()
+                    + "result," + (map.get("supply") - map.get("buy"));
+            writer.write(report);
+        } catch (FileNotFoundException e) {
             throw new RuntimeException("Can`t read this file", e);
-        }
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
-            writer.write("supply," + map.get("supply"));
-            writer.newLine();
-            writer.write("buy," + map.get("buy"));
-            writer.newLine();
-            writer.write("result," + (map.get("supply") - map.get("buy")));
         } catch (IOException e) {
             throw new RuntimeException("Can`t write to file", e);
         }
