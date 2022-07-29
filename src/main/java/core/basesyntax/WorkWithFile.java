1377588fe2a0c798e9d13d3472792e9ad3193bca
Mikhail Maiorov <majorov.michail@gmail.com>
2022-07-29 09:35:36 +0300

    class WorkWithFile was refactored

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -6,55 +6,51 @@ import java.io.FileReader;
 import java.io.FileWriter;
 
 public class WorkWithFile {
-    private static final String supplyRow = "supply";
-    private static final String buyRow = "buy";
-    private static final String lineSeparator = ",";
-    private static String operationTypeValue;
-    private static int amountValue;
+    private static final String OPERATION_TYPE_SUPPLY = "supply";
+    private static final String OPERATION_TYPE_BUY = "buy";
+    private static final String COMMA_SEPARATOR = ",";
+    private static final int INDEX_OF_OPERATION_TYPE_VALUE = 0;
+    private static final int INDEX_OF_AMOUNT_VALUE = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        int supplySum = getFromFileSumOf(fromFileName, supplyRow);
-        int buySum = getFromFileSumOf(fromFileName, buyRow);
+        int supplySum = getOperationTypeAmountSum(fromFileName, OPERATION_TYPE_SUPPLY);
+        int buySum = getOperationTypeAmountSum(fromFileName, OPERATION_TYPE_BUY);
         writeToFile(toFileName, supplySum, buySum);
     }
 
-    private int getFromFileSumOf(String fromFileName, String rowNameValue) {
-        try (BufferedReader file = new BufferedReader(new FileReader(fromFileName))) {
+    private int getOperationTypeAmountSum(String fileName, String operationType) {
+        try (BufferedReader file = new BufferedReader(new FileReader(fileName))) {
             String line = file.readLine();
             int amountSum = 0;
 
             while (line != null) {
-                lineSplit(line);
-                amountSum += getAmountValue(rowNameValue);
+                amountSum += getAmountValue(line, operationType);
                 line = file.readLine();
             }
             return amountSum;
         } catch (Exception e) {
-            throw new RuntimeException("Can't read data from the file " + fromFileName, e);
+            throw new RuntimeException("Can't read data from the file " + fileName, e);
         }
     }
 
-    private int getAmountValue(String word) {
-        return operationTypeValue.equals(word) ? amountValue : 0;
-    }
-
-    private void lineSplit(String line) {
-        String[] lineValues = line.split(lineSeparator);
-        operationTypeValue = lineValues[0];
-        amountValue = Integer.parseInt(lineValues[1]);
+    private int getAmountValue(String line, String operationType) {
+        String[] lineValues = line.split(COMMA_SEPARATOR);
+        String operationTypeValue = lineValues[INDEX_OF_OPERATION_TYPE_VALUE];
+        int amountValue = Integer.parseInt(lineValues[INDEX_OF_AMOUNT_VALUE]);
+        return operationTypeValue.equals(operationType) ? amountValue : 0;
     }
 
     private int getResultValue(int supplySum, int buySum) {
         return supplySum - buySum;
     }
 
-    private void writeToFile(String toFileName, int supplySum, int buySum) {
-        try (BufferedWriter toFile = new BufferedWriter(new FileWriter(toFileName))) {
-            toFile.write(supplyRow + "," + supplySum + System.lineSeparator()
-                    + buyRow + "," + buySum + System.lineSeparator()
+    private void writeToFile(String toFile, int supplySum, int buySum) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFile))) {
+            bufferedWriter.write(OPERATION_TYPE_SUPPLY + "," + supplySum + System.lineSeparator()
+                    + OPERATION_TYPE_BUY + "," + buySum + System.lineSeparator()
                     + "result," + getResultValue(supplySum, buySum));
         } catch (Exception e) {
-            throw new RuntimeException("Can't write data to the file " + toFileName, e);
+            throw new RuntimeException("Can't write data to the file " + toFile, e);
         }
     }
 }
