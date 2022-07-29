a30bd2d99531c6eb5ac70b2e6a91131be0a1b8d5
Alexandr Semashko <semashko123456789@gmail.com>
2022-07-29 17:44:10 +0300

    fixed mistakes in solution

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -9,21 +9,26 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class WorkWithFile {
+    private static int FIRST_INDEX = 0;
+    private static int SECOND_INDEX = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
-                BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
-            Map<String, Integer> map = reader.lines()
-                    .collect(Collectors.groupingBy(str -> str.split(",")[0],
-                    Collectors.reducing(0, str -> Integer.valueOf(str.split(",")[1]),
+        Map<String, Integer> map;
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
+            map = reader.lines().collect(Collectors.groupingBy(str -> str.split(",")[FIRST_INDEX],
+                    Collectors.reducing(0, str -> Integer.valueOf(str.split(",")[SECOND_INDEX]),
                             Integer::sum)));
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t read this file", e);
+        }
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
             writer.write("supply," + map.get("supply"));
             writer.newLine();
             writer.write("buy," + map.get("buy"));
             writer.newLine();
             writer.write("result," + (map.get("supply") - map.get("buy")));
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Can`t write to file", e);
         }
     }
 }
