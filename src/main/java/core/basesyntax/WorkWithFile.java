2f7ee7665b274acf3f0d424f173e509f9f63e821
Igor Petrichenko <i.petrichenko@gmail.com>
2022-07-30 10:53:32 +0300

    change logic method createReport

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -10,14 +10,19 @@ public class WorkWithFile {
     private static final int AMOUNT_INDEX = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
+        String report = createReport(fromFileName, toFileName);
+        writeReportToFile(report, toFileName);
+    }
+
+    private String createReport(String fromFileName, String toFileName) {
         int totalSupply = 0;
         int totalBuy = 0;
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             String line = bufferedReader.readLine();
             while (line != null) {
-                String[] valueArray = line.split(",");
-                String operation = valueArray[OPERATION_INDEX];
-                int amount = Integer.parseInt(valueArray[AMOUNT_INDEX]);
+                String[] splitedRow = line.split(",");
+                String operation = splitedRow[OPERATION_INDEX];
+                int amount = Integer.parseInt(splitedRow[AMOUNT_INDEX]);
                 if (operation.equals("supply")) {
                     totalSupply += amount;
                 } else if (operation.equals("buy")) {
@@ -25,24 +30,20 @@ public class WorkWithFile {
                 }
                 line = bufferedReader.readLine();
             }
-            writeReportToFile(totalSupply, totalBuy, toFileName);
         } catch (Exception e) {
-            throw new RuntimeException("Can't read a file", e);
+            throw new RuntimeException("Can't read a file" + toFileName, e);
         }
-    }
-
-    private String createReport(int totalSupply, int totalBuy) {
         int result = totalSupply - totalBuy;
         return "supply" + "," + totalSupply + System.lineSeparator() + "buy"
                 + "," + totalBuy + System.lineSeparator()
                 + "result," + result;
     }
 
-    private void writeReportToFile(int totalSupply, int totalBuy, String toFileName) {
+    private void writeReportToFile(String report, String toFileName) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
-            bufferedWriter.write(createReport(totalSupply, totalBuy));
+            bufferedWriter.write(report);
         } catch (Exception e) {
-            throw new RuntimeException("Can't write a file", e);
+            throw new RuntimeException("Can't write a file" + toFileName, e);
         }
     }
 }
