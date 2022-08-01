88d99adde079764eab43b5c3ced198f11267fec7
Valentyn Chepiha <valentyn.chepiha@gmail.com>
2022-08-01 23:01:45 +0300

    Added solution

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,8 +1,33 @@
 package core.basesyntax;
 
+import core.basesyntax.services.ParseLine;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WorkWithFile {
-
     public void getStatistic(String fromFileName, String toFileName) {
-
+        File inFile = new File(fromFileName);
+        File outFile = new File(toFileName);
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(inFile));
+                 BufferedWriter bufferedWriter = new BufferedWriter(
+                         new FileWriter(outFile, true))) {
+            ParseLine parseLine = new ParseLine();
+            String line = "";
+            while ((line = bufferedReader.readLine()) != null) {
+                parseLine.run(line);
+            }
+            bufferedWriter.write(parseLine.printSumSupply());
+            bufferedWriter.write(parseLine.printSumBuy());
+            bufferedWriter.write(parseLine.printResult());
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("Can not read file " + fromFileName, e);
+        } catch (IOException e) {
+            throw new RuntimeException("Fatal error i/o", e);
+        }
     }
 }
---
+++ src/main/java/core/basesyntax/services/ParseLine.java
@@ -0,0 +1,31 @@
+package core.basesyntax.services;
+
+public class ParseLine {
+    private int sumBuy = 0;
+    private int sumSupply = 0;
+
+    public void run(String lineText) {
+        if (lineText == null) {
+            return;
+        }
+        String[] dataLine = lineText.split(",");
+        if ("buy".equals(dataLine[0])) {
+            sumBuy += Integer.parseInt(dataLine[1]);
+        }
+        if ("supply".equals(dataLine[0])) {
+            sumSupply += Integer.parseInt(dataLine[1]);
+        }
+    }
+
+    public String printSumBuy() {
+        return "buy," + sumBuy + System.lineSeparator();
+    }
+
+    public String printSumSupply() {
+        return "supply," + sumSupply + System.lineSeparator();
+    }
+
+    public String printResult() {
+        return "result," + (sumSupply - sumBuy);
+    }
+}
