0610073be965e06724e074d1afa3206f42722f5a
Anatoliy Olkhovskiy <Anatoltbcm@gmail.com>
2022-08-01 22:17:58 +0300

    split the getStatistic method into several

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -13,6 +13,20 @@ import java.util.List;
 public class WorkWithFile {
 
     public void getStatistic(String fromFileName, String toFileName) {
+        int supply = 0;
+        int buy = 0;
+        for (String string : readFileToList(fromFileName)) {
+            String[] stringArray = string.split("\\,");
+            if (stringArray[0].equals("supply")) {
+                supply += Integer.parseInt(stringArray[1]);
+            } else {
+                buy += Integer.parseInt(stringArray[1]);
+            }
+        }
+        writeToFile(toFileName, supply, buy);
+    }
+
+    private static List<String> readFileToList(String fromFileName) {
         File fromFile = new File(fromFileName);
         List<String> list = new ArrayList<>();
         try {
@@ -27,23 +41,16 @@ public class WorkWithFile {
         } catch (IOException e) {
             throw new RuntimeException("Can't read file" + fromFile, e);
         }
-        int supply = 0;
-        int buy = 0;
-        for (String string : list) {
-            String[] stringArray = string.split("\\,");
-            if (stringArray[0].equals("supply")) {
-                supply += Integer.parseInt(stringArray[1]);
-            } else {
-                buy += Integer.parseInt(stringArray[1]);
-            }
-        }
-        int result = supply - buy;
+        return list;
+    }
+
+    private static void writeToFile(String toFileName, int supply, int buy) {
         File toFile = new File(toFileName);
         try {
             BufferedWriter writer = new BufferedWriter(new FileWriter(toFile));
             writer.write("supply," + supply + System.lineSeparator());
             writer.write("buy," + buy + System.lineSeparator());
-            writer.write("result," + result);
+            writer.write("result," + (supply - buy));
             writer.close();
         } catch (IOException e) {
             throw new RuntimeException("Can't write to file" + toFile, e);
