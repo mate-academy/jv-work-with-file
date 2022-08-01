d962e466894a45b9a1f292ed59dc822014b9a8eb
Myroslav_Voskoboinyk <maestro.of.pain@gmail.com>
2022-08-01 14:40:17 +0300

    Method getStatistic has been split

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -5,38 +5,56 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.ArrayList;
 
 public class WorkWithFile {
+    private static final int OPERATION_COLUMN = 0;
+    private static final int AMOUNT_COLUMN = 1;
     private static final String SUPPLY = "supply";
     private static final String BUY = "buy";
     private static final String REGEX = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        File fromFile = new File(fromFileName);
-        File toFile = new File(toFileName);
         int supply = 0;
         int buy = 0;
+        String[] strings = reedToStringArray(fromFileName);
+        for (String string : strings) {
+            String[] split = string.split(REGEX);
+            if (split[OPERATION_COLUMN].equals(SUPPLY)) {
+                supply += Integer.parseInt(split[AMOUNT_COLUMN]);
+            }
+            if (split[OPERATION_COLUMN].equals(BUY)) {
+                buy += Integer.parseInt(split[AMOUNT_COLUMN]);
+            }
+        }
+        writeToFile(toFileName, supply, buy);
+    }
+
+    private String[] reedToStringArray(String fromFileName) {
+        File file = new File(fromFileName);
+        ArrayList<String> arrayList = new ArrayList<>();
         try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFile));
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
             String string = bufferedReader.readLine();
             while (string != null) {
-                if (string.substring(0, string.indexOf(REGEX)).equals(SUPPLY)) {
-                    supply += Integer.parseInt(string.substring(string.indexOf(REGEX) + 1));
-                }
-                if (string.substring(0, string.indexOf(REGEX)).equals(BUY)) {
-                    buy += Integer.parseInt(string.substring(string.indexOf(REGEX) + 1));
-                }
+                arrayList.add(string);
                 string = bufferedReader.readLine();
             }
         } catch (Exception e) {
-            throw new RuntimeException("Can't read data from the file " + fromFile, e);
+            throw new RuntimeException("Can't read data from the file " + file, e);
         }
+        return arrayList.toArray(new String[arrayList.size()]);
+    }
+
+    private void writeToFile(String toFileName, int supply, int buy) {
+        File file = new File(toFileName);
         int result = supply - buy;
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFile))) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
             bufferedWriter.write("supply," + supply + System.lineSeparator()
-                    + "buy," + buy + System.lineSeparator() + "result," + result);
+                    + "buy," + buy + System.lineSeparator()
+                    + "result," + result);
         } catch (Exception e) {
-            throw new RuntimeException("Can't write data to the file " + toFile, e);
+            throw new RuntimeException("Can't write data to the file " + file, e);
         }
     }
 }
