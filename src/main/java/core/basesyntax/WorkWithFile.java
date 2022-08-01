7b691bb525cc9f93bec1358dc85681320fa9a244
Volodymyr Barasiuk <barmalej23@gmail.com>
2022-08-01 16:28:50 +0300

    code updated with recommendations, used split instead of substring

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,8 +1,47 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WorkWithFile {
 
     public void getStatistic(String fromFileName, String toFileName) {
+        int outputBuy = 0;
+        int outputSupply = 0;
+        File fileFrom = new File(fromFileName);
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileFrom))) {
+            String transaction = bufferedReader.readLine();
+            while (transaction != null) {
+                String[] trasactionInfoSplitted = transaction.split(",");
+                if (trasactionInfoSplitted[0].equals("buy")) {
+                    outputBuy = outputBuy + Integer.parseInt(trasactionInfoSplitted[1]);
+                } else {
+                    outputSupply = outputSupply + Integer.parseInt(trasactionInfoSplitted[1]);
+                }
+                transaction = bufferedReader.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("File not found" + fromFileName, e);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read the file" + fromFileName, e);
+        }
+        sendStatisticToFile(outputBuy, outputSupply, toFileName);
+    }
 
+    private void sendStatisticToFile(int outputBuy, int outputSupply, String fileTo) {
+        int result = outputSupply - outputBuy;
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileTo))) {
+            bufferedWriter.write("supply," + outputSupply + System.lineSeparator()
+                    + "buy," + outputBuy + System.lineSeparator()
+                    + "result," + result);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write data to file" + fileTo, e);
+        }
     }
 }
+
