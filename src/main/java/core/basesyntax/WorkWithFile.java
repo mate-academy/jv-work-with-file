ceb5b687471859e7f5ce1b9870d97a2663a86759
Vasyl Pankiv <wasilius2@gmail.com>
2022-08-01 17:39:57 +0300

    add writeStatisticToFile method

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,20 +1,32 @@
 package core.basesyntax;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
     private static final String SUPPLY_OPERATION_TYPE = "supply";
     private static final String BUY_OPERATION_TYPE = "buy";
+    private static final String RESULT = "result";
     private static final String COMA_SEPARATOR = ",";
 
     public void getStatistic (String fromFileName, String toFileName) {
         String[] lines = readFileByLines(fromFileName);
-        int[] statistic = countStatistic(lines);
+        String statistic = countStatistic(lines);
+        writeStatisticToFile(toFileName,statistic);
     }
 
-    private int[] countStatistic (String[] strings) {
+    private void writeStatisticToFile (String toFileName, String statistic) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName, true))) {
+            bufferedWriter.write(statistic);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write data to file " + toFileName, e);
+        }
+    }
+
+    private String countStatistic (String[] strings) {
         int buyAmount = 0;
         int supplyAmount = 0;
         for (String string : strings) {
@@ -25,7 +37,13 @@ public class WorkWithFile {
                 buyAmount += Integer.parseInt(operation[1]);
             }
         }
-        return new int[]{supplyAmount, buyAmount, supplyAmount - buyAmount};
+        return new StringBuilder()
+                .append(SUPPLY_OPERATION_TYPE).append(COMA_SEPARATOR)
+                .append(supplyAmount).append(System.lineSeparator())
+                .append(BUY_OPERATION_TYPE).append(COMA_SEPARATOR)
+                .append(buyAmount).append(System.lineSeparator())
+                .append(RESULT).append(buyAmount)
+                .toString();
     }
 
     private String[] readFileByLines (String fileName) {
@@ -37,7 +55,7 @@ public class WorkWithFile {
                 line = bufferedReader.readLine();
             }
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Can't read file " + fileName, e);
         }
         return stringBuilder.toString().split(System.lineSeparator());
     }
