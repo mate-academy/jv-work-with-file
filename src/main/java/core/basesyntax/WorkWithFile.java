b84a683ef9efdb158774cfa6bcfd727497d8da07
Mykyta Hulianytskyi <yadiver82@gmail.com>
2022-08-01 22:09:16 +0300

    v1.0 created separate methods for reading, calculating results and writing report to the file, all remarks were fixed.

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -13,35 +13,20 @@ import java.nio.file.StandardOpenOption;
 import java.util.Arrays;
 
 public class WorkWithFile {
-    private static final int OPERATION_NAME_INDEX = 0;
-    private static final int VALUE_INDEX = 1;
-    private static final int SUPPLY_VALUE_INDEX = 0;
-    private static final int BUY_VALUE_INDEX = 1;
-    private static final String BUY_NAME = "buy";
-    private static final String SUPPLY_NAME = "supply";
-    private static final String RESULT_NAME = "result";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        int[] readInfo = readFromFile(fromFileName);
-        String result = calculateResult(readInfo);
+        String[] data = readFromFile(fromFileName);
+        String result = createStatistic(data);
         writeToFile(result, toFileName);
     }
 
-    private int[] readFromFile(String fileName) {
-        int buyValue = 0;
-        int supplyValue = 0;
-        int[] values = new int[BUY_VALUE_INDEX + 1];
-        String[] lineValueArray;
+    private String[] readFromFile(String fileName) {
+        StringBuilder dataBuilder = new StringBuilder();
         File file = new File(fileName);
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
             String lineValue = bufferedReader.readLine();
             while (lineValue != null) {
-                lineValueArray = lineValue.split(",");
-                if (lineValueArray[OPERATION_NAME_INDEX].equals(BUY_NAME)) {
-                    buyValue += Integer.parseInt(lineValueArray[VALUE_INDEX]);
-                } else {
-                    supplyValue += Integer.parseInt(lineValueArray[VALUE_INDEX]);
-                }
+                dataBuilder.append(lineValue).append(",");
                 lineValue = bufferedReader.readLine();
             }
         } catch (FileNotFoundException e) {
@@ -49,37 +34,36 @@ public class WorkWithFile {
         } catch (IOException ex) {
             throw new RuntimeException("Can't read the file", ex);
         }
-        values[SUPPLY_VALUE_INDEX] = supplyValue;
-        values[BUY_VALUE_INDEX] = buyValue;
-        System.out.println(Arrays.toString(values));
-        return values;
+        return dataBuilder.toString().split(",");
     }
 
-    private String calculateResult(int[] inputValues) {
-        int resultValue = inputValues[SUPPLY_VALUE_INDEX] - inputValues[BUY_VALUE_INDEX];
-        StringBuilder result = new StringBuilder(SUPPLY_NAME);
-        result = result.append(",").append(inputValues[SUPPLY_VALUE_INDEX])
-                .append(System.lineSeparator()).append(BUY_NAME).append(",")
-                .append(inputValues[BUY_VALUE_INDEX]).append(System.lineSeparator())
-                .append(RESULT_NAME).append(",").append(resultValue);
-        System.out.println(result);
-        return String.valueOf(result);
+    private String createStatistic(String[] data) {
+        int supplyValue = 0;
+        int buyValue = 0;
+        StringBuilder statisticBuilder = new StringBuilder("supply");
+        for (int i = 0; i < data.length; i += 2) {
+            if (data[i].equals("supply")) {
+                supplyValue += Integer.parseInt(data[i + 1]);
+            } else {
+                buyValue += Integer.parseInt(data[i + 1]);
+            }
+        }
+        int result = supplyValue - buyValue;
+        statisticBuilder.append(",").append(supplyValue)
+                .append(System.lineSeparator()).append("buy").append(",")
+                .append(buyValue).append(System.lineSeparator())
+                .append("result").append(",").append(result);
+        return statisticBuilder.toString();
     }
 
     public void writeToFile(String result, String toFileName) {
         Path filePath = Paths.get(toFileName);
-        if (!Files.exists(filePath)) {
-            try {
-                Files.createFile(filePath);
-            } catch (IOException e) {
-                throw new RuntimeException("Can't create a file", e);
-            }
-        }
         try {
+            Files.createFile(filePath);
             Files.write(filePath, result.getBytes(StandardCharsets.UTF_8),
                     StandardOpenOption.APPEND);
         } catch (IOException e) {
-            throw new RuntimeException("Can't write to filePath", e);
+            throw new RuntimeException("Can't create a file", e);
         }
     }
 }
