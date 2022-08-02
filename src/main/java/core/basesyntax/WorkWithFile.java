531803e1cd9849cecd099a3a28927f032218d975
Alex Alexandrov <alexandrov.alex.al@gmail.com>
2022-08-02 20:45:39 +0300

    Added minor edits to the overall solution.

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,36 +7,37 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    private static final int FIRST_INDEX = 0;
+    private static final int AMOUNT_INDEX = 1;
+    private static final String LINE_SEPARATOR = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName));
                  BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
             int supplyAmount = 0;
             int buyAmount = 0;
-            String line;
+            String line = bufferedReader.readLine();
             String[] split;
-            do {
-                line = bufferedReader.readLine();
-                if (line == null) {
-                    break;
-                }
-                split = line.split(",");
-                switch (split[0]) {
+
+            while (line != null) {
+                split = line.split(LINE_SEPARATOR);
+                switch (split[FIRST_INDEX]) {
                     case "supply":
-                        supplyAmount += Integer.parseInt(split[1]);
+                        supplyAmount += Integer.parseInt(split[AMOUNT_INDEX]);
                         break;
                     case "buy":
-                        buyAmount += Integer.parseInt(split[1]);
+                        buyAmount += Integer.parseInt(split[AMOUNT_INDEX]);
                         break;
                     default:
                         break;
                 }
-            } while (line != null);
+                line = bufferedReader.readLine();
+            }
             bufferedWriter.write("supply," + supplyAmount + System.lineSeparator()
                     + "buy," + buyAmount + System.lineSeparator()
                     + "result," + (supplyAmount - buyAmount));
         } catch (IOException e) {
-            throw new RuntimeException("Is not possible to find a file.");
+            throw new RuntimeException("Is not possible to find a file.", e);
         }
     }
 }
