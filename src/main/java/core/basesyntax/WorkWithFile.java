ff5438f57ba343dd86b353a32f67d0b07cc96806
Oleksandr-Tovstyi <raketaplaneta@gmail.com>
2022-08-02 13:30:42 +0300

    method read was added

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,41 +1,46 @@
 package core.basesyntax;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
 
 public class WorkWithFile {
     public static final int AMMOUNT = 1;
     private static final String DELIMITER = ",";
     private static final int OPERATION_TYPE = 0;
     private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+    private static final String RESULT = "result";
 
     public void getStatistic(String fromFileName, String toFileName) {
         int totalSupply = 0;
         int totalBuy = 0;
-        File file = new File(fromFileName);
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
-            String value = bufferedReader.readLine();
-            while (value != null) {
-                String[] tempArray = value.split(DELIMITER);
-                if (tempArray[OPERATION_TYPE].equals(SUPPLY)) {
-                    totalSupply += Integer.parseInt(tempArray[AMMOUNT]);
-                } else {
-                    totalBuy += Integer.parseInt(tempArray[AMMOUNT]);
-                }
-                value = bufferedReader.readLine();
+
+        List<String> fileContent = read(fromFileName);
+        String[] tempArray;
+        for (int i = 0; i < fileContent.size(); i++) {
+            tempArray = fileContent.get(i).split(DELIMITER);
+            if (tempArray[OPERATION_TYPE].equals(SUPPLY)) {
+                totalSupply += Integer.parseInt(tempArray[AMMOUNT]);
+            } else {
+                totalBuy += Integer.parseInt(tempArray[AMMOUNT]);
             }
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException("File not found " + fromFileName + " ", e);
-        } catch (IOException e) {
-            throw new RuntimeException("Can't read a file" + fromFileName + " ", e);
         }
         String report = createReport(totalSupply, totalBuy, totalSupply - totalBuy);
         writeStatistic(toFileName, report);
+    }
+
+    private List<String> read(String fileName) {
+        File file = new File(fileName);
+        try {
+            List<String> list = Files.readAllLines(file.toPath());
+            return list;
+        } catch (IOException e) {
+            throw new RuntimeException("Cant read file", e);
+        }
     }
 
     private void writeStatistic(String toFileName, String report) {
@@ -48,12 +53,10 @@ public class WorkWithFile {
     }
 
     private String createReport(int totalSupply, int totalBuy, int totalResult) {
-        final String buy = "buy";
-        final String result = "result";
         StringBuilder stringBuilder = new StringBuilder();
         return stringBuilder.append(SUPPLY).append(DELIMITER).append(totalSupply)
                 .append(System.lineSeparator())
-                .append(buy).append(DELIMITER).append(totalBuy).append(System.lineSeparator())
-                .append(result).append(DELIMITER).append(totalResult).toString();
+                .append(BUY).append(DELIMITER).append(totalBuy).append(System.lineSeparator())
+                .append(RESULT).append(DELIMITER).append(totalResult).toString();
     }
 }
