80ce2863023e45e1f2950d340643febbc86cdb9a
buuufy <s1mpleworkpost@gmail.com>
2022-08-03 00:26:05 +0300

    add constants / renamed fields / adder result

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -6,10 +6,16 @@ import java.io.FileReader;
 import java.io.FileWriter;
 
 public class WorkWithFile {
+    public static final String COMMA = ",";
+    public static final String SUPPLY = "supply";
+    public static final String BUY = "buy";
+    public static final int FIRST_INDEX = 0;
+    public static final int SECOND_INDEX = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
         readDataFromFile(fromFileName);
-        writeDataToFile(toFileName, totalStatCalculation(readDataFromFile(fromFileName)));
+        calculateResult(readDataFromFile(fromFileName));
+        writeDataToFile(toFileName, calculateResult(readDataFromFile(fromFileName)));
     }
 
     private String[] readDataFromFile(String fromFileName) {
@@ -26,29 +32,29 @@ public class WorkWithFile {
         return builder.toString().split(System.lineSeparator());
     }
 
-    private String totalStatCalculation(String[] splitStrings) {
+    private String calculateResult(String[] splitStrings) {
         int supplySum = 0;
         int buySum = 0;
         for (String currentString : splitStrings) {
-            String[] twoValues = currentString.split(",");
-            if (twoValues[0].equals("supply")) {
-                supplySum += Integer.parseInt(twoValues[1]);
+            String[] twoValues = currentString.split(COMMA);
+            if (twoValues[FIRST_INDEX].equals(SUPPLY)) {
+                supplySum += Integer.parseInt(twoValues[SECOND_INDEX]);
             }
-            if (twoValues[0].equals("buy")) {
-                buySum += Integer.parseInt(twoValues[1]);
+            if (twoValues[FIRST_INDEX].equals(BUY)) {
+                buySum += Integer.parseInt(twoValues[SECOND_INDEX]);
             }
         }
         int result = supplySum - buySum;
 
-        return new StringBuilder().append("supply,")
+        return new StringBuilder().append(SUPPLY).append(COMMA)
                 .append(supplySum).append(System.lineSeparator())
-                .append("buy,").append(buySum).append(System.lineSeparator())
-                .append("result,").append(result).toString();
+                .append(BUY).append(COMMA).append(buySum).append(System.lineSeparator())
+                .append("result").append(COMMA).append(result).toString();
     }
 
-    private void writeDataToFile(String toFileName, String totalStatCalculation) {
+    private void writeDataToFile(String toFileName, String result) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName, true))) {
-            writer.write(totalStatCalculation);
+            writer.write(result);
         } catch (Exception e) {
             throw new RuntimeException("Can't write data to file:" + toFileName, e);
         }
