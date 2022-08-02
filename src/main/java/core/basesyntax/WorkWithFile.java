34cd91742f033f843ba8d1c124ac15486efae8b1
Oleksandr Gotin <alekander.gotin@gmail.com>
2022-08-02 19:30:45 +0300

    code is redone

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,46 +8,57 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    private static final int INDEX_ZERO = 0;
+    private static final int INDEX_ONE = 1;
+
     public void getStatistic(String fromFileName, String toFileName) {
-        String infoFromFile = getData(fromFileName);
+        String infoFromFile = readFromFile(fromFileName);
+        String report = formStatistic(infoFromFile);
+        writeToFile(report, toFileName);
+    }
+
+    private void writeToFile(String report, String toFileName) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
-            bufferedWriter.write(getResult(infoFromFile));
+            bufferedWriter.write(report);
         } catch (IOException e) {
-            throw new RuntimeException("Can't write file " + toFileName + e);
+            throw new RuntimeException("Can't write data to file: " + toFileName + e);
         }
     }
 
-    private String getData(String file) {
+    private String readFromFile(String fromFileName) {
         StringBuilder reportBuilder = new StringBuilder();
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                reportBuilder.append(line + " ");
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
+            String message = bufferedReader.readLine();
+            while (message != null) {
+                reportBuilder.append(message).append(" ");
+                message = bufferedReader.readLine();
             }
         } catch (FileNotFoundException e) {
-            throw new RuntimeException("File not found " + file, e);
+            throw new RuntimeException("File not found " + fromFileName, e);
         } catch (IOException e) {
-            throw new RuntimeException("Can't read file " + file, e);
+            throw new RuntimeException("Can't read file " + fromFileName, e);
         }
         return reportBuilder.toString();
     }
 
-    private String getResult(String data) {
-        int resultSupple = 0;
+    private String formStatistic(String data) {
+        StringBuilder builder = new StringBuilder();
+        String [] dataLine = data.split(" ");
+        int resultSupply = 0;
         int resultBuy = 0;
-        int resultTotal = 0;
-        String[] strings = data.split(",");
-        for (int i = 0; i < strings.length; i++) {
-            if (strings[i].contains("buy")) {
-                resultBuy += Integer.parseInt(strings[i + 1].replaceAll("[^0-9]", ""));
-            } else if (strings[i].contains("supply")) {
-                resultSupple += Integer.parseInt(
-                        strings[i + 1].replaceAll("[^0-9]", ""));
+        for (String record : dataLine) {
+            String[] lines = record.split(",");
+            if (lines[INDEX_ZERO].equals("buy")) {
+                resultBuy += Integer.parseInt(lines[INDEX_ONE]);
+            } else {
+                resultSupply += Integer.parseInt(lines[INDEX_ONE]);
             }
         }
-        resultTotal = resultSupple - resultBuy;
-        return "supply," + resultSupple + System.lineSeparator()
-                + "buy," + resultBuy + System.lineSeparator()
-                + "result," + resultTotal;
+        int resultTotal = resultSupply - resultBuy;
+        builder.append("supply").append(",").append(resultSupply)
+                .append(System.lineSeparator()).append("buy").append(",")
+                .append(resultBuy).append(System.lineSeparator()).append("result")
+                .append(",").append(resultTotal);
+        return builder.toString();
     }
 }
