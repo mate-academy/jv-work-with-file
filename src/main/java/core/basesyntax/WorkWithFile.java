7a4106d06be1adbb1822d8bf307a50168f822cd1
mr-Claster <67782747+mr-Claster@users.noreply.github.com>
2022-08-03 16:09:40 +0300

    I did my homework

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,8 +1,43 @@
 package core.basesyntax;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Collections;
+import java.util.List;
+
 public class WorkWithFile {
+    private static final String STRING_BUY = "buy";
+    private static final String STRING_SUPPLY = "supply";
+    private static final String STRING_СOMA = ",";
+    private static final String NEW_LINE = "\r\n";
+    private static final int ZERO = 0;
+    private static final int ONE = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
-
+        File fileReader = new File(fromFileName);
+        File fileWriter = new File(toFileName);
+        try {
+            List<String> lines = Files.readAllLines(fileReader.toPath());
+            int buy = ZERO;
+            int supply = ZERO;
+            for (String line : lines) {
+                switch (line.substring(ZERO, line.indexOf(STRING_СOMA))) {
+                    case STRING_SUPPLY :
+                        supply += Integer.parseInt(line.substring(line.indexOf(STRING_СOMA) + ONE));
+                        break;
+                    case STRING_BUY :
+                        buy += Integer.parseInt(line.substring(line.indexOf(STRING_СOMA) + ONE));
+                        break;
+                    default:
+                }
+            }
+            Files.write(fileWriter.toPath(), Collections.singleton(
+                    STRING_SUPPLY + STRING_СOMA + supply
+                    + NEW_LINE + STRING_BUY + STRING_СOMA + buy
+                    + NEW_LINE + "result" + STRING_СOMA + (supply - buy)));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
