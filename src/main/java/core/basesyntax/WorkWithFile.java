2fe3bc74d418326d457f70ef328803d4c9091be0
Andrii Nesterenko <nesterenkoandrii23@gmail.com>
2022-08-03 15:38:58 +0300

    added methods to read from and to write to file

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,10 +1,11 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 public class WorkWithFile {
     private static final String SUPPLY_ROW = "supply";
@@ -15,12 +16,8 @@ public class WorkWithFile {
     private static final String LINE_SPLITTER = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        String fileContent;
-        try {
-            fileContent = Files.readString(Path.of(fromFileName));
-        } catch (IOException e) {
-            throw new RuntimeException("Can't read from file" + fromFileName, e);
-        }
+        String fileContent = readFromFile(fromFileName);
+
         fileContent = replaceSymbols(fileContent);
         String[] appleArray = fileContent.split(LINE_SPLITTER);
         String[] categoriesArray = {SUPPLY_ROW, BUY_ROW};
@@ -42,35 +39,52 @@ public class WorkWithFile {
                 .append(getBuyRowForReport(buyAmount))
                 .append(getResultRowForReport(resultAmount));
 
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
-            bufferedWriter.write(reportToString(reportStringBuilder));
-        } catch (IOException e) {
-            throw new RuntimeException("Can't write to file" + toFileName, e);
-        }
+        writeToFile(reportStringBuilder, toFileName);
     }
 
-    public String replaceSymbols(String fileContent) {
+    private String replaceSymbols(String fileContent) {
         return fileContent.replace("\n", LINE_SPLITTER)
                 .replace("\r", "")
                 .replaceAll(".$", "");
     }
 
-    public String getSupplyRowForReport(int supplyAmount) {
+    private String getSupplyRowForReport(int supplyAmount) {
         return SUPPLY_ROW + LINE_SPLITTER
                 + supplyAmount + System.lineSeparator();
     }
 
-    public String getBuyRowForReport(int buyAmount) {
+    private String getBuyRowForReport(int buyAmount) {
         return BUY_ROW + LINE_SPLITTER
                 + buyAmount + System.lineSeparator();
     }
 
-    public String getResultRowForReport(int resultAmount) {
+    private String getResultRowForReport(int resultAmount) {
         return RESULT_ROW + LINE_SPLITTER
                 + resultAmount;
     }
 
-    public String reportToString(StringBuilder reportStringBuilder) {
-        return String.valueOf(reportStringBuilder);
+    private void writeToFile(StringBuilder reportStringBuilder, String toFileName) {
+        File file = new File(toFileName);
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+            bufferedWriter.write(String.valueOf(reportStringBuilder));
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write to file" + toFileName, e);
+        }
+    }
+
+    private String readFromFile(String fromFileName) {
+        File file = new File(fromFileName);
+        StringBuilder stringBuilder = new StringBuilder();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String value = reader.readLine();
+            while (value != null) {
+                stringBuilder.append(value).append(System.lineSeparator());
+                value = reader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read from file" + fromFileName, e);
+        }
+        return stringBuilder.toString();
     }
 }
