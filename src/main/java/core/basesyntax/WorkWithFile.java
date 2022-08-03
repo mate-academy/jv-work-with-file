dd6857de0fca2260b078240602a165e7d6be4d77
Heorhiy Nezbrytskiy <georgiy.nezbritskiy@gmail.com>
2022-08-03 08:37:01 +0300

    fix mistakes using Kseniia's tips

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,23 +8,23 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    private static final String FILE_SEPARATOR = System.lineSeparator();
+    private static final String COMA_SEPARATOR = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        WorkWithFile workWithFile = new WorkWithFile();
-        int[] report = workWithFile.readFromFile(fromFileName);
-        workWithFile.writeToFile(toFileName, report);
+        int[] report = readFromFile(fromFileName);
+        writeToFile(toFileName, report);
 
     }
 
     private int[] readFromFile(String fromFileName) {
         File fromFile = new File(fromFileName);
         int[] report = {0, 0};
-        try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName));
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             String value = bufferedReader.readLine();
             int parseInt = 0;
             while (value != null) {
-                parseInt = Integer.parseInt(value.split(",")[1]);
+                parseInt = Integer.parseInt(value.split(COMA_SEPARATOR)[1]);
                 if (value.startsWith("supply")) {
                     report[0] += parseInt;
                 } else {
@@ -33,7 +33,7 @@ public class WorkWithFile {
                 value = bufferedReader.readLine();
             }
         } catch (IOException e) {
-            throw new RuntimeException("Can't read data from file");
+            throw new RuntimeException("Can't read data from file " + fromFileName, e);
         }
         return report;
     }
@@ -41,12 +41,12 @@ public class WorkWithFile {
     private void writeToFile(String toFileName, int[] report) {
         File toFile = new File(toFileName);
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName, true))) {
-            bufferedWriter.write("supply," + report[0]
-                    + System.lineSeparator() + "buy," + report[1]
-                    + System.lineSeparator() + "result,"
-                    + (report[0] - report[1]));
+            bufferedWriter.write(new StringBuilder().append("supply,").append(report[0])
+                    .append(FILE_SEPARATOR).append("buy,").append(report[1])
+                    .append(FILE_SEPARATOR).append("result,")
+                    .append(report[0] - report[1]).toString());
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Can't writhe to file " + toFileName, e);
         }
     }
 }
