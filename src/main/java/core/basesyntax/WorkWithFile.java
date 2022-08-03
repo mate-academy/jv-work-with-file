ee4c6da68d9bbb0e6bb79c51268dc5ce3c3c8b12
Aptypio13 <aptypio668@gmail.com>
2022-08-03 14:12:37 +0300

    I`m so sorry for using russian at previous PR:) i just forget about it:) I make a tree methods to refactor code and replace a parameters outside of method because i have to use it in different methods

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,29 +7,42 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class WorkWithFile {
+    private static final String REGEX = "\\D+";
+    private int supplyCount = 0;
+    private int buyCount = 0;
+
     public void getStatistic(String fromFileName, String toFileName) {
-        try (FileWriter fileWriter = new FileWriter(toFileName)) {
-            if (fromFileName == null) {
-                throw new RuntimeException("No such file");
-            }
+        getCounts(fromFileName);
+        int resultCount = supplyCount - buyCount;
+        writeToFile(toFileName,
+                    "supply," + this.supplyCount + "\n"
+                            + "buy," + this.buyCount + "\n"
+                            + "result," + resultCount + "\n");
+    }
+
+    private void getCounts(String fromFileName) {
+        if (fromFileName == null) {
+            throw new RuntimeException("No such file" + fromFileName);
+        }
+        try {
             List<String> stringList = Files.readAllLines(Paths.get(fromFileName));
-            int supplyCount = 0;
-            int buyCount = 0;
-            int resultCount;
             for (String line : stringList) {
                 if (line.startsWith("supply")) {
-                    supplyCount += Integer.parseInt(line.replaceAll("\\D+",""));
+                    this.supplyCount += Integer.parseInt(line.replaceAll(REGEX, ""));
                 } else if (line.startsWith("buy")) {
-                    buyCount += Integer.parseInt(line.replaceAll("\\D+",""));
+                    this.buyCount += Integer.parseInt(line.replaceAll(REGEX, ""));
                 }
             }
-            resultCount = supplyCount - buyCount;
-            fileWriter.write("supply," + supplyCount + "\n");
-            fileWriter.write("buy," + buyCount + "\n");
-            fileWriter.write("result," + resultCount + "\n");
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Can`t read a file" + e);
         }
     }
 
+    private void writeToFile(String toFileName, String value) {
+        try (FileWriter fileWriter = new FileWriter(toFileName)) {
+            fileWriter.write(value);
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t write to file", e);
+        }
+    }
 }
