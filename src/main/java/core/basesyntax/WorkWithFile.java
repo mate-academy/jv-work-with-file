3abc56c6d092304dfe8ef1af486f3e1a3055da24
Sofiia <sofiakazzz@gmail.com>
2022-08-03 12:50:49 +0300

    Added 2 variables with informative names instead of  array

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -6,21 +6,21 @@ import java.io.FileReader;
 import java.io.FileWriter;
 
 public class WorkWithFile {
-    private static final int INDEX = 0;
-    private static final int AMMOUNT_INDEX = 1;
-    private static final int SUPPLY_INDEX = 0;
-    private static final int BUY_INDEX = 1;
+
+    private int[] result = new int[2];
+    private int totalSupply = result[0];
+    private int totalBuy = result[1];
 
     public void getStatistic(String fromFileName, String toFileName) {
-        int[] result = new int[2];
+
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             String line = bufferedReader.readLine();
             while (line != null) {
                 String[] splitedLine = line.split(",");
-                if (splitedLine[INDEX].equals("supply")) {
-                    result[0] += Integer.parseInt(splitedLine[AMMOUNT_INDEX]);
+                if (splitedLine[0].equals("supply")) {
+                    totalSupply += Integer.parseInt(splitedLine[1]);
                 } else {
-                    result[1] += Integer.parseInt(splitedLine[AMMOUNT_INDEX]);
+                    totalBuy += Integer.parseInt(splitedLine[1]);
                 }
                 line = bufferedReader.readLine();
             }
@@ -40,13 +40,13 @@ public class WorkWithFile {
 
     private String formReportFrom(int[] result) {
         return "supply,"
-                + result[SUPPLY_INDEX]
+                + totalSupply
                 + System.lineSeparator()
                 + "buy,"
-                + result[BUY_INDEX]
+                + totalBuy
                 + System.lineSeparator()
                 + "result,"
-                + (result[SUPPLY_INDEX] - result[BUY_INDEX])
+                + (totalSupply - totalBuy)
                 + System.lineSeparator();
     }
 }
