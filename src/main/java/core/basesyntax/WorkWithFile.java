93713f705c551f8b0c63012d44bb7928396d9b4c
Aptypio13 <aptypio668@gmail.com>
2022-08-03 20:46:08 +0300

    I don`t think that a creation of a new object just to print one line its such a good idea, but if you with, I was refactored my code

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,41 +8,54 @@ import java.util.List;
 
 public class WorkWithFile {
     private static final String REGEX = "\\D+";
-    private int supplyCount = 0;
-    private int buyCount = 0;
+    private static final int SUPPLY_INDEX = 0;
+    private static final int BUY_INDEX = 1;
+    private static final int RESULT_INDEX = 2;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        getCounts(fromFileName);
-        int resultCount = supplyCount - buyCount;
-        writeToFile(toFileName,
-                    "supply," + this.supplyCount + "\n"
-                            + "buy," + this.buyCount + "\n"
-                            + "result," + resultCount + "\n");
+        int[] resultCount = getCounts(fromFileName);
+        StringBuilder statistic = new StringBuilder();
+        writeToFile(
+                toFileName, String.valueOf(statistic.append(System.lineSeparator())
+                        .append("supply,")
+                        .append(resultCount[0])
+                        .append(System.lineSeparator())
+                        .append("buy,")
+                        .append(resultCount[1])
+                        .append(System.lineSeparator())
+                        .append("result,")
+                        .append(resultCount[2])
+                        .append(System.lineSeparator())));
     }
 
-    private void getCounts(String fromFileName) {
+    private int[] getCounts(String fromFileName) {
         if (fromFileName == null) {
-            throw new RuntimeException("No such file" + fromFileName);
+            throw new RuntimeException("No such file " + fromFileName);
         }
+        int[] result = new int[3];
         try {
             List<String> stringList = Files.readAllLines(Paths.get(fromFileName));
             for (String line : stringList) {
                 if (line.startsWith("supply")) {
-                    this.supplyCount += Integer.parseInt(line.replaceAll(REGEX, ""));
+                    result[SUPPLY_INDEX]
+                            += Integer.parseInt(line.replaceAll(REGEX, ""));
                 } else if (line.startsWith("buy")) {
-                    this.buyCount += Integer.parseInt(line.replaceAll(REGEX, ""));
+                    result[BUY_INDEX]
+                            += Integer.parseInt(line.replaceAll(REGEX, ""));
                 }
             }
+            result[RESULT_INDEX] = result[0] - result[1];
         } catch (IOException e) {
-            throw new RuntimeException("Can`t read a file" + e);
+            throw new RuntimeException("Can`t read a file " + fromFileName + e);
         }
+        return result;
     }
 
     private void writeToFile(String toFileName, String value) {
         try (FileWriter fileWriter = new FileWriter(toFileName)) {
             fileWriter.write(value);
         } catch (IOException e) {
-            throw new RuntimeException("Can`t write to file", e);
+            throw new RuntimeException("Can`t write to file " + toFileName + e);
         }
     }
 }
