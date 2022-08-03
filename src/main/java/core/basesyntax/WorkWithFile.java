b0cc85745c84e8cc2dc53ec2def0048408d7e019
Vyacheslav Verbanov <v.verbanov@gmail.com>
2022-08-03 11:26:27 +0200

    remove redundant constants, simplify naming of variables, remove empty line

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -11,8 +11,7 @@ public class WorkWithFile {
     private static final String SERVICE_SUPPLY = "supply";
     private static final String SERVICE_BUY = "buy";
     private static final String SERVICE_RESULT = "result";
-    private static final String SEPARATOR_IN_CSV_FILES = ",";
-    private static final String TEMPORARY_SEPARATOR = " ";
+    private static final String COMMA = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
         String[] dataFromFile = getStringFromFile(fromFileName);
@@ -26,36 +25,32 @@ public class WorkWithFile {
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileFrom))) {
             String lineFromFile = bufferedReader.readLine();
             while (lineFromFile != null) {
-                stringBuilder.append(lineFromFile).append(TEMPORARY_SEPARATOR);
+                stringBuilder.append(lineFromFile).append(System.lineSeparator());
                 lineFromFile = bufferedReader.readLine();
             }
         } catch (IOException e) {
             throw new RuntimeException("Can't read file" + fileFrom, e);
         }
-        return stringBuilder.toString().split(TEMPORARY_SEPARATOR);
+        return stringBuilder.toString().split(System.lineSeparator());
     }
 
     private String makeReport(String[] dataFromFile) {
-        int dailySupply = 0;
-        int dailyBuy = 0;
-        int dailyResult = 0;
-        String[] lineFromArray = new String[2];
-
+        int totalSupply = 0;
+        int totalBuy = 0;
         for (String data : dataFromFile) {
-            lineFromArray = data.split(SEPARATOR_IN_CSV_FILES);
+            String[] lineFromArray = data.split(COMMA);
             if (lineFromArray[0].equals(SERVICE_SUPPLY)) {
-                dailySupply += Integer.parseInt(lineFromArray[1]);
+                totalSupply += Integer.parseInt(lineFromArray[1]);
             } else if (lineFromArray[0].equals(SERVICE_BUY)) {
-                dailyBuy += Integer.parseInt(lineFromArray[1]);
+                totalBuy += Integer.parseInt(lineFromArray[1]);
             }
         }
-        dailyResult = dailySupply - dailyBuy;
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(SERVICE_SUPPLY).append(SEPARATOR_IN_CSV_FILES).append(dailySupply)
+        stringBuilder.append(SERVICE_SUPPLY).append(COMMA).append(totalSupply)
                 .append(System.lineSeparator())
-                .append(SERVICE_BUY).append(SEPARATOR_IN_CSV_FILES).append(dailyBuy)
+                .append(SERVICE_BUY).append(COMMA).append(totalBuy)
                 .append(System.lineSeparator())
-                .append(SERVICE_RESULT).append(SEPARATOR_IN_CSV_FILES).append(dailyResult);
+                .append(SERVICE_RESULT).append(COMMA).append(totalSupply - totalBuy);
         return stringBuilder.toString();
     }
 
