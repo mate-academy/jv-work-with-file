ef5e3ee134530ee473956683ae5819e04fc490d6
Dasha Horun <dhorunka@gmail.com>
2022-08-03 14:45:21 +0200

    all changes are done

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -14,19 +14,39 @@ public class WorkWithFile {
     private static final String RESULT = "result";
     private static final String DELIMITER = ",";
 
+    public void getStatistic(String fromFileName, String toFileName) {
+        int sumOfBuy = 0;
+        int sumOfSupply = 0;
+        int result = 0;
+
+        StringBuilder builder = new StringBuilder();
+        readFromFile(fromFileName, builder);
+        String[] textParts = builder.toString().split(DELIMITER);
+        for (int i = 0; i < textParts.length; i++) {
+            if (textParts[i].equals(SUPPLY)) {
+                sumOfSupply += Integer.parseInt(textParts[i + 1]);
+            }
+            if (textParts[i].equals(BUY)) {
+                sumOfBuy += Integer.parseInt(textParts[i + 1]);
+            }
+            result = sumOfSupply - sumOfBuy;
+        }
+        write(toFileName, sumOfSupply, sumOfBuy, result);
+    }
+
     private void readFromFile(String fromFileName,
                                     StringBuilder builder) {
-        File file = new File(fromFileName);
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+        File fileName = new File(fromFileName);
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
             String value = bufferedReader.readLine();
             while (value != null) {
                 builder.append(value).append(DELIMITER);
                 value = bufferedReader.readLine();
             }
         } catch (FileNotFoundException e) {
-            throw new RuntimeException("Can not find the file" + file, e);
+            throw new RuntimeException("Can not find the file" + fileName, e);
         } catch (IOException e) {
-            throw new RuntimeException("Can not read the file" + file, e);
+            throw new RuntimeException("Can not read the file" + fileName, e);
         }
     }
 
@@ -49,25 +69,5 @@ public class WorkWithFile {
         } catch (IOException e) {
             throw new RuntimeException("Can not find the file" + outputFile, e);
         }
-    }
-
-    public void getStatistic(String fromFileName, String toFileName) {
-        int sumOfBuy = 0;
-        int sumOfSupply = 0;
-        int result = 0;
-
-        StringBuilder builder = new StringBuilder();
-        readFromFile(fromFileName, builder);
-        String[] textParts = builder.toString().split(DELIMITER);
-        for (int i = 0; i < textParts.length; i++) {
-            if (textParts[i].equals(SUPPLY)) {
-                sumOfSupply += Integer.parseInt(textParts[i + 1]);
-            }
-            if (textParts[i].equals(BUY)) {
-                sumOfBuy += Integer.parseInt(textParts[i + 1]);
-            }
-            result = sumOfSupply - sumOfBuy;
-        }
-        write(toFileName,sumOfSupply,sumOfBuy,result);
     }
 }
