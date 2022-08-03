059a13f6973ff0b0feef79870c5a428204bc8c6d
mr-Claster <67782747+mr-Claster@users.noreply.github.com>
2022-08-03 19:24:45 +0300

    add method, BufferedWriter, fix naming

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,6 +1,8 @@
 package core.basesyntax;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.Collections;
@@ -11,45 +13,56 @@ public class WorkWithFile {
     private static final String STRING_SUPPLY = "supply";
     private static final String STRING_СOMA = ",";
     private static final int ZERO = 0;
+    private static final int OPERETION_INDEX = 0;
     private static final int ONE = 1;
+    private static final int AMOUNT_INDEX = 1;
+    private static final int TWO = 2;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        File fileReader = new File(fromFileName);
-        int[] buyAndSupply = new int[2];
-        buyAndSupply[ZERO] = ZERO;
-        buyAndSupply[ONE] = ZERO;
-        buyAndSupply = getSupplyAndBuyFromFile(buyAndSupply, fileReader);
-        File fileWriter = new File(toFileName);
-        setSupplyAndBuytoFile(buyAndSupply[ZERO], buyAndSupply[ONE], fileWriter);
+        File outputFile = new File(fromFileName);
+        int totalBuy = 0;
+        int totalSupply = 0;
+        totalSupply = getSupplyAndBuyFromFile(outputFile)[0];
+        totalBuy = getSupplyAndBuyFromFile(outputFile)[1];
+        File inputFile = new File(toFileName);
+        writeToFile(totalSupply, totalBuy, inputFile);
     }
 
-    private static int[] getSupplyAndBuyFromFile(int[] buyAndSupply, File fileReader) {
+    private static int[] getSupplyAndBuyFromFile(File fileReader) {
         try {
             List<String> lines = Files.readAllLines(fileReader.toPath());
-            for (String line : lines) {
-                String[] split = line.split(",");
-                switch (split[ZERO]) {
-                    case STRING_SUPPLY :
-                        buyAndSupply[ZERO] += Integer.parseInt(split[ONE]);
-                        break;
-                    case STRING_BUY :
-                        buyAndSupply[ONE] += Integer.parseInt(split[ONE]);
-                        break;
-                    default:
-                }
-            }
+            return addSupplyAndBuyto(lines);
+
         } catch (IOException e) {
             throw new RuntimeException("Can't read data from the file", e);
         }
+    }
+
+    private static int[] addSupplyAndBuyto(List<String> lines) {
+        int[] buyAndSupply = new int[2];
+        buyAndSupply[ZERO] = ZERO;
+        buyAndSupply[ONE] = ZERO;
+        for (String line : lines) {
+            String[] split = line.split(",");
+            switch (split[OPERETION_INDEX]) {
+                case STRING_SUPPLY :
+                    buyAndSupply[ZERO] += Integer.parseInt(split[AMOUNT_INDEX]);
+                    break;
+                case STRING_BUY :
+                    buyAndSupply[ONE] += Integer.parseInt(split[AMOUNT_INDEX]);
+                    break;
+                default:
+            }
+        }
+        lines.removeAll(Collections.singleton(""));
         return buyAndSupply;
     }
 
-    private void setSupplyAndBuytoFile(Integer supply, Integer buy, File fileWriter) {
-        try {
-            Files.write(fileWriter.toPath(), Collections.singleton(
-                    STRING_SUPPLY + STRING_СOMA + supply
-                            + System.lineSeparator() + STRING_BUY + STRING_СOMA + buy
-                            + System.lineSeparator() + "result" + STRING_СOMA + (supply - buy)));
+    private void writeToFile(int supply, int buy, File fileWriter) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileWriter))) {
+            bufferedWriter.write(STRING_SUPPLY + STRING_СOMA + supply);
+            bufferedWriter.write(System.lineSeparator() + STRING_BUY + STRING_СOMA + buy);
+            bufferedWriter.write(System.lineSeparator() + "result" + STRING_СOMA + (supply - buy));
         } catch (IOException e) {
             throw new RuntimeException("Can't write data from the file", e);
         }
