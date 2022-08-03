b659ff8c33dbaa430d1a6269abe72a0642b6c9d1
Volodymyr Kachan <vladimirkachan1996@gmail.com>
2022-08-03 12:43:47 +0300

    My HW jv-work-with-file refactoring code after mentors review

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,7 +2,6 @@ package core.basesyntax;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -10,14 +9,9 @@ import java.io.IOException;
 public class WorkWithFile {
 
     public void getStatistic(String fromFileName, String toFileName) {
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName,
-                true))) {
-            for (String res : arraysResult(readFile(fromFileName))) {
-                bufferedWriter.write(res + System.lineSeparator());
-            }
-        } catch (IOException e) {
-            throw new RuntimeException("Can't reader file", e);
-        }
+        readFile(fromFileName);
+        prepareReport(readFile(fromFileName));
+        writeToFile(prepareReport(readFile(fromFileName)), toFileName);
     }
 
     private String readFile(String fromFileName) {
@@ -25,29 +19,37 @@ public class WorkWithFile {
             StringBuilder builder = new StringBuilder();
             String value = bufferedReader.readLine();
             while (value != null) {
-                builder.append(value)
-                        .append(",");
+                builder.append(value).append(",");
                 value = bufferedReader.readLine();
             }
-            String stringBuilder = builder.toString().replace("[^A-Za-z0-9]", " ");
-            return stringBuilder;
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException("Can't reader file", e);
+            String dataFromFile = builder.toString().replace("[^A-Za-z0-9]", " ");
+            return dataFromFile;
         } catch (IOException e) {
             throw new RuntimeException("Can't reader file", e);
         }
     }
 
-    private String[] arraysResult(String stringBuilder) {
-        String[] arrayFromFileName = stringBuilder.split(",");
+    private void writeToFile(String[] results, String toFileName) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName,
+                true))) {
+            for (String res : results) {
+                bufferedWriter.write(res + System.lineSeparator());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't reader file", e);
+        }
+    }
+
+    private String[] prepareReport(String dataFromFile) {
+        String[] splittedData = dataFromFile.split(",");
         int supplyTotal = 0;
         int buyTotal = 0;
         int resultTotal;
-        for (int i = 0; i < arrayFromFileName.length; i++) {
-            if (arrayFromFileName[i].equals("supply")) {
-                supplyTotal += Integer.parseInt(arrayFromFileName[i + 1]);
-            } else if (arrayFromFileName[i].equals("buy")) {
-                buyTotal += Integer.parseInt(arrayFromFileName[i + 1]);
+        for (int i = 0; i < splittedData.length; i++) {
+            if (splittedData[i].equals("supply")) {
+                supplyTotal += Integer.parseInt(splittedData[i + 1]);
+            } else if (splittedData[i].equals("buy")) {
+                buyTotal += Integer.parseInt(splittedData[i + 1]);
             }
         }
         resultTotal = supplyTotal - buyTotal;
