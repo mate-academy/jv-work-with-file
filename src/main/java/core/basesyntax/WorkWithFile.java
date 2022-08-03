261d434dbab045e52695279dddebc67421a8e40e
EagleAndrew <tortloveduck4@gmail.com>
2022-08-03 16:59:01 +0200

    created new methods change String to StringBuilder change substring to split add exception message

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,37 +1,48 @@
 package core.basesyntax;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.List;
 
 public class WorkWithFile {
+    private int sumSupply = 0;
+    private int sumBuy = 0;
+
     public void getStatistic(String fromFileName, String toFileName) {
-        File file = new File(fromFileName);
-        int sumBuy = 0;
-        int sumSupply = 0;
+        int[] results = getValue(fromFileName);
+        writeToFile(toFileName, results);
+    }
+
+    private int[] getValue(String fromFileName) {
         List<String> strings;
         try {
-            strings = Files.readAllLines(file.toPath());
+            strings = Files.readAllLines(new File(fromFileName).toPath());
         } catch (IOException e) {
-            throw new RuntimeException("Can't read file", e);
+            throw new RuntimeException("Can't read file " + fromFileName, e);
         }
         for (String line : strings) {
-            int value = Integer.parseInt(line.substring(line.indexOf(",") + 1));
-            if (line.substring(0, line.indexOf(",")).equals("buy")) {
+            int value = Integer.parseInt(line.split(",")[1]);
+            if (line.contains("buy")) {
                 sumBuy += value;
             } else {
                 sumSupply += value;
             }
         }
-        File newFile = new File(toFileName);
-        String result = "supply," + sumSupply + System.lineSeparator()
-                + "buy," + sumBuy + System.lineSeparator()
-                + "result," + (sumSupply - sumBuy);
-        try {
-            Files.write(newFile.toPath(), result.getBytes());
+        return new int[]{sumBuy, sumSupply};
+    }
+
+    private void writeToFile(String toFileName, int[] base) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
+            writer.write("supply," + sumSupply);
+            writer.newLine();
+            writer.write("buy," + sumBuy);
+            writer.newLine();
+            writer.write("result," + (sumSupply - sumBuy));
         } catch (IOException e) {
-            throw new RuntimeException("Can't create file", e);
+            throw new RuntimeException("Can't write to file " + toFileName, e);
         }
     }
 }
