8b3905ca393228b2aaf352bcf7a96f6e21b4918d
max-bolotin <realprof.mx@gmail.com>
2022-08-03 18:43:15 +0300

    mentor's comments taken into account

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -14,27 +14,35 @@ public class WorkWithFile {
         int supply = 0;
         int buy = 0;
         int result;
+        String lineSplitter = ",";
+        String supplyString = "supply";
         try (BufferedReader lineReader = new BufferedReader(new FileReader(fromFile))) {
             String line = lineReader.readLine();
             while (line != null) {
-                if ("supply".equals(line.split(",")[0])) {
-                    supply += Integer.parseInt(line.split(",")[1]);
+                if (supplyString.equals(line.split(lineSplitter)[0])) {
+                    supply += Integer.parseInt(line.split(lineSplitter)[1]);
                 } else {
-                    buy += Integer.parseInt(line.split(",")[1]);
+                    buy += Integer.parseInt(line.split(lineSplitter)[1]);
                 }
                 line = lineReader.readLine();
             }
         } catch (IOException e) {
-            throw new RuntimeException("Can't read file", e);
+            throw new RuntimeException("Can't read file " + fromFileName, e);
         }
         result = supply - buy;
         File toFile = new File(toFileName);
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFile, true))) {
-            writer.write("supply," + supply + System.lineSeparator() + "buy,"
-                    + buy + System.lineSeparator()
-                    + "result," + result);
+            writer.write(getOutput(supply, buy, result).toString());
         } catch (IOException e) {
-            throw new RuntimeException("Can't write to file", e);
+            throw new RuntimeException("Can't write to file " + toFileName, e);
         }
+    }
+
+    private StringBuilder getOutput(int supply, int buy, int result) {
+        StringBuilder output = new StringBuilder();
+        output.append("supply,").append(supply).append(System.lineSeparator())
+                .append("buy,").append(buy).append(System.lineSeparator())
+                .append("result,").append(result);
+        return output;
     }
 }
