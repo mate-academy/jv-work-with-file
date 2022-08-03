4d5a6930610a3609125d7ea637fbc3179aab98bd
Nazar <litvik.12@gmail.com>
2022-08-03 18:54:04 +0300

    made dataList local variable and using BufferedReader instead of FileInputStream

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,44 +1,31 @@
 package core.basesyntax;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.List;
 
 public class WorkWithFile {
-    private ArrayList<DataFromFile> dataList = new ArrayList<>();
-
     public void getStatistic(String fromFileName, String toFileName) {
-        WorkWithFile work = new WorkWithFile();
-        work.getDataFile(fromFileName);
-        work.writeToFile(toFileName, work.calculating());
+        ArrayList<DataFromFile> dataList = new ArrayList<>();
+        getDataFile(fromFileName, dataList);
+        writeToFile(toFileName, calculating(dataList));
     }
 
-    private void getDataFile(String fromFileName) {
-        try (FileInputStream filein = new FileInputStream(fromFileName)) {
-            if (filein.available() == 0) {
-                throw new RuntimeException("File is empty");
-            } else {
-                BufferedReader br = new BufferedReader(new InputStreamReader(filein));
-                String line = null;
-                DataFromFile data = null;
-                while ((line = br.readLine()) != null) {
-                    String[] strings = line.split(",");
-                    data = new DataFromFile(strings[0], Integer.valueOf(strings[1]));
-                    dataList.add(data);
-                }
-                br.close();
+    private void getDataFile(String fromFileName, ArrayList<DataFromFile> dataList) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
+            String line = null;
+            DataFromFile data = null;
+            while ((line = reader.readLine()) != null) {
+                String[] strings = line.split(",");
+                data = new DataFromFile(strings[0], Integer.valueOf(strings[1]));
+                dataList.add(data);
             }
         } catch (IOException e) {
             throw new RuntimeException("Can't read data from file", e);
         }
     }
 
-    private int[] calculating() {
+    private int[] calculating(ArrayList<DataFromFile> dataList) {
         int[] sumOfResults = new int[3];
         for (DataFromFile data : dataList) {
             if (data.getOperationType().equals("supply")) {
@@ -56,10 +43,9 @@ public class WorkWithFile {
         String[] strings = new String[]{"supply," + sumOfResults[0], "buy,"
                 + sumOfResults[1], "result," + sumOfResults[2]};
         File file = new File(toFileName);
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(file, true))) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file, true))) {
             for (String data : strings) {
-                bw.write(data + System.lineSeparator());
-                bw.flush();
+                writer.write(data + System.lineSeparator());
             }
         } catch (IOException e) {
             throw new RuntimeException("Can't write data to file", e);
