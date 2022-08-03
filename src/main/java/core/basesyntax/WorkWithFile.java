3ee75af3ede2616f73bba035f7b750e08e744247
Misha Brovkin <haasiakira@gmail.com>
2022-08-03 14:41:00 +0300

    fixed issues

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,52 +8,59 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    static final int SUPPLY_BUY_INDEX = 0;
+    static final int AMOUNT_INDEX = 1;
+    static final String SPLIT_REGEX = ",";
+    private int supply = 0;
+    private int buy = 0;
+    private int result;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        int supply = 0;
-        int buy = 0;
-        int result;
+        readHealper(fromFileName);
 
+        result = supply - buy;
+
+        writeHelper(toFileName);
+    }
+
+    public void readHealper(String fromFileName) {
         File fromFile = new File(fromFileName);
-        File toFile = new File(toFileName);
 
         StringBuilder builder = new StringBuilder();
 
-        try ( BufferedReader reader = new BufferedReader(new FileReader(fromFile))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFile))) {
             String value = reader.readLine();
 
             while (value != null) {
                 builder.append(value);
-                String[] temp = value.split(",");
-                if (temp[0].equals("supply")) {
-                    supply += Integer.parseInt(temp[1]);
+                String[] splittedLine = value.split(SPLIT_REGEX);
+                if (splittedLine[SUPPLY_BUY_INDEX].equals("supply")) {
+                    supply += Integer.parseInt(splittedLine[AMOUNT_INDEX]);
                 } else {
-                    buy += Integer.parseInt(temp[1]);
+                    buy += Integer.parseInt(splittedLine[AMOUNT_INDEX]);
                 }
                 value = reader.readLine();
             }
-
-            result = supply - buy;
-            System.out.println("supply," + supply + System.lineSeparator()
-                                + "buy," + buy + System.lineSeparator()
-                                + "result," + result);
         } catch (IOException e) {
             throw new RuntimeException("Can't read file" + fromFileName, e);
         }
+    }
 
+    public void writeHelper(String toFileName) {
+        File toFile = new File(toFileName);
         String finalSupply = "supply," + supply;
         String finalBuy = "buy," + buy;
         String finalResult = "result," + result;
 
-        String[] res = new String[]{finalSupply, finalBuy, finalResult};
+        String[] report = new String[]{finalSupply, finalBuy, finalResult};
 
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFile, true))) {
-                for (String resources : res) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFile, true))) {
+            for (String resources : report) {
                 writer.write(resources);
                 writer.newLine();
-                }
-            } catch (IOException e) {
-                throw new RuntimeException("Can't write data to file" + toFileName, e);
             }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write data to file" + toFileName, e);
+        }
     }
 }
