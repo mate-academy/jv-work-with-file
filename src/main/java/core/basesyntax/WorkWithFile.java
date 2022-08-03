01d0381b15d20c43cfa587ec89edbc30b61fb9ae
IraHurko <iragurko19@gmail.com>
2022-08-03 13:38:22 +0300

    Created Reader for files with fruits and then created Writer for file with statistic

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,14 +1,25 @@
 package core.basesyntax;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class WorkWithFile {
 
     public void getStatistic(String fromFileName, String toFileName) {
-        File file = new File(fromFileName);
-        File myFile = new File(toFileName);
-        StringBuilder builder = new StringBuilder();
+        File readFile = new File(fromFileName);
+        File writeFile = new File(toFileName);
 
+        String fileContent = readFromFile(readFile);
+        MarketStatistic marketStatistic = getMarketStatistic(fileContent);
+        writeToFile(marketStatistic.getBuyCount(), marketStatistic.getSupplyCount(), writeFile);
+    }
+
+    private String readFromFile(File file) {
+        StringBuilder builder = new StringBuilder();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
             String value = bufferedReader.readLine();
             while (value != null) {
@@ -18,22 +29,28 @@ public class WorkWithFile {
         } catch (IOException e) {
             throw new RuntimeException("Can't read file", e);
         }
+        return builder.toString();
+    }
 
-        String[] listOfStatistic = builder.toString().split(";");
+    private MarketStatistic getMarketStatistic(String content) {
+        String[] listOfStatistic = content.split(";");
         int supplyCount = 0;
         int buyCount = 0;
         for (String statistic : listOfStatistic) {
+            int statisticNumber = Integer.parseInt(statistic
+                    .substring(statistic.indexOf(',') + 1));
             if (statistic.contains("supply")) {
-                supplyCount += Integer.parseInt(statistic
-                        .substring(statistic.indexOf(',') + 1));
+                supplyCount += statisticNumber;
             } else if (statistic.contains("buy")) {
-                buyCount += Integer.parseInt(statistic
-                        .substring(statistic.indexOf(',') + 1));
+                buyCount += statisticNumber;
             }
         }
-        int result = supplyCount - buyCount;
+        return new MarketStatistic(buyCount, supplyCount);
+    }
 
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(myFile))) {
+    private void writeToFile(int buyCount, int supplyCount, File file) {
+        int result = supplyCount - buyCount;
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
             bufferedWriter.write("supply," + supplyCount + System.lineSeparator()
                     + "buy," + buyCount + System.lineSeparator() + "result," + result);
             bufferedWriter.flush();
