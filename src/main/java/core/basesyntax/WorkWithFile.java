3660af562953ac232bd94fd0424a4b55028c132e
X3NON-135 <immort4ld3momo@gmail.com>
2022-08-04 12:21:12 +0100

    separated reading and counting logic int two methods, optimized code

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,71 +1,61 @@
 package core.basesyntax;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.ArrayList;
 import java.util.List;
 
 public class WorkWithFile {
     private static final String VALUE_CRITERIA = "supply";
-    private static final String LINE_SEPARATOR = " ";
+    private static final String SPLIT_REGEX = ",";
 
-    public static void getStatistic(String fromFileName, String toFileName) {
-        List<Integer> listOfSupplyCosts = new ArrayList<>();
-        List<Integer> listOfBuyCosts = new ArrayList<>();
-        readFromFile(fromFileName, listOfSupplyCosts, listOfBuyCosts);
-        int sumOfSupplies = getSumOfList(listOfSupplyCosts);
-        int sumOfBuys = getSumOfList(listOfBuyCosts);
-        String result = createReport(sumOfSupplies, sumOfBuys, sumOfSupplies - sumOfBuys);
-        createAndWriteToFile(toFileName, result);
+    public void getStatistic(String fromFileName, String toFileName) {
+        List<String> dataFromFile = readFromFile(fromFileName);
+        int[] statistic = countStatistic(dataFromFile);
+        String report = createReport(statistic);
+        writeToFile(toFileName, report);
     }
 
-    public static void readFromFile(String fromFile, List<Integer> supplies, List<Integer> buys) {
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFile))) {
-            String string = bufferedReader.readLine();
-            String[] splitedString = null;
-
-            while (string != null) {
-                splitedString = string.split(LINE_SEPARATOR);
-                for (String value : splitedString) {
-                    if (value.contains(VALUE_CRITERIA)) {
-                        supplies.add(Integer.valueOf(value
-                                .substring(value.indexOf(",") + 1, value.length())));
-                    } else {
-                        buys.add(Integer.valueOf(value
-                                .substring(value.indexOf(",") + 1, value.length())));
-                    }
-                }
-                string = bufferedReader.readLine();
+    private int[] countStatistic(List<String> dataFromFile) {
+        int sumOfSupplies = 0;
+        int sumOfBuys = 0;
+        String[] array;
+        for (int i = 0; i < dataFromFile.size(); i++) {
+            array = dataFromFile.get(i).split(SPLIT_REGEX);
+            if (array[0].equals(VALUE_CRITERIA)) {
+                sumOfSupplies += Integer.parseInt(array[1]);
+            } else {
+                sumOfBuys += Integer.parseInt(array[1]);
             }
+        }
+        return new int[]{sumOfSupplies, sumOfBuys};
+    }
+
+    private List<String> readFromFile(String fileName) {
+        try {
+            List<String> list = Files.readAllLines(new File(fileName).toPath());
+            return list;
         } catch (IOException e) {
-            throw new RuntimeException("Can`t read data from file " + fromFile + " ", e);
+            throw new RuntimeException("Can't read data from file " + fileName + "", e);
         }
     }
 
-    public static int getSumOfList(List<Integer> list) {
-        int sum = 0;
-        for (int number : list) {
-            sum += number;
-        }
-        return sum;
-    }
-
-    public static String createReport(int sumOfSupplies, int sumOfBuys, int result) {
-        StringBuilder stringBuilder = new StringBuilder();
-        return stringBuilder.append("supply,").append(sumOfSupplies)
-                .append(System.lineSeparator()).append("buy,")
-                .append(sumOfBuys).append(System.lineSeparator())
-                .append("result,").append(result).toString();
-    }
-
-    public static void createAndWriteToFile(String toFileName, String data) {
+    private void writeToFile(String toFileName, String data) {
         try {
             Files.write(new File(toFileName).toPath(), data.getBytes());
         } catch (IOException e) {
             throw new RuntimeException("Can't write data to file " + toFileName + " ", e);
         }
+    }
+
+    private String createReport(int[] statistic) {
+        final int supply = statistic[0];
+        final int buys = statistic[1];
+        final int result = supply - buys;
+        StringBuilder stringBuilder = new StringBuilder();
+        return stringBuilder.append("supply,").append(supply)
+                .append(System.lineSeparator()).append("buy,")
+                .append(buys).append(System.lineSeparator())
+                .append("result,").append(result).toString();
     }
 }
