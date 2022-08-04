3a1d6eb9e699091c058f4f26d0bee59d2ccbe32d
UmyaFamilia <umyafamilya1974@gmail.com>
2022-08-04 09:04:47 +0300

    this progtam first try

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,9 +8,16 @@ import java.nio.file.Files;
 import java.nio.file.StandardOpenOption;
 
 public class WorkWithFile {
+   public final static int ZERO_VALUE=0;
+   public final static int SECOND_POSITION=1;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        File file = new File(fromFileName);
+        readFromFile(fromFileName);
+        doNeedForm(fromFileName);
+        writeToFilee(fromFileName,toFileName);
+    }
+    public String readFromFile(String adres) {
+        File file = new File(adres);
         StringBuilder stringBuilder = new StringBuilder();
         try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
@@ -23,32 +30,43 @@ public class WorkWithFile {
             throw new RuntimeException(e);
         }
         String result = stringBuilder.toString();
-        String [] array = result.split(System.lineSeparator());
-        int sumOfSupply = 0;
-        int sumOfBuy = 0;
-        for (int i = 0; i < array.length; i++) {
+        return result;
+    }
+
+    public String doNeedForm(String adres) {
+
+        String[] array = readFromFile(adres).split(System.lineSeparator());
+        int sumOfSupply = ZERO_VALUE;
+        int sumOfBuy = ZERO_VALUE;
+        for (int i = ZERO_VALUE; i < array.length; i++) {
             if (array[i].startsWith("supply")) {
-                sumOfSupply += Integer.parseInt(array[i].split(",")[1]);
+                sumOfSupply += Integer.parseInt(array[i].split(",")[SECOND_POSITION]);
             }
             if (array[i].startsWith("buy")) {
-                sumOfBuy += Integer.parseInt(array[i].split(",")[1]);
+                sumOfBuy += Integer.parseInt(array[i].split(",")[SECOND_POSITION]);
             }
         }
         String result1 = "";
         result1 = result1.concat("supply,").concat(String.valueOf(sumOfSupply))
-                  .concat(System.lineSeparator())
+                .concat(System.lineSeparator())
                 .concat("buy,").concat(String.valueOf(sumOfBuy)).concat(System.lineSeparator())
-               .concat("result,").concat(String.valueOf(sumOfSupply - sumOfBuy));
-        File file1 = new File(toFileName);
-        try {
-            file1.createNewFile();
-        } catch (IOException e) {
-            throw new RuntimeException("file cant be creaded",e);
-        }
-        try {
-            Files.write(file1.toPath(), result1.getBytes(), StandardOpenOption.APPEND);
-        } catch (IOException e) {
-            throw new RuntimeException("Can not write data to file",e);
-        }
+                .concat("result,").concat(String.valueOf(sumOfSupply - sumOfBuy));
+        return result1;
     }
-}
+
+            public void writeToFilee(String from,String to ){
+            File file1 = new File(to);
+            try {
+                file1.createNewFile();
+            } catch (IOException e) {
+                throw new RuntimeException("file cant be creaded",e);
+            }
+            try {
+                Files.write(file1.toPath(), doNeedForm(from).getBytes(), StandardOpenOption.APPEND);
+            } catch (IOException e) {
+                throw new RuntimeException("Can not write data to file",e);
+            }
+         }
+    }
+
+
