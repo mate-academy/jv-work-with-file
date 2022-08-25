b4f252dfdbc5c994e05ce60e2aeef59272d4ccde
Valerii Kulibchenko <kulibchenkovalera@gmail.com>
2022-08-25 10:33:43 +0300

    first commit

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,8 +1,35 @@
 package core.basesyntax;
 
+import java.io.*;
+
 public class WorkWithFile {
 
     public void getStatistic(String fromFileName, String toFileName) {
-
+        File inputFile = new File(fromFileName);
+        File outputFile = new File(toFileName);
+        try {
+            int supply = 0;
+            int buy = 0;
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(inputFile));
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(outputFile));
+            String line = bufferedReader.readLine();
+            String[] split = line.split(",");
+            while (line != null){
+                switch (split[0]) {
+                    case ("supply"):
+                        supply += Integer.parseInt(split[1]);
+                        break;
+                    case ("buy"):
+                        buy += Integer.parseInt(split[1]);
+                        break;
+                }
+                line = bufferedReader.readLine();
+            }
+            bufferedWriter.write("supply," + supply + "\n" + "buy," + buy + "\n" + "result," + (supply - buy));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("File not found",e);
+        } catch (IOException e) {
+            throw new RuntimeException("Runtime Exception", e);
+        }
     }
 }
