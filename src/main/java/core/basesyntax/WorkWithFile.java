c011af87aa2698ff3c287a56ee1345aa20c9c8cb
Oleksandr Kravchenko <Pokemonishche@me.com>
2022-08-26 20:07:48 +0300

    Divided the code into methods and optimized it a bit

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,29 +2,17 @@ package core.basesyntax;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    private static final String SEPARATOR = System.lineSeparator();
+    private int supply = 0;
+    private int buy = 0;
 
-    public void getStatistic(String fromFileName, String toFileName) {
-        int supply = 0;
-        int buy = 0;
-        int result = 0;
-        File myFileFrom = new File(fromFileName);
-        File myFileTo = new File(toFileName);
-        FileReader fileReader = null;
-        try {
-            fileReader = new FileReader(myFileFrom);
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException("Error file read ", e);
-        }
-        BufferedReader bufferedReader = new BufferedReader(fileReader);
-        try {
+    public void getDataFromFile(String fromFileName) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             String value = bufferedReader.readLine();
             while (value != null) {
                 String[] name = value.split(",", 0);
@@ -36,32 +24,25 @@ public class WorkWithFile {
                 }
                 value = bufferedReader.readLine();
             }
-
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("Error file read ", e);
         } catch (IOException e) {
             throw new RuntimeException("Error read line", e);
-        } finally {
-            try {
-                bufferedReader.close();
-            } catch (IOException e) {
-                throw new RuntimeException("Error bufferedReader close", e);
-            }
+        }
+    }
 
-        }
-        result = supply - buy;
-        BufferedWriter bufferedWriter = null;
-        try {
-            bufferedWriter = new BufferedWriter(new FileWriter(myFileTo));
-            bufferedWriter.write("supply," + supply + SEPARATOR);
-            bufferedWriter.write("buy," + buy + SEPARATOR);
-            bufferedWriter.write("result," + result);
+    public void writeData(String toFileName) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+            bufferedWriter.write("supply," + supply + System.lineSeparator()
+                    + "buy," + buy + System.lineSeparator()
+                    + "result," + (supply - buy));
         } catch (IOException e) {
-            throw new RuntimeException("Can't write data to file " + myFileTo, e);
-        } finally {
-            try {
-                bufferedWriter.close();
-            } catch (IOException e) {
-                throw new RuntimeException("Can't close bufferedWriter " + myFileTo, e);
-            }
+            throw new RuntimeException("Can't write data to file " + toFileName, e);
         }
+    }
+
+    public void getStatistic(String fromFileName,String toFileName) {
+        getDataFromFile(fromFileName);
+        writeData(toFileName);
     }
 }
