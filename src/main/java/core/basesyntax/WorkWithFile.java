3c25514c30002fd44b626747910d754f4b4725fb
Oleksandr Kravchenko <Pokemonishche@me.com>
2022-08-26 21:09:10 +0300

    Changed to 3 methods

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -6,43 +6,57 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class WorkWithFile {
-    private int supply = 0;
-    private int buy = 0;
 
-    private void getDataFromFile(String fromFileName) {
+    private ArrayList<String> getDataFromFile(String fromFileName) {
+        ArrayList<String> dataFile = new ArrayList<>();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             String value = bufferedReader.readLine();
             while (value != null) {
-                String[] name = value.split(",", 0);
-                if (name[0].equals("supply")) {
-                    supply += Integer.parseInt(name[1]);
-                }
-                if (name[0].equals("buy")) {
-                    buy += Integer.parseInt(name[1]);
-                }
+                dataFile.add(value);
                 value = bufferedReader.readLine();
             }
+            return dataFile;
         } catch (FileNotFoundException e) {
-            throw new RuntimeException("Error file read ", e);
+            throw new RuntimeException("Can't read the file", e);
         } catch (IOException e) {
-            throw new RuntimeException("Error read line", e);
+            throw new RuntimeException("Can't read line from the file ", e);
         }
     }
 
-    private void writeData(String toFileName) {
+    private void writeData(String toFileName,String report) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
-            bufferedWriter.write("supply," + supply + System.lineSeparator()
-                    + "buy," + buy + System.lineSeparator()
-                    + "result," + (supply - buy));
+            bufferedWriter.write(report);
         } catch (IOException e) {
             throw new RuntimeException("Can't write data to file " + toFileName, e);
         }
     }
 
-    public void getStatistic(String fromFileName,String toFileName) {
-        getDataFromFile(fromFileName);
-        writeData(toFileName);
+    private String createReport(ArrayList<String> file) {
+        int supply = 0;
+        int buy = 0;
+        for (String value : file) {
+            String[] name = value.split(",", 0);
+            if (name[0].equals("supply")) {
+                supply += Integer.parseInt(name[1]);
+            }
+            if (name[0].equals("buy")) {
+                buy += Integer.parseInt(name[1]);
+            }
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("supply," + supply + System.lineSeparator())
+                .append("buy," + buy + System.lineSeparator())
+                .append("result," + (supply - buy));
+        return stringBuilder.toString();
+
+    }
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        ArrayList<String> data = getDataFromFile(fromFileName);
+        String report = createReport(data);
+        writeData(toFileName,report);
     }
 }
