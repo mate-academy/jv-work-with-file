8e356a80109f40f4be5e805c1eaff31f01e6012d
Valerii Kulibchenko <kulibchenkovalera@gmail.com>
2022-08-27 13:50:16 +0300

    second commit

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -13,8 +13,8 @@ public class WorkWithFile {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(inputFile));
             BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(outputFile));
             String line = bufferedReader.readLine();
-            String[] split = line.split(",");
             while (line != null){
+                String[] split = line.split(",");
                 switch (split[0]) {
                     case ("supply"):
                         supply += Integer.parseInt(split[1]);
@@ -25,7 +25,9 @@ public class WorkWithFile {
                 }
                 line = bufferedReader.readLine();
             }
-            bufferedWriter.write("supply," + supply + "\n" + "buy," + buy + "\n" + "result," + (supply - buy));
+            bufferedWriter.write("supply," + supply + System.lineSeparator() + "buy," + buy + System.lineSeparator() + "result," + (supply - buy));
+            bufferedReader.close();
+            bufferedWriter.close();
         } catch (FileNotFoundException e) {
             throw new RuntimeException("File not found",e);
         } catch (IOException e) {
