a18f40a7e61260e04d33a10cb327b0c740396214
Valerii Kulibchenko <kulibchenkovalera@gmail.com>
2022-08-27 14:29:12 +0300

    Third commit with fixed

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,7 +2,6 @@ package core.basesyntax;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -11,34 +10,27 @@ import java.io.IOException;
 public class WorkWithFile {
 
     public void getStatistic(String fromFileName, String toFileName) {
-        File inputFile = new File(fromFileName);
-        File outputFile = new File(toFileName);
-        try {
-            int supply = 0;
-            int buy = 0;
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(inputFile));
-            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(outputFile));
+        int supply = 0;
+        int buy = 0;
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             String line = bufferedReader.readLine();
             while (line != null) {
                 String[] split = line.split(",");
-                switch (split[0]) {
-                    case ("supply"):
-                        supply += Integer.parseInt(split[1]);
-                        break;
-                    case ("buy"):
-                        buy += Integer.parseInt(split[1]);
-                        break;
-                    default:
-                        break;
+                if (split[0].equals("supply")) {
+                    supply += Integer.parseInt(split[1]);
+                } else {
+                    buy += Integer.parseInt(split[1]);
                 }
                 line = bufferedReader.readLine();
             }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("Can`t open file",e);
+        } catch (IOException e) {
+            throw new RuntimeException("Runtime Exception", e);
+        }
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
             bufferedWriter.write("supply," + supply + System.lineSeparator() + "buy," + buy
                     + System.lineSeparator() + "result," + (supply - buy));
-            bufferedReader.close();
-            bufferedWriter.close();
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException("File not found",e);
         } catch (IOException e) {
             throw new RuntimeException("Runtime Exception", e);
         }
