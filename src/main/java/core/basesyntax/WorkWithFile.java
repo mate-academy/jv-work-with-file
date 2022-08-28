a954cbe8c5f0dc272996c60e8e1711c8900209a5
Valerii Kulibchenko <kulibchenkovalera@gmail.com>
2022-08-28 12:54:24 +0300

    5th commit with fixes

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,46 +1,48 @@
 package core.basesyntax;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 
 public class WorkWithFile {
-    private int totalSupply = 0;
-    private int totalBuy = 0;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        readFromFile(fromFileName);
-        writeToFile(toFileName);
+        List<String> dataFromFile = readFromFile(fromFileName);
+        String report = report(dataFromFile);
+        writeToFile(toFileName, report);
     }
 
-    public void readFromFile(String fromFileName) {
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
-            String line = bufferedReader.readLine();
-            while (line != null) {
-                String[] split = line.split(",");
-                if (split[0].equals("supply")) {
-                    totalSupply += Integer.parseInt(split[1]);
-                } else {
-                    totalBuy += Integer.parseInt(split[1]);
-                }
-                line = bufferedReader.readLine();
-            }
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException("Can`t open file",e);
+    public List<String> readFromFile(String fromFileName) {
+        try {
+            return Files.readAllLines(Path.of(fromFileName));
         } catch (IOException e) {
-            throw new RuntimeException("Runtime Exception", e);
+            throw new RuntimeException("Can`t read data from file", e);
         }
     }
 
-    public void writeToFile(String toFileName) {
+    public void writeToFile(String toFileName, String data) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
-            bufferedWriter.write("supply," + totalSupply + System.lineSeparator() + "buy,"
-                    + totalBuy + System.lineSeparator() + "result," + (totalSupply - totalBuy));
+            bufferedWriter.write(data);
         } catch (IOException e) {
-            throw new RuntimeException("Runtime Exception", e);
+            throw new RuntimeException("Couldn`t write data to file", e);
         }
+    }
+
+    public String report(List<String> data) {
+        int totalSupply = 0;
+        int totalBuy = 0;
+        for (String line: data) {
+            String[] split = line.split(",");
+            if (split[0].equals("supply")) {
+                totalSupply += Integer.parseInt(split[1]);
+            } else {
+                totalBuy += Integer.parseInt(split[1]);
+            }
+        }
+        return "supply," + totalSupply + System.lineSeparator() + "buy," + totalBuy
+                + System.lineSeparator() + "result," + (totalSupply - totalBuy);
     }
 }
