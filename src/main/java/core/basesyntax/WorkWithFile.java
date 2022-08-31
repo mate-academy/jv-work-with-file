96e36b814ed5fe8274be3b8562318c84156e49ba
Maksym Ivanov <ivanov1maxim@gmail.com>
2022-08-31 19:00:56 +0300

    Modify method logic

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -6,18 +6,19 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.StringJoiner;
 
 public class WorkWithFile {
 
     public void getStatistic(String fromFileName, String toFileName) {
-        String report = this.generateReport(fromFileName);
+        String dataFromFile = this.readFromFile(fromFileName);
+        String report = this.createReport(dataFromFile);
         this.writeToFile(report, toFileName);
-        System.out.println(report);
     }
 
-    private String generateReport(final String fromFileName) {
+    private String readFromFile(final String fromFileName) {
         File file = new File("./" + fromFileName);
-        StringBuilder result = new StringBuilder();
+        StringJoiner result = new StringJoiner("|");
 
         try (
                 FileReader fileReader = new FileReader(file);
@@ -25,36 +26,45 @@ public class WorkWithFile {
 
         ) {
             String nextString = bufferedReader.readLine();
-            int supply = 0;
-            int buy = 0;
-
             while (nextString != null) {
-                String[] nextStringArray = nextString.split(",");
-                int amount = Integer.parseInt(nextStringArray[1]);
-
-                if (nextStringArray[0].equals("supply")) {
-                    supply += amount;
-                } else {
-                    buy += amount;
-                }
+                result.add(nextString);
                 nextString = bufferedReader.readLine();
             }
-
-            result.append("supply")
-                    .append(",")
-                    .append(supply)
-                    .append(System.lineSeparator())
-                    .append("buy")
-                    .append(",")
-                    .append(buy)
-                    .append(System.lineSeparator())
-                    .append("result")
-                    .append(",")
-                    .append(supply - buy);
 
         } catch (IOException e) {
             throw new RuntimeException("Can't read file: File not Found", e);
         }
+        return result.toString();
+    }
+
+    private String createReport(final String dataFromFile) {
+        StringBuilder result = new StringBuilder();
+        int supply = 0;
+        int buy = 0;
+        String[] dataFromFileArray = dataFromFile.split("\\|");
+
+        for (final String datum : dataFromFileArray) {
+            String[] datumArray = datum.split(",");
+            String operation = datumArray[0];
+            int amount = Integer.parseInt(datumArray[1]);
+
+            if (operation.equals("supply")) {
+                supply += amount;
+            } else {
+                buy += amount;
+            }
+        }
+        result.append("supply")
+                .append(",")
+                .append(supply)
+                .append(System.lineSeparator())
+                .append("buy")
+                .append(",")
+                .append(buy)
+                .append(System.lineSeparator())
+                .append("result")
+                .append(",")
+                .append(supply - buy);
         return result.toString();
     }
 
@@ -69,5 +79,3 @@ public class WorkWithFile {
         }
     }
 }
-
-
