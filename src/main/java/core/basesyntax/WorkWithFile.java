7be61afe7fc7bf1c1055eb78bde21e00179b5fc3
Dima Senko <d.senko@i.ua>
2022-09-11 12:47:52 +0300

    Divided program logic

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,13 +7,10 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    public static final int OPERATION_TYPE = 0;
-    public static final int AMOUNT = 1;
+    public static final int OPERATION_TYPE_POSITION = 0;
+    public static final int AMOUNT_POSITION = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        int supply = 0;
-        int buy = 0;
-        int result = 0;
         String example = "";
         try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName));
@@ -23,27 +20,38 @@ public class WorkWithFile {
                 dateWithFile.append(value).append(System.lineSeparator());
                 value = bufferedReader.readLine();
             }
-            String[] lines = dateWithFile.toString().split(System.lineSeparator());
-            for (String line: lines) {
-                String[] arrayLine = line.split(",");
-                if (arrayLine[OPERATION_TYPE].equals("supply")) {
-                    supply += Integer.valueOf(arrayLine[AMOUNT]);
-                }
-                if (arrayLine[OPERATION_TYPE].equals("buy")) {
-                    buy += Integer.valueOf(arrayLine[AMOUNT]);
-                }
-            }
-            result = supply - buy;
-            example = "supply," + supply + System.lineSeparator()
-                    + "buy," + buy + System.lineSeparator()
-                    + "result," + result;
+            example = getExample(dateWithFile.toString());
         } catch (IOException e) {
             throw new RuntimeException("Can't read data from the file" + fromFileName, e);
         }
+        getWriterWithFile(example, toFileName);
+    }
+
+    public void getWriterWithFile(String example, String toFileName) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName, true))) {
             bufferedWriter.write(example);
         } catch (IOException e) {
             throw new RuntimeException("Can't write data from the file" + toFileName, e);
         }
+    }
+
+    public String getExample(String date) {
+        int supply = 0;
+        int buy = 0;
+        int result = 0;
+        String[] lines = date.split(System.lineSeparator());
+        for (String line: lines) {
+            String[] arrayLine = line.split(",");
+            if (arrayLine[OPERATION_TYPE_POSITION].equals("supply")) {
+                supply += Integer.valueOf(arrayLine[AMOUNT_POSITION]);
+            }
+            if (arrayLine[OPERATION_TYPE_POSITION].equals("buy")) {
+                buy += Integer.valueOf(arrayLine[AMOUNT_POSITION]);
+            }
+        }
+        result = supply - buy;
+        return "supply," + supply + System.lineSeparator()
+                + "buy," + buy + System.lineSeparator()
+                + "result," + result;
     }
 }
