27f3a6a0ca11f10d0f8ba550e7508a40fd5ffe7c
ValeriyM250 <valeriym250@gmail.com>
2022-09-15 15:17:42 +0300

    Divided my solution of 3 methods: separateData - common value of supply + common value of buy - current document fillFile - fill file with given values getStatistic - method which call "separateData" and "fillFile" given data from "separateData" method

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -14,33 +14,41 @@ public class WorkWithFile {
 
     public void getStatistic(String fromFileName, String toFileName) {
         File file = new File(fromFileName);
+        int supplyQuantity = separateData(file, "supply");
+        int buyQuantity = separateData(file, "buy");
+        file = new File(toFileName);
+        fillFile(file, supplyQuantity, buyQuantity);
+    }
+
+    private int separateData(File file, String comparisonWord) {
         String [] line = new String[2];
-        int supplyQuantity = 0;
-        int buyQuantity = 0;
+        int result = 0;
+
         try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
             String reading = bufferedReader.readLine();
             while (reading != null) {
                 line = reading.split(",");
-                if (line[OPERATION_INDEX].equals("supply")) {
-                    supplyQuantity += Integer.parseInt(line[COUNT_INDEX]);
-                } else {
-                    buyQuantity += Integer.parseInt(line[COUNT_INDEX]);
+                if (line[OPERATION_INDEX].equals(comparisonWord)) {
+                    result += Integer.parseInt(line[COUNT_INDEX]);
                 }
                 reading = bufferedReader.readLine();
             }
         } catch (FileNotFoundException e) {
-            System.out.println("Can`t read file: " + fromFileName + " " + e);
+            System.out.println("Can`t read file: " + file.getName() + " " + e);
         } catch (IOException e) {
-            System.out.println("Can`t read line from file: " + fromFileName + " " + e);
+            System.out.println("Can`t read line from file: " + file.getName() + " " + e);
         }
-        file = new File(toFileName);
+        return result;
+    }
+
+    private void fillFile(File file, int addition, int subtraction) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file, true))) {
-            bufferedWriter.write("supply," + supplyQuantity + System.lineSeparator()
-                    + "buy," + buyQuantity + System.lineSeparator()
-                    + "result," + (supplyQuantity - buyQuantity));
+            bufferedWriter.write("supply," + addition + System.lineSeparator()
+                    + "buy," + subtraction + System.lineSeparator()
+                    + "result," + (addition - subtraction));
         } catch (IOException e) {
-            System.out.println("Can`t write data in " + toFileName + ", " + e);
+            System.out.println("Can`t write data in " + file.getName() + ", " + e);
         }
     }
 }
