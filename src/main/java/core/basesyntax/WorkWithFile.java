fccb735cd1cbe0ff34be81984b8e420ef7188ad2
sasha <radovietsoleksandr@gmail.com>
2022-09-18 23:50:00 +0300

    file4

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -3,25 +3,12 @@ package core.basesyntax;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.io.FileWriter;
 
 public class WorkWithFile {
-
-    public static void getStatistic(String fromFileName, String toFileName) {
-        StringBuilder readTextBuilder = new StringBuilder();
-        /////////////////////////////READ from FILE/////////////////////////////////////////////
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
-            String readText = bufferedReader.readLine();
-            while (readText != null) {
-                String newReadText = readText.replace(",", " ");
-                readTextBuilder.append(newReadText).append(System.lineSeparator());
-                readText = bufferedReader.readLine();
-            }
-        } catch (IOException e) {
-            throw new RuntimeException("not is good read file", e);
-        }
-        //////////////////////////////// Operation Process /////////////////////////////////////
+    public void getStatistic(String fromFileName, String toFileName) {
+        StringBuilder readTextBuilder = getTextFromFile(fromFileName);
         long supply = 0;
         long buy = 0;
         long result = 0;
@@ -44,9 +31,28 @@ public class WorkWithFile {
                 .append("supply,").append(supply).append(System.lineSeparator())
                 .append("buy,").append(buy).append(System.lineSeparator())
                 .append("result,").append(result);
-        //////////////////////////////////////Write to Files////////////////////////////////////
+        String text = String.valueOf(readTextBuilder);
+        writeToFileName(toFileName, text);
+    }
+
+    public StringBuilder getTextFromFile(String fromFileName) {
+        StringBuilder readTextBuilder = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
+            String readText = bufferedReader.readLine();
+            while (readText != null) {
+                String newReadText = readText.replace(",", " ");
+                readTextBuilder.append(newReadText).append(System.lineSeparator());
+                readText = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("not is good read file", e);
+        }
+        return readTextBuilder;
+    }
+
+    public void writeToFileName(String toFileName, String textWriteFile) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
-            bufferedWriter.write(String.valueOf(readTextBuilder));
+            bufferedWriter.write(String.valueOf(textWriteFile));
             bufferedWriter.flush();
         } catch (IOException e) {
             throw new RuntimeException("not is good write to file", e);
