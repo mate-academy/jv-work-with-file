922a8d0414375adcd38007923b8fae6e2f95cc59
sasha <radovietsoleksandr@gmail.com>
2022-09-18 17:30:52 +0300

    file1

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,8 +1,52 @@
 package core.basesyntax;
 
+import java.io.*;
+
 public class WorkWithFile {
 
-    public void getStatistic(String fromFileName, String toFileName) {
-
+    public static void getStatistic(String fromFileName, String toFileName) {
+        StringBuilder readTextBuilder = new StringBuilder();
+        /////////////////////////////READ from FILE/////////////////////////////////////////////
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
+            String readText = bufferedReader.readLine();
+            while (readText != null) {
+                String newReadText = readText.replace(",", " ");
+                readTextBuilder.append(newReadText).append(System.lineSeparator());
+                readText = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("not is good read file", e);
+        }
+        //////////////////////////////// Operation Process /////////////////////////////////////
+        long supply = 0;
+        long buy = 0;
+        long result = 0;
+        String[] optimizationText = String.valueOf(readTextBuilder).split(System.lineSeparator());
+        for (String cloud : optimizationText) {
+            String[] cutText = cloud.split(" ");
+            if (cutText[0].equals("supply")) {
+                supply = supply + Integer.parseInt(cutText[1]);
+            } else if (cutText[0].equals("buy")) {
+                buy = buy + Integer.parseInt(cutText[1]);
+            }
+        }
+        readTextBuilder = new StringBuilder();
+        if (supply > buy) {
+            result = supply - buy;
+        } else if (supply < buy) {
+            result = buy - supply;
+        }
+        readTextBuilder
+                .append("supply,").append(supply).append(System.lineSeparator())
+                .append("buy,").append(buy).append(System.lineSeparator())
+                .append("result,").append(result);
+        //////////////////////////////////////Write to Files////////////////////////////////////
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+            bufferedWriter.write(String.valueOf(readTextBuilder));
+            bufferedWriter.flush();
+        } catch (IOException e) {
+            throw new RuntimeException("not is good write to file", e);
+        }
     }
 }
+
