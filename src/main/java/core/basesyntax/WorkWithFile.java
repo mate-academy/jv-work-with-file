a4363fbb4c4f8eefe7e56359524d65bac5ceaada
ValeriyM250 <valeriym250@gmail.com>
2022-09-19 13:53:46 +0300

    All logic in createReport  I used Files, readAllLines for my solution

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,69 +1,39 @@
 package core.basesyntax;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class WorkWithFile {
-    public static final int OPERATION_INDEX = 0;
-    public static final int COUNT_INDEX = 1;
-
     public void getStatistic(String fromFileName, String toFileName) {
-
-        String dataFromFile = readFromFile(fromFileName);
-        String report = createReport(dataFromFile);
-        writeToFile(report, toFileName);
-
-    }
-
-    private String readFromFile(String fromFileName) {
-        File file = new File(fromFileName);
-        String [] line = new String[2];
-        int supplyQuantity = 0;
-        int buyQuantity = 0;
+        int supply = 0;
+        int buy = 0;
         try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
-            String reading = bufferedReader.readLine();
-            while (reading != null) {
-                line = reading.split(",");
-                if (line[OPERATION_INDEX].equals("buy")) {
-                    supplyQuantity += Integer.parseInt(line[COUNT_INDEX]);
-                } else {
-                    buyQuantity += Integer.parseInt(line[COUNT_INDEX]);
+            String lines = Files.readAllLines(Paths.get(fromFileName)).toString();
+            String [] line = lines.split(" ");
+            line[0] = line[0].substring(1);
+            for (int i = 0; i < line.length; i++) {
+                line[i] = line[i].substring(0, line[i].length() - 1);
+                int index1 = line[i].indexOf(',');
+                if (line[i].substring(0, index1). equals("supply")) {
+                    supply += Integer.parseInt(line[i].substring(index1 + 1));
+                    continue;
                 }
-                reading = bufferedReader.readLine();
+                buy += Integer.parseInt(line[i].substring(index1 + 1));
             }
-        } catch (FileNotFoundException e) {
-            System.out.println("Can`t read file: " + fromFileName + " " + e);
         } catch (IOException e) {
-            System.out.println("Can`t read line from file: " + fromFileName + " " + e);
+            throw new RuntimeException("Can`t read current document", e);
         }
-        line[0] = supplyQuantity + "," + buyQuantity;
-        return line[0];
-    }
-
-    private String createReport(String dataForReport) {
-        int index = dataForReport.indexOf(',');
-        String report = dataForReport.substring(0, index);
-        dataForReport = dataForReport.substring(index + 1);
-
-        report = "supply," + dataForReport + System.lineSeparator()
-                    + "buy," + report + System.lineSeparator()
-                    + "result," + (Integer.parseInt(dataForReport)
-                    - Integer.parseInt(report));
-        return report;
-    }
-
-    private void writeToFile(String data, String fileName) {
-        File file = new File(fileName);
+        File file = new File(toFileName);
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file, true))) {
-            bufferedWriter.write(data);
+            bufferedWriter.write("supply," + supply + System.lineSeparator()
+                    + "buy," + buy + System.lineSeparator()
+                    + "result," + (supply - buy));
         } catch (IOException e) {
-            System.out.println("Can`t write data in " + file.getName() + ", " + e);
+            System.out.println("Can`t write data in " + toFileName + ", " + e);
         }
     }
 }
