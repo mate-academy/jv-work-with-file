e5d6ee6236e0072948e4cd0a9e566f1840c8c91e
EvgenyTyshchenko <evgeny.tyshchenko@gmail.com>
2022-09-29 12:37:06 +0300

    Vession 3: local variables, ordering class, redundant trim() fixed

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -16,9 +16,12 @@ public class WorkWithFile {
     private static final String RESULT_STRING = "result";
     private static final String LINE_SEPARATOR = System.lineSeparator();
     private static final String EMPTY_STRING = "";
-    private StringBuilder stringBuilder = new StringBuilder();
-    private int supplyAmount = 0;
-    private int buyAmount = 0;
+    private final StringBuilder stringBuilder = new StringBuilder();
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        readDataFromFile(fromFileName);
+        createReportAndWriteToFile(toFileName);
+    }
 
     private void readDataFromFile(String fromFileName) {
         File file = new File(fromFileName);
@@ -35,43 +38,26 @@ public class WorkWithFile {
         }
     }
 
-    private void calculateSums() {
+    private void createReportAndWriteToFile(String toFileName) {
+        int supplyAmount = 0;
+        int buyAmount = 0;
         String[] readingContent = stringBuilder.toString().split(LINE_SEPARATOR);
         for (String row : readingContent) {
             String[] rowInAray = row.split(DATA_SEPARATOR);
-            String operationType = rowInAray[OPERATION_TYPE_COLUMN_INDEX].trim();
-            int amount = Integer.parseInt(rowInAray[AMOUNT_COLUMN_INDEX].trim());
+            String operationType = rowInAray[OPERATION_TYPE_COLUMN_INDEX];
+            int amount = Integer.parseInt(rowInAray[AMOUNT_COLUMN_INDEX]);
             if (operationType.equals(SUPPLY_STRING)) {
                 supplyAmount += amount;
             } else {
                 buyAmount += amount;
             }
         }
-    }
-
-    private void writeDataToFile(String toFileName) {
-        BufferedWriter bufferedWriter = null;
-        try {
-            bufferedWriter = new BufferedWriter(new FileWriter(toFileName, true));
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName, true))) {
             bufferedWriter.write(SUPPLY_STRING + DATA_SEPARATOR + supplyAmount + LINE_SEPARATOR);
             bufferedWriter.write(BUE_STRING + DATA_SEPARATOR + buyAmount + LINE_SEPARATOR);
             bufferedWriter.write(RESULT_STRING + DATA_SEPARATOR + (supplyAmount - buyAmount));
         } catch (IOException e) {
             throw new RuntimeException("Can't write data to file", e);
-        } finally {
-            if (bufferedWriter != null) {
-                try {
-                    bufferedWriter.close();
-                } catch (IOException e) {
-                    throw new RuntimeException("Can't close BufferedWriter", e);
-                }
-            }
         }
-    }
-
-    public void getStatistic(String fromFileName, String toFileName) {
-        readDataFromFile(fromFileName);
-        calculateSums();
-        writeDataToFile(toFileName);
     }
 }
