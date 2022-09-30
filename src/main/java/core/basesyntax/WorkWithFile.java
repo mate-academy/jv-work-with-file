9ae686d0bacd91d02e319346d004f2a22174a6e1
Bodddd <skrypnikn@gmail.com>
2022-09-30 19:16:23 +0300

    add getSupplyAndBuy() and use one call the method write()

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -16,36 +16,44 @@ public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
         File fromFile = new File(fromFileName);
         File toFile = new File(toFileName);
-        int[] info = getInfoFromCsvFile(fromFile);
+        List<String> listFormat = getListFromCsvFile(fromFile);
+        int[] info = getSupplyAndBuy(listFormat);
         writeInfoToCsvFile(info[SUPPLY_INDEX],info[BUY_INDEX],toFile);
     }
 
-    private int[] getInfoFromCsvFile(File fromFile) {
-        int supply = 0;
-        int buy = 0;
+    private List<String> getListFromCsvFile(File fromFile) {
+        List<String> lines;
         try {
-            List<String> lines = Files.readAllLines(fromFile.toPath());
-            for (String info : lines) {
-                String[] data = info.split(SEPARATED_VALUES);
-                if (data[TEXT_INFO_INDEX].equals("supply")) {
-                    supply += Integer.parseInt(data[1]);
-                } else {
-                    buy += Integer.parseInt(data[1]);
-                }
-            }
+            lines = Files.readAllLines(fromFile.toPath());
         } catch (IOException e) {
             throw new RuntimeException("Can't read data from " + fromFile.getName(), e);
         }
-        return new int[]{supply,buy};
+        return lines;
+    }
+
+    private int[] getSupplyAndBuy(List<String> lines) {
+        int supply = 0;
+        int buy = 0;
+        for (String info : lines) {
+            String[] data = info.split(SEPARATED_VALUES);
+            if (data[TEXT_INFO_INDEX].equals("supply")) {
+                supply += Integer.parseInt(data[1]);
+            } else {
+                buy += Integer.parseInt(data[1]);
+            }
+        }
+        return new int[]{supply, buy};
     }
 
     private void writeInfoToCsvFile(int supply, int buy, File toFile) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFile))) {
-            bufferedWriter.write("supply," + supply + System.lineSeparator());
-            bufferedWriter.write("buy," + buy + System.lineSeparator());
-            bufferedWriter.write("result," + (supply - buy) + System.lineSeparator());
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append("supply,").append(supply).append(System.lineSeparator())
+                         .append("buy,").append(buy).append(System.lineSeparator())
+                         .append("result,").append(supply - buy).append(System.lineSeparator());
+            bufferedWriter.write(stringBuilder.toString());
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException("Can't find file " + toFile.getName(), e);
         }
     }
 }
