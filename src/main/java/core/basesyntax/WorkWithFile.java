e4e0d4cc1fdaaca97ad84fc83ab879aac3601e06
Volodymyr Oliinyk <volodymyr.oliinyk89@gmail.com>
2022-10-01 10:04:46 +0300

    added fixes after review

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -14,43 +14,45 @@ public class WorkWithFile {
     private static final String RESULT_ITEM = "result";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        File toFile = new File(toFileName);
+        File file = new File(toFileName);
         try {
-            toFile.createNewFile();
-        } catch (IOException e) {
-            throw new RuntimeException("Can't create file " + toFileName, e);
-        }
-        try {
-            Files.write(toFile.toPath(), readFromFile(fromFileName).getBytes(),
+            file.createNewFile();
+            Files.write(file.toPath(), createReport(readFromFile(fromFileName)).getBytes(),
                     StandardOpenOption.APPEND);
         } catch (IOException e) {
-            throw new RuntimeException("Can't write to file" + toFileName, e);
+            throw new RuntimeException("Can't write to file " + toFileName, e);
         }
     }
 
-    private String readFromFile(String fromFileName) {
-        File fromFile = new File(fromFileName);
+    private List<String> readFromFile(String fromFileName) {
+        File file = new File(fromFileName);
+        List<String> statistic;
+        try {
+            file.createNewFile();
+            statistic = Files.readAllLines(file.toPath());
+        } catch (IOException e) {
+            throw new RuntimeException("Cant't read file " + fromFileName, e);
+        }
+        return statistic;
+    }
+
+    private String createReport(List<String> statistic) {
         int supply = 0;
         int buy = 0;
         int result;
-        try {
-            List<String> statistic = Files.readAllLines(fromFile.toPath());
-            for (String items : statistic) {
-                String[] item = items.split(",");
-                if (item[NAME_INDEX].equals(SUPPLY_ITEM)) {
-                    supply += Integer.parseInt(item[SUM_INDEX]);
-                } else if (item[NAME_INDEX].equals(BUY_ITEM)) {
-                    buy += Integer.parseInt(item[SUM_INDEX]);
-                }
+        for (String items : statistic) {
+            String[] item = items.split(",");
+            if (item[NAME_INDEX].equals(SUPPLY_ITEM)) {
+                supply += Integer.parseInt(item[SUM_INDEX]);
+            } else if (item[NAME_INDEX].equals(BUY_ITEM)) {
+                buy += Integer.parseInt(item[SUM_INDEX]);
             }
-            result = supply - buy;
-        } catch (IOException e) {
-            throw new RuntimeException("Can't read file " + fromFileName, e);
         }
-        StringBuilder writeInfo = new StringBuilder();
-        writeInfo.append(SUPPLY_ITEM).append(",").append(supply).append(System.lineSeparator())
+        result = supply - buy;
+        StringBuilder report = new StringBuilder();
+        report.append(SUPPLY_ITEM).append(",").append(supply).append(System.lineSeparator())
                 .append(BUY_ITEM).append(",").append(buy).append(System.lineSeparator())
                 .append(RESULT_ITEM).append(",").append(result);
-        return writeInfo.toString();
+        return report.toString();
     }
 }
