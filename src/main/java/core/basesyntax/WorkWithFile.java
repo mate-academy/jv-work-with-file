650d21c2528189cfb680a2be95e39ea0a9b8a0d5
Vitalina Volkivska <113260060+Vitalina-V@users.noreply.github.com>
2022-10-02 10:45:47 +0300

    ComparisonFailure

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,8 +1,45 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WorkWithFile {
 
     public void getStatistic(String fromFileName, String toFileName) {
-
+        int supplyCount = 0;
+        int buyCount = 0;
+        File file = new File(fromFileName);
+        StringBuilder builder = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+            String value = bufferedReader.readLine();
+            while (value != null) {
+                builder.append(value).append(",");
+                value = bufferedReader.readLine();
+            }
+            String result = builder.toString();
+            String[] split = result.split(",");
+            File file1 = new File(toFileName);
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file1));
+            StringBuilder builder1 = new StringBuilder("supply,");
+            for (int i = 0; i < split.length; i++) {
+                if (split[i].equals("supply")) {
+                    supplyCount += Integer.parseInt(split[i + 1]);
+                }
+                if (split[i].equals("buy")) {
+                    buyCount += Integer.parseInt(split[i + 1]);
+                }
+            }
+            builder1.append(supplyCount).append("\nbuy,").append(buyCount)
+                    .append("\nresult,").append(supplyCount - buyCount);
+            String report = builder1.toString();
+            writer.write(report);
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("Can`t read a file");
+        }
     }
 }
