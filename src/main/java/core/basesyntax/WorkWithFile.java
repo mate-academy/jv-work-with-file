19b8c1c697eee9da91f63a5d009fac1221c7fc61
Tymur Prokhorov <tymurprokhorov@gmail.com>
2022-10-04 20:00:20 +0300

    method separated, import fixed, TWR implemented

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,28 +1,60 @@
 package core.basesyntax;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class WorkWithFile {
+    private static final String LINE_SEPARATOR = System.lineSeparator();
+    private static final String DATA_SEPARATOR = ", ";
+    private static final String SUPPLY_CONSTANT_FOR_RESULT = "supply,";
+    private static final String BUY_CONSTANT_FOR_RESULT = "buy,";
+    private static final String RESULT_CONSTANT = "result,";
+    private String dataFromFile;
+    private String report;
+
+    public String getDataFromFile() {
+        return dataFromFile;
+    }
+
+    public void setDataFromFile(String dataFromFile) {
+        this.dataFromFile = dataFromFile;
+    }
+
+    public String getReport() {
+        return report;
+    }
+
+    public void setReport(String report) {
+        this.report = report;
+    }
+
     public void getStatistic(String fromFileName, String toFileName) {
-        // read from file
+        readFromFile(fromFileName);
+        creatingReport();
+        writeToFile(toFileName);
+    }
+
+    private void readFromFile(String fromFileName) {
         File file = new File(fromFileName);
         StringBuilder stringBuilder = new StringBuilder();
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(file));
-
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             String value = reader.readLine();
             while (value != null) {
-                stringBuilder.append(value).append(System.lineSeparator());
+                stringBuilder.append(value).append(LINE_SEPARATOR);
                 value = reader.readLine();
             }
         } catch (IOException e) {
             throw new RuntimeException("Can`t read data from file", e);
         }
-        String dataFromFile = stringBuilder.toString();
-        // read from file ends
+        dataFromFile = stringBuilder.toString();
+    }
 
-        // report creating
-        String[] dataArray = dataFromFile.split(", ");
+    private void creatingReport() {
+        String[] dataArray = dataFromFile.split(DATA_SEPARATOR);
         int buyValues = 0;
         int supplyValues = 0;
         for (int i = 0; i < dataArray.length; i++) {
@@ -35,25 +67,24 @@ public class WorkWithFile {
         }
         int result = supplyValues - buyValues;
         StringBuilder reportBuilder = new StringBuilder();
-        String report = reportBuilder
-                .append("supply,")
+        report = reportBuilder
+                .append(SUPPLY_CONSTANT_FOR_RESULT)
                 .append(supplyValues)
                 .append(System.lineSeparator())
-                .append("buy,")
+                .append(BUY_CONSTANT_FOR_RESULT)
                 .append(buyValues)
                 .append(System.lineSeparator())
-                .append("result,")
+                .append(RESULT_CONSTANT)
                 .append(result)
                 .toString();
-        // report creating ends
+    }
 
-        // write to file
+    private void writeToFile(String toFileName) {
         File file1 = new File(toFileName);
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file1, true))) {
             bufferedWriter.write(report);
         } catch (IOException e) {
             throw new RuntimeException("Can`t write data to file", e);
         }
-        //write to file ends
     }
 }
