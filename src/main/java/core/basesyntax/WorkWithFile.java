091ce2eea909812b1b460b0e519ab1f7ca16e7d9
Oleksiy Masaltsev <a.o.masaltsev@gmail.com>
2022-10-04 17:22:06 +0300

    some literals are replaced with constants

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -12,6 +12,9 @@ public class WorkWithFile {
     public static final String BUY = "buy";
     public static final String RESULT = "result";
     public static final String ERROR_MESSAGE = "Error while working with file";
+    public static final String SEPARATOR = ",";
+    public static final int OPERATION_INDEX = 0;
+    public static final int AMOUNT_INDEX = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
         int supplySum = 0;
@@ -23,12 +26,12 @@ public class WorkWithFile {
                         new BufferedOutputStream(new FileOutputStream(toFileName)))) {
             String line;
             while ((line = reader.readLine()) != null) {
-                String[] lineArray = line.split(",");
+                String[] lineArray = line.split(SEPARATOR);
                 if (lineArray.length == 2) {
-                    if (lineArray[0].equals(SUPPLY)) {
-                        supplySum += Integer.parseInt(lineArray[1]);
-                    } else if (lineArray[0].equals(BUY)) {
-                        buySum += Integer.parseInt(lineArray[1]);
+                    if (lineArray[OPERATION_INDEX].equals(SUPPLY)) {
+                        supplySum += Integer.parseInt(lineArray[AMOUNT_INDEX]);
+                    } else if (lineArray[OPERATION_INDEX].equals(BUY)) {
+                        buySum += Integer.parseInt(lineArray[AMOUNT_INDEX]);
                     }
                 }
             }
