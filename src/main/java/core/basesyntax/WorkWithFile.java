b4c72f2439c032558b6c4bfbcb87d1803683afb3
Anton Pushkar <tosha.pushkar@gmail.com>
2022-10-04 21:41:20 +0300

    Correct hw

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,12 +8,14 @@ import java.nio.file.Files;
 import java.util.List;
 
 public class WorkWithFile {
+    public static final String COMA = "\\,";
+    public static final int INDEX_ONE = 1;
+    public static final int INDEX_ZERO = 0;
+    public static final String SEPARATOR = System.lineSeparator();
 
     public void getStatistic(String fromFileName, String toFileName) {
-        try {
-            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName));
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
             bufferedWriter.write(getResultOfFile(fromFileName));
-            bufferedWriter.close();
         } catch (IOException e) {
             throw new RuntimeException("Can`t read file" + e);
         }
@@ -27,15 +29,15 @@ public class WorkWithFile {
             int buyNum = 0;
             int supplyNum = 0;
             for (String line : allParth) {
-                if (line.split("\\,")[0].startsWith("supply")) {
-                    supplyNum += Integer.parseInt(line.split("\\,")[1]);
-                } else if (line.split("\\,")[0].startsWith("buy")) {
-                    buyNum += Integer.parseInt(line.split("\\,")[1]);
+                if (line.split(COMA)[INDEX_ZERO].startsWith("supply")) {
+                    supplyNum += Integer.parseInt(line.split(COMA)[INDEX_ONE]);
+                } else if (line.split(COMA)[INDEX_ZERO].startsWith("buy")) {
+                    buyNum += Integer.parseInt(line.split(COMA)[INDEX_ONE]);
                 }
             }
             int result = supplyNum - buyNum;
-            return "supply," + supplyNum + System.lineSeparator() + "buy," + buyNum
-                    + System.lineSeparator() + "result," + result;
+            return "supply," + supplyNum + SEPARATOR + "buy," + buyNum
+                    + SEPARATOR + "result," + result;
         } catch (IOException e) {
             throw new RuntimeException("Can`t read file" + e);
         }
