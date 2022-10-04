fa40934531a1d1feb72d6752e826f3783bd78727
thebitler <yurchyknikita@gmail.com>
2022-10-05 00:15:29 +0200

    Added constants

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -9,7 +9,10 @@ import java.nio.file.Files;
 public class WorkWithFile {
     private static final String OPERATION_SUPPLY = "supply";
     private static final String OPERATION_BUY = "buy";
+    private static final String RESULT_LOG_STRING = "result";
     private static final String ROW_SEPARATOR = ",";
+    private static final int DATA_INDEX_OPERATION_TYPE = 0;
+    private static final int DATA_INDEX_AMOUNT = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
@@ -17,8 +20,8 @@ public class WorkWithFile {
             int buy = 0;
             for (String row : Files.readAllLines(new File(fromFileName).toPath())) {
                 String[] rowData = row.split(ROW_SEPARATOR);
-                int amount = Integer.parseInt(rowData[1]);
-                if (OPERATION_SUPPLY.equals(rowData[0])) {
+                int amount = Integer.parseInt(rowData[DATA_INDEX_AMOUNT]);
+                if (OPERATION_SUPPLY.equals(rowData[DATA_INDEX_OPERATION_TYPE])) {
                     supply += amount;
                 } else {
                     buy += amount;
@@ -27,7 +30,7 @@ public class WorkWithFile {
             bufferedWriter.write(
                     OPERATION_SUPPLY + ROW_SEPARATOR + supply
                             + System.lineSeparator() + OPERATION_BUY + ROW_SEPARATOR + buy
-                            + System.lineSeparator() + "result" + ROW_SEPARATOR + (supply - buy)
+                            + System.lineSeparator() + RESULT_LOG_STRING + ROW_SEPARATOR + (supply - buy)
             );
         } catch (IOException e) {
             throw new RuntimeException("Cannot perform this operation", e);
