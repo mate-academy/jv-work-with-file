79b53374ce6dcd52c6a2766d47552da0f1955090
Veronika Tretiak <nikatretiak9@gmail.com>
2022-10-05 10:32:38 +0200

    implemented task after mvn clean package

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,11 +1,11 @@
 package core.basesyntax;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class WorkWithFile {
     private static final String REGEX = "\\W+";
@@ -22,19 +22,19 @@ public class WorkWithFile {
 
     }
 
-        private String readFromFile(String fromFileName) {
-            StringBuilder stringBuilder = new StringBuilder();
-            try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
-                String value = reader.readLine();
-                while (value != null) {
-                    stringBuilder.append(value).append(System.lineSeparator());
-                    value = reader.readLine();
-                }
-            } catch (IOException e) {
-                throw new RuntimeException("Can't read file", e);
+    private String readFromFile(String fromFileName) {
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
+            String value = reader.readLine();
+            while (value != null) {
+                stringBuilder.append(value).append(System.lineSeparator());
+                value = reader.readLine();
             }
-            return stringBuilder.toString();
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
         }
+        return stringBuilder.toString();
+    }
 
     private String[] getInfoForReport(String fileRead) {
         String[] separateFile = fileRead.split(REGEX);
