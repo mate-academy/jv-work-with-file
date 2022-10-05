ec9ccdd0bbc3b541b15c5b80b79db20e7b5c9e3f
Oleg Marchuk <mar4uk14@gmail.com>
2022-10-05 15:50:01 +0300

    Fix mistakes.

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,44 +8,56 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    private int supply = 0;
     private int buy = 0;
+    private int supply = 0;
     private int result = 0;
-    private final String split = ",";
-    private int index = 1;
+    private int[] res = {supply, buy, result};
 
     public void getStatistic(String fromFileName, String toFileName) {
-        File file = new File(fromFileName);
+        writeToFile(calculate(readFile(new File(fromFileName))), toFileName);
+    }
+
+    private StringBuilder readFile(File file) {
         StringBuilder builder = new StringBuilder();
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(file));
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             String value = reader.readLine();
             while (value != null) {
                 builder.append(value).append(System.lineSeparator());
                 value = reader.readLine();
             }
-            String[] split = builder.toString().replaceAll("\r\n", ",").split(",");
-            for (String test : split) {
-                if (test.equals("supply")) {
-                    supply = supply + Integer.parseInt(split[index]);
-                    index += 2;
-                }
-                if (test.equals("buy")) {
-                    buy = buy + Integer.parseInt(split[index]);
-                    index += 2;
-                }
-            }
-            result = supply - buy;
         } catch (IOException e) {
             throw new RuntimeException("Can't read this file", e);
         }
+        return builder;
+    }
+
+    private int[] calculate(StringBuilder builder) {
+        int index = 1;
+        final int value1 = 2;
+        String[] split = builder.toString().replaceAll("\r\n", ",").split(",");
+        for (String test : split) {
+            if (test.equals("supply")) {
+                supply = supply + Integer.parseInt(split[index]);
+                index += value1;
+            }
+            if (test.equals("buy")) {
+                buy = buy + Integer.parseInt(split[index]);
+                index += value1;
+            }
+        }
+        result = supply - buy;
+        int[] res = {supply, buy, result};
+        return res;
+    }
+
+    private void writeToFile(int[] res, String toFileName) {
         try {
             BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName, true));
-            writer.write("supply," + supply + System.lineSeparator());
+            writer.write("supply," + res[0] + System.lineSeparator());
             writer.flush();
-            writer.write("buy," + buy + System.lineSeparator());
+            writer.write("buy," + res[1] + System.lineSeparator());
             writer.flush();
-            writer.write("result," + result + System.lineSeparator());
+            writer.write("result," + res[2] + System.lineSeparator());
             writer.flush();
             writer.close();
         } catch (IOException e) {
@@ -53,3 +65,4 @@ public class WorkWithFile {
         }
     }
 }
+
