b806d73f0a44cf1d9a0f304af31267fd17cd139c
Roman Usyk <roman.usik.93@ukr.net>
2022-10-05 13:01:17 +0300

    the program was created

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,8 +1,82 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WorkWithFile {
+    private static final int SIZE_OF_RESULT_ARRAY = 3;
 
     public void getStatistic(String fromFileName, String toFileName) {
+        File file = new File(fromFileName);
+        int countS = 0;
+        int countB = 0;
+        int sumS = 0;
+        int sumB = 0;
+        int difference;
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            StringBuilder stringBuilder = new StringBuilder();
+            String value = bufferedReader.readLine();
+            while (value != null) {
+                stringBuilder.append(value).append("\n");
+                value = bufferedReader.readLine();
+            }
+            String stringBuilderToString = stringBuilder.toString();
+            String[] result = new String[SIZE_OF_RESULT_ARRAY];
+            String[] stringBuilderToStringArray = stringBuilderToString.split("\n");
+            for (int i = 0; i < stringBuilderToStringArray.length; i++) {
+                if (stringBuilderToStringArray[i].charAt(0) == 's') {
+                    countS++;
+                    if (countS == 1) {
+                        int index = stringBuilderToStringArray[i].indexOf(",");
+                        result[0] = stringBuilderToStringArray[i].substring(0, index + 1);
+                    }
+                }
+                if (stringBuilderToStringArray[i].charAt(0) == 'b') {
+                    countB++;
+                    if (countB == 1) {
+                        int index = stringBuilderToStringArray[i].indexOf(",");
+                        result[1] = stringBuilderToStringArray[i].substring(0, index + 1);
+                    }
+                }
+            }
+            for (int i = 0; i < stringBuilderToStringArray.length; i++) {
+                if (stringBuilderToStringArray[i].charAt(0) == 's') {
+                    int indexOf = stringBuilderToStringArray[i].indexOf(",");
+                    stringBuilderToStringArray[i] =
+                            stringBuilderToStringArray[i].substring(indexOf + 1);
+                    sumS = sumS + Integer.parseInt(stringBuilderToStringArray[i]);
 
+                }
+                if (stringBuilderToStringArray[i].charAt(0) == 'b') {
+                    int indexOf = stringBuilderToStringArray[i].indexOf(",");
+                    stringBuilderToStringArray[i] =
+                            stringBuilderToStringArray[i].substring(indexOf + 1);
+                    sumB = sumB + Integer.parseInt(stringBuilderToStringArray[i]);
+                }
+            }
+            difference = sumS - sumB;
+            result[0] = result[0] + sumS;
+            result[1] = result[1] + sumB;
+            result[2] = "result," + difference;
+            File toFile = new File(toFileName);
+            for (String res : result) {
+                try {
+                    BufferedWriter bufferedWriter =
+                            new BufferedWriter(new FileWriter(toFile, true));
+                    bufferedWriter.write(res);
+                    bufferedWriter.write(System.lineSeparator());
+                    bufferedWriter.close();
+                } catch (IOException e) {
+                    throw new RuntimeException("Can`t write data file", e);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t read file", e);
+        }
     }
 }
