65c7694309494e8af9af529c9d1df6d2729b4fac
thebitler <yurchyknikita@gmail.com>
2022-10-05 17:17:40 +0200

    Refactored. Split getStatistic() logics into smaller methods

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.List;
 
 public class WorkWithFile {
     private static final String OPERATION_SUPPLY = "supply";
@@ -13,27 +14,43 @@ public class WorkWithFile {
     private static final String ROW_SEPARATOR = ",";
     private static final int DATA_INDEX_OPERATION_TYPE = 0;
     private static final int DATA_INDEX_AMOUNT = 1;
+    private int supply = 0;
+    private int buy = 0;
+    private String fromFileName;
 
     public void getStatistic(String fromFileName, String toFileName) {
+        this.fromFileName = fromFileName;
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
-            int supply = 0;
-            int buy = 0;
-            for (String row : Files.readAllLines(new File(fromFileName).toPath())) {
-                String[] rowData = row.split(ROW_SEPARATOR);
-                int amount = Integer.parseInt(rowData[DATA_INDEX_AMOUNT]);
-                if (OPERATION_SUPPLY.equals(rowData[DATA_INDEX_OPERATION_TYPE])) {
-                    supply += amount;
-                } else {
-                    buy += amount;
-                }
-            }
-            bufferedWriter.write(
-                    OPERATION_SUPPLY + ROW_SEPARATOR + supply
-                            + System.lineSeparator() + OPERATION_BUY + ROW_SEPARATOR + buy
-                            + System.lineSeparator() + RESULT_LOG_STRING + ROW_SEPARATOR + (supply - buy)
-            );
+            countOperationsCost();
+            writeToFile(bufferedWriter);
         } catch (IOException e) {
             throw new RuntimeException("Cannot perform this operation", e);
         }
+    }
+
+    private void countOperationsCost() throws IOException {
+        for (String row : getFileLines()) {
+            String[] rowData = row.split(ROW_SEPARATOR);
+            int amount = Integer.parseInt(rowData[DATA_INDEX_AMOUNT]);
+            if (OPERATION_SUPPLY.equals(rowData[DATA_INDEX_OPERATION_TYPE])) {
+                supply += amount;
+            } else {
+                buy += amount;
+            }
+        }
+    }
+
+    private List<String> getFileLines() throws IOException {
+        return Files.readAllLines(new File(fromFileName).toPath());
+    }
+
+    private void writeToFile(BufferedWriter bufferedWriter) throws IOException {
+        bufferedWriter.write(getLogInfo());
+    }
+
+    private String getLogInfo() {
+        return OPERATION_SUPPLY + ROW_SEPARATOR + supply
+                + System.lineSeparator() + OPERATION_BUY + ROW_SEPARATOR + buy
+                + System.lineSeparator() + RESULT_LOG_STRING + ROW_SEPARATOR + (supply - buy);
     }
 }
