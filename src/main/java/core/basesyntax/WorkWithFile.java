bb151beee5ace81205117429476509e7deb1dd81
Mark Rybnikov <mark.rybnikov@amdaris.com>
2022-10-05 15:19:20 +0300

    Diveded try block for three blocks

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -9,36 +9,42 @@ import java.io.IOException;
 public class WorkWithFile {
 
     public void getStatistic(String fromFileName, String toFileName) {
-        char[] array = new char[99999];
+        char[] array = new char[9999];
         int supply = 0;
         int buy = 0;
         try (FileReader input = new FileReader(fromFileName);) {
             input.read(array);
-            String textFromFile = new String(array);
-            String[] strings = textFromFile.split("\n");
-
-            for (String line: strings) {
-                String[] strArr = line.split(",");
-                switch (strArr[0]) {
-                    case "supply": supply += Integer.parseInt(strArr[1].replaceAll("\r",""));
-                        break;
-                    case "buy": buy += Integer.parseInt(strArr[1].replaceAll("\r",""));
-                        break;
-                    default: ;
-                }
-            }
-            File file = new File(toFileName);
-            file.createNewFile();
-            FileWriter in = new FileWriter(file);
-            in.write("supply," + supply + System.lineSeparator());
-            in.write("buy," + buy + System.lineSeparator());
-            int result = supply - buy;
-            in.write("result," + result);
-            in.close();
         } catch (FileNotFoundException e) {
             throw new RuntimeException("File not found " + fromFileName, e);
         } catch (IOException e) {
             throw new RuntimeException("Can't read data from the file " + fromFileName, e);
+        }
+        String textFromFile = new String(array);
+        String[] strings = textFromFile.split("\n");
+        for (String line: strings) {
+            String[] strArr = line.split(",");
+            switch (strArr[0]) {
+                case "supply": supply += Integer.parseInt(strArr[1].replaceAll("\r",""));
+                    break;
+                case "buy": buy += Integer.parseInt(strArr[1].replaceAll("\r",""));
+                    break;
+                default: ;
+            }
+        }
+        File file = new File(toFileName);
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            throw new RuntimeException("Cant create file" + fromFileName, e);
+        }
+
+        try (FileWriter in = new FileWriter(file)) {
+            in.write("supply," + supply + System.lineSeparator());
+            in.write("buy," + buy + System.lineSeparator());
+            int result = supply - buy;
+            in.write("result," + result);
+        } catch (IOException e) {
+            throw new RuntimeException("Cant write from file " + fromFileName, e);
         }
     }
 }
