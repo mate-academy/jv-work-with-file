a44774f5bcda8389f41c529a36f5a795b120e785
Yulia Tsariv <purplesquirrel68@gmail.com>
2022-10-05 12:28:43 +0300

    Fix all mistakes

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,41 +2,39 @@ package core.basesyntax;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class WorkWithFile {
 
-    public void getStatistic(String fromFileName, String toFileName) throws IOException {
-        List<String[]> list = new ArrayList<>();
+    private static final String ENTER = "\r\n";
+    private static final String COMMA = ",";
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+    private static final int INDEX_TYPE_OF_OPERATION = 0;
+    private static final int INDEX_AMMOUNT = 1;
+
+    public void getStatistic(String fromFileName, String toFileName) {
         List<String> listOfPosition = new ArrayList<>();
+        List<String[]> list = getDataFromFile(fromFileName);
         int supplyResult = 0;
-        try (
-                BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName));
-                BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName,
-                        true));) {
-            while (bufferedReader.ready()) {
-                String[] rows = bufferedReader.readLine().split(",");
-                list.add(rows);
-            }
-            for (String[] str : list) {
-                if (!listOfPosition.contains(str[0])) {
-                    listOfPosition.add(str[0]);
-                }
-            }
-            Collections.sort(listOfPosition, Collections.reverseOrder());
+
+        try (BufferedWriter bufferedWriter =
+                     new BufferedWriter(new FileWriter(toFileName, true))) {
+            listOfPosition.add(SUPPLY);
+            listOfPosition.add(BUY);
             for (String lst : listOfPosition) {
                 int result = 0;
                 for (String[] str : list) {
-                    if (str[0].equals(lst)) {
-                        result += Integer.parseInt(str[1]);
+                    if (str[INDEX_TYPE_OF_OPERATION].equals(lst)) {
+                        result += Integer.parseInt(str[INDEX_AMMOUNT]);
                     }
                 }
-                bufferedWriter.write(lst + "," + result + "\r\n");
+                bufferedWriter.write(lst + COMMA + result + ENTER);
                 if (supplyResult == 0) {
                     supplyResult = result;
                 } else {
@@ -47,5 +45,22 @@ public class WorkWithFile {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static List<String[]> getDataFromFile(String fromFile) {
+        List<String[]> list = new ArrayList<>();
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFile));
+            while (bufferedReader.ready()) {
+                String[] rows = bufferedReader.readLine().split(",");
+                list.add(rows);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return list;
     }
 }
