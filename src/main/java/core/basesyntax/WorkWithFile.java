5dd6375e882161d0e20d16e6d8f6709d8dff1d2d
AlinaOryshak <alinaoryshak@gmail.com>
2022-10-05 15:54:27 +0300

    add constant COMMA

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -10,10 +10,9 @@ import java.io.IOException;
 public class WorkWithFile {
     static final String SUPPLY = "supply";
     static final String BUY = "buy";
+    static final String COMMA = ",";
     static final int OPERATION_INDEX = 0;
     static final int AMOUNT_INDEX = 1;
-    private int supplyCount = 0;
-    private int buyCount = 0;
 
     public void getStatistic(String fromFileName, String toFileName) {
         String input = readFile(fromFileName);
@@ -47,8 +46,10 @@ public class WorkWithFile {
 
     public String createReport(String data) {
         String[] dataStrings = data.split(System.lineSeparator());
+        int supplyCount = 0;
+        int buyCount = 0;
         for (String dataString : dataStrings) {
-            String[] operationAndAmount = dataString.split(",");
+            String[] operationAndAmount = dataString.split(COMMA);
             if (operationAndAmount[OPERATION_INDEX].equals(SUPPLY)) {
                 supplyCount += Integer.parseInt(operationAndAmount[AMOUNT_INDEX]);
             } else {
@@ -57,9 +58,9 @@ public class WorkWithFile {
         }
         int result = supplyCount - buyCount;
         StringBuilder report = new StringBuilder();
-        report.append(SUPPLY).append(",").append(supplyCount).append(System.lineSeparator())
-                .append(BUY).append(",").append(buyCount).append(System.lineSeparator())
-                .append("result").append(",").append(result);
+        report.append(SUPPLY).append(COMMA).append(supplyCount).append(System.lineSeparator())
+                .append(BUY).append(COMMA).append(buyCount).append(System.lineSeparator())
+                .append("result").append(COMMA).append(result);
         return report.toString();
     }
 }
