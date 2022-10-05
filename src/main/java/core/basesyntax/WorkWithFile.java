afc4745ca14a4480eaa0a8c59314254b1e26ddf9
Denys Domashevskyi <denis.domashevskiy28@gmail.com>
2022-10-05 13:12:42 +0300

    Move buy and supply variables to readFromFile method which now return String

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -9,25 +9,30 @@ import java.io.IOException;
 public class WorkWithFile {
     private static final int OPERATION_TYPE_INDEX = 0;
     private static final int AMMOUNT_INDEX = 1;
-    private int supply = 0;
-    private int buy = 0;
+    private static final int BUY_INDEX = 0;
+    private static final int SUPPLY_INDEX = 1;
+    private static final String SEPARATOR = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        readFromFile(fromFileName);
+        String[] value = readFromFile(fromFileName).split(SEPARATOR);
 
-        String builder = "supply," + supply + System.lineSeparator()
-                + "buy," + buy + System.lineSeparator()
-                + "result," + (supply - buy);
+        String builder = "supply," + value[SUPPLY_INDEX] + System.lineSeparator()
+                + "buy," + value[BUY_INDEX] + System.lineSeparator()
+                + "result,"
+                + (Integer.parseInt(value[SUPPLY_INDEX]) - Integer.parseInt(value[BUY_INDEX]));
 
         writeToFile(toFileName, builder);
     }
 
-    private void readFromFile(String fromFileName) {
+    private String readFromFile(String fromFileName) {
+        int buy = 0;
+        int supply = 0;
+
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             String value = bufferedReader.readLine();
 
             while (value != null) {
-                String[] lineValue = value.split(",");
+                String[] lineValue = value.split(SEPARATOR);
                 switch (lineValue[OPERATION_TYPE_INDEX]) {
                     case "buy":
                         buy += Integer.parseInt(lineValue[AMMOUNT_INDEX]);
@@ -43,6 +48,8 @@ public class WorkWithFile {
         } catch (IOException e) {
             throw new RuntimeException("Can't create a report for file: " + fromFileName, e);
         }
+
+        return buy + SEPARATOR + supply;
     }
 
     private void writeToFile(String toFileName, String message) {
