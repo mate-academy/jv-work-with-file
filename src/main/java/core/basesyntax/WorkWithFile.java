2cf5e81f5c6e2189815a7aef29a82fe13fa173e4
Oleksandr <oleksandrkorniychuk98@gmail.com>
2022-10-05 13:36:07 +0300

    It was so hard... But I did it! I think... 1. created new method which return statistic. 2. minimized code in writeToFile method

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,51 +8,53 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    private static final String LINE_SEPARATOR = System.lineSeparator();
-    private static final String SUPPLY = "supply,";
-    private static final String BUY = "buy,";
-    private static final String RESULT = "result,";
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+    private static final String RESULT = "result";
+    private static final String COMMA = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        File file = new File(fromFileName);
+        String [] getResult = readFromFile(new File(fromFileName)).split(System.lineSeparator());
+        writeToFile(toFileName, generateStatistic(getResult));
+    }
+
+    private String generateStatistic(String [] str) {
+        StringBuilder builder = new StringBuilder();
         int buy = 0;
         int supply = 0;
 
-        for (String s : readFromFile(file)) {
-            String[] data = s.split(",");
+        for (String s : str) {
+            String[] data = s.split(COMMA);
             buy += data[0].equals("buy") ? Integer.parseInt(data[1]) : 0;
             supply += data[0].equals("supply") ? Integer.parseInt(data[1]) : 0;
         }
-        writeToFile(toFileName, buy, supply);
+        return builder.append(SUPPLY).append(COMMA)
+                .append(supply)
+                .append(System.lineSeparator())
+                .append(BUY).append(COMMA)
+                .append(buy)
+                .append(System.lineSeparator())
+                .append(RESULT).append(COMMA)
+                .append(supply - buy).toString();
     }
 
-    public String [] readFromFile(File file) {
+    private String readFromFile(File file) {
         StringBuilder builder = new StringBuilder();
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             String value;
             while ((value = reader.readLine()) != null) {
                 builder.append(value);
-                builder.append(LINE_SEPARATOR);
+                builder.append(System.lineSeparator());
             }
         } catch (IOException e) {
-            throw new RuntimeException("file read error " + e);
+            throw new RuntimeException("Can't read from \\'" + file.getName() + "\\'" + e);
         }
-        return builder.toString().split(LINE_SEPARATOR);
+        return builder.toString();
     }
 
-    public void writeToFile(String toFileName, int buy, int supply) {
-        StringBuilder builder = new StringBuilder();
-
+    private void writeToFile(String toFileName, String statistic) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName, true))) {
-            builder.append(SUPPLY)
-                    .append(supply)
-                    .append(LINE_SEPARATOR)
-                    .append(BUY)
-                    .append(buy)
-                    .append(LINE_SEPARATOR)
-                    .append(RESULT)
-                    .append(supply - buy);
-            writer.write(builder.toString());
+            writer.write(statistic);
         } catch (IOException e) {
             throw new RuntimeException("Can't write to \\'" + toFileName + "\\' file.", e);
         }
