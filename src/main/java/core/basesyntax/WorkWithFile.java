1feb33a251efd453188606543b89f46c1adf5b8d
Artem Kurinniy <artem.kyrinniy@gmail.com>
2022-10-05 15:07:42 +0300

    removed redundant fields

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -9,53 +9,53 @@ import java.io.IOException;
 public class WorkWithFile {
     private static final String SUPPLY = "supply";
     private static final String BUY = "buy";
-    private int fullSupply;
-    private int fullBuy;
-    private String caseSupplyOrBuy;
-    private String amountOfBuyOrSupply;
+    private static final int TOTAL_SUPPLY_INDEX = 0;
+    private static final int TOTAL_BUY_INDEX = 1;
+    private static final int SUPPLY_OR_BUY_INDEX = 0;
+    private static final int VALUE_OF_SUPPLY_OR_BUY_INDEX = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        this.fullBuy = 0;
-        this.fullSupply = 0;
+        int totalSupply = 0;
+        int totalBuy = 0;
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             String line = bufferedReader.readLine();
             while (line != null) {
                 String[] refactored = line.split(",");
-                caseSupplyOrBuy = refactored[0];
-                amountOfBuyOrSupply = refactored[1];
-                totalSupplyAndBuyReport();
+                totalSupply += getTotalSupAndBuy(refactored)[TOTAL_SUPPLY_INDEX];
+                totalBuy += getTotalSupAndBuy(refactored)[TOTAL_BUY_INDEX];
                 line = bufferedReader.readLine();
             }
         } catch (IOException e) {
             throw new RuntimeException("Can`t read this file" + fromFileName, e);
         }
-        writeToFile(toFileName);
+        writeToFile(toFileName, totalSupply, totalBuy);
     }
 
-    private void totalSupplyAndBuyReport() {
-        switch (caseSupplyOrBuy) {
+    public int[] getTotalSupAndBuy(String[] refactored) {
+        int totalSupply = 0;
+        int totalBuy = 0;
+        switch (refactored[SUPPLY_OR_BUY_INDEX]) {
             case SUPPLY:
-                fullSupply += Integer.parseInt(amountOfBuyOrSupply);
+                totalSupply = Integer.parseInt(refactored[VALUE_OF_SUPPLY_OR_BUY_INDEX]);
                 break;
             case BUY:
-                fullBuy += Integer.parseInt(amountOfBuyOrSupply);
+                totalBuy = Integer.parseInt(refactored[VALUE_OF_SUPPLY_OR_BUY_INDEX]);
                 break;
             default:
                 break;
         }
+        return new int[] {totalSupply, totalBuy};
     }
 
-    private String workWithSupplyAndBuyValues() {
-        int result = fullSupply - fullBuy;
-        return SUPPLY + "," + fullSupply
-                + System.lineSeparator() + BUY
-                + "," + fullBuy + System.lineSeparator()
-                + "result," + result;
-    }
-
-    private void writeToFile(String toFileName) {
+    private void writeToFile(String toFileName, int totalSup, int totalBuy) {
+        StringBuilder reportAboutDay = new StringBuilder();
+        int result = totalSup - totalBuy;
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
-            bufferedWriter.write(workWithSupplyAndBuyValues());
+            reportAboutDay
+                    .append(SUPPLY).append(",").append(totalSup).append(System.lineSeparator())
+                    .append(BUY).append(",").append(totalBuy).append(System.lineSeparator())
+                    .append("result,").append(result);
+            bufferedWriter.write(reportAboutDay.toString());
         } catch (IOException e) {
             throw new RuntimeException("Can't write into file" + toFileName, e);
         }
