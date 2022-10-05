34f7f144acf15b21b485c7bc008de1082fc453f3
Anastasiia Romanova <ana.roma.vi@gmail.com>
2022-10-05 18:45:07 +0300

    optimized the method with TWR, added an additional method according to the logic

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -10,78 +10,71 @@ import java.io.IOException;
 public class WorkWithFile {
 
     private static final String BUY = "buy";
+    private static final String COMMA_SEPARATOR = ",";
     private static final String RESULT = "result";
     private static final String SUPPLY = "supply";
     private int buyAmount;
     private String[] statistics;
     private int supplyAmount;
-    private int totalAmount;
 
     public void getStatistic(String fromFileName, String toFileName) {
         readStatistic(fromFileName);
-        addStatistic();
-
-        File report = new File(toFileName);
-        BufferedWriter bufferedWriter = null;
-
-        try {
-            bufferedWriter = new BufferedWriter(new FileWriter(report, true));
-            for (String stat : statistics) {
-                bufferedWriter.write(stat);
-                bufferedWriter.flush();
-            }
-        } catch (IOException e) {
-            throw new RuntimeException("Cannot write data to file", e);
-        } finally {
-            try {
-                bufferedWriter.close();
-            } catch (IOException e) {
-                throw new RuntimeException("Cannot write data to file", e);
-            }
-        }
+        calculateStatistic();
+        createReport(toFileName);
     }
 
     public void readStatistic(String fromFileName) {
         File file = new File(fromFileName);
 
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
-            String inputData = bufferedReader.readLine();
+            String input = bufferedReader.readLine();
 
-            while (inputData != null) {
-                if (inputData.startsWith(SUPPLY)) {
+            while (input != null) {
+                if (input.startsWith(SUPPLY)) {
                     supplyAmount = supplyAmount
-                            + Integer.parseInt(inputData.substring(inputData.indexOf(",") + 1));
+                            + Integer.parseInt(input.substring(input.indexOf(COMMA_SEPARATOR) + 1));
                 } else {
                     buyAmount = buyAmount
-                            + Integer.parseInt(inputData.substring(inputData.indexOf(",") + 1));
+                            + Integer.parseInt(input.substring(input.indexOf(COMMA_SEPARATOR) + 1));
                 }
-                inputData = bufferedReader.readLine();
+                input = bufferedReader.readLine();
             }
-
         } catch (IOException e) {
             throw new RuntimeException("Cannot read the file", e);
         }
-        totalAmount = supplyAmount - buyAmount;
     }
 
-    public void addStatistic() {
+    public void calculateStatistic() {
         statistics = new String[3];
         StringBuilder builder = new StringBuilder();
         statistics[0] = builder.append(SUPPLY)
-                .append(",")
+                .append(COMMA_SEPARATOR)
                 .append(supplyAmount)
                 .append(System.lineSeparator())
                 .toString();
         StringBuilder builder1 = new StringBuilder();
         statistics[1] = builder1.append(BUY)
-                .append(",")
+                .append(COMMA_SEPARATOR)
                 .append(buyAmount)
                 .append(System.lineSeparator())
                 .toString();
         StringBuilder builder2 = new StringBuilder();
         statistics[2] = builder2.append(RESULT)
-                .append(",")
-                .append(totalAmount)
+                .append(COMMA_SEPARATOR)
+                .append(supplyAmount - buyAmount)
                 .toString();
+    }
+
+    public void createReport(String toFileName) {
+        File report = new File(toFileName);
+
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(report, true))) {
+            for (String stat : statistics) {
+                bufferedWriter.write(stat);
+                bufferedWriter.flush();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Cannot write data to file", e);
+        }
     }
 }
