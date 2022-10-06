5193764e2e21ef301a4aae2300b76664c9d580af
Bohdan Pankiv <pankivbogdan96@gmail.com>
2022-10-06 19:57:16 +0300

    created new method writedatatofile and wrote more informative exception

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -17,38 +17,43 @@ public class WorkWithFile {
     private static final String SPACE = " ";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        String string = getString(fromFileName);
-        String[] result = resultString(string).split(SPACE);
+        String string = readFromFile(fromFileName);
+        String result = resultString(string);
+        writeDataToFile(result,toFileName);
+    }
+
+    private void writeDataToFile(String data, String toFileName) {
+        String[] result = resultString(data).split(SPACE);
         File file = new File(toFileName);
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file, true))) {
             for (int i = 0; i < result.length; i++) {
                 bufferedWriter.write(result[i] + System.lineSeparator());
             }
         } catch (IOException e) {
-            throw new RuntimeException("Can't write file", e);
+            throw new RuntimeException("Can't write file" + toFileName, e);
         }
     }
 
-    private String getString(String fromFileName) {
+    private String readFromFile(String fileName) {
         StringBuilder stringBuilder = new StringBuilder();
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
             String value = bufferedReader.readLine();
             while (value != null) {
                 stringBuilder.append(value).append(System.lineSeparator());
                 value = bufferedReader.readLine();
             }
         } catch (FileNotFoundException e) {
-            throw new RuntimeException("Can't read file", e);
+            throw new RuntimeException("Can't read file" + fileName, e);
         } catch (IOException e) {
-            throw new RuntimeException("Can't read file", e);
+            throw new RuntimeException("Can't read file" + fileName, e);
         }
         return stringBuilder.toString();
     }
 
-    private String resultString(String date) {
+    private String resultString(String data) {
         int supply = 0;
         int buy = 0;
-        String[] split = date.split(REGEX);
+        String[] split = data.split(REGEX);
         for (int i = 0; i < split.length; i++) {
             if (split[i].equals(SUPPLY)) {
                 supply += Integer.parseInt(split[i + 1]);
