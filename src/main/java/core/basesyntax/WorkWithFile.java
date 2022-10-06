4c51a29222a1b4fae88fd9d07c50367b22f12d97
Alex Kharchuk <alexander.kharchuk.vwcenter@gmail.com>
2022-10-06 12:57:18 +0300

    splitted solution to the methods

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -9,6 +9,20 @@ import java.io.IOException;
 public class WorkWithFile {
 
     public void getStatistic(String fromFileName, String toFileName) {
+        WorkWithFile file = new WorkWithFile();
+        String[] allLines = file.readDataFromFile(fromFileName).split(" ");
+
+        String supply = new String();
+        String buy = new String();
+        String result = new String();
+        supply = "supply," + calculateSupply(allLines);
+        buy = "buy," + calculateBuy(allLines);
+        result = "result," + calculateResult(calculateSupply(allLines), calculateBuy(allLines));
+
+        file.writeDataToFile(toFileName, supply, buy, result);
+    }
+
+    public String readDataFromFile(String fromFileName) {
         StringBuilder array = new StringBuilder();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             String line = bufferedReader.readLine();
@@ -17,30 +31,12 @@ public class WorkWithFile {
                 line = bufferedReader.readLine();
             } while (line != null);
         } catch (IOException e) {
-            throw new RuntimeException("Can't read the file", e);
+            throw new RuntimeException("Can't read the file" + fromFileName, e);
         }
+        return array.toString();
+    }
 
-        String[] allLines = array.toString().split(" ");
-
-        int supplySum = 0;
-        int buySum = 0;
-        int resultSum = 0;
-
-        for (int i = 0; i < allLines.length; i++) {
-            String[] singleLine = allLines[i].split(",");
-            if (singleLine[0].equals("supply")) {
-                supplySum += Integer.valueOf(singleLine[1]);
-            }
-            if (singleLine[0].equals("buy")) {
-                buySum += Integer.valueOf(singleLine[1]);
-            }
-        }
-
-        resultSum = supplySum - buySum;
-        String supply = "supply," + supplySum;
-        String buy = "buy," + buySum;
-        String result = "result," + resultSum;
-
+    public void writeDataToFile(String toFileName, String supply, String buy, String result) {
         try (BufferedWriter bufferedWr = new BufferedWriter(new FileWriter(toFileName, true))) {
             bufferedWr.write(supply);
             bufferedWr.newLine();
@@ -48,8 +44,34 @@ public class WorkWithFile {
             bufferedWr.newLine();
             bufferedWr.write(result);
         } catch (IOException e) {
-            throw new RuntimeException("Can't write to the file", e);
+            throw new RuntimeException("Can't write to the file" + toFileName, e);
         }
+    }
 
+    public int calculateSupply(String[] allLines) {
+        int supplySum = 0;
+
+        for (int i = 0; i < allLines.length; i++) {
+            String[] singleLine = allLines[i].split(",");
+            if (singleLine[0].equals("supply")) {
+                supplySum += Integer.valueOf(singleLine[1]);
+            }
+        }
+        return supplySum;
+    }
+
+    public int calculateBuy(String[] allLines) {
+        int buySum = 0;
+        for (int i = 0; i < allLines.length; i++) {
+            String[] singleLine = allLines[i].split(",");
+            if (singleLine[0].equals("buy")) {
+                buySum += Integer.valueOf(singleLine[1]);
+            }
+        }
+        return buySum;
+    }
+
+    public int calculateResult(int supply, int buy) {
+        return supply - buy;
     }
 }
