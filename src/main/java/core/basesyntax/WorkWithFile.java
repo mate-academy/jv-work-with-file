e999f8d8f24939ddb4723cbace2a282c34ae3bf1
Oksana Rudenko <0ksaha_rudenko@ukr.net>
2022-10-06 17:48:02 +0300

    jv-work-with-file solution

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,8 +1,67 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WorkWithFile {
+    private static final int OPERATION_NAME_INDEX = 0;
+    private static final int AMOUNT_INDEX = 1;
+    private static final String SUPPLY_STRING = "supply";
+    private static final String BUY_STRING = "buy";
+    private static final String RESULT_STRING = "result";
+    private static final String COMA_STRING = ",";
+
+    private String[] readInfo(String fileName) {
+        File outputFile = new File(fileName);
+        StringBuilder builder = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(outputFile))) {
+            String value = bufferedReader.readLine();
+            while (value != null) {
+                builder.append(value).append(System.lineSeparator());
+                value = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
+        return builder.toString().split(System.lineSeparator());
+    }
+
+    private String getWorkingData(String fileName) {
+        WorkWithFile workWithFile = new WorkWithFile();
+        String[] dataFromFile = workWithFile.readInfo(fileName);
+        String[] dataInLineOfFile = new String[] {};
+        int countSupply = 0;
+        int countBuy = 0;
+        for (String data : dataFromFile) {
+            dataInLineOfFile = data.split(COMA_STRING);
+            if (dataInLineOfFile[OPERATION_NAME_INDEX].equals(SUPPLY_STRING)) {
+                countSupply += Integer.parseInt(dataInLineOfFile[AMOUNT_INDEX]);
+            }
+            if (dataInLineOfFile[OPERATION_NAME_INDEX].equals(BUY_STRING)) {
+                countBuy += Integer.parseInt(dataInLineOfFile[AMOUNT_INDEX]);
+            }
+        }
+        StringBuilder builder = new StringBuilder();
+        builder.append(SUPPLY_STRING).append(COMA_STRING)
+                .append(Integer.toString(countSupply)).append(System.lineSeparator())
+                .append(BUY_STRING).append(COMA_STRING)
+                .append(Integer.toString(countBuy)).append(System.lineSeparator())
+                .append(RESULT_STRING).append(COMA_STRING).append(countSupply - countBuy);
+        return builder.toString();
+    }
 
     public void getStatistic(String fromFileName, String toFileName) {
-
+        WorkWithFile workWithFile = new WorkWithFile();
+        String dataForWrite = workWithFile.getWorkingData(fromFileName);
+        File inputFile = new File(toFileName);
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(inputFile))) {
+            bufferedWriter.write(dataForWrite);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write data to file", e);
+        }
     }
 }
