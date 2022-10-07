fbb241aaafd44bde70322716c6787d2935759c3d
Roman Usyk <roman.usik.93@ukr.net>
2022-10-07 12:35:13 +0300

    the mistakes were corrected

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -10,20 +10,20 @@ import java.io.IOException;
 public class WorkWithFile {
     private static final int SIZE_OF_RESULT_ARRAY = 3;
     private static final String[] RESULT = new String[SIZE_OF_RESULT_ARRAY];
-    private int countS = 0;
-    private int countB = 0;
+    private static final String NEW_LINE = System.lineSeparator();
+    private static final String SUPPLY = "supply,";
+    private static final String BUY = "buy,";
+    private static final String RESULT_WORD = "result,";
     private int sumS = 0;
     private int sumB = 0;
-    private int difference;
 
     private String readFromFile(String fromFileName) {
         File file = new File(fromFileName);
         StringBuilder stringBuilder = new StringBuilder();
-        try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
             String value = bufferedReader.readLine();
             while (value != null) {
-                stringBuilder.append(value).append("\n");
+                stringBuilder.append(value).append(NEW_LINE);
                 value = bufferedReader.readLine();
             }
         } catch (IOException e) {
@@ -32,57 +32,29 @@ public class WorkWithFile {
         return stringBuilder.toString();
     }
 
-    private String[] writeSupplyAndBuyToArray(String[] stringBuilderToStringArray) {
-        for (int i = 0; i < stringBuilderToStringArray.length; i++) {
-            if (stringBuilderToStringArray[i].charAt(0) == 's') {
-                countS++;
-                if (countS == 1) {
-                    int index = stringBuilderToStringArray[i].indexOf(",");
-                    RESULT[0] = stringBuilderToStringArray[i].substring(0, index + 1);
-                }
+    private void fillAllArray(String[] sumBuyAndSupply) {
+        for (int i = 0; i < sumBuyAndSupply.length; i++) {
+            String[] split = sumBuyAndSupply[i].split(",");
+            if (split[0].charAt(0) == 's') {
+                sumS = sumS + Integer.parseInt(split[1]);
             }
-            if (stringBuilderToStringArray[i].charAt(0) == 'b') {
-                countB++;
-                if (countB == 1) {
-                    int index = stringBuilderToStringArray[i].indexOf(",");
-                    RESULT[1] = stringBuilderToStringArray[i].substring(0, index + 1);
-                }
-            }
-        }
-        return RESULT;
-    }
-
-    private void fillAllArray(String[] writeSupplyAndBuyToArray) {
-        for (int i = 0; i < writeSupplyAndBuyToArray.length; i++) {
-            if (writeSupplyAndBuyToArray[i].charAt(0) == 's') {
-                int indexOf = writeSupplyAndBuyToArray[i].indexOf(",");
-                writeSupplyAndBuyToArray[i] =
-                        writeSupplyAndBuyToArray[i].substring(indexOf + 1);
-                sumS = sumS + Integer.parseInt(writeSupplyAndBuyToArray[i]);
-
-            }
-            if (writeSupplyAndBuyToArray[i].charAt(0) == 'b') {
-                int indexOf = writeSupplyAndBuyToArray[i].indexOf(",");
-                writeSupplyAndBuyToArray[i] =
-                        writeSupplyAndBuyToArray[i].substring(indexOf + 1);
-                sumB = sumB + Integer.parseInt(writeSupplyAndBuyToArray[i]);
+            if (split[0].charAt(0) == 'b') {
+                sumB = sumB + Integer.parseInt(split[1]);
             }
         }
     }
 
     private void writeToFile(String toFileName) {
-        difference = sumS - sumB;
-        RESULT[0] = RESULT[0] + sumS;
-        RESULT[1] = RESULT[1] + sumB;
-        RESULT[2] = "result," + difference;
+        int difference = sumS - sumB;
+        RESULT[0] = SUPPLY + sumS;
+        RESULT[1] = BUY + sumB;
+        RESULT[2] = RESULT_WORD + difference;
         File toFile = new File(toFileName);
         for (String res : RESULT) {
-            try {
-                BufferedWriter bufferedWriter =
-                        new BufferedWriter(new FileWriter(toFile, true));
+            try (BufferedWriter bufferedWriter =
+                         new BufferedWriter(new FileWriter(toFile, true))) {
                 bufferedWriter.write(res);
-                bufferedWriter.write(System.lineSeparator());
-                bufferedWriter.close();
+                bufferedWriter.write(NEW_LINE);
             } catch (IOException e) {
                 throw new RuntimeException("Can`t write data file", e);
             }
@@ -91,8 +63,7 @@ public class WorkWithFile {
 
     public void getStatistic(String fromFileName, String toFileName) {
         String stringBuilderToString = readFromFile(fromFileName);
-        String[] stringBuilderToStringArray = stringBuilderToString.split("\n");
-        String[] writeSupplyAndBuyToArray = writeSupplyAndBuyToArray(stringBuilderToStringArray);
+        String[] stringBuilderToStringArray = stringBuilderToString.split(System.lineSeparator());
         fillAllArray(stringBuilderToStringArray);
         writeToFile(toFileName);
     }
