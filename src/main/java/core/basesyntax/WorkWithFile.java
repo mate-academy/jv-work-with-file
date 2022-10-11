d933ef0ec33dee385499452a4ee0a17c3b119ec8
Dmitri Kolos <skil@bigmir.net>
2022-10-11 23:21:25 +0300

    added changes after review

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,47 +7,47 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    private static final String BUY_OPERATION = "buy";
+    private static final String SUPPLY_OPERATION = "supply";
     private int amountOfBuy = 0;
     private int amountOfSupply = 0;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        writeToFile(toFileName, createReport(readFromFile(fromFileName)));
+        String data = readFromFile(fromFileName);
+        String report = createReport(data);
+        writeToFile(toFileName, report);
     }
 
     private String readFromFile(String fileName) {
         StringBuilder builder = new StringBuilder();
         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
-            String data = reader.readLine();
-            while (data != null) {
-                builder.append(data).append(System.lineSeparator());
-                data = reader.readLine();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                builder.append(line).append(System.lineSeparator());
             }
         } catch (IOException e) {
-            throw new RuntimeException("Can't write to the file", e);
+            throw new RuntimeException("Can't write to the file: " + fileName, e);
 
         }
         return builder.toString();
     }
 
-    private String createReport(String dataFromFile) {
-
-        String[] dataArray = dataFromFile.split(System.lineSeparator());
-        String buyOperation = "buy";
-        String supplyOperation = "supply";
+    private String createReport(String data) {
+        String[] dataArray = data.split(System.lineSeparator());
         for (String datum : dataArray) {
             String[] splittedData = datum.split(",");
             int indexOfOperation = 0;
             int indexOfAmount = 1;
-            if (splittedData[indexOfOperation].equals(buyOperation)) {
+            if (splittedData[indexOfOperation].equals(BUY_OPERATION)) {
                 amountOfBuy += Integer.parseInt(splittedData[indexOfAmount]);
             }
-            if (splittedData[indexOfOperation].equals(supplyOperation)) {
+            if (splittedData[indexOfOperation].equals(SUPPLY_OPERATION)) {
                 amountOfSupply += Integer.parseInt(splittedData[indexOfAmount]);
             }
         }
-        return supplyOperation + ","
+        return SUPPLY_OPERATION + ","
                     + amountOfSupply + System.lineSeparator()
-                    + buyOperation + ","
+                    + BUY_OPERATION + ","
                     + amountOfBuy + System.lineSeparator()
                     + "result" + ","
                     + (amountOfSupply - amountOfBuy);
@@ -60,7 +60,7 @@ public class WorkWithFile {
             bufferedWriter.write(report);
             bufferedWriter.close();
         } catch (IOException e) {
-            throw new RuntimeException("Can't read the file", e);
+            throw new RuntimeException("Can't read the file: " + fileName, e);
         }
     }
 }
