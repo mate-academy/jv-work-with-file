b0fee3ea2a7d570d95eb1f1b26df289ff6b5e2f4
Victoria Kharlanchuk <kaplabetty@gmail.com>
2022-10-13 00:01:43 +0200

    creat new metod and another corection

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,32 +7,21 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    
     private static final String SUPPLY = "supply";
     private static final String BUY = "buy";
-    private static final int FIRST_ELEMENT = 0;
-    private static final int SECOND_ELEMENT = 1;
-    private int summaSuply = 0;
-    private int summaBuy = 0;
+    private static final int OPERATION_INDEX = 0;
+    private static final int AMOUT_INDEX = 1;
     
     public void getStatistic(String fromFileName, String toFileName) {
         
-        String[] readToArray = readFile(fromFileName).split("\n");
-        
-        for (String s : readToArray) {
-            String[] dropLine = s.split(" ");
-            if ((SUPPLY.equals(dropLine[FIRST_ELEMENT]))) {
-                summaSuply = summaSuply + Integer.parseInt(dropLine[SECOND_ELEMENT]);
-            } else {
-                summaBuy = summaBuy + Integer.parseInt(dropLine[SECOND_ELEMENT]);
-            }
-        }
-        writeFile(getString(), toFileName);
+        String[] dataFromFile = readFile(fromFileName).split("\n");
+        String report = createReport(dataFromFile);
+        writeFile(report, toFileName);
     }
     
-    private void writeFile(String goods, String toFileName) {
+    private void writeFile(String report, String toFileName) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
-            bufferedWriter.write(goods);
+            bufferedWriter.write(report);
         } catch (IOException e) {
             throw new RuntimeException("Can`t write data to file ", e);
         }
@@ -44,10 +33,7 @@ public class WorkWithFile {
         
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             while ((readLineFIle = bufferedReader.readLine()) != null) {
-                String[] string = readLineFIle.split(",");
-                stringBuilder.append(string[FIRST_ELEMENT]).append(" ")
-                        .append(string[SECOND_ELEMENT])
-                        .append("\n");
+                stringBuilder.append(readLineFIle).append("\n");
             }
         } catch (IOException e) {
             throw new RuntimeException("Can`t read a file", e);
@@ -55,7 +41,18 @@ public class WorkWithFile {
         return stringBuilder.toString();
     }
     
-    private String getString() {
+    private String createReport(String[] dataFromFile) {
+        int summaSuply = 0;
+        int summaBuy = 0;
+        
+        for (String s : dataFromFile) {
+            String[] dropLine = s.split(",");
+            if ((SUPPLY.equals(dropLine[OPERATION_INDEX]))) {
+                summaSuply = summaSuply + Integer.parseInt(dropLine[AMOUT_INDEX]);
+            } else {
+                summaBuy = summaBuy + Integer.parseInt(dropLine[AMOUT_INDEX]);
+            }
+        }
         return new StringBuilder().append(SUPPLY).append(",").append(summaSuply)
                 .append("\n").append(BUY).append(",").append(summaBuy).append("\n")
                 .append("result").append(",").append(summaSuply - summaBuy).toString();
