3cf6799e3f3ed7c6d19291333d97efa100493238
alexeyukrainets <qaaq.com.ua@gmail.com>
2022-10-14 14:30:11 +0200

    Final version

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,79 +1,9 @@
 package core.basesyntax;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
-        File file = new File(fromFileName);
-
-        try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
-            StringBuilder stringBuilder = new StringBuilder();
-            String value = bufferedReader.readLine();
-
-            while (value != null) {
-                stringBuilder.append(value).append(" ");
-                value = bufferedReader.readLine();
-            }
-
-            String supplyAndBuyToString = stringBuilder.toString();
-
-            String[] supplyAndBuy = supplyAndBuyToString.split(" ");
-
-            StringBuilder supplyBuilder = new StringBuilder();
-            StringBuilder buyBuilder = new StringBuilder();
-
-            for (String s : supplyAndBuy) {
-                if (s.charAt(0) == 's') {
-                    supplyBuilder.append(s).append(" ");
-                } else {
-                    buyBuilder.append(s).append(" ");
-                }
-            }
-            String stringSupply = supplyBuilder.toString();
-            String stringBuy = buyBuilder.toString();
-            String[] arraySupply = stringSupply.split("\\D+");
-            String[] arrayBuy = stringBuy.split("\\D+");
-            int[] supplyNumbers = new int[arraySupply.length - 1];
-            int[] buyNumbers = new int[arrayBuy.length - 1];
-            for (int i = 1; i < arraySupply.length; i++) {
-                int local = Integer.parseInt(arraySupply[i]);
-                supplyNumbers[i - 1] += local;
-            }
-            for (int i = 1; i < arrayBuy.length; i++) {
-                int local = Integer.parseInt(arrayBuy[i]);
-                buyNumbers[i - 1] += local;
-            }
-
-            int sumSupply = 0;
-            int sumBuy = 0;
-            int result;
-
-            for (int supplyNumber : supplyNumbers) {
-                sumSupply += supplyNumber;
-            }
-            for (int buyNumber : buyNumbers) {
-                sumBuy += buyNumber;
-            }
-
-            result = sumSupply - sumBuy;
-
-            String completedTextFromFileWriter = "supply," + sumSupply + System.lineSeparator()
-                    + "buy," + sumBuy + System.lineSeparator()
-                    + "result," + result;
-
-            File finalFile = new File(toFileName);
-
-            BufferedWriter writerToFinalFile = new BufferedWriter(new FileWriter(finalFile, true));
-            writerToFinalFile.write(completedTextFromFileWriter);
-            writerToFinalFile.close();
-        } catch (IOException e) {
-            throw new RuntimeException("Can't read file or can't write data to file", e);
-        }
+        MethodsForFile methodsForFile = new MethodsForFile();
+        String completedText = methodsForFile.getReadFile(fromFileName);
+        methodsForFile.getWriteFile(toFileName, completedText);
     }
 }
