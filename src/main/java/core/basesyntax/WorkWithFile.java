66eaa02e854f26d7df117723acc1f0951af395a2
Sachamunn <denhackgaming@gmail.com>
2022-10-16 17:54:31 +0100

    Fix

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,11 +8,15 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class WorkWithFile {
+    private static final String LINE_SEPARATOR = ",";
+    private static final String BUY_TYPE = "buy";
+    private static final String SUPPLY_TYPE = "supply";
+
     public void getStatistic(String fromFileName, String toFileName) {
 
         createNewFile(toFileName);
         int[] readFromFileResult = readFromFile(fromFileName);
-        writeContent(readFromFileResult, toFileName);
+        write(createReport(readFromFileResult), toFileName);
 
     }
 
@@ -22,40 +26,35 @@ public class WorkWithFile {
             int supplySum = 0;
 
             while (scanner.hasNextLine()) {
-                String[] lineContent = scanner.nextLine().split(",");
+
+                String[] lineContent = scanner.nextLine().split(LINE_SEPARATOR);
 
                 switch (lineContent[0]) {
-                    case "buy":
+                    case BUY_TYPE:
                         buySum += Integer.parseInt(lineContent[1]);
                         break;
 
-                    case "supply":
+                    case SUPPLY_TYPE:
                         supplySum += Integer.parseInt(lineContent[1]);
                         break;
 
                     default:
-                        throw new RuntimeException("Incorrect element");
+                        break;
 
                 }
 
             }
-            return new int[] {buySum, supplySum};
+            return new int[] {supplySum, buySum};
 
         } catch (Exception e) {
             throw new RuntimeException("Can't read file", e);
         }
     }
 
-    private void writeContent(int[] readFromFileMethod, String toFileName) {
-        int buySum = readFromFileMethod[0];
-        int supplySum = readFromFileMethod[1];
+    private void write(String report, String toFileName) {
 
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName));) {
-            writer.write("supply," + supplySum);
-            writer.newLine();
-            writer.write("buy," + buySum);
-            writer.newLine();
-            writer.write("result," + (supplySum - buySum));
+            writer.write(report);
         } catch (IOException e) {
             throw new RuntimeException("Can't write content to new file", e);
         }
@@ -68,5 +67,15 @@ public class WorkWithFile {
         } catch (IOException e) {
             throw new RuntimeException("Can't create new file", e);
         }
+    }
+
+    private String createReport(int[] fileData) {
+        int supplyValue = fileData[0];
+        int buyValue = fileData[1];
+
+        String report = "supply," + supplyValue + System.lineSeparator()
+                + "buy," + buyValue + System.lineSeparator()
+                + "result," + (supplyValue - buyValue);
+        return report;
     }
 }
