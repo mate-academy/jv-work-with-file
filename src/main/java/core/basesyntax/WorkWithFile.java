fc6d7508096cf3a4ba51f17eb22a270998ab4d52
Mykhailo Levko <mykhailo.levko@gmail.com>
2022-10-15 23:25:22 -0400

    Added constants

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,19 +8,26 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+    private static final String RESULT = "result";
+    private static final String WHITE_SPACE = " ";
+    private static final String COMMA = ",";
+    private static final String LINE_SEPARATOR = System.lineSeparator();
+    private static final int PRICE_INDEX = 1;
+    private static final int OPERATION_INDEX = 0;
+
     public void getStatistic(String fromFileName, String toFileName) {
         String information = readFromFile(fromFileName);
         String report = createReport(information);
         writeToFile(toFileName, report);
     }
 
-    public void writeToFile(String toFileName, String report) {
-        try (FileWriter fileWriter = new FileWriter(toFileName)) {
-            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+    private void writeToFile(String toFileName, String report) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
             bufferedWriter.write(report);
-            bufferedWriter.flush();
         } catch (IOException e) {
-            throw new RuntimeException("Can't write to file", e);
+            throw new RuntimeException("Can't write to file " + toFileName, e);
         }
     }
 
@@ -30,11 +37,11 @@ public class WorkWithFile {
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
             String value = bufferedReader.readLine();
             while (value != null) {
-                info.append(value).append(" ");
+                info.append(value).append(WHITE_SPACE);
                 value = bufferedReader.readLine();
             }
         } catch (IOException e) {
-            throw new RuntimeException("Can't read file", e);
+            throw new RuntimeException("Can't read file " + fromFileName, e);
         }
         return info.toString();
     }
@@ -43,17 +50,17 @@ public class WorkWithFile {
         int supply = 0;
         int buy = 0;
         int result;
-        String[] array = data.split(" ");
+        String[] array = data.split(WHITE_SPACE);
         for (String operation : array) {
-            if (operation.split(",")[0].equals("supply")) {
-                supply += Integer.parseInt(operation.split(",")[1]);
-            } else if (operation.split(",")[0].equals("buy")) {
-                buy += Integer.parseInt(operation.split(",")[1]);
+            if (operation.split(COMMA)[OPERATION_INDEX].equals(SUPPLY)) {
+                supply += Integer.parseInt(operation.split(COMMA)[PRICE_INDEX]);
+            } else if (operation.split(COMMA)[OPERATION_INDEX].equals(BUY)) {
+                buy += Integer.parseInt(operation.split(COMMA)[PRICE_INDEX]);
             }
         }
         result = supply - buy;
-        return "supply," + supply + System.lineSeparator()
-                + "buy," + buy + System.lineSeparator()
-                + "result," + result;
+        return (SUPPLY + COMMA + supply + LINE_SEPARATOR
+                + BUY + COMMA + buy + LINE_SEPARATOR
+                + RESULT + COMMA + result);
     }
 }
