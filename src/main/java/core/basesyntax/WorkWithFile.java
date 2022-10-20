3d3d5a41974d7ae0cc535bba234d4dcf0f216df2
Dmytro Busyhin <zza4etik@gmail.com>
2022-10-20 22:13:28 +0300

    fixing according to the suggested changes

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,7 +2,6 @@ package core.basesyntax;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -10,58 +9,63 @@ import java.io.IOException;
 public class WorkWithFile {
     private static final int DATA_TYPE_INDEX = 0;
     private static final int DATA_VALUE_INDEX = 1;
-    private static final String STRING_BUY = "buy";
-    private static final String STRING_SUPPLY = "supply";
-    private static final String SEPARATOR_VALUE = ",";
-    private StringBuilder fileDataBuilder;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        int buyCounter = 0;
-        int supplyCounter = 0;
-        fileDataBuilder = new StringBuilder();
-        readFromFile(fromFileName);
-        String[] fileData = fileDataBuilder.toString().split(System.lineSeparator());
-        for (String datum : fileData) {
-            String[] arrayDatum = datum.split(SEPARATOR_VALUE);
-            if (arrayDatum[DATA_TYPE_INDEX].contains(STRING_BUY)) {
-                buyCounter += Integer.parseInt(arrayDatum[DATA_VALUE_INDEX]);
-            } else if (arrayDatum[DATA_TYPE_INDEX].contains(STRING_SUPPLY)) {
-                supplyCounter += Integer.parseInt(arrayDatum[DATA_VALUE_INDEX]);
-            } else {
-                throw new RuntimeException("Wrong data in file");
-            }
-        }
-        writeToFile(toFileName, buyCounter, supplyCounter);
+        String fileData = readFromFile(fromFileName);
+        String report = reportCreation(fileData);
+        writeToFile(toFileName, report);
     }
 
-    private void readFromFile(String fileName) {
+    private String readFromFile(String fileName) {
+        StringBuilder stringBuilder;
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            stringBuilder = new StringBuilder();
             while (bufferedReader.ready()) {
-                fileDataBuilder.append(bufferedReader.readLine()).append(System.lineSeparator());
+                stringBuilder.append(bufferedReader.readLine()).append(System.lineSeparator());
             }
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException("File not found", e);
         } catch (IOException e) {
             throw new RuntimeException("Can't read file", e);
         }
+        return stringBuilder.toString();
     }
 
-    private void writeToFile(String fileName, int buyCounter, int supplyCounter) {
+    private String reportCreation(String fileData) {
+        int buyCounter = 0;
+        int supplyCounter = 0;
+        final String stringBuy = "buy";
+        final String separator = ",";
+        String[] fileDataArray = fileData.split(System.lineSeparator());
+        for (String datum : fileDataArray) {
+            String[] datumArray = datum.split(separator);
+            if (datumArray[DATA_TYPE_INDEX].contains(stringBuy)) {
+                buyCounter += Integer.parseInt(datumArray[DATA_VALUE_INDEX]);
+            } else {
+                supplyCounter += Integer.parseInt(datumArray[DATA_VALUE_INDEX]);
+            }
+        }
+        return dataBuilding(buyCounter, supplyCounter);
+    }
+
+    private String dataBuilding(int buyCounter, int supplyCounter) {
+        StringBuilder fileDataBuilder = new StringBuilder();
+        final String stringBuy = "buy";
+        final String stringSupply = "supply";
+        final String stringResult = "result";
+        final String separator = ",";
+        return fileDataBuilder
+                .append(stringSupply).append(separator).append(supplyCounter)
+                .append(System.lineSeparator())
+                .append(stringBuy).append(separator).append(buyCounter)
+                .append(System.lineSeparator())
+                .append(stringResult).append(separator).append((supplyCounter - buyCounter))
+                .toString();
+    }
+
+    private void writeToFile(String fileName, String report) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName))) {
-            bufferedWriter.write(reportCreator(buyCounter, supplyCounter));
+            bufferedWriter.write(report);
         } catch (IOException e) {
             throw new RuntimeException("Can't write to file", e);
         }
-    }
-
-    private String reportCreator(int buyCounter, int supplyCounter) {
-        fileDataBuilder = new StringBuilder();
-        return fileDataBuilder
-                .append(STRING_SUPPLY).append(SEPARATOR_VALUE).append(supplyCounter)
-                .append(System.lineSeparator())
-                .append(STRING_BUY).append(SEPARATOR_VALUE).append(buyCounter)
-                .append(System.lineSeparator())
-                .append("result").append(SEPARATOR_VALUE)
-                .append((supplyCounter - buyCounter)).toString();
     }
 }
