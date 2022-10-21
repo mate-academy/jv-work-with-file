cbc5e3c60613a643b3ccd0e210ab596f8ee662f5
DOM <kasinets.ulia@gmail.com>
2022-10-21 17:03:10 +0300

    did everything she could to understand

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,7 +2,6 @@ package core.basesyntax;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -12,17 +11,19 @@ public class WorkWithFile {
     private static final String BUY = "buy";
     private static final String RESULT = "result";
     private static final int AMOUNT_INDEX = 1;
+    private String data;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        File file = new File(toFileName);
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file, true));) {
-            bufferedWriter.write(getReport(fromFileName));
+        String data = reedFromFile(fromFileName);
+        String report = getReport(data);
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName));) {
+            bufferedWriter.write(report);
         } catch (IOException e) {
             throw new RuntimeException("Can't write to file " + toFileName, e);
         }
     }
 
-    private StringBuilder redFromFile(String fromFileName) {
+    private String reedFromFile(String fromFileName) {
         StringBuilder stringBuilder;
         BufferedReader bufferedReader = null;
         try {
@@ -36,22 +37,23 @@ public class WorkWithFile {
         } catch (IOException e) {
             throw new RuntimeException("Can't read file " + fromFileName, e);
         }
-        return stringBuilder;
+        data = stringBuilder.toString();
+        return data;
     }
 
-    private String getReport(String fromFileName) {
+    private String getReport(String data) {
         int supply = 0;
         int buy = 0;
         int result;
-        String[] lines = redFromFile(fromFileName).toString().split(" ");
-        String[][] data = new String[lines.length][2];
+        String[] lines = data.split(" ");
+        String[][] strings = new String[lines.length][2];
         for (int i = 0; i < lines.length; i++) {
-            data[i] = lines[i].split(",");
-            if (data[i][0].equals(SUPPLY)) {
-                supply += Integer.parseInt(data[i][AMOUNT_INDEX]);
+            strings[i] = lines[i].split(",");
+            if (strings[i][0].equals(SUPPLY)) {
+                supply += Integer.parseInt(strings[i][AMOUNT_INDEX]);
             }
-            if (data[i][0].equals(BUY)) {
-                buy += Integer.parseInt(data[i][AMOUNT_INDEX]);
+            if (strings[i][0].equals(BUY)) {
+                buy += Integer.parseInt(strings[i][AMOUNT_INDEX]);
             }
         }
         result = supply - buy;
