1a58e5a1e26c5ff285b5a78f58f0aff76e859b5f
Sasha Vradiy <alexvradii@gmail.com>
2022-10-21 16:20:42 +0300

    1

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,8 +1,27 @@
 package core.basesyntax;
 
+import java.io.*;
+
 public class WorkWithFile {
 
     public void getStatistic(String fromFileName, String toFileName) {
+        File file = new File(fromFileName);
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            String value = bufferedReader.readLine();
+            while (value != null) {
+                value = bufferedReader.readLine();
+                String[] listFromFile = value.split(",");
+                for (int i = 0; i < listFromFile.length; i++) {
 
+                }
+            }
+
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("Can't find file", e);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
     }
 }
