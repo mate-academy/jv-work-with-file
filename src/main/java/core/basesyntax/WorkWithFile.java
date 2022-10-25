71745f9a8fb3fccc50e4de0dd0001913822a97a7
Vyacheslav Sukhov <makasin_12@hotmail.com>
2022-10-25 21:18:11 +0200

    refactored 25/10

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -10,6 +10,12 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public class WorkWithFile {
+    private static final String NEW_LINE = System.lineSeparator();
+    private static final int AMOUNT_INDEX = 1;
+    private static final int OPERATION_TYPE_INDEX = 0;
+    private static final StringBuilder STRING_BUILDER = new StringBuilder();
+    private static final Map<String, Integer> MAP = new TreeMap<>(Comparator.reverseOrder());
+
     public void getStatistic(String fromFileName, String toFileName) {
         List<String> data = readFromFile(fromFileName);
         String report = createReport(data);
@@ -35,30 +41,26 @@ public class WorkWithFile {
     }
 
     public String createReport(List<String> data) {
-        Map<String, Integer> map = new TreeMap<>(Comparator.reverseOrder());
-        StringBuilder builder = new StringBuilder();
         for (String row : data) {
             String[] split = row.split(",");
-            final int amountIndex = 1;
-            final int operationTypeIndex = 0;
-            int amount = Integer.parseInt(split[amountIndex]);
-            String operationType = split[operationTypeIndex];
-            if (map.containsKey(operationType)) {
-                map.replace(operationType, map.get(operationType) + amount);
+            int amount = Integer.parseInt(split[AMOUNT_INDEX]);
+            String operationType = split[OPERATION_TYPE_INDEX];
+            if (MAP.containsKey(operationType)) {
+                MAP.replace(operationType, MAP.get(operationType) + amount);
             }
-            map.putIfAbsent(operationType, amount);
+            MAP.putIfAbsent(operationType, amount);
         }
-        int supplyAmount = map.get("supply");
-        int buyAmount = map.get("buy");
+        int supplyAmount = MAP.get("supply");
+        int buyAmount = MAP.get("buy");
         int resultAmount = supplyAmount - buyAmount;
-        for (String operation : map.keySet()) {
-            builder.append(operation)
+        for (String operation : MAP.keySet()) {
+            STRING_BUILDER.append(operation)
                     .append(",")
-                    .append(map.get(operation))
-                    .append(System.lineSeparator());
+                    .append(MAP.get(operation))
+                    .append(NEW_LINE);
         }
-        builder.append("result,").append(resultAmount);
-        return builder.toString();
+        STRING_BUILDER.append("result,").append(resultAmount);
+        return STRING_BUILDER.toString();
     }
 
     @Override
