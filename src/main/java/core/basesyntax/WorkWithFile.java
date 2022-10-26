718c0ff77dfbcceff5f6c0a6d59152c5ba5fd9b1
Oleksandr Fedchik <fed4iksasha@gmail.com>
2022-10-26 21:49:15 +0300

    fixed code: -renamed constants; -separated method; -and deleted all empty extra line)))

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -6,46 +6,57 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 
 public class WorkWithFile {
     private static final String SPECIFIED_CHARACTER = ",";
     private static final String SUPPLY_OPERATION_NAME = "supply";
     private static final String BUY_OPERATION_NAME = "buy";
     private static final String REPORT_MESSAGE_FORMAT = "supply,%d%sbuy,%d%sresult,%d";
-    private static final int OPERATION_NAME = 0;
-    private static final int OPERATION_AMOUNT = 1;
+    private static final int OPERATION_NAME_INDEX = 0;
+    private static final int OPERATION_AMOUNT_INDEX = 1;
     private static final int SUPPLY_TOTAL_AMOUNT_INDEX = 0;
     private static final int BUY_TOTAL_AMOUNT_INDEX = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
         File fileFrom = new File(fromFileName);
-        int[] reportArray = processFile(fileFrom);
+        List<String> dataFromFile = readFromFile(fileFrom);
+        int[] reportArray = processDataFile(dataFromFile);
         String report = createReport(
                 reportArray[SUPPLY_TOTAL_AMOUNT_INDEX], reportArray[BUY_TOTAL_AMOUNT_INDEX]);
         writeToFile(toFileName, report);
     }
 
-    private int[] processFile(File fileFrom) {
-        int totalSupply = 0;
-        int totalBuy = 0;
+    private List<String> readFromFile(File fileFrom) {
+        List<String> dataFromFile = new LinkedList<String>();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileFrom))) {
             String dataLine = bufferedReader.readLine();
-            String[] separatorLine;
             while (dataLine != null) {
-                separatorLine = dataLine.split(SPECIFIED_CHARACTER);
-                totalSupply = handleOperation(totalSupply, separatorLine, SUPPLY_OPERATION_NAME);
-                totalBuy = handleOperation(totalBuy, separatorLine, BUY_OPERATION_NAME);
+                dataFromFile.add(dataLine);
                 dataLine = bufferedReader.readLine();
             }
         } catch (IOException e) {
             throw new RuntimeException("Can't read data from file: " + fileFrom, e);
         }
+        return dataFromFile;
+    }
+
+    private int[] processDataFile(List<String> dataFromFile) {
+        int totalSupply = 0;
+        int totalBuy = 0;
+        String[] separatorLine;
+        for (String dataLine : dataFromFile) {
+            separatorLine = dataLine.split(SPECIFIED_CHARACTER);
+            totalSupply = handleOperation(totalSupply, separatorLine, SUPPLY_OPERATION_NAME);
+            totalBuy = handleOperation(totalBuy, separatorLine, BUY_OPERATION_NAME);
+        }
         return new int[]{ totalBuy, totalSupply};
     }
 
     private int handleOperation(int amount, String[] operationItem, String operation) {
-        if (operation.equals(operationItem[OPERATION_NAME])) {
-            amount += Integer.parseInt(operationItem[OPERATION_AMOUNT]);
+        if (operation.equals(operationItem[OPERATION_NAME_INDEX])) {
+            amount += Integer.parseInt(operationItem[OPERATION_AMOUNT_INDEX]);
         }
         return amount;
     }
