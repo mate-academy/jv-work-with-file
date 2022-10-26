03fd837f119dbed661fc147675f853780f41046c
Oleksandr Fedchik <fed4iksasha@gmail.com>
2022-10-26 21:10:45 +0300

    Removed all redudant empty line

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,7 +8,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-
     private static final String SPECIFIED_CHARACTER = ",";
     private static final String SUPPLY_OPERATION_NAME = "supply";
     private static final String BUY_OPERATION_NAME = "buy";
@@ -19,39 +18,25 @@ public class WorkWithFile {
     private static final int BUY_TOTAL_AMOUNT_INDEX = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
-
         File fileFrom = new File(fromFileName);
-
         int[] reportArray = processFile(fileFrom);
-
         String report = createReport(
                 reportArray[SUPPLY_TOTAL_AMOUNT_INDEX], reportArray[BUY_TOTAL_AMOUNT_INDEX]);
-
         writeToFile(toFileName, report);
-
     }
 
     private int[] processFile(File fileFrom) {
         int totalSupply = 0;
         int totalBuy = 0;
-
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileFrom))) {
-
             String dataLine = bufferedReader.readLine();
             String[] separatorLine;
-
             while (dataLine != null) {
-
                 separatorLine = dataLine.split(SPECIFIED_CHARACTER);
-
                 totalSupply = handleOperation(totalSupply, separatorLine, SUPPLY_OPERATION_NAME);
-
                 totalBuy = handleOperation(totalBuy, separatorLine, BUY_OPERATION_NAME);
-
                 dataLine = bufferedReader.readLine();
-
             }
-
         } catch (IOException e) {
             throw new RuntimeException("Can't read data from file: " + fileFrom, e);
         }
@@ -59,33 +44,25 @@ public class WorkWithFile {
     }
 
     private int handleOperation(int amount, String[] operationItem, String operation) {
-
         if (operation.equals(operationItem[OPERATION_NAME])) {
             amount += Integer.parseInt(operationItem[OPERATION_AMOUNT]);
         }
-
         return amount;
     }
 
     private String createReport(int supply, int buy) {
-
         return String.format(REPORT_MESSAGE_FORMAT,
                 buy, System.lineSeparator(),
                 supply, System.lineSeparator(),
                 buy - supply);
-
     }
 
     private void writeToFile(String toFileName, String report) {
-
         try (BufferedWriter bufferedWriter =
                      new BufferedWriter(new FileWriter(new File(toFileName)))) {
-
             bufferedWriter.write(report);
-
         } catch (IOException e) {
             throw new RuntimeException("Can't write data to file: " + toFileName, e);
         }
-
     }
 }
