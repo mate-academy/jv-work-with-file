c332945508d6011c48726b2e7a4a15523034cc1d
Andrii First <andre_first@ukr.net>
2022-10-28 00:28:49 +0100

    Apply requested changes. Method getStatistic use 3 other methods to get correct information.

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,16 +8,23 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    private static final String REGEX = "[^A-Za-z0-9 ]+";
+    private String tempString;
+    private int supply = 0;
+    private int buy = 0;
+    private String result;
 
     public void getStatistic(String fromFileName, String toFileName) {
+        WorkWithFile workWithFile = new WorkWithFile();
+        workWithFile.read(fromFileName);
+        workWithFile.calculate();
+        workWithFile.write(toFileName);
+    }
 
+    public void read(String fromFileName) {
         File fileFrom = new File(fromFileName);
-        File fileTo = new File(toFileName);
-
-        String tempString;
         try {
             BufferedReader reader = new BufferedReader(new FileReader(fileFrom));
-
             StringBuilder builder = new StringBuilder();
             String value = reader.readLine();
             while (value != null) {
@@ -25,29 +32,36 @@ public class WorkWithFile {
                 value = reader.readLine();
             }
             tempString = builder.toString();
-            String[] tempStringArray;
-            int supply = 0;
-            int buy = 0;
-            int result;
-
-            tempStringArray = tempString.split("[^A-Za-z0-9 ]+");
-            for (int i = 0; i < tempStringArray.length; i = i + 2) {
-                tempString = tempStringArray[i];
-                if (tempString.startsWith("s")) {
-                    supply = supply + Integer.parseInt(String.valueOf(tempStringArray[i + 1]));
-                } else {
-                    buy = buy + Integer.parseInt(String.valueOf(tempStringArray[i + 1]));
-                }
-            }
-            BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName));
-            result = supply - buy;
-            writer.write("supply," + supply + System.lineSeparator()
-                    + "buy," + buy + System.lineSeparator() + "result," + result);
-            writer.close();
-
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Can't read from file",e);
         }
+    }
 
+    public void calculate() {
+        String[] tempStringArray = tempString.split(REGEX);
+        for (int i = 0; i < tempStringArray.length; i = i + 2) {
+            tempString = tempStringArray[i];
+            if (tempString.startsWith("s")) {
+                supply = supply + Integer.parseInt(String.valueOf(tempStringArray[i + 1]));
+            } else {
+                buy = buy + Integer.parseInt(String.valueOf(tempStringArray[i + 1]));
+            }
+        }
+        result = "supply," + supply + System.lineSeparator()
+                + "buy," + buy + System.lineSeparator() + "result," + (supply - buy);
+    }
+
+    public void write(String toFileName) {
+        BufferedWriter writer;
+        try {
+            writer = new BufferedWriter(new FileWriter(toFileName));
+            writer.write(result);
+            writer.close();
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write to file", e);
+        }
     }
 }
+
+
+
