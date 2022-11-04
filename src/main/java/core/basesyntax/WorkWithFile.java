cd11ebd0172252719c45849e880705a00c744eab
Anton Kalenskyi <anton_kalenskiy@ukr.net>
2022-11-04 11:44:31 +0200

    Moved some variables into methods, added one method createReport(), changed logic of method writeTheFile().

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -14,21 +14,17 @@ public class WorkWithFile {
     private static final String SUPPLY = "supply";
     private static final String BUY = "buy";
     private static final String RESULT = "result";
-    private StringBuilder stringBuilder;
-    private File file;
-    private int sumSupply = 0;
-    private int sumBuy = 0;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        readTheFile(fromFileName);
-        countSum();
-        writeTheFile(toFileName);
+        StringBuilder stringBuilder = readTheFile(fromFileName);
+        String resultData = createReport(stringBuilder);
+        writeTheFile(toFileName, resultData);
     }
 
-    private void readTheFile(String fromFileName) {
-        file = new File(fromFileName);
+    private StringBuilder readTheFile(String fromFileName) {
+        File file = new File(fromFileName);
+        StringBuilder stringBuilder = new StringBuilder();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
-            stringBuilder = new StringBuilder();
             String value = bufferedReader.readLine();
             while (value != null) {
                 stringBuilder.append(value).append(System.lineSeparator());
@@ -37,23 +33,33 @@ public class WorkWithFile {
         } catch (IOException e) {
             throw new RuntimeException("can`t read the file" + fromFileName, e);
         }
+        return stringBuilder;
     }
 
-    private void writeTheFile(String toFileName) {
-        file = new File(toFileName);
+    private void writeTheFile(String toFileName, String resultData) {
+        File file = new File(toFileName);
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file, true))) {
-            stringBuilder = new StringBuilder().append(SUPPLY).append(REGEX).append(sumSupply)
-                            .append(System.lineSeparator()).append(BUY).append(REGEX).append(sumBuy)
-                            .append(System.lineSeparator()).append(RESULT).append(REGEX)
-                            .append(Integer.valueOf(sumSupply - sumBuy));
-            bufferedWriter.write(stringBuilder.toString());
+            bufferedWriter.write(resultData);
             bufferedWriter.flush();
         } catch (IOException e) {
             throw new RuntimeException("can`t write to file" + toFileName, e);
         }
     }
 
-    private void countSum() {
+    private String createReport(StringBuilder stringBuilder) {
+        int[] count = countSum(stringBuilder);
+        int sumSupply = count[0];
+        int sumBuy = count[1];
+        StringBuilder resultData = new StringBuilder().append(SUPPLY).append(REGEX)
+                .append(sumSupply).append(System.lineSeparator()).append(BUY)
+                .append(REGEX).append(sumBuy).append(System.lineSeparator())
+                .append(RESULT).append(REGEX).append(Integer.valueOf(sumSupply - sumBuy));
+        return resultData.toString();
+    }
+
+    private int[] countSum(StringBuilder stringBuilder) {
+        int sumSupply = 0;
+        int sumBuy = 0;
         String[] splitArray = stringBuilder.toString().split(System.lineSeparator());
         for (String operationTypeAndValue : splitArray) {
             String[] insideOperation = operationTypeAndValue.split(REGEX);
@@ -67,5 +73,6 @@ public class WorkWithFile {
                     break;
             }
         }
+        return new int[]{sumSupply, sumBuy};
     }
 }
