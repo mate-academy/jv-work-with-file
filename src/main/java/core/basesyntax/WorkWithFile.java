c6cd735fae75db5f1df829419be8674e6b062ada
Aleksandr Vradii <alexvradii@gmail.com>
2022-11-04 22:22:23 +0200

    My solution

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -3,23 +3,24 @@ package core.basesyntax;
 import java.io.*;
 
 public class WorkWithFile {
+    private static final String SUPPLY = "Supply";
+    private static final String BUY = "Buy";
+    private static final String RESULT = "Result";
+
 
     public void getStatistic(String fromFileName, String toFileName) {
 
         File file = new File(fromFileName);
-        int result;
         try {
-            result = extracted(file);
+            prepareCountResult(file, toFileName);
         } catch (FileNotFoundException e) {
             throw new RuntimeException("Can't find file", e);
         } catch (IOException e) {
             throw new RuntimeException("Can't read file", e);
         }
-//        int result = calculateResult(supplyCount, buyCount);
-        System.out.println("");
     }
 
-    private int extracted(File file) throws IOException {
+    private void prepareCountResult(File file, String toFileName) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
         String value = bufferedReader.readLine();
         int supplyCount = 0;
@@ -27,15 +28,20 @@ public class WorkWithFile {
         while (value != null) {
             String[] listFromFile = value.split(",");
             for (int i = 0; i < listFromFile.length; i++) {
-                if (listFromFile[0].equals("supply")) {
+                if (listFromFile[0].equals(SUPPLY)) {
                     supplyCount = supplyCount + Integer.parseInt(listFromFile[1]);
                 }
-                if (listFromFile[0].equals("buy")) {
+                if (listFromFile[0].equals(BUY)) {
                     buyCount += Integer.parseInt(listFromFile[1]);
                 }
             }
             value = bufferedReader.readLine();
         }
-        return supplyCount - buyCount;
+        resultFile(toFileName, supplyCount, buyCount);
+    }
+
+    private void resultFile(String toFileName, int supplyCount, int buyCount) {
+        File file = new File(toFileName);
+        int resultCount = supplyCount - buyCount;
     }
 }
