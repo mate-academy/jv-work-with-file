b3692604a7b4d73ddf7e2f789a39f80df94e7b9c
Vadym Valchak <vadym.valchak@gmail.com>
2022-11-05 16:56:36 +0200

    Added reading and writing result to file

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,8 +1,44 @@
 package core.basesyntax;
 
+import java.io.*;
+import java.util.*;
+
 public class WorkWithFile {
-
     public void getStatistic(String fromFileName, String toFileName) {
+        Map<String, Integer> fileData = getDataFromFile(fromFileName);
+        writeResultToFile(toFileName, fileData);
+    }
 
+    private static Map<String, Integer> getDataFromFile(String fromFileName) {
+        String value;
+        Map<String, Integer> map = new HashMap<>();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
+            while ((value = bufferedReader.readLine()) != null) {
+                String[] arr = value.split(",");
+                String fruitName = arr[0];
+                int price = Integer.parseInt(arr[1]);
+
+                if (map.containsKey(fruitName)) {
+                    Integer sum = map.get(fruitName) + price;
+                    map.put(fruitName, sum);
+                } else {
+                    map.put(fruitName, price);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return map;
+    }
+
+    private static void writeResultToFile(String toFileName, Map<String, Integer> map) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+            int result = map.get("supply") - map.get("buy");
+            bufferedWriter.write("supply" + "," + map.get("supply") + "\r\n");
+            bufferedWriter.write("buy" + "," + map.get("buy") + "\r\n");
+            bufferedWriter.write("result" + "," + result + "\r\n");
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write the file " + toFileName, e);
+        }
     }
 }
