50d9ff5e2b900efcfd96cbe519a0904cdca632dc
Andrii Varlanovych <andrew_exp@ukr.net>
2022-11-10 15:39:34 +0200

    changes were added according your request

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -11,28 +11,26 @@ public class WorkWithFile {
 
     public void getStatistic(String fromFileName, String toFileName) {
         List<String> stringFromFile = readData(fromFileName);
-        String[] data = createData(stringFromFile);
-        writeData(data, toFileName);
+        String[] data = createReport(stringFromFile);
+        writeReport(data, toFileName);
     }
 
     private List<String> readData(String fromFileName) {
         File file = new File(fromFileName);
-        List<String> stringList;
         try {
-            stringList = Files.readAllLines(file.toPath());
+            return Files.readAllLines(file.toPath());
         } catch (IOException e) {
             throw new RuntimeException("Can't read data from the file " + fromFileName, e);
         }
-        return stringList;
     }
 
-    private String[] createData(List<String> stringList) {
-        String[] data = {"supply,", "buy,", "result,"};
+    private String[] createReport(List<String> dataList) {
+        String[] report = {"supply,", "buy,", "result,"};
         int supplyValue = 0;
         int buyValue = 0;
         int resultValue = 0;
 
-        for (String string : stringList) {
+        for (String string : dataList) {
             String[] split = string.split(",");
             if (split[0].equals("supply")) {
                 supplyValue = supplyValue + Integer.parseInt(split[1]);
@@ -42,29 +40,24 @@ public class WorkWithFile {
             }
         }
         resultValue = supplyValue - buyValue;
-        data[0] = data[0] + supplyValue;
-        data[1] = data[1] + buyValue;
-        data[2] = data[2] + resultValue;
-        return data;
+        report[0] = report[0] + supplyValue;
+        report[1] = report[1] + buyValue;
+        report[2] = report[2] + resultValue;
+        return report;
     }
 
-    private void writeData(String[] data, String toFileName) {
+    private void writeReport(String[] report, String toFileName) {
         File file = new File(toFileName);
         StringBuilder stringBuilder = new StringBuilder();
 
-        for (String stringData : data) {
+        for (String reportLine : report) {
             try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file, true))) {
-                bufferedWriter.write(stringBuilder.append(stringData)
+                bufferedWriter.write(stringBuilder.append(reportLine)
                         .append(System.lineSeparator()).toString());
                 stringBuilder.setLength(0);
             } catch (IOException e) {
-                throw new RuntimeException("Can't write data to file", e);
+                throw new RuntimeException("Can't write data to file" + toFileName, e);
             }
         }
-    }
-
-    public static void main(String[] args) {
-        WorkWithFile workWithFile = new WorkWithFile();
-        workWithFile.getStatistic("apple.csv", "test.csv");
     }
 }
