64560321266401dcce11c0b6af8c16479b85b4e9
Uka0001 <ukraine0001@gmail.com>
2022-11-10 23:22:28 +0200

    csv file read and write need fix

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,8 +1,43 @@
 package core.basesyntax;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class WorkWithFile {
 
-    public void getStatistic(String fromFileName, String toFileName) {
+    private static final String COMMA_DELIMITER = ",";
 
+    public void getStatistic(String fromFileName,
+                             String toFileName) {
+        String report;
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName));) {
+            String lines = bufferedReader.readLine();
+            while (lines != null) {
+                String[] values = lines.split(COMMA_DELIMITER);
+                stringBuilder.append(System.lineSeparator()).append(values[0]);
+                int sum = 0;
+                for (int i = 1; i < values.length; i++){
+                    sum += Integer.parseInt(values[i]);
+                }
+                stringBuilder.append(",").append(sum);
+            }
+            report = stringBuilder.toString();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("Can't find file " + fromFileName, e);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read data from the file " + fromFileName, e);
+        }
+
+        try (BufferedWriter bufferedWriter = Files.newBufferedWriter(Path.of("src/main/java/core/basesyntax/" + toFileName))) {
+            bufferedWriter.write("```csv");
+            bufferedWriter.newLine();
+            bufferedWriter.write(report);
+            bufferedWriter.newLine();
+            bufferedWriter.write("```");
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write data to the file " + toFileName, e);
+        }
     }
 }
