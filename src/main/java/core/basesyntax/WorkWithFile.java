d116b404f66fd082fb7505324978661adb5b62fe
Yaroslav Liashchenko <liaroswin@gmail.com>
2022-11-11 17:44:28 +0200

    divided methods

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -14,26 +14,37 @@ public class WorkWithFile {
     private static final String BUY_STRING = "buy";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        writeToFile(createReport(fromFileName), toFileName);
+        String dataFromFile = readFromFile(fromFileName);
+        String report = createReport(dataFromFile);
+        writeToFile(report, toFileName);
     }
 
-    private String createReport(String fileName) {
-        int supplySum = 0;
-        int buySum = 0;
+    private String readFromFile(String fileName) {
+        StringBuilder stringBuilder = new StringBuilder();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
             String value = bufferedReader.readLine();
             while (value != null) {
-                String[] line = value.split(SEPARATOR_SYMBOL);
-                if (line[OPERATION_INDEX].equals(SUPPLY_STRING)) {
-                    supplySum += Integer.parseInt(line[SUM_INDEX]);
-                }
-                if (line[OPERATION_INDEX].equals(BUY_STRING)) {
-                    buySum += Integer.parseInt(line[SUM_INDEX]);
-                }
+                stringBuilder.append(value).append(System.lineSeparator());
                 value = bufferedReader.readLine();
             }
         } catch (IOException e) {
             throw new RuntimeException("Can`t read file with name: " + fileName, e);
+        }
+        return stringBuilder.toString();
+    }
+
+    private String createReport(String data) {
+        int supplySum = 0;
+        int buySum = 0;
+        String[] splitedData = data.split(System.lineSeparator());
+        for (String line : splitedData) {
+            String[] splitedLine = line.split(SEPARATOR_SYMBOL);
+            if (splitedLine[OPERATION_INDEX].equals(SUPPLY_STRING)) {
+                supplySum += Integer.parseInt(splitedLine[SUM_INDEX]);
+            }
+            if (splitedLine[OPERATION_INDEX].equals(BUY_STRING)) {
+                buySum += Integer.parseInt(splitedLine[SUM_INDEX]);
+            }
         }
         return new StringBuilder(SUPPLY_STRING)
                 .append(SEPARATOR_SYMBOL).append(supplySum)
