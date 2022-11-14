7f764956988a4f3ee4519e5f45ade974f83af7df
Vadym Valchak <vadym.valchak@gmail.com>
2022-11-14 20:02:48 +0200

    Add reportResult() method, rename "readFromFile(), writeReportToFile()" methods

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -5,38 +5,49 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
-        Map<String, Integer> fileData = getDataFromFile(fromFileName);
-        writeResultToFile(toFileName, fileData);
+        List<String> fileData = readFromFile(fromFileName);
+        Map reportResult = createReport(fileData);
+        writeReportToFile(toFileName, reportResult);
     }
 
-    private Map<String, Integer> getDataFromFile(String fromFileName) {
+    private Map<String, Integer> createReport(List<String> fileRows) {
+        Map<String, Integer> reportValue = new HashMap<>();
+        for (String fileRow : fileRows) {
+            String[] nameAndPrice = fileRow.split(",");
+            String fruitName = nameAndPrice[0];
+            final int price = Integer.parseInt(nameAndPrice[1]);
+
+            if (reportValue.containsKey(fruitName)) {
+                Integer sum = reportValue.get(fruitName) + price;
+                reportValue.put(fruitName, sum);
+            } else {
+                reportValue.put(fruitName, price);
+            }
+        }
+        return reportValue;
+    }
+
+    private List<String> readFromFile(String fromFileName) {
         String value;
-        Map<String, Integer> map = new HashMap<>();
+        List<String> fileRowsValue = new ArrayList<>();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             while ((value = bufferedReader.readLine()) != null) {
-                String[] arr = value.split(",");
-                String fruitName = arr[0];
-                int price = Integer.parseInt(arr[1]);
-
-                if (map.containsKey(fruitName)) {
-                    Integer sum = map.get(fruitName) + price;
-                    map.put(fruitName, sum);
-                } else {
-                    map.put(fruitName, price);
-                }
+                fileRowsValue.add(value);
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        return map;
+        return fileRowsValue;
     }
 
-    private static void writeResultToFile(String toFileName, Map<String, Integer> map) {
+    private static void writeReportToFile(String toFileName, Map<String, Integer> map) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
             int result = map.get("supply") - map.get("buy");
             bufferedWriter.write("supply" + "," + map.get("supply") + System.lineSeparator());
