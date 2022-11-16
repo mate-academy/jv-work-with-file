b0e0a11248952c40948803af5781c9284f788734
Artem Bordichuk <artyombordichuk@gmail.com>
2022-11-16 18:36:26 +0200

    Code refactor: 1. Move logic to read data to a separate readFromFile()method. 2. Create and return File inside the writeToFile() method.

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,32 +1,39 @@
 package core.basesyntax;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
 
 public class WorkWithFile {
+    /* Is it this class have a next logic? One of the class fields must be File entity. Because in work situation
+    in eighteen's line of code we need to pass File entity in constructor. After that we are going to call writeToFile()
+    method. WorkWithFile class is kind of utility class.
+    */
     private static final int OPERATION_TYPE_INDEX = 0;
     private static final int AMOUNT_INDEX = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        String[] splitInformationFromFile;
+        String[] splitInformationFromFile = readFromFile(fromFileName);
+        String report = createReport(splitInformationFromFile);
+        WorkWithFile file = new WorkWithFile();
+        file.writeToFile(toFileName, report);
+    }
+
+    private String[] readFromFile(String input) {
         StringBuilder builder = new StringBuilder();
-        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(input))) {
             String value = reader.readLine();
             while (value != null) {
                 builder.append(value).append(System.lineSeparator());
                 value = reader.readLine();
             }
-            splitInformationFromFile = builder.toString().split(System.lineSeparator());
+            return builder.toString().split(System.lineSeparator());
         } catch (IOException e) {
-            throw new RuntimeException("Can't read a file " + fromFileName, e);
+            throw new RuntimeException("Can't read a file " + input, e);
         }
-        String report = createReport(splitInformationFromFile);
-        File newFile = new File(toFileName);
-        WorkWithFile file = new WorkWithFile();
-        file.writeToFile(newFile, report);
     }
 
     private String createReport(String[] array) {
@@ -53,9 +60,10 @@ public class WorkWithFile {
                 .append(supply - buy).toString();
     }
 
-    private void writeToFile(File file, String string) {
-        try {
-            Files.write(file.toPath(), string.getBytes());
+    private void writeToFile(String fileName, String information) {
+        File file = new File(fileName);
+        try (BufferedWriter bf = new BufferedWriter(new FileWriter(file))) {
+            bf.write(information);
         } catch (IOException e) {
             throw new RuntimeException("Can't write data to file " + file, e);
         }
