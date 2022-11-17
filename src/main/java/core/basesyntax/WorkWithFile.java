1615b4b5c17fd9851cd6e7c6209129664b9cd3bc
AndriiBohun <poikka69@gmail.com>
2022-11-17 16:04:59 +0200

    Commit003Done

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,6 +1,11 @@
 package core.basesyntax;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import static java.lang.Integer.parseInt;
 
@@ -10,36 +15,38 @@ public class WorkWithFile {
 
         File file1 = new File(fromFileName);
         StringBuilder stringBuilder = new StringBuilder();
+
         try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(file1));
-
             int value = bufferedReader.read();
             while (value != -1) {
                 stringBuilder.append((char) value);
                 value = bufferedReader.read();
             }
         } catch (IOException e) {
-            throw new RuntimeException("Can't read file", e);
+            throw new RuntimeException("Can't read file1", e);
         }
-        //-----------------------------------------------/
+
         String fileString = stringBuilder.toString();
         String operationSupply = "supply";
         String operationBuy = "buy";
         int summaSupply = 0;
         int summaBuy = 0;
-        int summaResult = 0;
+        int summaResult;
 
         String[] arrayFromFileName = fileString.split("\n");
-        for (int i = 0; i < arrayFromFileName.length; i++) {
-            if (arrayFromFileName[i].contains(operationSupply)) {
-                summaSupply += parseInt(arrayFromFileName[i].split(",")[1]);
+        for (String s : arrayFromFileName) {
+            if (s.contains(operationSupply)) {
+                summaSupply += parseInt(s.split(",")[1]);
             } else {
-                summaBuy += parseInt(arrayFromFileName[i].split(",")[1]);
+                summaBuy += parseInt(s.split(",")[1]);
             }
         }
         summaResult = summaSupply - summaBuy;
-        String mainString = operationSupply + "," + summaSupply + "\n" + operationBuy + "," + summaBuy + "\n" + "result" + "," + summaResult;
-        //-----------------------------------------------/
+        String mainString = operationSupply +
+                "," + summaSupply +
+                "\n" + operationBuy + "," + summaBuy +
+                "\n" + "result" + "," + summaResult;
 
         File file2 = new File(toFileName);
         BufferedWriter bufferedWriter = null;
