58b58475a12b7de75ed794884e2404fa75131ebd
Oleksandr <xtremesasha@outlook.com>
2022-11-27 18:49:06 +0200

    added methods and constance

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -11,9 +11,18 @@ public class WorkWithFile {
     private static final String WORD_SUPPLY = "supply";
     private static final String WORD_BUY = "buy";
     private static final String WORD_RESULT = "result";
+    private static final int INDEX_NAME = 0;
+    private static final int INDEX_NUMBER = 1;
+    private static final String COM_SPRT = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        File file = new File(fromFileName);
+        String s = readFromFile(fromFileName);
+        String report = createReport(s);
+        writeToFile(report,toFileName);
+    }
+
+    private static String readFromFile(String inputFileName) {
+        File file = new File(inputFileName);
         StringBuilder stringBuilder = new StringBuilder();
         try {
             BufferedReader reader = new BufferedReader(new FileReader(file));
@@ -25,27 +34,35 @@ public class WorkWithFile {
         } catch (IOException e) {
             throw new RuntimeException("Can't read file", e);
         }
+        return stringBuilder.toString();
+    }
+
+    private static String createReport(String dataFromFile) {
         int sumSupply = 0;
         int sumBuy = 0;
-        String[] strings = stringBuilder.toString().split(" ");
+        String[] strings = dataFromFile.split(" ");
         for (int i = 0; i < strings.length; i++) {
-            String[] s = strings[i].split(",");
-            if (s[0].equals(WORD_SUPPLY)) {
-                sumSupply += Integer.parseInt(s[1]);
+            String[] s = strings[i].split(COM_SPRT);
+            if (s[INDEX_NAME].equals(WORD_SUPPLY)) {
+                sumSupply += Integer.parseInt(s[INDEX_NUMBER]);
             } else {
-                sumBuy += Integer.parseInt(s[1]);
+                sumBuy += Integer.parseInt(s[INDEX_NUMBER]);
             }
         }
         int difference = sumSupply - sumBuy;
         StringBuilder builder = new StringBuilder();
-        String writeData = builder.append(WORD_SUPPLY).append(",").append(sumSupply)
+        String writeData = builder.append(WORD_SUPPLY).append(COM_SPRT).append(sumSupply)
                 .append(System.lineSeparator())
-                .append(WORD_BUY).append(",").append(sumBuy)
+                .append(WORD_BUY).append(COM_SPRT).append(sumBuy)
                 .append(System.lineSeparator())
-                .append(WORD_RESULT).append(",").append(difference).toString();
-        File file1 = new File(toFileName);
+                .append(WORD_RESULT).append(COM_SPRT).append(difference).toString();
+        return writeData;
+    }
+
+    private static void writeToFile(String outputData, String outputFileName) {
+        File file1 = new File(outputFileName);
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file1))) {
-            bufferedWriter.write(writeData);
+            bufferedWriter.write(outputData);
         } catch (IOException e) {
             throw new RuntimeException("Can't write data to file", e);
         }
