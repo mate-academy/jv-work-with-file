0472904212652418989c6ed5092272d9e3d0715e
Stepan Peknyi <stepan_peknyy@ukr.net>
2022-11-27 20:27:55 +0200

    1) variables stringBuilder and report changed to method level from class level 2) created method redFromFile() 3) used BufferedWriter

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,18 +1,25 @@
 package core.basesyntax;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
 
 public class WorkWithFile {
     private static final String SUPPLY = "supply";
     private static final String BUY = "buy";
-    private StringBuilder stringBuilder = new StringBuilder();
-    private String report = new String();
 
     public void getStatistic(String fromFileName, String toFileName) {
+        StringBuilder stringBuilder = new StringBuilder();
+        readFromFile(fromFileName, stringBuilder);
+        String report = createReport(stringBuilder).toString();
+        writeToFile(toFileName, report);
+    }
+
+    private StringBuilder readFromFile(String fromFileName, StringBuilder stringBuilder) {
+
         try {
             File fileForRead = new File(fromFileName);
             BufferedReader bufferedReader = new BufferedReader(new FileReader(fileForRead));
@@ -21,14 +28,13 @@ public class WorkWithFile {
                 stringBuilder.append(value).append(",");
                 value = bufferedReader.readLine();
             }
-            createReport();
-            writeToFile(toFileName);
+            return stringBuilder;
         } catch (IOException e) {
-            throw new RuntimeException(e + " Can't read file: " + fromFileName);
+            throw new RuntimeException(e + " Can't read a file: " + fromFileName);
         }
     }
 
-    private void createReport() {
+    private StringBuilder createReport(StringBuilder stringBuilder) {
         String[] statistic = stringBuilder.toString().split(",");
         int supplySum = 0;
         int buySum = 0;
@@ -44,20 +50,30 @@ public class WorkWithFile {
         stringBuilder.append(SUPPLY).append(",").append(supplySum)
         .append(System.lineSeparator()).append(BUY).append(",").append(buySum)
         .append(System.lineSeparator()).append("result").append(",").append(result);
-        report = stringBuilder.toString();
+        return stringBuilder;
     }
 
-    public void writeToFile(String toFileName) {
+    public void writeToFile(String toFileName, String report) {
         File fileForWrite = new File(toFileName);
         try {
             fileForWrite.createNewFile();
+            BufferedWriter bufferedWriter = null;
+            try {
+                bufferedWriter = new BufferedWriter(new FileWriter(fileForWrite));
+                bufferedWriter.write(report);
+            } catch (IOException e) {
+                throw new RuntimeException(e + " Can't write to a file: " + fileForWrite);
+            } finally {
+                if (bufferedWriter != null) {
+                    try {
+                        bufferedWriter.close();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e + " Cant close a file " + fileForWrite);
+                    }
+                }
+            }
         } catch (IOException e) {
             throw new RuntimeException(e + " Can't create a file: " + fileForWrite);
-        }
-        try {
-            Files.write(fileForWrite.toPath(), report.getBytes());
-        } catch (IOException e) {
-            throw new RuntimeException(e + " Can't write to file: " + fileForWrite);
         }
     }
 }
