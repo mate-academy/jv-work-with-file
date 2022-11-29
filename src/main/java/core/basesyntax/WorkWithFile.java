8a4da262c77c9dba3e4fd12afc96821290826d86
Yurii Storozhuk <95564174+urlo0k@users.noreply.github.com>
2022-11-29 21:11:18 +0200

    feature(method): create method createReport

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -15,41 +15,41 @@ public class WorkWithFile {
     private static final int ONE_INDEX = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        String text = readFromFileName(fromFileName);
-        writeToFileName(text, toFileName);
+        String result = createReport(readFromFileName(fromFileName));
+        writeToFile(result, toFileName);
     }
 
-    public String readFromFileName(String fromFileName) {
-        int resultSupply = 0;
-        int resultBuy = 0;
+    public String[] readFromFileName(String fromFileName) {
         try {
-            File file = new File(fromFileName);
-            List<String> input = Files.readAllLines(file.toPath());
-            String[] lines = input.toArray(new String[input.size()]);
-            for (String line : lines) {
-                String[] everyLine = line.split(",");
-                if (everyLine[ZERO_INDEX].equals(SUPPLY_WORD)) {
-                    resultSupply += Integer.parseInt(everyLine[ONE_INDEX]);
-                } else {
-                    resultBuy += Integer.parseInt(everyLine[ONE_INDEX]);
-                }
-            }
-            StringBuilder builder = new StringBuilder();
-            builder.append(SUPPLY_WORD).append(",").append(resultSupply);
-            builder.append(System.lineSeparator());
-            builder.append(BUY_WORD).append(",").append(resultBuy).append(System.lineSeparator());
-            builder.append(RESULT_WORD).append(",").append(resultSupply - resultBuy);
-            return builder.toString();
+            List<String> input = Files.readAllLines(new File(fromFileName).toPath());
+            return input.toArray(new String[input.size()]);
         } catch (IOException e) {
             throw new RuntimeException("Can't read data from the file " + fromFileName, e);
         }
     }
 
-    public void writeToFileName(String result, String toFileName) {
+    public String createReport(String[] lines) {
+        int resultSupply = 0;
+        int resultBuy = 0;
+        for (String line : lines) {
+            String[] everyLine = line.split(",");
+            if (everyLine[ZERO_INDEX].equals(SUPPLY_WORD)) {
+                resultSupply += Integer.parseInt(everyLine[ONE_INDEX]);
+            } else {
+                resultBuy += Integer.parseInt(everyLine[ONE_INDEX]);
+            }
+        }
+        StringBuilder builder = new StringBuilder();
+        builder.append(SUPPLY_WORD).append(",").append(resultSupply);
+        builder.append(System.lineSeparator());
+        builder.append(BUY_WORD).append(",").append(resultBuy).append(System.lineSeparator());
+        builder.append(RESULT_WORD).append(",").append(resultSupply - resultBuy);
+        return builder.toString();
+    }
+
+    public void writeToFile(String result, String toFileName) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName, true))) {
-            //FileWriter output = new FileWriter(toFileName);
             bufferedWriter.write(result);
-            //bufferedWriter.close();
         } catch (IOException e) {
             throw new RuntimeException("Can't write data to the file " + toFileName, e);
         }
