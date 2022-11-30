7f9e6314742ccf563fa8c0abbd9c5dab61298f6e
salesforcetrainees2 <loranazarenko26@gmail.com>
2022-11-30 21:33:42 +0200

    First commit homework

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,6 +1,11 @@
 package core.basesyntax;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class WorkWithFile {
     public static void main(String[] args) {
