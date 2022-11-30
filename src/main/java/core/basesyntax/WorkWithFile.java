0863b13c8f985b6fbf9dc465d8c312d069755d8c
Bohdan Shevtsov <shevtsov.b@gmail.com>
2022-11-30 17:51:20 +0200

    common mistakes fixed

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,56 +1,57 @@
 package core.basesyntax;
 
-import javax.imageio.IIOException;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class WorkWithFile {
 
     public void getStatistic(String fromFileName, String toFileName) {
+        writeToFile(new File(toFileName), makeReport(readLinesFromFile(fromFileName)));
+    }
+
+    private StringBuilder readLinesFromFile(String fromFileName) {
         File fileFrom = new File(fromFileName);
         StringBuilder stringBuilder = new StringBuilder();
-        int counter = 0;
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileFrom))) {
             String value = bufferedReader.readLine();
             while (value != null) {
                 stringBuilder.append(value).append(System.lineSeparator());
                 value = bufferedReader.readLine();
-                counter++;
             }
         } catch (IOException e) {
-            throw new RuntimeException("Can't read the file ",e);
+            throw new RuntimeException("Can't read the file ", e);
         }
+        return stringBuilder;
+    }
 
+    private String[] makeReport(StringBuilder linesFromFile) {
         int supply = 0;
         int buy = 0;
-        String[] operationType = new String[counter];
-        int[] amount = new int[counter];
-        String[] resultString = stringBuilder.toString().split("\\W+");
-
+        String[] resultString = linesFromFile.toString().split("\\W+");
         for (int i = 0; i < resultString.length; i++) {
-            if(i % 2 == 0) {
-                operationType[i / 2] = resultString[i];
-            } else {
-                amount[(i - 1) / 2] = Integer.parseInt(resultString[i]);
-            }
-
-        }
-        for (int i = 0; i < counter; i++) {
-            for (int j = 1; j < counter; j++){
-                if(operationType[i].equals(operationType[j]) && !operationType[i].equals("") && i!=j) {
-                    amount[i] += amount[j];
-                    operationType[j] = "";
+            if (i % 2 == 0) {
+                if (resultString[i].equals("supply")) {
+                    supply += Integer.parseInt(resultString[i + 1]);
+                } else {
+                    buy += Integer.parseInt(resultString[i + 1]);
                 }
             }
         }
-        File fileTo = new File(toFileName);
-        for (int i = 0; i < operationType.length; i++){
-            if (operationType[i].equals("")) break;
-            try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileTo, true))){
-                bufferedWriter.write(operationType[i] + "," + amount[i] + System.lineSeparator());
-            } catch (IOException e) {
-                throw new RuntimeException("Can't write data to file", e);
-            }
-        }
+        String[] result = {String.valueOf(supply), String.valueOf(buy),
+                String.valueOf(supply - buy)};
+        return result;
+    }
 
+    private void writeToFile(File toFile, String[] result) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFile))) {
+            bufferedWriter.write("supply," + result[0] + System.lineSeparator() + "buy," + result[1]
+                    + System.lineSeparator() + "result," + result[2]);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write data to file", e);
+        }
     }
 }
