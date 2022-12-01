c35c89442b780da601f136c4a5ecaa2c9e9e8b16
user <graban2253@gmail.com>
2022-12-01 19:45:48 +0200

    Corrected mistakes

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -6,15 +6,15 @@ import java.nio.file.Files;
 import java.util.List;
 
 public class WorkWithFile {
+    private static final int INDEX_ACTION = 0;
+    private static final int INDEX_AMOUNT = 1;
+    private List<String> listFromFile;
+    private int buy = 0;
+    private int supply = 0;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        List<String> listFromFile;
         File fromFile = new File(fromFileName);
-        File toFile = new File(toFileName);
-        StringBuilder result = new StringBuilder();
         String[] str;
-        int buy = 0;
-        int supply = 0;
 
         try {
             listFromFile = Files.readAllLines(fromFile.toPath());
@@ -23,19 +23,25 @@ public class WorkWithFile {
         }
         for (String s : listFromFile) {
             str = s.split(",");
-            if (str[0].equals("buy")) {
-                buy += Integer.parseInt(str[1]);
+            if (str[INDEX_ACTION].equals("buy")) {
+                buy += Integer.parseInt(str[INDEX_AMOUNT]);
             } else {
-                supply += Integer.parseInt(str[1]);
+                supply += Integer.parseInt(str[INDEX_AMOUNT]);
             }
         }
+        writeToFile(toFileName);
+    }
+
+    public void writeToFile(String toFileName) {
+        StringBuilder result = new StringBuilder();
+        File toFile = new File(toFileName);
+
         result.append("supply,").append(supply).append(System.lineSeparator()).append("buy,")
                 .append(buy).append(System.lineSeparator()).append("result,")
                 .append(supply - buy);
 
         try {
             Files.write(toFile.toPath(), result.toString().getBytes());
-
         } catch (IOException e) {
             throw new RuntimeException("Can't write data to file", e);
         }
