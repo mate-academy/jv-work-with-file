f78b4738aa363535df53bf06e3f88e7460b26e74
Tymofii Skrypko <skrypko270901@gmail.com>
2022-12-01 19:31:14 +0100

    has gone through the checklist and corrected all points described there

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,6 +8,25 @@ import java.io.IOException;
 
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
+        String[] dataFromFile = readFromFile(fromFileName);
+        int buyAmount = 0;
+        int supplyAmount = 0;
+        for (int i = 0; i < dataFromFile.length; i += 2) {
+            if (dataFromFile[i].equals("supply")) {
+                supplyAmount += Integer.parseInt(dataFromFile[i + 1]);
+            } else {
+                buyAmount += Integer.parseInt(dataFromFile[i + 1]);
+            }
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder
+                .append("supply,").append(supplyAmount).append(System.lineSeparator())
+                .append("buy,").append(buyAmount).append(System.lineSeparator())
+                .append("result,").append(supplyAmount - buyAmount);
+        writeToFile(toFileName, stringBuilder);
+    }
+
+    private String[] readFromFile (String fromFileName) {
         StringBuilder stringBuilder = new StringBuilder();
         try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName));
@@ -19,35 +38,14 @@ public class WorkWithFile {
         } catch (IOException e) {
             throw new RuntimeException("Can`t find the file", e);
         }
-        String[] split = stringBuilder.toString().split(",");
-        int buyAmount = 0;
-        int supplyAmount = 0;
-        for (int i = 0; i < split.length; i += 2) {
-            if (split[i].equals("supply")) {
-                supplyAmount += Integer.parseInt(split[i + 1]);
-            } else {
-                buyAmount += Integer.parseInt(split[i + 1]);
-            }
-        }
-        StringBuilder stringBuilder2 = new StringBuilder();
-        stringBuilder2
-                .append("supply,").append(supplyAmount).append(System.lineSeparator())
-                .append("buy,").append(buyAmount).append(System.lineSeparator())
-                .append("result,").append(supplyAmount - buyAmount);
-        BufferedWriter bufferedWriter = null;
-        try {
-            bufferedWriter = new BufferedWriter(new FileWriter(toFileName));
-            bufferedWriter.write(stringBuilder2.toString());
+        return stringBuilder.toString().split(",");
+    }
+
+    private void writeToFile (String toFileName, StringBuilder stringBuilder) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+            bufferedWriter.write(stringBuilder.toString());
         } catch (IOException e) {
             throw new RuntimeException("Can`t write to file", e);
-        } finally {
-            if (bufferedWriter != null) {
-                try {
-                    bufferedWriter.close();
-                } catch (IOException e) {
-                    throw new RuntimeException("Can`t close BufferedWriter", e);
-                }
-            }
         }
     }
 }
