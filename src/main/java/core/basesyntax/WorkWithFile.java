7c1778ef8e02b54f0e4368cf1368a2b423c0518c
salesforcetrainees2 <loranazarenko26@gmail.com>
2022-12-03 19:48:39 +0200

    Hot Changes

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,13 +1,15 @@
 package core.basesyntax;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
 public class WorkWithFile {
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+    private static final String RESULT = "result";
+    private static final int ONE = 1;
+    private static final String B_LETTER = "b";
+    private static final String S_LETTER = "s";
+    private static final String COMMA = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
         String dataFromFile = readFromFile(fromFileName);
@@ -16,26 +18,26 @@ public class WorkWithFile {
     }
 
     private String createReport(String dataFromFile) {
-        String[] split = dataFromFile.split(System.lineSeparator());
+        String[] linesFromFile = dataFromFile.split(System.lineSeparator());
         int supply = 0;
         int buy = 0;
         int result;
-        for (String word : split) {
-            String[] arrayValues = word.split(",");
-            String stringValue = arrayValues[1];
+        for (String lineFromFile : linesFromFile) {
+            String[] arrayValues = lineFromFile.split(",");
+            String stringValue = arrayValues[ONE];
             int value = Integer.parseInt(stringValue);
-            if (word.startsWith("s")) {
+            if (lineFromFile.startsWith(S_LETTER)) {
                 supply += value;
-            } else if (word.startsWith("b")) {
+            } else if (lineFromFile.startsWith(B_LETTER)) {
                 buy += value;
             }
         }
         result = supply - buy;
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("supply").append(",").append(supply)
-                .append(System.lineSeparator()).append("buy").append(",")
-                .append(buy).append(System.lineSeparator()).append("result")
-                .append(",").append(result);
+        stringBuilder.append(SUPPLY).append(COMMA).append(supply)
+                .append(System.lineSeparator()).append(BUY).append(COMMA)
+                .append(buy).append(System.lineSeparator()).append(RESULT)
+                .append(COMMA).append(result);
         return stringBuilder.toString();
     }
 
@@ -61,8 +63,7 @@ public class WorkWithFile {
 
     private String readFromFile(String fileName) {
         StringBuilder stringBuilder = new StringBuilder();
-        try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
             String value = bufferedReader.readLine();
             while (value != null) {
                 value = value.toLowerCase();
