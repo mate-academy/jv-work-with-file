095f36cc6405c49fc5bceee1a7543eddba25cb2e
user <graban2253@gmail.com>
2022-12-03 15:40:45 +0200

    Corrected mistakes

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,13 +8,11 @@ import java.util.List;
 public class WorkWithFile {
     private static final int INDEX_ACTION = 0;
     private static final int INDEX_AMOUNT = 1;
-    private int buy = 0;
-    private int supply = 0;
 
     public void getStatistic(String fromFileName, String toFileName) {
         List<String> dataFromFile = readFromFile(fromFileName);
-        createReport(dataFromFile);
-        writeToFile(toFileName);
+        String report = createReport(dataFromFile);
+        writeToFile(report, toFileName);
     }
 
     public List<String> readFromFile(String fromFileName) {
@@ -23,13 +21,17 @@ public class WorkWithFile {
         try {
             listFromFile = Files.readAllLines(fromFile.toPath());
         } catch (IOException e) {
-            throw new RuntimeException("Can't read fromFile", e);
+            throw new RuntimeException("Can't read from " + fromFileName, e);
         }
         return listFromFile;
     }
 
-    public void createReport(List<String> dataFromFile) {
+    public String createReport(List<String> dataFromFile) {
+        StringBuilder result = new StringBuilder();
+        int buy = 0;
+        int supply = 0;
         String[] dividedString;
+
         for (String s : dataFromFile) {
             dividedString = s.split(",");
             if (dividedString[INDEX_ACTION].equals("buy")) {
@@ -38,20 +40,21 @@ public class WorkWithFile {
                 supply += Integer.parseInt(dividedString[INDEX_AMOUNT]);
             }
         }
-    }
-
-    public void writeToFile(String toFileName) {
-        StringBuilder result = new StringBuilder();
-        File toFile = new File(toFileName);
 
         result.append("supply,").append(supply).append(System.lineSeparator()).append("buy,")
                 .append(buy).append(System.lineSeparator()).append("result,")
                 .append(supply - buy);
 
+        return result.toString();
+    }
+
+    public void writeToFile(String report, String toFileName) {
+        File toFile = new File(toFileName);
+
         try {
-            Files.write(toFile.toPath(), result.toString().getBytes());
+            Files.write(toFile.toPath(), report.getBytes());
         } catch (IOException e) {
-            throw new RuntimeException("Can't write data to file", e);
+            throw new RuntimeException("Can't write data to " + toFileName, e);
         }
     }
 }
