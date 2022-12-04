d5cf6904bab442e654a76d4fb9978601052b2eff
Iskamele <iskamele@gmail.com>
2022-12-05 01:01:06 +0200

    Added static class-level variables: - DATA_LINE_SEPARATOR for line-by-line separation of input data into operation type and amount; - DATA_BUILDER_SEPARATOR to separate the result of a file read and written by StringBuilder; - OPERATION_POSITION to read the operation type in a split line; - AMOUNT_POSITION for amount respectively.
    In the getStatistic() method, a new File object is initialized, which is then passed to the readFromFile() method.
    
    Added a readFromFile() method that reads a file using the BufferedReader and writes the result to a StringBuilder, after which it is passed to the calculateData() method before being passed to the writeToFile() method for writing.
    
    The calculateData() method has been added, in which the supply and buy variables are declared for ease of reading. After that, a String[] array is declared, which is initialized by builder.toString(), split by DATA_BUILDER_SEPARATOR (" ", since the StringBuilder formed in the previous method contains spaces instead of a line separator).
    Then we cycle through the new array. Inside the cycle, we create another auxiliary array, into which we pass the element of the first array, split by DATA_LINE_SEPARATOR (",").
    Using if, we calculate the supply and buy values, knowing their exact indices.
    After the cycle, we reset the StringBuilder, after which we add the calculated data to it, according to the template:
    ```
    supply,__amount__
    buy,__amount__
    result,__amount__
    ```
    We return StringBuilder.
    
    The writeToFile() method has been added, which, after counting, creates a new File object, initializes it with the name toFileName, and writes a StringBuilder to it byte by byte.

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,8 +1,75 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+
 public class WorkWithFile {
+    public static final String DATA_LINE_SEPARATOR = ",";
+    public static final String DATA_BUILDER_SEPARATOR = " ";
+    public static final int OPERATION_POSITION = 0;
+    public static final int AMOUNT_POSITION = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
+        File file = new File(fromFileName);
+        readFromFile(file, toFileName);
+    }
 
+    private void readFromFile(File file, String toFileName) {
+        try {
+            StringBuilder builder = new StringBuilder();
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String value = reader.readLine();
+            while (value != null) {
+                builder.append(value)
+                        .append(" ");
+                value = reader.readLine();
+            }
+            writeToFile(calculateData(builder), toFileName);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
+    }
+
+    private StringBuilder calculateData(StringBuilder builder) {
+        int supply = 0;
+        int buy = 0;
+        String[] array = builder.toString().split(DATA_BUILDER_SEPARATOR);
+        for (String line : array) {
+            String[] splitLine = line.split(DATA_LINE_SEPARATOR);
+            if (splitLine[OPERATION_POSITION].equals("supply")) {
+                supply += Integer.parseInt(splitLine[AMOUNT_POSITION]);
+            }
+            if (splitLine[OPERATION_POSITION].equals("buy")) {
+                buy += Integer.parseInt(splitLine[AMOUNT_POSITION]);
+            }
+        }
+        builder.setLength(0);
+        builder.append("supply,")
+                .append(supply)
+                .append(System.lineSeparator())
+                .append("buy,")
+                .append(buy)
+                .append(System.lineSeparator())
+                .append("result,")
+                .append(supply - buy);
+        return builder;
+    }
+
+    private void writeToFile(StringBuilder calculatedData, String toFileName) {
+        File file = new File(toFileName);
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            throw new RuntimeException("Can't create file", e);
+        }
+        try {
+            Files.write(file.toPath(), calculatedData.toString().getBytes(), StandardOpenOption.APPEND);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write data to file", e);
+        }
     }
 }
