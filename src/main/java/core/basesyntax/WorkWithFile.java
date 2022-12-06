0d042ff6fbb9512a05880e8b3c49849cf24cb3d9
Nikita Kolesnikov <kit1datasvit@gmail.com>
2022-12-06 16:01:07 +0200

    hw-1-solution

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,8 +1,45 @@
 package core.basesyntax;
 
+import java.io.*;
+
 public class WorkWithFile {
+    private final String supplyName = "supply";
+    private final String buyName = "buy";
+    private int supplySum = 0;
+    private int baySum = 0;
+    private int result = 0;
 
     public void getStatistic(String fromFileName, String toFileName) {
+        File file = new File(fromFileName);
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+            String value = bufferedReader.readLine();
+            while (value != null) {
+                String[] split = value.split(",");
+                if (split[0].equals(supplyName)) {
+                    supplySum += Integer.parseInt(split[1]);
+                } else if (split[0].equals(buyName)) {
+                    baySum += Integer.parseInt(split[1]);
+                }
+                value = bufferedReader.readLine();
+            }
+            result = supplySum - baySum;
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file" + fromFileName, e);
+        }
+        writeToFile(toFileName);
+    }
 
+    private void writeToFile(String toFileName) {
+        File file = new File(toFileName);
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file, true))) {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append(supplyName).append(",").append(supplySum)
+                    .append(System.lineSeparator())
+                    .append(buyName).append(",").append(baySum).append(System.lineSeparator())
+                    .append("result").append(",").append(result);
+            bufferedWriter.write(stringBuilder.toString());
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write data to file" + toFileName, e);
+        }
     }
 }
