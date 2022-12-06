b79f5ffe46531954a3574ecc07c4376326b3391c
Vladyslav Fesenko <vlad.11.fesenko@gmail.com>
2022-12-06 14:50:47 +0200

    split createReportFromFile onto 2 separated methods: to read data and to create report

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,7 +2,6 @@ package core.basesyntax;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -12,29 +11,40 @@ public class WorkWithFile {
     private static final int AMOUNT_POSITION = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        String reportData = createReportFromFile(fromFileName);
+        String dataFromFile = readDataFromFile(fromFileName);
+        String reportData = createReport(dataFromFile);
         writeToFile(reportData, toFileName);
     }
 
-    private String createReportFromFile(String fromFileName) {
-        File fromFile = new File(fromFileName);
+    private String readDataFromFile(String fromFileName) {
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
+            String line = reader.readLine();
+            while (line != null) {
+                stringBuilder.append(line).append(System.lineSeparator());
+                line = reader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read from file " + fromFileName,e);
+        }
+
+        return stringBuilder.toString();
+    }
+
+    private String createReport(String data) {
         StringBuilder stringBuilder = new StringBuilder();
         int supply = 0;
         int buy = 0;
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(fromFile))) {
-            String line = reader.readLine();
-            while (line != null) {
-                String[] split = line.split(",");
-                if (split[OPERATION_TYPE_POSITION].equals("supply")) {
-                    supply += Integer.parseInt(split[AMOUNT_POSITION]);
-                } else {
-                    buy += Integer.parseInt(split[AMOUNT_POSITION]);
-                }
-                line = reader.readLine();
+        for (String line: data.split(System.lineSeparator())) {
+            String operationType = line.split(",")[OPERATION_TYPE_POSITION];
+            String amount = line.split(",")[AMOUNT_POSITION];
+
+            if (operationType.equals("supply")) {
+                supply += Integer.parseInt(amount);
+            } else {
+                buy += Integer.parseInt(amount);
             }
-        } catch (IOException e) {
-            throw new RuntimeException("Can't read the file " + fromFileName, e);
         }
 
         stringBuilder.append("supply,").append(supply).append(System.lineSeparator())
@@ -45,8 +55,7 @@ public class WorkWithFile {
     }
 
     private void writeToFile(String reportData, String toFileName) {
-        File toFile = new File(toFileName);
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFile))) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
             writer.write(reportData);
         } catch (IOException e) {
             throw new RuntimeException("Can't write to file " + toFileName, e);
