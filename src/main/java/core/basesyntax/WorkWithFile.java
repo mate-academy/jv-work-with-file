df91bf58c5e7ecbb67ea6d9e2483c98fcdf5d474
Nikita Kolesnikov <kit1datasvit@gmail.com>
2022-12-09 13:47:10 +0200

    hw-1-solution fix mistakes

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,6 +1,11 @@
 package core.basesyntax;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 
 public class WorkWithFile {
     private final String supplyName = "supply";
@@ -10,6 +15,11 @@ public class WorkWithFile {
     private int result = 0;
 
     public void getStatistic(String fromFileName, String toFileName) {
+        readFile(fromFileName);
+        writeToFile(toFileName);
+    }
+
+    private String readFile(String fromFileName) {
         File file = new File(fromFileName);
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
             String value = bufferedReader.readLine();
@@ -26,7 +36,7 @@ public class WorkWithFile {
         } catch (IOException e) {
             throw new RuntimeException("Can't read file" + fromFileName, e);
         }
-        writeToFile(toFileName);
+        return fromFileName;
     }
 
     private void writeToFile(String toFileName) {
