7738fd35ed98936bb3516343c0f7950ac1ed07d3
Denys Yastrebov <programmer_p@outlook.com>
2022-12-12 12:03:46 +0200

    rqstd changes applied

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -10,49 +10,21 @@ import java.io.IOException;
 
 public class WorkWithFile {
 
-    private static final int columnsNumber = 2;
-
     public void getStatistic(String fromFileName, String toFileName) {
 
-        final int linesNumber = countLines(fromFileName);
-
-        String[][] operationData = new String[linesNumber][columnsNumber];
-        operationData = readFromFile(fromFileName);
-
-        int supply = 0;
-        int buy = 0;
-        int result;
-
-        for (int i = 0; i < linesNumber; i++) {
-            if (operationData[i][0].equals("supply")) {
-                supply += Integer.parseInt(operationData[i][1]);
-            } else {
-                buy += Integer.parseInt(operationData[i][1]);
-            }
-        }
-
-        result = supply - buy;
-
-        writeToFile(supply, buy, result, toFileName);
+        String data = readFromFile(fromFileName);
+        String report = createReport(data);
+        writeToFile(report, toFileName);
     }
 
-    private static String[][] readFromFile(String fileName) {
-
-        final int linesNumber = countLines(fileName);
-
+    private static String readFromFile(String fileName) {
         File file = new File(fileName);
-        String[][] data = new String[linesNumber][columnsNumber];
-
-        String[] lines = new String[columnsNumber];
-        int dataCounter = 0;
+        StringBuilder data = new StringBuilder();
 
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             String value = reader.readLine();
             while (value != null) {
-                lines = value.split(",");
-                data[dataCounter][0] = lines[0];
-                data[dataCounter][1] = lines[1];
-                dataCounter++;
+                data.append(value).append(',');
                 value = reader.readLine();
             }
         } catch (FileNotFoundException e) {
@@ -62,31 +34,43 @@ public class WorkWithFile {
         } catch (NullPointerException e) {
             throw new RuntimeException("Null pointer exception");
         }
-        return data;
+        return data.toString();
     }
 
-    private void writeToFile(int supply, int buy, int result, String fileName) {
+    private void writeToFile(String report, String fileName) {
         File file = new File(fileName);
-        StringBuilder report = new StringBuilder();
 
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(file, true))) {
-            report.append("supply,").append(supply).append(System.lineSeparator()).append("buy,")
-                    .append(buy).append(System.lineSeparator()).append("result,").append(result);
-            writer.write(report.toString());
+            writer.write(report);
         } catch (IOException e) {
             throw new RuntimeException("IO exception", e);
         }
     }
 
-    private static int countLines(String fileName) {
-        int lines = 0;
-        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
-            while (reader.readLine() != null) {
-                lines++;
+    private static String createReport(String data) {
+
+        int supply = 0;
+        int buy = 0;
+        int result;
+
+        StringBuilder report = new StringBuilder();
+        String[] split = data.split(",");
+
+        for (int i = 0; i < split.length; i += 2) {
+            if (split[i] != null) {
+                if (split[i].equals("supply")) {
+                    supply += Integer.parseInt(split[i + 1]);
+                } else if (split[i].equals("buy")) {
+                    buy += Integer.parseInt(split[i + 1]);
+                }
             }
-        } catch (IOException e) {
-            throw new RuntimeException("Cannot open file", e);
         }
-        return lines;
+
+        result = supply - buy;
+
+        report.append("supply,").append(supply).append(System.lineSeparator()).append("buy,")
+                .append(buy).append(System.lineSeparator()).append("result,").append(result);
+
+        return report.toString();
     }
 }
