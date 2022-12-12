1e1e8e94aff081d2e0f0c3a1adae4ae5436138f2
Ihor Vynogradskyi <avassorv@gmail.com>
2022-12-12 15:57:01 +0200

    first changes to project

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -9,47 +9,53 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class WorkWithFile {
+    static final int INDEX_OF_WORD = 0;
+    static final int INDEX_OF_NUMBER = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        String stringList = getCalculatedResult(getListFromFile(new File(fromFileName)));
-        try {
-            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName));
-            bufferedWriter.write(stringList);
-            bufferedWriter.close();
-        } catch (IOException e) {
-            throw new RuntimeException("Exception 2", e);
-        }
+        writeToFile(fromFileName, toFileName);
     }
 
-    public static ArrayList<String[]> getListFromFile(File file) {
+    public ArrayList<String[]> getListFromFile(File file) {
         String line;
-        ArrayList<String[]> lineList = new ArrayList<>();
+        ArrayList<String[]> lines = new ArrayList<>();
         try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
             while ((line = bufferedReader.readLine()) != null) {
-                lineList.add(line.split(","));
+                lines.add(line.split(","));
             }
-            return lineList;
+            return lines;
         } catch (IOException e) {
             throw new RuntimeException("Exception 1", e);
         }
     }
 
-    public static String getCalculatedResult(ArrayList<String[]> arrayList) {
+    public String getCalculatedResult(ArrayList<String[]> arrayList) {
         int supply = 0;
         int buy = 0;
         StringBuilder stringBuilder = new StringBuilder();
         for (String[] list: arrayList) {
-            if (list[0].equals("supply")) {
-                supply += Integer.parseInt(list[1].trim());
+            if (list[INDEX_OF_WORD].equals("supply")) {
+                supply += Integer.parseInt(list[INDEX_OF_NUMBER].trim());
             }
-            if (list[0].equals("buy")) {
-                buy += Integer.parseInt(list[1].trim());
+            if (list[INDEX_OF_WORD].equals("buy")) {
+                buy += Integer.parseInt(list[INDEX_OF_NUMBER].trim());
             }
         }
         stringBuilder.append("supply,").append(supply).append(System.lineSeparator())
                 .append("buy,").append(buy).append(System.lineSeparator())
                 .append("result,").append(supply - buy);
         return stringBuilder.toString();
+    }
+
+    public void writeToFile(String from, String to) {
+        String stringList = getCalculatedResult(getListFromFile(new File(from)));
+        try {
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(to));
+            bufferedWriter.write(stringList);
+            bufferedWriter.close();
+        } catch (IOException e) {
+            throw new RuntimeException("Exception 2", e);
+        }
     }
 }
