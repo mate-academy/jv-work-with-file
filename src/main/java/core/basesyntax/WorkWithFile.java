e50c15509734a2de4965f2cefcc0b5fbb4b9dbc2
Алексей <m.leshka955@gmail.com>
2022-12-14 01:31:31 +0200

    implemented three methods: getStatistics readFile count writer

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,8 +1,65 @@
 package core.basesyntax;
 
+import java.io.*;
+
 public class WorkWithFile {
 
-    public void getStatistic(String fromFileName, String toFileName) {
+    private String fileName;
+    private String toFileName;
+    private int supply;
+    private int buy;
+    private int result;
 
+    public void getStatistic(String fromFileName, String toFileName) {
+        this.fileName = fromFileName;
+        this.toFileName = toFileName;
+        readFile(fromFileName);
+        writer();
+    }
+
+    public String[] readFile(String fileName) {
+        StringBuilder stringBuilder = new StringBuilder();
+        String[] valueFile;
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String elementsInFile = reader.readLine();
+            while (elementsInFile != null) {
+                stringBuilder.append(elementsInFile).append(System.lineSeparator());
+                elementsInFile = reader.readLine();
+            }
+            valueFile = stringBuilder.toString().split("\\W+");
+            return valueFile;
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
+    }
+
+    public int count() {
+        String[] strings = readFile(fileName);
+        for (int i = 0; i < strings.length; i++) {
+            if (strings[i].equals("supply")) {
+                supply += Integer.parseInt(strings[i + 1]);
+            } else if (strings[i].equals("buy")) {
+                buy += Integer.parseInt(strings[i + 1]);
+            }
+        }
+        return result = supply - buy;
+    }
+
+    public void writer() {
+        count();
+        File file = new File(toFileName);
+        StringBuilder stringBuilder = new StringBuilder("supply,")
+                .append(this.supply)
+                .append(System.lineSeparator())
+                .append("buy,")
+                .append(buy)
+                .append(System.lineSeparator())
+                .append("result,")
+                .append(result);
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file, true))) {
+            writer.write(stringBuilder.toString());
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write file", e);
+        }
     }
 }
