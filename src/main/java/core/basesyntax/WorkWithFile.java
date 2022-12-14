d6873b865502a0627ce8310598cfb2f1ca18da67
Mykola Radzivon <kolian201497@gmail.com>
2022-12-14 13:35:37 +0200

    Added a file check fixed

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -3,45 +3,42 @@ package core.basesyntax;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
+import java.util.List;
 
 public class WorkWithFile {
     private static final String SUPPLY = "supply";
     private static final String BUY = "buy";
+    private static final String DIFFERENCE = "result";
+    private static final int RESULT_ARRAY_SIZE = 3;
+    private static final int INDEX_SUPPLY = 0;
+    private static final int INDEX_BUY = 1;
+    private static final int INDEX_RESULT = 2;
     private static final int INDEX_NAMES = 0;
     private static final int INDEX_PRICE = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        try {
-            Files.deleteIfExists(Path.of(toFileName));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
         File inputFile = new File(fromFileName);
         File outputFile = new File(toFileName);
-        String[] array = getStringFile(inputFile).split("\r\n");
-        try {
-            outputFile.createNewFile();
-        } catch (IOException e) {
-            throw new RuntimeException();
-        }
-        writeToFile(getCountResult(array), outputFile);
+        String[] resultData = getCountResult(getStringFile(inputFile));
+        writeToFile(resultData, outputFile);
     }
 
-    private String getStringFile(File file) {
-        StringBuilder builder = new StringBuilder();
+    private List<String> getStringFile(File file) {
         try {
-            builder.append(Files.readString(file.toPath()));
+            return (Files.readAllLines(file.toPath()));
         } catch (IOException e) {
             throw new RuntimeException("Can't read file " + e);
         }
-        return builder.toString();
     }
 
     private void writeToFile(String[] data, File fileName) {
+        if (fileName.exists()) {
+            fileName.delete();
+        }
         for (String str : data) {
             try {
+                fileName.createNewFile();
                 Files.write(fileName.toPath(), (str + System.lineSeparator()).getBytes(),
                         StandardOpenOption.APPEND);
             } catch (IOException e) {
@@ -50,8 +47,8 @@ public class WorkWithFile {
         }
     }
 
-    private String[] getCountResult(String[] array) {
-        String[] result = new String[3];
+    private String[] getCountResult(List<String> array) {
+        String[] result = new String[RESULT_ARRAY_SIZE];
         int sumSupply = 0;
         int sumBuy = 0;
         for (String s : array) {
@@ -63,9 +60,9 @@ public class WorkWithFile {
                 sumBuy += Integer.parseInt(data[INDEX_PRICE]);
             }
         }
-        result[0] = "supply," + sumSupply;
-        result[1] = "buy," + sumBuy;
-        result[2] = "result," + (sumSupply - sumBuy);
+        result[INDEX_SUPPLY] = SUPPLY + "," + sumSupply;
+        result[INDEX_BUY] = BUY + "," + sumBuy;
+        result[INDEX_RESULT] = DIFFERENCE + "," + (sumSupply - sumBuy);
         return result;
     }
 }
