b40e43adf63a795738fdbe5f6f7007a1f5bdd558
Polina Lysytska <nikolavna100500@gmail.com>
2022-12-14 22:41:15 +0200

    requested and suggested changes were implemented

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -5,43 +5,38 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.List;
 
 public class WorkWithFile {
+    private static final int INDEX_OF_OPERATION_TYPE_VALUE = 0;
+    private static final int INDEX_OF_AMOUNT_VALUE = 1;
+
     public void getStatistic(String fromFileName, String toFileName) {
-        String[] result = countData(dataGetter(fromFileName));
+        String[] result = generateResultStatistic(readDataFromFile(fromFileName));
         File file = new File(toFileName);
         if (file.exists()) {
             file.delete();
         }
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file, true))) {
-            for (String each : result) {
-                bufferedWriter.write(each);
-                bufferedWriter.write(System.lineSeparator());
-            }
-        } catch (IOException e) {
-            throw new RuntimeException("Can't write data");
-        }
+        writeDataToFile(file, result);
     }
 
-    private Object[] dataGetter(String fromFileName) {
+    private Object[] readDataFromFile(String fromFileName) {
         File file = new File(fromFileName);
-        List<String> dataFromFile;
         try {
-            dataFromFile = Files.readAllLines(file.toPath());
+            return Files.readAllLines(file.toPath()).toArray();
         } catch (IOException e) {
             throw new RuntimeException("Can't read the file");
         }
-        return dataFromFile.toArray();
     }
 
-    private String[] countData(Object[] data) {
+    private String[] generateResultStatistic(Object[] data) {
         int sumOfSupply = 0;
         int sumOfBuy = 0;
         for (Object each : data) {
             String[] splittedData = each.toString().split(",");
-            sumOfSupply += splittedData[0].equals("supply") ? Integer.parseInt(splittedData[1]) : 0;
-            sumOfBuy += splittedData[0].equals("buy") ? Integer.parseInt(splittedData[1]) : 0;
+            sumOfSupply += splittedData[INDEX_OF_OPERATION_TYPE_VALUE].equals("supply")
+                    ? Integer.parseInt(splittedData[INDEX_OF_AMOUNT_VALUE]) : 0;
+            sumOfBuy += splittedData[INDEX_OF_OPERATION_TYPE_VALUE].equals("buy")
+                    ? Integer.parseInt(splittedData[INDEX_OF_AMOUNT_VALUE]) : 0;
         }
         int result = sumOfSupply - sumOfBuy;
         StringBuilder stringBuilder = new StringBuilder("supply,");
@@ -49,5 +44,16 @@ public class WorkWithFile {
                 .append("buy,").append(sumOfBuy).append("_")
                 .append("result,").append(result);
         return stringBuilder.toString().split("_");
+    }
+
+    private void writeDataToFile(File file, String[] result) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file, true))) {
+            for (String line : result) {
+                bufferedWriter.write(line);
+                bufferedWriter.write(System.lineSeparator());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write data");
+        }
     }
 }
