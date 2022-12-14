434b99c3c2198f5fe558b7d3f77d2a8bee0e942f
Artem Voitiuk <artemon2504@gmail.com>
2022-12-14 15:19:26 +0200

    Artem Voitiuk work with file homework

---
+++ src/main/java/core/basesyntax/ReadData.java
@@ -0,0 +1,24 @@
+package core.basesyntax;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadData {
+
+    public String readFile(String fromFileName) {
+        File file = new File(fromFileName);
+        StringBuilder builder = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
+            String data = reader.readLine();
+            while (data != null) {
+                builder.append(data).append(System.lineSeparator());
+                data = reader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t read file " + e);
+        }
+        return builder.toString();
+    }
+}
--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,48 @@
 package core.basesyntax;
 
-public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class WorkWithFile {
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        WorkWithFile workWithFile = new WorkWithFile();
+        try {
+            File file = new File(toFileName);
+            file.createNewFile();
+        } catch (IOException e) {
+            throw new RuntimeException("Error");
+        }
+        String report = workWithFile.createReport(fromFileName);
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
+            writer.write(report);
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t write to file");
+        }
+    }
+
+    public String createReport(String fromFile) {
+        ReadData readData = new ReadData();
+        String readFile = readData.readFile(fromFile);
+        String[] array = readFile.split(System.lineSeparator());
+        int supply = 0;
+        int buy = 0;
+        for (int i = 0; i < array.length; i++) {
+            int index = array[i].indexOf(",");
+            if (array[i].substring(0, index).equals("supply")) {
+                supply += Integer.parseInt(array[i].substring(index + 1, array[i].length()));
+            } else if (array[i]
+                    .substring(0, index).equals("buy")) {
+                buy += Integer.parseInt(array[i].substring(index + 1, array[i].length()));
+            }
+        }
+        int result = supply - buy;
+        StringBuilder builder = new StringBuilder("supply");
+        builder.append(",").append(supply).append(System.lineSeparator())
+                .append("buy").append(",").append(buy).append(System.lineSeparator())
+                .append("result").append(",").append(result);
+        return builder.toString();
     }
 }
