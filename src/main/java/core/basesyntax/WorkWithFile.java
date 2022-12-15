9260a0920b661191c2b1b6c60a95ed153a88e842
Serhii Berdychevskyi <sirko120690@mail.com>
2022-12-16 01:54:20 +0200

    Added first version hw jv-work-with-file

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,72 @@
 package core.basesyntax;
 
-public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class WorkWithFile {
+    private static String RESULT = "result";
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+    private static final int OPERATIONS_INDEX = 0;
+    private static final int OPERATIONS_INDEX_VALUE = 1;
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        String readData = readFromFile(fromFileName);
+        String resultReport = createReport(readData);
+        writeFile(toFileName, resultReport);
+    }
+
+    private String readFromFile(String fromFileName) {
+        File fromFile = new File(fromFileName);
+        StringBuilder stringBuilder;
+        int value;
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFile))) {
+            stringBuilder = new StringBuilder();
+            value = bufferedReader.read();
+            while (value != -1) {
+                stringBuilder.append((char) value);
+                value = bufferedReader.read();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file" + e);
+        }
+        return stringBuilder.toString();
+    }
+
+    private void writeFile(String toFileName, String resultReport) {
+        File toFile = new File(toFileName);
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFile))) {
+            bufferedWriter.write(resultReport);
+            bufferedWriter.flush();
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write file" + e);
+        }
+    }
+
+    private String createReport(String fromFileName) {
+        StringBuilder resultString = new StringBuilder();
+        String[] lines = fromFileName.split(System.lineSeparator());
+        int buyCounter = 0;
+        int supplyCounter = 0;
+        for (int i = 0; i < lines.length; i++) {
+            String[] operations = lines[i].split(",");
+            if (operations[OPERATIONS_INDEX].equals(SUPPLY)) {
+                supplyCounter += Integer.parseInt(operations[OPERATIONS_INDEX_VALUE]);
+            }
+            if (operations[OPERATIONS_INDEX].equals(BUY)) {
+                buyCounter += Integer.parseInt(operations[OPERATIONS_INDEX_VALUE]);
+            }
+        }
+        int result = supplyCounter - buyCounter;
+        return resultString.append(SUPPLY).append(",").append(supplyCounter)
+                .append(System.lineSeparator())
+                .append(BUY).append(",")
+                .append(buyCounter)
+                .append(System.lineSeparator())
+                .append(RESULT).append(",").append(result).toString();
     }
 }
