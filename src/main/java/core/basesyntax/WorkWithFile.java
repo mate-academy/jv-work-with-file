dc2fd1d4b28f8a064dcee924b19196bfc4a64bba
Butovych <dreamextreme29@gmail.com>
2022-12-15 17:45:18 +0200

    Check this mistake

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -13,9 +13,13 @@ public class WorkWithFile {
     private static final String LINE_SEPARATOR = System.lineSeparator();
 
     public void getStatistic(String fromFileName, String toFileName) {
-        File fileForRead = new File(fromFileName);
+        String setStatistic = readFile(fromFileName);
+        String getStat = setStatistic(setStatistic);
+        writeStatistic(toFileName, forWright);
+    }
+    private static String readFile (String fromFileName) {
         StringBuilder firstBuilder = new StringBuilder();
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileForRead))) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             String value = bufferedReader.readLine();
             while (value != null) {
                 firstBuilder.append(value).append(" ");
@@ -24,9 +28,13 @@ public class WorkWithFile {
         } catch (IOException e) {
             throw new RuntimeException("Can't read this file", e);
         }
+        return firstBuilder.toString();
+    }
+
+    private  static String setStatistic (String value) {
         int countSupply = 0;
         int countBuy = 0;
-        String [] amounts = firstBuilder.toString().split(" ");
+        String[] amounts = value.split(" ");
         for (String amount : amounts) {
             if (amount.contains("supply")) {
                 countSupply += Integer.parseInt(amount.substring(INDEX_OF_SUPPLY_LENGTH,
@@ -39,10 +47,13 @@ public class WorkWithFile {
         String forWright = "supply," + countSupply + LINE_SEPARATOR
                 + "buy," + countBuy + LINE_SEPARATOR
                 + "result," + (countSupply - countBuy) + LINE_SEPARATOR;
+        return forWright;
+    }
+
+    private static void writeStatistic (String toFileName, String forWright) {
         File fileForWright = new File(toFileName);
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileForWright))) {
             bufferedWriter.write(forWright);
-            bufferedWriter.flush();
         } catch (IOException e) {
             throw new RuntimeException("We can't write this file", e);
         }
