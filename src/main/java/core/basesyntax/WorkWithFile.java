221a388bca95e8a97949878c379e0603661042ad
Your NameMykola <mykola.osolinskyi@gmail.com>
2022-12-15 13:25:10 +0100

    solution after 1st mentor's review

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,54 +1,49 @@
 package core.basesyntax;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileReader;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
 
 public class WorkWithFile {
     private static final String SUPPLY = "supply";
     private static final String BUY = "buy";
     private static final String RESULT = "result";
+    private static final String DELIMETER = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        String[] dataFromFile = readFromFile(fromFileName).split(" ");
+        List<String> dataFromFile = readFromFile(new File(fromFileName));
         String report = createReport(dataFromFile);
         writeToFile(report, toFileName);
     }
 
-    private String readFromFile(String fromFileName) {
-        StringBuilder builder = new StringBuilder();
-        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
-            String value = reader.readLine();
-            while (value != null) {
-                builder.append(value).append(" ");
-                value = reader.readLine();
-            }
+    private List<String> readFromFile(File file) {
+        try {
+            return Files.readAllLines(file.toPath());
         } catch (IOException ex) {
-            throw new RuntimeException("Cant read file" + fromFileName, ex);
+            throw new RuntimeException("Can't read file", ex);
         }
-        return builder.toString();
     }
 
-    private String createReport(String[] dataFromFile) {
+    private String createReport(List<String> dataFromFile) {
         int supply = 0;
         int buy = 0;
-        int result = 0;
         for (String line : dataFromFile) {
+            int positionValue = Integer.parseInt(line.substring(line.indexOf(',') + 1));
             if (line.substring(0, line.indexOf(',')).equals(SUPPLY)) {
-                supply += Integer.parseInt(line.substring(line.indexOf(',') + 1));
+                supply += positionValue;
             }
             if (line.substring(0, line.indexOf(',')).equals(BUY)) {
-                buy += Integer.parseInt(line.substring(line.indexOf(',') + 1));
+                buy += positionValue;
             }
         }
-        result = supply - buy;
-        StringBuilder reportBuilder = new StringBuilder();
-        return reportBuilder.append(SUPPLY).append(",")
-                .append(supply).append(System.lineSeparator())
-                .append(BUY).append(",").append(buy).append(System.lineSeparator())
-                .append(RESULT).append(",").append(result).toString();
+        int result = supply - buy;
+        String reportBuilder = SUPPLY + DELIMETER + supply + System.lineSeparator()
+                + BUY + DELIMETER + buy + System.lineSeparator()
+                + RESULT + DELIMETER + result;
+        return reportBuilder;
     }
 
     private void writeToFile(String report, String toFileName) {
