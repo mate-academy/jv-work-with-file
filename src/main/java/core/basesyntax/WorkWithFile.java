2a6b7980514373c49e9fb1b830fc4b7d3a39d159
Oleksandr Kostiukevych <kaasnake2@gmail.com>
2022-12-15 17:48:53 +0200

    jv-work-with-file 2nd commit

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,10 +1,12 @@
 package core.basesyntax;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class WorkWithFile {
     private static final String FIRST_PARAMETER = "supply";
@@ -13,43 +15,36 @@ public class WorkWithFile {
     private static final String SEPARATOR = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        String stringsFromFile = readFromFile(fromFileName);
+        List<String> stringsFromFile = readFromFile(fromFileName);
         String report = createReport(stringsFromFile);
         writeReport(report, toFileName);
     }
 
-    private String readFromFile(String fileName) {
-        StringBuilder stringBuilder = new StringBuilder();
+    private List<String> readFromFile(String fileName) {
+        Path path = Paths.get(fileName);
         try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
-
-            String value = bufferedReader.readLine();
-            while (value != null) {
-                stringBuilder.append(value).append(';');
-                value = bufferedReader.readLine();
-            }
-            return stringBuilder.toString();
+            return Files.readAllLines(path);
         } catch (IOException e) {
             throw new RuntimeException("Can't read file " + fileName + ".", e);
         }
     }
 
-    private String createReport(String readFromFile) {
-        StringBuilder stringBuilder = new StringBuilder();
-        String[] splitter = readFromFile.split(";");
+    private String createReport(List<String> readFromFile) {
         int supplySum = 0;
         int buySum = 0;
-        for (String element : splitter) {
-            supplySum += element.charAt(0) == FIRST_PARAMETER.charAt(0) ? Integer.parseInt(element
-                    .substring(FIRST_PARAMETER.length() + 1)) : 0;
-            buySum += element.charAt(0) == SECOND_PARAMETER.charAt(0) ? Integer.parseInt(element
-                    .substring(SECOND_PARAMETER.length() + 1)) : 0;
+        for (String element : readFromFile) {
+            if (element.charAt(0) == FIRST_PARAMETER.charAt(0)) {
+                supplySum += Integer.parseInt(element
+                    .substring(FIRST_PARAMETER.length() + 1));
+            }
+            if (element.charAt(0) == SECOND_PARAMETER.charAt(0)) {
+                buySum += Integer.parseInt(element
+                    .substring(SECOND_PARAMETER.length() + 1));
+            }
         }
-        return stringBuilder.append(FIRST_PARAMETER).append(SEPARATOR).append(supplySum)
-                .append(System.lineSeparator())
-                .append(SECOND_PARAMETER).append(SEPARATOR).append(buySum)
-                .append(System.lineSeparator())
-                .append(THIRD_PARAMETER).append(SEPARATOR).append(supplySum - buySum).toString();
+        return FIRST_PARAMETER + SEPARATOR + supplySum + System.lineSeparator()
+            + SECOND_PARAMETER + SEPARATOR + buySum + System.lineSeparator()
+            + THIRD_PARAMETER + SEPARATOR + (supplySum - buySum);
     }
 
     private void writeReport(String report, String fileName) {
@@ -60,3 +55,4 @@ public class WorkWithFile {
         }
     }
 }
+
