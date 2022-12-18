1f51f740e79a72ff7b29dd0a7c8ed99507c19371
Richard <richarddkochut@gmail.com>
2022-12-18 16:37:15 +0200

    separetad methods,readFromFile by ListString

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -3,72 +3,89 @@ package core.basesyntax;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WorkWithFile {
-    static final String SUPPLY = "supply,";
-    static final String BUY = "buy,";
-    static final String RESULT = "result,";
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+    private static final String RESULT = "result";
+
+    private static final String SEPARATOR = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        File fromMyFile = new File(fromFileName);
-        File toMyFile = new File(toFileName);
-        try {
-            FileReader fileReader = new FileReader(fromMyFile);
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-            String report = createReport(bufferedReader);
+        writeToFile(createReport(readFromFile(fromFileName)), toFileName);
+    }
 
-            FileWriter fileWriter = new FileWriter(toMyFile);
-            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-            createFile(bufferedWriter, report);
+    private List<String> readFromFile(String fromMyFileName) {
+        File fromMyFile = new File(fromMyFileName);
+        FileReader fileReader = null;
+        try {
+            fileReader = new FileReader(fromMyFile);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        String value = null;
+
+        try {
+            value = bufferedReader.readLine();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+        List<String> list = new ArrayList<>();
+        while (value != null) {
+            list.add(value);
+            try {
+                value = bufferedReader.readLine();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return list;
     }
 
-    private String createReport(BufferedReader reader) {
+    private String createReport(List<String> readFromFile) {
         StringBuilder resultBuilder = new StringBuilder();
         int supplySum = 0;
         int buySum = 0;
         int resultSum;
 
-        try {
-            StringBuilder stringBuilder = new StringBuilder();
-            String value = reader.readLine();
-            while (value != null) {
-                stringBuilder.append(value).append(System.lineSeparator());
-                value = reader.readLine();
-            }
-
-            String[] filter = stringBuilder.toString().split(System.lineSeparator());
-            int indexSupply = 7;
-            int indeBuy = 4;
-            for (String result : filter) {
+        for (String result : readFromFile) {
+            if (result != null) {
                 if (result.startsWith(SUPPLY)) {
-                    supplySum += Integer.parseInt(result.substring(indexSupply));
+                    supplySum += Integer.parseInt(result.substring(SUPPLY.length() + 1));
                 }
                 if (result.startsWith(BUY)) {
-                    buySum += Integer.parseInt(result.substring(indeBuy));
+                    buySum += Integer.parseInt(result.substring(BUY.length() + 1));
                 }
             }
-            resultSum = supplySum - buySum;
+        }
+        resultSum = supplySum - buySum;
+        return resultBuilder
+                .append(SUPPLY).append(SEPARATOR)
+                .append(supplySum)
+                .append(System.lineSeparator())
+                .append(BUY).append(SEPARATOR).append(buySum)
+                .append(System.lineSeparator()).append(RESULT).append(SEPARATOR)
+                .append(resultSum).toString();
+    }
 
-            return resultBuilder
-                    .append(SUPPLY)
-                    .append(supplySum)
-                    .append(System.lineSeparator())
-                    .append(BUY).append(buySum)
-                    .append(System.lineSeparator()).append(RESULT)
-                    .append(resultSum).toString();
+    private void writeToFile(String report, String toFileName) {
+        File toMyFile = new File(toFileName);
+        FileWriter fileWriter = null;
+        try {
+            fileWriter = new FileWriter(toMyFile);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-    }
-
-    private void createFile(BufferedWriter bufferedWriter, String report) {
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
         try (bufferedWriter) {
+
             bufferedWriter.write(report);
 
         } catch (IOException e) {
