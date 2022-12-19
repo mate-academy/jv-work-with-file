53559a7c99239e3fbde24bd723948db68e9b81b6
Denis Pugovkin <den.pugovkin@gmail.com>
2022-12-19 22:18:18 +0200

    1. Method "getStatistic"  was divided on small more readable methods. 2. Added name value to messages about exceptions.

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -15,39 +15,50 @@ public class WorkWithFile {
     private static final int VALUE = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        File file = new File(fromFileName);
-        StringBuilder builder = new StringBuilder();
-        try {
-            List<String> strings = Files.readAllLines(file.toPath());
-            int sumBuy = 0;
-            int sumSupply = 0;
-            for (String string : strings) {
-                String[] values = string.split(",");
-                if (values[OPERATION].equals(BUY)) {
-                    sumBuy = sumBuy + Integer.parseInt(values[VALUE]);
-                } else {
-                    sumSupply = sumSupply + Integer.parseInt(values[VALUE]);
-                }
-            }
-            int sumResult = sumSupply - sumBuy;
-            builder.append(SUPPLY)
-                    .append(",")
-                    .append(sumSupply)
-                    .append(System.lineSeparator())
-                    .append(BUY).append(",")
-                    .append(sumBuy)
-                    .append(System.lineSeparator())
-                    .append(RESULT)
-                    .append(",")
-                    .append(sumResult);
+        List<String> dataFromFile = readFromFile(fromFileName);
+        String report = createReport(dataFromFile);
+        writetoFile(report, toFileName);
+    }
 
+    private List<String> readFromFile(String fromFileName) {
+        try {
+            File file = new File(fromFileName);
+            return Files.readAllLines(file.toPath());
         } catch (IOException e) {
-            throw new RuntimeException("Can't read file", e);
+            throw new RuntimeException("Can't read file" + fromFileName, e);
         }
+    }
+
+    private String createReport(List<String> readFromFile) {
+        StringBuilder builder = new StringBuilder();
+        int sumBuy = 0;
+        int sumSupply = 0;
+        for (String string : readFromFile) {
+            String[] values = string.split(",");
+            if (values[OPERATION].equals(BUY)) {
+                sumBuy = sumBuy + Integer.parseInt(values[VALUE]);
+            } else {
+                sumSupply = sumSupply + Integer.parseInt(values[VALUE]);
+            }
+        }
+        int sumResult = sumSupply - sumBuy;
+        return builder.append(SUPPLY)
+                .append(",")
+                .append(sumSupply)
+                .append(System.lineSeparator())
+                .append(BUY).append(",")
+                .append(sumBuy)
+                .append(System.lineSeparator())
+                .append(RESULT)
+                .append(",")
+                .append(sumResult).toString();
+    }
+
+    private void writetoFile(String report, String toFileName) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
-            bufferedWriter.write(builder.toString());
+            bufferedWriter.write(report);
         } catch (IOException e) {
-            throw new RuntimeException("Can't write data to file", e);
+            throw new RuntimeException("Can't write data to file" + toFileName, e);
         }
     }
 }
