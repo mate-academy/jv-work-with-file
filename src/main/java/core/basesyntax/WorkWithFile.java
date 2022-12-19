f11f9ef57f5c3032496868b2a48567d6032ce2c8
Andrei_Bondarenko <bondorol007@gmail.com>
2022-12-19 22:52:31 +0200

    commit1

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,14 +1,14 @@
-
 package core.basesyntax;
 
-import java.io.BufferedWriter;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+
     public void getStatistic(String fromFileName, String toFileName) {
         int supply = 0;
         int buy = 0;
