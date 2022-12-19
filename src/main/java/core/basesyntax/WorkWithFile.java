b1b856d3485b83fc3bc9f1ad6c3dd3250d863524
Andrei_Bondarenko <bondorol007@gmail.com>
2022-12-19 22:25:28 +0200

    commit1

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,49 @@
 package core.basesyntax;
 
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
-
+        int supply = 0;
+        int buy = 0;
+        int result = 0;
+        File file = new File(fromFileName);
+        StringBuilder builder = new StringBuilder();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String value = reader.readLine();
+            while (value != null) {
+                builder.append(value).append(" ");
+                value = reader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
+        String[] splitBuilder = builder.toString().split(" ");
+        for (int i = 0; i < splitBuilder.length; i++) {
+            String[] splitBuilderArray = splitBuilder[i].split(",");
+            if (splitBuilderArray[0].equals("supply")) {
+                supply = supply + Integer.parseInt(splitBuilderArray[1]);
+            } else {
+                buy = buy + Integer.parseInt(splitBuilderArray[1]);
+            }
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        result = supply - buy;
+        stringBuilder.append("supply").append(",").append(supply).append(System.lineSeparator())
+                .append("buy").append(",").append(buy)
+                .append(System.lineSeparator()).append("result,").append(result);
+        System.out.println(stringBuilder.toString());
+        File newFile = new File(toFileName);
+        try( BufferedWriter writer = new BufferedWriter(new FileWriter(newFile))) {
+            writer.write(stringBuilder.toString());
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
     }
 }
