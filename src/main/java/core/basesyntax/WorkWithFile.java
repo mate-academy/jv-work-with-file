412d4c796ce80a1d0c8f33aefe3a146879dbb81b
Vlad Polikarpov <vladpolikarpovvp@gmail.com>
2022-12-21 19:44:55 +0200

    fixed code complexity

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -6,81 +6,68 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
-import java.nio.file.StandardOpenOption;
 
 public class WorkWithFile {
-
-    public void writeToFile(File resultFile, int supplySum, int buySum) {
+    private void writeToFile(String reportString, String toFileName) {
         try {
-            Files.write(resultFile.toPath(), "supply,".getBytes(),
-                    StandardOpenOption.APPEND);
-            Files.write(resultFile.toPath(), Integer.toString(supplySum).getBytes(),
-                    StandardOpenOption.APPEND);
-            Files.write(resultFile.toPath(), (System.lineSeparator() + "buy,").getBytes(),
-                    StandardOpenOption.APPEND);
-            Files.write(resultFile.toPath(), Integer.toString(buySum).getBytes(),
-                    StandardOpenOption.APPEND);
-            Files.write(resultFile.toPath(), (System.lineSeparator() + "result,").getBytes(),
-                    StandardOpenOption.APPEND);
-            Files.write(resultFile.toPath(), Integer.toString(supplySum - buySum).getBytes(),
-                    StandardOpenOption.APPEND);
+            PrintWriter writer = new PrintWriter(toFileName);
+            writer.print("");
+            writer.close();
 
+            new File(toFileName).createNewFile();
+            Files.write(new File(toFileName).toPath(), reportString.getBytes());
         } catch (IOException e) {
             throw new RuntimeException("Can not write to this file");
         }
     }
 
-    public void readFromFile(StringBuilder startStringBuilder, File startFile) {
+    private String readFromFile(String fromFileName) {
+        StringBuilder startStringBuilder = new StringBuilder();
         try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(startFile));
+            BufferedReader
+                    bufferedReader = new BufferedReader(new FileReader(new File(fromFileName)));
             int value = bufferedReader.read();
 
             while (value != -1) {
                 startStringBuilder.append((char) value);
                 value = bufferedReader.read();
             }
+            return startStringBuilder.toString();
         } catch (IOException e) {
             throw new RuntimeException("Can not read from this file");
         }
     }
-
-    public void getStatistic(String fromFileName, String toFileName) {
-        File startFile = new File(fromFileName);
-        File resultFile = new File(toFileName);
-        StringBuilder startStringBuilder = new StringBuilder();
+    
+    private String createReport(String dataFromFile) {
         String[] tmpArray;
         int supplySum = 0;
         int buySum = 0;
 
-        try {
-            resultFile.createNewFile();
-        } catch (IOException e) {
-            throw new RuntimeException("Can not create this file", e);
-        }
+        tmpArray = new String[dataFromFile.length()];
+        tmpArray = dataFromFile.split(",|\\n");
 
-        try {
-            readFromFile(startStringBuilder, startFile);
-
-            tmpArray = new String[startStringBuilder.length()];
-            tmpArray = startStringBuilder.toString().split(",|\\n");
-
-            for (int i = 0;i < tmpArray.length;i++) {
-                if (tmpArray[i].equals("supply")) {
-                    supplySum += Integer.parseInt(tmpArray[i + 1]);
-                }
-
-                if (tmpArray[i].equals("buy")) {
-                    buySum += Integer.parseInt(tmpArray[i + 1]);
-                }
+        for (int i = 0;i < tmpArray.length;i++) {
+            if (tmpArray[i].equals("supply")) {
+                supplySum += Integer.parseInt(tmpArray[i + 1]);
             }
 
-            PrintWriter writer = new PrintWriter(resultFile);
-            writer.print("");
-            writer.close();
-
-            writeToFile(resultFile, supplySum, buySum);
-        } catch (IOException e) {
-            throw new RuntimeException("Can not work with these files", e);
+            if (tmpArray[i].equals("buy")) {
+                buySum += Integer.parseInt(tmpArray[i + 1]);
+            }
         }
+
+        String reportString;
+
+        reportString = "supply," + supplySum + System.lineSeparator()
+                + "buy," + buySum + System.lineSeparator()
+                + "result," + (supplySum - buySum);
+
+        return reportString;
+    }
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        String dataFromFile = readFromFile(fromFileName);
+        String report = createReport(dataFromFile);
+        writeToFile(report, toFileName);
     }
 }
