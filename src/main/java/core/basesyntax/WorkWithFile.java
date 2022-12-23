b5f81be98b2a2c64c243d3651aa9f0db2b3c4831
chumpaq <119128499+chumpaq@users.noreply.github.com>
2022-12-23 20:29:33 +0200

    Update WorkWithFile.java

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -34,7 +34,6 @@ public class WorkWithFile {
         }
         return linesBuilder.toString();
     }
-
     private static String getReport(String lines) {
         StringBuilder reportBuilder = new StringBuilder();
         String[] splittedLines = lines.split(" ");
