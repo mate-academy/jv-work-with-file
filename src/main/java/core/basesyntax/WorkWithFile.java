75a8faa974308f9dce6772d9031c3ef13b75d809
Mik911 <911Sokol@gmail.com>
2023-01-04 21:32:00 +0200

    corrected decision 04.01.22 - 21-27 ch2

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,44 +2,33 @@ package core.basesyntax;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
-        String rezalt = readFile(fromFileName);
-        writeFile(toFileName, rezalt);
+
+        List dataFromFile = readFile(fromFileName);
+        String report = createReport(dataFromFile);
+        writeFile(report, toFileName);
     }
 
-    private String readFile(String fromFileName) {
-        StringBuilder builder = new StringBuilder();
+    private List readFile(String fromFileName) {
+        List<String> list = new ArrayList<>();
         BufferedReader bufferedReader = null;
         try {
             bufferedReader = new BufferedReader(new FileReader(fromFileName));
             String str = bufferedReader.readLine();
             if (str == null || str.length() == 0) {
-                return "";
+                return list;
             }
-            int sumBuy = 0;
-            int sumSupply = 0;
-            int result;
             while (str != null) {
-                String[] strSplit = str.split(",");
-                if (strSplit[0].length() > 0) {
-                    if (strSplit[0].equals("supply")) {
-                        sumSupply += Integer.parseInt(strSplit[1]);
-                    } else {
-                        sumBuy += Integer.parseInt(strSplit[1]);
-                    }
-                }
+                list.add(str);
                 str = bufferedReader.readLine();
             }
-            result = sumSupply - sumBuy;
-            builder.append("supply,").append(sumSupply).append(System.lineSeparator());
-            builder.append("buy,").append(sumBuy).append(System.lineSeparator());
-            builder.append("result,").append(result);
         } catch (IOException e) {
             throw new RuntimeException("Can't read file", e);
         } finally {
@@ -51,31 +40,39 @@ public class WorkWithFile {
                 System.out.println("Error in closing the BufferedWriter" + ex);
             }
         }
+        return list;
+    }
+
+    private String createReport(List dataFromFile) {
+        final StringBuilder builder = new StringBuilder();
+        if (dataFromFile == null || dataFromFile.size() == 0) {
+            return "";
+        }
+        int sumBuy = 0;
+        int sumSupply = 0;
+        int result;
+        for (Object str : dataFromFile) {
+            String[] strSplit = ((String) str).split(",");
+            if (strSplit[0].length() > 0) {
+                if (strSplit[0].equals("supply")) {
+                    sumSupply += Integer.parseInt(strSplit[1]);
+                } else {
+                    sumBuy += Integer.parseInt(strSplit[1]);
+                }
+            }
+        }
+        result = sumSupply - sumBuy;
+        builder.append("supply,").append(sumSupply).append(System.lineSeparator());
+        builder.append("buy,").append(sumBuy).append(System.lineSeparator());
+        builder.append("result,").append(result);
         return builder.toString();
     }
 
-    private void writeFile(String toFileName, String rezalt) {
-        BufferedWriter bufferedWriter = null;
-        try {
-            File file = new File(toFileName);
-
-            if (!file.exists()) {
-                file.createNewFile();
-            }
-            FileWriter fw = new FileWriter(file);
-            bufferedWriter = new BufferedWriter(fw);
-            bufferedWriter.write(rezalt);
-
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        } finally {
-            try {
-                if (bufferedWriter != null) {
-                    bufferedWriter.close();
-                }
-            } catch (Exception ex) {
-                System.out.println("Error in closing the BufferedWriter" + ex);
-            }
+    private void writeFile(String report, String toFileName) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+            bufferedWriter.write(report);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write data to file " + toFileName, e);
         }
     }
 }
