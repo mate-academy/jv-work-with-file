22fc527c523f62edcceefee3dc2e5869c8a225c9
Serhii Kopervas <sergkopervas@gmail.com>
2023-01-08 18:22:22 +0100

    The commit dated 08.01.2023

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -6,45 +6,51 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    private final String[] result = {"supply", "buy", "result"};
+    static final int ZERO_INDEX = 0;
+    static final int FIRST_INDEX = 1;
+    static final int SECOND_INDEX = 2;
+    private final String[] optionList = {"supply", "buy", "result"};
     private final String mySeparetor = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        writeDataToFile(createReport(parseDataFromFile(fromFileName)), toFileName);
+        writeToFile(createReport(readFromFile(fromFileName)), toFileName);
     }
 
-    public int[] parseDataFromFile(String fileName) {
-        String[] records;
-        int supplySum = 0;
-        int buySum = 0;
-
+    public String readFromFile(String fileName) {
+        StringBuilder stringBuilder = new StringBuilder();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
             String line;
             while ((line = bufferedReader.readLine()) != null) {
-                records = line.split(mySeparetor);
-                if (records[0].equals(result[0])) {
-                    supplySum += Integer.parseInt(records[1]);
-                } else if (records[0].equals(result[1])) {
-                    buySum += Integer.parseInt(records[1]);
-                }
+                stringBuilder.append(line).append(System.lineSeparator());
             }
         } catch (IOException e) {
             throw new RuntimeException("Can't read file", e);
         }
-
-        return new int[]{supplySum, buySum};
+        return stringBuilder.toString();
     }
 
-    public String createReport(int[] parseResult) {
+    public String createReport(String inputString) {
+        String[] records = inputString.split(System.lineSeparator());
+        String[] result;
+        int supplySum = 0;
+        int buySum = 0;
         String tempStr = "";
-        tempStr += result[0] + mySeparetor + parseResult[0] + System.lineSeparator();
-        tempStr += result[1] + mySeparetor + parseResult[1] + System.lineSeparator();
-        tempStr += result[2] + mySeparetor + (parseResult[0] - parseResult[1])
+        for (String s : records) {
+            result = s.split(mySeparetor);
+            if (result[ZERO_INDEX].equals(optionList[ZERO_INDEX])) {
+                supplySum += Integer.parseInt(result[FIRST_INDEX]);
+            } else if (result[ZERO_INDEX].equals(optionList[FIRST_INDEX])) {
+                buySum += Integer.parseInt(result[FIRST_INDEX]);
+            }
+        }
+        tempStr += optionList[ZERO_INDEX] + mySeparetor + supplySum + System.lineSeparator();
+        tempStr += optionList[FIRST_INDEX] + mySeparetor + buySum + System.lineSeparator();
+        tempStr += optionList[SECOND_INDEX] + mySeparetor + (supplySum - buySum)
                 + System.lineSeparator();
         return tempStr;
     }
 
-    public void writeDataToFile(String report, String fileName) {
+    public void writeToFile(String report, String fileName) {
         try (FileWriter fileWriter = new FileWriter(fileName, false)) {
             fileWriter.append(report);
         } catch (IOException ex) {
