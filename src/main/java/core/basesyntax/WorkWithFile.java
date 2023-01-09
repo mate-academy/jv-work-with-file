ba221ad1ece33e46157051fdddff02737d2b509b
Igor Parovenko <igorparovenko@yahoo.com>
2023-01-09 16:11:31 +0100

    Files checked with C/L

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,42 +7,49 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    private static final String BUY = "buy";
-    private static final String SUPPLY = "supply";
-    private static final String RESULT = "result";
-    private static final String COMMA = ",";
-    private static final String READ_ERROR_MSG = "Error: file not found: ";
-    private static final String WRITE_ERROR_MSG = "Cant write data to file: ";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        int buyQuantity = 0;
-        int supplyQuantity = 0;
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
+        writeToFile(toFileName, createReport(readFromFile(fromFileName)));
+    }
+
+    private String readFromFile(String fromFileName) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
+            StringBuilder stringBuilder = new StringBuilder();
             String value = reader.readLine();
             while (value != null) {
-                if (BUY.equals(value.replaceAll("\\W\\d+", ""))) {
-                    buyQuantity += Integer.parseInt(value.replaceAll("\\D", ""));
-                }
-                if (SUPPLY.equals(value.replaceAll("\\W\\d+", ""))) {
-                    supplyQuantity += Integer.parseInt(value.replaceAll("\\D", ""));
-                }
+                stringBuilder.append(value).append(" ");
                 value = reader.readLine();
             }
+            return stringBuilder.toString();
         } catch (IOException e) {
-            throw new RuntimeException(READ_ERROR_MSG, e);
+            throw new RuntimeException("Error: file not found: ", e);
         }
+    }
+
+    private String createReport(String dataFromFile) {
+        int buyQuantity = 0;
+        int supplyQuantity = 0;
         StringBuilder stringBuilder = new StringBuilder();
-        String result = stringBuilder.append(SUPPLY).append(COMMA).append(supplyQuantity)
-                .append(System.lineSeparator()).append(BUY).append(COMMA).append(buyQuantity)
-                .append(System.lineSeparator()).append(RESULT).append(COMMA)
-                .append(supplyQuantity - buyQuantity).toString();
-        try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName));
+        String[] split = dataFromFile.split(" ");
+        for (String arrays:split) {
+            if ("buy".equals(arrays.replaceAll("\\W\\d+", ""))) {
+                buyQuantity += Integer.parseInt(arrays.replaceAll("\\D", ""));
+            }
+            if ("supply".equals(arrays.replaceAll("\\W\\d+", ""))) {
+                supplyQuantity += Integer.parseInt(arrays.replaceAll("\\D", ""));
+            }
+        }
+        return stringBuilder.append("supply").append(",").append(supplyQuantity)
+                .append(System.lineSeparator()).append("buy").append(",")
+                .append(buyQuantity).append(System.lineSeparator())
+                .append("result").append(",").append(supplyQuantity - buyQuantity).toString();
+    }
+
+    private void writeToFile(String toFileName, String result) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
             writer.write(result);
-            writer.close();
         } catch (IOException e) {
-            throw new RuntimeException(WRITE_ERROR_MSG, e);
+            throw new RuntimeException("Cant write data to file: ", e);
         }
     }
 }
