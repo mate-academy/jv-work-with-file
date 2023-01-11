52e84552ad0652f965c9ef021b9b84fec30a27aa
Kovaev <eugenkoval1010@gmail.com>
2023-01-11 18:23:52 +0200

    added fixes after review

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -9,52 +9,51 @@ import java.io.IOException;
 public class WorkWithFile {
 
     public void getStatistic(String fromFileName, String toFileName) {
-        writeFile(processingData(readFile(fromFileName)), toFileName);
+        String stringFromFile = readFile(fromFileName);
+        String resultString = generateReport(stringFromFile);
+        writeFile(resultString, toFileName);
     }
 
-    private String processingData(String stringFile) {
+    private String generateReport(String data) {
         final int numberAmount = 1;
-        StringBuilder stringBuilder;
         int buySum = 0;
         int supplySum = 0;
-        String[] inOrder = stringFile.split(System.lineSeparator());
-        String[] word;
-        for (String order: inOrder) {
-            word = order.split(",");
-            if (word[0].equals("buy")) {
-                buySum += Integer.parseInt(word[numberAmount]);
+        String[] lines = data.split(System.lineSeparator());
+        for (String line: lines) {
+            String[] dataFromLine = line.split(",");
+            if (dataFromLine[0].equals("buy")) {
+                buySum += Integer.parseInt(dataFromLine[numberAmount]);
             } else {
-                supplySum += Integer.parseInt(word[numberAmount]);
+                supplySum += Integer.parseInt(dataFromLine[numberAmount]);
             }
         }
         int result = supplySum - buySum;
-        stringBuilder = new StringBuilder();
-        stringBuilder.append("supply").append(",").append(supplySum).append(System.lineSeparator())
+        StringBuilder resultString = new StringBuilder();
+        resultString.append("supply").append(",").append(supplySum).append(System.lineSeparator())
                 .append("buy").append(",").append(buySum).append(System.lineSeparator())
                 .append("result,").append(result);
-        return stringBuilder.toString();
+        return resultString.toString();
     }
 
     private String readFile(String fromFileName) {
-        StringBuilder stringBuilder;
+        StringBuilder stringBuilder = new StringBuilder();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
-            stringBuilder = new StringBuilder();
             String value = bufferedReader.readLine();
             while (value != null) {
                 stringBuilder.append(value).append(System.lineSeparator());
                 value = bufferedReader.readLine();
             }
         } catch (IOException e) {
-            throw new RuntimeException("Can't read file",e);
+            throw new RuntimeException("Can't read file" + fromFileName, e);
         }
         return stringBuilder.toString();
     }
 
-    private void writeFile(String resultToFile, String toFileName) {
+    private void writeFile(String text, String toFileName) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
-            bufferedWriter.write(resultToFile);
+            bufferedWriter.write(text);
         } catch (IOException e) {
-            throw new RuntimeException("Can't write data to file", e);
+            throw new RuntimeException("Can't write data to file" + toFileName, e);
         }
     }
 }
