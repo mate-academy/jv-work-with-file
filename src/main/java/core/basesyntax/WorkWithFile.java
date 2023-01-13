af102b59ab22a515db88347afe71783e82c5f3ea
Kostia Vershynin <vershynin.kostia@gmail.com>
2023-01-13 21:43:02 +0200

    fixed realisation method workWithFile

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,54 +8,56 @@ import java.nio.file.Files;
 import java.util.List;
 
 public class WorkWithFile {
-    private static final int INDEX_ZERO = 0;
-    private static final int INDEX_ONE = 1;
+    private static final int OPERATION_INDEX_ZERO = 0;
+    private static final int OPERATION_INDEX_ONE = 1;
     private static final String COMMA = ",";
     private static final String SUPPLY = "supply";
     private static final String BUY = "buy";
     private static final String RESULT = "result";
 
     public void getStatistic(String fromFileName, String toFileName) {
+        String report = createReport(readFromFile(fromFileName));
+        writeReportToFile(toFileName, report);
+    }
+
+    private String createReport(List<String> listData) {
         StringBuilder strResult = new StringBuilder();
-        List<String> listData = readCsv(fromFileName);
         int supplyResult = 0;
         int buyResult = 0;
         int sumResult;
         for (String listDatum : listData) {
-            String[] str = listDatum.split(COMMA);
-            if (str[INDEX_ZERO].equals(SUPPLY)) {
-                supplyResult += Integer.parseInt(str[INDEX_ONE]);
+            String[] lineData = listDatum.split(COMMA);
+            if (lineData[OPERATION_INDEX_ZERO].equals(SUPPLY)) {
+                supplyResult += Integer.parseInt(lineData[OPERATION_INDEX_ONE]);
             }
-            if (str[INDEX_ZERO].equals(BUY)) {
-                buyResult += Integer.parseInt(str[INDEX_ONE]);
+            if (lineData[OPERATION_INDEX_ZERO].equals(BUY)) {
+                buyResult += Integer.parseInt(lineData[OPERATION_INDEX_ONE]);
             }
         }
         sumResult = supplyResult - buyResult;
-        strResult.append(SUPPLY + COMMA).append(supplyResult)
+        return strResult.append(SUPPLY + COMMA).append(supplyResult)
                 .append(System.lineSeparator()).append(BUY).append(COMMA)
                 .append(buyResult).append(System.lineSeparator())
-                .append(RESULT).append(COMMA).append(sumResult);
-        writeCsv(toFileName, strResult.toString());
+                .append(RESULT).append(COMMA).append(sumResult).toString();
     }
 
-    private List<String> readCsv(String fromFileName) {
+    private List<String> readFromFile(String fromFileName) {
         File file = new File(fromFileName);
-        List<String> strings;
+        List<String> lines;
         try {
-            strings = Files.readAllLines(file.toPath());
-
+            lines = Files.readAllLines(file.toPath());
         } catch (IOException e) {
-            throw new RuntimeException("Can,t read file", e);
+            throw new RuntimeException("Can,t read file" + file.getName(), e);
         }
-        return strings;
+        return lines;
     }
 
-    private void writeCsv(String toFileName, String data) {
+    private void writeReportToFile(String toFileName, String data) {
         File file = new File(toFileName);
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
             bufferedWriter.write(data);
         } catch (IOException e) {
-            throw new RuntimeException("Can't write file", e);
+            throw new RuntimeException("Can't write file" + file.getName(), e);
         }
     }
 }
