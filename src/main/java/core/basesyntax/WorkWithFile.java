dbff3c86d2c147577d66b757d3086dca8b0d4380
Kateryna <katerynachi5@gmail.com>
2023-01-17 14:20:27 +0100

    Apply changes after code review

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,46 +7,59 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    private static final int FIRST_POSITION = 0;
-    private static final int SECOND_POSITION = 1;
+    private static final int TITLE_POSITION = 0;
+    private static final int NUM_POSITION = 1;
+    private static final String SEPARATOR = ",";
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+    private static final String RESULT = "result";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        String report = createReport(fromFileName);
+        String inputData = readFromFile(fromFileName);
+        String report = calculateData(inputData);
         writeToFile(report, toFileName);
     }
 
-    private String createReport(String fromFileName) {
-        String report;
+    private String readFromFile(String fromFileName) {
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
-            report = calculateData(bufferedReader);
+            StringBuilder builder = new StringBuilder();
+            String inputLine;
+            while ((inputLine = bufferedReader.readLine()) != null) {
+                builder.append(inputLine).append(System.lineSeparator());
+            }
+            return builder.toString();
         } catch (IOException e) {
             throw new RuntimeException("Can't read data from the file " + fromFileName, e);
         }
-        return report;
     }
 
-    private String calculateData(BufferedReader bufferedReader) throws IOException {
-        String inputLine;
+    private String calculateData(String dataFromFile) {
+        StringBuilder builder = new StringBuilder();
+        String[] splitData = dataFromFile.split(System.lineSeparator());
         int result;
         int supply = 0;
         int buy = 0;
-        while ((inputLine = bufferedReader.readLine()) != null) {
-            String[] splitArray = inputLine.split(",");
-            switch (splitArray[FIRST_POSITION]) {
-                case "supply":
-                    supply += Integer.parseInt(splitArray[SECOND_POSITION]);
+
+        for (String line : splitData) {
+            String[] splitLine = line.split(SEPARATOR);
+            switch (splitLine[TITLE_POSITION]) {
+                case SUPPLY:
+                    supply += Integer.parseInt(splitLine[NUM_POSITION]);
                     break;
-                case "buy":
-                    buy += Integer.parseInt(splitArray[SECOND_POSITION]);
+                case BUY:
+                    buy += Integer.parseInt(splitLine[NUM_POSITION]);
                     break;
                 default:
                     System.out.println("There is no acceptable option");
             }
         }
         result = supply - buy;
-        return "supply," + supply + System.lineSeparator()
-                + "buy," + buy + System.lineSeparator()
-                + "result," + result + System.lineSeparator();
+        builder.append(SUPPLY).append(SEPARATOR).append(supply)
+                .append(System.lineSeparator())
+                .append(BUY).append(SEPARATOR).append(buy)
+                .append(System.lineSeparator())
+                .append(RESULT).append(SEPARATOR).append(result);
+        return builder.toString();
     }
 
     private void writeToFile(String report, String toFileName) {
