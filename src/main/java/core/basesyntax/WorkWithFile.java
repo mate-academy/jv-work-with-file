4f98304f2aefb75350798336e72f4ac3b4ae8ce0
Maxim Skory <devisrush@gmail.com>
2023-01-18 22:42:29 +0200

    Class fields were added, createReport() method logic was delimited

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -11,46 +11,57 @@ public class WorkWithFile {
     private static final String[] OPERATION_NAME = {"supply", "buy"};
     private static final String DELIMITER = ",";
     private static final String RESULT_NAME = "result";
+    private StringBuilder report;
+    private int firstTotal;
+    private int secondTotal;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        StringBuilder report = createReport(fromFileName);
-        writeToFile(report, toFileName);
+        report = new StringBuilder();
+        firstTotal = 0;
+        secondTotal = 0;
+        readFile(fromFileName);
+        createReport();
+        writeToFile(toFileName);
     }
 
-    private void writeToFile(StringBuilder report, String toFileName) {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
-            writer.write(report.toString());
-        } catch (IOException e) {
-            throw new RuntimeException("Can't write to file" + toFileName + e);
-        }
-    }
-
-    private StringBuilder createReport(String fromFileName) {
-        StringBuilder report = new StringBuilder();
-        int supplyTotal = 0;
-        int buyTotal = 0;
+    private void readFile(String fromFileName) {
         try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
             String line;
-            String value;
             line = reader.readLine();
             while (line != null) {
-                if (line.contains(OPERATION_NAME[0])) {
-                    value = line.split(DELIMITER)[1];
-                    supplyTotal += Integer.parseInt(value);
-                } else {
-                    value = line.split(DELIMITER)[1];
-                    buyTotal += Integer.parseInt(value);
-                }
+                calculateTotal(line);
                 line = reader.readLine();
             }
         } catch (IOException e) {
             throw new RuntimeException("Can't read the file" + e);
         }
-        return report.append(OPERATION_NAME[0]).append(DELIMITER)
-                .append(supplyTotal).append(System.lineSeparator())
+    }
+
+    private void calculateTotal(String line) {
+        String amount;
+        if (line.contains(OPERATION_NAME[0])) {
+            amount = line.split(DELIMITER)[1];
+            firstTotal += Integer.parseInt(amount);
+        } else {
+            amount = line.split(DELIMITER)[1];
+            secondTotal += Integer.parseInt(amount);
+        }
+    }
+
+    private void createReport() {
+        report.append(OPERATION_NAME[0]).append(DELIMITER)
+                .append(firstTotal).append(System.lineSeparator())
                 .append(OPERATION_NAME[1]).append(DELIMITER)
-                .append(buyTotal).append(System.lineSeparator())
+                .append(secondTotal).append(System.lineSeparator())
                 .append(RESULT_NAME).append(DELIMITER)
-                .append(supplyTotal - buyTotal);
+                .append(firstTotal - secondTotal);
+    }
+
+    private void writeToFile(String toFileName) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
+            writer.write(report.toString());
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write to file" + toFileName + e);
+        }
     }
 }
