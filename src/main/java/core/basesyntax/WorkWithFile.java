91218415832b04f1cc47d6844f856679f3543946
Maxim Skory <devisrush@gmail.com>
2023-01-19 18:25:22 +0200

    split() method was changed to contains(). Class fields were changed to local variables. New local variables were added

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -5,47 +5,53 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class WorkWithFile {
     private static final String SUPPLY_LINE_NAME = "supply";
     private static final String BUY_LINE_NAME = "buy";
     private static final String RESULT_LINE_NAME = "result";
     private static final String DELIMITER = ",";
-    private int supplyTotal;
-    private int buyTotal;
+    private static final int OPERATION_INDEX = 0;
+    private static final int AMOUNT_INDEX = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        supplyTotal = 0;
-        buyTotal = 0;
-        readFile(fromFileName);
-        writeToFile(createReport(), toFileName);
+        ArrayList<String> data = readFile(fromFileName);
+        String report = generateReport(data);
+        writeToFile(report, toFileName);
     }
 
-    private void readFile(String fromFileName) {
+    private ArrayList<String> readFile(String fromFileName) {
         try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
             String line = reader.readLine();
+            ArrayList<String> separatedLines = new ArrayList<>();
             while (line != null) {
-                calculateTotal(line);
+                separatedLines.add(line);
                 line = reader.readLine();
             }
+            return separatedLines;
         } catch (IOException e) {
             throw new RuntimeException("Can't read the file" + fromFileName, e);
         }
     }
 
-    private void calculateTotal(String line) {
-        String amount;
-        if (line.contains(SUPPLY_LINE_NAME)) {
-            amount = line.split(DELIMITER)[1];
-            supplyTotal += Integer.parseInt(amount);
-        } else {
-            amount = line.split(DELIMITER)[1];
-            buyTotal += Integer.parseInt(amount);
-        }
-    }
-
-    private String createReport() {
+    private String generateReport(ArrayList<String> data) {
+        int supplyTotal = 0;
+        int buyTotal = 0;
+        int amount;
+        String operation;
+        String[] separatedLine;
         StringBuilder report = new StringBuilder();
+        for (String line : data) {
+            separatedLine = line.split(DELIMITER);
+            operation = separatedLine[OPERATION_INDEX];
+            amount = Integer.parseInt(separatedLine[AMOUNT_INDEX]);
+            if (SUPPLY_LINE_NAME.equals(operation)) {
+                supplyTotal += amount;
+            } else {
+                buyTotal += amount;
+            }
+        }
         return report.append(SUPPLY_LINE_NAME).append(DELIMITER)
                 .append(supplyTotal).append(System.lineSeparator())
                 .append(BUY_LINE_NAME).append(DELIMITER)
