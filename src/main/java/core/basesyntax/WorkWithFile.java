388905643c59463c33357ef92124a98cf53666f7
VictoriaMrkvch <markevuch.viktorija@gmail.com>
2023-01-20 20:23:00 +0200

    solution

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,42 @@
 package core.basesyntax;
 
+import java.io.*;
+
 public class WorkWithFile {
+    private final String SPLIT_BY = ",";
+    private final String SUPPLY = "supply";
+    private final String BUY = "buy";
+    private final String RESULT = "result";
+    private String line = "";
     public void getStatistic(String fromFileName, String toFileName) {
+        int supplyCount = 0;
+        int buyCount = 0;
+        try
+        {
+            BufferedReader br = new BufferedReader(new FileReader(fromFileName));
+            while ((line = br.readLine()) != null)
+            {
+                String[] data = line.split(SPLIT_BY);
+                if (data[0].equals(SUPPLY)) {
+                    supplyCount += Integer.parseInt(data[1]);
+                } else if (data[0].equals(BUY)) {
+                    buyCount += Integer.parseInt(data[1]);
+                }
+            }
+            br.close();
+            File csvFile = new File(toFileName);
+            FileWriter fileWriter = new FileWriter(csvFile);
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append(SUPPLY).append(SPLIT_BY).append(supplyCount).append("\n");
+            stringBuilder.append(BUY).append(SPLIT_BY).append(buyCount).append("\n");
+            stringBuilder.append(RESULT).append(SPLIT_BY).append(supplyCount - buyCount);
+            fileWriter.write(stringBuilder.toString());
+            fileWriter.close();
+        }
+        catch (IOException e)
+        {
+            throw new RuntimeException("Can't read data from the file " + fromFileName, e);
+        }
 
     }
 }
