18d93a8526a834f6b3d3bb69d50ad1c10697a6f6
Aleksandr Zahorodnii <git.hub.zagar@gmail.com>
2023-01-20 21:41:26 +0100

    fixed after review

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -14,33 +14,44 @@ public class WorkWithFile {
     private static final int NUMBER_INDEX = 1;
     private static final String GET_SUM_SUPPLY = "getSupply";
     private static final String GET_SUM_BUY = "getBuy";
+    private static final String COMMA = ",";
+    private static final String SPACE = " ";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        writeToFile(fromFileName, toFileName);
+        String[] arrayReport = writeToFile(fromFileName, toFileName);
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
+            for (String valueReport : arrayReport) {
+                writer.write(valueReport);
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write to file" + fromFileName, e);
+        }
     }
 
     private String[] readFile(String fileName) {
         File file = new File(fileName);
-        String[] stringFileArr;
+        String[] arrayFile;
         try {
-            stringFileArr = Files.readAllLines(file.toPath()).toArray(new String[0]);
+            arrayFile = Files.readAllLines(file.toPath()).toArray(new String[0]);
         } catch (IOException e) {
             throw new RuntimeException("Can't read file" + file, e);
         }
-        return stringFileArr;
+        return arrayFile;
     }
 
     private int generateReport(String getSum, String fileName) {
-        String[] stringFileArr = readFile(fileName);
+        String[] arrayReport = readFile(fileName);
         int sumSupply = 0;
         int sumBuy = 0;
 
-        for (int i = 0; i < stringFileArr.length; i++) {
-            String[] stringValue = stringFileArr[i].split(",");
-            sumSupply += stringValue[NAME_INDEX].equals(SUPPLY_WORD)
-                    ? Integer.parseInt(stringValue[NUMBER_INDEX]) : 0;
-            sumBuy += stringValue[NAME_INDEX].equals(BUY_WORD)
-                    ? Integer.parseInt(stringValue[NUMBER_INDEX]) : 0;
+        for (int i = 0; i < arrayReport.length; i++) {
+            String[] reportValue = arrayReport[i].split(COMMA);
+            sumSupply += reportValue[NAME_INDEX].equals(SUPPLY_WORD)
+                    ? Integer.parseInt(reportValue[NUMBER_INDEX]) : 0;
+            sumBuy += reportValue[NAME_INDEX].equals(BUY_WORD)
+                    ? Integer.parseInt(reportValue[NUMBER_INDEX]) : 0;
         }
         if (getSum.equals(GET_SUM_SUPPLY)) {
             return sumSupply;
@@ -48,23 +59,16 @@ public class WorkWithFile {
         return sumBuy;
     }
 
-    private void writeToFile(String fromFileName, String toFileName) {
+    private String[] writeToFile(String fromFileName, String toFileName) {
         int sumSupply = generateReport(GET_SUM_SUPPLY, fromFileName);
         int sumBuy = generateReport(GET_SUM_BUY, fromFileName);
         int sumResult = sumSupply - sumBuy;
-        StringBuilder builder = new StringBuilder();
-        builder.append(SUPPLY_WORD).append(",").append(sumSupply).append(" ")
-                .append(BUY_WORD).append(",").append(sumBuy).append(" ")
-                .append(RESULT_WORD).append(",").append(sumResult);
-        String[] builderArr = builder.toString().split(" ");
+        StringBuilder giveReport = new StringBuilder();
+        giveReport.append(SUPPLY_WORD).append(COMMA).append(sumSupply).append(SPACE)
+                .append(BUY_WORD).append(COMMA).append(sumBuy).append(SPACE)
+                .append(RESULT_WORD).append(COMMA).append(sumResult);
+        String[] arrayReport = giveReport.toString().split(SPACE);
 
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
-            for (String value : builderArr) {
-                writer.write(value);
-                writer.newLine();
-            }
-        } catch (IOException e) {
-            throw new RuntimeException("Can't write to file" + fromFileName, e);
-        }
+        return  arrayReport;
     }
 }
