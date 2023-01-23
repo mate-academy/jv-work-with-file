2341b0477ae19c5b11d2572192770bcf38060091
Yaroslav Kostenko <xsander3@gmail.com>
2023-01-23 23:51:03 +0200

    Added method getReport. updated stringArray.

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -15,7 +15,7 @@ public class WorkWithFile {
 
     public void getStatistic(String fromFileName, String toFileName) {
         String [] data = readFromFile(fromFileName);
-        writeToFile(data, toFileName);
+        writeToFile(getReport(data), toFileName);
     }
 
     private String [] readFromFile(String fromFileName) {
@@ -23,10 +23,10 @@ public class WorkWithFile {
             StringBuilder stringBuilder = new StringBuilder();
             String value = bufferedReader.readLine();
             while (value != null) {
-                stringBuilder.append(value).append(SEPARATE_SYMBOL);
+                stringBuilder.append(value).append(System.lineSeparator());
                 value = bufferedReader.readLine();
             }
-            return stringBuilder.toString().split(SEPARATE_SYMBOL);
+            return stringBuilder.toString().split(System.lineSeparator());
         } catch (FileNotFoundException e) {
             throw new RuntimeException("Can`t open file to read.", e);
         } catch (IOException e) {
@@ -34,27 +34,31 @@ public class WorkWithFile {
         }
     }
 
-    private void writeToFile(String [] data, String toFileName) {
-        int supplySum = 0;
-        int buySum = 0;
-        for (int i = 0; i < data.length; i += 2) {
-            if (data[i].equals(SUPPLY)) {
-                supplySum += Integer.parseInt(data[i + 1]);
-            }
-            if (data[i].equals(BUY)) {
-                buySum += Integer.parseInt(data[i + 1]);
-            }
-        }
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(SUPPLY).append(SEPARATE_SYMBOL).append(supplySum)
-                .append(System.lineSeparator())
-                .append(BUY).append(SEPARATE_SYMBOL).append(buySum)
-                .append(System.lineSeparator())
-                .append(RESULT).append(SEPARATE_SYMBOL).append(supplySum - buySum);
+    private void writeToFile(String report, String toFileName) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
-            bufferedWriter.write(stringBuilder.toString());
+            bufferedWriter.write(report);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private String getReport(String [] data) {
+        int supplySum = 0;
+        int buySum = 0;
+        for (String record: data) {
+            int value = Integer.parseInt(record.substring(record.indexOf(SEPARATE_SYMBOL) + 1));
+            if (record.startsWith(SUPPLY)) {
+                supplySum += value;
+            }
+            if (record.startsWith(BUY)) {
+                buySum += value;
+            }
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        return stringBuilder.append(SUPPLY).append(SEPARATE_SYMBOL).append(supplySum)
+                .append(System.lineSeparator())
+                .append(BUY).append(SEPARATE_SYMBOL).append(buySum)
+                .append(System.lineSeparator())
+                .append(RESULT).append(SEPARATE_SYMBOL).append(supplySum - buySum).toString();
     }
 }
