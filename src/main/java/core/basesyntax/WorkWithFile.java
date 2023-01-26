60ea4aecd49f54f052bb21241e22d085d3d60346
Yuriy Kononchuk <y_kononchuk@ukr.net>
2023-01-26 16:16:18 +0200

    apllied requested changes after 1st review

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,17 +8,22 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    private static final String SPLITTER_FIELD = ",";
+    private static final int OPERATION_TYPE_INDEX = 0;
+    private static final int AMOUNT_INDEX = 1;
+    private static final String OPERATION_SUPPLY = "supply";
+    private static final String OPERATION_BUY = "buy";
     private boolean isInitialRead = true;
 
     public void getStatistic(String fromFileName, String toFileName) {
         if (isInitialRead) {
-            String[] dataLinesFromFile = readFromFile(fromFileName);
-            String resultReport = getReport(dataLinesFromFile);
-            writeToFile(resultReport, toFileName);
+            String[] linesFromFile = readFromFile(fromFileName);
+            String report = getReport(linesFromFile);
+            writeToFile(report, toFileName);
         }
     }
 
-    static String[] readFromFile(String fileName) {
+    private String[] readFromFile(String fileName) {
         File file = new File(fileName);
         StringBuilder stringBuilder = new StringBuilder();
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
@@ -33,39 +38,34 @@ public class WorkWithFile {
         return stringBuilder.toString().split(System.lineSeparator());
     }
 
-    static String getReport(String[] dataLinesFromFile) {
-        final String splitterFields = ",";
-        final int operationTypeIndex = 0;
-        final int amountIndex = 1;
-        final String operationSupply = "supply";
-        final String operationBuy = "buy";
+    private String getReport(String[] dataLinesFromFile) {
+
         StringBuilder resultReport = new StringBuilder();
         int totalSupplyAmount = 0;
         int totalBuyAmount = 0;
         for (String dataLine : dataLinesFromFile) {
-            String[] dataField = dataLine.split(splitterFields);
-            String operationType = dataField[operationTypeIndex];
-            int amount = Integer.parseInt(dataField[amountIndex]);
+            String[] dataField = dataLine.split(SPLITTER_FIELD);
+            String operationType = dataField[OPERATION_TYPE_INDEX];
+            int amount = Integer.parseInt(dataField[AMOUNT_INDEX]);
             switch (operationType) {
-                case operationSupply:
+                case OPERATION_SUPPLY:
                     totalSupplyAmount += amount;
                     break;
-                case operationBuy:
+                case OPERATION_BUY:
                     totalBuyAmount += amount;
                     break;
                 default:
                     throw new RuntimeException("Data is incorrect");
             }
         }
-        return resultReport.append(operationSupply).append(splitterFields)
-                .append(totalSupplyAmount).append(System.lineSeparator()).append(operationBuy)
-                .append(splitterFields).append(totalBuyAmount).append(System.lineSeparator())
+        return resultReport.append(OPERATION_SUPPLY).append(SPLITTER_FIELD)
+                .append(totalSupplyAmount).append(System.lineSeparator()).append(OPERATION_BUY)
+                .append(SPLITTER_FIELD).append(totalBuyAmount).append(System.lineSeparator())
                 .append("result,").append(totalSupplyAmount - totalBuyAmount).toString();
     }
 
     private void writeToFile(String resultReport, String fileName) {
-        File file = new File(fileName);
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file, true))) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
             writer.write(resultReport);
         } catch (IOException e) {
             throw new RuntimeException("Can't write to file", e);
