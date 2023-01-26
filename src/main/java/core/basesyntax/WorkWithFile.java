72538ed7b30903a76cf0e7b240b7d873aad5d841
IhorPetrytskyi333 <mygit333@gmail.com>
2023-01-25 21:19:12 -0800

    solution_7_2

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -3,52 +3,49 @@ package core.basesyntax;
 import java.io.*;
 
 public class WorkWithFile {
-    private static final int FIRST_INDEX = 0;
-    private static final int SECOND_INDEX = 1;
+    private static final int ACTION_INDEX = 0;
+    private static final int AMOUNT_INDEX = 1;
+    private static final String SPLIT = ",";
+    private int buy = 0;
+    private int supply = 0;
+
+    private void writeToFile( String toFileName) {
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))){
+            writer.write(createResultString(supply,buy));
+
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t write to file: " + toFileName, e);
+        }
+    }
 
     public void getStatistic(String fromFileName, String toFileName) {
         String information;
-        int buy = 0;
-        int supply = 0;
 
-        try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName));
+        try ( BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))){
 
             while ((information = bufferedReader.readLine()) != null) {
-                String[] str = information.split(",");
-                if (str[FIRST_INDEX].equals("supply")) {
-                    supply += Integer.parseInt(str[SECOND_INDEX]);
+                String[] str = information.split(SPLIT);
+                if (str[ACTION_INDEX].equals("supply")) {
+                    supply += Integer.parseInt(str[AMOUNT_INDEX]);
                 } else {
-                    buy += Integer.parseInt(str[SECOND_INDEX]);
+                    buy += Integer.parseInt(str[AMOUNT_INDEX]);
+
                 }
             }
             bufferedReader.close();
         } catch (IOException e) {
-            System.out.println(e);
+            throw new RuntimeException("Can`t read from file: " + fromFileName, e);
         }
-        write(supply, buy, toFileName);
-    }
-
-    public void write(int supply, int buy, String toFileName) {
-        try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName));
-            writer.write("supply," + supply + System.lineSeparator());
-            writer.write("buy," + buy + System.lineSeparator());
-            writer.write("result," + (supply - buy) + System.lineSeparator());
-            writer.close();
-
-        } catch (IOException e) {
-            System.out.println(e);
-        }
-        System.out.println(createResultString(supply, buy));
+        writeToFile(toFileName);
     }
 
     public String createResultString(int supply, int buy) {
         int result = supply - buy;
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("supply, " + supply + System.lineSeparator())
-                .append("buy, " + buy + System.lineSeparator())
-                .append("result, " + result + System.lineSeparator());
+        stringBuilder.append("supply," + supply + System.lineSeparator())
+                .append("buy," + buy + System.lineSeparator())
+                .append("result," + result + System.lineSeparator());
 
         return stringBuilder.toString();
     }
