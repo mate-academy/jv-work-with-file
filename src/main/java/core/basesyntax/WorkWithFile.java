bbcc53117f397ba4947c978814ee07bf2c3fadce
Yuriy Kononchuk <y_kononchuk@ukr.net>
2023-01-26 21:54:54 +0200

    apllied requested changes after 2nd review

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,7 +2,6 @@ package core.basesyntax;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -13,20 +12,16 @@ public class WorkWithFile {
     private static final int AMOUNT_INDEX = 1;
     private static final String OPERATION_SUPPLY = "supply";
     private static final String OPERATION_BUY = "buy";
-    private boolean isInitialRead = true;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        if (isInitialRead) {
-            String[] linesFromFile = readFromFile(fromFileName);
-            String report = getReport(linesFromFile);
-            writeToFile(report, toFileName);
-        }
+        String[] linesFromFile = readFromFile(fromFileName);
+        String report = getReport(linesFromFile);
+        writeToFile(report, toFileName);
     }
 
     private String[] readFromFile(String fileName) {
-        File file = new File(fileName);
         StringBuilder stringBuilder = new StringBuilder();
-        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             String dataLine = reader.readLine();
             while (dataLine != null) {
                 stringBuilder.append(dataLine).append(System.lineSeparator());
@@ -39,7 +34,6 @@ public class WorkWithFile {
     }
 
     private String getReport(String[] dataLinesFromFile) {
-
         StringBuilder resultReport = new StringBuilder();
         int totalSupplyAmount = 0;
         int totalBuyAmount = 0;
@@ -68,9 +62,7 @@ public class WorkWithFile {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
             writer.write(resultReport);
         } catch (IOException e) {
-            throw new RuntimeException("Can't write to file", e);
-        } finally {
-            isInitialRead = false;
+            throw new RuntimeException("Can't write to file:" + fileName, e);
         }
     }
 }
