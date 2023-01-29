a8e4a054aa9e706b73be510d3c0e3f310dfdb7c9
slaybrute <slaybrute@gmail.com>
2023-01-29 14:42:18 +0200

    changes after verification

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,38 +8,41 @@ import java.nio.file.Files;
 import java.util.List;
 
 public class WorkWithFile {
+    private final static int INDEX_OF_NAME_OF_PRODUCT = 0;
+    private final static int INDEX_OF_COST_OF_PRODUCT = 1;
+
     public static void getStatistic(String fromFileName, String toFileName) {
-        File file = new File(toFileName);
-        File file1 = new File(fromFileName);
+        File fileForWrite = new File(toFileName);
+        File fileForRead = new File(fromFileName);
         List<String> strings;
         try {
-            strings = Files.readAllLines(file1.toPath());
+            strings = Files.readAllLines(fileForRead.toPath());
         } catch (IOException e) {
-            throw new RuntimeException("can't read file" + file1, e);
+            throw new RuntimeException("Can't read data from the file " + fileForRead, e);
         }
         try {
-            file.createNewFile();
+            fileForWrite.createNewFile();
         } catch (IOException e) {
-            throw new RuntimeException("can't create new file" + file, e);
+            throw new RuntimeException("Can't create new file" + fileForWrite, e);
         }
         int buy = 0;
         int supply = 0;
         int result;
         for (String string : strings) {
-            String[] tmpArray = string.split(",");
-            if (tmpArray[0].equals("buy")) {
-                buy += Integer.parseInt(tmpArray[1]);
+            String[] arrayWithInfoAboutProduct = string.split(",");
+            if (arrayWithInfoAboutProduct[INDEX_OF_NAME_OF_PRODUCT].equals("buy")) {
+                buy += Integer.parseInt(arrayWithInfoAboutProduct[INDEX_OF_COST_OF_PRODUCT]);
             } else {
-                supply += Integer.parseInt(tmpArray[1]);
+                supply += Integer.parseInt(arrayWithInfoAboutProduct[INDEX_OF_COST_OF_PRODUCT]);
             }
         }
         result = supply - buy;
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileForWrite))) {
             bufferedWriter.write("supply," + supply + System.lineSeparator());
             bufferedWriter.write("buy," + buy + System.lineSeparator());
             bufferedWriter.write("result," + result);
         } catch (IOException e) {
-            throw new RuntimeException("can't write file" + file, e);
+            throw new RuntimeException("Can't write data to the file " + fileForWrite, e);
         }
     }
 }
