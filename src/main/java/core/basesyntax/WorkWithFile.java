d5a12d856f86973c42354e8b73a9372717937442
IhorPetrytskyi333 <mygit333@gmail.com>
2023-01-30 19:01:42 -0800

    solution_7_7

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -12,44 +12,44 @@ public class WorkWithFile {
     private static final String BUY = "buy";
     private static final int ACTION_INDEX = 0;
     private static final int AMOUNT_INDEX = 1;
-    private StringBuilder builder = new StringBuilder();
+    private StringBuilder stringBuilder = new StringBuilder();
 
     public void getStatistic(String fromFileName, String toFileName) {
         writeToFile(createStringResult(readFromFile(fromFileName)), toFileName);
     }
 
     private String[] readFromFile(String fromFileName) {
-        builder = new StringBuilder();
+        stringBuilder = new StringBuilder();
         try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName));) {
             String data = reader.readLine();
             while (data != null) {
-                builder.append(data).append(System.lineSeparator());
+                stringBuilder.append(data).append(System.lineSeparator());
                 data = reader.readLine();
             }
         } catch (IOException e) {
             throw new RuntimeException("Can't read from file "
                     + fromFileName, e);
         }
-        return builder.toString().split(System.lineSeparator());
+        return stringBuilder.toString().split(System.lineSeparator());
     }
 
     private String createStringResult(String[] report) {
         int supply = 0;
         int buy = 0;
         int result = 0;
-        builder = new StringBuilder();
+        stringBuilder = new StringBuilder();
         for (String information : report) {
-            String[] str = information.split(SPLIT);
-            if (str[ACTION_INDEX].equals(BUY)) {
-                buy += Integer.parseInt(str[AMOUNT_INDEX]);
+            String[] linesFromFile = information.split(SPLIT);
+            if (linesFromFile[ACTION_INDEX].equals(BUY)) {
+                buy += Integer.parseInt(linesFromFile[AMOUNT_INDEX]);
             } else {
-                supply += Integer.parseInt(str[AMOUNT_INDEX]);
+                supply += Integer.parseInt(linesFromFile[AMOUNT_INDEX]);
             }
         }
 
         result = supply - buy;
 
-        return builder.append(SUPPLY + SPLIT + supply + System.lineSeparator())
+        return stringBuilder.append(SUPPLY + SPLIT + supply + System.lineSeparator())
                 .append(BUY + SPLIT + buy + System.lineSeparator())
                 .append("result" + SPLIT + result + System.lineSeparator()).toString();
     }
