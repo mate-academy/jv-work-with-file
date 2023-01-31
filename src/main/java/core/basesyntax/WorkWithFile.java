fef79213b915662d6e3172c2e497c17df181ef94
Volodymyr Sakalosh <sva2005@ukr.net>
2023-01-31 21:49:22 +0200

    Fixed import in class

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,6 +1,11 @@
 package core.basesyntax;
 
-import java.io.*;
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.File;
 
 public class WorkWithFile {
     private int supply;
