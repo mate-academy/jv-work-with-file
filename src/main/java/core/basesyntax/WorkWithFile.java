72cacdd3d6c629c77c4466d0bd50741b5e7814b4
MaksymHorielikov <hmv.fermats@gmail.com>
2023-02-05 02:00:58 +0200

    first commit (create method getStatistic)

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,43 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
+        int supplySum = 0;
+        int buySum = 0;
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
+            String line = reader.readLine();
+            while (line != null) {
+                String [] data = line.split(",");
+                switch (data[0]) {
+                    case "supply":
+                        supplySum += Integer.parseInt(data[1]);
+                        break;
+                    case "buy":
+                        buySum += Integer.parseInt(data[1]);
+                        break;
+                    default:
+                }
+                line = reader.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("File not found", e);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
 
+        try (BufferedWriter reader = new BufferedWriter(new FileWriter(toFileName))) {
+            reader.write("supply," + supplySum + System.lineSeparator());
+            reader.write("buy," + buySum + System.lineSeparator());
+            reader.write("result," + (supplySum - buySum) + System.lineSeparator());
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write to file", e);
+        }
     }
 }
