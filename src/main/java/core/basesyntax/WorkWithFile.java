6024c54a9b2f3d7e19c9788f7cb86ed7e6278310
Roman Morozov <morozov.ctk@gmail.com>
2023-02-06 22:13:41 +0200

    Change names of constants Add three methods: readList, fileWork, writeList Include filenames in the exception Use try with resources Make constants: Delimiter and Separator

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,47 +7,58 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    public static final int INDEX_1 = 0;
-    public static final int INDEX_2 = 1;
-    public static final int INDEX_3 = 2;
+    public static final int INDEX_SUPPLY = 0;
+    public static final int INDEX_BUY = 1;
+    public static final int INDEX_SUM_ARRAY_LENGTH = 2;
+    public static final int ARRAY_COUNT = 1;
     public static final String SUPPLY = "supply";
     public static final String BUY = "buy";
     public static final String TOTAL = "result";
+    public static final String DELIMITER = "\\W+";
+    public static final String SEPARATOR = System.lineSeparator();
 
     public void getStatistic(String fromFileName, String toFileName) {
-        BufferedReader reader;
+        writeList(fileWork(readList(fromFileName)),toFileName);
+    }
+
+    private String readList(String fromFileName) {
         StringBuilder stringBuilder = new StringBuilder();
-        try {
-            reader = new BufferedReader(new FileReader(fromFileName));
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
             int value = reader.read();
             while (value != -1) {
                 stringBuilder.append((char) value);
                 value = reader.read();
             }
         } catch (IOException e) {
-            throw new RuntimeException("Can't read file!", e);
+            throw new RuntimeException("Can't read: " + fromFileName);
         }
-        String list = stringBuilder.toString();
-        String[] all = list.split("\\W+");
-        int [] operationSum = new int[INDEX_3];
+        return stringBuilder.toString();
+    }
+
+    private StringBuilder fileWork(String list) {
+        String[] all = list.split(DELIMITER);
+        int[] operationSum = new int[INDEX_SUM_ARRAY_LENGTH];
         for (int j = 0; j < all.length; j += 2) {
             if (all[j].equals(SUPPLY)) {
-                operationSum[INDEX_1] += Integer.parseInt(all[j + INDEX_2]);
+                operationSum[INDEX_SUPPLY] += Integer.parseInt(all[j + ARRAY_COUNT]);
             } else if (all[j].equals(BUY)) {
-                operationSum[INDEX_2] += Integer.parseInt(all[j + INDEX_2]);
+                operationSum[INDEX_BUY] += Integer.parseInt(all[j + ARRAY_COUNT]);
             }
         }
-        int result = operationSum[INDEX_1] - operationSum[INDEX_2];
-        StringBuilder writer = new StringBuilder();
-        writer.append(SUPPLY).append(",").append(operationSum[INDEX_1]).append("\r\n");
-        writer.append(BUY).append(",").append(operationSum[INDEX_2]).append("\r\n");
-        writer.append(TOTAL).append(",").append(result);
-        try {
-            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName));
+        int result = operationSum[INDEX_SUPPLY] - operationSum[INDEX_BUY];
+        StringBuilder writer = new StringBuilder().append(SUPPLY).append(",");
+        writer.append(operationSum[INDEX_SUPPLY]).append(SEPARATOR).append(BUY).append(",");
+        writer.append(operationSum[INDEX_BUY]).append(SEPARATOR).append(TOTAL).append(",");
+        writer.append(result);
+        return writer;
+    }
+
+    private void writeList(StringBuilder writer, String toFileName) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
             bufferedWriter.write(String.valueOf(writer));
-            bufferedWriter.close();
+            bufferedWriter.flush();
         } catch (IOException e) {
-            throw new RuntimeException("Can't read file!", e);
+            throw new RuntimeException("Can't write: " + toFileName);
         }
     }
 }
