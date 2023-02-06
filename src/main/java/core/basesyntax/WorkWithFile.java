e1f83188a194087fcad1a7502f07b3b6b01cbc5f
WallRun <dimonty2003@gmail.com>
2023-02-06 19:39:20 +0200

    removed unneeded blank lines

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,24 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
+        writeReportIntoFile(toFileName, createReport(fromFileName));
+    }
+
+    public String createReport(String fromFileName) {
+        try (final BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName)) {
+
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void writeReportIntoFile(String toFileName, String report) {
 
     }
 }
