8dc238059cef742dee48abc8bc2cbdef4a2581a2
kromchk <kromchyk@yahoo.com>
2023-02-13 13:18:10 +0200

    Refactored WorkWithFile.java

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,52 +8,64 @@ import java.io.IOException;
 import java.util.HashMap;
 
 public class WorkWithFile {
+    private static final String DELIMITER = ",";
+    private static final int FIELDS = 2;
+    private final HashMap<String, Integer> stat = new HashMap<>();
+
     public void getStatistic(String fromFileName, String toFileName) {
-        String line;
-        HashMap<String, Integer> st = new HashMap<>();
-        try (BufferedReader bri = new BufferedReader(new FileReader(fromFileName))) {
-            while ((line = bri.readLine()) != null) {
-                String[] em = line.split(",");
-                if (em.length == 2) {
-                    var tmp1 = em[0].trim();
-                    var tmp2 = em[1].trim();
-                    if (st.containsKey(tmp1)) {
-                        st.replace(tmp1, st.get(tmp1) + Integer.parseInt(tmp2));
-                    } else {
-                        st.put(tmp1, Integer.parseInt(tmp2));
-                    }
-                }
+        try {
+            readFromCsv(fromFileName);
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage() + "Problem with input from CSV file");
+        } catch (NumberFormatException e) {
+            throw new RuntimeException(e.getMessage() + "Numeric parse exception");
+        }
+        try {
+            writeToCsv(toFileName);
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage() + "Problem with output to CSV file");
+        }
+    }
+
+    private void readFromCsv(String fromFileName) throws IOException, NumberFormatException {
+        BufferedReader bri = new BufferedReader(new FileReader(fromFileName));
+        while (true) {
+            String line = bri.readLine();
+            if (line == null) {
+                break;
             }
-        } catch (IOException | NumberFormatException e) {
-            if (e instanceof IOException) {
-                throw new RuntimeException(e.getMessage() + "Problem with input file");
-            } else {
-                throw new RuntimeException(e.getMessage());
+            String[] em = line.split(DELIMITER);
+            if (em.length == FIELDS) {
+                var tmp1 = em[0].trim();
+                var tmp2 = em[1].trim();
+                if (stat.containsKey(tmp1)) {
+                    stat.replace(tmp1, stat.get(tmp1) + Integer.parseInt(tmp2));
+                } else {
+                    stat.put(tmp1, Integer.parseInt(tmp2));
+                }
             }
         }
-        try (BufferedWriter bro = new BufferedWriter(new FileWriter(toFileName))) {
-            if (!st.isEmpty()) {
-                int dif = 0;
-                int i = 0;
-                String tmp = "";
-                for (String key : st.keySet()) {
-                    tmp = String.format("%s,%d\n", key, st.get(key)) + tmp;
-                    if (i == 0) {
-                        dif = st.get(key);
-                    } else {
-                        dif -= st.get(key);
-                    }
+        bri.close();
+    }
+
+    private void writeToCsv(String toFileName) throws IOException {
+        BufferedWriter bro = new BufferedWriter(new FileWriter(toFileName));
+        if (!stat.isEmpty()) {
+            int dif = 0;
+            int i = 0;
+            StringBuilder tmp = new StringBuilder();
+            for (String key : stat.keySet()) {
+                tmp.insert(0, String.format("%s,%d\n", key, stat.get(key)));
+                if (i == 0) {
+                    dif = stat.get(key);
                     i++;
+                } else {
+                    dif -= stat.get(key);
                 }
-                bro.write(tmp);
-                bro.write(String.format("result,%d\n", Math.abs(dif)));
             }
-        } catch (IOException | NumberFormatException e) {
-            if (e instanceof IOException) {
-                throw new RuntimeException(e.getMessage() + "Problem with output file");
-            } else {
-                throw new RuntimeException(e.getMessage());
-            }
+            bro.write(tmp.toString());
+            bro.write(String.format("result,%d\n", Math.abs(dif)));
+            bro.close();
         }
     }
 }
