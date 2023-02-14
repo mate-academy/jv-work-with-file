37bb95bc00564404fac0c8a91e9e3a44c7cf9807
Olena Matus <matus_ak16@nuwm.edu.ua>
2023-02-14 11:53:40 +0200

    fixed solution hw-6-work-with-file

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -11,13 +11,13 @@ public class WorkWithFile {
     public static final String SUPPLY = "supply";
     public static final String BUY = "buy";
     public static final String RESULT = "result";
-    public static final int NAME_INDEX = 0;
-    public static final int VALUE_INDEX = 1;
+    public static final int OPERATION_INDEX = 0;
+    public static final int AMOUNT_INDEX = 1;
     public static final String DELIMITER = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
         List<String> readDataList = readDataFromFile(fromFileName);
-        String dataToWrite = valuesCounter(readDataList);
+        String dataToWrite = countOperationsAmount(readDataList);
         writeDataInFile(toFileName, dataToWrite);
     }
 
@@ -26,37 +26,38 @@ public class WorkWithFile {
         List<String> readList = null;
         try {
             readList = Files.readAllLines(file.toPath());
-            System.out.println(readList);
-
         } catch (IOException e) {
             throw new RuntimeException("Can't read file", e);
         }
         return readList;
     }
 
-    private void writeDataInFile(String fileForResult, String data) {
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileForResult))) {
+    private void writeDataInFile(String resultFile, String data) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(resultFile))) {
             bufferedWriter.write(data);
         } catch (IOException e) {
             throw new RuntimeException("Can't write file", e);
         }
     }
 
-    private String valuesCounter(List<String> list) {
-        StringBuilder builder = new StringBuilder();
+    private String countOperationsAmount(List<String> linesFromFile) {
         int supplyCounter = 0;
         int buyCounter = 0;
-        for (int i = 0; i < list.size(); i++) {
-            String[] pointsArray = list.get(i).split(DELIMITER);
-            System.out.println(list.get(i));
-            if (pointsArray[NAME_INDEX].equals(SUPPLY)) {
-                supplyCounter += Integer.parseInt(pointsArray[VALUE_INDEX]);
-            } else if (pointsArray[NAME_INDEX].equals(BUY)) {
-                buyCounter += Integer.parseInt(pointsArray[VALUE_INDEX]);
+        for (int i = 0; i < linesFromFile.size(); i++) {
+            String[] operations = linesFromFile.get(i).split(DELIMITER);
+            if (operations[OPERATION_INDEX].equals(SUPPLY)) {
+                supplyCounter += Integer.parseInt(operations[AMOUNT_INDEX]);
+            } else if (operations[OPERATION_INDEX].equals(BUY)) {
+                buyCounter += Integer.parseInt(operations[AMOUNT_INDEX]);
             } else {
-                System.out.println("Invalid data.");
+                throw new RuntimeException("Invalid data to read.");
             }
         }
+        return createResultString(supplyCounter, buyCounter);
+    }
+
+    private String createResultString(int supplyCounter, int buyCounter) {
+        StringBuilder builder = new StringBuilder();
         return builder.append(SUPPLY).append(DELIMITER).append(supplyCounter)
                 .append(System.lineSeparator())
                 .append(BUY).append(DELIMITER).append(buyCounter)
