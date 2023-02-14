0ca8f95136cc4b1f3c18838064ded716b25d677b
Denys Dominskyi <dominskyj@gmail.com>
2023-02-14 16:38:16 +0200

    1.return List directly in method read() 2.Changed the use of the subString() method to Using an Array in calculation() 3.returned calculation result directly from method 4.Created new file directly in write() method 5.Removed clearFile() method 6.Created a constant for ","

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,56 +8,48 @@ import java.nio.file.Files;
 import java.util.List;
 
 public class WorkWithFile {
-    private final int[] result = new int[ReportColumn.values().length];
+    private static final String COMMA = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        calculation(read(fromFileName));
-        File fileWrite = new File(toFileName);
-        clearFile(toFileName);
-        write(fileWrite);
+        write(calculation(read(fromFileName)), toFileName);
     }
 
-    private void clearFile(String path) {
-        try (FileWriter clearFile = new FileWriter(path)) {
-            clearFile.write("");
+    private int[] calculation(List<String> list) {
+        int[] result = new int[ReportColumn.values().length];
+        for (String line : list) {
+            String[] operation = line.split(COMMA);
+            if (operation[0].equals(ReportColumn.values()[0].name().toLowerCase())) {
+                result[ReportColumn.SUPPLY.ordinal()] += Integer.parseInt(operation[1]);
+            } else {
+                result[ReportColumn.BUY.ordinal()] += Integer.parseInt(operation[1]);
+            }
+        }
+        result[ReportColumn.RESULT.ordinal()] =
+                result[ReportColumn.SUPPLY.ordinal()] - result[ReportColumn.BUY.ordinal()];
+        return result;
+    }
+
+    private void write(int[] info, String fileName) {
+        File file = new File(fileName);
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < ReportColumn.values().length; i++) {
+            stringBuilder.append(ReportColumn.values()[i].name().toLowerCase())
+                    .append(COMMA).append(info[i])
+                    .append(System.lineSeparator());
+        }
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+            writer.write(stringBuilder.toString());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private void calculation(List<String> list) {
-        for (String line : list) {
-            String[] operation = line.split(",");
-            if (operation[0].equals(ReportColumn.values()[0].name().toLowerCase())) {
-                result[ReportColumn.SUPPLY.ordinal()] += Integer.parseInt(line.substring(7));
-            } else {
-                result[ReportColumn.BUY.ordinal()] += Integer.parseInt(line.substring(4));
-            }
-        }
-        result[ReportColumn.RESULT.ordinal()] =
-                result[ReportColumn.SUPPLY.ordinal()] - result[ReportColumn.BUY.ordinal()];
-    }
-
-    private void write(File file) {
-        for (int i = 0; i < ReportColumn.values().length; i++) {
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter(file, true))) {
-                writer.write(ReportColumn.values()[i].name().toLowerCase() + ","
-                        + result[i] + System.lineSeparator());
-                result[i] = 0;
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
-
     private List<String> read(String fromFileName) {
         File fileRead = new File(fromFileName);
-        List<String> listOfLines;
         try {
-            listOfLines = Files.readAllLines(fileRead.toPath());
+            return Files.readAllLines(fileRead.toPath());
         } catch (IOException e) {
             throw new RuntimeException("Can't read the file ", e);
         }
-        return listOfLines;
     }
 }
