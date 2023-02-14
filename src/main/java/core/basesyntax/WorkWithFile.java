69c36ad5c930d7f7227e3909e5a0213bf0407f08
Oleksandr Karpiuk <karpo99old@gmail.com>
2023-02-14 16:34:33 +0200

    Make 2 private method

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,8 +1,12 @@
 package core.basesyntax;
 
+import static java.lang.Integer.parseInt;
+
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
@@ -25,8 +29,42 @@ public class WorkWithFile {
         } catch (IOException exception) {
             throw new RuntimeException("Can't read data from file " + readFile, exception);
         }
-        String formattedData = new FormattData().getFormattedData(builder.toString());
-        new WriteData().writeDataToFile(formattedData, toFileName);
+        String formattedData = getFormattedData(builder.toString());
+        writeDataToFile(formattedData, toFileName);
+    }
+
+    private static String getFormattedData(String data) {
+        int result;
+        int totalSupply = 0;
+        int totalBuy = 0;
+        StringBuilder builder = new StringBuilder();
+        String[] arr = data.split(REGEX);
+        for (String tips : arr) {
+            final int intValue = parseInt(tips.substring(tips.indexOf(",") + 1));
+            if (tips.contains("buy")) {
+                totalBuy += intValue;
+            } else if (tips.contains("supply")) {
+                totalSupply += intValue;
+            }
+        }
+        result = totalSupply - totalBuy;
+        builder.append(SUPPLY).append(RESULT_REGEX)
+                .append(totalSupply).append(System.lineSeparator())
+                .append(BUY).append(RESULT_REGEX).append(totalBuy)
+                .append(System.lineSeparator())
+                .append(RESULT_NAME).append(RESULT_REGEX).append(result);
+        return builder.toString();
+    }
+
+    private static void writeDataToFile(String formattedData, String direction) {
+        File managedData = new File(direction);
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(managedData))) {
+            for (int i = 0; i < formattedData.length(); i++) {
+                writer.write(formattedData.charAt(i));
+            }
+        } catch (IOException exception) {
+            throw new RuntimeException("Can't write data to file " + managedData, exception);
+        }
     }
 }
 
