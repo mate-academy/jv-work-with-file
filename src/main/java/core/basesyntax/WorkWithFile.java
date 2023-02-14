142d0130406862d776cc7ef68cacbe0d3ed3d0f1
Sushko Roman <formatesus@gmail.com>
2023-02-14 17:18:05 +0200

    implemented method getStatistic

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,42 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
+        int supply = 0;
+        int buy = 0;
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName));
+            String strings = bufferedReader.readLine();
+            while (strings != null) {
+                String[] data = strings.split(",");
+                if (data[0].equals("supply")) {
+                    supply += Integer.parseInt(data[1]);
+                } else if (data[0].equals("buy")) {
+                    buy += Integer.parseInt(data[1]);
+                }
+                strings = bufferedReader.readLine();
+            }
+            try {
+                BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName));
+                StringBuilder result = new StringBuilder();
+                result.append("supply,").append(supply).append(System.lineSeparator())
+                        .append("buy,").append(buy).append(System.lineSeparator())
+                        .append("result,").append(supply - buy);
+                writer.write(result.toString());
+                writer.close();
+            } catch (IOException e) {
+                throw new RuntimeException("Can`t write data to file", e);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t read data from file", e);
+        }
 
     }
+
 }
