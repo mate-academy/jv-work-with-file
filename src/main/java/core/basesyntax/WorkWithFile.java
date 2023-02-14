aed8c524147ac53e649c54a8b8fa249e5f156bb8
Vadym Kovalenko <axeshock322@gmail.com>
2023-02-14 13:08:53 +0200

    Extract read calculation and write to separate methods, add constants

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,34 +8,48 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    public static final String COMMA = ",";
+    public static final String BUY = "buy";
+    public static final String SUPPLY = "supply";
+    public static final String RESULT = "result";
+
     public void getStatistic(String fromFileName, String toFileName) {
         int supply = 0;
         int buy = 0;
+        supply = readAndCalculateFile(supply, buy, fromFileName)[0];
+        buy = readAndCalculateFile(supply, buy, fromFileName)[1];
+        writeToFile(supply, buy, toFileName);
+
+    }
+
+    public int[] readAndCalculateFile(int supply, int buy, String fromFileName) {
         try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
             String value = reader.readLine();
             while (value != null) {
-                if (value.contains("buy")) {
-                    buy += Integer.parseInt(value.split(",")[1]);
+                if (value.contains(BUY)) {
+                    buy += Integer.parseInt(value.split(COMMA)[1]);
                 }
-                if (value.contains("supply")) {
-                    supply += Integer.parseInt(value.split(",")[1]);
+                if (value.contains(SUPPLY)) {
+                    supply += Integer.parseInt(value.split(COMMA)[1]);
                 }
                 value = reader.readLine();
             }
         } catch (IOException e) {
             throw new RuntimeException("Can't read file", e);
         }
-        File toFile = new File(toFileName);
+        return new int[]{supply, buy};
+    }
 
+    public void writeToFile(int supply, int buy, String toFileName) {
+        File toFile = new File(toFileName);
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFile))) {
-            bufferedWriter.write("supply," + supply
+            bufferedWriter.write(SUPPLY + COMMA + supply
                     + System.lineSeparator()
-                    + "buy," + buy
+                    + BUY + COMMA + buy
                     + System.lineSeparator()
-                    + "result," + (supply - buy));
+                    + RESULT + COMMA + (supply - buy));
         } catch (IOException e) {
             throw new RuntimeException("Can't write to file", e);
         }
-
     }
 }
