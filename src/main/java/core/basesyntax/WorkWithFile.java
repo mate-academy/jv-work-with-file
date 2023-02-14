71e219d70e2d6336e9fb4d2e79be07bbe5a1d971
Maksym <maxim.yashyn@gmail.com>
2023-02-14 21:54:14 +0200

    create and implement createReport() method

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -33,4 +33,25 @@ public class WorkWithFile {
             throw new RuntimeException("Can't write data to file", e);
         }
     }
+
+    public String createReport(String data) {
+        int countSupply = 0;
+        int countBuy = 0;
+        StringBuilder result = new StringBuilder();
+        String[] lines = data.split(" ");
+        for (String line : lines) {
+            String[] spliter = line.split(",");
+            if (spliter[0].equals("supply")) {
+                countSupply += Integer.parseInt(spliter[1]);
+            }
+            if (spliter[0].equals("buy")) {
+                countBuy += Integer.parseInt(spliter[1]);
+            }
+        }
+        int countedResult = countSupply - countBuy;
+        result.append("supply").append(",").append(countSupply).append(System.lineSeparator());
+        result.append("buy").append(",").append(countBuy).append(System.lineSeparator());
+        result.append("result").append(",").append(countedResult).append(System.lineSeparator());
+        return result.toString();
+    }
 }
