1d4383ef96c08274117eeb88e39683ced91c7db8
Gleb Iaroshevych <randomeastecho@gmail.com>
2023-02-14 16:27:29 +0200

    Implemented solution to hw.

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,43 @@
 package core.basesyntax;
 
-public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class WorkWithFile {
+    private static int SUPPLY_INDEX = 0;
+    private static int AMOUNT_INDEX = 1;
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        int supplySum = 0;
+        int buySum = 0;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
+            String oneLine = reader.readLine();
+            while (oneLine != null) {
+                String[] fileInfo = oneLine.split(",");
+                if (fileInfo[SUPPLY_INDEX].equals("supply")) {
+                    supplySum += Integer.parseInt(fileInfo[AMOUNT_INDEX]);
+                } else {
+                    buySum += Integer.parseInt(fileInfo[AMOUNT_INDEX]);
+                }
+                oneLine = reader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t read file", e);
+        }
+        getInfoInFile(supplySum, buySum, toFileName);
+    }
+
+    private void getInfoInFile(int supplySum, int buySum, String toFileName) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
+            writer.write("supply," + supplySum + System.lineSeparator());
+            writer.write("buy," + buySum + System.lineSeparator());
+            writer.write("result," + (supplySum - buySum) + System.lineSeparator());
+        } catch (IOException e) {
+            throw new RuntimeException("Cant write info in file", e);
+        }
     }
 }
