23038764f391e1df941fc30c1f48b7b572c6fe55
Alona Klishchenko <alyonaklischenko@gmail.com>
2023-02-15 15:42:42 +0200

    made second round of requested changes

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -11,6 +11,8 @@ import java.io.IOException;
 public class WorkWithFile {
     private static final int WORD_INDEX = 0;
     private static final int AMOUNT_INDEX = 1;
+    private static final String BUY = "buy";
+    private static final String SUPPLY = "supply";
 
     public void getStatistic(String fromFileName, String toFileName) {
         String originalString = readFromFile(fromFileName);
@@ -40,26 +42,25 @@ public class WorkWithFile {
         int amountOfBuy = 0;
         for (String string : originalString.split(System.lineSeparator())) {
             String[] splited = string.split(",");
-            if (splited[WORD_INDEX].equals("supply")) {
+            if (splited[WORD_INDEX].equals(SUPPLY)) {
                 amountOfSupply += Integer.parseInt(splited[AMOUNT_INDEX]);
-            } else if (splited[WORD_INDEX].equals("buy")) {
+            } else if (splited[WORD_INDEX].equals(BUY)) {
                 amountOfBuy += Integer.parseInt(splited[AMOUNT_INDEX]);
             } else {
                 throw new RuntimeException("Wrong data format " + splited[WORD_INDEX]);
             }
         }
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("supply,")
+        stringBuilder.append(SUPPLY + ",")
                .append(amountOfSupply)
                .append(System.lineSeparator())
-               .append("buy,")
+               .append(BUY + ",")
                .append(amountOfBuy)
                .append(System.lineSeparator())
                .append("result,")
                .append(amountOfSupply - amountOfBuy)
                 .append(System.lineSeparator());
         return stringBuilder.toString();
-
     }
 
     private void writeToFile(String createReport, String toFileName) {
