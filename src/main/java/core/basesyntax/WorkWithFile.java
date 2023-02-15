31cb3eed730517e98698cfc491b03792f37c901c
Sanyk Volodymyr <sanikvladimir@gmail.com>
2023-02-15 18:31:56 +0200

    fixed indicated mistakes

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,16 +2,17 @@ package core.basesyntax;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
     private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+    private static final String RESULT = "result";
+    private static final String COMMA = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        File toFile = new File(toFileName);
         int[] numbers = readFile(fromFileName);
         writeToFile(createReport(numbers[0], numbers[1], numbers[2]), toFileName);
     }
@@ -22,7 +23,7 @@ public class WorkWithFile {
         String line;
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             while ((line = bufferedReader.readLine()) != null) {
-                String[] lineArray = line.split(",");
+                String[] lineArray = line.split(COMMA);
                 if (lineArray[0].equals(SUPPLY)) {
                     supplySum += Integer.parseInt(lineArray[1]);
                 } else {
@@ -38,17 +39,17 @@ public class WorkWithFile {
 
     private String createReport(int supplySum, int buySum, int result) {
         StringBuilder stringBuilder = new StringBuilder();
-        return stringBuilder.append("supply,").append(supplySum).append(System.lineSeparator())
-                .append("buy,").append(buySum).append(System.lineSeparator())
-                .append("result,").append(result).toString();
+        return stringBuilder.append(SUPPLY).append(COMMA).append(supplySum)
+                .append(System.lineSeparator()).append(BUY).append(COMMA)
+                .append(buySum).append(System.lineSeparator())
+                .append(RESULT).append(COMMA).append(result).toString();
     }
 
     private void writeToFile(String report, String toFileName) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
             bufferedWriter.write(report);
         } catch (IOException e) {
-            throw new RuntimeException("Can`t write data to file" + toFileName, e);
+            throw new RuntimeException("Can`t write data to file " + toFileName, e);
         }
     }
-
 }
