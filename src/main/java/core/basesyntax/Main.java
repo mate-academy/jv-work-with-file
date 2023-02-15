7c0c5f92e68831ac4eafd72671eb601d714a3c17
Olexandr Leshtaiev <leshtaev13.a@gmail.com>
2023-02-15 15:35:01 +0200

    rewrited code and used StringBuilder instead of List.Created constants

--- src/main/java/core/basesyntax/Main.java
+++
@@ -1,8 +0,0 @@
-package core.basesyntax;
-
-public class Main {
-    public static void main(String[] args) {
-        WorkWithFile workWithFile = new WorkWithFile();
-        System.out.println(workWithFile.readFromFile("apple.csv"));
-    }
-}
