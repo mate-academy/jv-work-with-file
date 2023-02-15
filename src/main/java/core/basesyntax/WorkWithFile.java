5f36777959e4cb0f7dee8551a1edad6cc67e9911
Vitalii Fedychkanych <fraugerschoh@gmail.com>
2023-02-15 16:36:14 +0200

    add desicion 1.0

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,37 @@
 package core.basesyntax;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
-
+        int supply = 0;
+        int buy = 0;
+        int result = 0;
+        try {
+            String[] lines = String.join(",", Files
+                    .readAllLines(Paths.get(fromFileName), StandardCharsets.UTF_8)).split(",");
+            for (int i = 0;i < lines.length; i++) {
+                if (lines[i].equals("supply")) {
+                    supply += Integer.parseInt(lines[i + 1]);
+                }
+                if (lines[i].equals("buy")) {
+                    buy += Integer.parseInt(lines[i + 1]);
+                }
+            }
+            result = supply - buy;
+            String resultat = "supply," + supply
+                    + "\r\n" + "buy," + buy
+                    + "\r\n" + "result," + result;
+            File resultFile = new File(toFileName);
+            resultFile.createNewFile();
+            Files.writeString(Paths.get(resultFile.toURI()), resultat, StandardOpenOption.WRITE);
+        } catch (IOException e) {
+            throw new RuntimeException("can not do it!",e);
+        }
     }
 }
