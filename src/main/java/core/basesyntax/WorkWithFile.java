7621ae8c0771c739293ef3a872808320eb1ba771
Mariana Polishchuk <tonksmaryan@gmail.com>
2023-02-15 13:16:17 +0200

    [HW-WORK-WITH-FILE] changes after review

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,42 +8,37 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    private static final String NEW_LINE = "\n";
     private static final String COMMA = ",";
-    private static final String FILE_IS_EMPTY_MESSAGE = "File is empty";
-    private static final String CANT_READ_FILE_MESSAGE = "Can't read file";
     private static final String SUPPLY = "supply";
     private static final String BUY = "buy";
-    private static final String CANT_WRITE_TO_THE_FILE_MESSAGE = "can't write to the file";
     private static final String RESULT = "result";
 
-    private StringBuilder inputData;
-
     public void getStatistic(String fromFileName, String toFileName) {
-        inputData = new StringBuilder();
-        readFromFile(fromFileName);
-        createReport(toFileName);
+        StringBuilder inputData = readFromFile(fromFileName);
+        StringBuilder report = createReport(inputData);
+        writeToFile(toFileName, report);
     }
 
-    private void readFromFile(String fileName) {
+    private StringBuilder readFromFile(String fileName) {
+        StringBuilder inputData = new StringBuilder();
         File file = new File(fileName);
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(file));
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             String readerFile = reader.readLine();
             if (readerFile == null) {
-                throw new RuntimeException(FILE_IS_EMPTY_MESSAGE);
+                throw new RuntimeException("File is empty");
             }
             while (readerFile != null) {
                 inputData.append(readerFile).append(System.lineSeparator());
                 readerFile = reader.readLine();
             }
+            return inputData;
         } catch (IOException e) {
-            throw new RuntimeException(CANT_READ_FILE_MESSAGE, e);
+            throw new RuntimeException("Can't read file", e);
         }
     }
 
-    private void createReport(String toFileName) {
-        String[] splitInfo = inputData.toString().split(NEW_LINE);
+    private StringBuilder createReport(StringBuilder inputData) {
+        String[] splitInfo = inputData.toString().split(System.lineSeparator());
         int supplyCount = 0;
         int buyCount = 0;
         for (int i = 0; i < splitInfo.length; i++) {
@@ -56,17 +51,18 @@ public class WorkWithFile {
             }
         }
         int result = supplyCount - buyCount;
-        writeToFile(toFileName, supplyCount, buyCount, result);
+        StringBuilder report = new StringBuilder();
+        return report.append(SUPPLY).append(COMMA).append(supplyCount)
+                .append(System.lineSeparator()).append(BUY).append(COMMA)
+                .append(buyCount).append(System.lineSeparator())
+                .append(RESULT).append(COMMA).append(result);
     }
 
-    private void writeToFile(String toFileName, int supplyCount, int buyCount, int result) {
-        try {
-            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName));
-            bufferedWriter.write(SUPPLY + COMMA + supplyCount + System.lineSeparator()
-                    + BUY + COMMA + buyCount + System.lineSeparator() + RESULT + COMMA + result);
-            bufferedWriter.close();
+    private void writeToFile(String toFileName, StringBuilder report) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+            bufferedWriter.write(report.toString());
         } catch (IOException e) {
-            throw new RuntimeException(CANT_WRITE_TO_THE_FILE_MESSAGE, e);
+            throw new RuntimeException("can't write to the file", e);
         }
     }
 }
