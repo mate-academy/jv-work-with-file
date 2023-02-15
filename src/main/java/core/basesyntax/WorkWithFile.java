fce33677d43c22509c690f7694b03a8abfe4c685
kyrylh <kyryl.huliuk@gmail.com>
2023-02-15 15:29:28 +0200

    Created readFromFile, createReport, writeFile methods

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,45 +1,76 @@
 package core.basesyntax;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 public class WorkWithFile {
     private static final int OPERATION = 0;
     private static final int AMOUNT = 1;
 
-    public void getStatistic(String fromFileName, String toFileName) {
-        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
-            int buy = 0;
-            int supply = 0;
-            Path toPath = new File(toFileName).toPath();
+    private String readFromFile(File pathFile) {
+        StringBuilder res = new StringBuilder();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(pathFile))) {
             String line = reader.readLine();
             while (line != null) {
-                switch (line.split(",")[OPERATION]) {
-                    case "buy":
-                        buy += Integer.parseInt(line.split(",")[AMOUNT]);
-                        break;
-                    case "supply":
-                        supply += Integer.parseInt(line.split(",")[AMOUNT]);
-                        break;
-                    default:
-                        break;
-                }
+                res.append(line).append(System.lineSeparator());
                 line = reader.readLine();
             }
-            if (!Files.exists(toPath)) {
-                Files.createFile(toPath);
-            }
-            Files.write(toPath, ("supply," + supply
-                    + System.lineSeparator() + "buy," + buy
-                    + System.lineSeparator() + "result,"
-                    + (supply - buy)).getBytes());
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Can't read from file",e);
+        }
+        return res.toString();
+    }
+
+    private String createReport(String dataForReport) {
+        StringBuilder report = new StringBuilder();
+        int buy = 0;
+        int supply = 0;
+
+        for (String data : dataForReport.split(System.lineSeparator())) {
+            switch (data.split(",")[OPERATION]) {
+                case "buy":
+                    buy += Integer.parseInt(data.split(",")[AMOUNT]);
+                    break;
+
+                case "supply":
+                    supply += Integer.parseInt(data.split(",")[AMOUNT]);
+                    break;
+
+                default:
+                    break;
+            }
         }
 
+        return report.append("supply,")
+                .append(supply)
+                .append(System.lineSeparator())
+                .append("buy,")
+                .append(buy)
+                .append(System.lineSeparator())
+                .append("result,")
+                .append(supply - buy).toString();
+    }
+
+    private void writeFile(String report, File file) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+            writer.write(report);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write to file",e);
+        }
+    }
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        writeFile(
+                createReport(
+                readFromFile(
+                        new File(fromFileName)
+                )
+                ), new File(toFileName)
+        );
     }
 }
