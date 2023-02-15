0763b6bf64c83866e2295ab305a94faccb7e6e05
Vitalii Fedychkanych <fraugerschoh@gmail.com>
2023-02-15 17:42:24 +0200

    add desicion 3.0

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,30 +8,51 @@ import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 
 public class WorkWithFile {
+    private final String supplyString = "supply";
+    private final String buyString = "buy";
+    private final String resultString = "result";
+
     public void getStatistic(String fromFileName, String toFileName) {
-        int supply = 0;
-        int buy = 0;
-        int result = 0;
         try {
-            String[] lines = String.join(",", Files
-                    .readAllLines(Paths.get(fromFileName), StandardCharsets.UTF_8)).split(",");
-            for (int i = 0;i < lines.length; i++) {
-                if (lines[i].equals("supply")) {
-                    supply += Integer.parseInt(lines[i + 1]);
-                }
-                if (lines[i].equals("buy")) {
-                    buy += Integer.parseInt(lines[i + 1]);
-                }
-            }
-            result = supply - buy;
-            String resultat = "supply," + supply
-                    + System.lineSeparator() + "buy," + buy
-                    + System.lineSeparator() + "result," + result;
-            File resultFile = new File(toFileName);
-            resultFile.createNewFile();
-            Files.writeString(Paths.get(resultFile.toURI()), resultat, StandardOpenOption.WRITE);
+            String resultat = createReport(fromFileName);
+            writeToFile(toFileName,resultat);
         } catch (IOException e) {
-            throw new RuntimeException("can not do it!",e);
+            throw new RuntimeException(e.getMessage());
         }
     }
+
+    public String[] readFromFile(String fromFileName) throws IOException {
+        String[] lines = String.join(",", Files
+        .readAllLines(Paths.get(fromFileName), StandardCharsets.UTF_8)).split(",");
+        return lines;
+    }
+
+    public String createReport(String fromFileName) throws IOException {
+        String[] lines = readFromFile(fromFileName);
+        int result = 0;
+        int supply = 0;
+        int buy = 0;
+        for (int i = 0;i < lines.length; i++) {
+            if (lines[i].equals(supplyString)) {
+                supply += Integer.parseInt(lines[i + 1]);
+            }
+            if (lines[i].equals(buyString)) {
+                buy += Integer.parseInt(lines[i + 1]);
+            }
+        }
+        result = supply - buy;
+        StringBuilder resultat = new StringBuilder();
+        resultat.append(supplyString).append(",").append(supply)
+                .append(System.lineSeparator()).append(buyString).append(",").append(buy)
+                .append(System.lineSeparator()).append(resultString).append(",").append(result);
+        return resultat.toString();
+    }
+
+    public void writeToFile(String toFileName, String resultat) throws IOException {
+        File resultFile = new File(toFileName);
+        resultFile.createNewFile();
+        Files.writeString(Paths.get(resultFile.toURI()), resultat, StandardOpenOption.WRITE);
+    }
 }
+
+
