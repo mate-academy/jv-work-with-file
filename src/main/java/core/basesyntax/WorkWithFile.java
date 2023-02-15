98910f571ae81d5991ad4b8bb59bcce7267eca51
Nazar Zvarych <zvarych45@gmail.com>
2023-02-15 12:00:57 +0200

    I fixed mistakes.

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,59 +2,59 @@ package core.basesyntax;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
     private static final String DELIMITER = "///";
-    private static final int INDEX_0 = 0;
-    private static final int INDEX_1 = 1;
+    private static final int POSITION_OF_TITLE = 0;
+    private static final int POSITION_OF_VALUE = 1;
+    private static final int POSITION_OF_SUPPLY_VALUE = 0;
+    private static final int POSITION_OF_BUY_VALUE = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        String[] text = readFile(fromFileName);
-        int[] data = calculateDataForReport(text);
-        String dataForPrint = createReport(data);
-        putDataToFile(dataForPrint,toFileName);
+        String text = readFile(fromFileName);
+        text = createReport(text);
+        putDataToFile(text,toFileName);
     }
 
-    private String[] readFile(String fileName) {
-        StringBuilder string = new StringBuilder();
-        try {
-            File file = new File(fileName);
-            FileReader fileReader = new FileReader(file);
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
+    private String readFile(String fileName) {
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
             String text = bufferedReader.readLine();
             while (text != null) {
-                string.append(text).append(DELIMITER);
+                stringBuilder.append(text).append(DELIMITER);
                 text = bufferedReader.readLine();
             }
-            bufferedReader.close();
         } catch (IOException e) {
             throw new RuntimeException("Can`t open file!" + fileName, e);
         }
-        if (string.toString().isEmpty()) {
-            String[] empty = {};
+        if (stringBuilder.toString().isEmpty()) {
+            String empty = new String();
             return empty;
         }
-        return string.toString().split(DELIMITER);
+        return stringBuilder.toString();
     }
 
+    /* methods calculateDataForReport return int array with two elements.
+    * first element mean sum of all values with supply
+    * second element mean sum of all values with buy */
     private int[] calculateDataForReport(String[] str) {
         int[] result = new int[]{0, 0};
         for (int i = 0; i < str.length; i++) {
             String[] strings = str[i].split(",");
-            if (strings[INDEX_0].equals("supply")) {
-                result[INDEX_0] += Integer.parseInt(strings[INDEX_1]);
-            } else if (strings[INDEX_0].equals("buy")) {
-                result[INDEX_1] += Integer.parseInt(strings[INDEX_1]);
+            if (strings[POSITION_OF_TITLE].equals("supply")) {
+                result[POSITION_OF_SUPPLY_VALUE] += Integer.parseInt(strings[POSITION_OF_VALUE]);
+            } else if (strings[POSITION_OF_TITLE].equals("buy")) {
+                result[POSITION_OF_BUY_VALUE] += Integer.parseInt(strings[POSITION_OF_VALUE]);
             }
         }
         return result;
     }
 
-    private String createReport(int[] data) {
+    private String createReport(String string) {
+        int[] data = calculateDataForReport(string.split(DELIMITER));
         StringBuilder result = new StringBuilder();
         result.append("supply,").append(data[0]).append("\n");
         result.append("buy,").append(data[1]).append("\n");
