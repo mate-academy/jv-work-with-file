958751bcdf98e771fd75fee926fac122058444ed
Sushko Roman <formatesus@gmail.com>
2023-02-15 13:12:01 +0200

    added constants and constructs try_with_resources

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,6 +7,12 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    private static final String DELIMITER = ",";
+    private static final int OPERATION_INDEX = 0;
+    private static final int AMOUNT_INDEX = 1;
+    private static final int SUPPLY_INDEX = 0;
+    private static final int BUY_INDEX = 1;
+
     public void getStatistic(String fromFileName, String toFileName) {
         int[] data = readFile(fromFileName);
         String report = createReport(data);
@@ -16,17 +22,16 @@ public class WorkWithFile {
     public int[] readFile(String fromFileName) {
         int supply = 0;
         int buy = 0;
-        try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName));
-            String strings = bufferedReader.readLine();
-            while (strings != null) {
-                String[] data = strings.split(",");
-                if (data[0].equals("supply")) {
-                    supply += Integer.parseInt(data[1]);
-                } else if (data[0].equals("buy")) {
-                    buy += Integer.parseInt(data[1]);
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
+            String line = bufferedReader.readLine();
+            while (line != null) {
+                String[] data = line.split(DELIMITER);
+                if (data[OPERATION_INDEX].equals("supply")) {
+                    supply += Integer.parseInt(data[AMOUNT_INDEX]);
+                } else if (data[OPERATION_INDEX].equals("buy")) {
+                    buy += Integer.parseInt(data[AMOUNT_INDEX]);
                 }
-                strings = bufferedReader.readLine();
+                line = bufferedReader.readLine();
             }
         } catch (IOException e) {
             throw new RuntimeException("Can`t read data from file" + fromFileName, e);
@@ -36,17 +41,15 @@ public class WorkWithFile {
 
     public String createReport(int[] data) {
         StringBuilder result = new StringBuilder();
-        result.append("supply,").append(data[0]).append(System.lineSeparator())
-                .append("buy,").append(data[1]).append(System.lineSeparator())
-                .append("result,").append(data[0] - data[1]);
+        result.append("supply,").append(data[SUPPLY_INDEX]).append(System.lineSeparator())
+                .append("buy,").append(data[BUY_INDEX]).append(System.lineSeparator())
+                .append("result,").append(data[SUPPLY_INDEX] - data[BUY_INDEX]);
         return result.toString();
     }
 
     public void writeToFile(String report, String toFileName) {
-        try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName));
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
             writer.write(report);
-            writer.close();
         } catch (IOException e) {
             throw new RuntimeException("Can`t write data to file" + toFileName, e);
         }
