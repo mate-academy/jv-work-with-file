062b2c9d48eb175c34ca1f12f75ae122c360c5d3
Olexandr Leshtaiev <leshtaev13.a@gmail.com>
2023-02-15 15:34:50 +0200

    rewrited code and used StringBuilder instead of List.Created constants

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,71 +1,68 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.List;
 
 public class WorkWithFile {
+    private static final int FIRST_POSITION = 0;
+    private static final int SECOND_POSITION = 1;
+    private static final String BUY = "buy";
+    private static final String SUPPLY = "supply";
+    private static final String RESULT = "result";
+    private static final String COMMA = ",";
     public void getStatistic(String fromFileName, String toFileName) {
-        File file = new File(toFileName);
-        if (file.exists()) {
-            file.delete();
-        }
-        String[] result = countResult(readFromFile(fromFileName));
-        writeToFile(file,result);
+        String extractedData = readFromFile(fromFileName);
+        writeToFile(toFileName, countResultAndGetReport(extractedData));
     }
 
-    public String[] readFromFile(String fileName) {
-        File file = new File(fileName);
-        List<String> arrayList = new ArrayList<>();
-        try {
-            List<String> strings = Files.readAllLines(file.toPath());
-            for (int i = 0; i < strings.size(); i++) {
-                arrayList.add(strings.get(i));
+    private String readFromFile(String fromFileName) {
+        File file = new File(fromFileName);
+        StringBuilder builder = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+            String value = bufferedReader.readLine();
+            while (value != null) {
+                builder.append(value).append(System.lineSeparator());
+                value = bufferedReader.readLine();
             }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("File not found" + file.getName(), e);
         } catch (IOException e) {
-            throw new RuntimeException("Can`t read file ", e);
+            throw new RuntimeException("Can't read file" + file.getName(), e);
         }
-        return arrayList.toArray(String[]::new);
+        return builder.toString();
     }
 
-    private String[] countResult(String[] array) {
-        int buy = 0;
-        int result = 0;
+    private String countResultAndGetReport(String extractedData) {
+        String[] arrayOfLines = extractedData.split(System.lineSeparator());
         int supply = 0;
-        String[] finalArray = new String[3];
-        for (int i = 0;i < array.length;i++) {
-            String[] splitPieces = array[i].split(",");
-            for (int j = 0;j < splitPieces.length;j++) {
-                if (splitPieces[j].equals("buy")) {
-                    buy += Integer.valueOf(splitPieces[1]);
-                }
-                if (splitPieces[j].equals("supply")) {
-                    supply += Integer.valueOf(splitPieces[1]);
-                }
+        int buy = 0;
+        for (String line : arrayOfLines) {
+            String[] dataParts = line.split(",");
+            if (dataParts[FIRST_POSITION].equals(SUPPLY)) {
+                supply += Integer.parseInt(dataParts[SECOND_POSITION]);
+            }
+            if (dataParts[FIRST_POSITION].equals(BUY)) {
+                buy += Integer.parseInt(dataParts[SECOND_POSITION]);
             }
         }
-        result = supply - buy;
-        finalArray[0] = "supply" + "," + supply;
-        finalArray[1] = "buy" + "," + buy;
-        finalArray[2] = "result" + "," + result;
-        return finalArray;
+        StringBuilder builder = new StringBuilder();
+        builder.append(SUPPLY).append(COMMA).append(supply).append(System.lineSeparator())
+                .append(BUY).append(COMMA).append(buy).append(System.lineSeparator())
+                .append(RESULT).append(COMMA).append(supply - buy);
+        return builder.toString();
     }
 
-    private void writeToFile(File fileName, String[] array) {
-        for (int i = 0;i < array.length;i++) {
-            try (BufferedWriter bufferedWriter =
-                         new BufferedWriter(new FileWriter(fileName, true))) {
-                bufferedWriter.write(array[i]);
-                if (i < array.length - 1) {
-                    bufferedWriter.write(System.lineSeparator());
-                }
-            } catch (IOException e) {
-                throw new RuntimeException("Can`t write to file ", e);
-            }
+    private void writeToFile(String toFileName, String report) {
+        File file = new File(toFileName);
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
+            bufferedWriter.write(report);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write report to file" + file.getName(), e);
         }
     }
 }
