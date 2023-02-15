f8de3462b59a7ce1637d7fb06d1b583ea54da26d
OlehSarapuk <olehsarapuk@gmail.com>
2023-02-15 12:55:43 +0200

    added all requested fixes

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,22 +2,15 @@ package core.basesyntax;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    private static final String[] FILE_STRING_VALUES = {"supply", "buy", "result"};
-    private static final String COMA = ",";
-    private static final int[] FILE_INT_VALUES = {0, 0, 0};
-    private static final int MINIMUM_FILE_LENGTH = 1;
-
     public void getStatistic(String fromFileName, String toFileName) {
         String fileText = readFromFile(fromFileName);
-        String[] fileTextArray = fileText.toString().split(COMA);
-        calculateAmountsForValues(fileTextArray);
-        writeToFile(toFileName);
+        String report = createReport(fileText);
+        writeToFile(toFileName, report);
     }
 
     private String readFromFile(String fileName) {
@@ -25,7 +18,7 @@ public class WorkWithFile {
         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             String line = reader.readLine();
             while (line != null) {
-                fileText.append(line).append(COMA);
+                fileText.append(line).append(System.lineSeparator());
                 line = reader.readLine();
             }
         } catch (IOException e) {
@@ -34,34 +27,36 @@ public class WorkWithFile {
         return fileText.toString();
     }
 
-    private void calculateAmountsForValues(String[] fileTextArray) {
-        FILE_INT_VALUES[0] = 0;
-        FILE_INT_VALUES[1] = 0;
-        for (int i = 0; i < fileTextArray.length; i += 2) {
-            if (fileTextArray[i].equals(FILE_STRING_VALUES[0])) {
-                FILE_INT_VALUES[0] += Integer.valueOf(fileTextArray[i + 1]);
+    private String createReport(String fileText) {
+        String[] fileTextArray = fileText.split(System.lineSeparator());
+        int totalSupply = 0;
+        int totalBuy = 0;
+        for (int i = 0; i < fileTextArray.length; i++) {
+            String[] line = fileTextArray[i].split(",");
+            if (line[0].equals("supply")) {
+                totalSupply += Integer.valueOf(line[1]);
             } else {
-                FILE_INT_VALUES[1] += Integer.valueOf(fileTextArray[i + 1]);
+                totalBuy += Integer.valueOf(line[1]);
             }
         }
-        FILE_INT_VALUES[2] = FILE_INT_VALUES[0] - FILE_INT_VALUES[1];
+        int totalResult = totalSupply - totalBuy;
+        StringBuilder report = new StringBuilder();
+        report.append("supply,")
+                .append(totalSupply)
+                .append(System.lineSeparator())
+                .append("buy,")
+                .append(totalBuy)
+                .append(System.lineSeparator())
+                .append("result,")
+                .append(totalResult);
+        return report.toString();
     }
 
-    private void writeToFile(String fileName) {
-        File fileToWrite = new File(fileName);
-        StringBuilder textToWrite = new StringBuilder();
-        if (fileToWrite.length() < MINIMUM_FILE_LENGTH) {
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true))) {
-                for (int i = 0; i < FILE_STRING_VALUES.length; i++) {
-                    textToWrite.append(FILE_STRING_VALUES[i])
-                            .append(COMA)
-                            .append(FILE_INT_VALUES[i])
-                            .append(System.lineSeparator());
-                }
-                writer.write(textToWrite.toString());
-            } catch (IOException e) {
-                throw new RuntimeException("can't write to this file " + fileName, e);
-            }
+    private void writeToFile(String fileName, String content) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+            writer.write(content);
+        } catch (IOException e) {
+            throw new RuntimeException("can't write to this file " + fileName, e);
         }
     }
 }
