35bd6707f72233c35e1b05560fbb953403b25eed
industrialeng <semonenko055@gmail.com>
2023-02-15 23:26:40 +0200

    Fixed mistakes and created writeToFile method

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,58 +7,53 @@ import java.nio.file.StandardOpenOption;
 import java.util.List;
 
 public class WorkWithFile {
-    public static final String COMMA = ",";
-    public static final String SUPPLY = "supply";
-    public static final String BUY = "buy";
+    public static final String SUPPLY = "supply,";
+    public static final String BUY = "buy,";
     public static final String RESULT = "result,";
     public static final String NEW_LINE = System.lineSeparator();
-    public static final int ONE = 1;
-    public static final String regex = "[\\[\\] ,]";
-    private int result;
-    private int sumSupply;
-    private int sumBuy;
-    private String[] arrayOfList;
-    private StringBuilder calculationResult = new StringBuilder();
 
-    private String [] readFile(String fromFileName) {
+    public void getStatistic(String fromFileName, String toFileName) {
+        List<String> listFromFile = readFile(fromFileName);
+        String report = calculateReport(listFromFile);
+        writeToFile(report,toFileName);
+    }
+
+    private List<String> readFile(String fromFileName) {
         File inputFile = new File(fromFileName);
+        List<String> listOfInputFile;
         try {
-            List<String> listOfInputFile = Files.readAllLines(inputFile.toPath());
-            arrayOfList = listOfInputFile.toString().split(regex);
+            listOfInputFile = Files.readAllLines(inputFile.toPath());
         } catch (IOException e) {
             throw new RuntimeException("Can't read file");
         }
-        return arrayOfList;
+        return listOfInputFile;
     }
 
-    private String calculationResult(String[] arrayOfList) {
-        sumSupply = 0;
-        sumBuy = 0;
+    private String calculateReport(List<String> listOfInputFile) {
+        int sumSupply = 0;
+        int sumBuy = 0;
+        StringBuilder calculationResult = new StringBuilder();
         calculationResult.delete(0,calculationResult.length());
-        for (int i = 0; i < arrayOfList.length;i++) {
-            switch (arrayOfList[i]) {
-                case SUPPLY:
-                    sumSupply = sumSupply + Integer.parseInt(arrayOfList[i + 1]);
-                    break;
-                case BUY:
-                    sumBuy = sumBuy + Integer.parseInt(arrayOfList[i + 1]);
-                    break;
-                default:
+        for (int i = 0; i < listOfInputFile.size();i++) {
+            if (listOfInputFile.get(i).contains(SUPPLY)) {
+                sumSupply = sumSupply + Integer.parseInt(listOfInputFile.get(i).substring(7));
+            } else {
+                sumBuy = sumBuy + Integer.parseInt(listOfInputFile.get(i).substring(4));
             }
         }
-        result = sumSupply - sumBuy;
-        calculationResult.append(SUPPLY).append(COMMA).append(sumSupply).append(NEW_LINE)
-                        .append(BUY).append(COMMA).append(sumBuy).append(NEW_LINE)
-                        .append(RESULT).append(result);
-        return calculationResult.toString();
+        int result = sumSupply - sumBuy;
+        calculationResult.append(SUPPLY).append(sumSupply).append(NEW_LINE).append(BUY)
+                .append(sumBuy).append(NEW_LINE).append(RESULT).append(result);
+        String calculationResultToString = calculationResult.toString();
+        return calculationResultToString;
     }
 
-    public void getStatistic(String fromFileName, String toFileName) {
+    private void writeToFile(String calculationResultToString, String toFileName) {
         File outputFile = new File(toFileName);
         outputFile.delete();
         try {
             outputFile.createNewFile();
-            Files.write(outputFile.toPath(),calculationResult(readFile(fromFileName)).getBytes(),
+            Files.write(outputFile.toPath(),calculationResultToString.getBytes(),
                     StandardOpenOption.APPEND);
         } catch (IOException e) {
             throw new RuntimeException("Cant create and write file");
