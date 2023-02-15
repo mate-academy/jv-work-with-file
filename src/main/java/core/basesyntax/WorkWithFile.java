273a05277d4813a3d21e6477596e569d08cc9fa8
Volodymyr Prysiazhniuk <verticalsyu@gmail.com>
2023-02-15 21:39:04 +0200

    Small corrections

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,6 +1,9 @@
 package core.basesyntax;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
