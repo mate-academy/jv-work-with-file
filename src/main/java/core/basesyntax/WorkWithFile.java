498528ebb6c4c45f1830c738678e6a94eb934e2e
Andrew kluchnik <andrey.kluchnik.10@gmail.com>
2023-02-15 16:46:19 +0200

    resolved all issues

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -9,16 +9,22 @@ import java.io.IOException;
 
 public class WorkWithFile {
     private static final int TYPE_OF_OPERATION_INDEX = 0;
-    private static final int COST_INDEX = 1;
+    private static final int AMOUNT_INDEX = 1;
     private static final String SUPPLY_WORD = "supply";
     private static final String BUY_WORD = "buy";
     private static final String RESULT_WORD = "result";
-    private static final String VIRGULE_SYMBOL = ",";
+    private static final String COMMA_SYMBOL = ",";
     private static final String WHITESPACE_SYMBOL = " ";
 
     public void getStatistic(String fromFileName, String toFileName) {
+        String fileData = readDataFromFile(fromFileName);
+        String resultData = calculate(fileData, toFileName);
+        writeToFile(toFileName, resultData);
+    }
+
+    private String readDataFromFile(String fileName) {
         StringBuilder stringBuilder = new StringBuilder();
-        File from = new File(fromFileName);
+        File from = new File(fileName);
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(from))) {
             String note = bufferedReader.readLine();
             while (note != null) {
@@ -28,29 +34,31 @@ public class WorkWithFile {
         } catch (IOException e) {
             throw new RuntimeException("Can`t read from file", e);
         }
-        calculate(stringBuilder, toFileName);
+        return stringBuilder.toString();
     }
 
-    private void calculate(StringBuilder stringBuilder, String toFileName) {
+    private String calculate(String fileData, String toFileName) {
         int sumSupply = 0;
         int sumBuy = 0;
-        String[] arrayOfNotes = stringBuilder.toString().split(WHITESPACE_SYMBOL);
+        int result;
+        String[] arrayOfNotes = fileData.split(WHITESPACE_SYMBOL);
         for (String note : arrayOfNotes) {
-            if (note.split(VIRGULE_SYMBOL)[TYPE_OF_OPERATION_INDEX].equals(SUPPLY_WORD)) {
-                sumSupply += Integer.parseInt(note.split(VIRGULE_SYMBOL)[COST_INDEX]);
-            } else if (note.split(VIRGULE_SYMBOL)[TYPE_OF_OPERATION_INDEX].equals(BUY_WORD)) {
-                sumBuy += Integer.parseInt(note.split(VIRGULE_SYMBOL)[COST_INDEX]);
+            if (note.split(COMMA_SYMBOL)[TYPE_OF_OPERATION_INDEX].equals(SUPPLY_WORD)) {
+                sumSupply += Integer.parseInt(note.split(COMMA_SYMBOL)[AMOUNT_INDEX]);
+            } else if (note.split(COMMA_SYMBOL)[TYPE_OF_OPERATION_INDEX].equals(BUY_WORD)) {
+                sumBuy += Integer.parseInt(note.split(COMMA_SYMBOL)[AMOUNT_INDEX]);
             }
         }
-        writeToFile(toFileName, sumSupply, sumBuy);
+        result = sumSupply - sumBuy;
+        return SUPPLY_WORD + COMMA_SYMBOL + sumSupply + System.lineSeparator()
+                + BUY_WORD + COMMA_SYMBOL + sumBuy + System.lineSeparator()
+                + RESULT_WORD + COMMA_SYMBOL + result;
     }
 
-    private void writeToFile(String fileName, int sumSupply, int sumBuy) {
+    private void writeToFile(String fileName, String resultData) {
         File file = new File(fileName);
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
-            bufferedWriter.write(SUPPLY_WORD + VIRGULE_SYMBOL + sumSupply + System.lineSeparator()
-                    + BUY_WORD + VIRGULE_SYMBOL + sumBuy + System.lineSeparator()
-                    + RESULT_WORD + VIRGULE_SYMBOL + (sumSupply - sumBuy));
+            bufferedWriter.write(resultData);
         } catch (IOException e) {
             throw new RuntimeException("Cant write to file", e);
         }
