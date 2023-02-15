233c64bf9f790ea86ab555a1eec4c97a26ce7bbe
OlehSarapuk <olehsarapuk@gmail.com>
2023-02-15 13:12:24 +0200

    added constants and changed for loop

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,6 +7,10 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    private static final String DELIMITER = ",";
+    private static final int OPERATION_INDEX = 0;
+    private static final int AMOUNT_INDEX = 1;
+
     public void getStatistic(String fromFileName, String toFileName) {
         String fileText = readFromFile(fromFileName);
         String report = createReport(fileText);
@@ -31,12 +35,12 @@ public class WorkWithFile {
         String[] fileTextArray = fileText.split(System.lineSeparator());
         int totalSupply = 0;
         int totalBuy = 0;
-        for (int i = 0; i < fileTextArray.length; i++) {
-            String[] line = fileTextArray[i].split(",");
-            if (line[0].equals("supply")) {
-                totalSupply += Integer.valueOf(line[1]);
+        for (String line : fileTextArray) {
+            String[] lineParts = line.split(DELIMITER);
+            if (lineParts[OPERATION_INDEX].equals("supply")) {
+                totalSupply += Integer.valueOf(lineParts[AMOUNT_INDEX]);
             } else {
-                totalBuy += Integer.valueOf(line[1]);
+                totalBuy += Integer.valueOf(lineParts[AMOUNT_INDEX]);
             }
         }
         int totalResult = totalSupply - totalBuy;
