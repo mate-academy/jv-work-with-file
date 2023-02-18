15592431f18b775c4c20586b5321df2a00a88938
Nataliia <ladna1984@gmail.com>
2023-02-18 22:35:06 +0200

    Added fixes after review

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,59 +8,50 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-
-    private static final String COMMA = ",";
-    private static final String SAMPLE = " ";
-    private static final String SU = "su";
-    private static final String BU = "bu";
-    private static final String SUPPLY1 = "supply";
-    private static final String BUY1 = "buy";
+    private static final String DELIMITER = ",";
+    private static final String SEPARATOR = System.lineSeparator();
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
     private static final String RESULT = "result";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        String[] array1 = readFromFile(fromFileName);
-        String newString = getReport(array1);
-        writeReport(newString, toFileName);
+        String contents = readFromFile(fromFileName);
+        String report = getReport(contents);
+        writeReport(report, toFileName);
     }
 
-    public static String[] readFromFile(String newFile) {
-        File file = new File(newFile);
+    public static String readFromFile(String file) {
         StringBuilder builder = new StringBuilder();
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(file));
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             String value = reader.readLine();
             while (value != null) {
-                builder.append(value).append(SAMPLE);
+                builder.append(value).append(SEPARATOR);
                 value = reader.readLine();
             }
+            return builder.toString();
         } catch (IOException e) {
-            throw new RuntimeException("Can't read file", e);
+            throw new RuntimeException("Can't read the file: " + file, e);
         }
-
-        String[] array = builder.toString().split(SAMPLE);
-        return array;
     }
 
-    public static String getReport(String[] array) {
+    public static String getReport(String contents) {
         int supply = 0;
         int buy = 0;
-        for (int i = 0; i < array.length; i++) {
-            if (array[i].substring(0, 2).equals(SU)) {
-                int l = array[i].length();
-                supply = supply + Integer.parseInt(array[i].substring(7, l));
-            }
-        }
-        for (int i = 0; i < array.length; i++) {
-            if (array[i].substring(0, 2).equals(BU)) {
-                int l1 = array[i].length();
-                buy = buy + Integer.parseInt(array[i].substring(4, l1));
+        String[] dataFromFile = contents.split(SEPARATOR);
+        for (int i = 0; i < dataFromFile.length; i++) {
+            String[] dataSplit = dataFromFile[i].split(DELIMITER);
+            if (dataSplit[0].equals(SUPPLY)) {
+                supply = supply + Integer.parseInt(dataSplit[1]);
+            } else {
+                buy = buy + Integer.parseInt(dataSplit[1]);
             }
         }
         int result = supply - buy;
+
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(SUPPLY1).append(COMMA).append(supply).append(System.lineSeparator())
-                .append(BUY1).append(COMMA).append(buy).append(System.lineSeparator())
-                .append(RESULT).append(COMMA).append(result);
+        stringBuilder.append(SUPPLY).append(DELIMITER).append(supply).append(System.lineSeparator())
+                .append(BUY).append(DELIMITER).append(buy).append(System.lineSeparator())
+                .append(RESULT).append(DELIMITER).append(result);
         return stringBuilder.toString();
     }
 
@@ -69,8 +60,7 @@ public class WorkWithFile {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(newFile))) {
             writer.write(str.toString());
         } catch (IOException e) {
-            throw new RuntimeException("Can't write file", e);
+            throw new RuntimeException("Can't write report to the file: " + toFileName, e);
         }
     }
 }
-
