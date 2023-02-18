3710b26d8c861b3b855f5b3b3853c207f6458292
Volodymyr Romanyshyn <Romanvlad000@gmail.com>
2023-02-18 09:40:07 +0200

    Starting

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,20 @@
 package core.basesyntax;
 
+import java.io.*;
+
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
-
+        File file = new File(fromFileName);
+        try {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+        StringBuilder stringBuilder = new StringBuilder();
+        String value = null;
+            value = bufferedReader.readLine();
+            int commaIndex = value.indexOf(',');
+            String firstPart = stringBuilder.substring(0,commaIndex);
+            int secondPart = stringBuilder(regex\d);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read from a file", e);
+        }
     }
 }
