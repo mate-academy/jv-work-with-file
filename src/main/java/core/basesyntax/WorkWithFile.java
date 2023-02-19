87c812e368116b1bf3a4f295c4db5ccad6c43e34
Roman Mariushkin <ramzzzesik99@gmail.com>
2023-02-19 17:05:45 +0200

    finished task

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,36 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
-
+        try {
+            FileReader fileReader = new FileReader(fromFileName);
+            BufferedReader bf = new BufferedReader(fileReader);
+            String line;
+            int totalBuy = 0;
+            int totalSupply = 0;
+            while ((line = bf.readLine()) != null) {
+                String[] str = line.split(",");
+                if (str[0].equals("supply")) {
+                    totalSupply = totalSupply + Integer.valueOf(str[1]);
+                }
+                if (str[0].equals("buy")) {
+                    totalBuy = totalBuy + Integer.valueOf(str[1]);
+                }
+            }
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName));
+            bufferedWriter.write("supply," + totalSupply);
+            bufferedWriter.newLine();
+            bufferedWriter.write("buy," + totalBuy);
+            bufferedWriter.newLine();
+            bufferedWriter.write("result," + (totalSupply - totalBuy));
+            bufferedWriter.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
