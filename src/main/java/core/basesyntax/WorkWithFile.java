2ef13365e83e0ad884b8d50f653c80d2c44bd314
Anatolii Nazarov <anatoly_n@ukr.net>
2023-02-21 10:44:19 +0200

    Ready for check

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,6 +1,11 @@
 package core.basesyntax;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.File;
+import java.io.FileWriter;
 
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
@@ -8,7 +13,7 @@ public class WorkWithFile {
         int buy = 0;
         int result = 0;
         StringBuilder builderWordsAll = new StringBuilder();
-        String NOTWORD = "\\W+";
+        String notWord = "\\W+";
         try {
             BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
             String value = reader.readLine();
@@ -16,21 +21,21 @@ public class WorkWithFile {
                 builderWordsAll.append(value).append(System.lineSeparator());
                 value = reader.readLine();
             }
-        } catch (IOException e) {
+        }catch(IOException e) {
             throw new RuntimeException("Can't read file", e);
         }
-        String[] datesFile = builderWordsAll.toString().split(NOTWORD);
+        String[] datesFile = builderWordsAll.toString().split(notWord);
         for (int i = 0; i < datesFile.length; i++) {
-            if(datesFile[i].equals("supply")) {
-                supply = supply + Integer.parseInt(datesFile[i+1]);
+            if (datesFile[i].equals("supply")) {
+                supply = supply + Integer.parseInt(datesFile[i + 1]);
             }
-            if(datesFile[i].equals("buy")) {
+            if (datesFile[i].equals("buy")) {
                 buy = buy + Integer.parseInt(datesFile[i+1]);
             }
         }
         result = supply - buy;
-        String[] forReport = new String[]{"supply", String.valueOf(supply), "buy", String.valueOf(buy),
-                "result", String.valueOf(result)};
+        String[] forReport = new String[]{"supply", String.valueOf(supply),
+                "buy", String.valueOf(buy), "result", String.valueOf(result)};
         File file = new File(toFileName);
         BufferedWriter bufferedWriter = null;
         try {
@@ -45,7 +50,7 @@ public class WorkWithFile {
         } catch (IOException e) {
             throw new RuntimeException("Can't write date to file", e);
         } finally {
-            if(bufferedWriter != null) {
+            if (bufferedWriter != null) {
                 try {
                     bufferedWriter.close();
                 } catch (IOException e) {
