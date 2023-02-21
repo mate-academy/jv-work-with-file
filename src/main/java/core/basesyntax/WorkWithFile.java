71b7eb6e85ca6d6a164600612f9c9659c6f339a9
Valeriy M <valeriym250@gmail.com>
2023-02-21 11:28:20 +0200

    Created 2 methods: readFrom - method which reading data from file out - method which get array values and fileName, and writing result in file
    implemented method getStatistic - method which call "readFrom" "out"

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -5,35 +5,45 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.util.List;
+import java.util.Objects;
 
 public class WorkWithFile {
+    private static final int ARRAY_LENGTH = 2;
+
     public void getStatistic(String fromFileName, String toFileName) {
-        int supply = 0;
-        int buy = 0;
+        out(readFrom(fromFileName), toFileName);
+    }
+
+    private int[] readFrom(String fromFileName) {
+        int [] result = new int[ARRAY_LENGTH];
+        String [] temp;
+        File file = new File(fromFileName);
         try {
-            String lines = Files.readAllLines(Paths.get(fromFileName)).toString();
-            String [] line = lines.split(" ");
-            line[0] = line[0].substring(1);
-            for (int i = 0; i < line.length; i++) {
-                line[i] = line[i].substring(0, line[i].length() - 1);
-                int index1 = line[i].indexOf(',');
-                if (line[i].substring(0, index1). equals("supply")) {
-                    supply += Integer.parseInt(line[i].substring(index1 + 1));
-                    continue;
-                }
-                buy += Integer.parseInt(line[i].substring(index1 + 1));
+            List<String> strings = Files.readAllLines(file.toPath());
+            for (int i = 0; i < strings.size(); i++) {
+                temp = strings.get(i).split(",");
+                result[0] = (Objects.equals(temp[0], "supply"))
+                        ? result[0] + Integer.parseInt(temp[1]) : result[0];
+                result[1] = (Objects.equals(temp[0], "buy"))
+                        ? result[1] + Integer.parseInt(temp[1]) : result[1];
             }
         } catch (IOException e) {
-            throw new RuntimeException("Can`t read current document", e);
+            throw new RuntimeException("Can`t read file", e);
         }
+        return result;
+    }
+
+    private void out(int [] values, String toFileName) {
+        String message = "supply," + values[0] + System.lineSeparator() + "buy," + values[1]
+                + System.lineSeparator() + "result," + (values[0] - values [1]);
         File file = new File(toFileName);
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file, true))) {
-            bufferedWriter.write("supply," + supply + System.lineSeparator()
-                    + "buy," + buy + System.lineSeparator()
-                    + "result," + (supply - buy));
+        try {
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));
+            bufferedWriter.write(message);
+            bufferedWriter.close();
         } catch (IOException e) {
-            System.out.println("Can`t write data in " + toFileName + ", " + e);
+            throw new RuntimeException("Can`t write file", e);
         }
     }
 }
