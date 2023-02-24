5400e9a80d7044ea1e880a22bf2a879bd0053033
Anatolii Nazarov <anatoly_n@ukr.net>
2023-02-24 08:56:28 +0200

    Fixed

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,23 +8,35 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    private static final String COMA = ",";
+    private static final String NOTWORD = "\\W+";
+    private StringBuilder builderWordsAll = new StringBuilder();
+    private static int supply;
+    private static int buy;
+    private static int result;
     public void getStatistic(String fromFileName, String toFileName) {
-        int supply = 0;
-        int buy = 0;
-        int result = 0;
-        StringBuilder builderWordsAll = new StringBuilder();
-        String notWord = "\\W+";
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
-            String value = reader.readLine();
-            while (value != null) {
-                builderWordsAll.append(value).append(System.lineSeparator());
-                value = reader.readLine();
+        readFromFile(fromFileName);
+        writeToFile(toFileName);
+    }
+
+    public void readFromFile(String fromFileName){
+            try {
+                BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
+                String value = reader.readLine();
+                while (value != null) {
+                    builderWordsAll.append(value).append(System.lineSeparator());
+                    value = reader.readLine();
+                }
+            } catch (IOException e) {
+                throw new RuntimeException("Can't read file", e);
             }
-        } catch (IOException e) {
-            throw new RuntimeException("Can't read file", e);
-        }
-        String[] datesFile = builderWordsAll.toString().split(notWord);
+    }
+
+    public String[] createReport() {
+        supply = 0;
+        buy = 0;
+        result = 0;
+        String[] datesFile = builderWordsAll.toString().split(NOTWORD);
         for (int i = 0; i < datesFile.length; i++) {
             if (datesFile[i].equals("supply")) {
                 supply = supply + Integer.parseInt(datesFile[i + 1]);
@@ -36,13 +48,19 @@ public class WorkWithFile {
         result = supply - buy;
         String[] forReport = new String[]{"supply", String.valueOf(supply),
                 "buy", String.valueOf(buy), "result", String.valueOf(result)};
+        builderWordsAll = new StringBuilder();
+        return forReport;
+    }
+
+    public void writeToFile(String toFileName) {
+        String[] forReport = createReport();
         File file = new File(toFileName);
         BufferedWriter bufferedWriter = null;
         try {
             bufferedWriter = new BufferedWriter(new FileWriter(file, false));
             for (int i = 0; i < forReport.length; i = i + 2) {
                 bufferedWriter.write(forReport[i]);
-                bufferedWriter.write(",");
+                bufferedWriter.write(COMA);
                 bufferedWriter.write(forReport[i + 1]);
                 bufferedWriter.write(System.lineSeparator());
                 bufferedWriter.flush();
@@ -58,6 +76,5 @@ public class WorkWithFile {
                 }
             }
         }
-
     }
 }
