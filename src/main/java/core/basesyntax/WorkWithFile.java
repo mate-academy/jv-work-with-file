117d4098d6ed2d39c14dd9c562fc3351455f44d7
Oleg Ravlyk <oleg.ravlyk2005@gmail.com>
2023-02-28 03:55:07 +0200

    initial commit, added convertFileToStringArray method

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,24 @@
 package core.basesyntax;
 
+import java.io.*;
+
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
+        File readFromFile = new File(fromFileName);
+        String[] fileArray = convertFileToStringArray(readFromFile);
+    }
+    private static String[] convertFileToStringArray(File file) {
+        final String delimiter = "$Delimiter$";
 
+        try(BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            StringBuilder string = new StringBuilder();
+            while (reader.readLine() != null) {
+                string.append(reader.readLine()).append(delimiter);
+            }
+            String[] resultArray = string.toString().split(delimiter);
+            return resultArray;
+        } catch (IOException e) {
+            throw new RuntimeException("Something went wrong.", e);
+        }
     }
 }
