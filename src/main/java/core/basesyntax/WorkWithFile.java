6211deba65046d736cf75abb1521776fb7a6789b
yermilov.m <yermilov.m@hotmail.com>
2023-03-05 19:52:32 +0100

    request #876 done

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -6,41 +6,58 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class WorkWithFile {
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+    private static final int KEY_INDEX = 0;
+    private static final int VALUE_INDEX = 1;
+
     public void getStatistic(String fromFileName, String toFileName) {
-        String report = createReport(readData(fromFileName));
+        String report = createReport(calcData(readData(fromFileName)));
         writeToFile(report, toFileName);
     }
 
-    private int[] readData(String fromFileName) {
+    private ArrayList<String> readData(String fromFileName) {
         File fromFile = new File(fromFileName);
-        int supply = 0;
-        int buy = 0;
-        String[] lineValue;
+        ArrayList<String> data = new ArrayList<>();
         String nextString;
         try (BufferedReader reader = new BufferedReader(new FileReader(fromFile))) {
             nextString = reader.readLine();
             while (nextString != null) {
-                lineValue = nextString.split(",");
-                if (lineValue[0].equals("supply")) {
-                    supply += Integer.parseInt(lineValue[1]);
-                } else if (lineValue[0].equals("buy")) {
-                    buy += Integer.parseInt(lineValue[1]);
-                }
+                data.add(nextString);
                 nextString = reader.readLine();
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        int[] data = new int[]{supply, buy};
+
         return data;
     }
 
-    private String createReport(int[] data) {
+    private int[] calcData(ArrayList<String> data) {
+        int supply = 0;
+        int buy = 0;
+
+        for (String line : data) {
+            String[] lineValue;
+            lineValue = line.split(",");
+            if (lineValue[KEY_INDEX].equals(SUPPLY)) {
+                supply += Integer.parseInt(lineValue[VALUE_INDEX]);
+            } else if (lineValue[KEY_INDEX].equals(BUY)) {
+                buy += Integer.parseInt(lineValue[VALUE_INDEX]);
+            }
+        }
+
+        int[] dataArray = new int[]{supply, buy};
+        return dataArray;
+    }
+
+    private String createReport(int[] dataArray) {
         StringBuilder report = new StringBuilder("supply,");
-        int supply = data[0];
-        int buy = data[1];
+        int supply = dataArray[0];
+        int buy = dataArray[1];
         report.append(supply);
         report.append(System.lineSeparator());
         report.append("buy,");
