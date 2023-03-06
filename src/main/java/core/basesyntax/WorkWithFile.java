fa8de92309f631bf4abeef9534197f9b2cc84a34
Artem <artempharm83@gmail.com>
2023-03-06 11:53:10 +0200

    created a method to read data from a file, created a result method where it looped through the data and added the values "supply" and "buy" wrote to the result, created a method to write to a file

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,64 @@
 package core.basesyntax;
 
-public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class WorkWithFile {
+    private static final int OPERATION_INDEX = 0;
+    private static final int VALUE_INDEX = 1;
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        String fromFile = readFromFileName(fromFileName);
+        String toFile = getResult(fromFile);
+        writeToFile(toFile,toFileName);
+    }
+
+    private String readFromFileName(String fromFileName) {
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName));
+            StringBuilder stringBuilder = new StringBuilder();
+            String readLine = bufferedReader.readLine();
+            while (readLine != null) {
+                stringBuilder.append(readLine).append(System.lineSeparator());
+                readLine = bufferedReader.readLine();
+            }
+            return stringBuilder.toString();
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read data from the file" + fromFileName,e);
+        }
+    }
+
+    private String getResult(String dateFromFile) {
+        int supply = 0;
+        int buy = 0;
+
+        String[] lines = dateFromFile.split(System.lineSeparator());
+        for (String line : lines) {
+            String[] arrayLine = line.split(",");
+            if (arrayLine[OPERATION_INDEX].equals(SUPPLY)) {
+                supply += Integer.valueOf(arrayLine[VALUE_INDEX]);
+            }
+            if (arrayLine[OPERATION_INDEX].equals(BUY)) {
+                buy += Integer.valueOf(arrayLine[VALUE_INDEX]);
+            }
+        }
+       int result = supply - buy;
+        return "supply," + supply + System.lineSeparator()
+                + "buy," + buy + System.lineSeparator()
+                + "result," + result;
+    }
+
+    private void writeToFile(String toFile, String fromFile) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fromFile))) {
+            bufferedWriter.write(toFile);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write data from the file" + toFile, e);
+        }
     }
 }
+
