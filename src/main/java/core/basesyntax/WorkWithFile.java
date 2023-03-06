23c99ed296ab8e93caec30779c1427c13224f821
Volodymyr Davidov <volodymyrdavidov1@gmail.com>
2023-03-06 16:50:33 +0200

    jv-work-with-file

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,67 @@
 package core.basesyntax;
 
-public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
+import java.io.*;
 
+import static java.lang.Integer.parseInt;
+
+public class WorkWithFile {
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+    private static final String RESULT = "result";
+    private static final String COMA = ",";
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        String string = readFromFile(fromFileName);
+        String result = resultOfDataReading(string);
+        recordToFile(result,toFileName);
+    }
+
+    private String readFromFile(String fromFileName) {
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName));
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuilder.append(line).append(COMA);
+            }
+            bufferedReader.close();
+        } catch (IOException e) {
+            throw new RuntimeException("This file cannot be read ", e);
+        }
+        return stringBuilder.toString();
+    }
+
+    private String resultOfDataReading(String dataOfFile) {
+        StringBuilder stringBuilder = new StringBuilder();
+        String[] dataArray = dataOfFile.split(",");
+        int amountOfSupplies = 0;
+        int amountOfBuy = 0;
+        for (int i = 0; i < dataArray.length; i++) {
+            if (dataArray[i].equals(SUPPLY)) {
+                String value = dataArray[i + 1];
+                amountOfSupplies = amountOfSupplies + parseInt(value);
+                i++;
+            }if (dataArray[i].equals(BUY)) {
+                String value = dataArray[i + 1];
+                amountOfBuy = amountOfBuy + parseInt(value);
+                i++;
+            }
+        }
+        int result = amountOfSupplies - amountOfBuy;
+        stringBuilder.append(SUPPLY).append(COMA).append(amountOfSupplies).append(System.lineSeparator())
+                .append(BUY).append(COMA).append(amountOfBuy).append(System.lineSeparator())
+                .append(RESULT).append(COMA).append(result);
+
+        return stringBuilder.toString();
+    }
+
+    private void recordToFile(String stringForWritingToFile, String toFileName) {
+        try {
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName));
+            bufferedWriter.write(stringForWritingToFile);
+            bufferedWriter.close();
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write data to the file " + toFileName, e);
+        }
     }
 }
