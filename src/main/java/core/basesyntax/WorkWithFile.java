602d8867698480b313ed77f78f25f22b0cb44737
Vitalii Romanyuk <vitaliy37777@gmail.com>
2023-03-06 23:19:27 +0200

    fixed code style and elements of logic from Mentor comments

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -9,54 +9,59 @@ import java.io.IOException;
 public class WorkWithFile {
     private static final String SPLIT_SYMBOL = " ";
     private static final String SPLIT_COMMA = ",";
-    private static final String SUPPLY = "supply";
-    private static final String BUY = "buy";
-    private static final String RESULT = "result";
+    private static final String OPERATION_TXT_0 = "supply";
+    private static final String OPERATION_TXT_1 = "buy";
+    private static final String OPERATION_RESULT = "result";
     private static final int OPERATION = 0;
     private static final int AMOUNT = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
         String[] linesFromFile = readTheFile(fromFileName);
+        String textReport = prepareReport(linesFromFile);
+        writeIntoFile(toFileName, textReport);
+    }
+
+    private String prepareReport(String[] linesFromFile) {
+        StringBuilder report = new StringBuilder();
         int supply = 0;
         int buy = 0;
 
         for (String line : linesFromFile) {
             String[] lineElements = line.split(SPLIT_COMMA);
-            if (SUPPLY.equals(lineElements[OPERATION])) {
+            if (OPERATION_TXT_0.equals(lineElements[OPERATION])) {
                 supply += Integer.parseInt(lineElements[AMOUNT]);
             } else {
                 buy += Integer.parseInt(lineElements[AMOUNT]);
             }
         }
         int result = supply - buy;
-        writeIntoFile(toFileName, supply, buy, result);
+        report.append(OPERATION_TXT_0).append(SPLIT_COMMA).append(supply)
+                .append(System.lineSeparator())
+                .append(OPERATION_TXT_1).append(SPLIT_COMMA).append(buy)
+                .append(System.lineSeparator())
+                .append(OPERATION_RESULT).append(SPLIT_COMMA).append(result);
+        return report.toString();
     }
 
     public String[] readTheFile(String file) {
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
-            StringBuilder stringBuilder = new StringBuilder();
+            StringBuilder lineCollection = new StringBuilder();
             String line;
             while ((line = bufferedReader.readLine()) != null) {
-                stringBuilder.append(line).append(SPLIT_SYMBOL);
+                lineCollection.append(line).append(SPLIT_SYMBOL);
             }
-            String finalText = stringBuilder.toString();
+            String finalText = lineCollection.toString();
             return finalText.length() == 0 ? new String[0] : finalText.split(SPLIT_SYMBOL);
         } catch (Exception e) {
-            throw new RuntimeException("File is absent");
+            throw new RuntimeException("Can't read data from the file: " + file, e);
         }
     }
 
-    public void writeIntoFile(String file, int supply, int buy, int result) {
+    public void writeIntoFile(String file, String report) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
-            StringBuilder stringBuilder = new StringBuilder();
-            stringBuilder.append(SUPPLY).append(SPLIT_COMMA).append(supply)
-                    .append(System.lineSeparator())
-                    .append(BUY).append(SPLIT_COMMA).append(buy).append(System.lineSeparator())
-                    .append(RESULT).append(SPLIT_COMMA).append(result);
-            bufferedWriter.write("");
-            bufferedWriter.write(stringBuilder.toString());
+            bufferedWriter.write(report);
         } catch (IOException e) {
-            throw new RuntimeException("Can't write data to file", e);
+            throw new RuntimeException("Can't write data to file: " + file, e);
         }
     }
 }
