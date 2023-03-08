f71d131107648c82bf9318005667373b6ea298bb
Volodymyr Davidov <volodymyrdavidov1@gmail.com>
2023-03-08 19:30:20 +0200

    fix method, constant name and used try-with-resources

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,15 +7,15 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    private static final String DEFAULT_SUPPLY = "supply";
-    private static final String DEFAULT_BUY = "buy";
-    private static final String DEFAULT_RESULT = "result";
-    private static final String COMA = ",";
+    private static final String OPERATION_SUPPLY = "supply";
+    private static final String OPERATION_BUY = "buy";
+    private static final String OPERATION_RESULT = "result";
+    private static final String SEPARATOR  = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
         String string = readFromFile(fromFileName);
-        String result = resultOfDataReading(string);
-        recordToFile(result, toFileName);
+        String result = generateReport(string);
+        writeToFile(result, toFileName);
     }
 
     private String readFromFile(String fromFileName) {
@@ -24,7 +24,7 @@ public class WorkWithFile {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName));
             String line;
             while ((line = bufferedReader.readLine()) != null) {
-                dataToString.append(line).append(COMA);
+                dataToString.append(line).append(SEPARATOR );
             }
             bufferedReader.close();
         } catch (IOException e) {
@@ -33,37 +33,36 @@ public class WorkWithFile {
         return dataToString.toString();
     }
 
-    private String resultOfDataReading(String dataOfFile) {
+    private String generateReport(String fileData) {
         StringBuilder resultString = new StringBuilder();
-        String[] dataArray = dataOfFile.split(COMA);
+        String[] dataArray = fileData.split(SEPARATOR );
         int amountOfSupplies = 0;
         int amountOfBuy = 0;
         for (int i = 0; i < dataArray.length; i++) {
-            if (dataArray[i].equals(DEFAULT_SUPPLY)) {
+            if (dataArray[i].equals(OPERATION_SUPPLY)) {
                 String value = dataArray[i + 1];
                 amountOfSupplies = amountOfSupplies + Integer.parseInt(value);
                 i++;
             }
-            if (dataArray[i].equals(DEFAULT_BUY)) {
+            if (dataArray[i].equals(OPERATION_BUY)) {
                 String value = dataArray[i + 1];
                 amountOfBuy = amountOfBuy + Integer.parseInt(value);
                 i++;
             }
         }
         int result = amountOfSupplies - amountOfBuy;
-        resultString.append(DEFAULT_SUPPLY).append(COMA).append(amountOfSupplies)
-                .append(System.lineSeparator()).append(DEFAULT_BUY).append(COMA)
+        resultString.append(OPERATION_SUPPLY).append(SEPARATOR )
+                .append(amountOfSupplies).append(System.lineSeparator())
+                .append(OPERATION_BUY).append(SEPARATOR )
                 .append(amountOfBuy).append(System.lineSeparator())
-                .append(DEFAULT_RESULT).append(COMA).append(result);
+                .append(OPERATION_RESULT).append(SEPARATOR ).append(result);
 
         return resultString.toString();
     }
 
-    private void recordToFile(String stringForWritingToFile, String toFileName) {
-        try {
-            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName));
-            bufferedWriter.write(stringForWritingToFile);
-            bufferedWriter.close();
+    private void writeToFile(String content, String toFileName) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+            bufferedWriter.write(content);
         } catch (IOException e) {
             throw new RuntimeException("Can't write data to the file " + toFileName, e);
         }
