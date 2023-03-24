59aca0fb47fa7cc37dc0bbe93a4b71ca5ff7192d
Sergey Zavalniuk <sergzavalnyuk@gmail.com>
2023-03-24 17:57:50 +0300

    Made literals to constants; GetStatistic broke up into smaller methods; Included filenames in exception messages;

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,43 +8,65 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    private final String supply = "supply";
+    private final String buy = "buy";
+    private final String result = "result";
+
     public void getStatistic(String fromFileName, String toFileName) {
         File fromFile = new File(fromFileName);
+        StringBuilder stringBuilder = readDataFromFile(fromFile);
+        String[] arrayString = stringBuilder.toString().split("/");
+        int supply = 0;
+        int buy = 0;
+        for (String string : arrayString) {
+            String[] str = string.split(",");
+            if (str[0].equals(this.supply)) {
+                supply += Integer.parseInt(str[1]);
+            } else if (str[0].equals(this.buy)) {
+                buy += Integer.parseInt(str[1]);
+            }
+        }
+        int result = supply - buy;
+        StringBuilder stringBuilderOut = createOutputString(supply, buy, result);
+        writeDataToFile(toFileName, stringBuilderOut);
+    }
+
+    private StringBuilder readDataFromFile(File fromFile) {
+        StringBuilder stringBuilder = new StringBuilder();
         try (BufferedReader reader = new BufferedReader(new FileReader(fromFile))) {
-            StringBuilder stringBuilder = new StringBuilder();
-            try {
-                String value = reader.readLine();
-                while (value != null) {
-                    stringBuilder.append(value).append("/");
-                    value = reader.readLine();
-                }
-            } catch (IOException e) {
-                throw new RuntimeException("Can`t read line", e);
-            }
-            String[] arrayString = stringBuilder.toString().split("/");
-            int supply = 0;
-            int buy = 0;
-            for (String string : arrayString) {
-                String[] str = string.split(",");
-                if (str[0].equals("supply")) {
-                    supply += Integer.parseInt(str[1]);
-                } else if (str[0].equals("buy")) {
-                    buy += Integer.parseInt(str[1]);
-                }
-            }
-            int result = supply - buy;
-            StringBuilder stringBuilderOut = new StringBuilder();
-            stringBuilderOut.append("supply,").append(supply).append(System.lineSeparator());
-            stringBuilderOut.append("buy,").append(buy).append(System.lineSeparator());
-            stringBuilderOut.append("result,").append(result).append(System.lineSeparator());
-            File toFile = new File(toFileName);
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFile))) {
-                writer.write(stringBuilderOut.toString());
-            } catch (IOException e) {
-                throw new RuntimeException("Can`t write date to file;", e);
+            String value = reader.readLine();
+            while (value != null) {
+                stringBuilder.append(value).append("/");
+                value = reader.readLine();
             }
         } catch (IOException e) {
-            throw new RuntimeException("Can`t read file", e);
+            throw new RuntimeException("Can`t read line from file: " + fromFile, e);
         }
+        return stringBuilder;
+    }
+
+    private StringBuilder createOutputString(int supply, int buy, int result) {
+        StringBuilder stringBuilderOut = new StringBuilder();
+        stringBuilderOut.append(this.supply).append(",").append(supply)
+                .append(System.lineSeparator());
+        stringBuilderOut.append(this.buy).append(",").append(buy)
+                .append(System.lineSeparator());
+        stringBuilderOut.append(this.result).append(",").append(result)
+                .append(System.lineSeparator());
+        return stringBuilderOut;
+    }
+
+    private void writeDataToFile(String toFileName, StringBuilder stringBuilderOut) {
+        File toFile = new File(toFileName);
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFile))) {
+            writer.write(stringBuilderOut.toString());
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t write date to file: " + toFileName, e);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "WorkWithFile{}";
     }
 }
