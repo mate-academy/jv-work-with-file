88ae161c8f2a70ad01d2731fe6b670ea9539aa25
Sergey Shestaka <sergey.shestaka@gmail.com>
2023-03-26 12:17:05 +0200

    changed the type of the return value - to String in the readFile method / deleted the useless "File checkFile" variable / made all constants in uppercase and changed their names to more meaningful

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,24 +8,23 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    private static final byte buySum = 0;
-    private static final byte supplySum = 1;
-    private static final String supplyOperation = "supply";
-    private static final String buyOperation = "buy";
-    private static final String result = "result";
+    private static final byte BUY_INDEX = 0;
+    private static final byte SUPPLY_INDEX = 1;
+    private static final String SUPPLY_OPERATION = "supply";
+    private static final String BUY_OPERATION = "buy";
+    private static final String RESULT_OPERATION = "result";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        String[] fromFileData = readFile(fromFileName).toString().split("\\W+");
+        String[] fromFileData = readFile(fromFileName).split("\\W+");
         int[] totalAmounts = calculateSum(fromFileData);
         writeFile(toFileName, totalAmounts);
     }
 
-    private StringBuilder readFile(String fromFile) {
+    private String readFile(String fromFile) {
         File file = new File(fromFile);
         StringBuilder textBox = new StringBuilder();
         try (BufferedReader reader = new BufferedReader(new FileReader(fromFile))) {
-            File checkFile = new File(fromFile);
-            if (checkFile.length() == 0) {
+            if (file.length() == 0) {
                 System.out.println("File: " + fromFile + "is empty.");
             }
             int symbol = 0;
@@ -35,17 +34,17 @@ public class WorkWithFile {
         } catch (IOException e) {
             throw new RuntimeException("Can't read file: " + fromFile, e);
         }
-        return textBox;
+        return textBox.toString();
     }
 
     private int[] calculateSum(String[] dataArray) {
         int[] operationValues = {0,0};
         for (int i = 0; i < dataArray.length; i++) {
             dataArray[i] = dataArray[i].toLowerCase();
-            if (dataArray[i].equals(supplyOperation)) {
-                operationValues[supplySum] += Integer.parseInt(dataArray[i + 1]);
-            } else if (dataArray[i].equals(buyOperation)) {
-                operationValues[buySum] += Integer.parseInt(dataArray[i + 1]);
+            if (dataArray[i].equals(SUPPLY_OPERATION)) {
+                operationValues[SUPPLY_INDEX] += Integer.parseInt(dataArray[i + 1]);
+            } else if (dataArray[i].equals(BUY_OPERATION)) {
+                operationValues[BUY_INDEX] += Integer.parseInt(dataArray[i + 1]);
             }
         }
         return operationValues;
@@ -53,12 +52,12 @@ public class WorkWithFile {
 
     private void writeFile(String toFile, int[] operationsSum) {
         File file = new File(toFile);
-        int resultSum = operationsSum[supplySum] - operationsSum[buySum];
+        int resultSum = operationsSum[SUPPLY_INDEX] - operationsSum[BUY_INDEX];
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file, false))) {
-            bufferedWriter.write(supplyOperation + "," + operationsSum[supplySum]
-                    + System.lineSeparator() + buyOperation + ","
-                    + operationsSum[buySum] + System.lineSeparator()
-                    + result + "," + resultSum + System.lineSeparator());
+            bufferedWriter.write(SUPPLY_OPERATION + "," + operationsSum[SUPPLY_INDEX]
+                    + System.lineSeparator() + BUY_OPERATION + ","
+                    + operationsSum[BUY_INDEX] + System.lineSeparator()
+                    + RESULT_OPERATION + "," + resultSum + System.lineSeparator());
         } catch (IOException e) {
             throw new RuntimeException("Can't write data to file " + toFile, e);
         }
