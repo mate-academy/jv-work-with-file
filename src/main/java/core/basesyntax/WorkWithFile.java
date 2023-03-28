8833fdd202671ec30c3a233fed111ca7eff3157a
z1row <pmeysarosh2004@gmail.com>
2023-03-28 19:34:33 +0200

    Fixed import error

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,8 +2,8 @@ package core.basesyntax;
 
 import java.io.BufferedWriter;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
