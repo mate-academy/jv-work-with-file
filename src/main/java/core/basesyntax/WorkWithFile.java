175bab5cc8d8b159f06b59904d79ef0913f3fa83
z1row <pmeysarosh2004@gmail.com>
2023-03-28 19:23:46 +0200

    Implemented method getStatistic()

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,54 @@
 package core.basesyntax;
 
+import java.io.*;
+
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
+        String fileContent = fileToString(fromFileName);
+        String[] separatedItems = fileContent.split("\n");
+        int buy = 0;
+        int supply = 0;
+        for (String item : separatedItems) {
+            String[] splited = item.split(",");
+            splited[1] = splited[1].substring(0, splited[1].indexOf("\r"));
+            if (splited[0].equals("buy")) {
+                buy += Integer.parseInt(splited[1]);
+            } else {
+                supply += Integer.parseInt(splited[1]);
+            }
+        }
+        int result = supply - buy;
+        createReport(buy, supply, result, toFileName);
+        System.out.println(supply + " - " + buy + " = " + result);
+    }
 
+    private String fileToString(String fromFileName) {
+        StringBuilder stringBuilder = new StringBuilder();
+        File file = new File(fromFileName);
+        try (FileReader fileReader = new FileReader(file)) {
+            int content;
+            while ((content = fileReader.read()) != -1) {
+                stringBuilder.append((char) content);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
+        return stringBuilder.toString();
+    }
+
+    private void createReport(int buy, int supply, int result, String toFileName) {
+        File file = new File(toFileName);
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
+            bufferedWriter.write("supply," + supply + System.lineSeparator());
+            bufferedWriter.write("buy," + buy + System.lineSeparator());
+            bufferedWriter.write("result," + result + System.lineSeparator());
+        } catch (IOException e) {
+            throw new RuntimeException("Can't make report", e);
+        }
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            throw new RuntimeException("Can't create file");
+        }
     }
 }
