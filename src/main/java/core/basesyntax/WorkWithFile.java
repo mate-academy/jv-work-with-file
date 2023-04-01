c23318d92fbd75cf17e58d3bb545022e93c518e5
testSouth <southparaplan@gmail.com>
2023-04-01 20:26:53 +0300

    added constants, changed implementations methods

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,19 +1,28 @@
 package core.basesyntax;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
 
 public class WorkWithFile {
-    private StringBuilder stringBuilder;
-    private String resultString;
+    private static final String DATA_FIELD_SEPARATOR = ",";
+    private static final int OPERATION_TYPE_INDEX = 0;
+    private static final int AMOUNT_INDEX = 1;
+    private static final String SUPPLY_FIELD_NAME = "supply";
+    private static final String BUY_FIELD_NAME = "buy";
+    private static final String RESULT_FIELD_NAME = "result";
 
-    private void readDataFromFile(String nameFile) {
+    public void getStatistic(String fromFileName, String toFileName) {
+        writeToFile(toFileName, processData(readDataFromFile(fromFileName)));
+    }
+
+    private String[] readDataFromFile(String nameFile) {
         File file = new File(nameFile);
+        StringBuilder stringBuilder = new StringBuilder();
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-            stringBuilder = new StringBuilder();
             String value = reader.readLine();
             while (value != null) {
                 stringBuilder.append(value).append(System.lineSeparator());
@@ -22,37 +31,36 @@ public class WorkWithFile {
         } catch (IOException e) {
             throw new RuntimeException("Cant read file", e);
         }
+        return stringBuilder.toString().split(System.lineSeparator());
     }
 
-    private void processData() {
-        int sumSupply = 0;
-        int sumBuy = 0;
-        String[] split = stringBuilder.toString().split("\\W+");
-        for (int i = 0; i < split.length; i++) {
-            if (split[i].equals("supply")) {
-                sumSupply += Integer.parseInt(split[i + 1]);
-            }
-            if (split[i].equals("buy")) {
-                sumBuy += Integer.parseInt(split[i + 1]);
+    private String processData(String[] data) {
+        int totalSupply = 0;
+        int totalBuy = 0;
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String record : data) {
+            String[] parsedRecord = record.split(DATA_FIELD_SEPARATOR);
+            if (parsedRecord[OPERATION_TYPE_INDEX].equals(SUPPLY_FIELD_NAME)) {
+                totalSupply += Integer.parseInt(parsedRecord[AMOUNT_INDEX]);
+            } else if (parsedRecord[OPERATION_TYPE_INDEX].equals(BUY_FIELD_NAME)) {
+                totalBuy += Integer.parseInt(parsedRecord[AMOUNT_INDEX]);
             }
         }
-        resultString = "supply," + sumSupply + System.lineSeparator() + "buy," + sumBuy
-                + System.lineSeparator() + "result," + (sumSupply - sumBuy);
+        int result = totalSupply - totalBuy;
+        return stringBuilder.append(SUPPLY_FIELD_NAME).append(DATA_FIELD_SEPARATOR)
+                .append(totalSupply).append(System.lineSeparator())
+                .append(BUY_FIELD_NAME).append(DATA_FIELD_SEPARATOR)
+                .append(totalBuy).append(System.lineSeparator())
+                .append(RESULT_FIELD_NAME).append(DATA_FIELD_SEPARATOR).append(result).toString();
+
     }
 
-    private void writeToFile(String toFile) {
+    private void writeToFile(String toFile, String fromData) {
         File fileOut = new File(toFile);
-        try {
-            Files.write(fileOut.toPath(), resultString.getBytes());
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileOut))) {
+            bufferedWriter.write(fromData);
         } catch (IOException e) {
             throw new RuntimeException("Cant write file", e);
         }
     }
-
-    public void getStatistic(String fromFileName, String toFileName) {
-        readDataFromFile(fromFileName);
-        processData();
-        writeToFile(toFileName);
-    }
-
 }
