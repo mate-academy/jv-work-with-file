06d116c66b00e71b5e09b3d49ec060b20a510469
Serhii Serdiuk <serhii.serdiuk@gmail.com>
2023-04-02 21:51:48 +0200

    Rewrote three methods: readFile, generateReport, writeToFile

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,67 +8,50 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
 
 public class WorkWithFile {
-    static final int INDEX_ZERO = 0;
-    static final int INDEX_ONE = 1;
+    int buy;
+    int supply;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        ArrayList<String> strings = new ArrayList<>();
-        readFile(strings, fromFileName);
-        generateReport(strings);
-        writeToFile(strings, toFileName);
+        String data = readFile(fromFileName);
+        String report = generateReport(data, supply, buy);
+        writeToFile(report, toFileName);
     }
 
-    public void readFile(ArrayList<String> array, String fileName) {
+    public String readFile(String fileName) {
+        StringBuilder builder = new StringBuilder();
         try {
             try (InputStream inputStream = new FileInputStream(fileName);
                     BufferedReader bufferedReader = new BufferedReader(
                             new InputStreamReader(inputStream))) {
                 while (bufferedReader.ready()) {
-                    array.add(bufferedReader.readLine());
+                    String line = bufferedReader.readLine();
+                    if (line.contains("supply")) {
+                        supply += Integer.parseInt(line.replaceAll("[a-zA-Z\\p{Punct}]", ""));
+                    }else {
+                        buy += Integer.parseInt(line.replaceAll("[a-zA-Z\\p{Punct}]", ""));
+                    }
                 }
             }
         } catch (IOException e) {
             throw new RuntimeException("Can't read file: " + fileName, e);
         }
+        return builder.append("supply,").append(supply).append(System.lineSeparator())
+                .append("buy,").append(buy).toString();
     }
 
-    public void generateReport(ArrayList<String> array) {
-        for (int i = 0; i < array.size(); i++) {
-            for (int j = i + 1; j < array.size(); j++) {
-                String[] currentStrings = array.get(i).split(",");
-                String[] nextString = array.get(j).split(",");
-                if (currentStrings[INDEX_ZERO].equals(nextString[INDEX_ZERO])) {
-                    int sum = (Integer.parseInt(currentStrings[INDEX_ONE]))
-                            + (Integer.parseInt(nextString[1]));
-                    array.set(i, currentStrings[INDEX_ZERO] + "," + sum);
-                    array.remove(j);
-                    j--;
-                }
-            }
-        }
-        if (array.get(INDEX_ZERO).length() < array.get(INDEX_ONE).length()) {
-            Collections.swap(array, INDEX_ZERO, INDEX_ONE);
-        }
-        StringBuilder result = new StringBuilder();
-        String[] supply = array.get(INDEX_ZERO).split(",");
-        String[] buy = array.get(INDEX_ONE).split(",");
-        int difference = Integer.parseInt(supply[INDEX_ONE]) - Integer.parseInt(buy[1]);
-        result.append("result").append(",").append(difference);
-        array.add(result.toString());
+    public String generateReport(String data, int supply, int buy) {
+        int difference =  supply - buy;
+        return data + System.lineSeparator() + "result" + "," + difference;
     }
 
-    public void writeToFile(ArrayList<String> array, String fileName) {
+    public void writeToFile(String report, String fileName) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName, true))) {
             File file = new File(fileName);
             if (file.length() == 0) {
-                for (String toWrite : array) {
-                    bufferedWriter.write(toWrite + System.lineSeparator());
-                    bufferedWriter.flush();
-                }
+                bufferedWriter.write(report);
+                bufferedWriter.flush();
             }
         } catch (IOException e) {
             throw new RuntimeException("Can't write to file: " + fileName, e);
