9580d80460f3b1bc91ce77e7b4ea5c2d5089a770
Mutaborr <123407682+Mutaborr@users.noreply.github.com>
2023-04-04 21:26:07 +0300

    Delete Main.java

--- src/main/java/core/basesyntax/Main.java
+++
@@ -1,9 +0,0 @@
-package core.basesyntax;
-
-public class Main {
-    public static void main(String[] args) {
-        WorkWithFile workWithFile = new WorkWithFile();
-        workWithFile.getStatistic("banana.csv","bananaResult.csv");
-    }
-}
-
