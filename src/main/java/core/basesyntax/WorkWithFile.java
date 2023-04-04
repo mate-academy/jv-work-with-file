3ad4c681753be8ea7841057e45755f859dd502c0
Mutaborr <123407682+Mutaborr@users.noreply.github.com>
2023-04-04 21:26:32 +0300

    jv-work-with-file-05
    jv-work-with-file-05

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,50 +1,24 @@
 package core.basesyntax;
 
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 
 public class WorkWithFile {
-
-    private List<String> readFromFile(String fromFileName) {
-        List<String> data;
-        File file = new File(fromFileName);
-        try {
-            data = Files.readAllLines(file.toPath());
-        } catch (IOException e) {
-            throw new RuntimeException("Can`t read file", e);
-        }
-        return data;
-    }
-
-    private String balanceCalculate(List<String> data) {
-        int supplySum = 0;
-        int buySum = 0;
-        final String separator = System.lineSeparator();
-        for (String element : data) {
-            String[] elements = element.split(",");
-            if (elements[0].equals("supply")) {
-                supplySum += Integer.parseInt(elements[1]);
-            } else if (elements[0].equals("buy")) {
-                buySum += Integer.parseInt(elements[1]);
-            }
-        }
-        int result = supplySum - buySum;
-        return ("supply," + supplySum + separator
-                + "buy," + buySum + separator
-                + "result," + result);
-    }
+    static final int ZEROELEMENT = 0;
+    static final int FIRSTELEMENT = 1;
+    static final String SUPPLY = "supply";
+    static final String BUY = "buy";
+    static final String RESULT = "result";
+    static final String COMA = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        String result = "";
         List<String> data = readFromFile(fromFileName);
-        if (data != null) {
-            result = balanceCalculate(data);
-        }
-        writeToFile(toFileName, result);
+        String report = prepareReport(data);
+        writeToFile(toFileName, report);
     }
 
     public void writeToFile(String toFileName, String statistic) {
@@ -53,8 +27,34 @@ public class WorkWithFile {
             bufferedWriter.write(statistic);
             bufferedWriter.close();
         } catch (IOException e) {
-            throw new RuntimeException("Can`t write data to file", e);
+            throw new RuntimeException("Can`t write data to file: " + toFileName, e);
         }
+    }
+
+    private List<String> readFromFile(String fromFileName) {
+        try {
+            return Files.readAllLines(Path.of(fromFileName));
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t read file: " + fromFileName, e);
+        }
+    }
+
+    private String prepareReport(List<String> data) {
+        int supplySum = 0;
+        int buySum = 0;
+        final String separator = System.lineSeparator();
+        for (String element : data) {
+            String[] elements = element.split(COMA);
+            if (elements[ZEROELEMENT].equals(SUPPLY)) {
+                supplySum += Integer.parseInt(elements[FIRSTELEMENT]);
+            } else if (elements[ZEROELEMENT].equals(BUY)) {
+                buySum += Integer.parseInt(elements[FIRSTELEMENT]);
+            }
+        }
+        int result = supplySum - buySum;
+        return (SUPPLY + COMA + supplySum + separator
+                + BUY + COMA + buySum + separator
+                + RESULT + COMA + result);
     }
 }
 
