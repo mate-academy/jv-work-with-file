31fa333c9272c676089cb562642589f5b2691891
Nikita Sachik <nikitasachik@gmail.com>
2023-04-06 06:15:15 +0300

    Split task by 3 methods

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,53 @@
 package core.basesyntax;
 
-public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class WorkWithFile {
+    private static final String SPLIT_BY_THIS_STRING = "\\W+";
+    private static final int EMPTY_STRING_LENGTH = 0;
+    private final StringBuilder stringBuilder = new StringBuilder();
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        int buyCounter = 0;
+        int supplyCounter = 0;
+        readInformationFromFile(fromFileName);
+        String[] buyAndSupplyArray = stringBuilder.toString().split(SPLIT_BY_THIS_STRING);
+        for (int i = 0; i < buyAndSupplyArray.length; i += 2) {
+            if (buyAndSupplyArray[i].equals("supply")) {
+                supplyCounter += Integer.parseInt(buyAndSupplyArray[i + 1]);
+            } else if (buyAndSupplyArray[i].equals("buy")) {
+                buyCounter += Integer.parseInt(buyAndSupplyArray[i + 1]);
+            }
+        }
+        int result = supplyCounter - buyCounter;
+        stringBuilder.setLength(EMPTY_STRING_LENGTH);
+        stringBuilder.append("supply,").append(supplyCounter).append(System.lineSeparator())
+                        .append("buy,").append(buyCounter).append(System.lineSeparator())
+                        .append("result,").append(result);
+        writeInformationToFile(toFileName);
+    }
+
+    private void readInformationFromFile(String fromFileName) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
+            String currentStringInFile = bufferedReader.readLine();
+            while (currentStringInFile != null) {
+                stringBuilder.append(currentStringInFile).append(" ");
+                currentStringInFile = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void writeInformationToFile(String toFileName) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+            bufferedWriter.write(stringBuilder.toString());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
