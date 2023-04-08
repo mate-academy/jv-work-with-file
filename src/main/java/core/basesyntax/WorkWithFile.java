d031910bf3743b587517fd88a3438242c7c5f845
vrnvv <voropiano@gmail.com>
2023-04-08 22:37:56 +0300

    add new solution

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,53 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.management.RuntimeErrorException;
+
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
+        String result = stringResultFromCsv(fromFileName);
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+            bufferedWriter.write(result);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write file", e);
+        }
 
+    }
+
+    private String stringResultFromCsv(String fromFileName) {
+        int sumOfsupply = 0;
+        int sumOfBuy = 0;
+        StringBuilder stringBuilder = new StringBuilder();
+        File file = new File(fromFileName);
+        try {
+            List<String> list = Files.readAllLines(file.toPath());
+            for(String line : list) {
+                String[] lineSplit = line.split(",");
+                if (lineSplit[0].equals("supply")) {
+                    sumOfsupply += Integer.parseInt(lineSplit[1]);
+                }
+                else {
+                    sumOfBuy += Integer.parseInt(lineSplit[1]);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("File not found", e);
+        } catch (IOException e) {
+            throw new RuntimeException("Сan't read file", e);
+        }
+        return stringBuilder.append("supply,").append(sumOfsupply).append(System.lineSeparator())
+            .append("buy,").append(sumOfBuy).append(System.lineSeparator())
+            .append("result,").append(sumOfsupply - sumOfBuy).toString();
     }
 }
