3f6768e87cb104ad4deb9e74f1219d4ba4ba5b90
vrnvv <voropiano@gmail.com>
2023-04-12 20:55:01 +0300

    Added constants and fixed text in exception

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -9,38 +9,55 @@ import java.nio.file.Files;
 import java.util.List;
 
 public class WorkWithFile {
+    private static final int ZEROELEMENT = 0;
+    private static final int FIRSTELEMENT = 1;
+    private static final String COMMA = ",";
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+    private static final String RESULT = "result";
+    
     public void getStatistic(String fromFileName, String toFileName) {
-        String result = stringResultFromCsv(fromFileName);
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
-            bufferedWriter.write(result);
-        } catch (IOException e) {
-            throw new RuntimeException("Can't write file", e);
-        }
-
+        List<String> dataFromFile = readFile(fromFileName);
+        String report = generateReport(dataFromFile);
+        writeToFile(report, toFileName);
     }
 
-    private String stringResultFromCsv(String fromFileName) {
-        int sumOfsupply = 0;
-        int sumOfBuy = 0;
-        StringBuilder stringBuilder = new StringBuilder();
+    private void writeToFile(String report, String toFileName) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+            bufferedWriter.write(report);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write to file" + toFileName, e);
+        }
+    }
+
+    private List<String> readFile(String fromFileName) {
         File file = new File(fromFileName);
+        List<String> dataFromFile;
         try {
-            List<String> list = Files.readAllLines(file.toPath());
-            for (String line : list) {
-                String[] lineSplit = line.split(",");
-                if (lineSplit[0].equals("supply")) {
-                    sumOfsupply += Integer.parseInt(lineSplit[1]);
-                } else {
-                    sumOfBuy += Integer.parseInt(lineSplit[1]);
-                }
-            }
+            dataFromFile = Files.readAllLines(file.toPath());
         } catch (FileNotFoundException e) {
             throw new RuntimeException("File not found", e);
-        } catch (IOException e) {
-            throw new RuntimeException("Сan't read file", e);
         }
-        return stringBuilder.append("supply,").append(sumOfsupply).append(System.lineSeparator())
-            .append("buy,").append(sumOfBuy).append(System.lineSeparator())
-            .append("result,").append(sumOfsupply - sumOfBuy).toString();
+        catch (IOException e) {
+            throw new RuntimeException("Failed to read file: ", e);
+        }
+        return dataFromFile;
+    }
+
+    private String generateReport(List<String> dataFromFile) {
+        int sumOfsupply = 0;
+        int sumOfBuy = 0;
+        StringBuilder reportData = new StringBuilder();
+        for (String line : dataFromFile) {
+            String[] lineSplit = line.split(COMMA);
+            if (lineSplit[ZEROELEMENT].equals(SUPPLY)) {
+                sumOfsupply += Integer.parseInt(lineSplit[FIRSTELEMENT]);
+            } else {
+                sumOfBuy += Integer.parseInt(lineSplit[FIRSTELEMENT]);
+            }
+        }
+        return reportData.append(SUPPLY + COMMA).append(sumOfsupply).append(System.lineSeparator())
+            .append(BUY + COMMA).append(sumOfBuy).append(System.lineSeparator())
+            .append(RESULT + COMMA).append(sumOfsupply - sumOfBuy).toString();
     }
 }
