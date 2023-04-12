7eabdd7274a5afef8a48e98a7f971269474c68e7
Artem <artemenko.as@gmail.com>
2023-04-12 18:24:35 +0300

    All change requests was made. Substring was replaced by split. Created 3 different methods for different purposes: readFile - only reads content from file; generateReport - only generate report in format we need; writeFile - only writes report to destination file; getStatistic - calls for methods above; mvn clean package passed, all tests passed.

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -10,40 +10,60 @@ import java.io.IOException;
 
 public class WorkWithFile {
     public static final String SEPARATE = System.lineSeparator();
+    public static final String SPLITTER = ",";
+    public static final String SUPPLY = "supply";
+    public static final String BUY = "buy";
 
-    public void getStatistic(String fromFileName, String toFileName) {
-        int supply = 0;
-        int buy = 0;
-        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
+    public String readFile(String fileName) {
+        StringBuilder fileContent = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             String line;
             while ((line = reader.readLine()) != null) {
-                int index = line.indexOf(",");
-                int length = line.substring(index).length();
-                int value = Integer.parseInt(line.substring(index + 1, index + length));
-                if (line.substring(0, index).equals("supply")) {
-                    supply += value;
-                }
-                if (line.substring(0, index).equals("buy")) {
-                    buy += value;
-                }
+                fileContent.append(line)
+                        .append(System.lineSeparator());
             }
         } catch (FileNotFoundException e) {
-            throw new RuntimeException("File not found " + fromFileName, e);
+            throw new RuntimeException("Can't find file " + fileName, e);
         } catch (IOException e) {
-            throw new RuntimeException("Can't read file " + fromFileName, e);
+            throw new RuntimeException("Can't read file " + fileName, e);
+        }
+        return fileContent.toString();
+    }
+
+    public String generateReport(String fileContent) {
+        int supply = 0;
+        int buy = 0;
+        String[] lines = fileContent.split(SEPARATE);
+        for (String line : lines) {
+            String[] split = line.split(SPLITTER);
+            if (split[0].equals(SUPPLY)) {
+                supply += Integer.parseInt(split[1]);
+            }
+            if (split[0].equals(BUY)) {
+                buy += Integer.parseInt(split[1]);
+            }
         }
         int result = supply - buy;
-        String content = "supply," + supply + SEPARATE
+        return "supply," + supply + SEPARATE
                 + "buy," + buy + SEPARATE
                 + "result," + result;
-        File file = new File(toFileName);
+    }
+
+    public void writeToFile(String report, String fileName) {
+        File file = new File(fileName);
         BufferedWriter writer;
-        try (FileWriter fWriter = new FileWriter(file)) {
-            writer = new BufferedWriter(fWriter);
-            writer.write(content);
+        try (FileWriter fileWriter = new FileWriter(file)) {
+            writer = new BufferedWriter(fileWriter);
+            writer.write(report);
             writer.flush();
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Can't write file " + fileName, e);
         }
+    }
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        String fileContent = readFile(fromFileName);
+        String report = generateReport(fileContent);
+        writeToFile(report, toFileName);
     }
 }
