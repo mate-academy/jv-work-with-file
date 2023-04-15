fd7f880922206232fd9c513e577d96a928c6e4ef
Sieryi D <hunterst@ukr.net>
2023-04-15 05:52:55 +0300

    first commit

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -9,10 +9,10 @@ import java.io.IOException;
 
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
-        File file = new File(fromFileName);
+        File fileForRead = new File(fromFileName);
         StringBuilder builder = new StringBuilder();
         try {
-            FileReader fileReader = new FileReader(file);
+            FileReader fileReader = new FileReader(fileForRead);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
             int value = bufferedReader.read();
             while (value != -1) {
@@ -25,22 +25,22 @@ public class WorkWithFile {
         String dataString = builder.toString();
         String[] dataStrings = dataString.split("\r\n");
         String[] forSolution = new String[2];
-        int supply = 0;
-        int buy = 0;
+        int variableSupply = 0;
+        int variableBuy = 0;
         for (int a = 0; a < dataStrings.length; a++) {
             forSolution = dataStrings[a].split(",");
             int b = Integer.valueOf(forSolution[1]);
             if (forSolution[0].equals("supply")) {
-                supply += b;
+                variableSupply += b;
             } else if (forSolution[0].equals("buy")) {
-                buy += b;
+                variableBuy += b;
             }
         }
-        int result = supply - buy;
+        int variableResult = variableSupply - variableBuy;
         File fileForWrite = new File(toFileName);
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileForWrite))) {
-            bufferedWriter.write("supply," + supply + "\r\n" + "buy," + buy + "\r\n"
-                    + "result," + result);
+            bufferedWriter.write("supply," + variableSupply + "\r\n" + "buy,"
+                    + variableBuy + "\r\n" + "result," + variableResult);
         } catch (IOException e) {
             throw new RuntimeException("can't create BufferedWriter", e);
         }
