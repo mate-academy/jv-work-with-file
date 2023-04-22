5813aa805771d33f038a19617b5f9de46a074294
Ros <rospsix@gmail.com>
2023-04-22 19:55:57 +0100

    I decided to use try-with-resources to be unsure that the BufferedReader and BufferedWriter object are closed after use.
    for reading file I use BufferedReader
    for write data to file I use BufferedWriter.
    
    Created variables in which I save Supply value and Buy value.
    
    Splits each line into two parts separated by comma.First part save as string variable "operation" and second one as a integer and store in "amount" variable.
    
    I use switch statment to increment totalSupply or totalBuy.
    
    After int variable "result" computes difference between totalSupply and totalBuy.
    
    In the end using write() method to write info to new file.

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,38 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
-
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
+                BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
+            int totalSupply = 0;
+            int totalBuy = 0;
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(",");
+                String operation = parts[0].trim();
+                int amount = Integer.parseInt(parts[1].trim());
+                switch (operation) {
+                    case "supply":
+                        totalSupply += amount;
+                        break;
+                    case "buy":
+                        totalBuy += amount;
+                        break;
+                    default:
+                }
+            }
+            int result = totalSupply - totalBuy;
+            writer.write("supply," + totalSupply + System.lineSeparator());
+            writer.write("buy," + totalBuy + System.lineSeparator());
+            writer.write("result," + result);
+        } catch (IOException e) {
+            throw new RuntimeException("Error reading/writing file", e);
+        }
     }
 }
