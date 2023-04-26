fad95a9d2f4a6a61aa9b3e6962fe4c181ad18fa8
Vladyslav Dovhopolyi <dovhopolii@gmail.com>
2023-04-26 19:00:12 +0200

    classes aare imported

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,6 +1,10 @@
 package core.basesyntax;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) throws IOException {
