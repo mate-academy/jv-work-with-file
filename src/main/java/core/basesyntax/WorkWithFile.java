88fda12611510881759e4b7c81d0c8469da06105
Yevhen Tolkunov <yevhentolkunov@gmail.com>
2023-04-26 17:00:09 +0300

    checkstyle v2

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,7 +2,6 @@ package core.basesyntax;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -14,51 +13,46 @@ public class WorkWithFile {
     private static final String COMMA = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        String[] infoFromFileName = getInfoFromFileName(fromFileName);
-        String reportSupplyAndBuy = getInfoSupplyAndBuy(infoFromFileName);
-        writeToFile(toFileName, reportSupplyAndBuy);
+        String[] infoFromFileName = readFile(fromFileName);
+        String reportSupplyAndBuy = getReport(infoFromFileName);
+        writeFile(toFileName, reportSupplyAndBuy);
     }
 
-    public String[] getInfoFromFileName(String fromFileName) {
-        File file = new File(fromFileName);
+    private String[] readFile(String fileName) {
         StringBuilder stringBuilder = new StringBuilder();
-        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             String value = reader.readLine();
             while (value != null) {
                 stringBuilder.append(value).append(System.lineSeparator());
                 value = reader.readLine();
             }
         } catch (IOException e) {
-            throw new RuntimeException("Can't read file - " + fromFileName, e);
+            throw new RuntimeException("Can't read file - " + fileName, e);
         }
-        String sentence = stringBuilder.toString();
-        return sentence.split(System.lineSeparator());
+        return stringBuilder.toString().split(System.lineSeparator());
     }
 
-    public String getInfoSupplyAndBuy(String[] split) {
-        int index;
+    private String getReport(String[] information) {
         int sumSupply = 0;
         int sumBuy = 0;
-        StringBuilder builder = new StringBuilder();
-        for (String word : split) {
-            index = word.indexOf(COMMA);
-            if (word.substring(0, index).equals(SUPPLY)) {
-                sumSupply += Integer.parseInt(word.substring(index + 1));
+        for (String sentence : information) {
+            String[] word = sentence.split(",");
+            if (word[0].equals(SUPPLY)) {
+                sumSupply += Integer.parseInt(word[1]);
             } else {
-                sumBuy += Integer.parseInt(word.substring(index + 1));
+                sumBuy += Integer.parseInt(word[1]);
             }
         }
-        builder.append(SUPPLY).append(COMMA).append(sumSupply).append(System.lineSeparator())
-                .append(BUY).append(COMMA).append(sumBuy).append(System.lineSeparator())
-                .append(RESULT).append(COMMA).append(sumSupply - sumBuy);
-        return builder.toString();
+        return (SUPPLY) + (COMMA) + (sumSupply) + (System.lineSeparator())
+                + (BUY) + (COMMA) + (sumBuy) + (System.lineSeparator())
+                + (RESULT) + (COMMA) + (sumSupply - sumBuy);
     }
 
-    public void writeToFile(String toFileName, String recordingInfo) {
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
-            bufferedWriter.write(recordingInfo);
+    private void writeFile(String fileName, String report) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName))) {
+            bufferedWriter.write(report);
         } catch (IOException e) {
-            throw new RuntimeException("Can`t write to file - " + toFileName, e);
+            throw new RuntimeException("Can`t write to file - " + fileName, e);
         }
     }
 }
