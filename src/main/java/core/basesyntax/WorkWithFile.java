fc5b8c73b3179b5e9a0c81b3f8f6833e77e83c68
Gamemod13 <igorfetov1997@gmail.com>
2023-04-26 18:16:32 +0300

    Fixed PR mistakes

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,19 +7,30 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.StandardOpenOption;
-import java.util.HashMap;
-import java.util.Map;
 
 public class WorkWithFile {
-    private static final String NAME_OF_FIRST_LINE = "supply";
-    private static final String NAME_OF_SECOND_LINE = "buy";
-    private static final String NAME_OF_THIRD_LINE = "result";
-    private static final String COMMA = ",";
-    private static final int NAME_INDEX = 0;
-    private static final int AMMOUNT_INDEX = 1;
+    private static final String NAME_OF_FIRST_LINE;
+    private static final String NAME_OF_SECOND_LINE;
+    private static final String NAME_OF_THIRD_LINE;
+    private static final String COMMA;
+    private static final int SUPPLY_INDEX;
+    private static final int BUY_INDEX;
+    private static final int RESULT_INDEX;
+    private static final int NAME_INDEX;
+    private static final int AMOUNT_INDEX;
+
+    static {
+        NAME_OF_FIRST_LINE = "supply";
+        NAME_OF_SECOND_LINE = "buy";
+        NAME_OF_THIRD_LINE = "result";
+        COMMA = ",";
+        NAME_INDEX = SUPPLY_INDEX = 0;
+        AMOUNT_INDEX = BUY_INDEX = 1;
+        RESULT_INDEX = 2;
+    }
 
     public void getStatistic(String fromFileName, String toFileName) {
-        String entity = systemizeEntity(parseLinesFromFile(fromFileName));
+        String entity = formTable(parseLinesFromFile(fromFileName));
         writeToFile(entity, toFileName);
     }
 
@@ -29,31 +40,43 @@ public class WorkWithFile {
             Files.write(file.toPath(), entity.getBytes(StandardCharsets.UTF_8),
                     StandardOpenOption.CREATE);
         } catch (IOException e) {
-            throw new RuntimeException("File cannot be written", e);
+            throw new RuntimeException("File cannot be written in file: " + toFileName, e);
         }
     }
 
-    private String systemizeEntity(String[] parsedLines) {
-        Map<String, String> consolidatedMap = new HashMap<>();
+    private String formTable(String[] parsedLines) {
+        String[][] stringArraySheet = new String[RESULT_INDEX + 1][AMOUNT_INDEX + 1];
+        stringArraySheet[SUPPLY_INDEX][NAME_INDEX] = NAME_OF_FIRST_LINE;
+        stringArraySheet[BUY_INDEX][NAME_INDEX] = NAME_OF_SECOND_LINE;
+        stringArraySheet[RESULT_INDEX][NAME_INDEX] = NAME_OF_THIRD_LINE;
         for (String line : parsedLines) {
-            String[] parts = line.split(COMMA);
-            consolidatedMap.put(parts[NAME_INDEX], consolidatedMap.get(parts[NAME_INDEX]) == null
-                    ? parts[AMMOUNT_INDEX] :
-                    String.valueOf(Integer.parseInt(consolidatedMap.get(parts[NAME_INDEX]))
-                            + Integer.parseInt(parts[AMMOUNT_INDEX])));
+            updateSheet(stringArraySheet, line);
         }
-        return tableFormation(consolidatedMap);
+        return formTable(stringArraySheet);
     }
 
-    private String tableFormation(Map<String, String> contentMap) {
-        String resultSum = String.valueOf(Integer.parseInt(contentMap.get(NAME_OF_FIRST_LINE))
-                - Integer.parseInt(contentMap.get(NAME_OF_SECOND_LINE)));
-        return NAME_OF_FIRST_LINE + COMMA + contentMap.get(NAME_OF_FIRST_LINE)
+    private String formTable(String[][] stringArraySheet) {
+        String resultSum = String.valueOf(Integer.parseInt(
+                stringArraySheet[SUPPLY_INDEX][AMOUNT_INDEX])
+                - Integer.parseInt(stringArraySheet[BUY_INDEX][AMOUNT_INDEX]));
+        return NAME_OF_FIRST_LINE + COMMA + stringArraySheet[SUPPLY_INDEX][AMOUNT_INDEX]
                 + System.lineSeparator()
-                + NAME_OF_SECOND_LINE + COMMA + contentMap.get(NAME_OF_SECOND_LINE)
+                + NAME_OF_SECOND_LINE + COMMA + stringArraySheet[BUY_INDEX][AMOUNT_INDEX]
                 + System.lineSeparator()
                 + NAME_OF_THIRD_LINE + COMMA + resultSum
                 + System.lineSeparator();
+    }
+
+    private void updateSheet(String[][] sheet, String line) {
+        String[] parts = line.split(COMMA);
+        for (int i = 0; i < sheet.length; i++) {
+            if (sheet[i][NAME_INDEX].equals(parts[NAME_INDEX])) {
+                sheet[i][AMOUNT_INDEX] = (sheet[i][AMOUNT_INDEX] != null)
+                        ? String.valueOf(Integer.parseInt(sheet[i][AMOUNT_INDEX])
+                        + Integer.parseInt(parts[AMOUNT_INDEX]))
+                        : parts[AMOUNT_INDEX];
+            }
+        }
     }
 
     private String[] parseLinesFromFile(String fromFileName) {
@@ -66,7 +89,7 @@ public class WorkWithFile {
             }
             return lines.toString().split(System.lineSeparator());
         } catch (IOException e) {
-            throw new RuntimeException("File cannot be read!", e);
+            throw new RuntimeException("File cannot be read from file: " + fromFileName, e);
         }
     }
 }
