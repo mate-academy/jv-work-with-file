fc75eb7e700955a09ccbdcccfc2702ca64aa067c
MykytaKat <129322235+MykytaKat@users.noreply.github.com>
2023-04-26 15:45:48 +0200

    Update WorkWithFile.java

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,42 +7,56 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    private static final String SUPLY_TEXT = "supply";
+    private static final String BUY_TEXT = "buy";
+    private static final String RESULT_TEXT = "result";
+
     public void getStatistic(String fromFileName, String toFileName) {
-        int supplyTotal = 0;
-        int buyTotal = 0;
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
+        String[] data = readFromFile(fromFileName);
+        String[] calculated = calculate(data);
+        writeToFile(toFileName, calculated);
+    }
+
+    private String[] readFromFile(String fileName) {
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             String line = reader.readLine();
             while (line != null) {
-                String[] arr = line.split(",");
-                int amount = Integer.parseInt(arr[1]);
-                String op = arr[0];
-                if (op.equals("supply")) {
-                    supplyTotal += amount;
-                } else if (op.equals("buy")) {
-                    buyTotal += amount;
-                }
+                stringBuilder.append(line).append(System.lineSeparator());
                 line = reader.readLine();
             }
         } catch (IOException e) {
-            throw new RuntimeException("Can`t read file.", e);
+            throw new RuntimeException("Can't read file " + fileName, e);
         }
-        BufferedWriter bufferedWriter = null;
-        try {
-            bufferedWriter = new BufferedWriter(new FileWriter(toFileName));
-            bufferedWriter.write("supply," + supplyTotal + System.lineSeparator());
-            bufferedWriter.write("buy," + buyTotal + System.lineSeparator());
-            bufferedWriter.write("result," + (supplyTotal - buyTotal) + System.lineSeparator());
+        return stringBuilder.toString().split(System.lineSeparator());
+    }
+
+    private void writeToFile(String fileName, String[] calculate) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName))) {
+            for (String data : calculate) {
+                bufferedWriter.write(data);
+                bufferedWriter.write(System.lineSeparator());
+            }
         } catch (IOException e) {
-            throw new RuntimeException("Can`t write data to file.", e);
-        } finally {
-            if (bufferedWriter != null) {
-                try {
-                    bufferedWriter.close();
-                } catch (IOException e) {
-                    throw new RuntimeException("Can`t close BufferWriter.", e);
-                }
+            throw new RuntimeException("Can't write data to file " + fileName, e);
+        }
+    }
+
+    private String[] calculate(String[] dataFromFile) {
+        int supply = 0;
+        int buy = 0;
+        int amount;
+        for (String line : dataFromFile) {
+            String[] lineSeparation = line.split(",");
+            amount = Integer.parseInt(lineSeparation[1]);
+            if (lineSeparation[0].equals(SUPLY_TEXT)) {
+                supply += amount;
+            } else {
+                buy += amount;
             }
         }
+        return new String[]{SUPLY_TEXT + ","
+                + supply, BUY_TEXT + ","
+                + buy, RESULT_TEXT + "," + (supply - buy)};
     }
 }
