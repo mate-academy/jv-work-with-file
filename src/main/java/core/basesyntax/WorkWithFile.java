7bec26da3e8d0f9ca13ddbbfc388831425198131
Vladyslav Dovhopolyi <dovhopolii@gmail.com>
2023-04-27 19:47:08 +0200

    requests added

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,13 +7,33 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
-        BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName));
+    public void getStatistic(String fromFileName, String toFileName) {
+        try {
+            String fileContent = readFromFile(fromFileName);
+            String report = createReport(fileContent);
+            writeToFile(report, toFileName);
+        } catch (IOException e) {
+            System.out.println("An error occurred while processing the file: " + e.getMessage());
+        }
+    }
+
+    private String readFromFile(String fromFileName) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line).append("\n");
+            }
+        }
+        return sb.toString();
+    }
+
+    private String createReport(String fileContent) {
         int supplySum = 0;
         int buySum = 0;
-        String line;
-        while ((line = reader.readLine()) != null) {
+        StringBuilder sb = new StringBuilder();
+        String[] lines = fileContent.split("\n");
+        for (String line : lines) {
             String[] splited = line.split(",");
             String operation = splited[0];
             int amount = Integer.parseInt(splited[1]);
@@ -23,11 +43,16 @@ public class WorkWithFile {
                 buySum += amount;
             }
         }
-        reader.close();
         int result = supplySum - buySum;
-        writer.write("supply," + supplySum + "\n");
-        writer.write("buy," + buySum + "\n");
-        writer.write("result," + result + "\n");
-        writer.close();
+        sb.append("supply,").append(supplySum).append("\n");
+        sb.append("buy,").append(buySum).append("\n");
+        sb.append("result,").append(result).append("\n");
+        return sb.toString();
+    }
+
+    private void writeToFile(String report, String toFileName) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
+            writer.write(report);
+        }
     }
 }
