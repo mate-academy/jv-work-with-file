924093a4da376bbf96101c9915e92ffa758e79b6
Arina Polna <polna.arina@gmail.com>
2023-04-28 12:52:39 +0300

    вшмdivideeReport methodmethod into createReport and writeToFile methods and change other mentors requests

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,39 +2,45 @@ package core.basesyntax;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
-        writeToFile(toFileName, createReport(fromFileName));
+        writeToFile(toFileName, createReport(readFromFile(fromFileName)));
     }
 
-    private String createReport(String fromFileName) {
-        int supplyTotal = 0;
-        int buyTotal = 0;
-        String line;
-        StringBuilder builder = new StringBuilder();
+    private int[] readFromFile(String fromFileName) {
         try (BufferedReader inputFile = new BufferedReader(new FileReader(fromFileName))) {
+            final String coma_separator = ",";
+            final String supply_word = "supply";
+            final String buy_word = "buy";
+            final int operation_type_index = 0;
+            final int amount_index = 1;
+            int supplyTotal = 0;
+            int buyTotal = 0;
+            String line;
             while ((line = inputFile.readLine()) != null) {
-                String[] splittedLine = line.split(",");
-                if (splittedLine[0].equals("supply")) {
-                    supplyTotal += Integer.parseInt(splittedLine[1]);
-                } else if (splittedLine[0].equals("buy")) {
-                    buyTotal += Integer.parseInt(splittedLine[1]);
+                String[] splittedLine = line.split(coma_separator);
+                if (splittedLine[operation_type_index].equals(supply_word)) {
+                    supplyTotal += Integer.parseInt(splittedLine[amount_index]);
+                } else if (splittedLine[operation_type_index].equals(buy_word)) {
+                    buyTotal += Integer.parseInt(splittedLine[amount_index]);
                 }
             }
-            final int result = supplyTotal - buyTotal;
-            builder.append("supply,").append(supplyTotal).append(System.lineSeparator())
-              .append("buy,").append(buyTotal).append(System.lineSeparator())
-              .append("result,").append(result).append(System.lineSeparator());
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException("Cant read from file", e);
+            return new int[] {supplyTotal, buyTotal};
         } catch (IOException e) {
             throw new RuntimeException("Cant read from file", e);
         }
+    }
+
+    private String createReport(int[] totals) {
+        final int result = totals[0] - totals[1];
+        StringBuilder builder = new StringBuilder();
+        builder.append("supply,").append(totals[0]).append(System.lineSeparator())
+              .append("buy,").append(totals[1]).append(System.lineSeparator())
+              .append("result,").append(result).append(System.lineSeparator());
         return builder.toString();
     }
 
