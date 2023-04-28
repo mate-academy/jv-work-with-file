af797ed06a086299a808b3848c8918d5acb7ec3e
Anastasiia Variichuk <stacyvariuchuk@gmail.com>
2023-04-28 10:23:48 +0300

    fixed rejected solution

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,37 +7,35 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    private static final String COMMA_SEPARATOR = ",";
     private static final String BUY_KEYWORD = "buy";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        StringBuilder stringBuilder = new StringBuilder();
+        writeDataToFile(calculateReadDataFromFile(fromFileName), toFileName);
+    }
+
+    private String calculateReadDataFromFile(String fromFileName) {
+        int supplyAllSum = 0;
+        int buyAllSum = 0;
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             String str = bufferedReader.readLine();
             while (str != null) {
-                stringBuilder.append(str).append(COMMA_SEPARATOR);
+                if (str.contains(BUY_KEYWORD)) {
+                    buyAllSum += Integer.parseInt(str.replaceAll("[a-zA-Z[.][,][_][-]]", ""));
+                } else {
+                    supplyAllSum += Integer.parseInt(str.replaceAll("[a-zA-Z[.][,][_][-]]", ""));
+                }
                 str = bufferedReader.readLine();
             }
         } catch (IOException e) {
             throw new RuntimeException("Can't read the file: ", e);
         }
-        String[] stringsFromFile = stringBuilder.toString().split(COMMA_SEPARATOR);
-        int supplyAllSum = 0;
-        int buyAllSum = 0;
-        for (int i = 0; i < stringsFromFile.length - 1; i += 2) {
-            if (stringsFromFile[i].equals(BUY_KEYWORD)) {
-                buyAllSum += Integer.parseInt(stringsFromFile[i + 1]);
-            } else {
-                supplyAllSum += Integer.parseInt(stringsFromFile[i + 1]);
-            }
-        }
-        String[] resultStrings = new String[]{"supply,"
-                + supplyAllSum, "buy," + buyAllSum, "result," + (supplyAllSum - buyAllSum)};
+        return "supply," + supplyAllSum
+                + "\nbuy," + buyAllSum + "\nresult," + (supplyAllSum - buyAllSum);
+    }
+
+    private void writeDataToFile(String calculatedData, String toFileName) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
-            for (String str : resultStrings) {
-                bufferedWriter.write(str);
-                bufferedWriter.write(System.lineSeparator());
-            }
+            bufferedWriter.write(calculatedData);
         } catch (IOException e) {
             throw new RuntimeException("Can't write in the file: ", e);
         }
