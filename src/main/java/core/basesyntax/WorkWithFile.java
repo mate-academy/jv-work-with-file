f9ff037b6bc5c3cac3976f5c6ce13a50028f9eff
Anastasiia Variichuk <stacyvariuchuk@gmail.com>
2023-04-28 15:36:49 +0300

    add private methods

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,29 +8,47 @@ import java.io.IOException;
 
 public class WorkWithFile {
     private static final String BUY_KEYWORD = "buy";
+    private static final String COMMA_SEPARATOR = ",";
+    private static final String SEPARATOR = " ";
+    private static final int AMOUNT_INDEX = 1;
+    private static final int ACTION_INDEX = 0;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        writeDataToFile(calculateReadDataFromFile(fromFileName), toFileName);
+        writeDataToFile(calculateData(readDataFromFile(fromFileName)), toFileName);
     }
 
-    private String calculateReadDataFromFile(String fromFileName) {
-        int supplyAllSum = 0;
-        int buyAllSum = 0;
+    private String[] readDataFromFile(String fromFileName) {
+        StringBuilder stringBuilder = new StringBuilder();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             String str = bufferedReader.readLine();
             while (str != null) {
-                if (str.contains(BUY_KEYWORD)) {
-                    buyAllSum += Integer.parseInt(str.replaceAll("[a-zA-Z.,_-]", ""));
-                } else {
-                    supplyAllSum += Integer.parseInt(str.replaceAll("[a-zA-Z.,_-]", ""));
-                }
+                stringBuilder.append(str).append(SEPARATOR);
                 str = bufferedReader.readLine();
             }
         } catch (IOException e) {
             throw new RuntimeException("Can't read the file: ", e);
         }
+        return stringBuilder.toString().split(SEPARATOR);
+    }
+
+    private String calculateData(String[] stringsFromFile) {
+        int supplyAllSum = 0;
+        int buyAllSum = 0;
+        for (String line : stringsFromFile) {
+            String[] lineSeparation = line.split(COMMA_SEPARATOR);
+            if (lineSeparation[ACTION_INDEX].equals(BUY_KEYWORD)) {
+                buyAllSum += Integer.parseInt(lineSeparation[AMOUNT_INDEX]);
+            } else {
+                supplyAllSum += Integer.parseInt(lineSeparation[AMOUNT_INDEX]);
+            }
+        }
+        return createReport(supplyAllSum, buyAllSum);
+    }
+
+    private static String createReport(int supplyAllSum, int buyAllSum) {
         return "supply," + supplyAllSum
-                + "\nbuy," + buyAllSum + "\nresult," + (supplyAllSum - buyAllSum);
+                + System.lineSeparator() + "buy,"
+                + buyAllSum + System.lineSeparator() + "result," + (supplyAllSum - buyAllSum);
     }
 
     private void writeDataToFile(String calculatedData, String toFileName) {
