afee5f1679313ccf71ef49a90ea681dd9f5869f0
Oleksandr Balan <litecloud.nohax@gmail.com>
2023-04-28 16:04:22 +0300

    fixed issues

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -12,7 +12,7 @@ public class WorkWithFile {
     private static final String RESULT = "result";
     private static final String COMA = ",";
     private static final int AMOUNT = 1;
-    private static final int FUNCTION = 0;
+    private static final int OPERATION = 0;
 
     public void getStatistic(String fromFileName, String toFileName) {
         String[] data = readFromFile(fromFileName);
@@ -34,31 +34,32 @@ public class WorkWithFile {
         return stringBuilder.toString().split(System.lineSeparator());
     }
 
+    private String calculate(String[] dataFromFile) {
+        int supply = 0;
+        int buy = 0;
+        int amount;
+        for (String line : dataFromFile) {
+            String[] lineSeparation = line.split(COMA);
+            amount = Integer.parseInt(lineSeparation[AMOUNT]);
+            if (lineSeparation[OPERATION].equals(SUPPLY)) {
+                supply += amount;
+            } else {
+                buy += amount;
+            }
+        }
+        return createReport(supply, buy);
+    }
+
+    public String createReport(int supply, int buy) {
+        StringBuilder builder = new StringBuilder();
+        return builder.append(SUPPLY).append(",").append(supply).append(System.lineSeparator()).append(BUY).append(",").append(buy).append(System.lineSeparator()).append(RESULT).append(",").append(supply - buy).toString();
+    }
+
     private void writeToFile(String fileName, String calculate) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName))) {
             bufferedWriter.write(calculate);
         } catch (IOException e) {
             throw new RuntimeException("Can't write data to file " + fileName, e);
         }
-    }
-
-    private String calculate(String[] dataFromFile) {
-        int supply = 0;
-        int buy = 0;
-        int amount;
-        StringBuilder stringBuilder = new StringBuilder();
-        for (String line : dataFromFile) {
-            String[] lineSeparation = line.split(COMA);
-            amount = Integer.parseInt(lineSeparation[AMOUNT]);
-            if (lineSeparation[FUNCTION].equals(SUPPLY)) {
-                supply += amount;
-            } else {
-                buy += amount;
-            }
-        }
-        return stringBuilder.append(SUPPLY).append(",").append(supply)
-                .append(System.lineSeparator()).append(BUY).append(",")
-                .append(buy).append(System.lineSeparator()).append(RESULT)
-                .append(",").append((supply - buy)).toString();
     }
 }
