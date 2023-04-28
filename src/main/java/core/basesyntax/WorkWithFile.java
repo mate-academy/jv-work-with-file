d82f715856ef9f8399966e17bf29e95a2410963c
Arina Polna <polna.arina@gmail.com>
2023-04-28 14:40:48 +0300

    create calculateStatistic method and change mentors requests

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,39 +7,50 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    private static String COMA_SEPARATOR = ",";
+    private static final String SUPPLY_WORD = "supply";
+    private static String BUY_WORD = "buy";
+    private static int OPERATION_TYPE_INDEX = 0;
+    private static int AMOUNT_INDEX = 1;
+    private static final int SUPPLY_AMOUNT_INDEX = 0;
+    private static final int BUY_AMOUNT_INDEX = 1;
+
     public void getStatistic(String fromFileName, String toFileName) {
-        writeToFile(toFileName, createReport(readFromFile(fromFileName)));
+        writeToFile(toFileName, createReport(calculateStatistic(readFromFile(fromFileName))));
     }
 
-    private int[] readFromFile(String fromFileName) {
+    private String[] readFromFile(String fromFileName) {
         try (BufferedReader inputFile = new BufferedReader(new FileReader(fromFileName))) {
-            final String coma_separator = ",";
-            final String supply_word = "supply";
-            final String buy_word = "buy";
-            final int operation_type_index = 0;
-            final int amount_index = 1;
-            int supplyTotal = 0;
-            int buyTotal = 0;
             String line;
+            StringBuilder builder = new StringBuilder();
             while ((line = inputFile.readLine()) != null) {
-                String[] splittedLine = line.split(coma_separator);
-                if (splittedLine[operation_type_index].equals(supply_word)) {
-                    supplyTotal += Integer.parseInt(splittedLine[amount_index]);
-                } else if (splittedLine[operation_type_index].equals(buy_word)) {
-                    buyTotal += Integer.parseInt(splittedLine[amount_index]);
-                }
+                builder.append(line).append(System.lineSeparator());
             }
-            return new int[] {supplyTotal, buyTotal};
+            String content = builder.toString();
+            return content.split(System.lineSeparator());
         } catch (IOException e) {
-            throw new RuntimeException("Cant read from file", e);
+            throw new RuntimeException("Cant read data from file" + fromFileName, e);
         }
     }
 
+    private int[] calculateStatistic(String[] contentArray) {
+        int supplyTotal = 0;
+        int buyTotal = 0;
+        for (String string : contentArray) {
+            if (string.split(COMA_SEPARATOR)[OPERATION_TYPE_INDEX].equals(SUPPLY_WORD)) {
+                supplyTotal += Integer.parseInt(string.split(COMA_SEPARATOR)[AMOUNT_INDEX]);
+            } else if (string.split(COMA_SEPARATOR)[OPERATION_TYPE_INDEX].equals(BUY_WORD)) {
+                buyTotal += Integer.parseInt(string.split(COMA_SEPARATOR)[AMOUNT_INDEX]);
+            }
+        }
+        return new int[]{supplyTotal, buyTotal};
+    }
+
     private String createReport(int[] totals) {
-        final int result = totals[0] - totals[1];
+        final int result = totals[SUPPLY_AMOUNT_INDEX] - totals[BUY_AMOUNT_INDEX];
         StringBuilder builder = new StringBuilder();
-        builder.append("supply,").append(totals[0]).append(System.lineSeparator())
-              .append("buy,").append(totals[1]).append(System.lineSeparator())
+        builder.append("supply,").append(totals[SUPPLY_AMOUNT_INDEX]).append(System.lineSeparator())
+              .append("buy,").append(totals[BUY_AMOUNT_INDEX]).append(System.lineSeparator())
               .append("result,").append(result).append(System.lineSeparator());
         return builder.toString();
     }
@@ -48,7 +59,7 @@ public class WorkWithFile {
         try (BufferedWriter outputFile = new BufferedWriter(new FileWriter(toFileName))) {
             outputFile.write(report);
         } catch (IOException e) {
-            throw new RuntimeException("Cant write to file", e);
+            throw new RuntimeException("Cant write report to file" + toFileName, e);
         }
     }
 }
