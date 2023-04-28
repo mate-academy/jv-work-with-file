7ed5f703c5f66a7b4da6ff668b0e901ff90c5413
Володимир <vova.malyarchuk2@gmail.com>
2023-04-28 12:06:27 +0300

    Implemented task produc-2e

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -12,37 +12,48 @@ public class WorkWithFile {
     private static final String SUPPLY = "supply";
     private static final String BUY = "buy";
     private static final String RESULT = "result";
+    private static final int OPERATION_INDEX = 0;
+    private static final int AMOUNT_INDEX = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        File file = new File(fromFileName);
-        int buyCount = 0;
-        int supplyCount = 0;
-        int resultCount = 0;
+        int buyCount = OPERATION_INDEX;
+        int supplyCount = OPERATION_INDEX;
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(file));
-                BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
-
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 String[] data = line.split(SEPARATOR);
-                if (data[0].equals(SUPPLY)) {
-                    supplyCount += Integer.parseInt(data[1]);
-                } else if (data[0].equals(BUY)) {
-                    buyCount += Integer.parseInt(data[1]);
-                } else if (data[0].equals(RESULT)) {
-                    resultCount = Integer.parseInt(data[1]);
+                if (data[OPERATION_INDEX].equals(SUPPLY)) {
+                    supplyCount += Integer.parseInt(data[AMOUNT_INDEX]);
+                } else {
+                    buyCount += Integer.parseInt(data[AMOUNT_INDEX]);
                 }
             }
-
-            writer.write(SUPPLY + SEPARATOR + supplyCount);
-            writer.newLine();
-            writer.write(BUY + SEPARATOR + buyCount);
-            writer.newLine();
-            writer.write(RESULT + SEPARATOR + (supplyCount - buyCount));
-            writer.newLine();
-
         } catch (IOException e) {
-            throw new RuntimeException("Error", e);
+            throw new RuntimeException("Can't read data from file", e);
+        }
+
+        String reportString = buildReportString(supplyCount, buyCount);
+
+        try {
+            writeReportToFile(reportString, toFileName);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write data to file", e);
+        }
+    }
+
+    private String buildReportString(int supplyCount, int buyCount) {
+        String reportString = "";
+        reportString += SUPPLY + SEPARATOR + supplyCount + System.lineSeparator();
+        reportString += BUY + SEPARATOR + buyCount + System.lineSeparator();
+        reportString += RESULT + SEPARATOR + (supplyCount - buyCount) + System.lineSeparator();
+
+        return reportString;
+    }
+
+    private void writeReportToFile(String reportString, String fileName) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+            writer.write(reportString);
         }
     }
 }
