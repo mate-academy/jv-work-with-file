ece405bc21de6a99f52b17650d7df4664b7b1e5b
Mykyta Udovycehnko <nikitaudav123@gmail.com>
2023-04-28 15:52:05 +0300

    solution jv-work-with-file

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,51 @@
 package core.basesyntax;
 
-public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class WorkWithFile {
+    private static final int NAME = 0;
+    private static final int NUMBER = 1;
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        int[] data = readFile(fromFileName);
+        String report = createReport(data);
+        writeReport(toFileName, report);
+    }
+
+    private int[] readFile(String fromFileName) {
+        int[] values = new int[2];
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
+            String line = reader.readLine();
+            while (line != null) {
+                String[] separate = line.split(",");
+                if (separate[NAME].equals("supply")) {
+                    values[NAME] += Integer.parseInt(separate[NUMBER]);
+                } else {
+                    values[NUMBER] += Integer.parseInt(separate[NUMBER]);
+                }
+                line = reader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read data from the file " + fromFileName, e);
+        }
+        return values;
+    }
+
+    private String createReport(int[] data) {
+        int result = data[0] - data[1];
+        return "supply," + data[0] + System.lineSeparator()
+                + "buy," + data[1] + System.lineSeparator()
+                + "result," + result + System.lineSeparator();
+    }
+
+    private void writeReport(String toFileName, String report) {
+        try (FileWriter writer = new FileWriter(toFileName)) {
+            writer.write(report);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write data to the file " + toFileName, e);
+        }
     }
 }
