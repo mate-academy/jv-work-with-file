0f9f60ee6a5a753088abb786329c1173e7641ab9
Vitalii <vitaliyplys1@gmail.com>
2023-05-01 20:10:20 +0300

    added fixes jv-work homework solution

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,12 +8,18 @@ import java.io.IOException;
 
 public class WorkWithFile {
     private static final String BUY = "buy";
-    private static final String SUPPLE = "supply";
+    private static final String SUPPLY = "supply";
     private static final String RESULT = "result";
     private static final int NAME_INDEX = 0;
     private static final int NUMBER_INDEX = 1;
     private static final String SPACE = " ";
     private static final String COMMA = ",";
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        String[] dataFromFile = readFile(fromFileName);
+        String report = generateReport(dataFromFile);
+        writeToFile(toFileName, report);
+    }
 
     private String [] readFile(String fromFileName) {
         StringBuilder builder = new StringBuilder();
@@ -37,22 +43,16 @@ public class WorkWithFile {
             if (words[NAME_INDEX].equals(BUY)) {
                 buyCounter += Integer.parseInt(words[NUMBER_INDEX]);
             }
-            if (words[NAME_INDEX].equals(SUPPLE)) {
+            if (words[NAME_INDEX].equals(SUPPLY)) {
                 supplyCounter += Integer.parseInt(words[NUMBER_INDEX]);
             }
         }
         StringBuilder report = new StringBuilder();
-        report.append(SUPPLE).append(COMMA).append(supplyCounter).append(System.lineSeparator())
+        report.append(SUPPLY).append(COMMA).append(supplyCounter).append(System.lineSeparator())
                 .append(BUY).append(COMMA).append(buyCounter).append(System.lineSeparator())
                 .append(RESULT).append(COMMA).append(supplyCounter - buyCounter)
                 .append(System.lineSeparator());
         return report.toString();
-    }
-
-    public void getStatistic(String fromFileName, String toFileName) {
-        String[] dataFromFile = readFile(fromFileName);
-        String report = generateReport(dataFromFile);
-        writeToFile(toFileName, report);
     }
 
     private void writeToFile(String toFileName, String report) {
