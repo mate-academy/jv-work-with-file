1e6b75a536748f7f88cd5c54db769e07cfdcca96
Arthur Dvinskykh <dv7art@gmail.com>
2023-05-01 19:23:33 +0300

    Fixed Code, delete StringBuilder_method

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,76 +2,58 @@ package core.basesyntax;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    public static final String SUPPLY = "supply,";
-    public static final String BUY = "buy,";
-    public static final String RESULT = "result,";
-    public static final String DELIMETER = ",";
-    private StringBuilder stringBuilder = new StringBuilder();
-    private int buy = 0;
-    private int supply = 0;
+    public static final String SUPPLY = "supply";
+    public static final String BUY = "buy";
+    public static final String RESULT = "result";
+    public static final String DELIMITER = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        readData(fromFileName);
-        extracted(supply,buy);
-        writeData(buy, supply, toFileName, stringBuilder.toString());
+        writeData(toFileName, analysisData(readData(fromFileName)));
     }
 
-    private void extracted(int supply, int buy) {
-        stringBuilder.append(SUPPLY)
-                    .append(supply)
-                    .append(System.lineSeparator())
-                    .append(BUY)
-                    .append(buy)
-                    .append(System.lineSeparator())
-                    .append(RESULT)
-                    .append(supply - buy)
-                    .append(System.lineSeparator());
-    }
-
-    public static boolean isFileExists(File file) {
-        return file.exists();
-    }
-
-    private void analysisData(String value) {
-        String[] arrayData = value.split(DELIMETER);
-        String equalsWord = "buy";
-        if (arrayData[0].equals(equalsWord)) {
-            buy += Integer.parseInt(arrayData[1]);
-        } else {
-            supply += Integer.parseInt(arrayData[1]);
+    private String analysisData(String[] value) {
+        int buy = 0;
+        int supply = 0;
+        String[] arrayData;
+        for (String words : value) {
+            arrayData = words.split(DELIMITER);
+            if (arrayData[0].equals(BUY)) {
+                buy += Integer.parseInt(arrayData[1]);
+            } else {
+                supply += Integer.parseInt(arrayData[1]);
+            }
         }
+        return SUPPLY + DELIMITER + supply + System.lineSeparator()
+                    + BUY + DELIMITER + buy + System.lineSeparator()
+                    + RESULT + DELIMITER + (supply - buy);
     }
 
-    private void readData(String fromFileName) {
-        File file = new File(fromFileName);
-
-        try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName));
+    private String[] readData(String fromFileName) {
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             String value = bufferedReader.readLine();
             while (value != null) {
-                //stringBuilder.append(value).append(System.lineSeparator());
-                analysisData(value);
+                stringBuilder.append(value).append(System.lineSeparator());
                 value = bufferedReader.readLine();
             }
+
         } catch (IOException e) {
-            throw new RuntimeException("Can't read file", e);
+            throw new RuntimeException("Can't read file: " + fromFileName, e);
         }
+        return stringBuilder.toString().split(System.lineSeparator());
     }
 
-    private void writeData(int b, int s, String toFileName, String d) {
-        File file = new File(toFileName);
-        if (!isFileExists(file)) {
-            try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file, true))) {
-                bufferedWriter.write(d);
-            } catch (IOException e) {
-                throw new RuntimeException("Can't write data", e);
-            }
+    private void writeData(String toFileName, String dataToWrite) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName, false))) {
+            bufferedWriter.write(dataToWrite);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write data: " + toFileName, e);
         }
     }
 }
+
