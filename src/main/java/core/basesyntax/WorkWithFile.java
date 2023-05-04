2f96eadcc5e84b4ce542ff8f35e10f3496faf50d
Vladyslav Denysiuk <denysiuk.vladyslav@gmail.com>
2023-05-04 17:23:35 +0300

    Report is generated

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,67 +2,53 @@ package core.basesyntax;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    private static final int FIRST_ARRAY = 0;
-    private static final int SECOND_ARRAY = 1;
-    private static final int REPORTSIZE = 3;
+    private static final int NAME_POSITION = 0;
+    private static final int NUMBER_POSITION = 1;
+    private static final String SEPARATOR = ",";
+    private static final String SUPPLY = "supply";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        File originalFile = new File(fromFileName);
-        File reportFile = new File(toFileName);
-        StringBuilder builder;
-        if (!originalFile.exists()) {
-            throw new RuntimeException("File does not exist");
-        }
-        try (BufferedReader reader = new BufferedReader(new FileReader(originalFile))) {
-            builder = new StringBuilder();
-            String content = reader.readLine();
-            while (content != null) {
-                builder.append(content).append(System.lineSeparator());
-                content = reader.readLine();
-            }
-            String[] lineOfStartFile = builder.toString().split(System.lineSeparator());
-            int buySum = 0;
-            int supplySum = 0;
-            for (String value : lineOfStartFile) {
-                String[] elementOfLine = value.split(",");
-                if (elementOfLine.length != 2) {
-                    throw new RuntimeException("Invalid input file format");
-                }
-                String action = elementOfLine[FIRST_ARRAY];
-                int quantity;
-                try {
-                    quantity = Integer.parseInt(elementOfLine[SECOND_ARRAY]);
-                } catch (NumberFormatException e) {
-                    throw new RuntimeException("Invalid input file format ", e);
-                }
-                if (action.equals("buy")) {
-                    buySum += quantity;
-                } else if (action.equals("supply")) {
-                    supplySum += quantity;
+        int[] data = readFile(fromFileName);
+        String report = generateReport(data);
+        writeReport(toFileName, report);
+    }
+
+    private int[] readFile(String fromFileName) {
+        int[] values = new int[2];
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
+            String line = reader.readLine();
+            while (line != null) {
+                String[] separate = line.split(SEPARATOR);
+                if (separate[NAME_POSITION].equals(SUPPLY)) {
+                    values[NAME_POSITION] += Integer.parseInt(separate[NUMBER_POSITION]);
                 } else {
-                    throw new RuntimeException("Invalid input");
+                    values[NUMBER_POSITION] += Integer.parseInt(separate[NUMBER_POSITION]);
                 }
-            }
-            int result = supplySum - buySum;
-            String[] report = new String[REPORTSIZE];
-            report[0] = "supply," + supplySum;
-            report[1] = "buy," + buySum;
-            report[2] = "result," + result;
-            try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(reportFile))) {
-                for (String s : report) {
-                    bufferedWriter.write(s + System.lineSeparator());
-                }
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+                line = reader.readLine();
             }
         } catch (IOException e) {
-            throw new RuntimeException("Can't write file", e);
+            throw new RuntimeException("Can't read " + fromFileName, e);
+        }
+        return values;
+    }
+
+    private String generateReport(int[] data) {
+        int result = data[0] - data[1];
+        return "supply," + data[0] + System.lineSeparator()
+                + "buy," + data[1] + System.lineSeparator()
+                + "result," + result + System.lineSeparator();
+    }
+
+    private void writeReport(String toFileName, String report) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+            bufferedWriter.write(report);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write file ", e);
         }
     }
 }
