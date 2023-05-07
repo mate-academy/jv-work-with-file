fa1dbde0c36a1de83663d75c70be5c8d9058d197
Maxym Meleshchenko <maximkame00@gmail.com>
2023-05-07 12:47:29 +0300

    Created three private methods and added constants.

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,34 +7,50 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    private static final int FiRST_POSITION = 0;
+    private static final int SECOND_POSITION = 1;
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+    private static final String SEPARATE = ",";
+
     public void getStatistic(String fromFileName, String toFileName) {
-        int supply = 0;
-        int buy = 0;
+        int [] data = readFile(fromFileName);
+        String report = createReport(data);
+        writeReport(toFileName,report);
+    }
+
+    private int [] readFile(String fromFileName) {
+        int [] values = new int[2];
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             String value = bufferedReader.readLine();
             while (value != null) {
-                String [] values = value.split(",");
-                if (values[0].equals("supply")) {
-                    supply += Integer.parseInt(values[1]);
+                String [] separate = value.split(SEPARATE);
+                if (separate[FiRST_POSITION].equals(SUPPLY)) {
+                    values[FiRST_POSITION] += Integer.parseInt(separate[SECOND_POSITION]);
                 }
-                if (values[0].equals("buy")) {
-                    buy += Integer.parseInt(values[1]);
+                if (separate[FiRST_POSITION].equals(BUY)) {
+                    values[SECOND_POSITION] += Integer.parseInt(separate[SECOND_POSITION]);
                 }
                 value = bufferedReader.readLine();
             }
         } catch (IOException e) {
-            throw new RuntimeException("Can't read data from the file", e);
+            throw new RuntimeException("Can't read data from the file" + fromFileName, e);
         }
-        int result = supply - buy;
+        return values;
+    }
 
+    private String createReport(int[] data) {
+        int result = data[0] - data[1];
+        return "supply," + data[0] + System.lineSeparator()
+                + "buy," + data[1] + System.lineSeparator()
+                + "result," + result;
+    }
+
+    private void writeReport(String toFileName, String report) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
-            bufferedWriter.write("supply," + supply);
-            bufferedWriter.newLine();
-            bufferedWriter.write("buy," + buy);
-            bufferedWriter.newLine();
-            bufferedWriter.write("result," + result);
+            bufferedWriter.write(report);
         } catch (IOException e) {
-            throw new RuntimeException("Can't write data to file", e);
+            throw new RuntimeException("Can't write data to file" + toFileName, e);
         }
     }
 }
