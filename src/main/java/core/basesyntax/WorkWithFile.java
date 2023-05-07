1bb55ded1e8938abfb78df17ef7716c1172f1b76
Artem <cr7realartem@gmail.com>
2023-05-07 18:50:38 +0300

    separated logic into private methods

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,39 +8,55 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    private static final String DELIMITER = "\\W";
+
     public void getStatistic(String fromFileName, String toFileName) {
         File checkFile = new File(fromFileName);
         File newFile = new File(toFileName);
+        readFromFile(checkFile);
+        String[] allText = readFromFile(checkFile).split(DELIMITER);
+        int countSupply = getCount(allText, "supply");
+        int countBuy = getCount(allText, "buy");
+        int result = countSupply - countBuy;
+        String report = generateReport(countSupply, countBuy, result);
+        writeReportToFile(newFile, report);
+    }
+
+    private String readFromFile(File file) {
         StringBuilder builder = new StringBuilder();
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(checkFile))) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
             String reader = bufferedReader.readLine();
             while (reader != null) {
                 builder.append(reader).append(System.lineSeparator());
                 reader = bufferedReader.readLine();
             }
         } catch (IOException e) {
-            throw new RuntimeException("Can't find file" + checkFile.getName());
+            throw new RuntimeException("Can't find file " + file);
         }
-        String[] allText = builder.toString().split("\\W");
-        int countSupply = 0;
-        int countBuy = 0;
-        for (int i = 0; i < allText.length; i++) {
-            if (allText[i].equals("supply")) {
-                countSupply += Integer.parseInt(allText[i + 1]);
-            }
-            if (allText[i].equals("buy")) {
-                countBuy += Integer.parseInt(allText[i + 1]);
+        return builder.toString();
+    }
+
+    private int getCount(String[] data, String keyword) {
+        int count = 0;
+        for (int i = 0; i < data.length; i++) {
+            if (data[i].equals(keyword)) {
+                count += Integer.parseInt(data[i + 1]);
             }
         }
-        int result = countSupply - countBuy;
-        String report = "supply," + countSupply + System.lineSeparator()
+        return count;
+    }
+
+    private String generateReport(int countSupply, int countBuy, int result) {
+        return "supply," + countSupply + System.lineSeparator()
                 + "buy," + countBuy + System.lineSeparator()
                 + "result," + result;
+    }
 
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(newFile,false))) {
+    private void writeReportToFile(File file, String report) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file,false))) {
             bufferedWriter.write(report);
         } catch (IOException e) {
-            throw new RuntimeException("Can't find file" + e);
+            throw new RuntimeException("Can't find file " + file);
         }
     }
 }
