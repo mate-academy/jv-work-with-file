d17d7dca43957387e719c34b88a34294472b3a22
Zhuk Sergei <svzhuk@ukr.net>
2023-05-10 18:51:49 +0300

    removed constants and variables useв StringBuilder rewrote block of code

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,62 +7,51 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    private static final int OPERATION_ID = 0;
-    private static final int AMOUNT_ID = 1;
-    private static final String BUY = "buy";
-    private static final String SUPPLY = "supply";
-    private static final String SEPARATOR = ",";
-    private static final String RESULT = "result";
+    private static final String REGEX_FOR_SPLITTING_DATA = "\\W+";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        String[] lines = readFromFile(fromFileName).split(System.lineSeparator());
-        String[][] data = new String[lines.length][2];
-        String[] buffer;
-        for (int i = 0; i < lines.length; i++) {
-            buffer = lines[i].split(SEPARATOR);
-            data[i][OPERATION_ID] = buffer[OPERATION_ID];
-            data[i][AMOUNT_ID] = buffer[AMOUNT_ID];
-        }
-        int[] report = countAmountByOperation(data);
-        String output = SUPPLY + SEPARATOR + report[1] + System.lineSeparator() + BUY + SEPARATOR
-                + report[0] + System.lineSeparator() + RESULT + SEPARATOR + report[2];
-        writeToFile(output, toFileName);
-    }
-
-    private void writeToFile(String text, String file) {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
-            writer.write(text);
-        } catch (IOException e) {
-            throw new RuntimeException("Can't write file" + file, e);
-        }
+        String[] lines = readFromFile(fromFileName).split(REGEX_FOR_SPLITTING_DATA);
+        String report = countAmountByOperation(lines);
+        writeToFile(report, toFileName);
     }
 
     private String readFromFile(String file) {
         StringBuilder text = new StringBuilder();
-        String line;
-        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-            line = reader.readLine();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String line = reader.readLine();
             while (line != null) {
                 text.append(line).append(System.lineSeparator());
                 line = reader.readLine();
             }
-            return text.toString();
         } catch (IOException e) {
-            throw new RuntimeException("Can't read file" + file, e);
+            throw new RuntimeException("Can't read file", e);
         }
+        return text.toString();
     }
 
-    private int[] countAmountByOperation(String[][] data) {
+    private String countAmountByOperation(String[] lines) {
         int buyAmount = 0;
         int supplyAmount = 0;
-        for (String[] line: data) {
-            if (line[OPERATION_ID].equals(BUY)) {
-                buyAmount += Integer.parseInt(line[AMOUNT_ID]);
-                continue;
+        for (int i = 0; i < lines.length; i++) {
+            if (lines[i].equals("buy")) {
+                buyAmount += Integer.parseInt(lines[++i]);
+            } else if (lines[i].equals("supply")) {
+                supplyAmount += Integer.parseInt(lines[++i]);
             }
-            supplyAmount += Integer.parseInt(line[AMOUNT_ID]);
         }
-        int result = supplyAmount - buyAmount;
-        return new int[]{buyAmount, supplyAmount, result};
+        StringBuilder string = new StringBuilder();
+        return string.append("supply,").append(supplyAmount)
+        .append(System.lineSeparator()).append("buy,")
+        .append(buyAmount).append(System.lineSeparator())
+        .append("result,").append((supplyAmount - buyAmount)).toString();
+    }
+
+    private void writeToFile(String report, String toFileName) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
+            writer.write(report);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write file", e);
+        }
     }
 }
