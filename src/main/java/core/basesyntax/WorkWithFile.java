cb811e626ff4aaca2e428f8f91325edacdf92057
Mykyta Oleksin <32339836+nacenik@users.noreply.github.com>
2023-05-12 10:32:51 +0300

    Update ci.yml to java 17

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,47 @@
 package core.basesyntax;
 
+import java.io.*;
+
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
+        File fromFile = new File(fromFileName);
+        try (BufferedReader br = new BufferedReader
+                (new FileReader(fromFile))) {
+            String[] namesOfArticles = {"supply", "buy", "result"};
+            int indexOfResult = namesOfArticles.length - 1;
+            int[] values = new int[namesOfArticles.length];
+            String text = br.readLine();
+            while (text != null) {
+                String[] tempArray = text.split(",");
+                for (int i = 0; i < indexOfResult; ++i){
+                    if (tempArray[0].equals(namesOfArticles[i])){
+                        values[i] += Integer.valueOf(tempArray[1]);
+                    }
+                }
+                text = br.readLine();
+            }
+            values[indexOfResult] = values[0] - values[1];
+            writeStatistic(toFileName, namesOfArticles, values);
+        } catch (FileNotFoundException ex) {
+            throw new RuntimeException("FileNotFoundException has happened");
+        } catch (IOException ex) {
+            throw new RuntimeException("IOException has happened");
+        }
+    }
 
+    public void writeStatistic(String toFileName, String[] namesOfArticles, int[] values) {
+        try (BufferedWriter bw = new BufferedWriter
+                (new FileWriter(new File(toFileName), true))) {
+            for (int i = 0; i < values.length; ++i) {
+                StringBuilder answer = new StringBuilder();
+                answer.append(namesOfArticles[i] + "," + values[i]);
+                bw.write(answer.toString());
+                bw.newLine();
+            }
+        } catch (FileNotFoundException ex) {
+            throw new RuntimeException("FileNotFoundException has happened");
+        } catch (IOException ex) {
+            throw new RuntimeException("IOException has happened");
+        }
     }
 }
