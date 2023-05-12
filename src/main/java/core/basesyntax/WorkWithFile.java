3d58ea3c8f37cfbd0d0f6eddb5087820ea3b6706
Nikita <kaptetos@gmail.com>
2023-05-12 17:59:32 +0300

    The method opens the file fromFileName for reading using the BufferedReader class and reads it line by line in a loop. Each line contains two fields separated by a comma: operation and amount. The first field indicates the type of operation ("supply" - delivery, "buy" - purchase), the second field indicates the quantity of goods.
    During the reading process, the method calculates the total quantity of the product that was supplied (totalSupply) and the total quantity of the product that was purchased (totalBuy).
    
    When the file is finished reading, the method closes it and calculates the difference between totalSupply and totalBuy, writing this result to the result variable.

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,41 @@
 package core.basesyntax;
 
-public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class WorkWithFile {
+
+    private static final String OPERATION_SUPPLY = "supply";
+    private static final String OPERATION_BUY = "buy";
+
+    public static void getStatistic(String fromFileName, String toFileName) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
+
+        int totalSupply = 0;
+        int totalBuy = 0;
+
+        String line;
+        while ((line = reader.readLine()) != null) {
+            String[] fields = line.split(",");
+            String operation = fields[0];
+            int amount = Integer.parseInt(fields[1]);
+            if (operation.equals(OPERATION_SUPPLY)) {
+                totalSupply += amount;
+            } else if (operation.equals(OPERATION_BUY)) {
+                totalBuy += amount;
+            }
+        }
+
+        reader.close();
+
+        int result = totalSupply - totalBuy;
+        FileWriter writer = new FileWriter(toFileName);
+        writer.write("supply," + totalSupply + "\n");
+        writer.write("buy," + totalBuy + "\n");
+        writer.write("result," + result + "\n");
+        writer.close();
     }
 }
