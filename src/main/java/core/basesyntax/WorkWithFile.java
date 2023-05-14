096346ce8234f5f9e17631d20155a8a4caa28c06
pochtalon <pochtalon1010@gmail.com>
2023-05-14 22:15:33 +0300

    Replace local variables to class constants Separated method to three

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -9,37 +9,49 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class WorkWithFile {
+    private final static String SUPPLY = "supply";
+    private final static String BUY = "buy";
+    private final static String RESULT = "result";
+    private final static String DELIMITER = ",";
+    private final static int keyIndex = 0;
+    private final static int valueIndex = 1;
+
     public void getStatistic(String fromFileName, String toFileName) {
-        Stream<String> str = null;
+        Stream<String> stream = readLines(fromFileName);
+        String toWrite = getReport(stream);
+        writeLines(toFileName, toWrite);
+    }
+
+    private Stream<String> readLines(String fromFileName) {
+        Stream<String> streamOfLines = null;
         try {
-            str = Files.lines(Path.of(fromFileName));
+            streamOfLines = Files.lines(Path.of(fromFileName));
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Lines were not read",e);
         }
-        String toWrite = getString(str);
+        return streamOfLines;
+    }
+
+    private void writeLines(String toFileName, String forWriting) {
         try {
-            Files.write(Path.of(toFileName), toWrite.getBytes(StandardCharsets.UTF_8));
+            Files.write(Path.of(toFileName), forWriting.getBytes(StandardCharsets.UTF_8));
         } catch (IOException e) {
             throw new RuntimeException("Your trying to write bytes to file was not successful", e);
         }
     }
 
-    private String getString(Stream<String> stream) {
-        final String Supply = "supply";
-        final String Buy = "buy";
-        final String Result = "result";
-        final String Delimiter = ",";
-        Map<String, Integer> csvSum = stream
-                .map(e -> e.split(","))
-                .collect(Collectors.groupingBy(e -> e[0],
-                        Collectors.summingInt(e -> Integer.valueOf(e[1]))));
-        int supply = csvSum.values().stream().max(Integer::compare).get();
-        int buy = csvSum.values().stream().min(Integer::compare).get();
+    private String getReport(Stream<String> stream) {
+        Map<String, Integer> reportMap = stream
+                .map(e -> e.split(DELIMITER))
+                .collect(Collectors.groupingBy(e -> e[keyIndex],
+                        Collectors.summingInt(e -> Integer.valueOf(e[valueIndex]))));
+        int supply = reportMap.values().stream().max(Integer::compare).get();
+        int buy = reportMap.values().stream().min(Integer::compare).get();
         int result = supply - buy;
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(Supply).append(Delimiter).append(supply).append(System.lineSeparator())
-                .append(Buy).append(Delimiter).append(buy).append(System.lineSeparator())
-                .append(Result).append(Delimiter).append(result);
-        return stringBuilder.toString();
+        StringBuilder builder = new StringBuilder()
+                .append(SUPPLY).append(DELIMITER).append(supply).append(System.lineSeparator())
+                .append(BUY).append(DELIMITER).append(buy).append(System.lineSeparator())
+                .append(RESULT).append(DELIMITER).append(result);
+        return builder.toString();
     }
 }
