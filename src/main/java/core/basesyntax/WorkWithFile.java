cecf5cc4c929908045068a1c475973351cf6b876
Nikita <kaptetos@gmail.com>
2023-05-18 13:44:45 +0300

    changed as recommended

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -6,36 +6,52 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-
     private static final String OPERATION_SUPPLY = "supply";
     private static final String OPERATION_BUY = "buy";
+    private static final String SEPARATOR = ",";
 
-    public static void getStatistic(String fromFileName, String toFileName) throws IOException {
+    public void getStatistic(String fromFileName, String toFileName) {
+        try {
+            String[] lines = readFile(fromFileName);
+            int totalSupply = 0;
+            int totalBuy = 0;
 
-        BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
-
-        int totalSupply = 0;
-        int totalBuy = 0;
-
-        String line;
-        while ((line = reader.readLine()) != null) {
-            String[] fields = line.split(",");
-            String operation = fields[0];
-            int amount = Integer.parseInt(fields[1]);
-            if (operation.equals(OPERATION_SUPPLY)) {
-                totalSupply += amount;
-            } else if (operation.equals(OPERATION_BUY)) {
-                totalBuy += amount;
+            for (String line : lines) {
+                String[] fields = line.split(SEPARATOR);
+                String operation = fields[0];
+                int amount = Integer.parseInt(fields[1]);
+                if (operation.equals(OPERATION_SUPPLY)) {
+                    totalSupply += amount;
+                } else if (operation.equals(OPERATION_BUY)) {
+                    totalBuy += amount;
+                }
             }
+
+            int result = totalSupply - totalBuy;
+            String report = generateReport(totalSupply, totalBuy, result);
+            writeToFile(report, toFileName);
+        } catch (IOException e) {
+            throw new RuntimeException("Error processing file", e);
         }
+    }
 
-        reader.close();
+    private String[] readFile(String fileName) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            return reader.lines().toArray(String[]::new);
+        }
+    }
 
-        int result = totalSupply - totalBuy;
-        FileWriter writer = new FileWriter(toFileName);
-        writer.write("supply," + totalSupply + "\n");
-        writer.write("buy," + totalBuy + "\n");
-        writer.write("result," + result + "\n");
-        writer.close();
+    private String generateReport(int totalSupply, int totalBuy, int result) {
+        StringBuilder report = new StringBuilder();
+        report.append("supply").append(SEPARATOR).append(totalSupply).append("\n");
+        report.append("buy").append(SEPARATOR).append(totalBuy).append("\n");
+        report.append("result").append(SEPARATOR).append(result).append("\n");
+        return report.toString();
+    }
+
+    private void writeToFile(String report, String fileName) throws IOException {
+        try (FileWriter writer = new FileWriter(fileName)) {
+            writer.write(report);
+        }
     }
 }
