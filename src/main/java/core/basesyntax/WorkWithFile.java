8b81faa412af6546823d3c586ab3720418250545
Olga M <axiomkaonline@gmail.com>
2023-05-21 18:01:36 +0300

    Implement getStatistic metod Read data from input file and calculate statistics. Create a report and write it to the output file. The getStatistic method reads data from a CSV file, where each line represents an operation type and amount. It calculates the total supply and buy amounts and calculates the result as the difference between supply and buy. The statistics are written to the output file in the required format.

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,38 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
+                BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
+            int supplyTotal = 0;
+            int buyTotal = 0;
 
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] fields = line.split(",");
+                String operationType = fields[0];
+                int amount = Integer.parseInt(fields[1]);
+                if (operationType.equals("supply")) {
+                    supplyTotal += amount;
+                } else if (operationType.equals("buy")) {
+                    buyTotal += amount;
+                }
+            }
+            int result = supplyTotal - buyTotal;
+            writer.write("supply," + supplyTotal);
+            writer.newLine();
+            writer.write("buy," + buyTotal);
+            writer.newLine();
+            writer.write("result," + result);
+            writer.newLine();
+        } catch (IOException e) {
+            System.out.println("Error occurred:" + e.getMessage());
+        }
     }
 }
