bcac33c725933c992a5824c5c974d0232247db1b
Nikita <kaptetos@gmail.com>
2023-05-21 15:47:21 +0300

    changed as recommended public void getStatistic(String fromFileName, String toFileName) { have 3 lines .

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -13,46 +13,44 @@ public class WorkWithFile {
     private static final int AMOUNT_INDEX = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        try {
-            String[] lines = readFile(fromFileName);
-            int totalSupply = 0;
-            int totalBuy = 0;
-
-            for (String line : lines) {
-                String[] fields = line.split(SEPARATOR);
-                String operation = fields[OPERATION_INDEX];
-                int amount = Integer.parseInt(fields[AMOUNT_INDEX]);
-                if (OPERATION_SUPPLY.equals(operation)) {
-                    totalSupply += amount;
-                } else if (OPERATION_BUY.equals(operation)) {
-                    totalBuy += amount;
-                }
-            }
-
-            int result = calculateResult(totalSupply, totalBuy);
-            String report = generateReport(totalSupply, totalBuy, result);
-            writeToFile(report, toFileName);
-        } catch (IOException e) {
-            throw new RuntimeException("Error processing file " + fromFileName, e);
-        }
+        String[] lines = readFile(fromFileName);
+        String report = generateReport(lines);
+        writeToFile(report, toFileName);
     }
 
-    private String[] readFile(String fileName) throws IOException {
+    private String[] readFile(String fileName) {
         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             return reader.lines().toArray(String[]::new);
+        } catch (IOException e) {
+            throw new RuntimeException("Error reading file " + fileName, e);
         }
     }
 
-    private int calculateResult(int totalSupply, int totalBuy) {
-        return totalSupply - totalBuy;
-    }
+    private String generateReport(String[] lines) {
+        int totalSupply = 0;
+        int totalBuy = 0;
 
-    private String generateReport(int totalSupply, int totalBuy, int result) {
+        for (String line : lines) {
+            String[] fields = line.split(SEPARATOR);
+            String operation = fields[OPERATION_INDEX];
+            int amount = Integer.parseInt(fields[AMOUNT_INDEX]);
+            if (OPERATION_SUPPLY.equals(operation)) {
+                totalSupply += amount;
+            } else if (OPERATION_BUY.equals(operation)) {
+                totalBuy += amount;
+            }
+        }
+
+        int result = calculateResult(totalSupply, totalBuy);
         StringBuilder report = new StringBuilder();
         report.append(OPERATION_SUPPLY).append(SEPARATOR).append(totalSupply).append("\n");
         report.append(OPERATION_BUY).append(SEPARATOR).append(totalBuy).append("\n");
         report.append("result").append(SEPARATOR).append(result).append("\n");
         return report.toString();
+    }
+
+    private int calculateResult(int totalSupply, int totalBuy) {
+        return totalSupply - totalBuy;
     }
 
     private void writeToFile(String report, String fileName) {
