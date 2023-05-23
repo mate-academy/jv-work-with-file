f94414fc0401c58cc8775ebd6597ec30b690437e
Taras Zakala <zakalataras4@gmail.com>
2023-05-23 11:03:19 +0300

    completed task

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,39 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
+                BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
+            int supplyTotal = 0;
+            int buyTotal = 0;
 
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] data = line.split(",");
+                String operationType = data[0].trim();
+                int amount = Integer.parseInt(data[1].trim());
+                if (operationType.equals("supply")) {
+                    supplyTotal += amount;
+                } else if (operationType.equals("buy")) {
+                    buyTotal += amount;
+                }
+            }
+
+            int result = supplyTotal - buyTotal;
+
+            writer.write("supply," + supplyTotal);
+            writer.newLine();
+            writer.write("buy," + buyTotal);
+            writer.newLine();
+            writer.write("result," + result);
+        } catch (IOException e) {
+            throw new RuntimeException("Error processing the files.", e);
+        }
     }
 }
