47c804c985294c8b82589702d13734a61d8ae04c
Olga M <axiomkaonline@gmail.com>
2023-05-23 19:33:46 +0300

    Fixed after review

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,32 +7,47 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    private static final String OPERATION_SUPPLY = "supply";
+    private static final String OPERATION_BUY = "buy";
+
     public void getStatistic(String fromFileName, String toFileName) {
         try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
                 BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
-            int supplyTotal = 0;
-            int buyTotal = 0;
+            int[] totals = new int[2]; // totals[0] - supplyTotal, totals[1] - buyTotal
 
             String line;
             while ((line = reader.readLine()) != null) {
                 String[] fields = line.split(",");
                 String operationType = fields[0];
                 int amount = Integer.parseInt(fields[1]);
-                if (operationType.equals("supply")) {
-                    supplyTotal += amount;
-                } else if (operationType.equals("buy")) {
-                    buyTotal += amount;
-                }
+                updateTotals(operationType, amount, totals);
             }
-            int result = supplyTotal - buyTotal;
-            writer.write("supply," + supplyTotal);
-            writer.newLine();
-            writer.write("buy," + buyTotal);
-            writer.newLine();
-            writer.write("result," + result);
-            writer.newLine();
+
+            int result = calculateResult(totals[0], totals[1]);
+            writeStatistics(writer, totals[0], totals[1], result);
         } catch (IOException e) {
-            System.out.println("Error occurred:" + e.getMessage());
+            throw new RuntimeException("Error occurred: " + e.getMessage(), e);
         }
+    }
+
+    private void updateTotals(String operationType, int amount, int[] totals) {
+        if (operationType.equals(OPERATION_SUPPLY)) {
+            totals[0] += amount;
+        } else if (operationType.equals(OPERATION_BUY)) {
+            totals[1] += amount;
+        }
+    }
+
+    private int calculateResult(int supplyTotal, int buyTotal) {
+        return supplyTotal - buyTotal;
+    }
+
+    private void writeStatistics(BufferedWriter writer, int supplyTotal, int buyTotal, int result)
+            throws IOException {
+        StringBuilder sb = new StringBuilder();
+        sb.append("supply,").append(supplyTotal).append(System.lineSeparator());
+        sb.append("buy,").append(buyTotal).append(System.lineSeparator());
+        sb.append("result,").append(result).append(System.lineSeparator());
+        writer.write(sb.toString());
     }
 }
