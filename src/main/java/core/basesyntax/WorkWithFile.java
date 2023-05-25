a8ab140a8cd1db0936d8815621da0e62e0e82643
Olga M <axiomkaonline@gmail.com>
2023-05-25 19:42:53 +0300

    Made code refactoring

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,47 +7,63 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    private static final String OPERATION_SUPPLY = "supply";
-    private static final String OPERATION_BUY = "buy";
+    private static final String SUPPLY_VALUE = "supply";
+    private static final String BUY_VALUE = "buy";
+    private static final String RESULT = "result";
+    private static final String SEPARATOR = ",";
+    private static final int INDEX_ZERO = 0;
+    private static final int INDEX_ONE = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
-                BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
-            int[] totals = new int[2]; // totals[0] - supplyTotal, totals[1] - buyTotal
+        String[] splitFile = read(fromFileName).split(System.lineSeparator());
+        int supplyCount = 0;
+        int buyCount = 0;
+        int result;
 
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String[] fields = line.split(",");
-                String operationType = fields[0];
-                int amount = Integer.parseInt(fields[1]);
-                updateTotals(operationType, amount, totals);
+        for (String eachElement : splitFile) {
+            String[] toCountElement = eachElement.split(SEPARATOR);
+            String type = toCountElement[INDEX_ZERO];
+            int count = Integer.parseInt((toCountElement[INDEX_ONE]));
+            if (type.equals(SUPPLY_VALUE)) {
+                supplyCount += count;
             }
+            if (type.equals(BUY_VALUE)) {
+                buyCount += count;
+            }
+        }
+        result = supplyCount - buyCount;
+        String finalDataFile = dataCreator(supplyCount, buyCount, result);
+        write(toFileName, finalDataFile);
+    }
 
-            int result = calculateResult(totals[0], totals[1]);
-            writeStatistics(writer, totals[0], totals[1], result);
+    private String dataCreator(int supplyCount, int buyCount, int result) {
+        StringBuilder builder = new StringBuilder();
+        return builder.append(SUPPLY_VALUE).append(SEPARATOR)
+                .append(supplyCount).append(System.lineSeparator())
+                .append(BUY_VALUE).append(SEPARATOR).append(buyCount)
+                .append(System.lineSeparator()).append(RESULT)
+                .append(SEPARATOR).append(result).append(System.lineSeparator()).toString();
+    }
+
+    private String read(String fromFileName) {
+        StringBuilder builder = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
+            String value;
+            while ((value = bufferedReader.readLine()) != null) {
+                builder.append(value).append(System.lineSeparator());
+            }
+            return builder.toString();
         } catch (IOException e) {
-            throw new RuntimeException("Error occurred: " + e.getMessage(), e);
+            throw new RuntimeException("Can not read file" + fromFileName, e);
         }
     }
 
-    private void updateTotals(String operationType, int amount, int[] totals) {
-        if (operationType.equals(OPERATION_SUPPLY)) {
-            totals[0] += amount;
-        } else if (operationType.equals(OPERATION_BUY)) {
-            totals[1] += amount;
+    private void write(String toFileName, String dataResult) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(
+                new FileWriter(toFileName, false))) {
+            bufferedWriter.write(dataResult);
+        } catch (IOException e) {
+            throw new RuntimeException("Can not write file" + toFileName, e);
         }
-    }
-
-    private int calculateResult(int supplyTotal, int buyTotal) {
-        return supplyTotal - buyTotal;
-    }
-
-    private void writeStatistics(BufferedWriter writer, int supplyTotal, int buyTotal, int result)
-            throws IOException {
-        StringBuilder sb = new StringBuilder();
-        sb.append("supply,").append(supplyTotal).append(System.lineSeparator());
-        sb.append("buy,").append(buyTotal).append(System.lineSeparator());
-        sb.append("result,").append(result).append(System.lineSeparator());
-        writer.write(sb.toString());
     }
 }
