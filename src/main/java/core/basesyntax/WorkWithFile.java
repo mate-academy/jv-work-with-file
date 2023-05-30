ee4d9c411be77f9d273b7b586dbf57bac3d1971e
Olena Protsailo <olenaprotsailo@gmail.com>
2023-05-30 18:52:56 +0300

    added fixes after review 2

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,7 +2,6 @@ package core.basesyntax;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -10,71 +9,57 @@ import java.io.IOException;
 public class WorkWithFile {
     private static final int INDEX_OPERATION = 0;
     private static final int INDEX_DATA = 1;
-    private static final String LIMIT_FIELD = ",";
-    private static final String OPEERATION_ARRIVAL = "supply";
+    private static final String DELIMITER = ",";
+    private static final String SUPPLY_OPERATION = "supply";
     private static final String OPERATION_EXPENSE = "buy";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        String inputInfo;
-        String reportText;
-        inputInfo = readFile(fromFileName);
-        reportText = generateReport(inputInfo);
-        wtiteToFile(reportText, toFileName);
+        String inputInfo = readFile(fromFileName);
+        String reportText = generateReport(inputInfo);
+        writeToFile(reportText, toFileName);
     }
 
     private String readFile(String fromFileName) {
-        StringBuilder stringBuilder = new StringBuilder();
+        StringBuilder content = new StringBuilder();
         try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
             int value = reader.read();
             while ((value != - 1)) {
-                stringBuilder.append((char) value);
+                content.append((char) value);
                 value = reader.read();
             }
         } catch (IOException e) {
             throw new RuntimeException("Can't read file " + fromFileName, e);
         }
-        return stringBuilder.toString();
+        return content.toString();
     }
 
     private String generateReport(String inputInfo) {
-        if (inputInfo.length() == 0) {
-            return null;
-        }
-        StringBuilder resultRepotr = new StringBuilder();
+        StringBuilder resultReport = new StringBuilder();
         String[] inputArr = inputInfo.split(System.lineSeparator());
         int countSupply = 0;
         int countBuy = 0;
         for (String lines : inputArr) {
-            if (lines.split(LIMIT_FIELD)[INDEX_OPERATION].equals(OPEERATION_ARRIVAL)) {
+            if (lines.split(DELIMITER)[INDEX_OPERATION].equals(SUPPLY_OPERATION)) {
                 countSupply = countSupply
-                        + Integer.parseInt(lines.split(LIMIT_FIELD)[INDEX_DATA]);
+                        + Integer.parseInt(lines.split(DELIMITER)[INDEX_DATA]);
             } else {
                 countBuy = countBuy
-                        + Integer.parseInt(lines.split(LIMIT_FIELD)[INDEX_DATA]);
+                        + Integer.parseInt(lines.split(DELIMITER)[INDEX_DATA]);
             }
         }
-        return (resultRepotr.append(OPEERATION_ARRIVAL).append(LIMIT_FIELD)
+        return (resultReport.append(SUPPLY_OPERATION).append(DELIMITER)
                 .append(countSupply).append(System.lineSeparator())
-                .append(OPERATION_EXPENSE).append(LIMIT_FIELD).append((countBuy))
+                .append(OPERATION_EXPENSE).append(DELIMITER).append((countBuy))
                 .append(System.lineSeparator())
-                .append("result").append(LIMIT_FIELD).append(countSupply - countBuy)
+                .append("result").append(DELIMITER).append(countSupply - countBuy)
                 .append(System.lineSeparator()).toString());
     }
 
-    private void wtiteToFile(String reportText, String toFileName) {
-
-        File file = new File(toFileName);
-        try {
-            file.createNewFile();
+    private void writeToFile(String reportText, String toFileName) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+            bufferedWriter.write(reportText);
         } catch (IOException e) {
-            throw new RuntimeException("Can't create file " + toFileName, e);
-        }
-        if (reportText.length() != 0) {
-            try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
-                bufferedWriter.write(reportText);
-            } catch (IOException e) {
-                throw new RuntimeException("Can't write data to file " + toFileName, e);
-            }
+            throw new RuntimeException("Can't write data to file " + toFileName, e);
         }
     }
 }
