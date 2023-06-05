8e62a62c097efaab4194ea4164fce295501bdfe0
Sergey Bogatkin <daniltisenko22@gmail.com>
2023-06-05 11:47:06 +0300

    Separated logic and improved code readability

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,6 +2,7 @@ package core.basesyntax;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -9,47 +10,66 @@ import java.io.IOException;
 public class WorkWithFile {
     public static final int OPERATION_TYPE = 0;
     public static final int AMOUNT = 1;
-    private int buy = 0;
-    private int supply = 0;
+    public static final String SEPARATOR = ",";
+    public static final String BUY = "buy";
+    public static final String SUPPLY = "supply";
+    public static final String RESULT = "result";
 
-    private void parsFile(String fileName) {
+    public void getStatistic(String fromFileName, String toFileName) {
+        String data = readFromFile(fromFileName);
+        String report = createReport(data);
+        writeToFile(toFileName, report);
+    }
+
+    private String readFromFile(String fileName) {
+        File file = new File(fileName);
+        StringBuilder result = new StringBuilder();
         try {
-            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            BufferedReader reader = new BufferedReader(new FileReader(file));
             String value = reader.readLine();
             while (value != null) {
-                String[] lineInFile = value.split(",");
-                if (lineInFile[OPERATION_TYPE] != null
-                        && lineInFile[OPERATION_TYPE].equals("buy")) {
-                    buy += Integer.parseInt(lineInFile[AMOUNT]);
-                }
-                if (lineInFile[OPERATION_TYPE] != null
-                        && lineInFile[OPERATION_TYPE].equals("supply")) {
-                    supply += Integer.parseInt(lineInFile[AMOUNT]);
-                }
+                result.append(value).append(System.lineSeparator());
                 value = reader.readLine();
             }
         } catch (IOException e) {
-            throw new RuntimeException("Can't read file", e);
+            throw new RuntimeException("Can't read file: " + fileName, e);
         }
+        return result.toString();
     }
 
-    private void createReport(String fileName) {
-        StringBuilder stringBuilder = new StringBuilder();
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
-            stringBuilder
-                    .append("supply,").append(supply).append(System.lineSeparator())
-                    .append("buy,").append(buy).append(System.lineSeparator())
-                    .append("result,").append(supply - buy);
-            writer.write(stringBuilder.toString());
-            buy = 0;
-            supply = 0;
+    private String createReport(String data) {
+        int buy = 0;
+        int supply = 0;
+        StringBuilder report = new StringBuilder();
+        if (data != null) {
+            String[] arrLines = data.split(System.lineSeparator());
+            for (String line:arrLines) {
+                String[] temp = line.split(SEPARATOR);
+                switch (temp[OPERATION_TYPE]) {
+                    case BUY:
+                        buy += Integer.parseInt(temp[AMOUNT]);
+                        break;
+                    case SUPPLY:
+                        supply += Integer.parseInt(temp[AMOUNT]);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            int result = supply - buy;
+            report.append(SUPPLY).append(SEPARATOR).append(supply).append(System.lineSeparator())
+                    .append(BUY).append(SEPARATOR).append(buy).append(System.lineSeparator())
+                    .append(RESULT).append(SEPARATOR).append(result);
+        }
+        return report.toString();
+    }
+
+    private void writeToFile(String fileName, String report) {
+        File file = new File(fileName);
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+            writer.write(report);
         } catch (IOException e) {
-            throw new RuntimeException("Can't write data to file", e);
+            throw new RuntimeException("Can't write data to file: " + fileName, e);
         }
-    }
-
-    public void getStatistic(String fromFileName, String toFileName) {
-        parsFile(fromFileName);
-        createReport(toFileName);
     }
 }
