0e2aafe0b61464d1517a248a2ddf487752cadc60
happytroya <happytroya2001@gmail.com>
2023-06-05 14:13:15 +0300

    split getStaticMethod to 3 different methods, removed textToWrite variable inside the method generateReport

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -13,49 +13,63 @@ public class WorkWithFile {
     private static final String RESULT = "result";
     private static final int NAME_INDEX = 0;
     private static final int VALUE_INDEX = 1;
-    private StringBuilder textToWrite = new StringBuilder();
 
     public void getStatistic(String fromFileName, String toFileName) {
+
+        String[] splittedText = readFile(fromFileName);
+
+        String report = generateReport(splittedText);
+
+        writeFromFile(toFileName, report);
+    }
+
+    private String[] readFile(String fromFileName) {
         try {
             String text = new String(Files.readAllBytes(Paths.get(fromFileName)));
 
             String[] splittedText = text.split(System.lineSeparator());
-
-            int supplyCount = 0;
-            int buyCount = 0;
-            int result = 0;
-
-            for (String line : splittedText) {
-                String[] split = line.split(SPLITTER);
-
-                if (split[NAME_INDEX].equals(SUPPLY)) {
-                    supplyCount += Integer.parseInt(split[VALUE_INDEX]);
-                } else if (split[NAME_INDEX].equals(BUY)) {
-                    buyCount += Integer.parseInt(split[VALUE_INDEX]);
-                }
-            }
-
-            result = supplyCount - buyCount;
-
-            textToWrite.append(SUPPLY)
-                    .append(SPLITTER)
-                    .append(supplyCount)
-                    .append(System.lineSeparator())
-                    .append(BUY)
-                    .append(SPLITTER)
-                    .append(buyCount)
-                    .append(System.lineSeparator())
-                    .append(RESULT)
-                    .append(SPLITTER)
-                    .append(result);
-
+            return splittedText;
         } catch (IOException e) {
             throw new RuntimeException("Can`t read the file", e);
         }
+    }
 
+    private String generateReport(String[] splittedTextFromFile) {
+        StringBuilder textToWrite = new StringBuilder();
+        int supplyCount = 0;
+        int buyCount = 0;
+        int result;
+
+        for (String line : splittedTextFromFile) {
+            String[] split = line.split(SPLITTER);
+
+            if (split[NAME_INDEX].equals(SUPPLY)) {
+                supplyCount += Integer.parseInt(split[VALUE_INDEX]);
+            } else if (split[NAME_INDEX].equals(BUY)) {
+                buyCount += Integer.parseInt(split[VALUE_INDEX]);
+            }
+        }
+
+        result = supplyCount - buyCount;
+
+        textToWrite.append(SUPPLY)
+                .append(SPLITTER)
+                .append(supplyCount)
+                .append(System.lineSeparator())
+                .append(BUY)
+                .append(SPLITTER)
+                .append(buyCount)
+                .append(System.lineSeparator())
+                .append(RESULT)
+                .append(SPLITTER)
+                .append(result);
+
+        return textToWrite.toString();
+    }
+
+    private void writeFromFile(String toFileName, String textToWrite) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
             writer.write(textToWrite.toString());
-            textToWrite.setLength(0);
         } catch (IOException e) {
             throw new RuntimeException("Can`t write to file", e);
         }
