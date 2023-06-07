515a544e689d8a501041bec735106db41e9d3e09
Yura Khilko <yurakhilko@gmail.com>
2023-06-07 21:39:34 +0300

    fixes after review: divided method into two separate readFromFile and createReport.

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -13,34 +13,44 @@ public class WorkWithFile {
     private static final String SUPPLY_OPERATION_TYPE = OperationType.SUPPLY.name().toLowerCase();
     private static final String BUY_OPERATION_TYPE = OperationType.BUY.name().toLowerCase();
     private static StringBuilder result;
+    private static String textFromFile;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        getReportFromFile(fromFileName);
+        readFromFile(fromFileName);
+        createReport();
         writeReportToFile(toFileName);
     }
 
-    private void getReportFromFile(String fromFileName) {
+    private void readFromFile(String fromFileName) {
         File fromFile = new File(fromFileName);
-        int firstOperationTypeSum = 0;
-        int secondOperationTypeSum = 0;
         String dataRow;
 
         try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFile));
             dataRow = bufferedReader.readLine();
+            result = new StringBuilder();
             do {
-                String[] dataRowSplit = dataRow.split(",");
-                if (SUPPLY_OPERATION_TYPE.equals(dataRowSplit[OPERATION_TYPE_INDEX])) {
-                    firstOperationTypeSum += Integer.valueOf(dataRowSplit[AMOUNT_INDEX]);
-                } else if (BUY_OPERATION_TYPE.equals(dataRowSplit[OPERATION_TYPE_INDEX])) {
-                    secondOperationTypeSum += Integer.valueOf(dataRowSplit[AMOUNT_INDEX]);
-                }
+                result.append(dataRow).append(System.lineSeparator());
                 dataRow = bufferedReader.readLine();
             } while (dataRow != null);
         } catch (IOException e) {
             throw new RuntimeException("Can't read file", e);
         }
+        textFromFile = result.toString();
+    }
 
+    private void createReport() {
+        int firstOperationTypeSum = 0;
+        int secondOperationTypeSum = 0;
+        String[] dataRowSplit = textFromFile.split(System.lineSeparator());
+        for (String dataRow : dataRowSplit) {
+            String[] dataRowAndColumnSplit = dataRow.split(",");
+            if (SUPPLY_OPERATION_TYPE.equals(dataRowAndColumnSplit[OPERATION_TYPE_INDEX])) {
+                firstOperationTypeSum += Integer.valueOf(dataRowAndColumnSplit[AMOUNT_INDEX]);
+            } else if (BUY_OPERATION_TYPE.equals(dataRowAndColumnSplit[OPERATION_TYPE_INDEX])) {
+                secondOperationTypeSum += Integer.valueOf(dataRowAndColumnSplit[AMOUNT_INDEX]);
+            }
+        }
         result = new StringBuilder().append(SUPPLY_OPERATION_TYPE).append(',')
                 .append(firstOperationTypeSum)
                 .append(System.lineSeparator())
