34cebea15686399e9d5d4d53f37352afb2f3ece1
Oleh Zhabrovets <zh_oleg1997@icloud.com>
2023-06-07 12:23:52 +0300

    created new csv file with informatoin about operation

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,37 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
-
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName));
+                BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+            int supplyTotal = 0;
+            int buyTotal = 0;
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] fields = line.split(",");
+                String operationType = fields[0];
+                int amount = Integer.parseInt(fields[1]);
+                if (operationType.equals("supply")) {
+                    supplyTotal += amount;
+                } else if (operationType.equals("buy")) {
+                    buyTotal += amount;
+                }
+            }
+            int result = supplyTotal - buyTotal;
+            bufferedWriter.write("supply," + supplyTotal);
+            bufferedWriter.newLine();
+            bufferedWriter.write("buy," + buyTotal);
+            bufferedWriter.newLine();
+            bufferedWriter.write("result," + result);
+            bufferedWriter.newLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
