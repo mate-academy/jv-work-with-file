8303a59e5d3a046f84182254960602140eef6bc4
Oleh Zhabrovets <zh_oleg1997@icloud.com>
2023-06-07 22:42:50 +0300

    added methods to code

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,34 +2,62 @@ package core.basesyntax;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName));
-                BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
-            int supplyTotal = 0;
-            int buyTotal = 0;
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                String[] fields = line.split(",");
-                String operationType = fields[0];
-                int amount = Integer.parseInt(fields[1]);
-                if (operationType.equals("supply")) {
-                    supplyTotal += amount;
-                } else if (operationType.equals("buy")) {
-                    buyTotal += amount;
-                }
+        String value = readFile(fromFileName);
+        String report = createReport(value);
+        fileWrite(toFileName, report);
+    }
+
+    private static String readFile(String fromFileName) {
+        File file = new File(fromFileName);
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            StringBuilder builder = new StringBuilder();
+            String value = bufferedReader.readLine();
+            while (value != null) {
+                builder.append(value).append(",");
+                value = bufferedReader.readLine();
             }
-            int result = supplyTotal - buyTotal;
-            bufferedWriter.write("supply," + supplyTotal);
-            bufferedWriter.newLine();
-            bufferedWriter.write("buy," + buyTotal);
-            bufferedWriter.newLine();
-            bufferedWriter.write("result," + result);
-            bufferedWriter.newLine();
+            return builder.toString();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String createReport(String value) {
+        int supplyTotal = 0;
+        int buyTotal = 0;
+        int result = 0;
+        String[] data = value.split(",");
+        for (int i = 0; i < data.length; i++) {
+            if (data[i].equals("supply")) {
+                supplyTotal += Integer.parseInt(data[i + 1]);
+            } else if (data[i].equals("buy")) {
+                buyTotal += Integer.parseInt(data[i + 1]);
+            }
+            result = supplyTotal - buyTotal;
+        }
+        StringBuilder resultBuilder = new StringBuilder();
+        StringBuilder report = resultBuilder.append("supply").append(",").append(supplyTotal)
+                .append(System.lineSeparator())
+                .append("buy").append(",").append(buyTotal)
+                .append(System.lineSeparator())
+                .append("result").append(",").append(result);
+        return String.valueOf(report);
+    }
+
+    private static void fileWrite(String toFileName, String report) {
+        File file = new File(toFileName);
+        try {
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName));
+            bufferedWriter.write(String.valueOf(report));
+            bufferedWriter.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
