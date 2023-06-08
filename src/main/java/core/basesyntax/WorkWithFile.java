ff7f0c734260ca798abf2d51e675306ee0cfddf9
OLYA <Ole4kastar79@gmail.com>
2023-06-08 11:47:18 +0300

    fixed CI builds errors: whitespaces constants names

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,11 +8,17 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    static final String SUPPLY_WORD = "supply";
+    static final String BUY_WORD = "buy";
+    static final String RESULT_WORD = "result";
+    static final int NAME_COLUMN_INDEX = 0;
+    static final int VALUE_COLUMN_INDEX = 1;
+
     public void getStatistic(String fromFileName, String toFileName) {
         writeToFile(getFromFile(fromFileName),toFileName);
     }
 
-    private  String getFromFile(String fromFileName) {
+    private String getFromFile(String fromFileName) {
         File file = new File(fromFileName);
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             StringBuilder stringBuilder = new StringBuilder();
@@ -31,29 +37,24 @@ public class WorkWithFile {
         }
     }
 
-    private  String countFromString (String input) {
-        final String SUPPLY_WORD = "supply";
-        final String BUY_WORD = "buy";
-        final String RESULT_WORD = "result";
-        final int SUPPLY_INDEX = 0;
-        final int BUY_INDEX = 1;
-        final int NAME_COLUMN_INDEX = 0;
-        final int VALUE_COLUMN_INDEX = 1;
-        int[] resultValues = new int[]{0,0};
+    private String countFromString(String input) {
+        final int indexSupply = 0;
+        final int indexBuy = 1;
+        final int[] resultValues = new int[]{0,0};
         String[] records = input.split(";");
-        for (String record:records) {
+        for(String record:records) {
             String[] dataRecord = record.split(",");
-            if (dataRecord[NAME_COLUMN_INDEX].equals(SUPPLY_WORD)) {
-                resultValues[SUPPLY_INDEX] += Integer.parseInt(dataRecord[VALUE_COLUMN_INDEX]);
-            } else if (dataRecord[NAME_COLUMN_INDEX].equals(BUY_WORD)) {
-                resultValues[BUY_INDEX] += Integer.parseInt(dataRecord[VALUE_COLUMN_INDEX]);
+            if(dataRecord[NAME_COLUMN_INDEX].equals(SUPPLY_WORD)) {
+                resultValues[indexSupply] += Integer.parseInt(dataRecord[VALUE_COLUMN_INDEX]);
+            } else if(dataRecord[NAME_COLUMN_INDEX].equals(BUY_WORD)) {
+                resultValues[indexBuy] += Integer.parseInt(dataRecord[VALUE_COLUMN_INDEX]);
             }
         }
-        int resultAmount =  resultValues[SUPPLY_INDEX] - resultValues[BUY_INDEX];
+        int resultAmount = resultValues[indexSupply] - resultValues[indexBuy];
         StringBuilder builder = new StringBuilder();
-        builder.append(SUPPLY_WORD).append(",").append(resultValues[SUPPLY_INDEX]);
+        builder.append(SUPPLY_WORD).append(",").append(resultValues[indexSupply]);
         builder.append(System.lineSeparator());
-        builder.append(BUY_WORD).append(",").append(resultValues[BUY_INDEX]);
+        builder.append(BUY_WORD).append(",").append(resultValues[indexBuy]);
         builder.append(System.lineSeparator());
         builder.append(RESULT_WORD).append(",").append(resultAmount);
         builder.append(System.lineSeparator());
