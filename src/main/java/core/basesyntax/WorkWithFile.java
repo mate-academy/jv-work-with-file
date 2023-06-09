346154f427b0f29daa2a0e83bbd47d32d3e84194
OLYA <Ole4kastar79@gmail.com>
2023-06-10 00:36:39 +0300

    fixed :CI - whitespaces

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,15 +8,13 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    static final String SUPPLY_WORD = "supply";
-    static final String BUY_WORD = "buy";
-    static final String RESULT_WORD = "result";
-    static final int NAME_COLUMN_INDEX = 0;
-    static final int VALUE_COLUMN_INDEX = 1;
-    static final int SUPPLY_INDEX = 0;
-    static final int BUY_INDEX = 1;
-    static final String DATA_SEPARATOR = ",";
-    static final String RECORD_SEPARATOR = ";";
+    private static final String SUPPLY_WORD = "supply";
+    private static final String BUY_WORD = "buy";
+    private static final String RESULT_WORD = "result";
+    private static final int NAME_COLUMN_INDEX = 0;
+    private static final int VALUE_COLUMN_INDEX = 1;
+    private static final String DATA_SEPARATOR = ",";
+    private static final String RECORD_SEPARATOR = ";";
 
     public void getStatistic(String fromFileName, String toFileName) {
         String dataFile = readFromFile(fromFileName);
@@ -39,32 +37,33 @@ public class WorkWithFile {
             }
             return stringBuilder.toString();
         } catch (IOException e) {
-            throw new RuntimeException("can't read file");
+            throw new RuntimeException("can't read file" + fromFileName, e );
         }
     }
 
     private String countReportResult(String input) {
         String[] records = input.split(RECORD_SEPARATOR);
-        final int[] resultValues = new int[]{0, 0};
+        int supply = 0;
+        int buy = 0;
         for (String record : records) {
             String[] dataRecord = record.split(DATA_SEPARATOR);
             if (dataRecord[NAME_COLUMN_INDEX].equals(SUPPLY_WORD)) {
-                resultValues[SUPPLY_INDEX] += Integer.parseInt(dataRecord[VALUE_COLUMN_INDEX]);
+                supply += Integer.parseInt(dataRecord[VALUE_COLUMN_INDEX]);
             } else if (dataRecord[NAME_COLUMN_INDEX].equals(BUY_WORD)) {
-                resultValues[BUY_INDEX] += Integer.parseInt(dataRecord[VALUE_COLUMN_INDEX]);
+                buy += Integer.parseInt(dataRecord[VALUE_COLUMN_INDEX]);
             }
         }
-        String result = createReportString(resultValues);
+        String result = createReportString(supply,buy);
         return result;
     }
 
-    private String createReportString(int[] resultValues) {
+    private String createReportString(int supply,int buy) {
         StringBuilder builder = new StringBuilder();
-        builder.append(SUPPLY_WORD).append(DATA_SEPARATOR).append(resultValues[SUPPLY_INDEX]);
+        builder.append(SUPPLY_WORD).append(DATA_SEPARATOR).append(supply);
         builder.append(System.lineSeparator());
-        builder.append(BUY_WORD).append(DATA_SEPARATOR).append(resultValues[BUY_INDEX]);
+        builder.append(BUY_WORD).append(DATA_SEPARATOR).append(buy);
         builder.append(System.lineSeparator());
-        int resultAmount = resultValues[SUPPLY_INDEX] - resultValues[BUY_INDEX];
+        int resultAmount = supply - buy;
         builder.append(RESULT_WORD).append(DATA_SEPARATOR).append(resultAmount);
         builder.append(System.lineSeparator());
         return builder.toString();
@@ -72,10 +71,10 @@ public class WorkWithFile {
 
     private void writeToFile(String output,String toFileName) {
         File file = new File(toFileName);
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
-            bufferedWriter.write(output);
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+            writer.write(output);
         } catch (IOException e) {
-            throw new RuntimeException();
+            throw new RuntimeException("can't write to  file" + toFileName, e );
         }
     }
 }
