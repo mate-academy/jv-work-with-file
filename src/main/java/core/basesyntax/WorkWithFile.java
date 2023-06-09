1d303f2421f11e5b04530d5d7ec45aa7392774ee
Anna Poplavska <semachka100201@gmail.com>
2023-06-09 20:10:42 +0200

    Create createReport()

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -6,9 +6,15 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class WorkWithFile {
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+    private static final int OPERATION_TYPE = 0;
+    private static final int AMMOUNT = 1;
+
     public void getStatistic(String fromFileName, String toFileName) {
         WorkWithFile workWithFile = new WorkWithFile();
-        String fileInfo = workWithFile.readFile(fromFileName);
+        String[] fileInfo = workWithFile.readFile(fromFileName).split(System.lineSeparator());
+        String report = workWithFile.createReport(fileInfo);
 
 
     }
@@ -27,5 +33,27 @@ public class WorkWithFile {
             throw new RuntimeException("Can't read data from the file " + fromFileName, e);
         }
         return stringBuilder.toString();
+    }
+
+    public String createReport(String[] fileInfo) {
+        String[] lineInfo;
+        int numberOfSupply = 0, numberOfBuy = 0, totalNumber = 0;
+        StringBuilder report = new StringBuilder();
+        for (String info : fileInfo) {
+            lineInfo = info.split(",");
+            if (lineInfo[OPERATION_TYPE].equals(SUPPLY)) {
+                numberOfSupply += Integer.parseInt(lineInfo[AMMOUNT]);
+                totalNumber += numberOfSupply;
+            } else if (lineInfo[OPERATION_TYPE].equals(BUY)) {
+                numberOfBuy += Integer.parseInt(lineInfo[AMMOUNT]);
+                totalNumber -= numberOfBuy;
+            }
+        }
+        report.append("supply," + numberOfSupply)
+                .append(System.lineSeparator())
+                .append("buy," + numberOfBuy)
+                .append(System.lineSeparator())
+                .append("result," + totalNumber);
+        return report.toString();
     }
 }
