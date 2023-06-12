e23962396ac540646d838092f1acb9e8420dd22e
Anna Poplavska <semachka100201@gmail.com>
2023-06-12 16:37:28 +0200

    Create writeFile(), task done

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,29 +1,30 @@
 package core.basesyntax;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
     private static final String SUPPLY = "supply";
     private static final String BUY = "buy";
     private static final int OPERATION_TYPE = 0;
-    private static final int AMMOUNT = 1;
+    private static final int AMOUNT = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
         WorkWithFile workWithFile = new WorkWithFile();
         String[] fileInfo = workWithFile.readFile(fromFileName).split(System.lineSeparator());
         String report = workWithFile.createReport(fileInfo);
-
-
+        workWithFile.writeFile(report, toFileName);
     }
 
-    public String readFile(String fromFileName) {
+    private String readFile(String fromFileName) {
         File fileWithInfo = new File(fromFileName);
         StringBuilder stringBuilder = new StringBuilder();
         try (FileReader fileReader = new FileReader(fileWithInfo);
-             BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+                BufferedReader bufferedReader = new BufferedReader(fileReader)) {
             String info = bufferedReader.readLine();
             while (info != null) {
                 stringBuilder.append(info).append(System.lineSeparator());
@@ -35,25 +36,35 @@ public class WorkWithFile {
         return stringBuilder.toString();
     }
 
-    public String createReport(String[] fileInfo) {
+    private String createReport(String[] fileInfo) {
         String[] lineInfo;
-        int numberOfSupply = 0, numberOfBuy = 0, totalNumber = 0;
+        int numberOfSupply = 0;
+        int numberOfBuy = 0;
+        int totalNumber = 0;
         StringBuilder report = new StringBuilder();
         for (String info : fileInfo) {
             lineInfo = info.split(",");
             if (lineInfo[OPERATION_TYPE].equals(SUPPLY)) {
-                numberOfSupply += Integer.parseInt(lineInfo[AMMOUNT]);
-                totalNumber += numberOfSupply;
+                numberOfSupply += Integer.parseInt(lineInfo[AMOUNT]);
             } else if (lineInfo[OPERATION_TYPE].equals(BUY)) {
-                numberOfBuy += Integer.parseInt(lineInfo[AMMOUNT]);
-                totalNumber -= numberOfBuy;
+                numberOfBuy += Integer.parseInt(lineInfo[AMOUNT]);
             }
         }
+        totalNumber = numberOfSupply - numberOfBuy;
         report.append("supply," + numberOfSupply)
                 .append(System.lineSeparator())
                 .append("buy," + numberOfBuy)
                 .append(System.lineSeparator())
                 .append("result," + totalNumber);
         return report.toString();
+    }
+
+    private void writeFile(String report, String toFileName) {
+        try (FileWriter fileWriter = new FileWriter(toFileName);
+                BufferedWriter bufferedWriter = new BufferedWriter(fileWriter)) {
+            bufferedWriter.write(report);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write data to the file " + toFileName, e);
+        }
     }
 }
