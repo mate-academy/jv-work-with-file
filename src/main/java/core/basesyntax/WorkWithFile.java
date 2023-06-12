6309c1f946aed0ea31223e297cd8ef063f8ffe27
Sergei Kudelin <chertterner686@gmail.com>
2023-06-12 17:28:50 +0300

    fixed  commit

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,6 +1,9 @@
 package core.basesyntax;
 
-import java.io.*;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class WorkWithFile {
@@ -11,26 +14,25 @@ public class WorkWithFile {
         int sumSupply = 0;
         int sumBuy = 0;
         StringBuilder stringBuilder = new StringBuilder();
-     try (Scanner scanner = new Scanner(new File(fromFileName))) {
-        while (scanner.hasNext()) {
-            String[] strings = scanner.nextLine().split(",");
-            int number = Integer.parseInt(strings[SECOND_INDEX]);
-            String doing = strings[FIRST_INDEX];
-            if (doing.equals("supply")) {
-                sumSupply += number;
-            } else {
-                sumBuy += number;
+        try (Scanner scanner = new Scanner(new File(fromFileName))) {
+            while (scanner.hasNext()) {
+                String[] strings = scanner.nextLine().split(",");
+                int number = Integer.parseInt(strings[SECOND_INDEX]);
+                String doing = strings[FIRST_INDEX];
+                if (doing.equals("supply")) {
+                    sumSupply += number;
+                } else {
+                    sumBuy += number;
+                }
             }
+            stringBuilder.append("supply,").append(sumSupply).append(System.lineSeparator());
+            stringBuilder.append("buy,").append(sumBuy).append(System.lineSeparator());
+            stringBuilder.append("result,").append(sumSupply - sumBuy);
+            BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName));
+            writer.write(stringBuilder.toString());
+            writer.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-         stringBuilder.append("supply,").append(sumSupply).append(System.lineSeparator());
-         stringBuilder.append("buy,").append(sumBuy).append(System.lineSeparator());
-         stringBuilder.append("result,").append(sumSupply - sumBuy);
-         BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName));
-         writer.write(stringBuilder.toString());
-         writer.close();
-     } catch (IOException e) {
-         throw new RuntimeException(e);
-     }
-
     }
 }
