9fc0efc22833fa65b01a564d2c4d90facb6c53c7
Sergei Kudelin <chertterner686@gmail.com>
2023-06-15 13:12:16 +0300

    divided logic

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,42 +8,49 @@ import java.util.Scanner;
 
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
-        int index = 0;
         try {
-            int[] sumArray = readSumFromFile(fromFileName);
-            writeInFile(sumArray[index], sumArray[index + 1], toFileName);
+            String dataFromFile = readFromFile(fromFileName);
+            String sumReport = getSumReportFromFile(dataFromFile);
+            writeInFile(toFileName, sumReport);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private int[] readSumFromFile(String fromFile) throws IOException {
+    private String readFromFile(String fromFile) throws IOException {
+        Scanner scanner = new Scanner(new File(fromFile));
+        StringBuilder stringBuilder = new StringBuilder();
+        while (scanner.hasNext()) {
+            stringBuilder.append(scanner.nextLine()).append(",");
+        }
+        scanner.close();
+
+        return stringBuilder.toString();
+    }
+
+    private void writeInFile(String toFile, String sumReport) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new FileWriter(toFile));
+        writer.write(sumReport);
+        writer.close();
+    }
+
+    private String getSumReportFromFile(String dataFromFile) {
         int supplySum = 0;
         int buySum = 0;
-        int index = 0;
-        Scanner scanner = new Scanner(new File(fromFile));
-        while (scanner.hasNext()) {
-            String[] strings = scanner.nextLine().split(",");
-            String doing = strings[index];
+        String[] strings = dataFromFile.split(",");
+        for (int index = 0; index < strings.length; index += 2) {
+            String info = strings[index];
             int number = Integer.parseInt(strings[index + 1]);
-            if (doing.equals("supply")) {
+            if (info.equals("supply")) {
                 supplySum += number;
             } else {
                 buySum += number;
             }
         }
-        scanner.close();
 
-        return new int[]{supplySum, buySum};
-    }
-
-    private void writeInFile(int sumSupply, int sumBuy, String toFile) throws IOException {
-        BufferedWriter writer = new BufferedWriter(new FileWriter(toFile));
-        String stringBuilder = "supply," + sumSupply + System.lineSeparator()
-                + "buy," + sumBuy + System.lineSeparator()
-                + "result," + (sumSupply - sumBuy);
-        writer.write(stringBuilder);
-        writer.close();
+        return "supply," + supplySum + System.lineSeparator()
+                + "buy," + buySum + System.lineSeparator()
+                + "result," + (supplySum - buySum);
     }
 }
 
