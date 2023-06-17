8bdde152a0f2fb1fac12199f651102fa29d37271
barbequeue <p99kab00@gmail.com>
2023-06-17 15:45:38 +0300

    Implement logic to read CSV from the file and generate report to other file

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,17 @@
 package core.basesyntax;
 
 public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
+    private final FileContentReader fileContentReader = new FileContentReader();
+    private final ReportBuilder reportBuilder = new ReportBuilder();
+    private final FileContentWriter fileContentWriter = new FileContentWriter();
 
+    public void getStatistic(String fromFileName, String toFileName) {
+        if (fromFileName == null || toFileName == null) {
+            return;
+        }
+
+        String content = fileContentReader.read(fromFileName);
+        String resultContent = reportBuilder.fromCsvString(content);
+        fileContentWriter.write(toFileName, resultContent);
     }
 }
