0443124180a57555e6ff66eb76c6587f5255cfc9
Efim Turkovskiy <turkovskiy.efim@outlook.com>
2023-06-21 14:29:15 +0300

    fixedTask

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -12,10 +12,26 @@ public class WorkWithFile {
     private static final int AMOUNT_INDEX = 1;
     private static final String SUPPLY = "supply";
     private static final String BUY = "buy";
+    private int supplyAmount = 0;
+    private int buyAmount = 0;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        int supplyAmount = 0;
-        int buyAmount = 0;
+        supplyAmount = 0;
+        buyAmount = 0;
+        String[] dataArray = readFromFile(fromFileName).split(System.lineSeparator());
+        for (String data : dataArray) {
+            String[] dataPerElement = data.split(",");
+            if (dataPerElement[OPERATION_INDEX].equals(SUPPLY)) {
+                supplyAmount += Integer.parseInt(dataPerElement[AMOUNT_INDEX]);
+            }
+            if (dataPerElement[OPERATION_INDEX].equals(BUY)) {
+                buyAmount += Integer.parseInt(dataPerElement[AMOUNT_INDEX]);
+            }
+        }
+        writeToFile(toFileName);
+    }
+
+    private String readFromFile(String fromFileName) {
         StringBuilder builder = new StringBuilder();
         try {
             BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
@@ -29,23 +45,17 @@ public class WorkWithFile {
         } catch (IOException e) {
             throw new RuntimeException("Couldn't read from file", e);
         }
-        String[] dataArray = builder.toString().split(System.lineSeparator());
-        for (String data : dataArray) {
-            String[] dataPerElement = data.split(",");
-            if (dataPerElement[OPERATION_INDEX].equals(SUPPLY)) {
-                supplyAmount += Integer.parseInt(dataPerElement[AMOUNT_INDEX]);
-            }
-            if (dataPerElement[OPERATION_INDEX].equals(BUY)) {
-                buyAmount += Integer.parseInt(dataPerElement[AMOUNT_INDEX]);
-            }
-        }
+        return builder.toString();
+    }
+
+    private void writeToFile(String toFileName) {
         int result = supplyAmount - buyAmount;
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
             writer.write("supply," + supplyAmount + System.lineSeparator());
             writer.write("buy," + buyAmount + System.lineSeparator());
             writer.write("result," + result);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Couldn't write data to file", e);
         }
     }
 }
