d3cbe6188db72ba142535672e73e74b7fccf0ac6
petrozagorodnii <patriculusus@gmail.com>
2023-06-21 14:32:43 +0300

    Fix: readFromFile method don't calculate doda from report (this action moved to createReport method);

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -12,54 +12,60 @@ public class WorkWithFile {
     private static final String BUY = "buy";
     private static final String RESULT = "result";
     private static final String COMMA = ",";
+    private static final int STRING_VALUE_INDEX = 0;
+    private static final int INTEGER_VALUE_INDEX = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        String reportData = readFromFile(fromFileName);
-        String report = createReport(reportData);
+        String fileContent = readFromFile(fromFileName);
+        String report = createReport(fileContent);
         writeToFile(report, toFileName);
     }
 
     private String readFromFile(String fromFileName) {
-        int supply = 0;
-        int buy = 0;
         File file = new File(fromFileName);
+        StringBuilder fileData = new StringBuilder();
         if (file.exists()) {
             try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-                String readeValue = reader.readLine();
-                while (readeValue != null) {
-                    String[] bufferString = readeValue.split(COMMA);
-                    if (bufferString[0].equals(SUPPLY)) {
-                        supply += Integer.parseInt(bufferString[1]);
-                    }
-                    if (bufferString[0].equals(BUY)) {
-                        buy += Integer.parseInt(bufferString[1]);
-                    }
-                    readeValue = reader.readLine();
+                int readeValue = reader.read();
+                while (readeValue != -1) {
+                    fileData.append((char) readeValue);
+                    readeValue = reader.read();
                 }
             } catch (IOException e) {
                 throw new RuntimeException("Can't read file" + fromFileName, e);
             }
         }
-        return supply + " " + buy;
+        return fileData.toString();
     }
 
     private void writeToFile(String report, String toFileName) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
             writer.write(report);
         } catch (IOException e) {
-            throw new RuntimeException("Can't write to file", e);
+            throw new RuntimeException("Can't write to file" + toFileName, e);
         }
     }
 
     private String createReport(String reportData) {
-        String[] report = reportData.split(" ");
+        int supply = 0;
+        int buy = 0;
+        String[] reportDataArray = reportData.split(System.lineSeparator());
+        for (String reportDataLine : reportDataArray) {
+            String[] bufferString = reportDataLine.split(COMMA);
+            if (bufferString[STRING_VALUE_INDEX].equals(SUPPLY)) {
+                supply += Integer.parseInt(bufferString[INTEGER_VALUE_INDEX]);
+            }
+            if (bufferString[STRING_VALUE_INDEX].equals(BUY)) {
+                buy += Integer.parseInt(bufferString[INTEGER_VALUE_INDEX]);
+            }
+        }
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(SUPPLY).append(COMMA).append(report[0])
+        stringBuilder.append(SUPPLY).append(COMMA).append(supply)
                 .append(System.lineSeparator());
-        stringBuilder.append(BUY).append(COMMA).append(report[1])
+        stringBuilder.append(BUY).append(COMMA).append(buy)
                 .append(System.lineSeparator());
         stringBuilder.append(RESULT).append(COMMA)
-                .append(Integer.parseInt(report[0]) - Integer.parseInt(report[1]));
+                .append(supply - buy);
         return stringBuilder.toString();
     }
 }
