37bddc3b787037931c295cb098162f49be21a161
Denys-Maksym Kachalup <denys.k82@gmail.com>
2023-06-21 21:57:44 +0300

    implemeted classes writeFile, readFile and work with data from file

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,58 @@
 package core.basesyntax;
 
-public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class WorkWithFile {
+    private static final String SPLIT_REGEX = ",";
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        int totalSupply = 0;
+        int totalBuy = 0;
+        String[] strings = readFile(fromFileName);
+
+        for (String string : strings) {
+            String[] splitString = string.split(SPLIT_REGEX);
+            if (splitString[0].equals("supply")) {
+                totalSupply += Integer.parseInt(splitString[1]);
+            }
+            if (splitString[0].equals("buy")) {
+                totalBuy += Integer.parseInt(splitString[1]);
+            }
+        }
+        writeFile(toFileName, totalSupply, totalBuy);
+    }
+
+    private String[] readFile(String fromFileName) {
+        File fromFile = new File(fromFileName);
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFile))) {
+            String value = bufferedReader.readLine();
+            while (value != null) {
+                stringBuilder.append(value).append(System.lineSeparator());
+                value = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return stringBuilder.toString().split(System.lineSeparator());
+    }
+
+    private void writeFile(String toFileName, int totalSupply, int totalBuy) {
+        File toFile = new File(toFileName);
+        int result = totalSupply - totalBuy;
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFile))) {
+            toFile.createNewFile();
+            bufferedWriter.write("supply," + totalSupply + System.lineSeparator()
+                    + "buy," + totalBuy + System.lineSeparator()
+                    + "result," + result);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
