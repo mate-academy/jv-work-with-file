ecc66a2036a7ffaacd853f7f86c27210b06f961b
Andriy Karpa <McMerfee@i.ua>
2023-06-21 19:07:11 +0300

    Fixed coment

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,13 +8,20 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    private static final String DELIMITER = System.lineSeparator();
-    private static final String SEPARATOR = ",";
+    private static final String SEPARATOR = System.lineSeparator();
+    private static final String DELIMITER = ",";
     private static final String ROW_RESULT = "result";
     private static final String ROW_SUPPLY = "supply";
     private static final String ROW_BUY = "buy";
     private static final int VALUE_INDEX = 1;
     private static final int LABEL_INDEX = 0;
+
+    public String getStatistic(String fromFileName, String toFileName) {
+        String fromFileData = readFromFile(fromFileName);
+        String report = generateReport(fromFileData);
+        writeToFile(toFileName, report);
+        return fromFileData;
+    }
 
     private void writeToFile(String toFileName, String report) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
@@ -22,13 +29,6 @@ public class WorkWithFile {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-    }
-
-    public String getStatistic(String fromFileName, String toFileName) {
-        String fromFileData = readFromFile(fromFileName);
-        String report = generateReport(fromFileData);
-        writeToFile(toFileName, report);
-        return fromFileData;
     }
 
     private String readFromFile(String fromFileName) {
@@ -48,13 +48,13 @@ public class WorkWithFile {
 
     private String generateReport(String fromFileData) {
         StringBuilder report = new StringBuilder();
-        String[] data = fromFileData.split(DELIMITER);
+        String[] data = fromFileData.split(SEPARATOR);
         int supply = 0;
         int buy = 0;
         int result = 0;
 
         for (String line : data) {
-            String[] splitLine = line.split(SEPARATOR);
+            String[] splitLine = line.split(DELIMITER);
             switch (splitLine[LABEL_INDEX]) {
                 case ROW_SUPPLY:
                     supply += Integer.parseInt(splitLine[VALUE_INDEX]);
@@ -68,9 +68,9 @@ public class WorkWithFile {
         }
 
         result = supply - buy;
-        report.append(ROW_SUPPLY).append(SEPARATOR).append(supply).append(System.lineSeparator())
-                .append(ROW_BUY).append(SEPARATOR).append(buy).append(System.lineSeparator())
-                .append(ROW_RESULT).append(SEPARATOR).append(result).append(System.lineSeparator());
+        report.append(ROW_SUPPLY).append(DELIMITER).append(supply).append(SEPARATOR)
+                .append(ROW_BUY).append(DELIMITER).append(buy).append(SEPARATOR)
+                .append(ROW_RESULT).append(DELIMITER).append(result).append(SEPARATOR);
         return report.toString();
     }
 }
