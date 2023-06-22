9b55783ea43e7f60971805ccac751c6f68ad73d5
ANDREY <andrey.kiyas@gmail.com>
2023-06-22 21:07:48 +0300

    hw-jv-work-with-file-solution-andrey-kiyas (Anastasia Len + report)

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -12,8 +12,9 @@ import java.util.Arrays;
 import java.util.List;
 
 public class WorkWithFile {
-    static final int NAME_INDEX = 0;
-    static final int PARAM_INDEX = 1;
+    private final int NAME_INDEX = 0;
+    private final int PARAM_INDEX = 1;
+    private final int RESULT_INDEX = 2;
 
     public void getStatistic(String fromFileName, String toFileName) {
         writeWtoFile(createReport(readFromFile(fromFileName)), toFileName);
@@ -21,7 +22,6 @@ public class WorkWithFile {
 
     public List<String> readFromFile(String fromFileName) {
         List<String> fileLines;
-        ArrayList<String> columnList;
         try {
             fileLines = Files.readAllLines(Paths.get(fromFileName));
         } catch (IOException e) {
@@ -30,30 +30,28 @@ public class WorkWithFile {
         return fileLines;
     }
 
-    public Integer[] createReport(List<String> fileLines) {
-        int supplySum = 0;
-        int buySum = 0;
+    public int[] createReport(List<String> fileLines) {
+        int[] result = new int[3];
         for (String fileLine : fileLines) {
             String[] splitedText = fileLine.split(",");
             ArrayList<String> columnList = new ArrayList<>(Arrays.asList(splitedText));
             if (columnList.get(NAME_INDEX).equals("supply")) {
-                supplySum += Integer.parseInt(columnList.get(PARAM_INDEX));
+                result[0] += Integer.parseInt(columnList.get(PARAM_INDEX));
             } else {
-                buySum += Integer.parseInt(columnList.get(PARAM_INDEX));
+                result[1] += Integer.parseInt(columnList.get(PARAM_INDEX));
             }
         }
-        return new Integer[]{supplySum, buySum};
+        result[2] = result[0] - result[1];
+        return result;
     }
 
-    public void writeWtoFile(Integer[] integers, String toFileName) {
+    public void writeWtoFile(int[] report, String toFileName) {
         File file = new File(toFileName);
-        int supplySum = integers[NAME_INDEX];
-        int buySum = integers[PARAM_INDEX];
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file, true))) {
             Files.writeString(Path.of(toFileName), "");
-            bufferedWriter.write("supply," + supplySum);
-            bufferedWriter.write(System.lineSeparator() + "buy," + buySum);
-            bufferedWriter.write(System.lineSeparator() + "result," + (supplySum - buySum));
+            bufferedWriter.write("supply," + report[NAME_INDEX]);
+            bufferedWriter.write(System.lineSeparator() + "buy," + report[PARAM_INDEX]);
+            bufferedWriter.write(System.lineSeparator() + "result," + report[RESULT_INDEX]);
         } catch (IOException e) {
             throw new RuntimeException("Can't write data to file: " + file.getName(), e);
         }
