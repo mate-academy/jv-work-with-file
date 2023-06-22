96c32d68e10b0d1afb752bb051393a6ce4b78564
Denys-Maksym Kachalup <denys.k82@gmail.com>
2023-06-22 15:48:48 +0300

    fixed message in exceptions and added method who makes report

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -9,11 +9,18 @@ import java.io.IOException;
 
 public class WorkWithFile {
     private static final String SPLIT_REGEX = ",";
+    private static final int TOTAL_BUY_INDEX = 0;
+    private static final int TOTAL_SUPPLY_INDEX = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
+        String[] strings = readFile(fromFileName);
+        int[] totalBuyAndSupply = generateReport(strings);
+        writeFile(toFileName, totalBuyAndSupply);
+    }
+
+    private int[] generateReport(String[] strings) {
         int totalSupply = 0;
         int totalBuy = 0;
-        String[] strings = readFile(fromFileName);
 
         for (String string : strings) {
             String[] splitString = string.split(SPLIT_REGEX);
@@ -24,12 +31,14 @@ public class WorkWithFile {
                 totalBuy += Integer.parseInt(splitString[1]);
             }
         }
-        writeFile(toFileName, totalSupply, totalBuy);
+
+        return new int[]{totalBuy, totalSupply};
     }
 
     private String[] readFile(String fromFileName) {
         File fromFile = new File(fromFileName);
         StringBuilder stringBuilder = new StringBuilder();
+
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFile))) {
             String value = bufferedReader.readLine();
             while (value != null) {
@@ -37,13 +46,16 @@ public class WorkWithFile {
                 value = bufferedReader.readLine();
             }
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Cant read data from the file", e);
         }
 
         return stringBuilder.toString().split(System.lineSeparator());
     }
 
-    private void writeFile(String toFileName, int totalSupply, int totalBuy) {
+    private void writeFile(String toFileName, int[] totalBuyAndSupply) {
+        int totalSupply = totalBuyAndSupply[TOTAL_SUPPLY_INDEX];
+        int totalBuy = totalBuyAndSupply[TOTAL_BUY_INDEX];
+
         File toFile = new File(toFileName);
         int result = totalSupply - totalBuy;
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFile))) {
@@ -52,7 +64,7 @@ public class WorkWithFile {
                     + "buy," + totalBuy + System.lineSeparator()
                     + "result," + result);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Cant write data in the file", e);
         }
     }
 }
