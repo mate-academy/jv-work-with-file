e8b7675ccda4a8504792bf7625817e62ec8cdc40
Wasja Zejkan <vasazejkan5@gmail.com>
2023-06-26 21:48:56 +0300

    Fix errors that the mentor suggested: delete redundant constant INCREMENT_VALUE, change methods: getTemporaryResult(String) to readFile(String), writeToFile(String, int, int) to writeToFile(String, String), merge methods: getSupplyStatistic(String) and getBuyStatistic(String) to generateReport(String)

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,63 +8,55 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    public static final int INCREMENT_VALUE = 1;
     public static final String SUPPLY_NAME = "supply";
     public static final String BUY_NAME = "buy";
     public static final String RESULT_NAME = "result";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        String temporaryResult = getTemporaryResult(fromFileName);
-        int supplyStatistic = getSupplyStatistic(temporaryResult);
-        int buyStatistic = getBuyStatistic(temporaryResult);
-        writeToFile(toFileName, supplyStatistic, buyStatistic);
+        String fileContent = readFile(fromFileName);
+        String report = generateReport(fileContent);
+        writeToFile(toFileName, report);
     }
 
-    private String getTemporaryResult(String fromFile) {
-        File fileToRead = new File(fromFile);
-        StringBuilder temporaryResult = new StringBuilder();
+    private String readFile(String fromFileName) {
+        File fileToRead = new File(fromFileName);
+        StringBuilder fileContent = new StringBuilder();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileToRead))) {
             String stringLine = bufferedReader.readLine();
             while (stringLine != null) {
-                temporaryResult.append(stringLine).append(',');
+                fileContent.append(stringLine).append(',');
                 stringLine = bufferedReader.readLine();
             }
         } catch (IOException e) {
-            throw new RuntimeException("Can't read from file", e);
+            throw new RuntimeException("Can't read from file " + fromFileName, e);
         }
-        return temporaryResult.toString();
+        return fileContent.toString();
     }
 
-    private int getSupplyStatistic(String temporaryResult) {
-        String[] variablesWithValues = temporaryResult.split(",");
-        int sum = 0;
+    private String generateReport(String fileContent) {
+        String[] variablesWithValues = fileContent.split(",");
+        int sumOfSupply = 0;
+        int sumOfBuy = 0;
         for (int i = 0; i < variablesWithValues.length; i += 2) {
             if (variablesWithValues[i].equals(SUPPLY_NAME)) {
-                sum += Integer.parseInt(variablesWithValues[i + INCREMENT_VALUE]);
+                sumOfSupply += Integer.parseInt(variablesWithValues[i + 1]);
+            } else {
+                sumOfBuy += Integer.parseInt(variablesWithValues[i + 1]);
             }
         }
-        return sum;
+        StringBuilder report = new StringBuilder();
+        report.append(SUPPLY_NAME).append(',').append(sumOfSupply).append(System.lineSeparator())
+                .append(BUY_NAME).append(',').append(sumOfBuy).append(System.lineSeparator())
+                .append(RESULT_NAME).append(',').append(sumOfSupply - sumOfBuy);
+        return report.toString();
     }
 
-    private int getBuyStatistic(String temporaryResult) {
-        String[] variablesWithValues = temporaryResult.split(",");
-        int sum = 0;
-        for (int i = 0; i < variablesWithValues.length; i += 2) {
-            if (variablesWithValues[i].equals(BUY_NAME)) {
-                sum += Integer.parseInt(variablesWithValues[i + INCREMENT_VALUE]);
-            }
-        }
-        return sum;
-    }
-
-    private void writeToFile(String toFile, int supplyStatistic, int buyStatistic) {
-        File fileToWrite = new File(toFile);
+    private void writeToFile(String toFileName, String text) {
+        File fileToWrite = new File(toFileName);
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileToWrite))) {
-            bufferedWriter.write(SUPPLY_NAME + "," + supplyStatistic + System.lineSeparator());
-            bufferedWriter.write(BUY_NAME + "," + buyStatistic + System.lineSeparator());
-            bufferedWriter.write(RESULT_NAME + "," + (supplyStatistic - buyStatistic));
+            bufferedWriter.write(text);
         } catch (IOException e) {
-            throw new RuntimeException("Can't write to file", e);
+            throw new RuntimeException("Can't write to file " + toFileName, e);
         }
     }
 }
