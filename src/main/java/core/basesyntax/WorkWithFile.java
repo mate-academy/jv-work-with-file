b352e34bd49843b535edf94e7d6c209475b52fe9
Hlib Bykovskyi <minexgleb@gmail.com>
2023-06-26 20:00:38 +0300

    Refactor getStatistic code by splitting it in the different methods

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -11,41 +11,62 @@ public class WorkWithFile {
     private static final String SUPPLY = "supply";
     private static final String BUY = "buy";
     private static final String COMMA = ",";
+    private int supplyAmount;
+    private int buyAmount;
+    private int resultAmount;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        int supplyAmount = 0;
-        int buyAmount = 0;
-        String line;
+        String fileContent = readFile(fromFileName);
+        calculateStatistics(fileContent);
+        writeResult(supplyAmount, buyAmount, resultAmount, toFileName);
+    }
 
+    private String readFile(String fromFileName) {
+        String line;
+        StringBuilder resultBuilder = new StringBuilder();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             while ((line = bufferedReader.readLine()) != null) {
-                String actionType = line.split(COMMA)[0];
-                int amount = Integer.parseInt(line.split(COMMA)[1]);
-
-                if (actionType.equals(SUPPLY)) {
-                    supplyAmount += amount;
-                }
-
-                if (actionType.equals(BUY)) {
-                    buyAmount += amount;
-                }
+               resultBuilder.append(line).append(System.lineSeparator());
             }
         } catch (FileNotFoundException e) {
             throw new RuntimeException("Cannot find file", e);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Error reading a file", e);
         }
 
-        writeResult(supplyAmount, buyAmount, toFileName);
+        return resultBuilder.toString().trim();
     }
 
-    private void writeResult(int supplyAmount, int buyAmount, String toFileName) {
+    private void calculateStatistics(String unsortedStats) {
+        String[] splitedStats = unsortedStats.split("[,\\r?\\n]+");
+
+
+        supplyAmount = 0;
+        buyAmount = 0;
+        resultAmount = 0;
+
+        for (int i = 1; i < splitedStats.length; i += 2) {
+            String actionType = splitedStats[i - 1];
+            int amount = Integer.parseInt(splitedStats[i]);
+
+            if (actionType.equals(SUPPLY)) {
+                supplyAmount += amount;
+            }
+            if (actionType.equals(BUY)) {
+                buyAmount += amount;
+            }
+        }
+
+        resultAmount = supplyAmount - buyAmount;
+    }
+
+    private void writeResult(int supplyAmount, int buyAmount, int resultAmount, String toFileName) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
             bufferedWriter.write(SUPPLY + COMMA + supplyAmount + System.lineSeparator());
             bufferedWriter.write(BUY + COMMA + buyAmount + System.lineSeparator());
-            bufferedWriter.write("result," + (supplyAmount - buyAmount));
+            bufferedWriter.write("result," + resultAmount);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Error writing to file", e);
         }
     }
 }
