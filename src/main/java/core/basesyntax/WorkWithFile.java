d9ad0901b3517afc4e0d70e97cc94a2095253ee6
Hlib Bykovskyi <minexgleb@gmail.com>
2023-06-26 14:38:38 +0300

    Fix import order

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,11 +1,11 @@
 package core.basesyntax;
 
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.FileReader;
+import java.io.FileWriter;
 
 public class WorkWithFile {
     private static final String SUPPLY = "supply";
