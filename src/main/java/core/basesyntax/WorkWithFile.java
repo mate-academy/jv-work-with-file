67b99c7b546a69058bf33f26bafda6423e696537
Hlib Bykovskyi <minexgleb@gmail.com>
2023-06-26 14:31:43 +0300

    Implement getStatistics

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,48 @@
 package core.basesyntax;
 
-public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
+import java.io.*;
+import java.util.Arrays;
 
+public class WorkWithFile {
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+    private static final String COMMA = ",";
+
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        int supplyAmount = 0;
+        int buyAmount = 0;
+        String line;
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
+            while ((line = bufferedReader.readLine()) != null) {
+                String actionType = line.split(COMMA)[0];
+                int amount = Integer.parseInt(line.split(COMMA)[1]);
+
+                if (actionType.equals(SUPPLY)) {
+                    supplyAmount += amount;
+                }
+
+                if (actionType.equals(BUY)) {
+                    buyAmount += amount;
+                }
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("Cannot find file", e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        writeResult(supplyAmount, buyAmount, toFileName);
+    }
+
+    private void writeResult(int supplyAmount, int buyAmount, String toFileName) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+            bufferedWriter.write(SUPPLY + COMMA + supplyAmount + System.lineSeparator());
+            bufferedWriter.write(BUY + COMMA + buyAmount + System.lineSeparator());
+            bufferedWriter.write("result," + (supplyAmount - buyAmount));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
