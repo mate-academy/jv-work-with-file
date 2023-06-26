d028b0de52c7745b7c5d37f3c7d9ec74dca4fdd1
Bohdan Kaziamyr <notpadumink01@gmail.com>
2023-06-26 15:48:33 +0300

    resolved all comments

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,7 +2,6 @@ package core.basesyntax;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -10,14 +9,15 @@ import java.io.IOException;
 public class WorkWithFile {
     private static final int OPERATION_TYPE_NUMBER = 0;
     private static final int OPERATION_VALUE_NUMBER = 1;
+    private static final int SUPPLY_SUM_INDEX = 0;
+    private static final int BUY_SUM_INDEX = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        writeToFile(createReport(readFile(fromFileName)), toFileName);
+        writeToFile(writeReport(calculateSums(readFile(fromFileName))), toFileName);
     }
 
     private String[] readFile(String fileName) {
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             String value = reader.readLine();
             StringBuilder stringBuilder = new StringBuilder();
             while (value != null) {
@@ -25,31 +25,33 @@ public class WorkWithFile {
                 value = reader.readLine();
             }
             return stringBuilder.toString().split(System.lineSeparator());
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException("Can't find file!", e);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Can't find file with name [" + fileName + "]!", e);
         }
     }
 
-    private String createReport(String[] linesFromFile) {
+    private int[] calculateSums(String[] linesFromFile) {
         int supplySum = 0;
         int buySum = 0;
         for (String line : linesFromFile) {
-            String[] dividedLine = line.split(",");
-            String operationType = dividedLine[OPERATION_TYPE_NUMBER];
-            int operationValue = Integer.parseInt(dividedLine[OPERATION_VALUE_NUMBER]);
+            String[] splitedLine = line.split(",");
+            String operationType = splitedLine[OPERATION_TYPE_NUMBER];
+            int operationValue = Integer.parseInt(splitedLine[OPERATION_VALUE_NUMBER]);
             if (operationType.equals("supply")) {
                 supplySum += operationValue;
             } else if (operationType.equals("buy")) {
                 buySum += operationValue;
             }
         }
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("supply,").append(supplySum).append(System.lineSeparator())
-                .append("buy,").append(buySum).append(System.lineSeparator()).append("result,")
-                .append(supplySum - buySum).append(System.lineSeparator());
-        return stringBuilder.toString();
+        return new int[]{supplySum, buySum};
+    }
+
+    private String writeReport(int[] calculatedSums) {
+        int supplySum = calculatedSums[SUPPLY_SUM_INDEX];
+        int buySum = calculatedSums[BUY_SUM_INDEX];
+        return "supply," + supplySum + System.lineSeparator() + "buy," + buySum
+                + System.lineSeparator() + "result," + (supplySum - buySum)
+                + System.lineSeparator();
     }
 
     private void writeToFile(String message, String fileName) {
