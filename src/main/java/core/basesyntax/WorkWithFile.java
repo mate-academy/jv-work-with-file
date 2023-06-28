62a3dd4a1b1395fdb511ee07ad934ffaabb3c3e3
Leshchenko <411100253g@gmail.com>
2023-06-28 16:55:06 +0300

    implemented getStatistic method in WorkWithFile

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,42 @@
 package core.basesyntax;
 
-public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
+public class WorkWithFile {
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+    private static final String RESULT = "result";
+    private static final String CSV_SEPARATOR = ",";
+    private static final int KEY_IN_ARRAY = 0;
+    private static final int VALUE_IN_ARRAY = 1;
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            Integer supply = 0;
+            Integer buy = 0;
+            for (String fileLine : Files.readString(Path.of(fromFileName))
+                    .split(System.lineSeparator())) {
+                String[] fileLineSplitted = fileLine.split(CSV_SEPARATOR);
+                if (fileLineSplitted[KEY_IN_ARRAY].equals(SUPPLY)) {
+                    supply += Integer.parseInt(fileLineSplitted[VALUE_IN_ARRAY]);
+                } else if (fileLineSplitted[KEY_IN_ARRAY].equals(BUY)) {
+                    buy += Integer.parseInt(fileLineSplitted[VALUE_IN_ARRAY]);
+                }
+            }
+            stringBuilder.append(SUPPLY + CSV_SEPARATOR)
+                    .append(supply)
+                    .append(System.lineSeparator())
+                    .append(BUY + CSV_SEPARATOR)
+                    .append(buy)
+                    .append(System.lineSeparator())
+                    .append(RESULT + CSV_SEPARATOR)
+                    .append(supply - buy);
+            Files.write(Path.of(toFileName), stringBuilder.toString().getBytes());
+        } catch (IOException e) {
+            throw new RuntimeException("Can't correctly read data from file " + fromFileName, e);
+        }
     }
 }
