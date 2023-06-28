ab1281171c8708ee8b0fb1b91463de57e0df05c1
Jean-Mark Kimbembe <marselium@gmail.com>
2023-06-28 14:07:06 +0300

    fixed method readFromFile, moved extra-logic to composeReport mmethod

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -9,50 +9,54 @@ import java.nio.file.Path;
 public class WorkWithFile {
     private static final String[] REPORT_CONTENT = {"supply", "buy", "result"};
     private static final String COMMA_SEPARATOR = ",";
-    private static final String REGEX = "\\r?\\n|\\r";
     private static final int SUPPLY_INDEX = 0;
     private static final int BUY_INDEX = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        writeToFile(composeReport(readFromFile(fromFileName)), toFileName);
+        writeToFile(
+                composeReport(
+                readFromFile(fromFileName)), toFileName);
     }
 
     public void writeToFile(String data, String toFileName) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
             bufferedWriter.write(data);
         } catch (IOException e) {
-            throw new RuntimeException("Can't write data", e);
+            throw new RuntimeException("Can't write data to " + toFileName, e);
         }
     }
 
-    public int[] readFromFile(String fromFileName) {
-        int[] result = new int[2];
-        String[] tmp;
+    public String readFromFile(String fromFileName) {
+        String result;
         try {
-            tmp = Files.readString(Path.of(fromFileName))
-                    .replaceAll(REGEX, COMMA_SEPARATOR).split(COMMA_SEPARATOR);
+            result = Files.readString(Path.of(fromFileName))
+                    .replaceAll("\\r?\\n|\\r", COMMA_SEPARATOR);
         } catch (IOException e) {
-            throw new RuntimeException("Can't read file", e);
-        }
-        for (int i = 0; i < tmp.length; i++) {
-            switch (tmp[i]) {
-                case ("supply"):
-                    result[SUPPLY_INDEX]
-                            = result[SUPPLY_INDEX] + Integer.parseInt(tmp[i + 1]);
-                    break;
-                case ("buy"):
-                    result[BUY_INDEX]
-                            = result[BUY_INDEX] + Integer.parseInt(tmp[i + 1]);
-                    break;
-                default:
-            }
+            throw new RuntimeException("Can't read file " + fromFileName, e);
         }
         return result;
     }
 
-    public String composeReport(int[] data) {
-        return REPORT_CONTENT[0] + COMMA_SEPARATOR + data[SUPPLY_INDEX] + System.lineSeparator()
-                + REPORT_CONTENT[1] + COMMA_SEPARATOR + data[BUY_INDEX] + System.lineSeparator()
-                + REPORT_CONTENT[2] + COMMA_SEPARATOR + (data[SUPPLY_INDEX] - data[BUY_INDEX]);
+    public String composeReport(String data) {
+        String [] tmp = data.split(COMMA_SEPARATOR);
+        int [] report = new int[2];
+
+        for (int i = 0; i < tmp.length; i++) {
+            switch (tmp[i]) {
+                case ("supply"):
+                    report[SUPPLY_INDEX]
+                            = report[SUPPLY_INDEX] + Integer.parseInt(tmp[i + 1]);
+                    break;
+                case ("buy"):
+                    report[BUY_INDEX]
+                            = report[BUY_INDEX] + Integer.parseInt(tmp[i + 1]);
+                    break;
+                default:
+            }
+        }
+
+        return REPORT_CONTENT[0] + COMMA_SEPARATOR + report[SUPPLY_INDEX] + System.lineSeparator()
+                + REPORT_CONTENT[1] + COMMA_SEPARATOR + report[BUY_INDEX] + System.lineSeparator()
+                + REPORT_CONTENT[2] + COMMA_SEPARATOR + (report[SUPPLY_INDEX] - report[BUY_INDEX]);
     }
 }
