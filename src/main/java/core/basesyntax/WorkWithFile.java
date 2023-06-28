5abf8dbe3428e221390b9d9f6e72e3fd83ac020c
Bohdan Kaziamyr <notpadumink01@gmail.com>
2023-06-28 11:36:00 +0300

    resolved all new comments

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -11,9 +11,15 @@ public class WorkWithFile {
     private static final int OPERATION_VALUE_NUMBER = 1;
     private static final int SUPPLY_SUM_INDEX = 0;
     private static final int BUY_SUM_INDEX = 1;
+    private static final String SUPPLY_NAME = "supply,";
+    private static final String BUY_NAME = "buy,";
+    private static final String RESULT_NAME = "result,";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        writeToFile(writeReport(calculateSums(readFile(fromFileName))), toFileName);
+        String[] wordsFromFile = readFile(fromFileName);
+        int[] listOfTwoSums = calculateSums(wordsFromFile);
+        String completedReport = writeReport(listOfTwoSums);
+        writeToFile(completedReport, toFileName);
     }
 
     private String[] readFile(String fileName) {
@@ -21,10 +27,10 @@ public class WorkWithFile {
             String value = reader.readLine();
             StringBuilder stringBuilder = new StringBuilder();
             while (value != null) {
-                stringBuilder.append(value).append(System.lineSeparator());
+                stringBuilder.append(value).append('.');
                 value = reader.readLine();
             }
-            return stringBuilder.toString().split(System.lineSeparator());
+            return stringBuilder.toString().split("\\.");
         } catch (IOException e) {
             throw new RuntimeException("Can't find file with name [" + fileName + "]!", e);
         }
@@ -49,8 +55,8 @@ public class WorkWithFile {
     private String writeReport(int[] calculatedSums) {
         int supplySum = calculatedSums[SUPPLY_SUM_INDEX];
         int buySum = calculatedSums[BUY_SUM_INDEX];
-        return "supply," + supplySum + System.lineSeparator() + "buy," + buySum
-                + System.lineSeparator() + "result," + (supplySum - buySum)
+        return SUPPLY_NAME + supplySum + System.lineSeparator() + BUY_NAME + buySum
+                + System.lineSeparator() + RESULT_NAME + (supplySum - buySum)
                 + System.lineSeparator();
     }
 
@@ -58,7 +64,7 @@ public class WorkWithFile {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName))) {
             bufferedWriter.write(message);
         } catch (IOException e) {
-            throw new RuntimeException("Can't write in file!", e);
+            throw new RuntimeException("Can't write in file with name [" + fileName + "]!", e);
         }
     }
 }
