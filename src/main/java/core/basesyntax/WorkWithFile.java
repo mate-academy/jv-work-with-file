208c63de6ff3a81df1e3812d6bf325d14ee544a4
Leshchenko <411100253g@gmail.com>
2023-06-30 11:56:01 +0300

    divided excercise into three function: read, generateReport, write

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -13,31 +13,33 @@ public class WorkWithFile {
     private static final int VALUE_IN_ARRAY = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        String stringReportFromFile = getReportFromFile(fromFileName);
-        try {
-            Files.write(Path.of(toFileName), stringReportFromFile.getBytes());
-        } catch (IOException e) {
-            throw new RuntimeException("Can't write data to file: " + toFileName);
-        }
+        String rawData = readDataFromFile(fromFileName);
+        String reportData = generateReport(rawData);
+        writeDataToFile(reportData, toFileName);
     }
 
-    private String getReportFromFile(String fromFileName) {
-        StringBuilder stringBuilder = new StringBuilder();
-        Integer supply = 0;
-        Integer buy = 0;
+    private String readDataFromFile(String fromFileName) {
         try {
-            for (String fileLine : Files.readString(Path.of(fromFileName))
-                    .split(System.lineSeparator())) {
-                String[] fileLineSplitted = fileLine.split(CSV_SEPARATOR);
-                if (fileLineSplitted[KEY_IN_ARRAY].equals(SUPPLY)) {
-                    supply += Integer.parseInt(fileLineSplitted[VALUE_IN_ARRAY]);
-                } else if (fileLineSplitted[KEY_IN_ARRAY].equals(BUY)) {
-                    buy += Integer.parseInt(fileLineSplitted[VALUE_IN_ARRAY]);
-                }
-            }
+            return Files.readString(Path.of(fromFileName));
         } catch (IOException e) {
             throw new RuntimeException("Can't read data from file: " + fromFileName);
         }
+    }
+
+    private String generateReport(String data) {
+        StringBuilder stringBuilder = new StringBuilder();
+        Integer supply = 0;
+        Integer buy = 0;
+
+        for (String fileLine : data.split(System.lineSeparator())) {
+            String[] fileLineSplitted = fileLine.split(CSV_SEPARATOR);
+            if (fileLineSplitted[KEY_IN_ARRAY].equals(SUPPLY)) {
+                supply += Integer.parseInt(fileLineSplitted[VALUE_IN_ARRAY]);
+            } else if (fileLineSplitted[KEY_IN_ARRAY].equals(BUY)) {
+                buy += Integer.parseInt(fileLineSplitted[VALUE_IN_ARRAY]);
+            }
+        }
+
         return stringBuilder.append(SUPPLY + CSV_SEPARATOR)
                 .append(supply)
                 .append(System.lineSeparator())
@@ -47,5 +49,14 @@ public class WorkWithFile {
                 .append(RESULT + CSV_SEPARATOR)
                 .append(supply - buy)
                 .toString();
+
+    }
+
+    private void writeDataToFile(String data, String toFileName) {
+        try {
+            Files.write(Path.of(toFileName), data.getBytes());
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write data to file: " + toFileName);
+        }
     }
 }
