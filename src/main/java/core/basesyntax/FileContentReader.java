1e513e48660fa160697d896b2f30ec3e7a2d09d5
barbequeue <p99kab00@gmail.com>
2023-07-02 19:53:36 +0300

    Add line separator before adding content line while reading from a file

--- src/main/java/core/basesyntax/FileContentReader.java
+++ src/main/java/core/basesyntax/FileContentReader.java
@@ -21,21 +21,13 @@ public class FileContentReader {
             String contentLine = bufferedReader.readLine();
 
             while (contentLine != null) {
-                fileContentBuilder.append(contentLine).append(DEFAULT_SYSTEM_LINE_SEPARATOR);
+                fileContentBuilder.append(DEFAULT_SYSTEM_LINE_SEPARATOR).append(contentLine);
                 contentLine = bufferedReader.readLine();
             }
         } catch (FileNotFoundException e) {
             throw new RuntimeException("Can't find the file to read: " + filePath, e);
         } catch (IOException e) {
             throw new RuntimeException("Can't read from the file: " + filePath, e);
-        }
-
-        // To remove rightmost DEFAULT_SYSTEM_LINE_SEPARATOR
-        // applied by the last iteration of reading the file content
-        if (fileContentBuilder.length() > 0) {
-            fileContentBuilder.setLength(
-                    fileContentBuilder.length() - DEFAULT_SYSTEM_LINE_SEPARATOR.length()
-            );
         }
 
         return fileContentBuilder.toString();
