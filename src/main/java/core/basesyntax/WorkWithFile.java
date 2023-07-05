b35823435c47b6ef2bc9636076e12730387e90af
Oleg Stashkiv <stashkivolegg@gmail.com>
2023-07-05 13:32:59 +0300

    correction of comments. Add new method to organize the report and replacing some variables with constants. Separation of the logic of reading from a file and writing to a file

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,25 +2,24 @@ package core.basesyntax;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
     private static final String SEPARATOR = System.lineSeparator();
+    private static final String SEPARATION_SYMBOL = ",";
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+    private static final String RESULT = "result";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        File fileToRead = new File(fromFileName);
-        File fileToWrite = new File(toFileName);
-        try (BufferedReader reader = new BufferedReader(new FileReader(fileToRead));
-                 BufferedWriter writer = new BufferedWriter(new FileWriter(fileToWrite))) {
-            int countSupply = 0;
-            int countBuy = 0;
-            int result = 0;
+        int countSupply = 0;
+        int countBuy = 0;
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
             String line = reader.readLine();
             while (line != null) {
-                String[] values = line.split(",");
+                String[] values = line.split(SEPARATION_SYMBOL);
                 if (values[0].equals("supply")) {
                     countSupply += Integer.parseInt(values[1]);
                 } else {
@@ -28,19 +27,31 @@ public class WorkWithFile {
                 }
                 line = reader.readLine();
             }
-            result = countSupply - countBuy;
-            StringBuilder report = new StringBuilder();
-            report.append("supply,")
-                    .append(countSupply)
-                    .append(SEPARATOR)
-                    .append("buy,")
-                    .append(countBuy)
-                    .append(SEPARATOR)
-                    .append("result,")
-                    .append(result);
-            writer.write(report.toString());
         } catch (IOException e) {
             throw new RuntimeException("The file was not read", e);
         }
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
+            writer.write(getReport(countSupply, countBuy));
+        } catch (IOException e) {
+            throw new RuntimeException("Data was not written to the file",e);
+        }
+
+    }
+
+    private String getReport(int countSupply, int countBuy) {
+        int result = countSupply - countBuy;
+        StringBuilder report = new StringBuilder();
+        report.append(SUPPLY)
+                .append(SEPARATION_SYMBOL)
+                .append(countSupply)
+                .append(SEPARATOR)
+                .append(BUY)
+                .append(SEPARATION_SYMBOL)
+                .append(countBuy)
+                .append(SEPARATOR)
+                .append(RESULT)
+                .append(SEPARATION_SYMBOL)
+                .append(result);
+        return report.toString();
     }
 }
