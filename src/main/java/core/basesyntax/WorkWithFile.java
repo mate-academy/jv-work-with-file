4e097c0dbf2b0dc195a55438902614148df30a9a
satori1111 <romakuchmii@gmail.com>
2023-07-05 11:53:53 +0300

    added method createReport, fixed variables names

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -15,10 +15,56 @@ public class WorkWithFile {
     public static final String SUPPLY_OPERATION = "supply";
     public static final String BUY_OPERATION = "buy";
     public static final String RESULT_OF_OPERATIONS = "result";
+    public static final String SPACE_SEPARATOR = " ";
+    public static final int POSITION_OF_SUPPLY = 0;
+    public static final int POSITION_OF_BUY = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
         String dataFromFile = readFromFile(fromFileName);
         String[] data = dataFromFile.split(LINE_SEPARATOR);
+        String report = createReport(data);
+        writeToFile(toFileName, report);
+    }
+
+    private String readFromFile(String fileName) {
+        String str;
+        StringBuilder stringBuilder = new StringBuilder();
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            while ((str = bufferedReader.readLine()) != null) {
+                stringBuilder.append(str).append("\n");
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("Can't find file " + fileName, e);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read data from the file " + fileName, e);
+        }
+        return stringBuilder.toString();
+    }
+
+    private void writeToFile(String fileName,String report) {
+        int supply = Integer.parseInt(
+                report.split(SPACE_SEPARATOR)[POSITION_OF_SUPPLY]);
+        int buy = Integer.parseInt(
+                report.split(SPACE_SEPARATOR)[POSITION_OF_BUY]);
+        int result = supply - buy;
+        StringBuilder operations = new StringBuilder();
+        operations.append(SUPPLY_OPERATION + COMA_SEPARATOR)
+                .append(supply).append(System.lineSeparator());
+        operations.append(BUY_OPERATION + COMA_SEPARATOR)
+                .append(buy).append(System.lineSeparator());
+        operations.append(RESULT_OF_OPERATIONS + COMA_SEPARATOR)
+                .append(result);
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName))) {
+            bufferedWriter.write(operations.toString());
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("Can't find file " + fileName, e);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write data to the file " + fileName, e);
+        }
+    }
+
+    private String createReport(String[] data) {
         int supplySum = 0;
         int buySum = 0;
         for (String dataLine : data) {
@@ -31,35 +77,6 @@ public class WorkWithFile {
                 buySum += amountOfOperation;
             }
         }
-        writeToFile(toFileName, supplySum, buySum);
-    }
-
-    private String readFromFile(String fileName) {
-        String str;
-        StringBuilder stringBuilder = new StringBuilder();
-
-        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
-            while ((str = br.readLine()) != null) {
-                stringBuilder.append(str).append("\n");
-            }
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException("Can't find file " + fileName, e);
-        } catch (IOException e) {
-            throw new RuntimeException("Can't read data from the file " + fileName, e);
-        }
-        return stringBuilder.toString();
-    }
-
-    private void writeToFile(String fileName, int supply, int buy) {
-        int result = supply - buy;
-        try (BufferedWriter br = new BufferedWriter(new FileWriter(fileName))) {
-            br.write(SUPPLY_OPERATION + COMA_SEPARATOR + supply + System.lineSeparator());
-            br.write(BUY_OPERATION + COMA_SEPARATOR + buy + System.lineSeparator());
-            br.write(RESULT_OF_OPERATIONS + COMA_SEPARATOR + result);
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException("Can't find file " + fileName, e);
-        } catch (IOException e) {
-            throw new RuntimeException("Can't write data to the file " + fileName, e);
-        }
+        return supplySum + " " + buySum;
     }
 }
