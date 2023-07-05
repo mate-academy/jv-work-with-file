8f040e0a67fac161a6d99db892d437eef58a81ad
Oleg Stashkiv <stashkivolegg@gmail.com>
2023-07-05 17:39:42 +0300

    correction of all comments. added methods for each operation

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -9,43 +9,70 @@ import java.io.IOException;
 public class WorkWithFile {
     private static final String SEPARATOR = System.lineSeparator();
     private static final String SEPARATION_SYMBOL = ",";
-    private static final String SUPPLY = "supply";
-    private static final String BUY = "buy";
+    private static final String SPACE_SIGN = " ";
+    private static final String OPERATION_TYPE_SUPPLY = "supply";
+    private static final String OPERATION_TYPE_BUY = "buy";
     private static final String RESULT = "result";
+    private static final int INDEX_OPERATION = 0;
+    private static final int INDEX_NUMBER = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        int countSupply = 0;
-        int countBuy = 0;
-        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
+        String textFile = readFromFile(fromFileName);
+        String report = parseDataByCategory(textFile);
+        writeToFile(report, toFileName);
+    }
+
+    private String readFromFile(String fileName) {
+        StringBuilder fileText = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             String line = reader.readLine();
             while (line != null) {
-                String[] values = line.split(SEPARATION_SYMBOL);
-                if (values[0].equals("supply")) {
-                    countSupply += Integer.parseInt(values[1]);
-                } else {
-                    countBuy += Integer.parseInt(values[1]);
-                }
+                fileText.append(line).append(SPACE_SIGN);
                 line = reader.readLine();
             }
+            System.out.println(fileText);
+            return fileText.toString();
         } catch (IOException e) {
-            throw new RuntimeException("The file was not read", e);
+            throw new RuntimeException("The file was not read: " + fileName, e);
         }
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
-            writer.write(getReport(countSupply, countBuy));
-        } catch (IOException e) {
-            throw new RuntimeException("Data was not written to the file",e);
-        }
+    }
 
+    private String parseDataByCategory(String data) {
+        int countSupply = calculateSumByCategory(data, OPERATION_TYPE_SUPPLY);
+        int countBuy = calculateSumByCategory(data, OPERATION_TYPE_BUY);
+        return getReport(countSupply, countBuy);
+    }
+
+    private int calculateSumByCategory(String data, String categoryToCalculate) {
+        int sum = 0;
+        String[] generalSplitDataArray = data.split(SPACE_SIGN);
+        for (int i = 0; i < generalSplitDataArray.length; i++) {
+            String[] derivedSplitDataArray = generalSplitDataArray[i].split(SEPARATION_SYMBOL);
+            String category = derivedSplitDataArray[INDEX_OPERATION];
+            if (category.equals(categoryToCalculate)) {
+                int price = Integer.parseInt(derivedSplitDataArray[INDEX_NUMBER]);
+                sum += price;
+            }
+        }
+        return sum;
+    }
+
+    private void writeToFile(String report, String fileName) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+            writer.write(report);
+        } catch (IOException e) {
+            throw new RuntimeException("Data was not written to the file: " + fileName,e);
+        }
     }
 
     private String getReport(int countSupply, int countBuy) {
         int result = countSupply - countBuy;
         StringBuilder report = new StringBuilder();
-        report.append(SUPPLY)
+        report.append(OPERATION_TYPE_SUPPLY)
                 .append(SEPARATION_SYMBOL)
                 .append(countSupply)
                 .append(SEPARATOR)
-                .append(BUY)
+                .append(OPERATION_TYPE_BUY)
                 .append(SEPARATION_SYMBOL)
                 .append(countBuy)
                 .append(SEPARATOR)
