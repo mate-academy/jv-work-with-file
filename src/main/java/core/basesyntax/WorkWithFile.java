841da043e1f66394055c1bac68f21200fb4fe05d
Iryna Mekh <iryna.mekh@gmail.com>
2023-07-05 13:00:56 +0300

    fixed mistakes after second review

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,46 +7,53 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    private static final String COMA_SEPARATOR = ",";
+    private static final int VALUE_INDEX = 1;
+    private static final int OPERATION_TYPE_INDEX = 0;
+
     protected void getStatistic(String fromFileName, String toFileName) {
         String[] fileInfo = this.readFromFile(fromFileName);
         int supplySum = 0;
         int buySum = 0;
         for (String oneLine : fileInfo) {
-            String separator = ",";
-            String[] stringInfo = oneLine.split(separator);
-            if (stringInfo[0].equals("supply")) {
-                supplySum += Integer.parseInt(stringInfo[1]);
+            String[] stringInfo = oneLine.split(COMA_SEPARATOR);
+            if (stringInfo[OPERATION_TYPE_INDEX].equals("supply")) {
+                supplySum += Integer.parseInt(stringInfo[VALUE_INDEX]);
             } else {
-                buySum += Integer.parseInt(stringInfo[1]);
+                buySum += Integer.parseInt(stringInfo[VALUE_INDEX]);
             }
         }
-        this.writeToFile(toFileName, supplySum, buySum);
+        String toFileInfo = createReport(supplySum, buySum);
+        this.writeToFile(toFileName, toFileInfo);
     }
 
     private String[] readFromFile(String fromFileName) {
         StringBuilder stringBuilder = new StringBuilder();
-        File fromFile = new File(fromFileName);
-        try (BufferedReader bufferedReader = new BufferedReader(new java.io.FileReader(fromFile))) {
+        try (BufferedReader bufferedReader = new BufferedReader(
+                new java.io.FileReader(fromFileName))) {
             String fromText = bufferedReader.readLine();
             while (fromText != null) {
                 stringBuilder.append(fromText).append(System.lineSeparator());
                 fromText = bufferedReader.readLine();
             }
         } catch (IOException e) {
-            throw new RuntimeException("Can't read file fromFileName", e);
+            throw new RuntimeException("Can't read file " + fromFileName, e);
         }
         return stringBuilder.toString().split(System.lineSeparator());
     }
 
-    private void writeToFile(String toFileName, int supplySum, int buySum) {
-        String toFileInfo = "supply," + supplySum + System.lineSeparator() + "buy,"
+    private String createReport(int supplySum, int buySum) {
+        return "supply," + supplySum + System.lineSeparator() + "buy,"
                 + buySum + System.lineSeparator()
                 + "result," + (supplySum - buySum) + System.lineSeparator();
+    }
+
+    private void writeToFile(String toFileName, String toFileInfo) {
         File toFile = new File(toFileName);
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFile))) {
             bufferedWriter.write(toFileInfo);
         } catch (IOException e) {
-            throw new RuntimeException("Can't write data to file toFileName", e);
+            throw new RuntimeException("Can't write data to file " + toFileName, e);
         }
     }
 }
