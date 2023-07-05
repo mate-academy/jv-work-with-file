24b6c2d3edd3e7e9bc0d8187079a2be98a5b5daf
Vitalii <mkukoba11@ukr.net>
2023-07-05 18:50:24 +0300

    added two methods and shortened the getStatistic method, renamed variables

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,68 +7,57 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    private static final int ROOF_OF_NAMES = 2;
+    private static final int INDEX_OF_NAMES = 0;
     private static final int INDEX_OF_VALUES = 1;
-    private static final int INDEX_OF_SUPPLY_VALUE = 7;
-    private static final int INDEX_OF_BUY_VALUE = 4;
+    private static final String COMMA_SEPARATOR = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        StringBuilder statisticBuilder = new StringBuilder();
         String textFromFile = readFromFile(fromFileName);
-        String[] dataArray = textFromFile.split(System.lineSeparator());
-        int totalValue;
-        for (int i = 0; i < ROOF_OF_NAMES; i++) {
-            if (i == 0) {
-                statisticBuilder.append("supply,");
-            } else {
-                statisticBuilder.append("buy,");
-            }
-            totalValue = 0;
-            for (int j = 0; j < dataArray.length; j++) {
-                String[] contentOfData = dataArray[j].split(",");
-                String name = contentOfData[0];
-                int valueOfData = Integer.parseInt(contentOfData[INDEX_OF_VALUES]);
-                if (i == 0 && name.equals("supply")) {
-                    totalValue += valueOfData;
-                }
-                if (i == 1 && name.equals("buy")) {
-                    totalValue += valueOfData;
-                }
-            }
-            statisticBuilder.append(totalValue).append(System.lineSeparator());
-        }
-        String completeStr = statisticBuilder.toString();
-        String[] completeData = completeStr.split(System.lineSeparator());
-        String valueOfSupply = completeData[0].substring(INDEX_OF_SUPPLY_VALUE);
-        String valueOfBuy = completeData[1].substring(INDEX_OF_BUY_VALUE);
-        try {
-            int resultValue = Integer.parseInt(valueOfSupply) - Integer.parseInt(valueOfBuy);
-            completeStr += "result," + resultValue;
-        } catch (NumberFormatException e) {
-            throw new RuntimeException("Incorrect integer format");
-        }
-        saveToFile(toFileName, completeStr);
+        int[] values = countValues(textFromFile);
+        String report = createReport(values);
+        saveToFile(toFileName, report);
     }
 
     public String readFromFile(String fileName) {
-        StringBuilder builder = new StringBuilder();
+        StringBuilder data = new StringBuilder();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
             String str = bufferedReader.readLine();
             while (str != null) {
-                builder.append(str).append(System.lineSeparator());
+                data.append(str).append(System.lineSeparator());
                 str = bufferedReader.readLine();
             }
         } catch (IOException e) {
             throw new RuntimeException("Can't read this file - " + fileName);
         }
-        return builder.toString();
+        return data.toString();
     }
 
-    public void saveToFile(String toFile, String data) {
+    public int[] countValues(String file) {
+        String[] linesArray = file.split(System.lineSeparator());
+        int totalSupplyValue = 0;
+        int totalBuyValue = 0;
+        for (String line : linesArray) {
+            String[] data = line.split(COMMA_SEPARATOR);
+            if (data[INDEX_OF_NAMES].equals("supply")) {
+                totalSupplyValue += Integer.parseInt(data[INDEX_OF_VALUES]);
+            } else {
+                totalBuyValue += Integer.parseInt(data[INDEX_OF_VALUES]);
+            }
+        }
+        return new int[]{totalSupplyValue, totalBuyValue};
+    }
+
+    public String createReport(int[] data) {
+        return "supply," + data[0] + System.lineSeparator()
+                + "buy," + data[1] + System.lineSeparator()
+                + "result," + (data[0] - data[1]);
+    }
+
+    public void saveToFile(String toFile, String report) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFile))) {
-            bufferedWriter.write(data);
+            bufferedWriter.write(report);
         } catch (IOException e) {
-            throw new RuntimeException("Can't save data to file - " + toFile);
+            throw new RuntimeException("Can't save report to file - " + toFile);
         }
     }
 }
