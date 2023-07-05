dda0837cdafa26a264510b9654163051730677e5
Vladyslav Keda <dakadofordisluv@gmail.com>
2023-07-05 20:15:39 +0300

    Completed task: - changed 'getStatistic' method - added 'readFile', 'calculateStatistic', 'writeReport', 'writeInFile' methods - added SUPPLY_TEXT, BUY_TEXT, RESULT_TEXT constants

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,60 @@
 package core.basesyntax;
 
-public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
+public class WorkWithFile {
+    private static String SUPPLY_TEXT = "supply";
+    private static String BUY_TEXT = "buy";
+    private static String RESULT_TEXT = "result";
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        String[] data = readFile(fromFileName);
+        String statistic = calculateStatistic(data);
+        writeInFile(toFileName, statistic);
+    }
+
+    private String[] readFile(String fileName) {
+        try {
+            return Files.readAllLines(Path.of(fileName)).toArray(String[]::new);
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t read file - " + fileName
+                    + System.lineSeparator() + e);
+        }
+    }
+
+    private String calculateStatistic(String... data) {
+        int supply = 0;
+        int buy = 0;
+        for (String line : data) {
+            if (line.matches("(" + SUPPLY_TEXT + "|" + BUY_TEXT + "),\\d+")) {
+                String[] arguments = line.split(",");
+                if (arguments[0].equals(SUPPLY_TEXT)) {
+                    supply += Integer.parseInt(arguments[1]);
+                } else {
+                    buy += Integer.parseInt(arguments[1]);
+                }
+            }
+        }
+        return writeReport(supply, buy);
+    }
+
+    private String writeReport(int supply, int buy) {
+        String report = new StringBuilder()
+                .append(SUPPLY_TEXT).append(",").append(supply).append(System.lineSeparator())
+                .append(BUY_TEXT).append(",").append(buy).append(System.lineSeparator())
+                .append(RESULT_TEXT).append(",").append(supply - buy)
+                .toString();
+        return report;
+    }
+
+    private void writeInFile(String fileName, String statistic) {
+        try {
+            Files.write(Path.of(fileName), statistic.getBytes());
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t write data in file - "
+                    + fileName + System.lineSeparator() + e);
+        }
     }
 }
