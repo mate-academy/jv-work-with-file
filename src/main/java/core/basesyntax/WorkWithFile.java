95fa88e706d517477eb9b7414494870a01c44185
Oleg Turyk <oleh.turyk.ki.2019@lpnu.ua>
2023-07-06 22:57:08 +0300

    implemented getStatistic method

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,37 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
+                BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
+            int supplyTotal = 0;
+            int buyTotal = 0;
+            String line;
 
+            while ((line = reader.readLine()) != null) {
+                String[] fields = line.split(",");
+                String operationType = fields[0].trim();
+                int amount = Integer.parseInt(fields[1].trim());
+                if (operationType.equals("supply")) {
+                    supplyTotal += amount;
+                } else if (operationType.equals("buy")) {
+                    buyTotal += amount;
+                }
+            }
+            int result = supplyTotal - buyTotal;
+            writer.write("supply," + supplyTotal);
+            writer.newLine();
+            writer.write("buy," + buyTotal);
+            writer.newLine();
+            writer.write("result," + result);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't open the file ", e);
+        }
     }
 }
