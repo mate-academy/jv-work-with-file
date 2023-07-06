bb301355a650664c80ee894486b576cb227b6ee9
Vladyslav Ihnatiuk <123470878+Someboty@users.noreply.github.com>
2023-07-06 12:19:56 +0300

    removed "static" from writeFile, fillString and readFile methods. Made fillString and readFile methods not void. Rewrite fillString method to make more simple

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,51 +7,47 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class WorkWithFile {
-    private static final String SUPPLY_TEXT = "supply,";
-    private static final String BUY_TEXT = "buy,";
+    private static final String SUPPLY_TEXT = "supply";
+    private static final String BUY_TEXT = "buy";
     private static final String RESULT_TEXT = "result,";
     private static final String COMA_SEPARATOR = ",";
     private static final String SPACE_SEPARATOR = " ";
 
     public void getStatistic(String fromFileName, String toFileName) {
         StringBuilder fileStringBuilder = new StringBuilder();
-        readFile(fromFileName, fileStringBuilder);
+        fileStringBuilder = readFile(fromFileName, fileStringBuilder);
         String[] linesArray = fileStringBuilder.toString().split(SPACE_SEPARATOR);
-        StringBuilder resultString = new StringBuilder();
-        resultString.append(SUPPLY_TEXT).append(0).append(System.lineSeparator())
-                .append(BUY_TEXT).append(0).append(System.lineSeparator());
-        fillString(linesArray, resultString);
+        String resultString = fillString(linesArray);
         writeFile(toFileName, resultString);
     }
 
-    private static void writeFile(String toFileName, StringBuilder result) {
+    private void writeFile(String toFileName, String result) {
         try (BufferedWriter writer = Files.newBufferedWriter(Path.of(toFileName))) {
-            writer.write(result.toString());
+            writer.write(result);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private static void fillString(String[] linesArray, StringBuilder result) {
+    private String fillString(String[] linesArray) {
+        int supply = 0;
+        int buy = 0;
         for (String line : linesArray) {
             String[] soloLineArray = line.split(COMA_SEPARATOR);
-            int startIndex = result.indexOf(soloLineArray[0]) + soloLineArray[0].length() + 1;
-            int endIndex = result.indexOf(System.lineSeparator(), startIndex);
-            result.replace(
-                    startIndex,
-                    endIndex,
-                    String.valueOf(
-                            Integer.parseInt(result.substring(startIndex, endIndex))
-                    + Integer.parseInt(soloLineArray[1])));
+            if (soloLineArray[0].equals(SUPPLY_TEXT)) {
+                supply += Integer.parseInt(soloLineArray[1]);
+            } else {
+                buy += Integer.parseInt(soloLineArray[1]);
+            }
         }
-        int supplyCount = Integer.parseInt(result.substring(result.indexOf(SUPPLY_TEXT) + 7,
-                        result.indexOf(System.lineSeparator(), result.indexOf(SUPPLY_TEXT))));
-        int buyCount = Integer.parseInt(result.substring(result.indexOf(BUY_TEXT) + 4,
-                        result.indexOf(System.lineSeparator(), result.indexOf(BUY_TEXT))));
-        result.append(RESULT_TEXT).append(supplyCount - buyCount);
+        return new StringBuilder().append(SUPPLY_TEXT).append(COMA_SEPARATOR)
+                .append(supply).append(System.lineSeparator())
+                .append(BUY_TEXT).append(COMA_SEPARATOR)
+                .append(buy).append(System.lineSeparator())
+                .append(RESULT_TEXT).append(supply - buy).toString();
     }
 
-    private static void readFile(String fromFileName, StringBuilder fileStringBuilder) {
+    private StringBuilder readFile(String fromFileName, StringBuilder fileStringBuilder) {
         try (BufferedReader reader = Files.newBufferedReader(Path.of(fromFileName))) {
             String line = reader.readLine();
             while (line != null) {
@@ -61,5 +57,6 @@ public class WorkWithFile {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+        return fileStringBuilder;
     }
 }
