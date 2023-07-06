349394bf17f878f8132ead2e6eb1f0e51fdfe6c9
Your Name <a.o.vozniuk@gmail.com>
2023-07-06 22:58:16 +0300

    updates after review

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -9,45 +9,55 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class WorkWithFile {
+    private static final int SUPPLY_INDEX = 0;
+    private static final int BUY_INDEX = 1;
+    private static final int RESULT_INDEX = 2;
+    private static final int RECORD_NAME_INDEX = 0;
+    private static final int RECORD_VALUE_INDEX = 1;
 
-    public void getStatistic(String fromFileName, String toFileName) {
+    public static void getStatistic(String fromFileName, String toFileName) {
+        deleteResultFileIfExist(toFileName);
+        List<String> records = readCsvToList(fromFileName);
+        String[] report = generateReport(records);
+        writeToFile(report, new File(toFileName));
+    }
+
+    private static String[] generateReport(List<String> records) {
+        int[] totalSupplyBuyResult = new int[3];
+        calculateSupplyAndBuy(totalSupplyBuyResult, records);
+        totalSupplyBuyResult[2] = totalSupplyBuyResult[SUPPLY_INDEX] - totalSupplyBuyResult[BUY_INDEX];
+        String[] resultData = new String[3];
+        resultData[0] = "supply," + totalSupplyBuyResult[SUPPLY_INDEX];
+        resultData[1] = "buy," + totalSupplyBuyResult[BUY_INDEX];
+        resultData[2] = "result," + totalSupplyBuyResult[RESULT_INDEX];
+        return resultData;
+    }
+
+    private static void deleteResultFileIfExist(String toFileName) {
         try {
             Files.deleteIfExists(Path.of(toFileName));
         } catch (IOException e) {
             throw new RuntimeException("Can not delete if exist file " + e);
         }
-        List<List<String>> records = readCsvToList(fromFileName);
-        int[] totalSupplyBuyResult = new int[3];// index 0 - supply, 1 - buy, 2 - result
-        calculateSupplyAndBuy(totalSupplyBuyResult, records);
-        totalSupplyBuyResult[2] = totalSupplyBuyResult[0] - totalSupplyBuyResult[1];
-        String[] resultData = new String[3];
-        resultData[0] = "supply," + totalSupplyBuyResult[0];
-        resultData[1] = "buy," + totalSupplyBuyResult[1];
-        resultData[2] = "result," + totalSupplyBuyResult[2];
-        writeToFile(resultData, new File(toFileName));
-        System.out.println();
-
     }
 
-    private void calculateSupplyAndBuy(int[] resultArray, List<List<String>> records) {
+    private static void calculateSupplyAndBuy(int[] resultArray, List<String> records) {
         for (int i = 0; i < records.size(); i++) {
-            List<String> record = records.get(i);
-            if (record.get(0).equals("supply")) {
-                resultArray[0] += Integer.parseInt(record.get(1));
-            } else if (record.get(0).equals("buy")) {
-                resultArray[1] += Integer.parseInt(record.get(1));
+            String[] record = records.get(i).split(",");
+            if (record[RECORD_NAME_INDEX].equals("supply")) {
+                resultArray[0] += Integer.parseInt(record[RECORD_VALUE_INDEX]);
+            } else if (record[RECORD_NAME_INDEX].equals("buy")) {
+                resultArray[1] += Integer.parseInt(record[RECORD_VALUE_INDEX]);
             } else {
-                throw new RuntimeException("Invalid record type " + record.get(0));
+                throw new RuntimeException("Invalid record type " + records.get(0));
             }
         }
     }
 
-    private void writeToFile(String[] result, File file) {
+    private static void writeToFile(String[] result, File file) {
         try {
             file.createNewFile();
         } catch (IOException e) {
@@ -64,18 +74,12 @@ public class WorkWithFile {
         }
     }
 
-    private List<List<String>> readCsvToList(String fileName) {
-        List<List<String>> records = new ArrayList<>();
-        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] values = line.split(",");
-                records.add(Arrays.asList(values));
-            }
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException("No file found ", e);
+    private static List<String> readCsvToList(String fileName) {
+        List<String> records = null;
+        try {
+            records = Files.readAllLines(Path.of(fileName));
         } catch (IOException e) {
-            throw new RuntimeException("Error reading file ", e);
+            throw new RuntimeException("Can not read file " + fileName + e);
         }
         return records;
     }
