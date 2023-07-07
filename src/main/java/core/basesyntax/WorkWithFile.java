8679aaf8daf9f2465ed19edd9fc397d7b91ca238
Volodymyr Lazarchuk <lazarchuk.v.u@gmail.com>
2023-07-07 17:14:20 +0300

    - Isolated report creation logic into a new private method 'createReport' - Moved IOException handling into 'readData' and 'writeData' methods - Fixed string formatting in RuntimeException messages

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -12,48 +12,56 @@ public class WorkWithFile {
     private static final int VALUE_INDEX = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        int supply = 0;
-        int buy = 0;
+        int supply;
+        int buy;
 
         try {
             supply = readData(fromFileName, "supply");
             buy = readData(fromFileName, "buy");
         } catch (IOException e) {
-            throw new RuntimeException("Can't read file" + fromFileName, e);
+            throw new RuntimeException("Can't read file " + fromFileName, e);
         }
 
-        int result = supply - buy;
-
-        StringBuilder report = new StringBuilder();
-        report.append("supply,").append(supply).append(System.lineSeparator());
-        report.append("buy,").append(buy).append(System.lineSeparator());
-        report.append("result,").append(result).append(System.lineSeparator());
+        String report = createReport(supply, buy);
 
         try {
-            writeData(toFileName, report.toString());
+            writeData(toFileName, report);
         } catch (IOException e) {
-            throw new RuntimeException("Can't write to file" + toFileName, e);
+            throw new RuntimeException("Can't write to file " + toFileName, e);
         }
     }
 
     private int readData(String fileName, String operationType) throws IOException {
         int total = 0;
+
         try (BufferedReader reader = Files.newBufferedReader(Paths.get(fileName))) {
             String line;
-
             while ((line = reader.readLine()) != null) {
                 String[] parts = line.split(DELIMITER);
                 if (parts[OPERATION_INDEX].equals(operationType)) {
                     total += Integer.parseInt(parts[VALUE_INDEX]);
                 }
             }
+        } catch (IOException e) {
+            throw new IOException("Can't read file " + fileName, e);
         }
+
         return total;
     }
 
     private void writeData(String fileName, String data) throws IOException {
         try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(fileName))) {
             writer.write(data);
+        } catch (IOException e) {
+            throw new IOException("Can't write to file " + fileName, e);
         }
+    }
+
+    private String createReport(int supply, int buy) {
+        int result = supply - buy;
+
+        return "supply," + supply + System.lineSeparator()
+                + "buy," + buy + System.lineSeparator()
+                + "result," + result + System.lineSeparator();
     }
 }
