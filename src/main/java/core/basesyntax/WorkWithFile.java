91518ce04f8db19b5aaef2e3c2c3ffee45689dad
Zhdanov Anton <anton1992xxxx@gmail.com>
2023-07-07 13:51:38 +0300

    redid the logic without two methods, removed unnecessary variables and changed access modifiers

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,92 +1,75 @@
 package core.basesyntax;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.List;
 
 public class WorkWithFile {
     private static final String STRING_SEPARATOR = ",";
-    private static final String SHOP_SUPPLIER = "supply,";
-    private static final String SHOP_BUY = "buy,";
-    private static final String RESULT_COUNT = "result,";
+    private static final String SHOP_SUPPLIER = "supply";
+    private static final String SHOP_BUY = "buy";
+    private static final String RESULT_COUNT = "result";
     private static final int ARGUMENT = 0;
-    private int sumSupply;
-    private int sumBuy;
-    private int result;
 
-    protected void getStatistic(String fromFileName, String toFileName) {
-        calculateNewFile(fromFileName);
-        createReportFile(toFileName);
-
+    public void getStatistic(String fromFileName, String toFileName) {
+        String storetable = readDataFromFile(fromFileName);
+        String report = filterData(storetable);
+        writeNewData(report, toFileName);
     }
 
-    private void createFile(String fileName) {
-
-        try {
-            File file = new File(fileName);
-            file.createNewFile();
-        } catch (IOException e) {
-            throw new RuntimeException("File not created" + fileName, e);
-        }
-    }
-
-    private void calculateNewFile(String fromFileName) {
-        sumSupply = 0;
-        sumBuy = 0;
-        for (String str : readDataFromFile(fromFileName)) {
-            if (getStringSeparator(str).equals("supply")) {
-                sumSupply += getSum(str);
-            } else {
-                sumBuy += getSum(str);
+    private String readDataFromFile(String fromFileName) {
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
+            String line = bufferedReader.readLine();
+            while (line != null) {
+                stringBuilder.append(line).append(System.lineSeparator());
+                line = bufferedReader.readLine();
             }
-
-        }
-        result = sumSupply - sumBuy;
-
-    }
-
-    private List<String> readDataFromFile(String fromFileName) {
-        try {
-            return Files.readAllLines(Path.of(fromFileName));
-
         } catch (IOException e) {
             throw new RuntimeException("Can't read the file" + fromFileName, e);
         }
+        return stringBuilder.toString();
     }
 
-    private void createReportFile(String toFileName) {
-        StringBuilder reportBuilder = new StringBuilder();
-        reportBuilder.append(SHOP_SUPPLIER)
-                .append(sumSupply)
+    private String filterData(String storetable) {
+        String[] datastore = storetable.split(System.lineSeparator());
+        int sumSupply = 0;
+        int sumBuy = 0;
+        for (String data : datastore) {
+            String[] store = data.split(STRING_SEPARATOR);
+            if (store[ARGUMENT].equals("supply")) {
+                sumSupply += Integer.parseInt(store[1]);
+            } else {
+                sumBuy += Integer.parseInt(store[1]);
+            }
+        }
+        return createReportFile(sumSupply, sumBuy);
+    }
+
+    private String createReportFile(int sumSupply, int sumBuy) {
+        int result = sumSupply - sumBuy;
+        StringBuilder reportBuilder = new StringBuilder(SHOP_SUPPLIER).append(STRING_SEPARATOR);
+        reportBuilder.append(sumSupply)
                 .append(System.lineSeparator())
                 .append(SHOP_BUY)
+                .append(STRING_SEPARATOR)
                 .append(sumBuy)
-                .append(System.lineSeparator()).append(RESULT_COUNT)
-                .append(result)
-                .append(System.lineSeparator());
-        createFile(toFileName);
-        writeNewData(toFileName, reportBuilder.toString());
-
+                .append(System.lineSeparator())
+                .append(RESULT_COUNT)
+                .append(STRING_SEPARATOR)
+                .append(result);
+        return reportBuilder.toString();
     }
 
-    private String getStringSeparator(String str) {
-        return str.split(STRING_SEPARATOR)[ARGUMENT];
-
-    }
-
-    private void writeNewData(String toFileName, String data) {
-        try {
-            Files.write(Path.of(toFileName), data.getBytes(StandardCharsets.UTF_8));
+    private void writeNewData(String createReport, String toFileName) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+            bufferedWriter.write(createReport);
         } catch (IOException e) {
-            throw new RuntimeException("Can't write this file", e);
+            throw new RuntimeException("Can't write data to file" + toFileName + e);
         }
     }
-
-    private int getSum(String str) {
-        return Integer.parseInt(str.split(STRING_SEPARATOR)[1]);
-    }
 }
+
 
