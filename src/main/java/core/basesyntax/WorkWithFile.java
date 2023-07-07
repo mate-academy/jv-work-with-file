eca7a9d43be90f919fd2f984313d2a783a3c6939
DaniilNepelev <kakashilvng@gmail.com>
2023-07-07 13:02:23 +0300

    fixed some more problems

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,8 +7,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    private static final String READING_FROM_FILE_ERROR = "I can't read from this file any "
-                                                          + "information.";
     private static final String SPLIT_FOR_COLUMNS = ",";
     private static final int OPERATION_INDEX = 0;
     private static final int VALUE_INDEX = 1;
@@ -17,17 +15,15 @@ public class WorkWithFile {
     private static final String WRITE_REPORT_ERROR = "Sorry, but I can't work with this data.";
     private static final String RESULT_TYPE = "result";
     private static final String WRITING_TO_FILE_ERROR = "Sorry, but i can't write in file";
-    private static final String OTHER_ERROR = "There were some problems during the execution"
-                                               + " of the program";
 
     public void getStatistic(String fromFileName, String toFileName) {
         String[] dataFromFile = readFileToString(fromFileName);
         int[] supplyBuyInfo = calculateSupplyBuy(dataFromFile);
-        String report = writeReport(supplyBuyInfo);
+        String report = createReport(supplyBuyInfo);
         writeToFile(report, toFileName);
     }
 
-    private static String[] readFileToString(String fileName) {
+    private String[] readFileToString(String fileName) {
         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             String line = reader.readLine();
             StringBuilder result = new StringBuilder();
@@ -37,13 +33,12 @@ public class WorkWithFile {
             }
             return result.toString().split("\\.");
         } catch (IOException e) {
-            throw new RuntimeException(READING_FROM_FILE_ERROR);
-        } catch (RuntimeException e) {
-            throw new RuntimeException(OTHER_ERROR + e);
+            throw new RuntimeException("I can't read from this file any "
+                                       + "information.", e);
         }
     }
 
-    private static int[] calculateSupplyBuy(String[] informationFromFile) {
+    private int[] calculateSupplyBuy(String[] informationFromFile) {
         int supplyAmount = 0;
         int buyAmount = 0;
 
@@ -59,7 +54,7 @@ public class WorkWithFile {
         return new int[] {supplyAmount, buyAmount};
     }
 
-    private static String writeReport(int[] supplyAndBuy) {
+    private String createReport(int[] supplyAndBuy) {
         if (supplyAndBuy.length != 2) {
             throw new RuntimeException(WRITE_REPORT_ERROR);
         }
