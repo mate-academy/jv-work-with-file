b480b04644e17ee9ce22da21918aaa78e0513e29
Volodymyr Lazarchuk <lazarchuk.v.u@gmail.com>
2023-07-07 13:55:03 +0300

    Refactoring the getStatistic() method: moved the read and write logic to private helper methods

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -15,46 +15,45 @@ public class WorkWithFile {
         int supply = 0;
         int buy = 0;
 
-        try (BufferedReader reader = Files.newBufferedReader(Paths.get(fromFileName))) {
+        try {
+            supply = readData(fromFileName, "supply");
+            buy = readData(fromFileName, "buy");
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file" + fromFileName, e);
+        }
+
+        int result = supply - buy;
+
+        StringBuilder report = new StringBuilder();
+        report.append("supply,").append(supply).append(System.lineSeparator());
+        report.append("buy,").append(buy).append(System.lineSeparator());
+        report.append("result,").append(result).append(System.lineSeparator());
+
+        try {
+            writeData(toFileName, report.toString());
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write to file" + toFileName, e);
+        }
+    }
+
+    private int readData(String fileName, String operationType) throws IOException {
+        int total = 0;
+        try (BufferedReader reader = Files.newBufferedReader(Paths.get(fileName))) {
             String line;
 
             while ((line = reader.readLine()) != null) {
                 String[] parts = line.split(DELIMITER);
-                switch (parts[OPERATION_INDEX]) {
-                    case "supply":
-                        supply += Integer.parseInt(parts[VALUE_INDEX]);
-                        break;
-                    case "buy":
-                        buy += Integer.parseInt(parts[VALUE_INDEX]);
-                        break;
-                    default:
-                        throw new RuntimeException("Something went terribly "
-                                + "wrong in switch-case block");
+                if (parts[OPERATION_INDEX].equals(operationType)) {
+                    total += Integer.parseInt(parts[VALUE_INDEX]);
                 }
             }
-
-            int result = supply - buy;
-
-            StringBuilder report = new StringBuilder();
-            report.append("supply,").append(supply).append(System.lineSeparator());
-            report.append("buy,").append(buy).append(System.lineSeparator());
-            report.append("result,").append(result).append(System.lineSeparator());
-
-            try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(toFileName))) {
-                writer.write(report.toString());
-            } catch (IOException e) {
-                throw new RuntimeException("Can't write to file" + toFileName, e);
-            }
-        } catch (IOException e) {
-            throw new RuntimeException("Can't read file" + fromFileName, e);
         }
+        return total;
     }
 
-    private void readFromFile(String fromFileName) {
-
-    }
-
-    private void writeToFile(String toFileName, int supply, int buy, int result) {
-
+    private void writeData(String fileName, String data) throws IOException {
+        try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(fileName))) {
+            writer.write(data);
+        }
     }
 }
