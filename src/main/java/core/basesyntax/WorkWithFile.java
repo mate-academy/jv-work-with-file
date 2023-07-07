68aa7dba2ca303562b29bda1877b863db992dd55
Your Name <a.o.vozniuk@gmail.com>
2023-07-07 09:24:21 +0300

    updated logic according to review

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,11 +1,11 @@
 package core.basesyntax;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
 import java.util.List;
 
 public class WorkWithFile {
@@ -18,20 +18,24 @@ public class WorkWithFile {
     public static void getStatistic(String fromFileName, String toFileName) {
         deleteResultFileIfExist(toFileName);
         List<String> records = readCsvToList(fromFileName);
-        String[] report = generateReport(records);
+        String report = generateReport(records);
         writeToFile(report, new File(toFileName));
     }
 
-    private static String[] generateReport(List<String> records) {
+    private static String generateReport(List<String> records) {
         int[] totalSupplyBuyResult = new int[3];
         calculateSupplyAndBuy(totalSupplyBuyResult, records);
-        totalSupplyBuyResult[2] = totalSupplyBuyResult[SUPPLY_INDEX]
+        totalSupplyBuyResult[RESULT_INDEX] = totalSupplyBuyResult[SUPPLY_INDEX]
                 - totalSupplyBuyResult[BUY_INDEX];
         String[] resultData = new String[3];
-        resultData[0] = "supply," + totalSupplyBuyResult[SUPPLY_INDEX];
-        resultData[1] = "buy," + totalSupplyBuyResult[BUY_INDEX];
-        resultData[2] = "result," + totalSupplyBuyResult[RESULT_INDEX];
-        return resultData;
+        resultData[SUPPLY_INDEX] = "supply," + totalSupplyBuyResult[SUPPLY_INDEX];
+        resultData[BUY_INDEX] = "buy," + totalSupplyBuyResult[BUY_INDEX];
+        resultData[RESULT_INDEX] = "result," + totalSupplyBuyResult[RESULT_INDEX];
+        StringBuilder result = new StringBuilder();
+        for(int i = 0; i < resultData.length; i++){
+            result.append(resultData[i]).append(System.lineSeparator());
+        }
+        return result.toString();
     }
 
     private static void deleteResultFileIfExist(String toFileName) {
@@ -46,29 +50,25 @@ public class WorkWithFile {
         for (int i = 0; i < records.size(); i++) {
             String[] record = records.get(i).split(",");
             if (record[RECORD_NAME_INDEX].equals("supply")) {
-                resultArray[0] += Integer.parseInt(record[RECORD_VALUE_INDEX]);
+                resultArray[SUPPLY_INDEX] += Integer.parseInt(record[RECORD_VALUE_INDEX]);
             } else if (record[RECORD_NAME_INDEX].equals("buy")) {
-                resultArray[1] += Integer.parseInt(record[RECORD_VALUE_INDEX]);
+                resultArray[BUY_INDEX] += Integer.parseInt(record[RECORD_VALUE_INDEX]);
             } else {
                 throw new RuntimeException("Invalid record type " + records.get(0));
             }
         }
     }
 
-    private static void writeToFile(String[] result, File file) {
+    private static void writeToFile(String result, File file) {
         try {
             file.createNewFile();
         } catch (IOException e) {
             throw new RuntimeException("Can not create file" + e);
         }
-        for (int i = 0; i < result.length; i++) {
-            try {
-                Files.write(file.toPath(), (result[i]
-                                + System.lineSeparator()).getBytes(StandardCharsets.UTF_8),
-                        StandardOpenOption.APPEND);
-            } catch (IOException e) {
-                throw new RuntimeException("Error writing file" + e);
-            }
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+            writer.write(result);
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t write file" + file, e);
         }
     }
 
