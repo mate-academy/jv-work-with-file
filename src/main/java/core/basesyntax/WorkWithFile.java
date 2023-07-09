6efb3fdaee51df74fa29f82e3779c9240b4b6d9d
Oleg Turyk <oleh.turyk.ki.2019@lpnu.ua>
2023-07-09 14:09:25 +0300

    Fixed errors

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,17 +7,23 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    private static final String DELIMITER = ",";
+    private static final int OPERATION_INDEX = 0;
+    private static final int VALUE_INDEX = 1;
+
     public void getStatistic(String fromFileName, String toFileName) {
-        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
-                BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
+        writeToFile(toFileName, readFromFile(fromFileName));
+    }
+
+    private int[] readFromFile(String fromFileName) {
+        try(BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
             int supplyTotal = 0;
             int buyTotal = 0;
             String line;
-
             while ((line = reader.readLine()) != null) {
-                String[] fields = line.split(",");
-                String operationType = fields[0].trim();
-                int amount = Integer.parseInt(fields[1].trim());
+                String[] fields = line.split(DELIMITER);
+                String operationType = fields[OPERATION_INDEX].trim();
+                int amount = Integer.parseInt(fields[VALUE_INDEX].trim());
                 if (operationType.equals("supply")) {
                     supplyTotal += amount;
                 } else if (operationType.equals("buy")) {
@@ -25,13 +31,22 @@ public class WorkWithFile {
                 }
             }
             int result = supplyTotal - buyTotal;
-            writer.write("supply," + supplyTotal);
-            writer.newLine();
-            writer.write("buy," + buyTotal);
-            writer.newLine();
-            writer.write("result," + result);
+            return new int[]{supplyTotal, buyTotal, result};
+
         } catch (IOException e) {
-            throw new RuntimeException("Can't open the file ", e);
+            throw new RuntimeException("Can't open the file " + fromFileName + " ", e);
+        }
+    }
+
+    private void writeToFile(String toFileName, int[] statistics) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
+            writer.write("supply," + statistics[0]);
+            writer.newLine();
+            writer.write("buy," + statistics[1]);
+            writer.newLine();
+            writer.write("result," + statistics[2]);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't open the file " + toFileName + " ", e);
         }
     }
 }
