e894754934b29efb2dbd3e0172a1e80d4fbece82
Danial <danialbest55@gmail.com>
2023-07-27 18:54:42 +0300

    Hi again)

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,56 @@
 package core.basesyntax;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
+        int supply = 0;
+        int buy = 0;
 
+        try {
+            Scanner scanner = new Scanner(new File(fromFileName));
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] nameAndNumber = line.split(",");
+                String action = nameAndNumber[0];
+                int amount = Integer.parseInt(nameAndNumber[1]);
+
+                switch (action) {
+                    case "supply":
+                        supply += amount;
+                        break;
+                    case "buy":
+                        buy += amount;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        String[][] report = {
+                {"supply", String.valueOf(supply)},
+                {"buy", String.valueOf(buy)},
+                {"result", String.valueOf(supply - buy)}
+        };
+
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName));
+            for (String[] row : report) {
+                writer.write(row[0] + "," + row[1]);
+                writer.newLine();
+            }
+            writer.close();
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write data to file " + toFileName, e);
+        }
     }
 }
+
+
