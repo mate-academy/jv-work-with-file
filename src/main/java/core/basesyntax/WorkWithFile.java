f742fa0882058758a95366725376cf0de94c4b17
Olha <olga.olegovna2802@online.ua>
2023-07-29 13:45:05 +0100

    Implement method "getStatistic" in "WorkWithFile" task, fix the mistakes

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,6 +7,9 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    private static final int FIRST_INDEX = 0;
+    private static final int SECOND_INDEX = 1;
+
     public void getStatistic(String fromFileName, String toFileName) {
         String fileContent = readFileContent(fromFileName);
         String report = makeReport(fileContent);
@@ -15,14 +18,13 @@ public class WorkWithFile {
 
     private String readFileContent(String fromFileName) {
         StringBuilder content = new StringBuilder();
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
             String value;
             while ((value = reader.readLine()) != null) {
-                content.append(value).append("\n");
+                content.append(value).append(System.lineSeparator());
             }
         } catch (IOException e) {
-            throw new RuntimeException("Can't reat this file" + fromFileName, e);
+            throw new RuntimeException("Can't reat this file:" + fromFileName, e);
         }
         return content.toString();
     }
@@ -31,11 +33,11 @@ public class WorkWithFile {
         int supplySum = 0;
         int buySum = 0;
         StringBuilder report = new StringBuilder();
-        String[] lines = fileContent.split("\n");
+        String[] lines = fileContent.split(System.lineSeparator());
         for (String line : lines) {
             String[] parts = line.split(",");
-            String operation = parts[0];
-            int amount = Integer.parseInt(parts[1].trim());
+            String operation = parts[FIRST_INDEX];
+            int amount = Integer.parseInt(parts[SECOND_INDEX].trim());
             if (operation.equals("supply")) {
                 supplySum += amount;
             } else if (operation.equals("buy")) {
@@ -43,9 +45,13 @@ public class WorkWithFile {
             }
         }
         int result = supplySum - buySum;
-        report.append("supply,").append(supplySum).append(System.lineSeparator());
-        report.append("buy,").append(buySum).append(System.lineSeparator());
-        report.append("result,").append(result);
+        report.append("supply,")
+                .append(supplySum)
+                .append(System.lineSeparator())
+                .append("buy,")
+                .append(buySum).append(System.lineSeparator())
+                .append("result,")
+                .append(result);
         return report.toString();
     }
 
