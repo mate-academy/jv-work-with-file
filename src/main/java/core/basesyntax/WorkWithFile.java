2036090dc189ba8db66e70e3516866599c03f3b9
Krasnov-Maksim <maksim.v.krasnov@gmail.com>
2023-07-30 15:36:39 +0300

    fixed: added createReport() and writeToFile() methods.

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -12,31 +12,41 @@ public class WorkWithFile {
     private static final String RESULT = "result";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName));
-                BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+        int[] report;
+        try {
+            report = createReport(fromFileName);
+            writeToFile(toFileName, report[0], report[1], report[2]);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read/write file.", e);
+        }
+    }
 
-            int supply = 0;
-            int buy = 0;
+    private int[] createReport(final String fromFileName) throws IOException {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
+            //report[0] = supply; report[1] = buy; report[2] = result = supply - buy;
+            int[] report = new int[3];
             String line;
-
             while ((line = bufferedReader.readLine()) != null) {
                 final String[] words = line.split(",");
                 if (SUPPLY.equals(words[0])) {
-                    supply += Integer.parseInt(words[1]);
+                    report[0] += Integer.parseInt(words[1]);
                 } else if (BUY.equals(words[0])) {
-                    buy += Integer.parseInt(words[1]);
+                    report[1] += Integer.parseInt(words[1]);
                 }
             }
-            int result = supply - buy;
+            report[2] = report[0] - report[1];
+            return report;
+        }
+    }
 
+    private void writeToFile(final String toFileName, final int supply, final int buy,
+                             final int result) throws IOException {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
             StringBuilder stringBuilder = new StringBuilder();
             stringBuilder.append(SUPPLY).append(",").append(supply).append(System.lineSeparator());
             stringBuilder.append(BUY).append(",").append(buy).append(System.lineSeparator());
             stringBuilder.append(RESULT).append(",").append(result).append(System.lineSeparator());
-
             bufferedWriter.write(stringBuilder.toString());
-        } catch (IOException e) {
-            throw new RuntimeException("Can't read/write file.", e);
         }
     }
 }
