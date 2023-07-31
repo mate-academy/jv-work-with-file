936be77f5a1cdfc5679e085aa0837b0a6e107a32
Oleksandr Pidhaiko <iviviv1999@gmail.com>
2023-08-01 00:36:42 +0300

    fixed errors pointed out by mentor

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,22 +7,23 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.StringReader;
 
 public class WorkWithFile {
     private static final String BUY_OPERATION = "buy";
     private static final String SUPPLY_OPERATION = "supply";
     private static final String RESULT_OPERATION = "result";
-    private static final String IO_OPERATION_ERROR = "I/O operation interrupted";
+    private static final int OPERATION_INDEX = 0;
+    private static final int OPERATION_AMOUNT_INDEX = 1;
+
     private static final String VALUES_SEPARATOR = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        String readFromFileResult = readFromFile(fromFileName);
-        String fileContentProcessed = processFileReadResults(readFromFileResult);
-        writeToFile(fileContentProcessed, toFileName);
+        String fileContent = readFromFile(fromFileName);
+        String report = generateReport(fileContent);
+        writeToFile(report, toFileName);
     }
 
-    private static String readFromFile(String fromFileName) {
+    private String readFromFile(String fromFileName) {
         StringBuilder fileRead = new StringBuilder();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             String fileReadLine = bufferedReader.readLine();
@@ -31,60 +32,53 @@ public class WorkWithFile {
                 fileReadLine = bufferedReader.readLine();
             }
         } catch (FileNotFoundException e) {
-            throw new RuntimeException("File not found.", e);
+            throw new RuntimeException("File " + fromFileName + " not found.", e);
         } catch (IOException e) {
-            throw new RuntimeException(IO_OPERATION_ERROR, e);
+            throw new RuntimeException("I/O operation interrupted.", e);
         }
         return String.valueOf(fileRead);
     }
 
-    private static String processFileReadResults(String fileContent) {
+    private String generateReport(String fileContent) {
         int supplyOperationSum = 0;
         int buyOperationSum = 0;
-        try (BufferedReader bufferedReader = new BufferedReader(new StringReader(fileContent))) {
-            final int operationAmountIndex = 1;
-            String line = bufferedReader.readLine();
-            while (line != null) {
-                String[] lineSplit = line.split(VALUES_SEPARATOR);
-                String operationType = lineSplit[0];
-                String operationAmountString = lineSplit[operationAmountIndex];
-                int operationAmount = 0;
+        String[] fileContentSplit = fileContent.split(System.lineSeparator());
+        for (String fileContentLine : fileContentSplit) {
+            String[] fileContentLineSplit = fileContentLine.split(VALUES_SEPARATOR);
+            String operationType = fileContentLineSplit[OPERATION_INDEX];
+            String operationAmountString = fileContentLineSplit[OPERATION_AMOUNT_INDEX];
+            int operationAmount = 0;
+            try {
                 operationAmount = Integer.parseInt(operationAmountString);
-                switch (operationType) {
-                    case BUY_OPERATION: {
-                        buyOperationSum += operationAmount;
-                        break;
-                    }
-                    case SUPPLY_OPERATION: {
-                        supplyOperationSum += operationAmount;
-                        break;
-                    }
-                    default: {
-                        throw new RuntimeException("Wrong operation type.");
-                    }
-                }
-                line = bufferedReader.readLine();
+            } catch (NumberFormatException e) {
+                throw new RuntimeException("Wrong operation amount format.", e);
             }
-        } catch (NumberFormatException e) {
-            throw new RuntimeException("Wrong operation amount format.", e);
-        } catch (IOException e) {
-            throw new RuntimeException(IO_OPERATION_ERROR, e);
+            switch (operationType) {
+                case BUY_OPERATION: {
+                    buyOperationSum += operationAmount;
+                    break;
+                }
+                case SUPPLY_OPERATION: {
+                    supplyOperationSum += operationAmount;
+                    break;
+                }
+                default: {
+                    throw new RuntimeException("Wrong operation type.");
+                }
+            }
         }
         return SUPPLY_OPERATION + VALUES_SEPARATOR + supplyOperationSum + System.lineSeparator()
                 + BUY_OPERATION + VALUES_SEPARATOR + buyOperationSum + System.lineSeparator()
                 + RESULT_OPERATION + VALUES_SEPARATOR + (supplyOperationSum - buyOperationSum);
     }
 
-    private static void writeToFile(String fileContentProcessed, String toFile) {
+    private static void writeToFile(String report, String toFile) {
         File resultFile = new File(toFile);
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(resultFile));
-                BufferedReader bufferedReader = new BufferedReader(
-                        new StringReader(fileContentProcessed))) {
-            String line = bufferedReader.readLine();
-            while (line != null) {
-                bufferedWriter.write(line);
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(resultFile))) {
+            String[] reportSplit = report.split(System.lineSeparator());
+            for (String reportSplitLine : reportSplit) {
+                bufferedWriter.write(reportSplitLine);
                 bufferedWriter.write(System.lineSeparator());
-                line = bufferedReader.readLine();
             }
         } catch (IOException e) {
             throw new RuntimeException("I/O operation interrupted", e);
