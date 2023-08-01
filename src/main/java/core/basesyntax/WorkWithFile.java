abcf8ab975ec752a15c62734bb149c2ac50ca038
Kolinko Oleksii <alexkolinko22@gmail.com>
2023-08-01 13:54:00 +0200

    HW8-solution Used radLine(), made "," constant;

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,7 +7,8 @@ import java.io.FileReader;
 import java.io.FileWriter;
 
 public class WorkWithFile {
-    private static final String INDEX_SEPARATOR = System.lineSeparator();
+    private static final String LINE_SEPARATOR = System.lineSeparator();
+    private static final String SPLIT_LINE = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
         String[] textFromFile = readFile(fromFileName);
@@ -19,29 +20,28 @@ public class WorkWithFile {
         File fromFile = new File(fromFileName);
         StringBuilder readedFromFile = new StringBuilder();
         try (BufferedReader fromFileReader = new BufferedReader(new FileReader(fromFile))) {
-            int data = fromFileReader.read();
-            while (data != -1) {
-                readedFromFile.append((char) data);
-                data = fromFileReader.read();
+            readedFromFile.append(fromFileReader.readLine()).append(LINE_SEPARATOR);
+            while (fromFileReader.ready()) {
+                readedFromFile.append(fromFileReader.readLine()).append(LINE_SEPARATOR);
             }
         } catch (Exception e) {
             throw new RuntimeException("Can't read file" + fromFileName, e);
         }
-        return readedFromFile.toString().split(INDEX_SEPARATOR);
+        return readedFromFile.toString().split(LINE_SEPARATOR);
     }
 
     private String createReport(String[] textFromFile) {
         int buy = 0;
         int supply = 0;
         for (String line : textFromFile) {
-            if (line.split(",")[0].equals("buy")) {
-                buy += Integer.parseInt(line.split(",")[1]);
+            if (line.split(SPLIT_LINE)[0].equals("buy")) {
+                buy += Integer.parseInt(line.split(SPLIT_LINE)[1]);
             } else {
-                supply += Integer.parseInt(line.split(",")[1]);
+                supply += Integer.parseInt(line.split(SPLIT_LINE)[1]);
             }
         }
-        return new StringBuilder().append("supply,").append(supply).append(INDEX_SEPARATOR)
-                .append("buy,").append(buy).append(INDEX_SEPARATOR).append("result,")
+        return new StringBuilder().append("supply,").append(supply).append(LINE_SEPARATOR)
+                .append("buy,").append(buy).append(LINE_SEPARATOR).append("result,")
                 .append(supply - buy).toString();
     }
 
