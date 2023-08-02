4acf1bd29137e19766e387acf1be38cedf9b8c6e
Andrey Polezhaiev <polezhaievandrey28@gmail.com>
2023-08-02 22:04:49 +0300

    code optimization

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -10,22 +10,8 @@ import java.io.IOException;
 
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
-        if (fromFileName == null || toFileName == null
-                || fromFileName.length() == 0 || toFileName.length() == 0) {
-            return;
-        }
-
-        StringBuilder stringBuilder = new StringBuilder();
-        int buy = 0;
-        int supply = 0;
-        int result = 0;
-
         File writeToFile = new File(toFileName);
         File writeFromFile = new File(fromFileName);
-        if (!writeFromFile.exists()) {
-            return;
-        }
-
         try {
             writeToFile.createNewFile();
         } catch (IOException e) {
@@ -34,42 +20,53 @@ public class WorkWithFile {
 
         try (BufferedReader reader = new BufferedReader(new FileReader(writeFromFile))) {
             try (BufferedWriter writer = new BufferedWriter(new FileWriter(writeToFile))) {
-
-                String dataLine = reader.readLine();
-                while (dataLine != null) {
-                    String[] data = dataLine.split("[, ]");
-                    if (dataLine.contains("buy")) {
-                        buy += Integer.parseInt(data[1]);
-                    } else {
-                        supply += Integer.parseInt(data[1]);
-                    }
-
-                    dataLine = reader.readLine();
-                }
-                result = supply - buy;
-
-                stringBuilder.append("supply")
-                        .append(",")
-                        .append(supply)
-                        .append(System.lineSeparator());
-
-                stringBuilder.append("buy")
-                        .append(",")
-                        .append(buy)
-                        .append(System.lineSeparator());
-
-                stringBuilder.append("result")
-                        .append(",")
-                        .append(result)
-                        .append(System.lineSeparator());
-
-                writer.write(stringBuilder.toString());
+                String[] sumOfTypes = getSumOpType(reader).split(" ");
+                String[] types = new String[]{"supply", "buy", "result"};
+                writer.write(getDataForFile(sumOfTypes,types));
                 writer.flush();
             }
         } catch (FileNotFoundException e) {
             throw new RuntimeException("Can't find a file " + fromFileName, e);
         } catch (IOException e) {
             throw new RuntimeException("Can't read from a file", e);
+        }
+    }
+
+    private String getDataForFile(String[] sumOpTypes, String[] types) {
+        StringBuilder builder = new StringBuilder();
+        int i = 0;
+        for (String typeValue: sumOpTypes) {
+            builder.append(System.lineSeparator())
+                    .append(types[i])
+                    .append(",")
+                    .append(typeValue);
+            i++;
+        }
+
+        return builder.toString();
+    }
+
+    private String getSumOpType(BufferedReader reader) {
+        int buy = 0;
+        int supply = 0;
+        int result = 0;
+        try (BufferedReader bufferedReader = reader) {
+            String dataLine = bufferedReader.readLine();
+            while (dataLine != null) {
+                String[] data = dataLine.split("[, ]");
+                if (dataLine.contains("buy")) {
+                    buy += Integer.parseInt(data[1]);
+                } else {
+                    supply += Integer.parseInt(data[1]);
+                }
+
+                dataLine = bufferedReader.readLine();
+            }
+            result = supply - buy;
+
+            return supply + " " + buy + " " + result;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
