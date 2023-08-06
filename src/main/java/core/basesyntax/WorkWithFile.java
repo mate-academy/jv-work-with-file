da424bb41ca34953901ebf9da9018bf72eec191f
Andrey Polezhaiev <polezhaievandrey28@gmail.com>
2023-08-06 22:48:48 +0300

    understantable structure

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,55 +2,41 @@ package core.basesyntax;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
-        File writeToFile = new File(toFileName);
-        File writeFromFile = new File(fromFileName);
-        try {
-            writeToFile.createNewFile();
+        String dataFromFile = readFile(fromFileName);
+        String report = createReport(dataFromFile);
+        writeToFile(report, toFileName);
+    }
+
+    private void writeToFile(String report, String toFileName) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+            bufferedWriter.write(report);
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
-        try (BufferedReader reader = new BufferedReader(new FileReader(writeFromFile))) {
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter(writeToFile))) {
-                String[] sumOfTypes = getSumOpType(reader).split(" ");
-                String[] types = new String[]{"supply", "buy", "result"};
-                writer.write(getDataForFile(sumOfTypes,types));
-                writer.flush();
-            }
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException("Can't find a file " + fromFileName, e);
-        } catch (IOException e) {
-            throw new RuntimeException("Can't read from a file", e);
-        }
     }
 
-    private String getDataForFile(String[] sumOpTypes, String[] types) {
-        StringBuilder builder = new StringBuilder();
-        int i = 0;
-        for (String typeValue: sumOpTypes) {
-            builder.append(System.lineSeparator())
-                    .append(types[i])
-                    .append(",")
-                    .append(typeValue);
-            i++;
-        }
+    private String createReport(String dataFromFile) {
+        String[] countedValues = dataFromFile.split(" ");
 
-        return builder.toString();
+        return "supply," + countedValues[0]
+                + System.lineSeparator()
+                + "buy," + countedValues[1]
+                + System.lineSeparator()
+                + "result," + countedValues[2];
     }
 
-    private String getSumOpType(BufferedReader reader) {
+    private String readFile(String fromFileName) {
         int buy = 0;
         int supply = 0;
         int result = 0;
-        try (BufferedReader bufferedReader = reader) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             String dataLine = bufferedReader.readLine();
             while (dataLine != null) {
                 String[] data = dataLine.split("[, ]");
