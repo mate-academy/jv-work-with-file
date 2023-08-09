07200e2eb5e436e6f79acecb9c420080d533a200
Olena Stankevych <yelenastankevych@gmail.com>
2023-08-09 19:12:26 +0300

    Added fixes after review

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -3,6 +3,8 @@ package core.basesyntax;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -15,24 +17,22 @@ public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
         Map<String, Integer> fileData = readFromFile(fromFileName);
         String report = getReport(fileData);
-        try {
-            Files.write(new File(toFileName).toPath(), report.getBytes());
-        } catch (IOException e) {
-            throw new RuntimeException("Can't write data to file", e);
-        }
+        writeReportToFile(report, toFileName);
     }
 
     private Map<String, Integer> readFromFile(String fileName) {
+        List<String> dataFromFile = new ArrayList<>();
         try {
-            return Files.readAllLines(new File(fileName).toPath()).stream()
-                    .collect(Collectors.toMap(
-                            s -> s.substring(0, s.indexOf(DATA_SEPARATOR)),
-                            s -> Integer.valueOf(s.substring(s.indexOf(DATA_SEPARATOR) + 1)),
-                            Integer::sum
-                    ));
+            dataFromFile.addAll(Files.readAllLines(new File(fileName).toPath()));
         } catch (IOException e) {
             throw new RuntimeException("Can't read data from file", e);
         }
+        return dataFromFile.stream()
+                .collect(Collectors.toMap(
+                        s -> s.substring(0, s.indexOf(DATA_SEPARATOR)),
+                        s -> Integer.valueOf(s.substring(s.indexOf(DATA_SEPARATOR) + 1)),
+                        Integer::sum
+                ));
     }
 
     private String getReport(Map<String, Integer> data) {
@@ -41,6 +41,14 @@ public class WorkWithFile {
                 formatData(SUPPLY_OPERATION, data.get(SUPPLY_OPERATION).toString()),
                 formatData(BUY_OPERATION, data.get(BUY_OPERATION).toString()),
                 formatData(REPORT_RESULT_ROW, String.valueOf(result)));
+    }
+
+    private void writeReportToFile(String report, String fileTo) {
+        try {
+            Files.write(new File(fileTo).toPath(), report.getBytes());
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write data to file", e);
+        }
     }
 
     private String formatData(String... data) {
