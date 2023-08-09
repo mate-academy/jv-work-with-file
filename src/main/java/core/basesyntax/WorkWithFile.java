3a15ac8412424fb6c10fd6cde157ad71e679068c
Epikano <epikano@gmail.com>
2023-08-09 18:19:56 +0300

    Added jv-work-with-file solution.

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,93 @@
 package core.basesyntax;
 
-public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
 
+public class WorkWithFile {
+    private static final String NEWLINE_CHARACTER = System.lineSeparator();
+    private static final String DATA_SEPARATOR_CHARACTER = ",";
+    private static final int DATA_COLUMNS = 2;
+    private static final int OPERATION_TYPE_INDEX = 0;
+    private static final int AMOUNT_INDEX = 1;
+    private static final String[] REPORT_DATA = new String[] {"supply", "buy", "result"};
+    private static final int SUPPLY_INDEX = 0;
+    private static final int BUY_INDEX = 1;
+    private static final int RESULT_INDEX = 2;
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        String report = createReport(fromFileName);
+        writeToFile(report, toFileName);
+    }
+
+    private void writeToFile(String report, String toFileName) {
+        File file = new File(toFileName);
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            throw new RuntimeException("Can't create the file " + toFileName, e);
+        }
+
+        try {
+            Files.write(file.toPath(), report.getBytes());
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write data to the file " + toFileName, e);
+        }
+    }
+
+    private String createReport(String fromFileName) {
+        StringBuilder report = new StringBuilder();
+        int[] reportDataAmounts = new int[REPORT_DATA.length];
+        for (int i = 0; i < REPORT_DATA.length; i++) {
+            report.append(REPORT_DATA[i]).append(DATA_SEPARATOR_CHARACTER);
+            reportDataAmounts[i] = (i != RESULT_INDEX)
+                    ? calculateAmountOfOperationType(fromFileName, REPORT_DATA[i])
+                    : reportDataAmounts[SUPPLY_INDEX] - reportDataAmounts[BUY_INDEX];
+            report.append(reportDataAmounts[i]);
+            if (i != RESULT_INDEX) {
+                report.append(System.lineSeparator());
+            }
+        }
+        return report.toString();
+    }
+
+    private int calculateAmountOfOperationType(String fromFileName, String operationType) {
+        String[][] dataRowsAndColumns = readFromFile(fromFileName);
+        int amount = 0;
+        //int i = 0; i < dataRowsAndColumns.length; i++
+        for (String[] row : dataRowsAndColumns) {
+            if (row[OPERATION_TYPE_INDEX].equals(operationType)) {
+                amount += Integer.parseInt(row[AMOUNT_INDEX]);
+            }
+        }
+        return amount;
+    }
+
+    private String[][] readFromFile(String fileName) {
+        String fileText = convertFileTextToString(fileName);
+        String[] dataRows = fileText.split(NEWLINE_CHARACTER);
+        String[][] dataRowsAndColumns = new String[dataRows.length][DATA_COLUMNS];
+        for (int i = 0; i < dataRows.length; i++) {
+            dataRowsAndColumns[i] = dataRows[i].split(DATA_SEPARATOR_CHARACTER);
+        }
+        return dataRowsAndColumns;
+    }
+
+    private String convertFileTextToString(String fileName) {
+        File file = new File(fileName);
+        StringBuilder fileText = new StringBuilder();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String value = reader.readLine();
+            while (value != null) {
+                fileText.append(value).append(System.lineSeparator());
+                value = reader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read data from the file " + fileName, e);
+        }
+        return fileText.toString();
     }
 }
