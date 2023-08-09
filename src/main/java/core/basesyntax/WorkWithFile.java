4ecb2cffdcb181a515ce4ac05a409703e6272d97
silchenko-arsen <silchenkoarsen@gmail.com>
2023-08-09 16:48:27 +0300

    fix problems4

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,9 +1,10 @@
 package core.basesyntax;
 
-import java.io.BufferedWriter;
+
 import java.io.BufferedReader;
-import java.io.FileWriter;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
