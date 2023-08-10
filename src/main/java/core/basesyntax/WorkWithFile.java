1a0067df37ad6b7c6a03694e683452afe74c6986
silchenko-arsen <silchenkoarsen@gmail.com>
2023-08-10 10:33:11 +0300

    separate method

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -4,46 +4,59 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 public class WorkWithFile {
     private static final String BUY = "buy";
     private static final String SUPPLY = "supply";
     private static final String RESULT = "result";
     private static final String COMA = ",";
+    private static final String LINE_SEPARATOR = System.lineSeparator();
+    private static final int OPERATION_NAME_INDEX = 0;
+    private static final int OPERATION_AMOUNT_INDEX = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        Map<String, Integer> data = new HashMap<>();
+        String textFromFile = readFromFile(fromFileName);
+        String report = createReport(textFromFile);
+        writeToFile(toFileName, report);
+    }
 
+    public String readFromFile(String fromFileName) {
+        StringBuilder stringBuilder = new StringBuilder();
         try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
             String line;
             while ((line = reader.readLine()) != null) {
-                String[] parts = line.split(COMA);
-                if (parts.length == 2) {
-                    String operationType = parts[0].trim();
-                    int amount = Integer.parseInt(parts[1].trim());
-
-                    data.put(operationType, data.getOrDefault(operationType, 0) + amount);
-                }
+                stringBuilder.append(line).append(LINE_SEPARATOR);
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't read data from the file " + fromFileName, e);
         }
+        return stringBuilder.toString();
+    }
 
-        int supplyTotal = data.get(SUPPLY);
-        int buyTotal = data.get(BUY);
+    public String createReport(String text) {
+        int supplyTotal = 0;
+        int buyTotal = 0;
+        final String[] lines = text.split(System.lineSeparator());
+        for (String line : lines) {
+            final String[] words = line.split(COMA);
+            int amount = Integer.parseInt(words[OPERATION_AMOUNT_INDEX]);
+            if (SUPPLY.equals(words[OPERATION_NAME_INDEX])) {
+                supplyTotal += amount;
+            } else if (BUY.equals(words[OPERATION_NAME_INDEX])) {
+                buyTotal += amount;
+            }
+        }
         int result = supplyTotal - buyTotal;
+        return SUPPLY + COMA + supplyTotal + LINE_SEPARATOR
+                + BUY + COMA + buyTotal + LINE_SEPARATOR
+                + RESULT + COMA + result;
+    }
 
+    public void writeToFile(String toFileName, String result) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
-            writer.write(SUPPLY + COMA + supplyTotal);
-            writer.newLine();
-            writer.write(BUY + COMA + buyTotal);
-            writer.newLine();
-            writer.write(RESULT + COMA + result);
-        } catch (IOException e) {
-            e.printStackTrace();
+            writer.write(result);
+        } catch (Exception e) {
+            throw new RuntimeException("Can't write data to the file " + toFileName, e);
         }
     }
 }
