94752e78e240e699e7fafc974b4cf0ac5effb802
Volodymyr Shashkov <finansist33333324@gmail.com>
2023-08-11 16:03:37 +0300

    jv-work-with-file-after-reviw

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,59 +2,50 @@ package core.basesyntax;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 
 public class WorkWithFile {
-    private static final int NUMBER_ONE = 1;
-    private String processingResult = "";
-    private StringBuilder builderReadMethod;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        readWithFile(fromFileName);
-        dataProcessing(builderReadMethod);
-        writeReportToFile(processingResult, toFileName);
+        StringBuilder dataFromFile = readWithFile(fromFileName);
+        String report = createReport(dataFromFile);
+        writeReportToFile(report, toFileName);
     }
 
     private StringBuilder readWithFile(String fileName) {
-        builderReadMethod = new StringBuilder();
+        StringBuilder builderReadMethod = new StringBuilder();
 
         try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
-            try {
-                String value = "";
-                while (value != null) {
-                    builderReadMethod.append(value).append(",");
-                    value = bufferedReader.readLine();
-                }
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+            String value = "";
+            while (value != null) {
+                builderReadMethod.append(value).append(",");
+                value = bufferedReader.readLine();
             }
-
-        } catch (FileNotFoundException e) {
+        } catch (IOException e) {
             throw new RuntimeException("Can't read this file", e);
         }
         return builderReadMethod;
     }
 
-    private void dataProcessing(StringBuilder stringBuilder) {
+    private String createReport(StringBuilder stringBuilder) {
         String[] dataArray = stringBuilder.toString().split(",");
         int supply = 0;
         int buy = 0;
         for (int i = 0; i < dataArray.length; i++) {
             if (dataArray[i].equals("supply")) {
-                supply += Integer.parseInt(dataArray[i + NUMBER_ONE]);
+                supply += Integer.parseInt(dataArray[i + 1]);
             } else if (dataArray[i].equals("buy")) {
-                buy += Integer.parseInt(dataArray[i + NUMBER_ONE]);
+                buy += Integer.parseInt(dataArray[i + 1]);
             }
         }
         StringBuilder builderProcessingMethod = new StringBuilder();
         int result = supply - buy;
-
-        processingResult = builderProcessingMethod.append("supply," + supply)
-                 .append(System.lineSeparator())
+        String report = "";
+        return report = builderProcessingMethod.append("supply," + supply)
+                .append(System.lineSeparator())
                 .append("buy," + buy).append(System.lineSeparator())
                 .append("result," + result).toString();
     }
@@ -63,13 +54,7 @@ public class WorkWithFile {
         File file = new File(toFile);
         try {
             file.createNewFile();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        try {
             Files.write(file.toPath(), processingResult.getBytes());
-
         } catch (IOException e) {
             throw new RuntimeException("don't write to file", e);
         }
