9753b2366ef17cb8cf53e671af9c4ba4bed7eb0c
Sonia Baranova <sofiasapsan@gmail.com>
2023-08-14 12:21:56 +0300

    improved the solution of implementation of getStatistic()

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -13,22 +13,26 @@ public class WorkWithFile {
     private static final String SUPPLY_WORD = "supply";
     private static final int INDEX_OF_WORD = 0;
     private static final int INDEX_OF_NUMBER = 1;
+    private static final String WHITESPACE_SEPARATOR = " ";
+    private static final String COMMA_SEPARATOR = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        writeDataToTheFile(toFileName, createReport(readDataFromFile(fromFileName)));
+        String[] data = readDataFromFile(fromFileName);
+        String report = createReport(data);
+        writeDataToTheFile(toFileName, report);
     }
 
-    private String[] readDataFromFile(String nameOfFile) {
+    private String[] readDataFromFile(String fileName) {
         StringBuilder stringBuilder = new StringBuilder();
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(nameOfFile))) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
             String temporary;
             while ((temporary = bufferedReader.readLine()) != null) {
-                stringBuilder.append(temporary).append(" ");
+                stringBuilder.append(temporary).append(WHITESPACE_SEPARATOR);
             }
         } catch (IOException e) {
-            throw new RuntimeException("Have some problems to read the file: ", e);
+            throw new RuntimeException("Can't read data from the file: " + fileName, e);
         }
-        return stringBuilder.toString().split(" ");
+        return stringBuilder.toString().split(WHITESPACE_SEPARATOR);
     }
 
     private String createReport(String[] data) {
@@ -36,34 +40,34 @@ public class WorkWithFile {
         int buyCounter = 0;
         String[] temporaryArray;
         for (String partOfData : data) {
-            temporaryArray = partOfData.split(",");
+            temporaryArray = partOfData.split(COMMA_SEPARATOR);
             if (temporaryArray[INDEX_OF_WORD].equals(BUY_WORD)) {
                 buyCounter += Integer.parseInt(temporaryArray[INDEX_OF_NUMBER]);
             } else if (temporaryArray[INDEX_OF_WORD].equals(SUPPLY_WORD)) {
                 supplyCounter += Integer.parseInt(temporaryArray[INDEX_OF_NUMBER]);
             }
         }
-        return SUPPLY_WORD + "," + supplyCounter + System.lineSeparator()
-                + BUY_WORD + "," + buyCounter + System.lineSeparator()
+        return SUPPLY_WORD + COMMA_SEPARATOR + supplyCounter + System.lineSeparator()
+                + BUY_WORD + COMMA_SEPARATOR + buyCounter + System.lineSeparator()
                 + "result," + (supplyCounter - buyCounter);
     }
 
-    private void writeDataToTheFile(String nameOfFile, String data) {
-        if (new File(nameOfFile).length() != 0) {
-            deleteContentsOfFile(nameOfFile);
+    private void writeDataToTheFile(String fileName, String data) {
+        if (new File(fileName).length() != 0) {
+            deleteContentsOfFile(fileName);
         }
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(nameOfFile, true))) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName, true))) {
             bufferedWriter.write(data);
         } catch (IOException e) {
-            throw new RuntimeException("Have some problems to write to the file: ", e);
+            throw new RuntimeException("Can't write data to the file: " + fileName, e);
         }
     }
 
-    private void deleteContentsOfFile(String nameOfFile) {
-        try (PrintWriter writer = new PrintWriter(nameOfFile)) {
+    private void deleteContentsOfFile(String fileName) {
+        try (PrintWriter writer = new PrintWriter(fileName)) {
             writer.write("");
         } catch (IOException e) {
-            throw new RuntimeException("Have some problems with deleting contents of file: ", e);
+            throw new RuntimeException("Can't delete content of the file: " + fileName, e);
         }
     }
 }
