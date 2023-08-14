6396107c0ec1e227d058ca411875d085d57f5012
nastasiiaKulinich <kussik2004@gmail.com>
2023-08-14 12:13:26 +0300

    implementation of method which read information from file

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,25 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
 
+    }
+
+    private String readFromFile (String fromFileName) {
+        StringBuilder builder = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))){
+            String value = reader.readLine();
+            while (value != null) {
+                builder.append(value + " ");
+                value = reader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read from file" + fromFileName, e);
+        }
+        return builder.toString();
     }
 }
