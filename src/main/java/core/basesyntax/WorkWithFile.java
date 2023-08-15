6a18edb2ea657aea397ccc1dedfd29aa90d2244b
Andreyepam <epameducation@gmail.com>
2023-08-15 10:59:02 +0300

    correction

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,11 +1,11 @@
 package core.basesyntax;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 public class WorkWithFile {
     private static final String DESIRED_LETTER = "w";
@@ -68,8 +68,8 @@ public class WorkWithFile {
     }
 
     private void writeToFile(String report, String toFileName) {
-        try {
-            Files.write(Path.of(toFileName), report.getBytes());
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
+            writer.write(report);
         } catch (IOException e) {
             throw new RuntimeException("Can't create or write data to the file " + toFileName, e);
         }
