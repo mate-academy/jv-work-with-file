3c31837b9fc6f2b2502dea6813f1ca992787f4b9
Denis Gorda <dnsgorda@gmail.com>
2023-08-16 13:16:22 +0300

    add requested changes

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,40 +7,58 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    private static final String SUP_OPERATION = "supply";
+    private static final String BUY_OPERATION = "buy";
+
     public void getStatistic(String fromFileName, String toFileName) {
+        String report = calculateReport(readFromFile(fromFileName));
+        writeReport(toFileName, report);
+    }
+
+    private String readFromFile(String fileName) {
+        StringBuilder data = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                data.append(line).append("\n");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Error while reading file: " + fileName, e);
+        }
+        return data.toString();
+    }
+
+    private String calculateReport(String data) {
         int supplyTotal = 0;
         int buyTotal = 0;
 
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String[] parts = line.split(",");
-                if (parts.length == 2) {
-                    String operation = parts[0].trim();
-                    int amount = Integer.parseInt(parts[1].trim());
+        String[] lines = data.split("\n");
+        for (String line : lines) {
+            String[] parts = line.split(",");
+            if (parts.length == 2) {
+                String operation = parts[0].trim();
+                int amount = Integer.parseInt(parts[1].trim());
 
-                    if (operation.equals("supply")) {
-                        supplyTotal += amount;
-                    } else if (operation.equals("buy")) {
-                        buyTotal += amount;
-                    }
+                if (operation.equals(SUP_OPERATION)) {
+                    supplyTotal += amount;
+                } else if (operation.equals(BUY_OPERATION)) {
+                    buyTotal += amount;
                 }
             }
-        } catch (IOException e) {
-            throw new RuntimeException("Error occured while reading data from: " + fromFileName, e);
-        } finally {
-            writeReport(toFileName, supplyTotal, buyTotal);
         }
+
+        StringBuilder reportBuilder = new StringBuilder();
+        reportBuilder.append("supply,").append(supplyTotal).append(System.lineSeparator());
+        reportBuilder.append("buy,").append(buyTotal).append(System.lineSeparator());
+        reportBuilder.append("result,").append(supplyTotal - buyTotal);
+        return reportBuilder.toString();
     }
 
-    private void writeReport(String toFileName, int supplyTotal, int buyTotal) {
+    private void writeReport(String toFileName, String report) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
-            writer.write("supply," + supplyTotal + System.lineSeparator());
-            writer.write("buy," + buyTotal + System.lineSeparator());
-            writer.write("result," + (supplyTotal - buyTotal));
+            writer.write(report);
         } catch (IOException e) {
-            throw new RuntimeException("Error occured while writing data to: " + toFileName, e);
+            throw new RuntimeException("Error writing report: " + toFileName, e);
         }
     }
 }
