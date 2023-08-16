22c73fcfa10c6c2d3b2648d4013733ec365a4946
Svyatoslav Tsytsyura <svatik.bos@gmail.com>
2023-08-16 22:16:59 +0300

    made requested changes

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,62 +8,84 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    private static final String SPECIAL_CHAR = ",";
+    private static final String SEPARATOR = ",";
     private static final String SUPPLY = "supply";
     private static final String BUY = "buy";
 
-    private StringBuilder getFileStat(String fromFileName) {
-        File fileToRead = new File(fromFileName);
-        String[] text;
-        String value;
+    public void getStatistic(String fromFileName, String toFileName) {
+        makeWrittenFile(stringStat(calculateArrayOfStat(stringDataReport(fromFileName))),
+                new File(toFileName));
+    }
+
+    private String[] stringDataReport(String fromFileName) {
         StringBuilder stringBuilder = new StringBuilder();
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileToRead))) {
-            int supply = 0;
-            int buy = 0;
-            int result = 0;
+        String value;
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             while ((value = bufferedReader.readLine()) != null) {
-                text = value.split(SPECIAL_CHAR);
-                for (String s : text) {
-                    if (s.equals(SUPPLY)) {
-                        supply += Integer.valueOf(text[1]);
-                    } else if (s.equals(BUY)) {
-                        buy += Integer.valueOf(text[1]);
-                    }
-                }
+                stringBuilder.append(value).append(System.lineSeparator());
             }
-            result = supply - buy;
-            stringBuilder.append("supply," + supply + System.lineSeparator())
-                    .append("buy," + buy + System.lineSeparator())
-                    .append("result," + result + System.lineSeparator());
         } catch (IOException e) {
-            throw new RuntimeException("Can't read this file", e);
+            throw new RuntimeException("Can't read file", e);
         }
+        String[] text = stringBuilder.toString().split(System.lineSeparator());
+        return text;
+    }
+
+    private int[] calculateArrayOfStat(String[] text) {
+        int supply = 0;
+        int buy = 0;
+        int result;
+        String[] strings;
+        for (String s : text) {
+            strings = s.split(SEPARATOR);
+            if (strings[0].equals(SUPPLY)) {
+                supply += Integer.valueOf(strings[1]);
+            } else if (strings[0].equals(BUY)) {
+                buy += Integer.valueOf(strings[1]);
+            }
+        }
+        result = supply - buy;
+        int[] arrayOfStat = new int[]{supply, buy, result};
+        return arrayOfStat;
+    }
+
+    private StringBuilder stringStat(int[] arrayOfStat) {
+        int supply = arrayOfStat[0];
+        int buy = arrayOfStat[1];
+        int result = arrayOfStat[2];
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("supply," + supply + System.lineSeparator())
+                .append("buy," + buy + System.lineSeparator())
+                .append("result," + result + System.lineSeparator());
         return stringBuilder;
     }
 
-    private void getWrittenFile(StringBuilder stringBuilder, String toFileName) {
-        File fileToWrite = new File(toFileName);
-        String[] stats = stringBuilder.toString().split(System.lineSeparator());
-        if (fileToWrite.exists()) {
-            fileToWrite.delete();
-        }
+    private boolean createFile(File toFileName) {
         try {
-            fileToWrite.createNewFile();
+            toFileName.createNewFile();
         } catch (IOException e) {
             throw new RuntimeException("Can't create this file", e);
         }
-        for (String stat : stats) {
-            try (BufferedWriter bufferedWriter =
-                         new BufferedWriter(new FileWriter(fileToWrite, true))) {
-                bufferedWriter.write(stat + System.lineSeparator());
-            } catch (IOException e) {
-                throw new RuntimeException("Can't write this file",e);
-            }
+        return true;
+    }
+
+    private void fileExistCheck(File toFileName) {
+        if (toFileName.exists()) {
+            toFileName.delete();
         }
     }
 
-    public void getStatistic(String fromFileName, String toFileName) {
-        StringBuilder stringBuilder = getFileStat(fromFileName);
-        getWrittenFile(stringBuilder,toFileName);
+    private void makeWrittenFile(StringBuilder stringBuilder, File toFileName) {
+        String[] stats = stringBuilder.toString().split(System.lineSeparator());
+        fileExistCheck(toFileName);
+        createFile(toFileName);
+        for (String stat : stats) {
+            try (BufferedWriter bufferedWriter =
+                         new BufferedWriter(new FileWriter(toFileName, true))) {
+                bufferedWriter.write(stat + System.lineSeparator());
+            } catch (IOException e) {
+                throw new RuntimeException("Can't write this file", e);
+            }
+        }
     }
 }
