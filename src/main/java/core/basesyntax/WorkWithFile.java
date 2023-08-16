66bd2b92b99330f84c9d2fc921c7b5ad57b4ee9f
Oleksandr Chernyonkov <cayman_raw@ukr.net>
2023-08-16 18:18:17 +0300

    fixed the mistakes and realize the method getStatistic() by other methods

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -9,13 +9,43 @@ import java.io.IOException;
 
 public class WorkWithFile {
     private static final String NEW_LINE = System.lineSeparator();
-    private static final String START_SUPPLY_STRING = "supply,";
-    private static final String START_BUY_STRING = "buy,";
-    private static final String START_RESULT_STRING = "result,";
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+    private static final String RESULT = "result";
+    private static final String COMMA = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        File fromFile = new File(fromFileName);
-        File toFile = new File(toFileName);
+        File fromFile = createFile(fromFileName);
+        File toFile = createFile(toFileName);
+        String[] strings = getReadLines(fromFile);
+        int supplyAmount = accountAmount(strings, SUPPLY);
+        int buyAmount = accountAmount(strings, BUY);
+        int resultAmount = supplyAmount - buyAmount;
+        String report = createReport(supplyAmount, buyAmount, resultAmount);
+        writeToFile(toFile, report);
+    }
+
+    private File createFile(String fileName) {
+        return new File(fileName);
+    }
+
+    private int accountAmount(String[] strings, String typeOfAmount) {
+        int amount = 0;
+        for (String line : strings) {
+            String[] typeAndAmount = line.split(COMMA);
+            if (typeAndAmount.length == 2 && typeOfAmount.equals(SUPPLY)
+                    && typeAndAmount[0].equals("supply")) {
+                amount += Integer.parseInt(typeAndAmount[1]);
+            }
+            if (typeAndAmount.length == 2 && typeOfAmount.equals(BUY)
+                    && typeAndAmount[0].equals("buy")) {
+                amount += Integer.parseInt(typeAndAmount[1]);
+            }
+        }
+        return amount;
+    }
+
+    private String[] getReadLines(File fromFile) {
         StringBuilder textFromFile = new StringBuilder();
         try (BufferedReader reader = new BufferedReader(new FileReader(fromFile))) {
             String line = reader.readLine();
@@ -24,25 +54,22 @@ public class WorkWithFile {
                 line = reader.readLine();
             }
         } catch (IOException e) {
-            throw new RuntimeException("You have an IOException", e);
+            throw new RuntimeException("Can`t read data from the file " + fromFile, e);
         }
-        String[] arrayOfLines = textFromFile.toString().split(NEW_LINE);
-        int supplyAmount = 0;
-        int buyAmount = 0;
-        for (String line : arrayOfLines) {
-            if (line.startsWith(START_SUPPLY_STRING)) {
-                supplyAmount += Integer.parseInt(line.substring(7));
-            }
-            if (line.startsWith(START_BUY_STRING)) {
-                buyAmount += Integer.parseInt(line.substring(4));
-            }
-        }
+        return textFromFile.toString().split(NEW_LINE);
+    }
+
+    private String createReport(int supplyAmount, int buyAmount, int resultAmount) {
+        return SUPPLY + COMMA + supplyAmount + NEW_LINE
+                + BUY + COMMA + buyAmount + NEW_LINE
+                + RESULT + COMMA + resultAmount;
+    }
+
+    private void writeToFile(File toFile, String report) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFile))) {
-            writer.write(START_SUPPLY_STRING + supplyAmount + NEW_LINE
-                    + START_BUY_STRING + buyAmount + NEW_LINE
-                    + START_RESULT_STRING + (supplyAmount - buyAmount));
+            writer.write(report);
         } catch (IOException e) {
-            throw new RuntimeException("You have the IOException", e);
+            throw new RuntimeException("Can`t write data to the file " + toFile, e);
         }
     }
 }
