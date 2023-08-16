07a2ba521bd35b1f2d511604f44165ec3b799297
Salnikov Markiian <salnikov.markiyan@gmail.com>
2023-08-16 14:58:16 +0300

    refactor code, correcting the indicated errors

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -3,7 +3,6 @@ package core.basesyntax;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -13,6 +12,8 @@ public class WorkWithFile {
     private static final String SUPLY = "supply";
     private static final String BUY = "buy";
     private static final String RESULT = "result";
+    private static final byte FIRST_ELEMENT_IN_ARRAY = 0;
+    private static final byte SECOND_ELEMENT_IN_ARRAY = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
         File fromFile = new File(fromFileName);
@@ -25,31 +26,31 @@ public class WorkWithFile {
     private String getStatisticFromText(String text) {
         int supplySum = 0;
         int buySum = 0;
-        String[] lines = text.split("\n");
+        String[] lines = text.split(System.lineSeparator());
         for (String line : lines) {
             String[] data = line.split(COMA);
-            if (data[0].equals(SUPLY)) {
-                supplySum += Integer.parseInt(data[1]);
+            if (data[FIRST_ELEMENT_IN_ARRAY].equals(SUPLY)) {
+                supplySum += Integer.parseInt(data[SECOND_ELEMENT_IN_ARRAY]);
                 continue;
             }
-            buySum += Integer.parseInt(data[1]);
+            buySum += Integer.parseInt(data[SECOND_ELEMENT_IN_ARRAY]);
         }
 
         return new StringBuilder().append(SUPLY).append(COMA)
                 .append(supplySum).append(System.lineSeparator()).append(BUY)
                 .append(COMA).append(buySum).append(System.lineSeparator())
-                .append(RESULT).append(COMA).append((supplySum - buySum)).toString();
+                .append(RESULT).append(COMA).append(supplySum - buySum).toString();
     }
 
     private String readInfo(File fromFile) {
-        BufferedReader reader;
-        try {
-            reader = new BufferedReader(new FileReader(fromFile));
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException("Can`t read " + fromFile, e);
-        }
         StringBuilder result = new StringBuilder();
-        reader.lines().forEach(s -> result.append(s).append("\n"));
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFile))) {
+            reader.lines().forEach(s -> result.append(s).append(System.lineSeparator()));
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read " + fromFile, e);
+        }
+
         return result.toString();
     }
 
@@ -57,8 +58,7 @@ public class WorkWithFile {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFile))) {
             writer.write(statistic);
         } catch (IOException e) {
-            throw new RuntimeException("Can`t write into " + toFile, e);
+            throw new RuntimeException("Can`t write into to file: " + toFile, e);
         }
     }
 }
-
