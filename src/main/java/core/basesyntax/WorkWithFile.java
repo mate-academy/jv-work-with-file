fffd062491a5c520bcad0e2f99834fc3d71d695a
evgenkolner <evgen.kolner@gmail.com>
2023-08-18 00:51:34 +0300

    added solution2

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,9 +7,13 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    private static final String BUY = "buy";
+    private static final String SUPPLY = "supply";
+    private static final String COMMA = ",";
+    private static final String RESULT = "result";
     public void getStatistic(String fromFileName, String toFileName) {
         String receivingFile = readFromFile(fromFileName);
-        String report = processedFile(receivingFile);
+        String report = calculateReport(receivingFile);
         writeToFile(report, toFileName);
     }
 
@@ -27,34 +31,34 @@ public class WorkWithFile {
         return builder.toString();
     }
 
-    private void writeToFile(String report, String toFileName) {
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
-            bufferedWriter.write(report);
-        } catch (IOException e) {
-            throw new RuntimeException("Can`t write to this file" + toFileName, e);
-        }
-    }
-
-    private String processedFile(String file) {
+    private String calculateReport(String fileContents) {
         StringBuilder builder1 = new StringBuilder();
-        String[] data = file.split(System.lineSeparator());
+        String[] data = fileContents.split(System.lineSeparator());
         int supply = 0;
         int buy = 0;
         for (String element : data) {
             String[] elements = element.split(",");
             String nameOperation = elements[0];
             int quantity = Integer.parseInt(elements[1]);
-            if (nameOperation.equals("buy")) {
+            if (nameOperation.equals(BUY)) {
                 buy += quantity;
             } else {
                 supply += quantity;
             }
         }
         int difference = supply - buy;
-        builder1.append("supply").append(",").append(supply).append(System.lineSeparator())
-                .append("buy").append(",").append(buy).append(System.lineSeparator())
-                .append("result").append(",").append(difference);
+        builder1.append(SUPPLY).append(COMMA).append(supply).append(System.lineSeparator())
+                .append(BUY).append(COMMA).append(buy).append(System.lineSeparator())
+                .append(RESULT).append(COMMA).append(difference);
         return builder1.toString();
+    }
+
+    private void writeToFile(String report, String toFileName) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+            bufferedWriter.write(report);
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t write to this file" + toFileName, e);
+        }
     }
 }
 
