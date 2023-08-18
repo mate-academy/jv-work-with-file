43b4c1a6f5cec3b13f3c76f4e82c0e8a22aad54f
Tetiana Denchyk <tanushka.den7@gmail.com>
2023-08-18 12:04:57 +0300

    fixed errors

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,7 +2,6 @@ package core.basesyntax;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -20,19 +19,18 @@ public class WorkWithFile {
     }
 
     private int[] readFile(String sourceFile) {
-        File file = new File(sourceFile);
         int supplyResult = 0;
         int buyResult = 0;
         int result = 0;
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(sourceFile))) {
             String value = reader.readLine();
             while (value != null) {
+                String amount = value.substring(value.indexOf(COMMA) + 1);
                 if (value.contains(SUPPLY)) {
-                    supplyResult += Integer.parseInt(value.substring(value.indexOf(COMMA) + 1));
-                }
-                if (value.contains(BUY)) {
-                    buyResult += Integer.parseInt(value.substring(value.indexOf(COMMA) + 1));
+                    supplyResult += Integer.parseInt(amount);
+                } else if (value.contains(BUY)) {
+                    buyResult += Integer.parseInt(amount);
                 }
                 result = supplyResult - buyResult;
                 value = reader.readLine();
@@ -44,16 +42,18 @@ public class WorkWithFile {
     }
 
     private String getResult(int[] data) {
+        int supplyResult = data[0];
+        int buyResult = data[1];
+        int result = data[2];
         StringBuilder builder = new StringBuilder();
-        builder.append(SUPPLY).append(COMMA).append(data[0]).append(System.lineSeparator())
-                .append(BUY).append(COMMA).append(data[1]).append(System.lineSeparator())
-                .append(RESULT).append(COMMA).append(data[2]);
+        builder.append(SUPPLY).append(COMMA).append(supplyResult).append(System.lineSeparator())
+                .append(BUY).append(COMMA).append(buyResult).append(System.lineSeparator())
+                .append(RESULT).append(COMMA).append(result);
         return builder.toString();
     }
 
     private void writeToFile(String data, String resultFile) {
-        File newFile = new File(resultFile);
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(newFile))) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(resultFile))) {
             bufferedWriter.write(data);
         } catch (IOException e) {
             throw new RuntimeException("Can't write into the file" + resultFile, e);
