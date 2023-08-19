cf6d0a9af2fecbac11f4a1620183da7fbf59cca3
Yana Bilyk <bilyk.yn@gmail.com>
2023-08-18 20:50:13 -0400

    created reader

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,19 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
+        StringBuilder stringBuilder = new StringBuilder();
+        File file = new File(fromFileName);
+       try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))){
 
+
+       }catch (IOException e){
+        throw new RuntimeException("Can't read the file", e);
+       }
     }
 }
