b642f1615a106fbd83c4fb1c35e31b4a78e2fc41
Roman Yuzviak <3romanyuzviak@gmail.com>
2023-08-20 00:10:07 +0300

    fixed

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,55 +2,55 @@ package core.basesyntax;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
 
-    private int supply;
-    private int buy;
-    private boolean isParsed = false;
-
-    private int getResult() {
-        return supply - buy;
-    }
+    private static final int SUPPLY_INDEX = 0;
+    private static final int BUY_INDEX = 1;
+    private static final int NAME_COLUMN = 0;
+    private static final int VALUE_COLUMN = 1;
+    private static final String SEPARATOR = ",";
+    private static final String BUY_WORD = "buy";
+    private static final String SUPPLY_WORD = "supply";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        if (!isParsed) {
-            parse(fromFileName);
-            isParsed = true;
-        }
-        makeReport(toFileName);
+
+        int[] data = parseAndAggregate(fromFileName);
+        makeReport(toFileName, data);
     }
 
-    public void parse(String fileName) {
-        File fromFile = new File(fileName);
-        try (BufferedReader reader = new BufferedReader(new FileReader(fromFile))) {
+    public int[] parseAndAggregate(String fileName) {
+        int[] results = new int[2];
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             String value = reader.readLine();
             String[] values;
             while (value != null) {
-                values = value.split(",");
-                if (values[0].equals("supply")) {
-                    supply += Integer.parseInt(values[1]);
+                values = value.split(SEPARATOR);
+                if (values[NAME_COLUMN].equals(SUPPLY_WORD)) {
+                    results[SUPPLY_INDEX] += Integer.parseInt(values[VALUE_COLUMN]);
                 }
-                if (values[0].equals("buy")) {
-                    buy += Integer.parseInt(values[1]);
+                if (values[NAME_COLUMN].equals(BUY_WORD)) {
+                    results[BUY_INDEX] += Integer.parseInt(values[VALUE_COLUMN]);
                 }
                 value = reader.readLine();
             }
         } catch (IOException e) {
             throw new RuntimeException("Can`t read file", e);
         }
+
+        return results;
     }
 
-    public void makeReport(String fileName) {
-        File fromFile = new File(fileName);
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fromFile))) {
-            bufferedWriter.write(makeLine("supply", supply));
-            bufferedWriter.write(makeLine("buy", buy));
-            bufferedWriter.write(makeLine("result", getResult()));
+    public void makeReport(String fileName, int[] data) {
+
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName))) {
+            bufferedWriter.write(makeLine("supply", data[SUPPLY_INDEX]));
+            bufferedWriter.write(makeLine("buy", data[BUY_INDEX]));
+            bufferedWriter.write(makeLine("result", data[SUPPLY_INDEX] - data[BUY_INDEX]));
         } catch (IOException e) {
             throw new RuntimeException("Can`t write to file", e);
         }
@@ -60,4 +60,3 @@ public class WorkWithFile {
         return name + "," + value + System.lineSeparator();
     }
 }
-
