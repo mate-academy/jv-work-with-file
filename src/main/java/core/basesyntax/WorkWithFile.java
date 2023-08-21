deb0a55d28ee091ff492cfb627953fc923e19222
Roman Yuzviak <3romanyuzviak@gmail.com>
2023-08-21 13:51:12 +0300

    fixed2

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -13,50 +13,61 @@ public class WorkWithFile {
     private static final int NAME_COLUMN = 0;
     private static final int VALUE_COLUMN = 1;
     private static final String SEPARATOR = ",";
+    private static final String HELPER_SEPARATOR = " ";
     private static final String BUY_WORD = "buy";
     private static final String SUPPLY_WORD = "supply";
 
+
     public void getStatistic(String fromFileName, String toFileName) {
 
-        int[] data = parseAndAggregate(fromFileName);
-        makeReport(toFileName, data);
+        String[] data = readFromFile(fromFileName);
+        int[] results = calculateReport(data);
+        makeReport(toFileName, results);
     }
 
-    public int[] parseAndAggregate(String fileName) {
+    private String[] readFromFile(String fileName) {
         int[] results = new int[2];
+        StringBuilder output = new StringBuilder();
 
         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
-            String value = reader.readLine();
-            String[] values;
-            while (value != null) {
-                values = value.split(SEPARATOR);
-                if (values[NAME_COLUMN].equals(SUPPLY_WORD)) {
-                    results[SUPPLY_INDEX] += Integer.parseInt(values[VALUE_COLUMN]);
-                }
-                if (values[NAME_COLUMN].equals(BUY_WORD)) {
-                    results[BUY_INDEX] += Integer.parseInt(values[VALUE_COLUMN]);
-                }
-                value = reader.readLine();
+            String line = reader.readLine();
+            while (line != null) {
+                output.append(line).append(" ");
+                line = reader.readLine();
             }
         } catch (IOException e) {
-            throw new RuntimeException("Can`t read file", e);
+            throw new RuntimeException("Can`t read file: " + fileName, e);
         }
 
+        return output.toString().split(HELPER_SEPARATOR);
+    }
+
+    private int[] calculateReport(String[] input) {
+        int[] results = new int[2];
+        String[] values;
+        for (String line : input) {
+            values = line.split(SEPARATOR);
+            if (values[NAME_COLUMN].equals(SUPPLY_WORD)) {
+                results[SUPPLY_INDEX] += Integer.parseInt(values[VALUE_COLUMN]);
+            }
+            if (values[NAME_COLUMN].equals(BUY_WORD)) {
+                results[BUY_INDEX] += Integer.parseInt(values[VALUE_COLUMN]);
+            }
+        }
         return results;
     }
 
-    public void makeReport(String fileName, int[] data) {
-
+    private void makeReport(String fileName, int[] data) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName))) {
             bufferedWriter.write(makeLine("supply", data[SUPPLY_INDEX]));
             bufferedWriter.write(makeLine("buy", data[BUY_INDEX]));
             bufferedWriter.write(makeLine("result", data[SUPPLY_INDEX] - data[BUY_INDEX]));
         } catch (IOException e) {
-            throw new RuntimeException("Can`t write to file", e);
+            throw new RuntimeException("Can`t write to file: " + fileName, e);
         }
     }
 
-    public String makeLine(String name, int value) {
-        return name + "," + value + System.lineSeparator();
+    private String makeLine(String name, int value) {
+        return name + SEPARATOR + value + System.lineSeparator();
     }
 }
