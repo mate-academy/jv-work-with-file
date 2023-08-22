adec22f17ab1e19804c626dabbb278611bdfff3e
Ihor Trokhymchuk <hameleon009@gmail.com>
2023-08-22 19:23:18 +0300

    fixed code after review

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,27 +8,33 @@ import java.io.IOException;
 
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
-        String readString = readFile(fromFileName);
-        int[] sums = createReport(readString);
-        String statistic = generateStatistic(sums[0], sums[1]);
+        final String readString = readFile(fromFileName);
+        final String statistic = createReport(readString);
         writeToFile(toFileName, statistic);
     }
 
-    private int[] createReport(String readString) {
-        if (!readString.isEmpty()) {
-            int[] sums = new int[2];
+    private String createReport(String readString) {
+        int supplySum = 0;
+        int buySum = 0;
 
-            String[] stringInput = readString.split("\\W+");
-            for (int i = 0; i < stringInput.length / 2; i++) {
-                if (stringInput[i * 2].equals("supply")) {
-                    sums[0] += Integer.parseInt(stringInput[i * 2 + 1]);
-                } else if (stringInput[i * 2].equals("buy")) {
-                    sums[1] += Integer.parseInt(stringInput[i * 2 + 1]);
-                }
+        String[] stringInput = readString.split("\\W+");
+        for (int i = 0; i < stringInput.length / 2; i++) {
+            if (stringInput[i * 2].equals("supply")) {
+                supplySum += Integer.parseInt(stringInput[i * 2 + 1]);
+            } else if (stringInput[i * 2].equals("buy")) {
+                buySum += Integer.parseInt(stringInput[i * 2 + 1]);
             }
-            return sums;
         }
-        return new int[0];
+
+        StringBuilder generatedStatistic = new StringBuilder();
+        generatedStatistic.append("supply,")
+                .append(supplySum)
+                .append("\nbuy,")
+                .append(buySum)
+                .append("\nresult,")
+                .append(supplySum - buySum);
+
+        return generatedStatistic.toString();
     }
 
     private String readFile(String fromFileName) {
@@ -44,17 +50,6 @@ public class WorkWithFile {
             throw new RuntimeException("cant read file", e);
         }
         return stringBuilder.toString();
-    }
-
-    private String generateStatistic(int supplySum, int buySum) {
-        StringBuilder generatedStatistic = new StringBuilder();
-        generatedStatistic.append("supply,")
-                .append(supplySum)
-                .append("\nbuy,")
-                .append(buySum)
-                .append("\nresult,")
-                .append(supplySum - buySum);
-        return generatedStatistic.toString();
     }
 
     private void writeToFile(String toFileName, String result) {
