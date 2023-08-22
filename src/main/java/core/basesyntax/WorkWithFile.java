572f653f503620cce32b6d8bdffc05156466fda2
Ihor Trokhymchuk <hameleon009@gmail.com>
2023-08-22 17:53:39 +0300

    fixed code after review

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,44 +8,55 @@ import java.io.IOException;
 
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
-        StringBuilder stringBuilder = new StringBuilder();
-        int count = 0;
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                stringBuilder.append(line).append(" ");
-                count++;
-            }
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException("file not found", e);
-        } catch (IOException e) {
-            throw new RuntimeException("cant read file", e);
-        }
-
-        if (stringBuilder.length() != 0) {
+        String readString = readFile(fromFileName);
+        if (!readString.isEmpty()) {
             int supplySum = 0;
             int buySum = 0;
 
-            String[] stringInput = stringBuilder.toString().split("\\W+");
-            for (int i = 0; i < count; i++) {
+            String[] stringInput = readString.split("\\W+");
+            for (int i = 0; i < stringInput.length / 2; i++) {
                 if (stringInput[i * 2].equals("supply")) {
                     supplySum += Integer.parseInt(stringInput[i * 2 + 1]);
                 } else if (stringInput[i * 2].equals("buy")) {
                     buySum += Integer.parseInt(stringInput[i * 2 + 1]);
                 }
             }
-            StringBuilder result = new StringBuilder();
-            result.append("supply,")
-                    .append(supplySum)
-                    .append("\nbuy,")
-                    .append(buySum)
-                    .append("\nresult,")
-                    .append(supplySum - buySum);
-            try (FileWriter fileWriter = new FileWriter(toFileName)) {
-                fileWriter.write(String.valueOf(result));
-            } catch (IOException e) {
-                throw new RuntimeException("cant write to file", e);
+            String statistic = generateStatistic(supplySum, buySum);
+            writeToFile(toFileName, statistic);
+        }
+    }
+
+    private String readFile(String fromFileName) {
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuilder.append(line).append(" ");
             }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("file not found", e);
+        } catch (IOException e) {
+            throw new RuntimeException("cant read file", e);
+        }
+        return stringBuilder.toString();
+    }
+
+    private String generateStatistic(int supplySum, int buySum) {
+        StringBuilder generatedStatistic = new StringBuilder();
+        generatedStatistic.append("supply,")
+                .append(supplySum)
+                .append("\nbuy,")
+                .append(buySum)
+                .append("\nresult,")
+                .append(supplySum - buySum);
+        return generatedStatistic.toString();
+    }
+
+    private void writeToFile(String toFileName, String result) {
+        try (FileWriter fileWriter = new FileWriter(toFileName)) {
+            fileWriter.write(String.valueOf(result));
+        } catch (IOException e) {
+            throw new RuntimeException("cant write to file", e);
         }
     }
 }
