1344b281e3458b3740c2f6ba3d5affc2f2352ede
mister.Sokor <mister.sokor@gmail.com>
2023-08-24 23:05:09 -0500

    methods has been divided into smaller methods, created constants for 'supply' and 'buy'

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -9,6 +9,9 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class WorkWithFile {
+    private static final String OPERATION_SUPPLY = "supply";
+    private static final String OPERATION_BUY = "buy";
+
     public void getStatistic(String fromFileName, String toFileName) {
         try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
                 BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
@@ -21,48 +24,50 @@ public class WorkWithFile {
                 String[] data = line.split(",");
                 String operationType = data[0];
                 int amount = Integer.parseInt(data[1]);
-                if ("supply".equals(operationType)) {
-                    supplyData.put(operationType,
-                            supplyData.getOrDefault(operationType, 0) + amount);
-                } else if ("buy".equals(operationType)) {
-                    buyData.put(operationType, buyData.getOrDefault(operationType, 0) + amount);
-                }
+                updateOperationData(operationType, amount, supplyData, buyData);
             }
 
-            int supplyResult = 0;
-            for (Map.Entry<String, Integer> unit : supplyData.entrySet()) {
-                supplyResult = unit.getValue() - supplyResult;
-            }
+            int supplyResult = calculateTotal(supplyData);
+            int buyResult = calculateTotal(buyData);
 
-            int buyResult = 0;
-            for (Map.Entry<String, Integer> unit : buyData.entrySet()) {
-                buyResult = unit.getValue() - buyResult;
-            }
+            writeDataToOutput(supplyData, writer);
+            writeDataToOutput(buyData, writer);
 
-            StringBuilder resultBuilder = new StringBuilder();
-            for (Map.Entry<String, Integer> entry : supplyData.entrySet()) {
-                int sum = entry.getValue();
-                String operationType = entry.getKey();
-                resultBuilder.append(operationType);
-                resultBuilder.append(",").append(sum).append(System.lineSeparator());
-            }
-
-            for (Map.Entry<String, Integer> entry : buyData.entrySet()) {
-                int sum = entry.getValue();
-                String operationType = entry.getKey();
-                resultBuilder.append(operationType);
-                resultBuilder.append(",").append(sum).append(System.lineSeparator());
-            }
-
-            resultBuilder.append("result,")
-                    .append(supplyResult - buyResult).append(System.lineSeparator());
-            writer.write(resultBuilder.toString());
-            System.out.println(resultBuilder);
+            writer.write("result," + (supplyResult - buyResult) + System.lineSeparator());
+            System.out.println("Report generated successfully.");
 
         } catch (IOException e) {
-            throw new RuntimeException("Error while reading/writing the file: "
-                    + e.getMessage());
+            throw new RuntimeException("Error while working with the file: "
+                                                                        + e.getMessage(), e);
+        }
+    }
+
+    private void updateOperationData(String operationType,
+                                     int amount, Map<String,
+                                     Integer> supplyData,
+                                     Map<String,
+                                     Integer> buyData) {
+        if (OPERATION_SUPPLY.equals(operationType)) {
+            supplyData.put(operationType, supplyData.getOrDefault(operationType, 0) + amount);
+        } else if (OPERATION_BUY.equals(operationType)) {
+            buyData.put(operationType, buyData.getOrDefault(operationType, 0) + amount);
+        }
+    }
+
+    private int calculateTotal(Map<String, Integer> data) {
+        int total = 0;
+        for (Map.Entry<String, Integer> entry : data.entrySet()) {
+            total += entry.getValue();
+        }
+        return total;
+    }
+
+    private void writeDataToOutput(Map<String, Integer> data, BufferedWriter writer)
+                                                              throws IOException {
+        for (Map.Entry<String, Integer> entry : data.entrySet()) {
+            String operationType = entry.getKey();
+            int sum = entry.getValue();
+            writer.write(operationType + "," + sum + System.lineSeparator());
         }
     }
 }
-
