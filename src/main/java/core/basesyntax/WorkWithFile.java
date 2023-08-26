0a3f0bbe1b1b5e403e916387b164bc9ad061c016
Stanislav Viner <stanislav.viner@gmail.com>
2023-08-26 10:33:56 +0300

    Added jv-work-with-file solution

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,58 @@
 package core.basesyntax;
 
-public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
+public class WorkWithFile {
+
+    private static final int ARRAY_INDEX = 1;
+    private static final String COMMA = ",";
+    private static final String BUY = "buy";
+    private static final String SUPPLY = "supply";
+    private static final String RESULT = "result";
+    private static final String LINE_SEPARATOR = System.lineSeparator();
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        String data = readDataFromFile(fromFileName);
+        String report = createReport(data);
+        writeReportToFile(toFileName, report);
+    }
+
+    private String readDataFromFile(String fileName) {
+        try {
+            return Files.readString(Path.of(fileName));
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read data from file " + fileName, e);
+        }
+    }
+
+    private String createReport(String dataFromFile) {
+        int totalBuy = 0;
+        int totalSupply = 0;
+        StringBuilder result = new StringBuilder();
+        String[] dataFile = dataFromFile.split(LINE_SEPARATOR);
+
+        for (String datum : dataFile) {
+            String[] data = datum.split(COMMA);
+
+            if (datum.startsWith(BUY)) {
+                totalBuy += Integer.parseInt(data[ARRAY_INDEX]);
+            } else {
+                totalSupply += Integer.parseInt(data[ARRAY_INDEX]);
+            }
+        }
+
+        return result.append(SUPPLY).append(COMMA).append(totalSupply).append(LINE_SEPARATOR)
+                     .append(BUY).append(COMMA).append(totalBuy).append(LINE_SEPARATOR)
+                     .append(RESULT).append(COMMA).append(totalSupply - totalBuy).toString();
+    }
+
+    private void writeReportToFile(String toFile, String report) {
+        try {
+            Files.writeString(Path.of(toFile), report);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write data to file " + toFile, e);
+        }
     }
 }
