81ad3e897af818c2a59aa84511579e9fa40f91aa
Lev Shtana <120460187+CapybaraFrog@users.noreply.github.com>
2023-08-29 20:30:32 +0300

    reworked the code based on the comment

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,7 +2,6 @@ package core.basesyntax;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -14,40 +13,48 @@ public class WorkWithFile {
     private static final String COMA = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        File read = new File(fromFileName);
+        String data = readFromFile(fromFileName);
+        String report = createReport(data);
+        writeToFile(report, toFileName);
+    }
+
+    private String readFromFile(String read) {
+        StringBuilder temporary = new StringBuilder();
         try (BufferedReader reader = new BufferedReader(new FileReader(read))) {
-            StringBuilder temporary = new StringBuilder();
             String value = reader.readLine();
             while (value != null) {
                 temporary.append(value).append(" ");
                 value = reader.readLine();
             }
-            String[] lines = temporary.toString().split(" ");
-            int supply = 0;
-            int buy = 0;
-            for (String line : lines) {
-                String[] operation = line.split(COMA);
-                if (operation[OPERATION_INDEX].equals(SUPPLY)) {
-                    supply += Integer.parseInt(operation[AMOUNT_INDEX]);
-                } else {
-                    buy += Integer.parseInt(operation[AMOUNT_INDEX]);
-                }
-            }
-            createReport(supply, buy, toFileName);
         } catch (IOException e) {
-            throw new RuntimeException("Can't read file " + fromFileName, e);
+            throw new RuntimeException("Can't read file " + read, e);
         }
+        return temporary.toString();
     }
 
-    private void createReport(int supply, int buy, String toFileName) {
+    private String createReport(String data) {
         StringBuilder stringBuilder1 = new StringBuilder();
-        File report = new File(toFileName);
+        String[] lines = data.split(" ");
+        int supply = 0;
+        int buy = 0;
+        for (String line : lines) {
+            String[] operation = line.split(COMA);
+            if (operation[OPERATION_INDEX].equals(SUPPLY)) {
+                supply += Integer.parseInt(operation[AMOUNT_INDEX]);
+            } else {
+                buy += Integer.parseInt(operation[AMOUNT_INDEX]);
+            }
+        }
         stringBuilder1
                 .append("supply,").append(supply).append(System.lineSeparator())
                 .append("buy,").append(buy).append(System.lineSeparator())
                 .append("result,").append(supply - buy).append(System.lineSeparator());
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(report))) {
-            writer.write(stringBuilder1.toString());
+        return stringBuilder1.toString();
+    }
+
+    private void writeToFile(String report, String toFileName) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
+            writer.write(report);
         } catch (IOException e) {
             throw new RuntimeException("Can't write data to the file " + toFileName, e);
         }
