9fc3a4230084a06fab8052e213f2d8f31d14c761
Roman K <romazan07sq@gmail.com>
2023-09-01 14:45:41 +0300

    add informative message to exceptions, do not create and delete a new file in getStatistic method, use try with resources and others mistakes

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,7 +2,6 @@ package core.basesyntax;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -10,8 +9,6 @@ import java.io.IOException;
 
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
-        File file = new File(toFileName);
-        file.delete();
         String readFromFileString = readFromFile(fromFileName);
         String[] makeReportArray = makeReport(readFromFileString);
         writeToFile(makeReportArray, toFileName);
@@ -19,15 +16,14 @@ public class WorkWithFile {
 
     private String readFromFile(String fromFileName) {
         StringBuilder stringBuilder = new StringBuilder();
-        try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName));
-            int value = bufferedReader.read();
-            while (value != -1) {
-                stringBuilder.append((char) value);
-                value = bufferedReader.read();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
+            String value = bufferedReader.readLine();
+            while (value != null) {
+                stringBuilder.append(value).append(" ");
+                value = bufferedReader.readLine();
             }
         } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Can't read data from " + fromFileName, e);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -37,8 +33,7 @@ public class WorkWithFile {
     private String[] makeReport(String input) {
         int supply = 0;
         int buy = 0;
-        int result = 0;
-        String[] inputArray = input.split("\n");
+        String[] inputArray = input.split(" ");
         for (int i = 0; i < inputArray.length; i++) {
             String[] splitWordAndDigit = inputArray[i].split(",");
             for (int j = 0; j < splitWordAndDigit.length; j++) {
@@ -51,7 +46,7 @@ public class WorkWithFile {
                 }
             }
         }
-        result = supply - buy;
+        int result = supply - buy;
         StringBuilder stringBuilderResult = new StringBuilder();
         stringBuilderResult.append("supply,").append(supply)
                 .append(" buy,").append(buy).append(" result,").append(result);
@@ -61,20 +56,13 @@ public class WorkWithFile {
     }
 
     private void writeToFile(String[] inputDate, String toFileName) {
-        File file = new File(toFileName);
-        try {
-            file.createNewFile();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        for (String word : inputDate) {
-            try (BufferedWriter bufferedWriter =
-                         new BufferedWriter(new FileWriter(toFileName, true))) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+            for (String word : inputDate) {
                 bufferedWriter.write(word);
                 bufferedWriter.write("\n");
-            } catch (IOException e) {
-                throw new RuntimeException("Can't write data to file", e);
             }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write data to file", e);
         }
     }
 }
