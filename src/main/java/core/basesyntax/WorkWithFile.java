1b57c85ccea83fc825099878e7dd063bde921dee
Petro Smoliar <smp22pl@gmail.com>
2023-09-01 09:18:55 +0200

    Improve code readability

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -5,41 +5,60 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WorkWithFile {
     private static final int INDEX_SUPPLY = 0;
     private static final int INDEX_BUY = 1;
     private static final int INDEX_OPERATION = 0;
     private static final int INDEX_QUANTITY = 1;
+    private static final String DELIMITER = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        int[] data = calculateSupplyAndBuy(fromFileName);
-        int result = data[INDEX_SUPPLY] - data[INDEX_BUY];
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
-            writer.write("supply," + data[INDEX_SUPPLY] + System.lineSeparator()
-                    + "buy," + data[INDEX_BUY] + System.lineSeparator()
-                    + "result," + result);
+        String[] lines = readingFile(fromFileName);
+        int[] calculate = calculateSupplyAndBuy(lines);
+        writingFile(toFileName, calculate[INDEX_SUPPLY], calculate[INDEX_BUY]);
+    }
+
+    private String[] readingFile(String fromFileName) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
+            String line = reader.readLine();
+            List<String> lines = new ArrayList<>();
+            while (line != null) {
+                lines.add(line);
+                line = reader.readLine();
+            }
+            return lines.toArray(new String[0]);
         } catch (IOException e) {
-            throw new RuntimeException("Can't write file", e);
+            throw new RuntimeException(e);
         }
     }
 
-    private int[] calculateSupplyAndBuy(String fromFileName) {
+    private int[] calculateSupplyAndBuy(String[] lines) {
         int supply = 0;
         int buy = 0;
-        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
-            String line = reader.readLine();
-            while (line != null) {
-                if (line.split(",")[INDEX_OPERATION].equals("supply")) {
-                    supply += Integer.parseInt(line.split(",")[INDEX_QUANTITY]);
-                } else {
-                    buy += Integer.parseInt(line.split(",")[INDEX_QUANTITY]);
-                }
-                line = reader.readLine();
+        for (String line : lines) {
+            String[] operationDate = line.split(DELIMITER);
+            String operation = operationDate[INDEX_OPERATION];
+            int quantity = Integer.parseInt(operationDate[INDEX_QUANTITY]);
+            if (operation.equals("supply")) {
+                supply += quantity;
+            } else {
+                buy += quantity;
             }
-            return new int[]{supply, buy};
+        }
+        return new int[]{supply, buy};
+    }
+
+    private void writingFile(String toFileName, int supply, int buy) {
+        int result = supply - buy;
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
+            writer.write("supply," + supply + System.lineSeparator()
+                    + "buy," + buy + System.lineSeparator()
+                    + "result," + result);
         } catch (IOException e) {
-            throw new RuntimeException("Can't read file", e);
+            throw new RuntimeException("Can't write file", e);
         }
     }
 }
