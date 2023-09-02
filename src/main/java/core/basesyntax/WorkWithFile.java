a5116112d65fe0cb425ee8810229ec047cb3cc38
Egor Reshetnik <mwm3335@gmail.com>
2023-09-02 12:21:57 +0300

    Divided program logic,add 3 methods

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -13,37 +13,52 @@ public class WorkWithFile {
     private static final String SEPARATOR = System.lineSeparator();
 
     public static void getStatistic(String fromFileName, String toFileName) {
-        File fileFrom = new File(fromFileName);
-        File fileTo = new File(toFileName);
+        WorkWithFile workWithFile = new WorkWithFile();
+        String[] splitLines = workWithFile.readLines(fromFileName);
+        String calculatedReport = workWithFile.calculateArray(splitLines);
+        workWithFile.writeToFile(toFileName, calculatedReport);
+    }
+
+    public String[] readLines(String filePath) {
+        File file = new File(filePath);
         StringBuilder stringBuilder = new StringBuilder();
         String[] splitLines;
-        int supply = 0;
-        int buy = 0;
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileFrom))) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
             String line = bufferedReader.readLine();
             while (line != null) {
                 stringBuilder.append(line + SPACE);
                 line = bufferedReader.readLine();
             }
             splitLines = stringBuilder.toString().split(SIGN);
-            for (int i = 0; i < splitLines.length; i++) {
-                if (splitLines[i].equals("supply")) {
-                    supply += Integer.parseInt(splitLines[i + 1]);
-                    i++;
-                } else {
-                    buy += Integer.parseInt(splitLines[i + 1]);
-                    i++;
-                }
-            }
+            return splitLines;
         } catch (IOException e) {
             throw new RuntimeException("Cant read file", e);
         }
+    }
+
+    public String calculateArray(String[] splitLines) {
+        int supply = 0;
+        int buy = 0;
+        for (int i = 0; i < splitLines.length; i++) {
+            if (splitLines[i].equals("supply")) {
+                supply += Integer.parseInt(splitLines[i + 1]);
+                i++;
+            } else {
+                buy += Integer.parseInt(splitLines[i + 1]);
+                i++;
+            }
+        }
         int result = supply - buy;
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileTo))) {
-            String report = "supply," + supply + SEPARATOR
-                    + "buy," + buy + SEPARATOR
-                    + "result," + result;
-            bufferedWriter.write(report);
+        String report = "supply," + supply + SEPARATOR
+                + "buy," + buy + SEPARATOR
+                + "result," + result;
+        return report;
+    }
+
+    public void writeToFile(String filePath, String message) {
+        File file = new File(filePath);
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
+            bufferedWriter.write(message);
         } catch (IOException e) {
             throw new RuntimeException("Cant write data to file", e);
         }
