d43103cfb202b917176d60ee7372b4e05f399d78
breakingtumble <yaroslavglavatsky@gmail.com>
2023-09-03 18:54:00 +0300

    implemented a solution

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,48 @@
 package core.basesyntax;
 
-public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
 
+public class WorkWithFile {
+    private static final int FIRST_COLUMN_INDEX = 0;
+    private static final int SECOND_COLUMN_INDEX = 1;
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
+            String value = bufferedReader.readLine();
+            int buySum = 0;
+            int supplySum = 0;
+            while (value != null) {
+                String[] splitData = value.split(",");
+                if (splitData[FIRST_COLUMN_INDEX].equals("buy")) {
+                    buySum += Integer.parseInt(splitData[SECOND_COLUMN_INDEX]);
+                } else if (splitData[FIRST_COLUMN_INDEX].equals("supply")) {
+                    supplySum += Integer.parseInt(splitData[SECOND_COLUMN_INDEX]);
+                }
+                value = bufferedReader.readLine();
+            }
+            writeToFile(toFileName, buySum, supplySum);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't access the data", e);
+        }
+    }
+
+    private void writeToFile(String toFileName, int buySum, int supplySum) {
+        File fileToWrite = new File(toFileName);
+        try {
+            Files.write(fileToWrite.toPath(), createReport(buySum, supplySum).getBytes());
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write data to file", e);
+        }
+    }
+
+    private String createReport(int buySum, int supplySum) {
+        return
+                String.format("supply,%d" + System.lineSeparator()
+                        + "buy,%d" + System.lineSeparator()
+                        + "result,%d", supplySum, buySum, supplySum - buySum);
     }
 }
