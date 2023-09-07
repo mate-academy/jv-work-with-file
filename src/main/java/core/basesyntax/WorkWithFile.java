bb78c7805b24b78663e2c0ff74e997262650a9a0
Khrystyna Didukh <khrystynadi29@gmail.com>
2023-09-07 10:53:13 +0300

    divided to three methods

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,46 +7,58 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
-        String data;
+    static final int ZERO = 0;
+    static final int ONE = 1;
+
+    public String readFile(String fromFileName) {
+        StringBuilder builder = new StringBuilder();
+        String text;
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
+            while ((text = reader.readLine()) != null) {
+                builder.append(text).append(System.lineSeparator());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return builder.toString();
+    }
+
+    public String createReport(String data) {
         int supplyCount = 0;
         int buyCount = 0;
         int result = 0;
-
-        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
-            data = reader.readLine();
-            while (data != null) {
-                String[] part = data.split(",");
-                if (part[0].startsWith("supply")) {
-                    supplyCount += Integer.parseInt(part[1]);
-                } else if (part[0].startsWith("buy")) {
-                    buyCount += Integer.parseInt(part[1]);
-                }
-                result = supplyCount - buyCount;
-                data = reader.readLine();
+        String[] lines = data.split(System.lineSeparator());
+        for (String line : lines) {
+            String[] part = line.split(",");
+            if (part[ZERO].startsWith("supply")) {
+                supplyCount += Integer.parseInt(part[ONE]);
+            } else if (part[ZERO].startsWith("buy")) {
+                buyCount += Integer.parseInt(part[ONE]);
             }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+            result = supplyCount - buyCount;
         }
 
-        BufferedWriter writer = null;
-        try {
-            writer = new BufferedWriter(new FileWriter(toFileName));
-            StringBuilder builder = new StringBuilder();
-            writer.write("supply," + supplyCount + System.lineSeparator()
-                    + "buy," + buyCount + System.lineSeparator()
-                    + "result," + result + System.lineSeparator());
+        StringBuilder builder = new StringBuilder();
+        builder.append("supply," + supplyCount + System.lineSeparator()
+                + "buy," + buyCount + System.lineSeparator()
+                + "result," + result + System.lineSeparator());
+        return builder.toString();
+    }
+
+    public void writeReportToFile(String fileData, String toFileName) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
+            writer.write(fileData);
             writer.flush();
+            writer.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
-        } finally {
-            if (writer != null) {
-                try {
-                    writer.close();
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
         }
     }
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        String info = readFile(fromFileName);
+        String report = createReport(info);
+        writeReportToFile(report, toFileName);
+    }
+
 }
