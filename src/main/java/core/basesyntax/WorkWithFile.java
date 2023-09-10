fad6e066fe5e3e5ec9c136902f8785b7dbee7dca
LeonIllya <dd0733567@gmail.com>
2023-09-10 14:08:29 +0300

    first commit

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -15,63 +15,63 @@ public class WorkWithFile {
         writeToFail(toFileName, calculateSolution);
     }
 
-        private String[] readLine(String FilePath) {
-            File file = new File(FilePath);
-            StringBuilder stringBuilder = new StringBuilder();
-            String[] splitArray;
+    private String[] readLine(String FilePath) {
+        File file = new File(FilePath);
+        StringBuilder stringBuilder = new StringBuilder();
+        String[] splitArray;
 
-            try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-                String value = reader.readLine();
-                while (value != null) {
-                    stringBuilder.append(value).append(SPASE);
-                    value = reader.readLine();
-                }
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String value = reader.readLine();
+            while (value != null) {
+                stringBuilder.append(value).append(SPASE);
+                value = reader.readLine();
+            }
 
-                splitArray = stringBuilder.toString().split(SIGN);
-                return splitArray;
+            splitArray = stringBuilder.toString().split(SIGN);
+            return splitArray;
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t read file", e);
+        }
+    }
+
+
+    private String calculateStatistic(String[] splitArray) {
+        int sumSupply = 0;
+        int sumBuy = 0;
+
+        for (int i = 0; i < splitArray.length; i++) {
+            if (splitArray[i].equals("supply")) {
+                sumSupply += Integer.parseInt(splitArray[i + 1]);
+                i++;
+            } else {
+                sumBuy += Integer.parseInt(splitArray[i + 1]);
+                i++;
+            }
+        }
+
+        int calculation = sumSupply - sumBuy;
+
+        String result = "supply," + sumSupply + SEPARATOR +
+            "buy," + sumBuy + SEPARATOR +
+            "result," + calculation;
+
+        return result;
+    }
+
+    private void writeToFail(String toFilePath, String result) {
+        File file = new File(toFilePath);
+        BufferedWriter writer = null;
+        try  {
+            writer = new BufferedWriter(new FileWriter(file));
+            writer.write(result);
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t read file", e);
+        } finally {
+            try {
+                writer.close();
             } catch (IOException e) {
-                throw new RuntimeException("Can`t read file", e);
+                throw new RuntimeException(e);
             }
         }
-
-
-        private String calculateStatistic(String[] splitArray) {
-            int sumSupply = 0;
-            int sumBuy = 0;
-
-            for (int i = 0; i < splitArray.length; i++) {
-                if (splitArray[i].equals("supply")) {
-                    sumSupply += Integer.parseInt(splitArray[i + 1]);
-                    i++;
-                } else {
-                    sumBuy += Integer.parseInt(splitArray[i + 1]);
-                    i++;
-                }
-            }
-
-            int calculation = sumSupply - sumBuy;
-
-            String result = "supply," + sumSupply + SEPARATOR +
-                "buy," + sumBuy + SEPARATOR +
-                "result," + calculation;
-
-            return result;
-        }
-
-        private void writeToFail(String toFilePath, String result) {
-            File file = new File(toFilePath);
-            BufferedWriter writer = null;
-            try  {
-                writer = new BufferedWriter(new FileWriter(file));
-                writer.write(result);
-            } catch (IOException e) {
-                throw new RuntimeException("Can`t read file", e);
-            } finally {
-                try {
-                    writer.close();
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        }
+    }
 }
