5fa8bcac112adae4cf0e8d7b13fc6f284f8e76f6
Ivan Kusurhashev <ivankusur@gmail.com>
2023-09-11 16:32:29 +0300

    Fixed MAIN Getstatistic() method in WorkWithFile.java, method only contain calls to other methods. Fixed other issues.

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -5,23 +5,35 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class WorkWithFile {
-    private static final int FIRST_INDEX = 0;
-    private static final int SECOND_INDEX = 1;
+    private static final int FIRST_OPERATION_INDEX = 0;
+    private static final int SECOND_OPERATION_INDEX = 1;
     private static final String SEPARATOR = ",";
     private static final String BUY_WORD = "buy";
     private static final String SUPPLY_WORD = "supply";
     private static final String RESULT_WORD = "result";
 
     public void getStatistic(String fromFileName, String toFileName) {
+        String[] data = readFile(fromFileName);
+        String report = generateReport(data);
+        writeToFile(toFileName, report);
+    }
+
+    private String[] readFile(String fromFileName) {
+        try {
+            return Files.readString(Path.of(fromFileName)).split(System.lineSeparator());
+        } catch (IOException e) {
+            throw new RuntimeException("Can't access the data from file", e);
+        }
+    }
+
+    private String generateReport(String[] lines) {
         int supply = 0;
         int buy = 0;
 
-        String[] lines = readFile(fromFileName);
-
         for (String line : lines) {
             String[] data = line.split(SEPARATOR);
-            String operation = data[FIRST_INDEX].trim();
-            int amount = Integer.parseInt(data[SECOND_INDEX].trim());
+            String operation = data[FIRST_OPERATION_INDEX].trim();
+            int amount = Integer.parseInt(data[SECOND_OPERATION_INDEX].trim());
 
             if (operation.equals(SUPPLY_WORD)) {
                 supply += amount;
@@ -29,28 +41,19 @@ public class WorkWithFile {
                 buy += amount;
             }
         }
-        writeToFile(toFileName, buy, supply);
-    }
 
-    private String[] readFile(String fromFileName) {
-        try {
-            return Files.readString(Path.of(fromFileName)).split(System.lineSeparator());
-        } catch (IOException e) {
-            throw new RuntimeException("Can't access the data", e);
-        }
-    }
-
-    private String generateReport(int buy, int supply) {
         int result = supply - buy;
+
         String supplyResult = SUPPLY_WORD + SEPARATOR + supply + System.lineSeparator();
         String buyResult = BUY_WORD + SEPARATOR + buy + System.lineSeparator();
         String resultResult = RESULT_WORD + SEPARATOR + result + System.lineSeparator();
+
         return supplyResult + buyResult + resultResult;
     }
 
-    private void writeToFile(String toFileName, int buy, int supply) {
+    private void writeToFile(String toFileName, String report) {
         try {
-            Files.writeString(Path.of(toFileName), generateReport(buy, supply));
+            Files.writeString(Path.of(toFileName), report);
         } catch (IOException e) {
             throw new RuntimeException("Can't write data to file", e);
         }
