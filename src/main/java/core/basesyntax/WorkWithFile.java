c9c3c66f99b614dabb7d46e8d2058a61a2503a43
Vitalii Voitseshuk <voitseshuk.v@gmail.com>
2023-09-12 00:50:18 +0300

    add getStatistic method implementation

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,48 @@
 package core.basesyntax;
 
-public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class WorkWithFile {
+    private static final int POSITION_OPERATION = 0;
+    private static final int POSITION_AMOUNT = 1;
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        int supply = 0;
+        int buy = 0;
+        String captionSupply = "supply";
+        String captionBuy = "buy";
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
+            String line = bufferedReader.readLine();
+            while (line != null) {
+                String[] elements = line.split(",");
+                if (elements[POSITION_OPERATION].equals(captionSupply)) {
+                    supply += Integer.parseInt(elements[POSITION_AMOUNT]);
+                } else if (elements[POSITION_OPERATION].equals(captionBuy)) {
+                    buy += Integer.parseInt(elements[POSITION_AMOUNT]);
+                }
+                line = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read from the file", e);
+        }
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+            bufferedWriter.write(captionSupply);
+            bufferedWriter.write(",");
+            bufferedWriter.write(String.valueOf(supply));
+            bufferedWriter.write(System.lineSeparator());
+            bufferedWriter.write(captionBuy);
+            bufferedWriter.write(",");
+            bufferedWriter.write(String.valueOf(buy));
+            bufferedWriter.write(System.lineSeparator());
+            bufferedWriter.write("result");
+            bufferedWriter.write(",");
+            bufferedWriter.write(String.valueOf(supply - buy));
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write to the file", e);
+        }
     }
 }
