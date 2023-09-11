c759fa8eb57b3173b5e083833b56c6e0098a103b
Ivan Kusurhashev <ivankusur@gmail.com>
2023-09-11 15:30:36 +0300

    Fixed requested issues and implemented new methods to READ, MAKE REPORT, WRITE data from\to file

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -5,37 +5,54 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class WorkWithFile {
+    private static final int FIRST_INDEX = 0;
+    private static final int SECOND_INDEX = 1;
+    private static final String SEPARATOR = ",";
+    private static final String BUY_WORD = "buy";
+    private static final String SUPPLY_WORD = "supply";
+    private static final String RESULT_WORD = "result";
 
     public void getStatistic(String fromFileName, String toFileName) {
         int supply = 0;
         int buy = 0;
-        int result = 0;
 
-        try {
-            String[] lines = Files.readString(Path.of(fromFileName)).split(System.lineSeparator());
+        String[] lines = readFile(fromFileName);
 
-            for (String line : lines) {
-                String[] data = line.split(",");
-                String operation = data[0].trim();
-                int amount = Integer.parseInt(data[1].trim());
+        for (String line : lines) {
+            String[] data = line.split(SEPARATOR);
+            String operation = data[FIRST_INDEX].trim();
+            int amount = Integer.parseInt(data[SECOND_INDEX].trim());
 
-                if (operation.equals("supply")) {
-                    supply += amount;
-                } else if (operation.equals("buy")) {
-                    buy += amount;
-                }
+            if (operation.equals(SUPPLY_WORD)) {
+                supply += amount;
+            } else if (operation.equals(BUY_WORD)) {
+                buy += amount;
             }
+        }
+        writeToFile(toFileName, buy, supply);
+    }
 
-            result = supply - buy;
-
-            String supplyResult = "supply," + supply + System.lineSeparator();
-            String buyResult = "buy," + buy + System.lineSeparator();
-            String resultResult = "result," + result + System.lineSeparator();
-            String report = supplyResult + buyResult + resultResult;
-
-            Files.writeString(Path.of(toFileName), report);
+    private String[] readFile(String fromFileName) {
+        try {
+            return Files.readString(Path.of(fromFileName)).split(System.lineSeparator());
         } catch (IOException e) {
-            throw new RuntimeException("Can't correctly read data from file " + fromFileName, e);
+            throw new RuntimeException("Can't access the data", e);
+        }
+    }
+
+    private String generateReport(int buy, int supply) {
+        int result = supply - buy;
+        String supplyResult = SUPPLY_WORD + SEPARATOR + supply + System.lineSeparator();
+        String buyResult = BUY_WORD + SEPARATOR + buy + System.lineSeparator();
+        String resultResult = RESULT_WORD + SEPARATOR + result + System.lineSeparator();
+        return supplyResult + buyResult + resultResult;
+    }
+
+    private void writeToFile(String toFileName, int buy, int supply) {
+        try {
+            Files.writeString(Path.of(toFileName), generateReport(buy, supply));
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write data to file", e);
         }
     }
 }
