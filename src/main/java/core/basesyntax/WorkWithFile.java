bf0147a93bce7352e6e69e77f6d1c8f8e64bcc36
Bohdan Sosunovych <bohdan.sosunovych@gmail.com>
2023-09-16 20:25:27 +0300

    solution_divided-to-methods

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,7 +2,6 @@ package core.basesyntax;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -16,23 +15,7 @@ public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
         int supplySum = 0;
         int buySum = 0;
-        File fileFrom = new File(fromFileName);
-        File fileTo = new File(toFileName);
-        StringBuilder stringBuilder = new StringBuilder();
-
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileFrom))) {
-            String line = bufferedReader.readLine();
-            while (line != null) {
-                stringBuilder.append(line).append(SPLITTER);
-                line = bufferedReader.readLine();
-            }
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException("Can't find a file " + fromFileName, e);
-        } catch (IOException e) {
-            throw new RuntimeException("Can't read data from the file " + fromFileName, e);
-        }
-
-        String fileContent = stringBuilder.toString();
+        String fileContent = readFileContent(fromFileName);
         String[] fileContentArray = fileContent.split(SPLITTER);
 
         for (int i = 0; i < fileContentArray.length; i += 2) {
@@ -44,18 +27,42 @@ public class WorkWithFile {
         }
 
         int result = supplySum - buySum;
+        String report = createReport(supplySum, buySum, result);
+        writeToFile(toFileName, report);
+    }
 
-        StringBuilder report = new StringBuilder();
-        report.append("supply,").append(supplySum).append(System.lineSeparator())
+    private String readFileContent(String fileName) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            String line = bufferedReader.readLine();
+            while (line != null) {
+                stringBuilder.append(line).append(SPLITTER);
+                line = bufferedReader.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("Can't find a file " + fileName, e);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read data from the file " + fileName, e);
+        }
+        return stringBuilder.toString();
+    }
+
+    private String createReport(int supplySum, int buySum, int result) {
+        StringBuilder report1 = new StringBuilder();
+        report1.append("supply,").append(supplySum).append(System.lineSeparator())
                 .append("buy,").append(buySum).append(System.lineSeparator())
                 .append("result,").append(result);
+        return report1.toString();
+    }
 
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileTo))) {
-            bufferedWriter.write(report.toString());
+    private void writeToFile(String fileName, String content) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName))) {
+            bufferedWriter.write(content);
         } catch (FileNotFoundException e) {
-            throw new RuntimeException("Can't find a file " + toFileName, e);
+            throw new RuntimeException("Can't find a file " + fileName, e);
         } catch (IOException e) {
-            throw new RuntimeException("Can't write data to the " + toFileName, e);
+            throw new RuntimeException("Can't write data to the " + fileName, e);
         }
     }
 }
