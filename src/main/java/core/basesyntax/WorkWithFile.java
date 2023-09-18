8dd0bcf3e19974c6d502fe02a35c800715c08962
Rostyslav Kuzmych <rostislavkuzmic401@gmail.com>
2023-09-18 19:02:20 +0300

    fixed the mistakes

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -10,6 +10,10 @@ import java.util.List;
 public class WorkWithFile {
     private static final int NAME = 0;
     private static final int NUMBER = 1;
+    private static final String COMA = ",";
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+    private static final String RESULT = "result";
 
     public void getStatistic(String fromFileName, String toFileName) {
         List<String> fileContent = readFile(fromFileName);
@@ -17,42 +21,40 @@ public class WorkWithFile {
         writeToFile(toFileName, report);
     }
 
-    private static List<String> readFile(String fromFileName) {
+    private List<String> readFile(String fromFileName) {
         try {
             List<String> lines = Files.readAllLines(Path.of(fromFileName));
             return lines;
         } catch (IOException e) {
-            throw new RuntimeException("Can't read data");
+            throw new RuntimeException("Can't read data from the file " + fromFileName, e);
         }
     }
 
-    private static String getResult(List<String> lines) {
+    private String getResult(List<String> lines) {
         int supplySum = 0;
         int buySum = 0;
         for (String line : lines) {
-            String [] parts = line.split(",");
-            if (parts[NAME].equals("supply")) {
+            String [] parts = line.split(COMA);
+            if (parts[NAME].equals(SUPPLY)) {
                 supplySum += Integer.parseInt(parts[NUMBER]);
             }
-            if (parts[NAME].equals("buy")) {
+            if (parts[NAME].equals(BUY)) {
                 buySum += Integer.parseInt(parts[NUMBER]);
             }
         }
         int result = supplySum - buySum;
-        StringBuilder builder = new StringBuilder();
-        builder.append("supply,").append(supplySum).append(System.lineSeparator());
-        builder.append("buy,").append(buySum).append(System.lineSeparator());
-        builder.append("result,").append(result);
-        String resultReport = builder.toString();
-        return resultReport;
-
+        StringBuilder resultReport = new StringBuilder();
+        resultReport.append(SUPPLY).append(COMA).append(supplySum).append(System.lineSeparator());
+        resultReport.append(BUY).append(COMA).append(buySum).append(System.lineSeparator());
+        resultReport.append(RESULT).append(COMA).append(result);
+        return resultReport.toString();
     }
 
     private void writeToFile(String toFileName, String resultReport) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
             writer.write(resultReport);
         } catch (IOException e) {
-            throw new RuntimeException("Can't write data", e);
+            throw new RuntimeException("Can't write data to the file " + toFileName, e);
         }
     }
 }
