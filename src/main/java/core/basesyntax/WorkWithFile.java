4cc5c0cc404345cbe5c30151843afb853f73a3aa
Artem Peiev <slaybrute@gmail.com>
2023-09-19 18:10:15 +0300

    Added solution

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,41 @@
 package core.basesyntax;
 
-public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
+import core.basesyntax.suppliers.AmountSupplier;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
 
+public class WorkWithFile {
+    private AmountSupplier amountSupplier = new AmountSupplier();
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        File file = new File(toFileName);
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
+            bufferedWriter.write("supply,"
+                    + amountSupplier.getTotalSupplyAmount(getDataFromFile(new File(fromFileName)))
+                    + System.lineSeparator());
+            bufferedWriter.write("buy,"
+                    + amountSupplier.getTotalBuyAmount(getDataFromFile(new File(fromFileName)))
+                    + System.lineSeparator());
+            bufferedWriter.write("result,"
+                    + amountSupplier.getResultAmount(getDataFromFile(new File(fromFileName))));
+        } catch (IOException ioException) {
+            throw new RuntimeException("Cannot write data to file - " + file, ioException);
+        }
+    }
+
+    public ArrayList<String[]> getDataFromFile(File file) {
+        ArrayList<String[]> data = new ArrayList<>();
+        try {
+            for (String datum : Files.readAllLines(file.toPath())) {
+                data.add(datum.split(","));
+            }
+        } catch (IOException ioException) {
+            throw new RuntimeException("Cannot read data from file - " + file + ioException);
+        }
+        return data;
     }
 }
