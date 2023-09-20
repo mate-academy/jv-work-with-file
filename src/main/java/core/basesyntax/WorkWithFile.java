ce0a1e6ce954a47a5236cb99b5e671ce709b1c91
Andrii Mikhaylovskiy <Andrew.chase999@gmail.com>
2023-09-20 18:40:35 +0300

    correction of some code positions, as a result - decomposition

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,43 +8,45 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    private static final String SUPPLY = "supply";
-    private static final String BUY = "buy";
-    private static final String RESULT = "result";
+    private static final String SUPPLY_CONSTANT = "supply";
+    private static final String BUY_CONSTANT = "buy";
+    private static final String RESULT_CONSTANT = "result";
     private static final String WORD_SPLIT_REGEX = "\\W+";
     private static final String SEPARATOR = " ";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        File inputFile = new File(fromFileName);
-        String[] words = readFromFile(inputFile).split(WORD_SPLIT_REGEX);
+        String data = readFromFile(fromFileName);
+        String report = generateReport(data);
+        writeToFile(toFileName, report);
+    }
 
+    private String generateReport(String data) {
+        String[] words = data.split(WORD_SPLIT_REGEX);
         int supplySum = 0;
         int buySum = 0;
 
         for (int i = 0; i < words.length; i += 2) {
-            if (words[i].equals(SUPPLY)) {
+            if (words[i].equals(SUPPLY_CONSTANT)) {
                 supplySum += Integer.parseInt(words[i + 1]);
-            } else if (words[i].equals(BUY)) {
+            } else if (words[i].equals(BUY_CONSTANT)) {
                 buySum += Integer.parseInt(words[i + 1]);
             }
         }
 
-        String report = generateReport(supplySum, buySum);
-        writeToFile(toFileName, report);
-    }
-
-    private String generateReport(int supplySum, int buySum) {
         int resultSum = supplySum - buySum;
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(SUPPLY).append(",").append(supplySum).append(System.lineSeparator())
-                .append(BUY).append(",").append(buySum).append(System.lineSeparator())
-                .append(RESULT).append(",").append(resultSum);
+        stringBuilder.append(SUPPLY_CONSTANT).append(",").append(supplySum)
+                .append(System.lineSeparator());
+        stringBuilder.append(BUY_CONSTANT).append(",").append(buySum)
+                .append(System.lineSeparator());
+        stringBuilder.append(RESULT_CONSTANT).append(",").append(resultSum);
         return stringBuilder.toString();
     }
 
-    private String readFromFile(File inputFile) {
+    private String readFromFile(String fromFileName) {
         StringBuilder stringBuilder = new StringBuilder();
         try {
+            File inputFile = new File(fromFileName);
             FileReader fileReader = new FileReader(inputFile);
             int value = fileReader.read();
             while (value != -1) {
@@ -52,9 +54,9 @@ public class WorkWithFile {
                 value = fileReader.read();
             }
         } catch (FileNotFoundException e) {
-            throw new RuntimeException("Failed to find a file: " + inputFile.getName(), e);
+            throw new RuntimeException("Failed to find a file: " + fromFileName, e);
         } catch (IOException e) {
-            throw new RuntimeException("Failed to read from a file: " + inputFile.getName(), e);
+            throw new RuntimeException("Failed to read from a file: " + fromFileName, e);
         }
         return stringBuilder.toString();
     }
@@ -64,7 +66,7 @@ public class WorkWithFile {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(outputFile))) {
             bufferedWriter.write(report);
         } catch (IOException e) {
-            throw new RuntimeException("Failed to create a file: " + outputFile.getName(), e);
+            throw new RuntimeException("Failed to create a file: " + toFileName, e);
         }
     }
 }
