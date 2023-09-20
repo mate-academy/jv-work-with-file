2feb3b8f07bee3e77077ff5d058c3aa9ceb692ec
Artem Peiev <slaybrute@gmail.com>
2023-09-20 18:27:46 +0300

    Added changes after verification

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -9,37 +9,42 @@ import java.nio.file.Files;
 import java.util.ArrayList;
 
 public class WorkWithFile {
+    private static final String SEPARATOR = ",";
     private AmountSupplier amountSupplier = new AmountSupplier();
 
     public void getStatistic(String fromFileName, String toFileName) {
-        File file = new File(toFileName);
-        ArrayList<String[]> data = getDataFromFile(new File(fromFileName));
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
-            bufferedWriter.write(getStringWithStatistic(data));
-        } catch (IOException ioException) {
-            throw new RuntimeException("Cannot write data to file - " + file, ioException);
-        }
+        ArrayList<String[]> data = readFromFile(new File(fromFileName));
+        String report = generateReport(data);
+        writeToFile(new File(toFileName), report);
     }
 
-    private String getStringWithStatistic(ArrayList<String[]> data) {
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("supply,").append(amountSupplier.getTotalSupplyAmount(data))
-                .append(System.lineSeparator())
-                .append("buy,").append(amountSupplier.getTotalBuyAmount(data))
-                .append(System.lineSeparator())
-                .append("result,").append(amountSupplier.getResultAmount(data));
-        return stringBuilder.toString();
-    }
-
-    private ArrayList<String[]> getDataFromFile(File file) {
+    private ArrayList<String[]> readFromFile(File file) {
         ArrayList<String[]> data = new ArrayList<>();
         try {
             for (String datum : Files.readAllLines(file.toPath())) {
-                data.add(datum.split(","));
+                data.add(datum.split(SEPARATOR));
             }
         } catch (IOException ioException) {
             throw new RuntimeException("Cannot read data from file - " + file + ioException);
         }
         return data;
+    }
+
+    private String generateReport(ArrayList<String[]> data) {
+        StringBuilder report = new StringBuilder();
+        report.append("supply,").append(amountSupplier.getTotalSupplyAmount(data))
+                .append(System.lineSeparator())
+                .append("buy,").append(amountSupplier.getTotalBuyAmount(data))
+                .append(System.lineSeparator())
+                .append("result,").append(amountSupplier.getResultAmount(data));
+        return report.toString();
+    }
+
+    private void writeToFile(File file, String report) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
+            bufferedWriter.write(report);
+        } catch (IOException ioException) {
+            throw new RuntimeException("Cannot write data to file - " + file, ioException);
+        }
     }
 }
--- src/main/java/core/basesyntax/suppliers/AmountSupplier.java
+++ src/main/java/core/basesyntax/suppliers/AmountSupplier.java
@@ -4,10 +4,11 @@ import java.util.ArrayList;
 
 public class AmountSupplier {
     private static final int OPERATION_TYPE_INDEX = 0;
+    private static final int START_AMOUNT = 0;
     private static final int AMOUNT_INDEX = 1;
 
     public int getTotalBuyAmount(ArrayList<String[]> data) {
-        int amount = 0;
+        int amount = START_AMOUNT;
         for (String[] datum : data) {
             if (datum[OPERATION_TYPE_INDEX].equals("buy")) {
                 amount += Integer.parseInt(datum[AMOUNT_INDEX]);
@@ -17,7 +18,7 @@ public class AmountSupplier {
     }
 
     public int getTotalSupplyAmount(ArrayList<String[]> data) {
-        int amount = 0;
+        int amount = START_AMOUNT;
         for (String[] datum : data) {
             if (datum[OPERATION_TYPE_INDEX].equals("supply")) {
                 amount += Integer.parseInt(datum[AMOUNT_INDEX]);
