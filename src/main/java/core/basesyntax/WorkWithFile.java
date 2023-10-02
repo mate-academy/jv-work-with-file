0a256d594236e0eb05aea9f0508f8d9c6f56d272
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2023-10-02 20:17:10 +0300

    Reworked WorkWithFile class. For now, we are  operating with arrays instead of HashMap. Method fillHashMapWithOperation -> fillArrayWithOperations. Create findValuePositionByOperationName(String operationName) - to find index in array of amount by operationName. Create method clearNamesArray -> clear operationNames array the end of getStatistic method. Create method operationAmounts -> clear operationNames array in the end of getStatistic method. Rework readDataFromFileIntoArray, writeStatisticReportIntoFile for work with arrays.

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -5,56 +5,71 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 public class WorkWithFile {
-
+    private static final int AMOUNT_OF_OPERATIONS = 2;
     private static final String DATA_SEPARATOR = ",";
-    private Report report;
+    private final String[] operationNames = new String[AMOUNT_OF_OPERATIONS];
+    private final int[] operationAmounts = new int[AMOUNT_OF_OPERATIONS];
 
     public void getStatistic(String fromFileName, String toFileName) {
-        Map<String, Integer> operationAmountMap = new HashMap<>();
-        fillMapWithKeys(operationAmountMap);
-        operationAmountMap = readDataFromFileIntoMap(fromFileName);
-        writeStatisticReportIntoFile(toFileName, operationAmountMap);
+        fillArrayWithOperations();
+        readDataFromFileIntoArray(fromFileName);
+        writeStatisticReportIntoFile(toFileName);
+        clearNamesArray();
+        clearAmountsArray();
     }
 
-    private void fillMapWithKeys(Map<String, Integer> operationAmountMap) {
-        operationAmountMap.put("buy", 0);
-        operationAmountMap.put("supply", 0);
+    private void fillArrayWithOperations() {
+        operationNames[0] = "buy";
+        operationNames[1] = "supply";
     }
 
-    private Map<String, Integer> readDataFromFileIntoMap(String fromFileName) {
-        Map<String, Integer> operationAmountMap = new HashMap<>();
-        fillMapWithKeys(operationAmountMap);
+    private void readDataFromFileIntoArray(String fromFileName) {
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             String dataLine = bufferedReader.readLine();
             while (dataLine != null) {
-                String[] operationAmount = dataLine.split(DATA_SEPARATOR);
-                String operation = operationAmount[0];
-                int amount = Integer.parseInt(operationAmount[1]);
-                int currentAmount = operationAmountMap.get(operation);
-                if (operationAmountMap.containsKey(operation)) {
-                    operationAmountMap.put(operation, currentAmount + amount);
-                }
+                String[] operationAmountDataPair = dataLine.split(DATA_SEPARATOR);
+                String operation = operationAmountDataPair[0];
+                int amount = Integer.parseInt(operationAmountDataPair[1]);
+                operationAmounts[findValuePositionByOperationName(operation)] += amount;
                 dataLine = bufferedReader.readLine();
             }
         } catch (IOException e) {
             throw new RuntimeException("Can't read data from file", e);
         }
-        return operationAmountMap;
     }
 
-    private void writeStatisticReportIntoFile(String toFileName, Map<String,
-            Integer> operationAmountMap) {
+    private void writeStatisticReportIntoFile(String toFileName) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(
                 new FileWriter(toFileName))) {
-            report = new Report(operationAmountMap.get("supply"),
-                    operationAmountMap.get("buy"));
+            int supplyValue = operationAmounts[findValuePositionByOperationName("supply")];
+            int buyValue = operationAmounts[findValuePositionByOperationName("buy")];
+            Report report = new Report(supplyValue, buyValue);
             bufferedWriter.write(report.createReportString());
         } catch (IOException e) {
             throw new RuntimeException("Can't write data into the file", e);
+        }
+    }
+
+    private int findValuePositionByOperationName(String operationName) {
+        for (int i = 0; i < operationNames.length; i++) {
+            if (operationNames[i].equals(operationName)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private void clearNamesArray() {
+        for (int i = 0; i < operationNames.length; i++) {
+            operationNames[i] = null;
+        }
+    }
+
+    private void clearAmountsArray() {
+        for (int i = 0; i < operationAmounts.length; i++) {
+            operationAmounts[i] = 0;
         }
     }
 }
