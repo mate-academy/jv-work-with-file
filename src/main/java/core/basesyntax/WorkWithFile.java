a0a039af9e3a9ac2688796a6ae7d30aa3cd50d8f
Jakub Sekowski <sekowskijakub@gmail.com>
2023-10-02 09:09:57 +0200

    Fixes applied: - the logic divided into 3 separate methods - constants added

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,31 +7,51 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class WorkWithFile {
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+    private static final String RESULT = "result";
+    private static final String COMMA = ",";
+    private static final int INDEX_OF_OPERATION = 0;
+    private static final int INDEX_OF_AMOUNT = 1;
+
     public void getStatistic(String fromFileName, String toFileName) {
-        int supplyCounter = 0;
-        int buyCounter = 0;
+        String[] dataFromfile = readData(fromFileName);
+        String report = createReport(dataFromfile);
+        writeToFile(report, toFileName);
+    }
+
+    private String[] readData(String fromFileName) {
         StringBuilder builder = new StringBuilder();
         try {
             builder.append(Files.readString(Path.of(fromFileName))).append(System.lineSeparator());
         } catch (IOException e) {
             throw new RuntimeException("Can't read from the file " + fromFileName, e);
         }
-        String[] dataArray = builder.toString().split(System.lineSeparator());
-        for (int i = 0; i < dataArray.length; i++) {
-            String[] oneLine = dataArray[i].split(",");
-            if (oneLine[0].equals("supply")) {
-                supplyCounter += Integer.parseInt(oneLine[1]);
+        return builder.toString().split(System.lineSeparator());
+    }
+
+    private String createReport(String[] dataFromfile) {
+        int supplyCounter = 0;
+        int buyCounter = 0;
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < dataFromfile.length; i++) {
+            String[] oneLine = dataFromfile[i].split(COMMA);
+            if (oneLine[INDEX_OF_OPERATION].equals(SUPPLY)) {
+                supplyCounter += Integer.parseInt(oneLine[INDEX_OF_AMOUNT]);
             }
-            if (oneLine[0].equals("buy")) {
-                buyCounter += Integer.parseInt(oneLine[1]);
+            if (oneLine[INDEX_OF_OPERATION].equals(BUY)) {
+                buyCounter += Integer.parseInt(oneLine[INDEX_OF_AMOUNT]);
             }
         }
-        builder.setLength(0);
-        builder.append("supply,").append(supplyCounter).append(System.lineSeparator())
-                .append("buy,").append(buyCounter).append(System.lineSeparator())
-                .append("result,").append(supplyCounter - buyCounter);
+        return builder.append(SUPPLY).append(COMMA)
+                .append(supplyCounter).append(System.lineSeparator())
+                .append(BUY).append(COMMA).append(buyCounter).append(System.lineSeparator())
+                .append(RESULT).append(COMMA).append(supplyCounter - buyCounter).toString();
+    }
+
+    private void writeToFile(String report, String toFileName) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
-            writer.write(builder.toString());
+            writer.write(report);
         } catch (IOException e) {
             throw new RuntimeException("Can't write data to file" + toFileName, e);
         }
