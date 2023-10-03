2d54b30a98cf0fdc7f09f6c3957649046b5361eb
Lenovo <liubchenkalexander@gmail.com>
2023-10-03 22:44:23 +0300

    added fixes after review

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,21 +1,19 @@
 package core.basesyntax;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class WorkWithFile {
-    private static final String emptyLine = "";
-    private static final String gap = " ";
+
     private static final String SEPARATOR = ",";
     private static final String SUPPLY = "supply";
     private static final String BUY = "buy";
     private static final String RESULT = "result";
-    private static final int ALL_SUPPLY_INDEX = 0;
-    private static final int ALL_BUY_INDEX = 1;
-    private static final int RESULT_INDEX = 2;
+    private static final String LINE_SEPARATOR = System.lineSeparator();
+    private static final int INDEX = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
         writeToFile(createReport(readData(fromFileName)), toFileName);
@@ -35,50 +33,28 @@ public class WorkWithFile {
     }
 
     private String createReport(String readData) {
-        String withoutSigns = readData
-                .replaceAll("[%$#@`~;:+{()_.!/?*',\n\r]", gap)
-                .toLowerCase();
-        String[] reportData = withoutSigns.split(gap);
-        int allSupply = 0;
         int allBuy = 0;
-        int result = 0;
-        for (int i = 0; i < reportData.length; i += 2) {
-            if (reportData[i].equals(emptyLine)) {
-                i -= 1;
-                continue;
-            }
-            if (reportData[i].equals(BUY)) {
-                allBuy += Integer.parseInt(reportData[i + 1]);
-            }
-            if (reportData[i].equals(SUPPLY)) {
-                allSupply += Integer.parseInt(reportData[i + 1]);
+        int allSupply = 0;
+        StringBuilder result = new StringBuilder();
+        String[] dataFile = readData.split(LINE_SEPARATOR);
+        for (String datum : dataFile) {
+            String[] data = datum.split(SEPARATOR);
+            if (datum.startsWith(BUY)) {
+                allBuy += Integer.parseInt(data[INDEX]);
+            } else {
+                allSupply += Integer.parseInt(data[INDEX]);
             }
         }
-        result = allSupply - allBuy;
-        return emptyLine + allSupply + gap + allBuy + gap + result;
+        return result.append(SUPPLY).append(SEPARATOR).append(allSupply).append(LINE_SEPARATOR)
+                .append(BUY).append(SEPARATOR).append(allBuy).append(LINE_SEPARATOR)
+                .append(RESULT).append(SEPARATOR).append(allSupply - allBuy).toString();
     }
 
     public void writeToFile(String createdReport, String toFileName) {
-        String[] report = createdReport.split(gap);
-        int allSupply = Integer.parseInt(report[ALL_SUPPLY_INDEX]);
-        int allBuy = Integer.parseInt(report[ALL_BUY_INDEX]);
-        int result = Integer.parseInt(report[RESULT_INDEX]);
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
-            writer.write(SUPPLY + SEPARATOR);
-            writer.write(Integer.toString(allSupply));
-            writer.write(System.lineSeparator());
-            writer.write(BUY + SEPARATOR);
-            writer.write(Integer.toString(allBuy));
-            writer.write(System.lineSeparator());
-            writer.write(RESULT + SEPARATOR);
-            writer.write(Integer.toString(result));
-            try {
-                writer.close();
-            } catch (IOException e) {
-                throw new RuntimeException("Can't close the file" + toFileName, e);
-            }
+        try {
+            Files.writeString(Path.of(toFileName), createdReport);
         } catch (IOException e) {
-            throw new RuntimeException("Can't open file" + toFileName, e);
+            throw new RuntimeException("Can't write data to file " + toFileName, e);
         }
     }
 }
