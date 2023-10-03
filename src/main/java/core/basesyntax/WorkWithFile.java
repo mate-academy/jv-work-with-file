f8f2b3e21eac0a25e50e1b1e53cfa23452811f52
Valerii Fedorovych <valera.fedorovich.2000@gmail.com>
2023-10-03 16:07:17 +0300

    fixed checkstyle mistakes

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,7 +2,6 @@ package core.basesyntax;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
