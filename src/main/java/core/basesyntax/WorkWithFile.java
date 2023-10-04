d9dadf0711869dd0f6ce99e59290f69b8ad928fa
Danil Ponomarenko <akame.dante13@gmail.com>
2023-10-04 15:40:37 +0300

    Created first version of program

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,45 @@
 package core.basesyntax;
 
+import java.io.*;
+
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
+        int totalSupply = 0;
+        int totalBuy = 0;
+        writeCsvFile(toFileName, readCsvFile(fromFileName, totalSupply, totalBuy));
+    }
 
+    private int[] readCsvFile(String fromFileName, int totalSupply, int totalBuy) {
+        try (BufferedReader br = new BufferedReader(new FileReader(fromFileName))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] readCsvData = line.split(",");
+                switch (readCsvData[0]) {
+                    case "supply":
+                        totalSupply += Integer.parseInt(readCsvData[1]);
+                        break;
+                    case "buy":
+                        totalBuy += Integer.parseInt(readCsvData[1]);
+                        break;
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read data from the file " + fromFileName, e);
+        }
+        return new int[]{totalSupply, totalBuy};
+    }
+
+    private void writeCsvFile(String toFileName, int[] statistics) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(toFileName))) {
+            createReport(bw, statistics);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write data to the file " + toFileName, e);
+        }
+    }
+
+    private void createReport(BufferedWriter bw, int[] statistics) throws IOException {
+        bw.write("supply," + statistics[0] + System.lineSeparator());
+        bw.write("buy," + statistics[1] + System.lineSeparator());
+        bw.write("result," + (statistics[0] - statistics[1]) + System.lineSeparator());
     }
 }
