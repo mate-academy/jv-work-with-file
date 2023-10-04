5bd4cfd46331054914aabf0ae2d26694741ba474
Nataliia Shtykh <forstudying93@gmail.com>
2023-10-04 23:40:24 +0300

    solved the task jv-work-with-file

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,62 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
+        String[] dataFromFile = readFromFile(fromFileName);
+        String report = createReport(dataFromFile);
+        writeToFile(toFileName, report);
+    }
 
+    public String createReport(String[] arrayWithData) {
+        StringBuilder stringBuilder = new StringBuilder();
+        int supplyCounter = 0;
+        int buyCounter = 0;
+        for (String data : arrayWithData) {
+            String[] dataArray = data.split(",");
+            String operation = dataArray[0].trim();
+            String amountStr = dataArray[1].trim();
+            int amount = Integer.parseInt(amountStr);
+            if ("supply".equals(operation)) {
+                supplyCounter += amount;
+            } else if ("buy".equals(operation)) {
+                buyCounter += amount;
+            }
+        }
+
+        stringBuilder.append("supply,").append(supplyCounter).append(System.lineSeparator())
+                     .append("buy,").append(buyCounter).append(System.lineSeparator())
+                     .append("result,").append(supplyCounter - buyCounter);
+
+        return stringBuilder.toString();
+    }
+
+    public void writeToFile(String toFileName, String report) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+            bufferedWriter.write(report);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write to file", e);
+        }
+    }
+
+    public String[] readFromFile(String fromFileName) {
+        StringBuilder stringWithData = new StringBuilder();
+        String readLine;
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
+            readLine = bufferedReader.readLine();
+            while (readLine != null) {
+                stringWithData.append(readLine).append(System.lineSeparator());
+                readLine = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read from file", e);
+        }
+        String readedString = stringWithData.toString();
+        return readedString.split("\n");
     }
 }
