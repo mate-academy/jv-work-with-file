546f896dda2873af2a9c7cdd4b26d7f6909a0959
Vladyslav Druhov <space.drugov@gmail.com>
2023-10-04 12:01:28 +0300

    The method logic was divided into three private methods: writeLine, readFile, and generateReport, as recommended by. System.lineSeparator() instead of System.getProperty("line.separator") Make ",", 0, 1 constants.

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,38 +7,47 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    private static final String COMMA_SEPARATOR = ",";
+    private static final int OPERATION_TYPE_INDEX = 0;
+    private static final int AMOUNT_INDEX = 1;
+
     public void getStatistic(String fromFileName, String toFileName) {
-        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
+        int supplyTotal = 0;
+        int buyTotal = 0;
 
-                int supplyTotal = 0;
-                int buyTotal = 0;
-                String line;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
+            String line;
 
-                while ((line = reader.readLine()) != null) {
-                    String[] parts = line.split(",");
-                    if (parts.length == 2) {
-                        String operationType = parts[0];
-                        int amount = Integer.parseInt(parts[1]);
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(COMMA_SEPARATOR);
+                if (parts.length == 2) {
+                    String operationType = parts[OPERATION_TYPE_INDEX];
+                    int amount = Integer.parseInt(parts[AMOUNT_INDEX]);
 
-                        if ("supply".equals(operationType)) {
-                            supplyTotal += amount;
-                        } else if ("buy".equals(operationType)) {
-                            buyTotal += amount;
-                        }
+                    if ("supply".equals(operationType)) {
+                        supplyTotal += amount;
+                    } else if ("buy".equals(operationType)) {
+                        buyTotal += amount;
                     }
                 }
-
-                int result = supplyTotal - buyTotal;
-
-                writer.write("supply," + supplyTotal + System.getProperty("line.separator"));
-                writer.write("buy," + buyTotal + System.getProperty("line.separator"));
-                writer.write("result," + result + System.getProperty("line.separator"));
-
             }
         } catch (IOException e) {
-            throw new RuntimeException("Error reading or writing files: "
-                    + fromFileName + ", " + toFileName, e);
+            throw new RuntimeException("Error reading the file: " + fromFileName, e);
         }
+
+        int result = supplyTotal - buyTotal;
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
+            writeLine(writer, "supply", supplyTotal);
+            writeLine(writer, "buy", buyTotal);
+            writeLine(writer, "result", result);
+        } catch (IOException e) {
+            throw new RuntimeException("Error writing to the file: " + toFileName, e);
+        }
+    }
+
+    private void writeLine(BufferedWriter writer, String key, int value) throws IOException {
+        writer.write(key + COMMA_SEPARATOR + value + System.lineSeparator());
     }
 }
