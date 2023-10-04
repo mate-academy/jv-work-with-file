1937cc2ec76fd2de8033eed62415f3b73c6358c3
HovorukhaBohdan <hovorukha.bohdan@gmail.com>
2023-10-04 12:35:29 +0300

    Simplefied 'getStatistic' method

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,32 +7,80 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    private static final String DIVIDER = ",";
+
     public void getStatistic(String fromFileName, String toFileName) {
-        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
-                BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
-            String values = reader.readLine();
-            int totalSupply = 0;
-            int totalBuy = 0;
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
+            int totalSupply = countTotalSupply(fromFileName);
+            int totalBuy = countTotalBuy(fromFileName);
+            int result = countResult(totalSupply, totalBuy);
 
-            while (values != null) {
-                String[] value = values.split(",");
-
-                if (value[0].equals("supply")) {
-                    totalSupply += Integer.parseInt(value[1]);
-                } else {
-                    totalBuy += Integer.parseInt(value[1]);
-                }
-
-                values = reader.readLine();
-            }
-
-            int result = totalSupply - totalBuy;
-
-            writer.write("supply," + totalSupply + System.lineSeparator()
-                    + "buy," + totalBuy + System.lineSeparator()
-                    + "result," + result);
+            createReport(writer, totalSupply, totalBuy, result);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private void createReport(BufferedWriter writer, int totalSupply, int totalBuy, int result) throws IOException {
+        writer.write("supply," + totalSupply + System.lineSeparator()
+                + "buy," + totalBuy + System.lineSeparator()
+                + "result," + result);
+        writer.close();
+    }
+
+    private int countResult(int totalSupply, int totalBuy) {
+        return totalSupply - totalBuy;
+    }
+
+    private int countTotalSupply(String fromFileName) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
+        int totalSupply = 0;
+        String infoOnLine = reader.readLine();
+
+        while (infoOnLine != null) {
+            totalSupply += findSupply(infoOnLine);
+            infoOnLine = reader.readLine();
+        }
+
+        reader.close();
+        return totalSupply;
+    }
+
+    private int findSupply(String infoOnLine) {
+        String[] info = infoOnLine.split(DIVIDER);
+        String status = info[0];
+        int price = Integer.parseInt(info[1]);
+
+        if (status.equals("supply")) {
+            return price;
+        }
+
+        return 0;
+    }
+
+    private int countTotalBuy(String fromFileName) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
+        int totalBuy = 0;
+        String infoOnLine = reader.readLine();
+
+        while (infoOnLine != null) {
+            totalBuy += findBuy(infoOnLine);
+            infoOnLine = reader.readLine();
+        }
+
+        reader.close();
+        return totalBuy;
+    }
+
+    private int findBuy(String infoOnLine) {
+        String[] info = infoOnLine.split(DIVIDER);
+        String status = info[0];
+        int price = Integer.parseInt(info[1]);
+
+        if (status.equals("buy")) {
+            return price;
+        }
+
+        return 0;
     }
 }
