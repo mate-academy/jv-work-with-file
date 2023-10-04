c2dc1c7c90532652fa2f6af0c30b120d34fefdc9
HovorukhaBohdan <hovorukha.bohdan@gmail.com>
2023-10-04 16:05:00 +0300

    Added StringBuilder to 'createReport' method

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,13 +1,12 @@
 package core.basesyntax;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
 public class WorkWithFile {
     private static final String DIVIDER = ",";
+    private static final String REPORT_SUPPLY = "supply,";
+    private static final String REPORT_BUY = "buy,";
+    private static final String REPORT_RESULT = "result,";
 
     public void getStatistic(String fromFileName, String toFileName) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
@@ -16,17 +15,19 @@ public class WorkWithFile {
             int result = countResult(totalSupply, totalBuy);
 
             createReport(writer, totalSupply, totalBuy, result);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("Can't find a file " + fromFileName, e);
         } catch (IOException e) {
-            throw new RuntimeException("Something went wrong "
-                    + fromFileName + " or " + toFileName, e);
+            throw new RuntimeException("Can't read a file  " + toFileName, e);
         }
     }
 
     private void createReport(BufferedWriter writer,
                               int totalSupply, int totalBuy, int result) throws IOException {
-        writer.write("supply," + totalSupply + System.lineSeparator()
-                + "buy," + totalBuy + System.lineSeparator()
-                + "result," + result);
+        StringBuilder report = new StringBuilder(REPORT_SUPPLY).append(totalSupply)
+                .append(System.lineSeparator()).append(REPORT_BUY).append(totalBuy)
+                .append(System.lineSeparator()).append(REPORT_RESULT).append(result);
+        writer.write(report.toString());
     }
 
     private int countResult(int totalSupply, int totalBuy) {
