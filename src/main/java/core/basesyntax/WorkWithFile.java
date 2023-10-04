c2d1bbcfd660ed19f2f2c1c1de9f9749ae145c2d
Oleksandr Voitenko <129169030+OlexVoit@users.noreply.github.com>
2023-10-04 17:12:05 +0200

    Implement method getStatistic

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,44 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
+        String[] result = readFile(fromFileName);
+        try (FileWriter fileWriter = new FileWriter(toFileName)) {
+            for (String line : result) {
+                fileWriter.write(line + System.lineSeparator());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write the file" + toFileName, e);
+        }
+    }
 
+    public static String[] readFile(String fromFileName) {
+        StringBuilder stringBuilder = new StringBuilder();
+        int numberOfPurchases = 0;
+        int numberOfSupply = 0;
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
+            String fileStr;
+            while ((fileStr = bufferedReader.readLine()) != null) {
+                stringBuilder.append(fileStr).append(",");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read the file " + fromFileName, e);
+        }
+        String[] cloneBuilder = stringBuilder.toString().split(",");
+        for (int i = 0; i < cloneBuilder.length; i++) {
+            if (cloneBuilder[i].equalsIgnoreCase("supply")) {
+                numberOfSupply += Integer.parseInt(cloneBuilder[i + 1]);
+            }
+            if (cloneBuilder[i].equalsIgnoreCase("buy")) {
+                numberOfPurchases += Integer.parseInt(cloneBuilder[i + 1]);
+            }
+        }
+        return new String[]{"supply," + numberOfSupply, "buy,"
+                + numberOfPurchases, "result," + (numberOfSupply - numberOfPurchases)};
     }
 }
