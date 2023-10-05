9c85f2a75db6b0758458f3cd58667eebe9162036
AdamMudrak <mudrets2000@gmail.com>
2023-10-06 00:27:07 +0300

    Divide reading and processing data into two different functions

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,47 +8,51 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    private int supply;
-    private int buy;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        readFromFileAndCount(fromFileName);
-        writeToFile(toFileName);
+        writeToFile(toFileName, processDataForReport(readFromFileAndHoldData(fromFileName)));
     }
 
-    private void readFromFileAndCount(String fromFileName) {
-        supply = 0;
-        buy = 0;
+    private StringBuilder readFromFileAndHoldData(String fromFileName) {
+        StringBuilder rawData = new StringBuilder();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             String line = bufferedReader.readLine();
             while (line != null) {
-                String[] info = line.split(",");
-                if (info[0].equals("supply")) {
-                    supply += Integer.parseInt(info[1]);
-                } else if (info[0].equals("buy")) {
-                    buy += Integer.parseInt(info[1]);
-                }
+                rawData.append(line).append(",");
                 line = bufferedReader.readLine();
             }
         } catch (IOException e) {
             throw new RuntimeException("Can't read from file", e);
         }
+        return rawData;
     }
 
-    private void writeToFile(String toFileName) {
-        String supplyInfo = "supply," + supply;
-        String buyInfo = "buy," + buy;
-        String resultInfo = "result," + (supply - buy);
+    private int[] processDataForReport(StringBuilder rawData) {
+        int supply = 0;
+        int buy = 0;
+        String[] preReportData = rawData.toString().split(",");
+        for (int i = 0; i < preReportData.length - 1; i += 2) {
+            if (preReportData[i].equals("supply")) {
+                supply += Integer.parseInt(preReportData[i + 1]);
+            } else if (preReportData[i].equals("buy")) {
+                buy += Integer.parseInt(preReportData[i + 1]);
+            }
+        }
+        int result = supply - buy;
+        return new int[]{supply, buy, result};
+    }
+
+    private void writeToFile(String toFileName, int[] supplyAndBuyData) {
         File file = new File(toFileName);
         BufferedWriter bufferedWriter = null;
         if (!file.exists()) {
             try {
                 bufferedWriter = new BufferedWriter(new FileWriter(toFileName, true));
-                bufferedWriter.write(supplyInfo);
+                bufferedWriter.write(Constants.SUPPLY_MSG + supplyAndBuyData[0]);
                 bufferedWriter.write(System.lineSeparator());
-                bufferedWriter.write(buyInfo);
+                bufferedWriter.write(Constants.BUY_MSG + supplyAndBuyData[1]);
                 bufferedWriter.write(System.lineSeparator());
-                bufferedWriter.write(resultInfo);
+                bufferedWriter.write(Constants.RESULT_MSG + supplyAndBuyData[2]);
             } catch (IOException e) {
                 throw new RuntimeException("Can't write to file", e);
             } finally {
@@ -60,30 +64,6 @@ public class WorkWithFile {
                     }
                 }
             }
-        }
-    }
-
-    // I have also come up with this idea, and I couldn't decide what would be better.
-    private void readFromFileAndCount1(String fromFileName) {
-        supply = 0;
-        buy = 0;
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
-            String line = bufferedReader.readLine();
-            StringBuilder builder = new StringBuilder();
-            while (line != null) {
-                builder.append(line).append(",");
-                line = bufferedReader.readLine();
-            }
-            String[] info = builder.toString().split(",");
-            for (int i = 0; i < info.length - 1; i += 2) {
-                if (info[i].equals("supply")) {
-                    supply += Integer.parseInt(info[i + 1]);
-                } else if (info[i].equals("buy")) {
-                    buy += Integer.parseInt(info[i + 1]);
-                }
-            }
-        } catch (IOException e) {
-            throw new RuntimeException("Can't read from file", e);
         }
     }
 }
