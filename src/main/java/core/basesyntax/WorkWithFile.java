1967a7cd5a4be08818a6475502422f8b2484a978
Mateusz Rostek <17rostek17@gmail.com>
2023-10-05 13:41:08 +0200

    Fulfilled required changes

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,41 +1,35 @@
 package core.basesyntax;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 
 public class WorkWithFile {
     private static final String SEPARATOR = ",";
     private static final int OPERATION_TYPE_INDEX = 0;
     private static final int OPERATION_VALUE_INDEX = 1;
 
-    private int supplySum = 0;
-    private int buySum = 0;
-
     public void getStatistic(String fromFileName, String toFileName) {
-        readFromFile(fromFileName);
-        writeToFile(toFileName, createReport());
+        String[] dataFromFile = readFromFile(fromFileName);
+        String report = createReport(dataFromFile);
+        writeToFile(toFileName, report);
     }
 
-    private void readFromFile(String fromFileName) {
+    private String[] readFromFile(String fromFileName) {
         File inputFile = new File(fromFileName);
-        resetSums();
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(inputFile))) {
-            String line = bufferedReader.readLine();
-            while (line != null) {
-                if (line.split(SEPARATOR)[OPERATION_TYPE_INDEX].equals("buy")) {
-                    buySum += Integer.parseInt(line.split(SEPARATOR)[OPERATION_VALUE_INDEX]);
-                } else {
-                    supplySum += Integer.parseInt(line.split(SEPARATOR)[OPERATION_VALUE_INDEX]);
-                }
-                line = bufferedReader.readLine();
-            }
+        Path inputFilePath = Path.of(inputFile.toURI());
+        List<String> lines;
+
+        try {
+            lines = Files.readAllLines(inputFilePath);
         } catch (IOException e) {
             throw new RuntimeException("Can't read from a file named: " + fromFileName, e);
         }
+        return lines.toArray(new String[]{});
     }
 
     private void writeToFile(String toFileName, String reportToWrite) {
@@ -46,14 +40,19 @@ public class WorkWithFile {
         }
     }
 
-    private String createReport() {
+    private String createReport(String[] dataFromFile) {
+        int supplySum = 0;
+        int buySum = 0;
+
+        for (String currentData : dataFromFile) {
+            if (currentData.split(SEPARATOR)[OPERATION_TYPE_INDEX].equals("buy")) {
+                buySum += Integer.parseInt(currentData.split(SEPARATOR)[OPERATION_VALUE_INDEX]);
+            } else {
+                supplySum += Integer.parseInt(currentData.split(SEPARATOR)[OPERATION_VALUE_INDEX]);
+            }
+        }
         return "supply," + supplySum + System.lineSeparator()
                 + "buy," + buySum + System.lineSeparator()
                 + "result," + (supplySum - buySum);
-    }
-
-    private void resetSums() {
-        supplySum = 0;
-        buySum = 0;
     }
 }
