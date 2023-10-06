fa298268d5c0b1358dbb55087fa90c052070465e
Volodymyr Ozemblovskyi <Vladimir.ozem.ps@gmail.com>
2023-10-06 17:52:01 +0300

    Implement StringBuilder in writeCsv() and create constants

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,6 +8,12 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    private static final String BUY = "buy";
+    private static final String RESULT = "result";
+    private static final String SUPPLY = "supply";
+    private static final String DIVIDER = ",";
+    private static final String SEPARATING_REGEX = " ";
+
     public void getStatistic(String fromFileName, String toFileName) {
         String[] data = readCsv(fromFileName);
         String[] resultData = processData(data);
@@ -17,24 +23,24 @@ public class WorkWithFile {
     private String[] readCsv(String fileName) {
         File file = new File(fileName);
         StringBuilder stringBuilder = new StringBuilder();
-        String separatingRegex = " ";
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
             String line = bufferedReader.readLine();
             while (line != null) {
-                stringBuilder.append(line).append(separatingRegex);
+                stringBuilder.append(line).append(SEPARATING_REGEX);
                 line = bufferedReader.readLine();
             }
         } catch (IOException e) {
             throw new RuntimeException("Can't read file" + fileName, e);
         }
-        return stringBuilder.toString().split(separatingRegex);
+        return stringBuilder.toString().split(SEPARATING_REGEX);
     }
 
     private void writeCsv(String fileName, String[] strings) {
         File file = new File(fileName);
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
             for (String str : strings) {
-                bufferedWriter.write(str + System.lineSeparator());
+                StringBuilder builder = new StringBuilder(str).append(System.lineSeparator());
+                bufferedWriter.write(builder.toString());
             }
         } catch (IOException e) {
             throw new RuntimeException("Can't write file" + fileName, e);
@@ -45,19 +51,19 @@ public class WorkWithFile {
         int supplyAmount = 0;
         int buyAmount = 0;
         for (String str : data) {
-            String [] splittedString = str.split(",");
+            String [] splittedString = str.split(DIVIDER);
             String inputData = splittedString[0];
             int inputAmount = Integer.parseInt(splittedString[1]);
-            if (inputData.equals("supply")) {
+            if (inputData.equals(SUPPLY)) {
                 supplyAmount += inputAmount;
             }
-            if (inputData.equals("buy")) {
+            if (inputData.equals(BUY)) {
                 buyAmount += inputAmount;
             }
         }
-        String supply = "supply," + supplyAmount;
-        String buy = "buy," + buyAmount;
-        String result = "result," + (supplyAmount - buyAmount);
-        return new String []{supply, buy, result};
+        String supplyData = SUPPLY + DIVIDER + supplyAmount;
+        String buyData = BUY + DIVIDER + buyAmount;
+        String resultData = RESULT + DIVIDER + (supplyAmount - buyAmount);
+        return new String []{supplyData, buyData, resultData};
     }
 }
