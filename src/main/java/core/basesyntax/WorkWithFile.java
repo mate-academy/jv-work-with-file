2157850b35c9eb4df43d7f8f824125a2451fbb93
Dmytro Potapov <potapovds1993@gmail.com>
2023-10-08 19:27:07 +0300

    creat new File

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,9 @@
 package core.basesyntax;
 
+import java.io.File;
+
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
-
+        File fileReader = new File(fromFileName);
     }
 }
