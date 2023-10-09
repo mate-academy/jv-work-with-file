35634659fd22a7fd1653f688d543d177ea835bb5
White EdenI <danil.gayvoronsky2201@gmail.com>
2023-10-09 16:55:52 +0300

    Everything works nww ^^

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,30 +8,53 @@ import java.io.IOException;
 
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
-        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
-                BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
+        writeToFile(readFile(fromFileName), toFileName);
+    }
+
+    private StringBuilder readFile(String fromFileName) {
+        StringBuilder stringBuilder = new StringBuilder();
+        final String splitChar = ",";
+        final String supplyString = "supply";
+        final String buyString = "buy";
+        final String resultString = "result";
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
             int totalSupply = 0;
             int totalBuy = 0;
             String line;
             while ((line = reader.readLine()) != null) {
-                String[] parts = line.split(",");
+                String[] parts = line.split(splitChar);
                 if (parts.length == 2) {
                     int amount = Integer.parseInt(parts[1].trim());
-                    if (parts[0].trim().equals("supply")) {
+                    if (parts[0].trim().equals(supplyString)) {
                         totalSupply += amount;
-                    } else if (parts[0].trim().equals("buy")) {
+                    } else if (parts[0].trim().equals(buyString)) {
                         totalBuy += amount;
                     }
                 }
             }
             int result = totalSupply - totalBuy;
-            writer.write("supply," + totalSupply);
-            writer.newLine();
-            writer.write("buy," + totalBuy);
-            writer.newLine();
-            writer.write("result," + result);
+            stringBuilder.append(supplyString)
+                    .append(splitChar)
+                    .append(totalSupply)
+                    .append(System.lineSeparator())
+                    .append(buyString)
+                    .append(splitChar)
+                    .append(totalBuy)
+                    .append(System.lineSeparator())
+                    .append(resultString)
+                    .append(splitChar)
+                    .append(result);
+            return stringBuilder;
         } catch (IOException e) {
-            throw new RuntimeException("Error reading or writing the file: ", e);
+            throw new RuntimeException("Failed to read from file file: " + fromFileName, e);
+        }
+    }
+
+    private void writeToFile(StringBuilder someBuilder, String toFileName) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+            bufferedWriter.write(someBuilder.toString());
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to write in file: " + toFileName,e);
         }
     }
 }
