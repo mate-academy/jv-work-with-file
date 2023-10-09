3e3d157dc5376243af6a58a9f140ac324a1e0c81
Denys Okhrymovych <denys.ohrymovych.2015@gmail.com>
2023-10-09 17:58:51 +0300

    split getStatistic into smaller methods, create constants, use try-with-resources

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,33 +8,54 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    private static final String OPERATION_1 = "supply";
+    private static final String OPERATION_2 = "buy";
+    private static final String OPERATION_AND_AMOUNT_SEPARATOR = ",";
+
     public void getStatistic(String fromFileName, String toFileName) {
-        int supplyResult = 0;
-        int buyResult = 0;
         File fromFile = new File(fromFileName);
-        try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFile));
-            String line = bufferedReader.readLine();
-            while (line != null) {
-                String[] splitLine = line.split(",");
-                if (splitLine[0].equals("supply")) {
-                    supplyResult += Integer.parseInt(splitLine[1]);
+        File toFile = new File(toFileName);
+        int[] totalAmounts = readFromFileAndGetAmounts(fromFile, fromFileName);
+        String report = createReport(totalAmounts);
+        writeToFile(toFile, toFileName, report);
+    }
+
+    private int[] readFromFileAndGetAmounts(File fromFile, String fromFileName) {
+        int supplyTotalAmount = 0;
+        int buyTotalAmount = 0;
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFile))) {
+            String currentLine = bufferedReader.readLine();
+            while (currentLine != null) {
+                String[] currentOpearationAndAmount
+                        = currentLine.split(OPERATION_AND_AMOUNT_SEPARATOR);
+                int amount = Integer.parseInt(currentOpearationAndAmount[1]);
+                if (currentOpearationAndAmount[0].equals(OPERATION_1)) {
+                    supplyTotalAmount += amount;
                 } else {
-                    buyResult += Integer.parseInt(splitLine[1]);
+                    buyTotalAmount += amount;
                 }
-                line = bufferedReader.readLine();
+                currentLine = bufferedReader.readLine();
             }
-            bufferedReader.close();
         } catch (IOException e) {
             throw new RuntimeException("Cannot read file " + fromFileName, e);
         }
+        return new int[] {supplyTotalAmount, buyTotalAmount};
+    }
+
+    private String createReport(int[] totalAmounts) {
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("supply,").append(supplyResult).append(System.lineSeparator());
-        stringBuilder.append("buy,").append(buyResult).append(System.lineSeparator());
-        stringBuilder.append("result,").append(supplyResult - buyResult);
-        File toFile = new File(toFileName);
+        stringBuilder.append(OPERATION_1 + OPERATION_AND_AMOUNT_SEPARATOR)
+                .append(totalAmounts[0]).append(System.lineSeparator());
+        stringBuilder.append(OPERATION_2 + OPERATION_AND_AMOUNT_SEPARATOR)
+                .append(totalAmounts[1]).append(System.lineSeparator());
+        stringBuilder.append("result" + OPERATION_AND_AMOUNT_SEPARATOR)
+                .append(totalAmounts[0] - totalAmounts[1]);
+        return stringBuilder.toString();
+    }
+
+    private void writeToFile(File toFile, String toFileName, String report) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFile))) {
-            bufferedWriter.write(stringBuilder.toString());
+            bufferedWriter.write(report);
         } catch (IOException e) {
             throw new RuntimeException("Cannot write to file " + toFileName, e);
         }
