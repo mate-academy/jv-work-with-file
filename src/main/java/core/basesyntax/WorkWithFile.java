4f076ddd8558785c6030988905315862b208c46b
White EdenI <danil.gayvoronsky2201@gmail.com>
2023-10-09 23:27:29 +0300

    Refactored everything!!!

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,54 +7,70 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    private final String splitChar = ",";
+    private final String supplyString = "supply";
+    private final String buyString = "buy";
+    private final String resultString = "result";
+    private int totalSupply = 0;
+    private int totalBuy = 0;
+    private int result = 0;
+
     public void getStatistic(String fromFileName, String toFileName) {
-        writeToFile(readFile(fromFileName), toFileName);
+        totalSupply = 0;
+        totalBuy = 0;
+        result = 0;
+        String fileContent = readFile(fromFileName);
+        calculateStatistics(fileContent);
+        writeToFile(toFileName);
     }
 
-    private StringBuilder readFile(String fromFileName) {
+    private String readFile(String fromFileName) {
         StringBuilder stringBuilder = new StringBuilder();
-        final String splitChar = ",";
-        final String supplyString = "supply";
-        final String buyString = "buy";
-        final String resultString = "result";
         try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
-            int totalSupply = 0;
-            int totalBuy = 0;
             String line;
             while ((line = reader.readLine()) != null) {
-                String[] parts = line.split(splitChar);
-                if (parts.length == 2) {
-                    int amount = Integer.parseInt(parts[1].trim());
-                    if (parts[0].trim().equals(supplyString)) {
-                        totalSupply += amount;
-                    } else if (parts[0].trim().equals(buyString)) {
-                        totalBuy += amount;
-                    }
-                }
+                stringBuilder.append(line).append(System.lineSeparator());
             }
-            int result = totalSupply - totalBuy;
-            stringBuilder.append(supplyString)
-                    .append(splitChar)
-                    .append(totalSupply)
-                    .append(System.lineSeparator())
-                    .append(buyString)
-                    .append(splitChar)
-                    .append(totalBuy)
-                    .append(System.lineSeparator())
-                    .append(resultString)
-                    .append(splitChar)
-                    .append(result);
-            return stringBuilder;
         } catch (IOException e) {
-            throw new RuntimeException("Failed to read from file file: " + fromFileName, e);
+            throw new RuntimeException("Failed to read from file: " + fromFileName, e);
         }
+        return stringBuilder.toString();
     }
 
-    private void writeToFile(StringBuilder someBuilder, String toFileName) {
+    private void calculateStatistics(String fileContent) {
+        final int partsCheckValue = 2;
+        String[] lines = fileContent.split(System.lineSeparator());
+        for (String line : lines) {
+            String[] parts = line.split(splitChar);
+            if (parts.length == partsCheckValue) {
+                int amount = Integer.parseInt(parts[1].trim());
+                if (parts[0].trim().equals(supplyString)) {
+                    totalSupply += amount;
+                } else if (parts[0].trim().equals(buyString)) {
+                    totalBuy += amount;
+                }
+            }
+        }
+        result = totalSupply - totalBuy;
+    }
+
+    private void writeToFile(String toFileName) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(supplyString)
+                .append(splitChar)
+                .append(totalSupply)
+                .append(System.lineSeparator())
+                .append(buyString)
+                .append(splitChar)
+                .append(totalBuy)
+                .append(System.lineSeparator())
+                .append(resultString)
+                .append(splitChar)
+                .append(result);
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
-            bufferedWriter.write(someBuilder.toString());
+            bufferedWriter.write(stringBuilder.toString());
         } catch (IOException e) {
-            throw new RuntimeException("Failed to write in file: " + toFileName,e);
+            throw new RuntimeException("Failed to write in file: " + toFileName, e);
         }
     }
 }
