53d3ef0d5d18441ed55e7753920d2afa1851a5a4
Vlasenko Yevhenii <vlasenkoevy@gmail.com>
2023-10-09 13:28:49 +0300

    fixed according to comments

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,33 +7,47 @@ import java.io.IOException;
 import java.nio.file.Files;
 
 public class WorkWithFile {
-
     public void getStatistic(String fromFileName, String toFileName) {
-        File sourceFile = new File(fromFileName);
-        File destinationfile = new File(toFileName);
-        try (BufferedReader reader = new BufferedReader(new FileReader(sourceFile))) {
+        int[] dataToWrite = readFromFile(fromFileName);
+        writeToFile(toFileName, dataToWrite);
+    }
+
+    private int[] readFromFile(String fileName) {
+        File file = new File(fileName);
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             String readerString = reader.readLine();
             int supplyCount = 0;
             int buyCount = 0;
-            int operatedAmount;
-            String[] splittedLine;
             while (readerString != null) {
-                splittedLine = readerString.split(",");
-                operatedAmount = Integer.parseInt(splittedLine[1]);
-                if (splittedLine[0].equals("supply")) {
+                final String[] splittedLine = readerString.split(",");
+                final String operationType = splittedLine[0];
+                final int operatedAmount = Integer.parseInt(splittedLine[1]);
+                if (operationType.equals("supply")) {
                     supplyCount += operatedAmount;
                 } else {
                     buyCount += operatedAmount;
                 }
                 readerString = reader.readLine();
             }
-            String resultData = "supply," + supplyCount + System.lineSeparator()
-                    + "buy," + buyCount + System.lineSeparator()
-                    + "result," + (supplyCount - buyCount);
+            return new int[]{supplyCount, buyCount};
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file" + fileName, e);
+        }
+    }
+
+    private void writeToFile(String destinationFileName, int[] dataArray) {
+        File destinationfile = new File(destinationFileName);
+        int supplyCount = dataArray[0];
+        int buyCount = dataArray[1];
+        try {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append("supply,").append(supplyCount).append(System.lineSeparator());
+            stringBuilder.append("buy,").append(buyCount).append(System.lineSeparator());
+            stringBuilder.append("result,").append(supplyCount - buyCount);
+            String resultData = stringBuilder.toString();
             Files.write(destinationfile.toPath(), resultData.getBytes());
         } catch (IOException e) {
-            throw new RuntimeException("Statistics can't be generated", e);
+            throw new RuntimeException("Can't write to file" + destinationFileName, e);
         }
     }
 }
-
