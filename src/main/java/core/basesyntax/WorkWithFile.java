de162f083844f3432ecd616b714ae530453ddf79
Влад <vladlesn86@gmail.com>
2023-10-10 14:26:37 +0300

    code has been improved

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,37 +1,39 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    private static final String SUPPLY = "supply";
-    private static final String BUY = "buy";
-    private static final String RESULT = "result";
-    private static final String WORD_SPLIT_PATTERN = "\\W+";
-    private static final String COMMA = ",";
+    private static final String supply = "supply";
+    private static final String buy = "buy";
+    private static final String result = "result";
+    private static final String wordSplitPattern = "\\W+";
+    private static final String comma = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        File mainFile = new File(fromFileName);
-        String[] words = readFromFile(mainFile).split(WORD_SPLIT_PATTERN);
-
-        int sumSupply = 0;
-        int buySupply = 0;
-
+        final File mainFile = new File(fromFileName);
+        final String[] words = readFromFile(mainFile).split(wordSplitPattern);
+        int sum = 0;
+        int buy = 0;
         for (int i = 0; i < words.length; i += 2) {
             String currentWord = words[i];
-            if (currentWord.equals(SUPPLY)) {
-                sumSupply += Integer.parseInt(words[i + 1]);
-            }
-            if (currentWord.equals(BUY)) {
-                buySupply += Integer.parseInt(words[i + 1]);
+            int quantity = Integer.parseInt(words[i + 1]);
+
+            if (currentWord.equals(supply)) {
+                sum += quantity;
+            } else {
+                buy += quantity;
             }
         }
 
-        String report = generateReport(sumSupply, buySupply);
+        writeReport(toFileName, generateReport(sum, buy));
+    }
+
+    private void writeReport(String toFileName, String report) {
         File outputFile = new File(toFileName);
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(outputFile))) {
             bufferedWriter.write(report);
@@ -44,23 +46,20 @@ public class WorkWithFile {
         int resultSum = supplySum - buySum;
 
         return new StringBuilder()
-                .append(SUPPLY).append(COMMA).append(supplySum).append(System.lineSeparator())
-                .append(BUY).append(COMMA).append(buySum).append(System.lineSeparator())
-                .append(RESULT).append(COMMA).append(resultSum).toString();
-
+                .append(supply).append(comma).append(supplySum).append(System.lineSeparator())
+                .append(buy).append(comma).append(buySum).append(System.lineSeparator())
+                .append(result).append(comma).append(resultSum).toString();
     }
 
     private String readFromFile(File inputFile) {
         StringBuilder stringBuilder = new StringBuilder();
-        try (FileReader fileReader = new FileReader(inputFile)) {
-            int value;
-            while ((value = fileReader.read()) != -1) {
-                stringBuilder.append((char) value);
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFile))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line).append(System.lineSeparator());
             }
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException("Failed find a file: " + inputFile.getName(), e);
         } catch (IOException e) {
-            throw new RuntimeException("Failed read from a file: " + inputFile.getName(), e);
+            throw new RuntimeException("Failed to read from a file: " + inputFile.getName(), e);
         }
         return stringBuilder.toString();
     }
