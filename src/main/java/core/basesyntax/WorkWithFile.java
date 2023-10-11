f3e8c55393d89bab2040ddfda6706040be97c912
Denys Okhrymovych <denys.ohrymovych.2015@gmail.com>
2023-10-11 15:12:47 +0300

    remove unnecessary File creation, replace magic numbers

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -10,26 +10,29 @@ import java.io.IOException;
 public class WorkWithFile {
     private static final String OPERATION_1 = "supply";
     private static final String OPERATION_2 = "buy";
+    private static final String RESULT = "result";
+    private static final int INDEX_FOR_CURRENT_OPERATION = 0;
+    private static final int INDEX_FOR_OPERATION_1_TOTAL_AMOUNT = 0;
+    private static final int INDEX_FOR_OPERATION_2_TOTAL_AMOUNT = 1;
     private static final String OPERATION_AND_AMOUNT_SEPARATOR = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        File fromFile = new File(fromFileName);
         File toFile = new File(toFileName);
-        int[] totalAmounts = readFromFileAndGetAmounts(fromFile, fromFileName);
+        int[] totalAmounts = readFromFileAndGetAmounts(fromFileName);
         String report = createReport(totalAmounts);
         writeToFile(toFile, toFileName, report);
     }
 
-    private int[] readFromFileAndGetAmounts(File fromFile, String fromFileName) {
+    private int[] readFromFileAndGetAmounts(String fromFileName) {
         int supplyTotalAmount = 0;
         int buyTotalAmount = 0;
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFile))) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             String currentLine = bufferedReader.readLine();
             while (currentLine != null) {
                 String[] currentOpearationAndAmount
                         = currentLine.split(OPERATION_AND_AMOUNT_SEPARATOR);
                 int amount = Integer.parseInt(currentOpearationAndAmount[1]);
-                if (currentOpearationAndAmount[0].equals(OPERATION_1)) {
+                if (currentOpearationAndAmount[INDEX_FOR_CURRENT_OPERATION].equals(OPERATION_1)) {
                     supplyTotalAmount += amount;
                 } else {
                     buyTotalAmount += amount;
@@ -45,11 +48,14 @@ public class WorkWithFile {
     private String createReport(int[] totalAmounts) {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append(OPERATION_1 + OPERATION_AND_AMOUNT_SEPARATOR)
-                .append(totalAmounts[0]).append(System.lineSeparator());
+                .append(totalAmounts[INDEX_FOR_OPERATION_1_TOTAL_AMOUNT])
+                .append(System.lineSeparator());
         stringBuilder.append(OPERATION_2 + OPERATION_AND_AMOUNT_SEPARATOR)
-                .append(totalAmounts[1]).append(System.lineSeparator());
-        stringBuilder.append("result" + OPERATION_AND_AMOUNT_SEPARATOR)
-                .append(totalAmounts[0] - totalAmounts[1]);
+                .append(totalAmounts[INDEX_FOR_OPERATION_2_TOTAL_AMOUNT])
+                .append(System.lineSeparator());
+        stringBuilder.append(RESULT + OPERATION_AND_AMOUNT_SEPARATOR)
+                .append(totalAmounts[INDEX_FOR_OPERATION_1_TOTAL_AMOUNT]
+                        - totalAmounts[INDEX_FOR_OPERATION_2_TOTAL_AMOUNT]);
         return stringBuilder.toString();
     }
 
