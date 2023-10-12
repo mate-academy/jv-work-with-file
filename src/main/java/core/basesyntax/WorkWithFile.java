bbe22d0abbb54ca0a429817699f9923effe6c586
Yevhenii Liashenko <paradoxrial@gmail.com>
2023-10-12 15:50:21 +0300

    create method createReport

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -12,11 +12,39 @@ import java.util.Map;
 public class WorkWithFile {
     private static final int INDEX_OPERATION_TYPE = 0;
     private static final int INDEX_AMOUNT = 1;
-    private Map<String, Integer> category;
+    private Map<String, Integer> category = new HashMap<>();
 
     public void getStatistic(String fromFileName, String toFileName) {
+        List<String> dataFromFile = readFile(fromFileName);
+        String report = createReport(dataFromFile);
+        writeToFile(report, toFileName);
+    }
+
+    private List<String> readFile(String fileName) {
+        try {
+            return Files.readAllLines(Path.of(fileName), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read data from the file " + fileName, e);
+        }
+    }
+
+    private void writeToFile(String data, String fileName) {
+        try (FileWriter fileWriter = new FileWriter(fileName)) {
+            fileWriter.write(data);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write data to the file " + fileName, e);
+        }
+    }
+
+    private void calculateTotalAmount(String operationType, int amount) {
+        int totalAmount = category.getOrDefault(operationType, 0);
+        totalAmount += amount;
+        category.put(operationType, totalAmount);
+    }
+
+    private String createReport(List<String> data) {
         category = new HashMap<>();
-        for (String line : getDataFromFile(fromFileName)) {
+        for (String line : data) {
             String[] parts = line.split(",");
             calculateTotalAmount(parts[INDEX_OPERATION_TYPE],
                     Integer.parseInt(parts[INDEX_AMOUNT]));
@@ -30,28 +58,6 @@ public class WorkWithFile {
                 .append(System.lineSeparator());
         result.append("result,")
                 .append(category.get("supply") - category.get("buy"));
-        writeDataToFile(toFileName, result.toString());
-    }
-
-    private List<String> getDataFromFile(String fileName) {
-        try {
-            return Files.readAllLines(Path.of(fileName), StandardCharsets.UTF_8);
-        } catch (IOException e) {
-            throw new RuntimeException("Can't read data from the file " + fileName, e);
-        }
-    }
-
-    private void writeDataToFile(String fileName, String data) {
-        try (FileWriter fileWriter = new FileWriter(fileName)) {
-            fileWriter.write(data);
-        } catch (IOException e) {
-            throw new RuntimeException("Can't write data to the file " + fileName, e);
-        }
-    }
-
-    private void calculateTotalAmount(String operationType, int amount) {
-        int totalAmount = category.getOrDefault(operationType, 0);
-        totalAmount += amount;
-        category.put(operationType, totalAmount);
+        return result.toString();
     }
 }
