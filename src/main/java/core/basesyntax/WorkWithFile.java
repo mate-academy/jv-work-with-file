4b870cd83deb8272724c1cc9212e16e7efe0c2d7
khorkovenko <khorkovenko>
2023-10-15 20:08:02 +0300

    refactored functions

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -5,76 +5,69 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Stream;
 
 public class WorkWithFile {
     private static final String BUY = "buy";
     private static final String SUPPLY = "supply";
     private static final String RESULT = "result";
     private static final String DELIMITER = ",";
-    private static final int DEFAULT_VALUE = 0;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        Map<String, Integer> operationCounts = getInfoFromFile(fromFileName);
-        saveStatistics(toFileName, getStatistics(operationCounts));
+        String report = getReport(readLinesFromFile(fromFileName));
+        saveStatistics(toFileName, report);
     }
 
-    public String getStatistics(Map<String, Integer> operationCounts) {
-        int supplyTotal = operationCounts.getOrDefault(SUPPLY, DEFAULT_VALUE);
-        int buyTotal = operationCounts.getOrDefault(BUY, DEFAULT_VALUE);
+    public void saveStatistics(String toFileName, String statistics) {
+        try {
+            Files.writeString(Path.of(toFileName), statistics);
+        } catch (IOException e) {
+            throw new RuntimeException("Error: " + e.getMessage());
+        }
+    }
+
+    private Stream<String> readLinesFromFile(String fromFileName) {
+        try {
+            return Files.lines(Path.of(fromFileName));
+        } catch (IOException e) {
+            throw new RuntimeException("Error while reading from file: " + e.getMessage());
+        }
+    }
+
+    private String getReport(Stream<String> lines) {
+        Map<String, Integer> operationCounts = new HashMap<>();
+
+        lines.map(line -> line.split(DELIMITER))
+                .forEach(parts -> {
+                    String operation = parts[0];
+                    int value = Integer.parseInt(parts[1].trim());
+                    operationCounts
+                            .compute(operation, (key, oldValue)
+                                    -> oldValue == null ? value : oldValue + value);
+                });
+
+        int supplyTotal = operationCounts.getOrDefault("supply", 0);
+        int buyTotal = operationCounts.getOrDefault("buy", 0);
         int result = supplyTotal - buyTotal;
 
         StringBuilder statistics = new StringBuilder();
         statistics
-                .append(SUPPLY)
+                .append("supply")
                 .append(DELIMITER)
                 .append(supplyTotal)
                 .append(System.lineSeparator());
 
         statistics
-                .append(BUY)
+                .append("buy")
                 .append(DELIMITER)
                 .append(buyTotal)
                 .append(System.lineSeparator());
 
         statistics
-                .append(RESULT)
+                .append("result")
                 .append(DELIMITER)
                 .append(result);
 
         return statistics.toString();
     }
-
-    public void saveStatistics(String toFileName, String statistics) {
-        try {
-            Files.writeString(Path.of(toFileName), statistics.toString());
-
-            System.out.println("Statistics successfully written to " + toFileName);
-        } catch (IOException e) {
-            System.out.println("Error: " + e.getMessage());
-        }
-    }
-
-    public Map<String, Integer> getInfoFromFile(String fromFileName) {
-        Map<String, Integer> operationCounts = new HashMap<>();
-
-        try (var lines = Files.lines(Path.of(fromFileName))) {
-            lines.map(line -> line.split(DELIMITER))
-                    .forEach(parts -> {
-                        String operation = parts[0];
-                        int value = Integer.parseInt(parts[1].trim());
-                        operationCounts
-                                .put(
-                                        operation,
-                                        operationCounts
-                                                .getOrDefault(operation, DEFAULT_VALUE)
-                                                + value);
-                    });
-
-            return operationCounts;
-        } catch (IOException e) {
-            System.out.println("Error while reading from file: " + e.getMessage());
-            return operationCounts;
-        }
-    }
 }
-
