5718d1389f8eeee0b234f149d3a25f7da98ad9fd
Vadim <waterfallkadas@gmail.com>
2023-10-15 07:22:51 +0300

    first solution 15/10

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,50 @@
 package core.basesyntax;
 
-public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class WorkWithFile {
+    private static final int OPERATION = 0;
+    private static final int AMOUNT = 1;
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        int buy = 0;
+        int supply = 0;
+        String [] lineInfo;
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
+            String readLine = bufferedReader.readLine();
+            while (readLine != null) {
+                lineInfo = readLine.split(",");
+                if (lineInfo[OPERATION].equals("buy")) {
+                    buy += Integer.parseInt(lineInfo[AMOUNT]);
+                }
+                if (lineInfo[OPERATION].equals("supply")) {
+                    supply += Integer.parseInt(lineInfo[AMOUNT]);
+                }
+                readLine = bufferedReader.readLine();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Can't read data from the file " + fromFileName, e);
+        }
+        createReport(buy,supply,toFileName);
+    }
+
+    public void createReport(int buy, int supply, String toFileName) {
+        String report = ("supply," + supply + System.lineSeparator()
+                + "buy," + buy + System.lineSeparator() + "result," + (supply - buy));
+        writeToFile(report, toFileName);
+    }
+
+    public void writeToFile(String report, String toFileName) {
+        File file = new File(toFileName);
+        try (BufferedWriter stringBuffer = new BufferedWriter(new FileWriter(file))) {
+            stringBuffer.write(report);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write data to " + toFileName, e);
+        }
     }
 }
