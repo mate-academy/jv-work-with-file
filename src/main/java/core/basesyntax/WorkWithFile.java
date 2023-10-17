287d98031ed3c02c025e1bf00318e57eb77966e5
Oleh Shvets <olehpost69@gmail.com>
2023-10-17 19:40:15 +0300

    my-solution_work_with_file

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,59 @@
 package core.basesyntax;
 
-public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class WorkWithFile {
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        BufferedReader bufferedReader = null;
+        BufferedWriter bufferedWriter = null;
+        try {
+            bufferedReader = new BufferedReader(new FileReader(fromFileName));
+            bufferedWriter = new BufferedWriter(new FileWriter(toFileName));
+
+            int supplyTotal = 0;
+            int buyTotal = 0;
+
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] data = line.split(",");
+                if (data.length != 2) {
+                    System.out.println("Invalid data format: " + line);
+                    continue;
+                }
+                String operationType = data[0];
+                int amount = Integer.parseInt(data[1]);
+
+                if (operationType.equalsIgnoreCase(SUPPLY)) {
+                    supplyTotal += amount;
+                } else if (operationType.equalsIgnoreCase(BUY)) {
+                    buyTotal += amount;
+                }
+            }
+            int result = supplyTotal - buyTotal;
+            bufferedWriter.write("supply," + supplyTotal);
+            bufferedWriter.newLine();
+            bufferedWriter.write("buy," + buyTotal);
+            bufferedWriter.newLine();
+            bufferedWriter.write("result," + result);
+        } catch (
+        IOException e) {
+            throw new RuntimeException("Can't read file:" + fromFileName, e);
+        } finally {
+            if (bufferedReader != null) {
+                try {
+                    bufferedReader.close();
+                    bufferedWriter.close();
+                } catch (IOException e) {
+                    throw new RuntimeException("Can`t close resource", e);
+                }
+            }
+        }
     }
 }
