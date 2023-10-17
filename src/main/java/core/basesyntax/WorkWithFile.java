c6be0f7694ab68cb48136a70fc8e0cbc0c615d0f
Roman Kuzyk <kuzya228337@mail.com>
2023-10-17 17:52:47 +0300

    It opens a file with the name fromFileName for reading using a BufferedReader, which allows reading the file line by line. It creates variables buyTotal and supplyTotal to store the sum of "buy" and "supply" operations, respectively. It starts a loop to parse each line in the file, splitting it into parts using a comma (,). It extracts the operation (the first part of the line) and the quantity (the second part of the line), parsing the quantity as an integer. If the operation is "supply," the amount value is added to the supplyTotal variable. If the operation is "buy," the amount value is added to the buyTotal variable. The process of parsing lines continues until the end of the file is reached. After processing all the lines, it calculates the difference between the sum of "supply" and "buy" operations and stores the result in the result variable. It then opens a file with the name toFileName for writing using a BufferedWriter, and the following lines are written to this file: "supply," and the value of supplyTotal. "buy," and the value of buyTotal. "result," and the value of result. If an error occurs while working with the files (e.g., the file doesn't exist or there are no permissions to access it), an IOException exception will be thrown, caught, converted into a RuntimeException, and rethrown.

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,44 @@
-package core.basesyntax;
+    package core.basesyntax;
 
-public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
+    import java.io.BufferedReader;
+    import java.io.BufferedWriter;
+    import java.io.FileReader;
+    import java.io.FileWriter;
+    import java.io.IOException;
 
+    public class WorkWithFile {
+        public void getStatistic(String fromFileName, String toFileName) {
+            int buyTotal = 0;
+            int supplyTotal = 0;
+
+            try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
+                String line = bufferedReader.readLine();
+                while (line != null) {
+                    String[] values = line.split(",");
+                    String operation = values[0];
+                    int amount = Integer.parseInt(values[1]);
+
+                    if ("supply".equals(operation)) {
+                        supplyTotal += amount;
+                    } else if ("buy".equals(operation)) {
+                        buyTotal += amount;
+                    }
+                    line = bufferedReader.readLine();
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            int result = supplyTotal - buyTotal;
+
+            try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+                bufferedWriter.write("supply," + supplyTotal);
+                bufferedWriter.newLine();
+                bufferedWriter.write("buy," + buyTotal);
+                bufferedWriter.newLine();
+                bufferedWriter.write("result," + result);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
-}
