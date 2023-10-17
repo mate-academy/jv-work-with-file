df84966eeef10db17fb9ddd15d4e6a0e43f461eb
Roman Kuzyk <kuzya228337@mail.com>
2023-10-17 18:01:49 +0300

    Fix build

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,44 +1,44 @@
-    package core.basesyntax;
+package core.basesyntax;
 
-    import java.io.BufferedReader;
-    import java.io.BufferedWriter;
-    import java.io.FileReader;
-    import java.io.FileWriter;
-    import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
-    public class WorkWithFile {
-        public void getStatistic(String fromFileName, String toFileName) {
-            int buyTotal = 0;
-            int supplyTotal = 0;
+public class WorkWithFile {
+    public void getStatistic(String fromFileName, String toFileName) {
+        int buyTotal = 0;
+        int supplyTotal = 0;
 
-            try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
-                String line = bufferedReader.readLine();
-                while (line != null) {
-                    String[] values = line.split(",");
-                    String operation = values[0];
-                    int amount = Integer.parseInt(values[1]);
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
+            String line = bufferedReader.readLine();
+            while (line != null) {
+                String[] values = line.split(",");
+                String operation = values[0];
+                int amount = Integer.parseInt(values[1]);
 
-                    if ("supply".equals(operation)) {
-                        supplyTotal += amount;
-                    } else if ("buy".equals(operation)) {
-                        buyTotal += amount;
-                    }
-                    line = bufferedReader.readLine();
+                if ("supply".equals(operation)) {
+                    supplyTotal += amount;
+                } else if ("buy".equals(operation)) {
+                    buyTotal += amount;
                 }
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+                line = bufferedReader.readLine();
             }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
-            int result = supplyTotal - buyTotal;
+        int result = supplyTotal - buyTotal;
 
-            try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
-                bufferedWriter.write("supply," + supplyTotal);
-                bufferedWriter.newLine();
-                bufferedWriter.write("buy," + buyTotal);
-                bufferedWriter.newLine();
-                bufferedWriter.write("result," + result);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+            bufferedWriter.write("supply," + supplyTotal);
+            bufferedWriter.newLine();
+            bufferedWriter.write("buy," + buyTotal);
+            bufferedWriter.newLine();
+            bufferedWriter.write("result," + result);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
+}
