4c921e5a633a00303cd1c673dc381a268edcf67b
Oleh Shvets <olehpost69@gmail.com>
2023-10-17 21:13:54 +0300

    my-changed_solution_work_with_file

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -10,50 +10,67 @@ public class WorkWithFile {
     private static final String SUPPLY = "supply";
     private static final String BUY = "buy";
 
+    private static final String GAP = " ";
+    private static final String COMMA = ",";
+    private static final String RESULT = "result";
+
     public void getStatistic(String fromFileName, String toFileName) {
-        BufferedReader bufferedReader = null;
-        BufferedWriter bufferedWriter = null;
-        try {
-            bufferedReader = new BufferedReader(new FileReader(fromFileName));
-            bufferedWriter = new BufferedWriter(new FileWriter(toFileName));
+        String data = readFile(fromFileName);
+        String report = generateReport(data);
+        writeToFile(toFileName, report);
+    }
 
-            int supplyTotal = 0;
-            int buyTotal = 0;
-
+    private String readFile(String fileName) {
+        StringBuilder builder = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
             String line;
             while ((line = bufferedReader.readLine()) != null) {
-                String[] data = line.split(",");
-                if (data.length != 2) {
-                    System.out.println("Invalid data format: " + line);
-                    continue;
-                }
-                String operationType = data[0];
-                int amount = Integer.parseInt(data[1]);
+                builder.append(line).append(GAP);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file: " + fileName, e);
+        }
+        return builder.toString();
+    }
 
-                if (operationType.equalsIgnoreCase(SUPPLY)) {
-                    supplyTotal += amount;
-                } else if (operationType.equalsIgnoreCase(BUY)) {
-                    buyTotal += amount;
-                }
+    private String generateReport(String data) {
+        int supplyTotal = 0;
+        int buyTotal = 0;
+
+        String[] lines = data.split(GAP);
+        StringBuilder reportBuilder = new StringBuilder();
+
+        for (String line : lines) {
+            String[] parts = line.split(COMMA);
+            if (parts.length != 2) {
+                System.out.println("Invalid data format: " + line);
+                continue;
             }
-            int result = supplyTotal - buyTotal;
-            bufferedWriter.write("supply," + supplyTotal);
-            bufferedWriter.newLine();
-            bufferedWriter.write("buy," + buyTotal);
-            bufferedWriter.newLine();
-            bufferedWriter.write("result," + result);
-        } catch (
-        IOException e) {
-            throw new RuntimeException("Can't read file:" + fromFileName, e);
-        } finally {
-            if (bufferedReader != null) {
-                try {
-                    bufferedReader.close();
-                    bufferedWriter.close();
-                } catch (IOException e) {
-                    throw new RuntimeException("Can`t close resource", e);
-                }
+            String operationType = parts[0];
+            int amount = Integer.parseInt(parts[1]);
+
+            if (operationType.equalsIgnoreCase(SUPPLY)) {
+                supplyTotal += amount;
+            } else if (operationType.equalsIgnoreCase(BUY)) {
+                buyTotal += amount;
             }
+        }
+
+        int result = supplyTotal - buyTotal;
+
+        reportBuilder.append(SUPPLY).append(COMMA).append(supplyTotal)
+                .append(System.lineSeparator());
+        reportBuilder.append(BUY).append(COMMA).append(buyTotal).append(System.lineSeparator());
+        reportBuilder.append(RESULT).append(COMMA).append(result);
+
+        return reportBuilder.toString();
+    }
+
+    private void writeToFile(String fileName, String report) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName))) {
+            bufferedWriter.write(report);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write to file: " + fileName, e);
         }
     }
 }
