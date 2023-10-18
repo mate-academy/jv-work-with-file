abcfaf8e55a2bb2cd7b959c0dcc983cdf3841759
Roman Kuzyk <kuzya228337@mail.com>
2023-10-18 18:13:30 +0300

    Added fixes after review

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,37 +8,49 @@ import java.io.IOException;
 
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
+        try {
+            String data = readFile(fromFileName);
+            String report = generateReport(data);
+            writeToFile(toFileName, report);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String readFile(String fromFileName) throws IOException {
+        StringBuilder data = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                data.append(line).append("\n");
+            }
+        }
+        return data.toString();
+    }
+
+    private String generateReport(String data) {
         int buyTotal = 0;
         int supplyTotal = 0;
-
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
-            String line = bufferedReader.readLine();
-            while (line != null) {
-                String[] values = line.split(",");
+        String[] lines = data.split("\n");
+        for (String line : lines) {
+            String[] values = line.split(",");
+            if (values.length == 2) {
                 String operation = values[0];
                 int amount = Integer.parseInt(values[1]);
-
                 if ("supply".equals(operation)) {
                     supplyTotal += amount;
                 } else if ("buy".equals(operation)) {
                     buyTotal += amount;
                 }
-                line = bufferedReader.readLine();
             }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
-
         int result = supplyTotal - buyTotal;
+        return "supply," + supplyTotal + "\nbuy," + buyTotal + "\nresult," + result;
+    }
 
+    private void writeToFile(String toFileName, String report) throws IOException {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
-            bufferedWriter.write("supply," + supplyTotal);
-            bufferedWriter.newLine();
-            bufferedWriter.write("buy," + buyTotal);
-            bufferedWriter.newLine();
-            bufferedWriter.write("result," + result);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+            bufferedWriter.write(report);
         }
     }
 }
