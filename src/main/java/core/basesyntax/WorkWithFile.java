b3bbd57c49c7a2703e86091b21dd51c9f3a458b6
Oleksandra Dorofieieva <olexsandradorofeieiva@gmail.com>
2023-10-19 10:20:01 +0300

    implements class WorkWithFile distribute the code logic intro methods: getStatistic / readFile / createReport / writeToFile make constant SUPPLY and BUY

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,60 @@
 package core.basesyntax;
 
-public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class WorkWithFile {
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        String[] dataFromFile = readFile(fromFileName);
+        String report = createReport(dataFromFile);
+        writeToFile(toFileName, report);
+    }
+
+    private String[] readFile(String fromFileName) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
+            int sumSupply = 0;
+            int sumBuy = 0;
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] split = line.split(",");
+                String operation = split[0];
+                int digit = Integer.parseInt(split[1]);
+
+                if (operation.equals(SUPPLY)) {
+                    sumSupply += digit;
+                } else {
+                    sumBuy += digit;
+                }
+            }
+            return new String[]{String.valueOf(sumSupply), String.valueOf(sumBuy)};
+        } catch (IOException exception) {
+            throw new RuntimeException("Can`t read data from file " + fromFileName, exception);
+        }
+    }
+
+    private String createReport(String[] resultInfo) {
+        int supply = Integer.parseInt(resultInfo[0]);
+        int buy = Integer.parseInt(resultInfo[1]);
+        int result = supply - buy;
+
+        return new StringBuilder()
+                .append(SUPPLY).append(",").append(supply).append(System.lineSeparator())
+                .append(BUY).append(",").append(buy).append(System.lineSeparator())
+                .append("result,").append(result).append(System.lineSeparator())
+                .toString();
+    }
+
+    private void writeToFile(String toFileName, String report) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
+            writer.write(report);
+        } catch (IOException exception) {
+            throw new RuntimeException("Can`t write data to file " + toFileName, exception);
+        }
     }
 }
