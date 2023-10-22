52f73c04b0114b11260935341bca91808c0306f1
danX <danildryzov@gmail.com>
2023-10-22 19:03:06 +0200

    improved code readability

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -10,6 +10,8 @@ import java.io.IOException;
 public class WorkWithFile {
     private static final int CONST_VALUE = 0;
     private static final int STEP_SIZE = 2;
+    private static final String SUPPLY_OPERATION = "supply";
+    private static final String BUY_OPERATION = "buy";
 
     public void getStatistic(String fromFileName, String toFileName) {
         String data = readFile(fromFileName);
@@ -18,53 +20,57 @@ public class WorkWithFile {
     }
 
     public String readFile(String fromFileName) {
-        StringBuilder stringBuilder = new StringBuilder();
+        StringBuilder fileContents = new StringBuilder();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             String readLine = bufferedReader.readLine();
             while (readLine != null) {
                 String[] matchLines = readLine.split("\\W+");
                 for (String word : matchLines) {
-                    stringBuilder.append(word).append(" ");
+                    fileContents.append(word).append(" ");
                 }
                 readLine = bufferedReader.readLine();
             }
         } catch (FileNotFoundException e) {
-            throw new RuntimeException("file" + fromFileName + "doesn't exist" + e);
+            throw new RuntimeException("file" + fromFileName + "doesn't exist", e);
         } catch (IOException e) {
-            throw new RuntimeException("Can`t read file " + fromFileName + " " + e);
+            throw new RuntimeException("Can`t read file " + fromFileName, e);
         }
-        return stringBuilder.toString();
+        return fileContents.toString();
     }
 
     public String createReport(String report) {
-        int supplyCounter = 0;
-        int buyCounter = 0;
+        int supplyCounter = CONST_VALUE;
+        int buyCounter = CONST_VALUE;
 
         String[] parts = report.split("\\W+");
 
-        for (int i = 0; i < parts.length; i += 2) {
+        for (int i = CONST_VALUE; i < parts.length; i += STEP_SIZE) {
             String operation = parts[i];
             int value = Integer.parseInt(parts[i + 1]);
 
-            if (operation.equals("supply")) {
-                supplyCounter += value;
-            } else if (operation.equals("buy")) {
-                buyCounter += value;
+            switch (operation) {
+                case SUPPLY_OPERATION:
+                    supplyCounter += value;
+                    break;
+                case BUY_OPERATION:
+                    buyCounter += value;
+                    break;
+                default:
             }
         }
 
-        return new StringBuilder()
-                .append("supply,").append(supplyCounter).append(System.lineSeparator())
-                .append("buy,").append(buyCounter).append(System.lineSeparator())
-                .append("result,").append(supplyCounter - buyCounter)
-                .toString();
+        int result = supplyCounter - buyCounter;
+
+        return "supply," + supplyCounter + System.lineSeparator()
+                + "buy," + buyCounter + System.lineSeparator()
+                + "result," + result;
     }
 
     public void writeToFile(String toFileName, String report) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
             bufferedWriter.write(report);
         } catch (IOException e) {
-            throw new RuntimeException("Can`t write data to file " + toFileName + " " + e);
+            throw new RuntimeException("Can`t write data to file " + toFileName, e);
         }
     }
 }
