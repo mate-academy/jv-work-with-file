05207f9a1178f06f88782a79e104415cff83f846
Kozharin-Vasul <kozharinvasyl@gmail.com>
2023-10-23 18:13:42 +0300

    Made read and write

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,47 @@
 package core.basesyntax;
 
-public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
+import java.io.*;
 
+public class WorkWithFile {
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+    private static final String RESULT = "result";
+    private static final String SEPARATOR = ",";
+    private static final int FIRST = 0;
+    private static final int SECOND = 1;
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        int[] data = readFromFile(fromFileName);
+        writeToFile(toFileName, SUPPLY, data[FIRST], false);
+        writeToFile(toFileName, BUY, data[SECOND], true);
+        writeToFile(toFileName, RESULT, data[FIRST] - data[SECOND], true);
+    }
+
+    private int[] readFromFile(String fromFileName){
+        int[] result = new int[] {FIRST,FIRST};
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
+            String value = reader.readLine();
+            while (value != null) {
+                String [] option = value.split(",");
+                if (option[FIRST].equals(SUPPLY)) {
+                    result[FIRST] +=  Integer.valueOf(option[SECOND]);
+                } else if (option[FIRST].equals(BUY)){
+                    result[SECOND] += Integer.valueOf(option[SECOND]);
+                }
+                value = reader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
+        return result;
+    }
+
+    private void writeToFile(String toFileName, String name, int value, boolean append){
+        File writeFile = new File(toFileName);
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(writeFile, append))) {
+            writer.write(name + SEPARATOR + value + System.lineSeparator());
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write file", e);
+        }
     }
 }
