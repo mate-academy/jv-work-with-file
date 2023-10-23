285e84ac53179b4ee6565c349c21e226b32ffa9e
TkachukTara <zaz42988@gmail.com>
2023-10-23 13:23:04 +0300

    Done WorkWithFile Task!V5

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -5,24 +5,23 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WorkWithFile {
     private static final int SUPPLY_INDEX = 0;
     private static final int BUY_INDEX = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        int[] totals = processLines(readFile(fromFileName));
+        List<String> lines = readFile(fromFileName);
+        int[] totals = processLines(lines);
         int supplyTotal = totals[SUPPLY_INDEX];
         int buyTotal = totals[BUY_INDEX];
 
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
-            writeResults(writer, supplyTotal, buyTotal);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        writeResults(toFileName, supplyTotal, buyTotal);
     }
 
-    private int[] processLines(String[] lines) {
+    private int[] processLines(List<String> lines) {
         int supplyTotal = 0;
         int buyTotal = 0;
 
@@ -43,27 +42,28 @@ public class WorkWithFile {
         return new int[]{supplyTotal, buyTotal};
     }
 
-    private String[] readFile(String fileName) {
-        StringBuilder content = new StringBuilder();
+    private List<String> readFile(String fileName) {
+        List<String> lines = new ArrayList<>();
         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             String line;
             while ((line = reader.readLine()) != null) {
-                content.append(line).append("\n");
+                lines.add(line);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return content.toString().split("\n");
+        return lines;
     }
 
-    private void writeResults(BufferedWriter writer, int supplyTotal, int buyTotal)
-            throws IOException {
+    private void writeResults(String toFileName, int supplyTotal, int buyTotal) {
         int result = supplyTotal - buyTotal;
 
-        writer.write("supply," + supplyTotal);
-        writer.newLine();
-        writer.write("buy," + buyTotal);
-        writer.newLine();
-        writer.write("result," + result);
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
+            writer.write(String.format("supply,%d\n", supplyTotal));
+            writer.write(String.format("buy,%d\n", buyTotal));
+            writer.write(String.format("result,%d\n", result));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
