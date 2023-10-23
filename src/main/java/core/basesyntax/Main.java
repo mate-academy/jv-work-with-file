42e55edc80b3781b7a32c73d5763e5a582522f3d
Taras Kushnir <taras.kushnir.tk@gmail.com>
2023-10-23 15:03:14 +0200

    Create method responsible for one task !

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,22 +1,12 @@
 package core.basesyntax;
 
-import java.io.File;
-import java.io.IOException;
-
 public class Main {
     public static void main(String[] args) {
+        String inputFileName = "apple.csv";
+        String outputFileName = "appleResult.csv";
+
         WorkWithFile workWithFile = new WorkWithFile();
-        File file = new File("appleResult.csv");
-        try {
-            file.createNewFile();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        String fromFileName = "apple.csv";
-        String toFileName = file.getPath();
-        workWithFile.getStatistic(fromFileName,toFileName);
-
+        workWithFile.getStatistic(inputFileName, outputFileName);
     }
-
 }
+
