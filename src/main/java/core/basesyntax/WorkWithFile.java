55d936887348ff969748da7edc31e87092bc19c0
TkachukTara <zaz42988@gmail.com>
2023-10-25 12:26:38 +0300

    Done HomeWork task!V3

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -9,20 +9,21 @@ import java.io.IOException;
 public class WorkWithFile {
     private static final int SUPPLY_TYPE = 0;
     private static final int BUY_TYPE = 1;
-    private static final String SUPPLY = "supply";
-    private static final String BUY = "buy";
-    private static final String RESULT = "result";
-
-    private int supply = 0;
-    private int buy = 0;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        supply = 0;
-        buy = 0;
         try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
+            int supply = 0;
+            int buy = 0;
             String line;
             while ((line = reader.readLine()) != null) {
-                processLine(line);
+                String[] parts = line.split(",");
+                int operationType = "supply".equals(parts[0]) ? SUPPLY_TYPE : BUY_TYPE;
+                int amount = Integer.parseInt(parts[1]);
+                if (operationType == SUPPLY_TYPE) {
+                    supply += amount;
+                } else if (operationType == BUY_TYPE) {
+                    buy += amount;
+                }
             }
             int result = supply - buy;
             writeResult(toFileName, supply, buy, result);
@@ -31,22 +32,11 @@ public class WorkWithFile {
         }
     }
 
-    private void processLine(String line) {
-        String[] parts = line.split(",");
-        int operationType = SUPPLY.equals(parts[0]) ? SUPPLY_TYPE : BUY_TYPE;
-        int amount = Integer.parseInt(parts[1]);
-        if (operationType == SUPPLY_TYPE) {
-            supply += amount;
-        } else if (operationType == BUY_TYPE) {
-            buy += amount;
-        }
-    }
-
     private void writeResult(String fileName, int supply, int buy, int result) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
-            writer.write(SUPPLY + "," + supply + "\n");
-            writer.write(BUY + "," + buy + "\n");
-            writer.write(RESULT + "," + result + "\n");
+            writer.write("supply," + supply + "\n");
+            writer.write("buy," + buy + "\n");
+            writer.write("result," + result + "\n");
         } catch (IOException e) {
             throw new RuntimeException("Can't write data to the file", e);
         }
