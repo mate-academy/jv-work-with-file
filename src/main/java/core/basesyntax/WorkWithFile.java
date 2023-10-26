e9520c81555689eed7704e8faf9bca0bdb5c8c8c
Leonid Bondarenko <leonidbondarenko69@gmail.com>
2023-10-26 15:17:38 +0300

    26/10/23 15:18

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -12,42 +12,57 @@ public class WorkWithFile {
     private static final String BUY = "buy";
     private static final String SUPPLY = "supply";
     private static final String COMMA = ",";
+    private static final String RESULT = "result";
+    private static final int NAME_OF_ACTION = 0;
+    private static final int VALUE_OF_ACTION = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
+        String data = readFile(fromFileName);
+        String report = generateReport(data);
+        writeToFile(toFileName, report);
+    }
+
+    private String readFile(String fromFileName) {
         File file = new File(fromFileName);
-        StringBuilder stringBuilder = new StringBuilder();
+        StringBuilder data = new StringBuilder();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
             int value = bufferedReader.read();
             while (value != -1) {
-                stringBuilder.append((char) value);
+                data.append((char) value);
                 value = bufferedReader.read();
             }
         } catch (IOException e) {
-            throw new RuntimeException();
+            throw new RuntimeException("C`ant read file", e);
         }
+        return data.toString();
+    }
 
-        String[] names = stringBuilder.toString().split(DELIMITER);
+    private String generateReport(String data) {
+        String[] names = data.toString().split(DELIMITER);
         int sumSupply = 0;
         int sumBuy = 0;
         for (String name : names) {
             String[] results = name.split(COMMA);
-            if (results[0].equals(SUPPLY)) {
-                int sum = Integer.parseInt(results[1]);
+            if (results[NAME_OF_ACTION].equals(SUPPLY)) {
+                int sum = Integer.parseInt(results[VALUE_OF_ACTION]);
                 sumSupply += sum;
             }
-            if (results[0].equals(BUY)) {
-                int sum = Integer.parseInt(results[1]);
+            if (results[NAME_OF_ACTION].equals(BUY)) {
+                int sum = Integer.parseInt(results[VALUE_OF_ACTION]);
                 sumBuy += sum;
             }
         }
+        return SUPPLY + COMMA + sumSupply + System.lineSeparator()
+                + BUY + COMMA + sumBuy + System.lineSeparator()
+                + RESULT + COMMA + (sumSupply - sumBuy);
+    }
 
-        File file1 = new File(toFileName);
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file1))) {
-            bufferedWriter.write("supply," + sumSupply + System.lineSeparator()
-                    + "buy," + sumBuy + System.lineSeparator()
-                    + "result," + (sumSupply - sumBuy));
+    private void writeToFile(String toFileName, String report) {
+        File file = new File(toFileName);
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
+            bufferedWriter.write(report);
         } catch (IOException e) {
-            throw new RuntimeException();
+            throw new RuntimeException("C`ant write data to file", e);
         }
     }
 }
