c6960667c883f8ebc041cab18dec6b9f8537181a
yevhenreuta <r3utaa@gmail.com>
2023-10-26 14:36:50 +0300

    changed how the methods work : readCsvFile,createReport

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,7 +2,6 @@ package core.basesyntax;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -13,55 +12,56 @@ public class WorkWithFile {
     private static final String BUY = "buy";
     private static final int TRANSACTION_TYPE_INDEX = 0;
     private static final int TRANSACTION_VALUE_INDEX = 1;
-    private static final int SUPPLY_AMOUNT_INDEX = 0;
-    private static final int BUY_AMOUNT_INDEX = 1;
-    private static final int RESULT_AMOUNT_INDEX = 2;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        int[] transactionsValues = readCsvFile(fromFileName);
-        String report = createReport(transactionsValues);
+        String fileContent = readCsvFile(fromFileName);
+        String report = createReport(fileContent);
         writeToFile(report,toFileName);
     }
 
-    private int[] readCsvFile(String fromFileName) {
-        int supply = 0;
-        int buy = 0;
+    private String readCsvFile(String fromFileName) {
+        String line;
+        StringBuilder stringFileContent = new StringBuilder();
 
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
-            String line;
             while ((line = bufferedReader.readLine()) != null) {
-                String[] transaction = line.split(SPLIT_CHARACTER);
-                int transactionValue = Integer.parseInt(transaction[TRANSACTION_VALUE_INDEX]);
-                if (transaction[TRANSACTION_TYPE_INDEX].equals(SUPPLY)) {
-                    supply += transactionValue;
-                } else {
-                    buy += transactionValue;
-                }
+                stringFileContent.append(line).append(System.lineSeparator());
             }
         } catch (IOException e) {
             throw new RuntimeException("Can't read data from the file " + fromFileName, e);
         }
-        return new int[] {supply, buy, supply - buy};
+        return stringFileContent.toString();
     }
 
-    private String createReport(int[] transactionsValues) {
+    private String createReport(String fileContent) {
+        String[] transactions = fileContent.split(System.lineSeparator());
+        int supply = 0;
+        int buy = 0;
+        for (String transaction : transactions) {
+            String[] transactionDetails = transaction.split(SPLIT_CHARACTER);
+            int transactionValue = Integer.parseInt(transactionDetails[TRANSACTION_VALUE_INDEX]);
+            if (transactionDetails[TRANSACTION_TYPE_INDEX].equals(SUPPLY)) {
+                supply += transactionValue;
+            } else {
+                buy += transactionValue;
+            }
+        }
+
         return new StringBuilder().append(SUPPLY)
                 .append(",")
-                .append(transactionsValues[SUPPLY_AMOUNT_INDEX])
+                .append(supply)
                 .append(System.lineSeparator())
                 .append(BUY)
                 .append(",")
-                .append(transactionsValues[BUY_AMOUNT_INDEX])
+                .append(buy)
                 .append(System.lineSeparator())
                 .append("result,")
-                .append(transactionsValues[RESULT_AMOUNT_INDEX]).toString();
+                .append(supply - buy).toString();
     }
 
     private void writeToFile(String report, String toFileName) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
             bufferedWriter.write(report);
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException("Error while creating file " + toFileName, e);
         } catch (IOException e) {
             throw new RuntimeException("Error while writing to file " + toFileName,e);
         }
