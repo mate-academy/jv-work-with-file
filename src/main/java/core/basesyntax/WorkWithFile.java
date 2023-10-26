37c9bf519c04dbac9b79bb9b6dee3a95f9b099ff
Ілля Лазарев <108271589+bagouser123@users.noreply.github.com>
2023-10-26 23:24:39 +0300

    work with file

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,42 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
+        int supplyTotal = 0;
+        int buyTotal = 0;
 
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
+             BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(",");
+                if (parts.length == 2) {
+                    String operation = parts[0].trim();
+                    int amount = Integer.parseInt(parts[1].trim());
+
+                    if (operation.equals("supply")) {
+                        supplyTotal += amount;
+                    } else if (operation.equals("buy")) {
+                        buyTotal += amount;
+                    }
+                }
+            }
+
+            writer.write("supply," + supplyTotal);
+            writer.newLine();
+            writer.write("buy," + buyTotal);
+            writer.newLine();
+            writer.write("result," + (supplyTotal - buyTotal));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
