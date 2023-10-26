a4f7190eb8b9296624f88b9990be8e529585b379
a_shtuka <ashtuka@gmail.com>
2023-10-26 12:20:01 +0300

    Do refactor readStatisticData method. Divide it on three part. Read data from file - parse data - create report

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,7 +2,6 @@ package core.basesyntax;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -10,63 +9,74 @@ import java.io.IOException;
 public class WorkWithFile {
     private static final int OPERATION_TYPE_INDEX = 0;
     private static final int AMOUNT_INDEX = 1;
+    private static final int SUPPLY_AMOUNT_INDEX = 0;
+    private static final int BUY_AMOUNT_INDEX = 1;
     private static final String SPECIFIED_CHARACTER = ",";
     private static final String LINE_SEPARATOR = System.lineSeparator();
     private static final String OPERATION_RESULT_TITLE = "result";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        String statisticData = readStatisticData(fromFileName);
-        writeStatisticData(toFileName, statisticData);
+        String statisticDataString = readStatisticData(fromFileName);
+        int[] statisticData = parseSourceDataString(statisticDataString);
+        String report = createReport(statisticData);
+        writeStatisticData(toFileName, report);
     }
 
     private String readStatisticData(String fromFileName) {
-        File sourceFile = new File(fromFileName);
-        int supplyAmount = 0;
-        int buyAmount = 0;
+        StringBuilder stringBuilder = new StringBuilder();
         String line;
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(sourceFile))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
             do {
                 line = reader.readLine();
                 if (line != null) {
-                    String[] dataRow = line.split(SPECIFIED_CHARACTER);
-                    if (dataRow[OPERATION_TYPE_INDEX].equals(OperationType.supply.name())) {
-                        supplyAmount += Integer.parseInt(dataRow[AMOUNT_INDEX]);
-                    }
-                    if (dataRow[OPERATION_TYPE_INDEX].equals(OperationType.buy.name())) {
-                        buyAmount += Integer.parseInt(dataRow[AMOUNT_INDEX]);
-                    }
+                    stringBuilder.append(line).append(LINE_SEPARATOR);
                 }
             } while (line != null);
         } catch (IOException e) {
-            throw new RuntimeException("Can't read file: " + sourceFile, e);
+            throw new RuntimeException("Can't read file: " + fromFileName, e);
         }
-        return createReport(supplyAmount, buyAmount);
+        return stringBuilder.toString();
     }
 
-    private String createReport(int supplyAmount, int buyAmount) {
+    private int[] parseSourceDataString(String dataString) {
+        int supplyAmount = 0;
+        int buyAmount = 0;
+        String[] lineArray = dataString.split(LINE_SEPARATOR);
+        for (String line: lineArray) {
+            String[] dataRow = line.split(SPECIFIED_CHARACTER);
+            if (dataRow[OPERATION_TYPE_INDEX].equals(OperationType.supply.name())) {
+                supplyAmount += Integer.parseInt(dataRow[AMOUNT_INDEX]);
+            }
+            if (dataRow[OPERATION_TYPE_INDEX].equals(OperationType.buy.name())) {
+                buyAmount += Integer.parseInt(dataRow[AMOUNT_INDEX]);
+            }
+        }
+        return new int [] {supplyAmount, buyAmount};
+    }
+
+    private String createReport(int[] statisticData) {
         StringBuilder builder = new StringBuilder();
         return builder
                     .append(OperationType.supply.name())
                     .append(SPECIFIED_CHARACTER)
-                    .append(supplyAmount)
+                    .append(statisticData[SUPPLY_AMOUNT_INDEX])
                     .append(LINE_SEPARATOR)
                     .append(OperationType.buy)
                     .append(SPECIFIED_CHARACTER)
-                    .append(buyAmount)
+                    .append(statisticData[BUY_AMOUNT_INDEX])
                     .append(LINE_SEPARATOR)
                     .append(OPERATION_RESULT_TITLE)
                     .append(SPECIFIED_CHARACTER)
-                    .append((supplyAmount - buyAmount))
+                    .append((statisticData[SUPPLY_AMOUNT_INDEX] - statisticData[BUY_AMOUNT_INDEX]))
                     .toString();
     }
 
     private void writeStatisticData(String toFileName, String data) {
-        File destinationFile = new File(toFileName);
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(destinationFile))) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
             writer.write(data);
         } catch (IOException e) {
-            throw new RuntimeException("Can't write data to file: " + destinationFile, e);
+            throw new RuntimeException("Can't write data to file: " + toFileName, e);
         }
     }
 }
