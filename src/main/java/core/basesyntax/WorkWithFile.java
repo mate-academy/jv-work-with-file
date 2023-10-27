1216cf6c7ee317d024ab357b66c8fee92862884a
Dmitriy Sumaroka <sumaroka.dmitriy@gmail.com>
2023-10-27 20:54:22 +0300

    changed constant name, created DELIMITER_REGEX constant, fixed bad name variables

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -5,13 +5,14 @@ import java.io.IOException;
 import java.nio.file.Files;
 
 public class WorkWithFile {
-    private static final String SUPPLY = "supply";
-    private static final String BUY = "buy";
-    private static final String RESULT = "result";
+    private static final String SUPPLY_KEYWORD = "supply";
+    private static final String BUY_KEYWORD = "buy";
+    private static final String RESULT_KEYWORD = "result";
+    private static final String DELIMITER_REGEX = "\\W+";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        String toFile = readFromFile(fromFileName);
-        String report = createReport(toFile);
+        String fileContens = readFromFile(fromFileName);
+        String report = createReport(fileContens);
         writeToFile(toFileName, report);
     }
 
@@ -25,22 +26,22 @@ public class WorkWithFile {
     }
 
     private String createReport(String strings) {
-        String[] strings1 = strings.split("\\W+");
+        String[] tokens = strings.split(DELIMITER_REGEX);
         int supply = 0;
         int buy = 0;
         int result;
-        for (int i = 1; i < strings1.length; i++) {
-            if (strings1[i].equals(SUPPLY)) {
-                supply += Integer.parseInt(strings1[i + 1]);
-            } else if (strings1[i].equals(BUY)) {
-                buy += Integer.parseInt(strings1[i + 1]);
+        for (int i = 1; i < tokens.length; i++) {
+            if (tokens[i].equals(SUPPLY_KEYWORD)) {
+                supply += Integer.parseInt(tokens[i + 1]);
+            } else if (tokens[i].equals(BUY_KEYWORD)) {
+                buy += Integer.parseInt(tokens[i + 1]);
             }
         }
         result = supply - buy;
         StringBuilder builder = new StringBuilder();
-        builder.append(SUPPLY).append(",").append(supply).append(System.lineSeparator())
-                .append(BUY).append(",").append(buy).append(System.lineSeparator())
-                .append(RESULT).append(",").append(result);
+        builder.append(SUPPLY_KEYWORD).append(",").append(supply).append(System.lineSeparator())
+                .append(BUY_KEYWORD).append(",").append(buy).append(System.lineSeparator())
+                .append(RESULT_KEYWORD).append(",").append(result);
         return builder.toString();
     }
 
