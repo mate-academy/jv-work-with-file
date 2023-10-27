e6a70f732cd21737a2851fbd0d8b89792daf1971
Dmitriy Sumaroka <sumaroka.dmitriy@gmail.com>
2023-10-27 16:22:31 +0300

    fix banana repeatedly method call

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -3,26 +3,24 @@ package core.basesyntax;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.OpenOption;
 import java.util.List;
 
 public class WorkWithFile {
-    private int supply = 0;
-    private int buy = 0;
-    private int result;
-
     private String[] readFromFile(String fileName) {
-        File file = new File(fileName);
-        try {
-            List<String> stringList = Files.readAllLines(file.toPath());
-            String[] inputFileToString = stringList.toString().split("\\W+");
-            return inputFileToString;
-        } catch (IOException e) {
-            throw new RuntimeException("Can`t read file " + fileName, e);
-        }
+             File file = new File(fileName);
+            try {
+                List<String> stringList = Files.readAllLines(file.toPath());
+                String[] inputFileToString = stringList.toString().split("\\W+");
+                return inputFileToString;
+            } catch (IOException e) {
+                throw new RuntimeException("Can`t read file " + fileName, e);
+            }
     }
     private String createReport(String[] strings) {
-        for (int i = 0; i < strings.length; i++) {
+        int supply = 0;
+        int buy = 0;
+        int result = 0;
+        for (int i = 1; i < strings.length; i++) {
             if (strings[i].equals("supply")) {
                 supply += Integer.parseInt(strings[i + 1]);
             } else if (strings[i].equals("buy")) {
@@ -35,6 +33,7 @@ public class WorkWithFile {
                 .append(buy).append(System.lineSeparator()).append("result,").append(result);
         return builder.toString();
     }
+
     public void getStatistic(String fromFileName, String toFileName) {
         String[] fromFile = readFromFile(fromFileName);
         String toFile = createReport(fromFile);
@@ -44,7 +43,5 @@ public class WorkWithFile {
         } catch (IOException e) {
             throw new RuntimeException("Can`t write to file: " + toFileName, e);
         }
-        System.out.println(toFile);
-
     }
 }
