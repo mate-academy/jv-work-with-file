c6de757756b50caba4ea0f3473363b8c0d154c8f
Dmitriy Sumaroka <sumaroka.dmitriy@gmail.com>
2023-10-27 19:48:35 +0300

    created  method "writeToFile"

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -3,40 +3,34 @@ package core.basesyntax;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.List;
 
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
-        String[] fromFile = readFromFile(fromFileName);
-        String toFile = createReport(fromFile);
-        File file = new File(toFileName);
-        try {
-            Files.write(file.toPath(), toFile.getBytes());
-        } catch (IOException e) {
-            throw new RuntimeException("Can`t write to file: " + toFileName, e);
-        }
+        String toFile = readFromFile(fromFileName);
+        String report = createReport(toFile);
+        writeToFile(toFileName, report);
     }
 
-    private String[] readFromFile(String fileName) {
+    private String readFromFile(String fileName) {
         File file = new File(fileName);
         try {
-            List<String> stringList = Files.readAllLines(file.toPath());
-            String[] inputFileToString = stringList.toString().split("\\W+");
-            return inputFileToString;
+            String string = Files.readAllLines(file.toPath()).toString();
+            return string;
         } catch (IOException e) {
             throw new RuntimeException("Can`t read file " + fileName, e);
         }
     }
 
-    private String createReport(String[] strings) {
+    private String createReport(String strings) {
+        String[] strings1 = strings.split("\\W+");
         int supply = 0;
         int buy = 0;
         int result;
-        for (int i = 1; i < strings.length; i++) {
-            if (strings[i].equals("supply")) {
-                supply += Integer.parseInt(strings[i + 1]);
-            } else if (strings[i].equals("buy")) {
-                buy += Integer.parseInt(strings[i + 1]);
+        for (int i = 1; i < strings1.length; i++) {
+            if (strings1[i].equals("supply")) {
+                supply += Integer.parseInt(strings1[i + 1]);
+            } else if (strings1[i].equals("buy")) {
+                buy += Integer.parseInt(strings1[i + 1]);
             }
         }
         result = supply - buy;
@@ -44,5 +38,14 @@ public class WorkWithFile {
         builder.append("supply,").append(supply).append(System.lineSeparator()).append("buy,")
                 .append(buy).append(System.lineSeparator()).append("result,").append(result);
         return builder.toString();
+    }
+
+    private void writeToFile(String fileName, String report) {
+        File file = new File(fileName);
+        try {
+            Files.write(file.toPath(), report.getBytes());
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t write to file: " + fileName, e);
+        }
     }
 }
