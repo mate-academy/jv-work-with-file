9a23f19f190263834c2f1d8a0149da4ffe2afc50
Dmitriy Sumaroka <sumaroka.dmitriy@gmail.com>
2023-10-27 19:59:23 +0300

    created constant SUPPLY, BUY, RESULT

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -5,6 +5,10 @@ import java.io.IOException;
 import java.nio.file.Files;
 
 public class WorkWithFile {
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+    private static final String RESULT = "result";
+
     public void getStatistic(String fromFileName, String toFileName) {
         String toFile = readFromFile(fromFileName);
         String report = createReport(toFile);
@@ -26,16 +30,17 @@ public class WorkWithFile {
         int buy = 0;
         int result;
         for (int i = 1; i < strings1.length; i++) {
-            if (strings1[i].equals("supply")) {
+            if (strings1[i].equals(SUPPLY)) {
                 supply += Integer.parseInt(strings1[i + 1]);
-            } else if (strings1[i].equals("buy")) {
+            } else if (strings1[i].equals(BUY)) {
                 buy += Integer.parseInt(strings1[i + 1]);
             }
         }
         result = supply - buy;
         StringBuilder builder = new StringBuilder();
-        builder.append("supply,").append(supply).append(System.lineSeparator()).append("buy,")
-                .append(buy).append(System.lineSeparator()).append("result,").append(result);
+        builder.append(SUPPLY).append(",").append(supply).append(System.lineSeparator())
+                .append(BUY).append(",").append(buy).append(System.lineSeparator())
+                .append(RESULT).append(",").append(result);
         return builder.toString();
     }
 
