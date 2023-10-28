d4b167d5ad20342342fbb08040a5add4053a8616
Ілля Лазарев <108271589+bagouser123@users.noreply.github.com>
2023-10-28 20:17:06 +0300

    work with file

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,33 +8,53 @@ import java.io.IOException;
 
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
+        String dataFromFile = readFile(fromFileName);
+        String report = createReport(dataFromFile);
+        writeReportToFile(report, toFileName);
+    }
+
+    public String readFile(String fileName) {
+        StringBuilder data = new StringBuilder();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName)) ) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                data.append(line).append(System.lineSeparator());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return data.toString();
+    }
+
+    public String createReport(String dataFromFile) {
         int supplyTotal = 0;
         int buyTotal = 0;
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
-                BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
+        String[] lines = dataFromFile.split(System.lineSeparator());
+        for (String line : lines) {
+            String[] parts = line.split(",");
+            if (parts.length == 2) {
+                String operation = parts[0].trim();
+                int amount = Integer.parseInt(parts[1].trim());
 
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String[] parts = line.split(",");
-                if (parts.length == 2) {
-                    String operation = parts[0].trim();
-                    int amount = Integer.parseInt(parts[1].trim());
-
-                    if (operation.equals("supply")) {
-                        supplyTotal += amount;
-                    } else if (operation.equals("buy")) {
-                        buyTotal += amount;
-                    }
+                if (operation.equals("supply")) {
+                    supplyTotal += amount;
+                } else if (operation.equals("buy")) {
+                    buyTotal += amount;
                 }
             }
+        }
 
-            writer.write("supply," + supplyTotal);
-            writer.newLine();
-            writer.write("buy," + buyTotal);
-            writer.newLine();
-            writer.write("result," + (supplyTotal - buyTotal));
+        return "supply," + supplyTotal + System.lineSeparator() +
+                "buy," + buyTotal + System.lineSeparator() +
+                "result," + (supplyTotal - buyTotal);
+    }
 
+    public void writeReportToFile(String report, String fileName) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+            writer.write(report);
         } catch (IOException e) {
             e.printStackTrace();
         }
