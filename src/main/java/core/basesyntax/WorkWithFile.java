1b1b9f15dcd9aa62ee71e48b2cffb50f0f95c7e8
Slava_Moiseev <qualityengineerinkom@gmail.com>
2023-11-06 17:56:47 +0200

    implemented recommendations, try #6

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -10,57 +10,59 @@ public class WorkWithFile {
     private static final String SUPPLY = "supply";
     private static final String BUY = "buy";
     private static final String COMMA = ",";
+    private static final String DELIMITER = " ";
     private static String resultString;
-    private String report;
+    private static final int ONE = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
 
-        readFile(fromFileName);
-        createReport(report);
-        writeFile(resultString,toFileName);
+        String data = readFile(fromFileName);
+        String report = createReport(data);
+        writeFile(report, toFileName);
     }
 
-    private void readFile(String fromFileName) {
+    private String readFile(String fromFileName) {
         StringBuilder builder = new StringBuilder();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             while (bufferedReader.ready()) {
-                builder.append(" ");
+                builder.append(DELIMITER);
                 builder.append(bufferedReader.readLine());
             }
             System.out.println(builder);
-            report = builder.toString();
         } catch (IOException e) {
-            throw new RuntimeException("Can`t read from file", e);
+            throw new RuntimeException("Can`t read from file: " + fromFileName, e);
         }
+        return builder.toString();
     }
 
-    private void createReport(String report) {
+    private String createReport(String report) {
         int intSupply = 0;
         int intBuy = 0;
         int result;
-        String[] innerArray = report.split(" ");
+        String[] innerArray = report.split(DELIMITER);
         for (String s : innerArray) {
 
             String[] inside = s.split(COMMA);
 
             if (inside[0].equals(SUPPLY)) {
-                intSupply += Integer.parseInt(inside[1]);
+                intSupply += Integer.parseInt(inside[ONE]);
             }
             if (inside[0].equals(BUY)) {
-                intBuy += Integer.parseInt(inside[1]);
+                intBuy += Integer.parseInt(inside[ONE]);
             }
             result = intSupply - intBuy;
             resultString = SUPPLY + COMMA + intSupply + System.lineSeparator()
                     + BUY + COMMA + intBuy + System.lineSeparator()
                     + "result" + COMMA + result;
         }
+        return resultString;
     }
 
-    private void writeFile(String res,String toFileName) {
+    private void writeFile(String res, String toFileName) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
             bufferedWriter.write(res);
         } catch (IOException e) {
-            throw new RuntimeException("Can`t write to file", e);
+            throw new RuntimeException("Can`t write to file: " + toFileName, e);
         }
     }
 }
