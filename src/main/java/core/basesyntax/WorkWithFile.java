3e116b1ba31791e4e41131158e6c8e9721bcc787
rus1980707 <rus1980707@gmail.com>
2023-11-11 14:22:11 +0200

    jv-work-with-file

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -15,30 +15,53 @@ public class WorkWithFile {
     private static final int AMOUNT = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
+        String data = readFile(fromFileName);
+        String report = generateReport(data);
+        writeToFile(toFileName, report);
+    }
 
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName));
-                 BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
-            int supply = 0;
-            int buy = 0;
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                String[] files = line.split(SEPARATE);
-                if (files[OPERATION].equals(SUPPLY)) {
-                    supply += Integer.parseInt(files[AMOUNT]);
-                } else if (files[OPERATION].equals(BUY)) {
-                    buy += Integer.parseInt(files[AMOUNT]);
-                }
+    private String readFile(String fromFileName) {
+        StringBuilder read = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
+            String value = bufferedReader.readLine();
+            while (value != null) {
+                read.append(value).append(System.lineSeparator());
+                value = bufferedReader.readLine();
             }
-            int result = supply - buy;
-
-            bufferedWriter.write(SUPPLY + SEPARATE + supply);
-            bufferedWriter.newLine();
-            bufferedWriter.write(BUY + SEPARATE + buy);
-            bufferedWriter.newLine();
-            bufferedWriter.write(RESULT + SEPARATE + result);
-
         } catch (IOException e) {
-            throw new RuntimeException("Can`t read/write files", e);
+            throw new RuntimeException("Can`t read from file", e);
+        }
+        return read.toString();
+    }
+
+    private String generateReport(String data) {
+        int supplyTotal = 0;
+        int buyTotal = 0;
+        String[] lines = data.split(System.lineSeparator());
+        for (String line : lines) {
+            String[] files = line.split(SEPARATE);
+            if (files[OPERATION].equals(SUPPLY)) {
+                supplyTotal += Integer.parseInt(files[AMOUNT]);
+            } else if (files[OPERATION].equals(BUY)) {
+                buyTotal += Integer.parseInt(files[AMOUNT]);
+            }
+        }
+        return SUPPLY + SEPARATE + supplyTotal
+                +
+                System.lineSeparator()
+                +
+                BUY + SEPARATE + buyTotal
+                +
+                System.lineSeparator()
+                +
+                RESULT + SEPARATE + (supplyTotal - buyTotal);
+    }
+
+    private void writeToFile(String toFileName, String report) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+            bufferedWriter.write(report);
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t write to file", e);
         }
     }
 }
