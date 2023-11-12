b0841ecc136bce06a1f90a482f8edcf5914e12b3
Marta Zatserkovna <mzatserkovna10@gmail.com>
2023-11-12 11:34:07 +0200

    added fixes after review

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,38 +7,69 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    private static final String SEPARATOR = ",";
+    private static final String NEW_LINE = "\n";
+    private static final String LABEL_SUPPLY = "supply";
+    private static final String LABEL_BUY = "buy";
+    private static final String LABEL_RESULT = "result";
+
     public void getStatistic(String fromFileName, String toFileName) {
+        try {
+            String fileContent = readFile(fromFileName);
+
+            String report = generateReport(fileContent);
+
+            writeToFile(report, toFileName);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read data from the file ", e);
+        }
+    }
+
+    private String readFile(String fileName) throws IOException {
+        StringBuilder content = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                content.append(line).append(NEW_LINE);
+            }
+        }
+        return content.toString();
+    }
+
+    private String generateReport(String fileContent) {
         int supplyTotal = 0;
         int buyTotal = 0;
 
-        try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName));
-            String line;
+        StringBuilder reportBuilder = new StringBuilder();
 
-            while ((line = bufferedReader.readLine()) != null) {
-                String[] parts = line.split(",");
-                if (parts.length == 2) {
-                    String label = parts[0];
-                    int value = Integer.parseInt(parts[1]);
-                    if ("supply".equals(label)) {
-                        supplyTotal += value;
-                    } else if ("buy".equals(label)) {
-                        buyTotal += value;
-                    }
+        String[] lines = fileContent.split(NEW_LINE);
+        for (String line : lines) {
+            String[] parts = line.split(SEPARATOR);
+            if (parts.length == 2) {
+                String label = parts[0];
+                int value = Integer.parseInt(parts[1]);
+                if (LABEL_SUPPLY.equals(label)) {
+                    supplyTotal += value;
+                } else if (LABEL_BUY.equals(label)) {
+                    buyTotal += value;
                 }
             }
+        }
 
-            int result = supplyTotal - buyTotal;
+        int result = supplyTotal - buyTotal;
 
-            BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName));
-            writer.write("supply," + supplyTotal);
-            writer.newLine();
-            writer.write("buy," + buyTotal);
-            writer.newLine();
-            writer.write("result," + result);
-            writer.close();
-        } catch (IOException e) {
-            throw new RuntimeException("Can't read data from the file " + fromFileName, e);
+        reportBuilder.append(LABEL_SUPPLY).append(SEPARATOR).append(supplyTotal)
+                .append(System.lineSeparator())
+                .append(LABEL_BUY).append(SEPARATOR).append(buyTotal)
+                .append(System.lineSeparator())
+                .append(LABEL_RESULT).append(SEPARATOR).append(result);
+
+        return reportBuilder.toString();
+    }
+
+    private void writeToFile(String report, String fileName) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+            writer.write(report);
         }
     }
 }
