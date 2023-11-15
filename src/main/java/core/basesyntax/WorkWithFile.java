6cc36817484587503b12daf263cf6870726eedfb
MaksOsypchuk <Promaks_ua@icloud.com>
2023-11-15 13:53:40 +0200

    I created separate methods

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,28 +8,43 @@ import java.io.IOException;
 
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
-        int supplySum = 0;
-        int buySum = 0;
+        String result = this.readFile(fromFileName);
+        String statistic = this.createReport(result);
+        this.writeStatistic(statistic, toFileName);
+    }
+
+    public String readFile(String fromFileName) {
         try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
+            StringBuilder stringBuilder = new StringBuilder();
             String value;
             while ((value = reader.readLine()) != null) {
-                String[] element = value.split(",");
-                if (element[0].equals("supply")) {
-                    supplySum = supplySum + Integer.parseInt(element[1]);
-                } else if (element[0].equals("buy")) {
-                    buySum = buySum + Integer.parseInt(element[1]);
-                }
+                stringBuilder.append(value + ",");
             }
+            return stringBuilder.toString();
         } catch (IOException e) {
             throw new RuntimeException("Can't read file", e);
         }
+    }
 
+    public String createReport(String readinggg) {
+        int supplySum = 0;
+        int buySum = 0;
+        String[] elements = readinggg.split(",");
+        for (int i = 0; i < elements.length; i++) {
+            if (elements[i].equals("supply")) {
+                supplySum = supplySum + Integer.parseInt(elements[i + 1]);
+            } else if (elements[i].equals("buy")) {
+                buySum = buySum + Integer.parseInt(elements[i + 1]);
+            }
+        }
+        return new String("supply," + supplySum + '\n'
+                + "buy," + buySum + '\n'
+                + "result," + (supplySum - buySum));
+    }
+
+    public void writeStatistic(String statistic, String toFileName) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
-            writer.write("supply," + supplySum);
-            writer.newLine();
-            writer.write("buy," + buySum);
-            writer.newLine();
-            writer.write("result," + (supplySum - buySum));
+            writer.write(statistic);
         } catch (IOException e) {
             throw new RuntimeException("Can't write file", e);
         }
