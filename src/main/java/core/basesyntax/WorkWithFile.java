222a2a666e43422807a4383bbd143a3dffcca31c
MilianCode <milian.code@gmail.com>
2023-11-16 12:35:53 +0100

    Refactored solution to  jv-work-with-file

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -9,37 +9,45 @@ import java.nio.file.Path;
 public class WorkWithFile {
     private static final String SUPPLIED = "supply";
     private static final String BOUGHT = "buy";
-    private int supplyCount = 0;
-    private int buyCount = 0;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        StringBuilder result = new StringBuilder();
-        countProducts(fromFileName);
-        result.append("supply,").append(supplyCount).append(System.lineSeparator())
-                .append("buy,").append(buyCount).append(System.lineSeparator())
-                .append("result,").append(supplyCount - buyCount);
-        createReport(result.toString(), toFileName);
-        buyCount = 0;
-        supplyCount = 0;
+        String dataFromFile = readFile(fromFileName);
+        String report = createReport(dataFromFile);
+        writeToFile(report, toFileName);
     }
 
-    private void countProducts(String fromFileName) {
+    private String readFile(String fromFileName) {
+        StringBuilder dataFromFile = new StringBuilder();
+        String line;
         try (BufferedReader reader = Files.newBufferedReader(Path.of(fromFileName))) {
-            String line;
             while ((line = reader.readLine()) != null) {
-                String[] lineSeparatedWords = line.split(",");
-                if (lineSeparatedWords[0].equals(SUPPLIED)) {
-                    supplyCount += Integer.parseInt(lineSeparatedWords[1]);
-                } else if (lineSeparatedWords[0].equals(BOUGHT)) {
-                    buyCount += Integer.parseInt(lineSeparatedWords[1]);
-                }
+                dataFromFile.append(line).append(System.lineSeparator());
             }
         } catch (IOException e) {
             throw new RuntimeException("Can't read file " + fromFileName, e);
         }
+        return dataFromFile.toString();
     }
 
-    public void createReport(String report, String toFileName) {
+    private String createReport(String dataFromFile) {
+        StringBuilder report = new StringBuilder();
+        String[] lines = dataFromFile.split(System.lineSeparator());
+        int supply = 0;
+        int buy = 0;
+        for (String line : lines) {
+            String[] words = line.split(",");
+            if (words[0].equals(SUPPLIED)) {
+                supply += Integer.parseInt(words[1]);
+            } else if (words[0].equals(BOUGHT)) {
+                buy += Integer.parseInt(words[1]);
+            }
+        }
+        return report.append("supply,").append(supply).append(System.lineSeparator())
+                .append("buy,").append(buy).append(System.lineSeparator())
+                .append("result,").append(supply - buy).toString();
+    }
+
+    private void writeToFile(String report, String toFileName) {
         try (BufferedWriter writer = Files.newBufferedWriter(Path.of(toFileName))) {
             writer.write(report);
         } catch (IOException e) {
