830d470cd6d4d97943ae5dcf25de680ae9c5a67f
Nazar Kolesnikov <nazarkolesnikov536@gmail.com>
2023-11-22 14:07:01 +0200

    Update throw new RuntimeException(), declare Operation Type and Amount indexes

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,50 +1,82 @@
 package core.basesyntax;
 
-import java.io.IOException;
-import java.io.FileWriter;
-import java.io.FileReader;
-import java.io.BufferedWriter;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class WorkWithFile {
-    private static final String BUY_INDEX = "buy";
-    private static final String SUPPLY_INDEX = "supply";
-    private static final String RESULT_INDEX = "result";
-    private static final int NAME_INDEX = 0;
-    private static final int NUMBER_INDEX = 1;
+    private static final String BUY_VALUE = "buy";
+    private static final String SUPPLY_VALUE = "supply";
+    private static final String RESULT_VALUE = "result";
+    private static final int OPERATION_TYPE_INDEX = 0;
+    private static final int AMOUNT_INDEX = 1;
     private static final String SPACE_SEPARATOR = " ";
     private static final String COMMA_SEPARATOR = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
         String[] dataFromFile = readFile(fromFileName);
         String report = generateReport(dataFromFile);
-        writeToFile(toFileName);
+        writeToFile(toFileName, report);
     }
 
-    public String[] readFile(String fromFileName) {
-        StringBuilder resultBuilder = new StringBuilder();
+    private String [] readFile(String fromFileName) {
+        StringBuilder builder = new StringBuilder();
         try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
-            String value = reader
-                    .readLine();
-            while (String.valueOf(value) != null) {
-                resultBuilder
+            String value = reader.readLine();
+            while (value != null) {
+                builder
                         .append(value)
                         .append(SPACE_SEPARATOR);
+                value = reader
+                        .readLine();
             }
         } catch (IOException e) {
-            throw new RuntimeException("Can't read to file", e);
+            throw new RuntimeException("Can`t read to file" + fromFileName, e);
         }
-        return resultBuilder
+        return builder
                 .toString()
                 .split(SPACE_SEPARATOR);
     }
 
     private String generateReport(String[] fromFileDate) {
-        int buyCount = 0;
-        int supplyCount = 0;
-        for (String line: fromFileDate) {
+        int buyCounter = 0;
+        int supplyCounter = 0;
+        for (String line : fromFileDate) {
             String[] words = line.split(COMMA_SEPARATOR);
+            if (words[OPERATION_TYPE_INDEX].equals(BUY_VALUE)) {
+                buyCounter += Integer.parseInt(words[AMOUNT_INDEX]);
+            }
+            if (words[OPERATION_TYPE_INDEX].equals(SUPPLY_VALUE)) {
+                supplyCounter += Integer.parseInt(words[AMOUNT_INDEX]);
+            }
+        }
+        StringBuilder reportBuilder;
+        reportBuilder = new StringBuilder();
+        reportBuilder
+                .append(SUPPLY_VALUE)
+                .append(COMMA_SEPARATOR)
+                .append(supplyCounter)
+                .append(System.lineSeparator())
+                .append(BUY_VALUE)
+                .append(COMMA_SEPARATOR)
+                .append(buyCounter)
+                .append(System.lineSeparator())
+                .append(RESULT_VALUE)
+                .append(COMMA_SEPARATOR)
+                .append(supplyCounter - buyCounter)
+                .append(System.lineSeparator());
+        return reportBuilder
+                .toString();
+    }
 
+    private void writeToFile(String toFileName, String report) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+            bufferedWriter
+                    .write(report);
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t writer to file" + toFileName, e);
         }
     }
- }
+}
