ab9934ab01ce8bf48f2f9019545260dfe684c5b1
bobik <oliferchukrivne@i.ua>
2023-11-24 14:51:14 +0200

    task done - after review

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,40 +7,62 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    private static final String OPERATION_SUPPLY = "supply";
+    private static final String OPERATION_BUY = "buy";
+    private int supplyTotal = 0;
+    private int buyTotal = 0;
+
     public void getStatistic(String fromFileName, String toFileName) {
-        try (BufferedReader newReader = new BufferedReader(new FileReader(fromFileName));
-                BufferedWriter newWriter = new BufferedWriter(new FileWriter(toFileName))) {
+        if (fromFileName == null || toFileName == null) {
+            throw new IllegalArgumentException("File names cannot be null");
+        }
 
-            int supplyTotal = 0;
-            int buyTotal = 0;
+        String[] allLines = getAllLines(fromFileName);
 
-            String line;
-            while ((line = newReader.readLine()) != null) {
-                String[] fields = line.split(",");
-                if (fields.length == 2) {
-                    String operationType = fields[0].trim();
-                    int amount = Integer.parseInt(fields[1].trim());
 
-                    if ("supply".equals(operationType)) {
-                        supplyTotal += amount;
-                    } else if ("buy".equals(operationType)) {
-                        buyTotal += amount;
-                    }
+        int result = processDataInFile(allLines);
+
+
+        writeDataToFile(toFileName, this.supplyTotal, this.buyTotal, result);
+        this.supplyTotal = 0;
+        this.buyTotal = 0;
+    }
+
+    private String[] getAllLines(String fromFileName) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
+            return reader.lines().toArray(String[]::new);
+        } catch (IOException e) {
+            throw new RuntimeException("Error reading file", e);
+        }
+    }
+
+    private int processDataInFile(String[] file) {
+        for (String line : file) {
+            String[] fields = line.split(",");
+            if (fields.length == 2) {
+                String operationType = fields[0].trim();
+                int amount = Integer.parseInt(fields[1].trim());
+
+                if (OPERATION_SUPPLY.equals(operationType)) {
+
+                    this.supplyTotal += amount;
+                } else if (OPERATION_BUY.equals(operationType)) {
+                    this.buyTotal += amount;
                 }
             }
+        }
+        return this.supplyTotal - this.buyTotal;
+    }
 
-            int result = supplyTotal - buyTotal;
-
-            newWriter.write("supply," + supplyTotal);
-            newWriter.newLine();
-            newWriter.write("buy," + buyTotal);
-            newWriter.newLine();
-            newWriter.write("result," + result);
-
-            System.out.println("Report generated successfully!");
-
-        } catch (IOException | NumberFormatException e) {
-            throw new RuntimeException(e);
+    private void writeDataToFile(String toFileName, int supplyTotal, int buyTotal, int result) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
+            writer.write("supply," + supplyTotal);
+            writer.newLine();
+            writer.write("buy," + buyTotal);
+            writer.newLine();
+            writer.write("result," + result);
+        } catch (IOException e) {
+            throw new RuntimeException("Error writing to file", e);
         }
     }
 }
