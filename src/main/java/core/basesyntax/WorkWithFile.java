7b52ab4b3ac917329485499865d3199d9f8fc2da
Serhii Nikolaiev <haccent12@gmail.com>
2023-11-25 15:15:59 +0200

    first solution

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,101 +1,56 @@
 package core.basesyntax;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class WorkWithFile {
     private static final String BUY = "buy";
     private static final String SUPPLY = "supply";
-    private static final int ACTION_INDEX = 0;
-    private static final int MOUNT_INDEX = 1;
+    private static final String RESULT = "result";
+
     public void getStatistic(String fromFileName, String toFileName) {
         File fileToRead = new File(fromFileName);
         File fileToWrite = new File(toFileName);
-        StringBuilder stringBuilder = new StringBuilder();
-        StringBuilder stringBuilderBuy = new StringBuilder();
-        StringBuilder stringBuilderSupply = new StringBuilder();
-        StringBuilder stringBuilderResult = new StringBuilder();
+        StringBuilder builder = new StringBuilder();
         int buyScore = 0;
         int supplyScore = 0;
-        int countBuyLength = 0;
-        int countSupplyLength = 0;
+        int resultScore = 0;
+
         try (BufferedReader reader = new BufferedReader(new FileReader(fileToRead))) {
             String value = reader.readLine();
             while (value != null) {
-                stringBuilder.append(value).append(',');
+                builder.append(value).append(',');
                 value = reader.readLine();
             }
         } catch (IOException e) {
-            throw new RuntimeException("Cannot read file", e);
+            throw new RuntimeException("Cannot read file:", e);
         }
-        String result = stringBuilder.toString();//buy,13,sell,2....
-        String[] allStrings = result.split(",");//[buy, 13, sell, 2]
-        for (int i = 0; i < allStrings.length; i = i + 2) {//[0]buy
-            if(allStrings[i].equals(BUY)) {
-                buyScore += Integer.parseInt(allStrings[i + 1]);
-                stringBuilderBuy.append(allStrings[i + 1]).append(',');
-                countBuyLength++;
-            }
-            if(allStrings[i].equals(SUPPLY)) {//+
-                supplyScore += Integer.parseInt(allStrings[i + 1]);
-                stringBuilderSupply.append(allStrings[i + 1]).append(",");
-                countSupplyLength++;
-            }
-        }
-        String[] buyWithComma = stringBuilderBuy.toString().split(",");
-        String[] supplyWithComma = stringBuilderSupply.toString().split(",");
 
-        String[] buy = new String[countBuyLength];
-        String[] supply  = new String[countSupplyLength];
-        int counterOfBuy = 0;
-        int counterOfSupply = 0;
-        for (String current : buyWithComma) {
-            if (!current.equals(",")) {
-                buy[counterOfBuy] = current;
-                counterOfBuy++;
+        String stringAllLines = builder.toString();
+        String[] allStringsArray = stringAllLines.split(",");
+
+        for (int i = 0; i < allStringsArray.length; i = i + 2) {
+            if (allStringsArray[i].equals(BUY)) {
+                buyScore += Integer.parseInt(allStringsArray[i + 1]);
+            }
+            if (allStringsArray[i].equals(SUPPLY)) {
+                supplyScore += Integer.parseInt(allStringsArray[i + 1]);
             }
         }
-        for (String current : supplyWithComma) {
-            if (!current.equals(",")) {
-                supply[counterOfSupply] = current;
-                counterOfSupply++;
-            }
-        }
-        stringBuilderBuy.setLength(0);
-        stringBuilderBuy.append("buy = ");
-        stringBuilderBuy.setLength(0);
-        for (int i = 0; i < buy.length; i++) {
-            stringBuilderBuy.append(buy[i]);
-            if(i != buy.length - 1) {
-                stringBuilderBuy.append(" + ");
-            }
-            if (i == buy.length - 1) {
-                stringBuilderBuy.append(" = ").append(buyScore);
-            }
-        }
-        String resultBuy = stringBuilderBuy.toString();
-        stringBuilderSupply.setLength(0);
-        stringBuilderSupply.append("supply = ");
-        stringBuilderSupply.setLength(0);
-        for (int i = 0; i < supply.length; i++) {
-            stringBuilderSupply.append(supply[i]);
-            if(i != supply.length - 1) {
-                stringBuilderSupply.append(" + ");
-            }
-            if (i == supply.length - 1) {
-                stringBuilderSupply.append(" = ").append(supplyScore);
-            }
-        }
-        String resultSupply = stringBuilderSupply.toString();
-        String resultFinal = stringBuilderResult.append("result = supply - buy = ")
-                .append(supplyScore).append(" - ").append(buyScore).append(" = ")
-                .append(supplyScore - buyScore).toString();
+
+        resultScore = supplyScore - buyScore;
 
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileToWrite))) {
-            writer.write(resultSupply + System.lineSeparator());
-            writer.write(resultBuy + System.lineSeparator());
-            writer.write(resultFinal + System.lineSeparator());
+            writer.write(SUPPLY + "," + supplyScore + System.lineSeparator());
+            writer.write(BUY + "," + buyScore + System.lineSeparator());
+            writer.write(RESULT + "," + resultScore + System.lineSeparator());
         } catch (IOException e) {
-            throw new RuntimeException("Cannot write in file",e);
+            throw new RuntimeException("Cannot write in file:",e);
         }
     }
 }
+
