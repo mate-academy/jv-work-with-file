52ce7bfe9dd9647cdbc3eee0a2c3095d3e5738c2
Serhii Nikolaiev <haccent12@gmail.com>
2023-11-26 16:47:50 +0100

    made private methods and separate logic to this methods

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -15,11 +15,17 @@ public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
         File fileToRead = new File(fromFileName);
         File fileToWrite = new File(toFileName);
-        StringBuilder builder = new StringBuilder();
+        StringBuilder stringBuilder = new StringBuilder();
         int buyScore = 0;
         int supplyScore = 0;
-        int resultScore = 0;
+        String infoTostring = readToFile(fileToRead, stringBuilder);
+        buyScore = calculateCount(infoTostring, buyScore, BUY);
+        supplyScore = calculateCount(infoTostring, supplyScore, SUPPLY);
+        int resultScore = supplyScore - buyScore;
+        writeToFile(fileToWrite, buyScore,supplyScore,resultScore);
+    }
 
+    private String readToFile(File fileToRead, StringBuilder builder) {
         try (BufferedReader reader = new BufferedReader(new FileReader(fileToRead))) {
             String value = reader.readLine();
             while (value != null) {
@@ -29,21 +35,23 @@ public class WorkWithFile {
         } catch (IOException e) {
             throw new RuntimeException("Cannot read file:", e);
         }
+        return builder.toString();
+    }
 
-        String stringAllLines = builder.toString();
+    private int calculateCount(String stringAllLines, int score, String subtraction) {
         String[] allStringsArray = stringAllLines.split(",");
-
         for (int i = 0; i < allStringsArray.length; i = i + 2) {
-            if (allStringsArray[i].equals(BUY)) {
-                buyScore += Integer.parseInt(allStringsArray[i + 1]);
+            if (allStringsArray[i].equals(BUY) && allStringsArray[i].equals(subtraction)) {
+                score += Integer.parseInt(allStringsArray[i + 1]);
             }
-            if (allStringsArray[i].equals(SUPPLY)) {
-                supplyScore += Integer.parseInt(allStringsArray[i + 1]);
+            if (allStringsArray[i].equals(SUPPLY) && allStringsArray[i].equals(subtraction)) {
+                score += Integer.parseInt(allStringsArray[i + 1]);
             }
         }
+        return score;
+    }
 
-        resultScore = supplyScore - buyScore;
-
+    private void writeToFile(File fileToWrite, int buyScore, int supplyScore, int resultScore) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileToWrite))) {
             writer.write(SUPPLY + "," + supplyScore + System.lineSeparator());
             writer.write(BUY + "," + buyScore + System.lineSeparator());
