e65d48d892fed223c564854794318c7b31966416
Oleh <mihailyakyb45@gmail.com>
2023-11-28 18:15:28 +0200

    added readDataFromFile method and fixed writeReportToFile, createReport methods

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -15,45 +15,48 @@ public class WorkWithFile {
     private static final int DATA_INDEX_AMOUNT = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        String report = createAReport(fromFileName);
-        writeAReportToFile(toFileName, report);
+        String report = createReport(readDataFromFile(fromFileName));
+        writeReportToFile(toFileName, report);
     }
 
-    public String createAReport(String fromFileName) {
-        try {
-            List<String> lines = Files.readAllLines(Path.of(fromFileName));
+    public String createReport(List<String> dataFromFile) {
+        int supplyCounter = 0;
+        int buyCounter = 0;
 
-            int supplyCounter = 0;
-            int buyCounter = 0;
+        for (String line : dataFromFile) {
+            String[] data = line.split("\\W+");
+            String action = data[DATA_INDEX_TO_DO];
+            int amount = Integer.parseInt(data[DATA_INDEX_AMOUNT]);
 
-            for (String line : lines) {
-                String[] data = line.split("\\W+");
-                String action = data[DATA_INDEX_TO_DO];
-                int amount = Integer.parseInt(data[DATA_INDEX_AMOUNT]);
-
-                if (action.equals(TO_BUY)) {
-                    buyCounter += amount;
-                } else {
-                    supplyCounter += amount;
-                }
+            if (action.equals(TO_BUY)) {
+                buyCounter += amount;
+            } else {
+                supplyCounter += amount;
             }
-            int result = supplyCounter - buyCounter;
-            String report = TO_SUPPLY + "," + supplyCounter + System.lineSeparator()
-                    + TO_BUY + "," + buyCounter + System.lineSeparator()
-                    + RESULT + "," + result;
-
-            return report;
-        } catch (IOException e) {
-            throw new RuntimeException("Something went wrong..", e);
         }
+        int result = supplyCounter - buyCounter;
+        String report = TO_SUPPLY + "," + supplyCounter + System.lineSeparator()
+                + TO_BUY + "," + buyCounter + System.lineSeparator()
+                + RESULT + "," + result;
+
+        return report;
     }
 
-    public void writeAReportToFile(String toFileName, String report) {
+    public void writeReportToFile(String toFileName, String report) {
         try {
             Files.write(Path.of(toFileName), report.getBytes(StandardCharsets.UTF_8),
                     StandardOpenOption.CREATE);
         } catch (IOException e) {
-            throw new RuntimeException("Something went wrong..", e);
+            throw new RuntimeException("Can't write report to file", e);
+        }
+    }
+
+    public List<String> readDataFromFile(String fromFileName) {
+        try {
+            List<String> dataFromFile = Files.readAllLines(Path.of(fromFileName));
+            return dataFromFile;
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read data from file");
         }
     }
 }
