c528a1631919c0097466b0afa0b654af2936b0d0
oKukHtina <olakukhtina95@gmail.com>
2023-11-28 17:43:00 +0100

    fixed code review

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,6 +1,5 @@
 package core.basesyntax;
 
-import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -12,73 +11,38 @@ public class WorkWithFile {
     private static final String CASE_SUPPLY = "supply";
     private static final String RESULT = "result";
     private static final String EXCEPTION_MESSAGE = "Key doesn't found";
-    private static final String PARSE_EXCEPTION_MESSAGE = "Something went wrong";
-    private static final int SUPPLY_VALUE = 1;
-    private static final int BUY_VALUE = 3;
     private static final int KEY = 0;
     private static final int KEY_VALUE = 1;
     private static final String SPLIT_BY_NEW_LINE = System.lineSeparator();
-    private static final int END_FILE_CODE = -1;
 
     public void getStatistic(String fromFileName, String toFileName) {
+        String data = readFile(fromFileName);
+        String report = generateReport(data);
+        writeToFile(toFileName, report);
+    }
+
+    private void writeToFile(String toFileName, String report) {
+        try (FileWriter writer = new FileWriter(toFileName)) {
+            writer.write(report);
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+    }
+
+    private String generateReport(String data) {
+        StringBuilder report = new StringBuilder();
         int sumForBuy = 0;
         int sumForSupply = 0;
-        StringBuilder textForSavingToFile = new StringBuilder();
-        StringBuilder constructTextFromFile = new StringBuilder();
-        try (
-                BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
-                FileWriter writer = new FileWriter(toFileName)
-        ) {
-            readFile(reader, constructTextFromFile);
-            String[] splitConstructTextFromFile =
-                    getSplitConstructTextFromFile(constructTextFromFile);
-
-            String[] processData = processData(splitConstructTextFromFile, sumForSupply, sumForBuy);
-
-            StringBuilder textForSavingFile =
-                    generateReport(
-                            Integer.parseInt(processData[BUY_VALUE]),
-                            Integer.parseInt(processData[SUPPLY_VALUE]),textForSavingToFile);
-            writeToFile(textForSavingFile.toString(), writer);
-
-        } catch (IOException e) {
-            throw new NoSuchElementException(PARSE_EXCEPTION_MESSAGE);
-        }
-    }
-
-    private void writeToFile(String textForSavingFile, FileWriter writer) {
-        try {
-            writer.write(textForSavingFile);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private String[] processData(
-            String[] splitConstructTextFromFile,
-            int sumForSupply,
-            int sumForBuy) {
-        int copySumForSupply = sumForSupply;
-        int copySumForBuy = sumForBuy;
-        for (String s : splitConstructTextFromFile) {
+        for (String s : data.split(SPLIT_BY_NEW_LINE)) {
             String[] currentLine = s.split(SPLITERATOR);
             switch (currentLine[KEY]) {
-                case CASE_BUY -> copySumForBuy += Integer.parseInt(currentLine[KEY_VALUE]);
-                case CASE_SUPPLY -> copySumForSupply += Integer.parseInt(currentLine[KEY_VALUE]);
+                case CASE_BUY -> sumForBuy += Integer.parseInt(currentLine[KEY_VALUE]);
+                case CASE_SUPPLY -> sumForSupply += Integer.parseInt(currentLine[KEY_VALUE]);
                 default -> throw new NoSuchElementException(EXCEPTION_MESSAGE);
             }
         }
-        return new String[]{
-                CASE_SUPPLY,
-                String.valueOf(copySumForSupply),
-                CASE_BUY,
-                String.valueOf(copySumForBuy)
-        };
-    }
-
-    private StringBuilder generateReport(int sumForBuy, int sumForSupply,
-            StringBuilder textForSavingToFile) {
-        return textForSavingToFile.append(CASE_SUPPLY).append(SPLITERATOR)
+        return report.append(CASE_SUPPLY)
+                .append(SPLITERATOR)
                 .append(sumForSupply)
                 .append(System.lineSeparator())
                 .append(CASE_BUY)
@@ -87,29 +51,20 @@ public class WorkWithFile {
                 .append(System.lineSeparator())
                 .append(RESULT)
                 .append(SPLITERATOR)
-                .append(sumForSupply - sumForBuy);
+                .append(sumForSupply - sumForBuy)
+                .toString();
     }
 
-    private String[] getSplitConstructTextFromFile(StringBuilder constructTextFromFile) {
-        return constructTextFromFile.toString().split(SPLIT_BY_NEW_LINE);
-    }
-
-    private void readFile(
-            BufferedReader reader,
-            StringBuilder constructTextFromFile) {
-        int value;
-        try {
-            value = reader.read();
-        } catch (IOException e) {
-            throw new RuntimeException(EXCEPTION_MESSAGE);
-        }
-        while (value != END_FILE_CODE) {
-            constructTextFromFile.append((char) value);
-            try {
-                value = reader.read();
-            } catch (IOException e) {
-                throw new RuntimeException(EXCEPTION_MESSAGE);
+    private String readFile(String fromFileName) {
+        StringBuilder data = new StringBuilder();
+        try (FileReader reader = new FileReader(fromFileName)) {
+            int value;
+            while ((value = reader.read()) != -1) {
+                data.append((char)value);
             }
+        } catch (IOException ex) {
+            System.err.println(ex.getMessage());
         }
+        return data.toString();
     }
 }
