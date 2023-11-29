b11f0d1c4a2c3f5f598e3fa71fb4d1af095670b3
Bohdan <kbs20043131@gmail.com>
2023-11-29 18:30:07 +0200

    divide program logic

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,48 +2,68 @@ package core.basesyntax;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+    private static final int ACTION_TYPE_INDEX = 0;
+    private static final int AMOUNT_INDEX = 1;
+
     public void getStatistic(String fromFileName, String toFileName) {
-        File file = new File(fromFileName);
-        File writeFile = new File(toFileName);
-        int supplyAmount = 0;
-        int buyAmount = 0;
-        try (BufferedReader reader = new BufferedReader(new FileReader(file));
-                BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(writeFile));) {
+        String[] fileContent = readFromFile(fromFileName);
+        String report = prepareReport(fileContent);
+        writeToFile(report, toFileName);
+    }
+
+    private String [] readFromFile(String fileName) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName));) {
             StringBuilder stringBuilder = new StringBuilder();
             String value = reader.readLine();
             while (value != null) {
                 stringBuilder.append(value).append(";");
                 value = reader.readLine();
             }
-            String[] table = stringBuilder.toString().split(";");
-
-            for (int i = 0; i < table.length; i++) {
-                String[] data = table[i].split(",");
-                switch (data[0]) {
-                    case "supply":
-                        supplyAmount += Integer.parseInt(data[1]);
-                        break;
-                    case "buy":
-                        buyAmount += Integer.parseInt(data[1]);
-                        break;
-                    default: break;
-                }
-            }
-            StringBuilder stringBuildera = new StringBuilder();
-            stringBuildera.append("supply").append(",").append(supplyAmount)
-                    .append(System.lineSeparator()).append("buy").append(",").append(buyAmount)
-                    .append(System.lineSeparator()).append("result").append(",")
-                    .append(supplyAmount - buyAmount);
-            bufferedWriter.write(stringBuildera.toString());
-
+            return stringBuilder.toString().split(";");
         } catch (IOException e) {
-            throw new RuntimeException("can`t do it", e);
+            throw new RuntimeException("can`t read file", e);
+        }
+    }
+
+    private String prepareReport(String[] fileContent) {
+        int supplyAmount = 0;
+        int buyAmount = 0;
+        for (int i = 0; i < fileContent.length; i++) {
+            String[] data = fileContent[i].split(",");
+            switch (data[ACTION_TYPE_INDEX]) {
+                case SUPPLY:
+                    supplyAmount += Integer.parseInt(data[AMOUNT_INDEX]);
+                    break;
+                case BUY:
+                    buyAmount += Integer.parseInt(data[AMOUNT_INDEX]);
+                    break;
+                default:
+                    throw new RuntimeException("Report type should be "
+                            + SUPPLY + " or " + BUY);
+            }
+        }
+        StringBuilder report = new StringBuilder();
+        return report.append(SUPPLY).append(",").append(supplyAmount)
+                .append(System.lineSeparator()).append(BUY).append(",").append(buyAmount)
+                .append(System.lineSeparator()).append("result").append(",")
+                .append(supplyAmount - buyAmount).toString();
+
+    }
+
+    private void writeToFile(String report, String toFileName) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+            bufferedWriter.write(report);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to write data to file.", e);
         }
     }
 }
+
+
