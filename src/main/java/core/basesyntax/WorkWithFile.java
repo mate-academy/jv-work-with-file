03695502ac18fe8a1d1341c60715f58ef374c532
Tetiana Khrestina <khrestinatatiana@gmail.com>
2023-11-30 17:34:02 +0200

    1. Deleted blank lines. 2. Made this method like algorithm. Used this template to refactor your code:String dataFromFile = readFile(fromFileName);String report = createReport(dataFromFile); writeReportToFile(report, toFileName); 3. Made magic values as CONSTANTS.

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,50 +7,63 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
-        int supply = 0;
-        int buy = 0;
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+    private static final int FIRST_INDEX = 0;
+    private static final int NEXT_INDEX = 1;
 
+    public void getStatistic(String fromFileName, String toFileName) {
+        String dataFromFile = readFile(fromFileName);
+        String report = createReport(dataFromFile);
+        writeReportToFile(report, toFileName);
+    }
+
+    private String readFile(String fromFileName) {
+        StringBuilder data = new StringBuilder();
         try {
             BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
-            String line = reader.readLine();
-            while (line != null) {
-                String[] parts = line.split(",");
-                if (parts[0].equals("supply")) {
-                    supply += Integer.parseInt(parts[1]);
-                } else if (parts[0].equals("buy")) {
-                    buy += Integer.parseInt(parts[1]);
-                }
-                line = reader.readLine();
+            String lineFromScV = reader.readLine();
+
+            while (lineFromScV != null) {
+                data.append(lineFromScV).append(System.lineSeparator());
+                lineFromScV = reader.readLine();
             }
+
             reader.close();
         } catch (IOException e) {
-            throw new RuntimeException("Can't read the data from Filename", e);
+            throw new RuntimeException("Can't read the data from " + fromFileName, e);
         }
-        int result = supply - buy;
+        return data.toString();
+    }
 
+    private String createReport(String dataFromFile) {
+        int supplyAmout = 0;
+        int buyAmount = 0;
+        String[] lines = dataFromFile.split(System.lineSeparator());
+
+        for (String line: lines) {
+            String[] partsOfData = line.split(",");
+            if (partsOfData[FIRST_INDEX].equals(SUPPLY)) {
+                supplyAmout += Integer.parseInt(partsOfData[NEXT_INDEX]);
+            } else if (partsOfData[FIRST_INDEX].equals(BUY)) {
+                buyAmount += Integer.parseInt(partsOfData[NEXT_INDEX]);
+            }
+
+        }
+        int result = supplyAmout - buyAmount;
+        return SUPPLY + "," + supplyAmout + System.lineSeparator() + BUY
+                + "," + buyAmount + System.lineSeparator() + "result" + ","
+                + result + System.lineSeparator();
+    }
+
+    private void writeReportToFile(String report,String toFileName) {
+        BufferedWriter writer = null;
         try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName));
-            writer.write("supply," + supply + System.lineSeparator());
-            writer.write("buy," + buy + System.lineSeparator());
-            writer.write("result," + result);
+            writer = new BufferedWriter(new FileWriter(toFileName));
+            writer.write(report);
             writer.close();
-
         } catch (IOException e) {
-            throw new RuntimeException("Can't write data to FileName", e);
+            throw new RuntimeException("Can't write the data to " + toFileName, e);
         }
-
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
