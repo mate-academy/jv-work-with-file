94f55de81772cd64158e6afc0092207eddc6708e
Maksym Mazlov <mazlovmaxim@gmail.com>
2023-12-01 07:57:47 +0200

    decomposed read Method

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.List;
 
 public class WorkWithFile {
@@ -14,46 +15,49 @@ public class WorkWithFile {
     public static final String BUY = "buy";
     public static final String RESULT = "result";
     public static final String SEPARATOR = ",";
-    private int supplyAmount = 0;
-    private int buyAmount = 0;
+    private List<String> data = new ArrayList<>();
 
     public void getStatistic(String fromFileName, String toFileName) {
-        readData(fromFileName);
+        data = readData(fromFileName);
+        createReport(data);
         writeData(toFileName);
     }
 
-    private void readData(String fromFileName) {
+    private List<String> readData(String fromFileName) {
         File fromFile = new File(fromFileName);
         try {
-            List<String> allDataLines = Files.readAllLines(fromFile.toPath());
-            for (String line : allDataLines) {
-                String[] split = line.split(SEPARATOR);
-                if (split[INDEX_OPERATION_TYPE].equals(SUPPLY)) {
-                    supplyAmount += Integer.parseInt(split[INDEX_AMOUNT]);
-                }
-                if (split[INDEX_OPERATION_TYPE].equals(BUY)) {
-                    buyAmount += Integer.parseInt(split[INDEX_AMOUNT]);
-                }
-            }
+            return Files.readAllLines(fromFile.toPath());
         } catch (IOException e) {
-            throw new RuntimeException("Can`t read file", e);
+            throw new RuntimeException("Can't read data from the file " + fromFileName, e);
         }
+    }
+
+    private String createReport(List<String> dataForReport) {
+        int supplyAmount = 0;
+        int buyAmount = 0;
+
+        for (String line : dataForReport) {
+            String[] split = line.split(SEPARATOR);
+            if (split[INDEX_OPERATION_TYPE].equals(SUPPLY)) {
+                supplyAmount += Integer.parseInt(split[INDEX_AMOUNT]);
+            }
+            if (split[INDEX_OPERATION_TYPE].equals(BUY)) {
+                buyAmount += Integer.parseInt(split[INDEX_AMOUNT]);
+            }
+        }
+        return SUPPLY + SEPARATOR + supplyAmount + System.lineSeparator()
+                + BUY + SEPARATOR + buyAmount + System.lineSeparator()
+                + RESULT + SEPARATOR + (supplyAmount - buyAmount) + System.lineSeparator();
     }
 
     private void writeData(String toFileName) {
         File toFile = new File(toFileName);
         if (!toFile.exists()) {
             try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFile, true))) {
-                bufferedWriter.write(createReport());
+                bufferedWriter.write(createReport(data));
             } catch (IOException e) {
-                throw new RuntimeException("Can`t write file", e);
+                throw new RuntimeException("Can't write report to the file " + toFileName, e);
             }
         }
-    }
-
-    private String createReport() {
-        return SUPPLY + SEPARATOR + supplyAmount + System.lineSeparator()
-                + BUY + SEPARATOR + buyAmount + System.lineSeparator()
-                + RESULT + SEPARATOR + (supplyAmount - buyAmount) + System.lineSeparator();
     }
 }
