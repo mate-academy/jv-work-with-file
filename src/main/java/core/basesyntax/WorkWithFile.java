021df2140e608ed767182c3734c5b33124f0df9d
Roman Soproniuk <anteumbra@gmail.com>
2023-12-06 14:46:24 +0200

    add all changes from review, thanks for your feedback

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,42 +7,35 @@ import java.io.IOException;
 import java.nio.file.Files;
 
 public class WorkWithFile {
+    private static final char SEPARATOR = ';';
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+    private static final String RESULT_STR = "result";
 
     public void getStatistic(String fromFileName, String toFileName) {
         String[] dataFromFile = readDataFromFile(fromFileName);
-        String finishReport = createReport(dataFromFile);
-        File file = new File(toFileName);
-        try {
-            file.createNewFile();
-        } catch (IOException e) {
-            throw new RuntimeException("Can't creat file");
-        }
-        try {
-            Files.write(file.toPath(), finishReport.getBytes());
-        } catch (IOException e) {
-            throw new RuntimeException("Can't write report to file");
-        }
+        String report = createReport(dataFromFile);
+        WorkWithFile.writeDataToFile(toFileName, report);
     }
 
-    public String createReport(String[]dataForReport) {
-        String[] supplyBuyResult = {"supply", "buy", "result"};
+    public String createReport(String[] dataForReport) {
         int supplyAmount = 0;
         int buyAmount = 0;
         for (int i = 0; i < dataForReport.length; i++) {
             String[] operationTypeAndAmount = dataForReport[i].split(",");
             String operationType = operationTypeAndAmount[0];
             int amount = Integer.parseInt(operationTypeAndAmount[1]);
-            if (operationType.equals(supplyBuyResult[0])) {
+            if (operationType.equals(SUPPLY)) {
                 supplyAmount += amount;
             }
-            if (operationType.equals(supplyBuyResult[1])) {
+            if (operationType.equals(BUY)) {
                 buyAmount += amount;
             }
         }
         int result = supplyAmount - buyAmount;
-        return supplyBuyResult[0] + "," + supplyAmount + System.lineSeparator()
-                + supplyBuyResult[1] + "," + buyAmount + System.lineSeparator()
-                + supplyBuyResult[2] + "," + result;
+        return SUPPLY + "," + supplyAmount + System.lineSeparator()
+                + BUY + "," + buyAmount + System.lineSeparator()
+                + RESULT_STR + "," + result;
     }
 
     public String[] readDataFromFile(String fromFileName) {
@@ -50,15 +43,28 @@ public class WorkWithFile {
         try {
             BufferedReader reader = new BufferedReader(new FileReader(file));
             StringBuilder stringBuilder = new StringBuilder();
-            String value = reader.readLine();
-            while (value != null) {
-                stringBuilder.append(value).append(";");
-                value = reader.readLine();
+            String value;
+            while ((value = reader.readLine()) != null) {
+                stringBuilder.append(value).append(SEPARATOR);
             }
             String[] dataFromFile = stringBuilder.toString().split(";");
             return dataFromFile;
         } catch (IOException e) {
-            throw new RuntimeException("Can't read data from file");
+            throw new RuntimeException("Can't read data from file", e);
+        }
+    }
+
+    public static void writeDataToFile(String toFileName, String report) {
+        File file = new File(toFileName);
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            throw new RuntimeException("Can't creat file", e);
+        }
+        try {
+            Files.write(file.toPath(), report.getBytes());
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write report to file", e);
         }
     }
 }
