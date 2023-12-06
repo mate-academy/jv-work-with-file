d8e31f4e388e240d06dc863845c98236101f9528
arrem4eg <arem4eg@gmail.com>
2023-12-06 21:01:17 +0100

    fixed HW jv-work-with-file

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -14,31 +14,31 @@ public class WorkWithFile {
 
     public void getStatistic(String fromFileName, String toFileName) {
 
-            try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName));
-                 BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName));
+             BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
 
-                int sumBuy = 0;
-                int sumSupply = 0;
-                String line;
-                while ((line = bufferedReader.readLine()) != null) {
-                    System.out.println(line);
-                    if (line.contains(BUY)) {
-                        sumBuy += Integer.parseInt(line.replaceAll("[^0-9]+", ""));
-                    } else {
-                        sumSupply += Integer.parseInt(line.replaceAll("[^0-9]+", ""));
-                    }
+            int sumBuy = 0;
+            int sumSupply = 0;
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+                if (line.contains(BUY)) {
+                    sumBuy += Integer.parseInt(line.replaceAll("[^0-9]+", ""));
+                } else {
+                    sumSupply += Integer.parseInt(line.replaceAll("[^0-9]+", ""));
                 }
-
-                StringBuilder resul = new StringBuilder();
-                resul.append(SUPPLY).append(COMMA).append(sumSupply).append(System.lineSeparator())
-                        .append(BUY).append(COMMA).append(sumBuy).append(System.lineSeparator())
-                        .append(RESULT).append(COMMA).append(sumSupply - sumBuy);
-
-                bufferedWriter.write(resul.toString());
-                bufferedWriter.flush();
-
-            } catch (IOException e) {
-                throw new RuntimeException();
             }
+
+            StringBuilder resul = new StringBuilder();
+            resul.append(SUPPLY).append(COMMA).append(sumSupply).append(System.lineSeparator())
+                    .append(BUY).append(COMMA).append(sumBuy).append(System.lineSeparator())
+                    .append(RESULT).append(COMMA).append(sumSupply - sumBuy);
+
+            bufferedWriter.write(resul.toString());
+            bufferedWriter.flush();
+
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
     }
 }
