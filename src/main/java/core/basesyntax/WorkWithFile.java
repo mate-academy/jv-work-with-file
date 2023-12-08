af912e5498210f641b4ece74e948a663826a06bd
Vlad Bondarev <bondarev.21@icloud.com>
2023-12-08 12:25:58 +0200

    hm for review variant 2

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,6 +8,41 @@ import java.io.IOException;
 
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
+        String fileGotRead = readFromFile(fromFileName);
+        String[] dividedByEmptySpace = fileGotRead.split(" ");
+        int supplySum = getSupplySum(dividedByEmptySpace);
+        int buySum = getBuySum(dividedByEmptySpace);
+        int benefit = supplySum - buySum;
+        writeToFile(toFileName, supplySum, buySum, benefit);
+    }
+
+    public int getSupplySum(String[] dividedByEmptySpace) {
+        int supplySum = 0;
+        for (String info : dividedByEmptySpace) {
+            String[] dividedByComma = info.split(",");
+            String firstElement = dividedByComma[0];
+            int amountOfMoney = Integer.parseInt(dividedByComma[1]);
+            if (firstElement.equals("supply")) {
+                supplySum += amountOfMoney;
+            }
+        }
+        return supplySum;
+    }
+
+    public int getBuySum(String[] dividedByEmptySpace) {
+        int buySum = 0;
+        for (String info : dividedByEmptySpace) {
+            String[] dividedByComma = info.split(",");
+            String firstElement = dividedByComma[0];
+            int amountOfMoney = Integer.parseInt(dividedByComma[1]);
+            if (firstElement.equals("buy")) {
+                buySum += amountOfMoney;
+            }
+        }
+        return buySum;
+    }
+
+    public String readFromFile(String fromFileName) {
         StringBuilder fullFileText = new StringBuilder();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             String line = bufferedReader.readLine();
@@ -16,23 +51,12 @@ public class WorkWithFile {
                 line = bufferedReader.readLine();
             }
         } catch (IOException e) {
-            throw new RuntimeException("There is an exception at line 17 named ", e);
+            throw new RuntimeException("There is an exception at line 70 named ", e);
         }
-        String[] dividedByEmptySpace = fullFileText.toString().split(" ");
-        int supplySum = 0;
-        int buySum = 0;
-        for (String info : dividedByEmptySpace) {
-            String[] dividedByComma = info.split(",");
-            int amountOfMoney = Integer.parseInt(dividedByComma[1]);
-            String firstElememt = dividedByComma[0];
-            if (firstElememt.equals("supply")) {
-                supplySum += amountOfMoney;
-            }
-            if (firstElememt.equals("buy")) {
-                buySum += amountOfMoney;
-            }
-        }
-        int benefit = supplySum - buySum;
+        return fullFileText.toString();
+    }
+
+    public void writeToFile(String toFileName, int supplySum, int buySum, int benefit) {
         StringBuilder result = new StringBuilder("supply,").append(supplySum)
                 .append("\n").append("buy,").append(buySum)
                 .append("\n").append("result,").append(benefit);
