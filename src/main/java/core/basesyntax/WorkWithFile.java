41fc9e92dcb2c558e6e22b2113e21eae2a5bfb03
Valentyn Sharshon <woosay333@gmail.com>
2023-12-14 11:50:13 +0200

    implemented method getStatistic(String fromFileName, String toFileName), which reads data from file 'fromFileName', processes it, and writes the result to file 'toFileName'

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,60 @@
 package core.basesyntax;
 
-public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
+public class WorkWithFile {
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        List<String> lines = readFromFile(fromFileName);
+        Map<String, Integer> map = new TreeMap<>(Comparator.reverseOrder());
+
+        for (String line : lines) {
+            String[] splitLine = line.split(",");
+            map.merge(splitLine[0], Integer.parseInt(splitLine[1]), Integer::sum);
+        }
+
+        writeToFile(map, toFileName);
+    }
+
+    private List<String> readFromFile(String fromFileName) {
+        try {
+            return Files.readAllLines(Paths.get(fromFileName));
+        } catch (IOException ex) {
+            throw new RuntimeException("Can`t read data from the file "
+                    + fromFileName, ex);
+        }
+    }
+
+    private void writeToFile(Map<String, Integer> map, String toFileName) {
+        try (BufferedWriter bufferedWriter = Files.newBufferedWriter(Paths.get(toFileName),
+                StandardCharsets.UTF_8)) {
+            map.forEach((key, value) -> {
+                try {
+                    bufferedWriter.write(key + "," + value);
+                    bufferedWriter.write(System.lineSeparator());
+                } catch (IOException ex) {
+                    throw new RuntimeException("Can`t write entry with key = " + key
+                            + " and value = " + value, ex);
+                }
+            });
+
+            bufferedWriter.write("result," + getDifferenceBetweenSupplierAndBuy(map.get("supply"),
+                    map.get("buy")));
+        } catch (IOException ex) {
+            throw new RuntimeException("Can`t write data to the file "
+                    + toFileName, ex);
+        }
+    }
+
+    private Integer getDifferenceBetweenSupplierAndBuy(Integer supply, Integer buy) {
+        return supply - buy;
     }
 }
