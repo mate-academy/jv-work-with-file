05ebff7f333d2f831032f90de6b57999c20f00a4
Yevhen <shymeiko.zenya@gmail.com>
2023-12-25 16:58:26 +0200

    fixedmistakes

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,22 +7,22 @@ import java.io.IOException;
 import java.nio.file.Files;
 
 public class WorkWithFile {
+
+    private static final String STRING_SUPPLY = "supply";
+    private static final String STRING_BUY = "buy";
+    private static final String STRING_RESULT = "result";
+    private static final int SUPPLY_START_NUMBER = 7;
+    private static final int BUY_START_NUMBER = 4;
+
     public void getStatistic(String fromFileName, String toFileName) {
-        int supply = 0;
-        int buy = 0;
-        String[] tableFromFile = readFile(fromFileName);
-        for (String line : tableFromFile) {
-            if (line.contains("supply")) {
-                supply += Integer.parseInt(line.substring(7));
-            } else if (line.contains("buy")) {
-                buy += Integer.parseInt(line.substring(4));
-            }
-        }
-        String report = createReport(supply, buy);
+        String data = readFile(fromFileName);
+        String[] tableFromFile = formatData(data);
+        int[] supplyAndBuy = parseStatistic(tableFromFile);
+        String report = createReport(supplyAndBuy[0], supplyAndBuy[1]);
         writeToFile(report, toFileName);
     }
 
-    public String[] readFile(String fromFile) {
+    public String readFile(String fromFile) {
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFile))) {
             StringBuilder stringBuilder = new StringBuilder();
             String line = bufferedReader.readLine();
@@ -30,22 +30,38 @@ public class WorkWithFile {
                 stringBuilder.append(line).append(System.lineSeparator());
                 line = bufferedReader.readLine();
             }
-            String[] fileTable = stringBuilder.toString().split(System.lineSeparator());
-            return fileTable;
+            return stringBuilder.toString();
         } catch (IOException e) {
             throw new RuntimeException("Can't read file!", e);
         }
     }
 
+    private String[] formatData(String data) {
+        String[] fileTable = data.split(System.lineSeparator());
+        return fileTable;
+    }
+
+    private int[] parseStatistic(String[] table) {
+        int[] supplyAndBuy = new int[] {0, 0};
+        for (String line : table) {
+            if (line.contains(STRING_SUPPLY)) {
+                supplyAndBuy[0] += Integer.parseInt(line.substring(SUPPLY_START_NUMBER));
+            } else if (line.contains(STRING_BUY)) {
+                supplyAndBuy[1] += Integer.parseInt(line.substring(BUY_START_NUMBER));
+            }
+        }
+        return supplyAndBuy;
+    }
+
     public String createReport(int supply, int buy) {
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("supply,")
+        stringBuilder.append(STRING_SUPPLY).append(",")
                 .append(supply)
                 .append(System.lineSeparator())
-                .append("buy,")
+                .append(STRING_BUY).append(",")
                 .append(buy)
                 .append(System.lineSeparator())
-                .append("result,")
+                .append(STRING_RESULT).append(",")
                 .append(supply - buy);
         return stringBuilder.toString();
     }
