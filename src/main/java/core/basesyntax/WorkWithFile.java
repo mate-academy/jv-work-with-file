20d1d41098126c723cbc2aff4d7de34e8998b589
Jakub <jszymanski97@gmail.com>
2023-12-28 15:20:15 +0000

    Split the code in to three methods dataFromFile, report and writeToFile

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -15,47 +15,59 @@ public class WorkWithFile {
     private static final String RESULT = "result";
 
     public void getStatistic(String fromFileName, String toFileName) {
+        String dataFromFile = readFile(fromFileName);
+        String report = createReport(dataFromFile);
+        writeToFile(report, toFileName);
+    }
 
+    private String readFile(String fromFileName) {
         List<String> lines = new ArrayList<>();
-        File file = new File(fromFileName);
-
-        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(new File(fromFileName)))) {
             String value = reader.readLine();
             while (value != null) {
                 lines.add(value);
                 value = reader.readLine();
             }
         } catch (IOException e) {
-            throw new RuntimeException("Can't read from the file", e);
+            throw new RuntimeException("Can't read from the file " + fromFileName, e);
         }
+        return String.join("\n", lines);
+    }
 
+    private String createReport(String dataFromFile) {
         int supplyTotal = 0;
         int buyTotal = 0;
 
+        String[] lines = dataFromFile.split("\n");
+        StringBuilder reportBuilder = new StringBuilder();
+
         for (String line : lines) {
-            if (line.startsWith(SUPPLY)) {
+            if (line.startsWith(SUPPLY) || line.startsWith(BUY)) {
                 String[] parts = line.split(",");
                 String numberValue = parts[1];
                 int value = Integer.parseInt(numberValue);
-                supplyTotal += value;
-            } else if (line.startsWith(BUY)) {
-                String[] parts = line.split(",");
-                String numberValue = parts[1];
-                int value = Integer.parseInt(numberValue);
-                buyTotal += value;
+
+                if (line.startsWith(SUPPLY)) {
+                    supplyTotal += value;
+                } else {
+                    buyTotal += value;
+                }
             }
         }
-        int resultTotal = supplyTotal - buyTotal;
-        File finalFile = new File(toFileName);
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(finalFile))) {
-            writer.write(SUPPLY + "," + supplyTotal);
-            writer.newLine();
-            writer.write(BUY + "," + buyTotal);
-            writer.newLine();
-            writer.write(RESULT + "," + resultTotal);
-        } catch (IOException e) {
-            throw new RuntimeException("Can't write to the file", e);
-        }
 
+        int resultTotal = supplyTotal - buyTotal;
+        reportBuilder.append(SUPPLY).append(",").append(supplyTotal).append("\n")
+                .append(BUY).append(",").append(buyTotal).append("\n")
+                .append(RESULT).append(",").append(resultTotal);
+
+        return reportBuilder.toString();
+    }
+
+    private void writeToFile(String report, String toFileName) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(new File(toFileName)))) {
+            writer.write(report);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write to the file " + toFileName, e);
+        }
     }
 }
