c89030c79ba75cb51291cbbee3e271077a2aba5d
Anton <StarAntonU@gmail.com>
2023-12-29 16:01:07 +0200

    create method readFile, implement getStatistic and write here

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,48 @@
 package core.basesyntax;
 
-public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class WorkWithFile {
+    private static int totalSupply;
+    private static int totalBuy;
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        String fileData = readFile(fromFileName);
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
+            writer.write(fileData);
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t write file", e);
+        }
+    }
+
+    private String readFile(String fileName) {
+        totalSupply = 0;
+        totalBuy = 0;
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] splitLine = line.split(",");
+                if (splitLine[0].equals("supply")) {
+                    totalSupply += Integer.parseInt(splitLine[1]);
+                }
+                if (splitLine[0].equals("buy")) {
+                    totalBuy += Integer.parseInt(splitLine[1]);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("Can`t find file " + fileName, e);
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t read file " + fileName, e);
+        }
+        StringBuilder builderResult = new StringBuilder();
+        builderResult.append("supply,").append(totalSupply).append(System.lineSeparator());
+        builderResult.append("buy,").append(totalBuy).append(System.lineSeparator());
+        builderResult.append("result,").append(totalSupply - totalBuy);
+        return builderResult.toString();
     }
 }
