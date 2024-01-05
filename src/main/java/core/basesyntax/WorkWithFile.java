e2f8b69d264ff505a00fcdc3a0812fda3c7be7ba
Oleksandr Kalenyk <carbonmail88@gmail.com>
2024-01-05 20:36:00 +0200

    jv-work-with-file solution

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,50 @@
 package core.basesyntax;
 
-public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class WorkWithFile {
+    private static final String SPLIT_CHARACTER = ",";
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        File fromFile = new File(fromFileName);
+        File toFile = new File(toFileName);
+        StringBuilder builder = new StringBuilder();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFile))) {
+            String value = reader.readLine();
+
+            while (value != null) {
+                builder.append(value).append(SPLIT_CHARACTER);
+                value = reader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read from file", e);
+        }
+
+        String[] data = builder.toString().split(SPLIT_CHARACTER);
+        int supply = 0;
+        int buy = 0;
+
+        for (int i = 0; i < data.length; i++) {
+            if (data[i].equals("supply")) {
+                supply += Integer.valueOf(data[i + 1]);
+            }
+            if (data[i].equals("buy")) {
+                buy += Integer.valueOf(data[i + 1]);
+            }
+        }
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFile))) {
+            writer.write("supply," + supply + System.lineSeparator()
+                          + "buy," + buy + System.lineSeparator()
+                          + "result," + (supply - buy));
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write to file", e);
+        }
     }
 }
