fc501f32ed6ee80c4a0505499e83a278b866c42b
Viktor Oblapenko <viktor.oblapenko@gmail.com>
2024-01-09 22:28:19 +0200

    refactored code as advised

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,72 +1,66 @@
 package core.basesyntax;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    private static final int NUMBER_OF_REPORT_FIELD = 3;
-    private static final int SUPPLY_INDEX = 0;
-    private static final int BUY_INDEX = 1;
-    private static final int RESULT_INDEX = 2;
     private static final String BUY_FIELD = "buy";
     private static final String SUPPLY_FIELD = "supply";
     private static final String RESULT_FIELD = "result";
+    private static final String COMA_SPLITTER = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        int[] reportArray = new int[NUMBER_OF_REPORT_FIELD];
+        String dataFromFile = readFile(fromFileName);
+        String reportFromData = generateReport(dataFromFile);
+        writeToFile(reportFromData,toFileName);
+    }
+
+    private String readFile(String fromFileName) {
+        StringBuilder content = new StringBuilder();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             String line;
             while ((line = bufferedReader.readLine()) != null) {
-                String[] wordsArray = line.split(",");
-                for (int i = 0; i < wordsArray.length; i++) {
-                    if (wordsArray[i].equals(SUPPLY_FIELD)) {
-                        i++;
-                        reportArray[SUPPLY_INDEX] += Integer.parseInt(wordsArray[i]);
-                        break;
-                    }
-                    if (wordsArray[i].equals(BUY_FIELD)) {
-                        i++;
-                        reportArray[BUY_INDEX] += Integer.parseInt(wordsArray[i]);
-                        break;
-                    }
-                }
-            }
-            reportArray[RESULT_INDEX] = reportArray[SUPPLY_INDEX] - reportArray[BUY_INDEX];
-            writeToFile(reportArray, toFileName);
-        } catch (IOException e) {
-            throw new RuntimeException("Can't read the file", e);
-        }
-    }
-
-    private void writeToFile(int[] report, String fileName) throws IOException {
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName))) {
-            for (int i = 0; i < report.length; i++) {
-                if (i == SUPPLY_INDEX) {
-                    writeField(bufferedWriter, SUPPLY_FIELD, report[i], true);
-                }
-                if (i == BUY_INDEX) {
-                    writeField(bufferedWriter, BUY_FIELD, report[i], true);
-                }
-                if (i == RESULT_INDEX) {
-                    writeField(bufferedWriter, RESULT_FIELD, report[i], false);
-                }
+                content.append(line).append(System.lineSeparator());
             }
         } catch (IOException e) {
-            throw new RuntimeException("Can't read the file", e);
+            throw new RuntimeException("Can't read the file " + fromFileName, e);
         }
+        return content.toString();
     }
 
-    private void writeField(BufferedWriter writer, String field, int value, boolean isSeparator)
-            throws IOException {
-        StringBuilder reportBuilder = new StringBuilder();
-        reportBuilder.append(field).append(",").append(value);
-        if (isSeparator) {
-            reportBuilder.append(System.lineSeparator());
+    private String generateReport(String data) {
+        String[] lines = data.split(System.lineSeparator());
+        StringBuilder report = new StringBuilder();
+        int totalSupply = 0;
+        int totalBuy = 0;
+        for (String line : lines) {
+            String[] values = line.split(COMA_SPLITTER);
+            for (int i = 0; i < values.length; i++) {
+                if (values[i].equals(SUPPLY_FIELD)) {
+                    i++;
+                    totalSupply += Integer.parseInt(values[i]);
+                } else if (values[i].equals(BUY_FIELD)) {
+                    i++;
+                    totalBuy += Integer.parseInt(values[i]);
+                }
+            }
         }
-        writer.write(reportBuilder.toString());
-        writer.flush();
+        int result = totalSupply - totalBuy;
+        report.append(SUPPLY_FIELD).append(COMA_SPLITTER).append(totalSupply)
+                .append(System.lineSeparator());
+        report.append(BUY_FIELD).append(COMA_SPLITTER).append(totalBuy)
+                .append(System.lineSeparator());
+        report.append(RESULT_FIELD).append(COMA_SPLITTER).append(result);
+        return report.toString();
+    }
+
+    private void writeToFile(String report, String toFile) {
+        try (FileWriter writer = new FileWriter(toFile)) {
+            writer.write(report);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write the report to file " + toFile, e);
+        }
     }
 }
