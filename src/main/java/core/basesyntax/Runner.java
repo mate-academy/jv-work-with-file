131ef3c23377783d74b7a1af089a6b1600012b97
voody <tarasyashchuk089@gmail.com>
2024-01-12 19:40:34 +0200

    Applied suggested changes Changed logic of writing and reading Decided to use Files.readAllLines to read all lines to list at once

--- src/main/java/core/basesyntax/Runner.java
+++
@@ -1,8 +0,0 @@
-package core.basesyntax;
-
-public class Runner {
-    public static void main(String[] args) {
-        WorkWithFile workWithFile = new WorkWithFile();
-        workWithFile.getStatistic("banana.csv","test.txt");
-    }
-}
--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,48 +1,61 @@
 package core.basesyntax;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName));
-                BufferedWriter bufferedWriter =
-                     new BufferedWriter(new FileWriter(toFileName))) {
+    private static final int INDEX_OF_WORD = 0;
+    private static final int INDEX_OF_NUMBER = 1;
 
-            StringBuilder stringBuilder = new StringBuilder();
-            String line = bufferedReader.readLine();
+    public void getStatistic(String fromFileName, String toFileName) {
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(fromFileName));
+
             int supplySum = 0;
             int buySum = 0;
-            while (line != null) {
-                stringBuilder.append(line).append(System.lineSeparator());
+
+            for (String line : lines) {
                 String[] parts = line.split(",");
-                int extractedNumber = Integer.parseInt(parts[1]);
-                if (parts[0].equals("supply")) {
+                int extractedNumber = Integer.parseInt(parts[INDEX_OF_NUMBER]);
+
+                if (parts[INDEX_OF_WORD].equals("supply")) {
                     supplySum += extractedNumber;
-                } else if (parts[0].equals("buy")) {
+                } else if (parts[INDEX_OF_WORD].equals("buy")) {
                     buySum += extractedNumber;
                 }
-
-                line = bufferedReader.readLine();
             }
+
             int result = supplySum - buySum;
-            makeReport(supplySum, buySum, result, bufferedWriter);
+
+            List<String> report = createReport(supplySum, buySum, result);
+            writeReport(toFileName, report);
+
         } catch (IOException e) {
             throw new RuntimeException("Error while processing file: "
                     + e.getMessage(), e);
         }
     }
 
-    private static void makeReport(int supplySum, int buySum, int result,
-                                   BufferedWriter bufferedWriter) throws IOException {
-        String[] report = {"supply," + supplySum + System.lineSeparator(),
-                "buy," + buySum + System.lineSeparator(),
-                "result," + result + System.lineSeparator()};
-        for (String s : report) {
-            bufferedWriter.write(s);
+    private List<String> createReport(int supplySum, int buySum, int result) {
+        List<String> report = new ArrayList<>();
+        report.add("supply," + supplySum);
+        report.add("buy," + buySum);
+        report.add("result," + result);
+        return report;
+    }
+
+    private void writeReport(String toFileName, List<String> report) {
+        try (BufferedWriter bufferedWriter = Files.newBufferedWriter(Paths.get(toFileName))) {
+            for (String line : report) {
+                bufferedWriter.write(line + System.lineSeparator());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Error while writing report to file: "
+                    + e.getMessage(), e);
         }
     }
 }
