0e34633d9af78b08bb2e380f4847a41bd83e2767
NekitCool1 <gnnikita97@gmail.com>
2024-01-15 14:40:44 +0200

    All mistakes after review were fixed

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -9,15 +9,45 @@ import java.util.List;
 
 public class WorkWithFile {
 
-    private StringBuilder stringBuilder = new StringBuilder();
-
     public void getStatistic(String fromFileName, String toFileName) {
         File file = new File(toFileName);
         if (file.exists()) {
             file.delete();
         }
+        final String[] arrayToFile = calculatingArrayToFile(fromFileName);
+        writeToFile(arrayToFile, toFileName);
+    }
+
+    private String[] readFromFile(String fileName) {
+        File file = new File(fileName);
+        try {
+            List<String> list = Files.readAllLines(file.toPath());
+            String[] result = new String[list.size()];
+            for (int i = 0; i < result.length; i++) {
+                result [i] = list.get(i);
+            }
+            return result;
+        } catch (IOException e) {
+            throw new RuntimeException("can't read from the file", e);
+        }
+    }
+
+    private void writeToFile(String[] fromArray, String toFileName) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName, true))) {
+            for (String array : fromArray) {
+                bufferedWriter.write(array + "\n");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("can't write to the file", e);
+        }
+    }
+
+    private String[] calculatingArrayToFile(String fromFileName) {
+        final String supply = "supply,";
+        final String buy = "buy,";
+        final String result = "result,";
         String[] arrayFromFile = readFromFile(fromFileName);
-        String[] arrayToFile = {"supply,", "buy,", "result,"};
+        String[] arrayToFile = {supply, buy, result};
         int tempForSupply = 0;
         int tempForBuy = 0;
 
@@ -34,36 +64,6 @@ public class WorkWithFile {
         arrayToFile[1] += String.valueOf(tempForBuy);
         arrayToFile[2] += String.valueOf(tempForSupply - tempForBuy);
 
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName, true))) {
-            for (String array : arrayToFile) {
-                bufferedWriter.write(array + "\n");
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public String[] readFromFile(String fileName) {
-        File file = new File(fileName);
-        try {
-            List<String> list = Files.readAllLines(file.toPath());
-            String[] result = new String[list.size()];
-            for (int i = 0; i < result.length; i++) {
-                result [i] = list.get(i);
-            }
-            return result;
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public void writeToFile(String[] fromArray, String toFileName) {
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName, true))) {
-            for (String array : fromArray) {
-                bufferedWriter.write(array);
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        return arrayToFile;
     }
 }
