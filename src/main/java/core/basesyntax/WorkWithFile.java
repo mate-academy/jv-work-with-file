2b5be03222cbd2e78f40061066f4366d3e812e44
Vitaliy <vitaliyfilimonov3@gmail.com>
2024-01-17 20:19:38 +0200

    Implemented getStatistic method to read, sort, calculate data, and write to a new file

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,57 @@
 package core.basesyntax;
 
-public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class WorkWithFile {
+    private static final int OPTION = 0;
+    private static final int VALUE = 1;
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        int buy = 0;
+        int supply = 0;
+        File file = new File(fromFileName);
+        String[] fileContent = new String[(int) file.length()];
+        if (file.length() != 0) {
+            try {
+                BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+                String line = bufferedReader.readLine();
+                int i = 0;
+                while (line != null) {
+                    fileContent[i] = line;
+                    i++;
+                    line = bufferedReader.readLine();
+                }
+            } catch (IOException e) {
+                throw new RuntimeException("Can't read the file", e);
+            }
+            for (String content : fileContent) {
+                if (content != null) {
+                    String[] currentLine = content.split(",");
+                    if (currentLine[OPTION].equals("supply")) {
+                        supply += Integer.parseInt(currentLine[VALUE]);
+                    }
+                    if (currentLine[OPTION].equals("buy")) {
+                        buy += Integer.parseInt(currentLine[VALUE]);
+                    }
+                }
+            }
+        }
+        String[] actualStatistic = new String[3];
+        File resultFile = new File(toFileName);
+        try (BufferedWriter bufferedWriter
+                     = new BufferedWriter(new FileWriter(resultFile, false))) {
+            bufferedWriter.write("supply," + supply);
+            bufferedWriter.newLine();
+            bufferedWriter.write("buy," + buy);
+            bufferedWriter.newLine();
+            bufferedWriter.write("result," + (supply - buy));
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write the file", e);
+        }
     }
 }
