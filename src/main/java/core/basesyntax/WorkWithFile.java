b73fa9488378dabcfd6d206a8b72934452f5aa88
Alex Naumenko <alex_naumenko_@ukr.net>
2024-01-18 15:17:27 +0200

    implemented readFile and createReport methods

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,7 +2,6 @@ package core.basesyntax;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -13,40 +12,58 @@ public class WorkWithFile {
     private static final int NUMBER_INDEX = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        File file = new File(fromFileName);
-        int supply = 0;
-        int buy = 0;
+        String dataFromFile = readFile(fromFileName);
+        String report = createReport(dataFromFile);
+        writeReportToFile(report, toFileName);
+    }
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+    private String readFile(String fromFileName) {
+        StringBuilder dataFromFile = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
             String line = reader.readLine();
             while (line != null) {
-                String[] textLine = line.split(SEPARATOR);
-                if (textLine[WORD_INDEX].equals("supply")) {
-                    supply += Integer.parseInt(textLine[NUMBER_INDEX]);
-                } else {
-                    buy += Integer.parseInt(textLine[NUMBER_INDEX]);
-                }
+                dataFromFile
+                        .append(line)
+                        .append(System.lineSeparator());
                 line = reader.readLine();
             }
         } catch (IOException e) {
             throw new RuntimeException("Can't read file", e);
         }
 
-        writeReportToFile(toFileName, supply, buy);
+        return dataFromFile.toString();
     }
 
-    private void writeReportToFile(String filename, int supply, int buy) {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
-            StringBuilder report = new StringBuilder()
-                    .append("supply,")
-                    .append(supply)
-                    .append(System.lineSeparator())
-                    .append("buy,")
-                    .append(buy)
-                    .append(System.lineSeparator())
-                    .append("result,")
-                    .append(supply - buy);
-            writer.write(report.toString());
+    private String createReport(String dataFromFile) {
+        int supply = 0;
+        int buy = 0;
+        String[] dataLines = dataFromFile.split(System.lineSeparator());
+
+        for (int i = 0; i < dataLines.length; i++) {
+            String[] separateWords = dataLines[i].split(SEPARATOR);
+            if (separateWords[WORD_INDEX].equals("supply")) {
+                supply += Integer.parseInt(separateWords[NUMBER_INDEX]);
+            } else {
+                buy += Integer.parseInt(separateWords[NUMBER_INDEX]);
+            }
+        }
+
+        StringBuilder report = new StringBuilder()
+                .append("supply,")
+                .append(supply)
+                .append(System.lineSeparator())
+                .append("buy,")
+                .append(buy)
+                .append(System.lineSeparator())
+                .append("result,")
+                .append(supply - buy);
+
+        return report.toString();
+    }
+
+    private void writeReportToFile(String report, String toFileName) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
+            writer.write(report);
         } catch (IOException e) {
             throw new RuntimeException("Can't write data to file", e);
         }
