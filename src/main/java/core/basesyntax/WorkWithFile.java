2b941f1267f0f23ce1e03d3a21e3886c72e98581
Kateryna Riznychenko <lyqvin.kr@gmail.com>
2024-01-19 19:26:47 +0200

    implemented program logic

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,65 @@
 package core.basesyntax;
 
-public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
+import java.io.*;
 
+public class WorkWithFile {
+    private static final String SUPPLY_TYPE = "supply,";
+    private static final String BUY_TYPE = "buy,";
+    private static final String RESULT_TYPE = "result,";
+    private static final String NEW_LINE = System.lineSeparator();
+    private static final int MINUS_ONE_INDEX = -1;
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        String readData = readFromFile(fromFileName);
+        StringBuilder dataToWrite = new StringBuilder();
+        int supplySum = getCalculations(readData, SUPPLY_TYPE);
+        int buySum = getCalculations(readData, BUY_TYPE);
+        int result = supplySum - buySum;
+
+        dataToWrite.append(SUPPLY_TYPE).append(supplySum).append(NEW_LINE)
+                .append(BUY_TYPE).append(buySum).append(NEW_LINE)
+                .append(RESULT_TYPE).append(result);
+
+        writeToFile(toFileName, dataToWrite.toString());
     }
+
+    public String readFromFile(String fromFileName) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
+            int value = bufferedReader.read();
+            while (value != MINUS_ONE_INDEX) {
+                char c = (char) value;
+                stringBuilder.append(c);
+                value = bufferedReader.read();
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read from file", e);
+        }
+        return stringBuilder.toString();
+    }
+
+    public void writeToFile(String toFileName, String data) {
+        try (FileWriter myWriter = new FileWriter(toFileName)) {
+            myWriter.write(data);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write to file", e);
+        }
+    }
+
+    public int getCalculations(String data, String type) {
+        int calculatedSum = 0;
+        String[] parts;
+        for (String line : data.split(NEW_LINE)) {
+            if (line.startsWith(type)) {
+                parts = line.split(",");
+                for (int i = 1; i < parts.length; i += 2) {
+                    calculatedSum += Integer.parseInt(parts[i].trim());
+                }
+            }
+        }
+        return calculatedSum;
+    }
+
 }
