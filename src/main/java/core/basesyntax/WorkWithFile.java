23897503ba135519f58ac75f46253f14b0bb6d69
moskalenkosergii <moskalenko.s.work@gmail.com>
2024-01-20 13:31:53 +0100

    refactoring code, changed readFile, generateReport and writeToFile methods

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,64 +8,73 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    public static final String SEPARATOR = " ";
+    public static final String DELIMITER = ",";
+    public static final int NAME = 0;
+    public static final int VALUE = 1;
+    public static final int SUPPLY = 0;
+    public static final int BUY = 1;
+    public static final int RESULT = 2;
+    public static final int LINES_IN_REPORT = 3;
+    public static final String[] NAME_VALUES = {"supply", "buy", "result"};
+
     public void getStatistic(String fromFileName, String toFileName) {
+        String data = readFile(fromFileName);
+        String report = generateReport(data);
+        writeToFile(toFileName,report);
+    }
+
+    private String readFile(String filePath) {
         StringBuilder saveDataToStringFromExistFile = new StringBuilder();
 
         try {
-            BufferedReader readerFromExistFile = new BufferedReader(new FileReader(fromFileName));
+            BufferedReader readerFromExistFile = new BufferedReader(new FileReader(filePath));
             String value = readerFromExistFile.readLine();
 
             while (value != null) {
-                saveDataToStringFromExistFile.append(value).append(" ");
+                saveDataToStringFromExistFile.append(value).append(SEPARATOR);
                 value = readerFromExistFile.readLine();
             }
 
         } catch (IOException e) {
             throw new RuntimeException("File is not found ",e);
         }
-
-        createReport(calculateValue(saveDataToStringFromExistFile), toFileName);
-
+        return saveDataToStringFromExistFile.toString();
     }
 
-    public int[] calculateValue(StringBuilder stringBuilderValues) {
-
-        String[] splitSaveDatas = stringBuilderValues.toString().split("\\s+");
-        int supply = 0;
-        int buy = 0;
-        int result;
+    public String generateReport(String data) {
+        String[] splitSaveDatas = data.split("\\s+");
+        int [] resultData = new int[LINES_IN_REPORT];
 
         for (String splitSaveData : splitSaveDatas) {
-            String[] splitValue = splitSaveData.split(",");
-            String value = splitValue[0];
-            int valueCount = Integer.parseInt(splitValue[1]);
+            String[] splitValue = splitSaveData.split(DELIMITER);
+            String value = splitValue[NAME];
+            int valueCount = Integer.parseInt(splitValue[VALUE]);
 
             if (value.equals("supply")) {
-                supply += valueCount;
+                resultData[SUPPLY] += valueCount;
             } else {
-                buy += valueCount;
+                resultData[BUY] += valueCount;
             }
         }
-        result = supply - buy;
-
-        return new int[]{supply, buy, result};
-    }
-
-    public void createReport(int[] result, String toFileName) {
-        File file = new File(toFileName);
-        String[] nameValues = {"supply", "buy", "result"};
+        resultData[RESULT] = resultData[SUPPLY] - resultData[BUY];
         StringBuilder resultValuesString = new StringBuilder();
 
-        for (int i = 0; i < nameValues.length; i++) {
+        for (int i = 0; i < NAME_VALUES.length; i++) {
             resultValuesString
-                    .append(nameValues[i])
-                    .append(",")
-                    .append(result[i])
+                    .append(NAME_VALUES[i])
+                    .append(DELIMITER)
+                    .append(resultData[i])
                     .append(System.lineSeparator());
         }
+        return resultValuesString.toString();
+    }
+
+    public void writeToFile(String toFileName, String report) {
+        File file = new File(toFileName);
 
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
-            bufferedWriter.write(resultValuesString.toString());
+            bufferedWriter.write(report);
         } catch (IOException e) {
             throw new RuntimeException("Can't write to file", e);
         }
