02e7cec1bdef6ceff24bab6ce67211042ac29b7f
Kateryna Riznychenko <lyqvin.kr@gmail.com>
2024-01-22 17:14:28 +0200

    remade program structure

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -10,23 +10,16 @@ public class WorkWithFile {
     private static final String BUY_TYPE = "buy,";
     private static final String RESULT_TYPE = "result,";
     private static final String NEW_LINE = System.lineSeparator();
+    private static final String SEPARATOR = ",";
     private static final int MINUS_ONE_INDEX = -1;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        String readData = readFromFile(fromFileName);
-        StringBuilder dataToWrite = new StringBuilder();
-        int supplySum = getCalculations(readData, SUPPLY_TYPE);
-        int buySum = getCalculations(readData, BUY_TYPE);
-        int result = supplySum - buySum;
-
-        dataToWrite.append(SUPPLY_TYPE).append(supplySum).append(NEW_LINE)
-                .append(BUY_TYPE).append(buySum).append(NEW_LINE)
-                .append(RESULT_TYPE).append(result);
-
-        writeToFile(toFileName, dataToWrite.toString());
+        String readData = readFile(fromFileName);
+        String report = generateReport(readData);
+        writeToFile(toFileName, report);
     }
 
-    public String readFromFile(String fromFileName) {
+    public String readFile(String fromFileName) {
         StringBuilder readData = new StringBuilder();
 
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
@@ -51,12 +44,20 @@ public class WorkWithFile {
         }
     }
 
+    private String generateReport(String data) {
+        int supplySum = getCalculations(data, SUPPLY_TYPE);
+        int buySum = getCalculations(data, BUY_TYPE);
+        int result = supplySum - buySum;
+
+        return String.format("%s%d%n%s%d%n%s%d", SUPPLY_TYPE, supplySum, BUY_TYPE, buySum, RESULT_TYPE, result);
+    }
+
     public int getCalculations(String data, String type) {
         int calculatedSum = 0;
         String[] parts;
         for (String line : data.split(NEW_LINE)) {
             if (line.startsWith(type)) {
-                parts = line.split(",");
+                parts = line.split(SEPARATOR);
                 for (int i = 1; i < parts.length; i += 2) {
                     calculatedSum += Integer.parseInt(parts[i].trim());
                 }
