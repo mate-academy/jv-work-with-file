d3fa21b65e89228095811223057f0e8274412643
Kate Drevilo <rabmargo@gmail.com>
2024-01-25 15:34:37 +0200

    new added fixed specify filename, create method createReport.

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -12,7 +12,7 @@ public class WorkWithFile {
     private static final String BUY = "buy";
     private static final int INDEX_ZERO = 0;
     private static final int INDEX_ONE = 1;
-    private static final int TWO = 2;
+    private static final int VALID_PARTS_AMOUNT = 2;
 
     public void getStatistic(String fromFileName, String toFileName) {
         String dataFromFile = readFromFile(fromFileName);
@@ -29,7 +29,7 @@ public class WorkWithFile {
             }
             return data.toString();
         } catch (IOException ex) {
-            throw new RuntimeException("Error reading file", ex);
+            throw new RuntimeException("Error reading file: " + fromFileName, ex);
         }
     }
 
@@ -40,20 +40,24 @@ public class WorkWithFile {
         String[] lines = dataFromFile.split(LINE_SEPARATOR);
         for (String line : lines) {
             String[] parts = line.split(",");
-            if (parts.length == TWO) {
-                int quantity = Integer.parseInt(parts[INDEX_ONE].trim());
-                if (SUPPLY.equals(parts[INDEX_ZERO].trim())) {
-                    supplyTotal += quantity;
-                } else if (BUY.equals(parts[INDEX_ZERO].trim())) {
-                    buyTotal += quantity;
-                }
+            if (parts.length != VALID_PARTS_AMOUNT) {
+                throw new RuntimeException("Incorrect number of parts in the string. Expected: "
+                        + VALID_PARTS_AMOUNT + ", actual: " + parts.length);
+            }
+            int quantity = Integer.parseInt(parts[INDEX_ONE].trim());
+            if (SUPPLY.equals(parts[INDEX_ZERO].trim())) {
+                supplyTotal += quantity;
+            } else if (BUY.equals(parts[INDEX_ZERO].trim())) {
+                buyTotal += quantity;
             }
         }
+        return createReport(supplyTotal, buyTotal);
+    }
 
+    private String createReport(int supplyTotal, int buyTotal) {
         int result = supplyTotal - buyTotal;
-
         StringBuilder report = new StringBuilder();
-        report.append(SUPPLY + "," + supplyTotal).append(LINE_SEPARATOR);
+        report.append(SUPPLY + ",").append(supplyTotal).append(LINE_SEPARATOR);
         report.append(BUY).append(",").append(buyTotal).append(LINE_SEPARATOR);
         report.append("result").append(",").append(result);
 
@@ -65,7 +69,7 @@ public class WorkWithFile {
             writer.write(report);
             writer.newLine();
         } catch (IOException e) {
-            throw new RuntimeException("Can't write file", e);
+            throw new RuntimeException("Can't write file: " + toFileName, e);
         }
     }
 }
