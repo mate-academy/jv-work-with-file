4cac7ca4a73478f39c44d1015a677a539d706f7c
Igor Hunder <gunderigorv@gmail.com>
2024-01-25 12:17:41 +0200

    I read and wrote the data to a file

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,7 +2,6 @@ package core.basesyntax;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -11,59 +10,57 @@ public class WorkWithFile {
     private static final int OPERATION_TYPE_INDEX = 0;
     private static final int AMOUNT_INDEX = 1;
     private static final String NEW_LINE = System.lineSeparator();
-    private static final String VARIABLE1 = "supply";
-    private static final String VARIABLE2 = "buy";
-    private static final String RESULT_VARIABLES = "result";
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+    private static final String RESULT = "result";
 
     public void getStatistic(String fromFileName, String toFileName) {
         String data = readFile(fromFileName);
-        String result = calculateDate(data);
-        writeDateToFile(toFileName, result);
+        String report = createReport(data);
+        writeDateToFile(toFileName, report);
     }
 
     private String readFile(String fileName) {
         StringBuilder builder = new StringBuilder();
-        File file = new File(fileName);
-        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             String value = reader.readLine();
             while (value != null) {
                 builder.append(value).append(NEW_LINE);
                 value = reader.readLine();
             }
         } catch (IOException e) {
-            throw new RuntimeException("Can`t read file", e);
+            throw new RuntimeException("Can`t read file" + fileName, e);
         }
         return builder.toString();
     }
 
-    private String calculateDate(String data) {
+    private String createReport(String data) {
         StringBuilder builder = new StringBuilder();
         String[] line = data.split(NEW_LINE);
         int supplySum = 0;
         int buySum = 0;
         for (String lines : line) {
             String[] parts = lines.split(",");
-            if (parts[OPERATION_TYPE_INDEX].equals(VARIABLE1)) {
+            if (parts[OPERATION_TYPE_INDEX].equals(SUPPLY)) {
                 supplySum += Integer.parseInt(parts[AMOUNT_INDEX]);
-            } else if (parts[OPERATION_TYPE_INDEX].equals(VARIABLE2)) {
+            } else if (parts[OPERATION_TYPE_INDEX].equals(BUY)) {
                 buySum += Integer.parseInt(parts[AMOUNT_INDEX]);
             }
         }
-        builder.append(VARIABLE1).append(",").append(supplySum)
+        builder.append(SUPPLY).append(",").append(supplySum)
                 .append(NEW_LINE)
-                .append(VARIABLE2).append(",").append(buySum)
+                .append(BUY).append(",").append(buySum)
                 .append(NEW_LINE)
-                .append(RESULT_VARIABLES)
+                .append(RESULT)
                 .append(",").append(supplySum - buySum);
         return builder.toString();
     }
 
     private void writeDateToFile(String toFile, String result) {
-        File newFile = new File(toFile);
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(newFile))) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFile))) {
             writer.write(result);
         } catch (IOException e) {
-            throw new RuntimeException("Can't write to file", e);
+            throw new RuntimeException("Can't write to file" + toFile, e);
         }
     }
 }
