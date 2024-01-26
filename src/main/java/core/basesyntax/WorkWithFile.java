161dd4512df7f68417f7ddbce29187743527ca89
yaroslav olieniev <olieniev.od.ua@gmail.com>
2024-01-26 19:07:50 +0300

    divided method into submethods, fixed minor mistakes

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,35 +8,46 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    static final int OPERATION_TYPE_POSITION = 0;
+    static final int AMOUNT_POSITION = 1;
     public void getStatistic(String fromFileName, String toFileName) {
-        File fromFile = new File(fromFileName);
+        String report = processDataFromFile(fromFileName);
+        writeToFile(report, toFileName);
+    }
+    public String processDataFromFile (String fromFileName) {
         int supplyCount = 0;
         int buyCount = 0;
         try {
-            BufferedReader reader = new BufferedReader(new FileReader(fromFile));
+            BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
             String fileString = reader.readLine();
             while (fileString != null) {
                 String[] lineValues = fileString.split(",");
-                if (lineValues[0].equals("supply")) {
-                    supplyCount += Integer.parseInt(lineValues[1]);
+                if (lineValues[OPERATION_TYPE_POSITION].equals("supply")) {
+                    supplyCount += Integer.parseInt(lineValues[AMOUNT_POSITION]);
                 } else {
-                    buyCount += Integer.parseInt((lineValues[1]));
+                    buyCount += Integer.parseInt((lineValues[AMOUNT_POSITION]));
                 }
                 fileString = reader.readLine();
             }
         } catch (IOException e) {
-            throw new RuntimeException("Can't read file", e);
+            throw new RuntimeException("Can't read file" + fromFileName, e);
         }
-        StringBuilder builder = new StringBuilder();
+        return createReport(supplyCount, buyCount);
+    }
+    public String createReport (int supplyCount, int buyCount) {
+        StringBuilder report = new StringBuilder();
         int result = supplyCount - buyCount;
-        builder.append("supply,").append(supplyCount).append(System.lineSeparator());
-        builder.append("buy,").append(buyCount).append(System.lineSeparator());
-        builder.append("result,").append(result).append(System.lineSeparator());
-        File toFile = new File(toFileName);
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFile))) {
-            bufferedWriter.write(builder.toString());
+        report.append("supply,").append(supplyCount).append(System.lineSeparator());
+        report.append("buy,").append(buyCount).append(System.lineSeparator());
+        report.append("result,").append(result).append(System.lineSeparator());
+        return report.toString();
+    }
+
+    public void writeToFile (String report, String toFileName) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+            bufferedWriter.write(report);
         } catch (IOException e) {
-            throw new RuntimeException("Can't write to file", e);
+            throw new RuntimeException("Can't write to file" + toFileName, e);
         }
     }
 }
