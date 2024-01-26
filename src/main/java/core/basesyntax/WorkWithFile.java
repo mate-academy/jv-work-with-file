843e162e3699dcfa6072e092ea3fd14e3bd15f30
yaroslav olieniev <olieniev.od.ua@gmail.com>
2024-01-26 19:09:50 +0300

    divided method into submethods, fixed minor mistakes

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,7 +2,6 @@ package core.basesyntax;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -10,11 +9,13 @@ import java.io.IOException;
 public class WorkWithFile {
     static final int OPERATION_TYPE_POSITION = 0;
     static final int AMOUNT_POSITION = 1;
+
     public void getStatistic(String fromFileName, String toFileName) {
         String report = processDataFromFile(fromFileName);
         writeToFile(report, toFileName);
     }
-    public String processDataFromFile (String fromFileName) {
+
+    public String processDataFromFile(String fromFileName) {
         int supplyCount = 0;
         int buyCount = 0;
         try {
@@ -34,7 +35,8 @@ public class WorkWithFile {
         }
         return createReport(supplyCount, buyCount);
     }
-    public String createReport (int supplyCount, int buyCount) {
+
+    public String createReport(int supplyCount, int buyCount) {
         StringBuilder report = new StringBuilder();
         int result = supplyCount - buyCount;
         report.append("supply,").append(supplyCount).append(System.lineSeparator());
@@ -43,7 +45,7 @@ public class WorkWithFile {
         return report.toString();
     }
 
-    public void writeToFile (String report, String toFileName) {
+    public void writeToFile(String report, String toFileName) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
             bufferedWriter.write(report);
         } catch (IOException e) {
