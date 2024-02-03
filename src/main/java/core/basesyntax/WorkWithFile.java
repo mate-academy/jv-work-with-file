e7e91534fdcc4b2e0ab551f61d8f6c8e2ea77867
Sergey Sukhachov <westard@proton.me>
2024-02-03 20:47:37 +0200

    logic has been splited to 3 separate methodss

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -11,13 +11,18 @@ public class WorkWithFile {
     private static final String OPERATION_BUY = "buy";
 
     public void getStatistic(String fromFileName, String toFileName) {
+        int supplySum = readFromFile(fromFileName)[0];
+        int buySum = readFromFile(fromFileName)[1];
+        writeReport(createReport(supplySum, buySum), toFileName);
+    }
+
+    private int[] readFromFile(String fromFileName) {
         int supplySum = 0;
         int buySum = 0;
-        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
-                BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
-            String value = reader.readLine();
-            while (value != null) {
-                String[] parts = value.split(",");
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(",");
                 String operationType = parts[0];
                 int amount = Integer.parseInt(parts[1]);
                 if (operationType.equals(OPERATION_SUPPLY)) {
@@ -25,14 +30,26 @@ public class WorkWithFile {
                 } else if (operationType.equals(OPERATION_BUY)) {
                     buySum += amount;
                 }
-                value = reader.readLine();
             }
-            StringBuilder builder = new StringBuilder();
-            builder.append(OPERATION_SUPPLY).append(",").append(supplySum)
-                    .append(System.lineSeparator()).append(OPERATION_BUY)
-                    .append(",").append(buySum).append(System.lineSeparator())
-                    .append("result,").append(supplySum - buySum);
-            writer.write(builder.toString());
+        } catch (IOException e) {
+            throw new RuntimeException("File cannot be found");
+        }
+        return new int[] {supplySum, buySum};
+    }
+
+    private String createReport(int supplySum, int buySum) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(OPERATION_SUPPLY).append(",").append(supplySum)
+                .append(System.lineSeparator())
+                .append(OPERATION_BUY).append(",").append(buySum)
+                .append(System.lineSeparator())
+                .append("result").append(",").append(supplySum - buySum);
+        return builder.toString();
+    }
+
+    private void writeReport(String data, String toFileName) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
+            writer.write(data);
         } catch (IOException e) {
             throw new RuntimeException("Cannot write on file");
         }
