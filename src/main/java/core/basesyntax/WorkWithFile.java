3e619e3ab298be79cc7c786d167cf0b2a1fa64d6
Vitaliy <vitaliyufc@gmail.com>
2024-02-05 23:15:14 +0300

    Fixed spreadsheet task

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -63,15 +63,4 @@ public class WorkWithFile {
             throw new RuntimeException("Can`t to write into the file!", e);
         }
     }
-
-    public static void main(String[] args) {
-        WorkWithFile workWithFile = new WorkWithFile();
-        workWithFile.getStatistic("apple.csv", "apple_report.csv");
-        System.out.println(System.lineSeparator());
-        workWithFile.getStatistic("banana.csv", "banana_report.csv");
-        System.out.println(System.lineSeparator());
-        workWithFile.getStatistic("grape.csv", "grape_report.csv");
-        System.out.println(System.lineSeparator());
-        workWithFile.getStatistic("orange.csv", "orange_report.csv");
-    }
 }
