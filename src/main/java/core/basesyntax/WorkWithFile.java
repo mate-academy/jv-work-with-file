7b8a1a76e8a200feea800d3f816c0a8693c6e94f
Evhen Timofeiev <timofeevzhenya2107@gmail.com>
2024-02-07 16:26:27 +0200

    added methods getStatistic, readFromFile, createReport after review

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,33 +8,51 @@ import java.io.IOException;
 
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
+        String data = readFromFile(fromFileName);
+        String report = createReport(data);
+        writeToFile(report,toFileName);
+    }
+
+    private String readFromFile(String fileName) {
         String line;
-        int supplyCount = 0;
-        int buyCount = 0;
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
             while ((line = bufferedReader.readLine()) != null) {
-                String [] input = line.split(",");
-                switch (input[0]) {
-                    case "supply":
-                        supplyCount += Integer.parseInt(input[1]);
-                        break;
-                    case "buy":
-                        buyCount += Integer.parseInt(input[1]);
-                        break;
-                    default:
-                        break;
-                }
+                stringBuilder.append(line).append(System.lineSeparator());
             }
         } catch (IOException e) {
             throw new RuntimeException("problems with file!", e);
         }
-        String result = "supply," + supplyCount + System.lineSeparator()
+        return stringBuilder.toString();
+    }
+
+    private String createReport(String data) {
+        int supplyCount = 0;
+        int buyCount = 0;
+        String [] lines = data.split(System.lineSeparator());
+        for (String line : lines) {
+            String [] input = line.split(",");
+            switch (input[0]) {
+                case "supply":
+                    supplyCount += Integer.parseInt(input[1]);
+                    break;
+                case "buy":
+                    buyCount += Integer.parseInt(input[1]);
+                    break;
+                default:
+                    break;
+            }
+        }
+        return "supply," + supplyCount + System.lineSeparator()
                 + "buy," + buyCount + System.lineSeparator()
                 + "result," + (supplyCount - buyCount);
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
-            bufferedWriter.write(result);
+    }
+
+    private void writeToFile(String report, String fileName) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName))) {
+            bufferedWriter.write(report);
         } catch (IOException e) {
-            throw new RuntimeException("problems with file!", e);
+            throw new RuntimeException("Problems with file!");
         }
     }
 }
