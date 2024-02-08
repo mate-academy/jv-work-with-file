fe10651835bf17366e76d2b6bcb19e6b670fe041
Evhen Timofeiev <timofeevzhenya2107@gmail.com>
2024-02-08 16:03:38 +0200

    add fixes after review

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,6 +7,10 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    private static final int OPERATION_INDEX = 0;
+    private static final int AMOUNT_INDEX = 1;
+    private static final String LINE_SEPARATOR = System.lineSeparator();
+
     public void getStatistic(String fromFileName, String toFileName) {
         String data = readFromFile(fromFileName);
         String report = createReport(data);
@@ -18,10 +22,10 @@ public class WorkWithFile {
         StringBuilder stringBuilder = new StringBuilder();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
             while ((line = bufferedReader.readLine()) != null) {
-                stringBuilder.append(line).append(System.lineSeparator());
+                stringBuilder.append(line).append(LINE_SEPARATOR);
             }
         } catch (IOException e) {
-            throw new RuntimeException("problems with file!", e);
+            throw new RuntimeException("Problems with file: " + fileName, e);
         }
         return stringBuilder.toString();
     }
@@ -29,22 +33,22 @@ public class WorkWithFile {
     private String createReport(String data) {
         int supplyCount = 0;
         int buyCount = 0;
-        String [] lines = data.split(System.lineSeparator());
+        String [] lines = data.split(LINE_SEPARATOR);
         for (String line : lines) {
             String [] input = line.split(",");
-            switch (input[0]) {
+            switch (input[OPERATION_INDEX]) {
                 case "supply":
-                    supplyCount += Integer.parseInt(input[1]);
+                    supplyCount += Integer.parseInt(input[AMOUNT_INDEX]);
                     break;
                 case "buy":
-                    buyCount += Integer.parseInt(input[1]);
+                    buyCount += Integer.parseInt(input[AMOUNT_INDEX]);
                     break;
                 default:
                     break;
             }
         }
-        return "supply," + supplyCount + System.lineSeparator()
-                + "buy," + buyCount + System.lineSeparator()
+        return "supply," + supplyCount + LINE_SEPARATOR
+                + "buy," + buyCount + LINE_SEPARATOR
                 + "result," + (supplyCount - buyCount);
     }
 
@@ -52,7 +56,7 @@ public class WorkWithFile {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName))) {
             bufferedWriter.write(report);
         } catch (IOException e) {
-            throw new RuntimeException("Problems with file!");
+            throw new RuntimeException("Problems with file: " + fileName, e);
         }
     }
 }
