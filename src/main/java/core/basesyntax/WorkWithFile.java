851a1a202fbd0dcea35d9dcbc925892abd2ac7a9
Dominik Gągało <cliffcamer@gmail.com>
2024-02-12 20:37:26 +0100

    WorkWithFile homework

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,54 @@
 package core.basesyntax;
 
-public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
+import java.io.*;
 
+public class WorkWithFile {
+    int supplyTotal = 0;
+    int buyTotal = 0;
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        try {
+
+            try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    processFile(line, supplyTotal, buyTotal);
+                }
+            }
+            int result = supplyTotal - buyTotal;
+
+            writeReport(toFileName, supplyTotal, buyTotal, result);
+        } catch (IOException e) {
+            throw new RuntimeException("Error processing file", e);
+        }
+    }
+    private void processFile(String line, int supplyTotal, int buyTotal) {
+        String[] lines = line.split(",");
+        if (lines.length == 2) {
+            String operationType = lines[0].trim();
+            int amount = Integer.parseInt(lines[1].trim());
+
+            if ("supply".equals(operationType)) {
+                supplyTotal += amount;
+            }
+
+            if ("buy".equals(operationType)) {
+                buyTotal += amount;
+            }
+            this.supplyTotal = supplyTotal;
+            this.buyTotal = buyTotal;
+        }
+    }
+
+    private static void writeReport (String toFileName, int supplyTotal, int buyTotal, int result) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
+            writer.write("supply, " + supplyTotal);
+            writer.newLine();
+            writer.write("buy, " + buyTotal);
+            writer.newLine();
+            writer.write("result, " + result);
+        } catch (IOException e) {
+            throw new RuntimeException("Error wtiting to file " + toFileName, e);
+        }
     }
 }
