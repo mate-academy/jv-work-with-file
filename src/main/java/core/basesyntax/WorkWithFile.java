2286b52fa79f54120f9e50b4ecb5da2103799444
HAIOVIY <gaevoywork@gmail.com>
2024-02-14 16:37:33 +0200

    add some constans

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,18 +7,22 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    private static final String SPLITTER = ",";
+    private static final String SUPPLY = "supply";
+    private static final int STEP_COUNT = 2;
+
     public void getStatistic(String fromFileName, String toFileName) {
         int supplySum = 0;
         int buySum = 0;
         String readString = null;
         try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
             while ((readString = reader.readLine()) != null) {
-                String[] strArray = readString.split(",");
-                for (int i = 0; i < strArray.length; i += 2) {
-                    if (strArray[i].startsWith("s")) {
-                        supplySum += Integer.parseInt(strArray[i + 1]);
+                String[] data = readString.split(SPLITTER);
+                for (int i = 0; i < data.length; i += STEP_COUNT) {
+                    if (data[i].startsWith(SUPPLY)) {
+                        supplySum += Integer.parseInt(data[i + 1]);
                     } else {
-                        buySum += Integer.parseInt(strArray[i + 1]);
+                        buySum += Integer.parseInt(data[i + 1]);
                     }
                 }
             }
@@ -31,7 +35,7 @@ public class WorkWithFile {
                     + buySum + "\n" + "result,"
                     + (supplySum - buySum));
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Can't open file", e);
         }
     }
 }
