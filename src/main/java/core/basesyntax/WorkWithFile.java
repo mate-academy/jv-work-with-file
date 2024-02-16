d8bb36c902416e841810e34e6a18c100407d62d2
Eduard Fakhrutdinov <edikf03@gmail.com>
2024-02-16 20:36:53 +0200

    update version

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,63 +2,61 @@ package core.basesyntax;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    public static void getStatistic(String fromFileName, String toFileName) {
+    private static final String SEPARATOR = ",";
+    private static final int OPERATION_TYPE_INDEX = 0;
+    private static final int AMOUNT_INDEX = 1;
 
-        File fileRead = new File(fromFileName);
+    public void getStatistic(String fromFileName, String toFileName) {
+        writeToFile(toFileName, getSumOperationType(fromFileName));
+    }
 
-        int sumSupply = 0;
-        int sumBuy = 0;
-
+    private String[] readFromFile(String fileName) {
+        StringBuilder stringBuilder = new StringBuilder();
         try (BufferedReader reader = new BufferedReader(
-                new FileReader(fileRead))) {
-
-            StringBuilder stringBuilder = new StringBuilder();
-
-            // Read from file
+                new FileReader(fileName))) {
             String value = reader.readLine();
             while (value != null) {
                 stringBuilder.append(value).append(" ");
                 value = reader.readLine();
             }
-
-            String[] splitBySpace = stringBuilder.toString().split(" ");
-
-            for (int i = 0; i < splitBySpace.length; ++i) {
-                String[] splitNameSum = splitBySpace[i].toString().split(",");
-                if (splitNameSum[0].equals("supply")) {
-                    sumSupply += Integer.parseInt(splitNameSum[1]);
-                } else {
-                    sumBuy += Integer.parseInt(splitNameSum[1]);
-                }
-            }
-
-            String[] resultSupply = new String[]{"supply", String.valueOf(sumSupply)};
-            String[] resultBuy = new String[]{"buy", String.valueOf(sumBuy)};
-            String[] resultResult = new String[]{"result", String.valueOf(sumSupply - sumBuy)};
-
-            // Write to file
-            File writeFile = new File(toFileName);
-            try (BufferedWriter bufferedWriter = new BufferedWriter(
-                    new FileWriter(writeFile))) {
-
-                bufferedWriter.write(String.join(",", resultSupply));
-                bufferedWriter.newLine();
-                bufferedWriter.write(String.join(",", resultBuy));
-                bufferedWriter.newLine();
-                bufferedWriter.write(String.join(",", resultResult));
-            } catch (IOException e) {
-                throw new RuntimeException("Can't write to file", e);
-            }
-
         } catch (IOException e) {
             throw new RuntimeException("Can't read a file", e);
         }
+        return stringBuilder.toString().split(" ");
+    }
 
+    private String[] getSumOperationType(String fromFileName) {
+        int sumSupply = 0;
+        int sumBuy = 0;
+        String[] readFromFile = readFromFile(fromFileName);
+        for (String s : readFromFile) {
+            String[] splitNameSum = s.split(",");
+            if (splitNameSum[OPERATION_TYPE_INDEX].equals("supply")) {
+                sumSupply += Integer.parseInt(splitNameSum[AMOUNT_INDEX]);
+            } else {
+                sumBuy += Integer.parseInt(splitNameSum[AMOUNT_INDEX]);
+            }
+        }
+        return new String[]{String.valueOf(sumSupply), String.valueOf(sumBuy)};
+    }
+
+    private void writeToFile(String toFileWrite, String[] sumOperationType) {
+        int sumSupply = Integer.parseInt(sumOperationType[OPERATION_TYPE_INDEX]);
+        int sumBuy = Integer.parseInt(sumOperationType[AMOUNT_INDEX]);
+        try (BufferedWriter bufferedWriter = new BufferedWriter(
+                new FileWriter(toFileWrite))) {
+            bufferedWriter.write("supply" + SEPARATOR + sumSupply);
+            bufferedWriter.newLine();
+            bufferedWriter.write("buy" + SEPARATOR + sumBuy);
+            bufferedWriter.newLine();
+            bufferedWriter.write("result" + SEPARATOR + (sumSupply - sumBuy));
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write to file", e);
+        }
     }
 }
