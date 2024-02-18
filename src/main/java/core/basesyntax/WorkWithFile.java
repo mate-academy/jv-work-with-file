f70ca493220477ce8e00472993ec2b266e6173d4
bodya4243 <105318502+bodya4243@users.noreply.github.com>
2024-02-18 20:04:57 +0200

    1)Method Decomposition 2)Create magic variables 3)Try with resources in "readFromFile"

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,30 +8,28 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+
     public void getStatistic(String fromFileName, String toFileName) {
         File readFile = new File(fromFileName);
         File writeFile = new File(toFileName);
-        StringBuilder builder = new StringBuilder();
 
-        try {
-            String line;
-            BufferedReader reader = new BufferedReader(new FileReader(readFile));
-            while ((line = reader.readLine()) != null) {
-                builder.append(line).append(System.lineSeparator());
-            }
-        } catch (IOException e) {
-            throw new RuntimeException("can't read the file", e);
-        }
-
-        String[] infoArr = builder.toString().split("[\\r\\n\\s]+");
+        String[] infoArr = readFromFile(readFile).toString().split("[\\r\\n\\s]+");
 
         int buy = 0;
         int supply = 0;
+        String buyPrefix = "buy,";
+        String supplyPrefix = "supply,";
+        int buyIndex;
+        int supplyIndex;
+
         for (String info : infoArr) {
-            if (info.startsWith("buy,")) {
-                buy += Integer.parseInt(info.substring(4));
-            } else if (info.startsWith("supply,")) {
-                supply += Integer.parseInt(info.substring(7));
+
+            if (info.startsWith(buyPrefix)) {
+                buyIndex = Integer.parseInt(info.substring(buyPrefix.length()));
+                buy += buyIndex;
+            } else if (info.startsWith(supplyPrefix)) {
+                supplyIndex = Integer.parseInt(info.substring(supplyPrefix.length()));
+                supply += supplyIndex;
             }
         }
 
@@ -40,6 +38,24 @@ public class WorkWithFile {
                 .append("buy,").append(buy).append(System.lineSeparator()).append("result,")
                 .append(supply - buy);
 
+        writeToFile(reportBuilder, writeFile);
+    }
+
+    private StringBuilder readFromFile(File readFile) {
+        StringBuilder builder = new StringBuilder();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(readFile))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                builder.append(line).append(System.lineSeparator());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("can't read the file", e);
+        }
+        return builder;
+    }
+
+    private void writeToFile(StringBuilder reportBuilder, File writeFile) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(writeFile))) {
             writer.write(reportBuilder.toString());
         } catch (IOException e) {
