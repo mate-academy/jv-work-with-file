c3e009856af33c226e82746bcb5451e7736878d6
John <pandastars535@gmail.com>
2024-02-20 00:55:35 +0200

    Added separate methods with own task each implemented that method in getStatistic method

--- src/main/java/core/basesyntax/ReadFile.java
+++ src/main/java/core/basesyntax/ReadFile.java
@@ -5,14 +5,13 @@ import java.io.IOException;
 import java.nio.file.Files;
 
 public class ReadFile {
-
     public String readFileContent(File file) {
         StringBuilder contentOfFile = new StringBuilder();
         try {
             Files.readAllLines(file.toPath()).forEach(line
                     -> contentOfFile.append(line).append(System.lineSeparator()));
         } catch (IOException e) {
-            throw new RuntimeException("cant read the content of the file" + file, e);
+            throw new RuntimeException("cant read the content of the file" + file,e);
         }
         return contentOfFile.toString();
     }
