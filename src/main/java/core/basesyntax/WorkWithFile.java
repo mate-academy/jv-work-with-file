efb294fee411d310d79e2cc63c959325a53658aa
Dmytro Hadiuchko <hadiuchko.dmytro@gmail.com>
2024-02-19 20:32:17 +0200

    the ability to read a file, write and sort is implemented

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,62 @@
 package core.basesyntax;
 
-public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class WorkWithFile {
+    private static final String SEPARATOR = ",";
+    private static final String BUYING_OPERATION = "buy";
+    private static final String SUPPLY_OPERATION = "supply";
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        String readData = readFromFile(fromFileName);
+        String report = makeReport(readData);
+        writeToFile(toFileName,report);
+    }
+
+    private String readFromFile(String fileName) {
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            String value;
+            while ((value = bufferedReader.readLine()) != null) {
+                stringBuilder.append(value).append(System.lineSeparator());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read data from file", e);
+        }
+        return stringBuilder.toString();
+    }
+
+    private String makeReport(String data) {
+        int supplyTotal = 0;
+        int buyTotal = 0;
+        String[] lines = data.split(System.lineSeparator());
+        for (String line : lines) {
+            String[] parts = line.split(SEPARATOR);
+            if (parts.length == 2) {
+                String operationType = parts[0];
+                int amount = Integer.parseInt(parts[1]);
+                if (operationType.equals(BUYING_OPERATION)) {
+                    buyTotal += amount;
+                } else if (operationType.equals(SUPPLY_OPERATION)) {
+                    supplyTotal += amount;
+                }
+            }
+        }
+        int result = supplyTotal - buyTotal;
+        return "supply," + supplyTotal + System.lineSeparator()
+                + "buy," + buyTotal + System.lineSeparator()
+                + "result," + result;
+    }
+
+    private void writeToFile(String toFile,String data) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFile))) {
+            bufferedWriter.write(data);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write to file", e);
+        }
     }
 }
