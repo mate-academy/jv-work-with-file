99f0d24a709218f66faf1f8a4fe66e77d7a52dad
John <pandastars535@gmail.com>
2024-02-19 13:46:13 +0200

    changed read method instead BF added Files

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,22 +1,20 @@
 package core.basesyntax;
 
 import java.io.File;
-import java.nio.file.Files;
 import java.util.Arrays;
 
 public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
-
-
+    public static void main(String[] args) {
+        File file = new File("apple.csv");
+        ReadFile readFile = new ReadFile();
+        String str = readFile.readFileContent(file);
+        String[] result = str.split(System.lineSeparator());
+        System.out.println(Arrays.asList(result));
 
     }
 
-    public static void main(String[] args) {
-        File file = new File("apple.csv");
-   ReadFile readFile = new ReadFile();
-   String str = readFile.readFileContent(file);
-   String[] result = str.split("\\W+");
-        System.out.println(Arrays.asList(result));
+    public void getStatistic(String fromFileName, String toFileName) {
+
 
     }
 }
