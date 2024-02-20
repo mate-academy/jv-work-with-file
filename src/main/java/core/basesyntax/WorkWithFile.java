484c8eb22abd57c77726d19a5955351e7b67a69c
Chopyk Mykola <record7654@gmail.com>
2024-02-20 15:37:46 +0200

    Improved solution

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -6,45 +6,59 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    private static final int POSITION_OF_CATEGORY_IN_DATA = 0;
-    private static final int POSITION_OF_COST_IN_DATA = 1;
+    private static final int OPERATION_TYPE = 0;
+    private static final int AMOUNT = 1;
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+    private static final String COMMA = ",";
+    private static final String RESULT = "result";
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        String createdReport = createReport(fromFileName);
+        writeToFile(toFileName, createdReport);
+    }
 
     private void writeToFile(String toFileName, String data) {
         try (FileWriter writer = new FileWriter(toFileName)) {
             writer.write(data);
         } catch (IOException e) {
-            throw new RuntimeException("Can't open a file" + e);
+            throw new RuntimeException("Can't write into file" + e);
+        }
+    }
+
+    private String readFromFile(String fromFileName) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
+            StringBuilder stringBuilder = new StringBuilder();
+            String lineFromFile = bufferedReader.readLine();
+
+            while (lineFromFile != null) {
+                stringBuilder.append(lineFromFile).append(System.lineSeparator());
+                lineFromFile = bufferedReader.readLine();
+            }
+            return stringBuilder.toString();
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read the data from file" + e);
         }
     }
 
     private String createReport(String fromFileName) {
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
-            int supplyTotal = 0;
-            int buyTotal = 0;
-            String lineFromFile = bufferedReader.readLine();
+        int supplyTotal = 0;
+        int buyTotal = 0;
+        String[] readData = readFromFile(fromFileName).split(System.lineSeparator());
 
-            while (lineFromFile != null) {
-                String[] splitResult = lineFromFile.split(",");
-                if (splitResult[POSITION_OF_CATEGORY_IN_DATA].equals("supply")) {
-                    supplyTotal += Integer.parseInt(splitResult[POSITION_OF_COST_IN_DATA]);
-                } else {
-                    buyTotal += Integer.parseInt(splitResult[POSITION_OF_COST_IN_DATA]);
-                }
-                lineFromFile = bufferedReader.readLine();
+        for (String line : readData) {
+            String[] splitResult = line.split(COMMA);
+            if (splitResult[OPERATION_TYPE].equals(SUPPLY)) {
+                supplyTotal += Integer.parseInt(splitResult[AMOUNT]);
+            } else {
+                buyTotal += Integer.parseInt(splitResult[AMOUNT]);
             }
-            StringBuilder stringBuilder = new StringBuilder();
-            stringBuilder.append("supply,").append(supplyTotal).append(System.lineSeparator());
-            stringBuilder.append("buy,").append(buyTotal).append(System.lineSeparator());
-            stringBuilder.append("result,").append(supplyTotal - buyTotal);
-
-            return stringBuilder.toString();
-        } catch (IOException e) {
-            throw new RuntimeException("Can't open a file" + e);
         }
-    }
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(SUPPLY + COMMA).append(supplyTotal).append(System.lineSeparator());
+        stringBuilder.append(BUY + COMMA).append(buyTotal).append(System.lineSeparator());
+        stringBuilder.append(RESULT + COMMA).append(supplyTotal - buyTotal);
 
-    public void getStatistic(String fromFileName, String toFileName) {
-        String createdReport = createReport(fromFileName);
-        writeToFile(toFileName, createdReport);
+        return stringBuilder.toString();
     }
 }
