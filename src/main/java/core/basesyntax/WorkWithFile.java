9b8c9e4fca4e7ee4a1cac5d9ff6f8e6655433cc8
VolandevlodD <vladradko2000@gmail.com>
2024-02-20 17:41:58 +0200

    Add Method to Generate Market Statistics from CSV File
    Implemented a method getStatistic(String fromFileName, String toFileName) that reads data from a CSV file named fromFileName, processes the information, generates a report, and writes the result to another CSV file named toFileName. The input CSV file contains two columns: "operation type" and "amount", representing different operations and their respective quantities. The method computes the total amounts for each operation type, calculates the result based on the supplied operations, and writes the report to the designated output file.

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,82 @@
 package core.basesyntax;
 
-public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 
+public class WorkWithFile {
+    private static final int OPERATION_TYPE = 0;
+    private static final int OPERATION_QUANTITY = 1;
+    private static final String SUPPLY_OPERATION = "supply";
+    private static final String BUY_OPERATION = "buy";
+    private static final String COMMA = ",";
+    private static final int SUPPLY_COUNTED = 0;
+    private static final int BUY_COUNTED = 1;
+    private static final int SUPPLY_BUY_DIFFERENCE = 2;
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        List<String> fromFileData = readFromFile(fromFileName);
+        String report = makeReport(fromFileData);
+        writeToFile(report, toFileName);
+    }
+
+    private List<String> readFromFile(String fromFileName) {
+        try {
+            return Files.readAllLines(Path.of(fromFileName));
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t read from the file: " + fromFileName, e);
+        }
+    }
+
+    private String makeReport(List<String> dataLines) {
+        String[][] separatedData = separateData(dataLines);
+        int[] buySupplyDiff = countOperations(separatedData);
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(SUPPLY_OPERATION)
+                .append(COMMA)
+                .append(buySupplyDiff[SUPPLY_COUNTED])
+                .append(System.lineSeparator())
+                .append(BUY_OPERATION)
+                .append(COMMA)
+                .append(buySupplyDiff[BUY_COUNTED])
+                .append(System.lineSeparator())
+                .append("result")
+                .append(COMMA)
+                .append(buySupplyDiff[SUPPLY_BUY_DIFFERENCE]);
+        return stringBuilder.toString();
+    }
+
+    private String[][] separateData(List<String> dataLines) {
+        String[][] separatedData = new String[dataLines.size()][];
+        int counter = 0;
+        for (String dataLine : dataLines) {
+            separatedData[counter] = dataLine.split("\\p{Punct}");
+            counter++;
+        }
+        return separatedData;
+    }
+
+    private int[] countOperations(String[][] separatedData) {
+        int buy = 0;
+        int supply = 0;
+        int difference;
+        for (String[] operation : separatedData) {
+            if (operation[OPERATION_TYPE].equals(SUPPLY_OPERATION)) {
+                supply += Integer.parseInt(operation[OPERATION_QUANTITY]);
+            } else {
+                buy += Integer.parseInt(operation[OPERATION_QUANTITY]);
+            }
+        }
+        difference = supply - buy;
+        return new int[]{supply, buy, difference};
+    }
+
+    private void writeToFile(String result, String toFileName) {
+        try {
+            Files.write(Path.of(toFileName), result.getBytes());
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t write to file", e);
+        }
     }
 }
