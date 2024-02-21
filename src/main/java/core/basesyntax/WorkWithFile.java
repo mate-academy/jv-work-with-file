de4a5760150354bbe41151982feaa2acb3689466
Pavlo Betsa <bigworld2303@gmail.com>
2024-02-21 18:49:22 +0200

    Second solution - remove common mistakes, have problems with tests

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,38 +7,57 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    private static final String COMMA = ",";
+    private static final int OPERATION = 0;
+    private static final int COUNT = 1;
+
     public void getStatistic(String fromFileName, String toFileName) {
-        int totalSupply = 0;
-        int totalBuy = 0;
-        String[] parts = new String[] {};
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName));
-                BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
-            String line = "";
-            while ((line = bufferedReader.readLine()) != null) {
-                parts = line.split(",");
-                if (parts.length == 2) {
-                    String operation = parts[0];
-                    int amount = Integer.parseInt(parts[1]);
-                    if ("supply".equals(operation)) {
-                        totalSupply += amount;
-                    }
-                    if ("buy".equals(operation)) {
-                        totalBuy += amount;
-                    }
-                }
-            }
-            bufferedWriter.write("supply," + totalSupply);
-            bufferedWriter.newLine();
-            bufferedWriter.write("buy," + totalBuy);
-            bufferedWriter.newLine();
-            bufferedWriter.write("result," + (totalSupply - totalBuy));
-        } catch (IOException e) {
-            throw new RuntimeException("Can't write info");
-        }
+        String info = getInfo(fromFileName);
+        String report = writeReport(info);
+        writeReportInFile(toFileName, report);
     }
 
-    public static void main(String[] args) {
-        WorkWithFile fileWorker = new WorkWithFile();
-        fileWorker.getStatistic("input.csv", "output.csv");
+    private String getInfo(String fromFileName) {
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
+            String value = bufferedReader.readLine();
+            while (value != null) {
+                stringBuilder.append(value).append(System.lineSeparator());
+                value = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
+        return stringBuilder.toString();
+    }
+
+    private String writeReport(String info) {
+        StringBuilder stringBuilder = new StringBuilder();
+        String[] operations = info.split(System.lineSeparator());
+        int totalSupply = 0;
+        int totalBuy = 0;
+        for (String operation : operations) {
+            String[] separate = operation.split(COMMA);
+            if (separate[OPERATION].equals("supply")) {
+                totalSupply += Integer.parseInt(separate[COUNT]);
+            }
+            if (separate[OPERATION].equals("buy")) {
+                totalBuy += Integer.parseInt(separate[COUNT]);
+            }
+        }
+        stringBuilder.append("supply").append(COMMA).append(totalSupply)
+                .append(System.lineSeparator())
+                .append("buy").append(COMMA).append(totalBuy)
+                .append(System.lineSeparator())
+                .append("result").append(totalSupply - totalBuy);
+        return stringBuilder.toString();
+    }
+
+    private void writeReportInFile(String toFileName, String report) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+            bufferedWriter.write(report);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write info", e);
+        }
     }
 }
