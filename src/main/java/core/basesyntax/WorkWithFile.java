63d91be509ea04ae7ce5f8aca4421c448bdc0dda
Pavlo Betsa <bigworld2303@gmail.com>
2024-02-23 09:06:27 +0200

    Second solution - made constants;

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,16 +8,19 @@ import java.io.IOException;
 
 public class WorkWithFile {
     private static final String COMMA = ",";
-    private static final int OPERATION = 0;
+    private static final int OPERATION_INDEX = 0;
     private static final int COUNT = 1;
+    private static final String BUY = "buy";
+    private static final String SUPPLY = "supply";
+    private static final String RESULT = "result";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        String info = getInfo(fromFileName);
-        String report = writeReport(info);
+        String info = readFromFile(fromFileName);
+        String report = generateReport(info);
         writeReportInFile(toFileName, report);
     }
 
-    private String getInfo(String fromFileName) {
+    private String readFromFile(String fromFileName) {
         StringBuilder stringBuilder = new StringBuilder();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             String value = bufferedReader.readLine();
@@ -26,30 +29,30 @@ public class WorkWithFile {
                 value = bufferedReader.readLine();
             }
         } catch (IOException e) {
-            throw new RuntimeException("Can't read file", e);
+            throw new RuntimeException("Can't read file" + fromFileName, e);
         }
         return stringBuilder.toString();
     }
 
-    private String writeReport(String info) {
+    private String generateReport(String info) {
         StringBuilder stringBuilder = new StringBuilder();
         String[] operations = info.split(System.lineSeparator());
         int totalSupply = 0;
         int totalBuy = 0;
         for (String operation : operations) {
             String[] separate = operation.split(COMMA);
-            if (separate[OPERATION].equals("supply")) {
+            if (separate[OPERATION_INDEX].equals(SUPPLY)) {
                 totalSupply += Integer.parseInt(separate[COUNT]);
             }
-            if (separate[OPERATION].equals("buy")) {
+            if (separate[OPERATION_INDEX].equals(BUY)) {
                 totalBuy += Integer.parseInt(separate[COUNT]);
             }
         }
-        stringBuilder.append("supply").append(COMMA).append(totalSupply)
+        stringBuilder.append(SUPPLY).append(COMMA).append(totalSupply)
                 .append(System.lineSeparator())
-                .append("buy").append(COMMA).append(totalBuy)
+                .append(BUY).append(COMMA).append(totalBuy)
                 .append(System.lineSeparator())
-                .append("result").append(COMMA)
+                .append(RESULT).append(COMMA)
                 .append(totalSupply - totalBuy);
         return stringBuilder.toString();
     }
@@ -58,7 +61,7 @@ public class WorkWithFile {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
             bufferedWriter.write(report);
         } catch (IOException e) {
-            throw new RuntimeException("Can't write info", e);
+            throw new RuntimeException("Can't write info" + toFileName, e);
         }
     }
 }
