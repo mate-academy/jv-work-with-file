e7e99022e847dcc536d7cf3a6f18e172a82ca693
Andriy Gromysh <andriygromysh@ukr.net>
2024-02-23 21:16:06 +0200

    Corrected

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -6,32 +6,47 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class WorkWithFile {
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+    private static final String RESULT = "result";
+
     public void getStatistic(String fromFileName, String toFileName) {
+        List<String> lines = readLinesFromFile(fromFileName);
         int supply = 0;
         int buy = 0;
 
-        try {
-            List<String> lines = Files.readAllLines(Paths.get(fromFileName));
-            for (String line : lines) {
-                String[] parts = line.split(",");
-                if (parts[0].equals("supply")) {
-                    supply += Integer.parseInt(parts[1]);
-                } else if (parts[0].equals("buy")) {
-                    buy += Integer.parseInt(parts[1]);
-                }
+        for (String line : lines) {
+            String[] parts = line.split(",");
+            if (parts[0].equals(SUPPLY)) {
+                supply += Integer.parseInt(parts[1]);
+            } else if (parts[0].equals(BUY)) {
+                buy += Integer.parseInt(parts[1]);
             }
-            int result = supply - buy;
+        }
+        int result = supply - buy;
+        writeResultToFile(toFileName, supply, buy, result);
+    }
 
+    private List<String> readLinesFromFile(String fileName) {
+        try {
+            return Files.readAllLines(Paths.get(fileName));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void writeResultToFile(String fileName, int supply, int buy, int result) {
+        try {
             StringBuilder builder = new StringBuilder();
-            builder.append("supply,").append(supply).append(System.lineSeparator());
-            builder.append("buy,").append(buy).append(System.lineSeparator());
-            builder.append("result,").append(result).append(System.lineSeparator());
+            builder.append(SUPPLY).append(",").append(supply).append(System.lineSeparator());
+            builder.append(BUY).append(",").append(buy).append(System.lineSeparator());
+            builder.append(RESULT).append(",").append(result).append(System.lineSeparator());
             String data = builder.toString();
 
-            Files.write(Paths.get(toFileName), data.getBytes());
-
+            Files.write(Paths.get(fileName), data.getBytes());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 }
+
