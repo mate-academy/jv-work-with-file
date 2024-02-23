a82a1d04fb1ecf9b65d6fef2b41e3678ddc2c1de
Andriy Gromysh <andriygromysh@ukr.net>
2024-02-23 20:13:37 +0200

    Task is done

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,37 @@
 package core.basesyntax;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
+        int supply = 0;
+        int buy = 0;
 
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(fromFileName));
+            for (String line : lines) {
+                String[] parts = line.split(",");
+                if (parts[0].equals("supply")) {
+                    supply += Integer.parseInt(parts[1]);
+                } else if (parts[0].equals("buy")) {
+                    buy += Integer.parseInt(parts[1]);
+                }
+            }
+            int result = supply - buy;
+
+            StringBuilder builder = new StringBuilder();
+            builder.append("supply,").append(supply).append(System.lineSeparator());
+            builder.append("buy,").append(buy).append(System.lineSeparator());
+            builder.append("result,").append(result).append(System.lineSeparator());
+            String data = builder.toString();
+
+            Files.write(Paths.get(toFileName), data.getBytes());
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
