5a416154e2d32fd06db5adae87c92bfd2aa3eb62
Vladyslav Vyshynskyi <crusotdeath@gmail.com>
2024-02-25 15:23:13 +0200

    readFromFile - now only read file and return String createReport now takes the line from readFromFile and creates new String with calculations TotalSupply and totalBut are not constants and I cannot declaire them out of this class because of last test writeTOfile now writes String from createReport and clears 2 builder in case that getStatistic will be called again

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,50 +8,66 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    private static String fileLine;
+    private static String line;
+    private static String report;
+    private StringBuilder readerBuilder = new StringBuilder();
+    private StringBuilder reportBuilder = new StringBuilder();
+    private int totalAmount;
+    private int amountNumber;
+    private String operation;
+    private String[] separatedItems;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        String amount = createReport(fromFileName);
-        writeToFile(toFileName, amount);
+        line = readFromFile(fromFileName);
+        report = createReport(line);
+        writeToFile(toFileName, report);
     }
 
-    public String createReport(String fromFileName) {
-        String operation = "";
-        String fileLine = "";
-        int totalSupply = 0;
-        int totalBuy = 0;
-        int totalResult = 0;
+    public String readFromFile(String fromFileName) {
         File incomingFile = new File(fromFileName);
-        StringBuilder builder = new StringBuilder();
         try (BufferedReader reader = new BufferedReader(new FileReader(incomingFile))) {
             fileLine = reader.readLine();
             while (fileLine != null) {
-                String[] separatedItems = fileLine.split(",");
-                operation = separatedItems[0].trim();
-                if (operation.equals("supply")) {
-                    totalSupply += Integer.parseInt(separatedItems[1]);
-                } else if (operation.equals("buy")) {
-                    totalBuy += Integer.parseInt(separatedItems[1]);
-                }
+                readerBuilder.append(fileLine).append(",");
                 fileLine = reader.readLine();
             }
-            totalResult = totalSupply - totalBuy;
+            return readerBuilder.toString();
         } catch (IOException b) {
             throw new RuntimeException("Cannot read from file" + b.getMessage());
         }
-        return builder.append(totalSupply)
-                .append(",")
+    }
+
+    public String createReport(String line) {
+        int totalSupply = 0;
+        int totalBuy = 0;
+        separatedItems = line.split(",");
+        for (int i = 0; i < separatedItems.length; i += 2) {
+            operation = separatedItems[i].trim();
+            amountNumber = Integer.parseInt(separatedItems[i + 1].trim());
+            if (operation.equals("supply")) {
+                totalSupply += amountNumber;
+            } else if (operation.equals("buy")) {
+                totalBuy += amountNumber;
+            }
+        }
+        totalAmount = totalSupply - totalBuy;
+        return reportBuilder.append("supply,")
+                .append(totalSupply)
+                .append(System.lineSeparator())
+                .append("buy,")
                 .append(totalBuy)
-                .append(",")
-                .append(totalResult)
+                .append(System.lineSeparator())
+                .append("result,")
+                .append(totalAmount)
                 .toString();
     }
 
-    public void writeToFile(String toFileName, String amount) {
-        String[] count = amount.split(",");
+    public void writeToFile(String toFileName, String result) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
-            writer.write("supply," + count[0] + System.lineSeparator());
-            writer.write("buy," + count[1] + System.lineSeparator());
-            writer.write("result," + count[2]);
+            writer.write(result);
+            reportBuilder.setLength(0);
+            readerBuilder.setLength(0);
         } catch (IOException e) {
             throw new RuntimeException("Cannot write to file" + e.getMessage());
         }
