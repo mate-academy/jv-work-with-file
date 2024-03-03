84907b3926d498015e807ff948c246e8b693e851
Bohdan Buhrak <bohdan0410@gmail.com>
2024-03-03 18:43:16 +0200

    Added static fields and 3 methods (readFromFile, writeToFile, generateReport) to divide logics

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,58 +2,75 @@ package core.basesyntax;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+
+    private static final String SUPPLY_OPERATION = "supply";
+    private static final String BUY_OPERATION = "buy";
+    private static final String RESULT_OPERATION = "result";
+    private static final String COMMA_SEPARATOR = ",";
+    private static final String NEW_LINE_SEPARATOR = System.lineSeparator();
+    private static final int OPERATOR_TYPE_INDEX = 0;
+    private static final int AMOUNT_INDEX = 1;
+
     public void getStatistic(String fromFileName, String toFileName) {
 
-        File fromFile = new File(fromFileName);
-        File toFile = new File(toFileName);
+        String content = readFromFile(fromFileName);
+        String report = generateReport(content);
+        writeToFile(toFileName,report);
+    }
 
-        int supplyTotal = 0;
-        int buyTotal = 0;
-
-        StringBuilder readDate = new StringBuilder();
-        BufferedWriter writer = null;
-
-        try (BufferedReader reader = new BufferedReader(new FileReader(fromFile))) {
-            writer = new BufferedWriter(new FileWriter(toFile));
-            String dateLine = reader.readLine();
-            while (dateLine != null) {
-                readDate.append(dateLine).append(System.lineSeparator());
-                dateLine = reader.readLine();
+    private String readFromFile(String fromFileName) {
+        StringBuilder readContent = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
+            String line;
+            while (((line = reader.readLine()) != null)) {
+                readContent.append(line).append(NEW_LINE_SEPARATOR);
             }
 
-            String[] arrayReadData = readDate.toString().split(System.lineSeparator());
-            for (String date : arrayReadData) {
-                String[] parts = date.split(",");
-                String operationType = parts[0];
-                int amount = Integer.parseInt(parts[1]);
-                if (operationType.equals("supply")) {
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t read from file: " + fromFileName, e);
+        }
+
+        return readContent.toString();
+    }
+
+    private void writeToFile(String toFileName, String report) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
+            writer.write(report);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write data into file: " + toFileName,e);
+        }
+    }
+
+    private String generateReport(String data) {
+        StringBuilder report = new StringBuilder();
+        String[] lines = data.split(NEW_LINE_SEPARATOR);
+        int supplyTotal = 0;
+        int buyTotal = 0;
+        for (String entry : lines) {
+            String[] parts = entry.split(COMMA_SEPARATOR);
+            if (parts.length == 2) {
+                String operationType = parts[OPERATOR_TYPE_INDEX];
+                int amount = Integer.parseInt(parts[AMOUNT_INDEX]);
+                if (operationType.equals(SUPPLY_OPERATION)) {
                     supplyTotal += amount;
-                } else if (operationType.equals("buy")) {
+                } else if (operationType.equals(BUY_OPERATION)) {
                     buyTotal += amount;
                 } else {
                     throw new IllegalArgumentException("Invalid operation type: " + operationType);
                 }
             }
-
-            int result = supplyTotal - buyTotal;
-            writer.write("supply," + supplyTotal + System.lineSeparator());
-            writer.write("buy," + buyTotal + System.lineSeparator());
-            writer.write("result," + result + System.lineSeparator());
-
-        } catch (IOException e) {
-            throw new RuntimeException("can't read file",e);
-        } finally {
-            try {
-                writer.close();
-            } catch (IOException e) {
-                throw new RuntimeException("can't write date into file",e);
-            }
         }
+
+        report.append(SUPPLY_OPERATION).append(COMMA_SEPARATOR).append(supplyTotal)
+                .append(NEW_LINE_SEPARATOR)
+                .append(BUY_OPERATION).append(COMMA_SEPARATOR).append(buyTotal)
+                .append(NEW_LINE_SEPARATOR)
+                .append(RESULT_OPERATION).append(COMMA_SEPARATOR).append(supplyTotal - buyTotal);
+        return report.toString();
     }
 }
