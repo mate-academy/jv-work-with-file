dae635fb411a87f655efec8f0a31a0a0d2f1557a
Dmytro Kolchak <dima.kolhac@gmail.com>
2024-03-05 17:21:35 +0200

    Implemented a getStatistic method. But there is a quite messy and long code, because for instance I created String[] object in a loop (I could try to divide single line by "," and "\n" in parallel, but didn't know whether implement it) and used whole three try-catch constructors.

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,48 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
-
+        File fromFile = new File(fromFileName);
+        File writeFile = new File(toFileName);
+        int supply = 0;
+        int buy = 0;
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFile))) {
+            StringBuilder stringBuilder = new StringBuilder();
+            String line = bufferedReader.readLine();
+            while (line != null) {
+                stringBuilder.append(line).append("\n");
+                line = bufferedReader.readLine();
+            }
+            String stringFromFile = stringBuilder.toString();
+            String[] parts = stringFromFile.split("\n");
+            for (String entry : parts) {
+                String[] divideParts = entry.split(",");
+                if (divideParts[0].equals("supply")) {
+                    supply = supply + Integer.parseInt(divideParts[1]);
+                } else {
+                    buy = buy + Integer.parseInt(divideParts[1]);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
+        int resultInt = supply - buy;
+        StringBuilder report = new StringBuilder();
+        report.append("supply,").append(supply).append(System.lineSeparator())
+                .append("buy,").append(buy).append(System.lineSeparator())
+                .append("result,").append(resultInt);
+        String result = report.toString();
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(writeFile, false))) {
+            bufferedWriter.write(result);
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
     }
 }
