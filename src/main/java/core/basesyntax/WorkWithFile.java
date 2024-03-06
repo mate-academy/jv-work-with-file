70eea4473e75e546a59665b7e3505cd272ee3517
Dariia <daradyradara@gmail.com>
2024-03-06 14:59:17 +0200

    Refactored code - changed access from public into private and implemented better code

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,35 +8,48 @@ import java.io.IOException;
 
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
-        int supplyAmount = 0;
-        int buyAmount = 0;
+        String data = readFile(fromFileName);
+        String report = generateReport(data);
+        writeToFile(toFileName, report);
+    }
 
+    private String readFile(String fromFileName) {
+        StringBuilder rawData = new StringBuilder();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             String line;
             while ((line = bufferedReader.readLine()) != null) {
-                String[] parts = line.split(",");
-                if (line.startsWith("supply")) {
-                    supplyAmount += Integer.parseInt(parts[1]);
-                } else {
-                    buyAmount += Integer.parseInt(parts[1]);
-                }
+                rawData.append(line).append(System.lineSeparator());
             }
         } catch (IOException e) {
             throw new RuntimeException("Can't read data from the file " + fromFileName, e);
         }
-        writeToFile(toFileName, supplyAmount, buyAmount);
+        return rawData.toString();
     }
 
-    public String createReport(int supplyAmount, int buyAmount) {
+    private int parseAmount(String rawData, String operationType) {
+        int amount = 0;
+        String[] lines = rawData.split(System.lineSeparator());
+        for (String line : lines) {
+            String[] parts = line.split(",");
+            if (parts[0].equals(operationType)) {
+                amount += Integer.parseInt(parts[1]);
+            }
+        }
+        return amount;
+    }
+
+    private String generateReport(String rawData) {
+        int supplyAmount = parseAmount(rawData, "supply");
+        int buyAmount = parseAmount(rawData, "buy");
         int resultAmount = supplyAmount - buyAmount;
         return "supply," + supplyAmount + System.lineSeparator()
                 + "buy," + buyAmount + System.lineSeparator()
                 + "result," + resultAmount;
     }
 
-    public void writeToFile(String toFileName, int supplyAmount, int buyAmount) {
+    private void writeToFile(String toFileName, String report) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
-            bufferedWriter.write(createReport(supplyAmount, buyAmount));
+            bufferedWriter.write(report);
         } catch (IOException e) {
             throw new RuntimeException("Can't write data to file" + toFileName, e);
         }
