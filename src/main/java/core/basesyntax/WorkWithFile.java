f067cd23914a89d5059d786f1bbf3c1f643bf900
Lolita <ilolita796@gmail.com>
2024-03-11 12:12:59 +0200

    fixed issues

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -10,42 +10,58 @@ import java.io.IOException;
 public class WorkWithFile {
     private static final String DIVIDE_CHARACTER = ",";
     private static final String FIRST_COLUMN_WORD = "supply";
-    private static final int FIRST_COLUMN_INDEX = 0;
-    private static final int SECOND_COLUMN_INDEX = 1;
+    private static final String SECOND_COLUMN_WORD = "buy";
+    private static final String SEPARATOR = System.lineSeparator();
 
     public void getStatistic(String fromFileName, String toFileName) {
+        String dataFromFile = readFromFile(fromFileName);
+        String report = processData(dataFromFile);
+        writeToFile(report, toFileName);
+    }
+
+    private String readFromFile(String fromFileName) {
+        StringBuilder dataFromFile = new StringBuilder();
         File file = new File(fromFileName);
-        int supply = 0;
-        int buy = 0;
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             String line = reader.readLine();
             while (line != null) {
-                String[] lineDividedByWhitespace = line.split(DIVIDE_CHARACTER);
-                if (lineDividedByWhitespace[FIRST_COLUMN_INDEX].equals(FIRST_COLUMN_WORD)) {
-                    supply += Integer.parseInt(lineDividedByWhitespace[SECOND_COLUMN_INDEX]);
-                } else {
-                    buy += Integer.parseInt(lineDividedByWhitespace[SECOND_COLUMN_INDEX]);
-                }
+                dataFromFile.append(line).append(DIVIDE_CHARACTER);
                 line = reader.readLine();
             }
         } catch (IOException e) {
             throw new RuntimeException("Can't read file", e);
         }
-        writeToFile(toFileName, supply, buy);
+        return dataFromFile.toString();
     }
 
-    private void writeToFile(String toFileName, int supply, int buy) {
+    private String processData(String dataFromFile) {
+        int supply = 0;
+        int buy = 0;
+        String[] dataDividedByComma = dataFromFile.split(DIVIDE_CHARACTER);
+        for (int i = 0; i < dataDividedByComma.length; i++) {
+            switch (dataDividedByComma[i]) {
+                case FIRST_COLUMN_WORD:
+                    i++;
+                    supply += Integer.parseInt(dataDividedByComma[i]);
+                    break;
+                case SECOND_COLUMN_WORD:
+                    i++;
+                    buy += Integer.parseInt(dataDividedByComma[i]);
+                    break;
+                default:
+                    break;
+            }
+        }
+        int result = supply - buy;
+        return "supply," + supply + SEPARATOR + "buy," + buy + SEPARATOR + "result," + result;
+    }
+
+    private void writeToFile(String report, String toFileName) {
         File file = new File(toFileName);
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
-            bufferedWriter.write(createReport(supply, buy));
+            bufferedWriter.write(report);
         } catch (IOException e) {
             throw new RuntimeException("Can't write to file", e);
         }
-    }
-
-    private String createReport(int supply, int buy) {
-        int result = supply - buy;
-        String separator = System.lineSeparator();
-        return "supply," + supply + separator + "buy," + buy + separator + "result," + result;
     }
 }
