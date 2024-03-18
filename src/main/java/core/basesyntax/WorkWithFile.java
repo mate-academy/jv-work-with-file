2fd5e017ec941f86a45c727aa1165dd061b738b2
Banderan <bartoszwojcik2004@gmail.com>
2024-03-18 10:34:44 +0100

    fix missed change and editing method name

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -9,34 +9,29 @@ import java.nio.file.Files;
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
         String[] inputArray = readFromFile(fromFileName).split(",");
-        String calculateResult = editFile(inputArray);
+        String calculateResult = calculateTransactionResults(inputArray);
         writeToNewFile(toFileName, calculateResult);
     }
 
     private String readFromFile(String fromFileName) {
         File fileFrom = new File(fromFileName);
         StringBuilder builder = new StringBuilder();
-
         try {
             BufferedReader reader = new BufferedReader(new FileReader(fileFrom));
-            String value = reader.readLine();
-            while (value != null) {
+            String value;
+            while ((value = reader.readLine()) != null) {
                 builder.append(value).append(System.lineSeparator());
-                value = reader.readLine();
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
         return builder.toString().replace(System.lineSeparator(), ",");
-
     }
 
-    private String editFile(String[] inputArray) {
+    private String calculateTransactionResults(String[] inputArray) {
         int buy = 0;
         int supply = 0;
         int result;
-
         for (int i = 0; i < inputArray.length; i++) {
             if (inputArray[i].equals("buy")) {
                 buy += Integer.parseInt(inputArray[i + 1]);
