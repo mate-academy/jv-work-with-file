3d85479d6adedc0f3912e04d3550e06c08a60a56
Olexander Savchuk <savchukolexanderwork@gmail.com>
2024-03-21 16:37:00 +0200

    Created methods to read file by name, to write to file by name Added main implementation to read data, summarise report, and write to another file

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,33 @@
 package core.basesyntax;
 
+import java.io.*;
+
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
+        String readedFileData = readFile(fromFileName);
+        String report = new ReportCSVSupplier(readedFileData).createSummeryReport();
+        writeToFile(toFileName, report);
+    }
 
+    private String readFile(String fileName) {
+        StringBuilder fileData = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            String lastLine = bufferedReader.readLine();
+            while (lastLine != null) {
+                fileData.append(lastLine).append(System.lineSeparator());
+                lastLine = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Cannot read a file", e);
+        }
+        return fileData.toString();
+    }
+
+    private void writeToFile(String fileName, String data) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName))) {
+            bufferedWriter.write(data);
+        } catch (IOException e) {
+            throw new RuntimeException("Cannot write to the file", e);
+        }
     }
 }
