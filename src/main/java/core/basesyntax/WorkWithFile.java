0f2f652cfdab573a76efc82d59685891650d6f76
oleksandr-saulko <aleksandrsaulko@gmail.com>
2024-04-02 22:21:16 +0300

    add hw jw-work-with-file solution

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,41 @@
 package core.basesyntax;
 
+import java.io.*;
+
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
+        int supply = 0;
+        int buy = 0;
+        int result = 0;
 
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
+            String value = reader.readLine();
+
+            while (value != null) {
+                String[] valueArr = value.split(",");
+
+                if (valueArr[0].equals("supply")) {
+                    supply += Integer.parseInt(valueArr[1]);
+                } else {
+                    buy += Integer.parseInt(valueArr[1]);
+                }
+
+                value = reader.readLine();
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read data from file", e);
+        }
+
+        result = supply - buy;
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName, true));) {
+            writer.write("supply" + "," + supply + "\n");
+            writer.write("buy" + "," + buy + "\n");
+            writer.write("result" + "," + result + "\n");
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write data to file", e);
+        }
     }
 }
