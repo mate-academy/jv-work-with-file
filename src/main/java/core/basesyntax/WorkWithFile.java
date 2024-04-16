06a4f867459f14ba34ce8607a687872c3881015e
BelitskyiAlexandr <vilisapett@gmail.com>
2024-04-16 18:02:51 +0300

    read, calculate, write with csv

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,73 @@
 package core.basesyntax;
 
-public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class WorkWithFile {
+    private static final int OPERATION_TYPE_INDEX = 0;
+    private static final int AMOUNT_INDEX = 1;
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        // required variables - are not class variables, because
+        // they do not reflect the state of the class
+        int supply = 0;
+        int buy = 0;
+
+        String[] lines = readFile(fromFileName).split(System.lineSeparator());
+
+        // calculating
+        for (String stringLine : lines) {
+            String[] parsingLine = stringLine.split(",");
+            int amount = Integer.parseInt(parsingLine[AMOUNT_INDEX]);
+            switch (parsingLine[OPERATION_TYPE_INDEX]) {
+                case "supply" :
+                    supply += amount;
+                    break;
+                case "buy" :
+                    buy += amount;
+                    break;
+                default:
+                    throw new RuntimeException("This type of operation is not supported");
+            }
+        }
+
+        this.writeFile(toFileName, this.createReport(supply, buy));
+    }
+
+    private String readFile(String fromFileName) {
+        StringBuilder sbInput = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                sbInput.append(line).append(System.lineSeparator());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        if (sbInput.length() == 0) {
+            throw new RuntimeException("File is empty");
+        }
+        return sbInput.toString();
+    }
+
+    private void writeFile(String toFileName, String data) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+            bufferedWriter.write(data);
+        } catch (IOException e) {
+            throw new RuntimeException("The specified file does not exist");
+        }
+    }
+
+    private String createReport(int supply, int buy) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("supply").append(",").append(supply).append(System.lineSeparator())
+                .append("buy").append(",").append(buy).append(System.lineSeparator())
+                .append("result").append(",").append(supply - buy)
+                .append(System.lineSeparator());
+        return sb.toString();
     }
 }
