69b384e44c8a2f4839c53af3510fd21b77d12d8d
BelitskyiAlexandr <vilisapett@gmail.com>
2024-04-17 19:30:54 +0300

    hot fix

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -15,14 +15,40 @@ public class WorkWithFile {
     private static final String SEPARATOR = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        // required variables - are not class variables, because
-        // they do not reflect the state of the class
+        String[] lines = readFile(fromFileName).split(System.lineSeparator());
+
+        this.writeFile(toFileName, this.createReport(lines));
+    }
+
+    private String readFile(String fromFileName) {
+        StringBuilder stringBuilderInput = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuilderInput.append(line).append(System.lineSeparator());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        if (stringBuilderInput.length() == 0) {
+            throw new RuntimeException("File is empty");
+        }
+        return stringBuilderInput.toString();
+    }
+
+    private void writeFile(String toFileName, String data) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+            bufferedWriter.write(data);
+        } catch (IOException e) {
+            throw new RuntimeException("The specified file does not exist");
+        }
+    }
+
+    private String createReport(String[] lines) {
         int supply = 0;
         int buy = 0;
 
-        String[] lines = readFile(fromFileName).split(System.lineSeparator());
-
-        // calculating
         for (String stringLine : lines) {
             String[] parsingLine = stringLine.split(SEPARATOR);
             int amount = Integer.parseInt(parsingLine[AMOUNT_INDEX]);
@@ -38,40 +64,11 @@ public class WorkWithFile {
             }
         }
 
-        this.writeFile(toFileName, this.createReport(supply, buy));
-    }
-
-    private String readFile(String fromFileName) {
-        StringBuilder sbInput = new StringBuilder();
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                sbInput.append(line).append(System.lineSeparator());
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        if (sbInput.length() == 0) {
-            throw new RuntimeException("File is empty");
-        }
-        return sbInput.toString();
-    }
-
-    private void writeFile(String toFileName, String data) {
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
-            bufferedWriter.write(data);
-        } catch (IOException e) {
-            throw new RuntimeException("The specified file does not exist");
-        }
-    }
-
-    private String createReport(int supply, int buy) {
-        StringBuilder sb = new StringBuilder();
-        sb.append(SUPPLY).append(SEPARATOR).append(supply).append(System.lineSeparator())
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(SUPPLY).append(SEPARATOR).append(supply).append(System.lineSeparator())
                 .append(BUY).append(SEPARATOR).append(buy).append(System.lineSeparator())
                 .append(RESULT).append(SEPARATOR).append(supply - buy)
                 .append(System.lineSeparator());
-        return sb.toString();
+        return stringBuilder.toString();
     }
 }
