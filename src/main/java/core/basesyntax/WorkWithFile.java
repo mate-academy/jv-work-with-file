b8573d5197fc3799c7b76a3aa21a1eee259ca08f
flamehost <flamehost@syneforge.com>
2024-04-20 22:48:12 +0300

    display edits

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -4,8 +4,9 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.util.HashMap;
 import java.io.IOException;
+import java.util.HashMap;
+
 
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
