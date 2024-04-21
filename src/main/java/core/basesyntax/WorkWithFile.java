a9dabacc32a054ef82df5a12b46981a7fba944c3
Svyatoslav Nadorozhnyi <svyatoslavnadorozhniy@gmail.com>
2024-04-21 15:56:17 -0400

    corrected variables naming and changed methods mistakes

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,28 +7,30 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    private static final String STR_SUPPLY = "supply,";
-    private static final String STR_BUY = "buy,";
-    private static final String STR_RESULT = "result,";
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+    private static final String RESULT = "result";
     private static final String COMMA_SYMBOL = ",";
+    private int supply;
+    private int buy;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        StringBuilder report = new StringBuilder();
-
-        readCalculateFile(fromFileName, report);
+        readCalculateFile(fromFileName);
+        String report = createReport();
         writeFile(toFileName, report);
     }
 
-    public void readCalculateFile(String fromFileName, StringBuilder report) {
-        int supply = 0;
-        int buy = 0;
+    private void readCalculateFile(String fromFileName) {
+        supply = 0;
+        buy = 0;
+
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             String currentString = bufferedReader.readLine();
             while (currentString != null) {
                 String[] splitedLine = currentString.split(COMMA_SYMBOL);
                 int operationAmount = Integer.parseInt(splitedLine[1]);
                 String operation = splitedLine[0];
-                if (operation.equals("supply")) {
+                if (operation.equals(SUPPLY)) {
                     supply += operationAmount;
                 } else {
                     buy += operationAmount;
@@ -38,24 +40,30 @@ public class WorkWithFile {
         } catch (IOException e) {
             throw new RuntimeException("Can't read data from file " + fromFileName, e);
         }
-        createReport(supply, buy, report);
     }
 
-    public void createReport(int supply, int buy, StringBuilder report) {
+    private String createReport() {
+        StringBuilder report = new StringBuilder();
+
         int result = supply - buy;
-        report.append(STR_SUPPLY)
+        report.append(SUPPLY)
+                .append(COMMA_SYMBOL)
                 .append(supply)
                 .append(System.lineSeparator())
-                .append(STR_BUY)
+                .append(BUY)
+                .append(COMMA_SYMBOL)
                 .append(buy)
                 .append(System.lineSeparator())
-                .append(STR_RESULT)
+                .append(RESULT)
+                .append(COMMA_SYMBOL)
                 .append(result);
+
+        return report.toString();
     }
 
-    public void writeFile(String toFileName, StringBuilder report) {
+    private void writeFile(String toFileName, String report) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
-            bufferedWriter.write(report.toString());
+            bufferedWriter.write(report);
         } catch (IOException e) {
             throw new RuntimeException("Can't write data to file" + toFileName, e);
         }
