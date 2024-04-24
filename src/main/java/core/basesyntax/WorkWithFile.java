b66ed722ec4b5624dc6f01a73710bcb956fc9d85
AnnaAAA3432 <144123194+AnnaAAA3432@users.noreply.github.com>
2024-04-24 19:14:28 +0200

    task solution

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -5,31 +5,32 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashMap;
 
 public class WorkWithFile {
     private static final String SUPPLY = "supply";
     private static final String BUY = "buy";
     private static final String RESULT = "result";
     private static final String COMMA_SYMBOL = ",";
-    private int supply;
-    private int buy;
+    private static final int CSV_VALUE_INDEX = 1;
+    private static final int CSV_KEY_INDEX = 0;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        getData(fromFileName);
-        String report = createReport();
+        HashMap<String, Integer> data = getData(fromFileName);
+        String report = createReport(data);
         writeResultToFile(toFileName, report);
     }
 
-    private void getData(String fileName) {
-        supply = 0;
-        buy = 0;
+    private HashMap<String, Integer> getData(String fileName) {
+        int supply = 0;
+        int buy = 0;
 
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
             String currentString = bufferedReader.readLine();
             while (currentString != null) {
                 String[] splitedLine = currentString.split(COMMA_SYMBOL);
-                int operationAmount = Integer.parseInt(splitedLine[1]);
-                String operation = splitedLine[0];
+                int operationAmount = Integer.parseInt(splitedLine[CSV_VALUE_INDEX]);
+                String operation = splitedLine[CSV_KEY_INDEX];
                 if (operation.equals(SUPPLY)) {
                     supply += operationAmount;
                 } else {
@@ -37,14 +38,24 @@ public class WorkWithFile {
                 }
                 currentString = bufferedReader.readLine();
             }
+            int finalSupply = supply;
+            int finalBuy = buy;
+            return new HashMap<>() {
+                {
+                    this.put("supply", finalSupply);
+                    this.put("buy", finalBuy);
+                }
+            };
         } catch (IOException e) {
             throw new RuntimeException("Can't read data from file " + fileName, e);
         }
     }
 
-    private String createReport() {
+    private String createReport(HashMap<String, Integer> data) {
         StringBuilder report = new StringBuilder();
 
+        int supply = data.get("supply");
+        int buy = data.get("buy");
         int result = supply - buy;
         report.append(SUPPLY)
                 .append(COMMA_SYMBOL)
