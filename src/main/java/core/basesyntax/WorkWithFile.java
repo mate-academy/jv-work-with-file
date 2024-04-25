a3b36e2942523a73dba387919a2cc568c048c8ae
Letu <lean7ua@gmail.com>
2024-04-25 18:10:29 +0300

    Implemented getStatistic method for end-of-day market data analysis then saved to the specified output file.
    -added readFile method to read file from file name
    -added createReport method to calculates relevant statistics, such as total supply, total purchase quantity, and overall result

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,65 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
+        String[] data = readFile(fromFileName);
+        String result = createReport(data);
 
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
+            writer.write(result);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write data to the file " + toFileName, e);
+        }
     }
+
+    private String[] readFile(String fromFileName) {
+        List<String> lines = new ArrayList<>();
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                lines.add(line);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read data from the file " + fromFileName, e);
+        }
+
+        return lines.toArray(new String[0]);
+    }
+
+    private String createReport(String[] data) {
+        StringBuilder builder = new StringBuilder();
+        int supplySum = 0;
+        int buySum = 0;
+
+        for (String item : data) {
+            String[] parts = item.split(",");
+            int number = Integer.parseInt(parts[1]);
+
+            if (item.startsWith("supply")) {
+                supplySum += number;
+            }
+
+            if (item.startsWith("buy")) {
+                buySum += number;
+            }
+        }
+        int result = supplySum - buySum;
+
+        return builder.append("supply,").append(supplySum)
+                .append(System.lineSeparator())
+                .append("buy,").append(buySum)
+                .append(System.lineSeparator())
+                .append("result,").append(result)
+                .append(System.lineSeparator()).toString();
+    }
+
 }
