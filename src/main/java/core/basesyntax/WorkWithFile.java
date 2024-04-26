ca223a517db7a947635d61eec6ec51f178fad165
Vadik Rakitin <vadym.rakitin@gmail.com>
2024-04-26 16:29:40 +0300

    divided program logic

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,7 +2,6 @@ package core.basesyntax;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -11,38 +10,57 @@ public class WorkWithFile {
     private static final String BUY_OPTION = "buy";
     private static final String SUPPLY_OPTION = "supply";
     private static final String RESULT_OPTION = "result";
+    private static final int AMOUNT_OPTIONS = 2;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        File fromFile = new File(fromFileName);
-        File toFile = new File(toFileName);
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFile));
-                BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFile))) {
-            int sumBuy = 0;
-            int sumSupply = 0;
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                String[] parts = line.split(",");
-                if (parts.length == 2) {
-                    String type = parts[0].trim().toLowerCase();
-                    int amount = Integer.parseInt(parts[1]);
-                    if (BUY_OPTION.equals(type)) {
-                        sumBuy += amount;
-                    } else if (SUPPLY_OPTION.equals(type)) {
-                        sumSupply += amount;
-                    }
-                }
-            }
+        try {
+            String[] data = readFromFile(fromFileName);
+            int sumSupply = calculateSum(data, SUPPLY_OPTION);
+            int sumBuy = calculateSum(data, BUY_OPTION);
             int result = sumSupply - sumBuy;
+            createReport(toFileName, sumSupply, sumBuy, result);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read from file" + fromFileName, e);
+        }
+    }
+
+    private String[] readFromFile(String fileName) throws IOException {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            return bufferedReader.lines().toArray(String[]::new);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read from file" + fileName, e);
+        }
+    }
+
+    private int calculateSum(String[] data, String option) {
+        int sum = 0;
+        for (String line : data) {
+            String[] parts = line.split(",");
+            if (parts.length == AMOUNT_OPTIONS && parts[0].equalsIgnoreCase(option)) {
+                sum += Integer.parseInt(parts[1]);
+            }
+        }
+        return sum;
+    }
+
+    private void createReport(String fileName, int sumSupply, int sumBuy, int result) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName))) {
             writeToFile(bufferedWriter, SUPPLY_OPTION, sumSupply);
             writeToFile(bufferedWriter, BUY_OPTION, sumBuy);
             writeToFile(bufferedWriter, RESULT_OPTION, result);
         } catch (IOException e) {
-            throw new RuntimeException("Can't read or write a file", e);
+            throw new RuntimeException("Can't write to file" + fileName, e);
         }
     }
 
-    private void writeToFile(BufferedWriter writer, String option, int value) throws IOException {
-        writer.write(option + "," + value);
-        writer.newLine();
+    private void writeToFile(BufferedWriter bufferedWriter, String option, int value) {
+        try {
+            bufferedWriter.write(option + "," + value);
+            if (!option.equals(RESULT_OPTION)) {
+                bufferedWriter.newLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write to file", e);
+        }
     }
 }
