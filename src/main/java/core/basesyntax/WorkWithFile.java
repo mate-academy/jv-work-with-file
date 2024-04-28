4b7ed05b90da535b61766f22042b5afd9b811943
Roman Chygryn <chygryn.ra@gmail.com>
2024-04-28 18:01:46 +0200

    changes after review

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -10,32 +10,32 @@ public class WorkWithFile {
     public static final int AMOUNT_INDEX = 1;
     public static final int OPERATION_TYPE_INDEX = 0;
     public static final int SUPPLY_DATA_INDEX = 0;
-    public static final int VALUE_FROM_DATA_INDEX = 1;
     public static final int BUY_DATA_INDEX = 1;
-    public static final int SUPPLY_DATA_IN_REPORT_INDEX = 0;
-    public static final int BUY_DATA_IN_REPORT_INDEX = 1;
-    public static final int RESULT_DATA_IN_REPORT_INDEX = 2;
+    public static final String COMMA_CHAR = ",";
+    public static final String SUPPLY = "supply";
+    public static final String BUY = "buy";
+    public static final String RESULT = "result";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        String dataFromFile = readFile(fromFileName);
+        int[] dataFromFile = readFile(fromFileName);
         String report = createReport(dataFromFile);
         writeReport(toFileName, report);
     }
 
-    private String readFile(String fromFileName) {
+    private int[] readFile(String fromFileName) {
         int totalSupply = 0;
         int totalBuy = 0;
 
         try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
             String line = reader.readLine();
             while (line != null) {
-                String[] data = line.split(",");
+                String[] data = line.split(COMMA_CHAR);
                 String operationType = data[OPERATION_TYPE_INDEX].trim();
                 int amount = Integer.parseInt(data[AMOUNT_INDEX].trim());
 
-                if ("supply".equals(operationType)) {
+                if (SUPPLY.equals(operationType)) {
                     totalSupply += amount;
-                } else if ("buy".equals(operationType)) {
+                } else if (BUY.equals(operationType)) {
                     totalBuy += amount;
                 }
 
@@ -44,40 +44,30 @@ public class WorkWithFile {
         } catch (IOException e) {
             throw new RuntimeException("Cannot read file", e);
         }
-
-        return "Total Supply:" + totalSupply + ";Total Buy:" + totalBuy;
+        return new int[]{totalSupply, totalBuy};
     }
 
-    private String createReport(String data) {
-        int supply = 0;
-        int buy = 0;
-        int result = 0;
-        if (data.split(";")[SUPPLY_DATA_INDEX] != null
-                && data.split(";")[SUPPLY_DATA_INDEX]
-                .split(":")[VALUE_FROM_DATA_INDEX] != null) {
-            supply = Integer.parseInt(data.split(";")[SUPPLY_DATA_INDEX]
-                    .split(":")[VALUE_FROM_DATA_INDEX]);
-        }
-        if (data.split(";")[BUY_DATA_INDEX] != null
-                && data.split(";")[BUY_DATA_INDEX]
-                .split(":")[VALUE_FROM_DATA_INDEX] != null) {
-            buy = Integer.parseInt(data.split(";")[BUY_DATA_INDEX]
-                    .split(":")[VALUE_FROM_DATA_INDEX]);
-        }
-        result = supply - buy;
-
-        return "supply," + supply + ";buy," + buy + ";result," + result;
+    private String createReport(int[] data) {
+        int totalSupply = data[SUPPLY_DATA_INDEX];
+        int totalBuy = data[BUY_DATA_INDEX];
+        int result = totalSupply - totalBuy;
+        StringBuilder csvData = new StringBuilder(SUPPLY)
+                .append(COMMA_CHAR)
+                .append(totalSupply)
+                .append(System.lineSeparator())
+                .append(BUY)
+                .append(COMMA_CHAR)
+                .append(totalBuy)
+                .append(System.lineSeparator())
+                .append(RESULT)
+                .append(COMMA_CHAR)
+                .append(result);
+        return csvData.toString();
     }
 
     private void writeReport(String toFileName, String report) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
-            String[] reportParts = report.split(";");
-
-            writer.write(reportParts[SUPPLY_DATA_IN_REPORT_INDEX]);
-            writer.newLine();
-            writer.write(reportParts[BUY_DATA_IN_REPORT_INDEX]);
-            writer.newLine();
-            writer.write(reportParts[RESULT_DATA_IN_REPORT_INDEX]);
+            writer.write(report);
         } catch (IOException e) {
             throw new RuntimeException("Cannot write to file", e);
         }
