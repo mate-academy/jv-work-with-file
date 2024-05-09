7f23708dfbad323cdd83359611394a17cee7ff7c
Artur <a_blyskal@wp.pl>
2024-05-09 23:38:29 +0200

    created readFromFile, calculateTotals, calculateResult, writeToFile methods - created new constants - used Path.of

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,44 +1,68 @@
 package core.basesyntax;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 
 public class WorkWithFile {
     private static final String OPERATION_SUPPLY = "supply";
     private static final String OPERATION_BUY = "buy";
     private static final String RESULT = "result";
+    private static final int OPERATION_INDEX = 0;
+    private static final int AMOUNT_INDEX = 1;
+    private static final String DELIMITER = ",";
+    private static final int SUPPLY_INDEX = 0;
+    private static final int BUY_INDEX = 1;
 
-    public void getStatistic(String fromFileName, String toFileName) throws RuntimeException {
-        int supplyTotal = 0;
-        int buyTotal = 0;
+    public void getStatistic(String fromFileName, String toFileName) {
+        List<String> lines = readFromFile(fromFileName);
+        int[] totals = calculateTotals(lines);
+        int result = calculateResult(totals[SUPPLY_INDEX], totals[BUY_INDEX]);
+        writeToFile(toFileName, totals[SUPPLY_INDEX], totals[BUY_INDEX], result);
+    }
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
-                BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
+    private List<String> readFromFile(String fileName) {
+        final Path path = Path.of(fileName);
+        try {
+            return Files.readAllLines(path);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read from file", e);
+        }
+    }
 
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String[] splitLine = line.split(",");
-                String operation = splitLine[0];
-                int amount = Integer.parseInt(splitLine[1]);
+    private int[] calculateTotals(List<String> lines) {
+        int operationSupply = 0;
+        int operationBuy = 0;
 
-                if (operation.equals(OPERATION_SUPPLY)) {
-                    supplyTotal += amount;
-                } else if (operation.equals(OPERATION_BUY)) {
-                    buyTotal += amount;
-                }
+        for (String line : lines) {
+            String[] splitLine = line.split(DELIMITER);
+            String operation = splitLine[OPERATION_INDEX];
+            int amount = Integer.parseInt(splitLine[AMOUNT_INDEX]);
+
+            if (operation.equals(OPERATION_SUPPLY)) {
+                operationSupply += amount;
+            } else if (operation.equals(OPERATION_BUY)) {
+                operationBuy += amount;
             }
+        }
 
-            int result = supplyTotal - buyTotal;
+        return new int[]{operationSupply, operationBuy};
+    }
 
+    private int calculateResult(int supplyTotal, int buyTotal) {
+        return supplyTotal - buyTotal;
+    }
+
+    private void writeToFile(String toFileName, int supplyTotal, int buyTotal, int result) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
             writer.write(String.format("%s,%d%n", OPERATION_SUPPLY, supplyTotal));
             writer.write(String.format("%s,%d%n", OPERATION_BUY, buyTotal));
             writer.write(String.format("%s,%d", RESULT, result));
-
         } catch (IOException e) {
-            throw new RuntimeException("Can't write/read to/from file",e);
+            throw new RuntimeException("Can't write to file", e);
         }
     }
 }
