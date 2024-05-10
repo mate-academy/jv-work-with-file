7e98d5569d3dc6211ba0a04113b4800d958846f1
Ievgenii Kazaov <Jacalius1@gmail.com>
2024-05-10 12:00:40 -0600

    Created separate methods, Changed bufferedWriter.write on StringBuilder.append(), improved exception message.

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,41 +7,60 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    private static final String OPERATION_SUPPLY = "supply";
-    private static final String OPERATION_BUY = "buy";
-    private static final String RESULT_SUPPLY = "supply";
-    private static final String RESULT_BUY = "buy";
-    private static final String RESULT_RESULT = "result";
+    private static final String COMMA = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
+        String report = readFile(fromFileName);
+        String result = getResult(report);
+        writeToFile(result, toFileName);
+    }
+
+    private String readFile(String fromFileName) {
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuilder.append(line).append("\n");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file:" + fromFileName, e);
+        }
+        return stringBuilder.toString();
+    }
+
+    private String getResult(String report) {
         int countSupply = 0;
         int countBuy = 0;
 
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName));
-                BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                String[] parts = line.split(",");
-                if (parts.length == 2) {
-                    String operation = parts[0].trim();
-                    int amount = Integer.parseInt(parts[1].trim());
-                    if (operation.equals(OPERATION_SUPPLY)) {
-                        countSupply += amount;
-                    }
-                    if (operation.equals(OPERATION_BUY)) {
-                        countBuy += amount;
-                    }
+        String[] lines = report.split("\n");
+        for (String line : lines) {
+            String[] parts = line.split(COMMA);
+            if (parts.length == 2) {
+                String operation = parts[0].trim();
+                int amount = Integer.parseInt(parts[1].trim());
+                if (operation.equals("supply")) {
+                    countSupply += amount;
+                }
+                if (operation.equals("buy")) {
+                    countBuy += amount;
                 }
             }
-            int result = countSupply - countBuy;
+        }
 
-            bufferedWriter.write(RESULT_SUPPLY + "," + countSupply);
-            bufferedWriter.newLine();
-            bufferedWriter.write(RESULT_BUY + "," + countBuy);
-            bufferedWriter.newLine();
-            bufferedWriter.write(RESULT_RESULT + "," + result);
+        int result = countSupply - countBuy;
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(" supply").append(COMMA).append(countSupply)
+                .append(System.lineSeparator())
+                .append("buy").append(COMMA).append(countBuy).append(System.lineSeparator())
+                .append("result").append(COMMA).append(result).append(System.lineSeparator());
+        return stringBuilder.toString();
+    }
+
+    private void writeToFile(String result, String toFileName) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+            bufferedWriter.write(result);
         } catch (IOException e) {
-            throw new RuntimeException("Can't read file:", e);
+            throw new RuntimeException("Can't write to file:" + toFileName, e);
         }
     }
 }
