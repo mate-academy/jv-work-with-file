031ea807244a03cd1a52e7cae5757e71ee85c1b4
Anton <panyutin15@gmail.com>
2024-05-17 17:07:04 +0300

    moved stringBuilder to methods level, created constants of all magic numbers, added method createReport()

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -12,35 +12,20 @@ public class WorkWithFile {
     private static final String SUPPLY_OPERATION = "supply";
     private static final String BUY_OPERATION = "buy";
     private static final String RESULT_OPERATION = "result";
-    private StringBuilder stringBuilder = new StringBuilder();
+    private static final int AMOUNT_DATA_IN_LINE = 2;
+    private static final int OPERATION_COLUMN_INDEX = 0;
+    private static final int AMOUNT_COLUMN_INDEX = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        int supplyAmount = 0;
-        int buyAmount = 0;
-
         String[][] inputData = readDataFromFile(fromFileName);
-
-        for (int i = 0; i < inputData.length; i++) {
-            if (inputData[i][0].equals(SUPPLY_OPERATION)) {
-                supplyAmount += Integer.parseInt(inputData[i][1]);
-            } else if (inputData[i][0].equals(BUY_OPERATION)) {
-                buyAmount += Integer.parseInt(inputData[i][1]);
-            }
-        }
-
-        stringBuilder.setLength(0);
-        stringBuilder
-                .append(SUPPLY_OPERATION).append(SIGN_COMMA).append(supplyAmount).append(NEW_LINE)
-                .append(BUY_OPERATION).append(SIGN_COMMA).append(buyAmount).append(NEW_LINE)
-                .append(RESULT_OPERATION).append(SIGN_COMMA).append(supplyAmount - buyAmount);
-
-        writeDataToFile(toFileName, stringBuilder.toString());
+        String report = createReport(inputData);
+        writeDataToFile(toFileName, report);
     }
 
     private String[][] readDataFromFile(String fileName) {
         String line = null;
+        StringBuilder stringBuilder = new StringBuilder();
 
-        stringBuilder.setLength(0);
         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             while ((line = reader.readLine()) != null) {
                 stringBuilder.append(line).append(System.lineSeparator());
@@ -48,12 +33,13 @@ public class WorkWithFile {
         } catch (IOException e) {
             throw new RuntimeException("Can't open file", e);
         }
+
         return getTwoDemensionalArray(stringBuilder.toString());
     }
 
     private void writeDataToFile(String fileName, String data) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
-            writer.append(stringBuilder.toString());
+            writer.append(data);
         } catch (IOException e) {
             throw new RuntimeException("Can't write file", e);
         }
@@ -61,11 +47,33 @@ public class WorkWithFile {
 
     private String[][] getTwoDemensionalArray(String string) {
         String[] strings = string.split(NEW_LINE);
-        String[][] data = new String[strings.length][2];
+        String[][] data = new String[strings.length][AMOUNT_DATA_IN_LINE];
 
         for (int i = 0; i < strings.length; i++) {
             data[i] = strings[i].split(SIGN_COMMA);
         }
+
         return data;
+    }
+
+    private String createReport(String[][] inputData) {
+        int supplyAmount = 0;
+        int buyAmount = 0;
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < inputData.length; i++) {
+            if (inputData[i][OPERATION_COLUMN_INDEX].equals(SUPPLY_OPERATION)) {
+                supplyAmount += Integer.parseInt(inputData[i][AMOUNT_COLUMN_INDEX]);
+            } else if (inputData[i][OPERATION_COLUMN_INDEX].equals(BUY_OPERATION)) {
+                buyAmount += Integer.parseInt(inputData[i][AMOUNT_COLUMN_INDEX]);
+            }
+        }
+
+        stringBuilder
+                .append(SUPPLY_OPERATION).append(SIGN_COMMA).append(supplyAmount).append(NEW_LINE)
+                .append(BUY_OPERATION).append(SIGN_COMMA).append(buyAmount).append(NEW_LINE)
+                .append(RESULT_OPERATION).append(SIGN_COMMA).append(supplyAmount - buyAmount);
+
+        return stringBuilder.toString();
     }
 }
