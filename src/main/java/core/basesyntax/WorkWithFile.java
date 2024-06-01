8e0891c36c89e1196d1cf6835458e3ce7f94f5dd
dmitry <dmarmul228@gmail.com>
2024-06-01 15:42:59 +0300

    used bufferedWriter and bufferedReader for reading and writing data to file

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,36 @@
 package core.basesyntax;
 
-public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class WorkWithFile {
+    private static final String SORT_SUPPLY = "upply";
+    private static final String SORT_BUY = "uy";
+
+    public static void getStatistic(String fromFileName, String toFileName) {
+        int supplyAmount = 0;
+        int byeAmount = 0;
+        File file = new File(toFileName);
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName));
+            String fileLine = bufferedReader.readLine();
+            while (fileLine != null) {
+                if (fileLine.contains(SORT_SUPPLY)) {
+                    supplyAmount += Integer.parseInt(fileLine.substring(7));
+                } else if (fileLine.contains(SORT_BUY)) {
+                    byeAmount += Integer.parseInt(fileLine.substring(4));
+                }
+                fileLine = bufferedReader.readLine();
+            }
+            bufferedWriter.write("supply," + supplyAmount + System.lineSeparator()
+                    + "buy," + byeAmount + System.lineSeparator()
+                    + "result," + (supplyAmount - byeAmount));
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
     }
 }
