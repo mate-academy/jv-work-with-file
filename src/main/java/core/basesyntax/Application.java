c4d649818937d924a8f4fdaf31b88a5a94b82d3c
foxstudent106239 <mykhailo.kornukh@gmail.com>
2024-06-05 18:38:23 +0300

    Application change

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,7 +2,6 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        WorkWithFile workWithFile = new WorkWithFile();
-        workWithFile.getStatistic("apple.csv", "result.csv");
+
     }
 }
