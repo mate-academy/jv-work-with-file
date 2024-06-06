f44a40fef3cd23905d984d4177146bf05812932a
JaverJaver <mopasaj205@arpizol.com>
2024-06-06 16:14:17 +0300

    last 2

--- src/main/java/core/basesyntax/FileParser.java
+++ src/main/java/core/basesyntax/FileParser.java
@@ -3,8 +3,8 @@ package core.basesyntax;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
 public class FileParser {
 
