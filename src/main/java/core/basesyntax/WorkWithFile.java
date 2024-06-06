77a10e024ec36d166f3246adcdc4347e1f107872
maaarynet <mashajazz.mj@gmail.com>
2024-06-06 19:34:19 +0300

    implemented method getStatistic

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,55 @@
 package core.basesyntax;
 
-public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class WorkWithFile {
+    private String line;
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        int supply = 0;
+        int buy = 0;
+        int result = 0;
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] parts = line.split(",");
+                String type = parts[0];
+                int value = Integer.parseInt(parts[1]);
+                if (type.equals("supply")) {
+                    supply += value;
+                } else if (type.equals("buy")) {
+                    buy += value;
+                }
+            }
+            result = supply - buy;
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read data from the file " + fromFileName, e);
+        }
+
+        toFileName(toFileName, supply, buy, result);
+    }
+
+    public void toFileName(String toFileName, int supply, int buy, int result) {
+        File file = new File(toFileName);
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            throw new RuntimeException("Cannot create file " + toFileName, e);
+        }
+
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
+            bufferedWriter.write("supply," + supply);
+            bufferedWriter.newLine();
+            bufferedWriter.write("buy," + buy);
+            bufferedWriter.newLine();
+            bufferedWriter.write("result," + result);
+        } catch (IOException e) {
+            throw new RuntimeException("Cannot write to file " + toFileName, e);
+        }
     }
 }
