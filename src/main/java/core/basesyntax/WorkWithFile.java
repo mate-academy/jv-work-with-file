751c761fd62cac07d3de9e057c2f0245df741661
Oleh <oleh.vodolazov@gmail.com>
2024-06-09 18:07:37 +0300

    read data from a file, create a report from this data, write this report to another file

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,64 @@
 package core.basesyntax;
 
-public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class WorkWithFile {
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+    private static final String RESULT = "result";
+    private static final String COMMA = ",";
+    private static final String NEW_LINE = System.lineSeparator();
+    private static final String SPACE = " ";
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        String dataFromFile = readFile(fromFileName);
+        String report = createReport(dataFromFile);
+        writeToFile(toFileName, report);
+    }
+
+    public String readFile(String fromFileName) {
+        File file = new File(fromFileName);
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            String fileLine = bufferedReader.readLine();
+            while (fileLine != null) {
+                stringBuilder.append(fileLine).append(SPACE);
+                fileLine = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
+        return stringBuilder.toString();
+    }
+
+    public String createReport(String dataFromFile) {
+        int totalSupply = 0;
+        int totalBuy = 0;
+        String[] parts = dataFromFile.split(SPACE);
+        for (String part : parts) {
+            if (part.contains(SUPPLY)) {
+                totalSupply += Integer.parseInt(part.replaceAll(SUPPLY + COMMA, ""));
+            } else if (part.contains(BUY)) {
+                totalBuy += Integer.parseInt(part.replaceAll(BUY + COMMA, ""));
+            }
+        }
+        return SUPPLY + COMMA + totalSupply + NEW_LINE
+                + BUY + COMMA + totalBuy + NEW_LINE
+                + RESULT + COMMA + (totalSupply - totalBuy);
+    }
+
+    public void writeToFile(String toFileName, String report) {
+        File file = new File(toFileName);
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
+            bufferedWriter.write(report);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write data to file", e);
+        }
     }
 }
