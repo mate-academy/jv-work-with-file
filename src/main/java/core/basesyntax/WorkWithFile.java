98528657d011e1758e4914d58402cdf359d76e1b
Nikita Boyko <nikitabojko57@gmail.com>
2024-06-11 18:59:11 +0300

    change methods in WorkWithFile.java file after review

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,57 +2,82 @@ package core.basesyntax;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class WorkWithFile {
-    public static final String SUPPLY = "supply";
-    public static final String BUY = "buy";
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        int supply = 0;
-        int buy = 0;
-        int result = 0;
-        try (BufferedReader reader = new BufferedReader(new FileReader(new File(fromFileName)));
-                BufferedWriter writer = new BufferedWriter(new FileWriter(new File(toFileName)))) {
-            int[] data = getDataFromFile(reader);
-            supply = data[0];
-            buy = data[1];
-            result = data[2];
-            writeDataToTheFile(writer, supply, buy, result);
+        try {
+            String data = readFile(fromFileName);
+            String report = generateReport(data);
+            writeToFile(toFileName, report);
         } catch (IOException e) {
-            throw new RuntimeException("Something went wrong.");
+            e.printStackTrace();
         }
     }
 
-    public int[] getDataFromFile(BufferedReader reader) throws IOException {
-        int[] array = new int[3];
-        int supply = 0;
-        int buy = 0;
-        int result = 0;
-        String line = reader.readLine();
-        while (line != null) {
-            String[] separatedLine = line.split(",");
-            if (separatedLine[0].equals(SUPPLY)) {
-                supply += Integer.parseInt(separatedLine[1]);
-            } else if (separatedLine[0].equals(BUY)) {
-                buy += Integer.parseInt(separatedLine[1]);
+    private String generateReport(String data) {
+        StringBuilder report = new StringBuilder();
+        data = data.replaceAll("\\[|\\]", "");
+        String[] stringArray = data.split(", ");
+        System.out.println(data);
+        report.append("supply,").append(Integer.parseInt(stringArray[0]))
+                .append(System.lineSeparator());
+        report.append("buy,").append(Integer.parseInt(stringArray[1]))
+                .append(System.lineSeparator());
+        report.append("result,").append(Integer.parseInt(stringArray[2]))
+                .append(System.lineSeparator());
+        return report.toString();
+    }
+
+    private String readFile(String fileName) throws IOException {
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader(fileName));
+            int[] array = new int[3];
+            int supply = 0;
+            int buy = 0;
+            int result = 0;
+            String line = reader.readLine();
+            while (line != null) {
+                String[] separatedLine = line.split(",");
+                if (separatedLine[0].equals(SUPPLY)) {
+                    supply += Integer.parseInt(separatedLine[1]);
+                } else if (separatedLine[0].equals(BUY)) {
+                    buy += Integer.parseInt(separatedLine[1]);
+                }
+                line = reader.readLine();
             }
-            line = reader.readLine();
+            result = supply - buy;
+            array[0] = supply;
+            array[1] = buy;
+            array[2] = result;
+            return Arrays.toString(array);
+        } catch (IOException e) {
+            throw new RuntimeException("Something went wrong");
+        } finally {
+            if (reader != null) {
+                reader.close();
+            }
         }
-        result = supply - buy;
-        array[0] = supply;
-        array[1] = buy;
-        array[2] = result;
-        return array;
     }
 
-    public void writeDataToTheFile(BufferedWriter writer, int supply, int buy,
-                                   int result) throws IOException {
-        writer.write("supply," + supply + System.lineSeparator());
-        writer.write("buy," + buy + System.lineSeparator());
-        writer.write("result," + result);
+    private void writeToFile(String fileName, String report) throws IOException {
+        BufferedWriter writer = null;
+        try {
+            writer = new BufferedWriter(new FileWriter(fileName));
+            writer.write(report);
+        } catch (IOException e) {
+            throw new RuntimeException("Something went wrong");
+        } finally {
+            if (writer != null) {
+                writer.close();
+            }
+        }
     }
 }
