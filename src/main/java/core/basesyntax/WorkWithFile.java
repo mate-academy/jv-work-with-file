8c8ee564b4585d5cd212a22fbc0ecdac87e25ecc
Artem Hurievskyi <gurievsky.artem@gmail.com>
2024-06-14 11:23:40 +0300

    connected two calculating methods into one, created local veribles for data values in getStatistic method, changed String concatination to StringBuilder append method

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -10,12 +10,15 @@ public class WorkWithFile {
     private static final String SUPPLY = "supply";
     private static final String BUY = "buy";
     private static final String RESULT = "result";
+    private static final int SUPPLY_INDEX = 0;
+    private static final int BUY_INDEX = 1;
     private static final int START_VALUE = 0;
-    private static final String COMA = ",";
+    private static final String COMMA = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        writeInfoToFile(calculateSupplyValue(readFromFile(fromFileName)),
-                calculateBuyValue(readFromFile(fromFileName)), toFileName);
+        String[] separateDataFromFile = readFromFile(fromFileName);
+        int[] reportValues = calculateReport(separateDataFromFile);
+        writeInfoToFile(reportValues, toFileName);
     }
 
     private String[] readFromFile(String fromFileName) {
@@ -24,40 +27,37 @@ public class WorkWithFile {
             StringBuilder builder = new StringBuilder();
             String line = bufferedReader.readLine();
             while (line != null) {
-                builder.append(line).append(COMA);
+                builder.append(line).append(COMMA);
                 line = bufferedReader.readLine();
             }
-            return builder.toString().split(COMA);
+            return builder.toString().split(COMMA);
         } catch (IOException e) {
             throw new RuntimeException("Can't read from file " + fromFileName + e.getMessage());
         }
     }
 
-    private int calculateSupplyValue(String[] dataLines) {
+    private int[] calculateReport(String[] dataLines) {
         int supplyValue = START_VALUE;
+        int buyValue = START_VALUE;
         for (int i = 0; i < dataLines.length; i++) {
             if (dataLines[i].equals(SUPPLY)) {
                 supplyValue += Integer.parseInt(dataLines[i + 1]);
-            }
-        }
-        return supplyValue;
-    }
-
-    private int calculateBuyValue(String[] dataLines) {
-        int buyValue = START_VALUE;
-        for (int i = 0; i < dataLines.length; i++) {
-            if (dataLines[i].equals(BUY)) {
+            } else if (dataLines[i].equals(BUY)) {
                 buyValue += Integer.parseInt(dataLines[i + 1]);
             }
         }
-        return buyValue;
+        return new int[]{supplyValue, buyValue};
     }
 
-    private void writeInfoToFile(int supply, int buy, String toFile) {
+    private void writeInfoToFile(int[] calculateReport, String toFile) {
+        StringBuilder builder = new StringBuilder();
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFile))) {
-            bufferedWriter.write(SUPPLY + "," + supply + System.lineSeparator()
-                    + BUY + "," + buy + System.lineSeparator()
-                    + RESULT + "," + (supply - buy));
+            builder.append(SUPPLY).append(COMMA).append(calculateReport[SUPPLY_INDEX])
+                    .append(System.lineSeparator()).append(BUY).append(COMMA)
+                    .append(calculateReport[BUY_INDEX])
+                    .append(System.lineSeparator()).append(RESULT).append(COMMA)
+                    .append(calculateReport[SUPPLY_INDEX] - calculateReport[BUY_INDEX]);
+            bufferedWriter.write(builder.toString());
         } catch (IOException e) {
             throw new RuntimeException("Can't write down to the file " + toFile + e.getMessage());
         }
