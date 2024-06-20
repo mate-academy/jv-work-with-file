aabb78998b8522bdaf86fe897354ed700468d789
Fedir Lozovyi <sumkin112@gmail.com>
2024-06-20 19:34:23 +0200

    rewrote the code according to the comments

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,62 +1,59 @@
 package core.basesyntax;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
 public class WorkWithFile {
-    private int buySum = 0;
-    private int supplySum = 0;
+    private final String SUPPLY = "supply";
+    private final String BUY = "buy";
+    private final String SEPARATOR = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        readFromFile(fromFileName);
-        calculateReport();
-        writeToFile(toFileName);
-        cleanUp();
+        int[] output = readFromFile(fromFileName);
+        String result = calculateReport(output);
+        writeToFile(result, toFileName);
     }
 
-    private void cleanUp() {
-        buySum = 0;
-        supplySum = 0;
-    }
-
-    public void readFromFile(String fromFileName) {
+    public int[] readFromFile(String fromFileName) {
+        int buySum = 0;
+        int supplySum = 0;
         try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
             String line;
             while ((line = reader.readLine()) != null) {
-                String[] parts = line.split(",");
+                String[] parts = line.split(SEPARATOR);
                 String type = parts[0];
                 int amount = Integer.parseInt(parts[1]);
 
-                if ("supply".equals(type)) {
+                if (SUPPLY.equals(type)) {
                     supplySum += amount;
-                } else if ("buy".equals(type)) {
+                } else if (BUY.equals(type)) {
                     buySum += amount;
                 }
             }
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException("Can't find file ", e);
         } catch (IOException e) {
-            throw new RuntimeException("Can't read file ", e);
+            throw new RuntimeException("Can't read file "+ fromFileName, e);
         }
+        int supplyResult = supplySum;
+        int buyResult = buySum;
+        int result = supplySum - buySum;
+        return new int[] {supplyResult, buyResult, result};
     }
 
-    public int calculateReport() {
-        return supplySum - buySum;
+    public String calculateReport(int[] data) {
+        int supplyResult = data[0];
+        int buyResult = data[1];
+        int result = data[2];
+        StringBuilder builder = new StringBuilder();
+        builder.append(SUPPLY).append(SEPARATOR).append(supplyResult).append(System.lineSeparator())
+                .append(BUY).append(SEPARATOR).append(buyResult).append(System.lineSeparator())
+                .append("result,").append(result);
+        return builder.toString();
     }
 
-    public void writeToFile(String toFileName) {
+    public void writeToFile(String data, String toFileName) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
-            StringBuilder builder = new StringBuilder();
-            builder.append("supply,").append(supplySum).append(System.lineSeparator())
-                    .append("buy,").append(buySum).append(System.lineSeparator())
-                    .append("result,").append(calculateReport());
-            writer.write(builder.toString());
+            writer.write(data);
         } catch (IOException e) {
-            throw new RuntimeException("Can't write file ", e);
+            throw new RuntimeException("Can't write file " + toFileName, e);
         }
     }
 }
