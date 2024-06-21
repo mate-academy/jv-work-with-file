dd529e5ce2643240d7e0626f6eaef73d72ee4a40
Panchenko Dmytro <panchenko0795@gmail.com>
2024-06-21 03:00:00 +0300

    Separated 'readDataFromFile' method for 2 logical methods 'readDataFromFile' and 'dataProcessing'

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -16,7 +16,8 @@ public class WorkWithFile {
     private static final String COMA_SEPARATOR = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        String[] resultParameters = readDataFromFile(fromFileName);
+        String[] receivedData = readDataFromFile(fromFileName);
+        String[] resultParameters = dataProcessing(receivedData);
         int supplyAmount = Integer.parseInt(resultParameters[INDEX_OF_SUPPLY_PARAMETER]);
         int buyAmount = Integer.parseInt(resultParameters[INDEX_OF_BUY_PARAMETER]);
         String report = createReport(supplyAmount, buyAmount);
@@ -24,28 +25,30 @@ public class WorkWithFile {
     }
 
     private String[] readDataFromFile(String fromFileName) {
-        int supplyAmount = 0;
-        int buyAmount = 0;
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             StringBuilder stringBuilder = new StringBuilder();
             String lineFromFile;
             while ((lineFromFile = bufferedReader.readLine()) != null) {
                 stringBuilder.append(lineFromFile).append(System.lineSeparator());
             }
-
-            String[] receivedData = stringBuilder.toString().split(System.lineSeparator());
-            for (String data : receivedData) {
-                String[] dataArray = data.split(COMA_SEPARATOR);
-                if (dataArray[INDEX_OF_BUSINESS_TYPE_PARAMETER].equalsIgnoreCase(SUPPLY)) {
-                    supplyAmount += Integer.parseInt(dataArray[INDEX_OF_AMOUNT]);
-                } else if (dataArray[INDEX_OF_BUSINESS_TYPE_PARAMETER].equalsIgnoreCase(BUY)) {
-                    buyAmount += Integer.parseInt(dataArray[INDEX_OF_AMOUNT]);
-                } else {
-                    System.out.println("Invalid data, skip it");
-                }
-            }
+            return stringBuilder.toString().split(System.lineSeparator());
         } catch (IOException e) {
             throw new RuntimeException("Can't read data from the file " + fromFileName, e);
+        }
+    }
+
+    private String[] dataProcessing(String[] receivedData) {
+        int supplyAmount = 0;
+        int buyAmount = 0;
+        for (String data : receivedData) {
+            String[] dataArray = data.split(COMA_SEPARATOR);
+            if (dataArray[INDEX_OF_BUSINESS_TYPE_PARAMETER].equalsIgnoreCase(SUPPLY)) {
+                supplyAmount += Integer.parseInt(dataArray[INDEX_OF_AMOUNT]);
+            } else if (dataArray[INDEX_OF_BUSINESS_TYPE_PARAMETER].equalsIgnoreCase(BUY)) {
+                buyAmount += Integer.parseInt(dataArray[INDEX_OF_AMOUNT]);
+            } else {
+                throw new RuntimeException("Invalid data");
+            }
         }
         return new String[]{String.valueOf(supplyAmount), String.valueOf(buyAmount)};
     }
