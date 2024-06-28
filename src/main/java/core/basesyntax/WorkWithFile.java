484a4a51c66787f7fe8df9c9f7f64569854b7797
Xoteki <xotekijke@gmail.com>
2024-06-28 18:42:50 +0300

    provided recommended fixes

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,41 +8,34 @@ import java.io.IOException;
 
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
-        int supplyOutput = 0;
-        int buyOutput = 0;
-        int result = 0;
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName));
+                BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
 
-        try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName));
-            StringBuilder stringBuilder = new StringBuilder();
-            String value = bufferedReader.readLine();
-            while (value != null) {
+            int supplyOutput = 0;
+            int buyOutput = 0;
+            String value;
+
+            while ((value = bufferedReader.readLine()) != null) {
                 String[] spitedLine = value.split(",");
                 String name = spitedLine[0];
-                value = bufferedReader.readLine();
                 int amount = Integer.parseInt(spitedLine[1]);
 
-                if (name.equals("supply")) {
+                if ("supply".equals(name)) {
                     supplyOutput += amount;
-                } else if (name.equals("buy")) {
+                } else if ("buy".equals(name)) {
                     buyOutput += amount;
                 }
-                result = supplyOutput - buyOutput;
             }
-        } catch (IOException e) {
-            throw new RuntimeException("Can't read file", e);
-        }
 
-        try {
-            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName));
+            int result = supplyOutput - buyOutput;
             bufferedWriter.write("supply," + supplyOutput);
             bufferedWriter.newLine();
             bufferedWriter.write("buy," + buyOutput);
             bufferedWriter.newLine();
             bufferedWriter.write("result," + result);
-            bufferedWriter.close();
+
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException("Error processing files", e);
         }
     }
 }
