84d927470e5881bdcff40f83cb5b8ed3de9fa074
KLICKICE <ledmaskiight@gmail.com>
2024-07-12 13:41:27 +0300

    Help please, i add method which reading data, but idk what to do next

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,23 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
+        try {
+            FileReader fileReader = new FileReader(fromFileName);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            StringBuilder stringBuilder = new StringBuilder();
+            String value = bufferedReader.readLine();
+            while (value != null){
+                stringBuilder.append(value).append(", ");
+                value = bufferedReader.readLine();}
+        } catch (IOException e) {
+                throw new RuntimeException("Can't read the file");
+        }
 
+        }
     }
 }
