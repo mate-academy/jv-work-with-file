5976f931d19bf6a6f47549b7c589e5f190955fe7
OD <sdenisenko055@gmail.com>
2024-07-12 11:59:14 +0300

    6

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -11,73 +11,59 @@ public class WorkWithFile {
     private static final String BUY = "buy";
     private static final String RESULT = "result";
     private static final String COMMA = ",";
-    private static final int INNER_ARRAY_SIZE = 2;
     private static final int NUMBER_ZERO = 0;
     private static final int NUMBER_ONE = 1;
     private static final int NUMBER_TWO = 2;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        String[][] data = readFromFile(fromFileName);
-        int[] statistics = calculateReport(data);
-        writeToFile(toFileName, statistics);
+        String data = readFromFile(fromFileName);
+        String report = calculateReport(data);
+        writeToFile(toFileName, report);
     }
 
-    private String[][] readFromFile(String fileName) {
+    private String readFromFile(String fileName) {
+        StringBuilder data = new StringBuilder();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
-            int arraySize = createArraySize(fileName);
-            String[][] sourceArray = new String[arraySize][INNER_ARRAY_SIZE];
             String line;
-            int i = NUMBER_ZERO;
             while ((line = bufferedReader.readLine()) != null) {
-                sourceArray[i] = line.split(COMMA);
-                i++;
+                data.append(line).append(System.lineSeparator());
             }
-            return sourceArray;
+            return data.toString();
         } catch (IOException e) {
             throw new RuntimeException("Can't read info from file", e);
         }
     }
 
-    private int[] calculateReport(String[][] originalSourceArray) {
+    private String calculateReport(String data) {
         int totalSupply = NUMBER_ZERO;
         int totalBuy = NUMBER_ZERO;
-        for (String[] sourceElement : originalSourceArray) {
-            if (sourceElement[NUMBER_ZERO].equals(SUPPLY)) {
-                totalSupply += Integer.parseInt(sourceElement[NUMBER_ONE]);
-            } else if (sourceElement[NUMBER_ZERO].equals(BUY)) {
-                totalBuy += Integer.parseInt(sourceElement[NUMBER_ONE]);
+        String[] lines = data.split(System.lineSeparator());
+        for (String line : lines) {
+            String[] parts = line.split(COMMA);
+            if (parts[NUMBER_ZERO].equals(SUPPLY)) {
+                totalSupply += Integer.parseInt(parts[NUMBER_ONE]);
+            } else if (parts[NUMBER_ZERO].equals(BUY)) {
+                totalBuy += Integer.parseInt(parts[NUMBER_ONE]);
             }
         }
-        return new int[]{totalSupply, totalBuy, totalSupply - totalBuy};
+        return buildReportContent(totalSupply, totalBuy);
     }
 
-    private void writeToFile(String reportName, int[] statistics) {
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(reportName))) {
-            bufferedWriter.write(buildReportContent(statistics));
-        } catch (IOException e) {
-            throw new RuntimeException("Can't write info in file", e);
-        }
-    }
-
-    private String buildReportContent(int[] statistics) {
+    private String buildReportContent(int totalSupply, int totalBuy) {
         StringBuilder text = new StringBuilder();
-        text.append(SUPPLY).append(COMMA).append(statistics[NUMBER_ZERO])
+        text.append(SUPPLY).append(COMMA).append(totalSupply)
                 .append(System.lineSeparator())
-                .append(BUY).append(COMMA).append(statistics[NUMBER_ONE])
+                .append(BUY).append(COMMA).append(totalBuy)
                 .append(System.lineSeparator())
-                .append(RESULT).append(COMMA).append(statistics[NUMBER_TWO]);
+                .append(RESULT).append(COMMA).append(totalSupply - totalBuy);
         return text.toString();
     }
 
-    private int createArraySize(String fileName) {
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
-            int arraySize = NUMBER_ZERO;
-            while (bufferedReader.readLine() != null) {
-                arraySize++;
-            }
-            return arraySize;
+    private void writeToFile(String reportName, String report) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(reportName))) {
+            bufferedWriter.write(report);
         } catch (IOException e) {
-            throw new RuntimeException("Can't read info from file", e);
+            throw new RuntimeException("Can't write info in file", e);
         }
     }
 }
