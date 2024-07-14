a6e2dec0be4c5282bbeaffd1ba62610ebe770405
homeworkfirst <zookotelif@gmail.com>
2024-07-15 01:36:05 +0300

    WorkWithFile done

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -6,7 +6,6 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
-
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
         int supply = 0;
