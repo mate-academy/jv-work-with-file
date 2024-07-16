7c293f287eb82f087534d43629fac3858844b952
kozma <kozmakruckov@gmail.com>
2024-07-16 20:46:04 +0300

    added jv-work-with-file 2

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -13,55 +13,44 @@ public class WorkWithFile {
         writeFile(result, toFileName);
     }
 
-    public void writeFile(String result, String toFileName) {
-        BufferedWriter writer = null;
-            try {
-                writer = new BufferedWriter(new FileWriter(toFileName, true));
-                writer.write(result);
-            } catch (IOException e) {
-                throw new RuntimeException("Can't write data to file", e);
-            } finally {
-                try {
-                    writer.close();
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-
-    }
-
-    public int[] readFile(String fromFileName) {
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
-            StringBuilder stringBuilder = new StringBuilder();
-            String value = reader.readLine();
-            while (value != null) {
-                stringBuilder.append(value).append(System.lineSeparator());
-                value = reader.readLine();
-            }
-            String string = stringBuilder.toString();
-            String[] split = string.split(System.lineSeparator());
-            int supply = 0;
-            int buy = 0;
-            for (int i = 0; i < split.length; i++) {
-                String substring = split[i].substring(split[i].indexOf(",") + 1);
-                int parseInt = Integer.parseInt(substring);
-                if (split[i].substring(0, split[i].indexOf(",")).equals("supply")) {
-                    supply += parseInt;
-                } else {
-                    buy += parseInt;
-                }
-            }
-            return new int[] {supply, buy, (supply - buy)};
+    public void writeFile(String data, String toFileName) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName, true))) {
+            writer.write(data);
         } catch (IOException e) {
-            throw new RuntimeException("Can't read file", e);
+            throw new RuntimeException("Can't write data to file" + toFileName, e);
         }
     }
 
-    public String getResult(int[] arrays) {
-        String result = "supply," + arrays[0] + System.lineSeparator()
-                + "buy," + arrays[1] + System.lineSeparator()
-                + "result," + arrays[2] + System.lineSeparator();
-        return result;
+    public int[] readFile(String fromFileName) {
+        int supplyResult = 0;
+        int buyResult = 0;
+        int result = 0;
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
+            String value = reader.readLine();
+            while (value != null) {
+                String amount = value.substring(value.indexOf(",") + 1);
+                if (value.contains("supply")) {
+                    supplyResult += Integer.parseInt(amount);
+                } else if (value.contains("buy")) {
+                    buyResult += Integer.parseInt(amount);
+                }
+                result = supplyResult - buyResult;
+                value = reader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
+        return new int[] {supplyResult, buyResult, result};
+    }
+
+    public String getResult(int[] data) {
+        int supplyResult = data[0];
+        int buyResult = data[1];
+        int result = data[2];
+        StringBuilder builder = new StringBuilder();
+        builder.append("supply").append(",").append(supplyResult).append(System.lineSeparator())
+                .append("buy").append(",").append(buyResult).append(System.lineSeparator())
+                .append("result").append(",").append(result);
+        return builder.toString();
     }
 }
