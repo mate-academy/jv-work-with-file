dae21d885f728e6815ec6c78cc013e0eca86a9d9
Ivan Koshovyi <ivan2023koshovyi@gmail.com>
2024-07-17 17:34:16 +0300

    Completed task

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -6,46 +6,44 @@ import java.io.FileReader;
 import java.io.FileWriter;
 
 public class WorkWithFile {
-    private int supply;
-    private int buy;
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+    private static final String RESULT = "result";
+    private static final String COMA = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        readFile(fromFileName);
-        writeFile(toFileName);
-        cleanBuffered();
+        int[] newInfoForSupplyAndBuy = readFile(fromFileName);
+        writeFile(toFileName, newInfoForSupplyAndBuy);
     }
 
-    private void writeFile(String toFileName) {
+    private void writeFile(String toFileName, int[] information) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
-            bufferedWriter.write("supply," + supply
-                    + System.lineSeparator() + "buy," + buy
-                    + System.lineSeparator() + "result," + (supply - buy));
+            String stringBuilder = SUPPLY + COMA + information[0] + System.lineSeparator()
+                    + BUY + COMA + information[1] + System.lineSeparator()
+                    + RESULT + COMA + (information[0] - information[1]);
+            bufferedWriter.write(stringBuilder);
         } catch (Exception e) {
-            throw new RuntimeException("Can't read data from the file " + toFileName, e);
+            throw new RuntimeException("Can't write data from the file " + toFileName, e);
         }
     }
 
-    private void readFile(String fromFileName) {
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
+    private int[] readFile(String fromFileName) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
+            int[] infoForSupplyAndBuy = new int[2];
             String info = reader.readLine();
             while (info != null) {
-                String[] strings = info.split(",");
-                if (strings[0].equals("supply")) {
-                    supply += Integer.parseInt(strings[1]);
-                } else if (strings[0].equals("buy")) {
-                    buy += Integer.parseInt(strings[1]);
+                String[] strings = info.split(COMA);
+                if (strings[0].equals(SUPPLY)) {
+                    infoForSupplyAndBuy[0] += Integer.parseInt(strings[1]);
+                } else if (strings[0].equals(BUY)) {
+                    infoForSupplyAndBuy[1] += Integer.parseInt(strings[1]);
                 }
                 info = reader.readLine();
             }
+            return infoForSupplyAndBuy;
         } catch (Exception e) {
             throw new RuntimeException("Can't read data from the file " + fromFileName, e);
         }
-    }
-
-    private void cleanBuffered() {
-        supply = 0;
-        buy = 0;
     }
 }
 
