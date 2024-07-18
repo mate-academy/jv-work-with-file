882c9ce54f81be3619e095aeb87a80777144ae3c
Ivan Koshovyi <ivan2023koshovyi@gmail.com>
2024-07-18 15:20:27 +0300

    Reworked the code according to the mentor's recommendations

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -12,37 +12,51 @@ public class WorkWithFile {
     private static final String COMA = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
+        String dataFromFile = readFile(fromFileName);
+        String report = createReport(dataFromFile);
+        writeReportToFile(report, toFileName);
+    }
+
+    private void writeReportToFile(String report, String toFileName) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+            bufferedWriter.write(report);
+        } catch (Exception e) {
+            throw new RuntimeException("Can't write data from the file " + toFileName, e);
+        }
+    }
+
+    private String createReport(String dataFromFile) {
+        String[] dataFromMarket = dataFromFile.split(COMA);
+        int sumSupple = Integer.parseInt(dataFromMarket[0]);
+        int sumBuy = Integer.parseInt(dataFromMarket[1]);
+        StringBuilder stringBuilder = new StringBuilder()
+                .append(SUPPLY).append(COMA).append(sumSupple)
+                .append(System.lineSeparator())
+                .append(BUY).append(COMA).append(sumBuy)
+                .append(System.lineSeparator())
+                .append(RESULT).append(COMA).append(sumSupple - sumBuy);
+        return stringBuilder.toString();
+    }
+
+    private String readFile(String fromFileName) {
         try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
             int sumSupply = 0;
             int sumBuy = 0;
             String infoLine = reader.readLine();
             while (infoLine != null) {
                 String[] marketDataArrays = infoLine.split(COMA);
-                if (marketDataArrays[0].equals(SUPPLY)) {
-                    sumSupply += Integer.parseInt(marketDataArrays[1]);
-                } else if (marketDataArrays[0].equals(BUY)) {
-                    sumBuy += Integer.parseInt(marketDataArrays[1]);
+                String dataType = marketDataArrays[0];
+                int dataPlace = Integer.parseInt(marketDataArrays[1]);
+                if (dataType.equals(SUPPLY)) {
+                    sumSupply += dataPlace;
+                } else if (dataType.equals(BUY)) {
+                    sumBuy += dataPlace;
                 }
                 infoLine = reader.readLine();
             }
-            writeFile(toFileName, sumSupply, sumBuy);
+            return sumSupply + COMA + sumBuy;
         } catch (Exception e) {
-            throw new RuntimeException("Can't read data from the file " + fromFileName, e);
+            throw new RuntimeException("Can't read data from the file  " + fromFileName, e);
         }
-    }
-
-    private void writeFile(String toFileName, int supple, int buy) {
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
-            bufferedWriter.write(String.valueOf(generateReport(supple, buy)));
-        } catch (Exception e) {
-            throw new RuntimeException("Can't write data from the file " + toFileName, e);
-        }
-    }
-
-    private StringBuilder generateReport(int sumSupple, int sumBuy) {
-        return new StringBuilder().append(SUPPLY).append(COMA).append(sumSupple)
-                .append(System.lineSeparator()).append(BUY).append(COMA)
-                .append(sumBuy).append(System.lineSeparator()).append(RESULT)
-                .append(COMA).append(sumSupple - sumBuy);
     }
 }
