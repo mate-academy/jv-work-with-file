2d2c6dafbc44121af4c964e81d3750057dee44e3
kozma <kozmakruckov@gmail.com>
2024-07-18 15:01:23 +0300

    added jv-work-with-file 3

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,12 +8,13 @@ import java.io.IOException;
 
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
-        int[] output = readFile(fromFileName);
+        String[] strings = readFile(fromFileName);
+        int[] output = dataFile(strings);
         String result = getResult(output);
         writeFile(result, toFileName);
     }
 
-    public void writeFile(String data, String toFileName) {
+    private void writeFile(String data, String toFileName) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName, true))) {
             writer.write(data);
         } catch (IOException e) {
@@ -21,29 +22,37 @@ public class WorkWithFile {
         }
     }
 
-    public int[] readFile(String fromFileName) {
-        int supplyResult = 0;
-        int buyResult = 0;
-        int result = 0;
+    private String[] readFile(String fromFileName) {
+        StringBuilder stringBuilder = new StringBuilder();
         try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
             String value = reader.readLine();
             while (value != null) {
-                String amount = value.substring(value.indexOf(",") + 1);
-                if (value.contains("supply")) {
-                    supplyResult += Integer.parseInt(amount);
-                } else if (value.contains("buy")) {
-                    buyResult += Integer.parseInt(amount);
-                }
-                result = supplyResult - buyResult;
+                stringBuilder.append(value).append(System.lineSeparator());
                 value = reader.readLine();
             }
         } catch (IOException e) {
             throw new RuntimeException("Can't read file", e);
         }
+        return stringBuilder.toString().split(System.lineSeparator());
+    }
+
+    private int[] dataFile(String[] arrays) {
+        int supplyResult = 0;
+        int buyResult = 0;
+        int result = 0;
+        for (int i = 0; i < arrays.length; i++) {
+            String substring = arrays[i].substring(0, arrays[i].indexOf(","));
+            if (substring.equals("supply")) {
+                supplyResult += Integer.parseInt(arrays[i].substring(arrays[i].indexOf(",") + 1));
+            } else if (substring.equals("buy")) {
+                buyResult += Integer.parseInt(arrays[i].substring(arrays[i].indexOf(",") + 1));
+            }
+            result = supplyResult - buyResult;
+        }
         return new int[] {supplyResult, buyResult, result};
     }
 
-    public String getResult(int[] data) {
+    private String getResult(int[] data) {
         int supplyResult = data[0];
         int buyResult = data[1];
         int result = data[2];
