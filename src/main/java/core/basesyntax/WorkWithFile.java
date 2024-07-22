bc3928934c34fad789177ef2d7bf9e52f8e080b3
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-07-22 18:41:42 +0300

    delete all unnecessary methods and refactor logic by using Map

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,93 +1,60 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class WorkWithFile {
+    private static final String COMMA = ",";
+
     public void getStatistic(String fromFileName, String toFileName) {
-        String[] data = getSortedData(getSumItems(fromFileName));
-        StringBuilder resultData = new StringBuilder();
-        for (String line : data) {
-            resultData.append(line).append(System.lineSeparator());
-        }
-        resultData.append(getOddOfValues(data));
-        writeToFile(resultData.toString().split(System.lineSeparator()), toFileName);
+        Map<String, Integer> textFromFile = readFromFile(fromFileName);
+        String report = generateReport(textFromFile);
+        writeToFile(report, toFileName);
     }
 
-    private String[] getSortedData(String[] resultData) {
-        Arrays.sort(resultData, (o1, o2) -> {
-            int number1 = Integer.parseInt(o1.split(",")[1]);
-            int number2 = Integer.parseInt(o2.split(",")[1]);
-            return Integer.compare(number2, number1);
-        });
-        return resultData;
-    }
+    private Map<String, Integer> readFromFile(String fromFileName) {
+        Map<String, Integer> productQuantities = new HashMap<>();
 
-    private String[] getSumItems(String fromFileName) {
-        String[] data = readFromFile(fromFileName);
-        StringBuilder resultData = new StringBuilder();
-        String[] keyWords = getUniqueWords(data);
-        int counter = 0;
-        for (String datum : keyWords) {
-            for (String word : data) {
-                String[] compareWords = word.split(",");
-                if (datum.equals(compareWords[0])) {
-                    counter += Integer.parseInt(compareWords[1]);
-                }
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(COMMA);
+                String product = parts[0];
+                int quantity = Integer.parseInt(parts[1]);
+
+                productQuantities.put(product, productQuantities.getOrDefault(
+                        product, 0) + quantity);
             }
-            resultData.append(datum).append(",").append(counter).append(System.lineSeparator());
-            counter = 0;
-        }
-        return resultData.toString().split(System.lineSeparator());
-    }
-
-    private String getOddOfValues(String[] resultData) {
-        StringBuilder result = new StringBuilder();
-        int odd = 0;
-        for (String number : resultData) {
-            int digit = Integer.parseInt(number.split(",")[1]);
-            if (digit > odd) {
-                odd = digit - odd;
-            } else {
-                odd -= digit;
-            }
-        }
-        result.append("result").append(",").append(odd);
-        return result.toString();
-    }
-
-    private String[] getUniqueWords(String[] dataFromFile) {
-        StringBuilder uniqueArray = new StringBuilder();
-        String allStrings = Arrays.toString(dataFromFile).replaceAll("[^a-zA-Z ]+", "");
-        String[] strings = allStrings.split(" ");
-        for (String temp : strings) {
-            if (!uniqueArray.toString().contains(temp)) {
-                uniqueArray.append(temp).append(" ");
-            }
-        }
-        return String.valueOf(uniqueArray).split(" ");
-    }
-
-    private String[] readFromFile(String fromFile) {
-        try {
-            return Files.readString(Paths.get(fromFile)).split("\r?\n");
         } catch (IOException e) {
             throw new RuntimeException("Can't read from file", e);
         }
+
+        return productQuantities;
     }
 
-    private void writeToFile(String[] text, String toFile) {
-        StringBuilder data = new StringBuilder();
-        for (String line : text) {
-            data.append(line).append(System.lineSeparator());
-        }
+    private String generateReport(Map<String, Integer> productQuantities) {
+        StringBuilder report = new StringBuilder();
+        int supplySum = productQuantities.getOrDefault("supply", 0);
+        int buySum = productQuantities.getOrDefault("buy", 0);
+        int result = supplySum - buySum;
+
+        report.append("supply,").append(supplySum).append(System.lineSeparator());
+        report.append("buy,").append(buySum).append(System.lineSeparator());
+        report.append("result,").append(result);
+
+        return report.toString();
+    }
+
+    private void writeToFile(String text, String toFile) {
+
         try (FileWriter file = new FileWriter(toFile);
                 BufferedWriter writer = new BufferedWriter(file)) {
-            writer.write(String.valueOf(data));
+            writer.write(text);
         } catch (IOException e) {
             throw new RuntimeException("Can't write to file", e);
         }
