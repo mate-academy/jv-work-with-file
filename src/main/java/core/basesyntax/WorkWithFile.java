528196ac4a22c42f056fa5f03101373d62535980
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-07-22 19:07:02 +0300

    delete all unnecessary methods and refactor logic.

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,48 +1,42 @@
 package core.basesyntax;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class WorkWithFile {
     private static final String COMMA = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        Map<String, Integer> textFromFile = readFromFile(fromFileName);
+        String[] textFromFile = readFromFile(fromFileName);
         String report = generateReport(textFromFile);
         writeToFile(report, toFileName);
     }
 
-    private Map<String, Integer> readFromFile(String fromFileName) {
-        Map<String, Integer> productQuantities = new HashMap<>();
-
-        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String[] parts = line.split(COMMA);
-                String product = parts[0];
-                int quantity = Integer.parseInt(parts[1]);
-
-                productQuantities.put(product, productQuantities.getOrDefault(
-                        product, 0) + quantity);
-            }
+    private String[] readFromFile(String fromFileName) {
+        try {
+            return Files.readString(Path.of(fromFileName)).split("\r?\n");
         } catch (IOException e) {
             throw new RuntimeException("Can't read from file", e);
         }
-
-        return productQuantities;
     }
 
-    private String generateReport(Map<String, Integer> productQuantities) {
+    private String generateReport(String[] data) {
         StringBuilder report = new StringBuilder();
-        int supplySum = productQuantities.getOrDefault("supply", 0);
-        int buySum = productQuantities.getOrDefault("buy", 0);
-        int result = supplySum - buySum;
-
+        int supplySum = 0;
+        int buySum = 0;
+        int result;
+        for (String product : data) {
+            String[] line = product.split(COMMA);
+            if (line[0].equals("supply")) {
+                supplySum += Integer.parseInt(line[1]);
+            } else {
+                buySum += Integer.parseInt(line[1]);
+            }
+        }
+        result = supplySum - buySum;
         report.append("supply,").append(supplySum).append(System.lineSeparator());
         report.append("buy,").append(buySum).append(System.lineSeparator());
         report.append("result,").append(result);
