b038ff2447512696773c73242273d041920441e5
Dmytro Tkachuk <dmitrotkacuk071@gmail.com>
2024-07-26 21:59:24 +0300

    added fixes after review

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -5,42 +5,38 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.List;
 
 public class WorkWithFile {
     private static final int OPERATION_TYPE_INDEX = 0;
     private static final int OPERATION_VALUE_INDEX = 1;
-    private  static  final  String  SUPPLY = "supply" ;
-    private  static  final  String  BUY = "buy" ;
-    private  static  final  String  RESULT = "result" ;
-    private  static  final  String  SEPARATOR = "," ;
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+    private static final String RESULT = "result";
+    private static final String SEPARATOR = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        String[] dataFromFile = readFromFile(fromFileName);
-        String report = calculateReport (dataFromFile);
-        writeIntoFile (report, toFileName);
+        String[] data = readFromFile(fromFileName);
+        String[] report = calculateReport(data);
+        writeIntoFile(toFileName,report);
     }
-    private void writeIntoFile (String report, String toFileName) {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName,false))) {
-            writer.write(report);
+
+    private String[] readFromFile(String fromFileName) {
+        StringBuilder data = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                data.append(line).append(System.lineSeparator());
+            }
         } catch (IOException e) {
-            throw new RuntimeException("Unable to write file data" + toFileName, e);
+            throw new RuntimeException("Can't read data from the file " + fromFileName, e);
         }
+        return data.toString().split(System.lineSeparator());
     }
-    private String[] readFromFile(String filepath) {
-        try {
-        List<String> lines = Files.readAllLines(Path.of(filepath));
-        return lines.toArray(new String[]{});
-    } catch (IOException e) {
-        throw new RuntimeException("Unable to read data from file " + filepath, e);
-        }
-    }
-    private String calculateReport (String[] report) {
+
+    private String [] calculateReport(String[] data) {
         int totalSupply = 0;
         int totalBuy = 0;
-        for (String reports : report) {
+        for (String reports : data) {
             String[] recortSplit = reports.split(SEPARATOR);
             String operationType = recortSplit[OPERATION_TYPE_INDEX];
             int amount = Integer.parseInt(recortSplit[OPERATION_VALUE_INDEX]);
@@ -50,11 +46,22 @@ public class WorkWithFile {
                 totalBuy += amount;
             }
         }
-            int result = totalSupply - totalBuy;
-            StringBuilder stats = new StringBuilder()
-                    .append(SUPPLY).append(totalSupply).append(System.lineSeparator())
-                    .append(BUY).append(totalBuy).append(System.lineSeparator())
-                    .append(RESULT).append(result);
-            return stats.toString();
+        int result = totalSupply - totalBuy;
+        return new String[] {
+                SUPPLY + SEPARATOR + totalSupply,
+                BUY + SEPARATOR + totalBuy,
+                RESULT + SEPARATOR + result
+        };
+    }
+
+    private void writeIntoFile(String toFileName, String[] report) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
+            for (String line : report) {
+                writer.write(line);
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to write data " + toFileName, e);
+        }
     }
 }
