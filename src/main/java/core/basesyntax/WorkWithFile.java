bf3e636590ab6e9a4ab120c601aabf03e3aa1eaf
Andrii Datko <a.p.datko@gmail.com>
2024-07-27 19:19:18 +0300

    Fixes have been made in accordance with the recommendations.

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -15,29 +15,30 @@ public class WorkWithFile {
     private static final int AMOUNT_INDEX = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        String[] output = readFile(fromFileName);
-        String[] result = calculateAndFormatReport(output);
-        writeToFile(result, toFileName);
+        String data = readFile(fromFileName);
+        String report = createReport(data);
+        writeToFile(report, toFileName);
     }
 
-    public String[] readFile(String fromFileName) {
+    public String readFile(String fromFileName) {
+        StringBuilder fileContent = new StringBuilder();
         try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
             String line;
-            StringBuilder fileContent = new StringBuilder();
             while ((line = reader.readLine()) != null) {
                 fileContent.append(line).append(System.lineSeparator());
             }
-            return fileContent.toString().split(System.lineSeparator());
         } catch (IOException e) {
             throw new RuntimeException("Can't read the file.", e);
         }
+        return fileContent.toString();
     }
 
-    public String[] calculateAndFormatReport(String[] output) {
+    public String createReport(String output) {
         int totalSupply = 0;
         int totalBuy = 0;
-        String[] report = new String[3];
-        for (String part : output) {
+        StringBuilder builder = new StringBuilder();
+        String[] data = output.split(System.lineSeparator());
+        for (String part : data) {
             String[] parts = part.split(COMMA_SEPARATOR);
             String operationType = parts[OPERATION_TYPE_INDEX].trim();
             int amount = Integer.parseInt(parts[AMOUNT_INDEX].trim());
@@ -48,21 +49,18 @@ public class WorkWithFile {
                 totalBuy += amount;
             }
         }
-        report[0] = SUPPLY + COMMA_SEPARATOR + totalSupply;
-        report[1] = BUY + COMMA_SEPARATOR + totalBuy;
-        report[2] = RESULT + COMMA_SEPARATOR + (totalSupply - totalBuy);
-        return report;
+        String supply = SUPPLY + COMMA_SEPARATOR + totalSupply + System.lineSeparator();
+        String buy = BUY + COMMA_SEPARATOR + totalBuy
+                + System.lineSeparator();
+        String result = RESULT + COMMA_SEPARATOR + (totalSupply - totalBuy);
+        return builder.append(supply).append(buy).append(result).toString();
     }
 
-    public void writeToFile(String[] report, String toFileName) {
+    public void writeToFile(String report, String toFileName) {
         try (BufferedWriter fileWriter = new BufferedWriter(new FileWriter(toFileName, false))) {
-            for (String part : report) {
-                fileWriter.write(part);
-                fileWriter.newLine();
-            }
+            fileWriter.write(report);
         } catch (IOException e) {
             throw new RuntimeException("Can`t write data to file.", e);
         }
     }
-
 }
