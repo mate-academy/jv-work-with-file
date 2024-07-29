24e7df8d4e7808f2f662ed8e7e438bab29aacfb5
Nikolai <colya.reznick@gmail.com>
2024-07-29 15:35:13 +0300

    Refactored createReport()

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -13,6 +13,33 @@ public class WorkWithFile {
         writeToFile(toFileName, createReport(readFromFile(fromFileName)));
     }
 
+    private class DataCounter {
+        private int buyAmount = 0;
+        private int supplyAmount = 0;
+
+        private DataCounter() {
+        }
+
+        private void countValues(String[] dataArray) {
+            for (int i = 0; i < dataArray.length; i += 2) {
+                switch (dataArray[i]) {
+                    case BUY_STRING_VALUE -> buyAmount += Integer.parseInt(dataArray[i + 1]);
+                    case SUPPLY_STRING_VALUE -> supplyAmount += Integer.parseInt(dataArray[i + 1]);
+                    default -> throw new RuntimeException(
+                            "Unexpected data in countBuyAndSupplyValues()");
+                }
+            }
+        }
+
+        private int getSupplyAmount() {
+            return supplyAmount;
+        }
+
+        private int getBuyAmount() {
+            return buyAmount;
+        }
+    }
+
     private String readFromFile(String fromFileName) {
         try {
             return Files.readString(new File(fromFileName).toPath());
@@ -22,25 +49,16 @@ public class WorkWithFile {
     }
 
     private String createReport(String data) {
-        String[] dataArray = data.split(SPLITTER_REGEX);
-        int buyAmount = 0;
-        int supplyAmount = 0;
-
-        for (int i = 0; i < dataArray.length; i += 2) {
-            switch (dataArray[i]) {
-                case BUY_STRING_VALUE -> buyAmount += Integer.parseInt(dataArray[i + 1]);
-                case SUPPLY_STRING_VALUE -> supplyAmount += Integer.parseInt(dataArray[i + 1]);
-                default -> throw new RuntimeException(
-                        "Unexpected data in WorkWithFile.createReport()");
-            }
-        }
-        return createStringReport(buyAmount, supplyAmount);
+        DataCounter dataCounter = new DataCounter();
+        dataCounter.countValues(data.split(SPLITTER_REGEX));
+        return createStringReport(dataCounter.getBuyAmount(), dataCounter.getSupplyAmount());
     }
 
     private String createStringReport(int buyAmount, int supplyAmount) {
-        return "supply," + supplyAmount + System.lineSeparator()
-                + "buy," + buyAmount + System.lineSeparator()
-                + "result," + (supplyAmount - buyAmount) + System.lineSeparator();
+        return new StringBuilder("supply,").append(supplyAmount).append(System.lineSeparator())
+                .append("buy,").append(buyAmount).append(System.lineSeparator())
+                .append("result,").append(supplyAmount - buyAmount).append(System.lineSeparator())
+                .toString();
     }
 
     private void writeToFile(String toFileName, String report) {
