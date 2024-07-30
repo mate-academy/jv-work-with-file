70500673639d71399396a3d3979d9df403c2bbb2
Nikita Kravtsov <deez999deez@gmail.com>
2024-07-30 16:03:56 +0300

    Implement "writeFile" method to split the "getStatistic" method ito smaller ones - "getStatistic" which read data from file and recalculate it and "writeFile" which write data to file.
    Turn all magic number in constants.

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -6,33 +6,40 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    private static final int START_AMOUNT_VALUE = 0;
+    private static final int SPLIT_POINT_0 = 0;
+    private static final int SPLIT_POINT_1 = 1;
+
     public void getStatistic(String fromFileName, String toFileName) {
         StringBuilder builder = new StringBuilder();
         String text;
-        int supplyAmount = 0;
-        int buyAmount = 0;
+        int supplyAmount = START_AMOUNT_VALUE;
+        int buyAmount = START_AMOUNT_VALUE;
 
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             while ((text = bufferedReader.readLine()) != null) {
                 String[] splitLine = text.split(",");
-                if (splitLine[0].equals("supply")) {
-                    supplyAmount += Integer.parseInt(splitLine[1]);
+                if (splitLine[SPLIT_POINT_0].equals("supply")) {
+                    supplyAmount += Integer.parseInt(splitLine[SPLIT_POINT_1]);
                     //Check is the "supply" operation type and increase supply amount.
-                } else if (splitLine[0].equals("buy")) {
-                    buyAmount += Integer.parseInt(splitLine[1]);
+                } else if (splitLine[SPLIT_POINT_0].equals("buy")) {
+                    buyAmount += Integer.parseInt(splitLine[SPLIT_POINT_1]);
                     //Check is the "buy" operation type and increase buy amount.
                 }
             }
         } catch (IOException e) {
-            throw new RuntimeException("Can't read a file" + fromFileName, e);
+            throw new RuntimeException("Can't read a file " + fromFileName, e);
         }
         builder.append("supply,").append(supplyAmount).append(System.lineSeparator())
                 .append("buy,").append(buyAmount).append(System.lineSeparator())
                 .append("result,").append(supplyAmount - buyAmount); //Build final statistic string.
         String statistic = builder.toString();
+        writeFile(statistic, toFileName);
+    }
 
+    private void writeFile(String data, String toFileName) {
         try (FileWriter fileWriter = new FileWriter(toFileName)) {
-            fileWriter.write(statistic);
+            fileWriter.write(data);
             //Write final statistic to file.
         } catch (IOException e) {
             throw new RuntimeException("Can't write data to file " + toFileName, e);
