c50658f03c41b2128686d79fcd0630360cf16966
Nikita Kravtsov <deez999deez@gmail.com>
2024-07-31 19:23:38 +0300

    Divine main method for processes - "getStatistic", "createReport" and "writeFile" respectively. Correct variables names. Make all string in String Builder as constant. Remove comments.

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,40 +7,45 @@ import java.io.IOException;
 
 public class WorkWithFile {
     private static final int START_AMOUNT_VALUE = 0;
-    private static final int SPLIT_POINT_0 = 0;
-    private static final int SPLIT_POINT_1 = 1;
+    private static final int ARRAY_SPLIT_POINT_0 = 0;
+    private static final int ARRAY_SPLIT_POINT_1 = 1;
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+    private static final String RESULT = "result";
+    private static final String COMA = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        StringBuilder builder = new StringBuilder();
         String text;
         int supplyAmount = START_AMOUNT_VALUE;
         int buyAmount = START_AMOUNT_VALUE;
 
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             while ((text = bufferedReader.readLine()) != null) {
-                String[] splitLine = text.split(",");
-                if (splitLine[SPLIT_POINT_0].equals("supply")) {
-                    supplyAmount += Integer.parseInt(splitLine[SPLIT_POINT_1]);
-                    //Check is the "supply" operation type and increase supply amount.
-                } else if (splitLine[SPLIT_POINT_0].equals("buy")) {
-                    buyAmount += Integer.parseInt(splitLine[SPLIT_POINT_1]);
-                    //Check is the "buy" operation type and increase buy amount.
+                String[] splitLine = text.split(COMA);
+                if (splitLine[ARRAY_SPLIT_POINT_0].equals(SUPPLY)) {
+                    supplyAmount += Integer.parseInt(splitLine[ARRAY_SPLIT_POINT_1]);
+                } else if (splitLine[ARRAY_SPLIT_POINT_0].equals(BUY)) {
+                    buyAmount += Integer.parseInt(splitLine[ARRAY_SPLIT_POINT_1]);
                 }
             }
         } catch (IOException e) {
             throw new RuntimeException("Can't read a file " + fromFileName, e);
         }
-        builder.append("supply,").append(supplyAmount).append(System.lineSeparator())
-                .append("buy,").append(buyAmount).append(System.lineSeparator())
-                .append("result,").append(supplyAmount - buyAmount); //Build final statistic string.
-        String statistic = builder.toString();
+        String statistic = createReport(supplyAmount, buyAmount);
         writeFile(statistic, toFileName);
+    }
+
+    private String createReport(int supplyAmount, int buyAmount) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(SUPPLY).append(COMA).append(supplyAmount).append(System.lineSeparator())
+                .append(BUY).append(COMA).append(buyAmount).append(System.lineSeparator())
+                .append(RESULT).append(COMA).append(supplyAmount - buyAmount);
+        return builder.toString();
     }
 
     private void writeFile(String data, String toFileName) {
         try (FileWriter fileWriter = new FileWriter(toFileName)) {
             fileWriter.write(data);
-            //Write final statistic to file.
         } catch (IOException e) {
             throw new RuntimeException("Can't write data to file " + toFileName, e);
         }
