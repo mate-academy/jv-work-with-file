9f4e25fc32408f7375d8465d07ecee05bc694866
skrypkasv <skrypkasv@gmail.com>
2024-08-04 17:09:53 +0300

    change Files.write to BufferedWriter

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,10 +1,10 @@
 package core.basesyntax;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 public class WorkWithFile {
     private static final String SUPPLY = "supply";
@@ -52,8 +52,8 @@ public class WorkWithFile {
     }
 
     private void writeDataToFile(String toFileName, String report) {
-        try {
-            Files.write(Path.of(toFileName), report.getBytes());
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
+            writer.write(report);
         } catch (IOException e) {
             throw new RuntimeException("Can't write report to " + toFileName, e);
         }
