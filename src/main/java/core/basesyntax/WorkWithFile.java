dbfcca3d80b28c7c0582846762fa5aa2d6b479b6
Bohdan Stratiychuk <vasyahunta@gmail.com>
2024-08-07 16:33:42 +0300

    implemented method

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,37 @@
 package core.basesyntax;
 
-public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class WorkWithFile {
+    public static final int ACTION_PART = 0;
+    public static final int NUMBERS_PART = 1;
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        int supplySum = 0;
+        int buySum = 0;
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] parts = line.split(",");
+                if (parts[ACTION_PART].equals("supply")) {
+                    supplySum += Integer.parseInt(parts[NUMBERS_PART]);
+                } else {
+                    buySum += Integer.parseInt(parts[NUMBERS_PART]);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+            bufferedWriter.write("supply," + supplySum + System.lineSeparator());
+            bufferedWriter.write(("buy," + buySum + System.lineSeparator()));
+            bufferedWriter.write(("result," + (supplySum - buySum)));
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write data to file", e);
+        }
     }
 }
