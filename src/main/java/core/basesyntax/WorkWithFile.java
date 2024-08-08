2f59e437a19a9e02a014498c9fcc68ac866b57de
Yehor Nazarets <nazarets1111@gmail.com>
2024-08-08 10:53:37 +0200

    refactored method and created read and write methods

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -12,9 +12,13 @@ public class WorkWithFile {
     public static final String SEPARATOR = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
+        readDataFromFile(fromFileName, toFileName);
+    }
+
+    public void readDataFromFile(String fromFile, String toFile) {
         int supplySum = 0;
         int buySum = 0;
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFile))) {
             String line;
             while ((line = bufferedReader.readLine()) != null) {
                 String[] linesOfFile = line.split(SEPARATOR);
@@ -27,7 +31,11 @@ public class WorkWithFile {
         } catch (IOException e) {
             throw new RuntimeException("can`t read from file", e);
         }
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+        writeDataToFile(toFile, supplySum, buySum);
+    }
+
+    public void writeDataToFile(String toFile, int supplySum, int buySum) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFile))) {
             bufferedWriter.write("supply," + supplySum + System.lineSeparator());
             bufferedWriter.write("buy," + buySum + System.lineSeparator());
             bufferedWriter.write("result," + (supplySum - buySum));
