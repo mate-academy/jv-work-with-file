e307689626ab0ac119471782e2f2f94dcd6df639
Bohdan Stratiychuk <vasyahunta@gmail.com>
2024-08-09 15:29:42 +0300

    divited method

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -5,33 +5,57 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class WorkWithFile {
-    public static final int ACTION_PART = 0;
-    public static final int NUMBERS_PART = 1;
+    public static final int ACTION_PART_INDEX = 0;
+    public static final int NUMBERS_PART_INDEX = 1;
+    public static final int SUPPLY_PART_INDEX = 0;
+    public static final int BUY_PART_INDEX = 1;
+    public static final String SUPPLY_STRING = "supply";
+    public static final String SEPARATION_PLACE = ",";
+    public static final String BUY_STRING = "buy";
+    public static final String REPORT_STRING = "result";
 
-    public void getStatistic(String fromFileName, String toFileName) {
-        int supplySum = 0;
-        int buySum = 0;
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
+    private String[] readFromFile(String fileName) {
+        ArrayList<String> linesList = new ArrayList<>();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
             String line;
             while ((line = bufferedReader.readLine()) != null) {
-                String[] parts = line.split(",");
-                if (parts[ACTION_PART].equals("supply")) {
-                    supplySum += Integer.parseInt(parts[NUMBERS_PART]);
-                } else {
-                    buySum += Integer.parseInt(parts[NUMBERS_PART]);
-                }
+                linesList.add(line);
             }
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Can't read file", e);
         }
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
-            bufferedWriter.write("supply," + supplySum + System.lineSeparator());
-            bufferedWriter.write(("buy," + buySum + System.lineSeparator()));
-            bufferedWriter.write(("result," + (supplySum - buySum)));
+        return linesList.toArray(new String[0]);
+    }
+
+    private int[] generateReport(String[] readFileStrings) {
+        int supplySum = 0;
+        int buySum = 0;
+        for (String string : readFileStrings) {
+            String[] splitedString = string.split(SEPARATION_PLACE);
+            if (splitedString[ACTION_PART_INDEX].equals(SUPPLY_STRING)) {
+                supplySum += Integer.parseInt(splitedString[NUMBERS_PART_INDEX]);
+            } else {
+                buySum += Integer.parseInt(splitedString[NUMBERS_PART_INDEX]);
+            }
+        }
+        return new int[]{supplySum,buySum};
+    }
+
+    private void writeIntoFile(int[] sums, String fileName) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName))) {
+            String report = SUPPLY_STRING + "," + sums[SUPPLY_PART_INDEX] + System.lineSeparator()
+                    + BUY_STRING + "," + sums[BUY_PART_INDEX] + System.lineSeparator()
+                    + REPORT_STRING + "," + (sums[SUPPLY_PART_INDEX] - sums[BUY_PART_INDEX]);
+            bufferedWriter.write(report);
         } catch (IOException e) {
             throw new RuntimeException("Can't write data to file", e);
         }
+    }
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        writeIntoFile(generateReport(readFromFile(fromFileName)),toFileName);
     }
 }
