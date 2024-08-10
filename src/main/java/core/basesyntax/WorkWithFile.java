f7f619da40d1fe105a459e81348e44f4ab4401fe
Nikita Lafinskiy <niklafinskiy@gmail.com>
2024-08-10 19:03:28 +0200

    split the method into chunks

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,49 +8,64 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    public static final String SUPPLY = "supply";
-    public static final String BUY = "buy";
-    public static final String SEPARATOR = ",";
-    public static final String RESULT = "result";
+    private static final String SEPARATOR = ",";
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+    private static final String RESULT = "result";
+    private static final int STARTING_COUNT = 0;
+    private static final int ELEMENTS_ON_THE_LINE = 2;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName));
-                BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+        String[] data = readData(fromFileName);
+        int[] results = processStatistics(data);
+        writeResults(toFileName, results);
+    }
 
+    private String[] readData(String fromFileName) {
+        StringBuilder data = new StringBuilder();
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             String value = bufferedReader.readLine();
-            int supplyCount = 0;
-            int buyCount = 0;
-
             while (value != null) {
-                String[] currentLine = value.split(SEPARATOR);
-                if (currentLine.length == 2) {
-                    int count = Integer.parseInt(currentLine[1]);
-                    if (currentLine[0].contains(SUPPLY)) {
-                        supplyCount += count;
-                    } else if (currentLine[0].contains(BUY)) {
-                        buyCount += count;
-                    }
-                }
-
+                data.append(value).append(System.lineSeparator());
                 value = bufferedReader.readLine();
             }
-
-            int result = supplyCount - buyCount;
-            bufferedWriter.write(SUPPLY
-                    + ","
-                    + String.valueOf(supplyCount)
-                    + System.lineSeparator());
-            bufferedWriter.write(BUY
-                    + ","
-                    + String.valueOf(buyCount)
-                    + System.lineSeparator());
-            bufferedWriter.write(RESULT
-                    + ","
-                    + String.valueOf(result));
         } catch (FileNotFoundException e) {
             throw new RuntimeException("Was unable to find the file: ", e);
         } catch (IOException e) {
             throw new RuntimeException("Encountered an error while processing this file: ", e);
+        }
+
+        return data.toString().split(System.lineSeparator());
+    }
+
+    private int[] processStatistics(String[] data) {
+        int supplyCount = STARTING_COUNT;
+        int buyCount = STARTING_COUNT;
+
+        for (String value : data) {
+            String[] currentLine = value.split(SEPARATOR);
+            if (currentLine.length == ELEMENTS_ON_THE_LINE) {
+                int count = Integer.parseInt(currentLine[1]);
+                if (currentLine[0].contains(SUPPLY)) {
+                    supplyCount += count;
+                } else if (currentLine[0].contains(BUY)) {
+                    buyCount += count;
+                }
+            }
+        }
+
+        int result = supplyCount - buyCount;
+        return new int[]{supplyCount, buyCount, result};
+    }
+
+    private void writeResults(String toFileName, int[] results) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+            bufferedWriter.write(SUPPLY + SEPARATOR + results[0] + System.lineSeparator());
+            bufferedWriter.write(BUY + SEPARATOR + results[1] + System.lineSeparator());
+            bufferedWriter.write(RESULT + SEPARATOR + results[2]);
+        } catch (IOException e) {
+            throw new RuntimeException("Encountered an error while writing to the file: ", e);
         }
     }
 }
