bf1dfa79d9fd5ee697c4d9ddfe0a1c4bb6b35307
d.nosenko <daniil.nosenkoo@gmail.com>
2024-08-13 20:27:18 +0300

    Add getStatistic Method for CSV Data Processing
    Implemented getStatistic(String fromFileName, String toFileName) method which:
    Reads data from the input CSV file specified by fromFileName.
    Processes the data to generate a report:
    Sums values for operations of type supply and buy.
    Calculates the result as the difference between the total supply and buy.
    Writes the generated report to the file specified by toFileName.
    Added error handling for file reading and writing.

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,62 @@
 package core.basesyntax;
 
-public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
+public class WorkWithFile {
+    private static final int ARRAY_VALUE_INDEX = 1;
+    private static final int ARRAY_NAME_INDEX = 0;
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        String data = readFile(fromFileName);
+        String report = generateReport(data);
+        writeToFile(toFileName, report);
+    }
+
+    private String readFile(String fromFileName) {
+        File fileCopy = new File(fromFileName);
+        StringBuilder stringBuilder = new StringBuilder();
+
+        try (Scanner scanner = new Scanner(fileCopy)) {
+            while (scanner.hasNext()) {
+                stringBuilder.append(scanner.next()).append(System.lineSeparator());
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return stringBuilder.toString();
+    }
+
+    private String generateReport(String string) {
+        int totalSupply = 0;
+        int totalBuy = 0;
+        String[] lines = string.split(System.lineSeparator());
+        for (String item : lines) {
+            String[] array = item.split(",");
+            if (array[ARRAY_NAME_INDEX].equals("supply")) {
+                totalSupply += Integer.parseInt(array[ARRAY_VALUE_INDEX]);
+            } else {
+                totalBuy += Integer.parseInt(array[ARRAY_VALUE_INDEX]);
+            }
+        }
+
+        StringBuilder reportBuilder = new StringBuilder();
+        reportBuilder.append("supply,").append(totalSupply).append(System.lineSeparator());
+        reportBuilder.append("buy,").append(totalBuy).append(System.lineSeparator());
+        reportBuilder.append("result,").append(totalSupply - totalBuy)
+                .append(System.lineSeparator());
+        return reportBuilder.toString();
+    }
+
+    private void writeToFile(String fileName, String report) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName))) {
+            bufferedWriter.write(report);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
