5a738f58e577c76ef09c4e72e7a812f0f4846cdd
d.nosenko <daniil.nosenkoo@gmail.com>
2024-08-14 17:36:04 +0300

    added constants

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,12 +8,12 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    private static final int VALUE_INDEX = 1;
-    private static final int NAME_INDEX = 0;
     public static final String SUPPLY_STRING = "supply";
     public static final String SEPARATION_PLACE = ",";
     public static final String BUY_STRING = "buy";
     public static final String RESULT_STRING = "result";
+    private static final int VALUE_INDEX = 1;
+    private static final int NAME_INDEX = 0;
 
     public void getStatistic(String fromFileName, String toFileName) {
         String data = readFileContent(fromFileName);
@@ -44,16 +44,18 @@ public class WorkWithFile {
             String[] array = item.split(SEPARATION_PLACE);
             if (array[NAME_INDEX].equals(SUPPLY_STRING)) {
                 totalSupply += Integer.parseInt(array[VALUE_INDEX]);
-            } else {
+            } else if (array[NAME_INDEX].equals(BUY_STRING)) {
                 totalBuy += Integer.parseInt(array[VALUE_INDEX]);
             }
         }
 
         StringBuilder reportBuilder = new StringBuilder()
-                .append(SUPPLY_STRING).append(totalSupply).append(System.lineSeparator())
-                .append(BUY_STRING).append(totalBuy).append(System.lineSeparator())
-                .append(RESULT_STRING).append(totalSupply - totalBuy)
+                .append(SUPPLY_STRING).append(SEPARATION_PLACE).append(totalSupply)
+                .append(System.lineSeparator()).append(BUY_STRING).append(SEPARATION_PLACE)
+                .append(totalBuy).append(System.lineSeparator()).append(RESULT_STRING)
+                .append(SEPARATION_PLACE).append(totalSupply - totalBuy)
                 .append(System.lineSeparator());
+
         return reportBuilder.toString();
     }
 
