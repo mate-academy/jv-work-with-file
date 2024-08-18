ad2cfb75c0e277a4f20d95a08b70e219e621422e
Dmytro <dimadukalis@gmail.com>
2024-08-18 19:12:57 +0300

    reworked code

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -9,44 +9,59 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    private static final int DEFAULT_SUM = 0;
+
     public void getStatistic(String fromFileName, String toFileName) {
+        String data = readFromFile(fromFileName);
+        String report = calculateReport(data);
+        writeIntoFile(toFileName, report);
+    }
+
+    private String readFromFile(String fromFileName) {
         File file = new File(fromFileName);
         StringBuilder builder = new StringBuilder();
-        String report;
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
             String value = bufferedReader.readLine();
             while (value != null) {
                 builder.append(value).append(System.lineSeparator());
                 value = bufferedReader.readLine();
             }
-            String[] lines = builder.toString().split(System.lineSeparator());
-            int supplySum = 0;
-            int buySum = 0;
-            for (String line : lines) {
-                String[] fields = line.split(",");
-                if (fields.length == 2) {
-                    String operation = fields[0].trim();
-                    int amount = Integer.parseInt(fields[1].trim());
-                    if (operation.equals("supply")) {
-                        supplySum += amount;
-                    } else if (operation.equals("buy")) {
-                        buySum += amount;
-                    }
-                }
-            }
-            int result = supplySum - buySum;
-
-            report = "supply," + supplySum + System.lineSeparator()
-                    + "buy," + buySum + System.lineSeparator()
-                    + "result," + result;
         } catch (FileNotFoundException e) {
             throw new RuntimeException("Can't find file", e);
         } catch (IOException e) {
             throw new RuntimeException("Can't read file", e);
         }
+        return builder.toString();
+    }
+
+    private String calculateReport(String data) {
+        StringBuilder stringBuilder = new StringBuilder();
+        String[] lines = data.split(System.lineSeparator());
+        int supplySum = DEFAULT_SUM;
+        int buySum = DEFAULT_SUM;
+        for (String line : lines) {
+            String[] fields = line.split(",");
+            if (fields.length == 2) {
+                String operation = fields[0].trim();
+                int amount = Integer.parseInt(fields[1].trim());
+                if (operation.equals("supply")) {
+                    supplySum += amount;
+                } else if (operation.equals("buy")) {
+                    buySum += amount;
+                }
+            }
+        }
+        int result = supplySum - buySum;
+        stringBuilder.append("supply,").append(supplySum).append(System.lineSeparator())
+                .append("buy,").append(buySum).append(System.lineSeparator())
+                .append("result,").append(result);
+        return stringBuilder.toString();
+    }
+
+    private void writeIntoFile(String toFileName, String report) {
         File toFile = new File(toFileName);
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFile))) {
-            bufferedWriter.write(report);
+            bufferedWriter.write(calculateReport(report));
         } catch (IOException e) {
             throw new RuntimeException("Can't write data to file", e);
         }
