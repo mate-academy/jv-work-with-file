ef25a9f591b10896f4541cefca659106daa4945a
StoneBlood <brkalix@gmail.com>
2024-08-30 19:28:16 +0300

    replaced the method getStatistic with separate methods readFile, calculateStatistics, writeFile and getStatistic

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,34 +7,50 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+
     protected void getStatistic(String fromFileName, String toFileName) {
+        String[] data = readFile(fromFileName);
+        int[] results = calculateStatistics(data);
+        writeFile(toFileName, results);
+    }
+
+    private String[] readFile(String fromFileName) {
+        StringBuilder content = new StringBuilder();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
-            String line = bufferedReader.readLine();
-            int supply = 0;
-            int buy = 0;
-
-            while (line != null) {
-                String[] lineContent = line.split(",");
-                if (lineContent[0].equals("supply")) {
-                    supply += Integer.parseInt(lineContent[1]);
-                } else if (lineContent[0].equals("buy")) {
-                    buy += Integer.parseInt(lineContent[1]);
-                }
-                line = bufferedReader.readLine();
-            }
-
-            try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
-                int result = supply - buy;
-                bufferedWriter.write("supply," + supply);
-                bufferedWriter.newLine();
-                bufferedWriter.write("buy," + buy);
-                bufferedWriter.newLine();
-                bufferedWriter.write("result," + result);
-            } catch (IOException e) {
-                throw new RuntimeException("Can't write data to file", e);
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                content.append(line).append(System.lineSeparator());
             }
         } catch (IOException e) {
-            throw new RuntimeException("Can't open file" + fromFileName, e);
+            throw new RuntimeException("Can't open file " + fromFileName, e);
+        }
+        return content.toString().split(System.lineSeparator());
+    }
+
+    private int[] calculateStatistics(String[] data) {
+        int supply = 0;
+        int buy = 0;
+        for (String line : data) {
+            String[] lineContent = line.split(",");
+            if (lineContent[0].equals("supply")) {
+                supply += Integer.parseInt(lineContent[1]);
+            } else if (lineContent[0].equals("buy")) {
+                buy += Integer.parseInt(lineContent[1]);
+            }
+        }
+        int result = supply - buy;
+        return new int[]{supply, buy, result};
+    }
+
+    private void writeFile(String toFileName, int[] results) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+            bufferedWriter.write("supply," + results[0]);
+            bufferedWriter.newLine();
+            bufferedWriter.write("buy," + results[1]);
+            bufferedWriter.newLine();
+            bufferedWriter.write("result," + results[2]);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write data to file", e);
         }
     }
 }
