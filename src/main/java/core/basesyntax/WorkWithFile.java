a6f0c08a591dbe9ae046fe5b0a4a936f8c74859c
Vladyslav Supruniuk <Vladyslav.Supruniuk@contracted.pmi.com>
2024-09-03 12:28:44 +0300

    Fix

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -3,9 +3,12 @@ package core.basesyntax;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 
 public class WorkWithFile {
+    private final String CSV_DATA_SEPARATOR = ",";
+
     public void getStatistic(String fromFileName, String toFileName) {
         int supplyAmount = 0;
         int buyAmount = 0;
@@ -13,16 +16,10 @@ public class WorkWithFile {
         File fromFile = new File(fromFileName);
         File toFile = new File(toFileName);
 
-        List<String> fileData = null;
-
-        try {
-            fileData = Files.readAllLines(fromFile.toPath());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        List<String> fileData = this.readFile(fromFile.toPath());
 
         for (String data : fileData) {
-            String[] values = data.split(",");
+            String[] values = data.split(CSV_DATA_SEPARATOR);
 
             String operation = values[0];
             String amount = values[1];
@@ -36,23 +33,39 @@ public class WorkWithFile {
             }
         }
 
-        int result = supplyAmount - buyAmount;
+        String report = this.createReport(supplyAmount, buyAmount);
 
+        this.writeToFile(toFile.toPath(), report.getBytes());
+    }
+
+    private String createReport(int supplyAmount, int buyAmount) {
         StringBuilder stringBuilder = new StringBuilder()
-                .append("supply,")
-                .append(supplyAmount)
-                .append(System.lineSeparator())
-                .append("buy,")
-                .append(buyAmount)
-                .append(System.lineSeparator())
-                .append("result,")
-                .append(result)
-                .append(System.lineSeparator());
+            .append("supply,")
+            .append(supplyAmount)
+            .append(System.lineSeparator())
+            .append("buy,")
+            .append(buyAmount)
+            .append(System.lineSeparator())
+            .append("result,")
+            .append(supplyAmount - buyAmount)
+            .append(System.lineSeparator());
 
+        return stringBuilder.toString();
+    }
+
+    private List<String> readFile(Path path) {
         try {
-            Files.write(toFile.toPath(), stringBuilder.toString().getBytes());
+            return Files.readAllLines(path);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Error while reading file", e);
+        }
+    }
+
+    private void writeToFile(Path path, byte[] content) {
+        try {
+            Files.write(path, content);
+        } catch (IOException e) {
+            throw new RuntimeException("Error while writing to file", e);
         }
     }
 }
