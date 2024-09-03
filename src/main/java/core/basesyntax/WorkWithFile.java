0719da683da319484bbf8f4ef76e30af82c69925
Artem Lavrivskyi <lavrivskyi@gmail.com>
2024-09-03 23:36:52 +0300

    implemented logic in WorkWithFile class

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,37 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
-
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
+                BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
+            Map<String, Integer> operations = new LinkedHashMap<>();
+            operations.put("supply", 0);
+            operations.put("buy", 0);
+            operations.put("result", 0);
+            String value = reader.readLine();
+            while (value != null) {
+                String[] lineValue = value.split(",");
+                operations.put(lineValue[0],
+                        operations.get(lineValue[0]) + Integer.parseInt(lineValue[1]));
+                value = reader.readLine();
+            }
+            operations.put("result", operations.get("supply") - operations.get("buy"));
+            Set<String> keys = operations.keySet();
+            for (String key : keys) {
+                writer.write(key + "," + operations.get(key));
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file: " + e);
+        }
     }
 }
