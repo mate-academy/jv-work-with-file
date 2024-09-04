a3f6089d92cb703e4d0df4f3d970b96daea8845f
Volodymyr Nyshta <volodymyr.nyshta@gmail.com>
2024-09-05 00:22:30 +0300

    made change according AI comments

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -13,34 +13,46 @@ public class WorkWithFile {
     private static final int AMOUNT = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        List<String> readedFile;
+        String[] values;
+        StringBuilder stringBuilder = new StringBuilder();
         int supplyValue = 0;
         int buyValue = 0;
-        int result = 0;
-        String[] values;
-        File file = new File(fromFileName);
-        try {
-            readedFile = Files.readAllLines(file.toPath());
-            for (String str : readedFile) {
-                values = str.split(",");
+        int resultValue = 0;
+        for (String str : readFromFile(fromFileName)) {
+            values = str.split(",");
+            try {
                 if (values[OPERATION_TYPE].equals(SUPPLY_OPERATION)) {
                     supplyValue += Integer.parseInt(values[AMOUNT]);
                 } else if (values[OPERATION_TYPE].equals(BUY_OPERATION)) {
                     buyValue += Integer.parseInt(values[AMOUNT]);
                 }
+            } catch (NumberFormatException e) {
+                throw new RuntimeException("Failed to convert value to number " + e);
             }
-            result = supplyValue - buyValue;
-        } catch (IOException e) {
-            throw new RuntimeException("Cant read file" + e);
-        } catch (NumberFormatException e) {
-            throw new RuntimeException("Failed to convert value to number" + e);
         }
-        try (FileWriter fileWriter = new FileWriter(new File(toFileName))) {
-            fileWriter.write("supply," + supplyValue + System.lineSeparator());
-            fileWriter.write("buy," + buyValue + System.lineSeparator());
-            fileWriter.write("result," + result + System.lineSeparator());
+        resultValue = supplyValue - buyValue;
+        stringBuilder.append("supply," + supplyValue + System.lineSeparator())
+                        .append("buy," + buyValue + System.lineSeparator())
+                                .append("result," + resultValue + System.lineSeparator());
+        writeToFile(toFileName, stringBuilder.toString());
+    }
+
+    public List<String> readFromFile(String fromFileName) {
+        List<String> linesFromFile;
+        File file = new File(fromFileName);
+        try {
+            linesFromFile = Files.readAllLines(file.toPath());
         } catch (IOException e) {
-            throw new RuntimeException("Cant write data to file" + e);
+            throw new RuntimeException("Cant read file - " + fromFileName + " " + e);
+        }
+        return linesFromFile;
+    }
+
+    public void writeToFile(String toFileName, String content) {
+        try (FileWriter fileWriter = new FileWriter(new File(toFileName))) {
+            fileWriter.write(content);
+        } catch (IOException e) {
+            throw new RuntimeException("Cant write data to file " + toFileName + " " + e);
         }
     }
 }
