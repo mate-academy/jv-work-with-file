6286a91a9fd048169bd5229e8e6e01c41c723bcf
ttikr <ia03.danchuk.volodymyr@gmail.com>
2024-09-04 20:57:09 +0300

    refactored after first attemption

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -12,46 +12,56 @@ public class WorkWithFile {
     private static final String NAME_OF_OPERATION_BUY = "buy";
     private static final int INDEX_OF_OPERATION_TYPE = 0;
     private static final int INDEX_OF_AMOUNT = 1;
+    private int supplyAmount = 0;
+    private int buyAmount = 0;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        int supplyAmount = 0;
-        int buyAmount = 0;
+
+        readAmountsOfOperations(fromFileName);
+
+        String report = createReportMessage();
+        File fileToWrite = new File(toFileName);
+
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileToWrite))) {
+            bufferedWriter.write(report);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write to file", e);
+        }
+
+        setDefaultAmountsValuesAfterWriteReport();
+    }
+
+    private void readAmountsOfOperations(String fromFileName) {
         File file = new File(fromFileName);
 
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             String value = reader.readLine();
-            String[] temp;
+            String[] lineFromInputfile;
             while (value != null) {
-                temp = value.split(",");
-                String operationType = temp[INDEX_OF_OPERATION_TYPE];
-                int amount = Integer.parseInt(temp[INDEX_OF_AMOUNT]);
+                lineFromInputfile = value.split(",");
+                String operationType = lineFromInputfile[INDEX_OF_OPERATION_TYPE];
+                int amount = Integer.parseInt(lineFromInputfile[INDEX_OF_AMOUNT]);
                 if (operationType.equals("supply")) {
                     supplyAmount += amount;
                 } else if (operationType.equals("buy")) {
                     buyAmount += amount;
                 }
-
                 value = reader.readLine();
             }
         } catch (IOException e) {
             throw new RuntimeException("Can't read file", e);
         }
+    }
 
-        StringBuilder resultString = new StringBuilder();
-        resultString.append(NAME_OF_OPERATION_SUPPLY).append(",").append(supplyAmount)
-                .append(System.lineSeparator());
-
-        resultString.append(NAME_OF_OPERATION_BUY).append(",").append(buyAmount)
-                .append(System.lineSeparator());
+    private String createReportMessage() {
         int resultAmount = supplyAmount - buyAmount;
-        resultString.append("result,").append(resultAmount).append(System.lineSeparator());
+        return NAME_OF_OPERATION_SUPPLY + "," + supplyAmount + System.lineSeparator()
+                + NAME_OF_OPERATION_BUY + "," + buyAmount + System.lineSeparator()
+                + "result," + resultAmount + System.lineSeparator();
+    }
 
-        File fileToWrite = new File(toFileName);
-
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileToWrite))) {
-            bufferedWriter.write(resultString.toString());
-        } catch (IOException e) {
-            throw new RuntimeException("Can't write to file", e);
-        }
+    private void setDefaultAmountsValuesAfterWriteReport() {
+        supplyAmount = 0;
+        buyAmount = 0;
     }
 }
