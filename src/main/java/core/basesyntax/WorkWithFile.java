c539997822f92e0b96f7d81f82f2ce0394305e44
Oleksandr <oleksandrshtonda>
2024-09-11 15:41:52 +0200

    fixed code according to the comments of the mentor

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -5,9 +5,23 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 public class WorkWithFile {
+    private static final String SUPPLY_STR = "supply";
+    private static final String BUY_STR = "buy";
+    private static final String RESULT_STR = "result";
+    private static final String COMMA = ",";
+    private static final String LINE_SEPARATOR = System.lineSeparator();
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        ArrayList<String> data = readFile(fromFileName);
+        String operations = createReport(data);
+        writeDataToFile(toFileName, operations);
+    }
+
     private ArrayList<String> readFile(String fileName) {
         File fileToRead = new File(fileName);
 
@@ -25,9 +39,11 @@ public class WorkWithFile {
         }
     }
 
-    private String[] createReport(ArrayList<String> operationsList) {
+    private String createReport(ArrayList<String> operationsList) {
+
         int supplyOperations = 0;
         int buyOperations = 0;
+        StringBuilder builder = new StringBuilder();
 
         for (String operation : operationsList) {
             String[] partsOfOperation = operation.split(",");
@@ -41,32 +57,28 @@ public class WorkWithFile {
             }
         }
 
-        return new String[]{
-                "supply," + supplyOperations,
-                "buy," + buyOperations,
-                "result," + (supplyOperations - buyOperations)
-        };
+        builder.append(SUPPLY_STR)
+                .append(COMMA)
+                .append(supplyOperations)
+                .append(LINE_SEPARATOR);
+        builder.append(BUY_STR)
+                .append(COMMA)
+                .append(buyOperations)
+                .append(LINE_SEPARATOR);
+        builder.append(RESULT_STR)
+                .append(COMMA)
+                .append(supplyOperations - buyOperations)
+                .append(LINE_SEPARATOR);
+
+        return builder.toString();
     }
 
-    private void writeDataToFile(String fileName, String[] operations) {
-        File file = new File(fileName);
+    private void writeDataToFile(String fileName, String operations) {
         try {
-            file.createNewFile();
-        } catch (IOException e) {
-            throw new RuntimeException("Can't create a file: ", e);
-        }
-
-        try {
-            String joinedOperations = String.join(System.lineSeparator(), operations);
-            Files.write(file.toPath(), joinedOperations.getBytes());
+            Path path = Paths.get(fileName);
+            Files.write(path, operations.getBytes());
         } catch (IOException e) {
             throw new RuntimeException("Can't write data to the file: ", e);
         }
-    }
-
-    public void getStatistic(String fromFileName, String toFileName) {
-        ArrayList<String> data = readFile(fromFileName);
-        String[] operations = createReport(data);
-        writeDataToFile(toFileName, operations);
     }
 }
