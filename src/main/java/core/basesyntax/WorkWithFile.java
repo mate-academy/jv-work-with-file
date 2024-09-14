e8e3f56990acb06930eeaf285ca44277480d1264
clo3eX <pansydaisy83@gmail.com>
2024-09-14 12:09:39 +0200

    added solution

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,39 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
+        int totalSupply = 0;
+        int totalBuy = 0;
 
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] data = line.split(",");
+                String operation = data[0];
+                int amount = Integer.parseInt(data[1]);
+
+                if (operation.equals("supply")) {
+                    totalSupply += amount;
+                } else if (operation.equals("buy")) {
+                    totalBuy += amount;
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t read the file");
+        }
+        int result = totalSupply - totalBuy;
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+            bufferedWriter.write("supply," + totalSupply + System.lineSeparator());
+            bufferedWriter.write("buy," + totalBuy + System.lineSeparator());
+            bufferedWriter.write("result," + result + System.lineSeparator());
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t write to file");
+        }
     }
 }
