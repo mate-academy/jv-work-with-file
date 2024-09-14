6614c27d09377d255e4f5b565995f3da792dc073
Lilia <liliazuravel6@gmail.com>
2024-09-14 12:42:16 +0300

    Divided program logic

---
+++ src/main/java/core/basesyntax/Counters.java
@@ -0,0 +1,11 @@
+package core.basesyntax;
+
+public class Counters {
+    protected int supplyCounter;
+    protected int buyCounter;
+
+    public Counters(int supplyCounter, int buyCounter) {
+        this.supplyCounter = supplyCounter;
+        this.buyCounter = buyCounter;
+    }
+}
--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,6 @@
 package core.basesyntax;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -9,22 +8,39 @@ import java.io.IOException;
 public class WorkWithFile {
     public static final int OPERATION_TYPE = 0;
     public static final int AMOUNT = 1;
+    public static final String OPERATION_SUPPLY = "supply";
+    public static final String OPERATION_BUY = "buy";
+    public static final String OPERATION_RESULT = "result";
+    public static final String COMMA = ",";
 
     public void getStatistic(String fromFileName, String toFileName) throws IOException {
-        int supplyCounter = 0;
-        int buyCounter = 0;
+        Counters counters = new Counters(0,0);
+        int result = readFromFile(fromFileName, counters);
 
-        try (FileReader fileReader = new FileReader(fromFileName)) {
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
+        StringBuilder data = new StringBuilder(OPERATION_SUPPLY
+                + COMMA + counters.supplyCounter + System.lineSeparator()
+                + OPERATION_BUY + COMMA + counters.buyCounter + System.lineSeparator()
+                + OPERATION_RESULT + COMMA + result + System.lineSeparator());
+
+        try (FileWriter fileWriter = new FileWriter(toFileName)) {
+            fileWriter.write(data.toString());
+        } catch (IOException e) {
+            throw new IOException("can not write to file", e);
+        }
+    }
+
+    public int readFromFile(String fromFileName, Counters counters)
+            throws IOException {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             String line;
             while ((line = bufferedReader.readLine()) != null) {
-                String[] dataFromFile = line.split(",");
+                String[] dataFromFile = line.split(COMMA);
                 int amount = Integer.parseInt(dataFromFile[AMOUNT]);
 
-                if (dataFromFile[OPERATION_TYPE].equals("supply")) {
-                    supplyCounter += amount;
+                if (dataFromFile[OPERATION_TYPE].equals(OPERATION_SUPPLY)) {
+                    counters.supplyCounter += amount;
                 } else {
-                    buyCounter += amount;
+                    counters.buyCounter += amount;
                 }
 
             }
@@ -32,19 +48,6 @@ public class WorkWithFile {
             throw new IOException("File can not be open", e);
         }
 
-        String[][] data = {{"supply,", String.valueOf(supplyCounter)},
-                           {"buy,", String.valueOf(buyCounter)},
-                           {"result,", String.valueOf(supplyCounter - buyCounter)}};
-
-        File newFile = new File(toFileName);
-
-        try (FileWriter fileWriter = new FileWriter(newFile)) {
-            for (String[] string : data) {
-                fileWriter.write(string[OPERATION_TYPE] + string[AMOUNT]);
-                fileWriter.write(System.lineSeparator());
-            }
-        } catch (IOException e) {
-            throw new IOException("can not write to file", e);
-        }
+        return counters.supplyCounter - counters.buyCounter;
     }
 }
