1c89af8b21b91fbbefaaa4d949b498504169d0e0
Ihor Tkachuk <iono19031@gmail.com>
2024-09-19 14:20:23 +0300

    added generateReport() method, changed names of variables, fixed bugs after review

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -2,61 +2,63 @@ package core.basesyntax;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    private static final String SUPPLY = "supply";
-    private static final String BUY = "buy";
-    private static final int ONE_ARRAY_INDEX = 1;
-
-    public WorkWithFile() {
-    }
+    private static final String SUPPLY_OPERATION_TYPE = "supply";
+    private static final String BUY_OPERATION_TYPE = "buy";
+    private static final String COMMA = ",";
+    private static final int ARRAY_INDEX_FOR_AMOUNT = 1;
+    private static final int ARRAY_INDEX_FOR_TOTAL_SUPPLY_AMOUNT = 0;
+    private static final int ARRAY_INDEX_FOR_TOTAL_BUY_AMOUNT = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        String statistic = readFromFile(fromFileName);
+        int[] supplyAndBuyAmounts = readFromFile(fromFileName);
+        String statistic = generateReport(supplyAndBuyAmounts);
         writeToFile(toFileName, statistic);
     }
 
     private void writeToFile(String toFileName, String statistic) {
-        File myFile = new File(toFileName);
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(myFile))) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
             bufferedWriter.write(statistic);
         } catch (IOException e) {
-            throw new RuntimeException("Can't write data to file " + myFile, e);
+            throw new RuntimeException("Can't write data to file " + toFileName, e);
         }
     }
 
-    private String readFromFile(String fromFileName) {
-        File file = new File(fromFileName);
-        int supplyCounter = 0;
-        int buyCounter = 0;
+    private int[] readFromFile(String fromFileName) {
+        int supplyAmount = 0;
+        int buyAmount = 0;
 
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             String value = bufferedReader.readLine();
             while (value != null) {
-                String stringNumber = value.split(",")[ONE_ARRAY_INDEX];
-                if (value.startsWith(SUPPLY)) {
-                    supplyCounter += Integer.parseInt(stringNumber);
+                String stringNumber = value.split(COMMA)[ARRAY_INDEX_FOR_AMOUNT];
+                if (value.startsWith(SUPPLY_OPERATION_TYPE)) {
+                    supplyAmount += Integer.parseInt(stringNumber);
                 }
-                if (value.startsWith(BUY)) {
-                    buyCounter += Integer.parseInt(stringNumber);
+                if (value.startsWith(BUY_OPERATION_TYPE)) {
+                    buyAmount += Integer.parseInt(stringNumber);
                 }
                 value = bufferedReader.readLine();
             }
         } catch (IOException e) {
-            throw new RuntimeException("Can't read data from file " + file, e);
+            throw new RuntimeException("Can't read data from file " + fromFileName, e);
         } catch (NullPointerException e) {
-            throw new RuntimeException("Data not found in file " + file, e);
+            throw new RuntimeException("Data not found in file " + fromFileName, e);
         }
-        return report(supplyCounter, buyCounter);
+        return new int[]{supplyAmount, buyAmount};
     }
 
-    private String report(int supplyCounter, int buyCounter) {
-        return "supply," + supplyCounter + System.lineSeparator()
-                + "buy," + buyCounter + System.lineSeparator()
-                + "result," + (supplyCounter - buyCounter);
+    private String generateReport(int[] supplyAndBuyAmounts) {
+        return "supply,"
+                + supplyAndBuyAmounts[ARRAY_INDEX_FOR_TOTAL_SUPPLY_AMOUNT]
+                + System.lineSeparator()
+                + "buy," + supplyAndBuyAmounts[ARRAY_INDEX_FOR_TOTAL_BUY_AMOUNT]
+                + System.lineSeparator() + "result,"
+                + (supplyAndBuyAmounts[ARRAY_INDEX_FOR_TOTAL_SUPPLY_AMOUNT]
+                - supplyAndBuyAmounts[ARRAY_INDEX_FOR_TOTAL_BUY_AMOUNT]);
     }
 }
