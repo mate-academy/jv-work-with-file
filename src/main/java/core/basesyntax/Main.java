69b6dcf992b5304fbbe1446522fc088dc1be8d5c
Olha Oleshchuk <olha.oleshchuk@gmail.com>
2024-09-20 14:48:57 +0300

    work with file done

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -5,11 +5,14 @@ public class Main {
         String[] inputFiles = {"apple.csv", "banana.csv", "grape.csv", "orange.csv"};
         String[] outputFiles = {"newFile1.txt", "newFile2.txt", "newFile3.txt", "newFile4.txt"};
         WorkWithFile workWithFile = new WorkWithFile();
+        String[] results = new String[inputFiles.length];
+
         for (int i = 0; i < inputFiles.length; i++) {
             String fromFile = inputFiles[i];
             String newFile = outputFiles[i];
             workWithFile.writeStatistic(fromFile, newFile);
-            System.out.println(workWithFile.countStatistic(inputFiles[i]).toString());
+            results[i] = workWithFile.getStatistic(fromFile, newFile);
+            System.out.println(results[i]);
         }
     }
 }
