07416756519656271116cb37068273de33beb4f2
Olha Oleshchuk <olha.oleshchuk@gmail.com>
2024-09-21 23:09:38 +0300

    work with file done

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -9,16 +9,40 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class WorkWithFile {
+    private static final String BUY_CONSTANT = "buy,";
+    private static final String COMMA = ",";
+    private static final String RESULT_CONSTANT = "result,";
+    private static final String SUPPLY_CONSTANT = "supply,";
     private final Map<String, String> statisticsCache = new HashMap<>();
 
-    public int[] readStatistic(String fromFileName) {
+    public void writeStatistic(String fromFileName, String newFile) {
+        String result = countStatistic(fromFileName);
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(newFile))) {
+            bufferedWriter.write(result);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write file", e);
+        }
+    }
+
+    public String getStatistic(String fromFileName, String newFile) {
+        if (statisticsCache.containsKey(fromFileName)) {
+            return statisticsCache.get(fromFileName);
+        }
+
+        writeStatistic(fromFileName, newFile);
+        String result = countStatistic(fromFileName);
+        statisticsCache.put(fromFileName, result);
+        return result;
+    }
+
+    private int[] readStatistic(String fromFileName) {
         int supply = 0;
         int buy = 0;
 
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             String value = bufferedReader.readLine();
             while (value != null) {
-                String[] split = value.split(",");
+                String[] split = value.split(COMMA);
                 if (split[0].equals("supply")) {
                     supply += Integer.parseInt(split[1]);
                 } else if (split[0].equals("buy")) {
@@ -32,42 +56,15 @@ public class WorkWithFile {
         return new int[]{supply, buy};
     }
 
-    public StringBuilder countStatistic(String fromFileName) {
+    private String countStatistic(String fromFileName) {
         int[] stats = readStatistic(fromFileName);
         int supply = stats[0];
         int buy = stats[1];
         int countedResult = supply - buy;
 
         StringBuilder result = new StringBuilder();
-        result.append("supply,");
-        result.append(supply);
-        result.append("\r\n");
-        result.append("buy,");
-        result.append(buy);
-        result.append("\r\n");
-        result.append("result,");
-        result.append(countedResult);
-
-        return result;
-    }
-
-    public void writeStatistic(String fromFileName, String newFile) {
-        StringBuilder result = countStatistic(fromFileName);
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(newFile))) {
-            bufferedWriter.write(result.toString());
-        } catch (IOException e) {
-            throw new RuntimeException("Can't write file", e);
-        }
-    }
-
-    public String getStatistic(String fromFileName, String newFile) {
-        if (statisticsCache.containsKey(fromFileName)) {
-            return statisticsCache.get(fromFileName);
-        }
-
-        writeStatistic(fromFileName, newFile);
-        String result = countStatistic(fromFileName).toString();
-        statisticsCache.put(fromFileName, result);
-        return result;
+        return result.append(SUPPLY_CONSTANT).append(supply).append(System.lineSeparator())
+                .append(BUY_CONSTANT).append(buy).append(System.lineSeparator())
+                .append(RESULT_CONSTANT).append(countedResult).toString();
     }
 }
