ca5b1c07c11f6bfdd55d2a4e34a3978c057728cd
Olha Oleshchuk <olha.oleshchuk@gmail.com>
2024-09-23 14:34:12 +0300

    okuzan's remarks done

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -9,10 +9,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class WorkWithFile {
-    private static final String BUY_CONSTANT = "buy,";
+    private static final String BUY_CONSTANT = "buy";
     private static final String COMMA = ",";
-    private static final String RESULT_CONSTANT = "result,";
-    private static final String SUPPLY_CONSTANT = "supply,";
+    private static final String RESULT_CONSTANT = "result";
+    private static final String SUPPLY_CONSTANT = "supply";
+    private static final int OPERATION_INDEX = 0;
+    private static final int AMOUNT_INDEX = 1;
     private final Map<String, String> statisticsCache = new HashMap<>();
 
     public void writeStatistic(String fromFileName, String newFile) {
@@ -43,28 +45,29 @@ public class WorkWithFile {
             String value = bufferedReader.readLine();
             while (value != null) {
                 String[] split = value.split(COMMA);
-                if (split[0].equals("supply")) {
-                    supply += Integer.parseInt(split[1]);
-                } else if (split[0].equals("buy")) {
-                    buy += Integer.parseInt(split[1]);
+                if (split[OPERATION_INDEX].equals(SUPPLY_CONSTANT)) {
+                    supply += Integer.parseInt(split[AMOUNT_INDEX]);
+                } else if (split[OPERATION_INDEX].equals(BUY_CONSTANT)) {
+                    buy += Integer.parseInt(split[AMOUNT_INDEX]);
                 }
                 value = bufferedReader.readLine();
             }
         } catch (IOException e) {
-            throw new RuntimeException("Can't read file", e);
+            throw new RuntimeException("Can't read file: " + fromFileName, e);
         }
         return new int[]{supply, buy};
     }
 
     private String countStatistic(String fromFileName) {
         int[] stats = readStatistic(fromFileName);
-        int supply = stats[0];
-        int buy = stats[1];
+        int supply = stats[OPERATION_INDEX];
+        int buy = stats[AMOUNT_INDEX];
         int countedResult = supply - buy;
 
         StringBuilder result = new StringBuilder();
-        return result.append(SUPPLY_CONSTANT).append(supply).append(System.lineSeparator())
-                .append(BUY_CONSTANT).append(buy).append(System.lineSeparator())
-                .append(RESULT_CONSTANT).append(countedResult).toString();
+        return result.append(SUPPLY_CONSTANT).append(COMMA).append(supply)
+                .append(System.lineSeparator()).append(BUY_CONSTANT).append(COMMA)
+                .append(buy).append(System.lineSeparator()).append(RESULT_CONSTANT)
+                .append(COMMA).append(countedResult).toString();
     }
 }
