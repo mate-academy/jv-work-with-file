beb3f5d56aef5ce2911af1bd8c3c87872774bf5a
Ostap <ostzat@gmail.com>
2024-09-24 16:32:11 +0200

    Fix

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -10,40 +10,23 @@ public class WorkWithFile {
     public static final String SUPPLY = "supply";
     public static final String BUY = "buy";
     public static final String RESULT = "result";
-    private int supplyAmount = 0;
-    private int buyAmount = 0;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        StringBuilder stringBuilder = new StringBuilder();
-
-        supplyAmount = 0;
-        buyAmount = 0;
-        File fromFile = new File(fromFileName);
-        final File toFile = new File(toFileName);
-
-        calculateReport(fromFile, stringBuilder);
-
-        writeIntoFile(toFile, stringBuilder);
+        int[] output = readFile(fromFileName);
+        String result = getResult(output);
+        writeToFile(result, toFileName);
     }
 
-    private void calculateReport(File fromFile, StringBuilder stringBuilder) {
-
-        readFromFile(fromFile);
-
-        stringBuilder.append(SUPPLY).append(COMMA).append(supplyAmount)
-                .append(System.lineSeparator())
-                .append(BUY).append(COMMA).append(buyAmount)
-                .append(System.lineSeparator())
-                .append(RESULT).append(COMMA).append(supplyAmount - buyAmount);
-    }
-
-    private void readFromFile(File fromFile) {
+    private int[] readFile(String fromFileName) {
         List<String> fileData;
         try {
-            fileData = Files.readAllLines(fromFile.toPath());
+            fileData = Files.readAllLines(new File(fromFileName).toPath());
         } catch (IOException e) {
             throw new RuntimeException("Can't read from file", e);
         }
+
+        int supplyAmount = 0;
+        int buyAmount = 0;
 
         for (String data : fileData) {
             String[] values = data.split(COMMA);
@@ -59,19 +42,35 @@ public class WorkWithFile {
                 int value = Integer.parseInt(amountString);
                 if (operation.equals(BUY)) {
                     buyAmount += value;
-                } else {
+                } else if (operation.equals(SUPPLY)) {
                     supplyAmount += value;
                 }
             } catch (NumberFormatException e) {
                 throw new RuntimeException("Invalid number format in file: " + amountString);
             }
         }
+
+        return new int[]{supplyAmount, buyAmount};
     }
 
-    private void writeIntoFile(File toFile, StringBuilder stringBuilder) {
+    private String getResult(int[] output) {
+        int supplyAmount = output[0];
+        int buyAmount = output[1];
+        int resultAmount = supplyAmount - buyAmount;
 
+        return new StringBuilder()
+                .append(SUPPLY).append(COMMA).append(supplyAmount)
+                .append(System.lineSeparator())
+                .append(BUY).append(COMMA).append(buyAmount)
+                .append(System.lineSeparator())
+                .append(RESULT).append(COMMA).append(resultAmount)
+                .toString();
+    }
+
+    private void writeToFile(String result, String toFileName) {
+        File toFile = new File(toFileName);
         try {
-            Files.write(toFile.toPath(), stringBuilder.toString().getBytes());
+            Files.write(toFile.toPath(), result.getBytes());
         } catch (IOException e) {
             throw new RuntimeException("Can't write to the file", e);
         }
