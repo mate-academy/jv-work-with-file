98003560de809eecbb8d5cc02ba1d4ec061332ac
Sergii Konovalov <sergii2konovalov@gmail.com>
2024-09-25 13:46:59 +0300

    fixed solution

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,6 +7,8 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    private static final String DELIMITER = ",";
+
     public void getStatistic(String fromFileName, String toFileName) {
         int amountBuy = 0;
         int amountSupply = 0;
@@ -14,9 +16,9 @@ public class WorkWithFile {
         try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
             String line;
             while ((line = reader.readLine()) != null) {
-                String[] parts = line.split(",");
-                String operationType = parts[0];
-                int amount = Integer.parseInt(parts[1]);
+                String[] parts = line.split(DELIMITER);
+                String operationType = parts[0].trim();
+                int amount = Integer.parseInt(parts[1].trim());
 
                 if (operationType.equals("buy")) {
                     amountBuy += amount;
@@ -31,9 +33,14 @@ public class WorkWithFile {
         int result = amountSupply - amountBuy;
 
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
-            bufferedWriter.write("supply," + amountSupply + System.lineSeparator());
-            bufferedWriter.write("buy," + amountBuy + System.lineSeparator());
-            bufferedWriter.write("result," + result);
+            StringBuilder stringBuilder = new StringBuilder();
+            bufferedWriter.write(stringBuilder.append("supply,").append(amountSupply)
+                    .append(System.lineSeparator()).toString());
+            stringBuilder.setLength(0);
+            bufferedWriter.write(stringBuilder.append("buy,").append(amountBuy)
+                    .append(System.lineSeparator()).toString());
+            stringBuilder.setLength(0);
+            bufferedWriter.write(stringBuilder.append("result,").append(result).toString());
         } catch (IOException e) {
             throw new RuntimeException("Can't write to the file", e);
         }
