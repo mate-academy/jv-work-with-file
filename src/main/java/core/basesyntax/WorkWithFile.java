d71a21540b6af4ffcd5c4eb3d900793f14cbb097
DariaBakal <bakal.dasha01@gmail.com>
2024-09-26 19:16:48 +0300

    implement getStatistic method

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -12,12 +12,53 @@ public class WorkWithFile {
     private static final String LINE_DELIMITER = System.lineSeparator();
 
     public void getStatistic(String fromFileName, String toFileName) {
-        String report = formReport(fromFileName);
+        String report = formReport(readFile(fromFileName));
         writeToFile(toFileName, report);
-
     }
 
-    public void writeToFile(String toFileName, String formedReport) {
+    private String[] readFile(String fromFileName) {
+        File sourceFile = new File(fromFileName);
+        StringBuilder stringBuilder = new StringBuilder();
+        String[] data;
+        try {
+            String fileContent = Files.readString(sourceFile.toPath());
+            data = fileContent.toLowerCase().split(LINE_DELIMITER);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read data from the file " + sourceFile, e);
+        }
+        return data;
+    }
+
+    private String formReport(String[] data) {
+        StringBuilder stringBuilder = new StringBuilder();
+        int supplySum = 0;
+        int buySum = 0;
+        for (String string : data) {
+            String operationType = string.split(COMMA_DELIMITER)[0];
+            int amount = Integer.parseInt(string.split(COMMA_DELIMITER)[1]);
+            if (operationType.equals("supply")) {
+                supplySum += amount;
+            } else {
+                buySum += amount;
+            }
+        }
+        int result = supplySum - buySum;
+        stringBuilder.append("supply")
+                .append(COMMA_DELIMITER)
+                .append(supplySum)
+                .append(LINE_DELIMITER)
+                .append("buy")
+                .append(COMMA_DELIMITER)
+                .append(buySum)
+                .append(LINE_DELIMITER)
+                .append("result")
+                .append(COMMA_DELIMITER)
+                .append(result);
+
+        return stringBuilder.toString();
+    }
+
+    private void writeToFile(String toFileName, String formedReport) {
         File targetFile = new File(toFileName);
         Path path = Paths.get(toFileName);
         try {
@@ -25,40 +66,5 @@ public class WorkWithFile {
         } catch (IOException e) {
             throw new RuntimeException("Can't write data in the file " + targetFile, e);
         }
-    }
-
-    public String formReport(String fromFileName) {
-        File sourceFile = new File(fromFileName);
-        StringBuilder stringBuilder = new StringBuilder();
-        try {
-            String fileContent = Files.readString(sourceFile.toPath());
-            String[] data = fileContent.toLowerCase().split(LINE_DELIMITER);
-            int supplySum = 0;
-            int buySum = 0;
-            for (String string : data) {
-                String operationType = string.split(COMMA_DELIMITER)[0];
-                int amount = Integer.parseInt(string.split(COMMA_DELIMITER)[1]);
-                if (operationType.equals("supply")) {
-                    supplySum += amount;
-                } else {
-                    buySum += amount;
-                }
-            }
-            int result = supplySum - buySum;
-            stringBuilder.append("supply")
-                    .append(COMMA_DELIMITER)
-                    .append(supplySum)
-                    .append(LINE_DELIMITER)
-                    .append("buy")
-                    .append(COMMA_DELIMITER)
-                    .append(buySum)
-                    .append(LINE_DELIMITER)
-                    .append("result")
-                    .append(COMMA_DELIMITER)
-                    .append(result);
-        } catch (IOException e) {
-            throw new RuntimeException("Can't read data from the file " + sourceFile, e);
-        }
-        return stringBuilder.toString();
     }
 }
