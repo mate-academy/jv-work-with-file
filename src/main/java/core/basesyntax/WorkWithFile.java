bbda5730436fe602c4f90c0670e4216025a152e8
kupnovytskyi <kupnovytskyinazar@gmail.com>
2024-09-26 18:19:30 +0300

    fixed comments

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -4,18 +4,23 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class WorkWithFile {
-
     private static final String OPERATION_TYPE_BUY = "buy";
     private static final String OPERATION_TYPE_SUPPLY = "supply";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        List<String> stringFromFile = new ArrayList<>();
+        List<String> records = new ArrayList<>();
         StringBuilder report = new StringBuilder();
+        readFromFile(fromFileName, records, report);
+        writeToFile(toFileName, report);
+    }
 
+    private static void readFromFile(String fromFileName,
+                                     List<String> stringFromFile, StringBuilder report) {
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             String read = bufferedReader.readLine();
             while (read != null) {
@@ -27,13 +32,14 @@ public class WorkWithFile {
 
             for (String s : stringFromFile) {
                 String[] split = s.split(",");
-                int digit = Integer.decode(split[1]);
+                int digit = Integer.parseInt(split[1]);
                 if (split[0].equals(OPERATION_TYPE_BUY)) {
                     buy += digit;
                 } else if (split[0].equals(OPERATION_TYPE_SUPPLY)) {
                     supply += digit;
                 }
             }
+
             int result = supply - buy;
             report.append("supply,")
                     .append(supply)
@@ -44,16 +50,16 @@ public class WorkWithFile {
                     .append("result,")
                     .append(result);
 
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to read from file: " + fromFileName, e);
         }
+    }
 
+    private static void writeToFile(String toFileName, StringBuilder report) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
-            for (int i = 0; i < report.length(); i++) {
-                bufferedWriter.write(report.charAt(i));
-            }
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+            bufferedWriter.write(report.toString());
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to write from file: " + toFileName, e);
         }
     }
 }
