9bbedb34fa96813e1517c2d113858b20fa0bee60
Roman <romanvoynahiy@gmail.com>
2024-09-26 17:27:56 +0300

    Refactor WorkWithFile: - Split geStatistic into two methods (calculateTotalValue and writeReport) - Improve exception messages for better clarity and debugging - Changed output writing to use StringBuilder

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -10,45 +10,54 @@ import java.io.IOException;
 
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
-        File inputFile = new File(fromFileName);
-        int totalSupplyAmount = 0;
-        int totalBuyAmount = 0;
+        File inputCsvFile = new File(fromFileName);
+        int totalSupplyAmount = calculateTotalValue(inputCsvFile, "supply");
+        int totalBuyAmount = calculateTotalValue(inputCsvFile, "buy");
+        writeReport(toFileName, totalSupplyAmount, totalBuyAmount);
+    }
 
+    private int calculateTotalValue(File inputFile, String operationType) {
+        int totalValue = 0;
         try (BufferedReader fileReader = new BufferedReader(new FileReader(inputFile))) {
             String lineFromFile;
 
             while ((lineFromFile = fileReader.readLine()) != null) {
                 String[] elements = lineFromFile.split(",");
 
-                if (elements[0].equals("supply")) {
-                    totalSupplyAmount += Integer.parseInt(elements[1]);
-                } else if (elements[0].equals("buy")) {
-                    totalBuyAmount += Integer.parseInt(elements[1]);
+                if (elements[0].equals(operationType)) {
+                    totalValue += Integer.parseInt(elements[1]);
                 }
             }
         } catch (FileNotFoundException e) {
-            throw new RuntimeException("Can't find file" + e);
+            throw new RuntimeException("Can't find file: " + inputFile.getName() + e);
         } catch (IOException e) {
-            throw new RuntimeException("Can't read file" + e);
+            throw new RuntimeException("Can't read data from file:" + inputFile.getName() + e);
         }
+        return totalValue;
+    }
 
+    private void writeReport(String toFileName, int totalSupplyAmount, int totalBuyAmount) {
         File outputFile = new File(toFileName);
         try {
-            if (outputFile.exists()) {
-                outputFile.delete();
-            }
             outputFile.createNewFile();
         } catch (IOException e) {
-            throw new RuntimeException("Can't create new file" + e);
+            throw new RuntimeException("Can't create new file:" + outputFile.getName() + e);
         }
 
         try (BufferedWriter outputFileWriter = new BufferedWriter(new FileWriter(outputFile))) {
-            outputFileWriter.write("supply," + totalSupplyAmount + System.lineSeparator());
-            outputFileWriter.write("buy," + totalBuyAmount + System.lineSeparator());
-            outputFileWriter.write("result," + (totalSupplyAmount - totalBuyAmount)
-                    + System.lineSeparator());
+            StringBuilder finalOutput = new StringBuilder();
+            finalOutput.append("supply,")
+                    .append(totalSupplyAmount)
+                    .append(System.lineSeparator())
+                    .append("buy,")
+                    .append(totalBuyAmount)
+                    .append(System.lineSeparator())
+                    .append("result,")
+                    .append(totalSupplyAmount - totalBuyAmount)
+                    .append(System.lineSeparator());
+            outputFileWriter.write(finalOutput.toString());
         } catch (IOException e) {
-            throw new RuntimeException("Can't write data to file" + e);
+            throw new RuntimeException("Can't write data to file: " + outputFile.getName() + e);
         }
     }
 }
