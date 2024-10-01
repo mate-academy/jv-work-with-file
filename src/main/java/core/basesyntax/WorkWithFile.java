e23260386a12b2ecfd55910419562dac186dc50f
Kate Kraska <kate2002.kk42@gmail.com>
2024-10-01 20:24:39 +0300

    fix problem after review

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,12 +8,17 @@ import java.io.IOException;
 
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
+        int[] supplyAndBuy = readData(fromFileName);
+        int result = processStatistics(supplyAndBuy[0], supplyAndBuy[1]);
+        writeData(toFileName, supplyAndBuy[0], supplyAndBuy[1], result);
+    }
+
+    private int[] readData(String fromFileName) {
         int totalSupply = 0;
         int totalBuy = 0;
 
         try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
             String line;
-
             while ((line = reader.readLine()) != null) {
                 String[] data = line.split(",");
                 String operation = data[0];
@@ -26,17 +31,26 @@ public class WorkWithFile {
                 }
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException("Error reading from file: " + fromFileName, e);
         }
 
-        int result = totalSupply - totalBuy;
+        return new int[]{totalSupply, totalBuy};
+    }
+
+    private int processStatistics(int totalSupply, int totalBuy) {
+        return totalSupply - totalBuy;
+    }
+
+    private void writeData(String toFileName, int totalSupply, int totalBuy, int result) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("supply,").append(totalSupply).append(System.lineSeparator());
+        sb.append("buy,").append(totalBuy).append(System.lineSeparator());
+        sb.append("result,").append(result).append(System.lineSeparator());
 
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
-            writer.write("supply," + totalSupply + System.lineSeparator());
-            writer.write("buy," + totalBuy + System.lineSeparator());
-            writer.write("result," + result + System.lineSeparator());
+            writer.write(sb.toString());
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException("Error writing to file: " + toFileName, e);
         }
     }
 }
