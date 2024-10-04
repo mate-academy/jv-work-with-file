6ab38332f9fe9e7fab46f92752481d96e7f030b3
Heorhii Tsarenko <heorhii.tsarenko@gmail.com>
2024-10-04 19:46:31 +0300

    now method readData only read clear magic numbers change methods logic

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -5,7 +5,6 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Arrays;
 
 public class WorkWithFile {
     private static final String LINE_SEPARATOR = System.lineSeparator();
