23f1118e3b8afde92807d91d4adf40eb6a312dc0
Nikita Chaikovskyi <cha9n.1488@gmail.com>
2024-10-08 17:26:47 +0300

    Done

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -10,31 +10,32 @@ public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
         int totalSupply = 0;
         int totalBuy = 0;
+        String split = ",";
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
-            String value = reader.readLine();
+        try (BufferedReader fileReader = new BufferedReader(new FileReader(fromFileName))) {
+            String value = fileReader.readLine();
             while (value != null) {
-                String[] valueParts = value.split(",");
-                if (valueParts[0].trim().equalsIgnoreCase("buy")) {
+                String[] valueParts = value.split(split);
+                if (valueParts[0].equalsIgnoreCase("buy")) {
                     totalBuy += Integer.parseInt(valueParts[1]);
-                } else if (valueParts[0].trim().equalsIgnoreCase("supply")) {
+                } else if (valueParts[0].equalsIgnoreCase("supply")) {
                     totalSupply += Integer.parseInt(valueParts[1]);
                 }
-                value = reader.readLine();
+                value = fileReader.readLine();
             }
         } catch (IOException e) {
-            throw new RuntimeException("Can't read file", e);
+            throw new RuntimeException("Can't read file: " + fromFileName, e);
         }
 
         int result = totalSupply - totalBuy;
 
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
-            writer.write("supply," + String.valueOf(totalSupply) + System.lineSeparator());
-            writer.write("buy," + String.valueOf(totalBuy) + System.lineSeparator());
-            writer.write("result," + String.valueOf(result) + System.lineSeparator());
+        try (BufferedWriter fileWriter = new BufferedWriter(new FileWriter(toFileName))) {
+            fileWriter.write("supply," + String.valueOf(totalSupply) + System.lineSeparator());
+            fileWriter.write("buy," + String.valueOf(totalBuy) + System.lineSeparator());
+            fileWriter.write("result," + String.valueOf(result) + System.lineSeparator());
 
         } catch (IOException e) {
-            throw new RuntimeException("Can't write file", e);
+            throw new RuntimeException("Can't write file: " + toFileName, e);
         }
     }
 }
