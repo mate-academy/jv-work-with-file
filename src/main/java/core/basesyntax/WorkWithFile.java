4545a3f83e9233346bf2650d9c34a3c218d8cee4
Anton <windsweptofficial@gmail.com>
2024-10-11 07:55:58 +0200

    fixed-2

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -6,37 +6,44 @@ import java.nio.file.Files;
 import java.util.List;
 
 public class WorkWithFile {
+    private static final int SECOND_ELEMENT = 1;
+    private static final String TXT_SUPPLY = "supply";
+    private static final String TXT_BUY = "buy";
+    private static final String TXT_RESULT = "result";
+    private static final String TXT_SEPARATOR = ",";
+
     public void getStatistic(String fromFileName, String toFileName) {
-        File firstFile = new File(fromFileName);
-        File secondFile = new File(toFileName);
-
-        int buy = 0;
-        int supply = 0;
-        int result = 0;
-
         try {
+            File firstFile = new File(fromFileName);
+            File secondFile = new File(toFileName);
+
+            int buy = 0;
+            int supply = 0;
+
             List<String> stringList = Files.readAllLines(firstFile.toPath());
             for (String s : stringList) {
-                if (s.contains("buy")) {
-                    buy += Integer.parseInt(s.substring(s.indexOf(',') + 1, s.length()));
+                if (s.contains(TXT_BUY)) {
+                    String[] str = s.split(TXT_SEPARATOR);
+                    buy += Integer.parseInt(str[SECOND_ELEMENT]);
                 }
-                if (s.contains("supply")) {
-                    supply += Integer.parseInt(s.substring(s.indexOf(',') + 1, s.length()));
+                if (s.contains(TXT_SUPPLY)) {
+                    String[] str = s.split(TXT_SEPARATOR);
+                    supply += Integer.parseInt(str[SECOND_ELEMENT]);
                 }
             }
 
-            result = supply - buy;
+            int result = supply - buy;
 
             StringBuilder stringBuilder = new StringBuilder();
             stringBuilder
-                    .append("supply,").append(supply).append(System.lineSeparator())
-                    .append("buy,").append(buy).append(System.lineSeparator())
-                    .append("result,").append(result).append(System.lineSeparator());
+                    .append(TXT_SUPPLY).append(TXT_SEPARATOR).append(supply).append(System.lineSeparator())
+                    .append(TXT_BUY).append(TXT_SEPARATOR).append(buy).append(System.lineSeparator())
+                    .append(TXT_RESULT).append(TXT_SEPARATOR).append(result).append(System.lineSeparator());
 
             secondFile.createNewFile();
             Files.write(secondFile.toPath(), stringBuilder.toString().getBytes());
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Can't read data from the file", e);
         }
     }
 }
