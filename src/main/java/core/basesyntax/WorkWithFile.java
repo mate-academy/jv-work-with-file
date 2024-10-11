a5ec94c3efffd499c9fea671fb6d2261cc7d8d43
Anton <windsweptofficial@gmail.com>
2024-10-11 08:48:37 +0200

    fixed-5

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class WorkWithFile {
     private static final int SECOND_ELEMENT = 1;
@@ -19,27 +20,32 @@ public class WorkWithFile {
 
     private List<String> readFromFile(String fromFileName) {
         File file = new File(fromFileName);
-        List<String> stringList = null;
+        Stream<String> lines = null;
         try {
-            stringList = Files.readAllLines(file.toPath());
+            lines = Files.lines(file.toPath());
         } catch (IOException e) {
             throw new RuntimeException("Can't read from file", e);
         }
-        return stringList;
+        return lines.toList();
+    }
+
+    private int addElementIfContains(String line, String isContained,
+                                      String[] fromArray, int element) {
+        if (line.contains(isContained)) {
+            int value = Integer.parseInt(fromArray[element]);
+            return value;
+        }
+        return 0;
     }
 
     private String calculateData(List<String> stringList) {
         int buy = 0;
         int supply = 0;
         for (String s : stringList) {
-            if (s.contains(TXT_BUY)) {
-                String[] str = s.split(TXT_SEPARATOR);
-                buy += Integer.parseInt(str[SECOND_ELEMENT]);
-            }
-            if (s.contains(TXT_SUPPLY)) {
-                String[] str = s.split(TXT_SEPARATOR);
-                supply += Integer.parseInt(str[SECOND_ELEMENT]);
-            }
+            buy += addElementIfContains(s, TXT_BUY,
+                    s.split(TXT_SEPARATOR), SECOND_ELEMENT);
+            supply += addElementIfContains(s, TXT_SUPPLY,
+                    s.split(TXT_SEPARATOR), SECOND_ELEMENT);
         }
         int result = supply - buy;
         StringBuilder stringBuilder = new StringBuilder();
@@ -52,7 +58,6 @@ public class WorkWithFile {
     private void writeToFileResult(String toFileName, String data) {
         File file = new File(toFileName);
         try {
-            file.createNewFile();
             Files.write(file.toPath(), data.getBytes());
         } catch (IOException e) {
             throw new RuntimeException("Can't create new file", e);
