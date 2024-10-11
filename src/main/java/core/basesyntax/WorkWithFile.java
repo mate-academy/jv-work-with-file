a63447da4c0556ff9a88645811e9d6b33eb64a3b
Anton <windsweptofficial@gmail.com>
2024-10-11 07:45:08 +0200

    fixed-1

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -3,7 +3,6 @@ package core.basesyntax;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.StandardOpenOption;
 import java.util.List;
 
 public class WorkWithFile {
