185e24ec75b0213efa4af88bd79e5e30f25a9547
Alexander Tyschenko <qwitelite@gmail.com>
2024-10-15 20:41:22 +0300

    split readFileAndCreateReport() method in two methods, fixed little mistakes

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -12,22 +12,23 @@ public class WorkWithFile {
     private static final String SUPPLY = "supply";
     private static final String RESULT = "result";
     private static final String BUY = "buy";
+    private static final String COMMA = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        String text = readFileAndCreateReport(fromFileName);
+        String text = createReport(fromFileName);
         writeReportToFile(toFileName, text);
     }
 
-    private String readFileAndCreateReport(String fromFile) {
+    private int[] readFile(String fromFile) {
+        int[] result = new int[3];
         int supplyAmount = 0;
         int buyAmount = 0;
         int resultAmount;
-        StringBuilder reportBuilder = new StringBuilder();
         try (BufferedReader reader
                      = new BufferedReader(new FileReader(fromFile))) {
             String data = reader.readLine();
             while (data != null) {
-                String[] line = data.split(",");
+                String[] line = data.split(COMMA);
                 if (line[TYPE_INDEX].equals(SUPPLY)) {
                     supplyAmount += Integer.parseInt(line[AMOUNT_INDEX]);
                 } else {
@@ -36,21 +37,35 @@ public class WorkWithFile {
                 data = reader.readLine();
             }
         } catch (IOException e) {
-            throw new RuntimeException("Can't read file", e);
+            throw new RuntimeException("Can't read file" + fromFile, e);
         }
         resultAmount = supplyAmount - buyAmount;
-        return reportBuilder.append(SUPPLY).append(",")
-                .append(supplyAmount).append(System.lineSeparator())
-                .append(BUY).append(",").append(buyAmount).append(System.lineSeparator())
-                .append(RESULT).append(",").append(resultAmount).toString();
+        result[0] = supplyAmount;
+        result[1] = buyAmount;
+        result[2] = resultAmount;
+        return result;
     }
-    
+
+    private String createReport(String fromFile) {
+        int[] data = readFile(fromFile);
+        int supplyAmount = data[0];
+        int buyAmount = data[1];
+        int resultAmount = data[2];
+
+        StringBuilder reportBuilder = new StringBuilder();
+        return reportBuilder.append(SUPPLY).append(COMMA)
+                .append(supplyAmount).append(System.lineSeparator())
+                .append(BUY).append(COMMA).append(buyAmount).append(System.lineSeparator())
+                .append(RESULT).append(COMMA).append(resultAmount).toString();
+
+    }
+
     private void writeReportToFile(String toFile, String text) {
         try (BufferedWriter writer
                      = new BufferedWriter(new FileWriter(toFile))) {
             writer.write(text);
         } catch (IOException e) {
-            throw new RuntimeException("Can't write file", e);
+            throw new RuntimeException("Can't write file" + toFile, e);
         }
     }
 }
