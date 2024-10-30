b9d511f97fe29af652ae52257ddb047edc84616e
Liza <lizabalashova7@gmail.com>
2024-10-30 17:41:57 +0400

    fixed

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,10 +8,14 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    private static final int OPERATION = 0;
-    private static final int VALUE = 1;
+    private static final int OPERATION_INDEX = 0;
+    private static final int VALUE_INDEX = 1;
 
     public void getStatistic(String fromFileName, String toFileName) {
+        readFile(fromFileName, toFileName);
+    }
+
+    private void readFile(String fromFileName, String toFileName) {
         File fromFile = new File(fromFileName);
         int countSupply = 0;
         int countBuy = 0;
@@ -19,17 +23,20 @@ public class WorkWithFile {
             String value = bufferedReader.readLine();
             while (value != null) {
                 String[] data = value.split(",");
-                if (data[OPERATION].equals("supply")) {
-                    countSupply += Integer.parseInt(data[VALUE]);
-                } else if (data[OPERATION].equals("buy")) {
-                    countBuy += Integer.parseInt(data[VALUE]);
+                if (data[OPERATION_INDEX].equals("supply")) {
+                    countSupply += Integer.parseInt(data[VALUE_INDEX]);
+                } else if (data[OPERATION_INDEX].equals("buy")) {
+                    countBuy += Integer.parseInt(data[VALUE_INDEX]);
                 }
                 value = bufferedReader.readLine();
             }
+            writeFile(toFileName, countSupply, countBuy);
         } catch (IOException e) {
-            throw new RuntimeException("Can't read data from file", e);
+            throw new RuntimeException("Can't read data from file " + fromFile, e);
         }
+    }
 
+    private void writeFile(String toFileName, int countSupply, int countBuy) {
         File toFile = new File(toFileName);
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFile))) {
             bufferedWriter.write(createReport(countSupply, countBuy));
@@ -38,7 +45,7 @@ public class WorkWithFile {
         }
     }
 
-    public String createReport(int countSupply, int countBuy) {
+    private String createReport(int countSupply, int countBuy) {
         StringBuilder report = new StringBuilder();
         int result = countSupply - countBuy;
         return report.append("supply,").append(countSupply).append("\n")
@@ -46,6 +53,3 @@ public class WorkWithFile {
                 .append("result,").append(result).toString();
     }
 }
-
-
-
