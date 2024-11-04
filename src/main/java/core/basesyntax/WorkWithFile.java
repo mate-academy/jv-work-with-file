b70d65bc1256c86225adc744f79c429d5ed57df4
Vasyl <vasiliy.azarov.vl1@gmail.com>
2024-11-04 10:19:16 +0100

    fix code style 5

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -12,6 +12,10 @@ import java.util.List;
 import java.util.Map;
 
 public class WorkWithFile {
+    private static final String BUY = "buy";
+    private static final String SUPPLY = "supply";
+    private static final String RESULT = "result";
+
     public void getStatistic(String fromFileName, String toFileName) {
         Map<String, Integer> resultMap;
         try {
@@ -21,28 +25,36 @@ public class WorkWithFile {
         } catch (IOException e) {
             throw new RuntimeException("Can`t read file", e);
         }
+        if (resultMap.containsKey(BUY) || resultMap.containsKey(SUPPLY)) {
+            writeFile(resultMap, toFileName);
+        } else {
+            System.out.println("Cant`t write to file, because not found results from file");
+        }
+    }
+
+    private void writeFile(Map<String, Integer> resultMap, String toFileName) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
             List<String> resultList = new ArrayList<>();
             for (String key : resultMap.keySet()) {
-                String stringBuilder = key
-                        + ","
-                        + resultMap.get(key).toString();
-                resultList.add(stringBuilder);
+                StringBuilder stringBuilder = new StringBuilder();
+                stringBuilder.append(key);
+                stringBuilder.append(",");
+                stringBuilder.append(resultMap.get(key).toString());
+                resultList.add(stringBuilder.toString());
             }
             resultList.sort(Collections.reverseOrder());
             for (String line : resultList) {
                 writer.write(line);
                 writer.newLine();
             }
-            int result = resultMap.get("supply") - resultMap.get("buy");
-            writer.write("result," + result);
+            int result = resultMap.get(SUPPLY) - resultMap.get(BUY);
+            writer.write(RESULT + "," + result);
         } catch (IOException e) {
             throw new RuntimeException("Can`t create file", e);
         }
-
     }
 
-    public List<String> getAllFieldNames(List<String> lines) {
+    private List<String> getAllFieldNames(List<String> lines) {
         List<String> names = new ArrayList<>();
         for (String line : lines) {
             String[] words = line.split(",");
@@ -52,23 +64,25 @@ public class WorkWithFile {
         return names;
     }
 
-    public Map<String, Integer> getResultMap(List<String> lines) {
+    private Map<String, Integer> getResultMap(List<String> lines) {
         Map<String, Integer> resultMap = new HashMap<>();
         List<String> names = getAllFieldNames(lines);
         for (String name : names) {
             resultMap.put(name, 0);
         }
-        for (String line : lines) {
-            String[] splitLine = line.split(",");
-            if (names.contains(splitLine[0])) {
+        try {
+            for (String line : lines) {
+                String[] splitLine = line.split(",");
                 int currentCount = resultMap.get(splitLine[0]);
                 for (int i = 1; i < splitLine.length; i++) {
                     currentCount += Integer.parseInt(splitLine[i]);
                 }
                 resultMap.put(splitLine[0], currentCount);
             }
+            return resultMap;
+        } catch (NumberFormatException e) {
+            throw new RuntimeException("Error while formatting numbers", e);
         }
-        return resultMap;
     }
 
 }
