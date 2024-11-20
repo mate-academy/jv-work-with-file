60a18976f2faba95850c9b990d19ddedf99b0ba6
Serhii Slobodianiuk <sergey.slobodianiuk@gmail.com>
2024-11-20 19:29:15 +0200

    issues are fixed

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -10,21 +10,17 @@ import java.io.IOException;
 public class WorkWithFile {
 
     public void getStatistic(String fromFileName, String toFileName) {
-        if (fromFileName == null) {
-            System.out.println("Source file doesn't exists");
-            return;
-        }
-        if (toFileName == null) {
-            System.out.println("Destination file doesn't exists");
-            return;
-        }
 
-        File file = new File(fromFileName);
+        File sourceFile = new File(fromFileName);
+        if (!sourceFile.exists() || !sourceFile.canRead()) {
+            System.out.println("Source file doesn't exists or not readable");
+            return;
+        }
         StringBuilder builder = new StringBuilder();
         int sumSupply = 0;
         int sumBuy = 0;
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(sourceFile))) {
             String value = reader.readLine();
             while (value != null) {
                 builder.append(value).append(System.lineSeparator());
@@ -37,27 +33,14 @@ public class WorkWithFile {
         String[] splitValues = builder.toString().split(System.lineSeparator());
 
         for (String value : splitValues) {
-            if (value == null) {
-                System.out.println("From file name is empty!");
-            }
-
-            assert value != null;
             String[] items = value.split(",");
 
             if (items[0].equals("supply")) {
-                try {
-                    sumSupply += Integer.parseInt(items[1]);
-                } catch (NumberFormatException e) {
-                    throw new RuntimeException("Invalid number, invalid value entered", e);
-                }
+                sumSupply += Integer.parseInt(items[1]);
             }
 
             if (items[0].equals("buy")) {
-                try {
-                    sumBuy += Integer.parseInt(items[1]);
-                } catch (NumberFormatException e) {
-                    throw new RuntimeException("Invalid number, invalid value entered", e);
-                }
+                sumBuy += Integer.parseInt(items[1]);
             }
         }
 
@@ -66,9 +49,13 @@ public class WorkWithFile {
         builder.append("buy,").append(sumBuy).append(System.lineSeparator());
         builder.append("result,").append(sumSupply - sumBuy);
 
-        File fileName = new File(toFileName);
+        File destinationFile = new File(toFileName);
+        if (!destinationFile.exists() || !destinationFile.canWrite()) {
+            System.out.println("Destination file doesn't exists or not writable");
+            return;
+        }
 
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(destinationFile))) {
             writer.write(builder.toString());
         } catch (IOException e) {
             throw new RuntimeException("Can`t write file", e);
