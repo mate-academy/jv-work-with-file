1fc7a6ed17faa483ab60169705bfad2f2cb3f4e5
Yevhen <zheckakoroluk@gmail.com>
2024-12-24 21:17:15 +0100

    executed

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,26 @@
 package core.basesyntax;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
-
+        File file = new File(fromFileName);
+        try {
+            List<String> strings = Files.readAllLines(file.toPath());
+            System.out.println(strings);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read fails ", e);
+        }
+        try {
+            Files.write(file.toPath(), toFileName.getBytes(), StandardOpenOption.APPEND);
+        } catch (Exception e) {
+            throw new RuntimeException("Can't write to data to file", e);
+        }
     }
+
 }
+
