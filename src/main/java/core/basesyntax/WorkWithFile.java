c7cdb414b9f4ad1cad3087556168ec14c34a5746
Piotr Hic <piotrhic@gmail.com>
2024-12-25 18:24:36 +0100

    v3

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -9,20 +9,20 @@ public class WorkWithFile {
 
     public void getStatistic(String fromFileName, String toFileName) {
 
-        if (fromFileName== null || toFileName == null){
+        if (fromFileName == null || toFileName == null) {
             return;
         }
 
-        String sCurrentLine;
+        String sCurrentLine = null;
         Integer supplyCount = 0;
         Integer buyCount = 0;
 
-        try (BufferedReader br = new BufferedReader(new FileReader (fromFileName))) {
+        try (BufferedReader br = new BufferedReader(new FileReader(fromFileName))) {
 
             while ((sCurrentLine = br.readLine()) != null)
             {
-                String [] elements = sCurrentLine.split( ",");
-                if (elements[0].equals("supply")){
+                String [] elements = sCurrentLine.split(",");
+                if (elements[0].equals("supply")) {
                     supplyCount += Integer.parseInt(elements[1]);
                 } else {
                     buyCount += Integer.parseInt(elements[1]);
@@ -35,10 +35,11 @@ public class WorkWithFile {
         StringBuilder sb = new StringBuilder();
         sb.append("supply").append(",").append(supplyCount).append(System.lineSeparator());
         sb.append("buy").append(",").append(buyCount).append(System.lineSeparator());
-        sb.append("result").append(",").append(supplyCount - buyCount).append(System.lineSeparator());
+        sb.append("result").append(",").append(supplyCount - buyCount).
+                append(System.lineSeparator());
 
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter (toFileName))) {
-            bw.write (sb.toString());
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(toFileName))) {
+            bw.write(sb.toString());
         } catch (Exception e) {
             throw new RuntimeException("Can't write data from the file " + toFileName, e);
         }
