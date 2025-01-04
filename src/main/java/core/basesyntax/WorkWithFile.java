06686cab7043ae808d179d515fc2f97c4917c954
Mykola <mykola.turak.ki.2023@lpnu.ua>
2025-01-04 23:22:52 +0200

    mke read and write from file task

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,45 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
+        String[] readFileData = new String[2];
+        int buyValue = 0;
+        int supplyValue = 0;
+        String value;
 
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
+            while ((value = value = reader.readLine()) != null) {
+                readFileData = value.split(",");
+                if (readFileData[0].equals("buy")) {
+                    buyValue += Integer.valueOf(readFileData[1]);
+                } else {
+                    supplyValue += Integer.valueOf(readFileData[1]);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("Can't open the file");
+        } catch (IOException e) {
+            throw new RuntimeException("Cant read from file");
+        }
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
+            writer.write("supply," + supplyValue);
+            writer.newLine();
+            writer.write("buy," + buyValue);
+            writer.newLine();
+            int result = supplyValue - buyValue;
+            writer.write("result," + result);
+            writer.newLine();
+        } catch (IOException e) {
+            throw new RuntimeException("Can't open the file");
+        }
     }
 }
