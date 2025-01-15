b2ed5bc6d53730786f1a0509328e7d6094f94681
xMax <xmaxchil@gmail.com>
2025-01-15 16:58:52 +0100

    refactor

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,10 +7,9 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) throws IOException {
-        final int elementsQuantity = 2;
-        String[] keys = new String[elementsQuantity];
-        int[] values = new int[elementsQuantity];
+    public void getStatistic(String fromFileName, String toFileName) {
+        String[] keys = new String[] {"supply", "buy"};
+        int[] values = new int[keys.length];
 
         try {
             BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
@@ -19,39 +18,25 @@ public class WorkWithFile {
                 String[] keyValuePair = line.split("\\,");
                 int value = Integer.parseInt(keyValuePair[1]);
                 for (int i = 0; i < keys.length; i++) {
-                    if (keys[i] == null) {
-                        keys[i] = keyValuePair[0];
-                        values[i] = value;
-                        break;
-                    } else if (keys[i].equals(keyValuePair[0])) {
+                    if (keys[i].equals(keyValuePair[0])) {
                         values[i] += value;
                         break;
                     }
                 }
                 line = reader.readLine();
             }
-            if (keys[0].equals("buy")) {
-                String tempKey = keys[0];
-                keys[0] = keys[1];
-                int tempValue = values[0];
-                values[0] = values[1];
-                keys[1] = tempKey;
-                values[1] = tempValue;
-            }
-            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName));
             StringBuilder stringBuilder = new StringBuilder();
             for (int i = 0; i < keys.length; i++) {
-                if (keys[i] == null) {
-                    break;
-                }
                 stringBuilder.append(keys[i])
                         .append(",")
                         .append(values[i])
                         .append(System.lineSeparator());
             }
             stringBuilder.append("result,").append(values[0] - values[1]);
-            bufferedWriter.write(stringBuilder.toString());
-            bufferedWriter.close();
+            try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))){
+                bufferedWriter.write(stringBuilder.toString());
+            }
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
