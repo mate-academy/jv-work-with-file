c9909a315ce10909cd6d6d1194cff3587cf40fee
IgorArsionov <valisgvalis@gmail.com>
2025-01-16 21:40:24 +0200

    update

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -13,6 +13,13 @@ public class WorkWithFile {
     private Map<String, Integer> value = new HashMap<>();
 
     public void getStatistic(String fromFileName, String toFileName) {
+
+        readerFile(fromFileName);
+
+        writerFile(toFileName);
+    }
+
+    private void readerFile(String fromFileName) {
         File file = new File(fromFileName);
 
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
@@ -22,29 +29,33 @@ public class WorkWithFile {
                 String[] arr = line.split(",");
                 setValue(arr);
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (Exception e) {
+            throw new RuntimeException("Ошибка при чтении в файла " + file.getName(), e);
         }
+    }
 
+    private void writerFile(String toFileName) {
         File endFile = new File(toFileName);
 
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(endFile))) {
+            if ( value.containsKey("supply") && value.containsKey("buy")) {
 
-            writer.write(String.format("%s,%d%n", "supply", value.get("supply")));
-            writer.write(String.format("%s,%d%n", "buy", value.get("buy")));
-            int result = value.get("supply") - value.get("buy");
-            writer.write(String.format("%s,%d", "result", result));
+                writer.write(String.format("%s,%d%n", "supply", value.get("supply")));
+                writer.write(String.format("%s,%d%n", "buy", value.get("buy")));
+                int result = value.get("supply") - value.get("buy");
+                writer.write(String.format("%s,%d", "result", result));
 
-            value.clear();
+                value.clear();
+            }
 
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (Exception e) {
+            throw new RuntimeException("Ошибка при записи в файл " + endFile.getName(), e);
         }
     }
 
     private void setValue(String[] arr) {
         if (value.containsKey(arr[0])) {
-            int amount = value.get(arr[0]) + Integer.valueOf(arr[1]);
+            int amount = value.get(arr[0]) + Integer.parseInt(arr[1]);
             value.put(arr[0], amount);
         } else {
             value.put(arr[0], Integer.valueOf(arr[1]));
