2b66ec8e2709c09d88baae46e8e885a4cb467196
Roman Malyarenko <malyarenko.roman@gmail.com>
2025-02-03 21:08:19 +0200

    implemented method "getStatistic" which return data from the market after the working day

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,61 @@
 package core.basesyntax;
 
-public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class WorkWithFile {
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        String readedString = readFromFile(fromFileName);
+        String resultString = createReport(readedString);
+        writeToFile(toFileName, resultString);
+    }
+
+    private String readFromFile(String fileName) {
+        File fromFile = new File(fileName);
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFile))) {
+            String value = bufferedReader.readLine();
+            while (value != null) {
+                stringBuilder.append(value).append(System.lineSeparator());
+                value = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read data from the file " + fromFile, e);
+        }
+        return stringBuilder.toString();
+    }
+
+    private String createReport(String readedString) {
+        int supplyInt = 0;
+        int buyInt = 0;
+        String[] arrayOfStrings = readedString.split(System.lineSeparator());
+        for (String string : arrayOfStrings) {
+            String[] stringSplit = string.split("\\W+");
+            if (stringSplit[0].equals(SUPPLY)) {
+                supplyInt += Integer.parseInt(stringSplit[1]);
+            } else {
+                buyInt += Integer.parseInt(stringSplit[1]);
+            }
+        }
+        int resultInt = supplyInt - buyInt;
+        return SUPPLY + "," + supplyInt + System.lineSeparator() + BUY + "," + buyInt
+                + System.lineSeparator() + "result" + "," + resultInt;
+    }
+
+    private void writeToFile(String toFileName, String resultString) {
+        File toFile = new File(toFileName);
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFile))) {
+            bufferedWriter.write("");
+            bufferedWriter.write(resultString);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read data to the file " + toFile, e);
+        }
     }
 }
