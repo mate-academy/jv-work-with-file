21f3248e92059cbfa77dcc3b48065472012e2e90
Irina Levchenko <ira-levch@ukr.net>
2025-02-21 14:39:59 +0200

    implemented method getStatistic(...) that reads data line by line from the file using BufferedReader class, processes each line splitting it with method split(","), compares String values ("buy" or "supply") and calculates the corresponding sums, and finally writes the result to the target file CSV

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,43 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
-
+        File sourceFile = new File(fromFileName);
+        int supplySum = 0;
+        int buySum = 0;
+        int result = 0;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(sourceFile));
+            String fileLine = reader.readLine();
+            while (fileLine != null) {
+                System.out.println(fileLine);
+                String[] line = fileLine.split(",");
+                if (line[0].equals("supply")) {
+                    supplySum += Integer.parseInt(line[1]);
+                } else {
+                    buySum += Integer.parseInt(line[1]);
+                }
+                fileLine = reader.readLine();
+            }
+            reader.close();
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
+        result = supplySum - buySum;
+        String inputData = "supply," + supplySum + System.lineSeparator() + "buy," + buySum
+                + System.lineSeparator() + "result," + result;
+        File targetFile = new File(toFileName);
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(targetFile))) {
+            bufferedWriter.write(inputData);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write data to file", e);
+        }
     }
 }
