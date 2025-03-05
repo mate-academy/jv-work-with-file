2b7c541c90d805bc0c4992feebaa4ba04e4733bd
Viktoriia Dragneva <viktoriiadragneva@gmail.com>
2025-03-05 14:03:03 +0200

    solution

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,35 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
-
+        int supplySum = 0;
+        int buySum = 0;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(",");
+                String operation = parts [0];
+                int value = Integer.parseInt(parts[1]);
+                if (operation.equals("supply")) {
+                    supplySum += value;
+                } else if (operation.equals("buy")) {
+                    buySum += value;
+                }
+            }
+            int result = supplySum - buySum;
+            FileWriter fileWriter = new FileWriter(toFileName);
+            fileWriter.write("supply," + supplySum + System.lineSeparator());
+            fileWriter.write("buy," + buySum + System.lineSeparator());
+            fileWriter.write("result," + result + System.lineSeparator());
+            fileWriter.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
