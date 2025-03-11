e36af4f539a2cfb1338bde88f46f9929f42061e2
Ks228337 <kiskemova11@gmail.com>
2025-03-12 00:29:00 +0200

    Implement method getStatistic(String fromFileName, String toFileName) which will return some data from the market after the working day.

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -5,8 +5,14 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class WorkWithFile {
+    private static final Logger LOGGER = Logger.getLogger(WorkWithFile.class.getName());
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+
     public void getStatistic(String fromFileName, String toFileName) {
         int supply = 0;
         int buy = 0;
@@ -16,27 +22,44 @@ public class WorkWithFile {
             while ((line = reader.readLine()) != null) {
                 String[] parts = line.split(",");
                 String type = parts[0];
-                int amount = Integer.parseInt(parts[1]);
+                int amount = parseAmount(parts[1]);
 
-                if (type.equals("supply")) {
+                if (SUPPLY.equals(type)) {
                     supply += amount;
-                } else if (type.equals("buy")) {
+                } else if (BUY.equals(type)) {
                     buy += amount;
                 }
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            LOGGER.log(Level.SEVERE, "Error reading the file: " + fromFileName, e);
         }
 
-        int result = supply - buy;
+        int result = calculateResult(supply, buy);
 
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
-            writer.write("supply," + supply + System.lineSeparator());
-            writer.write("buy," + buy + System.lineSeparator());
-            writer.write("result," + result + System.lineSeparator());
+            writeResult(writer, supply, buy, result);
         } catch (IOException e) {
-            e.printStackTrace();
+            LOGGER.log(Level.SEVERE, "Error writing to the file: " + toFileName, e);
         }
     }
-}
 
+    private int parseAmount(String amountStr) {
+        try {
+            return Integer.parseInt(amountStr);
+        } catch (NumberFormatException e) {
+            LOGGER.log(Level.WARNING, "Unable to parse amount: " + amountStr, e);
+            return 0;
+        }
+    }
+
+    private int calculateResult(int supply, int buy) {
+        return supply - buy;
+    }
+
+    private void writeResult(BufferedWriter writer, int supply, int buy, int result)
+            throws IOException {
+        writer.write(SUPPLY + "," + supply + System.lineSeparator());
+        writer.write(BUY + "," + buy + System.lineSeparator());
+        writer.write("result," + result + System.lineSeparator());
+    }
+}
