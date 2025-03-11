751f89f1b40ca6f2522ad29564d8067f7dd03335
DZotsenko <diana.zotsenko@gmail.com>
2025-03-11 10:51:34 +0200

    implemented getStatistic() method

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,69 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
+        String fruitStatistic = readFile(fromFileName);
+        String report = createReport(fruitStatistic);
+        writeFile(report, toFileName);
+    }
 
+    private String readFile(String fromFileName) {
+        StringBuilder builder = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
+            int value = reader.read();
+            while (value != -1) {
+                builder.append((char) value);
+                value = reader.read();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file " + fromFileName, e);
+        }
+        return builder.toString();
+    }
+
+    private String createReport(String fruitStatistic) {
+        final String delimiter = System.lineSeparator();
+        final String recordSeparator = ",";
+        final String supplyAction = "supply";
+        final String buyAction = "buy";
+        final String resultAction = "result";
+        int supply = 0;
+        int buy = 0;
+        String[] statisticRecords = fruitStatistic.split(delimiter);
+        for (String record : statisticRecords) {
+            String[] splittedRecord = record.split(recordSeparator);
+            if (splittedRecord[0].equals(supplyAction)) {
+                supply += Integer.parseInt(splittedRecord[1]);
+            } else {
+                buy += Integer.parseInt(splittedRecord[1]);
+            }
+        }
+        int result = supply - buy;
+        StringBuilder reportBuilder = new StringBuilder(supplyAction).append(recordSeparator)
+                .append(supply).append(System.lineSeparator())
+                .append(buyAction).append(recordSeparator).append(buy)
+                .append(System.lineSeparator())
+                .append(resultAction).append(recordSeparator).append(result);
+        return reportBuilder.toString();
+    }
+
+    private void writeFile(String report, String toFileName) {
+        File file = new File(toFileName);
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            throw new RuntimeException("Can't create file", e);
+        }
+        try {
+            Files.write(file.toPath(), report.getBytes());
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write file " + toFileName, e);
+        }
     }
 }
