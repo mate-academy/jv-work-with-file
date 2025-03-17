fb4a1ea604ab30ea7ffbec59f2cacb89ab398909
Illia Dobrovolskyi <illia.dobrovolskyi2@gmail.com>
2025-03-18 00:20:10 +0200

    separated responsibilities, changed access modifiers

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,11 +1,11 @@
 package core.basesyntax;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 public class WorkWithFile {
     private static final String SEPARATOR = " ";
@@ -16,17 +16,17 @@ public class WorkWithFile {
     private static final int TYPE_INDEX = 0;
 
     public void getStatistic(String fromFileName, String toFileName) {
-        String[] arrayOfLines = readFromFile(fromFileName);
-        String report = createReport(arrayOfLines);
+        String textFromFile = readFromFile(fromFileName);
+        String report = createReport(textFromFile);
         writeToFile(report, toFileName);
     }
 
-    public static String[] readFromFile(String fromFileName) {
+    private static String readFromFile(String fromFileName) {
         StringBuilder stringBuilder = new StringBuilder();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             String line = bufferedReader.readLine();
             if (line == null) {
-                return new String[0];
+                return "";
             }
             while (line != null) {
                 stringBuilder.append(line).append(SEPARATOR);
@@ -37,12 +37,11 @@ public class WorkWithFile {
         } catch (IOException e) {
             throw new RuntimeException("Can't read data from the file", e);
         }
-        String[] arrayOfLines = stringBuilder.toString().split(SEPARATOR);
-        return arrayOfLines;
+        return stringBuilder.toString();
     }
 
-    public static String createReport(String[] arrayOfLines) {
-        String report;
+    private static String createReport(String textFromFile) {
+        String[] arrayOfLines = textFromFile.split(SEPARATOR);
         int totalSupply = 0;
         int totalBuy = 0;
         int result;
@@ -51,20 +50,20 @@ public class WorkWithFile {
             int sum = Integer.parseInt(splittedString[AMOUNT_INDEX]);
             if (splittedString[TYPE_INDEX].equalsIgnoreCase(SUPPLY)) {
                 totalSupply += sum;
-            } else if (splittedString[0].equalsIgnoreCase("buy")) {
+            } else if (splittedString[0].equalsIgnoreCase(BUY)) {
                 totalBuy += sum;
             }
         }
         result = totalSupply - totalBuy;
-        report = SUPPLY + COMMA + totalSupply + System.lineSeparator()
+        String report = SUPPLY + COMMA + totalSupply + System.lineSeparator()
                 + BUY + COMMA + totalBuy + System.lineSeparator()
                 + "result" + COMMA + result;
         return report;
     }
 
-    public static void writeToFile(String report, String toFileName) {
-        try {
-            Files.write(Path.of(toFileName), report.getBytes());
+    private static void writeToFile(String report, String toFileName) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+            bufferedWriter.write(report);
         } catch (IOException e) {
             throw new RuntimeException("Can't write to the file", e);
         }
