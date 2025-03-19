49644596dc81db4d6e4be5516b8414aaf85cad1a
Marika Żaboklicka <marika.zaboklicka@wp.pl>
2025-03-19 13:04:52 +0100

    modif of class WorkWithFile.java

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -3,13 +3,14 @@ package core.basesyntax;
 import java.io.*;
 
 public class WorkWithFile {
+
     public void getStatistic(String fromFileName, String toFileName) {
 
         StringBuilder builder = new StringBuilder();
         try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
             String line = reader.readLine();
             while (line != null) {
-                builder.append(line).append(" ");
+                builder.append(line).append(System.lineSeparator());
                 line = reader.readLine();
             }
         } catch (IOException e) {
@@ -21,28 +22,30 @@ public class WorkWithFile {
         int sumOfBuy = 0;
 
         for (int i = 0; i < splitArray.length; i++) {
-            if (splitArray[i].startsWith("s")) {
+            if (splitArray[i].startsWith("s") && i+1 < splitArray.length) {
                 sumOfSupply += Integer.parseInt(splitArray[i + 1]);
-            } else if (splitArray[i].startsWith("b")) {
+            } else if (splitArray[i].startsWith("b") && i+1 < splitArray.length) {
                 sumOfBuy += Integer.parseInt(splitArray[i + 1]);
             }
         }
 
         int result = sumOfSupply - sumOfBuy;
 
+        File file = new File(toFileName);
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+            writer.append(saveToFile(result, sumOfSupply, sumOfBuy).toString());
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write the file", e);
+        }
 
+    }
+    public static StringBuilder saveToFile(int result, int sumOfSupply, int sumOfBuy){
         StringBuilder resultToFile = new StringBuilder();
         resultToFile.append("supply,").append(sumOfSupply).
                 append(System.lineSeparator()).append("buy,").
                 append(sumOfBuy).append(System.lineSeparator()).
                 append("result,").append(result);
 
-        File file = new File(toFileName);
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
-            writer.append(resultToFile.toString());
-        } catch (IOException e) {
-            throw new RuntimeException("Can't write the file", e);
-        }
-
+        return resultToFile;
     }
 }
