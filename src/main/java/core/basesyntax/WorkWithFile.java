a000feb1e33f3134ce8eb466774b11838cb644d0
Acid <deadfoxua1337@gmail.com>
2025-03-21 22:55:03 +0100

    style fix

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,7 @@
 package core.basesyntax;
 
-import java.io.BufferedWriter;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
