e9131d300baae965f1d8f7a3f84ca083a54f1242
Acid <deadfoxua1337@gmail.com>
2025-03-21 22:54:05 +0100

    style fix

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,10 +1,10 @@
 package core.basesyntax;
 
-import java.io.FileWriter;
 import java.io.BufferedWriter;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
@@ -28,21 +28,19 @@ public class WorkWithFile {
             throw new IllegalArgumentException("Need an even number of lines");
         }
 
-        String[][] results = new String[lines.length/2][2];
+        String[][] results = new String[lines.length / 2][2];
         int firstItem = 0;
         int secondItem = 1;
-
 
         for (int lineCounter = 0; lineCounter < results.length; lineCounter++) {
             results[lineCounter][0] = lines[firstItem];
             results[lineCounter][1] = lines[secondItem];
-            firstItem+=2;
-            secondItem+=2;
+            firstItem += 2;
+            secondItem += 2;
         }
 
         String[] temp = new String[results.length];
         int identityCounter = 0;
-
 
         for (String[] result : results) {
             boolean isIdentity = false;
@@ -58,10 +56,10 @@ public class WorkWithFile {
             }
         }
 
-        String[][] resultCSV = new String[identityCounter + 1][2];
+        String[][] resultCsv = new String[identityCounter + 1][2];
 
         for (int i = 0; i < identityCounter; i++) {
-            resultCSV[i][0] = temp[i];
+            resultCsv[i][0] = temp[i];
         }
         for (int i = 0; i < identityCounter; i++) {
             int amount = 0;
@@ -70,25 +68,25 @@ public class WorkWithFile {
                     amount += Integer.parseInt(strings[1]);
                 }
             }
-            resultCSV[i][1] = Integer.toString(amount);
+            resultCsv[i][1] = Integer.toString(amount);
         }
 
-        if (resultCSV.length > 1) {
+        if (resultCsv.length > 1) {
             int sum = 0;
 
-            if (Integer.parseInt(resultCSV[0][1]) < Integer.parseInt(resultCSV[1][1])) {
-                String[] temp2 = resultCSV[0];
-                resultCSV[0] = resultCSV[1];
-                resultCSV[1] = temp2;
+            if (Integer.parseInt(resultCsv[0][1]) < Integer.parseInt(resultCsv[1][1])) {
+                String[] temp2 = resultCsv[0];
+                resultCsv[0] = resultCsv[1];
+                resultCsv[1] = temp2;
             }
 
-            sum = Integer.parseInt(resultCSV[0][1]) - Integer.parseInt(resultCSV[1][1]);
-            resultCSV[resultCSV.length - 1][0] = "result";
-            resultCSV[resultCSV.length - 1][1] = Integer.toString(sum);
+            sum = Integer.parseInt(resultCsv[0][1]) - Integer.parseInt(resultCsv[1][1]);
+            resultCsv[resultCsv.length - 1][0] = "result";
+            resultCsv[resultCsv.length - 1][1] = Integer.toString(sum);
         }
 
         StringBuilder builder1 = new StringBuilder();
-        for (String[] strings : resultCSV) {
+        for (String[] strings : resultCsv) {
             builder1.append(strings[0]);
             builder1.append(",");
             builder1.append(strings[1]);
