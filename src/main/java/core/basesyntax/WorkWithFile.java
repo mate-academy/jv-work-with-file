ea0e6b55df70c54602f253d4fdd3bbc0af2d6fb2
romanbilokinsky <isidzukuro@gmail.com>
2025-03-26 17:10:17 +0200

    TaskSolution2

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -3,54 +3,45 @@ package core.basesyntax;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.StandardOpenOption;
 
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
-        File file = new File(fromFileName);
         int sumBuy = 0;
         int sumSupply = 0;
-
+        File file = new File(fromFileName);
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             String line;
-
             while ((line = reader.readLine()) != null) {
-
                 String[] parts = line.split(",");
-
                 if (parts.length >= 2) {
                     String type = parts[0].trim();
                     int value = Integer.parseInt(parts[1].trim());
-
-                    if (type.equals("buy")) {
+                    if ("buy".equals(type)) {
                         sumBuy += value;
-                    } else if (type.equals("supply")) {
+                    } else if ("supply".equals(type)) {
                         sumSupply += value;
                     }
                 }
             }
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException("File nor found", e);
         } catch (IOException e) {
-            throw new RuntimeException("Can't read file", e);
+            throw new RuntimeException("Can't read file " + fromFileName, e);
         }
+        writeReport(toFileName, sumSupply, sumBuy);
+    }
 
+    private void writeReport(String toFileName, int sumSupply, int sumBuy) {
         File newFile = new File(toFileName);
-        try {
-            FileWriter fileWriter = new FileWriter(newFile);
-            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-            Files.write(newFile.toPath(),
-                    ("supply," + sumSupply + "\n"
-                            + "buy," + sumBuy + "\n"
-                            + "result," + (sumSupply - sumBuy))
-                            .getBytes(), StandardOpenOption.APPEND);
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(newFile))) {
+            writer.write("supply," + sumSupply);
+            writer.newLine();
+            writer.write("buy," + sumBuy);
+            writer.newLine();
+            writer.write("result," + (sumSupply - sumBuy));
         } catch (IOException e) {
-            throw new RuntimeException("Can't write file", e);
+            throw new RuntimeException("Can't write file " + toFileName, e);
         }
     }
 }
