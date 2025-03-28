15357678cbf5fdc1ef16ae5ddfdcfdf4d6955fd5
Oleksii Korovnichenko <korovnichenkoA@gmail.com>
2025-03-28 22:54:36 +0200

    my hw8-solution ver1.0

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,36 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
+        int sumSupply = 0;
+        int sumBuy = 0;
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String [] dataLine = line.split(",");
+                if (dataLine[0].equals("supply")) {
+                    sumSupply += Integer.parseInt(dataLine[1]);
+                }
+                if (dataLine[0].equals("buy")) {
+                    sumBuy += Integer.parseInt(dataLine[1]);
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + e.getMessage());
+        }
 
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+            bufferedWriter.write("supply," + sumSupply + System.lineSeparator()
+                                + "buy," + sumBuy + System.lineSeparator()
+                                + "result," + (sumSupply - sumBuy));
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write data to file: ", e);
+        }
     }
 }
