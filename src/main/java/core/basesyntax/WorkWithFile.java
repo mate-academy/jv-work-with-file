7cb0548b2d530b7a3ebbc68bc997ef5ef5c4db15
Oleksii Korovnichenko <korovnichenkoA@gmail.com>
2025-03-28 23:48:29 +0200

    my hw8-solution ver2.0

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,30 +7,49 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+    private static final String DELIMITER = ",";
+
     public void getStatistic(String fromFileName, String toFileName) {
+        int[] report = createReport(fromFileName);
+        writeToFile(toFileName, report);
+    }
+
+    public int[] createReport(String fromFileName) {
         int sumSupply = 0;
         int sumBuy = 0;
+
         try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
             String line;
             while ((line = reader.readLine()) != null) {
-                String [] dataLine = line.split(",");
-                if (dataLine[0].equals("supply")) {
-                    sumSupply += Integer.parseInt(dataLine[1]);
-                }
-                if (dataLine[0].equals("buy")) {
-                    sumBuy += Integer.parseInt(dataLine[1]);
+                String[] data = line.split(DELIMITER);
+                int amount = Integer.parseInt(data[1]);
+
+                if (SUPPLY.equals(data[0])) {
+                    sumSupply += amount;
+                } else if (BUY.equals(data[0])) {
+                    sumBuy += amount;
                 }
             }
         } catch (IOException e) {
-            System.err.println("Error reading file: " + e.getMessage());
+            throw new RuntimeException("Error reading file: " + fromFileName, e);
         }
 
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
-            bufferedWriter.write("supply," + sumSupply + System.lineSeparator()
-                                + "buy," + sumBuy + System.lineSeparator()
-                                + "result," + (sumSupply - sumBuy));
+        return new int[]{sumSupply, sumBuy, sumSupply - sumBuy};
+    }
+
+    public void writeToFile(String toFileName, int[] report) {
+        StringBuilder reportString = new StringBuilder()
+                .append(SUPPLY).append(DELIMITER).append(report[0]).append(System.lineSeparator())
+                .append(BUY).append(DELIMITER).append(report[1]).append(System.lineSeparator())
+                .append("result").append(DELIMITER).append(report[2]);
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
+            writer.write(reportString.toString());
         } catch (IOException e) {
-            throw new RuntimeException("Can't write data to file: ", e);
+            throw new RuntimeException("Can't write data to file: " + toFileName, e);
         }
     }
 }
+
