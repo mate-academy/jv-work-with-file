b390705fb918f62577b37ca01d52d39f6b759b0a
Iryna Kravchenko <kravchenkoiryna1207@gmail.com>
2025-04-08 18:46:19 +0200

    Break big method, to smaller methods, remove varibles, add ','

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -3,35 +3,45 @@ package core.basesyntax;
 import java.io.*;
 
 public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) throws IOException {
-        int supplySum = 0;
-        int buySum = 0;
-        int temporaryValue = 0;
-        String temporaryValueName = "";
-        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
+    public void getStatistic(String fromFileName, String toFileName) {
+        int[] stats = readData(fromFileName);
+        String report = createReport(stats[0], stats[1]);
+        writeData(toFileName, report);
+    }
+
+    private int[] readData(String fileName) {
+        int supply = 0;
+        int buy = 0;
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 String[] parts = line.split(",");
                 String operation = parts[0];
                 int amount = Integer.parseInt(parts[1]);
                 if (operation.equals("supply")) {
-                    supplySum += amount;
-                } else if(operation.equals("buy")) {
-                    buySum += amount;
+                    supply += amount;
+                } else if (operation.equals("buy")) {
+                    buy += amount;
                 }
             }
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Can't read data from file " + fileName, e);
         }
+        return new int[]{supply, buy};
+    }
 
-        int result = supplySum - buySum;
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
-            writer.write("supply,"+ supplySum + System.lineSeparator());
-            writer.write("buy" + buySum + System.lineSeparator());
-            writer.write("result" + result + System.lineSeparator());
+    private String createReport(int supply, int buy) {
+        int result = supply - buy;
+        return "supply," + supply + System.lineSeparator()
+                + "buy," + buy + System.lineSeparator()
+                + "result," + result + System.lineSeparator();
+    }
+
+    private void writeData(String fileName, String data) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+            writer.write(data);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Can't write data to file " + fileName, e);
         }
-
     }
 }
