02901dd2b16a3c3d5047a055635f680021c95526
Oleksandr <odombrovskyi26@gmail.com>
2025-05-08 14:09:25 +0300

    Java/Java Core/Files/Work with CSV File

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,36 @@
 package core.basesyntax;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
+        int countSupply = 0;
+        int countBuy = 0;
+        try {
+            List<String> allLines = Files.readAllLines(new File(fromFileName).toPath());
+            for (String line : allLines) {
+                String[] splited = line.split(",");
+                if (splited[0].equals("supply")) {
+                    countSupply += Integer.parseInt(splited[1]);
+                } else {
+                    countBuy += Integer.parseInt(splited[1]);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("It is not able to read file!", e);
+        }
 
+        String result = "supply," + countSupply + System.lineSeparator()
+                + "buy," + countBuy + System.lineSeparator()
+                + "result," + (countSupply - countBuy);
+
+        try {
+            Files.write(new File(toFileName).toPath(), result.getBytes());
+        } catch (IOException e) {
+            throw new RuntimeException("It is not able to write to file!", e);
+        }
     }
 }
