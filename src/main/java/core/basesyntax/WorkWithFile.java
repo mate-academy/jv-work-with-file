2192770094059d809d64ef5cf0855fe1768d009d
User <n0938951561@gmail.com>
2025-05-14 15:10:42 +0200

    done reading from file

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,28 @@
 package core.basesyntax;
 
+import java.io.*;
+
 public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
+    public void getStatistic(String fromFileName, String toFileName) throws IOException {
+        String[] dataToWrite = null;
+        try {
+            BufferedReader readFromFile = new BufferedReader(new FileReader(fromFileName));
+            StringBuilder stringBuilder = new StringBuilder();
+            int value = readFromFile.read();
+            while (value != -1) {
+                stringBuilder.append((char) value);
+                value = readFromFile.read();
+            }
+            dataToWrite = stringBuilder.toString().split("\n");
+        } catch (IOException e) {
+            throw new IOException("Can`t read file", e);
+        }
+
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName, true))) {
+            bufferedWriter.write(fromFileName);
+        } catch (IOException e) {
+            throw new IOException("Can`t write to file", e);
+        }
 
     }
 }
