f045e2d43cfc89c3d36592827ced4364c874ceb5
Denis Basarab <denya2007basarab@gmail.com>
2025-05-17 15:01:29 +0300

    created Class for get date from file

---
+++ src/main/java/core/basesyntax/GetDateFromFile.java
@@ -0,0 +1,22 @@
+package core.basesyntax;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class GetDateFromFile {
+    public String getDate(String fileName) {
+        StringBuilder builder = new StringBuilder();
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
+            String index = bufferedReader.readLine();
+            while (index != null) {
+                builder.append(index).append(" ");
+                index = bufferedReader.readLine();
+            }
+            return builder.toString();
+        } catch (IOException e) {
+            return null;
+        }
+    }
+}
