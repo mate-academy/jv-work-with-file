e4b1b3078c7e996cb9eca2bb329cef6a87c08866
patronus246 <pespatron.original@gmail.com>
2025-05-20 07:39:28 +0300

    Completed simple task

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,88 @@
 package core.basesyntax;
 
-public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
+import javax.swing.text.html.CSS;
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+public class WorkWithFile {
+    private static final String COMMA_DELIMITER = ",";
+    private static final String BUY = "buy";
+    private static final String SUPPLY = "supply";
+    private static final String RESULT = "result";
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        writeToFile(prepareData(calculate(readFromFile(fromFileName))), toFileName);
     }
+
+    public Map<String, List<Integer>> readFromFile(String fileName) {
+        Map<String, List<Integer>> map = new HashMap<>();
+        map.put(SUPPLY, new ArrayList<>());
+        map.put(BUY, new ArrayList<>());
+
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(COMMA_DELIMITER);
+                map.get(values[0]).add(Integer.valueOf(values[1]));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return map;
+    }
+
+    public Map<String, Integer> calculate(Map<String, List<Integer>> map) {
+        Map<String, Integer> result = new HashMap<>();
+        result.put(SUPPLY, map.get(SUPPLY).stream().mapToInt(Integer::intValue).sum());
+        result.put(BUY, map.get(BUY).stream().mapToInt(Integer::intValue).sum());
+        result.put(RESULT, result.get(SUPPLY) - result.get(BUY));
+        return result;
+    }
+
+    public List<String[]> prepareData(Map<String, Integer> map) {
+        List<String[]> data = new ArrayList<>();
+        data.add(new String[]{SUPPLY, String.valueOf(map.get(SUPPLY))});
+        data.add(new String[]{BUY, String.valueOf(map.get(BUY))});
+        data.add(new String[]{RESULT, String.valueOf(map.get(RESULT))});
+        return data;
+    }
+
+    public static void main(String[] args) {
+        WorkWithFile workWithFile = new WorkWithFile();
+        workWithFile.getStatistic("apple.csv", "output.txt");
+    }
+
+    public String convertToCSV(String[] data) {
+        return Stream.of(data)
+                .map(this::escapeSpecialCharacters)
+                .collect(Collectors.joining(","));
+    }
+
+    public void writeToFile(List<String[]> dataLines, String CSV_FILE_NAME) {
+        File csvOutputFile = new File(CSV_FILE_NAME);
+        try (PrintWriter pw = new PrintWriter(csvOutputFile)) {
+            dataLines.stream()
+                    .map(this::convertToCSV)
+                    .forEach(pw::println);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String escapeSpecialCharacters(String data) {
+        if (data == null) {
+            throw new IllegalArgumentException("Input data cannot be null");
+        }
+        String escapedData = data.replaceAll("\\R", " ");
+        if (escapedData.contains(",") || escapedData.contains("\"") || escapedData.contains("'")) {
+            escapedData = escapedData.replace("\"", "\"\"");
+            escapedData = "\"" + escapedData + "\"";
+        }
+        return escapedData;
+    }
+
+
 }
