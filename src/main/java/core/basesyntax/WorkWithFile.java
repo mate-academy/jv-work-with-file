3edd0f38f6194340e333b79c8c7c5d083ec3e5c5
Denis Burlaka <burlakadenis65@gmail.com>
2025-06-08 19:30:50 +0300

    Implemets getStatistic, readFromFile, writeToFile methods

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,53 @@
 package core.basesyntax;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
-
+        String result = readFromFile(fromFileName);
+        writeToFile(toFileName, result);
+    }
+    
+    public static String readFromFile(String fromFileName) {
+        StringBuilder newCv = new StringBuilder();
+        Path path = Paths.get(fromFileName);
+        
+        try {
+            List<String> lines = Files.readAllLines(path);
+            int supluyTotal = 0;
+            int buyTotal = 0;
+            for (String line : lines) {
+                String[] parts = line.split(",");
+                String type = parts[0].trim();
+                int value = Integer.parseInt(parts[1].trim());
+                
+                if (type.equals("buy")) {
+                    buyTotal += value;
+                } else if (type.equals("supply")) {
+                    supluyTotal += value;
+                }
+            }
+            int result = supluyTotal - buyTotal;
+            newCv.append("supply,").append(supluyTotal).append(System.lineSeparator());
+            newCv.append("buy,").append(buyTotal).append(System.lineSeparator());
+            newCv.append("result,").append(result).append(System.lineSeparator());
+            return newCv.toString();
+            
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file: " + fromFileName + "\n", e);
+        }
+    }
+    
+    public static void writeToFile(String toFileName, String content) {
+        Path path = Paths.get(toFileName);
+        try {
+            Files.writeString(path, content);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write file: " + toFileName + "\n", e);
+        }
     }
 }
