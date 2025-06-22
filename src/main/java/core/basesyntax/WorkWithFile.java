7954765c5c66b6e948570020014363b402998ba9
Vova Kondysiuk <vovakondysiuk20@gmail.com>
2025-06-22 19:43:32 +0300

    the logic for the getStatistic method is written

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,45 @@
 package core.basesyntax;
 
+import java.io.*;
+
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
+        StringBuilder builder = new StringBuilder();
+        int totalSupply = 0;
+        int totalBuy = 0;
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
+            String value = reader.readLine();
+            while (value != null) {
+                String[] split = value.split(",");
+                String operation = split[0];
+                int amount = Integer.parseInt(split[1]);
 
+                if (operation.equals("supply")) {
+                    totalSupply += amount;
+                } else if (operation.equals("buy")) {
+                    totalBuy += amount;
+                }
+                value = reader.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
+        int result = totalSupply - totalBuy;
+        builder.append("supply,").append(totalSupply)
+                .append(System.lineSeparator())
+                .append("buy,")
+                .append(totalBuy)
+                .append(System.lineSeparator())
+                .append("result,")
+                .append(result);
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
+            writer.write(builder.toString());
+
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write data to file", e);
+        }
     }
 }
