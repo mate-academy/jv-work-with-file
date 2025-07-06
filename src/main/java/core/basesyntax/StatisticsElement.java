2e917f1211a0f7ff33c73964dbb3bf7b32b03e47
Andrei Filonenko <a1.sankai@gmail.com>
2025-07-06 12:53:28 +0300

    file reading/writing implementation

---
+++ src/main/java/core/basesyntax/StatisticsElement.java
@@ -0,0 +1,31 @@
+package core.basesyntax;
+
+public class StatisticsElement {
+
+    private String name;
+    private int sum;
+
+    public StatisticsElement(String name) {
+        this.name = name;
+        this.sum = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSum() {
+        return sum;
+    }
+
+    public void setSum(int sum) {
+        this.sum = this.sum + sum;
+    }
+
+    @Override
+    public String toString() {
+        return "StatisticsElement{"
+                + "name='" + name + '\''
+                + ", sum=" + sum + '}';
+    }
+}
--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,101 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class WorkWithFile {
+
     public void getStatistic(String fromFileName, String toFileName) {
 
+        String inputString;
+        String csvSplitBy = ",";
+        ArrayList<StatisticsElement> listElement = new ArrayList<>();
+        StatisticsElement elem;
+        //read
+
+        try {
+
+            BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
+
+            while ((inputString = reader.readLine()) != null) {
+
+                String[] strData = inputString.split(csvSplitBy);
+
+                String name = strData[0];
+                String strSum = strData[1];
+
+                int sum = 0;
+                if (!strSum.isEmpty()) {
+                    sum = Integer.parseInt(strSum);
+                }
+
+                elem = getStatElem(name,listElement);
+                if (elem != null && sum != 0) {
+                    elem.setSum(sum);
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("Can`t find file",e);
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t read a line from file",e);
+        }
+
+        //System.out.println("write to file " + listElement.size() + " elements");
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
+
+            StatisticsElement elemSupply;
+            StatisticsElement elemBuy;
+
+            if (!listElement.isEmpty()) {
+
+                if (listElement.get(1).getName().equals("supply")) {
+                    elemSupply = listElement.get(1);
+                    elemBuy = listElement.get(0);
+                } else {
+                    elemSupply = listElement.get(0);
+                    elemBuy = listElement.get(1);
+                }
+
+                writer.write(elemSupply.getName() + ","
+                                 + elemSupply.getSum()
+                                 + System.lineSeparator());
+                writer.write(elemBuy.getName() + ","
+                                 + elemBuy.getSum()
+                                 + System.lineSeparator());
+                writer.write("result," + (elemSupply.getSum() - elemBuy.getSum()));
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
+
+    public StatisticsElement getStatElem(String str,ArrayList<StatisticsElement> listElement) {
+
+        StatisticsElement elem = null;
+
+        for (StatisticsElement elemArr : listElement) {
+            String name = elemArr.getName();
+
+            if (name.equals(str)) {
+                return elemArr;
+            }
+        }
+
+        if (elem == null) {
+            elem = new StatisticsElement(str);
+            listElement.add(elem);
+        }
+
+        return elem;
+    }
+
 }
