189ab0f5298c957660af7bb22d1768ea121885d9
Maksym Maksymenko <Maksimfonmm2@gmail.com>
2025-07-09 17:21:24 +0300

    Remove unused imports from Main.java to fix Checkstyle errors

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,9 +1,5 @@
 package core.basesyntax;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
 public class Main {
     public static void main(String[] args) {
         String inputFileName = "input.csv";
