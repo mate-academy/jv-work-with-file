91ba27cd4473e3724f998a1c8c00195a9783b88d
Iryna Finenko <irynafinenko@gmail.com>
2025-07-13 09:36:51 +0300

    Sopply Calculator

---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        SupplyCalculator calculator = new SupplyCalculator();
+        calculator.getStatistic("input.csv", "report.csv");
+    }
+}
