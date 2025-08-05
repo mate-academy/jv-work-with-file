23755b3f45d70d241411b9fad86f470a9f816285
Piotr Seredynski <sersoft14@gmail.com>
2025-08-05 07:24:08 +0200

    Add ignore empty line

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -17,10 +17,11 @@ public class WorkWithFile {
             int buy = 0;
             while ((line = reader.readLine()) != null) {
                 String[] parts = line.split(",");
-                if (parts[0].equals("supply")) {
+                if (parts[0].equals("supply") || parts.length != 2) {
                     supply += Integer.parseInt(parts[1]);
                 } else if (parts[0].equals("buy")) {
                     buy += Integer.parseInt(parts[1]);
+                } else {
                 }
             }
             builder.append("supply,").append(supply).append(System.lineSeparator());
