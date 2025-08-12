128be6701f07b84d1016b11123a1ae31f49f6edd
AliakseiS97 <aliakseis97@gmail.com>
2025-08-12 21:15:26 +0200

    Work with file

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,42 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
 
+        int supply = 0;
+        int buy = 0;
+
+        try (BufferedReader br = new BufferedReader(new FileReader(fromFileName))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+
+                String[] parts = line.split(",");
+                String type = parts[0].trim();
+                int amount = Integer.parseInt(parts[1].trim());
+
+                if (type.equals("supply")) {
+                    supply += amount;
+                }
+                if (type.equals("buy")) {
+                    buy += amount;
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read from file", e);
+        }
+
+        try (FileWriter writer = new FileWriter(toFileName)) {
+            int result = supply - buy;
+            writer.write("supply," + supply + System.lineSeparator());
+            writer.write("buy," + buy + System.lineSeparator());
+            writer.write("result," + result + System.lineSeparator());
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write from file", e);
+        }
     }
 }
