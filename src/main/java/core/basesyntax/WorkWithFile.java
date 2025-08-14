4a5c4e9e34a456d3136d8cd8358141e78d250817
Andrew <reznichenko.a200212@gmail.com>
2025-08-14 20:10:37 +0300

    My try 2

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,16 +1,17 @@
 package core.basesyntax;
 
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.*;
 
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
 
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
-            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName));
+            try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+                bufferedWriter.write(supplyResult);
+                bufferedWriter.write(buyResult);
+                bufferedWriter.write(resultResult);
+            }
             String value = bufferedReader.readLine();
             int supplySum = 0;
             int buySum = 0;
@@ -30,9 +31,6 @@ public class WorkWithFile {
             String supplyResult = "supply," + supplySum + "\n";
             String buyResult = "buy," + buySum + "\n";
             String resultResult = "result," + result + "\n";
-            bufferedWriter.write(supplyResult);
-            bufferedWriter.write(buyResult);
-            bufferedWriter.write(resultResult);
         } catch (FileNotFoundException e) {
             throw new RuntimeException("Can`t found the file", e);
         } catch (IOException e) {
