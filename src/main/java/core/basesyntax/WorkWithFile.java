fb5e1d5896c4deccf7b8b36cc37380809584b700
Andrew <reznichenko.a200212@gmail.com>
2025-08-17 13:09:08 +0300

    My try 8

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class WorkWithFile {
@@ -12,12 +11,12 @@ public class WorkWithFile {
     private static final String BUY = "buy";
 
     public void getStatistic(String fromFileName, String toFileName) {
+        int supplySum = 0;
+        int buySum = 0;
+        int result = 0;
 
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
             String value = bufferedReader.readLine();
-            int supplySum = 0;
-            int buySum = 0;
-            int result = 0;
             while (value != null) {
                 String[] parts = value.split(",");
                 if (parts.length == 2) {
@@ -31,12 +30,14 @@ public class WorkWithFile {
                 value = bufferedReader.readLine();
             }
             result = supplySum - buySum;
-            try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
-                bufferedWriter.write("supply," + supplySum + "\n");
-                bufferedWriter.write("buy," + buySum + "\n");
-                bufferedWriter.write("result," + result + "\n");
-            }
-        } catch (FileNotFoundException | IOException e) {
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t process with file", e);
+        }
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+            bufferedWriter.write("supply," + supplySum + "\n");
+            bufferedWriter.write("buy," + buySum + "\n");
+            bufferedWriter.write("result," + result + "\n");
+        } catch (IOException e) {
             throw new RuntimeException("Can`t process whith file", e);
         }
     }
