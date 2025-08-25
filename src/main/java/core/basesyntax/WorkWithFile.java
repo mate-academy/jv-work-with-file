ab8c63ccb208d2a0fb140241b8025e1d5229e0e8
Senjars <jaroslawsendecki1@gmail.com>
2025-08-25 23:36:12 +0200

    Solution 3

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,17 +1,25 @@
 package core.basesyntax;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
 public class WorkWithFile {
-    public static void getStatistic(String fromFileName, String toFileName) {
 
+    private static final String SUPPLY_NAME = "supply";
+    private static final String BUY_NAME = "buy";
+    private static final String RESULT_NAME = "result";
+
+    public static void getStatistic(String fromFileName, String toFileName) {
+        int[] values = readFiles(fromFileName);
+        int supply = values[0];
+        int buy = values[1];
+        int result = supply - buy;
+
+        writeToFile(toFileName, supply, buy, result);
+    }
+
+    public static int[] readFiles(String fromFileName) {
         int supply = 0;
         int buy = 0;
-        int result;
         String line;
         String[] lineSplit;
 
@@ -19,24 +27,36 @@ public class WorkWithFile {
             while ((line = reader.readLine()) != null) {
                 lineSplit = line.split(",");
                 int num;
-
-                if (lineSplit[0].equals("supply")) {
-                    num = Integer.parseInt(lineSplit[1]);
-                    supply += num;
-                } else if (lineSplit[0].equals("buy")) {
-                    num = Integer.parseInt(lineSplit[1]);
-                    buy += num;
+                if (lineSplit[0].equals(SUPPLY_NAME)) {
+                    try {
+                        num = Integer.parseInt(lineSplit[1]);
+                        supply += num;
+                    } catch (NumberFormatException e) {
+                        throw new RuntimeException("Invalid number format in line:" + line, e);
+                    }
+                } else if (lineSplit[0].equals(BUY_NAME)) {
+                    try {
+                        num = Integer.parseInt(lineSplit[1]);
+                        buy += num;
+                    } catch (NumberFormatException e) {
+                        throw new RuntimeException("Invalid number format in line:" + line, e);
+                    }
                 }
             }
         } catch (IOException e) {
-            throw new RuntimeException("Can't read from a file.",e);
+            throw new RuntimeException("Can't read from a file.", e);
         }
+        return new int[]{supply,buy};
+    }
 
+    public static void writeToFile(String toFileName, int supply, int buy, int result) {
         try (BufferedWriter write = new BufferedWriter(new FileWriter(toFileName))) {
-            result = supply - buy;
+            StringBuilder builder = new StringBuilder();
+            builder.append(SUPPLY_NAME).append(",").append(supply).append(System.lineSeparator())
+                    .append(BUY_NAME).append(",").append(buy).append(System.lineSeparator())
+                    .append(RESULT_NAME).append(",").append(result);
 
-            write.write("supply," + supply + System.lineSeparator()
-                    + "buy," + buy + System.lineSeparator() + "result," + result);
+            write.write(builder.toString());
         } catch (IOException e) {
             throw new RuntimeException("Can't write to a file.", e);
         }
