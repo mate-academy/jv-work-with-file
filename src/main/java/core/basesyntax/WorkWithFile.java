0d30d32527dd6a99a85f60f451a893049f0ac6c4
Senjars <jaroslawsendecki1@gmail.com>
2025-08-25 20:02:13 +0200

    Solution 2

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -6,7 +6,6 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
-
 public class WorkWithFile {
     public static void getStatistic(String fromFileName, String toFileName) {
 
