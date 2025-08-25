3e635b3a4f15bc4ed1278d78c6e3f0849a5f4fbf
Senjars <jaroslawsendecki1@gmail.com>
2025-08-25 23:39:42 +0200

    Solution 4

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,6 +1,10 @@
 package core.basesyntax;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class WorkWithFile {
 
