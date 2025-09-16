78fd5d179fbcc2537b649a74faa53746deb633a6
Vlad Loran <vladloran17@gmail.com>
2025-09-16 02:36:34 +0100

    Added asked changes

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,29 +7,53 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
+    private static String SUPPLY_LABEL = "supply";
+    private static String BUY_LABEL = "buy";
+    private static String RESULT_LABEL = "result";
+    private static String DELIMITER = ",";
+    private static String NEW_LINE = "\n";
+
+    private int[] readAndAggregate(String fromFileName) {
         int supply = 0;
         int buy = 0;
+        int[] result = new int[2];
         try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
             String line = reader.readLine();
+            String[] key = line.split(",");
             while (line != null) {
-                if (line.contains("supply")) {
-                    supply += Integer.parseInt(line.substring(line.indexOf(",") + 1));
+                key = line.split(",");
+                if (key[0].equals("supply")) {
+                    result[0] += Integer.parseInt(key[1]);
                     line = reader.readLine();
                 } else {
-                    buy += Integer.parseInt(line.substring(line.indexOf(",") + 1));
+                    result[1] += Integer.parseInt(key[1]);
                     line = reader.readLine();
                 }
             }
         } catch (IOException e) {
             throw new RuntimeException("Not able to read a file", e);
         }
+        return result;
+    }
+
+    private String buildReport(int totalSupply, int totalBuy) {
+        String report = "";
+        report += SUPPLY_LABEL + DELIMITER + totalSupply + NEW_LINE;
+        report += BUY_LABEL + DELIMITER + totalBuy + NEW_LINE;
+        report += RESULT_LABEL + DELIMITER + (totalSupply - totalBuy);
+        return report;
+    }
+
+    private void writeToFile(String toFileName, String report) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
-            writer.write("supply," + supply + "\n");
-            writer.write("buy," + buy + "\n");
-            writer.write("result," + (supply - buy));
+            writer.write(report);
         } catch (IOException e) {
             throw new RuntimeException("Not able to write a file", e);
         }
+    }
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        int[] totalResult = readAndAggregate(fromFileName);
+        writeToFile(toFileName, buildReport(totalResult[0], totalResult[1]));
     }
 }
