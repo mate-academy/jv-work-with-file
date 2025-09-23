3f3604543261b3b2032e0469f077cd8adc274734
Maks <maksimronkanen1@gmail.com>
2025-09-24 01:17:31 +0300

    fixes

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,42 +1,39 @@
 package core.basesyntax;
 
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
         int supply = 0;
         int buy = 0;
-        try {
-            BufferedReader br = new BufferedReader(new FileReader(fromFileName));
-            String value = br.readLine();
-            while (value != null) {
+
+        try (BufferedReader br = new BufferedReader(new FileReader(fromFileName))) {
+            String value;
+            while ((value = br.readLine()) != null) {
                 String[] values = value.split(",");
                 if (values[0].equals("supply")) {
                     supply += Integer.parseInt(values[1]);
                 } else if (values[0].equals("buy")) {
                     buy += Integer.parseInt(values[1]);
                 }
-                value = br.readLine();
             }
-            br.close();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException();
         }
-        try {
-            File file = new File(toFileName);
-            BufferedWriter bw = new BufferedWriter(new FileWriter(toFileName));
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(toFileName))) {
             bw.write("supply," + supply);
             bw.newLine();
             bw.write("buy," + buy);
             bw.newLine();
             bw.write("result," + (supply - buy));
-            bw.close();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException();
         }
     }
+
 }
