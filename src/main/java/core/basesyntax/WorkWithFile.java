c921f154524f7ac912e6f0cd01021a424dbddd36
Anton <antonstendal19@gmail.com>
2025-10-30 15:54:47 +0100

    read data from CSV files, created report in new file

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,40 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
+        File fromFile = new File(fromFileName);
+        File toFile = new File(toFileName);
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFile))) {
+            String lines;
 
+            String[] value;
+            int buy = 0;
+            int supply = 0;
+            while ((lines = reader.readLine()) != null) {
+                value = lines.split(",");
+                if (value[0].equals("buy")) {
+                    buy += Integer.parseInt(value[1]);
+                } else if (value[0].equals("supply")) {
+                    supply += Integer.parseInt(value[1]);
+                }
+            }
+            int result = supply - buy;
+            try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFile))) {
+                bufferedWriter.write("supply," + supply);
+                bufferedWriter.newLine();
+                bufferedWriter.write("buy," + buy);
+                bufferedWriter.newLine();
+                bufferedWriter.write("result," + result);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
