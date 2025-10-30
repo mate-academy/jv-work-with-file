3dcf38fe3ee84d869b87f80f42f8579b774bf54f
Anton <antonstendal19@gmail.com>
2025-10-30 16:18:58 +0100

    changed after review

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -9,6 +9,8 @@ import java.io.IOException;
 
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
+        final String operationBuy = "buy";
+        final String operationSupply = "supply";
         File fromFile = new File(fromFileName);
         File toFile = new File(toFileName);
         try (BufferedReader reader = new BufferedReader(new FileReader(fromFile))) {
@@ -19,20 +21,29 @@ public class WorkWithFile {
             int supply = 0;
             while ((lines = reader.readLine()) != null) {
                 value = lines.split(",");
-                if (value[0].equals("buy")) {
+                if (value.length < 2) {
+                    continue;
+                }
+                if (value[0].equals(operationBuy)) {
                     buy += Integer.parseInt(value[1]);
-                } else if (value[0].equals("supply")) {
+                } else if (value[0].equals(operationSupply)) {
                     supply += Integer.parseInt(value[1]);
                 }
             }
             int result = supply - buy;
-            try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFile))) {
-                bufferedWriter.write("supply," + supply);
-                bufferedWriter.newLine();
-                bufferedWriter.write("buy," + buy);
-                bufferedWriter.newLine();
-                bufferedWriter.write("result," + result);
-            }
+            writeReport(toFile, supply, buy, result);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void writeReport(File toFile, int supply, int buy, int result) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFile))) {
+            bufferedWriter.write("supply," + supply);
+            bufferedWriter.newLine();
+            bufferedWriter.write("buy," + buy);
+            bufferedWriter.newLine();
+            bufferedWriter.write("result," + result);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
