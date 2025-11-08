ffd199c21d74731c234b8852139e0d88fbc24340
Przemysław Ciżmiński <cizminski@interia.pl>
2025-11-08 12:03:05 +0100

    Make some changes

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -9,52 +9,69 @@ import java.util.List;
 public class WorkWithFile {
     private static final int INDEX_DATA_0 = 0;
     private static final int INDEX_DATA_1 = 1;
-    private static final String firstWord = "supply";
-    private static final String secondWord = "buy";
+    private static final String SUPPLY_OPERATION = "supply";
+    private static final String BUY_OPERATION = "buy";
+    private static final String RESULT_OPERATION = "result";
 
     public void getStatistic(String fromFileName, String toFileName) {
         int supplyAll = 0;
         int buyAll = 0;
-        int result;
-        Path pathFromFile = Path.of(fromFileName);
-        String[] valuesFromFile;
+        File file = createTargetFile(toFileName);
+
+        for (String valueFromFile : readLines(fromFileName)) {
+            String[] value = valueFromFile.split(",");
+            if (value[INDEX_DATA_0].equals(SUPPLY_OPERATION)) {
+                supplyAll += Integer.parseInt(value[INDEX_DATA_1]);
+            }
+
+            if (value[INDEX_DATA_0].equals(BUY_OPERATION)) {
+                buyAll += Integer.parseInt(value[INDEX_DATA_1]);
+            }
+        }
+        writToFiles(file, buildReport(supplyAll,buyAll));
+    }
+
+    private File createTargetFile(String toFileName) {
         File file = new File(toFileName);
         try {
             file.createNewFile();
         } catch (IOException e) {
             throw new RuntimeException("Can't create a file",e);
         }
+        return file;
+    }
+
+    private String[] readLines(String fromFileName) {
+        String[] valuesFromFile;
+        Path pathFromFile = Path.of(fromFileName);
         try {
             List<String> lines = Files.readAllLines(pathFromFile);
             valuesFromFile = lines.toArray(new String[]{});
         } catch (IOException e) {
             throw new RuntimeException("Can't read from file",e);
         }
-        for (String valueFromFile : valuesFromFile) {
-            String[] value = valueFromFile.split(",");
-            if (value[INDEX_DATA_0].equals(firstWord)) {
-                supplyAll += Integer.parseInt(value[INDEX_DATA_1]);
-            }
+        return valuesFromFile;
+    }
 
-            if (value[INDEX_DATA_0].equals(secondWord)) {
-                buyAll += Integer.parseInt(value[INDEX_DATA_1]);
-            }
-        }
-
+    private String buildReport(int supply, int buy) {
         StringBuilder builder = new StringBuilder();
 
-        builder.append(firstWord).append(",")
-                .append(supplyAll)
+        builder.append(SUPPLY_OPERATION).append(",")
+                .append(supply)
                 .append(System.lineSeparator())
-                .append(secondWord)
+                .append(BUY_OPERATION)
                 .append(",")
-                .append(buyAll)
+                .append(buy)
                 .append(System.lineSeparator())
-                .append("result")
+                .append(RESULT_OPERATION)
                 .append(",")
-                .append(supplyAll - buyAll);
+                .append(supply - buy);
+        return builder.toString();
+    }
+
+    private void writToFiles(File file, String report) {
         try {
-            Files.write(file.toPath(), builder.toString().getBytes());
+            Files.write(file.toPath(), report.getBytes());
         } catch (IOException e) {
             throw new RuntimeException("Can't write to file",e);
         }
