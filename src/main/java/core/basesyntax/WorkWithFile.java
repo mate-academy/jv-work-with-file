851ffac6ae9859f5257cc461af3a2658c3e659a6
Dmytro <sibertdom.wot@gmail.com>
2025-12-02 17:10:45 +0200

    WorkFile3

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -4,9 +4,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap; // H перед M
 import java.util.List;
 import java.util.Map;
-import java.util.HashMap;
 
 public class WorkWithFile {
     private static final String OPERATION_SUPPLY = "supply";
