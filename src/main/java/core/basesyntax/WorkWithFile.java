c3ba0591b72c15b3bc6456f9c377a86ebe73141e
kastashenkova <astashenkova.katya@gmail.com>
2025-12-05 17:56:58 +0200

    fixed style

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,6 +1,10 @@
 package core.basesyntax;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
 
 public class WorkWithFile {
     private static final String LINE_SEPARATOR = System.lineSeparator();
