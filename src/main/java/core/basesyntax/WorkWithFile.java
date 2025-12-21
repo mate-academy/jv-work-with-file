7b6512a089cee70edd90907e7f897e32fffa9c22
MerZakhar <merunovych@gmail.com>
2025-12-21 14:27:38 +0200

    My commit2

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -9,26 +9,43 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    private static final String SUPPLY_OPERATION = "supply";
+    private static final String BUY_OPERATION = "buy";
+    private static final String RESULT_OPERATION = "result";
+
+    private int bought;
+    private int supplied;
+    private int resultNumber;
+
     public void getStatistic(String fromFileName, String toFileName) {
-        File toFile = new File(toFileName);
+        createFile(toFileName);
+
+        readFile(fromFileName);
+
+        writeFile(toFileName);
+    }
+
+    public void createFile(String fileName) {
+        File toFile = new File(fileName);
 
         try {
             toFile.createNewFile();
         } catch (IOException e) {
             throw new RuntimeException("Can't create file", e);
         }
+    }
 
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName));
-                BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+    public void readFile(String fileName) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
             String inputLine = bufferedReader.readLine();
 
-            int bought = 0;
-            int supplied = 0;
+            bought = 0;
+            supplied = 0;
 
             while (inputLine != null) {
                 String[] splitLine = inputLine.split(",");
 
-                if (splitLine[0].equals("supply")) {
+                if (splitLine[0].equals(SUPPLY_OPERATION)) {
                     supplied = Integer.parseInt(splitLine[1]) + supplied;
                 } else {
                     bought = Integer.parseInt(splitLine[1]) + bought;
@@ -37,15 +54,23 @@ public class WorkWithFile {
                 inputLine = bufferedReader.readLine();
             }
 
-            int resultNumber = supplied - bought;
+            resultNumber = supplied - bought;
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("Can't find file", e);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
+    }
 
-            bufferedWriter.write("supply" + "," + supplied);
+    public void writeFile(String fileName) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName))) {
+            bufferedWriter.write(SUPPLY_OPERATION + "," + supplied);
             bufferedWriter.newLine();
 
-            bufferedWriter.write("buy" + "," + bought);
+            bufferedWriter.write(BUY_OPERATION + "," + bought);
             bufferedWriter.newLine();
 
-            bufferedWriter.write("result" + "," + resultNumber);
+            bufferedWriter.write(RESULT_OPERATION + "," + resultNumber);
             bufferedWriter.newLine();
         } catch (FileNotFoundException e) {
             throw new RuntimeException("Can't find file", e);
