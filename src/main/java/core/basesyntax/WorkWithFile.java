236bb294bfe572189ee5af1dfd8552411e128092
Denys Basarab <denya2007basarab@gmail.com>
2025-12-25 13:24:29 +0200

    use StringBuilder for String concatenation and add constant for comma

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -16,6 +16,7 @@ public class WorkWithFile {
     private static final String NAME_BUY = "buy";
     private static final String NAME_RESULT = "result";
     private static final String REGEX = ",";
+    private static final char COMMA = ',';
 
     public void getStatistic(String fromFileName, String toFileName) {
         Map<String, Integer> dataFromFile = countDataFromFile(fromFileName);
@@ -48,9 +49,22 @@ public class WorkWithFile {
     private void writeDataToFile(int howMuchSupply, int howMuchBuy, String toFileName) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
             int result = howMuchSupply - howMuchBuy;
-            writer.write(NAME_SUPPLY + ',' + howMuchSupply + System.lineSeparator());
-            writer.write(NAME_BUY + ',' + howMuchBuy + System.lineSeparator());
-            writer.write(NAME_RESULT + ',' + result + System.lineSeparator());
+            StringBuilder builder = new StringBuilder();
+            builder.append(NAME_SUPPLY)
+                    .append(COMMA)
+                    .append(howMuchSupply)
+                    .append(System.lineSeparator());
+
+            builder.append(NAME_BUY)
+                    .append(COMMA)
+                    .append(howMuchBuy)
+                    .append(System.lineSeparator());
+
+            builder.append(NAME_RESULT)
+                    .append(COMMA)
+                    .append(result)
+                    .append(System.lineSeparator());
+            writer.write(builder.toString());
         } catch (IOException e) {
             throw new RuntimeException("Can't write data to file", e);
         }
