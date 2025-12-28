57847bfa5f8439dd519f1c436531e1e48a6f6e90
iliassohub <ilias.ppthanasiou@gmail.com>
2025-12-28 20:56:34 +0100

    Revert "first try."
    This reverts commit 391d01aae6d89bb073cf19bb8e3fe71d98b6235d.

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,41 +1,7 @@
 package core.basesyntax;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.List;
-
 public class WorkWithFile {
-
     public void getStatistic(String fromFileName, String toFileName) {
-        int supply = 0;
-        int buy = 0;
-        Path pathOfTest = Path.of(fromFileName);
-        try {
-            List<String> readEachLine = Files.readAllLines(pathOfTest);
-            for (String eachLine : readEachLine) {
-                if (eachLine.isBlank()) {
-                    continue;
-                }
-                String[] tokens = eachLine.split(",");
-                if (tokens.length >= 2) {
-                    String trimmedToken0 = tokens[0].trim();
-                    String trimmedToken1 = tokens[1].trim();
-                    if (trimmedToken0.equals("supply")) {
-                        supply += Integer.parseInt(trimmedToken1);
-                    } else if (trimmedToken0.equals("buy")) {
-                        buy += Integer.parseInt(trimmedToken1);
-                    }
-                }
-            }
-            String report = "supply," + supply + System.lineSeparator() + "buy," + buy + System.lineSeparator() + "result," + (supply - buy);
-            BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName));
-            writer.write(report);
-            writer.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+
     }
 }
