eb3296cd3e067e5f49694ae40d1966a29790eae0
Oleksandr <alex.yurkin4@gmail.com>
2026-01-14 20:17:57 +0100

    created three methods first one readTheFile second one is calculations third one iswriteIntoFile and that's it

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,67 @@
 package core.basesyntax;
 
-public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 
+public class WorkWithFile {
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        String givenText = readTheFile(fromFileName);
+        String result = calculations(givenText);
+        writeIntoFile(result, toFileName);
+    }
+
+    private String readTheFile(String fromFileName) {
+        String fileName = new File(fromFileName).getName();
+        StringBuilder stringBuilder = new StringBuilder();
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName));) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuilder.append(line).append(System.lineSeparator());
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Can't read data from the file " + fileName, e);
+        }
+        return stringBuilder.toString();
+    }
+
+    private String calculations(String givenText) {
+        String ls = System.lineSeparator();
+        int supply = 0;
+        int buy = 0;
+        StringBuilder stringBuilder = new StringBuilder();
+
+        String[] lines = givenText.split("\\R");
+        for (String line : lines) {
+            String[] oneLine = line.split(",");
+            int number = Integer.parseInt(oneLine[1]);
+            if (oneLine[0].equals("supply")) {
+                supply += number;
+            } else if (oneLine[0].equals("buy")) {
+                buy += number;
+            }
+        }
+
+        int result = supply - buy;
+
+        stringBuilder.append("supply,").append(supply).append(ls)
+                .append("buy,").append(buy).append(ls)
+                .append("result,").append(result);
+
+        return stringBuilder.toString();
+    }
+
+    private void writeIntoFile(String result, String toFileName) {
+        String fileName = new File(toFileName).getName();
+
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName));) {
+            bufferedWriter.write(result);
+        } catch (Exception e) {
+            throw new RuntimeException("Can't write data to the file " + fileName, e);
+        }
     }
 }
