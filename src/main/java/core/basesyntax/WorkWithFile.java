fa1d5c29ac64ae75406b5d9ccfadc3baa3badb18
Krzysiek <krzysztof.orawski@wp.pl>
2026-02-07 17:46:02 +0100

    asd

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,36 +1,36 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
+import java.io.IOException;
 
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
         int supply = 0;
         int buy = 0;
-        List<String> result = new ArrayList<>();
-        File file = new File(fromFileName);
-        try (Scanner scanner = new Scanner(file)) {
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
                 String[] parts = line.split(",");
                 if ("supply".equals(parts[0].trim())) {
-                    int liczba = Integer.parseInt(parts[1]);
-                    supply += liczba;
+                    supply += Integer.parseInt(parts[1].trim());
                 } else if ("buy".equals(parts[0].trim())) {
-                    int liczba = Integer.parseInt(parts[1]);
-                    buy += liczba;
+                    buy += Integer.parseInt(parts[1].trim());
                 }
             }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
         }
+
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
             writer.write("supply," + supply + System.lineSeparator());
             writer.write("buy," + buy + System.lineSeparator());
             writer.write("result," + (supply - buy));
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write file", e);
         }
     }
 }
