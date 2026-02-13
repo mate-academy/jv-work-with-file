50009261eb262d4d6b7077d62472ca8561422c09
Krzysiek <krzysztof.orawski@wp.pl>
2026-02-13 12:50:09 +0100

    correct method

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -9,11 +9,38 @@ import java.io.IOException;
 public class WorkWithFile {
     private static final String SUPPLY = "supply";
     private static final String BUY = "buy";
+    private static final String RESULT = "result";
     private static final String SEPARATOR = ",";
 
     public void getStatistic(String fromFileName, String toFileName) {
-        int supply = 0;
-        int buy = 0;
+        Statistic result = readStatistic(fromFileName);
+        String report = buildReport(result);
+        writeReport(toFileName, report);
+    }
+
+    private void writeReport(String toFileName, String report) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
+            writer.write(report);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write file " + toFileName, e);
+        }
+    }
+
+    private String buildReport(Statistic result) {
+        return SUPPLY
+                + SEPARATOR
+                + result.supply()
+                + System.lineSeparator()
+                + BUY
+                + SEPARATOR + result.buy()
+                + System.lineSeparator()
+                + RESULT
+                + SEPARATOR
+                + result.getResult();
+    }
+
+    private static Statistic readStatistic(String fromFileName) {
+        Statistic result = new Statistic(0,0);
         try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
             String line;
             while ((line = reader.readLine()) != null) {
@@ -21,23 +48,34 @@ public class WorkWithFile {
                     continue;
                 }
                 String[] parts = line.split(SEPARATOR);
+
+                int amount = Integer.parseInt(parts[1].trim());
+
                 if (SUPPLY.equals(parts[0].trim())) {
-                    supply += Integer.parseInt(parts[1].trim());
+                    result = result.addSupply(amount);
                 } else if (BUY.equals(parts[0].trim())) {
-                    buy += Integer.parseInt(parts[1].trim());
+                    result = result.addBuy(amount);
+
                 }
             }
 
         } catch (IOException e) {
             throw new RuntimeException("Can't read file " + fromFileName, e);
         }
+        return result;
+    }
 
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
-            writer.write(SUPPLY + SEPARATOR + supply + System.lineSeparator());
-            writer.write(BUY + SEPARATOR + buy + System.lineSeparator());
-            writer.write("result" + SEPARATOR + (supply - buy));
-        } catch (IOException e) {
-            throw new RuntimeException("Can't write file " + toFileName, e);
+    private record Statistic(int supply, int buy) {
+        public Statistic addSupply(int amount) {
+            return new Statistic(supply + amount, buy);
+        }
+
+        public Statistic addBuy(int amount) {
+            return new Statistic(supply, buy + amount);
+        }
+
+        public int getResult() {
+            return supply - buy;
         }
     }
 }
