c5c66f62dc08a34f6e4b7c49a2daafdd398b6265
Yaroslav Petruk <yarroslawjob@gmail.com>
2026-02-24 17:23:56 +0100

    fix

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -4,31 +4,48 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 import java.util.List;
-
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
+        List<String> lines = readLines(fromFileName);
+        int[] result = getResult(lines);
+        String report = buildReport(result);
+        writeReport(toFileName, report);
+    }
+    private List<String> readLines(String fromFileName) {
         List<String> lines;
         try {
             lines = Files.readAllLines(Path.of(fromFileName));
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Can't read data from file: " + fromFileName, e);
         }
+        return lines;
+    }
+    private int[] getResult(List<String> lines) {
+        int[] result = new int[2];
         int supply = 0;
         int buy = 0;
         for (String el : lines) {
             String[] words = el.split(",");
             if (words[0].equals("supply")) {
                 supply += Integer.parseInt(words[1]);
-            }
-            else if (words[0].equals("buy")) {
+            } else if (words[0].equals("buy")) {
                 buy += Integer.parseInt(words[1]);
             }
         }
-        int result = supply - buy;
-        String report =
-                "supply," + supply + System.lineSeparator()
-                        + "buy," + buy + System.lineSeparator()
-                        + "result," + result;
+        result[0] = supply;
+        result[1] = buy;
+        return result;
+    }
+    private String buildReport(int[] result) {
+        int supply = result[0];
+        int buy = result[1];
+        int total = supply - buy;
+
+        return "supply," + supply + System.lineSeparator()
+                + "buy," + buy + System.lineSeparator()
+                + "result," + total;
+    }
+    private void writeReport(String toFileName, String report) {
         try {
             Files.writeString(
                     Path.of(toFileName),
@@ -37,7 +54,7 @@ public class WorkWithFile {
                     StandardOpenOption.TRUNCATE_EXISTING
             );
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Can't write data to file: " + toFileName, e);
         }
     }
 }
