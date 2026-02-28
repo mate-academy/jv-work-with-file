14bc2e3a463705182ca50b6bd7c3b1d4ac1f6923
ivann <ivan.netishin@gmail.com>
2026-02-28 12:45:48 +0100

    solved task

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,40 @@
 package core.basesyntax;
 
-public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 
+public class WorkWithFile {
+    public static final int DATA_OPERATION = 0;
+    public static final int DATA_AMOUNT = 1;
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        File file = new File(fromFileName);
+        try {
+            List<String> strings = Files.readAllLines(file.toPath());
+
+            int buyAmount = 0;
+            int supplyAmount = 0;
+
+            for (String str : strings) {
+                String[] parts = str.split(",");
+                String operation = parts[DATA_OPERATION];
+                int amount = Integer.parseInt(parts[DATA_AMOUNT]);
+                if (operation.equals("buy")) {
+                    buyAmount += amount;
+                } else if (operation.equals("supply")) {
+                    supplyAmount += amount;
+                }
+            }
+            String result = "supply," + supplyAmount + System.lineSeparator()
+                    + "buy," + buyAmount + System.lineSeparator() + "result,"
+                    + (supplyAmount - buyAmount) + System.lineSeparator();
+
+            Files.writeString(Path.of(toFileName), result);
+        } catch (IOException e) {
+            throw new RuntimeException("Cant read file", e);
+        }
     }
 }
