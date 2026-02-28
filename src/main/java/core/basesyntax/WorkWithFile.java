219cdcfbf4e9594e33f60a1bed352b77eb02fda7
ivann <ivan.netishin@gmail.com>
2026-02-28 17:27:44 +0100

    divided into three methods

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,26 +1,37 @@
 package core.basesyntax;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 
 public class WorkWithFile {
-    public static final int DATA_OPERATION = 0;
-    public static final int DATA_AMOUNT = 1;
+    private static final int DATA_OPERATION = 0;
+    private static final int DATA_AMOUNT = 1;
 
-    public void getStatistic(String fromFileName, String toFileName) {
-        File file = new File(fromFileName);
-        List<String> strings;
+    private List<String> readFile(String fromFileName) {
         try {
-            strings = Files.readAllLines(file.toPath());
+            return Files.readAllLines(Path.of(fromFileName));
         } catch (IOException e) {
             throw new RuntimeException("Cant read file", e);
         }
+    }
+
+    private void writeFile(String toFileName, int supplyAmount, int buyAmount) {
+        try {
+            Files.writeString(Path.of(toFileName), "supply,"
+                    + supplyAmount + System.lineSeparator()
+                    + "buy," + buyAmount + System.lineSeparator() + "result,"
+                    + (supplyAmount - buyAmount) + System.lineSeparator());
+        } catch (IOException e) {
+            throw new RuntimeException("Cannot write file: ", e);
+        }
+    }
+
+    public void getStatistic(String fromFileName, String toFileName) {
         int buyAmount = 0;
         int supplyAmount = 0;
-
+        List<String> strings = readFile(fromFileName);
         for (String str : strings) {
             String[] parts = str.split(",");
             String operation = parts[DATA_OPERATION];
@@ -31,13 +42,6 @@ public class WorkWithFile {
                 supplyAmount += amount;
             }
         }
-        String result = "supply," + supplyAmount + System.lineSeparator()
-                + "buy," + buyAmount + System.lineSeparator() + "result,"
-                + (supplyAmount - buyAmount) + System.lineSeparator();
-        try {
-            Files.writeString(Path.of(toFileName), result);
-        } catch (IOException e) {
-            throw new RuntimeException("Cannot write file: ", e);
-        }
+        writeFile(toFileName, supplyAmount, buyAmount);
     }
 }
