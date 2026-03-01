2b735e8e130698f454ae4186afd28809a799b4ce
Eugeniusz Dzikowicz <eugeniusz.dzikowicz@gmail.com>
2026-03-01 10:17:59 +0100

    Implemented getStatistic method after review 2

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,22 +8,26 @@ import java.io.IOException;
 
 public class WorkWithFile {
 
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+    private static final String RESULT = "result";
+
     public void getStatistic(String fromFileName, String toFileName) {
         int[] calc = calculateValue(fromFileName);
         String report = getReport(calc);
         writeToFile(report, toFileName);
     }
 
-    public int[] calculateValue(String fromFileName) {
+    private int[] calculateValue(String fromFileName) {
         try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
             int supply = 0;
             int buy = 0;
             String line = reader.readLine();
             while (line != null) {
                 String[] lineArray = line.split(",");
-                if (lineArray[0].equals("supply")) {
+                if (lineArray[0].equals(SUPPLY)) {
                     supply += Integer.parseInt(lineArray[1]);
-                } else if (lineArray[0].equals("buy")) {
+                } else if (lineArray[0].equals(BUY)) {
                     buy += Integer.parseInt(lineArray[1]);
                 }
                 line = reader.readLine();
@@ -35,16 +39,16 @@ public class WorkWithFile {
         }
     }
 
-    public String getReport(int[] calculated) {
+    private String getReport(int[] calculated) {
         int supply = calculated[0];
         int buy = calculated[1];
-        String report = "supply," + supply + System.lineSeparator()
-                + "buy," + buy + System.lineSeparator()
-                + "result," + (supply - buy) + System.lineSeparator();
+        String report = SUPPLY + "," + supply + System.lineSeparator()
+                + BUY + "," + buy + System.lineSeparator()
+                + RESULT + "," + (supply - buy) + System.lineSeparator();
         return report;
     }
 
-    public void writeToFile(String report, String toFileName) {
+    private void writeToFile(String report, String toFileName) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
             writer.write(report);
         } catch (IOException e) {
