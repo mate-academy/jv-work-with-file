9afec71f6426e564d9fa3893ca6497e3911d88d5
Brynkiewicz <michal.brynkiewicz@envistaco.com>
2026-03-02 21:09:05 +0100

    WorkWithFile - code redesigned adding try catch

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -6,51 +6,56 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class WorkWithFile {
-    public String[] getStatistic(String fromFileName, String toFileName) throws IOException {
+    public String[] getStatistic(String fromFileName, String toFileName) throws Exception {
         if (fromFileName == null || toFileName == null) {
             throw new NullPointerException("No file to read and/or no file to write");
         }
 
-        String[] lines = Files.readAllLines(Path.of(fromFileName)).toArray(new String[0]);
-
-        if (lines.length == 0) {
-            throw new IllegalArgumentException("File is empty");
-        }
-
-        int buyAmount = 0;
-        int supplyAmount = 0;
+        String[] lines = null;
         StringBuilder result = new StringBuilder();
-        final String ls = System.lineSeparator();
 
-        for (int i = 0; i < lines.length; i++) {
-            String toSplit = lines[i];
-            String[] splittedLine = toSplit.split(",");
-            String operationType = splittedLine[0];
-            String amountStr = splittedLine[1];
-            int amount = Integer.parseInt(amountStr);
+        try {
+            lines = Files.readAllLines(Path.of(fromFileName)).toArray(new String[0]);
+            if (lines.length == 0) ;
 
-            if (operationType.equals("buy")) {
-                buyAmount += amount;
-            } else if (operationType.equals("supply")) {
-                supplyAmount += amount;
+            int buyAmount = 0;
+            int supplyAmount = 0;
+            final String ls = System.lineSeparator();
+
+            for (int i = 0; i < lines.length; i++) {
+                String toSplit = lines[i];
+                String[] splittedLine = toSplit.split(",");
+                String operationType = splittedLine[0];
+                String amountStr = splittedLine[1];
+                int amount = Integer.parseInt(amountStr);
+
+                if (operationType.equals("buy")) {
+                    buyAmount += amount;
+                } else if (operationType.equals("supply")) {
+                    supplyAmount += amount;
+                }
             }
-        }
 
-        int difference = supplyAmount - buyAmount;
-        result.append("supply,")
-                .append(supplyAmount)
-                .append(ls)
-                .append("buy,")
-                .append(buyAmount)
-                .append(ls)
-                .append("result,")
-                .append(difference)
-                .append(ls);
+                int difference = supplyAmount - buyAmount;
+                result.append("supply,")
+                        .append(supplyAmount)
+                        .append(ls)
+                        .append("buy,")
+                        .append(buyAmount)
+                        .append(ls)
+                        .append("result,")
+                        .append(difference)
+                        .append(ls);
+            } catch (Exception e) {
+                throw new NullPointerException("File is empty");
+            }
 
         try (BufferedWriter writer = Files.newBufferedWriter(Path.of(toFileName))) {
             writer.write(result.toString());
+        } catch (Exception e) {
+            throw new NullPointerException();
         }
-        return new String[]{result.toString()};
+    return new String[]{result.toString()};
     }
 }
 
