4a56fde5df848797c47c07b5e2cdc9638571c987
Brynkiewicz <michal.brynkiewicz@envistaco.com>
2026-03-02 22:08:43 +0100

    exception catch

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,30 +1,28 @@
 package core.basesyntax;
 
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class WorkWithFile {
-    public String[] getStatistic(String fromFileName, String toFileName) throws Exception {
+    public String[] getStatistic(String fromFileName, String toFileName) {
         if (fromFileName == null || toFileName == null) {
             throw new NullPointerException("No file to read and/or no file to write");
         }
 
         StringBuilder result = new StringBuilder();
+        final String ls = System.lineSeparator();
 
         try {
             String[] lines = Files.readAllLines(Path.of(fromFileName)).toArray(new String[0]);
-
             int buyAmount = 0;
             int supplyAmount = 0;
-            final String ls = System.lineSeparator();
 
             for (int i = 0; i < lines.length; i++) {
-                String toSplit = lines[i];
-                String[] splittedLine = toSplit.split(",");
+                String[] splittedLine = lines[i].split(",");
                 String operationType = splittedLine[0];
-                String amountStr = splittedLine[1];
-                int amount = Integer.parseInt(amountStr);
+                int amount = Integer.parseInt(splittedLine[1]);
 
                 if (operationType.equals("buy")) {
                     buyAmount += amount;
@@ -43,8 +41,10 @@ public class WorkWithFile {
                     .append("result,")
                     .append(difference)
                     .append(ls);
-        } catch (Exception e) {
-            throw new NullPointerException("File is empty");
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read input file: " + fromFileName, e);
+        } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+            throw new RuntimeException("Bad input format in file: " + fromFileName, e);
         }
 
         try (BufferedWriter writer = Files.newBufferedWriter(Path.of(toFileName))) {
