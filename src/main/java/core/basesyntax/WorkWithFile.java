fb05e81d89528385c2b3def5c43749f0508b5a19
Krupkolllia <krupkoilla397@gmail.com>
2026-03-02 16:50:17 +0100

    Implement getStatistics method

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,58 @@
 package core.basesyntax;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class WorkWithFile {
+    public static final int OPERATION_TYPE_INDEX = 0;
+    public static final int OPERATION_VALUE_INDEX = 1;
+    public static final String SUPPLY_OPERATION_NAME = "supply";
+    public static final String BUY_OPERATION_NAME = "buy";
+
     public void getStatistic(String fromFileName, String toFileName) {
+        String[] statistics;
+        try {
+            statistics = readStatisticsFromFile(fromFileName);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        int supplyAmount = 0;
+        int buyAmount = 0;
+
+        for (String operation : statistics) {
+            String operationType = operation.split(",")[OPERATION_TYPE_INDEX];
+            int operationValue = Integer.parseInt(operation.split(",")[OPERATION_VALUE_INDEX]);
+
+            if (operationType.equals(SUPPLY_OPERATION_NAME)) {
+                supplyAmount += operationValue;
+            } else if (operationType.equals(BUY_OPERATION_NAME)) {
+                buyAmount += operationValue;
+            }
+        }
+
+        StringBuilder report = new StringBuilder();
+        report.append(SUPPLY_OPERATION_NAME).append(",").append(supplyAmount)
+                .append(System.lineSeparator());
+        report.append(BUY_OPERATION_NAME).append(",").append(buyAmount)
+                .append(System.lineSeparator());
+        report.append("result,").append(supplyAmount - buyAmount);
+
+        try {
+            writeReportToFile(toFileName, report.toString());
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write report to file", e);
+        }
 
     }
+
+    private String[] readStatisticsFromFile(String fileName) throws IOException {
+        return Files.readAllLines(Path.of(fileName)).toArray(new String[0]);
+    }
+
+    private void writeReportToFile(String fileName, String report) throws IOException {
+        Files.writeString(Path.of(fileName), report);
+    }
+
 }
