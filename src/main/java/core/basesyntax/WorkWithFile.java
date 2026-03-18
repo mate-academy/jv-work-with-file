5523d3c0cee04e165221ae461ef262fc22f1ced8
Ivan Lydin <ivanlydin2008@gmail.com>
2026-03-18 15:16:53 +0200

    task competed magic string debug

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,15 +8,17 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
-    private static final int onceElemArray = 0;
-    private static final int twiceElemArray = 1;
-    private static int supply = 0;
-    private static int buy = 0;
-    private static int result = 0;
+    private static final int OPERATION_TYPE_INDEX = 0;
+    private static final int AMOUNT_INDEX = 1;
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
 
     public void getStatistic(String fromFileName, String toFileName) {
         File file = new File(fromFileName);
         String[] content;
+        int supply = 0;
+        int buy = 0;
+        int result = 0;
 
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
             supply = 0;
@@ -25,23 +27,23 @@ public class WorkWithFile {
             String line;
             while ((line = bufferedReader.readLine()) != null) {
                 content = line.split(",");
-                String operation = content[onceElemArray].trim();
-                int amount = Integer.parseInt(content[twiceElemArray]);
+                String operation = content[OPERATION_TYPE_INDEX].trim();
+                int amount = Integer.parseInt(content[AMOUNT_INDEX]);
 
-                if (operation.equals("supply")) {
+                if (operation.equals(SUPPLY)) {
                     supply += amount;
-                } else if (operation.equals("buy")) {
+                } else if (operation.equals(BUY)) {
                     buy += amount;
                 }
             }
         } catch (IOException e) {
             throw new RuntimeException("File is not found", e);
         }
-        writer(toFileName);
+        writer(toFileName, result, supply, buy);
 
     }
 
-    public void writer(String toFileName) {
+    private void writer(String toFileName, int result, int supply, int buy) {
         result = supply - buy;
 
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
