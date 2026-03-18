e3c7872290bdc9fe5e78e79c4e70d650b44d0be0
Ivan Lydin <ivanlydin2008@gmail.com>
2026-03-18 15:03:29 +0200

    task competed logic restructed

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -10,18 +10,22 @@ import java.io.IOException;
 public class WorkWithFile {
     private static final int onceElemArray = 0;
     private static final int twiceElemArray = 1;
+    private static int supply = 0;
+    private static int buy = 0;
+    private static int result = 0;
 
     public void getStatistic(String fromFileName, String toFileName) {
         File file = new File(fromFileName);
         String[] content;
-        int supply = 0;
-        int buy = 0;
-        int result = 0;
+
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+            supply = 0;
+            buy = 0;
+            result = 0;
             String line;
             while ((line = bufferedReader.readLine()) != null) {
                 content = line.split(",");
-                String operation = content[onceElemArray];
+                String operation = content[onceElemArray].trim();
                 int amount = Integer.parseInt(content[twiceElemArray]);
 
                 if (operation.equals("supply")) {
@@ -33,6 +37,11 @@ public class WorkWithFile {
         } catch (IOException e) {
             throw new RuntimeException("File is not found", e);
         }
+        writer(toFileName);
+
+    }
+
+    public void writer(String toFileName) {
         result = supply - buy;
 
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
@@ -42,7 +51,7 @@ public class WorkWithFile {
             bufferedWriter.newLine();
             bufferedWriter.write("result," + result);
         } catch (IOException e) {
-            throw new RuntimeException("File is not found", e);
+            throw new RuntimeException("Error writing to file", e);
         }
     }
 }
