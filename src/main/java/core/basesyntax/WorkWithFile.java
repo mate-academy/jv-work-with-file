a1473e105943a03cfff2d271e27208f92a9ff561
VadimN637 <turnedofff@gmail.com>
2026-03-28 00:28:32 +0200

    1

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,41 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
 public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
+    public void getStatistic(String fromFileName, String toFileName) throws IOException {
+        int supplyInt = 0;
+        int buyInt = 0;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
+            String value = "";
+
+            while ((value = reader.readLine()) != null) {
+                String firstString = value.split(",")[0];
+                int secondInt = Integer.parseInt(value.split(",")[1]);
+                if (firstString.equals("supply")) {
+                    supplyInt += secondInt;
+                } else {
+                    buyInt += secondInt;
+                }
+            }
+            int resultInt = supplyInt - buyInt;
+            String supply = "supply," + supplyInt;
+            String buy = "buy," + buyInt;
+            String result = "result," + resultInt;
+            String[] list = { supply, buy, result };
+            for (String sentence : list) {
+                Files.write(Paths.get(toFileName),
+                        (sentence + "\n").getBytes(),
+                        StandardOpenOption.CREATE,
+                        StandardOpenOption.APPEND);
+            }
+        }
 
     }
 }
