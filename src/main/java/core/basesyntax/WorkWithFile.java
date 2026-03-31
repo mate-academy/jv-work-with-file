54d7c5b89dc35694c218ce2c904f767189399b1d
MartaRusanovska <kotykmarta@gmail.com>
2026-03-31 21:25:56 +0200

    fixing solution for jv-work-with-file

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,6 +8,10 @@ import java.io.IOException;
 
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
+        final String supplying = "supply";
+        final String buying = "buy";
+        final String result = "result";
+        String report;
         int supply = 0;
         int buy = 0;
         try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
@@ -17,25 +21,28 @@ public class WorkWithFile {
                 String[] parts = value.split(",");
                 String typeOfOperation = parts[0];
                 int sum = Integer.parseInt(parts[1]);
-                if (typeOfOperation.equals("supply")) {
+                if (typeOfOperation.equals(supplying)) {
                     supply += sum;
                 } else {
                     buy += sum;
                 }
             }
-            stringBuilder.append("supply,").append(supply).append(System.lineSeparator())
-                    .append("buy,").append(buy)
-                    .append(System.lineSeparator()).append("result,").append(supply - buy);
-            String report = stringBuilder.toString();
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
-                try {
-                    writer.write(report);
-                } catch (IOException e) {
-                    throw new RuntimeException("Can't write data to file", e);
-                }
+            stringBuilder.append(supplying).append(",").append(supply)
+                    .append(System.lineSeparator())
+                    .append(buying).append(",").append(buy)
+                    .append(System.lineSeparator()).append(result).append(",").append(supply - buy);
+            report = stringBuilder.toString();
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
+            try {
+                writer.write(report);
+            } catch (IOException e) {
+                throw new RuntimeException("Can't write data to file", e);
             }
         } catch (IOException e) {
-            throw new RuntimeException("Can't read file",e);
+            throw new RuntimeException("Can't write data to file", e);
         }
     }
 }
