434bd6a5319109e03ea6955c9ee98741ee4a968f
Vitalii Smuraha <smuraga.v@gmail.com>
2026-05-02 23:29:26 +0200

    finaly-edition

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,42 +8,64 @@ import java.nio.file.Path;
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
 
-        try {
-            String text = Files.readString(Path.of(fromFileName));
-
-            String[] lines = text.split("\n");
-
-            int moneyBuy = 0;
-            int moneySupply = 0;
-            int result;
-
-            for (String string : lines) {
-                string = string.trim();
-
-                if (string.startsWith("buy,")) {
-
-                    moneyBuy += Integer.parseInt(string.substring(4));
-
-                } else if (string.startsWith("supply,")) {
-
-                    moneySupply += Integer.parseInt(string.substring(7));
-
-                }
-
-            }
-
-            result = moneySupply - moneyBuy;
-
-            try (FileWriter writer = new FileWriter(toFileName)) {
-                writer.write("supply," + moneySupply + System.lineSeparator());
-                writer.write("buy," + moneyBuy + System.lineSeparator());
-                writer.write("result," + result + System.lineSeparator());
-            }
-
-        } catch (IOException e) {
-
-            throw new RuntimeException(e);
-        }
+        int[] values = getValuesAndResult(readFile(fromFileName));
+        writeToFile(toFileName, values);
 
     }
+
+    private String readFile(String nameOfTheFile) {
+
+        try {
+            String text = Files.readString(Path.of(nameOfTheFile));
+            return text;
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read data from the file " + nameOfTheFile, e);
+        }
+    }
+
+    private int[] getValuesAndResult(String text) {
+
+        String[] lines = text.split("\n");
+
+        int buyValues = 0;
+        int supplyValues = 0;
+        int result;
+
+        for (String string : lines) {
+
+            String[] typeAndValues = string.split(",");
+
+            typeAndValues[0].trim();
+
+            if (typeAndValues[0].equals("buy")) {
+
+                buyValues += Integer.parseInt(typeAndValues[1].trim());
+
+            }
+
+            if (typeAndValues[0].equals("supply")) {
+
+                supplyValues += Integer.parseInt(typeAndValues[1].trim());
+
+            }
+
+        }
+        result = supplyValues - buyValues;
+        return new int[] { supplyValues, buyValues, result };
+
+    }
+
+    private void writeToFile(String fileName, int[] values) {
+
+        try {
+            FileWriter writer = new FileWriter(fileName);
+            writer.write("supply," + values[0] + System.lineSeparator());
+            writer.write("buy," + values[1] + System.lineSeparator());
+            writer.write("result," + values[2] + System.lineSeparator());
+            writer.close();
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t create the file.", e);
+        }
+    }
+
 }
