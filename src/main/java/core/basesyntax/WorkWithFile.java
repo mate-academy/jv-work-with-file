d0ae26cba719bd26c803438f2e78e0ad3c9ec2f9
3xacT <vovaoliynyk99@gmail.com>
2026-06-25 20:50:49 +0300

    fixed some issues

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,19 +8,19 @@ import java.nio.file.Path;
 import java.util.List;
 
 public class WorkWithFile {
-    private static final String supplyString = "supply";
-    private static final String buyString = "buy";
-    private static final String commaString = ",";
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+    private static final String COMMA = ",";
     private StringBuilder stringBuilder = new StringBuilder();
 
-    void processLine(String line, int[] sums) {
-        String[] parts = line.split(commaString);
+    private void processLine(String line, int[] sums) {
+        String[] parts = line.split(COMMA);
         String type = parts[0];
         int amount = Integer.parseInt(parts[1]);
-        if (type.equals(supplyString)) {
+        if (type.equals(SUPPLY)) {
             sums[0] += amount;
         }
-        if (type.equals(buyString)) {
+        if (type.equals(BUY)) {
             sums[1] += amount;
         }
 
@@ -28,15 +28,15 @@ public class WorkWithFile {
 
     private void writeResult(String fileName, int supplySum, int buySum) {
         int result = supplySum - buySum;
-
+        stringBuilder.setLength(0);
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
 
-            writer.write(supplyString + commaString + supplySum);
-            stringBuilder.append(supplyString + commaString + supplySum);
+            writer.write(SUPPLY + COMMA + supplySum);
+            stringBuilder.append(SUPPLY + COMMA + supplySum);
             writer.newLine();
 
-            writer.write(buyString + commaString + buySum);
-            stringBuilder.append(buyString + commaString + buySum);
+            writer.write(BUY + COMMA + buySum);
+            stringBuilder.append(BUY + COMMA + buySum);
             writer.newLine();
 
             writer.write("result," + result);
@@ -44,7 +44,7 @@ public class WorkWithFile {
             writer.newLine();
 
         } catch (IOException e) {
-            throw new RuntimeException("Error while writing file: " + e.getMessage());
+            throw new RuntimeException("Error while writing file: " + fileName, e);
         }
     }
 
@@ -55,7 +55,7 @@ public class WorkWithFile {
         try {
             allLines = Files.readAllLines(Path.of(fromFileName));
         } catch (IOException e) {
-            throw new RuntimeException("Cannot read file", e);
+            throw new RuntimeException("Cannot read file" + fromFileName, e);
         }
         for (String line : allLines) {
             processLine(line, countersSum);
