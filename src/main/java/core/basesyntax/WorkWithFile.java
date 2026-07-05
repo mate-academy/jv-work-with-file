f712cc9107e79d75cd5c1a76ea3568bb52849af6
Denys <werdumbox@gmail.com>
2026-07-05 19:47:43 +0200

    wrote the solution

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,36 @@
 package core.basesyntax;
 
-public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
+import java.io.*;
 
+public class WorkWithFile {
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+    private static final String RESULT = "result";
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName));
+             BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName)))
+        {
+            String line;
+            int supplySummary = 0;
+            int buySummary = 0;
+
+            while ((line = reader.readLine()) != null) {
+                String[] data = line.split(",");
+                if (SUPPLY.equals(data[0])) {
+                    supplySummary += Integer.parseInt(data[1]);
+                } else if (BUY.equals(data[0])) {
+                    buySummary += Integer.parseInt(data[1]);
+                }
+            }
+            writer.write(SUPPLY + "," + supplySummary);
+            writer.newLine();
+            writer.write(BUY + "," + buySummary);
+            writer.newLine();
+            writer.write(RESULT + "," + (supplySummary - buySummary));
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
