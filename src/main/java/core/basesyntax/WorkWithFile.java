d2279817ecc09aaf1b4c21b668aaf0ccc548b27b
Liza Brnk <liza.brnk@gmail.com>
2026-08-01 18:15:45 +0300

    Implemented solution to getStatistic(String fromFileName, String toFileName) method

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,35 @@
 package core.basesyntax;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
+        try {
+            String content = Files.readString(Path.of(fromFileName));
+            String[] lines = content.split("\\R");
+            int supply = 0;
+            int buy = 0;
+            for (String line : lines) {
+                String[] parts = line.split(",");
+                String operation = parts[0];
+                int amount = Integer.parseInt(parts[1]);
 
+                if (operation.equals("supply")) {
+                    supply += amount;
+                } else {
+                    buy += amount;
+                }
+            }
+
+            String data = "supply," + supply + System.lineSeparator()
+                    + "buy," + buy + System.lineSeparator()
+                    + "result," + (supply - buy);
+
+            Files.writeString(Path.of(toFileName), data);
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t read a file!", e);
+        }
     }
 }
