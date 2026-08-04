2a1e106b53919a37e1cb981fb2eb0a8aff0119ee
Artem <artem.kozakevych@gmail.com>
2026-08-04 18:29:22 +0300

    Release WorkWithFile class and getStatistic method

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,38 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
-
+        int supplyAll = 0;
+        int buyAll = 0;
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
+            String value = reader.readLine();
+            while (value != null && !value.isEmpty()) {
+                String[] line = value.split(",");
+                switch (line[0]) {
+                    case ("supply"):
+                        supplyAll += Integer.parseInt(line[1]);
+                        break;
+                    case ("buy"):
+                        buyAll += Integer.parseInt(line[1]);
+                        break;
+                }
+                value = reader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t read file", e);
+        }
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
+            writer.write("supply," + supplyAll + System.lineSeparator());
+            writer.write("buy," + buyAll + System.lineSeparator());
+            writer.write("result," + (supplyAll - buyAll) + System.lineSeparator());
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t write to file", e);
+        }
     }
 }
