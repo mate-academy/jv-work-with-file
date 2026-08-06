c4535eaae3f18d84345b98b2e36588d3244fea97
ivanmedvedievedu <ivanmedvedievedu@gmail.com>
2026-08-06 18:17:33 +0200

    Implement file reading, processing, and writing

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,62 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.IOException;
+
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
+        String statistics = readFile(fromFileName);
+        String report = statisticCalculation(statistics);
+        writeToFile(report, toFileName);
+    }
 
+    private String readFile(String fromFileName) {
+        File file = new File(fromFileName);
+        StringBuilder stringBuilder = new StringBuilder();
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+            String oneLine = bufferedReader.readLine();
+            while (oneLine != null) {
+                stringBuilder.append(oneLine).append(System.lineSeparator());
+                oneLine = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read the file", e);
+        }
+
+        return stringBuilder.toString();
+    }
+
+    private String statisticCalculation(String statiscics) {
+        String[] separateStatisticsLines = statiscics.split(System.lineSeparator());
+
+        int supplySum = 0;
+        int buySum = 0;
+        for (String separateStatisticsLine : separateStatisticsLines) {
+            String[] categoryAndValue = separateStatisticsLine.split(",");
+            if (categoryAndValue[0].equals("supply")) {
+                supplySum += Integer.parseInt(categoryAndValue[1]);
+            } else {
+                buySum += Integer.parseInt(categoryAndValue[1]);
+            }
+        }
+
+        return "supply" + "," + supplySum + System.lineSeparator()
+                + "buy" + "," + buySum + System.lineSeparator()
+                + "result" + "," + (supplySum - buySum);
+    }
+
+    private void writeToFile(String statistics, String toFileName) {
+        File file = new File(toFileName);
+
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
+            bufferedWriter.write(statistics);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write to the file", e);
+        }
     }
 }
