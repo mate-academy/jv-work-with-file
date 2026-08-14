40387a150994631688c3bb94330672ca50538ff3
vadym <evansfalter@gmail.com>
2026-08-14 17:48:17 +0200

    fix: split code into methods

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,33 +7,49 @@ import java.nio.file.Path;
 import java.util.List;
 
 public class WorkWithFile {
+    private static final String BUY = "buy";
+    private static final String SUPPLY = "supply";
+
+    private static final String BUY_RESULT = "buy,";
+    private static final String SUPPLIES_RESULT = "supply,";
+    private static final String RESULT = "result,";
+
     public void getStatistic(String fromFileName, String toFileName) {
         Path path = Path.of(fromFileName);
         List<String> lines;
+        try {
+            lines = Files.readAllLines(path);
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+        String result = calculateStat(lines);
+        writeToFile(result ,toFileName);
+    }
+
+    private String calculateStat(List<String> lines) {
         int supply = 0;
         int buy = 0;
         int resultOfBuy = 0;
-        try {
-            lines = Files.readAllLines(path);
-            StringBuilder stringBuilder = new StringBuilder();
-            for (String line : lines) {
-                stringBuilder.append(line).append(" ");
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String line : lines) {
+            stringBuilder.append(line).append(" ");
+        }
+        String[] result = stringBuilder.toString().split("\\s+");
+        for (String line : result) {
+            if (line.contains(SUPPLY)) {
+                supply += Integer.parseInt(line.split(",")[1]);
             }
-            String[] result = stringBuilder.toString().split("\\s+");
-            for (String line : result) {
-                if (line.contains("supply")) {
-                    supply += Integer.parseInt(line.split(",")[1]);
-                }
-                if (line.contains("buy")) {
-                    buy += Integer.parseInt(line.split(",")[1]);
-                }
+            if (line.contains(BUY)) {
+                buy += Integer.parseInt(line.split(",")[1]);
             }
-            resultOfBuy = supply - buy;
-            String resultOut = "supply," + supply + "\nbuy," + buy + "\nresult," + resultOfBuy;
-            FileWriter fileWriter = new FileWriter(toFileName);
-            fileWriter.write(resultOut);
-            fileWriter.close();
+        }
+        resultOfBuy = supply - buy;
+        return SUPPLIES_RESULT + supply + "\n" + BUY_RESULT + buy + "\n" + RESULT + resultOfBuy;
+    }
 
+    private void writeToFile(String resource, String toFileName) {
+        try (FileWriter fileWriter = new FileWriter(toFileName)) {
+            fileWriter.write(resource);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
