bfa73be3b72159f2001d1d7f06119dbff36f2d87
goffa-exe <n4v4rm0re666@gmail.com>
2026-08-17 22:11:48 +0200

    add getStatistic implementation

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,62 @@
 package core.basesyntax;
 
-public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class WorkWithFile {
+    private static final String CSV_SEPARATOR = ",";
+    private static final String OPERATION_BUY = "buy";
+    private static final String OPERATION_SUPPLY = "supply";
+    private static final String RESULT = "result";
+    private static final int OPERATION_POSITION = 0;
+    private static final int COUNT_POSITION = 1;
+    
+    public void getStatistic(String fromFileName, String toFileName) {
+        File inputFile = new File(fromFileName);
+        File outputFile = new File(toFileName);
+        int supplyCount = 0;
+        int buyCount = 0;
+        
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(inputFile));
+                BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(outputFile));
+        ) {
+            String fileLine = bufferedReader.readLine();
+            
+            while (fileLine != null) {
+                int operationCount = getOperationCount(fileLine);
+                
+                if (getOperation(fileLine).equals(OPERATION_BUY)) {
+                    buyCount += operationCount;
+                } else {
+                    supplyCount += operationCount;
+                }
+                fileLine = bufferedReader.readLine();
+            }
+            
+            bufferedWriter.write(OPERATION_SUPPLY + CSV_SEPARATOR
+                    + supplyCount + System.lineSeparator()
+                    + OPERATION_BUY + CSV_SEPARATOR + buyCount + System.lineSeparator()
+                    + RESULT + CSV_SEPARATOR + (supplyCount - buyCount)
+            );
+        } catch (IOException e) {
+            throw new RuntimeException("File wasn't found or couldn't be opened", e);
+        }
+    }
+    
+    private String getOperation(String line) {
+        String[] operationAndCount = line.split(CSV_SEPARATOR);
+        return operationAndCount[OPERATION_POSITION].equals(OPERATION_BUY)
+                ? OPERATION_BUY
+                : OPERATION_SUPPLY;
+    }
+    
+    public int getOperationCount(String line) {
+        String[] operationAndCount = line.split(CSV_SEPARATOR);
+        
+        return Integer.parseInt(operationAndCount[COUNT_POSITION]);
     }
 }
