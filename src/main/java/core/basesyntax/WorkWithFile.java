9cb5dbc903b7e20f4d35e42c6b44e70a8653f138
Stepan Liakhovskiy <s.lyakhovskiy06@gmail.com>
2026-09-08 15:31:57 +0200

    Task done

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,47 @@
 package core.basesyntax;
 
+import java.io.*;
+
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
+        File file = new File(fromFileName);
+        File file1 = new File(toFileName);
 
+        if (file.length() == 0) {
+            System.out.println("File is empty");
+            return;
+        }
+
+        int amountSupply = 0;
+        int amountBuy = 0;
+        int result;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            reader.readLine();
+
+            String value;
+            while ((value = reader.readLine()) != null) {
+                String[] parts = value.split(",");
+                if (parts[0].equals("supply")) {
+                    amountSupply += Integer.parseInt(parts[1]);
+                } else if (parts[0].equals("buy")) {
+                    amountBuy += Integer.parseInt(parts[1]);
+                }
+            }
+            result = amountSupply - amountBuy;
+
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
+
+        String report = "supply," + amountSupply + "\n"
+                + "buy," + amountBuy + "\n"
+                + "result," + result;
+
+        try (FileWriter writer = new FileWriter(file1)) {
+            writer.write(report);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write file", e);
+        }
     }
 }
